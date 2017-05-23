ae8b4e6719079ab253509c358be81989a67089b9
Mark Fickett <mwf@google.com>
2017-05-23 17:46:50 -0400

    Add routes to heroes list and dashboard which also uses HeroesService.

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -4,7 +4,11 @@ import { Component } from '@angular/core';
   selector: 'my-app',
   template: `
     <h1>{{title}}</h1>
-    <my-heroes></my-heroes>
+    <nav>
+      <a routerLink="/dashboard">Dashboard</a>
+      <a routerLink="/heroes">Heroes</a>
+    </nav>
+    <router-outlet></router-outlet>
   `
 })
 
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,19 +1,37 @@
-import { NgModule }            from '@angular/core';
 import { BrowserModule }       from '@angular/platform-browser';
 import { FormsModule }         from '@angular/forms';
+import { NgModule }            from '@angular/core';
+import { RouterModule }        from '@angular/router'
+
+import { AppComponent }        from './app.component';
+import { DashboardComponent }  from './dashboard.component'
 import { HeroDetailComponent } from './hero-detail.component'
 import { HeroService }         from './hero.service'
 import { HeroesComponent }     from './heroes.component'
 
-import { AppComponent }  from './app.component';
-
 @NgModule({
   imports:      [
     BrowserModule,
-    FormsModule
+    FormsModule,
+    RouterModule.forRoot([
+      {
+        path: '',
+        redirectTo: '/dashboard',
+        pathMatch: 'full'
+      },
+      {
+        path: 'heroes',
+        component: HeroesComponent
+      },
+      {
+        path: 'dashboard',
+        component: DashboardComponent
+      }
+    ])
   ],
   declarations: [
     AppComponent,
+    DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
   ],
