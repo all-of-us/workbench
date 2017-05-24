ca9d6458952c3f3829eeaa4acc7e066bb226f64b
Mark Fickett <mwf@google.com>
2017-05-24 15:25:51 -0400

    Add searching with Observables.

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -14,6 +14,7 @@ import { DashboardComponent }  from './dashboard.component'
 import { HeroDetailComponent } from './hero-detail.component'
 import { HeroService }         from './hero.service'
 import { HeroesComponent }     from './heroes.component'
+import { HeroSearchComponent } from './hero-search.component'
 
 @NgModule({
   imports:      [
@@ -28,6 +29,7 @@ import { HeroesComponent }     from './heroes.component'
     DashboardComponent,
     HeroDetailComponent,
     HeroesComponent,
+    HeroSearchComponent
   ],
   providers: [ HeroService ],
   bootstrap: [ AppComponent ]
