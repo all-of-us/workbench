6b3f8ed22aedff93cf2a2b892aeed67115153e0d
Mark Fickett <mwf@google.com>
2017-05-24 14:45:16 -0400

    Add saving changes.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -8,6 +8,7 @@ import { Hero } from './hero';
 @Injectable()
 export class HeroService {
   private heroesUrl = 'api/heroes';  // URL to (fake in-memory) web API
+  private headers = new Headers({'Content-Type': 'application/json'});
 
   constructor(private http: Http) { }
 
@@ -29,6 +30,15 @@ export class HeroService {
     return this.http.get(url)
       .toPromise()
       .then(response => response.json().data as Hero)
+      .catch(this.handleError);
+  }
+
+  update(hero: Hero): Promise<Hero> {
+    const url = `${this.heroesUrl}/${hero.id}`;
+    return this.http
+      .put(url, JSON.stringify(hero), {headers: this.headers})
+      .toPromise()
+      .then(() => hero)
       .catch(this.handleError);
   }
 }
