d33b1bfdab5a6872211dafb26d36108a9d40076d
Mark Fickett <mwf@google.com>
2017-05-24 14:52:31 -0400

    Add adding.

--- src/app/hero.service.ts
+++ src/app/hero.service.ts
@@ -41,4 +41,12 @@ export class HeroService {
       .then(() => hero)
       .catch(this.handleError);
   }
+
+  create(name: string): Promise<Hero> {
+    return this.http
+      .post(this.heroesUrl, JSON.stringify({name: name}), {headers: this.headers})
+      .toPromise()
+      .then(res => res.json().data as Hero)
+      .catch(this.handleError);
+  }
 }
--- src/app/heroes.component.ts
+++ src/app/heroes.component.ts
@@ -33,4 +33,14 @@ export class HeroesComponent implements OnInit {
   goToDetail(): void {
     this.route.navigate(['/detail', this.selectedHero.id]);
   }
+
+  add(name: string): void {
+    name = name.trim();
+    if (!name) { return; }
+    this.heroService.create(name)
+      .then(hero => {
+        this.heroes.push(hero);
+        this.selectedHero = null;
+      });
+  }
 }
