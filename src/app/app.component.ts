fe32f767b67735b8015b8eca7abfe4962411d9f5
David Mohs <dgit@mohsinc.com>
2017-06-14 17:03:07 -0400

    Add logos and favicon (#2)
    * Add logos and favicon
    
    * Fragile location selectors option
    
    * Classes for everything
    
    * Decision: prefer inline styles when few-enough

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -7,8 +7,13 @@ import {Component} from '@angular/core';
   selector: 'aou-app',
   styleUrls: ['./app.component.css'],
   template: `
-    <h1>{{title}}</h1>
-    <nav>
+    <div>
+      <img class="main-logo" src="images/all-of-us-logo.svg" alt="All of Us"
+        style="width: 18rem;">
+      <img class="portal-logo" src="images/portal-logo.svg" alt="Researcher Portal"
+        style="margin-left: 0.5rem; width: 13rem;">
+    </div>
+    <nav style="margin-top: 1rem;">
       <a routerLink="/login" routerLinkActive="active">Switch User</a>
       <a routerLink="/repository" routerLinkActive="active">Select CDR</a>
     </nav>
@@ -20,6 +25,4 @@ import {Component} from '@angular/core';
   `
 })
 
-export class AppComponent {
-  title = 'All of Us Researcher Portal Prototype';
-}
+export class AppComponent {}
