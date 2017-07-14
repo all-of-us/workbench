f5c389c68578397476fbbefab0c51256c353b6a3
Mark Fickett <mark.fickett@gmail.com>
2017-07-14 13:08:15 -0400

    Switch from skip_* to target=.

--- tools/deploy.py
+++ tools/deploy.py
@@ -16,32 +16,43 @@ import gcloud_auth
 def deploy(args):
   gcloud_auth.do_auth(args)
 
-  get_confirmation(
-      'Deploy to %r%s%s?' %
-      (args.project, ' (skip API)' if args.skip_api else '', ' (skip UI)' if args.skip_ui else ''))
+  targets = (
+      _TargetChoices.ALL_TARGETS if args.target == _TargetChoices.ALL
+      else (args.target,))
+  get_confirmation('Deploy to %r (%s)?' % (args.project, ', '.join(targets)))
 
-  if args.skip_api:
-    logging.info('Skipping deploy of API.')
-  else:
+  if _TargetChoices.API in targets:
     logging.info('Deploying API')
     subprocess.check_call(
         ['./gradlew', ':appengineDeploy'], cwd=paths.get_api_dir())
-
-  if args.skip_ui:
-    logging.info('Skipping deploy of UI.')
   else:
+    logging.info('Skipping deploy of API.')
+
+  if _TargetChoices.UI in targets:
     logging.info('Building UI using %r', paths.get_ng())
     subprocess.check_call([paths.get_ng(), 'build'], cwd=paths.get_ui_dir())
     logging.info('Deploying UI')
     subprocess.check_call(['gcloud', 'app', 'deploy'], cwd=paths.get_ui_dir())
+  else:
+    logging.info('Skipping deploy of UI.')
 
   logging.info('Deploy complete.')
+
+
+class _TargetChoices(object):
+  ALL = 'all'
+  API = 'api'
+  UI = 'ui'
+  ALL_TARGETS = [API, UI]
+  ALL_TARGET_CHOICES = [ALL] + ALL_TARGETS
 
 
 if __name__ == '__main__':
   configure_logging()
   parser = get_parser()
-  parser.add_argument('--skip_api', action='store_true')
-  parser.add_argument('--skip_ui', action='store_true')
+  parser.add_argument(
+      '-t', '--target',
+      default=_TargetChoices.ALL, choices=_TargetChoices.ALL_TARGET_CHOICES,
+      help='Which part of the Workbench to deploy.')
   args = gcloud_auth.add_parse_and_check_args(parser)
   deploy(args)
