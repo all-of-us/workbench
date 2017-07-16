98c7f1e3a810ab12ad9b2c88e9f177934a04c3c2
Josh Mandel <jmandel@gmail.com>
2017-07-16 15:29:37 -0400

    Add codegen in angular

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -6,3 +6,6 @@ echo Installing the Angular command-line tool, to serve/build the UI.
 sudo npm install -g @angular/cli
 echo Installing UI project dependencies.
 npm install
+
+cd ../tools
+wget https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar -O swagger-codegen-cli.jar
