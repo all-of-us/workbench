bd6ed78bc61f906b2e5e65196ec647bb4e8bccfb
danqrodney <danrodney@google.com>
2017-07-17 09:51:07 -0500

    Adding Swagger-generated cohorts API. (#17)
    * removing project files, setting up user authentication
    
    * Adding Swagger-generated cohorts API.
    
    * Addressing PR comments

--- api/src/main/java/org/pmiops/workbench/api/Constants.java
+++
@@ -1,7 +0,0 @@
-package org.pmiops.workbench.api;
-
-public interface Constants {
-
-  static final String V1_API_PREFIX = "/v1";
-
-}
---
+++ api/src/main/java/org/pmiops/workbench/auth/UserAuthentication.java
@@ -0,0 +1,52 @@
+package org.pmiops.workbench.auth;
+
+import com.google.api.services.oauth2.model.Userinfoplus;
+import com.google.common.collect.ImmutableList;
+import java.util.Collection;
+import org.springframework.security.core.Authentication;
+import org.springframework.security.core.GrantedAuthority;
+
+public class UserAuthentication implements Authentication {
+
+  private final Userinfoplus userInfo;
+  private final String bearerToken;
+
+  public UserAuthentication(Userinfoplus userInfo, String bearerToken) {
+    this.userInfo = userInfo;
+    this.bearerToken = bearerToken;
+  }
+
+  @Override
+  public String getName() {
+    return userInfo.getEmail();
+  }
+
+  @Override
+  public Collection<? extends GrantedAuthority> getAuthorities() {
+    return ImmutableList.of();
+  }
+
+  @Override
+  public Object getCredentials() {
+    return bearerToken;
+  }
+
+  @Override
+  public Object getDetails() {
+    return null;
+  }
+
+  @Override
+  public Object getPrincipal() {
+    return userInfo;
+  }
+
+  @Override
+  public boolean isAuthenticated() {
+    return true;
+  }
+
+  @Override
+  public void setAuthenticated(boolean isAuthenticated) throws IllegalArgumentException {
+  }
+}
