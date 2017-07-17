59c38700b2c78eeac353741b1559a4230c932e55
Josh Mandel <jmandel@gmail.com>
2017-07-16 20:40:10 -0400

    Use latest codegen and generate security headers

--- tools/setup_env.sh
+++ tools/setup_env.sh
@@ -8,4 +8,4 @@ echo Installing UI project dependencies.
 npm install
 
 cd ../tools
-wget https://repo1.maven.org/maven2/io/swagger/swagger-codegen-cli/2.2.3/swagger-codegen-cli-2.2.3.jar -O swagger-codegen-cli.jar
+wget https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-2.3.0-20170716.142514-29.jar -O swagger-codegen-cli.jar
