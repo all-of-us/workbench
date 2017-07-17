171e3dc7bf4f13dd1965f7427d88b7229e3c5a27
Josh Mandel <jmandel@alum.mit.edu>
2017-07-17 15:34:02 -0400

    Jmandel/working signing and services (#21)
    * Checkpoint
    
    * SignIn service using BehaviorSubject observable
    
    * Add codegen in angular
    
    * Use latest codegen and generate security headers
    
    * Use behavior subject to expose current state
    
    Use behavior subject to expose current state
    
    * Access tokens used in Api requests
    
    * Remove refs to aou-service
    
    * fix tests
    
    * Comments on workbench.yaml security properties
    
    * Code review fixes

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -5,15 +5,17 @@ import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
 
-import {AllOfUsService} from 'app/services/all-of-us.service';
 import {AppRoutingModule} from 'app/app-routing.module';
 import {AppComponent} from 'app/views/app/component';
 import {CohortBuilderComponent} from 'app/views/cohort-builder/component';
 import {LoginComponent} from 'app/views/login/component';
 import {RepositoryService} from 'app/services/repository.service';
 import {SelectRepositoryComponent} from 'app/views/select-repository/component';
+import {SignInService} from 'app/services/sign-in.service';
 import {UserService} from 'app/services/user.service';
 import {VAADIN_CLIENT} from 'app/vaadin-client';
+import {CohortsService, Configuration, ConfigurationParameters} from 'generated';
+import {environment} from 'environments/environment';
 
 export function getVaadin(): VaadinNs {
   // If the Vaadin javascript file fails to load, the "vaadin" symbol doesn't get defined,
@@ -23,6 +25,13 @@ export function getVaadin(): VaadinNs {
   } else {
     return vaadin;
   }
+}
+
+export function getConfiguration(signInService: SignInService): Configuration {
+    return new Configuration({
+      basePath: environment.allOfUsApiUrl,
+      accessToken: () => signInService.currentAccessToken
+    });
 }
 
 @NgModule({
@@ -36,13 +45,19 @@ export function getVaadin(): VaadinNs {
     AppComponent,
     LoginComponent,
     SelectRepositoryComponent,
-    CohortBuilderComponent
+    CohortBuilderComponent,
   ],
   providers: [
-    AllOfUsService,
     UserService,
     RepositoryService,
-    {provide: VAADIN_CLIENT, useFactory: getVaadin}
+    SignInService,
+    {provide: VAADIN_CLIENT, useFactory: getVaadin},
+    {
+      provide: Configuration,
+      deps: [SignInService],
+      useFactory: getConfiguration
+    },
+    CohortsService
   ],
 
   // This specifies the top-level component, to load first.
--- ui/src/app/views/app/component.ts
+++ ui/src/app/views/app/component.ts
@@ -1,11 +1,15 @@
 // UI Component framing the overall app (title and nav).
 // Content is in other Components.
 
-import {Component, OnInit} from '@angular/core';
-import {Router, NavigationEnd, ActivatedRoute} from '@angular/router';
-import {Title} from '@angular/platform-browser';
-
 import {environment} from 'environments/environment';
+import {ChangeDetectorRef, Component, OnInit} from '@angular/core';
+import {Router, NavigationEnd, ActivatedRoute} from '@angular/router';
+import {SignInService, SignInDetails} from 'app/services/sign-in.service';
+import {Title} from '@angular/platform-browser';
+import {Observable} from 'rxjs/Observable';
+import {CohortsService, Configuration, ConfigurationParameters} from 'generated';
+
+declare const gapi: any;
 
 @Component({
   selector: 'app-aou',
@@ -14,16 +18,20 @@ import {environment} from 'environments/environment';
 })
 export class AppComponent implements OnInit {
   private baseTitle: string;
+  user: Observable<SignInDetails>;
 
   constructor(
       private titleService: Title,
       private activatedRoute: ActivatedRoute,
-      private router: Router
+      private router: Router,
+      private signInService: SignInService,
+      private cohortsService: CohortsService
   ) {}
 
   ngOnInit(): void {
     // Pick up the global site title from HTML, and (for non-prod) add a tag
     // naming the current environment.
+
     this.baseTitle = this.titleService.getTitle();
     if (environment.displayTag) {
       this.baseTitle = `[${environment.displayTag}] ${this.baseTitle}`;
@@ -44,5 +52,14 @@ export class AppComponent implements OnInit {
         }
       }
     });
+    this.user = this.signInService.user;
+  }
+
+  signIn(e: Event): void {
+    this.signInService.signIn();
+  }
+
+  signOut(e: Event): void {
+    this.signInService.signOut();
   }
 }
