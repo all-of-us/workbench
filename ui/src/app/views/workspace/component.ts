1f152771dca5965e881bb95d9cfb0845a29e5bee
Brian Rubenstein <brubenst@wma78-22d.broadinstitute.org>
2017-07-18 13:48:05 -0400

    Changed notebook paradigm and pulled cohorts from server

--- ui/src/app/views/workspace/component.ts
+++ ui/src/app/views/workspace/component.ts
@@ -1,41 +1,14 @@
 import {Component, OnInit} from '@angular/core';
 import {Router} from '@angular/router';
 
+import {CohortsService} from 'generated';
 import {Repository} from 'app/models/repository';
 import {RepositoryService} from 'app/services/repository.service';
 import {User} from 'app/models/user';
 import {UserService} from 'app/services/user.service';
-// TODO: Pull cohort definition from #21
-class Cohort {
-  id: string;
-  name: string;
-  description: string;
-  url: string;
-  date: Date;
-  notebook: string;
-  constructor(id: string, name: string,
-              description: string, url: string, date: Date,
-              notebook: string) {
-    this.id = id;
-    this.name = name;
-    this.description = description;
-    this.url = url;
-    this.date = date;
-    this.notebook = notebook;
-  }
-}
-// TODO: Remove hardcoded cohorts
-const cohort1 = new Cohort('cohort1', 'Cohort 1',
-                          'Male, 40-50, Various ethnic groups, MA', 'URL to the cohort',
-                          new Date('July 1, 2017'), 'Notebook name');
-const cohort2 = new Cohort('cohort2', 'Cohort 2',
-                          'Male, 10-50, Various ethnic groups, MA', 'URL to the cohort',
-                          new Date('July 1, 2017'), 'Notebook name');
-const cohort3 = new Cohort('cohort3', 'Cohort 3',
-                            'Male, 10-50, Various ethnic groups, MA', 'URL to the cohort',
-                            new Date('July 1, 2017'), 'Notebook name');
 
-const cohortList = [cohort1, cohort2];
+
+const cohortList = [];
 
 
 @Component({
@@ -50,12 +23,19 @@ export class WorkspaceComponent implements OnInit {
   constructor(
       private router: Router,
       private userService: UserService,
-      private repositoryService: RepositoryService
+      private repositoryService: RepositoryService,
+      private cohortsService: CohortsService
   ) {}
 
   ngOnInit(): void {
     this.userService.getLoggedInUser()
         .then(user => this.user = user);
+    this.cohortsService.getCohortsInWorkspace("123", "123").subscribe(
+          cohortsReceived => {
+            for(let cohortIn of cohortsReceived){
+              cohortList.push(cohortIn);
+            }
+          });
   }
   // TODO: edit/add cohort link - blrubenstein
   goToCohort(id: number): void {
