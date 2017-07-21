74a08acca44c353365b40b4cc2aabf6cbc05223b
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-07-21 12:52:08 -0500

    Removing application.resources, moving to appengine-web.xml

--- api/src/main/java/org/pmiops/workbench/Application.java
+++ api/src/main/java/org/pmiops/workbench/Application.java
@@ -5,7 +5,6 @@ import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.ComponentScan;
 
 @SpringBootApplication
-@ComponentScan
 public class Application {
 
   public static void main(String[] args) {
