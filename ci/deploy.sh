f3d83fa573862c1acf0717d43091fa5bb41b2ecb
Josh Mandel <jmandel@alum.mit.edu>
2017-07-21 11:31:54 -0400

    Use our own build image (#37)
    * Use our own build image
    
    Add node to path
    
    Use our own docker image to build
    
    * Update build tag

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,13 +1,8 @@
 #!/bin/bash -e
 
-./ci/install_gcloud.sh && source ~/.bashrc
 ./ci/activate_creds.sh ~/gcloud-credentials.key
-
-gcloud components install app-engine-java
-
 ./tools/deploy.py \
   --target $1 \
   --skip-confirmation \
   --project all-of-us-workbench-test \
   --account circle-deploy-account@all-of-us-workbench-test.iam.gserviceaccount.com
-
