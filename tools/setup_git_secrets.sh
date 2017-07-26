f41125bb81238f1734fdba73ee24011627376df9
Brian Rubenstein <brubenst@wma78-22d.broadinstitute.org>
2017-07-26 13:56:02 -0400

    Git secrets setup script.

---
+++ tools/setup_git_secrets.sh
@@ -0,0 +1,12 @@
+#!/bin/bash -e
+
+#The git secrets command will default to placing it in the .git hooks,
+# we need to provide it a different directory to make them in and then
+# move them to the proper directory.
+cd ../hooks
+git secrets --install .
+mv hooks/commit-msg .
+mv hooks/pre-commit .
+mv hooks/prepare-commit-msg .
+rm -rf hooks
+cd ../tools
