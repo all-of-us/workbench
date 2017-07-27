8f103c301dfd8b74bdb213e4dc39eb835b4bdc11
danqrodney <danrodney@google.com>
2017-07-27 15:30:03 -0400

    Removing external ID from cohort DB representation. (#54)
    Changing ID used for cohorts in external API to be the numeric database ID, assigned by the server on creation.

---
+++ api/src/main/java/org/pmiops/workbench/exceptions/BadRequestException.java
@@ -0,0 +1,16 @@
+package org.pmiops.workbench.exceptions;
+
+import org.springframework.http.HttpStatus;
+import org.springframework.web.bind.annotation.ResponseStatus;
+
+@ResponseStatus(HttpStatus.BAD_REQUEST)
+public class BadRequestException extends RuntimeException {
+
+  public BadRequestException() {
+    super();
+  }
+
+  public BadRequestException(String message) {
+    super(message);
+  }
+}
