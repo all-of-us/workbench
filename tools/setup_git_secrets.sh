89cf18d5546d241ba546a50498ec7579cb43d470
Brian Rubenstein <brubenst@wma78-22d.broadinstitute.org>
2017-07-31 14:34:03 -0400

    Hide error if no secrets currently exist.

--- tools/setup_git_secrets.sh
+++ tools/setup_git_secrets.sh
@@ -3,10 +3,12 @@
 #The git secrets command will default to placing it in the .git hooks,
 # we need to provide it a different directory to make them in and then
 # move them to the proper directory.
-cd ../hooks
-git config --remove-section secrets
+{
+  git config --remove-section secrets 2>/dev/null
+} || {
+  :
+}
 git secrets --add 'private_key'
 git secrets --add --allowed --literal "'private_key'"
 git secrets --add 'private_key_id'
 git secrets --add --allowed --literal "'private_key_id'"
-cd ../tools
