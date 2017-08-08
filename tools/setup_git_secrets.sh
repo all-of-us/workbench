874f884522b451fa068524984f6e16f99c4226dc
Brian Rubenstein <brubenst@wma78-22d.broadinstitute.org>
2017-08-08 16:02:54 -0400

    Removed secrets management and assumed onetime run.

--- tools/setup_git_secrets.sh
+++ tools/setup_git_secrets.sh
@@ -4,11 +4,11 @@
 # we need to provide it a different directory to make them in and then
 # move them to the proper directory.
 {
-  git --git-dir ../.git --work-tree .. config --remove-section secrets 2>/dev/null
+  git config --remove-section secrets 2>/dev/null
 } || {
   :
 }
-git --git-dir ../.git --work-tree .. secrets --add 'private_key'
-git --git-dir ../.git --work-tree .. secrets --add --allowed --literal "'private_key'"
-git --git-dir ../.git --work-tree .. secrets --add 'private_key_id'
-git --git-dir ../.git --work-tree .. secrets --add --allowed --literal "'private_key_id'"
+git secrets --add 'private_key'
+git secrets --add 'private_key_id'
+git secrets --add --allowed --literal "git secrets --add 'private_key'"
+git secrets --add --allowed --literal "git secrets --add 'private_key_id'"
