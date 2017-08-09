f8ef6935a9111eb05651d0ecdfb8201547515d42
David Mohs <dgit@mohsinc.com>
2017-08-09 15:43:16 -0400

    Initialize database

--- api/.project/devstart.rb
+++ api/.project/devstart.rb
@@ -4,14 +4,38 @@ def startdb()
   c = Common.new
   env = c.load_env
   cname = "#{env.namespace}-db"
+
+  db_volume_exists = false
+  unless c.capture_stdout(%W{docker volume ls --quiet --filter name=#{cname}}).chomp == cname
+    db_volume_exists = true
+  end
+
   c.run_inline %W{docker run --name #{cname}
     -d
     --network #{env.namespace}
     -e MYSQL_ROOT_PASSWORD=root -e MYSQL_USER=workbench -e MYSQL_PASSWORD=workbench!pwd
     -e MYSQL_DATABASE=workbench
+    -v #{cname}:/var/lib/mysql
     library/mysql
   }
   at_exit { c.run_inline %W{docker rm -f #{cname}} }
+
+  if db_volume_exists
+    c.status "Waiting a few seconds for the database to initialize itself..."
+    sleep 15 # experimentally-derived
+  end
+
+  c.status "Running database migrations..."
+  c.run_inline %W{
+    docker run
+      --rm
+      -v gradle-cache:/root/.gradle
+      --network #{env.namespace}
+      -w /w/db
+      -v #{ENV["PWD"]}:/w
+      #{env.namespace}-devserver
+      ../gradlew --no-daemon update
+  }
 end
 
 def serve()
