af6a28aa7925cdbbc072751fe41f42083ff8df84
David Mohs <dgit@mohsinc.com>
2017-08-11 11:41:29 -0400

    Fix and clean up migration scripts

--- api/db/setup_local_vars.sh
+++
@@ -1,4 +0,0 @@
-source setup_vars.sh
-export WORKBENCH_PASSWORD=${WORKBENCH_PASSWORD:=workbench!pwd}
-export DB_SERVER=${DB_SERVER:=aouwb-api-db:3306}
-export DB_CONNECTION_STRING=${DB_CONNECTION_STRING:=jdbc:mysql://${DB_SERVER}/${DB_NAME}}
