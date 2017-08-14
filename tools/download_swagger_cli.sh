22046e0303adbf31e39b3b61490b4b25903514da
David Mohs <dgit@mohsinc.com>
2017-08-14 11:45:26 -0400

    Fix the version of swagger-codegen-cli

--- tools/download_swagger_cli.sh
+++
@@ -1,1 +0,0 @@
-wget https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-2.3.0-20170814.101630-90.jar -O swagger-codegen-cli.jar
