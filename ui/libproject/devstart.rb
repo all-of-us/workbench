44e33d010b6df2f240140e0ee8fc25f1d74fb0a5
David Mohs <dgit@mohsinc.com>
2017-08-14 17:26:06 -0400

    Dockerized UI (#63)
    * Dockerized UI
    
    * Run tests via docker
    
    * Hack to fix Circle build
    
    * Use custom image
    
    * Bump swagger-codegen-cli version

---
+++ ui/libproject/devstart.rb
@@ -0,0 +1,86 @@
+require "optparse"
+require_relative "utils/common"
+
+def install_dependencies()
+  common = Common.new
+  common.docker.requires_docker
+
+  common.run_inline %W{docker-compose run --rm ui npm install}
+end
+
+def swagger_regen()
+  common = Common.new
+  common.docker.requires_docker
+
+  common.run_inline %W{docker-compose run --rm ui npm run codegen}
+end
+
+class DevUpOptions
+  ENV_CHOICES = %W{local test prod}
+  attr_accessor :env
+
+  def initialize
+    self.env = "test"
+  end
+
+  def parse args
+    parser = OptionParser.new do |parser|
+      parser.banner = "Usage: ./project.rb dev-up [options]"
+      parser.on(
+          "--environment ENV", ENV_CHOICES, "Environment [local (default), test, prod]") do |v|
+        self.env = v
+      end
+    end
+    parser.parse args
+    self
+  end
+end
+
+def dev_up(*args)
+  common = Common.new
+  common.docker.requires_docker
+
+  options = DevUpOptions.new.parse(args)
+
+  unless Dir.exist?("node_modules")
+    install_dependencies
+  end
+
+  ENV["ENV_FLAG"] = options.env == "local" ? "" : "--environment=#{options.env}"
+  common.run_inline %W{docker-compose up -d}
+  at_exit { common.run_inline %W{docker-compose down} }
+
+  common.status "Tests started. Open\n"
+  common.status "    http://localhost:9876/debug.html\n"
+  common.status "in a browser to view/run tests."
+
+  common.run_inline_swallowing_interrupt %W{docker-compose up ui}
+end
+
+def clean_git_hooks()
+  common.run_inline %W{find ../.git/hooks -type l -delete}
+end
+
+Common.register_command({
+  :invocation => "dev-up",
+  :description => "Brings up the development environment.",
+  :fn => Proc.new { |*args| dev_up(*args) }
+})
+
+Common.register_command({
+  :invocation => "install-dependencies",
+  :description => "Installs dependencies via npm.",
+  :fn => Proc.new { |*args| install_dependencies(*args) }
+})
+
+Common.register_command({
+  :invocation => "swagger-regen",
+  :description => "Regenerates API client libraries from Swagger definitions.",
+  :fn => Proc.new { |*args| swagger_regen(*args) }
+})
+
+Common.register_command({
+  :invocation => "clean-git-hooks",
+  :description => "Removes symlinks created by shared-git-hooks. Necessary before re-installing.",
+  :fn => Proc.new { |*args| clean-git-hooks(*args) }
+})
