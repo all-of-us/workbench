b1cc1a7c8c09ddaac4f2173837d69b39d0f57434
David Mohs <dgit@mohsinc.com>
2017-08-14 11:34:23 -0400

    Bump swagger-codegen-cli version

--- tools/download_swagger_cli.sh
+++ tools/download_swagger_cli.sh
@@ -1,1 +1,1 @@
-wget https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-2.3.0-20170809.095725-71.jar -O swagger-codegen-cli.jar
+wget https://oss.sonatype.org/content/repositories/snapshots/io/swagger/swagger-codegen-cli/2.3.0-SNAPSHOT/swagger-codegen-cli-2.3.0-20170814.101630-90.jar -O swagger-codegen-cli.jar
