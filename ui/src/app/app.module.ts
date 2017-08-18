22b6943b14849236abada645afc3b211a57ebe9a
blrubenstein <blrubenstein@wpi.edu>
2017-08-18 09:59:05 -0400

    Error message if cohort loading fails. (#77)
    * Error message if cohort loading fails.
    
    * Updated tests

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -4,6 +4,7 @@ import {NgModule} from '@angular/core';
 import {FormsModule} from '@angular/forms';
 import {HttpModule} from '@angular/http';
 import {BrowserModule} from '@angular/platform-browser';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ClarityModule} from 'clarity-angular';
 
 import {AppRoutingModule} from 'app/app-routing.module';
@@ -43,6 +44,7 @@ export function getConfiguration(signInService: SignInService): Configuration {
   imports:      [
     AppRoutingModule,
     BrowserModule,
+    BrowserAnimationsModule,
     FormsModule,
     HttpModule,
     ClarityModule.forRoot()
--- ui/src/app/views/workspace/component.ts
+++ ui/src/app/views/workspace/component.ts
@@ -51,6 +51,7 @@ export class WorkspaceComponent implements OnInit {
   private notebookNameFilter = new NotebookNameFilter();
   private notebookDescriptionFilter = new NotebookDescriptionFilter();
   cohortsLoading = true;
+  cohortsError = false;
   notebooksLoading = false;
   repositories: Repository[] = [];
   user: User;  // to detect if logged in
@@ -81,6 +82,10 @@ export class WorkspaceComponent implements OnInit {
                 this.cohortList.push(coho);
               }
               this.cohortsLoading = false;
+            },
+            error => {
+              this.cohortsLoading = false;
+              this.cohortsError = true;
             });
 
   }
