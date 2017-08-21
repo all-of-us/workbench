875d5f6320d53cefaa96b90c6e0957295cb92dad
blrubenstein <blrubenstein@wpi.edu>
2017-08-21 13:25:10 -0400

    Sorting (#83)
    * Sorting
    
    * Removed extra space.

--- ui/src/app/views/workspace/component.ts
+++ ui/src/app/views/workspace/component.ts
@@ -1,7 +1,7 @@
 import {Component, OnInit, Inject} from '@angular/core';
 import {Router, ActivatedRoute} from '@angular/router';
+import {StringFilter, Comparator} from 'clarity-angular';
 import {DOCUMENT} from '@angular/platform-browser';
-import {StringFilter} from 'clarity-angular';
 
 import {Cohort} from 'generated';
 import {CohortsService} from 'generated';
@@ -39,6 +39,31 @@ class NotebookDescriptionFilter implements StringFilter<Notebook> {
     return notebook.description.toLowerCase().indexOf(search) >= 0;
   }
 }
+/*
+* Sort comparators used by the cohort and notebook data tables to
+* determine the order that the cohorts loaded into client side memory
+* are displayed.
+*/
+class CohortNameComparator implements Comparator<Cohort> {
+  compare(a: Cohort, b: Cohort) {
+    return a.name.localeCompare(b.name);
+  }
+}
+class CohortDescriptionComparator implements Comparator<Cohort> {
+  compare(a: Cohort, b: Cohort) {
+    return a.description.localeCompare(b.description);
+  }
+}
+class NotebookNameComparator implements Comparator<Notebook> {
+  compare(a: Notebook, b: Notebook) {
+    return a.name.localeCompare(b.name);
+  }
+}
+class NotebookDescriptionComparator implements Comparator<Notebook> {
+  compare(a: Notebook, b: Notebook) {
+    return a.description.localeCompare(b.description);
+  }
+}
 
 
 @Component({
@@ -53,6 +78,10 @@ export class WorkspaceComponent implements OnInit {
   private cohortDescriptionFilter = new CohortDescriptionFilter();
   private notebookNameFilter = new NotebookNameFilter();
   private notebookDescriptionFilter = new NotebookDescriptionFilter();
+  private cohortNameComparator = new CohortNameComparator();
+  private cohortDescriptionComparator = new CohortDescriptionComparator();
+  private notebookNameComparator = new NotebookNameComparator();
+  private notebookDescriptionComparator = new NotebookDescriptionComparator();
   cohortsLoading = true;
   cohortsError = false;
   notebooksLoading = false;
