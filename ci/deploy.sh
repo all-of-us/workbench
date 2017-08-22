a242e751dea6f074da39d0b23ffa75f3ec19ab89
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-08-22 14:36:55 -0500

    Adding creds_file option to commands.
    Adding run_with_creds and run_with_cloud_sql_proxy for common logic.
    
    Changing Circle deploy.sh to run cloud migrations before deploying changes.

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,6 +1,8 @@
 #!/bin/bash -e
 
 ./ci/activate_creds.sh ~/gcloud-credentials.key
+(cd ./api && ./project.rb run-cloud-migrations --project pmi-drc-api-test \
+  --creds_file ~/gcloud-credentials.key)
 ./tools/deploy.py \
   --target $1 \
   --skip-confirmation \
