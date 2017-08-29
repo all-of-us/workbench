95b588065cd1665a5931206bd955c0734ff7d7b1
Mark Fickett <mwf@google.com>
2017-08-29 12:58:11 -0400

    Add doc to Date helpers. (#102)

--- ui/src/helper-functions.ts
+++ ui/src/helper-functions.ts
@@ -1,6 +1,12 @@
 interface DateObject {
   millis?: string;
 }
+
+/**
+ * Converts a Swagger-generated date-like object into a real Date. Swagger codegen creates generic
+ * objects with properties (millis etc) but no methods. As a workaround and to satisfy the TS compiler,
+ * we convert those to equivalent real Date objects.
+ */
 export function resetDateObject(date: Date): Date {
   const obj = date as DateObject;
   return new Date(obj.millis);
