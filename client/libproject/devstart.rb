22c490d3113733184d7683465f193df3a38a0048
Mark Fickett <mwf@google.com>
2017-09-08 16:36:10 -0400

    Switch to using new common swagger.rb.

--- client/libproject/devstart.rb
+++ client/libproject/devstart.rb
@@ -1,13 +1,14 @@
 require "optparse"
-# How can we tell what symbols come out of this import?
-require_relative "../../libproject/download-swagger-codegen-cli"
 require_relative "../../libproject/utils/common"
+require_relative "../../libproject/swagger"
 
 def swagger_regen()
-  download_swagger_codegen_cli
+  Workbench::Swagger.download_swagger_codegen_cli
 
   common = Common.new
-  common.run_inline %W{java -jar #{SWAGGER_CODEGEN_CLI_JAR} generate --lang python --input-spec #{SWAGGER_SPEC} --output py/}
+  common.run_inline %W{
+      java -jar #{Workbench::Swagger::SWAGGER_CODEGEN_CLI_JAR}
+      generate --lang python --input-spec #{Workbench::Swagger::SWAGGER_SPEC} --output py/}
 end
 
 Common.register_command({
