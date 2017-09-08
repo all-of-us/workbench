b3a60de2efdbd33bc901ffe48d836dccc37e4ca7
Mark Fickett <mwf@google.com>
2017-09-08 16:52:32 -0400

    Switch client/project.rb to using new Workbench.handle_argv_or_die.

--- client/project.rb
+++ client/project.rb
@@ -1,13 +1,6 @@
 #!/usr/bin/env ruby
+
+require_relative "../libproject/workbench"
 require_relative "libproject/devstart.rb"
 
-common = Common.new
-if ARGV.length == 0 or ARGV[0] == "--help"
-  common.print_usage
-  exit 0
-end
-
-command = ARGV.first
-args = ARGV.drop(1)
-
-common.handle_or_die(command, *args)
+Workbench.handle_argv_or_die(__FILE__)
