b62567c30575aac5fcb5ab255dee8dfa94342774
Brian Rubenstein <brubenst@wma78-22d.broadinstitute.org>
2017-09-11 09:54:58 -0400

    Start on email capability

---
+++ api/src/main/java/org/pmiops/workbench/api/BugReportController.java
@@ -0,0 +1,39 @@
+package org.pmiops.workbench.api;
+
+import javax.mail;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import org.pmiops.workbench.firecloud.ApiException;
+import org.pmiops.workbench.model.BugReport;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class BugReportController implements BugReportApiDelegate {
+
+  private static final Logger log = Logger.getLogger(BugReportController.class.getName());
+
+
+
+  @Override
+  public ResponseEntity<BugReport> sendBug(BugReport bugReport) {
+    Properties props = new Properties();
+    Session session = Session.getDefaultInstance(props, null);
+
+    try {
+      Message msg = new MimeMessage(session);
+      // TODO: sender address msg.setFrom(new InternetAddress(""))
+      msg.addRecipient(Message.RecipientType.TO, new InternetAddress("brubenst@broadinstitute.org", "My Email"));
+      msg.setSubject("[AofU Bug Report]" + bugReport.shortDescription);
+      msg.setText(bugReport.reproSteps);
+      Transport.send(msg);
+    } catch (AddressException e) {
+      // ...
+    } catch (MessagingException e) {
+
+    } catch (UnsupportedEncodingException e) {
+
+    }
+  }
+}
