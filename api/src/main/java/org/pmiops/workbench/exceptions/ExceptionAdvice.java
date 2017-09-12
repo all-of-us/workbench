cae355c0ce3ddc2ffb09e48eeeaeb0784a15af34
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-09-12 12:46:08 -0500

    Tweaking

--- api/src/main/java/org/pmiops/workbench/exceptions/ExceptionAdvice.java
+++ api/src/main/java/org/pmiops/workbench/exceptions/ExceptionAdvice.java
@@ -2,6 +2,7 @@ package org.pmiops.workbench.exceptions;
 
 import java.util.logging.Level;
 import java.util.logging.Logger;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.ResponseStatus;
@@ -14,20 +15,21 @@ public class ExceptionAdvice {
   private static final String DEFAULT_ERROR_VIEW = "error";
 
   @ExceptionHandler({Exception.class})
-  public String serverError(Exception e) {
+  public ResponseEntity<?> serverError(Exception e) {
+    int statusCode = 500;
     if (e.getClass().getPackage().getName().equals(
         ExceptionAdvice.class.getPackage().getName())) {
       ResponseStatus responseStatus = e.getClass().getAnnotation(ResponseStatus.class);
       if (responseStatus != null) {
+        statusCode = responseStatus.code().value();
         log.log(Level.WARNING, "[{0}] {1}: {2}",
-            new Object[]{responseStatus.code().value(), e.getClass().getSimpleName(),
-                e.getMessage()});
+            new Object[]{statusCode, e.getClass().getSimpleName(), e.getMessage()});
         if (responseStatus.code().value() < 500) {
-          return DEFAULT_ERROR_VIEW;
+          return ResponseEntity.status(statusCode).body(e.getMessage());
         }
       }
     }
     log.log(Level.SEVERE, "Server error", e);
-    return DEFAULT_ERROR_VIEW;
+    return ResponseEntity.status(statusCode).body(e.getMessage());
   }
 }
