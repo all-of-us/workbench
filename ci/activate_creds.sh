db9d129899cdb8e063a2556bc5fbdd1aa19a12aa
Mark Fickett <mwf@google.com>
2017-09-13 17:24:15 -0400

    Check for required env var before use, report error in activate_creds.sh. (#125)

--- ci/activate_creds.sh
+++ ci/activate_creds.sh
@@ -3,9 +3,14 @@
 # Creates credentials file $1 from two environment variables (see
 # below) which combine to decrypt the keys for a service account.
 # Does gcloud auth using the result.
+if [ ! "${GCLOUD_CREDENTIALS}" ]
+then
+  echo "No GCLOUD_CREDENTIALS env var defined, aborting creds activation."
+  exit 1
+fi
 
 echo $GCLOUD_CREDENTIALS | \
-     openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k $GCLOUD_CREDENTIALS_KEY \
+     openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "${GCLOUD_CREDENTIALS_KEY}" \
      > $1
 
 gcloud auth activate-service-account --key-file $1
