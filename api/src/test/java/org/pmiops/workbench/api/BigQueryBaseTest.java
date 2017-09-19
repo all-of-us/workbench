2dcce2e636a295084047232894d9b0e3d3a2cf39
Brian <brian.d.freeman@vumc.org>
2017-09-19 16:07:05 -0500

    added a real test.

---
+++ api/src/test/java/org/pmiops/workbench/api/BigQueryBaseTest.java
@@ -0,0 +1,53 @@
+package org.pmiops.workbench.api;
+
+import com.google.cloud.bigquery.*;
+import org.junit.runner.RunWith;
+import org.pmiops.workbench.api.config.TestAppConfig;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+
+import java.util.List;
+import java.util.Map;
+import java.util.logging.Logger;
+
+@RunWith(SpringJUnit4ClassRunner.class)
+@SpringBootTest(classes = {TestAppConfig.class})
+@ActiveProfiles("local")
+public class BigQueryBaseTest {
+
+    private static final Logger log = Logger.getLogger(BigQueryBaseTest.class.getName());
+
+    @Autowired
+    BigQuery bigquery;
+
+    public void createDataSet(String dataSetId) {
+        Dataset dataSet = bigquery.create(DatasetInfo.newBuilder(dataSetId).build());
+    }
+
+    public void createTable(String dataSetId, String tableId, List<Field> fields) {
+        StandardTableDefinition tableDef = StandardTableDefinition.of(Schema.of(fields));
+        bigquery.create(TableInfo.of(TableId.of(dataSetId, tableId), tableDef));
+    }
+
+    public boolean insertRow(String dataSetId, String tableId, Map<String, Object> row) {
+        InsertAllRequest insertRequest = InsertAllRequest
+                .newBuilder(TableId.of(dataSetId, tableId))
+                .addRow(row).build();
+        InsertAllResponse insertResponse = bigquery.insertAll(insertRequest);
+        if (insertResponse.hasErrors()) {
+            log.info("Errors occurred while inserting rows");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean deleteTable(String dataSetId, String tableId) {
+        return bigquery.delete(TableId.of(dataSetId, tableId));
+    }
+
+    public boolean deleteDataSet(String dataSetId) {
+        return bigquery.delete(dataSetId);
+    }
+}
--- api/src/test/java/org/pmiops/workbench/api/CohortBuilderControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/CohortBuilderControllerTest.java
@@ -1,24 +1,78 @@
 package org.pmiops.workbench.api;
 
+import com.google.cloud.bigquery.Field;
+import org.junit.After;
+import org.junit.Before;
 import org.junit.Test;
-import org.junit.runner.RunWith;
-import org.pmiops.workbench.api.config.TestAppConfig;
+import org.pmiops.workbench.model.Criteria;
+import org.pmiops.workbench.model.CriteriaListResponse;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.context.SpringBootTest;
-import org.springframework.test.context.ActiveProfiles;
-import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
+import org.springframework.context.annotation.Import;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 
-@RunWith(SpringJUnit4ClassRunner.class)
-@SpringBootTest(classes = {TestAppConfig.class, CohortBuilderController.class})
-@ActiveProfiles("local")
-public class CohortBuilderControllerTest {
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import static com.google.common.truth.Truth.assertThat;
+
+@Import(CohortBuilderController.class)
+public class CohortBuilderControllerTest extends BigQueryBaseTest {
 
     @Autowired
     CohortBuilderController controller;
 
+    @Before
+    public void setUp() {
+        List<Field> fields = Arrays.asList(
+                Field.of("id", Field.Type.integer()),
+                Field.of("parent_id", Field.Type.integer()),
+                Field.of("type", Field.Type.string()),
+                Field.of("code", Field.Type.string()),
+                Field.of("name", Field.Type.string()),
+                Field.of("is_group", Field.Type.bool()),
+                Field.of("is_selectable", Field.Type.bool()),
+                Field.of("est_count", Field.Type.integer()),
+                Field.of("domain_id", Field.Type.string())
+        );
+        Map<String, Object> row = new HashMap<String, Object>();
+        row.put("id", 1);
+        row.put("parent_id", 0);
+        row.put("type", "ICD9");
+        row.put("code", "001.1");
+        row.put("name", "Cholera");
+        row.put("is_group", true);
+        row.put("is_selectable", false);
+        row.put("est_count", 10);
+        row.put("domain_id", "Condition");
+        createDataSet(controller.getDataSetId());
+        createTable(controller.getDataSetId(), "icd9_criteria", fields );
+        insertRow(controller.getDataSetId(), "icd9_criteria", row);
+    }
+
+    @After
+    public void tearDown() {
+        deleteTable(controller.getDataSetId(), "icd9_criteria");
+        deleteDataSet(controller.getDataSetId());
+    }
+
     @Test
     public void getCriteriaByTypeAndParentId() throws Exception {
-        controller.getCriteriaByTypeAndParentId("icd9", 0L );
+        ResponseEntity response = controller.getCriteriaByTypeAndParentId("icd9", 0L );
+        assertThat(response.getStatusCode()).isEqualTo(HttpStatus.OK);
+
+        CriteriaListResponse listResponse = (CriteriaListResponse) response.getBody();
+        Criteria criteria = listResponse.getItems().get(0);
+        assertThat(criteria.getId()).isEqualTo(1);
+        assertThat(criteria.getType()).isEqualTo("ICD9");
+        assertThat(criteria.getCode()).isEqualTo("001.1");
+        assertThat(criteria.getName()).isEqualTo("Cholera");
+        assertThat(criteria.getGroup()).isEqualTo(true);
+        assertThat(criteria.getSelectable()).isEqualTo(false);
+        assertThat(criteria.getCount()).isEqualTo(10);
+        assertThat(criteria.getDomainId()).isEqualTo("Condition");
     }
 
 }
