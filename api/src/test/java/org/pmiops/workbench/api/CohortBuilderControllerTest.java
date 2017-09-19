5e98ef449788d4720372307ee1fd0be3ee67c312
Brian <brian.d.freeman@vumc.org>
2017-09-19 10:58:56 -0500

    changed profile to be local for now.

--- api/src/test/java/org/pmiops/workbench/api/CohortBuilderControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/CohortBuilderControllerTest.java
@@ -10,7 +10,7 @@ import org.springframework.test.context.junit4.SpringJUnit4ClassRunner;
 
 @RunWith(SpringJUnit4ClassRunner.class)
 @ContextConfiguration(classes = { TestAppConfig.class, CohortBuilderController.class })
-@ActiveProfiles("test")
+@ActiveProfiles("local")
 public class CohortBuilderControllerTest {
 
     @Autowired
