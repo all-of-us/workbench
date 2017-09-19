f14adf4ca2d4e6db958ececfde4b91c22d09053b
Mark Fickett <mwf@google.com>
2017-09-19 18:46:21 -0400

    Add pylint rule for project.

--- client/libproject/devstart.rb
+++ client/libproject/devstart.rb
@@ -1,6 +1,7 @@
 require "optparse"
 require "fileutils"
 require_relative "../../libproject/utils/common"
+require_relative "../../libproject/workbench"
 require_relative "../../libproject/swagger"
 
 def swagger_regen()
@@ -21,8 +22,35 @@ def swagger_regen()
   common.status "Publish a version with `git tag pyclient-vN-N-rcN` and `git push --tags`."
 end
 
+def pylint()
+  py_module_root = File.join(Workbench::WORKBENCH_ROOT, 'client', 'py', 'aou_workbench_client')
+  rc_file_path = File.join(Workbench::WORKBENCH_ROOT, 'libproject', 'pylintrc')
+
+  # As well as the client Python module, lint setup.py and other support files.
+  Dir.chdir(File.join(Workbench::WORKBENCH_ROOT, 'client', 'py'))
+  support_py_files = Dir.glob('*.py').map(&File.method(:realpath))
+
+  enabled = "bad-indentation,broad-except,bare-except,logging-too-many-args," +
+      "unused-argument,redefined-outer-name,redefined-builtin," +
+      "superfluous-parens,syntax-error,trailing-whitespace,unused-import," +
+      "unused-variable," +
+      "undefined-variable,bad-whitespace,line-too-long,unused-import," +
+      "unused-variable"
+
+  common = Common.new
+  common.run_inline %W{pylint --rcfile #{rc_file_path} --reports=n --score=n
+      --ignore=swagger_client --disable=all --enable=#{enabled}
+      #{py_module_root}} + support_py_files
+end
+
 Common.register_command({
   :invocation => "swagger-regen",
   :description => "rebuilds the Swagger-generated client libraries",
   :fn => Proc.new { |*args| swagger_regen(*args) }
+})
+
+Common.register_command({
+  :invocation => "pylint",
+  :description => "Lint Python",
+  :fn => Proc.new { |*args| pylint(*args) }
 })
