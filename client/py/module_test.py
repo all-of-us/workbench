2288707d85bd70abc337dd9d7066661a2d4d4d99
Mark Fickett <mwf@google.com>
2017-09-19 15:02:33 -0400

    Add test script for py client.

---
+++ client/py/module_test.py
@@ -0,0 +1,9 @@
+import unittest
+import aou_workbench_client
+from aou_workbench_client import swagger_client
+
+
+class ModuleTest(unittest.TestCase):
+
+  def test_noop_as_verification_that_module_imports_worked(self):
+    pass
