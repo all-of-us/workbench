4f9e60481f5527ebc9d72c22ade43649f92dc1eb
Mark Fickett <mwf@google.com>
2017-09-22 13:16:14 -0400

    2 => 4 spaces in client/py

--- client/py/run_tests.py
+++ client/py/run_tests.py
@@ -8,16 +8,16 @@ import unittest
 
 
 def run_tests(root_dir, pattern):
-  suite = unittest.loader.defaultTestLoader.discover(root_dir, pattern)
-  return unittest.TextTestRunner(verbosity=2).run(suite)
+    suite = unittest.loader.defaultTestLoader.discover(root_dir, pattern)
+    return unittest.TextTestRunner(verbosity=2).run(suite)
 
 
 if __name__ == '__main__':
-  parser = argparse.ArgumentParser()
-  parser.add_argument(
-      '--pattern',
-      default='*_test.py',
-      help='Shell glob for test files to include.')
-  args = parser.parse_args()
-  result = run_tests(os.path.abspath(os.path.dirname(__file__)), args.pattern)
-  sys.exit(0 if result.wasSuccessful() else 1)
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+            '--pattern',
+            default='*_test.py',
+            help='Shell glob for test files to include.')
+    args = parser.parse_args()
+    result = run_tests(os.path.abspath(os.path.dirname(__file__)), args.pattern)
+    sys.exit(0 if result.wasSuccessful() else 1)
