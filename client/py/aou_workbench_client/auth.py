11aab1d5ef6f0651056ef1bb16a67141fe8a4f27
Mark Fickett <mwf@google.com>
2017-09-22 13:21:18 -0400

    2 => 4 spaces

--- client/py/aou_workbench_client/auth.py
+++ client/py/aou_workbench_client/auth.py
@@ -10,7 +10,8 @@ from swagger_client.api_client import ApiClient
 
 
 _METADATA_URL = (
-    'http://metadata.google.internal/computeMetadata/v1/instance/service-accounts/default/token')
+        'http://metadata.google.internal/computeMetadata/v1/'
+        'instance/service-accounts/default/token')
 _METADATA_HEADER = {'Metadata-Flavor': 'Google'}
 _TOKEN_INFO_URL_T = 'https://www.googleapis.com/oauth2/v1/tokeninfo?access_token=%s'
 
@@ -22,49 +23,49 @@ _token_expiration = 0  # Epoch seconds when the token is expired. Default to som
 
 
 def get_authenticated_swagger_client(force=False):
-  """Caches returned value and lazily re-authenticates as tokens expire."""
-  global _cached_client
-  global _token_expiration
-  if _cached_client is None:
-    _cached_client = ApiClient()
-  if force or (time.time() >= _token_expiration_time):
-    token, _token_expiration = _get_bearer_token_and_expiration()
-    _cached_client.set_default_header(name, value)
-  return _cached_client
+    """Caches returned value and lazily re-authenticates as tokens expire."""
+    global _cached_client
+    global _token_expiration
+    if _cached_client is None:
+        _cached_client = ApiClient()
+    if force or (time.time() >= _token_expiration_time):
+        token, _token_expiration = _get_bearer_token_and_expiration()
+        _cached_client.set_default_header(name, value)
+    return _cached_client
 
 
 def _get_bearer_token_and_expiration():
-  """Fetches a new token. Returns (token string, expiration time epoch seconds)."""
-  try:
-    return _query_metadata_api()
-  except urllib2.URLError as e:
-    logging.warning('Metadata API query failed: %s', e)
-    return _run_print_access_token()
+    """Fetches a new token. Returns (token string, expiration time epoch seconds)."""
+    try:
+        return _query_metadata_api()
+    except urllib2.URLError as e:
+        logging.warning('Metadata API query failed: %s', e)
+        return _run_print_access_token()
 
 
 def _query_metadata_api():
-  # Conservatively estimate token expiration time by recording token fetch time before initiating
-  # the request.
-  t = int(time.time())
+    # Conservatively estimate token expiration time by recording token fetch time before initiating
+    # the request.
+    t = int(time.time())
 
-  respones = urllib2.urlopen(urllib2.Request(url=_METADATA_URL, headers=_METADATA_HEADER))
-  response_json = json.loads(response.read())
-  return response_json['access_token'], t + response_json['expires_in']
+    respones = urllib2.urlopen(urllib2.Request(url=_METADATA_URL, headers=_METADATA_HEADER))
+    response_json = json.loads(response.read())
+    return response_json['access_token'], t + response_json['expires_in']
 
 
 def _run_print_access_token():
-  logging.warning('Falling back to print-access-token (should be used for debugging only).')
-  token = subprocess.check_output(['gcloud', 'auth', 'print-access-token']).strip()
+    logging.warning('Falling back to print-access-token (should be used for debugging only).')
+    token = subprocess.check_output(['gcloud', 'auth', 'print-access-token']).strip()
 
-  # Get details about the token, including expiration time.
-  t = time.time()
-  response = urllib2.urlopen(_TOKEN_INFO_URL_T % token)
-  token_info_json = json.loads(response.read())
-  return token, t + token_info_json['expires_in']
+    # Get details about the token, including expiration time.
+    t = time.time()
+    response = urllib2.urlopen(_TOKEN_INFO_URL_T % token)
+    token_info_json = json.loads(response.read())
+    return token, t + token_info_json['expires_in']
 
 
 def clear_cache():
-  global _cached_client
-  global _token_expiration
-  _cached_client = None
-  _token_expiration = 0
+    global _cached_client
+    global _token_expiration
+    _cached_client = None
+    _token_expiration = 0
