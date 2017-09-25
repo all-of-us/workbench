ed193d87c388e4cbcc88031bd74aeb15a15993cc
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-09-25 17:05:16 -0500

    Reference files from classpath

--- api/src/test/java/org/pmiops/workbench/api/BigQueryBaseTest.java
+++ api/src/test/java/org/pmiops/workbench/api/BigQueryBaseTest.java
@@ -2,10 +2,21 @@ package org.pmiops.workbench.api;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
-import com.google.cloud.bigquery.*;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.Dataset;
+import com.google.cloud.bigquery.DatasetInfo;
+import com.google.cloud.bigquery.InsertAllRequest;
+import com.google.cloud.bigquery.InsertAllResponse;
+import com.google.cloud.bigquery.Schema;
+import com.google.cloud.bigquery.StandardTableDefinition;
+import com.google.cloud.bigquery.TableId;
+import com.google.cloud.bigquery.TableInfo;
 import com.google.gson.Gson;
 import com.google.gson.reflect.TypeToken;
+import org.junit.After;
+import org.junit.Before;
 import org.pmiops.workbench.api.config.TestBigQueryConfig;
+import org.pmiops.workbench.config.WorkbenchConfig;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 
@@ -19,22 +30,45 @@ import java.util.Map;
 import java.util.logging.Logger;
 
 @SpringBootTest(classes = {TestBigQueryConfig.class})
-public class BigQueryBaseTest {
+public abstract class BigQueryBaseTest {
 
     private static final Logger log = Logger.getLogger(BigQueryBaseTest.class.getName());
 
     @Autowired
     BigQuery bigquery;
 
-    public void createDataSet(String dataSetId) {
+    @Autowired
+    WorkbenchConfig workbenchConfig;
+
+    public static final String BASE_PATH = "src/test/resources/bigquery/";
+
+    @Before
+    public void setUp() throws Exception {
+        createDataSet(workbenchConfig.bigquery.dataSetId);
+        for (String tableName: getTableNames()) {
+            createTable(workbenchConfig.bigquery.dataSetId, tableName);
+            insertData(workbenchConfig.bigquery.dataSetId, tableName);
+        }
+    }
+
+    @After
+    public void tearDown() {
+        for (String tableName: getTableNames()) {
+            deleteTable(workbenchConfig.bigquery.dataSetId, tableName);
+        }
+        deleteDataSet(workbenchConfig.bigquery.dataSetId);
+    }
+
+    public abstract List<String> getTableNames();
+
+    private void createDataSet(String dataSetId) {
         Dataset dataSet = bigquery.create(DatasetInfo.newBuilder(dataSetId).build());
     }
 
-    public void createTable(String dataSetId, String tableId) throws Exception {
-        String basePath = "src/test/resources/bigquery/schema/";
+    private void createTable(String dataSetId, String tableId) throws Exception {
         ObjectMapper jackson = new ObjectMapper();
         String rawJson =
-                new String(Files.readAllBytes(Paths.get(basePath + tableId + ".json")), Charset.defaultCharset());
+                new String(Files.readAllBytes(Paths.get(BASE_PATH + "schema/" + tableId.toLowerCase() + ".json")), Charset.defaultCharset());
         JsonNode newJson = jackson.readTree(rawJson);
 
         Gson gson = new Gson();
@@ -43,11 +77,10 @@ public class BigQueryBaseTest {
         bigquery.create(TableInfo.of(TableId.of(dataSetId, tableId), tableDef));
     }
 
-    public boolean insertData(String dataSetId, String tableId) throws Exception {
-        String basePath = "src/test/resources/bigquery/data/";
+    private boolean insertData(String dataSetId, String tableId) throws Exception {
         ObjectMapper jackson = new ObjectMapper();
         String rawJson =
-                new String(Files.readAllBytes(Paths.get(basePath + tableId + "_data.json")), Charset.defaultCharset());
+                new String(Files.readAllBytes(Paths.get(BASE_PATH + "data/" + tableId.toLowerCase() + "_data.json")), Charset.defaultCharset());
         JsonNode newJson = jackson.readTree(rawJson);
 
         Gson gson = new Gson();
@@ -72,11 +105,11 @@ public class BigQueryBaseTest {
         return true;
     }
 
-    public boolean deleteTable(String dataSetId, String tableId) {
+    private boolean deleteTable(String dataSetId, String tableId) {
         return bigquery.delete(TableId.of(dataSetId, tableId));
     }
 
-    public boolean deleteDataSet(String dataSetId) {
+    private boolean deleteDataSet(String dataSetId) {
         return bigquery.delete(dataSetId);
     }
 }
