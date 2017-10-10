9a69a6d4660699a73d5840730c02626e63938632
David Mohs <dgit@mohsinc.com>
2017-10-10 11:23:32 -0400

    Make GSuite domain configurable (#166)
    * Add db dependencies
    
    * Add generic `gradle` script
    
    * Make GSuite domain configurable
    - also adds some explanations around GDS API usage
    
    * Remove unused imports

--- api/src/integration/java/org/pmiops/workbench/google/DirectoryServiceImplIntegrationTest.java
+++ api/src/integration/java/org/pmiops/workbench/google/DirectoryServiceImplIntegrationTest.java
@@ -1,18 +1,18 @@
 package org.pmiops.workbench.google;
 
-import java.io.IOException;
-
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.Assert.assertTrue;
 import org.junit.Before;
 import org.junit.Test;
+import org.pmiops.workbench.config.WorkbenchConfig;
+import org.pmiops.workbench.test.Providers;
 
 public class DirectoryServiceImplIntegrationTest {
   private DirectoryServiceImpl service;
+  private final WorkbenchConfig workbenchConfig = createConfig();
 
   @Before
   public void setup() {
-    service = new DirectoryServiceImpl();
+    service = new DirectoryServiceImpl(Providers.of(workbenchConfig));
   }
 
   @Test
@@ -23,5 +23,12 @@ public class DirectoryServiceImplIntegrationTest {
   @Test
   public void testDirectoryServiceUsernameIsTaken() {
     assertThat(service.isUsernameTaken("directory-service")).isTrue();
+  }
+
+  private static WorkbenchConfig createConfig() {
+    WorkbenchConfig config = new WorkbenchConfig();
+    config.googleDirectoryService = new WorkbenchConfig.GoogleDirectoryServiceConfig();
+    config.googleDirectoryService.gSuiteDomain = "fake-research-aou.org";
+    return config;
   }
 }
