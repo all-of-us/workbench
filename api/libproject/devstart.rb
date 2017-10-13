all-of-us/workbench

commit 84999d3ad92b4a9dc7c35edfefb49cc8f8d99a69

Refactor options parser to be more extensible (#181)

api/libproject/devstart.rb
@@ -3,43 +3,41 @@ require_relative "../../libproject/workbench"
 require "io/console"
 require "json"
 require "optparse"
+require "ostruct"
 require "tempfile"
 require "fileutils"
 
-class ProjectAndAccountOptions
-  attr_accessor :project
-  attr_accessor :account
-  attr_accessor :command
-  attr_accessor :creds_file
-  attr_accessor :parser
+class Options < OpenStruct
+end
 
-  def initialize(command)
-    self.command = command
-    self.parser = OptionParser.new do |parser|
-      parser.banner = "Usage: ./project.rb #{self.command} [options]"
-      parser.on("--project [PROJECT]",
-          "Project to create credentials for (e.g. all-of-us-workbench-test)") do |project|
-        self.project = project
-      end
-      parser.on("--account [ACCOUNT]",
-           "Account to use when creating credentials (your.name@pmi-ops.org); "\
-           "use this or --creds_file") do |account|
-        self.account = account
-      end
-      parser.on("--creds_file [CREDS_FILE]",
-           "Path to a file containing credentials; use this or --account.") do |creds_file|
-        self.creds_file = creds_file
-      end
-    end
+def create_parser(command)
+  OptionParser.new do |parser|
+    parser.banner = "Usage: ./project.rb #{command} [options]"
+    parser
   end
+end
 
-  def parse args
-    self.parser.parse args
-    if self.project == nil || !((self.account == nil) ^ (self.creds_file == nil))
-      puts self.parser.help
-      raise("Invalid arguments")
-    end
-    self
+def add_default_options(parser, options)
+  parser.on("--project [PROJECT]",
+      "Project to create credentials for (e.g. all-of-us-workbench-test)") do |project|
+    options.project = project
+  end
+  parser.on("--account [ACCOUNT]",
+       "Account to use when creating credentials (your.name@pmi-ops.org); "\
+       "use this or --creds-file") do |account|
+    options.account = account
+  end
+  parser.on("--creds-file [CREDS-FILE]",
+       "Path to a file containing credentials; use this or --account.") do |creds_file|
+    options.creds_file = creds_file
+  end
+  parser
+end
+
+def validate_default_options(parser, options)
+  if options.project == nil || !((options.account == nil) ^ (options.creds_file == nil))
+    puts parser.help
+    exit 1
   end
 end
 
@@ -247,7 +245,10 @@ def get_auth_login_account()
 end
 
 def run_with_creds(args, command, proc)
-  options = ProjectAndAccountOptions.new(command).parse(args)
+  parser = create_parser(command)
+  options = Options.new
+  add_default_options parser, options
+  validate_default_options parser, options
   do_run_with_creds(options.project, options.account, options.creds_file, proc)
 end
 

commit 5b0fb9fd44e54596b4a1551fc4251fbabc2f8a77

Cleanup in API Ruby: Use Dir.chdir context guard, common.run_inline. Use spawn() for all of Workbench. (#189)
Clean rollforward of #184, rolled back in #186. (I believe the error was actually caused by #181, fixed separately in #187.)

api/libproject/devstart.rb
@@ -1,3 +1,7 @@
+# Calls to common.run_inline in this file may use a quoted string purposefully
+# to cause system() or spawn() to run the command in a shell. Calls with arrays
+# are not run in a shell, which can break usage of the CloudSQL proxy.
+
 require_relative "../../libproject/utils/common"
 require_relative "../../libproject/workbench"
 require "io/console"
@@ -10,9 +14,11 @@ require "fileutils"
 class Options < OpenStruct
 end
 
-def create_parser(command)
+# Creates a default command-line argument parser.
+# command_name: For help text.
+def create_parser(command_name)
   OptionParser.new do |parser|
-    parser.banner = "Usage: ./project.rb #{command} [options]"
+    parser.banner = "Usage: ./project.rb #{command_name} [options]"
     parser
   end
 end
@@ -121,7 +127,7 @@ def docker_clean(*args)
 
   docker_images = `docker ps -aq`.gsub(/\s+/, " ")
   if !docker_images.empty?
-    system("docker rm -f #{docker_images}")
+    common.run_inline("docker rm -f #{docker_images}")
   end
   common.run_inline %W{docker-compose down --volumes}
 end
@@ -200,52 +206,53 @@ def run_cloud_sql_proxy(project, creds_file)
   return pid
 end
 
+# Common.run_inline uses spawn() which doesn't handle pipes/redirects.
+def run_with_redirects(command_string, to_redact = "")
+  common = Common.new
+  command_to_echo = command_string.clone
+  if to_redact
+    command_to_echo.sub! to_redact, "*" * to_redact.length
+  end
+  common.put_command(command_to_echo)
+  unless system(command_string)
+    raise("Error running: " + command_to_echo)
+  end
+end
+
 def do_run_migrations(creds_file, project)
   read_db_vars(creds_file, project)
   common = Common.new
-  create_db_file = Tempfile.new("#{project}-create-db.sql")
-  begin
-    unless system("cat db/create_db.sql | envsubst > #{create_db_file.path}")
-      raise("Error generating create_db file; exiting.")
-    end
-    puts "Creating database if it does not exist..."
-    unless system("mysql -u \"root\" -p\"#{ENV["MYSQL_ROOT_PASSWORD"]}\" --host 127.0.0.1 "\
-              "--port 3307 < #{create_db_file.path}")
-      raise("Error creating database; exiting.")
-    end
-  ensure
-    create_db_file.unlink
-  end
+  puts "Creating database if it does not exist..."
+  pw = ENV["MYSQL_ROOT_PASSWORD"]
+  run_with_redirects(
+      "cat db/create_db.sql | envsubst | " \
+      "mysql -u \"root\" -p\"#{pw}\" --host 127.0.0.1 --port 3307",
+      to_redact=pw)
   ENV["DB_PORT"] = "3307"
   puts "Upgrading database..."
-  unless system("cd db && ../gradlew --info update && cd ..")
-    raise("Error upgrading database. Exiting.")
+  Dir.chdir("db") do
+    common.run_inline("../gradlew --info update")
   end
 end
 
 def do_drop_db(creds_file, project)
   read_db_vars(creds_file, project)
   drop_db_file = Tempfile.new("#{project}-drop-db.sql")
-  begin
-    unless system("cat db/drop_db.sql | envsubst > #{drop_db_file.path}")
-      raise("Error generating drop_db file; exiting.")
-    end
-    puts "Dropping database..."
-    unless system("mysql -u \"root\" -p\"#{ENV["MYSQL_ROOT_PASSWORD"]}\" --host 127.0.0.1 "\
-              "--port 3307 < #{drop_db_file.path}")
-      raise("Error dropping database; exiting.")
-    end
-  ensure
-    drop_db_file.unlink
-  end
+  common = Common.new
+  puts "Dropping database..."
+  pw = ENV["MYSQL_ROOT_PASSWORD"]
+  run_with_redirects(
+      "cat db/drop_db.sql | envsubst > #{drop_db_file.path} | " \
+      "mysql -u \"root\" -p\"#{pw}\" --host 127.0.0.1 --port 3307",
+      to_redact=pw)
 end
 
 def get_auth_login_account()
   return `gcloud config get-value account`.strip()
 end
 
-def run_with_creds(args, command, proc)
-  parser = create_parser(command)
+def run_with_creds(args, command_name, proc)
+  parser = create_parser(command_name)
   options = Options.new
   add_default_options parser, options
   parser.parse args
@@ -290,8 +297,8 @@ def do_run_with_creds(project, account, creds_file, proc)
   end
 end
 
-def run_with_cloud_sql_proxy(args, command, proc)
-  run_with_creds(args, command, lambda { |project, account, creds_file|
+def run_with_cloud_sql_proxy(args, command_name, proc)
+  run_with_creds(args, command_name, lambda { |project, account, creds_file|
     pid = run_cloud_sql_proxy(project, creds_file)
     begin
       proc.call(project, account, creds_file)
@@ -305,8 +312,10 @@ def register_service_account(*args)
   run_with_creds(args, "register-service-account", lambda { |project, account, creds_file|
     common = Common.new
     ENV["GOOGLE_APPLICATION_CREDENTIALS"] = creds_file
-    system("cd ../firecloud-tools &&  ./run.sh " \
-      "register_service_account/register_service_account.py -j #{creds_file} -o #{account}")
+    Dir.chdir("../firecloud-tools") do
+      common.run_inline("./run.sh " \
+        "register_service_account/register_service_account.py -j #{creds_file} -o #{account}")
+    end
   })
 end
 
@@ -319,8 +328,13 @@ end
 def connect_to_cloud_db(*args)
   run_with_cloud_sql_proxy(args, "connect-to-cloud-db", lambda { |project, account, creds_file|
     read_db_vars(creds_file, project)
-    system("mysql -u \"workbench\" -p\"#{ENV["WORKBENCH_DB_PASSWORD"]}\" --host 127.0.0.1 "\
-           "--port 3307 --database #{ENV["DB_NAME"]}")
+    common = Common.new
+    pw = ENV["WORKBENCH_DB_PASSWORD"]
+    # TODO Switch this to run_inline once Common supports redaction.
+    run_with_redirects(
+        "mysql -u \"workbench\" -p\"#{pw}\" --host 127.0.0.1 "\
+        "--port 3307 --database #{ENV["DB_NAME"]}",
+        to_redact=pw)
   })
 end
 
@@ -328,8 +342,9 @@ def update_cloud_config(*args)
   run_with_cloud_sql_proxy(args, "connect-to-cloud-db", lambda { |project, account, creds_file|
     read_db_vars(creds_file, project)
     ENV["DB_PORT"] = "3307"
-    unless system("cd tools && ../gradlew --info loadConfig && cd ..")
-        raise("Error updating configuration. Exiting.")
+    common = Common.new
+    Dir.chdir("tools") do
+      common.run_inline("../gradlew --info loadConfig")
     end
   })
 end

libproject/workbench.rb
@@ -25,6 +25,7 @@ module Workbench
   module_function :ensure_git_hooks
 
   def handle_argv_or_die(main_filename)
+    ENV["PROJECTRB_DEBUG"] = "true"  # Use spawn() instead of system() so stderr shows up.
     common = Common.new
     Dir.chdir(File.dirname(main_filename))
 