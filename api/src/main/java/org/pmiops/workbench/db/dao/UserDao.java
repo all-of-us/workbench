4f30a8d473c581199446ca1a29aebe8a48086f1e
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2017-10-16 15:24:38 -0500

    Adding UserDao.findUserWithAuthorities(); using it where authorities are needed.
    Changing workspace logic to handle null values

--- api/src/main/java/org/pmiops/workbench/db/dao/UserDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/UserDao.java
@@ -1,9 +1,17 @@
 package org.pmiops.workbench.db.dao;
 
 import org.pmiops.workbench.db.model.User;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface UserDao extends CrudRepository<User, Long> {
 
   User findUserByEmail(String email);
+
+  /**
+   * Returns the user with their authorities loaded.
+   */
+  @Query("SELECT user FROM User user LEFT JOIN FETCH user.authorities WHERE user.userId = :id")
+  User findUserWithAuthorities(@Param("id") long id);
 }
