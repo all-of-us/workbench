c250582d731ef0eddc550f1de1d0ed49e5b2522e
Dan Rodney <danrodney@google.com>
2017-10-18 00:44:09 -0500

    ARevert

--- api/src/main/java/org/pmiops/workbench/google/DirectoryServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/google/DirectoryServiceImpl.java
@@ -4,12 +4,16 @@ import static com.google.api.client.googleapis.util.Utils.getDefaultJsonFactory;
 import static com.google.api.client.googleapis.util.Utils.getDefaultTransport;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
+import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.admin.directory.Directory;
 import com.google.api.services.admin.directory.DirectoryScopes;
 import com.google.api.services.admin.directory.model.User;
 import com.google.api.services.admin.directory.model.UserName;
 import java.io.IOException;
+import java.lang.reflect.Field;
+import java.lang.reflect.InvocationTargetException;
+import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
 import java.util.logging.Logger;
@@ -47,18 +51,15 @@ public class DirectoryServiceImpl implements DirectoryService {
     this.configProvider = configProvider;
   }
 
+
   private GoogleCredential createCredentialWithImpersonation() {
     String gSuiteDomain = configProvider.get().googleDirectoryService.gSuiteDomain;
-    log.info("A");
     GoogleCredential credential = Utils.getDefaultGoogleCredential();
-    log.info("B = " + credential.getServiceAccountId());
-    credential = credential.createScoped(SCOPES);
-    log.info("B2 = " + credential.getServiceAccountId());
+    return credential.createScoped(SCOPES);
+
+    /*log.info("B2 = " + credential.getServiceAccountId());
     try {
       boolean refreshed = credential.refreshToken();
-      if (!refreshed) {
-        throw new RuntimeException("OMFG");
-      }
       log.info("C = " + credential.getServiceAccountId());
     } catch (IOException e) {
       throw new RuntimeException(e);
@@ -74,6 +75,7 @@ public class DirectoryServiceImpl implements DirectoryService {
         .setServiceAccountPrivateKey(credential.getServiceAccountPrivateKey())
         .setServiceAccountPrivateKeyId(credential.getServiceAccountPrivateKeyId())
         .setTokenServerEncodedUrl(credential.getTokenServerEncodedUrl()).build();
+    */
   }
 
   private Directory getGoogleDirectoryService() {
