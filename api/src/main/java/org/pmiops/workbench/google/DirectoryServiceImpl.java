a2458d0b4f0ab86f78a6294d7c377c47d7178b62
Dan Rodney <danrodney@google.com>
2017-10-18 00:47:00 -0500

    Revert

--- api/src/main/java/org/pmiops/workbench/google/DirectoryServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/google/DirectoryServiceImpl.java
@@ -4,22 +4,15 @@ import static com.google.api.client.googleapis.util.Utils.getDefaultJsonFactory;
 import static com.google.api.client.googleapis.util.Utils.getDefaultTransport;
 
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
-import com.google.api.client.googleapis.extensions.appengine.auth.oauth2.AppIdentityCredential;
 import com.google.api.client.googleapis.json.GoogleJsonResponseException;
 import com.google.api.services.admin.directory.Directory;
 import com.google.api.services.admin.directory.DirectoryScopes;
 import com.google.api.services.admin.directory.model.User;
 import com.google.api.services.admin.directory.model.UserName;
 import java.io.IOException;
-import java.lang.reflect.Field;
-import java.lang.reflect.InvocationTargetException;
-import java.lang.reflect.Method;
 import java.util.Arrays;
 import java.util.List;
-import java.util.logging.Logger;
 import javax.inject.Provider;
-
-import org.pmiops.workbench.api.WorkspacesController;
 import org.pmiops.workbench.config.WorkbenchConfig;
 import org.pmiops.workbench.google.Utils;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -27,8 +20,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class DirectoryServiceImpl implements DirectoryService {
-
-  private static final Logger log = Logger.getLogger(DirectoryServiceImpl.class.getName());
 
   static final String APPLICATION_NAME = "All of Us Researcher Workbench";
 
@@ -51,20 +42,9 @@ public class DirectoryServiceImpl implements DirectoryService {
     this.configProvider = configProvider;
   }
 
-
   private GoogleCredential createCredentialWithImpersonation() {
     String gSuiteDomain = configProvider.get().googleDirectoryService.gSuiteDomain;
     GoogleCredential credential = Utils.getDefaultGoogleCredential();
-    return credential.createScoped(SCOPES);
-
-    /*log.info("B2 = " + credential.getServiceAccountId());
-    try {
-      boolean refreshed = credential.refreshToken();
-      log.info("C = " + credential.getServiceAccountId());
-    } catch (IOException e) {
-      throw new RuntimeException(e);
-    }
-    log.info("D = " + credential.getServiceAccountId());
     return new GoogleCredential.Builder()
         .setTransport(getDefaultTransport())
         .setJsonFactory(getDefaultJsonFactory())
@@ -75,7 +55,6 @@ public class DirectoryServiceImpl implements DirectoryService {
         .setServiceAccountPrivateKey(credential.getServiceAccountPrivateKey())
         .setServiceAccountPrivateKeyId(credential.getServiceAccountPrivateKeyId())
         .setTokenServerEncodedUrl(credential.getTokenServerEncodedUrl()).build();
-    */
   }
 
   private Directory getGoogleDirectoryService() {
