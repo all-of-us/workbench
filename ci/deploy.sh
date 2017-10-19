a93a4a07ffb0cef5cb601f00a6c9522e4bd125fb
danqrodney <danrodney@google.com>
2017-10-19 13:03:23 -0500

    Attempt to fix api build (#210)

--- ci/deploy.sh
+++ ci/deploy.sh
@@ -1,15 +1,16 @@
 #!/bin/bash -e
 
-if [ -n "${CIRCLE_TAG}" ]
+if [ -z "${CIRCLE_TAG}" ]
 then
-  # For a tagged commit, deploy a version matching that tag. We tag some commits
-  # on master to tell CircleCI to deploy them as named versions.
-  VERSION=${CIRCLE_TAG}
-else
   # On test, CircleCI automatically deploys all commits to master, for testing.
   # These versions need not persist, so give them all the same name.
   VERSION=circle-ci-test
+else
+  # For a tagged commit, deploy a version matching that tag. We tag some commits
+  # on master to tell CircleCI to deploy them as named versions.
+  VERSION=${CIRCLE_TAG}
 fi
+echo "Version: ${VERSION}"
 
 ./ci/activate_creds.sh ~/gcloud-credentials.key
 if [ "$1" == "api" ]
