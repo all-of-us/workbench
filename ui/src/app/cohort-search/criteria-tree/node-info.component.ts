6f5f2b8687999efdad34bc751dd6a3ccbc5a2fd4
John Still <john.m.still@vanderbilt.edu>
2017-10-20 09:29:53 -0500

    FIX display error for criteria with a name but a null code

--- ui/src/app/cohort-search/criteria-tree/node-info.component.ts
+++ ui/src/app/cohort-search/criteria-tree/node-info.component.ts
@@ -1,6 +1,7 @@
 import {
   Component,
   ChangeDetectionStrategy,
+  HostBinding,
   Input,
   EventEmitter,
   Output,
@@ -12,7 +13,7 @@ import {
   template: `
     <div class="col-lg-8 col-md-8 col-sm-6 text-truncate">
       <small class="font-weight-bold">
-        {{ node.get('code', node.get('name')) }}
+        {{ displayName }}
       </small>
       <small *ngIf="node.get('code')" class="text-muted">
         {{ node.get('name') }}
@@ -20,14 +21,14 @@ import {
     </div>
 
     <div class="col-lg col-md col-sm text-right">
-      <span *ngIf="nonZeroCount()" class="badge badge-light-blue">
+      <span *ngIf="nonZeroCount" class="badge badge-light-blue">
         {{ node.get('count') }}
       </span>
     </div>
 
     <div class="col-lg-1 col-md-1 col-sm-1">
       <button
-        *ngIf="selectable()"
+        *ngIf="selectable"
         type="button"
         class="btn btn-sm btn-link"
         style="margin: 0 0 0 0;"
@@ -36,15 +37,15 @@ import {
       </button>
     </div>
   `,
-  host: {
-    '[class.clr-treenode-link]': '"true"',
-  }
 })
 export class CriteriaTreeNodeInfoComponent {
   @Input() node;
   @Output() onSelect = new EventEmitter<boolean>();
 
+  @HostBinding('class.clr-treenode-link') clrTreenodeLink = true;
+
   select(event) { this.onSelect.emit(true); }
-  nonZeroCount() { return this.node.get('count', 0) > 0; }
-  selectable() { return this.node.get('selectable', false); }
+  get nonZeroCount() { return this.node.get('count', 0) > 0; }
+  get selectable() { return this.node.get('selectable', false); }
+  get displayName() { return this.node.get('code') || this.node.get('name'); }
 }
