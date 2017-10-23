3900bb7651aefb14a114247846c5fa44950a0d18
Brian Freeman <brian.d.freeman@vumc.org>
2017-10-23 16:28:14 -0500

    Freemabd1/rw 190 - moved criteria trees from BQ to Cloud Sql (#223)
    * RW-190 creating db stuff for cdr for cloud sql.
    
    * RW-190 trying to create tables for critieria tree.
    
    * RW-190 added liquibase changesets for loading the icd9 criteria tree.
    
    * RW-190 tyring to write cdr db test cases.
    
    * RW-190 aded test case for criteria dao.
    
    * RW-190 aded test another test case and code cleanup.
    
    * RW-190 fixed test cases to use constructor of class CohortBuilderController.
    
    * RW-190 returning test case to original state.
    
    * RW-190 removed unused imports.
    
    * RW-190 add some changes to build the cdr cloud sql database onc ci build.
    
    * RW-190 change csv to become ordered by id(removed sort_order column) and fixed test cases.

--- api/src/test/java/org/pmiops/workbench/testconfig/TestBigQueryConfig.java
+++ api/src/test/java/org/pmiops/workbench/testconfig/TestBigQueryConfig.java
@@ -1,4 +1,4 @@
-package org.pmiops.workbench.api.config;
+package org.pmiops.workbench.testconfig;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
