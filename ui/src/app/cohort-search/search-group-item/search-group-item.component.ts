7e30cd24b2dd381a6e8120fdce39ce63b474d575
johnstill <31938607+johnstill@users.noreply.github.com>
2017-10-25 15:15:55 -0500

    Johnstill/rw 193 codes tooltip (#232)
    * display the codes in a search group item
    
    * tooltips and truncating code display
    
    * change color of item on hover to blueish
    
    * Pluralization, proper name for Demo codes, wider tooltip
    
    * tweak

--- ui/src/app/cohort-search/search-group-item/search-group-item.component.ts
+++ ui/src/app/cohort-search/search-group-item/search-group-item.component.ts
@@ -1,11 +1,42 @@
 import {Component, Input} from '@angular/core';
 import {NgRedux} from '@angular-redux/store';
 
-import {CohortSearchActions, CohortSearchState, getItem} from '../redux';
+import {
+  CohortSearchActions,
+  CohortSearchState,
+  getItem,
+  parameterList
+} from '../redux';
+
+const getDisplayName = (criteria) =>
+  criteria.get('type').match(/^DEMO.*/i)
+    ?  criteria.get('name')
+    : criteria.get('code');
 
 @Component({
   selector: 'app-search-group-item',
   templateUrl: './search-group-item.component.html',
+  styles: [`
+    .flex-container {
+      display: flex;
+    }
+    .flex-container > clr-tooltip {
+      min-width: 0;
+      flex: 4;
+    }
+    .flex-container > div.count {
+      flex: 1;
+      text-align: right;
+    }
+    .line-item {
+      overflow: hidden;
+      white-space: nowrap;
+      text-overflow: ellipsis;
+    }
+    .trigger:hover > span {
+      color: rgb(0, 124, 187)!important;
+    }
+  `]
 })
 export class SearchGroupItemComponent {
   @Input() role: string;
@@ -16,9 +47,13 @@ export class SearchGroupItemComponent {
   constructor(private ngRedux: NgRedux<CohortSearchState>,
               private actions: CohortSearchActions) {}
 
-  get description() {
-    const _type = this.item.get('type');
-    return this.item.get('description', `${_type} Codes`);
+  get codeType() {
+    const _type = this.item.get('type').toUpperCase();
+    return this.item.get('description', `${_type} ${this.pluralizedCode}`);
+  }
+
+  get pluralizedCode() {
+    return this._rawCodes.count() > 1 ? 'Codes' : 'Code';
   }
 
   get item() {
@@ -27,6 +62,14 @@ export class SearchGroupItemComponent {
 
   get isRequesting() {
     return this.item.get('isRequesting', false);
+  }
+
+  get _rawCodes() {
+    return parameterList(this.itemId)(this.ngRedux.getState());
+  }
+
+  get codes() {
+    return this._rawCodes.map(getDisplayName).join(', ');
   }
 
   launchWizard() {
