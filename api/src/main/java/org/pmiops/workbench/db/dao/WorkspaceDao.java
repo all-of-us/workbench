bafb6f662ed078e405d3022bde3378697dfcf984
Mark Fickett <mwf@google.com>
2017-10-25 09:56:16 -0400

    Add pointer to README in WorkspaceDao.

--- api/src/main/java/org/pmiops/workbench/db/dao/WorkspaceDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/WorkspaceDao.java
@@ -5,7 +5,12 @@ import org.pmiops.workbench.db.model.User;
 import org.pmiops.workbench.db.model.Workspace;
 import org.springframework.data.repository.CrudRepository;
 
-public interface WorkspaceDao extends CrudRepository<Workspace, Long> {
+
+/**
+ * Declaration of automatic query methods for Workspaces. The methods declared here are
+ * automatically interpreted by Spring Data (see README).
+ */
+public interface WorkspaceDao extends CrudRepository<Workspace, Long>, WorkspaceDaoCustom {
   List<Workspace> findByWorkspaceNamespace(String workspaceNamespace);
   Workspace findByWorkspaceNamespaceAndFirecloudName(String workspaceNamespace,
       String firecloudName);
