9a0c14f633213a8b31db453a9c83aee6fc35d2bb
Mark Fickett <mwf@google.com>
2017-10-30 12:27:16 -0400

    [RW-183] Create WorkspaceService to handle research purpose review. (#239)
    * Factor out WorkspaceService (wrapping WorkspaceDao).
    * Combine copies of 'get workspace and throw if it doesn not exist'.
    * Add methods for research purpose review, and associated tests.
    * Incidental fix of string formatting in exception messages (in WS files only).
    
    TODOs:
     * `@Version` to prevent conflicting edits. RW-215
     * Fix remaining string formatting in exceptions. RW-217
     * Remove remaining redundant Java index declarations. RW-220

--- api/src/main/java/org/pmiops/workbench/db/dao/README.md
+++ api/src/main/java/org/pmiops/workbench/db/dao/README.md
@@ -1,2 +1,6 @@
 DAOs' query implementations are
 [automatically derived](https://docs.spring.io/spring-data/jpa/docs/current/reference/html/#repositories.query-methods.details).
+
+Services implement custom behavior which can't be auto-generated. Where both a
+Service and a DAO are available, inject the Service and use its public dao field
+where necessary. [Design doc](https://docs.google.com/document/d/1uNf6_5TZxnQt8BP_wWoGxPcGwaIZAhZswW3bXwbswHU).
--- api/src/main/java/org/pmiops/workbench/db/dao/WorkspaceDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/WorkspaceDao.java
@@ -4,10 +4,19 @@ import java.util.List;
 import org.pmiops.workbench.db.model.User;
 import org.pmiops.workbench.db.model.Workspace;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.jpa.repository.Query;
 
+
+/**
+ * Declaration of automatic query methods for Workspaces. The methods declared here are
+ * automatically interpreted by Spring Data (see README).
+ *
+ * Use of @Query is discouraged; if desired, define aliases in WorkspaceService.
+ */
 public interface WorkspaceDao extends CrudRepository<Workspace, Long> {
+  Workspace findByWorkspaceNamespaceAndFirecloudName(
+      String workspaceNamespace, String firecloudName);
   List<Workspace> findByWorkspaceNamespace(String workspaceNamespace);
-  Workspace findByWorkspaceNamespaceAndFirecloudName(String workspaceNamespace,
-      String firecloudName);
   List<Workspace> findByCreatorOrderByNameAsc(User creator);
+  List<Workspace> findByApprovedIsNullAndReviewRequestedTrueOrderByTimeRequested();
 }
