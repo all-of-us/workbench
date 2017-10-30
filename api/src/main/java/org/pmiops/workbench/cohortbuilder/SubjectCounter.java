e88acc5019b44c60875eebf8b0f0145d419ad773
Brian <brian.d.freeman@vumc.org>
2017-10-30 13:40:25 -0500

    RW-208 fixing bug with request only containing excludes.

--- api/src/main/java/org/pmiops/workbench/cohortbuilder/SubjectCounter.java
+++ api/src/main/java/org/pmiops/workbench/cohortbuilder/SubjectCounter.java
@@ -4,6 +4,7 @@ import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.cloud.bigquery.QueryParameterValue;
 import org.pmiops.workbench.cohortbuilder.querybuilder.FactoryKey;
 import org.pmiops.workbench.cohortbuilder.querybuilder.QueryParameters;
+import org.pmiops.workbench.exceptions.BadRequestException;
 import org.pmiops.workbench.model.SearchGroup;
 import org.pmiops.workbench.model.SearchGroupItem;
 import org.pmiops.workbench.model.SearchRequest;
@@ -44,38 +45,20 @@ public class SubjectCounter {
      * defined by the provided {@link SearchRequest}.
      */
     public QueryJobConfiguration buildSubjectCounterQuery(SearchRequest request) {
+        if(request.getIncludes().isEmpty() && request.getExcludes().isEmpty()) {
+            throw new BadRequestException("Invalid SearchRequest: includes[] and excludes[] cannot both be empty");
+        }
         Map<String, QueryParameterValue> params = new HashMap<>();
         List<String> queryParts = new ArrayList<>();
 
         // build query for included search groups
-        StringJoiner joiner = new StringJoiner("and ");
-        for (SearchGroup includeGroup : request.getIncludes()) {
-            for (SearchGroupItem includeItem : includeGroup.getItems()) {
-                QueryJobConfiguration queryRequest = QueryBuilderFactory
-                        .getQueryBuilder(FactoryKey.getType(includeItem.getType()))
-                        .buildQueryJobConfig(new QueryParameters()
-                                .type(includeItem.getType())
-                                .parameters(includeItem.getSearchParameters()));
-                params.putAll(queryRequest.getNamedParameters());
-                queryParts.add(queryRequest.getQuery());
-            }
-            joiner.add(INCLUDE_SQL_TEMPLATE.replace("${includeSql}", String.join(UNION_TEMPLATE, queryParts)));
-            queryParts = new ArrayList<>();
-        }
+        StringJoiner joiner = buildQuery(request.getIncludes(), params, false);
 
-        // build query for excluded search groups
-        for (SearchGroup excludeGroup : request.getExcludes()) {
-            for (SearchGroupItem excludeItem : excludeGroup.getItems()) {
-                QueryJobConfiguration queryRequest = QueryBuilderFactory
-                        .getQueryBuilder(FactoryKey.getType(excludeItem.getType()))
-                        .buildQueryJobConfig(new QueryParameters()
-                                .type(excludeItem.getType())
-                                .parameters(excludeItem.getSearchParameters()));
-                params.putAll(queryRequest.getNamedParameters());
-                queryParts.add(queryRequest.getQuery());
-            }
-            joiner.add(EXCLUDE_SQL_TEMPLATE.replace("${excludeSql}", String.join(UNION_TEMPLATE, queryParts)));
-            queryParts = new ArrayList<>();
+        // if includes is empty then don't add the excludes clause
+        if (joiner.toString().isEmpty()) {
+            joiner.merge(buildQuery(request.getExcludes(), params, false));
+        } else {
+            joiner.merge(buildQuery(request.getExcludes(), params, true));
         }
 
         String finalSql = COUNT_SQL_TEMPLATE + joiner.toString();
@@ -85,5 +68,28 @@ public class SubjectCounter {
                         .setNamedParameters(params)
                         .setUseLegacySql(false)
                         .build();
+    }
+
+    private StringJoiner buildQuery(List<SearchGroup> groups, Map<String, QueryParameterValue> params, Boolean excludeSQL) {
+        StringJoiner joiner = new StringJoiner("and ");
+        List<String> queryParts = new ArrayList<>();
+        for (SearchGroup includeGroup : groups) {
+            for (SearchGroupItem includeItem : includeGroup.getItems()) {
+                QueryJobConfiguration queryRequest = QueryBuilderFactory
+                        .getQueryBuilder(FactoryKey.getType(includeItem.getType()))
+                        .buildQueryJobConfig(new QueryParameters()
+                                .type(includeItem.getType())
+                                .parameters(includeItem.getSearchParameters()));
+                params.putAll(queryRequest.getNamedParameters());
+                queryParts.add(queryRequest.getQuery());
+            }
+            if (excludeSQL) {
+                joiner.add(EXCLUDE_SQL_TEMPLATE.replace("${excludeSql}", String.join(UNION_TEMPLATE, queryParts)));
+            } else {
+                joiner.add(INCLUDE_SQL_TEMPLATE.replace("${includeSql}", String.join(UNION_TEMPLATE, queryParts)));
+            }
+            queryParts = new ArrayList<>();
+        }
+        return joiner;
     }
 }
