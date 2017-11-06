d377dcf828d62019ad599fcb7e2fad7e44057bf4
John Still <john.m.still@vanderbilt.edu>
2017-11-06 12:00:16 -0600

    fix up the tests

--- ui/src/app/cohort-search/search-group-list/search-group-list.component.spec.ts
+++ ui/src/app/cohort-search/search-group-list/search-group-list.component.spec.ts
@@ -1,25 +1,67 @@
-import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {async as _async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
+import {DebugElement} from '@angular/core';
+import {ClarityModule} from 'clarity-angular';
+import {NgRedux, dispatch} from '@angular-redux/store';
+import {MockNgRedux} from '@angular-redux/store/testing';
+import {List, fromJS} from 'immutable';
+import {Observable} from 'rxjs/Observable';
 
-import { SearchGroupListComponent } from './search-group-list.component';
+import {CohortSearchActions} from '../redux';
+import {SearchGroupComponent} from '../search-group/search-group.component';
+import {SearchGroupItemComponent} from '../search-group-item/search-group-item.component';
+import {CohortBuilderService} from 'generated';
+
+import {SearchGroupListComponent} from './search-group-list.component';
+
+class MockActions {
+}
 
 describe('SearchGroupListComponent', () => {
-  let component: SearchGroupListComponent;
   let fixture: ComponentFixture<SearchGroupListComponent>;
+  let component: SearchGroupListComponent;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ SearchGroupListComponent ]
-    })
-    .compileComponents();
+  let mockReduxInst;
+
+  beforeEach(_async(() => {
+    mockReduxInst = MockNgRedux.getInstance();
+    const _old = mockReduxInst.getState;
+    const _wrapped = () => fromJS(_old());
+    mockReduxInst.getState = _wrapped;
+
+    TestBed
+      .configureTestingModule({
+        declarations: [
+          SearchGroupListComponent,
+          SearchGroupComponent,
+          SearchGroupItemComponent,
+        ],
+        imports: [
+          ClarityModule,
+        ],
+        providers: [
+          {provide: NgRedux, useValue: mockReduxInst},
+          {provide: CohortBuilderService, useValue: {}},
+          {provide: CohortSearchActions, useValue: new MockActions()},
+        ],
+      })
+      .compileComponents();
   }));
 
   beforeEach(() => {
+    MockNgRedux.reset();
+
     fixture = TestBed.createComponent(SearchGroupListComponent);
     component = fixture.componentInstance;
+
+    // Default Inputs for tests
+    component.role = 'includes';
+    component.groups$ = Observable.of(List());
+
     fixture.detectChanges();
   });
 
-  it('should create', () => {
+  it('Should render', () => {
     expect(component).toBeTruthy();
   });
 });
