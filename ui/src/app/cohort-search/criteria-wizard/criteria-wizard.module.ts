810d6fbffe7c69d7016d93edf19f40abe447918c
John Still <john.m.still@vanderbilt.edu>
2017-11-10 14:45:42 -0600

    basic search bar in place

--- ui/src/app/cohort-search/criteria-wizard/criteria-wizard.module.ts
+++ ui/src/app/cohort-search/criteria-wizard/criteria-wizard.module.ts
@@ -1,9 +1,12 @@
-import {NgModule} from '@angular/core';
-import {CommonModule} from '@angular/common';
+import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {ClarityModule} from 'clarity-angular';
+import {CommonModule} from '@angular/common';
+import {NgModule} from '@angular/core';
+import {ReactiveFormsModule} from '@angular/forms';
 
 import {AttributesModule} from './attributes/attributes.module';
 import {AlertsComponent} from './alerts/alerts.component';
+import {FuzzyFinderComponent} from './fuzzy-finder/fuzzy-finder.component';
 import {RootSpinnerComponent} from './root-spinner/root-spinner.component';
 import {SelectionComponent} from './selection/selection.component';
 import {TreeComponent} from './tree/tree.component';
@@ -13,12 +16,15 @@ import {WizardComponent} from './wizard/wizard.component';
 @NgModule({
   imports: [
     AttributesModule,
+    BrowserAnimationsModule,
     CommonModule,
     ClarityModule,
+    ReactiveFormsModule,
   ],
   exports: [WizardComponent],
   declarations: [
     AlertsComponent,
+    FuzzyFinderComponent,
     RootSpinnerComponent,
     SelectionComponent,
     TreeComponent,
