28567e19164e9043b98dc188686c1b3a6cf6f561
John Still <john.m.still@vanderbilt.edu>
2017-11-17 12:23:01 -0600

    linting

--- client/py/test_aou_workbench_client.py
+++ client/py/test_aou_workbench_client.py
@@ -7,6 +7,6 @@ import unittest
 class ModuleTest(unittest.TestCase):
     def test_module_imports(self):
         try:
-            import aou_workbench_client
+            import aou_workbench_client  #pylint: disable=unused-variable
         except RuntimeError:
             self.fail()
