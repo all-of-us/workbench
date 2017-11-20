5ad30f86b7e5f7d53aa4b94656d4260edd2a8236
David Mohs <dgit@mohsinc.com>
2017-11-20 12:24:36 -0800

    Connect to cloud DB via docker (#250)
    * Connect to cloud DB via docker
    * New approach
    * Require --project parameter and document why
    * DRY per Dan
    * Better suggestion for account
    * Don't store database credentials
    * docker stop/rm instead of down

--- api/src/dev/server/with-uid.sh
+++ api/src/dev/server/with-uid.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-if [[ "$UID" == 0 ]]; then
-  echo 'UID is not set. Exiting.';
+if [[ "$(whoami 2>/dev/null)" == 'root' ]]; then
+  >&2 echo 'This container should not be run as the root user. Exiting.';
   exit 1;
 fi
 
