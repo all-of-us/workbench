3ef0e0c7181dcba02a908d69cc749d283d8c5c66
John Still <john.m.still@vanderbilt.edu>
2017-11-28 12:30:08 -0600

    unnecessary router module

--- ui/src/app/cohort-review/cohort-review.module.ts
+++ ui/src/app/cohort-review/cohort-review.module.ts
@@ -1,16 +1,28 @@
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
+import {RouterModule} from '@angular/router';
 import {ClarityModule} from 'clarity-angular';
 
 import {CohortReviewComponent} from './cohort-review/cohort-review.component';
 
 import {CohortReviewRouter} from './router.module';
 
+const routes = [{
+    path: 'workspace/:ns/:wsid/cohorts/:cid/review',
+    component: CohortReviewComponent,
+    data: {title: 'Review Cohort Subjects'}
+  }, {
+    path: 'workspace/:ns/:wsid/cohorts/:cid/review/:subjectid',
+    component: CohortReviewComponent,
+    data: {title: 'Review Cohort Subjects'}
+}];
+
 @NgModule({
   imports: [
     ClarityModule,
     CohortReviewRouter,
     CommonModule,
+    RouterModule.forChild(routes),
   ],
   declarations: [
     CohortReviewComponent,
