42b67cdc53b0ea1e052888ad8a8d263f8ceb08e0
John Still <john.m.still@vanderbilt.edu>
2017-11-28 11:43:49 -0600

    clean slate

--- ui/src/app/cohort-review/cohort-review.module.ts
+++ ui/src/app/cohort-review/cohort-review.module.ts
@@ -2,11 +2,7 @@ import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {ClarityModule} from 'clarity-angular';
 
-import {AnnotationsComponent} from './annotations/annotations.component';
 import {CohortReviewComponent} from './cohort-review/cohort-review.component';
-import {MedicationsComponent} from './medications/medications.component';
-import {SubjectDetailComponent} from './subject-detail/subject-detail.component';
-import {SubjectListComponent} from './subject-list/subject-list.component';
 
 import {CohortReviewRouter} from './router.module';
 
@@ -17,11 +13,7 @@ import {CohortReviewRouter} from './router.module';
     CommonModule,
   ],
   declarations: [
-    AnnotationsComponent,
     CohortReviewComponent,
-    MedicationsComponent,
-    SubjectDetailComponent,
-    SubjectListComponent,
   ]
 })
 export class CohortReviewModule {}
