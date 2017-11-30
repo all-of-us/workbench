728357a03b69d15f3ab42e54fbcc455437ba2c2d
Brian <brian.d.freeman@vumc.org>
2017-11-30 10:18:52 -0600

    RW-272 implemented test case for CohortReviewControllerTest.

--- api/src/test/java/org/pmiops/workbench/api/CohortReviewControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/CohortReviewControllerTest.java
@@ -3,17 +3,27 @@ package org.pmiops.workbench.api;
 import static org.junit.Assert.*;
 import static org.mockito.Mockito.*;
 
+import com.google.cloud.bigquery.FieldValue;
+import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.QueryResult;
+import com.google.gson.Gson;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
 
 import org.mockito.runners.MockitoJUnitRunner;
+import org.pmiops.workbench.cohortbuilder.SubjectCounter;
+import org.pmiops.workbench.db.dao.CohortDao;
+import org.pmiops.workbench.db.dao.CohortReviewDao;
 import org.pmiops.workbench.db.dao.ParticipantCohortStatusDao;
+import org.pmiops.workbench.db.model.CohortDefinition;
+import org.pmiops.workbench.db.model.CohortReview;
 import org.pmiops.workbench.db.model.ParticipantCohortStatus;
 import org.pmiops.workbench.db.model.ParticipantCohortStatusKey;
 import org.pmiops.workbench.model.CohortStatus;
 import org.pmiops.workbench.model.ParticipantCohortStatusListResponse;
+import org.pmiops.workbench.model.SearchRequest;
 import org.springframework.data.domain.Page;
 import org.springframework.data.domain.PageImpl;
 import org.springframework.data.domain.PageRequest;
@@ -21,7 +31,11 @@ import org.springframework.data.domain.Sort;
 import org.springframework.http.ResponseEntity;
 
 import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.HashMap;
+import java.util.Iterator;
 import java.util.List;
+import java.util.Map;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CohortReviewControllerTest {
@@ -29,8 +43,84 @@ public class CohortReviewControllerTest {
     @Mock
     ParticipantCohortStatusDao participantCohortStatusDao;
 
+    @Mock
+    CohortReviewDao cohortReviewDao;
+
+    @Mock
+    CohortDao cohortDao;
+
+    @Mock
+    BigQueryService bigQueryService;
+
+    @Mock
+    SubjectCounter subjectCounter;
+
     @InjectMocks
     CohortReviewController reviewController;
+
+    @Test
+    public void getCohortReviewInfo_ReviewExists() throws Exception {
+        long cohortId = 1;
+        long cdrVersionId = 1;
+
+        CohortReview cohortReview = new CohortReview().cohortReviewId(1).matchedParticipantCount(1000);
+
+        when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(cohortReview);
+
+        reviewController.getCohortReviewInfo(cohortId, cdrVersionId);
+
+        verify(cohortReviewDao, times(1)).findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
+        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, subjectCounter);
+    }
+
+    @Test
+    public void getCohortReviewInfo_ReviewNotExists() throws Exception {
+        long cohortId = 1;
+        long cdrVersionId = 1;
+
+        CohortDefinition definition = new CohortDefinition() {
+            @Override
+            public String getCriteria() {
+                return "{\"includes\":[{\"items\":[{\"type\":\"DEMO\",\"searchParameters\":" +
+                        "[{\"value\":\"Age\",\"subtype\":\"AGE\",\"conceptId\":null,\"attribute\":" +
+                        "{\"operator\":\"between\",\"operands\":[18,66]}}],\"modifiers\":[]}]}],\"excludes\":[]}";
+            }
+        };
+
+        SearchRequest request = new Gson().fromJson(definition.getCriteria(), SearchRequest.class);
+        QueryResult queryResult = mock(QueryResult.class);
+        Iterable testIterable = new Iterable() {
+            @Override
+            public Iterator iterator() {
+                List<FieldValue> list = new ArrayList<>();
+                list.add(null);
+                return list.iterator();
+            }
+        };
+        Map<String, Integer> rm = new HashMap<>();
+        rm.put("count", 0);
+
+        when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(null);
+        when(cohortDao.findCohortByCohortId(cohortId)).thenReturn(definition);
+        when(subjectCounter.buildSubjectCounterQuery(request)).thenReturn(null);
+        when(bigQueryService.filterBigQueryConfig(null)).thenReturn(null);
+        when(bigQueryService.executeQuery(null)).thenReturn(queryResult);
+        when(bigQueryService.getResultMapper(queryResult)).thenReturn(rm);
+        when(queryResult.iterateAll()).thenReturn(testIterable);
+        when(bigQueryService.getLong(null, 0)).thenReturn(1000L);
+
+        reviewController.getCohortReviewInfo(cohortId, cdrVersionId);
+
+        verify(cohortReviewDao, times(1)).findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
+        verify(cohortDao, times(1)).findCohortByCohortId(cohortId);
+        verify(subjectCounter, times(1)).buildSubjectCounterQuery(request);
+        verify(bigQueryService, times(1)).filterBigQueryConfig(null);
+        verify(bigQueryService, times(1)).executeQuery(null);
+        verify(bigQueryService, times(1)).getResultMapper(queryResult);
+        verify(bigQueryService, times(1)).getLong(null, 0);
+        verify(queryResult, times(1)).iterateAll();
+        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, subjectCounter);
+    }
 
     @Test
     public void getParticipants() throws Exception {
