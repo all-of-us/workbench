cc2ea3259edb92f6014819b9906032a2ad2c4153
John Still <john.m.still@vanderbilt.edu>
2017-12-06 15:29:22 -0600

    cohort review frame

--- ui/src/app/cohort-review/cohort-review/cohort-review.component.ts
+++ ui/src/app/cohort-review/cohort-review/cohort-review.component.ts
@@ -1,6 +1,4 @@
-// TODO(jms) - remove the linting silencer when this is beyond a stub
-/* tslint:disable:no-unused-variable */
-import {Component} from '@angular/core';
+import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 
@@ -8,10 +6,21 @@ import {CohortReviewService} from 'generated';
 
 const CDR_VERSION = 1;
 
+const pixel = (n: number) => `${n}px`;
+
 const subjects$ = Observable.of([
   {id: 1, status: 'NR'},
   {id: 2, status: 'NR'},
-  {id: 3, status: 'NR'},
+  {id: 4, status: 'NR'},
+  {id: 5, status: 'NR'},
+  {id: 5, status: 'NR'},
+  {id: 6, status: 'NR'},
+  {id: 7, status: 'NR'},
+  {id: 8, status: 'NR'},
+  {id: 9, status: 'NR'},
+  {id: 10, status: 'NR'},
+  {id: 11, status: 'NR'},
+  {id: 12, status: 'NR'},
 ]);
 
 @Component({
@@ -19,15 +28,38 @@ const subjects$ = Observable.of([
   templateUrl: './cohort-review.component.html',
   styleUrls: ['./cohort-review.component.css']
 })
-export class CohortReviewComponent {
-  private loading: boolean;
-  private collapsed = false;
-
+export class CohortReviewComponent implements OnInit {
   private subjects$ = subjects$;
-  private activeSubject$;
+  private open = false;
 
-  constructor(
-    route: ActivatedRoute,
-    reviewApi: CohortReviewService,
-  ) {}
+  @ViewChild('wrapper') _wrapper;
+  @ViewChild('subjectNav') _subjectNav;
+  @ViewChild('openNav') _openNav;
+
+  get wrapper() { return this._wrapper.nativeElement; }
+  get openNav() { return this._openNav.nativeElement; }
+  get subjectNav() { return this._subjectNav.nativeElement; }
+
+  ngOnInit() {
+    this._updateWrapperDimensions();
+  }
+
+  @HostListener('document:click', ['$event'])
+  onClick(event) {
+    if (this.subjectNav.contains(event.target)
+        || this.openNav.contains(event.target)) {
+      return;
+    }
+    this.open = false;
+  }
+
+  @HostListener('window:resize')
+  onResize() {
+    this._updateWrapperDimensions();
+  }
+
+  _updateWrapperDimensions() {
+    const {top} = this.wrapper.getBoundingClientRect();
+    this.wrapper.style.minHeight = pixel(window.innerHeight - top);
+  }
 }
