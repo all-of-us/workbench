35180d74c9fe1baa24ef278653e316a27573eb77
CH Albach <calbach@google.com>
2017-12-06 15:31:27 -0800

    Update UI deployment to match API; rename script

--- ci/deploy_ui.sh
+++ ci/deploy_ui.sh
@@ -7,13 +7,15 @@ then
   # On test, CircleCI automatically deploys all commits to master, for testing.
   # These versions need not persist, so give them all the same name.
   VERSION=circle-ci-test
+  PROMOTE=--promote
 else
   # For a tagged commit, deploy a version matching that tag. We tag some commits
   # on master to tell CircleCI to deploy them as named versions.
   VERSION=${CIRCLE_TAG}
+  PROMOTE=--no-promote
 fi
 echo "Version: ${VERSION}"
 
 (cd ./ui && ./project.rb deploy-ui --project all-of-us-workbench-test \
   --account circle-deploy-account@all-of-us-workbench-test.iam.gserviceaccount.com \
-  --version $VERSION --promote)
+  --version $VERSION $PROMOTE)
