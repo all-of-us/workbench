4f89b36e639b99bba6796035ee4b9004b1875509
John Still <john.m.still@vanderbilt.edu>
2017-12-07 10:24:06 -0600

    subject master list roughed in

--- ui/src/app/cohort-review/cohort-review/cohort-review.component.ts
+++ ui/src/app/cohort-review/cohort-review/cohort-review.component.ts
@@ -2,25 +2,24 @@ import {Component, HostListener, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 
-import {CohortReviewService} from 'generated';
+import {CohortReviewService, CohortStatus} from 'generated';
 
 const CDR_VERSION = 1;
 
 const pixel = (n: number) => `${n}px`;
 
 const subjects$ = Observable.of([
-  {id: 1, status: 'NR'},
-  {id: 2, status: 'NR'},
-  {id: 4, status: 'NR'},
-  {id: 5, status: 'NR'},
-  {id: 5, status: 'NR'},
-  {id: 6, status: 'NR'},
-  {id: 7, status: 'NR'},
-  {id: 8, status: 'NR'},
-  {id: 9, status: 'NR'},
-  {id: 10, status: 'NR'},
-  {id: 11, status: 'NR'},
-  {id: 12, status: 'NR'},
+  {id: 1, status: CohortStatus.INCLUDED},
+  {id: 2, status: CohortStatus.INCLUDED},
+  {id: 4, status: CohortStatus.EXCLUDED},
+  {id: 5, status: CohortStatus.NEEDSFURTHERREVIEW},
+  {id: 6, status: CohortStatus.NEEDSFURTHERREVIEW},
+  {id: 7, status: CohortStatus.EXCLUDED},
+  {id: 8, status: CohortStatus.NOTREVIEWED},
+  {id: 9, status: CohortStatus.NOTREVIEWED},
+  {id: 10, status: CohortStatus.EXCLUDED},
+  {id: 11, status: CohortStatus.NOTREVIEWED},
+  {id: 12, status: CohortStatus.EXCLUDED},
 ]);
 
 @Component({
@@ -61,5 +60,24 @@ export class CohortReviewComponent implements OnInit {
   _updateWrapperDimensions() {
     const {top} = this.wrapper.getBoundingClientRect();
     this.wrapper.style.minHeight = pixel(window.innerHeight - top);
+  }
+
+  statusText(stat: CohortStatus): string {
+    return {
+      [CohortStatus.EXCLUDED]: 'Excluded',
+      [CohortStatus.INCLUDED]: 'Included',
+      [CohortStatus.NEEDSFURTHERREVIEW]: 'Undecided',
+      [CohortStatus.NOTREVIEWED]: 'Unreviewed',
+    }[stat];
+  }
+
+  statusClass(stat: CohortStatus) {
+    if (stat === CohortStatus.INCLUDED) {
+      return {'label-success': true};
+    } else if (stat === CohortStatus.EXCLUDED) {
+      return {'label-warning': true};
+    } else {
+      return {'label-info': true};
+    }
   }
 }
