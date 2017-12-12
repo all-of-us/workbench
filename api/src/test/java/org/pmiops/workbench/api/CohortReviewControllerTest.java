6235698b67b6a1cca7717edfdd178fd711d805b4
Brian Freeman <brian.d.freeman@vumc.org>
2017-12-12 14:48:39 -0600

    Freemabd1/rw 112 implemented the update of review statuses. (#342)
    * added test case for WorkspaceDao.
    
    * RW-112 implementing call for update on status.
    
    * RW-112 added cdr version id on ui.
    
    * RW-112 cleanup.
    
    * RW-112 cleanup.
    
    * RW-112 more cleanup.
    
    * RW-112 injected WorkspaceService instead of workspaceDao.
    
    * RW-112 added comment.
    
    * RW-112 refactored to consolidate exception on cohort definition.
    
    * RW-112 make sure CohortReview object is updated when status is updated.

--- api/src/test/java/org/pmiops/workbench/api/CohortReviewControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/CohortReviewControllerTest.java
@@ -1,8 +1,5 @@
 package org.pmiops.workbench.api;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
-
 import com.google.cloud.bigquery.FieldValue;
 import com.google.cloud.bigquery.QueryResult;
 import com.google.gson.Gson;
@@ -10,13 +7,12 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.mockito.InjectMocks;
 import org.mockito.Mock;
-
 import org.mockito.runners.MockitoJUnitRunner;
 import org.pmiops.workbench.cohortbuilder.ParticipantCounter;
 import org.pmiops.workbench.db.dao.CohortDao;
 import org.pmiops.workbench.db.dao.CohortReviewDao;
-import org.pmiops.workbench.db.dao.WorkspaceDao;
 import org.pmiops.workbench.db.dao.ParticipantCohortStatusDao;
+import org.pmiops.workbench.db.dao.WorkspaceService;
 import org.pmiops.workbench.db.model.Cohort;
 import org.pmiops.workbench.db.model.CohortReview;
 import org.pmiops.workbench.db.model.ParticipantCohortStatus;
@@ -25,6 +21,7 @@ import org.pmiops.workbench.db.model.Workspace;
 import org.pmiops.workbench.exceptions.BadRequestException;
 import org.pmiops.workbench.model.CohortStatus;
 import org.pmiops.workbench.model.CreateReviewRequest;
+import org.pmiops.workbench.model.ModifyCohortStatusRequest;
 import org.pmiops.workbench.model.ReviewStatus;
 import org.pmiops.workbench.model.SearchRequest;
 import org.springframework.data.domain.Page;
@@ -40,6 +37,10 @@ import java.util.HashMap;
 import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.fail;
+import static org.mockito.Mockito.*;
 
 @RunWith(MockitoJUnitRunner.class)
 public class CohortReviewControllerTest {
@@ -60,7 +61,7 @@ public class CohortReviewControllerTest {
     ParticipantCounter participantCounter;
 
     @Mock
-    WorkspaceDao workspaceDao;
+    WorkspaceService workspaceService;
 
     @Mock
     CodeDomainLookupService codeDomainLookupService;
@@ -82,13 +83,14 @@ public class CohortReviewControllerTest {
 
         try {
             reviewController.createCohortReview(namespace, name, cohortId, cdrVersionId, new CreateReviewRequest().size(200));
+            fail("Should have thrown a BadRequestException!");
         } catch (BadRequestException e) {
             assertEquals("Invalid Request: Cohort Review already created for cohortId: "
                     + cohortId + ", cdrVersionId: " + cdrVersionId, e.getMessage());
         }
 
         verify(cohortReviewDao, times(1)).findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
-        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, participantCounter);
+        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, workspaceService, participantCounter);
     }
 
     @Test
@@ -100,11 +102,12 @@ public class CohortReviewControllerTest {
 
         try {
             reviewController.createCohortReview(namespace, name, cohortId, cdrVersionId, new CreateReviewRequest().size(20000));
+            fail("Should have thrown a BadRequestException!");
         } catch (BadRequestException e) {
             assertEquals("Invalid Request: Cohort Review size must be between 0 and 10000", e.getMessage());
         }
 
-        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, participantCounter);
+        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, workspaceService, participantCounter);
     }
 
     @Test
@@ -118,13 +121,14 @@ public class CohortReviewControllerTest {
 
         try {
             reviewController.createCohortReview(namespace, name, cohortId, cdrVersionId, new CreateReviewRequest().size(200));
+            fail("Should have thrown a BadRequestException!");
         } catch (BadRequestException e) {
             assertEquals("Invalid Request: Cohort Review does not exist for cohortId: "
                     + cohortId + ", cdrVersionId: " + cdrVersionId, e.getMessage());
         }
 
         verify(cohortReviewDao, times(1)).findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
-        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, participantCounter);
+        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, workspaceService, participantCounter);
     }
 
     @Test
@@ -135,7 +139,9 @@ public class CohortReviewControllerTest {
         long cdrVersionId = 1;
 
         CohortReview cohortReview = new CohortReview();
+        cohortReview.setCohortId(cohortId);
         Cohort cohort = new Cohort();
+        cohort.setCohortId(cohortId);
         cohort.setWorkspaceId(0);
 
         Workspace workspace = new Workspace();
@@ -145,22 +151,21 @@ public class CohortReviewControllerTest {
 
         when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(cohortReview);
         when(cohortDao.findOne(cohortId)).thenReturn(cohort);
-        when(workspaceDao.findOne(cohort.getWorkspaceId())).thenReturn(workspace);
+        when(workspaceService.getRequired(namespace, name)).thenReturn(workspace);
 
         try {
             reviewController.createCohortReview(namespace, name, cohortId, cdrVersionId, new CreateReviewRequest().size(200));
+            fail("Should have thrown a BadRequestException!");
         } catch (BadRequestException e) {
             assertEquals("Invalid Request: No Cohort definition matching cohortId: "
-                         + cohortId
-                         + ", workspaceNamespace: " + namespace
-                         + ", workspaceId: " + name,
+                         + cohortId,
                          e.getMessage());
         }
 
         verify(cohortReviewDao, times(1)).findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
         verify(cohortDao, times(1)).findOne(cohortId);
-        verify(workspaceDao, times(1)).findOne(cohort.getWorkspaceId());
-        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, participantCounter, workspaceDao);
+        verify(workspaceService, times(1)).getRequired(namespace, name);
+        verifyNoMoreInteractions(cohortReviewDao, cohortDao, bigQueryService, participantCounter, workspaceService);
     }
 
     @Test
@@ -217,7 +222,7 @@ public class CohortReviewControllerTest {
 
         when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(cohortReview);
         when(cohortDao.findOne(cohortId)).thenReturn(cohort);
-        when(workspaceDao.findOne(cohort.getWorkspaceId())).thenReturn(workspace);
+        when(workspaceService.getRequired(namespace, name)).thenReturn(workspace);
         when(participantCounter.buildParticipantIdQuery(request, 200)).thenReturn(null);
         when(bigQueryService.filterBigQueryConfig(null)).thenReturn(null);
         when(bigQueryService.executeQuery(null)).thenReturn(queryResult);
@@ -230,7 +235,7 @@ public class CohortReviewControllerTest {
 
         verify(cohortReviewDao, times(1)).findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
         verify(cohortDao, times(1)).findOne(cohortId);
-        verify(workspaceDao, times(1)).findOne(cohort.getWorkspaceId());
+        verify(workspaceService, times(1)).getRequired(namespace, name);
         verify(participantCounter, times(1)).buildParticipantIdQuery(request, 200);
         verify(bigQueryService, times(1)).filterBigQueryConfig(null);
         verify(bigQueryService, times(1)).executeQuery(null);
@@ -238,7 +243,7 @@ public class CohortReviewControllerTest {
         verify(bigQueryService, times(1)).getLong(null, 0);
         verify(queryResult, times(1)).iterateAll();
         verify(cohortReviewDao, times(1)).save(cohortReviewAfter);
-        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, participantCounter);
+        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, workspaceService, participantCounter);
     }
 
     @Test
@@ -259,6 +264,231 @@ public class CohortReviewControllerTest {
         assertFindByCohortIdAndCdrVersionId(namespace, name, cohortId, cdrVersionId, null, null, null, column);
         assertFindByCohortIdAndCdrVersionId(namespace, name, cohortId, cdrVersionId, page, limit, order, "participantId");
         assertFindByCohortIdAndCdrVersionId(namespace, name, cohortId, cdrVersionId, page, limit, order, column);
+    }
+
+    @Test
+    public void updateParticipantCohortStatus_BadCohortId() {
+        String workspaceNamespace = "aou-test";
+        String workspaceName = "test";
+        long cohortId = 1;
+        long cdrVersionId = 1;
+        long participantId = 1;
+        ModifyCohortStatusRequest cohortStatusRequest = new ModifyCohortStatusRequest();
+
+        when(cohortDao.findOne(cohortId)).thenReturn(null);
+
+        try {
+            reviewController.updateParticipantCohortStatus(
+                    workspaceNamespace,
+                    workspaceName,
+                    cohortId,
+                    cdrVersionId,
+                    participantId,
+                    cohortStatusRequest);
+            fail("Should have thrown a BadRequestException!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: No Cohort exists for cohortId: "
+                    + cohortId, e.getMessage());
+        }
+
+        verify(cohortDao, times(1)).findOne(cohortId);
+        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, workspaceService, participantCounter);
+    }
+
+    @Test
+    public void updateParticipantCohortStatus_BadWorkspaceNameOrNamespace() {
+        String workspaceNamespace = "aou-test";
+        String workspaceName = "test";
+        long cohortId = 1;
+        long cdrVersionId = 1;
+        long participantId = 1;
+        long workspaceId = 1;
+        ModifyCohortStatusRequest cohortStatusRequest = new ModifyCohortStatusRequest();
+
+        Cohort cohort = new Cohort();
+        cohort.setWorkspaceId(workspaceId);
+
+        Workspace workspace = new Workspace();
+        workspace.setWorkspaceId(2);
+        workspace.setWorkspaceNamespace(workspaceNamespace);
+        workspace.setFirecloudName("bad" + workspaceName);
+
+        when(cohortDao.findOne(cohortId)).thenReturn(cohort);
+        when(workspaceService.getRequired(workspaceNamespace, workspaceName)).thenReturn(workspace);
+
+        try {
+            reviewController.updateParticipantCohortStatus(
+                    workspaceNamespace,
+                    workspaceName,
+                    cohortId,
+                    cdrVersionId,
+                    participantId,
+                    cohortStatusRequest);
+            fail("Should have thrown a BadRequestException!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: No workspace matching workspaceNamespace: "
+                    + workspaceNamespace + ", workspaceId: " + workspaceName, e.getMessage());
+        }
+
+        verify(cohortDao, times(1)).findOne(cohortId);
+        verify(workspaceService, times(1)).getRequired(workspaceNamespace, workspaceName);
+        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, workspaceService, participantCounter);
+    }
+
+    @Test
+    public void updateParticipantCohortStatus_BadCohortReview() {
+        String workspaceNamespace = "aou-test";
+        String workspaceName = "test";
+        long cohortId = 1;
+        long cdrVersionId = 1;
+        long participantId = 1;
+        long workspaceId = 1;
+        ModifyCohortStatusRequest cohortStatusRequest = new ModifyCohortStatusRequest();
+
+        Cohort cohort = new Cohort();
+        cohort.setWorkspaceId(workspaceId);
+
+        Workspace workspace = new Workspace();
+        workspace.setWorkspaceId(1);
+        workspace.setWorkspaceNamespace(workspaceNamespace);
+        workspace.setFirecloudName(workspaceName);
+
+        when(cohortDao.findOne(cohortId)).thenReturn(cohort);
+        when(workspaceService.getRequired(workspaceNamespace, workspaceName)).thenReturn(workspace);
+        when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(null);
+
+        try {
+            reviewController.updateParticipantCohortStatus(
+                    workspaceNamespace,
+                    workspaceName,
+                    cohortId,
+                    cdrVersionId,
+                    participantId,
+                    cohortStatusRequest);
+            fail("Should have thrown a BadRequestException!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: Cohort Review does not exist for cohortId: "
+                    + cohortId + ", cdrVersionId: " + cdrVersionId, e.getMessage());
+        }
+
+        verify(cohortDao, times(1)).findOne(cohortId);
+        verify(workspaceService, times(1)).getRequired(workspaceNamespace, workspaceName);
+        verify(cohortReviewDao, times(1))
+                .findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
+        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, workspaceService, participantCounter);
+    }
+
+    @Test
+    public void updateParticipantCohortStatus_BadParticipantCohortStatus() {
+        String workspaceNamespace = "aou-test";
+        String workspaceName = "test";
+        long cohortId = 1;
+        long cdrVersionId = 1;
+        long participantId = 1;
+        long workspaceId = 1;
+        ModifyCohortStatusRequest cohortStatusRequest = new ModifyCohortStatusRequest();
+
+        Cohort cohort = new Cohort();
+        cohort.setWorkspaceId(workspaceId);
+
+        Workspace workspace = new Workspace();
+        workspace.setWorkspaceId(workspaceId);
+        workspace.setWorkspaceNamespace(workspaceNamespace);
+        workspace.setFirecloudName(workspaceName);
+
+        CohortReview cohortReview = new CohortReview();
+        cohortReview.setCohortReviewId(1);
+
+        when(cohortDao.findOne(cohortId)).thenReturn(cohort);
+        when(workspaceService.getRequired(workspaceNamespace, workspaceName)).thenReturn(workspace);
+        when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(cohortReview);
+        when(participantCohortStatusDao
+                .findByParticipantKey_CohortReviewIdAndParticipantKey_ParticipantId(
+                        cohortReview.getCohortReviewId(), participantId)).thenReturn(null);
+
+        try {
+            reviewController.updateParticipantCohortStatus(
+                    workspaceNamespace,
+                    workspaceName,
+                    cohortId,
+                    cdrVersionId,
+                    participantId,
+                    cohortStatusRequest);
+            fail("Should have thrown a BadRequestException!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: No participant exists for participantId: "
+                    + participantId, e.getMessage());
+        }
+
+        verify(cohortDao, times(1)).findOne(cohortId);
+        verify(workspaceService, times(1)).getRequired(workspaceNamespace, workspaceName);
+        verify(cohortReviewDao, times(1))
+                .findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
+        verify(participantCohortStatusDao, times(1))
+                .findByParticipantKey_CohortReviewIdAndParticipantKey_ParticipantId(
+                        cohortReview.getCohortReviewId(), participantId);
+        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, workspaceService, participantCounter);
+    }
+
+    @Test
+    public void updateParticipantCohortStatus() {
+        String workspaceNamespace = "aou-test";
+        String workspaceName = "test";
+        long cohortId = 1;
+        long cdrVersionId = 1;
+        long participantId = 1;
+        long workspaceId = 1;
+        ModifyCohortStatusRequest cohortStatusRequest = new ModifyCohortStatusRequest();
+        cohortStatusRequest.setStatus(CohortStatus.INCLUDED);
+
+        Cohort cohort = new Cohort();
+        cohort.setWorkspaceId(workspaceId);
+
+        Workspace workspace = new Workspace();
+        workspace.setWorkspaceId(workspaceId);
+        workspace.setWorkspaceNamespace(workspaceNamespace);
+        workspace.setFirecloudName(workspaceName);
+
+        CohortReview cohortReview = new CohortReview();
+        cohortReview.setCohortReviewId(1);
+
+        ParticipantCohortStatusKey key = new ParticipantCohortStatusKey();
+        key.setParticipantId(participantId);
+        ParticipantCohortStatus participantCohortStatus = new ParticipantCohortStatus();
+        participantCohortStatus.setStatus(CohortStatus.NOT_REVIEWED);
+        participantCohortStatus.setParticipantKey(key);
+
+        when(cohortDao.findOne(cohortId)).thenReturn(cohort);
+        when(workspaceService.getRequired(workspaceNamespace, workspaceName)).thenReturn(workspace);
+        when(cohortReviewDao.findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId)).thenReturn(cohortReview);
+        when(participantCohortStatusDao
+                .findByParticipantKey_CohortReviewIdAndParticipantKey_ParticipantId(
+                        cohortReview.getCohortReviewId(), participantId)).thenReturn(participantCohortStatus);
+        when(cohortReviewDao.save(isA(CohortReview.class))).thenReturn(cohortReview);
+
+        org.pmiops.workbench.model.ParticipantCohortStatus expectedPcs =
+                new org.pmiops.workbench.model.ParticipantCohortStatus();
+        expectedPcs.setParticipantId(participantId);
+        expectedPcs.setStatus(CohortStatus.INCLUDED);
+
+        org.pmiops.workbench.model.ParticipantCohortStatus pcs = reviewController.updateParticipantCohortStatus(
+                workspaceNamespace,
+                workspaceName,
+                cohortId,
+                cdrVersionId,
+                participantId,
+                cohortStatusRequest).getBody();
+        assertEquals(expectedPcs, pcs);
+
+        verify(cohortDao, times(1)).findOne(cohortId);
+        verify(workspaceService, times(1)).getRequired(workspaceNamespace, workspaceName);
+        verify(cohortReviewDao, times(1))
+                .findCohortReviewByCohortIdAndCdrVersionId(cohortId, cdrVersionId);
+        verify(participantCohortStatusDao, times(1))
+                .findByParticipantKey_CohortReviewIdAndParticipantKey_ParticipantId(
+                        cohortReview.getCohortReviewId(), participantId);
+        verify(cohortReviewDao, times(1)).save(isA(CohortReview.class));
+        verifyNoMoreInteractions(cohortReviewDao, bigQueryService, workspaceService, participantCounter);
     }
 
     private void assertFindByCohortIdAndCdrVersionId(String namespace,
