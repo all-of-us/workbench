893a8b66d40722c44c272549f04d058c3ca00aab
Brian <brian.d.freeman@vumc.org>
2017-12-19 10:32:47 -0600

    RW-114 created dao call to find annotation definition by cohortId and cohortAnnotationDefinitionId.

--- api/src/main/java/org/pmiops/workbench/db/dao/CohortAnnotationDefinitionDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/CohortAnnotationDefinitionDao.java
@@ -3,6 +3,11 @@ package org.pmiops.workbench.db.dao;
 
 import org.pmiops.workbench.db.model.CohortAnnotationDefinition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface CohortAnnotationDefinitionDao extends JpaRepository<CohortAnnotationDefinition, Long> {
+
+    CohortAnnotationDefinition findByCohortIdAndAndCohortAnnotationDefinitionId(
+            @Param("cohortId") long cohortId,
+            @Param("cohortAnnotationDefinitionId") long cohortAnnotationDefinitionId);
 }
