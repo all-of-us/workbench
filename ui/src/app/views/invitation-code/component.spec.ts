d731f4554c02cb75e67dff998e256ece46d33291
Neha Saxena <nsaxena@broadinstitute.org>
2017-12-21 10:29:48 -0500

    correcting imports and whitespace

--- ui/src/app/views/invitation-code/component.spec.ts
+++ ui/src/app/views/invitation-code/component.spec.ts
@@ -3,6 +3,12 @@ import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing'
 import {FormsModule} from '@angular/forms';
 import {UrlSegment} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
+
+import {ClarityModule} from 'clarity-angular';
+import {ProfileService} from 'generated';
+import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
+import {ProfileServiceStub} from 'testing/stubs/profile-service-stub';
+
 import {
     queryByCss, simulateClick,
     updateAndTick
@@ -11,10 +17,6 @@ import {ErrorHandlingService} from '../../services/error-handling.service';
 import {SignInService} from '../../services/sign-in.service';
 import {AccountCreationComponent} from '../account-creation/component';
 import {InvitationCodeComponent} from '../invitation-code/component';
-import {ClarityModule} from 'clarity-angular';
-import {ProfileService} from 'generated';
-import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
-import {ProfileServiceStub} from 'testing/stubs/profile-service-stub';
 
 
 
--- ui/src/app/views/invitation-code/component.ts
+++ ui/src/app/views/invitation-code/component.ts
@@ -1,5 +1,5 @@
 import {Component} from '@angular/core';
-import {InvitationVerRequest,ProfileService} from 'generated';
+import {InvitationVerRequest, ProfileService} from 'generated';
 
 
 function isBlank(s: string) {
