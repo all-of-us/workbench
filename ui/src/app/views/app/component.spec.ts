afa26e37b5f5eb1209cc20c63f6a4628f47270f5
Neha Saxena <nsaxena@broadinstitute.org>
2017-12-21 10:33:48 -0500

    correcting imports

--- ui/src/app/views/app/component.spec.ts
+++ ui/src/app/views/app/component.spec.ts
@@ -2,18 +2,18 @@ import {async, TestBed} from '@angular/core/testing';
 import {FormsModule} from '@angular/forms';
 import {RouterTestingModule} from '@angular/router/testing';
 
+import {ClarityModule} from 'clarity-angular';
+import {CohortsService, ProfileService} from 'generated';
+
+import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
+import {ProfileServiceStub} from 'testing/stubs/profile-service-stub';
 
 import {ErrorHandlingService} from '../../services/error-handling.service';
 import {SignInService} from '../../services/sign-in.service';
 import {AccountCreationComponent} from '../account-creation/component';
 import {AppComponent} from '../app/component';
 import {InvitationCodeComponent} from '../invitation-code/component';
-import {ClarityModule} from 'clarity-angular';
 
-import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
-import {ProfileServiceStub} from 'testing/stubs/profile-service-stub';
-
-import {CohortsService, ProfileService} from 'generated';
 
 describe('AppComponent', () => {
 
