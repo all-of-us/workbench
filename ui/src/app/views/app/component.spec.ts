76bf74791bf50f967ac92149e9efc602269d8195
Brian Rubenstein <brubenst@broadinstitute.org>
2018-01-05 16:02:45 -0500

    Fix merge error.

--- ui/src/app/views/app/component.spec.ts
+++ ui/src/app/views/app/component.spec.ts
@@ -8,6 +8,8 @@ import {ErrorHandlingService} from '../../services/error-handling.service';
 import {SignInService} from '../../services/sign-in.service';
 import {AccountCreationComponent} from '../account-creation/component';
 import {AppComponent} from '../app/component';
+import {InvitationKeyComponent} from '../invitation-key/component';
+
 
 import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
 import {ProfileServiceStub} from 'testing/stubs/profile-service-stub';
@@ -26,6 +28,7 @@ describe('AppComponent', () => {
       declarations: [
         AccountCreationComponent,
         AppComponent,
+        InvitationKeyComponent
       ],
       providers: [
         { provide: AuthDomainService, useValue: {} },
