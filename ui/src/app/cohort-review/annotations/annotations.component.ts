54fd23dd7f1d30d12e727e9a6289a91e6c5c8fe7
johnstill <31938607+johnstill@users.noreply.github.com>
2018-01-10 07:29:40 -0600

    Stylistic Tweaks and Stubs (#400)
    * Header component
    
    * Remove lorem
    
    * Fix review wrapper margin transitions
    
    * stop sidebar wrapping / unwrapping
    
    * add moment.js; update detail to use realish dummy data
    
    * date formatting
    
    * Annotations stub
    
    * Dedupe dummy data
    
    * move dummydata into own dummy file
    
    * fix typo
    
    * clarity version bump to .21
    
    * Participant table make whole id cell clickable

---
+++ ui/src/app/cohort-review/annotations/annotations.component.ts
@@ -0,0 +1,7 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-annotations',
+  templateUrl: './annotations.component.html',
+})
+export class AnnotationsComponent {}
---
+++ ui/src/app/cohort-review/review-nav/review-nav.component.ts
@@ -0,0 +1,8 @@
+import {Component} from '@angular/core';
+
+@Component({
+  selector: 'app-review-nav',
+  templateUrl: './review-nav.component.html',
+  styleUrls: ['./review-nav.component.css']
+})
+export class ReviewNavComponent {}
