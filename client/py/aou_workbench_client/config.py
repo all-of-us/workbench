b95f7cf89b77b7dd9037204878d4c380b7512636
danqrodney <danrodney@google.com>
2018-01-16 14:55:51 -0600

    Adding bigquery.jobUser role to users on projects during workspace creation. (#405)
    * Adding CDR version properties.
    
    Moving config file to config/ dir.
    
    Adding BQ job user role on workspace creation.
    
    * Changing config path.
    
    Adding CDR version project, dataset config fields; cdr_table() function for referencing the CDR.
    
    * De-lint
    
    * Fixing tests
    
    * Addressing PR comments
    
    * Removing imports
    
    * Addressing PR comments

--- client/py/aou_workbench_client/config.py
+++ client/py/aou_workbench_client/config.py
@@ -4,11 +4,13 @@
 import json
 
 # Constants for keys in the configuration
-ALL_OF_US_CONFIG_FILE = 'all_of_us_config.json'
+ALL_OF_US_CONFIG_FILE = '~/config/all_of_us_config.json'
 WORKSPACE_NAMESPACE_KEY = 'WORKSPACE_NAMESPACE'
 WORKSPACE_ID_KEY = 'WORKSPACE_ID'
 API_HOST_KEY = 'API_HOST'
 BUCKET_NAME_KEY = 'BUCKET_NAME'
+CDR_VERSION_CLOUD_PROJECT = 'CDR_VERSION_CLOUD_PROJECT'
+CDR_VERSION_BIGQUERY_DATASET = 'CDR_VERSION_BIGQUERY_DATASET'
 
 class AllOfUsConfig(object):
     def __init__(self):
@@ -18,5 +20,12 @@ class AllOfUsConfig(object):
             self.workspace_id = self.config_json[WORKSPACE_ID_KEY]
             self.api_host = self.config_json[API_HOST_KEY]
             self.bucket_name = self.config_json[BUCKET_NAME_KEY]
+            self.cdr_version_cloud_project = self.config_json[CDR_VERSION_CLOUD_PROJECT]
+            self.cdr_version_bigquery_dataset = self.config_json[CDR_VERSION_BIGQUERY_DATASET]
 
 all_of_us_config = AllOfUsConfig()
+
+def cdr_table(table_name):
+    return '`%s.%s.%s`' % (all_of_us_config.cdr_version_cloud_project,
+                           all_of_us_config.cdr_version_bigquery_dataset,
+                           table_name)
