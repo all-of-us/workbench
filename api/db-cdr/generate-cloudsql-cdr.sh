aa1a2f8bba32a383bee186b1c1c72519935c5b07
Peter Speltz <peter.speltz@vanderbilt.edu>
2018-01-18 16:49:13 -0600

    Start of gererate cloudsql for a cdr

---
+++ api/db-cdr/generate-cloudsql-cdr.sh
@@ -0,0 +1,41 @@
+#!/bin/bash
+
+# This generates new cloudsql database for a cdr
+# note dev-up must be run to generate the schema
+
+set -xeuo pipefail
+IFS=$'\n\t'
+
+# Make the vocabulary table from cdr with no changes
+#bq --project=all-of-us-ehr-dev cp test_merge_dec26.vocabulary test_vocabulary_ppi.vocabulary
+project="all-of-us-ehr-dev"
+bqcdr="test_merge_dec26"
+cdr_schema_file="cdr_schema.sql"
+public_schema_file="tmp/public_schema.sql"
+cloudsql_instance=workbenchtest
+cdr_db_name=cdr
+gcs_bucket=gs://all-of-us-workbench-cloudsql-create
+
+echo "Dumping cdr schema "
+mysqldump -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} \
+    --no-data --add-drop-table --ignore-table=$cdr_db_name.DATABASECHANGELOG \
+    --ignore-table=$cdr_db_name.DATABASECHANGELOGLOCK $cdr_db_name > $cdr_schema_file
+
+# Grant access to buckets for service account
+SQL_SERVICE_ACCOUNT=`gcloud sql instances describe --project $project \
+    --account peter.speltz@pmi-ops.org $cloudsql_instance | grep serviceAccountEmailAddress \
+    | cut -d: -f2`
+
+echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to bucket $gcs_bucket..."
+gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:W $gcs_bucket
+
+# Copy dump to cloud storage for importing
+gsutil cp $cdr_schema_file $gcs_bucket/$cdr_schema_file
+
+# Import the schema into the cloud sql instance
+gcloud beta sql import sql $cloudsql_instance $gcs_bucket/$cdr_schema_file
+
+
+# Download data and import
+REMOTE_DATA_LOC=https://storage.googleapis.com/all-of-us-workbench-cdr-init
+
