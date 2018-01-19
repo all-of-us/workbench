47f03137dfa727e578510e4a763112e4d9c5c97e
Peter Speltz <peter.speltz@vanderbilt.edu>
2018-01-19 08:36:27 -0600

    initial dump of cdr schema working

--- api/db-cdr/generate-cloudsql-cdr.sh
+++ api/db-cdr/generate-cloudsql-cdr.sh
@@ -8,7 +8,7 @@ IFS=$'\n\t'
 
 # Make the vocabulary table from cdr with no changes
 #bq --project=all-of-us-ehr-dev cp test_merge_dec26.vocabulary test_vocabulary_ppi.vocabulary
-project="all-of-us-ehr-dev"
+project="all-of-us-workbench-test"
 bqcdr="test_merge_dec26"
 cdr_schema_file="cdr_schema.sql"
 public_schema_file="tmp/public_schema.sql"
@@ -19,22 +19,31 @@ gcs_bucket=gs://all-of-us-workbench-cloudsql-create
 echo "Dumping cdr schema "
 mysqldump -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} \
     --no-data --add-drop-table --ignore-table=$cdr_db_name.DATABASECHANGELOG \
-    --ignore-table=$cdr_db_name.DATABASECHANGELOGLOCK $cdr_db_name > $cdr_schema_file
-
-
-
-ls -la /
-
-# Grant access to buckets for service account
-SQL_SERVICE_ACCOUNT=`gcloud sql instances describe --project $project \
-    --account peter.speltz@pmi-ops.org $cloudsql_instance | grep serviceAccountEmailAddress \
-    | cut -d: -f2`
-
-echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to bucket $gcs_bucket..."
-gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:W $gcs_bucket
+    --ignore-table=$cdr_db_name.DATABASECHANGELOGLOCK $cdr_db_name --database cdr \
+    --add-drop-database > $cdr_schema_file
 
 # Copy dump to cloud storage for importing
 gsutil cp $cdr_schema_file $gcs_bucket/$cdr_schema_file
 
+# Todo Maybe will need these for a service account
+#SERVICE_ACCOUNT=all-of-us-workbench-test@appspot.gserviceaccount.com
+#CREDS_FILE=all-of-us-workbench-test-f8d191035ffe.json
+#gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$CREDS_FILE
+
+# Grant access to buckets for service account for cloudsql
+SQL_SERVICE_ACCOUNT=`gcloud sql instances describe --project $project \
+    --account $SERVICE_ACCOUNT $cloudsql_instance | grep serviceAccountEmailAddress \
+    | cut -d: -f2`
+
+echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to bucket $gcs_bucket..."
+# Trim leading whitespace from sql service account
+SQL_SERVICE_ACCOUNT=${SQL_SERVICE_ACCOUNT//[[:blank:]]/}
+# Note, this only applies to files already existing in the bucket . So must rerun after adding files
+gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:W $gcs_bucket
+gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:R $gcs_bucket/*.sql
+
 # Import the schema into the cloud sql instance
-gcloud beta sql import sql $cloudsql_instance $gcs_bucket/$cdr_schema_file
+# Todo install gcloud beta
+# gcloud beta sql import sql $cloudsql_instance $gcs_bucket/$cdr_schema_file
+
+gcloud sql instances import --quiet --project $project  $cloudsql_instance $gcs_bucket/$cdr_schema_file
