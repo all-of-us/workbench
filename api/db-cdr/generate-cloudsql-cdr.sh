17f0daf1ad74677b231046611ce6e3e19dc7e749
Peter Speltz <peter.speltz@vanderbilt.edu>
2018-01-19 11:33:47 -0600

    in progress , schema and hard coded data filled in cdr

--- api/db-cdr/generate-cloudsql-cdr.sh
+++ api/db-cdr/generate-cloudsql-cdr.sh
@@ -2,6 +2,7 @@
 
 # This generates new cloudsql database for a cdr
 # note dev-up must be run to generate the schema
+# note run-local-data-migrations must be run to generate hard coded data from liquibase
 
 set -xeuo pipefail
 IFS=$'\n\t'
@@ -10,40 +11,55 @@ IFS=$'\n\t'
 #bq --project=all-of-us-ehr-dev cp test_merge_dec26.vocabulary test_vocabulary_ppi.vocabulary
 project="all-of-us-workbench-test"
 bqcdr="test_merge_dec26"
-cdr_schema_file="cdr_schema.sql"
-public_schema_file="tmp/public_schema.sql"
 cloudsql_instance=workbenchtest
 cdr_db_name=cdr
 gcs_bucket=gs://all-of-us-workbench-cloudsql-create
 
+# output files
+tmp_dir="/tmp"
+cdr_schema_file="cdr_schema.sql"
+public_schema_file="public_schema.sql"
+hard_data_file="hard_data.sql"
+
+# Dump cdr schema and copy to gcs
 echo "Dumping cdr schema "
 mysqldump -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} \
     --no-data --add-drop-table --ignore-table=$cdr_db_name.DATABASECHANGELOG \
     --ignore-table=$cdr_db_name.DATABASECHANGELOGLOCK $cdr_db_name --database cdr \
-    --add-drop-database > $cdr_schema_file
+    --add-drop-database > $tmp_dir/$cdr_schema_file
+gsutil cp $tmp_dir/$cdr_schema_file $gcs_bucket/$cdr_schema_file
+rm $tmp_dir/$cdr_schema_file
 
-# Copy dump to cloud storage for importing
-gsutil cp $cdr_schema_file $gcs_bucket/$cdr_schema_file
+# Dump the data from the hardcoded data tables -- criteria, db_domain... and copy to gcs
+echo "Dumping Hard coded data "
+mysqldump -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} \
+     --add-drop-table --disable-keys cdr db_domain criteria > $tmp_dir/$hard_data_file
+gsutil cp $tmp_dir/$hard_data_file $gcs_bucket/$hard_data_file
+rm $tmp_dir/$hard_data_file
 
-# Todo Maybe will need these for a service account
-#SERVICE_ACCOUNT=all-of-us-workbench-test@appspot.gserviceaccount.com
-#CREDS_FILE=all-of-us-workbench-test-f8d191035ffe.json
-#gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$CREDS_FILE
+# Todo  need these for a service account
+SERVICE_ACCOUNT=all-of-us-workbench-test@appspot.gserviceaccount.com
+CREDS_FILE=all-of-us-workbench-test-f8d191035ffe.json
+gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$CREDS_FILE
 
 # Grant access to buckets for service account for cloudsql
 SQL_SERVICE_ACCOUNT=`gcloud sql instances describe --project $project \
     --account $SERVICE_ACCOUNT $cloudsql_instance | grep serviceAccountEmailAddress \
     | cut -d: -f2`
-
-echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to bucket $gcs_bucket..."
 # Trim leading whitespace from sql service account
 SQL_SERVICE_ACCOUNT=${SQL_SERVICE_ACCOUNT//[[:blank:]]/}
+
+echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to bucket $gcs_bucket..."
 # Note, this only applies to files already existing in the bucket . So must rerun after adding files
 gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:W $gcs_bucket
 gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:R $gcs_bucket/*.sql
 
 # Import the schema into the cloud sql instance
-# Todo install gcloud beta
+# Todo install gcloud beta for importing csv and sql . It is intended to replace sql instances import
 # gcloud beta sql import sql $cloudsql_instance $gcs_bucket/$cdr_schema_file
 
 gcloud sql instances import --quiet --project $project  $cloudsql_instance $gcs_bucket/$cdr_schema_file
+
+# Import hard data
+gcloud sql instances import --quiet --project $project  --database cdr $cloudsql_instance $gcs_bucket/$hard_data_file
+
