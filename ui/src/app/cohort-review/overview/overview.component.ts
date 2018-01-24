4689dcac881f82b67345bc9698b91bed7830bce7
johnstill <31938607+johnstill@users.noreply.github.com>
2018-01-24 15:12:52 -0600

    Johnstill/rw 260 d3 (#436)
    * add d3
    
    * first pass: d3 in Angular does work
    
    * Extract D3 margin convention into its own component
    
    * Using our own data with D3 now
    
    * Extract data prep logic into class
    
    * Linting
    
    * package lock

--- ui/src/app/cohort-review/overview/overview.component.ts
+++ ui/src/app/cohort-review/overview/overview.component.ts
@@ -1,12 +1,34 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {Subscription} from 'rxjs/Subscription';
+
+import {ReviewStateService} from '../review-state.service';
+
+import {CohortBuilderService, SearchRequest} from 'generated';
+
 
 @Component({
   selector: 'app-overview',
   templateUrl: './overview.component.html',
 })
-export class OverviewComponent implements OnInit {
+export class OverviewComponent implements OnInit, OnDestroy {
 
-  constructor() {}
+  data;
+  private subscription: Subscription;
 
-  ngOnInit() {}
+  constructor(
+    private chartAPI: CohortBuilderService,
+    private state: ReviewStateService,
+  ) {}
+
+  ngOnInit() {
+    this.subscription = this.state.cohort$
+      .map(({criteria}) => <SearchRequest>(JSON.parse(criteria)))
+      .switchMap(request => this.chartAPI.getChartInfo(request))
+      .map(response => response.items)
+      .subscribe(data => this.data = data);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
