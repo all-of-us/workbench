6e8d1d585153fd062378d05385ecf9e7cbf046d4
Neha Saxena <nsaxena@broadinstitute.org>
2018-01-29 16:42:47 -0500

    Imports

--- ui/src/app/views/workspace/component.spec.ts
+++ ui/src/app/views/workspace/component.spec.ts
@@ -11,8 +11,8 @@ import {WorkspaceComponent} from 'app/views/workspace/component';
 import {ClarityModule} from 'clarity-angular';
 import {ClusterService, CohortsService, WorkspacesService} from 'generated';
 import {CohortsServiceStub} from 'testing/stubs/cohort-service-stub';
-import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
 import {ClusterServiceStub} from 'testing/stubs/cluster-service-stub';
+import {ErrorHandlingServiceStub} from 'testing/stubs/error-handling-service-stub';
 import {HttpStub} from 'testing/stubs/Http-stub';
 import {WorkspacesServiceStub, WorkspaceStubVariables} from 'testing/stubs/workspace-service-stub';
 
