d77a46e84e43c49a130e4f361f9f8ef2fc46a2d8
CH Albach <calbach@google.com>
2018-01-30 15:18:23 -0800

    review changes + test

--- ui/src/app/services/server-config.service.ts
+++ ui/src/app/services/server-config.service.ts
@@ -10,14 +10,16 @@ import {ConfigResponse, ConfigService} from 'generated';
 export class ServerConfigService {
   private configObs: Observable<ConfigResponse>;
   constructor(
-    private configService: ConfigService, private errorHandlingService: ErrorHandlingService) {}
+    private configService: ConfigService,
+    private errorHandlingService: ErrorHandlingService
+  ) {}
 
   public getConfig(): Observable<ConfigResponse> {
     if (!this.configObs) {
       // Use of a replaySubject() caches the output of the API call across subscriptions.
       const subject = new ReplaySubject<ConfigResponse>();
       this.errorHandlingService.retryApi(this.configService.getConfig()).subscribe(subject);
-      this.configObs = subject;
+      this.configObs = subject.asObservable();
     }
     return this.configObs;
   }
