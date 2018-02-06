67f472be0c46ee793d8483a75bbea6999354e0f0
Peter Speltz <peter.speltz@vanderbilt.edu>
2018-02-06 15:37:48 -0600

    more cleanup of comments and documentation

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -760,7 +760,7 @@ Creates and populates local mysql database from data in bucket made by generate-
 Common.register_command({
                             :invocation => "generate-local-count-dbs",
                             :description => "generate-local-count-dbs.sh --cdr-version <''|YYYYMMDD> --bucket <BUCKET>
-Creates and populates mysql databases cdr<VERSION> and public<VERSION> from data in bucket made by generate-cdr-counts.",
+Creates and populates local mysql databases cdr<VERSION> and public<VERSION> from data in bucket made by generate-cdr-counts.",
                             :fn => lambda { |*args| generate_local_count_dbs(*args) }
                         })
 Common.register_command({
