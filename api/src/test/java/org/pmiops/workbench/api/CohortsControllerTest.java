f7a879f6ccd298594c4cdcdd8d976a2f1ca2272a
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2018-02-06 13:05:30 -0500

    Automate localization of config and notebook folder files after a cluster is created (#451)
    * Localizing notebook/config after creating notebook cluster
    
    * PR Review changes
    
    * Hookup launch with localize

--- api/src/test/java/org/pmiops/workbench/api/CohortsControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/CohortsControllerTest.java
@@ -40,6 +40,7 @@ import org.pmiops.workbench.model.ResearchPurpose;
 import org.pmiops.workbench.model.SearchRequest;
 import org.pmiops.workbench.model.Workspace;
 import org.pmiops.workbench.model.WorkspaceAccessLevel;
+import org.pmiops.workbench.notebooks.NotebooksService;
 import org.pmiops.workbench.test.FakeClock;
 import org.pmiops.workbench.test.SearchRequests;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -73,7 +74,7 @@ public class CohortsControllerTest {
 
   @TestConfiguration
   @Import({WorkspaceServiceImpl.class, CohortService.class})
-  @MockBean(FireCloudService.class)
+  @MockBean({FireCloudService.class, NotebooksService.class})
   static class Configuration {
     @Bean
     Clock clock() {
@@ -99,6 +100,8 @@ public class CohortsControllerTest {
   Provider<User> userProvider;
   @Autowired
   FireCloudService fireCloudService;
+  @Autowired
+  NotebooksService notebooksService;
   @Mock
   CloudStorageService cloudStorageService;
 
@@ -131,7 +134,7 @@ public class CohortsControllerTest {
     CLOCK.setInstant(NOW);
     WorkspacesController workspacesController = new WorkspacesController(workspaceService,
         cdrVersionDao, userDao, userProvider, fireCloudService, cloudStorageService, CLOCK,
-        "https://api.blah.com");
+        "https://api.blah.com",notebooksService);
     stubGetWorkspace(WORKSPACE_NAMESPACE, WORKSPACE_NAME, "bob@gmail.com",
         WorkspaceAccessLevel.OWNER);
     workspace = workspacesController.createWorkspace(workspace).getBody();
