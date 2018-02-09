f5a191c95b9f806f403f676c4e4bd7b16f6bad16
Dan Rodney <danrodney@danrodney-goobuntu.aus.corp.google.com>
2018-02-09 10:10:38 -0600

    Applying git secrets

---
+++ aou-utils/hooks/apply-git-secrets.sh
@@ -0,0 +1,17 @@
+#!/usr/bin/env bash
+set -euo pipefail
+IFS=$'\n\t'
+
+{ which git-secrets; } || {
+  echo 'git-secrets required and not found. See README for installation instructions.'
+  echo
+  exit 1
+}
+
+{
+  git config --remove-section secrets 2>/dev/null
+} || {
+  :
+}
+git secrets --add '"private_key":'
+git secrets --add --allowed 'git secrets --add *'
