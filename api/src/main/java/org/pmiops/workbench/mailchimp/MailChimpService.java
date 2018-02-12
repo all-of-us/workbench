f37a629ea1799068828bb57a05216901979b12da
Rebecca S. Asch <rsasch@users.noreply.github.com>
2018-02-12 15:01:41 -0500

    RW-365 Mailchimp Email Verification changes (#491)
    * added get function for mailchim list id
    
    * added error handling, fix to enum and logic
    
    * added default value for user.email_verification_status
    
    * fixed tests and wayward space
    
    * added comment to clarify DB change
    
    * modified switch statement to pacify Codacy
    
    * changed email_verification_status default to 0, so there is a distinction between UNVERIFIED and PENDING
    
    * neatened up handling of status to reflect new error handling

--- api/src/main/java/org/pmiops/workbench/mailchimp/MailChimpService.java
+++ api/src/main/java/org/pmiops/workbench/mailchimp/MailChimpService.java
@@ -1,9 +1,5 @@
 package org.pmiops.workbench.mailchimp;
 
-import com.ecwid.maleorang.MailchimpException;
-import java.io.IOException;
-import java.util.List;
-import java.util.HashMap;
 
 /**
  * Encapsulate mailchimp API interaction details and provide a simple/mockable interface
@@ -11,11 +7,10 @@ import java.util.HashMap;
  */
 public interface MailChimpService {
   static final String MAILCHIMP_PENDING = "pending";
-  static final String MAILCHIMP_SUBSCRIBED = "subscribed";
   static final String MAILCHIMP_KEY_ID = "id";
   static final String MAILCHIMP_KEY_STATUS = "status";
 
   String addUserContactEmail(String contactEmail);
 
-  String getMember(String userEmailHash) throws MailchimpException, IOException;
+  String getMember(String userEmailHash);
 }
