e04bf6b25ec62739357489d7ffb1f12db9fc3120
David Mohs <dgit@mohsinc.com>
2018-02-13 11:20:44 -0500

    Script changes to make things work with a second environment (#484)
    * Stable environment changes
    * Add public API URL to UI config

--- ui/src/environments/environment.test.ts
+++ ui/src/environments/environment.test.ts
@@ -1,6 +1,7 @@
 export const environment = {
   displayTag: 'Test',
   allOfUsApiUrl: 'https://api-dot-all-of-us-workbench-test.appspot.com',
+  clientId: '602460048110-5uk3vds3igc9qo0luevroc2uc3okgbkt.apps.googleusercontent.com',
   publicApiUrl: 'https://public-api-dot-all-of-us-workbench-test.appspot.com',
   debug: false,
   testing: true
