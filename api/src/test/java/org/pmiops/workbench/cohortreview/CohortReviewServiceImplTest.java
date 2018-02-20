475db641481454d203fbb9d5ad45f722ce977403
Brian <brian.d.freeman@vumc.org>
2018-02-20 13:06:39 -0600

    RW-367 refactor of createParticipantCohortAnnotation so update can take advantage of validation.

--- api/src/test/java/org/pmiops/workbench/cohortreview/CohortReviewServiceImplTest.java
+++ api/src/test/java/org/pmiops/workbench/cohortreview/CohortReviewServiceImplTest.java
@@ -19,8 +19,11 @@ import org.pmiops.workbench.db.model.CohortReview;
 import org.pmiops.workbench.db.model.ParticipantCohortAnnotation;
 import org.pmiops.workbench.db.model.ParticipantCohortStatus;
 import org.pmiops.workbench.db.model.Workspace;
+import org.pmiops.workbench.exceptions.BadRequestException;
 import org.pmiops.workbench.exceptions.NotFoundException;
+import org.pmiops.workbench.model.AnnotationType;
 import org.pmiops.workbench.model.Filter;
+import org.pmiops.workbench.model.ModifyParticipantCohortAnnotationRequest;
 import org.pmiops.workbench.model.WorkspaceAccessLevel;
 
 import javax.inject.Provider;
@@ -285,6 +288,212 @@ public class CohortReviewServiceImplTest {
     }
 
     @Test
+    public void saveParticipantCohortAnnotation() throws Exception {
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .annotationValueBoolean(Boolean.TRUE)
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        CohortAnnotationDefinition cohortAnnotationDefinition = createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.BOOLEAN);
+
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinitionId)).thenReturn(cohortAnnotationDefinition);
+        when(participantCohortAnnotationDao.findByCohortReviewIdAndCohortAnnotationDefinitionIdAndParticipantId(cohortReviewId,
+                cohortAnnotationDefinitionId, participantId)).thenReturn(null);
+
+        cohortReviewService.saveParticipantCohortAnnotation(cohortReviewId, participantCohortAnnotation);
+
+        verify(cohortAnnotationDefinitionDao).findOne(cohortAnnotationDefinitionId);
+        verify(participantCohortAnnotationDao).findByCohortReviewIdAndCohortAnnotationDefinitionIdAndParticipantId(cohortReviewId,
+                cohortAnnotationDefinitionId, participantId);
+        verifyNoMoreMockInteractions();
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationNotFoundCohortAnnotationDefinition() throws Exception {
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .annotationValueBoolean(Boolean.TRUE)
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinitionId)).thenReturn(null);
+
+        try {
+            cohortReviewService.saveParticipantCohortAnnotation(cohortReviewId, participantCohortAnnotation);
+            fail("Should have thrown NotFoundExcpetion!");
+        } catch (NotFoundException e) {
+            assertEquals("Not Found: No cohort annotation definition found for id: " + cohortAnnotationDefinitionId, e.getMessage());
+        }
+
+        verify(cohortAnnotationDefinitionDao).findOne(cohortAnnotationDefinitionId);
+        verifyNoMoreMockInteractions();
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationBadRequestCohortAnnotationDefinitionExists() throws Exception {
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .annotationValueBoolean(Boolean.TRUE)
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        CohortAnnotationDefinition cohortAnnotationDefinition = createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.BOOLEAN);
+
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinitionId)).thenReturn(cohortAnnotationDefinition);
+        when(participantCohortAnnotationDao.findByCohortReviewIdAndCohortAnnotationDefinitionIdAndParticipantId(cohortReviewId,
+                cohortAnnotationDefinitionId, participantId)).thenReturn(participantCohortAnnotation);
+
+        try {
+            cohortReviewService.saveParticipantCohortAnnotation(cohortReviewId, participantCohortAnnotation);
+            fail("Should have thrown BadRequestException!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: Cohort annotation definition exists for id: " + cohortAnnotationDefinitionId, e.getMessage());
+        }
+
+        verify(cohortAnnotationDefinitionDao).findOne(cohortAnnotationDefinitionId);
+        verifyNoMoreMockInteractions();
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationBadRequest() throws Exception {
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        assertParticipantCohortAnnotationBadRequest(participantCohortAnnotation, createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.BOOLEAN));
+        assertParticipantCohortAnnotationBadRequest(participantCohortAnnotation, createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.STRING));
+        assertParticipantCohortAnnotationBadRequest(participantCohortAnnotation, createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.INTEGER));
+        assertParticipantCohortAnnotationBadRequest(participantCohortAnnotation, createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.STRING));
+        assertParticipantCohortAnnotationBadRequest(participantCohortAnnotation, createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.ENUM));
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationModify() throws Exception {
+        long annotationId = 1;
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .annotationValueBoolean(Boolean.TRUE)
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        ModifyParticipantCohortAnnotationRequest modifyRequest = new ModifyParticipantCohortAnnotationRequest()
+                .valueBoolean(Boolean.FALSE);
+
+        CohortAnnotationDefinition cohortAnnotationDefinition = createCohortAnnotationDefinition(cohortAnnotationDefinitionId, AnnotationType.BOOLEAN);
+
+        when(participantCohortAnnotationDao.findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId)).thenReturn(participantCohortAnnotation);
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinitionId)).thenReturn(cohortAnnotationDefinition);
+
+        cohortReviewService.saveParticipantCohortAnnotation(annotationId, cohortReviewId, participantId, modifyRequest);
+
+        verify(participantCohortAnnotationDao).findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId);
+        verify(cohortAnnotationDefinitionDao).findOne(cohortAnnotationDefinitionId);
+        verifyNoMoreMockInteractions();
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationModifyNotFoundCohortAnnotationDefinition() throws Exception {
+        long annotationId = 1;
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .annotationValueBoolean(Boolean.TRUE)
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        ModifyParticipantCohortAnnotationRequest modifyRequest = new ModifyParticipantCohortAnnotationRequest()
+                .valueBoolean(Boolean.FALSE);
+
+        when(participantCohortAnnotationDao.findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId)).thenReturn(participantCohortAnnotation);
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinitionId)).thenReturn(null);
+
+        try {
+            cohortReviewService.saveParticipantCohortAnnotation(annotationId, cohortReviewId, participantId, modifyRequest);
+            fail("Should have thrown NotFoundException!");
+        } catch(NotFoundException e) {
+            assertEquals("Not Found: No cohort annotation definition found for id: " + cohortAnnotationDefinitionId, e.getMessage());
+        }
+
+        verify(participantCohortAnnotationDao).findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId);
+        verify(cohortAnnotationDefinitionDao).findOne(cohortAnnotationDefinitionId);
+        verifyNoMoreMockInteractions();
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationModifyNotFoundParticipantCohortAnnotation() throws Exception {
+        long annotationId = 1;
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ModifyParticipantCohortAnnotationRequest modifyRequest = new ModifyParticipantCohortAnnotationRequest()
+                .valueBoolean(Boolean.FALSE);
+
+        when(participantCohortAnnotationDao.findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId)).thenReturn(null);
+
+        try {
+            cohortReviewService.saveParticipantCohortAnnotation(annotationId, cohortReviewId, participantId, modifyRequest);
+            fail("Should have thrown NotFoundException!");
+        } catch(NotFoundException e) {
+            assertEquals("Not Found: Participant Cohort Annotation does not exist for annotationId: " + annotationId + ", cohortReviewId: "
+                    + cohortReviewId + ", participantId: " + cohortAnnotationDefinitionId, e.getMessage());
+        }
+
+        verify(participantCohortAnnotationDao).findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId);
+        verifyNoMoreMockInteractions();
+    }
+
+    @Test
+    public void saveParticipantCohortAnnotationModifyBadRequest() throws Exception {
+        long annotationId = 1;
+        long cohortAnnotationDefinitionId = 1;
+        long cohortReviewId = 1;
+        long participantId = 1;
+
+        ParticipantCohortAnnotation participantCohortAnnotation = new ParticipantCohortAnnotation()
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortReviewId(cohortReviewId)
+                .participantId(participantId);
+
+        assertParticipantCohortAnnotationModifyBadRequest(annotationId, participantCohortAnnotation, AnnotationType.BOOLEAN);
+        assertParticipantCohortAnnotationModifyBadRequest(annotationId, participantCohortAnnotation, AnnotationType.INTEGER);
+        assertParticipantCohortAnnotationModifyBadRequest(annotationId, participantCohortAnnotation, AnnotationType.STRING);
+        assertParticipantCohortAnnotationModifyBadRequest(annotationId, participantCohortAnnotation, AnnotationType.ENUM);
+        assertParticipantCohortAnnotationModifyBadRequest(annotationId, participantCohortAnnotation, AnnotationType.DATE);
+    }
+
+    @Test
     public void deleteParticipantCohortAnnotationNotFound() throws Exception {
         long annotationId = 1;
         long cohortReviewId = 2;
@@ -343,6 +552,54 @@ public class CohortReviewServiceImplTest {
                 cohortReviewDao,
                 participantCohortStatusDao,
                 workspaceService);
+    }
+
+    private CohortAnnotationDefinition createCohortAnnotationDefinition(long cohortAnnotationDefinitionId, AnnotationType annotationType) {
+        return new CohortAnnotationDefinition()
+                .annotationType(annotationType)
+                .columnName("name")
+                .cohortAnnotationDefinitionId(cohortAnnotationDefinitionId)
+                .cohortId(1);
+    }
+
+    private void assertParticipantCohortAnnotationBadRequest(ParticipantCohortAnnotation participantCohortAnnotation, CohortAnnotationDefinition cohortAnnotationDefinition) {
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinition.getCohortAnnotationDefinitionId())).thenReturn(cohortAnnotationDefinition);
+
+        try {
+            cohortReviewService.saveParticipantCohortAnnotation(participantCohortAnnotation.getCohortReviewId(), participantCohortAnnotation);
+            fail("Should have thrown BadRequestExcpetion!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: Please provide a valid " + cohortAnnotationDefinition.getAnnotationType().name()
+                    + " value for annotation defintion id: " + cohortAnnotationDefinition.getCohortAnnotationDefinitionId(), e.getMessage());
+        }
+
+        verify(cohortAnnotationDefinitionDao, atLeastOnce()).findOne(cohortAnnotationDefinition.getCohortAnnotationDefinitionId());
+        verifyNoMoreMockInteractions();
+    }
+
+    private void assertParticipantCohortAnnotationModifyBadRequest(Long annotationId, ParticipantCohortAnnotation participantCohortAnnotation,
+                                                                   AnnotationType annotationType) {
+        Long cohortAnnotationDefinitionId = participantCohortAnnotation.getCohortAnnotationDefinitionId();
+        Long cohortReviewId = participantCohortAnnotation.getCohortReviewId();
+        Long participantId = participantCohortAnnotation.getParticipantId();
+
+        CohortAnnotationDefinition cohortAnnotationDefinition = createCohortAnnotationDefinition(cohortAnnotationDefinitionId, annotationType);
+
+        when(participantCohortAnnotationDao.findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId,
+                cohortReviewId, participantId)).thenReturn(participantCohortAnnotation);
+        when(cohortAnnotationDefinitionDao.findOne(cohortAnnotationDefinitionId)).thenReturn(cohortAnnotationDefinition);
+
+        try {
+            cohortReviewService.saveParticipantCohortAnnotation(annotationId, cohortReviewId, participantId, new ModifyParticipantCohortAnnotationRequest());
+            fail("Should have thrown BadRequestExcpetion!");
+        } catch (BadRequestException e) {
+            assertEquals("Invalid Request: Please provide a valid " + cohortAnnotationDefinition.getAnnotationType().name()
+                    + " value for annotation defintion id: " + cohortAnnotationDefinition.getCohortAnnotationDefinitionId(), e.getMessage());
+        }
+
+        verify(participantCohortAnnotationDao, atLeastOnce()).findByAnnotationIdAndCohortReviewIdAndParticipantId(annotationId, cohortReviewId, participantId);
+        verify(cohortAnnotationDefinitionDao, atLeastOnce()).findOne(cohortAnnotationDefinitionId);
+        verifyNoMoreMockInteractions();
     }
 
 }
