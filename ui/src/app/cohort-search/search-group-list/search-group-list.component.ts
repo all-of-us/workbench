17617e714e223490e7bb55630c0366054ac03701
Brian Freeman <brian.d.freeman@vumc.org>
2018-02-22 10:04:03 -0600

    Freemabd1/rw 370 show/hide charts and reverse save and total count (#540)
    * RW-370 implemented show/hide of charts on initial land and swapped total count and save buttons positions.
    
    * RW-370 added show of charts before request.
    
    * RW-370 changed text.
    
    * Add yarn coverage command (#527)
    
    * RW-370 implemented show/hide of charts on initial land and swapped total count and save buttons positions.
    
    * RW-370 added show of charts before request.
    
    * RW-370 changed text.
    
    * Freemabd1/rw 367 implemented api calls for update/get for participant cohort annotations (#531)
    
    * RW-367 refactor of createParticipantCohortAnnotation so update can take advantage of validation.
    
    * RW-367 refactor of createParticipantCohortAnnotation so update can take advantage of validation.
    
    * RW-367 added comments.
    
    * RW-367 implemented the getParticipantCohortAnnotations endpoint.
    
    * RW-367 renamed saveParticipantCohortAnnotation to updateParticipantCohortAnnotation.
    
    * Removing migrate_public_database call (#532)
    
    * Yet another build fix (#533)
    
    * RW-367 removed unused variables.
    
    * RW-367 removed unnecessary full qualified names.
    
    * Fixes to Cloud SQL import script (#534)
    
    * Start of fix
    
    * Cloud SQL import fixes
    
    * Updating echo
    
    * Instance fix
    
    * Fix disabled message. (#529)
    
    * Fix disabled message.
    
    * Added enum for error codes, created superclass exception, and nullcheck.
    
    * made account creation component more standards-compliant and accessible (#538)
    
    * Codacy config file test (#537)
    
    * Codacy config file test
    
    * Trying to trigger codacy
    
    * Restore file
    
    * RW-367 removed unecessary line of code.
    
    * RW-367 added test cases.
    
    * RW-367 added index.
    
    * RW-367 added index.
    
    * Add yarn coverage command (#527)
    
    * RW-367 refactor of createParticipantCohortAnnotation so update can take advantage of validation.
    
    * RW-367 refactor of createParticipantCohortAnnotation so update can take advantage of validation.
    
    * RW-367 added comments.
    
    * RW-367 implemented the getParticipantCohortAnnotations endpoint.
    
    * RW-367 renamed saveParticipantCohortAnnotation to updateParticipantCohortAnnotation.
    
    * RW-367 removed unused variables.
    
    * RW-367 removed unnecessary full qualified names.
    
    * RW-367 removed unecessary line of code.
    
    * RW-367 added test cases.
    
    * RW-367 added index.
    
    * RW-367 added index.
    
    * Fixing integration, bigquery test (#535)
    
    * RW-370 updated per Johns comments.
    
    * RW-370 implemented show/hide of charts on initial land and swapped total count and save buttons positions.
    
    * RW-370 implemented show/hide of charts on initial land and swapped total count and save buttons positions.
    
    * RW-370 added show of charts before request.
    
    * RW-370 added show of charts before request.
    
    * RW-370 changed text.
    
    * RW-370 changed text.
    
    * RW-370 updated per Johns comments.

--- ui/src/app/cohort-search/search-group-list/search-group-list.component.ts
+++ ui/src/app/cohort-search/search-group-list/search-group-list.component.ts
@@ -23,7 +23,6 @@ export class SearchGroupListComponent {
   }
 
   get title() {
-    const verb = this.role.charAt(0).toUpperCase() + this.role.slice(1, -1) + 'd';
-    return `${verb} Participants`;
+    return this.role.slice(0, -1) + ` Participants Where`;
   }
 }
