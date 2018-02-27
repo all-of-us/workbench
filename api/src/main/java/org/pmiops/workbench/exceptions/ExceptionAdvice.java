cc046b5ad0a811240a32eb73d96af82f39dfb9d0
Rebecca Asch <rasch@broadinstitute.org>
2018-02-27 16:28:45 -0500

    consolidated API errors into ErrorResponse format and changed logging

--- api/src/main/java/org/pmiops/workbench/exceptions/ExceptionAdvice.java
+++ api/src/main/java/org/pmiops/workbench/exceptions/ExceptionAdvice.java
@@ -4,6 +4,7 @@ import java.util.logging.Level;
 import java.util.logging.Logger;
 
 import com.ecwid.maleorang.MailchimpException;
+import org.pmiops.workbench.model.ErrorCode;
 import org.pmiops.workbench.model.ErrorResponse;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
@@ -20,13 +21,14 @@ public class ExceptionAdvice {
     ErrorResponse errorResponse = new ErrorResponse();
     Integer statusCode = 500;
 
+    // capture application-specific error code
     ResponseStatus responseStatus = null;
     responseStatus = e.getClass().getAnnotation(ResponseStatus.class);
     if (responseStatus != null) {
-      statusCode = responseStatus.code().value();
+      errorResponse.setErrorCode(ErrorCode.fromValue(responseStatus.code().toString()));
     }
 
-    // because MailChimp is special
+    // handle the way MailChimp (via maleorang) formats its errors
     if (e.getCause() instanceof MailchimpException) {
       statusCode = ((MailchimpException) e.getCause()).code;
       errorResponse.setMessage(((MailchimpException) e.getCause()).description);
@@ -38,7 +40,16 @@ public class ExceptionAdvice {
     errorResponse.setStatusCode(statusCode);
     errorResponse.setErrorClassName(e.getCause().getClass().getSimpleName());
 
-    log.log(Level.SEVERE, e.getCause().getClass().getName(), e);
+    // different logging levels for different error codes
+    if (statusCode >= 500) {
+      log.log(Level.SEVERE, e.getCause().getClass().getName(), e);
+    }
+    else if (statusCode >= 400) {
+      log.log(Level.WARNING, e.getCause().getClass().getName(), e);
+    }
+    else {
+      log.log(Level.INFO, e.getCause().getClass().getName(), e);
+    }
     return ResponseEntity.status(statusCode).body(errorResponse);
   }
 }
