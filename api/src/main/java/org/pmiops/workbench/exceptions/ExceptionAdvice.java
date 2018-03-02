bc122c0b826467c2ee9bd126f976b22ae0042597
Rebecca Asch <rasch@broadinstitute.org>
2018-03-02 13:03:26 -0500

    reworking ExceptionAdvice

--- api/src/main/java/org/pmiops/workbench/exceptions/ExceptionAdvice.java
+++ api/src/main/java/org/pmiops/workbench/exceptions/ExceptionAdvice.java
@@ -4,8 +4,12 @@ package org.pmiops.workbench.exceptions;
 import com.ecwid.maleorang.MailchimpException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
+
+import org.aspectj.weaver.ast.Not;
+import org.pmiops.workbench.mailchimp.MailChimpService;
 import org.pmiops.workbench.model.ErrorCode;
 import org.pmiops.workbench.model.ErrorResponse;
+import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
@@ -19,37 +23,51 @@ public class ExceptionAdvice {
   @ExceptionHandler({Exception.class})
   public ResponseEntity<?> serverError(Exception e) {
     ErrorResponse errorResponse = new ErrorResponse();
-    Integer statusCode = 500;
+    Integer statusCode = HttpStatus.INTERNAL_SERVER_ERROR.value();
+    Throwable cause = e;
 
-    // capture application-specific error code
-    ResponseStatus responseStatus = null;
-    responseStatus = e.getClass().getAnnotation(ResponseStatus.class);
-    if (responseStatus != null) {
-      errorResponse.setErrorCode(ErrorCode.fromValue(responseStatus.code().toString()));
+    if (e.getCause() != null) {
+      cause = e.getCause();
     }
 
-    // handle the way MailChimp (via maleorang) formats its errors
-    if (e.getCause() instanceof MailchimpException) {
-      statusCode = ((MailchimpException) e.getCause()).code;
-      errorResponse.setMessage(((MailchimpException) e.getCause()).description);
-    }
-    else {
-      errorResponse.setMessage(e.getMessage());
+    // TODO: figure out how to get ErrorCode content from internal errors
+//    ResponseStatus responseStatus = null;
+//    responseStatus = e.getClass().getAnnotation(ResponseStatus.class);
+//    if (responseStatus != null) {
+//      errorResponse.setErrorCode(ErrorCode.fromValue(responseStatus.code().toString()));
+//    }
+
+    errorResponse.setMessage(cause.getMessage());
+
+    if (cause instanceof MailchimpException) {
+      MailchimpException me = (MailchimpException) cause;
+      statusCode = me.code;
+      errorResponse.setMessage(me.description);
+    } else if (cause instanceof BadRequestException) {
+      statusCode = HttpStatus.BAD_REQUEST.value();
+    } else if (cause instanceof ConflictException) {
+      statusCode = HttpStatus.CONFLICT.value();
+    } else if (cause instanceof FailedPreconditionException) {
+      statusCode = HttpStatus.PRECONDITION_FAILED.value();
+    } else if (cause instanceof ForbiddenException) {
+      statusCode = HttpStatus.FORBIDDEN.value();
+    } else if (cause instanceof NotFoundException) {
+      statusCode = HttpStatus.NOT_FOUND.value();
+    } else if (cause instanceof ServerUnavailableException) {
+      statusCode = HttpStatus.SERVICE_UNAVAILABLE.value();
     }
 
     errorResponse.setStatusCode(statusCode);
-    errorResponse.setErrorClassName(e.getCause().getClass().getSimpleName());
+    errorResponse.setErrorClassName(cause.getClass().getSimpleName());
 
     // different logging levels for different error codes
+    Level level = Level.INFO;
     if (statusCode >= 500) {
-      log.log(Level.SEVERE, e.getCause().getClass().getName(), e);
+      level = Level.SEVERE;
+    } else if (statusCode >= 400) {
+      level = Level.WARNING;
     }
-    else if (statusCode >= 400) {
-      log.log(Level.WARNING, e.getCause().getClass().getName(), e);
-    }
-    else {
-      log.log(Level.INFO, e.getCause().getClass().getName(), e);
-    }
+    log.log(level, cause.getClass().getName(), e);
     return ResponseEntity.status(statusCode).body(errorResponse);
   }
 }
