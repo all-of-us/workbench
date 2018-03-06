c04e0ac5ca3c8eb5c2baa5d62ff03bea97a67c3b
danqrodney <danrodney@google.com>
2018-03-06 17:14:22 -0600

    Adding method for merging Swagger YAML files (#7)

--- aou-utils/swagger.rb
+++ aou-utils/swagger.rb
@@ -1,4 +1,5 @@
 require "open-uri"
+require "yaml"
 require_relative "utils/common"
 require_relative "workbench"
 
@@ -12,8 +13,23 @@ module Workbench
         IO.copy_stream(open(url), f)
       end
     end
-    module_function :download
-
+    module_function :download    
+    
+    def merge_yaml(main_yaml_file, files_to_include, output_yaml_file)
+      main_yaml = YAML.load_file(main_yaml_file)
+      sections_to_merge = ['parameters', 'paths', 'definitions']
+      files_to_include.each do |file_to_include|
+        file_yaml = YAML.load_file(file_to_include)
+        sections_to_merge.each do |section|
+          if file_yaml.key?(section)
+            main_yaml[section] = main_yaml[section].merge(file_yaml[section])
+          end
+        end        
+      end
+      File.open(output_yaml_file, 'w') { |f| YAML.dump(main_yaml, f) }
+    end
+    module_function :merge_yaml
+      
     def download_swagger_codegen_cli()
       unless File.exist?(SWAGGER_CODEGEN_CLI_JAR)
         common = Common.new
