88bf64b440760ea356240fa566df22641fedb307
Brian <brian.d.freeman@vumc.org>
2018-03-15 15:32:44 -0500

    RW-391 more experimentation.

--- api/src/test/java/org/pmiops/workbench/ApplicationTest.java
+++ api/src/test/java/org/pmiops/workbench/ApplicationTest.java
@@ -3,21 +3,29 @@ package org.pmiops.workbench;
 import com.google.apphosting.api.ApiProxy;
 import org.junit.Test;
 import org.junit.runner.RunWith;
-import org.mockito.Mock;
 import org.pmiops.workbench.api.CohortBuilderController;
 import org.pmiops.workbench.api.ProfileController;
+import org.pmiops.workbench.config.WebMvcConfig;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
+import org.powermock.modules.junit4.PowerMockRunnerDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.context.annotation.Import;
-import org.springframework.test.context.ActiveProfiles;
 import org.springframework.test.context.junit4.SpringRunner;
 
-import static org.assertj.core.api.Assertions.assertThat;
+import java.util.HashMap;
+import java.util.Map;
 
-@RunWith(SpringRunner.class)
-@SpringBootTest(classes = {Application.class})
+import static org.assertj.core.api.Assertions.assertThat;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
+import static org.powermock.api.mockito.PowerMockito.when;
+
+@RunWith(PowerMockRunner.class)
+@PowerMockRunnerDelegate(SpringRunner.class)
 @Import({TestJpaConfig.class})
-@ActiveProfiles("test")
+@SpringBootTest(classes = {Application.class})
+@PrepareForTest(ApiProxy.class)
 public class ApplicationTest {
 
     @Autowired
@@ -26,12 +34,70 @@ public class ApplicationTest {
     @Autowired
     private ProfileController profileController;
 
-    @Mock
-    private ApiProxy apiProxy;
+    @Autowired
+    private WebMvcConfig webMvcConfig;
 
     @Test
     public void contextLoads() throws Exception {
+        mockStatic(ApiProxy.class);
+        when(ApiProxy.getCurrentEnvironment()).thenReturn(new TestEnvironment());
+
         assertThat(controller).isNotNull();
         assertThat(profileController).isNotNull();
+
+//        verifyStatic();
+    }
+
+    public class TestEnvironment implements ApiProxy.Environment {
+
+        @Override
+        public String getAppId() {
+            return null;
+        }
+
+        @Override
+        public String getModuleId() {
+            return null;
+        }
+
+        @Override
+        public String getVersionId() {
+            return null;
+        }
+
+        @Override
+        public String getEmail() {
+            return null;
+        }
+
+        @Override
+        public boolean isLoggedIn() {
+            return false;
+        }
+
+        @Override
+        public boolean isAdmin() {
+            return false;
+        }
+
+        @Override
+        public String getAuthDomain() {
+            return null;
+        }
+
+        @Override
+        public String getRequestNamespace() {
+            return null;
+        }
+
+        @Override
+        public Map<String, Object> getAttributes() {
+            return new HashMap<>();
+        }
+
+        @Override
+        public long getRemainingMillis() {
+            return 0;
+        }
     }
 }
