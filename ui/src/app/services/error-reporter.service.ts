0d8d44ae67be3da6a740dc702acb4f02671d4bd3
CH Albach <calbach@google.com>
2018-03-14 22:33:42 -0700

    Disable Stackdriver for http:// hosts, to catch local dev

--- ui/src/app/services/error-reporter.service.ts
+++ ui/src/app/services/error-reporter.service.ts
@@ -10,6 +10,12 @@ export class ErrorReporterService extends ErrorHandler {
 
   constructor(serverConfigService: ServerConfigService) {
     super();
+    if (window.location.protocol !== 'https:') {
+      // The scheme is an indirect proxy for whether this is a local dev
+      // deployment. If it is local, we want to disable Stackdriver reporting as
+      // it's not useful and likely won't work due to the origin.
+      return;
+    }
     serverConfigService.getConfig().subscribe((config: ConfigResponse) => {
       if (!config.stackdriverApiKey) {
         return;
@@ -29,6 +35,8 @@ export class ErrorReporterService extends ErrorHandler {
       return;
     }
     this.stackdriverReporter.report(error, (e) => {
+      // Note: this does not detect non-200 responses from Stackdriver:
+      // https://github.com/GoogleCloudPlatform/stackdriver-errors-js/issues/32
       if (e) {
         console.log('failed to send error report: ' + e);
       }
