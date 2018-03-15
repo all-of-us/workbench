ba148649daa829df4b4d0a73969ae7a05bfae470
Brian Rubenstein <brubenst@broadinstitute.org>
2018-03-15 13:24:31 -0400

    Use viewchild instead.

--- ui/src/app/views/page-template-signed-out/component.ts
+++ ui/src/app/views/page-template-signed-out/component.ts
@@ -1,4 +1,11 @@
-import {Component, Input, OnChanges, OnInit} from '@angular/core';
+import {
+  Component,
+  ElementRef,
+  Input,
+  OnChanges,
+  OnInit,
+  ViewChild
+} from '@angular/core';
 
 @Component({
   selector: 'app-page-template-signed-out',
@@ -9,23 +16,22 @@ export class PageTemplateSignedOutComponent implements OnChanges, OnInit {
   @Input() imageSrc = '';
   @Input() smallerImageSrc = '';
   headerImg = '/assets/images/logo-registration-non-signed-in.svg';
-  templateElement: any;
-
+  @ViewChild('template') template: ElementRef;
   constructor() {}
 
   ngOnInit() {
-    this.templateElement = document.getElementsByClassName('template')[0];
     this.updateImages();
   }
 
-  ngOnChanges(changes) {
+  ngOnChanges() {
     this.updateImages();
   }
 
   updateImages(): void {
-    if (this.templateElement !== undefined) {
-      this.templateElement.style.setProperty('--smaller-image', `url("${this.smallerImageSrc}")`);
-      this.templateElement.style.setProperty('--larger-image', `url("${this.imageSrc}")`);
+    if (this.template !== undefined) {
+      this.template.nativeElement.style
+          .setProperty('--smaller-image', `url("${this.smallerImageSrc}")`);
+      this.template.nativeElement.style.setProperty('--larger-image', `url("${this.imageSrc}")`);
     }
   }
 
