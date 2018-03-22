0c04e8a977e14d9c71c20660d07b547b93fdeb5e
David Mohs <dgit@mohsinc.com>
2018-03-22 17:41:32 -0400

    Fix unchecked warning (#637)

--- api/src/main/java/org/pmiops/workbench/mailchimp/MailChimpServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/mailchimp/MailChimpServiceImpl.java
@@ -57,7 +57,8 @@ public class MailChimpServiceImpl implements MailChimpService {
   }
 
   // general function that handles MailChimp client creation and error handling
-  private MailchimpObject executeMailChimpRequest(MailchimpMethod method) {
+  private <R extends MailchimpObject> MailchimpObject
+      executeMailChimpRequest(MailchimpMethod<R> method) {
     if (apiKey == null) {
       apiKey = cloudStorageServiceProvider.get().readMailChimpApiKey();
     }
