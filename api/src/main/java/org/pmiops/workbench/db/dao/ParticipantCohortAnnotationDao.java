1420298e843ec08995286fc8b4046091e0db74c4
Brian Freeman <brian.d.freeman@vumc.org>
2018-03-26 14:04:01 -0500

    RW-435 adding bulk inserts.

--- api/src/main/java/org/pmiops/workbench/db/dao/ParticipantCohortAnnotationDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/ParticipantCohortAnnotationDao.java
@@ -2,11 +2,19 @@ package org.pmiops.workbench.db.dao;
 
 import org.pmiops.workbench.db.model.ParticipantCohortAnnotation;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface ParticipantCohortAnnotationDao extends JpaRepository<ParticipantCohortAnnotation, Long> {
+
+    // Important: Keep in sync with all DB rows that should be copied.
+    static final String ALL_COLUMNS_EXCEPT_REVIEW_ID =
+            "cohort_annotation_definition_id, participant_id, annotation_value_string, " +
+                    "annotation_value_integer, last_modified_time, annotation_value_date, " +
+                    "cohort_annotation_enum_value_id, annotation_value_boolean";
 
     ParticipantCohortAnnotation findByCohortReviewIdAndCohortAnnotationDefinitionIdAndParticipantId(
             @Param("cohortReviewId") long cohortReviewId,
@@ -21,4 +29,20 @@ public interface ParticipantCohortAnnotationDao extends JpaRepository<Participan
     List<ParticipantCohortAnnotation> findByCohortReviewIdAndParticipantId(
             @Param("cohortReviewId") long cohortReviewId,
             @Param("participantId") long participantId);
+
+    // We use native SQL here as there may be a large number of rows within a
+    // given cohort review; this avoids loading them into memory.
+    @Modifying
+    @Query(
+        value = "INSERT INTO participant_cohort_annotations" +
+        " (cohort_review_id, " + ALL_COLUMNS_EXCEPT_REVIEW_ID + ")" +
+        " SELECT (:toCohortReviewId), " + ALL_COLUMNS_EXCEPT_REVIEW_ID +
+        " FROM participant_cohort_annotations pca" +
+        " JOIN cohort_annotation_definition cad on (:toCohortId) = cad.cohort_id" +
+        " WHERE pca.cohort_review_id = (:fromCohortReviewId)",
+        nativeQuery = true)
+    void bulkCopyByCohortReviewAndCohort(
+            @Param("fromCohortReviewId") long fromCohortReviewId,
+            @Param("toCohortReviewId") long toCohortReviewId,
+            @Param("toCohortId") long toCohortId);
 }
