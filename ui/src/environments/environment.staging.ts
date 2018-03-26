956d41d4acb2d433e9b84b67a742c981e6df6036
CH Albach <calbach@google.com>
2018-03-26 16:24:13 -0700

    Add a staging environment

---
+++ ui/src/environments/environment.staging.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  displayTag: 'Staging',
+  allOfUsApiUrl: 'https://api-dot-all-of-us-rw-staging.appspot.com',
+  clientId: '657299777109-kvb5qafr70bl01i6bnpgsiq5nt6v1o8u.apps.googleusercontent.com',
+  publicApiUrl: 'https://public-api-dot-all-of-us-rw-staging.appspot.com',
+  debug: false,
+  testing: true
+};
