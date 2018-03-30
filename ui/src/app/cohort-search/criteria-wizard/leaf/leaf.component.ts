73f2d8099adb0d6008faa26a0533eda9bd0e62d5
John Still <john.m.still@vumc.org>
2018-03-30 16:29:59 -0500

    Fix presentation of Physical measurements codes

--- ui/src/app/cohort-search/criteria-wizard/leaf/leaf.component.ts
+++ ui/src/app/cohort-search/criteria-wizard/leaf/leaf.component.ts
@@ -60,11 +60,15 @@ export class LeafComponent implements OnInit, OnDestroy {
   }
 
   get displayName() {
+    const isPM = this.node.get('type', '') === 'PM';
     const nameIsCode = this.node.get('name', '') === this.node.get('code', '');
-    return (nameIsCode) ? '' : this.node.get('name', '');
-}
+    return (isPM || nameIsCode) ? '' : this.node.get('name', '');
+  }
 
   get displayCode() {
+    if (this.node.get('type', '') === 'PM') {
+      return this.node.get('name', '');
+    }
     return this.node.get('code', '');
   }
 
