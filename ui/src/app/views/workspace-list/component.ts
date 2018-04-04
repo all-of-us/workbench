ef928864ad4c9663161252fa61641569a4d5aa19
Brian Rubenstein <brubenst@broadinstitute.org>
2018-04-04 15:46:45 -0400

    Make not dismissable

--- ui/src/app/views/workspace-list/component.ts
+++ ui/src/app/views/workspace-list/component.ts
@@ -52,7 +52,6 @@ export class WorkspaceListComponent implements OnInit, OnDestroy {
   billingProjectInitialized = false;
   billingProjectQuery: NodeJS.Timer;
   errorText: string;
-  twoFactorShow = true;
   workspaceList: WorkspaceResponse[] = [];
   workspacesLoading = false;
   workspaceAccessLevel = WorkspaceAccessLevel;
