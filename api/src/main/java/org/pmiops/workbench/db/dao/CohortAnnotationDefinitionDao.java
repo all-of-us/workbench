8afe5c849e6037551bca81ca6dfc630419f4c1d8
Brian Freeman <brian.d.freeman@vumc.org>
2018-04-04 22:38:40 -0500

    Freemabd1/rw 435 Clone Annotations data with the workbench clone. (#682)
    * RW-435 adding bulk inserts.
    
    * RW-435 fixing test case.
    
    * RW-435 fixing test case.
    
    * RW-435 fixed test cases to inlcude annotations.
    
    * RW-435 fixing problem.
    
    * RW-435 added bulk copy for ParticipantCohortAnnotations.
    
    * RW-435 cleanup.
    
    * RW-435 added bulk inserts for annotation definitions.
    
    * RW-435 had to split out annotation bulk save cause of H2 database does not like union with insert statement.
    
    * RW-435 finished cloning.
    
    * RW-435 formatting.
    
    * RW-435 removed jdbcTemplate.
    
    * Workspace list revamp and gate on approval (#676)
    
    * Workspace list revamp and gate on approval
    
    * Remove unused code
    
    * Fix tests
    
    * PR feedback
    
    * Remove extra whitespace
    
    * RW-435 added comment for order to call methods.
    
    * RW-435 renamed aliases.
    
    * RW-435 reordered parameters.
    
    * RW-435 removed the fromCohortReviewId param.
    
    * RW-435 renamed method to remove 1 from fat finger.
    
    * RW-435 refactored asserts to private methods.
    
    * RW-435 inlined all the creates for CAD/PCA in test case.
    
    * RW-435 making codacy happy.
    
    * RW-487 created contract for observations and drugs. (#683)
    
    * RW-435 moving annotation definition persist outside of review iteration.
    
    * RW-435 moving list size check into helper method.
    
    * RW-435 inlined request objects into the API call invocation as they are not used otherwise.
    
    * RW-435 codacy check.
    
    * RW-435 adding bulk inserts.
    
    * RW-435 fixing test case.
    
    * RW-435 fixing test case.
    
    * RW-435 fixed test cases to inlcude annotations.
    
    * RW-435 fixing problem.
    
    * RW-435 added bulk copy for ParticipantCohortAnnotations.
    
    * RW-435 cleanup.
    
    * RW-435 added bulk inserts for annotation definitions.
    
    * RW-435 had to split out annotation bulk save cause of H2 database does not like union with insert statement.
    
    * RW-435 finished cloning.
    
    * RW-435 formatting.
    
    * RW-435 removed jdbcTemplate.
    
    * RW-435 added comment for order to call methods.
    
    * RW-435 renamed aliases.
    
    * RW-435 reordered parameters.
    
    * RW-435 removed the fromCohortReviewId param.
    
    * RW-435 renamed method to remove 1 from fat finger.
    
    * RW-435 refactored asserts to private methods.
    
    * RW-435 inlined all the creates for CAD/PCA in test case.
    
    * RW-435 making codacy happy.
    
    * RW-435 moving annotation definition persist outside of review iteration.
    
    * RW-435 moving list size check into helper method.
    
    * RW-435 inlined request objects into the API call invocation as they are not used otherwise.
    
    * RW-435 codacy check.

--- api/src/main/java/org/pmiops/workbench/db/dao/CohortAnnotationDefinitionDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/CohortAnnotationDefinitionDao.java
@@ -3,15 +3,52 @@ package org.pmiops.workbench.db.dao;
 
 import org.pmiops.workbench.db.model.CohortAnnotationDefinition;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CohortAnnotationDefinitionDao extends JpaRepository<CohortAnnotationDefinition, Long> {
 
-    CohortAnnotationDefinition findByCohortIdAndColumnName(@Param("cohortId") long cohortId, @Param("ColumnName") String ColumnName);
+    CohortAnnotationDefinition findByCohortIdAndColumnName(@Param("cohortId") long cohortId,
+                                                           @Param("ColumnName") String ColumnName);
 
     List<CohortAnnotationDefinition> findByCohortId(@Param("cohortId") long cohortId);
 
-    CohortAnnotationDefinition findByCohortIdAndCohortAnnotationDefinitionId( @Param("cohortId") long cohortId, @Param("cohortAnnotationDefinitionId") long cohortAnnotationDefinitionId);
+    CohortAnnotationDefinition findByCohortIdAndCohortAnnotationDefinitionId(@Param("cohortId") long cohortId,
+                                                                             @Param("cohortAnnotationDefinitionId") long cohortAnnotationDefinitionId);
+
+    // We use native SQL here as there may be a large number of rows within a
+    // given cohort review; this avoids loading them into memory.
+    @Modifying
+    @Query(
+        value="INSERT INTO cohort_annotation_definition" +
+        " (cohort_id, column_name, annotation_type)" +
+        " SELECT :toCohortId, column_name, annotation_type" +
+        " FROM cohort_annotation_definition" +
+        " WHERE cohort_id = :fromCohortId",
+        nativeQuery=true)
+    void bulkCopyCohortAnnotationDefinitionByCohort(@Param("fromCohortId") long fromCohortId,
+                                                    @Param("toCohortId") long toCohortId);
+
+    // We use native SQL here as there may be a large number of rows within a
+    // given cohort review; this avoids loading them into memory.
+    @Modifying
+    @Query(
+        value="INSERT INTO cohort_annotation_enum_value" +
+        " (cohort_annotation_definition_id, name, enum_order)" +
+        " SELECT cad2.cohort_annotation_definition_id, name, enum_order" +
+        " FROM cohort_annotation_definition cad1" +
+        " JOIN cohort_annotation_enum_value caev ON (caev.cohort_annotation_definition_id = cad1.cohort_annotation_definition_id " +
+        "                                            AND cad1.cohort_id = :fromCohortId)" +
+        " JOIN cohort_annotation_definition cad2 on (cad2.cohort_id = :toCohortId AND cad2.column_name = cad1.column_name)",
+        nativeQuery=true)
+    /**
+     * IMPORTANT NOTE:
+     * This method will only bulk copy correctly when called after
+     * {@link CohortAnnotationDefinitionDao#bulkCopyCohortAnnotationDefinitionByCohort(long, long)}
+     */
+    void bulkCopyCohortAnnotationEnumsByCohort(@Param("fromCohortId") long fromCohortId,
+                                               @Param("toCohortId") long toCohortId);
 }
