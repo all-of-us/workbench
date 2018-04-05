ed7f18d6c83ae54ce7e189a64fda462004f0116c
John Still <john.m.still@vumc.org>
2018-04-05 15:59:23 -0500

    expand the selection panel

---
+++ ui/src/app/cohort-search/criteria-wizard/modifier-selection/modifier-selection.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'crit-modifier-selection',
+  templateUrl: './modifier-selection.component.html',
+  styleUrls: ['./modifier-selection.component.css']
+})
+export class ModifierSelectionComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
