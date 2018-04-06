1ad19cb0a5b26c7cafcd8127bc6fa5ce0b259764
johnstill <31938607+johnstill@users.noreply.github.com>
2018-04-06 10:58:23 -0500

    Js/rw 310 stub modifier page (#688)
    * stub out modifiers page
    
    * Adding Modifiers works
    
    * expand the selection panel
    
    * basic display and removal of modifiers
    
    * Send the modifiers back to the API
    
    * Remove nonexistent handler
    
    * Add destroy hook

--- ui/src/app/cohort-search/criteria-wizard/wizard/wizard.component.spec.ts
+++ ui/src/app/cohort-search/criteria-wizard/wizard/wizard.component.spec.ts
@@ -14,19 +14,21 @@ import {
   WIZARD_FINISH,
 } from '../../redux';
 
+/* tslint:disable */
 import {AlertsComponent} from '../alerts/alerts.component';
 import {AttributesModule} from '../attributes/attributes.module';
 import {DemoFormComponent} from '../demo-form/demo-form.component';
 import {ExplorerComponent} from '../explorer/explorer.component';
 import {LeafComponent} from '../leaf/leaf.component';
-import {
-  QuickSearchResultsComponent
-} from '../quicksearch-results/quicksearch-results.component';
+import {ModifiersComponent} from '../modifiers/modifiers.component';
+import {ModifierSelectionComponent} from '../modifier-selection/modifier-selection.component';
+import {QuickSearchResultsComponent} from '../quicksearch-results/quicksearch-results.component';
 import {QuickSearchComponent} from '../quicksearch/quicksearch.component';
 import {RootSpinnerComponent} from '../root-spinner/root-spinner.component';
-import {SelectionComponent} from '../selection/selection.component';
+import {CriteriaSelectionComponent} from '../criteria-selection/criteria-selection.component';
 import {TreeComponent} from '../tree/tree.component';
 import {WizardComponent} from './wizard.component';
+/* tslint:enable */
 
 import {CohortBuilderService} from 'generated';
 
@@ -50,10 +52,12 @@ describe('WizardComponent', () => {
           DemoFormComponent,
           ExplorerComponent,
           LeafComponent,
+          ModifiersComponent,
+          ModifierSelectionComponent,
           QuickSearchComponent,
           QuickSearchResultsComponent,
           RootSpinnerComponent,
-          SelectionComponent,
+          CriteriaSelectionComponent,
           TreeComponent,
           WizardComponent,
         ],
