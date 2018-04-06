8b01f8344095bd0fb2aaee117b841c9216bbcea4
CH Albach <calbach@google.com>
2018-04-06 12:54:06 -0700

    Use --aot in test, --prod elsewhere

--- ui/src/environments/environment.prod.ts
+++
@@ -1,7 +0,0 @@
-export const environment: object = {
-  displayTag: null,
-  allOfUsApiUrl: 'https://api-dot-all-of-us-workbench.appspot.com',
-  publicApiUrl: 'https://public-api-dot-all-of-us-workbench.appspot.com',
-  debug: false,
-  testing: false
-};
