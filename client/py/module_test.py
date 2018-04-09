62248bd85c0dad13f816e25ef2ccbe899b4c10fd
CH Albach <calbach@users.noreply.github.com>
2018-04-09 10:01:38 -0700

    rm deprecated client/ dir; see github.com/all-of-us/pyclient (#696)

--- client/py/module_test.py
+++
@@ -1,9 +0,0 @@
-import unittest
-import aou_workbench_client  # pylint: disable=unused-import
-from aou_workbench_client import swagger_client  # pylint: disable=unused-import
-
-
-class ModuleTest(unittest.TestCase):
-
-    def test_noop_as_verification_that_module_imports_worked(self):
-        pass
