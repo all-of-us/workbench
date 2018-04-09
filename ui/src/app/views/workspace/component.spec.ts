6ff5f149dae65c13196f53fa10febc852aa5ddd5
Rebecca Asch <rasch@broadinstitute.org>
2018-04-09 15:43:29 -0400

    lint change (2)

--- ui/src/app/views/workspace/component.spec.ts
+++ ui/src/app/views/workspace/component.spec.ts
@@ -8,8 +8,8 @@ import {ClarityModule} from '@clr/angular';
 
 import {IconsModule} from 'app/icons/icons.module';
 import {SignInService} from 'app/services/sign-in.service';
-import {WorkspaceComponent} from 'app/views/workspace/component';
 import {WorkspaceShareComponent} from 'app/views/workspace-share/component';
+import {WorkspaceComponent} from 'app/views/workspace/component';
 import {ClusterService, CohortsService, WorkspaceAccessLevel, WorkspacesService} from 'generated';
 import {ClusterServiceStub} from 'testing/stubs/cluster-service-stub';
 import {CohortsServiceStub} from 'testing/stubs/cohort-service-stub';
