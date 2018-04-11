436e5365c8a298ddc01d223979f4afc9c6188dfd
Brian Freeman <brian.d.freeman@vumc.org>
2018-04-11 09:37:57 -0500

    RW-495 refactored UI to use enums.

---
+++ ui/src/app/cohort-search/criteria-wizard/modifier-selection/modifier-selection.component.ts
@@ -0,0 +1,37 @@
+import {select} from '@angular-redux/store';
+import {Component} from '@angular/core';
+
+import {activeModifierList, CohortSearchActions} from '../../redux';
+import {ModifierType, Operator} from 'generated';
+
+@Component({
+  selector: 'crit-modifier-selection',
+  templateUrl: './modifier-selection.component.html',
+  styleUrls: ['./modifier-selection.component.css']
+})
+export class ModifierSelectionComponent {
+  @select(activeModifierList) modifiers$;
+  constructor(private actions: CohortSearchActions) {}
+
+  remove(modifier): void {
+    this.actions.removeModifier(modifier);
+  }
+
+  name(modifier) {
+    return {
+      [ModifierType.AGEATEVENT]: 'Age At Event',
+      [ModifierType.NUMOFOCCURRENCES]: 'Number of Occurrences',
+      [ModifierType.EVENTDATE]: 'Date of Event',
+    }[modifier.get('name')];
+  }
+
+  value(modifier) {
+    const op = {
+      [Operator.EQUAL]: 'Equal To',
+      [Operator.GREATERTHAN]: 'Greater Than',
+      [Operator.LESSTHAN]: 'Less Than',
+    }[modifier.get('operator')];
+
+    return `${op} ${modifier.getIn(['operands', 0])}`;
+  }
+}
---
+++ ui/src/app/cohort-search/criteria-wizard/modifiers/modifiers.component.ts
@@ -0,0 +1,107 @@
+import {NgRedux} from '@angular-redux/store';
+import {Component, OnDestroy, OnInit} from '@angular/core';
+import {FormControl, FormGroup} from '@angular/forms';
+import {fromJS, List} from 'immutable';
+import {Subscription} from 'rxjs/Subscription';
+
+import {
+  activeModifierList,
+  CohortSearchActions,
+  CohortSearchState,
+} from '../../redux';
+
+import {Modifier, ModifierType, Operator} from 'generated';
+
+@Component({
+  selector: 'crit-modifiers',
+  templateUrl: './modifiers.component.html',
+  styleUrls: ['./modifiers.component.css']
+})
+export class ModifiersComponent implements OnInit, OnDestroy {
+  modifierType = ModifierType;
+  existing = List();
+  subscription: Subscription;
+  ageAtEventMapEntries: any;
+  numOfOccurrencesMapEntries: any;
+  eventDateMapEntries: any;
+
+  form = new FormGroup({
+    [this.modifierType.AGEATEVENT]: new FormGroup({
+      operator: new FormControl(),
+      value: new FormControl(),
+    }),
+    [this.modifierType.NUMOFOCCURRENCES]: new FormGroup({
+      operator: new FormControl(),
+      value: new FormControl(),
+    }),
+    [this.modifierType.EVENTDATE]: new FormGroup({
+      operator: new FormControl(),
+      value: new FormControl(),
+    }),
+  });
+
+  constructor(
+    private ngRedux: NgRedux<CohortSearchState>,
+    private actions: CohortSearchActions,
+  ) {}
+
+  ngOnInit() {
+    const ageAtEventMap = new Map([
+        [Operator.EQUAL, 'Equal To'],
+        [Operator.GREATERTHAN, 'Greater Than'],
+        [Operator.LESSTHAN, 'Less Than']
+    ]);
+    this.ageAtEventMapEntries = Array.from(ageAtEventMap.entries());
+
+    const numOfOccurrencesMap = new Map([
+        [Operator.EQUAL, 'Equal To'],
+        [Operator.GREATERTHAN, 'Greater Than'],
+        [Operator.LESSTHAN, 'Less Than']
+    ]);
+    this.numOfOccurrencesMapEntries = Array.from(numOfOccurrencesMap.entries());
+
+    const eventDateMap = new Map([
+        [Operator.EQUAL, 'On'],
+        [Operator.GREATERTHAN, 'After'],
+        [Operator.LESSTHAN, 'Before']
+    ]);
+    this.eventDateMapEntries = Array.from(eventDateMap.entries());
+
+    this.form.valueChanges.subscribe(console.log);
+    this.subscription = this.ngRedux
+      .select(activeModifierList)
+      .subscribe(mods => this.existing = mods);
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  isSelected(name) {
+    const modifier = this.toModifier(name);
+    if (modifier) {
+      return this.existing.includes(fromJS(modifier));
+    }
+  }
+
+  select(name) {
+    const modifier = this.toModifier(name);
+    if (modifier) {
+      this.actions.addModifier(modifier);
+    }
+  }
+
+  private toModifier(name) {
+    const group = this.form.get(name);
+    const operator = group.get('operator').value;
+    const value = group.get('value').value;
+    if (value === null  || operator === null) {
+      return ; // noop
+    }
+    return <Modifier>{
+      name: this.modifierType[this.modifierType[name]],
+      operator: Operator[Operator[operator]],
+      operands: [value]
+    };
+  }
+}
