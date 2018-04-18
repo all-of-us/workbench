c62869a79a01f8031bfc908f47d8a29954f84099
johnstill <31938607+johnstill@users.noreply.github.com>
2018-04-18 07:34:47 -0500

    Fix sidebar, cohort-review wrapper, persistent scrollbar (#737)
    * Fix sidebar, cohort-review wrapper, persistent scrollbar
    
    * Add comment about height

--- ui/src/app/cohort-review/page-layout/page-layout.ts
+++ ui/src/app/cohort-review/page-layout/page-layout.ts
@@ -1,39 +1,22 @@
-import {
-  Component,
-  ElementRef,
-  HostListener,
-  OnInit,
-  Renderer2,
-  ViewChild,
-} from '@angular/core';
+import {Component, OnInit} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Subscription} from 'rxjs/Subscription';
 
-import {ReviewStateService} from '../review-state.service';
-
 import {ReviewStatus} from 'generated';
-
-const pixel = (n: number) => `${n}px`;
-const ONE_REM = 24;  // value in pixels
-
+import {ReviewStateService} from '../review-state.service';
 
 @Component({
   templateUrl: './page-layout.html',
   styleUrls: ['./page-layout.css']
 })
 export class PageLayout implements OnInit {
-  @ViewChild('wrapper') wrapper: ElementRef;
-
   constructor(
     private state: ReviewStateService,
     private route: ActivatedRoute,
     private router: Router,
-    private renderer: Renderer2,
   ) {}
 
   ngOnInit() {
-    this.updateWrapperDimensions();
-
     const {annotationDefinitions, cohort, review} = this.route.snapshot.data;
     this.state.annotationDefinitions.next(annotationDefinitions);
     this.state.cohort.next(cohort);
@@ -42,21 +25,5 @@ export class PageLayout implements OnInit {
     if (review.reviewStatus === ReviewStatus.NONE) {
       this.router.navigate(['create'], {relativeTo: this.route});
     }
-  }
-
-  @HostListener('window:resize')
-  onResize() {
-    this.updateWrapperDimensions();
-  }
-
-  updateWrapperDimensions() {
-    const nativeEl = this.wrapper.nativeElement;
-    const {top} = nativeEl.getBoundingClientRect();
-
-    // margin-top, margin-bottom each one rem, see the css file
-    const margins = ONE_REM * 2;
-
-    const minHeight = pixel(window.innerHeight - top - margins);
-    this.renderer.setStyle(nativeEl, 'min-height', minHeight);
   }
 }
