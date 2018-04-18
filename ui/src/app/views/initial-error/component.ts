99975d0a94b50b7633cca025f76e150dda81b749
blrubenstein <blrubenstein@wpi.edu>
2018-04-18 09:15:58 -0400

    Clean up PR (#740)

--- ui/src/app/views/initial-error/component.ts
+++ ui/src/app/views/initial-error/component.ts
@@ -4,23 +4,24 @@ import {NavigationError, Router} from '@angular/router';
 
 // Mostly borrowed from the Job Manager.
 @Component({
-    selector: 'app-not-found',
+    selector: 'app-initial-error',
     templateUrl: './component.html',
     styleUrls: ['./component.css']
 })
-@Injectable()
-export class NotFoundComponent {
+export class InitialErrorComponent {
   initialLoadErrorHeader: string;
   initialLoadFailure = false;
 
   constructor(router: Router, private loc: Location) {
     router.events.subscribe((e) => {
       if (e instanceof NavigationError && !router.navigated) {
-        // Hack: On navigation failure, Angular strips the path from the URL.
-        // This is undesirable in the event of a 500/503 on initial load refresh
-        // the page, or on other errors where the user may want to inspect the
-        // URL. Restore the URL here; luckily this doesn't cause another Angular
-        // Router nagivate.
+        /*
+         * Hack: On navigation failure, Angular strips the path from the URL.
+         * This is undesirable in the event of a 500/503 on initial load refresh
+         * the page, or on other errors where the user may want to inspect the
+         * URL. Restore the URL here; luckily this doesn't cause another Angular
+         * Router nagivate.
+         */
         loc.replaceState(e.url);
         this.initialLoadFailure = true;
         const status = e.error.status || 'unknown';
@@ -28,15 +29,13 @@ export class NotFoundComponent {
         this.initialLoadErrorHeader = `${status}: ${title}`;
       }
       if (router.navigated) {
-        // In the event that one of our resolvers/activators did another
-        // navigate on failure, hide the error. Unexpected and not ideal as we'd
-        // briefly flash the error html on screen.
+        /*
+         * In the event that one of our resolvers/activators did another
+         * navigate on failure, hide the error. Unexpected and not ideal as we'd
+         * briefly flash the error html on screen.
+         */
         this.initialLoadFailure = false;
       }
     });
-  }
-
-  reload(): void {
-    window.location.reload();
   }
 }
