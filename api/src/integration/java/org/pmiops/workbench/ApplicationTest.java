ceb67e808647edb0ee826cac2c153783d65665e8
Brian Freeman <brian.d.freeman@vumc.org>
2018-04-19 12:11:12 -0500

    RW-391 added comment about initializing the database.

--- api/src/integration/java/org/pmiops/workbench/ApplicationTest.java
+++ api/src/integration/java/org/pmiops/workbench/ApplicationTest.java
@@ -20,6 +20,16 @@ public class ApplicationTest {
     @Autowired
     private ApplicationContext context;
 
+    /**
+     * Spring Boot provides a mechanism that will create a schema and load data into it.
+     * It loads SQL from the standard root classpath locations: src/integration/resources/schema.sql
+     * and src/integration/resources/data.sql, respectively. In addition, Spring Boot processes the schema-${platform}.sql
+     * and data-${platform}.sql files (if present), where platform is the value of spring.datasource.platform.
+     * This allows you to switch to database-specific scripts if necessary. For example, you might choose
+     * to set it to the vendor name of the database (hsqldb, h2, oracle, mysql, postgresql, and so on).
+     *
+     * @throws Exception
+     */
     @Test
     public void contextLoads() throws Exception {
         //This loads all dao's that implement JPA repositories
