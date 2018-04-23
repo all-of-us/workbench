f13c5bceaf7093e8608f7d47ea2dfefb1f568362
John Still <john.m.still@vumc.org>
2018-04-23 10:12:47 -0500

    wip

--- ui/src/app/cohort-search/modal/modal.component.ts
+++ ui/src/app/cohort-search/modal/modal.component.ts
@@ -1,15 +1,22 @@
-import { Component, OnInit } from '@angular/core';
+import {select} from '@angular-redux/store';
+import {Component, Input, OnInit} from '@angular/core';
+
+import {wizardOpen} from '../redux';
 
 @Component({
   selector: 'app-modal',
   templateUrl: './modal.component.html',
   styleUrls: ['./modal.component.css']
 })
-export class ModalComponent implements OnInit {
-
-  constructor() { }
+export class ModalComponent {
+  @select(wizardOpen) open$: Observable<boolean>;
+  open = false;
 
   ngOnInit() {
+    this.subscription = this.open$.subscribe(open => this.open = open);
   }
 
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
 }
