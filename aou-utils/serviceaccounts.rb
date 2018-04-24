8fae8515d8829eb41f730ff42aef62d7da57437a
CH Albach <calbach@users.noreply.github.com>
2018-04-24 13:56:08 -0700

    Don't generate another key if the target SA is already active (#11)

--- aou-utils/serviceaccounts.rb
+++ aou-utils/serviceaccounts.rb
@@ -1,10 +1,13 @@
+require "json"
 require_relative "workbench"
 require_relative "utils/common"
 
-# Puts the AppEngine service account credentials in "sa-key.json" for use in command line
-# utilities; sets GOOGLE_APPLICATION_CREDENTIALS to point at it, so application default
-# credentials will use it.
-# For environments other than test, deletes the credentials after use.
+# Entering a service account context ensures that a keyfile exists at the given
+# path for the given service account, and that GOOGLE_APPLICATION_CREDENTIALS is
+# pointing to it (for application default credentials). Creates this SA key and
+# file if necessary, and destroys it when leaving the context.
+# The test SA key is a special case for local development, and is initialized
+# but not deleted.
 class ServiceAccountContext
 
   SERVICE_ACCOUNT_KEY_PATH = "sa-key.json"
@@ -18,20 +21,39 @@ class ServiceAccountContext
     end
   end
 
+  def existing_file_account(path)
+    if File.exists?(path)
+      begin
+        return JSON.parse(File.read(path))["client_email"]
+      rescue JSON::ParserError => e
+        return nil
+      end
+    end
+    return nil
+  end
+
   def run()
-    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = @path
     common = Common.new
-    if @project == "all-of-us-workbench-test" and not @service_account
+    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = @path
+    service_account = @service_account
+    if not service_account
+      service_account = "#{@project}@appspot.gserviceaccount.com"
+    end
+    if service_account == existing_file_account(@path)
+      # Don't generate another key if this account is already active. This can
+      # happen for nested service account contexts, for example.
+      common.status "Attaching to existing keyfile @ #{@path}"
+      yield
+      return
+    end
+
+    if service_account == "all-of-us-workbench-test@appspot.gserviceaccount.com"
       unless File.exists?(@path)
         common.run_inline %W{gsutil cp gs://#{@project}-credentials/app-engine-default-sa.json
             #{@path}}
       end
       yield
     else
-      service_account = @service_account
-      if not service_account
-        service_account = "#{@project}@appspot.gserviceaccount.com"
-      end
       common.run_inline %W{gcloud iam service-accounts keys create #{@path}
           --iam-account=#{service_account} --project=#{@project}}
       begin
