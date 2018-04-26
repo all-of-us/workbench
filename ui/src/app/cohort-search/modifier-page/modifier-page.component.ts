907101c74d526611773dc2aa7a2f5066c159c03e
John Still <john.m.still@vumc.org>
2018-04-26 15:32:35 -0500

    Modifiers WORKS!!!

--- ui/src/app/cohort-search/modifier-page/modifier-page.component.ts
+++ ui/src/app/cohort-search/modifier-page/modifier-page.component.ts
@@ -1,16 +1,17 @@
-import {NgRedux} from '@angular-redux/store';
+import {select} from '@angular-redux/store';
 import {Component, OnDestroy, OnInit} from '@angular/core';
 import {FormControl, FormGroup} from '@angular/forms';
 import {fromJS, List} from 'immutable';
 import {Subscription} from 'rxjs/Subscription';
 
 import {
-    activeModifierList,
-    CohortSearchActions,
-    CohortSearchState,
+  activeModifierList,
+  CohortSearchActions,
+  CohortSearchState,
 } from '../redux';
 
 import {Modifier, Operator} from 'generated';
+import {Modifier, ModifierType, Operator} from 'generated';
 
 @Component({
     selector: 'crit-modifier-page',
@@ -18,6 +19,10 @@ import {Modifier, Operator} from 'generated';
     styleUrls: ['./modifier-page.component.css']
 })
 export class ModifierPageComponent implements OnInit, OnDestroy {
+  @select(activeModifierList) modifiers$;
+
+  existing = List();
+  subscription: Subscription;
     existing = List();
     subscription: Subscription;
     ageAtEventMap: any;
@@ -25,72 +30,135 @@ export class ModifierPageComponent implements OnInit, OnDestroy {
     eventDateMap: any;
     ageAtEventMapEntries: any;
 
-    form = new FormGroup({
-        ageAtEvent: new FormGroup({
-            operator: new FormControl(),
-            value: new FormControl(),
-        }),
-        numOfOccurrences: new FormGroup({
-            operator: new FormControl(),
-            value: new FormControl(),
-        }),
-        eventDate: new FormGroup({
-            operator: new FormControl(),
-            value: new FormControl(),
-        }),
+  readonly modifiers = [{
+    name: 'ageAtEvent',
+    label: 'Age At Event',
+    inputType: 'number',
+    modType: 'AGE_AT_EVENT',
+    operators: [{
+      name: 'Greater Than or Equal To',
+      value: Operator.GREATER_THAN_OR_EQUAL_TO,
+    }, {
+      name: 'Less Than or Equal To',
+      value: Operator.LESS_THAN_OR_EQUAL_TO,
+    }, {
+      name: 'Between',
+      value: Operator.BETWEEN,
+    }]
+  }, {
+    name: 'eventDate',
+    label: 'Event Date',
+    inputType: 'date'
+    modType: 'EVENT_DATE',
+    operators: [{
+      name: 'Is On or Before',
+      value: Operator.GREATER_THAN_OR_EQUAL_TO,
+    }, {
+      name: 'Is On or After',
+      value: Operator.LESS_THAN_OR_EQUAL_TO,
+    }, {
+      name: 'Is Between',
+      value: Operator.BETWEEN,
+    }]
+  }, {
+    name: 'hasOccurrences',
+    label: 'Has Occurrences',
+    inputType: 'number',
+    modType: 'NUM_OF_OCCURRENCES',
+    operators: [{
+      name: 'N or More',
+      value: Operator.GREATER_THAN_OR_EQUAL_TO,
+    }]
+  }];
+
+  form = new FormGroup({
+    ageAtEvent: new FormGroup({
+      operator: new FormControl(),
+      valueA: new FormControl(),
+      valueB: new FormControl(),
+    }),
+    hasOccurrences: new FormGroup({
+      operator: new FormControl(),
+      valueA: new FormControl(),
+      valueB: new FormControl(),
+    }),
+    eventDate: new FormGroup({
+      operator: new FormControl(),
+      valueA: new FormControl(),
+      valueB: new FormControl(),
+    }),
+  });
+
+  constructor(private actions: CohortSearchActions) {}
+
+  ngOnInit() {
+    this.subscription = this.modifiers$.subscribe(mods => this.existing = mods);
+
+    // This reseeds the form with existing data if we're editing an existing group
+    this.subscription.add(this.modifiers$.first().subscribe(mods => {
+      mods.forEach(mod => {
+        const meta = this.modifiers.find(_mod => mod.get('name') === _mod.modType);
+        if (meta) {
+          this.form.get(meta.name).setValue({
+            operator: mod.get('operator'),
+            valueA: mod.getIn(['operands', 0]),
+            valueB: mod.getIn(['operands', 1]),
+          }, {emitEvent: false});
+        }
+      });
+    }));
+
+    this.subscription.add(this.form.valueChanges
+      .do(console.log)
+      .map(vals => this.currentMods(vals))
+      .subscribe(newMods => {
+        console.log(this.existing);
+        console.log(newMods);
+
+        /*
+         * NOTE: the way this process works is basically as follows: 1) compute
+         * a modifier per modifier category 2) merge those with the existing
+         * modifiers 3) check for any existing modifiers that no longer exist
+         * and remove them 4) iterate through the new mods and add them
+         *
+         * This is a "good enough for now" kind of thing.  If it gets too slow,
+         * then the faster way would be to have an "update mods" action" and
+         * move all the work into the reducer, effectively ending the story at
+         * step two.  Leaving it this way, though, gives us some flexibility
+         * with individual modifiers if we need it.
+         */
+        this.existing
+          .filter(mod => !newMods.includes(mod))
+          .forEach(mod => this.actions.removeModifier(mod));
+
+        // dispatch the new
+        newMods
+          .filter(mod => !!mod)
+          .filter(mod => !this.existing.includes(mod))
+          .forEach(mod => this.actions.addModifier(mod));
+      })
+    );
+  }
+
+  ngOnDestroy() {
+    this.subscription.unsubscribe();
+  }
+
+  currentMods(vals) {
+    return this.modifiers.map(({name, inputType, modType}) => {
+      const {operator, valueA, valueB} = vals[name];
+      const between = operator === Operator.BETWEEN;
+      if (!operator || !valueA || (between && !valueB)) {
+        return ;
+      }
+      let operands = [valueA];
+      if (between) { operands.push(valueB); }
+      const mod = <Modifier>{name: modType, operator, operands};
+      return fromJS(mod);
     });
+  }
 
-    constructor(
-        private ngRedux: NgRedux<CohortSearchState>,
-        private actions: CohortSearchActions,
-    ) {}
-
-    ngOnInit() {
-        this.ageAtEventMap = new Map();
-        this.ageAtEventMap.set(Operator.EQUAL, 'Equal To');
-        this.ageAtEventMap.set(Operator.GREATERTHAN, 'Greater Than');
-        this.ageAtEventMap.set(Operator.LESSTHAN, 'Less Than');
-        this.ageAtEventMapEntries = Array.of(this.ageAtEventMap.entries());
-        this.numOfOccurrencesMap = new Map();
-        this.numOfOccurrencesMap.set(Operator.EQUAL, 'Equal To');
-        this.numOfOccurrencesMap.set(Operator.GREATERTHAN, 'Greater Than');
-        this.numOfOccurrencesMap.set(Operator.LESSTHAN, 'Less Than');
-        this.eventDateMap = new Map();
-        this.eventDateMap.set(Operator.EQUAL, 'On');
-        this.eventDateMap.set(Operator.GREATERTHAN, 'After');
-        this.eventDateMap.set(Operator.LESSTHAN, 'Before');
-
-        this.form.valueChanges.subscribe(console.log);
-        this.subscription = this.ngRedux
-            .select(activeModifierList)
-            .subscribe(mods => this.existing = mods);
-    }
-
-    ngOnDestroy() {
-        this.subscription.unsubscribe();
-    }
-
-    isSelected(name) {
-        const modifier = this.toModifier(name);
-        if (modifier) {
-            return this.existing.includes(fromJS(modifier));
-        }
-    }
-
-    select(name) {
-        const modifier = this.toModifier(name);
-        if (modifier) {
-            this.actions.addModifier(modifier);
-        }
-    }
-
-    private toModifier(name) {
-        const group = this.form.get(name);
-        const operator = group.get('operator').value;
-        const value = group.get('value').value;
-        if (value === null  || operator === null) {
-            return ; // noop
-        }
-        return <Modifier>{name, operator, operands: [value]};
-    }
+  showValueB(modName) {
+    return this.form.get([modName, 'operator']).value === Operator.BETWEEN;
+  }
 }
