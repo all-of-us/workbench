42ec9e106f6f0a6dfc0f44fb9b6752cecc32c0dc
CH Albach <calbach@google.com>
2018-04-26 15:28:09 -0700

    Unchecked exceptions...

--- api/src/main/java/org/pmiops/workbench/google/DirectoryService.java
+++ api/src/main/java/org/pmiops/workbench/google/DirectoryService.java
@@ -9,9 +9,8 @@ import java.io.IOException;
  * Encapsulate Googe APIs for handling GSuite user accounts.
  */
 public interface DirectoryService {
-  public boolean isUsernameTaken(String username) throws IOException;
-  public User getUser(String email) throws IOException;
-  public User createUser(String givenName, String familyName, String username, String password)
-      throws IOException;
-  public void deleteUser(String username) throws IOException;
+  public boolean isUsernameTaken(String username);
+  public User getUser(String email);
+  public User createUser(String givenName, String familyName, String username, String password);
+  public void deleteUser(String username);
 }
