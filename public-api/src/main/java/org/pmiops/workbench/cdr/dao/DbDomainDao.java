214299d8c6e41f9cca09262c3a4478e66e791fdf
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-04-26 13:22:25 -0500

    Survey List && Domain Filter List

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
@@ -1,5 +1,6 @@
 package org.pmiops.workbench.cdr.dao;
 import org.pmiops.workbench.cdr.model.DbDomain;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
 
 import java.util.List;
@@ -7,4 +8,9 @@ import java.util.List;
 public interface DbDomainDao extends CrudRepository<DbDomain, Long> {
     // TODO -- maybe add order by
     List<DbDomain> findAll();
+
+    List<DbDomain> findByDbType(String db_type);
+
+    @Query(nativeQuery = true,value = "select * from db_domain where db_type=?1 and concept_id <> 0")
+    List<DbDomain> findSurveys(String db_type);
 }
