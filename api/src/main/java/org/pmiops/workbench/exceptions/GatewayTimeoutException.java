639166069948e11dd6b2ef47db8df5dc075696d4
Brian Rubenstein <brubenst@broadinstitute.org>
2018-04-30 09:39:21 -0400

    Remove unneeded constructor

--- api/src/main/java/org/pmiops/workbench/exceptions/GatewayTimeoutException.java
+++ api/src/main/java/org/pmiops/workbench/exceptions/GatewayTimeoutException.java
@@ -4,8 +4,4 @@ import org.springframework.http.HttpStatus;
 import org.springframework.web.bind.annotation.ResponseStatus;
 
 @ResponseStatus(HttpStatus.GATEWAY_TIMEOUT)
-public class GatewayTimeoutException extends WorkbenchException {
-  public GatewayTimeoutException() {
-    super();
-  }
-}
+public class GatewayTimeoutException extends WorkbenchException {}
