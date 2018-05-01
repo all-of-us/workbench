5833e4f26850d7236d0dd57a997c5f3be2575a3a
Brian Freeman <brian.d.freeman@vumc.org>
2018-05-01 14:52:57 -0500

    Freemabd1/concept ancestor - adding table concept_ancestor to build (#784)
    * added concept_ancestor.
    
    * formatting.
    
    * removed smaller file.
    
    * Remove share route (#786)
    
    * [RW-573] Default to tab on the left (#785)
    
    * Default to tab on the left
    
    * Fix tests
    
    * Remove extra line
    
    * [RW-437] - more updates based on reviews (#787)
    
    * [RW-475] Move cluster creation as soon as possible (#769)
    
    * Move cluster creation as soon as possible
    
    * Test fix
    
    * Code style improvements
    
    * Code style improvement
    
    * PR Feedback
    
    * Remove unused imports
    
    * Gateway Timeout
    
    * Remove unused imports
    
    * Change status code
    
    * Move to isSocketTimeout, and remove unused constructors
    
    * Avoid if/else statement that is unnecessary
    
    * Remove unneeded constructor
    
    * Move to getCause
    
    * Survey List && Domain Filter List (#773)
    
    * Survey List && Domain Filter List
    
    * Change in Dao Query
    
    * Change in Dao Query
    
    * Db Domain Dao
    
    * Nulled concept ids for survey modules that are not yet present
    
    * CSV and Dao changes of Db Domain
    
    * Added test to DbDomainDao and changed null for few concepts to 0 in db domain
    
    * Changed the concept_id column to be nullable in db_domain model
    
    * Addressing quality check comments
    
    * Addressing quality check comments
    
    * Addressing quality check comments
    
    * Addressing quality check comments
    
    * Added survey module relevant rows in concept and the concept_relationship to the local csv
    
    * Concept.csv changes added
    
    * Concept & concept relationship changes
    
    * Check if profile is loaded (#790)
    
    * Increase criteria limit (#791)
    
    * Switch to @Lob
    
    * added concept_ancestor.
    
    * formatting.
    
    * removed smaller file.

--- api/db-cdr/generate-cdr/make-bq-data-dump.sh
+++ api/db-cdr/generate-cdr/make-bq-data-dump.sh
@@ -40,14 +40,22 @@ fi
 
 echo "Dumping tables to csv from $BUCKET\n"
 
+##echo "Echoing dataset name"
+##echo ${DATASET}
+
 # Get tables in project, stripping out tableId.
 # Note tables larger than 1 G need to be dumped into more than one file.
 # concept_relationship and concept are only big ones now.
-tables=(achilles_analysis achilles_results achilles_results_concept concept concept_relationship criteria db_domain domain vocabulary )
+if [[ $DATASET == *public* ]] || [[ $DATASET == *PUBLIC* ]];
+then
+    tables=(achilles_analysis achilles_results achilles_results_concept concept concept_relationship criteria db_domain domain vocabulary)
+else
+    tables=(achilles_analysis achilles_results achilles_results_concept concept concept_relationship criteria db_domain domain vocabulary concept_ancestor)
+fi
 
 for table in ${tables[@]}; do
   echo "Dumping table : $table"
-  if [[ $table =~ ^(concept|concept_relationship)$ ]]
+  if [[ $table =~ ^(concept|concept_relationship|concept_ancestor)$ ]]
   then
     bq extract $PROJECT:$DATASET.$table gs://$BUCKET/$DATASET/$table*.csv
   else
