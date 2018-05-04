a9f68b19707a5a3186d2d3aa54468915b59818ee
CH Albach <calbach@users.noreply.github.com>
2018-05-04 09:35:13 -0700

    [RW-470] Unify login navigation for sign in guard and sign out button (#807)

--- ui/src/app/guards/sign-in-guard.service.ts
+++ ui/src/app/guards/sign-in-guard.service.ts
@@ -8,8 +8,9 @@ import {
 
 import {Observable} from 'rxjs/Observable';
 
-
 import {SignInService} from 'app/services/sign-in.service';
+import {navigateLogin} from 'app/utils';
+
 
 declare const gapi: any;
 
@@ -18,11 +19,8 @@ export class SignInGuard implements CanActivate, CanActivateChild {
   constructor(private signInService: SignInService, private router: Router) {}
 
   canActivate(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
-    return this.signInService.isSignedIn$
-      .do(isSignedIn => isSignedIn || this.router.navigate(['/login', {
-        from: state.url,
-      }]));
-
+    return this.signInService.isSignedIn$.do(
+      isSignedIn => isSignedIn || navigateLogin(this.router, state.url));
   }
   canActivateChild(route: ActivatedRouteSnapshot, state: RouterStateSnapshot): Observable<boolean> {
     return this.canActivate(route, state);
