a05c1c3e324645f976c56445d420bd03c73158ea
John Still <john.m.still@vumc.org>
2018-05-08 14:44:54 -0500

    Fetch all data for tab upfront

--- ui/src/app/cohort-review/detail-tabs/detail-tabs.component.spec.ts
+++ ui/src/app/cohort-review/detail-tabs/detail-tabs.component.spec.ts
@@ -1,8 +1,10 @@
 import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
+import {ActivatedRoute} from '@angular/router';
+
+import {CohortReviewService} from 'generated';
 
 import {DetailTabsComponent} from './detail-tabs.component';
-
 
 describe('DetailTabsComponent', () => {
   let component: DetailTabsComponent;
@@ -12,6 +14,10 @@ describe('DetailTabsComponent', () => {
     TestBed.configureTestingModule({
       declarations: [DetailTabsComponent],
       schemas: [NO_ERRORS_SCHEMA],
+      providers: [
+        {provide: ActivatedRoute, useValue: {}},
+        {provide: CohortReviewService, useValue: {}},
+      ]
     })
     .compileComponents();
   }));
