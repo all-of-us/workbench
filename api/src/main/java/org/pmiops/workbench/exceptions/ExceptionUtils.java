d42184fcf42b91b8a7933eb0f1b29963ea26befd
Brian Rubenstein <brubenst@broadinstitute.org>
2018-05-08 14:01:56 -0400

    Make 1 log statement, up to warn

--- api/src/main/java/org/pmiops/workbench/exceptions/ExceptionUtils.java
+++ api/src/main/java/org/pmiops/workbench/exceptions/ExceptionUtils.java
@@ -53,8 +53,7 @@ public class ExceptionUtils {
 
 
   public static RuntimeException convertFirecloudException(ApiException e) {
-    log.log(e.getCode() >= 500 ? Level.SEVERE : Level.INFO, "Exception calling FireCloud", e);
-    log.log(Level.WARNING, e.getResponseBody());
+    log.log(e.getCode() >= 500 ? Level.SEVERE : Level.WARNING, "Exception calling FireCloud" + e.getResponseBody(), e);
     if (isSocketTimeoutException(e.getCause())) {
       throw new GatewayTimeoutException();
     }
@@ -63,8 +62,7 @@ public class ExceptionUtils {
 
   public static RuntimeException convertNotebookException(
       org.pmiops.workbench.notebooks.ApiException e) {
-    log.log(e.getCode() >= 500 ? Level.SEVERE : Level.INFO, "Exception calling notebooks API", e);
-    log.log(Level.WARNING, e.getResponseBody());
+    log.log(e.getCode() >= 500 ? Level.SEVERE : Level.WARNING, "Exception calling notebooks API" + e.getResponseBody(), e);
     if (isSocketTimeoutException(e)) {
       throw new GatewayTimeoutException();
     }
