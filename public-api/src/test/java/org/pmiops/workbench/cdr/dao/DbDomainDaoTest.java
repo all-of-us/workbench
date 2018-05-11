d5d6f5ec9dc6e5a590d2863ace42d596f4f33378
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-05-11 09:17:41 -0500

    Tests

--- public-api/src/test/java/org/pmiops/workbench/cdr/dao/DbDomainDaoTest.java
+++ public-api/src/test/java/org/pmiops/workbench/cdr/dao/DbDomainDaoTest.java
@@ -5,6 +5,7 @@ import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pmiops.workbench.cdr.model.DbDomain;
+import org.pmiops.workbench.cdr.model.Concept;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
 import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
@@ -25,15 +26,25 @@ public class DbDomainDaoTest {
     @Autowired
     private DbDomainDao dao;
 
+    @Autowired
+    private QuestionConceptDao conceptDao;
+
     private DbDomain dbDomain1;
+    private Concept concept1;
+    private Concept concept2;
+    private Concept concept3;
 
     @Before
     public void setUp() {
 
         dbDomain1 = createDbDomain("Domain1","Sample Domain");
-
+        concept1 = createConcept("Sample Hypertension","Condition");
+        concept2 = createConcept("Sample Diabetes","Condition");
+        concept3 = createConcept("Cancer Treatment","Procedure");
         dao.save(dbDomain1);
-
+        conceptDao.save(concept1);
+        conceptDao.save(concept2);
+        conceptDao.save(concept3);
     }
 
     @Test
@@ -60,6 +71,12 @@ public class DbDomainDaoTest {
         Assert.assertEquals(list.get(0).getDomainId(),dbDomain1.getDomainId());
     }
 
+    @Test
+    public void findDomainMatchResults() throws Exception{
+        final List<DbDomain> list=dao.findDomainSearchResults("hypertension");
+        Assert.assertNotEquals(list,null);
+    }
+
     private DbDomain createDbDomain(String domainId, String dbType) {
         return new DbDomain()
                 .domainId(domainId)
@@ -69,6 +86,22 @@ public class DbDomainDaoTest {
                 .domainRoute("Domain Route")
                 .conceptId(Long.valueOf(0))
                 .countValue(Long.valueOf(0));
+    }
+
+    private Concept createConcept(String conceptName,String domainId) {
+        return new Concept()
+                .conceptId(Long.valueOf(0))
+                .conceptName(conceptName)
+                .domainId(domainId)
+                .vocabularyId("Sample Vocabulary")
+                .conceptClassId("Sample Class")
+                .standardConcept("S")
+                .conceptCode(conceptCode)
+                .valid_start_date("1970-01-01")
+                .valid_end_date("2099-12-31")
+                .invalid_reason(null)
+                .count_value(2)
+                .prevalence(0.00);
     }
 
 
