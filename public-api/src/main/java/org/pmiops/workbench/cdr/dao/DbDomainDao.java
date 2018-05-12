c6cefcaf69ddeb7063a9a0f22888a114259c3874
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-05-12 09:03:14 -0500

    Tests

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
@@ -33,7 +33,6 @@ public interface DbDomainDao extends CrudRepository<DbDomain, Long> {
 
     @Query(nativeQuery=true,value="select d.domain_id, d.domain_display, d.domain_desc, d.db_type, d.domain_route,d.concept_id, sum(c.count_value) as count_value from db_domain d\n"+
             "join concept c on d.domain_id = c.domain_id\n" +
-            "where d.db_type = 'domain_filter'\n" +
             "and  (match(c.concept_name) against(?1 in boolean mode) )\n" +
             "group by d.domain_id, d.domain_display, d.domain_desc, d.db_type, d.concept_id\n")
     List<DbDomain> findDomainResults(String keyword);
