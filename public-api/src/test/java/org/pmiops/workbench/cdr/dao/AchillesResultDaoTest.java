de335ef6049c785bd8eba30b564854e3e4217f2d
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-05-14 09:55:36 -0500

    Tests

--- public-api/src/test/java/org/pmiops/workbench/cdr/dao/AchillesResultDaoTest.java
+++ public-api/src/test/java/org/pmiops/workbench/cdr/dao/AchillesResultDaoTest.java
@@ -15,8 +15,6 @@ import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit4.SpringRunner;
 import org.springframework.transaction.annotation.Transactional;
 
-import java.util.List;
-
 @RunWith(SpringRunner.class)
 @DataJpaTest
 @Import(LiquibaseAutoConfiguration.class)
