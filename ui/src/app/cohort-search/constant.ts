82eb27b1535504b0c06937b3660251d3580b8dfa
John Still <john.m.still@vumc.org>
2018-05-14 15:26:06 -0500

    Disable Phys Meas for the time being

--- ui/src/app/cohort-search/constant.ts
+++ ui/src/app/cohort-search/constant.ts
@@ -4,7 +4,7 @@ export const CRITERIA_TYPES = [
   { name: 'ICD10 Codes',  type: 'icd10' },
   // { name: 'PheCodes',     type: 'phecode' },
   { name: 'CPT Codes',    type: 'cpt' },
-  { name: 'Physical Measurements',    type: 'pm' },
+  // { name: 'Physical Measurements',    type: 'pm' },
   // { name: 'Medications',  type: 'meds' },
   // { name: 'Labs',         type: 'labs' },
   // { name: 'Vitals',       type: 'vitals' },
