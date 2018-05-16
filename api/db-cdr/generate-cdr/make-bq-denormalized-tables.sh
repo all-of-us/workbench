68c05ad1d6473fb15e9ab4f6251037d71b2b4b93
Brian Freeman <brian.d.freeman@vumc.org>
2018-05-16 14:09:45 -0500

    RW-692 codacy.

--- api/db-cdr/generate-cdr/make-bq-denormalized-tables.sh
+++ api/db-cdr/generate-cdr/make-bq-denormalized-tables.sh
@@ -51,7 +51,7 @@ else
 fi
 
 # Check that bq_dataset exists and exit if not
-datasets=`bq --project=$BQ_PROJECT ls`
+datasets=$(bq --project=$BQ_PROJECT ls)
 if [ -z "$datasets" ]
 then
   echo "$BQ_PROJECT.$BQ_DATASET does not exist. Please specify a valid project and dataset."
