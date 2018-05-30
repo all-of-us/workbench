90affba120c094746e44330302b551dda0aebac4
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-05-30 11:01:40 -0500

    Fixing codacy errors

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptSearchDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptSearchDao.java
@@ -3,7 +3,6 @@ package org.pmiops.workbench.cdr.dao;
 import org.pmiops.workbench.cdr.model.Concept;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.repository.query.Param;
 
 public interface ConceptSearchDao extends JpaRepository<Concept,Long>,JpaSpecificationExecutor<Concept>{
 
