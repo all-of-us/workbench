58877e9e9cc57ef757fcabd076cba6354ab12736
Brian Freeman <brian.d.freeman@vumc.org>
2018-06-07 10:24:41 -0500

    RW-803 init of PM criteria.

--- ui/src/app/cohort-search/constant.ts
+++ ui/src/app/cohort-search/constant.ts
@@ -1,12 +1,15 @@
-export const CRITERIA_TYPES = [
-  { name: 'Demographics', type: 'demo' },
-  { name: 'ICD9 Codes',   type: 'icd9' },
-  { name: 'ICD10 Codes',  type: 'icd10' },
-  // { name: 'PheCodes',     type: 'phecode' },
-  { name: 'CPT Codes',    type: 'cpt' },
-  // { name: 'Physical Measurements',    type: 'pm' },
-  // { name: 'Medications',  type: 'meds' },
-  // { name: 'Labs',         type: 'labs' },
-  // { name: 'Vitals',       type: 'vitals' },
-  // { name: 'Temporal',     type: 'temporal' }
+export const PROGRAM_TYPES = [
+  { name: 'Physical Measurements',    type: 'pm' },
+];
+
+export const DOMAIN_TYPES = [
+    { name: 'Demographics', type: 'demo' },
+    { name: 'ICD9 Codes',   type: 'icd9' },
+    { name: 'ICD10 Codes',  type: 'icd10' },
+    // { name: 'PheCodes',     type: 'phecode' },
+    { name: 'CPT Codes',    type: 'cpt' },
+    // { name: 'Medications',  type: 'meds' },
+    // { name: 'Labs',         type: 'labs' },
+    // { name: 'Vitals',       type: 'vitals' },
+    // { name: 'Temporal',     type: 'temporal' }
 ];
