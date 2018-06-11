bcf4fd069e86bf1e05f3167a39b8282879765aed
Greg Rushton <grushton@broadinstitute.org>
2018-06-11 07:22:24 -0400

    fix service annotation

--- api/src/main/java/org/pmiops/workbench/mail/MailService.java
+++ api/src/main/java/org/pmiops/workbench/mail/MailService.java
@@ -1,11 +1,8 @@
 package org.pmiops.workbench.mail;
 
-import org.springframework.stereotype.Service;
-
 import javax.mail.Message;
 import javax.mail.MessagingException;
 
-@Service
 public interface MailService {
 
     void send(Message msg) throws MessagingException;
--- api/src/main/java/org/pmiops/workbench/mail/MailServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/mail/MailServiceImpl.java
@@ -1,9 +1,12 @@
 package org.pmiops.workbench.mail;
 
+import org.springframework.stereotype.Service;
+
 import javax.mail.Message;
 import javax.mail.MessagingException;
 import javax.mail.Transport;
 
+@Service
 public class MailServiceImpl implements MailService {
 
     public void send(Message msg) throws MessagingException {
