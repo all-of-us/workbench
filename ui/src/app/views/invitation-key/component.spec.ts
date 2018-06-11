4b1c5cf4ebe016c843aa1a04361fd37d81c441ff
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-06-11 15:44:41 -0400

    first attempt to fix tests

--- ui/src/app/views/invitation-key/component.spec.ts
+++ ui/src/app/views/invitation-key/component.spec.ts
@@ -19,6 +19,7 @@ import {
 import {ServerConfigService} from '../../services/server-config.service';
 import {SignInService} from '../../services/sign-in.service';
 
+import {AccountCreationModalsComponent} from "../account-creation-modals/component";
 import {AccountCreationSuccessComponent} from '../account-creation-success/component';
 import {AccountCreationComponent} from '../account-creation/component';
 import {InvitationKeyComponent} from '../invitation-key/component';
@@ -58,6 +59,7 @@ describe('InvitationKeyComponent', () => {
       declarations: [
         LoginComponent,
         AccountCreationComponent,
+        AccountCreationModalsComponent,
         AccountCreationSuccessComponent,
         InvitationKeyComponent,
         PageTemplateSignedOutComponent,
