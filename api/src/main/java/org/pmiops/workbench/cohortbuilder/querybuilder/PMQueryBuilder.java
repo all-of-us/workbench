25a32601bb1613dfa492a81926e6427dbba68f97
Brian Freeman <brian.d.freeman@vumc.org>
2018-06-12 14:20:38 -0500

    RW-801 added column to signal if has attributes.

--- api/src/main/java/org/pmiops/workbench/cohortbuilder/querybuilder/PMQueryBuilder.java
+++ api/src/main/java/org/pmiops/workbench/cohortbuilder/querybuilder/PMQueryBuilder.java
@@ -1,7 +1,11 @@
 package org.pmiops.workbench.cohortbuilder.querybuilder;
 
 import com.google.cloud.bigquery.QueryJobConfiguration;
+import com.google.cloud.bigquery.QueryParameterValue;
 import org.springframework.stereotype.Service;
+
+import java.util.HashMap;
+import java.util.Map;
 
 @Service
 public class PMQueryBuilder extends AbstractQueryBuilder {
@@ -9,12 +13,15 @@ public class PMQueryBuilder extends AbstractQueryBuilder {
   private static final String SQL_TEMPLATE =
     "select 0\n" +
       "from `${projectId}.${dataSetId}.measurement`\n" +
-      "limit 1\n";
+      "limit :limit\n";
 
   @Override
   public QueryJobConfiguration buildQueryJobConfig(QueryParameters parameters) {
+    Map<String, QueryParameterValue> params = new HashMap<>();
+    params.put("limit", QueryParameterValue.newBuilder().setValue("1").build());
     return QueryJobConfiguration
       .newBuilder(SQL_TEMPLATE)
+      .setNamedParameters(params)
       .setUseLegacySql(false)
       .build();
   }
