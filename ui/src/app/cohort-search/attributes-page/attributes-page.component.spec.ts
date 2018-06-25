c5cbd3b5a4f5922268ebe69fe6ae37a6dd3c6af2
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-06-25 12:51:55 -0500

    RW-803 add attribute property to criteria requests

--- ui/src/app/cohort-search/attributes-page/attributes-page.component.spec.ts
+++ ui/src/app/cohort-search/attributes-page/attributes-page.component.spec.ts
@@ -3,23 +3,23 @@ import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 import { AttributesPageComponent } from './attributes-page.component';
 
 describe('AttributesPageComponent', () => {
-  let component: AttributesPageComponent;
-  let fixture: ComponentFixture<AttributesPageComponent>;
+    let component: AttributesPageComponent;
+    let fixture: ComponentFixture<AttributesPageComponent>;
 
-  beforeEach(async(() => {
-    TestBed.configureTestingModule({
-      declarations: [ AttributesPageComponent ]
-    })
-    .compileComponents();
-  }));
+    beforeEach(async(() => {
+        TestBed.configureTestingModule({
+            declarations: [ AttributesPageComponent ]
+        })
+            .compileComponents();
+    }));
 
-  beforeEach(() => {
-    fixture = TestBed.createComponent(AttributesPageComponent);
-    component = fixture.componentInstance;
-    fixture.detectChanges();
-  });
+    beforeEach(() => {
+        fixture = TestBed.createComponent(AttributesPageComponent);
+        component = fixture.componentInstance;
+        fixture.detectChanges();
+    });
 
-  it('should create', () => {
-    expect(component).toBeTruthy();
-  });
+    it('should create', () => {
+        expect(component).toBeTruthy();
+    });
 });
