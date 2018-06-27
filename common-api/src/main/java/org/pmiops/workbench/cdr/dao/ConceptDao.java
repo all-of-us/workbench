5da77f5e8659707a6d4107e7833b5d7f09099010
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-06-27 17:01:12 -0500

    concepts with non empty count

--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptDao.java
@@ -15,6 +15,7 @@ public interface ConceptDao extends CrudRepository<Concept, Long> {
 
     @Query(value = "select c.* from concept c " +
             "where c.domain_id in ('Condition','Observation','Procedure', 'Measurement', 'Drug') " +
+            "and c.count_value > 0 or c.source_count_value > 0 " +
             "order by c.count_value desc limit :maxResults ",
     nativeQuery = true)
     List<Concept> findAllConceptsOrderedByCount(@Param("maxResults") long maxResults);
@@ -22,6 +23,7 @@ public interface ConceptDao extends CrudRepository<Concept, Long> {
     @Query(value = "select c.* from concept c " +
             "where c.domain_id in ('Condition','Observation','Procedure', 'Measurement', 'Drug') and " +
             "c.standard_concept='S' " +
+            "and (c.count_value > 0 or c.source_count_value > 0) " +
             "order by c.count_value desc limit :maxResults ",
             nativeQuery = true)
     List<Concept> findAllStandardConceptsOrderedByCount(@Param("maxResults") long maxResults);
@@ -29,6 +31,7 @@ public interface ConceptDao extends CrudRepository<Concept, Long> {
     @Query(value = "select c.* from concept c " +
             "where c.domain_id in ('Condition','Observation','Procedure', 'Measurement', 'Drug') and " +
             "c.standard_concept='' or c.standard_concept is null " +
+            "and (c.count_value > 0 or c.source_count_value > 0) " +
             "order by c.count_value desc limit :maxResults ",
             nativeQuery = true)
     List<Concept> findAllNonStandardConceptsOrderedByCount(@Param("maxResults") long maxResults);
