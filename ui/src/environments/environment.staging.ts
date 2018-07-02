034790484fcb67fd62ce8193ead2bb09b4ead167
CH Albach <calbach@users.noreply.github.com>
2018-07-02 12:51:57 -0700

    Update environment.staging.ts

--- ui/src/environments/environment.staging.ts
+++ ui/src/environments/environment.staging.ts
@@ -2,7 +2,7 @@ export const environment = {
   displayTag: 'Staging',
   allOfUsApiUrl: 'https://api-dot-all-of-us-rw-staging.appspot.com',
   clientId: '657299777109-kvb5qafr70bl01i6bnpgsiq5nt6v1o8u.apps.googleusercontent.com',
-  leoBaseUrl: 'https://notebooks.firecloud.org',
+  leoApiUrl: 'https://notebooks.firecloud.org',
   publicApiUrl: 'https://public-api-dot-all-of-us-rw-staging.appspot.com',
   debug: false,
   testing: true
