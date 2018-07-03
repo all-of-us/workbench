9fea6b614322f48a1269ac3a21e9e49034693809
Brian Rubenstein <brubenst@broadinstitute.org>
2018-07-03 15:26:54 -0400

    Change to consistent name

--- api/src/main/java/org/pmiops/workbench/google/CloudStorageServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/google/CloudStorageServiceImpl.java
@@ -37,7 +37,7 @@ public class CloudStorageServiceImpl implements CloudStorageService {
 
   public void copyDemoNotebook(String workspaceBucket)  {
     Storage storage = StorageOptions.getDefaultInstance().getService();
-    Blob demoNotebook = storage.get(getDemosBucketName(), "demo_notebook.ipynb");
+    Blob demoNotebook = storage.get(getDemosBucketName(), "demo-notebook.ipynb");
     BlobId demoNotebookId = demoNotebook.getBlobId();
     BlobId targetLocation = BlobId.of(workspaceBucket, "notebooks/demo_notebook.ipynb");
     copyBlob(demoNotebookId, targetLocation);
