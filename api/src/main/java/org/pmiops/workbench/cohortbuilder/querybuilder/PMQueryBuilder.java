6093e147cd9202d7405f348aabbbd8afd649dd43
Brian Freeman <brian.d.freeman@vumc.org>
2018-07-09 11:09:45 -0500

    RW-801 codacy fix.

--- api/src/main/java/org/pmiops/workbench/cohortbuilder/querybuilder/PMQueryBuilder.java
+++ api/src/main/java/org/pmiops/workbench/cohortbuilder/querybuilder/PMQueryBuilder.java
@@ -16,7 +16,6 @@ import java.util.Arrays;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
-import java.util.function.Function;
 import java.util.function.Predicate;
 import java.util.stream.Collectors;
 
