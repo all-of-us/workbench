6e1638fc8563f9095fe25cb3e89ea6520ae4bd51
CH Albach <calbach@users.noreply.github.com>
2018-07-12 14:39:04 -0700

    [RW-940][risk=medium] Add initial prod configuration (#996)

---
+++ ui/src/environments/environment.prod.ts
@@ -0,0 +1,8 @@
+export const environment = {
+  displayTag: '',
+  allOfUsApiUrl: 'https://api-dot-all-of-us-rw-prod.appspot.com',
+  clientId: '684273740878-d7i68in5d9hqr6n9mfvrdh53snekp79f.apps.googleusercontent.com',
+  leoApiUrl: 'https://notebooks.firecloud.org',
+  publicApiUrl: 'https://public-api-dot-all-of-us-rw-prod.appspot.com',
+  debug: false
+};
