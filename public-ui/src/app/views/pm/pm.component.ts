ed29f54c9a8843be1289971b4a5dfa68128b8443
Peter Speltz <peter.speltz@vanderbilt.edu>
2018-07-27 12:56:29 -0500

    pm in prog

--- public-ui/src/app/views/pm/pm.component.ts
+++ public-ui/src/app/views/pm/pm.component.ts
@@ -1,15 +1,58 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnDestroy, OnInit } from '@angular/core';
+import { Router } from '@angular/router';
+import { ISubscription } from 'rxjs/Subscription';
+import {DataBrowserService} from '../../../publicGenerated/api/dataBrowser.service';
+import {MeasurementAnalysisListResponse} from '../../../publicGenerated/model/measurementAnalysisListResponse';
 
 @Component({
   selector: 'app-physical-measurements',
   templateUrl: './pm.component.html',
-  styleUrls: ['./pm.component.css']
+  styleUrls: ['../../styles/template.css', '../../styles/cards.css', './pm.component.css']
 })
 export class PhysicalMeasurementsComponent implements OnInit {
+  title = 'Browse Program Physical Measurements';
+  private subscriptions: ISubscription[] = [];
+  concepts = [
+    {conceptId: '903118', conceptName: 'Systlic mean bp', analyses: null},
+    {conceptId: '903115', conceptName: 'Diastolic mean bp', analyses: null },
+    {conceptId: '903133', conceptName: 'Height', analyses: null },
+    {conceptId: '903121', conceptName: 'Weight', analyses: null },
+    {conceptId: '903135', conceptName: 'Mean waist circumference', analyses: null },
+    {conceptId: '903136', conceptName: 'Mean hip circumference', analyses: null },
+    {conceptId: '903126', conceptName: 'Mean heart rate', analyses: null },
+    {conceptId: '903111', conceptName: 'wheel chair use', analyses: null },
+    {conceptId: '903120', conceptName: 'Pregnancy', analyses: null }
+  ];
+  selectedConcept = this.concepts[0];
 
-  constructor() { }
+  measurement: MeasurementAnalysisListResponse;
+
+  constructor(private api: DataBrowserService) { }
 
   ngOnInit() {
+    this.subscriptions.push(this.api.getMeasurementAnalysisResults(this.selectedConcept.conceptId)
+      .subscribe( result => {
+        this.selectedConcept.analyses = result;
+        console.log(result);
+      }));
+  }
+
+  ngOnDestroy() {
+    for (const s of this.subscriptions) {
+      s.unsubscribe();
+    }
+  }
+
+  showMeasurement(concept:any) {
+    this.selectedConcept = concept;
+    if (!this.selectedConcept.analyses) {
+      this.subscriptions.push(this.api.getMeasurementAnalysisResults(this.selectedConcept.conceptId)
+        .subscribe(result => {
+          this.selectedConcept.analyses = result;
+          console.log(result);
+      }));
+    }
+
   }
 
 }
