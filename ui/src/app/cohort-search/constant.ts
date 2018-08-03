6dda5bdafa454f96bcb4315bffdc8e2c7d7ffeff
Brian Freeman <brian.d.freeman@vumc.org>
2018-08-03 15:05:21 -0500

    RW-1071 adding labs tree.

--- ui/src/app/cohort-search/constant.ts
+++ ui/src/app/cohort-search/constant.ts
@@ -8,14 +8,14 @@ export const DOMAIN_TYPES = [
     { name: 'Conditions',    type: 'cond', disabled: true },
     { name: 'Procedures',    type: 'procedures', disabled: true },
     { name: 'Drugs',    type: 'drugs', disabled: true },
-    { name: 'Measurements',    type: 'measure', disabled: true },
+    { name: 'Measurements',    type: 'meas' },
     { name: 'Visits',    type: 'visit', fullTree: true },
     { name: 'ICD9 Codes',   type: 'icd9' },
     { name: 'ICD10 Codes',  type: 'icd10' },
     // { name: 'PheCodes',     type: 'phecode' },
     { name: 'CPT Codes',    type: 'cpt' },
     // { name: 'Medications',  type: 'meds' },
-    // { name: 'Labs',         type: 'labs' },
+    // { name: 'Labs',         type: 'lab' },
     // { name: 'Vitals',       type: 'vitals' },
     // { name: 'Temporal',     type: 'temporal' }
 ];
