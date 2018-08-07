ad67009198d7ecfdf16541179bd7b2d393faca83
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-08-07 15:35:22 -0500

    changes

--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/ConceptDao.java
@@ -47,11 +47,6 @@ public interface ConceptDao extends CrudRepository<Concept, Long> {
     @Query(value = "select c.* from concept c join db_domain d on c.concept_id=d.concept_id", nativeQuery = true)
     List<Concept> findDbDomainParticpantCounts();
 
-    /*
-    @Query(value = "select distinct c.* from concept_synonym cs join concept c on c.concept_id=cs.concept_id where (match(cs.concept_synonym_name) against(?1 in boolean mode) and c.domain_id in (?2)) group by c.concept_id", nativeQuery = true)
-    List<Concept> findConceptSynonyms(String query,List<String> domainIds);
-    */
-
     @Query(value = "select distinct c from Concept c left join FETCH c.conceptSynonyms as cs where match(cs.conceptSynonymName,?1) > 0")
     List<Concept> findConceptSynonyms(String query);
 
