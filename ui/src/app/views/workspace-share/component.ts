b4022e9460487d71675214102d90cc69d542c3fb
Greg Rushton <grushton@broadinstitute.org>
2018-08-10 12:47:23 -0400

    fix workspace user sharing

--- ui/src/app/views/workspace-share/component.ts
+++ ui/src/app/views/workspace-share/component.ts
@@ -95,13 +95,17 @@ export class WorkspaceShareComponent implements OnInit {
     }
     if (!this.usersLoading) {
       this.usersLoading = true;
-      const updateList = Array.from(this.workspace.userRoles);
-      updateList.push({
-        email: this.convertToEmail(this.toShare),
-        role: this.selectedAccessLevel
-      });
-
-      this.workspacesService.shareWorkspace(
+      const email = this.convertToEmail(this.toShare);
+      const role = this.selectedAccessLevel;
+      if (this.checkUnique(email, role)) {
+        // A user can only have one role on a workspace so we replace their current role with the new one
+        const updateList = Array.from(this.workspace.userRoles)
+          .filter(function(r) { return r.email !== email});
+        updateList.push({
+          email: email,
+          role: role
+        });
+        this.workspacesService.shareWorkspace(
           this.workspace.namespace,
           this.workspace.id, {
             workspaceEtag: this.workspace.etag,
@@ -110,7 +114,7 @@ export class WorkspaceShareComponent implements OnInit {
           (resp: ShareWorkspaceResponse) => {
             this.workspace.etag = resp.workspaceEtag;
             this.usersLoading = false;
-            this.workspace.userRoles = updateList;
+            this.workspace.userRoles = resp.items;
             this.toShare = '';
             this.input.nativeElement.focus();
           },
@@ -122,7 +126,10 @@ export class WorkspaceShareComponent implements OnInit {
             }
             this.usersLoading = false;
           }
-      );
+        );
+      } else {
+        this.usersLoading = false;
+      }
     }
   }
 
@@ -145,7 +152,7 @@ export class WorkspaceShareComponent implements OnInit {
         (resp: ShareWorkspaceResponse) => {
           this.workspace.etag = resp.workspaceEtag;
           this.usersLoading = false;
-          this.workspace.userRoles = updateList;
+          this.workspace.userRoles = resp.items;
         },
         (error) => {
           this.usersLoading = false;
@@ -194,6 +201,14 @@ export class WorkspaceShareComponent implements OnInit {
 
   navigateBack(): void {
     this.locationService.back();
+  }
+
+  // Checks for an email + role combination in the current list of user roles.
+  checkUnique(email: String, role: WorkspaceAccessLevel): Boolean {
+    return Array.from(this.workspace.userRoles)
+      .filter(function(r) { return r.email === email})
+      .filter(function(r) { return r.role === role})
+      .length === 0;
   }
 
 }
