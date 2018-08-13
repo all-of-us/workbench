583b15791f03a4c2d643b51e557973c2f4add8aa
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-08-13 09:47:40 -0500

    fixed jpa failing match

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
@@ -2,7 +2,6 @@ package org.pmiops.workbench.cdr.dao;
 
 import org.pmiops.workbench.cdr.model.AchillesResult;
 import org.springframework.data.repository.CrudRepository;
-import org.springframework.data.jpa.repository.Query;
 
 public interface AchillesResultDao extends CrudRepository<AchillesResult, Long> {
     AchillesResult findAchillesResultByAnalysisId(Long analysisId);
