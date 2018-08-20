de2049c436610e1837341f4f507fab2ae25dca2f
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-08-20 13:53:48 -0500

    RW-1092 lint error fixes

--- ui/src/app/cohort-review/participant-status/participant-status.component.ts
+++ ui/src/app/cohort-review/participant-status/participant-status.component.ts
@@ -1,4 +1,4 @@
-import {Component, Input, OnDestroy, OnChanges, OnInit} from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy, OnInit} from '@angular/core';
 import {FormControl} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
