7c8223d2953c92110c250de28853409addef35cc
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-08-20 13:28:46 -0500

    RW-1092 save function changes with detect changes

--- ui/src/app/cohort-review/set-annotation-list/set-annotation-list.component.ts
+++ ui/src/app/cohort-review/set-annotation-list/set-annotation-list.component.ts
@@ -37,17 +37,19 @@ export class SetAnnotationListComponent implements OnInit, OnDestroy {
     isPosting(flag: boolean, defn: CohortAnnotationDefinition): void {
         const id = defn.cohortAnnotationDefinitionId;
         this.cancelFlag = false;
+        this.saveButtonEvent = false;
         this.cdr.detectChanges();
         if (flag) {
             this.postSet.add(id);
+           // this.saveButtonEvent = false;
         } else if (this.postSet.has(id)) {
             this.postSet.delete(id);
         }
     }
 
-    get posting() {
-        return this.postSet.size > 0;
-    }
+    // get posting() {
+    //     return this.postSet.size > 0;
+    // }
 
     get openEdit() {
         return this.state.editAnnotationManagerOpen.getValue();
@@ -60,7 +62,9 @@ export class SetAnnotationListComponent implements OnInit, OnDestroy {
     cancelFromChange (){
         this.cancelFlag = true;
         this.openEdit = false;
+        this.saveButtonEvent = false;
     }
+
     saveFromChange(){
         this.saveButtonEvent = true;
         this.openEdit = false;
