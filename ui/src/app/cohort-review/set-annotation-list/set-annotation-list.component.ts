f36488b9a5152a6d8c8aa8591083a63867d54a2e
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-08-20 16:50:06 -0500

    RW-1092 on edit modal data update fixed

--- ui/src/app/cohort-review/set-annotation-list/set-annotation-list.component.ts
+++ ui/src/app/cohort-review/set-annotation-list/set-annotation-list.component.ts
@@ -1,4 +1,4 @@
-import {Component, ChangeDetectionStrategy, ChangeDetectorRef, OnDestroy, OnInit} from '@angular/core';
+import {Component, OnDestroy, OnInit} from '@angular/core';
 import {ActivatedRoute} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
@@ -12,18 +12,14 @@ type DefnId = CohortAnnotationDefinition['cohortAnnotationDefinitionId'];
 @Component({
     selector: 'app-set-annotation-list',
     templateUrl: './set-annotation-list.component.html',
-    styleUrls: ['./set-annotation-list.component.css'],
-    changeDetection: ChangeDetectionStrategy.OnPush
+    styleUrls: ['./set-annotation-list.component.css']
 })
 export class SetAnnotationListComponent implements OnInit, OnDestroy {
     subscription: Subscription;
     definitions: CohortAnnotationDefinition[];
     postSet: Set<DefnId> = new Set<DefnId>();
-    // cancelFlag = false;
-    // saveButtonEvent = false;
-    // posting = false;
-    constructor(private state: ReviewStateService,
-                private cdr: ChangeDetectorRef) {}
+
+    constructor(private state: ReviewStateService) {}
 
     ngOnInit() {
         this.subscription = this.state.annotationDefinitions$
@@ -36,9 +32,6 @@ export class SetAnnotationListComponent implements OnInit, OnDestroy {
 
     isPosting(flag: boolean, defn: CohortAnnotationDefinition): void {
         const id = defn.cohortAnnotationDefinitionId;
-        // this.cancelFlag = false;
-        // this.saveButtonEvent = false;
-        // this.cdr.detectChanges();
         if (flag) {
             this.postSet.add(id);
         } else if (this.postSet.has(id)) {
@@ -58,17 +51,4 @@ export class SetAnnotationListComponent implements OnInit, OnDestroy {
         this.state.editAnnotationManagerOpen.next(value);
     }
 
-    cancelFromChange () {
-      //  this.cancelFlag = true;
-        this.openEdit = false;
-      //  this.saveButtonEvent = false;
-    }
-
-
-    saveFromChange() {
-        this.openEdit = false;
-      //  this.saveButtonEvent = true;
-    //     setTimeout (() => {
-    //        this.cancelFromChange() },5000);
-     }
 }
