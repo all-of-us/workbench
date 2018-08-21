986669820634c5fad2a0133b99fff1387c704f99
Neha Saxena <nsaxena@broadinstitute.org>
2018-08-21 19:13:02 -0400

    unused import

--- api/src/main/java/org/pmiops/workbench/api/WorkspacesController.java
+++ api/src/main/java/org/pmiops/workbench/api/WorkspacesController.java
@@ -12,7 +12,6 @@ import java.util.HashSet;
 import java.util.List;
 import java.util.Random;
 import java.util.Set;
-import java.util.concurrent.CompletableFuture;
 import java.util.function.BiFunction;
 import java.util.function.Function;
 import java.util.regex.Pattern;
