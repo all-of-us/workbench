3013531e40d5adaff31a0edaeb9fbfa33259cba0
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-08-22 09:23:59 -0500

    RW-1092 spec lint error fixes

--- ui/src/app/cohort-review/participant-status/participant-status.component.spec.ts
+++ ui/src/app/cohort-review/participant-status/participant-status.component.spec.ts
@@ -2,13 +2,11 @@ import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
 import {ActivatedRoute} from '@angular/router';
 import {ClarityModule} from '@clr/angular';
-import {Observable} from 'rxjs/Observable';
-
-import {ParticipantStatusComponent} from './participant-status.component';
-
 import {CohortReviewService, CohortStatus} from 'generated';
-import {ReviewStateService} from '../review-state.service';
+import {ParticipantStatusComponent} from './participant-status.component';
 import {Participant} from '../participant.model';
+import {Observable} from 'rxjs/Observable';
+import {ReviewStateService} from '../review-state.service';
 import {updateAndTick} from '../../../testing/test-helpers';
 
 class StubRoute {
