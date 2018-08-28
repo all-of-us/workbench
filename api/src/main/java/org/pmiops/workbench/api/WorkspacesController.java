c1455c57fa306ce6a96221943683a3656cd99d1d
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-08-28 15:29:41 -0400

    little codacy cleanup

--- api/src/main/java/org/pmiops/workbench/api/WorkspacesController.java
+++ api/src/main/java/org/pmiops/workbench/api/WorkspacesController.java
@@ -765,7 +765,7 @@ public class WorkspacesController implements WorkspacesApiDelegate {
 
   @Override
   public ResponseEntity<EmptyResponse> deleteNotebook(String workspace, String workspaceName, String notebookName) {
-    FileDetail fileDetail = performNotebookOperation("delete", workspace, workspaceName, notebookName, "");
+    performNotebookOperation("delete", workspace, workspaceName, notebookName, "");
     return ResponseEntity.ok(new EmptyResponse());
   }
 
@@ -787,7 +787,7 @@ public class WorkspacesController implements WorkspacesApiDelegate {
     workspaceId = workspaceService.getRequired(workspace, workspaceName).getWorkspaceId();
     User user = userProvider.get();
     FileDetail fileDetail = new FileDetail();
-    if (operation == "rename" || operation == "copy") {
+    if (operation.equals("rename") || operation.equals("copy")) {
       cloudStorageService.copyBlob(blobId, BlobId.of(bucket, newPath));
       fileDetail.setName(newName);
       fileDetail.setPath(fullPath);
@@ -795,7 +795,7 @@ public class WorkspacesController implements WorkspacesApiDelegate {
       fileDetail.setLastModifiedTime(now.getTime());
       userRecentResourceService.updateNotebookEntry(workspaceId, user.getUserId(), fullPath, now);
     }
-    if (operation == "delete" || operation == "rename") {
+    if (operation.equals("delete") || operation.equals("rename")) {
       cloudStorageService.deleteBlob(blobId);
       userRecentResourceService.deleteNotebookEntry(workspaceId, user.getUserId(), fullPath);
     }
