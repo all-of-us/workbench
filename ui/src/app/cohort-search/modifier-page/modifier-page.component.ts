35c087910a74fa326387b3d3f3e86ad70047ca80
Susan Osgood <susan.osgood@vanderbilt.edu>
2018-08-28 13:59:28 -0500

    RW-1252 Set modifier default values and set up form

--- ui/src/app/cohort-search/modifier-page/modifier-page.component.ts
+++ ui/src/app/cohort-search/modifier-page/modifier-page.component.ts
@@ -35,7 +35,7 @@ export class ModifierPageComponent implements OnInit, OnDestroy, AfterContentChe
   selectedDate: any;
   subscription: Subscription;
   dropdownOption = {
-    selected: ['', '', '', '']
+    selected: ['Any', 'Any', 'Any', 'Any']
   };
   visitCounts: any;
 
@@ -45,6 +45,9 @@ export class ModifierPageComponent implements OnInit, OnDestroy, AfterContentChe
     inputType: 'number',
     modType: ModifierType.AGEATEVENT,
     operators: [{
+      name: 'Any',
+      value: undefined,
+    }, {
       name: 'Greater Than or Equal To',
       value: 'GREATER_THAN_OR_EQUAL_TO',
     }, {
@@ -60,6 +63,9 @@ export class ModifierPageComponent implements OnInit, OnDestroy, AfterContentChe
     inputType: 'date',
     modType: ModifierType.EVENTDATE,
     operators: [{
+      name: 'Any',
+      value: undefined,
+    }, {
       name: 'Is On or Before',
       value: 'LESS_THAN_OR_EQUAL_TO',
     }, {
@@ -75,6 +81,9 @@ export class ModifierPageComponent implements OnInit, OnDestroy, AfterContentChe
     inputType: 'number',
     modType: ModifierType.NUMOFOCCURRENCES,
     operators: [{
+        name: 'Any',
+        value: undefined,
+    }, {
       name: 'N or More',
       value: 'GREATER_THAN_OR_EQUAL_TO',
     }]
@@ -114,7 +123,10 @@ export class ModifierPageComponent implements OnInit, OnDestroy, AfterContentChe
           label: 'During Visit Type',
           inputType: null,
           modType: ModifierType.ENCOUNTERS,
-          operators: []
+          operators: [{
+              name: 'Any',
+              value: undefined,
+          }]
         });
         this.form.addControl('encounters', new FormGroup({operator: new FormControl()}));
       }
@@ -180,24 +192,26 @@ export class ModifierPageComponent implements OnInit, OnDestroy, AfterContentChe
           .filter(mod => !!mod)
           .filter(mod => !this.existing.includes(mod))
           .forEach(mod => this.actions.addModifier(mod));
+
+        // update the calculate button
+         this.formChanges = !newMods.every(element => element === undefined);
       })
     );
   }
-    ngAfterContentChecked() {
-        this.cdref.detectChanges();
-    }
-    selectChange(opt, index, e, mod) {
-      this.dropdownOption.selected[index] = opt.name;
-      if (e.target.value || this.form.controls.valueA) {
-        this.formChanges = true;
-      }
-      const modForm = <FormArray>this.form.controls[mod.name];
-      const valueForm = <FormArray>modForm;
-      valueForm.get('operator').patchValue(opt.value);
-    }
+
+  ngAfterContentChecked() {
+      this.cdref.detectChanges();
+  }
+
+  selectChange(opt, index, e, mod) {
+    this.dropdownOption.selected[index] = opt.name;
+    const modForm = <FormArray>this.form.controls[mod.name];
+    const valueForm = <FormArray>modForm;
+    valueForm.get('operator').patchValue(opt.value);
+  }
 
   currentMods(vals) {
-      this.ngAfterContentChecked();
+    this.ngAfterContentChecked();
     return this.modifiers.map(({name, inputType, modType}) => {
       if (modType === ModifierType.ENCOUNTERS) {
         if (!vals[name].operator) {
