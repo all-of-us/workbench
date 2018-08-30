d3fab8d8a9a267bef8a9d5aad55806312543ac4d
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-08-30 16:15:19 -0500

    query change

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/DbDomainDao.java
@@ -18,7 +18,7 @@ public interface DbDomainDao extends CrudRepository<DbDomain, Long> {
         @Query(nativeQuery=true,value="select d.domain_id, d.domain_display, d.domain_desc, d.db_type, d.domain_route,d.concept_id, count(distinct c.concept_id) as count_value from db_domain d\n" +
                 "join concept c on d.domain_id = c.domain_id left join concept_synonym cs on c.concept_id=cs.concept_id \n" +
                 "where d.db_type = 'domain_filter' and (c.count_value > 0 or c.source_count_value > 0) \n" +
-                "and  ((match(c.concept_name) against(?1 in boolean mode) ) or (match(cs.concept_synonym_name) against(?1 in boolean mode) and c.standard_concept = 'S') or (c.concept_id=?2 or c.concept_code=?2) )\n" +
+                "and  ((((match(c.concept_name) against(?1 in boolean mode) ) or (match(cs.concept_synonym_name) against(?1 in boolean mode))) and c.standard_concept = 'S') or (c.concept_id=?2 or c.concept_code=?2))\n" +
                 "group by d.domain_id, d.domain_display, d.domain_desc, d.db_type, d.concept_id\n" +
                 " \n" +
                 "union\n" +
