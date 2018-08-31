e34213c6f588b42ca3171e0d077c8251e9f09438
Brian Rubenstein <brubenst@broadinstitute.org>
2018-08-31 14:46:43 -0400

    Final working circle code

--- api/project.rb
+++ api/project.rb
@@ -4,4 +4,3 @@ require_relative "../aou-utils/workbench"
 require_relative "libproject/devstart.rb"
 
 Workbench.handle_argv_or_die(__FILE__)
-#test
--- api/test-bash.sh
+++
@@ -1,4 +0,0 @@
-if [ $(git diff --name-only $(git merge-base master ch/ci-strawman) | grep api/ | wc -l | xargs) == 0 ] && [ ch/ci-strawman != "master" ]; then
-    echo $(git diff --name-only $(git merge-base master ch/ci-strawman) | grep api/ | wc -l)
-    echo No relevant changes on non-master branch, skipping
-fi
