33c27cc4d6130bda1c53b75ab25a7cdae192bd7d
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-09-04 13:35:41 -0500

    RW-1261 add test for create-review-page component

---
+++ ui/src/app/cohort-review/create-review-page/create-review-page.component.spec.ts
@@ -0,0 +1,38 @@
+import { async, ComponentFixture, TestBed } from '@angular/core/testing';
+import {ReactiveFormsModule} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
+import { RouterTestingModule } from '@angular/router/testing';
+import {ClarityModule} from '@clr/angular';
+
+import {CohortReviewService} from 'generated';
+import {CreateReviewPage} from './create-review-page';
+
+describe('CreateReviewPage', () => {
+  let component: CreateReviewPage;
+  let fixture: ComponentFixture<CreateReviewPage>;
+  let route;
+
+  beforeEach(async(() => {
+
+    TestBed.configureTestingModule({
+      declarations: [ CreateReviewPage ],
+      imports: [ClarityModule, ReactiveFormsModule, RouterTestingModule.withRoutes([])],
+      providers: [
+        {provide: CohortReviewService, useValue: {}},
+        {provide: ActivatedRoute, useValue: {parent: {snapshot: {data: {review: {}, cohort: {}}}}}},
+      ],
+    })
+      .compileComponents();
+  }));
+
+  beforeEach(() => {
+    fixture = TestBed.createComponent(CreateReviewPage);
+    component = fixture.componentInstance;
+    route = new ActivatedRoute();
+    fixture.detectChanges();
+  });
+
+  it('should create', () => {
+    expect(component).toBeTruthy();
+  });
+});
