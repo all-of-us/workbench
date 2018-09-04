a3591352c6e7f069299b76b08a2821bb2aa02b6a
Neha Saxena <nsaxena@broadinstitute.org>
2018-09-04 14:44:04 -0400

    card actions

--- ui/src/app/views/card/component.ts
+++ ui/src/app/views/card/component.ts
@@ -19,6 +19,9 @@ export class CardComponent implements OnInit {
   header: string;
 
   constructor(
+      private cohortsService: CohortsService,
+      private workspacesService: WorkspacesService,
+      private route: ActivatedRoute,
   ) {
    this.actionList = [{
      notebook: {
@@ -26,8 +29,51 @@ export class CardComponent implements OnInit {
        link: 'query'}
    }];
    this.type = 'notebook';
+
   }
   ngOnInit() {
+    this.wsNamespace = this.route.snapshot.params['ns'];
+    this.wsId = this.route.snapshot.params['wsid'];
+    this.type = this.card && this.card.notebook == null ? 'cohort' : 'notebook';
     this.actions = this.actionList.filter(elem =>  elem.type === this.type);
+
+  }
+
+  confirmDelete(notebook: FileDetail): void {
+    this.notebookInFocus = notebook;
+    this.deleteModal.open();
+  }
+
+  receiveNotebookDelete($event: FileDetail): void {
+    this.workspacesService.deleteNotebook(this.wsNamespace, this.wsId, $event.name)
+        .subscribe(() => {
+         // this.notebooksLoading = true;
+         // this.loadNotebookList();
+          this.deleteModal.close();
+        });
+  }
+
+  cloneThis(notebook: string): void {
+    this.workspacesService.cloneNotebook(this.wsNamespace, this.wsId, notebook)
+        .subscribe(() => {
+         // this.loadNotebookList();
+        });
+  }
+
+  receiveDelete($event): void {
+    this.deleteCohort($event);
+  }
+
+  public deleteCohort(cohort: Cohort): void {
+    /*this.cohortsService.deleteCohort(this.wsNamespace, this.wsId, cohort.id).subscribe(() => {
+      this.cohortList.splice(
+          this.cohortList.indexOf(cohort), 1);
+      this.deleteModal.close();
+    });*/
+  }
+
+  updateFinished(): void {
+    // this.editModal.close();
+    // this.reloadCohorts();
   }
 }
