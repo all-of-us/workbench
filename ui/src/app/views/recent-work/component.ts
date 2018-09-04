d89424566a5086f1cd0fe8477433d804cf59b58c
Neha Saxena <nsaxena@broadinstitute.org>
2018-09-04 12:05:43 -0400

    more changes

--- ui/src/app/views/recent-work/component.ts
+++ ui/src/app/views/recent-work/component.ts
@@ -16,13 +16,14 @@ export class RecentWorkComponent implements OnInit {
   fullList: RecentResource[];
   constructor(
     private userMetricsService: UserMetricsService
-  ){}
-
+  ) {}
+  index: Number;
   ngOnInit(): void {
+    this.index = 0 ;
     this.userMetricsService.getUserRecentResources().subscribe((resources) => {
       this.fullList = resources;
       // this should actually be first 3 elements of full List
-      this.resourceList = this.fullList.slice(0,3);
+      this.resourceList = this.fullList.slice(this.index, 3);
     });
   }
 
@@ -30,13 +31,13 @@ export class RecentWorkComponent implements OnInit {
       this.fullList.push(this.fullList.shift());
       console.log(this.fullList);
       //again should actually be first 3 elements of fullList
-      this.resourceList = ["testing2", "testing3", "testing1"];
+    this.resourceList = this.fullList.slice(++this.index, this.index + 2);
   }
 
   moveUpList(): void {
       this.fullList.unshift(this.fullList.pop());
       console.log(this.fullList);
-      this.resourceList = ["testing1", "testing2", "testing3"];
+    this.resourceList = this.fullList.slice(--this.index, 3);
   }
 
 }
