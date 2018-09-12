dec6b3c2c203eb2343ea8958f0b3ccd97ca0ecbe
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-09-12 14:49:02 -0400

    seriously, apparently really cannot alphabetize

--- ui/src/app/views/recent-work/component.spec.ts
+++ ui/src/app/views/recent-work/component.spec.ts
@@ -20,8 +20,8 @@ import {simulateClick, updateAndTick} from 'testing/test-helpers';
 import {CohortEditModalComponent} from 'app/views/cohort-edit-modal/component';
 import {ConfirmDeleteModalComponent} from 'app/views/confirm-delete-modal/component';
 import {RecentWorkComponent} from 'app/views/recent-work/component';
-import {ResourceCardComponent} from 'app/views/resource-card/component';
 import {RenameModalComponent} from 'app/views/rename-modal/component';
+import {ResourceCardComponent} from 'app/views/resource-card/component';
 
 import {LeftScrollComponent} from 'app/icons/left-scroll/component';
 import {RightScrollComponent} from 'app/icons/right-scroll/component';
