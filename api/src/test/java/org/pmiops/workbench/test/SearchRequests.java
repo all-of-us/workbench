876114ab08ff32f4c197874882b1bcb49c102bcd
Brian Freeman <brian.d.freeman@vumc.org>
2018-09-14 14:54:57 -0500

    [RW-1301][risk=no] fixing broken BQ test case. (#1199)
    * RW-1301 fixing broken test case.
    
    * RW-1301 removed white space.
    
    * [RW-1286] Adding getDataTableQuery and getCohortAnnotations methods [risk=no] (#1196)
    
    * Adding API and stub implementations for getDataTableQuery and
    getCohortAnnotations methods.
    
    * Moving cohort materialization API into workbench.yaml (in preparation
    for removing it from the client libs.)
    
    * PR comments
    
    * RW-1301 fixing broken test case.
    
    * RW-1301 removed white space.

--- api/src/test/java/org/pmiops/workbench/test/SearchRequests.java
+++ api/src/test/java/org/pmiops/workbench/test/SearchRequests.java
@@ -4,6 +4,7 @@ import org.pmiops.workbench.model.SearchGroup;
 import org.pmiops.workbench.model.SearchGroupItem;
 import org.pmiops.workbench.model.SearchParameter;
 import org.pmiops.workbench.model.SearchRequest;
+import org.pmiops.workbench.model.TreeSubType;
 import org.pmiops.workbench.model.TreeType;
 
 public class SearchRequests {
@@ -15,6 +16,7 @@ public class SearchRequests {
   private static final String GENDER_SUBTYPE = "GEN";
   private static final String DEMO_TYPE = TreeType.DEMO.name();
   public static final String ICD9_TYPE = TreeType.ICD9.name();
+  public static final String ICD9_SUBTYPE = TreeSubType.CM.name();
   public static final String ICD9_GROUP_CODE = "001";
 
   private SearchRequests() {
@@ -30,10 +32,10 @@ public class SearchRequests {
     return searchRequest(searchGroupItem);
   }
 
-  public static SearchRequest codesRequest(String type, String... codes) {
+  public static SearchRequest codesRequest(String type, String subtype, String... codes) {
     SearchGroupItem searchGroupItem = new SearchGroupItem().id("id1").type(type);
     for (String code: codes) {
-      SearchParameter parameter = new SearchParameter().type(type).group(true).value(code);
+      SearchParameter parameter = new SearchParameter().type(type).subtype(subtype).group(true).value(code);
       searchGroupItem.addSearchParametersItem(parameter);
     }
     return searchRequest(searchGroupItem);
@@ -51,7 +53,7 @@ public class SearchRequests {
   }
 
   public static SearchRequest icd9Codes() {
-    return codesRequest(ICD9_TYPE, ICD9_GROUP_CODE);
+    return codesRequest(ICD9_TYPE, ICD9_SUBTYPE, ICD9_GROUP_CODE);
   }
 
   public static SearchRequest males() {
