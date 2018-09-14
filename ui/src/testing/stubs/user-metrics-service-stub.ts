29091f77f13bb5d812a72715fd61f1764918f885
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-09-14 07:30:42 -0400

    forgot to revert this

--- ui/src/testing/stubs/user-metrics-service-stub.ts
+++ ui/src/testing/stubs/user-metrics-service-stub.ts
@@ -1,9 +1,8 @@
 import {Observable} from 'rxjs/Observable';
 
-import {UserMetricsService} from 'generated/api/userMetrics.service';
 import {RecentResourceResponse} from 'generated/model/recentResourceResponse';
 
-export class UserMetricsServiceStub extends UserMetricsService {
+export class UserMetricsServiceStub {
 
   getUserRecentResources(extraHttpRequestParams?: any): Observable<RecentResourceResponse> {
     return new Observable<RecentResourceResponse>(observer => {
