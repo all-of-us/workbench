0ca66bf29bc718f6e4cbc437733cb7903a55e22f
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-09-17 13:09:49 -0500

    RW-1303 demographic lint error fixes

--- ui/src/app/cohort-search/multi-select/multi-select.component.ts
+++ ui/src/app/cohort-search/multi-select/multi-select.component.ts
@@ -1,12 +1,13 @@
 
 import {Component, EventEmitter, Input, OnDestroy, OnInit, Output} from '@angular/core';
 import {FormControl} from '@angular/forms';
+import {NgRedux} from '@angular-redux/store';
 import {List, Set} from 'immutable';
 import {Subscription} from 'rxjs/Subscription';
 
 import {activeParameterList, CohortSearchActions, CohortSearchState, isParameterActive} from '../redux';
 import {TreeType} from 'generated';
-import {NgRedux} from '@angular-redux/store';
+
 @Component({
   selector: 'crit-multi-select',
   templateUrl: './multi-select.component.html',
@@ -44,8 +45,8 @@ export class MultiSelectComponent implements OnInit, OnDestroy {
             .select(activeParameterList)
             .subscribe(val => {
                 this.selectedOption = [];
-                val.forEach( paramList =>{
-                    if(paramList.get('type') === TreeType.DEMO){
+                val.forEach( paramList => {
+                    if (paramList.get('type') === TreeType.DEMO) {
                         this.selectedOption.push(paramList.get('parameterId'));
                     }
                 });
