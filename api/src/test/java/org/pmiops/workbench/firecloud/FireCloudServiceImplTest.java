b05451969b8477a7f92a43f09ec2cde3f5dbbff9
Greg Rushton <grushton@broadinstitute.org>
2018-09-18 10:38:00 -0400

    whitespace

--- api/src/test/java/org/pmiops/workbench/firecloud/FireCloudServiceImplTest.java
+++ api/src/test/java/org/pmiops/workbench/firecloud/FireCloudServiceImplTest.java
@@ -5,7 +5,6 @@ import static org.mockito.Mockito.when;
 
 import com.google.common.collect.Lists;
 import java.util.ArrayList;
-
 import org.junit.Before;
 import org.junit.Rule;
 import org.junit.Test;
