04eca7340e5ed4b7711f71d2cc57dba2cc8e99d4
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2018-09-18 11:14:06 -0400

    [RISK=NO] Unable to edit data set on edit workspace, if the Fill later option is selected (#1198)
    * Bug: unable to edit data set if fill later checkbox is selected
    
    * PR Comments 1

--- ui/src/app/views/workspace-edit/component.ts
+++ ui/src/app/views/workspace-edit/component.ts
@@ -189,6 +189,7 @@ export class WorkspaceEditComponent implements OnInit {
   researchPurposeItems = ResearchPurposeItems;
   fillDetailsLater = false;
   hideDetailsLaterOption = false;
+  canEditResearchPurpose = true;
   constructor(
       private locationService: Location,
       private route: ActivatedRoute,
@@ -245,8 +246,10 @@ export class WorkspaceEditComponent implements OnInit {
       this.accessLevel = wsData.accessLevel;
       if (isBlank(this.workspace.description)) {
         this.fillDetailsLater = true;
+        this.canEditResearchPurpose = true;
       } else {
         this.hideDetailsLaterOption = true;
+        this.canEditResearchPurpose = false;
       }
     } else if (this.mode === WorkspaceEditMode.Clone) {
       this.workspace.name = 'Clone of ' + wsData.name;
