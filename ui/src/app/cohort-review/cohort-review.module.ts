76f652f68f850d37725ee0eef24328d059697504
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-09-20 15:18:34 -0500

    RW-269 fixed reducer code

--- ui/src/app/cohort-review/cohort-review.module.ts
+++ ui/src/app/cohort-review/cohort-review.module.ts
@@ -1,4 +1,5 @@
 /* tslint:disable:max-line-length */
+import {NgReduxModule} from '@angular-redux/store';
 import {CommonModule} from '@angular/common';
 import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
@@ -12,33 +13,27 @@ import {DetailPage} from './detail-page/detail-page';
 import {OverviewPage} from './overview-page/overview-page';
 import {PageLayout} from './page-layout/page-layout';
 import {TablePage} from './table-page/table-page';
-
 import {ReviewNavComponent} from './review-nav/review-nav.component';
-
 import {DetailHeaderComponent} from './detail-header/detail-header.component';
 import {DetailTabTableComponent} from './detail-tab-table/detail-tab-table.component';
 import {DetailTabsComponent} from './detail-tabs/detail-tabs.component';
-import {NgReduxModule} from '@angular-redux/store';
 import {AnnotationItemComponent} from './annotation-item/annotation-item.component';
 import {AnnotationListComponent} from './annotation-list/annotation-list.component';
 import {SetAnnotationCreateComponent} from './set-annotation-create/set-annotation-create.component';
 import {SetAnnotationItemComponent} from './set-annotation-item/set-annotation-item.component';
 import {SetAnnotationListComponent} from './set-annotation-list/set-annotation-list.component';
 import {SetAnnotationModalComponent} from './set-annotation-modal/set-annotation-modal.component';
-
 import {ChoiceFilterComponent} from './choice-filter/choice-filter.component';
 import {StatusFilterComponent} from './status-filter/status-filter.component';
-
 import {ParticipantStatusComponent} from './participant-status/participant-status.component';
 import {SidebarContentComponent} from './sidebar-content/sidebar-content.component';
-
 import {CohortCommonModule} from '../cohort-common/module';
 import {ReviewStateService} from './review-state.service';
 import {CohortReviewRoutingModule} from './routing/routing.module';
 // This is a temporary measure until we have specs and APIs for overview specific charts
 import { DetailAllEventsComponent } from './detail-all-events/detail-all-events.component';
 // This is to get cohortsaerchstore acces, might need to change
-import {CohortSearchActions, CohortSearchEpics, ConfigureStore} from "../cohort-search/redux";
+import {CohortSearchActions, CohortSearchEpics, ConfigureStore} from '../cohort-search/redux';
 /* tslint:enable:max-line-length */
 
 
