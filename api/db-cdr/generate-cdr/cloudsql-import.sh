05a8b84732467d96b35ced1187df2ef0fc9b8321
Peter Speltz <biopete@users.noreply.github.com>
2018-09-21 15:17:01 -0500

    Biopete/gcs cloudsql direct [risk=low] (#1203)
    * in progress cloud sql import from gcs
    
    * cloudsql import in progress. New goal for denormalizing workbench. New goal for generate-cloudsql-db
    
    * import working. Stub of create_indexes for future use.
    
    * remove ignore lines from local mysql import. The dumps are made without headers now.
    
    * csv dump files renamed with table.*.csv.gz, readme updated
    
    * cloudsql-import-bucket moved to cloudsql-import . Has file option.
    
    * Readme updated
    
    * concept synonm import fixed
    
    * Made cloudsql-import more robust, better factored. Fixed devstart.rb so it doesn't use opt parser. I don't like opts specified in two places.
    
    * make-bq-data-dump now dumps all tables as if they are big with table.*.csv.gz  so we don't maintain a list of big tables in there
    
    * codacy clean up, removed some old todo comments.
    
    * Bug when creating database and passing --database option
    
    * Updated all gcloud versions in 217.0.0 . Cleanup review
    
    * import_wait interval tweaked a bit
    
    * import wait tweak
    
    * Updated readme. Made cloudsql import check if database exists and fail if it doesn't.
    
    * added todo for next pr to test this and remove this function as i don't think we need it.
    
    * new test cdr
    
    * Fixed readme about cloudsql release and fixed some formatting.

--- api/db-cdr/generate-cdr/cloudsql-import.sh
+++ api/db-cdr/generate-cdr/cloudsql-import.sh
@@ -1,22 +1,31 @@
 #!/bin/bash
 
-# This makes a mysqldump of the cdr database specified and copies it to a gcs bucket
-# ACCOUNT must be authorized with gcloud auth login previously
+# Imports sql and csv files in a bucket to a cloudsql database.
+# IF --create-db-sql-file is specified, it is ran first to create the database
+# IF --file is specified, it is ran next after create-db-sql-file and then it quits.
+# IF the above are provided and do not exist it errors out.
+# IF you want to import a single file only,  use the --file option or make sure it is the only file in the bucket.
+# Table names for csvs are determined from the filename.
+# The files can be gzipped or not.
 
 set -xeuo pipefail
 IFS=$'\n\t'
 
+CREATE_DB_SQL_FILE=
+FILE=
 
 # get options
-USAGE="./generate-clousql-cdr/cloudsql-import.sh --project <PROJECT> --instance <INSTANCE> --sql-dump-file <cdrYYYYMMDD.sql> --bucket <BUCKET>"
+USAGE="./generate-clousql-cdr/cloudsql-import.sh --project <PROJECT> --instance <INSTANCE> --bucket <BUCKET> \
+--database <database> [--create-db-sql-file <filename.sql>] [--file <just_import_me_filename>]"
 # example account for test : all-of-us-workbench-test@appspot.gserviceaccount.com
 while [ $# -gt 0 ]; do
-  echo "1 is $1"
   case "$1" in
     --project) PROJECT=$2; shift 2;;
     --instance) INSTANCE=$2; shift 2;;
-    --sql-dump-file) SQL_DUMP_FILE=$2; shift 2;;
     --bucket) BUCKET=$2; shift 2;;
+    --database) DATABASE=$2; shift 2;;
+    --create-db-sql-file) CREATE_DB_SQL_FILE=$2; shift 2;;
+    --file) FILE=$2; shift 2;;
     -- ) shift; break ;;
     * ) break ;;
   esac
@@ -34,50 +43,164 @@ then
   exit 1
 fi
 
-if [ -z "${SQL_DUMP_FILE}" ]
-then
-  echo "Usage: $USAGE"
-  exit 1
-fi
-
 if [ -z "${BUCKET}" ]
 then
   echo "Usage: $USAGE"
   exit 1
 fi
 
-echo "Creating cloudsql DB from dump file ${SQL_DUMP_FILE}"
-SERVICE_ACCOUNT="${PROJECT}@appspot.gserviceaccount.com"
+if [ -z "${DATABASE}" ]
+then
+  echo "Usage: $USAGE"
+  exit 1
+fi
 
-gsutil acl ch -u $SERVICE_ACCOUNT:O gs://$BUCKET/$SQL_DUMP_FILE
-gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$GOOGLE_APPLICATION_CREDENTIALS
+# Function for waiting on import to finish.
+# import_wait($file, $seconds_wait_interval)
+function import_wait () {
+  gs_file=$1
+  wait_interval=$2
+  # Sleep an initial 5 seconds before checking for small files to import
+  sleep 5
+  seconds_waited=5
+  while true; do
+    if [[ $(gcloud sql operations list --instance $INSTANCE --project $PROJECT | grep "IMPORT.*RUNNING") ]]
+    then
+        sleep $wait_interval
+        seconds_waited=$((seconds_waited + wait_interval))
+    else
+        echo "Import of $gs_file finished after ${seconds_waited} seconds."
+        break
+    fi
+  done
+}
 
-# Grant access to buckets for service account for cloudsql
-SQL_SERVICE_ACCOUNT=$(gcloud sql instances describe --project $PROJECT \
-    --account $SERVICE_ACCOUNT $INSTANCE | grep serviceAccountEmailAddress \
-    | cut -d: -f2)
-# Trim leading whitespace from sql service account
-SQL_SERVICE_ACCOUNT=${SQL_SERVICE_ACCOUNT//[[:blank:]]/}
+# Function to Grant Access to files for service account and db
+function grant_access_to_files () {
+  bucket_path=$1
+  SERVICE_ACCOUNT="${PROJECT}@appspot.gserviceaccount.com"
 
-echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to bucket $BUCKET/$SQL_DUMP_FILE"
-# Note, this only applies to files already existing in the bucket.
-gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:R gs://$BUCKET/$SQL_DUMP_FILE
+  gsutil acl ch -u $SERVICE_ACCOUNT:O $bucket_path 2> /dev/null || true # Don't error if there are no files
+  gcloud auth activate-service-account $SERVICE_ACCOUNT --key-file=$GOOGLE_APPLICATION_CREDENTIALS
 
-# Import asynch
-gcloud sql instances import --project $PROJECT --account $SERVICE_ACCOUNT $INSTANCE gs://$BUCKET/$SQL_DUMP_FILE --async
+  # Grant access to buckets for service account for cloudsql
+  SQL_SERVICE_ACCOUNT=$(gcloud sql instances describe --project $PROJECT \
+      --account $SERVICE_ACCOUNT $INSTANCE | grep serviceAccountEmailAddress \
+      | cut -d: -f2)
+  # Trim leading whitespace from sql service account
+  SQL_SERVICE_ACCOUNT=${SQL_SERVICE_ACCOUNT//[[:blank:]]/}
 
-echo "Import started, waiting for it to complete."
-echo "You can also kill this script and check status at at https://console.cloud.google.com/sql/instances/${INSTANCE}/operations?project=${PROJECT}."
-minutes_waited=0
-while true; do
-  sleep 1m
-  minutes_waited=$((minutes_waited + 1))
-  import_status=$(gcloud sql operations list --instance $INSTANCE --project $PROJECT | grep "IMPORT")
-  if [[ $import_status =~ .*RUNNING* ]]
+  echo "Granting GCS access to ${SQL_SERVICE_ACCOUNT} to $bucket_path"
+  # Note, this only applies to files already existing in the bucket.
+  gsutil acl ch -u ${SQL_SERVICE_ACCOUNT}:R $bucket_path 2> /dev/null || true # Don't error if no files
+}
+
+# Function to check database exists
+# if [ $(db_exists($DATABASE)) == 1 ]; then echo "database exists"; fi
+function db_exists () {
+  local db_name=$1
+  local db=$(gcloud sql databases list -i workbenchmaindb --project all-of-us-workbench-test --filter="NAME = $db_name" \
+--format="csv [no-heading] (NAME)")
+  if [ "$db" == "$db_name" ]
   then
-     echo "Import is still running after ${minutes_waited} minutes."
+    echo 1
   else
-    echo "Import finished after ${minutes_waited} minutes."
-    break
+    echo 0;
   fi
+}
+
+
+# Create db from sql file if specified
+create_gs_file=gs://
+if [ "${CREATE_DB_SQL_FILE}" ]
+then
+    create_gs_file=$create_gs_file$BUCKET/$CREATE_DB_SQL_FILE
+    grant_access_to_files $create_gs_file
+    echo "Creating cloudsql DB from sql file ${CREATE_DB_SQL_FILE}"
+    gcloud sql import sql $INSTANCE $create_gs_file --project $PROJECT  \
+        --account $SERVICE_ACCOUNT --quiet --async
+    import_wait $create_gs_file 10
+
+    if [ $(db_exists $DATABASE) == 1 ]
+    then
+      echo "$DATABASE created successfully"
+      gsutil mv $create_gs_file gs://$BUCKET/imported_to_cloudsql/
+    else
+      echo "Error importing create sql. "
+      exit 1
+    fi
+fi
+
+#Import sql and csv files, do sql first as they are intended for schema changes and such
+# If file argument passed we just import that one
+sqls=()
+csvs=()
+
+if [ "${FILE}" ]
+then
+  # Just grant access to this file
+  gs_file=gs://$BUCKET/$FILE
+  grant_access_to_files $gs_file
+  if [[ $FILE =~ \.sql.* ]]
+  then
+    sqls[0]=$gs_file
+  elif [[ $FILE =~ \.csv.* ]]
+  then
+    csvs[0]=$gs_file
+  else
+    echo "Error. Cannot import file, $FILE,  that is not a .sql or .csv"
+    exit 1;
+  fi
+else
+    grant_access_to_files gs://$BUCKET/*
+    # gsutil returns error if no files match thus the "2> /dev/null || true" part to ignore error
+    sqls=( $(gsutil ls gs://$BUCKET/*.sql* 2> /dev/null || true) )
+    csvs=( $(gsutil ls gs://$BUCKET/*.csv* 2> /dev/null || true) )
+fi
+
+for gs_file in "${sqls[@]}"
+do
+    if [ "$gs_file" = "$create_gs_file" ]
+    then
+        echo "Skipping create db sql file ";
+    else
+        # Don't pass database to sql import . Use use <db> inside sql.
+        gcloud sql import sql $INSTANCE $gs_file --project $PROJECT --account $SERVICE_ACCOUNT --quiet --async
+        import_wait $gs_file 10
+        # Move file to imported dir
+        gsutil mv $gs_file gs://$BUCKET/imported_to_cloudsql/
+    fi
 done
+
+# Check database exists before importing csvs as they need it.
+# It may have been created in sql imports above.
+# Even if we don't have any csv's we check it exists to ensure sql above ran
+if [ $(db_exists ${DATABASE}) == 1 ]
+then
+      echo "$DATABASE exists. Carrying on."
+else
+      echo "Error. $DATABASE database does not exist."
+      exit 1
+fi
+
+for gs_file in "${csvs[@]}"
+do
+   # Get table name from file
+   filename="${gs_file##*/}"  # gets everything after last /
+   table=${filename%%.*}      # truncates everything starting with first .
+   if [[ $table ]]
+   then
+        echo "Importing file into $table. If table does not exist in the database, this file is moved to imported."
+        gcloud sql import csv $INSTANCE $gs_file --project $PROJECT --quiet --account $SERVICE_ACCOUNT \
+        --database=$DATABASE --table=$table --async
+        import_wait $gs_file 10
+        # Move file to imported dir
+        gsutil mv $gs_file gs://$BUCKET/imported_to_cloudsql/
+   else
+        echo "Unable to parse table from $filename. Skipping it."
+   fi
+done
+
+
+
+
