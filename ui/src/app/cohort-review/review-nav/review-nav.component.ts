6dba7a9991a8ec8a5b7c34cfd58a15ab650c4dfb
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-09-25 12:24:20 -0500

    removed reveiw nav and fix the grid for overview

--- ui/src/app/cohort-review/review-nav/review-nav.component.ts
+++ ui/src/app/cohort-review/review-nav/review-nav.component.ts
@@ -1,8 +1,8 @@
-import {Component} from '@angular/core';
-
-@Component({
-  selector: 'app-review-nav',
-  templateUrl: './review-nav.component.html',
-  styleUrls: ['./review-nav.component.css'],
-})
-export class ReviewNavComponent {}
+// import {Component} from '@angular/core';
+//
+// @Component({
+//   selector: 'app-review-nav',
+//   templateUrl: './review-nav.component.html',
+//   styleUrls: ['./review-nav.component.css'],
+// })
+// export class ReviewNavComponent {}
