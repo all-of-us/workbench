411faafc0680fd815d14fb926edf0e9d5c970464
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-09-28 07:52:27 -0400

    almost there

--- ui/src/app/views/recent-work/component.ts
+++ ui/src/app/views/recent-work/component.ts
@@ -1,9 +1,20 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 
 import {
+  CohortsService,
   RecentResource,
-  UserMetricsService
+  UserMetricsService,
+  Workspace,
+  WorkspacesService,
 } from 'generated';
+
+import {convertToResources, ResourceType} from 'app/utils/resourceActions';
+import {WorkspaceData} from "../../services/workspace-storage.service";
+import {WorkspaceAccessLevel} from "../../../generated/model/workspaceAccessLevel";
+import {Observable} from "rxjs/Observable";
+import {ActivatedRoute} from "@angular/router";
+import {FileDetail} from "../../../generated/model/fileDetail";
+import {CohortListResponse} from "../../../generated/model/cohortListResponse";
 
 @Component({
   selector: 'app-recent-work',
@@ -14,21 +25,55 @@ import {
 export class RecentWorkComponent implements OnInit {
   resourceList: RecentResource[];
   fullList: RecentResource[] = [];
+  @Input('workspace')
+  workspace: Workspace;
+  accessLevel: WorkspaceAccessLevel;
   startIndex = 0;
+  cssClass: string;
+  resourcesLoading = false;
   constructor(
-    private userMetricsService: UserMetricsService
+    private userMetricsService: UserMetricsService,
+    private workspacesService: WorkspacesService,
+    private cohortsService: CohortsService,
+    private route: ActivatedRoute,
   ) {}
   index: Number;
   size = 3;
+
   ngOnInit(): void {
+    this.resourcesLoading = true;
     this.updateList();
   }
 
   updateList(): void {
-    this.userMetricsService.getUserRecentResources().subscribe((resources) => {
-      this.fullList = resources;
-      this.resourceList = this.fullList.slice(this.startIndex, this.startIndex + this.size);
-    });
+    if (this.workspace) {
+      this.cssClass = 'for-list';
+      const wsData: WorkspaceData = this.route.snapshot.data.workspace;
+      this.accessLevel = wsData.accessLevel;
+      let notebookCall = this.workspacesService.getNoteBookList(this.workspace.namespace, this.workspace.id);
+      let cohortCall = this.cohortsService.getCohortsInWorkspace(this.workspace.namespace, this.workspace.id);
+      Observable.forkJoin(notebookCall, cohortCall).subscribe(([notebooks, cohorts]) =>
+      {
+        let notebookResources = convertToResources(notebooks, this.workspace.namespace,
+          this.workspace.id, this.accessLevel, ResourceType.NOTEBOOK);
+        let cohortResources = convertToResources(cohorts.items, this.workspace.namespace,
+          this.workspace.id, this.accessLevel, ResourceType.COHORT);
+        this.fullList = notebookResources.concat(cohortResources);
+        this.fullList.sort((leftSide, rightSide): number => {
+          if (leftSide.modifiedTime < rightSide.modifiedTime) return 1;
+          if (leftSide.modifiedTime > rightSide.modifiedTime) return -1;
+          return 0;
+        });
+        this.resourceList = this.fullList.slice(this.startIndex, this.startIndex + this.size);
+        this.resourcesLoading = false;
+      });
+    } else {
+      this.userMetricsService.getUserRecentResources().subscribe((resources) => {
+        this.fullList = resources;
+        this.resourceList = this.fullList.slice(this.startIndex, this.startIndex + this.size);
+        this.resourcesLoading = false;
+      });
+    }
   }
 
   scrollLeft(): void {
