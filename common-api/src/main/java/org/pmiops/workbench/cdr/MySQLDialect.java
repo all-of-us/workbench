e3b5c987b3ad8c75935de87aa40cf21ca911d4a8
danqrodney <danrodney@google.com>
2018-10-01 10:21:46 -0500

    [RW-1277] Adding (optionally) per-vocabulary and per-domain counts of matching concepts [risk=no] (#1247)
    * Start of search changes
    
    * Changes
    
    * Stuff
    
    * Switching to JQL
    
    * Gradle dialect fixes
    
    * Test fix
    
    * Removing commented out code
    
    * Adding includeVocabularyCounts parameter
    
    * Fixing ConceptControllersTest to actually run tests; remove multi-word
    tests. Changing CommonTestDialect to strip out +
    and use LOCATE so at least it matches single words properly.
    
    * Adding handling for empty queries for vocab counts
    
    * Moar tests
    
    * Fixes
    
    * Even moar tests
    
    * Fixing stuff

--- common-api/src/main/java/org/pmiops/workbench/cdr/MySQLDialect.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/MySQLDialect.java
@@ -3,14 +3,13 @@ package org.pmiops.workbench.cdr;
 import org.hibernate.dialect.function.SQLFunctionTemplate;
 import org.hibernate.type.StandardBasicTypes;
 
-public class MySQLDialect extends org.hibernate.dialect.MySQLDialect {
+public class MySQLDialect extends org.hibernate.dialect.MySQL57InnoDBDialect {
 
   public MySQLDialect() {
     super();
     // Define the custom "match" function to use "match(column) against (<string> in boolean mode)"
     // for MySQL.
-    // For some weird reason, we need to have this function use DOUBLE; see
-    // https://pavelmakhov.com/2016/09/jpa-custom-function
+    // Because MATCH returns a number, we need to have this function use DOUBLE.
 
     registerFunction("match", new SQLFunctionTemplate(StandardBasicTypes.DOUBLE,
         "match(?1) against  (?2 in boolean mode)"));
