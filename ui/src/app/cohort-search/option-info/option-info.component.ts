603e2100fbe34036ba4433f4c0ceb4ac3ff84297
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-10-09 07:53:40 -0500

    RW-1389 update width calculation, fix broken test

--- ui/src/app/cohort-search/option-info/option-info.component.ts
+++ ui/src/app/cohort-search/option-info/option-info.component.ts
@@ -32,14 +32,12 @@ export class OptionInfoComponent implements AfterViewInit, OnInit {
     const elem = this.button.nativeElement;
     const id = 'match' + this.option.id.toString();
     const highlight = document.getElementById(id);
-    this.isTruncated = (elem.offsetWidth - 15) < elem.scrollWidth;
+    this.isTruncated = elem.offsetWidth < elem.scrollWidth;
     if (this.isTruncated && highlight) {
       this.checkPosition(elem, highlight);
     } else if (highlight !== null) {
-      console.log(highlight);
       highlight.style.background =
         'linear-gradient(to right, rgba(101,159,61,0.2) 0, rgba(101,159,61,0.2) 100%)';
-    } else {
     }
   }
 
@@ -49,24 +47,13 @@ export class OptionInfoComponent implements AfterViewInit, OnInit {
     const padding = parseFloat(window.getComputedStyle(elem).getPropertyValue('padding-right'));
     if (hCoords.right > (eCoords.right - padding)) {
       const diff = hCoords.right - (eCoords.right - padding - 15);
-      if (diff > hCoords.width) {
-        console.log(this.option.name);
-        highlight.style.background = 'none';
-      } else {
-        const percentage = (((hCoords.width - diff) / hCoords.width) * 100);
-        // this.option.displayName =
-        //   highlightMatches(
-        //     [this.searchTerm], this.option.name, this.option.id.toString(), percentage
-        //   );
-        highlight.style.background =
-          'linear-gradient(to right, rgba(101,159,61,0.2) 0, rgba(101,159,61,0.2) '
-          + percentage + '%, transparent ' + percentage + '%)';
-      }
+      const percentage = (((hCoords.width - diff) / hCoords.width) * 100);
+      highlight.style.background =
+        'linear-gradient(to right, rgba(101,159,61,0.2) 0, rgba(101,159,61,0.2) '
+        + percentage + '%, transparent ' + percentage + '%)';
+    } else {
+      highlight.style.background =
+        'linear-gradient(to right, rgba(101,159,61,0.2) 0, rgba(101,159,61,0.2) 100%)';
     }
   }
-
-  get popperName() {
-    return stripHtml(this.option.displayName);
-  }
-
 }
