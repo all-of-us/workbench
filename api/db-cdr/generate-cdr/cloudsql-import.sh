d69191db911233dd7547f983e0b357ec3ca2ccee
Peter Speltz <peter.speltz@vanderbilt.edu>
2018-10-09 00:11:39 -0500

    remove hardcoded instance in db check

--- api/db-cdr/generate-cdr/cloudsql-import.sh
+++ api/db-cdr/generate-cdr/cloudsql-import.sh
@@ -98,7 +98,7 @@ function grant_access_to_files () {
 # if [ $(db_exists($DATABASE)) == 1 ]; then echo "database exists"; fi
 function db_exists () {
   local db_name=$1
-  local db=$(gcloud sql databases list -i workbenchmaindb --project $PROJECT --filter="NAME = $db_name" \
+  local db=$(gcloud sql databases list -i $INSTANCE --project $PROJECT --filter="NAME = $db_name" \
 --format="csv [no-heading] (NAME)")
   if [ "$db" == "$db_name" ]
   then
