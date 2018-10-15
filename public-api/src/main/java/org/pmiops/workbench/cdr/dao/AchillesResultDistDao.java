3a780e7ce0b321388a121fd34c55c936c762182f
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-10-15 10:10:11 -0500

    query and up update

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDistDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDistDao.java
@@ -11,6 +11,9 @@ public interface AchillesResultDistDao extends CrudRepository<AchillesResultDist
     @Query(value = "select * from achilles_results_dist where analysis_id=?1 and stratum_1=?2",nativeQuery=true)
     List<AchillesResultDist> fetchConceptDistResults(Long analysisId, String conceptId);
 
+    @Query(value = "select * from achilles_results_dist where analysis_id in (?1) and stratum_1 in (?2)", nativeQuery = true)
+    List<AchillesResultDist> fetchByAnalysisIdsAndConceptIds(List<Long> analysisIds,List<String> conceptIds);
+
 }
 
 
