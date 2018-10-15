d2a14770f432f2f7eab3f538ecd70a2790e81427
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-10-14 23:42:35 -0500

    resolving conflict

--- public-ui/src/app/data-browser/concept-charts/concept-charts.component.ts
+++ public-ui/src/app/data-browser/concept-charts/concept-charts.component.ts
@@ -6,8 +6,8 @@ import {Analysis} from '../../../publicGenerated/model/analysis';
 import {DataBrowserService} from '../../../publicGenerated/api/dataBrowser.service';
 import {Concept} from '../../../publicGenerated/model/concept';
 import {ConceptAnalysis} from '../../../publicGenerated/model/conceptAnalysis';
+import {ConceptWithAnalysis} from  '../../utils/conceptWithAnalysis';
 import {DbConfigService} from '../../utils/db-config.service';
-import {ConceptWithAnalysis} from "../../utils/conceptWithAnalysis";
 
 @Component({
   selector: 'app-concept-charts',
