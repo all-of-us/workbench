d7076d35777a869c4842397984b8389361eed724
Brian Freeman <brian.d.freeman@vumc.org>
2018-10-15 14:57:22 -0500

    RW-1295 fixed query for snomed.

--- api/src/bigquerytest/java/org/pmiops/workbench/api/CohortBuilderControllerBQTest.java
+++ api/src/bigquerytest/java/org/pmiops/workbench/api/CohortBuilderControllerBQTest.java
@@ -103,6 +103,18 @@ public class CohortBuilderControllerBQTest extends BigQueryBaseTest {
     SearchRequest searchRequest = createSearchRequests(TreeType.ICD9.name(), new ArrayList<>(), new ArrayList<>());
     assertMessageException(searchRequest, EMPTY_MESSAGE, PARAMETERS);
 
+    //icd10 no search attributes
+    searchRequest = createSearchRequests(TreeType.ICD10.name(), new ArrayList<>(), new ArrayList<>());
+    assertMessageException(searchRequest, EMPTY_MESSAGE, PARAMETERS);
+
+    //condition no search attributes
+    searchRequest = createSearchRequests(TreeType.CONDITION.name(), new ArrayList<>(), new ArrayList<>());
+    assertMessageException(searchRequest, EMPTY_MESSAGE, PARAMETERS);
+
+    //procedure no search attributes
+    searchRequest = createSearchRequests(TreeType.PROCEDURE.name(), new ArrayList<>(), new ArrayList<>());
+    assertMessageException(searchRequest, EMPTY_MESSAGE, PARAMETERS);
+
     //demo no search parameters
     searchRequest = createSearchRequests(TreeType.DEMO.name(), new ArrayList<>(), new ArrayList<>());
     assertMessageException(searchRequest, EMPTY_MESSAGE, PARAMETERS);
@@ -222,6 +234,38 @@ public class CohortBuilderControllerBQTest extends BigQueryBaseTest {
     searchRequest = createSearchRequests(TreeType.ICD9.name(), Arrays.asList(icd9), new ArrayList<>());
     assertMessageException(searchRequest, NOT_VALID_MESSAGE,
       PARAMETER, CODE, icd9.getValue());
+
+    //snomed no type
+    Criteria snomedCrtieria =
+      createCriteriaChild(null, null, 0, "", null, null);
+    SearchParameter snomed = createSearchParameter(snomedCrtieria, "");
+    searchRequest = createSearchRequests(TreeType.CONDITION.name(), Arrays.asList(snomed), new ArrayList<>());
+    assertMessageException(searchRequest, NOT_VALID_MESSAGE,
+      PARAMETER, TYPE, snomed.getType());
+
+    //snomed bad type
+    snomed.type(TreeType.VISIT.name());
+    searchRequest = createSearchRequests(TreeType.CONDITION.name(), Arrays.asList(snomed), new ArrayList<>());
+    assertMessageException(searchRequest, NOT_VALID_MESSAGE,
+      PARAMETER, TYPE, snomed.getType());
+
+    //snomed no domain
+    snomed.type(TreeType.SNOMED.name()).subtype(TreeSubType.CM.name());
+    searchRequest = createSearchRequests(TreeType.CONDITION.name(), Arrays.asList(snomed), new ArrayList<>());
+    assertMessageException(searchRequest, NOT_VALID_MESSAGE,
+      PARAMETER, DOMAIN, snomed.getDomain());
+
+    //snomed bad domain
+    snomed.domain("baddomain");
+    searchRequest = createSearchRequests(TreeType.CONDITION.name(), Arrays.asList(snomed), new ArrayList<>());
+    assertMessageException(searchRequest, NOT_VALID_MESSAGE,
+      PARAMETER, DOMAIN, snomed.getDomain());
+
+    //snomed child no concept id
+    snomed.domain(DomainType.CONDITION.name());
+    searchRequest = createSearchRequests(TreeType.CONDITION.name(), Arrays.asList(snomed), new ArrayList<>());
+    assertMessageException(searchRequest, NOT_VALID_MESSAGE,
+      PARAMETER, CONCEPT_ID, snomed.getConceptId());
 
     //demo no type
     Criteria demo =
