598bd84e31817faf8810e1d836420233557e76d6
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2018-10-17 12:16:24 -0400

    add tests

--- ui/src/app/views/homepage/component.spec.ts
+++ ui/src/app/views/homepage/component.spec.ts
@@ -1,4 +1,5 @@
 import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
+import {By} from '@angular/platform-browser';
 import {RouterTestingModule} from '@angular/router/testing';
 
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
@@ -12,7 +13,7 @@ import {ProfileStorageServiceStub} from 'testing/stubs/profile-storage-service-s
 import {ServerConfigServiceStub} from 'testing/stubs/server-config-service-stub';
 import {UserMetricsServiceStub} from 'testing/stubs/user-metrics-service-stub';
 
-import {updateAndTick} from 'testing/test-helpers';
+import {simulateClick, updateAndTick} from 'testing/test-helpers';
 
 import {ProfileStorageService} from 'app/services/profile-storage.service';
 import {ServerConfigService} from 'app/services/server-config.service';
@@ -23,6 +24,7 @@ import {WorkspacesService} from 'generated/api/workspaces.service';
 import {ConfirmDeleteModalComponent} from 'app/views/confirm-delete-modal/component';
 import {EditModalComponent} from 'app/views/edit-modal/component';
 import {HomepageComponent} from 'app/views/homepage/component';
+import {QuickTourModalComponent} from 'app/views/quick-tour-modal/component';
 import {RecentWorkComponent} from 'app/views/recent-work/component';
 import {RenameModalComponent} from 'app/views/rename-modal/component';
 import {ResourceCardComponent} from 'app/views/resource-card/component';
@@ -44,6 +46,7 @@ describe('HomepageComponent', () => {
         HomepageComponent,
         RecentWorkComponent,
         LeftScrollComponent,
+        QuickTourModalComponent,
         RightScrollComponent,
         ResourceCardComponent,
         ConfirmDeleteModalComponent,
@@ -73,4 +76,18 @@ describe('HomepageComponent', () => {
     updateAndTick(fixture);
     expect(fixture).toBeTruthy();
   }));
+
+  it('should display quick tour when clicked', fakeAsync(() =>  {
+    simulateClick(fixture, fixture.debugElement.query(By.css('#learn')));
+    updateAndTick(fixture);
+    expect(fixture.debugElement.query(By.css('#quick-tour'))).toBeTruthy();
+  }));
+
+  it('should close quick tour when closed', fakeAsync(() => {
+    simulateClick(fixture, fixture.debugElement.query(By.css('#learn')));
+    updateAndTick(fixture);
+    simulateClick(fixture, fixture.debugElement.query(By.css('#close')));
+    updateAndTick(fixture);
+    expect(fixture.debugElement.query(By.css('#quick-tour'))).toBeNull();
+  }))
 });
