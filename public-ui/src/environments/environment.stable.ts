71af41db933751d555613e37d4f82070e02ad704
Brian Rubenstein <brubenst@broadinstitute.org>
2018-10-18 09:43:59 -0400

    PR Feedback

--- public-ui/src/environments/environment.stable.ts
+++ public-ui/src/environments/environment.stable.ts
@@ -2,6 +2,6 @@ export const environment = {
   displayTag: 'Stable',
   publicApiUrl: 'https://public-api-dot-all-of-us-rw-stable.appspot.com',
   workbenchUrl: 'https://all-of-us-rw-stable.appspot.com',
-  clients: '56507752110-ovdus1lkreopsfhlovejvfgmsosveda6.apps.googleusercontent.com',
+  clientId: '56507752110-ovdus1lkreopsfhlovejvfgmsosveda6.apps.googleusercontent.com',
   debug: false,
 };
