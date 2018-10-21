b5f354c323d4cd0eeb3d12a252982b15cb4adafe
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-10-20 21:44:17 -0500

    clean up

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
@@ -1,10 +1,7 @@
 package org.pmiops.workbench.cdr.dao;
 
 import org.pmiops.workbench.cdr.model.AchillesResult;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
-import java.util.TreeMap;
-import java.util.List;
 
 public interface AchillesResultDao extends CrudRepository<AchillesResult, Long> {
     AchillesResult findAchillesResultByAnalysisId(Long analysisId);
