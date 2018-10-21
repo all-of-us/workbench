4eacaf1608c429a93073f2179c2436d220342984
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-10-20 21:37:14 -0500

    clean up

--- public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
+++ public-api/src/main/java/org/pmiops/workbench/cdr/dao/AchillesResultDao.java
@@ -8,9 +8,4 @@ import java.util.List;
 
 public interface AchillesResultDao extends CrudRepository<AchillesResult, Long> {
     AchillesResult findAchillesResultByAnalysisId(Long analysisId);
-
-    @Query(value = "select distinct id,analysis_id,stratum_1,stratum_2,stratum_3,stratum_4,stratum_5," +
-            "count_value,source_count_value from achilles_results where stratum_1='1586134' and analysis_id=3110\n" +
-            "order by CAST(stratum_5 AS UNSIGNED) asc;", nativeQuery = true)
-    List<AchillesResult> findPPIQuestionOrderBySurveyConceptId(String survey_concept_id);
 }
