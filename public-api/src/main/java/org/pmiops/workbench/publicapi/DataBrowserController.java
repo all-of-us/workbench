eb46c996d34994a95d99cf5ba90134d6c7d39951
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-10-20 22:16:03 -0500

    clean up

--- public-api/src/main/java/org/pmiops/workbench/publicapi/DataBrowserController.java
+++ public-api/src/main/java/org/pmiops/workbench/publicapi/DataBrowserController.java
@@ -10,7 +10,6 @@ import java.util.List;
 import java.util.Map;
 import java.util.Set;
 import java.util.TreeSet;
-import java.util.TreeMap;
 import java.util.function.Function;
 import java.util.stream.Collectors;
 import javax.inject.Provider;
