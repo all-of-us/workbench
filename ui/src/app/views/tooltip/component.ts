1428892394778bf8a772002fe8f24510941e0e3d
Ursa Stutsman <ursas@broadinstitute.org>
2018-10-22 16:27:26 -0400

    [RW-1513][Risk=no] Adding tooltips (#1382)
    * Tooltips
    
    * clean code
    
    * fixing tests
    
    * little cleanup
    
    * PR comments
    
    * Trigger tests I think?

---
+++ ui/src/app/views/tooltip/component.ts
@@ -0,0 +1,20 @@
+import {Component, Input, OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-tooltip',
+  styleUrls: ['./component.css'],
+  templateUrl: './component.html',
+})
+export class ToolTipComponent {
+  @Input('direction') direction = 'right';
+
+  findPosition(): void {
+    const el = document.getElementById('toolTip');
+    const bottom = el.getBoundingClientRect().bottom;
+    if (bottom <= 0) {
+      this.direction = this.direction.replace('top', 'bottom');
+    } else {
+      this.direction = this.direction.replace('bottom', 'top');
+    }
+  }
+}
