441946da116072e2daefec273e1486ffeedb81ab
CH Albach <calbach@google.com>
2018-10-23 18:33:01 -0700

    Fix cohorts loading, remove lots of dead code

--- ui/src/app/views/cohort-list/component.ts
+++ ui/src/app/views/cohort-list/component.ts
@@ -1,4 +1,4 @@
-import {Component, OnDestroy, OnInit, ViewChild} from '@angular/core';
+import {Component, OnInit, ViewChild} from '@angular/core';
 import {ActivatedRoute, Router} from '@angular/router';
 import {Observable} from 'rxjs/Observable';
 
@@ -16,7 +16,6 @@ import {
 } from 'generated';
 
 import {convertToResources, ResourceType} from 'app/utils/resourceActions';
-import {ToolTipComponent} from '../tooltip/component';
 
 @Component({
   styleUrls: ['../../styles/buttons.css',
@@ -24,22 +23,15 @@ import {ToolTipComponent} from '../tooltip/component';
     './component.css'],
   templateUrl: './component.html',
 })
-export class CohortListComponent implements OnInit, OnDestroy {
+export class CohortListComponent implements OnInit {
   accessLevel: WorkspaceAccessLevel;
-  cohortList: Cohort[] = [];
   resourceList: RecentResource[] = [];
-  workspace: Workspace;
   cohortsLoading = true;
-  cohortsError = false;
   wsNamespace: string;
   wsId: string;
 
-
   @ViewChild(ConfirmDeleteModalComponent)
   deleteModal: ConfirmDeleteModalComponent;
-
-  @ViewChild(ToolTipComponent)
-  toolTip: ToolTipComponent;
 
   constructor(
     private route: ActivatedRoute,
@@ -47,7 +39,6 @@ export class CohortListComponent implements OnInit, OnDestroy {
     private router: Router,
   ) {
     const wsData: WorkspaceData = this.route.snapshot.data.workspace;
-    this.workspace = wsData;
     this.accessLevel = wsData.accessLevel;
   }
 
@@ -57,50 +48,27 @@ export class CohortListComponent implements OnInit, OnDestroy {
     this.reloadCohorts();
   }
 
-  reloadCohorts(): Observable<CohortListResponse> {
+  reloadCohorts(): void {
     this.cohortsLoading = true;
-    this.cohortList = [];
-    const call = this.cohortsService.getCohortsInWorkspace(this.wsNamespace, this.wsId);
-    call
+    this.resourceList = [];
+    this.cohortsService.getCohortsInWorkspace(this.wsNamespace, this.wsId)
       .subscribe(
-        cohortsReceived => {
-          this.cohortList = cohortsReceived.items.map(function(cohorts) {
-            return cohorts;
-          });
-          this.resourceList = convertToResources(this.cohortList, this.wsNamespace,
+        resp => {
+          this.resourceList = convertToResources(resp.items, this.wsNamespace,
             this.wsId, this.accessLevel, ResourceType.COHORT);
           this.cohortsLoading = false;
         },
         error => {
           this.cohortsLoading = false;
-          this.cohortsError = true;
         });
-    return call;
-  }
-
-  ngOnDestroy(): void {
-
   }
 
   buildCohort(): void {
     this.router.navigate(['build'], {relativeTo: this.route});
   }
 
-  public deleteCohort(cohort: Cohort): void {
-    this.cohortsService.deleteCohort(this.wsNamespace, this.wsId, cohort.id).subscribe(() => {
-      this.cohortList.splice(
-        this.cohortList.indexOf(cohort), 1);
-      this.deleteModal.close();
-    });
-  }
-
   get writePermission(): boolean {
     return this.accessLevel === WorkspaceAccessLevel.OWNER
       || this.accessLevel === WorkspaceAccessLevel.WRITER;
   }
-
-  get actionsDisabled(): boolean {
-    return !this.writePermission;
-  }
-
 }
