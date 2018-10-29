65de13e5a031efe5403aa9949a95553e68a8a7e5
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-10-29 13:38:57 -0500

    lint issue fixes

--- ui/src/app/cohort-review/cohort-review.module.ts
+++ ui/src/app/cohort-review/cohort-review.module.ts
@@ -26,9 +26,9 @@ import {DetailTabTableComponent} from './detail-tab-table/detail-tab-table.compo
 import {DetailTabsComponent} from './detail-tabs/detail-tabs.component';
 import {IndividualParticipantsChartsComponent} from './individual-participants-charts/individual-participants-charts';
 import {OverviewPage} from './overview-page/overview-page';
-import {ParticipantsChartsComponent} from './participants-charts/participant-charts';
 import {PageLayout} from './page-layout/page-layout';
 import {ParticipantStatusComponent} from './participant-status/participant-status.component';
+import {ParticipantsChartsComponent} from './participants-charts/participant-charts';
 import {ReviewStateService} from './review-state.service';
 import {CohortReviewRoutingModule} from './routing/routing.module';
 import {SetAnnotationCreateComponent} from './set-annotation-create/set-annotation-create.component';
