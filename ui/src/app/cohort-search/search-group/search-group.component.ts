d6667913436a89cca0d1639e5322a5b21da2826e
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-10-31 11:09:27 -0500

    commented HTML for temporal

--- ui/src/app/cohort-search/search-group/search-group.component.ts
+++ ui/src/app/cohort-search/search-group/search-group.component.ts
@@ -17,13 +17,18 @@ import {SearchRequest} from 'generated';
 export class SearchGroupComponent {
   @Input() group;
   @Input() role: keyof SearchRequest;
-  temporalDropdown = false;
-  whichMention = ['Any mention', 'First mention', 'Last mention'];
-  timeDropDown = ['During same encounter as',
-                  'X Days before', 'X Days after', 'Within X days of',
-    'On or X days before', 'On or X days after'];
-  dropdownOption: any;
-  timeDropdownOption: any;
+
+  //******* Temporal Changes are commented bellow Please
+  // uncomment below codes when using Temporal HTML **********//
+
+  // temporalDropdown = false;
+  // whichMention = ['Any mention', 'First mention', 'Last mention'];
+  // timeDropDown = ['During same encounter as',
+  //                 'X Days before', 'X Days after', 'Within X days of',
+  //   'On or X days before', 'On or X days after'];
+  // dropdownOption: any;
+  // timeDropdownOption: any;
+
   readonly domainTypes = DOMAIN_TYPES;
   readonly programTypes = PROGRAM_TYPES;
 
@@ -55,21 +60,22 @@ export class SearchGroupComponent {
     const context = {criteriaType, criteriaSubtype, role, groupId, itemId, fullTree, codes};
     this.actions.openWizard(itemId, criteria.type, context);
   }
+  // *******Temporal Changes are commented bellow Please
+  // uncomment below codes when using Temporal HTML ******//
 
-  getTemporal(e) {
-    if (e.target.checked === true) {
-      this.temporalDropdown = true;
-    } else {
-      this.temporalDropdown = false;
-    }
-
-  }
-
-  getMentionTitle(mention) {
-    this.dropdownOption = mention;
-  }
-  getTimeTitle(time) {
-    this.timeDropdownOption = time;
-
-  }
+  // getTemporal(e) {
+  //   if (e.target.checked === true) {
+  //     this.temporalDropdown = true;
+  //   } else {
+  //     this.temporalDropdown = false;
+  //   }
+  //
+  // }
+  //
+  // getMentionTitle(mention) {
+  //   this.dropdownOption = mention;
+  // }
+  // getTimeTitle(time) {
+  //   this.timeDropdownOption = time;
+  // }
 }
