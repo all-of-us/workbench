4efa9d0cdf4c1c1f24a24e7110cad50c01dc4f04
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-10-31 13:49:59 -0500

    RW-1475 fix condition for second level nodes

--- ui/src/app/cohort-search/modal/modal.component.ts
+++ ui/src/app/cohort-search/modal/modal.component.ts
@@ -1,21 +1,20 @@
 import {select} from '@angular-redux/store';
 import {Component, OnDestroy, OnInit} from '@angular/core';
-import {ActivatedRoute} from '@angular/router';
-import {CohortBuilderService, DomainType, TreeSubType, TreeType} from 'generated';
+import {DomainType, TreeSubType, TreeType} from 'generated';
 import {Map} from 'immutable';
 import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
 import {DOMAIN_TYPES, PROGRAM_TYPES} from '../constant';
 import {
-activeCriteriaSubtype,
-activeCriteriaTreeType,
-activeCriteriaType,
-activeItem,
-activeParameterList,
-CohortSearchActions,
-nodeAttributes,
-subtreeSelected,
-wizardOpen,
+  activeCriteriaSubtype,
+  activeCriteriaTreeType,
+  activeCriteriaType,
+  activeItem,
+  activeParameterList,
+  CohortSearchActions,
+  nodeAttributes,
+  subtreeSelected,
+  wizardOpen,
 } from '../redux';
 import {stripHtml, subtypeToTitle, typeToTitle} from '../utils';
 
@@ -56,11 +55,7 @@ export class ModalComponent implements OnInit, OnDestroy {
   demoParam: string;
   count = 0;
   originalNode: any;
-  constructor(
-    private route: ActivatedRoute,
-    private api: CohortBuilderService,
-    private actions: CohortSearchActions
-  ) {}
+  constructor(private actions: CohortSearchActions) {}
 
   ngOnInit() {
     this.subscription = this.open$
@@ -188,7 +183,7 @@ export class ModalComponent implements OnInit, OnDestroy {
       type: this.ctype,
       subtype: this.subtype,
       fullTree: this.fullTree,
-      id: 0
+      id: 0,    // root parent ID is always 0
     });
   }
 
@@ -254,24 +249,6 @@ export class ModalComponent implements OnInit, OnDestroy {
 
   get altTab() {
     return this.attributesNode.size > 0;
-  }
-
-  get secondLevel() {
-    return this.ctype === TreeType[TreeType.ICD10]
-      || (this.ctype === TreeType[TreeType.ICD9] && this.subtype === TreeSubType[TreeSubType.PROC]);
-  }
-
-  get parentId() {
-    const cdrid = this.route.snapshot.data.workspace.cdrVersionId;
-    if (this.secondLevel) {
-      this.api.getCriteriaBy(cdrid, this.ctype, this.subtype, 0)
-        .subscribe(result => {
-          console.log(result);
-          return result.items[0].id;
-        });
-    } else {
-      return 0;
-    }
   }
 
   selectionHeader(_type: string) {
