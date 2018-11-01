e751bf34c9502368b25f29eac32e2607874af25a
dolbeew <40036095+dolbeew@users.noreply.github.com>
2018-11-01 07:26:52 -0500

    [RW-1475][risk=no] Skip root level for certain trees (#1435)
    * RW-1475 add api call to get second level nodes
    
    * RW-1475 test moving second level function into node component
    
    * RW-1475 fix condition for second level nodes
    
    * RW-1475 fix loadChildren condition for full trees
    
    * [RW-1556][risk = no]P2] When creating a list of enum values -> list is empty (#1429)
    
    * annotation enumuration changes
    
    * added condition to check if no value there just take enum value
    
    * upgrade jasmine (#1432)
    
    * Upgrade Jasmine
    
    * [risk=no] public database version change (#1426)
    
    * Database version change
    
    * Database version change
    
    * Leaving prod version as it is for now
    
    * [RW-1560][risk=no] refactor denormalized tables and cohort chart queries (#1434)
    
    * RW-1560 adding concept_ids to denormalized tables.
    
    * RW-1560 adding concept_ids to denormalized tables.
    
    * RW-1560 fix.
    
    * RW-1560 fixing cohort charts.
    
    * RW-1560 adding concept_ids to denormalized tables.
    
    * RW-1560 adding concept_ids to denormalized tables.
    
    * RW-1560 fix.
    
    * RW-1560 fixing cohort charts.
    
    * RW-1560 fixing test case.
    
    * annotation enumuration changes
    
    * added condition to check if no value there just take enum value
    
    * [risk=no] fix parent searh (#1436)
    
    * tweek.
    
    * tweek.
    
    * [RW-1545][Risk=no] Fix for quickTour Modal not opening on first visit (#1431)
    
    * fix for quickTour Modal not opening on first visit
    
    * add and fix tests
    
    * RW-1475 add api call to get second level nodes
    
    * RW-1475 test moving second level function into node component
    
    * RW-1475 fix condition for second level nodes
    
    * RW-1475 fix loadChildren condition for full trees

--- ui/src/app/cohort-search/node/node.component.ts
+++ ui/src/app/cohort-search/node/node.component.ts
@@ -160,6 +160,9 @@ export class NodeComponent implements OnInit, OnDestroy {
     if (this.fullTree) {
       this.expanded = this.node.get('expanded', false);
     }
+    if (this.secondLevel) {
+      setTimeout(() => this.loadChildren(true));
+    }
   }
 
   ngOnDestroy() {
@@ -200,7 +203,7 @@ export class NodeComponent implements OnInit, OnDestroy {
      */
     if (_type === TreeType[TreeType.DRUG]) {
       this.actions.fetchDrugCriteria(_type, parentId, TreeSubType[TreeSubType.ATC]);
-    } else if (this.fullTree && _type === TreeType[TreeType.PM]) {
+    } else if (this.fullTree) {
       this.actions.fetchAllCriteria(_type, parentId);
     } else if (this.codes && this.node.get('subtype')) {
       this.actions.fetchCriteriaBySubtype(_type, this.node.get('subtype'), parentId);
@@ -286,5 +289,12 @@ export class NodeComponent implements OnInit, OnDestroy {
 
   get multipleMatches() {
     return this.ingredients.length > 0 || this.subMatches > 1;
+  }
+
+  get secondLevel() {
+    return this.node.get('parentId') === 0
+      && (this.node.get('type') === TreeType[TreeType.ICD10]
+      || (this.node.get('type') === TreeType[TreeType.ICD9]
+      && this.node.get('subtype') === TreeSubType[TreeSubType.PROC]));
   }
 }
