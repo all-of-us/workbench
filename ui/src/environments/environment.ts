ab2c5bf29e2262ab5d400505a66ce4beec096ded
smazumd <42041705+smazumd@users.noreply.github.com>
2018-11-01 15:39:38 -0500

    [temp_flag][risk=no] Added temp flag in environtment.ts (#1442)
    * added flag to environment file
    
    * changed the spec

--- ui/src/environments/environment.ts
+++ ui/src/environments/environment.ts
@@ -3,5 +3,6 @@ import {testEnvironmentBase} from 'environments/test-env-base';
 export const environment = {
   ...testEnvironmentBase,
   displayTag: 'Local->Test',
-  debug: true
+  debug: true,
+  enableTemporal: false
 };
