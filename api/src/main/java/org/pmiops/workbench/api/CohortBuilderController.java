8708c81378ff38330cbf1108dcdc7051edb52428
Brian Freeman <brian.d.freeman@vumc.org>
2018-11-08 16:28:32 -0600

    [risk=no] small fix for autocomplete search (#1482)
    * fixing search terms.
    
    * fine tuning autocomplete.
    
    * remove unused imports.
    
    * [risk=no] DB-121/Added support to display intersex/none sexes (#1467)
    
    * DB-121/Added support to display intersex/none genders
    
    * resolving codacy error
    
    * removing console print
    
    * DB-125 mouse hover over survey responses
    
    * Removed the expandable graph option on did not answer survey answer
    
    * Addressing comments
    
    * Addressing comments
    
    * Fixing prev indentation
    
    * Fixing prev indentation
    
    * fixing test case.
    
    * fixing search terms.
    
    * fine tuning autocomplete.
    
    * remove unused imports.
    
    * fixing test case.
    
    * [RW-1598][risk=no] null recent resource regression test (slightly contrived) (#1478)
    
    * fixing search terms.
    
    * fine tuning autocomplete.
    
    * remove unused imports.
    
    * fixing test case.
    
    * fine tuning autocomplete.
    
    * fixing test case.

--- api/src/main/java/org/pmiops/workbench/api/CohortBuilderController.java
+++ api/src/main/java/org/pmiops/workbench/api/CohortBuilderController.java
@@ -29,6 +29,7 @@ import java.util.Map;
 import java.util.Optional;
 import java.util.function.Function;
 import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 
 @RestController
@@ -284,12 +285,20 @@ public class CohortBuilderController implements CohortBuilderApiDelegate {
       throw new BadRequestException(
         String.format("Bad Request: Please provide a valid search term: \"%s\" is not valid.", value));
     }
-    String[] keywords = value.split("[,+\\s+]");
+    String[] keywords = value.split("\\W+");
     if (keywords.length == 1 && keywords[0].length() <= 3) {
-      return "+" + keywords[0];
+      return "+\"" + keywords[0] + "\"";
     }
-    return Arrays.stream(keywords)
-      .map(term -> "+" + term + "*")
+
+    return IntStream
+      .range(0, keywords.length)
+      .filter(i -> keywords[i].length() > 2)
+      .mapToObj(i -> {
+        if (i == 0 && keywords.length > 1) {
+          return "+\"" + keywords[i] + "\"";
+        }
+        return "+" + keywords[i] + "*";
+      })
       .collect(Collectors.joining());
   }
 
--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/CriteriaDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/CriteriaDao.java
@@ -32,7 +32,8 @@ public interface CriteriaDao extends CrudRepository<Criteria, Long> {
   @Query(value = "select min(cr.id) as id from Criteria cr " +
     "    where cr.type = upper(?1) " +
     "    and (match(synonyms, ?2) > 0 or cr.code like upper(concat(?3,'%')))" +
-    "    group by cr.name")
+    "    group by cr.name, cr.count " +
+    "    order by convert(cr.count, decimal) desc")
   List<CriteriaId> findCriteriaByTypeForCodeOrName(String type,
                                                    String modifiedValue,
                                                    String value,
@@ -42,7 +43,8 @@ public interface CriteriaDao extends CrudRepository<Criteria, Long> {
     "    where cr.type = upper(?1) " +
     "    and cr.subtype = upper(?2) " +
     "    and (match(synonyms, ?3) > 0 or cr.code like upper(concat(?4,'%')))" +
-    "    group by cr.name")
+    "    group by cr.name, cr.count " +
+    "    order by convert(cr.count, decimal) desc")
   List<CriteriaId> findCriteriaByTypeAndSubtypeForCodeOrName(String type,
                                                              String subtype,
                                                              String modifiedValue,
@@ -53,7 +55,8 @@ public interface CriteriaDao extends CrudRepository<Criteria, Long> {
     "    where cr.type = upper(?1) " +
     "    and cr.subtype = upper(?2) " +
     "    and match(synonyms, ?3) > 0 " +
-    "    group by cr.name")
+    "    group by cr.name, cr.count " +
+    "    order by convert(cr.count, decimal) desc")
   List<CriteriaId> findCriteriaByTypeAndSubtypeForCodeOrName(String type,
                                                              String subtype,
                                                              String value,
@@ -78,7 +81,7 @@ public interface CriteriaDao extends CrudRepository<Criteria, Long> {
     "order by c.name asc " +
     "limit :limit", nativeQuery = true)
   List<Criteria> findDrugBrandOrIngredientByValue(@Param("value") String value,
-                                                 @Param("limit") Long limit);
+                                                  @Param("limit") Long limit);
 
   @Query(value = "select * from criteria c " +
     "inner join ( " +
@@ -108,13 +111,16 @@ public interface CriteriaDao extends CrudRepository<Criteria, Long> {
     "and code = :code " +
     "and is_group = 1 " +
     "and is_selectable = 1))", nativeQuery = true)
-  List<String> findCriteriaByTypeAndSubtypeAndCode(@Param("type") String type, @Param("subtype") String subtype, @Param("code") String code);
+  List<String> findCriteriaByTypeAndSubtypeAndCode(@Param("type") String type,
+                                                   @Param("subtype") String subtype,
+                                                   @Param("code") String code);
 
   @Query(value = "select * from criteria c " +
     "where c.type = :type " +
     "and (match(c.name) against(:value in boolean mode) or match(c.code) against(:value in boolean mode)) " +
     "and c.is_selectable = 1 " +
     "order by c.code asc", nativeQuery = true)
-  List<Criteria> findCriteriaByTypeAndNameOrCode(@Param("type") String type, @Param("value") String value);
+  List<Criteria> findCriteriaByTypeAndNameOrCode(@Param("type") String type,
+                                                 @Param("value") String value);
 
 }
