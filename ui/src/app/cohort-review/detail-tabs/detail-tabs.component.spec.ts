c317802c68301b66660343e56d637882a1b9d54a
Brian Freeman <brian.d.freeman@vumc.org>
2018-11-13 13:15:08 -0600

    RW-1655 fixing test case.

--- ui/src/app/cohort-review/detail-tabs/detail-tabs.component.spec.ts
+++ ui/src/app/cohort-review/detail-tabs/detail-tabs.component.spec.ts
@@ -2,6 +2,7 @@ import {NO_ERRORS_SCHEMA} from '@angular/core';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ActivatedRoute} from '@angular/router';
 import {RouterTestingModule} from '@angular/router/testing';
+import {ClarityModule} from '@clr/angular';
 import {ChartModule} from 'angular2-highcharts';
 import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
 import {CohortReviewService} from 'generated';
@@ -52,7 +53,7 @@ describe('DetailTabsComponent', () => {
   beforeEach(async(() => {
     TestBed.configureTestingModule({
       declarations: [DetailTabsComponent, IndividualParticipantsChartsComponent],
-      imports: [ChartModule, RouterTestingModule],
+      imports: [ChartModule, ClarityModule, RouterTestingModule],
        schemas: [NO_ERRORS_SCHEMA],
       providers: [
         {
