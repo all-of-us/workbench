317348b71623fd38edb6b45dacc93b62e55d8446
Greg Rushton <grushton@broadinstitute.org>
2018-11-16 15:50:22 -0500

    add client id config for prod

--- public-ui/src/environments/environment.prod.ts
+++ public-ui/src/environments/environment.prod.ts
@@ -1,5 +1,6 @@
 export const environment = {
   displayTag: '',
   publicApiUrl: 'https://public.api.researchallofus.org',
+  clientId: '684273740878-d7i68in5d9hqr6n9mfvrdh53snekp79f.apps.googleusercontent.com',
   debug: false,
 };
