3bc15e28796dff888844b3d0f1ac034db615bd72
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-11-19 14:00:16 -0600

    addressing comments

--- public-ui/src/app/highlight-search/highlight-search.component.ts
+++ public-ui/src/app/highlight-search/highlight-search.component.ts
@@ -1,24 +1,19 @@
-import { Component, Input, OnChanges, OnInit } from '@angular/core';
+import { Component, Input, OnChanges } from '@angular/core';
 
 @Component({
   selector: 'app-highlight-search',
   templateUrl: './highlight-search.component.html',
   styleUrls: ['./highlight-search.component.css']
 })
-export class HighlightSearchComponent implements OnInit, OnChanges {
+export class HighlightSearchComponent implements OnChanges {
   @Input() text: string;
   @Input() searchTerm: string;
   @Input() isSource: boolean;
   @Input() conceptId: string;
   @Input() isStandard: string;
   @Input() indNum: number;
-  chunks: string[];
+  words: string[];
   reString: RegExp;
-
-  constructor() { }
-
-  ngOnInit() {
-  }
   ngOnChanges() {
     if (this.searchTerm) {
       let searchWords = this.searchTerm.split(new RegExp(',| '));
@@ -26,6 +21,6 @@ export class HighlightSearchComponent implements OnInit, OnChanges {
       searchWords = searchWords.map(word => word.replace(/[&!^\/\\#,+()$~%.'":*?<>{}]/g, ''));
       this.reString = new RegExp(searchWords.join('|'));
     }
-    this.chunks = this.text.split(' ');
+    this.words = this.text.split(' ');
   }
 }
