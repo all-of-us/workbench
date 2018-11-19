da1db0fe99421da1f2aaa213be97164fb99733f7
Greg Rushton <grushton@broadinstitute.org>
2018-11-19 09:02:39 -0500

    lint

--- public-ui/src/environments/environment.stable.ts
+++ public-ui/src/environments/environment.stable.ts
@@ -3,7 +3,7 @@ export const environment = {
   publicApiUrl: 'https://public-api-dot-all-of-us-rw-stable.appspot.com',
   workbenchUrl: 'https://all-of-us-rw-stable.appspot.com',
   clientId: '56507752110-ovdus1lkreopsfhlovejvfgmsosveda6.apps.googleusercontent.com',
-  tcellappid: "AoUNonProd-WZFW2",
-  tcellapikey: "AQEBBAEkx4iE2KxNyI7Wx08EwU1ycTM7E4FMSmaibbMUQxNU6uQvuAJt7fyABAtFYSYfgEE",
+  tcellappid: 'AoUNonProd-WZFW2',
+  tcellapikey: 'AQEBBAEkx4iE2KxNyI7Wx08EwU1ycTM7E4FMSmaibbMUQxNU6uQvuAJt7fyABAtFYSYfgEE',
   debug: false,
 };
