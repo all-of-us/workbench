f45ca8bff23894c220c166442ae05b2297ac62cc
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-11-20 07:46:15 -0600

    RW-1642 fix modifiers-page test

--- ui/src/app/cohort-search/modifier-page/modifier-page.component.spec.ts
+++ ui/src/app/cohort-search/modifier-page/modifier-page.component.spec.ts
@@ -2,12 +2,15 @@ import {dispatch, NgRedux} from '@angular-redux/store';
 import {MockNgRedux} from '@angular-redux/store/testing';
 import {async, ComponentFixture, TestBed} from '@angular/core/testing';
 import {ReactiveFormsModule} from '@angular/forms';
+import {ActivatedRoute} from '@angular/router';
 import {ClarityModule} from '@clr/angular';
 
+import {CohortBuilderService} from 'generated';
 import {fromJS} from 'immutable';
 import {NgxPopperModule} from 'ngx-popper';
 import {activeModifierList, CohortSearchActions, previewStatus} from '../redux';
 import {ModifierPageComponent} from './modifier-page.component';
+import {Observable} from 'rxjs/Observable';
 
 class MockActions {
   @dispatch() activeModifierList = activeModifierList;
@@ -36,7 +39,12 @@ describe('ModifierPageComponent', () => {
       ],
       providers: [
         {provide: NgRedux, useValue: mockReduxInst},
+        {provide: CohortBuilderService, useValue: {}},
         {provide: CohortSearchActions, useValue: new MockActions()},
+        {
+          provide: ActivatedRoute,
+          useValue: {snapshot: {data: {workspace: {cdrid: 1}}}}
+        },
       ],
     })
       .compileComponents();
