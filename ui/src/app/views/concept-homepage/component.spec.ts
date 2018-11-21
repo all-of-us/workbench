cbbbda5122abb0851b60b308678b97a601f4a294
Brian Rubenstein <brubenst@broadinstitute.org>
2018-11-21 13:02:10 -0500

    Fix tests

--- ui/src/app/views/concept-homepage/component.spec.ts
+++ ui/src/app/views/concept-homepage/component.spec.ts
@@ -27,7 +27,7 @@ import {
 import {ConceptSetsServiceStub} from 'testing/stubs/concept-sets-service-stub';
 import {ConceptsServiceStub, DomainStubVariables} from 'testing/stubs/concepts-service-stub';
 import {WorkspacesServiceStub, WorkspaceStubVariables} from 'testing/stubs/workspace-service-stub';
-import {simulateClick, simulateInput, updateAndTick} from 'testing/test-helpers';
+import {simulateClick, simulateEvent, simulateInput, updateAndTick} from 'testing/test-helpers';
 
 
 const activatedRouteStub  = {
@@ -107,8 +107,10 @@ describe('ConceptHomepageComponent', () => {
 
     const searchTerm = 'test';
 
-    simulateInput(fixture, fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
-    simulateClick(fixture, fixture.debugElement.query(By.css('.btn-search')));
+    simulateInput(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
+    simulateEvent(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), 'keydown.enter');
     updateAndTick(fixture);
 
     DomainStubVariables.STUB_DOMAINS.forEach((domain) => {
@@ -151,8 +153,10 @@ describe('ConceptHomepageComponent', () => {
     simulateClick(fixture, fixture.debugElement
       .query(By.css('.standard-concepts-checkbox')).children[0]);
 
-    simulateInput(fixture, fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
-    simulateClick(fixture, fixture.debugElement.query(By.css('.btn-search')));
+    simulateInput(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
+    simulateEvent(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), 'keydown.enter');
     updateAndTick(fixture);
 
     DomainStubVariables.STUB_DOMAINS.forEach((domain) => {
@@ -181,8 +185,10 @@ describe('ConceptHomepageComponent', () => {
     const searchTerm = 'test';
     simulateClick(fixture, fixture.debugElement
         .query(By.css('.standard-concepts-checkbox')).children[0]);
-    simulateInput(fixture, fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
-    simulateClick(fixture, fixture.debugElement.query(By.css('.btn-search')));
+    simulateInput(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
+    simulateEvent(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), 'keydown.enter');
     updateAndTick(fixture);
     const dataRow = fixture.debugElement.queryAll(By.css('.concept-row'));
     const checkBox = dataRow[0].queryAll(By.css('.datagrid-select'))[0]
@@ -201,8 +207,10 @@ describe('ConceptHomepageComponent', () => {
     const searchTerm = 'test';
     simulateClick(fixture, fixture.debugElement
         .query(By.css('.standard-concepts-checkbox')).children[0]);
-    simulateInput(fixture, fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
-    simulateClick(fixture, fixture.debugElement.query(By.css('.btn-search')));
+    simulateInput(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), searchTerm);
+    simulateEvent(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), 'keydown.enter');
     updateAndTick(fixture);
     const button = fixture.debugElement.query(By.css('.sliding-button'))
        .query(By.css('.text'));
@@ -225,7 +233,8 @@ describe('ConceptHomepageComponent', () => {
 
     simulateClick(fixture, de.query(By.css('.standard-concepts-checkbox')).children[0]);
     simulateInput(fixture, de.query(By.css('#concept-search-input')), 'test');
-    simulateClick(fixture, de.query(By.css('.btn-search')));
+    simulateEvent(fixture,
+      fixture.debugElement.query(By.css('#concept-search-input')), 'keydown.enter');
     updateAndTick(fixture);
     const dataRow = de.queryAll(By.css('.concept-row'));
     const checkBox = dataRow[0].queryAll(By.css('.datagrid-select'))[0]
