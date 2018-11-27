0264555b2c20b3749465a226ee16c10fcf16dc89
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-11-27 15:28:58 -0600

    RW-1718 add functions for mapping cohort definition

--- ui/src/app/cohort-review/query-report/query-report.component.ts
+++ ui/src/app/cohort-review/query-report/query-report.component.ts
@@ -9,7 +9,7 @@ import {ActivatedRoute} from '@angular/router';
 export class QueryReportComponent implements OnInit {
   cohort: any;
   review: any;
-  definition: any;
+  definition: Array<any>;
 
   constructor(private route: ActivatedRoute) {}
 
@@ -17,7 +17,28 @@ export class QueryReportComponent implements OnInit {
     const {cohort, review} = this.route.snapshot.data;
     this.cohort = cohort;
     this.review = review;
-    this.definition = JSON.parse(cohort.criteria);
+    console.log(JSON.parse(cohort.criteria));
+    this.mapDefinition();
+  }
+
+  mapDefinition() {
+    this.definition = [];
+    const definition = JSON.parse(this.cohort.criteria);
+    ['includes', 'excludes'].forEach(role => {
+      if (definition[role].length) {
+        const roleObj = {role, groups: []};
+        definition[role].forEach(group => {
+          roleObj.groups.push(this.mapGroup(group));
+        });
+        this.definition.push(roleObj);
+      }
+    });
+  }
+
+  mapGroup(group: any) {
+    group.items.forEach(item => {
+
+    });
   }
 
 }
