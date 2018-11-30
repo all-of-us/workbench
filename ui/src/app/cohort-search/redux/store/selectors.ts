a40815c1fa065b72de5f0244ef8896d308da249c
dolbeew <40036095+dolbeew@users.noreply.github.com>
2018-11-30 11:12:20 -0600

    [RW-1760][risk=no] Error handling for cohort search (#1556)
    * RW-1760 delete total count when sending new request
    
    * RW-1760 add error handling for total count request
    
    * RW-1760 add error handling for search groups and items
    
    * RW-1760 fix broken tests
    
    * RW-1760 switch to popper tooltips
    
    * RW-1760 remove unused subscription

--- ui/src/app/cohort-search/redux/store/selectors.ts
+++ ui/src/app/cohort-search/redux/store/selectors.ts
@@ -35,6 +35,15 @@ export const getGroup = groupId => state =>
 export const getSearchRequest = searchRequestId => state =>
   state.getIn(['entities', 'searchRequests', searchRequestId], Map());
 
+export const searchRequestError = (state): boolean =>
+  state.getIn(['entities', 'searchRequests', SR_ID, 'error'], false);
+
+export const groupError = (groupId) => (state): boolean =>
+  state.getIn(['entities', 'groups', groupId, 'error'], false);
+
+export const itemError = (itemId) => (state): boolean =>
+  state.getIn(['entities', 'items', itemId, 'error'], false);
+
 export const countFor = (kind, id) => state =>
   state.getIn(['entities', kind, id, 'count'], null);
 
