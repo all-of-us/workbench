fa84ac854f12319afa88383748b4865beb49b9af
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-12-05 10:48:51 -0600

    addressing comments

--- public-ui/src/app/highlight-search/highlight-search.component.ts
+++ public-ui/src/app/highlight-search/highlight-search.component.ts
@@ -13,20 +13,19 @@ export class HighlightSearchComponent implements OnChanges {
   @Input() isStandard: string;
   @Input() indNum: number;
   words: string[];
-  reString: RegExp;
-  toBeHighlightedWords: string[];
+  matchString: RegExp;
   ngOnChanges() {
     if (this.searchTerm) {
       let searchWords = this.searchTerm.split(new RegExp(',| '));
       searchWords = searchWords.filter(w => w.length > 0 );
       searchWords = searchWords.map(word => word.replace(/[&!^\/\\#,+()$~%.'":*?<>{}]/g, ''));
-      this.reString = new RegExp(searchWords.join('|'));
+      this.matchString = new RegExp(searchWords.join('|'));
     }
     this.words = this.text.split(' ');
   }
   public highlight(word) {
-    const matches = word.match(new RegExp(this.reString, 'g'));
-    const splits = word.split(new RegExp(this.reString));
+    const matches = word.match(new RegExp(this.matchString, 'g'));
+    const splits = word.split(new RegExp(this.matchString));
     const temp = [];
     if (matches) {
       for (let i = 0; i < matches.length; i++) {
