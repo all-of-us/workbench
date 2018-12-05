fce488e41196ab8521a258cd4a16412e0ef7a3a6
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-12-05 17:38:37 -0600

    adding highlight test in ui

--- ui/src/app/highlight-search/highlight-search.component.spec.ts
+++ ui/src/app/highlight-search/highlight-search.component.spec.ts
@@ -5,21 +5,25 @@ import { HighlightSearchComponent } from './highlight-search.component';
 describe('HighlightSearchComponent', () => {
   let component: HighlightSearchComponent;
   let fixture: ComponentFixture<HighlightSearchComponent>;
-
   beforeEach(async(() => {
     TestBed.configureTestingModule({
-      declarations: [ HighlightSearchComponent ]
-    })
-    .compileComponents();
+        declarations: [ HighlightSearchComponent ]
+      })
+      .compileComponents();
   }));
-
   beforeEach(() => {
     fixture = TestBed.createComponent(HighlightSearchComponent);
     component = fixture.componentInstance;
     fixture.detectChanges();
   });
-
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('highlight_split_test', () => {
+    component = new HighlightSearchComponent();
+    component.matchString = new RegExp('lung|disorder');
+    const matchedWords = component.highlight('lung_enlargement_with_another_disorder');
+    expect(matchedWords).toContain('lung');
+    expect(matchedWords).toContain('disorder');
   });
 });
