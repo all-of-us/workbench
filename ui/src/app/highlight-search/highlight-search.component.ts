f260bf90758e2239aecebf92a6e9f3e77d34b6bb
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-12-05 13:45:53 -0600

    Fixing highlighting issue

--- ui/src/app/highlight-search/highlight-search.component.ts
+++ ui/src/app/highlight-search/highlight-search.component.ts
@@ -9,34 +9,27 @@ export class HighlightSearchComponent implements OnChanges {
   @Input() text: string;
   @Input() searchTerm: string;
   words: string[];
-  reString: RegExp;
+  matchString: RegExp;
   ngOnChanges() {
     if (this.searchTerm) {
       let searchWords = this.searchTerm.split(new RegExp(',| '));
       searchWords = searchWords.filter(w => w.length > 0 );
       searchWords = searchWords.map(word => word.replace(/[&!^\/\\#,+()$~%.'":*?<>{}]/g, ''));
-      this.reString = new RegExp(searchWords.join('|'));
+      this.matchString = new RegExp(searchWords.join('|'));
     }
     this.words = this.text.split(' ');
   }
-  public highlight(word: string) {
-    const splitWords = word.split(' ');
+  public highlight(word) {
+    const matches = word.match(new RegExp(this.matchString, 'g'));
+    const splits = word.split(new RegExp(this.matchString));
     const temp = [];
-    for (let i = 0; i < splitWords.length; i++) {
-      const m = splitWords[i].match(this.reString);
-      if (m) {
-        const splitMatchedWord = splitWords[i].split(m[0]);
-        for (let j = 0; j < splitMatchedWord.length; j++) {
-          if (j !== splitMatchedWord.length - 1 ) {
-            temp.push(splitMatchedWord[j]);
-            temp.push(m[0]);
-          } else {
-            temp.push(splitMatchedWord[j]);
-          }
-        }
-      } else {
-        temp.push(splitWords[i]);
+    if (matches) {
+      for (let i = 0; i < matches.length; i++) {
+        temp.push(splits[i], matches[i]);
       }
+      temp.push(splits[splits.length - 1]);
+    } else {
+      temp.push(word);
     }
     return temp;
   }
