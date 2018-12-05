b6a6b4cd49a25539fcdc6fcd386ff8f62ea1d6f1
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-12-05 17:15:33 -0600

    Testing the ut

--- public-ui/src/app/highlight-search/highlight-search.component.spec.ts
+++ public-ui/src/app/highlight-search/highlight-search.component.spec.ts
@@ -1,5 +1,7 @@
 import { async, ComponentFixture, TestBed } from '@angular/core/testing';
 
+import {DebugElement} from '@angular/core';
+import {By} from '@angular/platform-browser';
 import { HighlightSearchComponent } from './highlight-search.component';
 
 describe('HighlightSearchComponent', () => {
@@ -21,5 +23,12 @@ describe('HighlightSearchComponent', () => {
 
   it('should create', () => {
     expect(component).toBeTruthy();
+  });
+  it('highlight_split_test', () => {
+    component = new HighlightSearchComponent();
+    component.matchString = new RegExp('lung|disorder');
+    const matchedWords = component.highlight('lung_enlargement_with_another_disorder');
+    expect(matchedWords).toContain('lung');
+    expect(matchedWords).toContain('disorder');
   });
 });
