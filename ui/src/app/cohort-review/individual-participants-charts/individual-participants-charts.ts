be8dfb0f8c948687f23171def707d334df4b0b93
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-12-11 10:38:18 -0600

    grouping with same name

--- ui/src/app/cohort-review/individual-participants-charts/individual-participants-charts.ts
+++ ui/src/app/cohort-review/individual-participants-charts/individual-participants-charts.ts
@@ -36,6 +36,7 @@ export class IndividualParticipantsChartsComponent implements OnChanges {
        findName =>
          findName.name === items.standardName
        );
+      // console.log(duplicateFound)
       // duplicate items found return true otherwise push the the item in duplicateItems array
       if (duplicateFound) {
         Object.assign(items, {
