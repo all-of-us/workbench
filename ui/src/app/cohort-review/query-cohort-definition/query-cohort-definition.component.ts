e572abceee37dcb939acab32e78efc7a2d75c036
Mazumder <mazumds@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-12-13 14:47:57 -0600

    cleanup codes

--- ui/src/app/cohort-review/query-cohort-definition/query-cohort-definition.component.ts
+++ ui/src/app/cohort-review/query-cohort-definition/query-cohort-definition.component.ts
@@ -13,7 +13,6 @@ export class QueryCohortDefinitionComponent implements OnInit {
   definition: Array<any>;
   ppiParents: any;
   values: Array<any>;
-  // types = ['ICD9', 'ICD10', 'CPT', 'SNOMED' ];
   @Input() cohort: any;
   @Input() review: any;
   constructor(private api: CohortBuilderService) {}
@@ -36,17 +35,13 @@ export class QueryCohortDefinitionComponent implements OnInit {
           return roleObj;
         }
       });
-      console.log(this.definition);
+      // console.log(this.definition);
     });
   }
 
   mapGroup(group: any) {
     return group.items.map(item => {
       switch (item.type) {
-        case TreeType.PM:
-          return this.mapPMParams(item.searchParameters, item.type);
-        case TreeType.PPI:
-          return this.mapPPIParams(item.searchParameters, item.type);
         case TreeType.DRUG:
           return this.mapDrugParams(item.searchParameters);
         default:
@@ -55,8 +50,8 @@ export class QueryCohortDefinitionComponent implements OnInit {
     });
   }
 
-// Drug
 
+// Drug
   mapDrugParams(params: Array<any>) {
     const groupedData = this.getGroupedData(params, 'group');
     const drugArray = [];
@@ -72,34 +67,67 @@ export class QueryCohortDefinitionComponent implements OnInit {
      return this.removeDuplicates(drugArray);
   }
 
-  // Physical Measurement
 
-  mapPMParams(params: Array<any>, _type) {
-     this.getValues(params, _type);
-    const PMarray = params.map(param => {
+  // other than Drug
+  mapParams(_type: string, params: Array<any>, mod) {
+    const groupedData = this.getGroupedData(params, 'type');
+    if (mod.length) {
+      return this.getModifierFormattedData(groupedData,params, mod, _type);
+    } else {
+      return this.getOtherTreeFormattedData(groupedData,params, _type);
+    }
+  }
+
+  getModifierFormattedData(groupedData, params, mod, _type) {
+    const modArray =  params.map(eachParam => {
+      const typeMatched = groupedData.find( matched => matched.group === eachParam.type);
+      let name;
+      name = mod.reduce((acc, m) => {
+        const concatOperand = m.operands.reduce((final, o) => {
+          return final !== '' ? `${final} & ${o}` : `${final} ${o}`;
+        } , '');
+        return acc !== '' ?
+          `${acc} ,  ${this.operatorConversion(m.name)}
+            ${this.operatorConversion(m.operator)}
+            ${concatOperand}`
+          :
+          `${this.operatorConversion(m.name)} ${this.operatorConversion(m.operator)}
+            ${concatOperand}`;
+      }, '');
       return {
-        items: typeToTitle(param.type) + ' | ' + this.values,
-        type: param.type
+        items: `${typeToTitle(_type)} | ${eachParam.type} |
+          ${typeMatched.customString} | ${name}`,
+        type: eachParam.type
       };
     });
-    return this.removeDuplicates(PMarray);
+    return this.removeDuplicates(modArray);
+  }
+
+  getOtherTreeFormattedData(groupedData,params, _type) {
+
+    const noModArray = params.map(param => {
+      //  TODO to avoid undefined
+      const typeMatched = groupedData.find( matched => matched.group === param.type);
+      if (param.type === 'DEMO') {
+        return {items: `${typeToTitle(_type)}
+                      | ${param.type} | ${this.operatorConversion(param.subtype)} ${param.name}`,
+          type: param.type};
+      } else if (param.type === 'VISIT') {
+        return {items: `${typeToTitle(_type)} | ${typeMatched.customString}`,
+          type: param.type};
+      } else {
+        return _type === 'CONDITION' || _type === 'PROCEDURE' ?
+          {items: `${typeToTitle(_type)} | ${param.type} | ${typeMatched.customString}`,
+            type: param.type} :
+          {items: `${typeToTitle(_type)} | ${typeMatched.customString}`,
+            type: param.type};
+      }
+    });
+    return this.removeDuplicates(noModArray);
   }
 
 
-  // PPI
-  mapPPIParams(params: Array<any>, _type) {
-     this.getValues(params, _type);
-    const PpiArray = params.map(param => {
-      return {
-        items: typeToTitle(param.type)
-          + ' | ' + this.values,
-        type: param.type
-      };
-    });
-    return this.removeDuplicates(PpiArray);
-  }
-
-
+// to get PPI DATA
   async ppiCheck(definition: any) {
     const parents = {};
     for (const role in definition) {
@@ -131,94 +159,51 @@ export class QueryCohortDefinitionComponent implements OnInit {
     return name;
   }
 
-  // other than ppi and PM
-
-  mapParams(_type: string, params: Array<any>, mod) {
-    const groupedData = this.getGroupedData(params, 'type');
-    if (mod.length > 0) {
-      const modArray =  params.map(eachParam => {
-      //  TODO to avoid undefined
-      const typeMatched = groupedData.find( matched => matched.group === eachParam.type);
-        let name;
-        name = mod.reduce((acc, m) => {
-          const concatOperand = m.operands.reduce((final, o) => {
-            return final !== '' ? `${final} & ${o}` : `${final} ${o}`;
-          } , '');
-          return acc !== '' ?
-            `${acc} ,  ${this.operatorConversion(m.name)}
-            ${this.operatorConversion(m.operator)}
-            ${concatOperand}`
-            :
-            `${this.operatorConversion(m.name)} ${this.operatorConversion(m.operator)}
-            ${concatOperand}`;
-        }, '');
-        return {
-          items: `${typeToTitle(_type)} | ${eachParam.type} |
-          ${typeMatched.customString} | ${name}`,
-          type: eachParam.type
-        };
-      });
-      return this.removeDuplicates(modArray);
-    } else {
-      const noModArray = params.map(param => {
-        //  TODO to avoid undefined
-        const typeMatched = groupedData.find( matched => matched.group === param.type);
-        if (param.type === 'DEMO') {
-          return {items: `${typeToTitle(_type)}
-                      | ${param.type} | ${this.operatorConversion(param.subtype)} ${param.name}`,
-            type: param.type};
-        } else if (param.type === 'VISIT') {
-          return {items: `${typeToTitle(_type)} | ${param.name}`,
-            type: param.type};
-        } else {
-          return _type === 'CONDITION' || _type === 'PROCEDURE' ?
-            {items: `${typeToTitle(_type)} | ${param.type} | ${typeMatched.customString}`,
-            type: param.type} :
-            {items: `${typeToTitle(_type)} | ${typeMatched.customString}`,
-            type: param.type};
-        }
-      });
-      return this.removeDuplicates(noModArray);
-    }
-  }
 
 // utils
-
   getGroupedData(p, t) {
     const test = p.reduce((acc, i) => {
       const key = i[t];
-      // console.log(key);
       acc[key] = acc[key] || { data: []};
       acc[key].data.push(i);
       return acc;
     }, {});
-
     return Object.keys(test).map(k => {
       return Object.assign({}, {
         group: k,
         data: test[k].data,
         customString : test[k].data.reduce((acc, d) => {
-          //TODO for PM, PPI, Condition, Procedure
-          if(d.type === 'DRUG') {
-            if (d.group === false) {
-              return acc === '' ? `RXNORM | ${d.value}` : `${acc}, ${d.value}`;
-            } else {
-              return acc === '' ? `ATC | ${d.value}` : `${acc}, ${d.value}`;
-            }
-          } else {
-            if (d.group === false) {
-              return acc === '' ? d.value : `${acc}, ${d.value}`;
-            } else {
-              return acc === '' ? `Parent ${d.value}` : `${acc}, Parent ${d.value}`;
-            }
-          }
-
+         return this.getFormattedString(acc,d);
         }, '')
       });
     });
   }
 
-
+  getFormattedString(acc, d) {
+    if (d.type === 'DRUG') {
+      if (d.group === false) {
+        return acc === '' ? `RXNORM | ${d.value}` : `${acc}, ${d.value}`;
+      } else {
+        return acc === '' ? `ATC | ${d.value}` : `${acc}, ${d.value}`;
+      }
+    } else if (d.type === 'PM' || d.type === 'VISIT') {
+      return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
+    } else if (d.type === 'PPI'){
+      if (d.group === false) {
+        return acc === '' ? `${this.ppiParents[d.conceptId]} | ${d.name}` :
+          `${acc}, ${this.ppiParents[d.conceptId]} | ${d.name}`;
+      } else {
+        return acc === '' ? `Parent ${this.ppiParents[d.conceptId]} | ${d.name}` :
+          `${acc}, Parent ${this.ppiParents[d.conceptId]} | ${d.name}`;
+      }
+    } else {
+      if (d.group === false) {
+        return acc === '' ? d.value : `${acc}, ${d.value}`;
+      } else {
+        return acc === '' ? `Parent ${d.value}` : `${acc}, Parent ${d.value}`;
+      }
+    }
+  }
 
   removeDuplicates(arr) {
     return arr.filter((thing, index, self) =>
@@ -227,10 +212,6 @@ export class QueryCohortDefinitionComponent implements OnInit {
       ))
     );
   }
-
-  // removeUnderScoreLowerCase(name: string) {
-  //   return name.replace(/_/g, ' ').toLowerCase();
-  // }
 
   operatorConversion(operator) {
     switch (operator) {
@@ -248,42 +229,12 @@ export class QueryCohortDefinitionComponent implements OnInit {
         return 'Race';
       case 'AGE' :
         return 'Age';
+      case 'GEN' :
+        return 'Gender';
       case 'AGE_AT_EVENT' :
         return 'Age at Event';
       case 'NUM_OF_OCCURRENCES' :
         return 'Num of Occurrences';
     }
   }
-
-  getValues(p, type) {
-
-    this.values = p.map(m => {
-      if (m.group === false) {
-        if (type === 'PM') {
-          if (m.name) {
-            return m.name;
-          }
-        } else if (type === 'PPI') {
-          if (m.name) {
-            return  this.ppiParents[m.conceptId] + ' | ' + m.name ;
-          }
-        }
-      } else {
-        if (type === 'PM') {
-          if (m.name) {
-            return 'Parent' + m.name;
-          }
-        } else if (type === 'PPI') {
-          if (m.name) {
-            return  'Parent' + this.ppiParents[m.conceptId] + ' | '  + m.name ;
-          }
-        }
-      }
-
-    }).reduce((acc, v) => {
-      return acc !== '' ? ` ${acc}, ${v}  ` : `${acc} ${v} `;
-    }, '');
-  }
-
-//  TODO create search param mapping functions for each for each domain/type with different format
 }
