ab6c427e856c45d481c2bc1169ed48b5bd7b76a7
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-12-19 13:31:26 -0600

    RW-1853 add date validator function

--- ui/src/app/cohort-search/validation.ts
+++ ui/src/app/cohort-search/validation.ts
@@ -1,38 +1,37 @@
 import {AbstractControl, ValidatorFn} from '@angular/forms';
 import * as moment from 'moment';
 
-export function validRange(value: number, min: number, max: number, name?: string): string {
-  name = name || 'Form';
-  if (value < min || value > max) {
-    return name + ' cannot accept values outside range ' + min + ' - ' + max;
-  }
-  return null;
-}
-
-export function rangeValidator(min: number, max: number): ValidatorFn {
+export function rangeValidator(name: string, min: number, max: number): ValidatorFn {
   return (control: AbstractControl): {[key: string]: any} | null => {
-    console.log(control);
     const value = control.value;
     const isInt = Number.isInteger(parseFloat(value));
     const inRange = value >= min && value <= max;
     const message: any = {};
     if (!isInt) {
-      message.integer = {message : 'Value must be a whole number'};
+      message.integer = {
+        message : name + ' must be a whole number'
+      };
     } else if (!inRange) {
-      message.range = {message: 'Value must be between ' + min + ' and ' + max};
+      message.range = {
+        message: name + ' must be between ' + min + ' and ' + max
+      };
     }
     const isValid = control.pristine || (isInt && inRange);
     return isValid ? null : message;
   };
 }
 
-
-export function validInteger(value: string, name?: string): string {
-  name = name || 'Form';
-  if (!Number.isInteger(parseFloat(value))) {
-    return name + ' can only accept valid integers';
-  }
-  return null;
+export function dateValidator(): ValidatorFn {
+  return (control: AbstractControl): { [key: string]: any } | null => {
+    const value = control.value;
+    const isValid = control.pristine || moment(value, 'YYYY-MM-DD', true).isValid();
+    const message: any = {
+      dateFormat: {
+        message: 'Dates must be in the format \'YYYY-MM-DD\''
+      }
+    };
+    return isValid ? null : message;
+  };
 }
 
 export function validDateString(date: string, name?: string): string {
@@ -40,4 +39,9 @@ export function validDateString(date: string, name?: string): string {
     return 'Dates must be in the format \'YYYY-MM-DD\'';
   }
   return null;
+}
+
+function getControlName(control: AbstractControl) {
+  const formGroup = control.parent.controls;
+  return Object.keys(formGroup).find(name => control === formGroup[name]) || null;
 }
