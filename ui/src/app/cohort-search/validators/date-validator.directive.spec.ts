8519a62701266091c4fb32786cb4088eb8c5bb3c
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-12-19 15:19:15 -0600

    RW-1853 remove old validator directive

--- ui/src/app/cohort-search/validators/date-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { DateValidatorDirective } from './date-validator.directive';
-
-describe('DateValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new DateValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
