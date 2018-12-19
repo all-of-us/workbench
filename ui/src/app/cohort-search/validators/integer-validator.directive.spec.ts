d599104c249d50222b07313e53c4f95734409f77
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2018-12-19 12:44:57 -0600

    RW-1853 use custom validator functions instead of directives

--- ui/src/app/cohort-search/validators/integer-validator.directive.spec.ts
+++
@@ -1,8 +0,0 @@
-import { IntegerValidatorDirective } from './integer-validator.directive';
-
-describe('IntegerValidatorDirective', () => {
-  it('should create an instance', () => {
-    const directive = new IntegerValidatorDirective();
-    expect(directive).toBeTruthy();
-  });
-});
