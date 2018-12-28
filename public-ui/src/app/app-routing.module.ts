97bdeb1f20799959a3c2d5f8e483a32388190c25
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2018-12-28 14:26:08 -0600

    scrolling to top

--- public-ui/src/app/app-routing.module.ts
+++ public-ui/src/app/app-routing.module.ts
@@ -73,6 +73,7 @@ export class AppRoutingModule {
     this.router.events.subscribe(event => {
       if (event instanceof NavigationEnd) {
         gtag('config', environment.gaId, { 'page_path': event.urlAfterRedirects });
+        window.scrollTo(0, 0);
       }
     });
   }
