1d86010061ef2be106fb1cf327c02bbf2a4c48e6
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2019-01-02 12:06:07 -0500

    de-linting

--- ui/src/app/views/account-creation-modals/component.tsx
+++ ui/src/app/views/account-creation-modals/component.tsx
@@ -3,7 +3,7 @@ import {Component, DoCheck, Input, OnInit} from '@angular/core';
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 
-import {handleErrors, fullUrl} from 'app/utils/fetch';
+import {fullUrl, handleErrors} from 'app/utils/fetch';
 
 import {
   FetchArgs,
