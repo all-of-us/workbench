8cf5b9b6b8da38e430e0b2888a4d69ea1059015a
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-01-02 15:00:31 -0500

    a suite of tests

--- ui/src/app/views/quick-tour-modal/component.spec.tsx
+++ ui/src/app/views/quick-tour-modal/component.spec.tsx
@@ -6,9 +6,69 @@ import { shallow } from 'enzyme';
 
 describe('QuickTourModalComponent', () => {
 
-  it('should render', () => {
-    const component = shallow(<QuickTourReact learning={true} closeFunction={ () => {} }/>);
-    expect(component).toMatchSnapshot();
+  let props: {
+    learning: boolean,
+    closeFunction: Function
+  };
+
+  const component = () => {
+    return shallow(<QuickTourReact {...props}/>);
+  };
+
+  beforeEach(() => {
+    props = {
+      learning: true,
+      closeFunction: () => {}
+    }
+  });
+
+  it('should render, should have a next and close button', () => {
+    const wrapper = component();
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.exists('#close')).toBeTruthy();
+    expect(wrapper.exists('#next')).toBeTruthy();
+  });
+
+  it('should not show the previous button when we are on the first slide', () => {
+    const wrapper = component();
+    expect(wrapper.exists('#previous')).toBeFalsy();
+  });
+
+  it('should go to the next slide when we click next', () => {
+    const wrapper = component();
+    const firstSlide = wrapper.state().selected;
+    wrapper.find('#next').simulate('click');
+    expect(wrapper.state().selected).toBe(firstSlide + 1);
+  });
+
+  it('should go to the previous slide when we click previous', () => {
+    const wrapper = component();
+    const firstSlide = wrapper.state().selected;
+    wrapper.find('#next').simulate('click');
+    wrapper.find('#previous').simulate('click');
+    expect(wrapper.state().selected).toBe(firstSlide);
+  });
+
+  it('should not show the next button when we are on the last slide', () => {
+    const wrapper = component();
+    let i = 0;
+    do {
+      wrapper.find('#next').simulate('click');
+      i++;
+    }
+    while (i < 4);
+    expect(wrapper.exists('#close')).toBeFalsy();
+    expect(wrapper.find('#next').text()).toBe('Close');
+  });
+
+  it('should expand the image when we click the expand icon', () => {
+    const wrapper = component();
+    expect(wrapper.state().fullImage).toBeFalsy();
+    // TODO
+  });
+
+  it('should retract the image when we click the retract icon', () => {
+
   });
 
 });
