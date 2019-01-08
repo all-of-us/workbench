5de1d9d8b757a4a7422fd8ebf5cc5cdd6905be2f
Brett Heath-Wlaz <panentheos@gmail.com>
2019-01-08 14:57:18 -0500

    [risk=no] add react-modal (#1638)

--- ui/src/app/components/modals.tsx
+++ ui/src/app/components/modals.tsx
@@ -1,57 +1,47 @@
 import * as React from 'react';
-export const styles = {
-  modalMain: {
-    // tricky little bit required to get certain CSSProperties
-    // to work properly in this setup
-    position: 'fixed' as 'fixed',
-    background: 'white',
-    borderRadius: '8px',
-    width: '30%',
-    height: 'auto',
-    top: '50%',
-    left: '50%',
-    transform: 'translate(-50%,-50%)',
-    zIndex: 1050
+import ReactModal from 'react-modal';
+import {ReactComponent} from '../utils/index';
+
+const styles = {
+  modal: {
+    borderRadius: 8, position: 'relative',
+    padding: '1rem', margin: 'auto', outline: 'none',
+    backgroundColor: 'white', boxShadow: '0 1px 2px 2px rgba(0,0,0,.2)'
   },
 
-  modalBackdrop: {
-    position: 'fixed' as 'fixed',
-    top: 0,
-    bottom: 0,
-    right: 0,
-    left: 0,
-    backgroundColor: '#313131',
-    opacity: .85,
-    zIndex: 1040
+  overlay: {
+    backgroundColor: 'rgba(49, 49, 49, 0.85)', padding: '1rem', display: 'flex',
+    position: 'fixed', left: 0, right: 0, top: 0, bottom: 0, overflowY: 'auto'
   },
 
   modalTitle: {
-    marginTop: '4%',
-    marginLeft: '5%',
     fontSize: '20px',
-    color: '#302973'
+    color: '#302973',
+    marginBottom: '1rem'
   },
 
   modalBody: {
     fontSize: '14px',
     lineHeight: '.8rem',
-    marginLeft: '5%',
-    marginTop: '3%'
   },
 
   modalFooter: {
     display: 'flex' as 'flex',
     justifyContent: 'flex-end' as 'flex-end',
-    marginBottom: '.4rem',
-    marginRight: '.4rem',
-    marginTop: '.4rem'
+    marginTop: '1rem'
   }
 };
 
-export const Modal = ({style = {}, ...props}) => {
-  return <div><div style={{...styles.modalBackdrop}} />
-    <div {...props} style={{...styles.modalMain, ...style}} /></div>;
+export const Modal = ({width = 450, ...props}) => {
+  return <ReactModal
+    parentSelector={() => document.getElementById('popup-root')}
+    isOpen
+    style={{overlay: styles.overlay, content: {...styles.modal, width}}}
+    ariaHideApp={false}
+    {...props}
+  />;
 };
+
 export const ModalTitle = ({style = {}, ...props}) =>
   <div {...props} style={{...styles.modalTitle, ...style}} />;
 export const ModalBody = ({style = {}, ...props}) =>
