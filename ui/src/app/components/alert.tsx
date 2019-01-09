a30c2721d8ab7124f4ec0c4a660d1750c702f229
Neha Saxena <nsaxena@broadinstitute.org>
2019-01-09 10:29:09 -0500

    invitation key component

---
+++ ui/src/app/components/alert.tsx
@@ -0,0 +1,30 @@
+import * as React from 'react';
+
+export const styles = {
+  alert: {
+    fontSize: '.54167rem',
+    letterSpacing: 'normal',
+    lineHeight: '.75rem',
+    position: 'relative' as 'relative',
+    boxSizing: 'border-box' as 'border-box',
+    display: 'flex',
+    flexDirection: 'row' as 'row',
+    width: 'auto',
+    borderRadius: '.125rem',
+    marginTop: '.25rem',
+    background: '#e1f1f6',
+    color: '#565656',
+    border: '1px solid #49afd9'
+  },
+
+  danger: {
+    background: '#f5dbd9',
+    color: '#565656',
+    border: '1px solid #ebafa6'
+  }
+};
+
+export const Alert = ({style = {}, ...props}) =>
+    <div {...props} style={{...styles.alert, ...style}} />;
+export const AlertDanger = ({style = {}, ...props}) =>
+    <div {...props} style={{...styles.alert, ...styles.danger, ...style}}  />;
---
+++ ui/src/app/views/invitation-key/style.tsx
@@ -0,0 +1,14 @@
+import * as React from 'react';
+
+import {Button} from 'app/components/buttons';
+
+export const styles = {
+  button: {
+    width: '10rem',
+    height: '2rem',
+    margin: '.25rem .5rem .25rem 0'
+  }
+};
+
+export const NextButton = ({style = {}, ...props}) =>
+    <Button {...props} style={{...style, ...styles.button}}/>;
