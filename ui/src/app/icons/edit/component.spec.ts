2b4a92d17c493dfac6ce425af11eb2eac9ba037f
Greg Rushton <grushton@broadinstitute.org>
2019-01-09 16:13:13 -0500

    test experimentation

--- ui/src/app/icons/edit/component.spec.ts
+++ ui/src/app/icons/edit/component.spec.ts
@@ -1,23 +1,37 @@
 import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
-import {updateAndTick} from '../../../testing/test-helpers';
+import {By} from '@angular/platform-browser';
+import {
+  simulateEvent,
+  updateAndTick
+} from 'testing/test-helpers';
 import {EditComponent} from './component';
 
-describe('EditComponent', () => {
+// Note that the description is slightly different from the component for easier test filtering.
+describe('EditIconComponent', () => {
 
-    let fixture: ComponentFixture<EditComponent>;
+  let fixture: ComponentFixture<EditComponent>;
 
-    beforeEach(fakeAsync(() => {
-        TestBed.configureTestingModule({
-            declarations: [EditComponent]
-        }).compileComponents().then(() => {
-            fixture = TestBed.createComponent(EditComponent);
-            tick();
-        });
-    }));
+  beforeEach(fakeAsync(() => {
+    TestBed.configureTestingModule({
+      declarations: [EditComponent]
+    }).compileComponents().then(() => {
+      fixture = TestBed.createComponent(EditComponent);
+      tick();
+    });
+  }));
 
-    it('should render', fakeAsync(() => {
-        updateAndTick(fixture);
-        expect(fixture).toBeTruthy();
-    }));
+  it('should render', fakeAsync(() => {
+    updateAndTick(fixture);
+    expect(fixture).toBeTruthy();
+  }));
+
+  // TODO: Figure out how to get the style out of the hovered element.
+  it('should change style on hover', fakeAsync(() => {
+    simulateEvent(fixture, fixture.debugElement, 'onmouseover');
+    // let thing = fixture.debugElement.query(By.css('icon'));
+    // console.log(JSON.stringify(thing));
+    // console.log(fixture.debugElement.childNodes[0]);
+    expect(fixture).toBeTruthy();
+  }));
 
 });
