7cc88232a7e73e5375587df86a62303c724c0b5a
Greg Rushton <grushton@broadinstitute.org>
2019-01-10 10:03:36 -0500

    add mouse over tests

--- ui/src/app/icons/edit/component.spec.ts
+++ ui/src/app/icons/edit/component.spec.ts
@@ -1,15 +1,15 @@
 import {ComponentFixture, fakeAsync, TestBed, tick} from '@angular/core/testing';
-import {By} from '@angular/platform-browser';
 import {
-  simulateEvent,
   updateAndTick
 } from 'testing/test-helpers';
 import {EditComponent} from './component';
+import * as ReactTestUtils from 'react-dom/test-utils';
 
 // Note that the description is slightly different from the component for easier test filtering.
 describe('EditIconComponent', () => {
 
   let fixture: ComponentFixture<EditComponent>;
+  const styleAttribute = 'style';
 
   beforeEach(fakeAsync(() => {
     TestBed.configureTestingModule({
@@ -25,13 +25,31 @@ describe('EditIconComponent', () => {
     expect(fixture).toBeTruthy();
   }));
 
-  // TODO: Figure out how to get the style out of the hovered element.
-  it('should change style on hover', fakeAsync(() => {
-    simulateEvent(fixture, fixture.debugElement, 'onmouseover');
-    // let thing = fixture.debugElement.query(By.css('icon'));
-    // console.log(JSON.stringify(thing));
-    // console.log(fixture.debugElement.childNodes[0]);
-    expect(fixture).toBeTruthy();
+  it('should change style on mouse over', fakeAsync(() => {
+    updateAndTick(fixture);
+    const icon = fixture.debugElement.nativeElement.querySelector('svg');
+    const style = icon.getAttribute(styleAttribute);
+    ReactTestUtils.Simulate.mouseOver(icon);
+    updateAndTick(fixture);
+    const hoverStyle = icon.getAttribute(styleAttribute);
+
+    expect(style).not.toEqual(hoverStyle);
+    expect(hoverStyle).toContain('fill');
+  }));
+
+  it('should change style on mouse out', fakeAsync(() => {
+    updateAndTick(fixture);
+    const icon = fixture.debugElement.nativeElement.querySelector('svg');
+    ReactTestUtils.Simulate.mouseOver(icon);
+    updateAndTick(fixture);
+    const hoverStyle = icon.getAttribute(styleAttribute);
+
+    ReactTestUtils.Simulate.mouseLeave(icon);
+    updateAndTick(fixture);
+    const mouseOutStyle = icon.getAttribute(styleAttribute);
+
+    expect(mouseOutStyle).not.toEqual(hoverStyle);
+    expect(mouseOutStyle).not.toContain('fill');
   }));
 
 });
