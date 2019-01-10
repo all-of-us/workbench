d7d5f247616103a83f1da10fb35cbec3c8690208
CH Albach <calbach@users.noreply.github.com>
2019-01-10 08:58:09 -0800

    [RW-1897][risk=no] Switch resource rename modal to React (#1640)
    * Switch resource rename modal to React
    
    * PR feedback
    
    * Switch to AoT compatible approach
    
    * Integrate new ReactWrapperBase

--- ui/src/testing/test-helpers.ts
+++ ui/src/testing/test-helpers.ts
@@ -1,6 +1,16 @@
 import {DebugElement} from '@angular/core';
 import {ComponentFixture, tick} from '@angular/core/testing';
 
+import * as ReactTestUtils from 'react-dom/test-utils';
+
+
+/** Modal usage requires the existence of a global div. */
+export function setupModals<C>(fixture: ComponentFixture<C>) {
+  const popupRoot = document.createElement('div');
+  popupRoot.id = 'popup-root';
+  fixture.nativeElement.appendChild(popupRoot);
+}
+
 export function updateAndTick<C>(fixture: ComponentFixture<C>) {
   fixture.detectChanges();
   tick();
@@ -27,4 +37,24 @@ export function simulateClick<C>(
     fixture: ComponentFixture<C>,
     element: DebugElement) {
   simulateEvent(fixture, element, 'click');
+}
+
+export function simulateInputReact<C>(
+  fixture: ComponentFixture<C>,
+  selector: string,
+  text: string
+) {
+  const el = fixture.debugElement.nativeElement.querySelector(selector);
+  el.value = text;
+  ReactTestUtils.Simulate.change(el);
+  updateAndTick(fixture);
+}
+
+export function simulateClickReact<C>(
+  fixture: ComponentFixture<C>,
+  selector: string
+) {
+  const el = fixture.debugElement.nativeElement.querySelector(selector);
+  ReactTestUtils.Simulate.click(el);
+  updateAndTick(fixture);
 }
