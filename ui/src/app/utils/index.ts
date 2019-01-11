c2b885b6157da12558853308e0a5c1801a46a551
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-01-11 10:20:22 -0500

    [RW-1855][risk=no] CSS for quick tour (#1644)

--- ui/src/app/utils/index.ts
+++ ui/src/app/utils/index.ts
@@ -151,6 +151,35 @@ export function cookiesEnabled(): boolean {
     }
 }
 
+type ReactStyles<T> = {
+  readonly [P in keyof T]: React.CSSProperties;
+};
+
+/**
+ * Helper to assert the React.CSSProperties type for all properties in a tuple,
+ * while maintaining property names. This will fail compilation if input CSS
+ * properties are invalid and will avoid the need for a type assertion on the
+ * output. Also makes the properties readonly.
+ *
+ * This is a workaround to an issue in the Typescript compiler which should
+ * eventually be fixed: https://github.com/Microsoft/TypeScript/issues/11152.
+ *
+ * This approach works only with a single-level nested tuples currently:
+ * const styles = reactStyles({
+ *   style1: {color: 'red'},
+ *   style2: {color: 'blue', position: 'relative'}
+ * });
+ *
+ * Alternatively, style tuples can be cast individually (with arbitrary nesting):
+ * const styles = {
+ *   style1: {color: 'red'} as React.CssProperties,
+ *   style2: {color: 'blue', position: 'relative'} as React.CssProperties
+ * };
+ */
+export function reactStyles<T extends {[key: string]: React.CSSProperties}>(t: T): ReactStyles<T> {
+  return t;
+}
+
 /**
  * Helper base class for defining an Angular-wrapped React component. This is a
  * stop-gap for React migration.
