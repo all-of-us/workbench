f72c159a7624a2037ab94b14cf0a56e901cd0547
Greg Rushton <grushton@broadinstitute.org>
2019-01-14 12:58:27 -0500

    lint

--- ui/src/app/icons/edit/component.tsx
+++ ui/src/app/icons/edit/component.tsx
@@ -54,10 +54,10 @@ export class EditComponentReact extends React.Component<EditComponentProps, Edit
         onMouseOver={() => this.mouseOver()}
         onMouseLeave={() => this.mouseLeave()}>
       <title>Edit</title>
-        <path d="M4.22,23.2l-1.9,8.2a2.06,2.06,0,0,0,2,2.5,2.14,2.14,0,0,0,.43,0L13,32,28.84,16.22,
-                 20,7.4Z"/>
-        <path d="M33.82,8.32l-5.9-5.9a2.07,2.07,0,0,0-2.92,0L21.72,5.7l8.83,8.83,3.28-3.28A2.07,
-                 2.07,0,0,0,33.82,8.32Z"/>
+        <path d='M4.22,23.2l-1.9,8.2a2.06,2.06,0,0,0,2,2.5,2.14,2.14,0,0,0,.43,0L13,32,28.84,16.22,
+                 20,7.4Z'/>
+        <path d='M33.82,8.32l-5.9-5.9a2.07,2.07,0,0,0-2.92,0L21.72,5.7l8.83,8.83,3.28-3.28A2.07,
+                 2.07,0,0,0,33.82,8.32Z'/>
         <rect x='0' y='0'
               width={this.state.style['width']}
               height={this.state.style['height']}
