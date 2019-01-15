d66e5060162e022e588a5497bf67037dbbb27b79
Scout Rubenstein <brubenst@broadinstitute.org>
2019-01-15 10:43:44 -0500

    move to beforeAll and remove after

--- ui/src/app/views/settings/component.spec.tsx
+++ ui/src/app/views/settings/component.spec.tsx
@@ -13,11 +13,18 @@ describe('SettingsComponent', () => {
     (<SettingsReact {...props}/>);
   };
 
-  beforeEach(() => {
-    props = {};
+  beforeAll(() => {
     const popupRoot = document.createElement('div');
     popupRoot.setAttribute('id', 'popup-root');
     document.body.appendChild(popupRoot);
+  });
+
+  afterAll(() => {
+    document.removeChild(document.getElementById('popup-root'));
+  });
+
+  beforeEach(() => {
+    props = {};
   });
 
   it('should not open the cluster reset modal when no cluster', () => {
@@ -25,6 +32,26 @@ describe('SettingsComponent', () => {
     expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(0);
     wrapper.find('[data-test-id="reset-notebook-button"]').at(0).simulate('click');
     expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(0);
+  });
+
+  it('should open the cluster reset modal when there is cluster', () => {
+    const wrapper = component();
+    wrapper.setState({cluster: {clusterName: 'testing',
+      clusterNamespace: 'testing',
+      status: ClusterStatus.Running}});
+    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(0);
+    wrapper.find('[data-test-id="reset-notebook-button"]').at(0).simulate('click');
+    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(1);
+  });
+
+  it('should open the cluster reset modal when there is cluster', () => {
+    const wrapper = component();
+    wrapper.setState({cluster: {clusterName: 'testing',
+      clusterNamespace: 'testing',
+      status: ClusterStatus.Running}});
+    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(0);
+    wrapper.find('[data-test-id="reset-notebook-button"]').at(0).simulate('click');
+    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(1);
   });
 
   it('should open the cluster reset modal when there is cluster', () => {
