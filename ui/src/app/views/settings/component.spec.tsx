59a7ccafa9f920f319801a502222b193fc4f41df
Scout Rubenstein <brubenst@broadinstitute.org>
2019-01-15 15:50:28 -0500

    Remove extra tests...

--- ui/src/app/views/settings/component.spec.tsx
+++ ui/src/app/views/settings/component.spec.tsx
@@ -43,24 +43,4 @@ describe('SettingsComponent', () => {
     wrapper.find('[data-test-id="reset-notebook-button"]').at(0).simulate('click');
     expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(1);
   });
-
-  it('should open the cluster reset modal when there is cluster', () => {
-    const wrapper = component();
-    wrapper.setState({cluster: {clusterName: 'testing',
-      clusterNamespace: 'testing',
-      status: ClusterStatus.Running}});
-    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(0);
-    wrapper.find('[data-test-id="reset-notebook-button"]').at(0).simulate('click');
-    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(1);
-  });
-
-  it('should open the cluster reset modal when there is cluster', () => {
-    const wrapper = component();
-    wrapper.setState({cluster: {clusterName: 'testing',
-      clusterNamespace: 'testing',
-      status: ClusterStatus.Running}});
-    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(0);
-    wrapper.find('[data-test-id="reset-notebook-button"]').at(0).simulate('click');
-    expect(wrapper.find('Modal[data-test-id="reset-notebook-modal"]').length).toBe(1);
-  });
 });
