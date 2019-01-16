f223a824e9b276514f68a9cd6fbd1590a9379545
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2019-01-16 14:20:05 -0500

    alphabetical order!

--- ui/src/app/views/account-creation/component.tsx
+++ ui/src/app/views/account-creation/component.tsx
@@ -1,6 +1,7 @@
 import * as React from 'react';
-import * as fp from 'lodash';
 import * as ReactDOM from 'react-dom';
+
+import * as fp from 'lodash';
 
 import {fullUrl, handleErrors} from 'app/utils/fetch';
 
