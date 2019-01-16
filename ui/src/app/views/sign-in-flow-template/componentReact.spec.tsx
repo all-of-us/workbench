fbfa40ee23e65fe26f88d5f4a42e39c8d8107f9b
Neha Saxena <nsaxena@broadinstitute.org>
2019-01-15 21:13:22 -0500

    Registration flow test

---
+++ ui/src/app/views/sign-in-flow-template/componentReact.spec.tsx
@@ -0,0 +1,67 @@
+import {Button} from 'app/components/buttons';
+
+import {mount} from 'enzyme';
+import * as React from 'react';
+
+import RegistrationPageTemplateReact, {pageImages, PageTemplateProps} from './component';
+
+describe('RegistrationPageTemplateReact', () => {
+  let props: PageTemplateProps;
+
+  const signIn = jest.fn();
+
+  const component = () => {
+    return mount<RegistrationPageTemplateReact>
+    (<RegistrationPageTemplateReact {...props}/>);
+  };
+
+  beforeEach(() => {
+    props = {
+      signIn: signIn,
+      windowSize: {width: 1400, height: 0}
+    } as PageTemplateProps;
+  });
+
+  it('should display login background image and directive by default', () => {
+    const wrapper = component();
+    const templateImage = wrapper.find('div').find({id: 'imgTemplate'});
+    const backgroundImage = templateImage.prop('style').backgroundImage;
+    expect(backgroundImage).toBe('url(\'' + pageImages.login.backgroundImgSrc + '\')');
+    expect(wrapper.find('div').find({id: 'login'})).toBeTruthy();
+  });
+
+  it('should display small background image when window width is between 900 and 1300', () => {
+    props.windowSize.width = 999;
+    const wrapper = component();
+    const templateImage = wrapper.find('div').find({id: 'imgTemplate'});
+    const backgroundImage = templateImage.prop('style').backgroundImage;
+
+    expect(backgroundImage)
+        .toBe('url(\'' + pageImages.login.smallerBackgroundImgSrc + '\')');
+    expect(wrapper.find('div').find({id: 'invitation'})).toBeTruthy();
+  });
+
+  it('should display invitation key component on clicking Create account on login page ', () => {
+    const wrapper = component();
+    const createAccountButton = wrapper.find(Button).find({type: 'secondary'});
+    createAccountButton.simulate('click');
+    wrapper.update();
+    const templateImage = wrapper.find('div').find({id: 'imgTemplate'});
+    const backgroundImage = templateImage.prop('style').backgroundImage;
+
+    expect(backgroundImage)
+        .toBe('url(\'' + pageImages.invitationKey.backgroundImgSrc + '\')');
+    expect(wrapper.find('div').find({id: 'invitation'})).toBeTruthy();
+  });
+
+  it('should display invitation key with small image when width is between 900 and 1300 ', () => {
+    props.windowSize.width = 999;
+    const wrapper = component();
+    const createAccountButton = wrapper.find(Button).find({type: 'secondary'});
+    createAccountButton.simulate('click');
+    wrapper.update();
+    const templateImage = wrapper.find('div').find({id: 'imgTemplate'});
+    expect(templateImage.prop('style').backgroundImage)
+        .toBe('url(\'' + pageImages.invitationKey.smallerBackgroundImgSrc + '\')');
+  });
+});
