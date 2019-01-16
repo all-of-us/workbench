367d4ba1f08640a4cacb65ad276a62ee3fce6228
Isaac Zarsky <zarsky@broadinstitute.org>
2019-01-16 18:34:28 -0500

    indentation cleanup

--- ui/src/app/views/quick-tour-modal/component.tsx
+++ ui/src/app/views/quick-tour-modal/component.tsx
@@ -1,94 +1,94 @@
 import {Component, Input} from '@angular/core';
-import {reactStyles, ReactWrapperBase} from 'app/utils';
 
 import * as React from 'react';
 
 import {
   Button
 } from 'app/components/buttons';
+import {reactStyles, ReactWrapperBase} from 'app/utils';
 
 const OMOPTutorialsLink = 'https://www.ohdsi.org/past-events/2017-tutorials-' +
-    'omop-common-data-model-and-standardized-vocabularies/';
+  'omop-common-data-model-and-standardized-vocabularies/';
 const OMOPDataSetLink = 'https://www.ohdsi.org/data-standardization/the-common-data-model/';
 
 export const panels = [
-    {
-      title: 'Introduction',
-      shortTitle: 'Intro',
-      content: <div>Welcome to the All of Us Research Workbench!<br/><br/>All workbench analyses
-        happen in a “Workspace.” Within a Workspace you can select participants
-        using the “Cohort Builder” tool.  Another tool, the “Concept Set Builder,”
-        allows you to select data types for analysis.  The cohorts and concept sets
-        you make can then be accessed from “Notebooks,” the analysis environment. <br/><br/>
-        For illustration, let's consider research on 'Type 2 diabetes' for this quick tour.</div>,
-      image: '/assets/images/intro.png'
-    },
-    {
-      title: 'Workspaces',
-      shortTitle: 'Workspaces',
-      content: <div>A Workspace is your place to store and analyze data for a specific project.
-       You can share this Workspace with other users, allowing them to view or edit
-       your work. The dataset referenced by a workspace is in
-        {' '}<a className='link' href={OMOPDataSetLink} target='_blank'>
-          OMOP common data model
-        </a>{' '}
-          format. Here are some
-        {' '}<a className='link' href={OMOPTutorialsLink} target='_blank'>
-          tutorials
-        </a>{' '}
-        to understand OMOP data model.
-        <br/><br/>
-        When you create your Workspace, you will be prompted
-        to state your research purpose.  For example, when you create a Workspace to study Type
-        2 Diabetes, for research purpose you could enter: “I will use this Workspace to
-        investigate the impact of Geography on use of different medications to treat
-        Type 2Diabetes.”</div>,
-      image: '/assets/images/workspaces.png'
-    },
-    {
-      title: 'Cohorts',
-      shortTitle: 'Cohorts',
-      content: <div>A “Cohort” is a group of participants you are interested in researching.
-        The Cohort Builder allows you to create and review cohorts and annotate
-        participants in your study group.
-        <br/><br/>
-        For example, you can build a Cohort called “diabetes cases,” to include people
-        who have been diagnosed with type II diabetes, using a combination of billing codes and
-        laboratory values. You can also have a “controls” Cohort. Once you build your cohorts,
-        you can go through and manually review the records for each participant and decide if
-        you want to include or exclude them from your Cohort and make specific
-        annotations/notes to each record.</div>,
-      image: '/assets/images/cohorts.png'
-    },
-    {
-      title: 'Concepts',
-      shortTitle: 'Concepts',
-      content: <div>Concepts describe information in a patient’s medical record, such as a
-          condition they have, a  prescription they are taking or their physical measurements.
-          In the Workbench we refer to subject areas such as conditions, drugs, measurements
-          etc. as “domains.” You can search for and save collections of concepts from a
-          particular domain as a “Concept Set.”
-          <br/><br/>
-          For example, if you want to select height, weight and blood pressure information
-          (concepts) from your “diabetes cases” Cohort, you can search for the 3 concepts
-          from the “Measurements” domain and call it “biometrics” Concept Set. You can then
-          use Notebooks to extract that information from your cohort.</div>,
-      image: '/assets/images/concepts.png'
-    },
-    {
-      title: 'Notebooks',
-      shortTitle: 'Notebooks',
-      content: <div>A Notebook is a computational environment where you can analyze data with basic
-          programming knowledge in R or Python. Several template Notebooks and resources
-          are available within your Workspace that will guide you how to import your
-          Cohort(s) and Concept Set(s) into the Notebook and can assist with basic analyses.
-          <br/><br/>
-         For example, you can launch a Notebook
-          to import your “diabetes cases” Cohort and then select your “biometrics” Concept Set, to
-          get biometrics data for the participants in your Cohort. You can then analyze the data to
-          study correlation between hypertension and diabetes.</div>,
-      image: '/assets/images/notebooks.png'
-    }];
+  {
+    title: 'Introduction',
+    shortTitle: 'Intro',
+    content: <div>Welcome to the All of Us Research Workbench!<br/><br/>All workbench analyses
+      happen in a “Workspace.” Within a Workspace you can select participants
+      using the “Cohort Builder” tool. Another tool, the “Concept Set Builder,”
+      allows you to select data types for analysis. The cohorts and concept sets
+      you make can then be accessed from “Notebooks,” the analysis environment. <br/><br/>
+      For illustration, let's consider research on 'Type 2 diabetes' for this quick tour.</div>,
+    image: '/assets/images/intro.png'
+  },
+  {
+    title: 'Workspaces',
+    shortTitle: 'Workspaces',
+    content: <div>A Workspace is your place to store and analyze data for a specific project.
+      You can share this Workspace with other users, allowing them to view or edit
+      your work. The dataset referenced by a workspace is in
+      {' '}<a className='link' href={OMOPDataSetLink} target='_blank'>
+        OMOP common data model
+      </a>{' '}
+      format. Here are some
+      {' '}<a className='link' href={OMOPTutorialsLink} target='_blank'>
+        tutorials
+      </a>{' '}
+      to understand OMOP data model.
+      <br/><br/>
+      When you create your Workspace, you will be prompted
+      to state your research purpose. For example, when you create a Workspace to study Type
+      2 Diabetes, for research purpose you could enter: “I will use this Workspace to
+      investigate the impact of Geography on use of different medications to treat
+      Type 2Diabetes.”</div>,
+    image: '/assets/images/workspaces.png'
+  },
+  {
+    title: 'Cohorts',
+    shortTitle: 'Cohorts',
+    content: <div>A “Cohort” is a group of participants you are interested in researching.
+      The Cohort Builder allows you to create and review cohorts and annotate
+      participants in your study group.
+      <br/><br/>
+      For example, you can build a Cohort called “diabetes cases,” to include people
+      who have been diagnosed with type II diabetes, using a combination of billing codes and
+      laboratory values. You can also have a “controls” Cohort. Once you build your cohorts,
+      you can go through and manually review the records for each participant and decide if
+      you want to include or exclude them from your Cohort and make specific
+      annotations/notes to each record.</div>,
+    image: '/assets/images/cohorts.png'
+  },
+  {
+    title: 'Concepts',
+    shortTitle: 'Concepts',
+    content: <div>Concepts describe information in a patient’s medical record, such as a
+      condition they have, a prescription they are taking or their physical measurements.
+      In the Workbench we refer to subject areas such as conditions, drugs, measurements
+      etc. as “domains.” You can search for and save collections of concepts from a
+      particular domain as a “Concept Set.”
+      <br/><br/>
+      For example, if you want to select height, weight and blood pressure information
+      (concepts) from your “diabetes cases” Cohort, you can search for the 3 concepts
+      from the “Measurements” domain and call it “biometrics” Concept Set. You can then
+      use Notebooks to extract that information from your cohort.</div>,
+    image: '/assets/images/concepts.png'
+  },
+  {
+    title: 'Notebooks',
+    shortTitle: 'Notebooks',
+    content: <div>A Notebook is a computational environment where you can analyze data with basic
+      programming knowledge in R or Python. Several template Notebooks and resources
+      are available within your Workspace that will guide you how to import your
+      Cohort(s) and Concept Set(s) into the Notebook and can assist with basic analyses.
+      <br/><br/>
+      For example, you can launch a Notebook
+      to import your “diabetes cases” Cohort and then select your “biometrics” Concept Set, to
+      get biometrics data for the participants in your Cohort. You can then analyze the data to
+      study correlation between hypertension and diabetes.</div>,
+    image: '/assets/images/notebooks.png'
+  }];
 
 const styles = reactStyles({
   modalBackdrop: {
