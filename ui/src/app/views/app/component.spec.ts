dd51a7dea1a06a7b841dd03620f501e84c0f41b4
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2019-01-16 20:57:07 -0500

    fix for tests

--- ui/src/app/views/app/component.spec.ts
+++ ui/src/app/views/app/component.spec.ts
@@ -6,9 +6,9 @@ import {RouterTestingModule} from '@angular/router/testing';
 import {ClarityModule} from '@clr/angular';
 
 import {SignInService} from '../../services/sign-in.service';
-import {AccountCreationComponent} from '../account-creation/component';
+import {AccountCreation} from '../account-creation/component';
 import {AppComponent} from '../app/component';
-import {InvitationKeyComponent} from '../invitation-key/component';
+import {InvitationKey} from '../invitation-key/component';
 import {PageTemplateSignedOutComponent} from '../page-template-signed-out/component';
 
 import {ProfileServiceStub} from 'testing/stubs/profile-service-stub';
@@ -25,9 +25,9 @@ describe('AppComponent', () => {
         ClarityModule.forRoot()
       ],
       declarations: [
-        AccountCreationComponent,
+        AccountCreation,
         AppComponent,
-        InvitationKeyComponent,
+        InvitationKey,
         PageTemplateSignedOutComponent
       ],
       schemas: [CUSTOM_ELEMENTS_SCHEMA],
