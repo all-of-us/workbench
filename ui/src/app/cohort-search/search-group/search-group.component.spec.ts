5c3fe56e27949e5a8819b4c2d9e932cb3d4b01ad
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-01-17 15:33:50 -0600

    RW-1385 fix lint errors

--- ui/src/app/cohort-search/search-group/search-group.component.spec.ts
+++ ui/src/app/cohort-search/search-group/search-group.component.spec.ts
@@ -132,8 +132,8 @@ describe('SearchGroupComponent', () => {
     const removeButton = fixture.debugElement
       .query(By.css('button[clrdropdownitem]:nth-of-type(2)'));
     removeButton.triggerEventHandler('click', null);
-    jasmine.clock().tick(3000)
-    fixture.detectChanges()
+    jasmine.clock().tick(3000);
+    fixture.detectChanges();
     expect(spy).toHaveBeenCalledWith({
       type: REMOVE_GROUP,
       role: 'includes',
