921688fda897e1c9072633f7ab7631f1be73bf70
Ursa Stutsman <ursas@broadinstitute.org>
2019-01-17 18:14:11 -0500

    [RW-1828][Risk=low] Account Creation Flow/Sign In template React conversion/refactor (#1675)
    * registration flow I
    
    * removing ref invitation key
    
    * tying it all together
    
    * de-linting
    
    * fix for tests
    
    * fixing more tests
    
    * getting branches in sync
    
    * trying to fix tests
    
    * forgot to take this out after debugging
    
    * PR comments
    
    * function component
    
    * PR comments
    
    * PR comments
    
    * whitespace errors
    
    * using wrapper react style
    
    * error

---
+++ ui/src/app/views/invitation-key/component.tsx
@@ -0,0 +1,115 @@
+import {fullUrl} from '../../utils/fetch';
+
+import {AlertDanger} from 'app/components/alert';
+import {Button} from 'app/components/buttons';
+import {BoldHeader} from 'app/components/headers';
+import {FormInput} from 'app/components/inputs';
+
+import {
+  FetchArgs,
+  InvitationVerificationRequest,
+  ProfileApiFetchParamCreator
+} from 'generated/fetch/api';
+
+import * as React from 'react';
+
+function isBlank(s: string) {
+  return (!s || /^\s*$/.test(s));
+}
+
+interface InvitationKeyProps {
+  onInvitationKeyVerify: (invitationKey: any) => void;
+}
+
+interface InvitationKeyState {
+  invitationKey: string;
+  invitationKeyReq: boolean;
+  invitationKeyInvalid: boolean;
+}
+
+export class InvitationKey extends React.Component<InvitationKeyProps, InvitationKeyState> {
+
+  private inputElement = React.createRef<HTMLInputElement>();
+
+
+  constructor(props: InvitationKeyProps) {
+    super(props);
+    this.state = {
+      invitationKey: '',
+      invitationKeyReq: false,
+      invitationKeyInvalid: false
+    };
+  }
+
+  next() {
+    this.setState({
+      invitationKeyReq: false,
+      invitationKeyInvalid: false
+    });
+    const input = this.inputElement.current;
+
+    if (isBlank(this.state.invitationKey)) {
+      this.setState({
+        invitationKeyReq: true
+      });
+      if (input) {
+        input.focus();
+      }
+      return;
+    }
+
+    const request: InvitationVerificationRequest = {
+      invitationKey: this.state.invitationKey
+    };
+
+    const args: FetchArgs = ProfileApiFetchParamCreator().invitationKeyVerification(request);
+    fetch(fullUrl(args.url), args.options)
+      .then(response => {
+        if (response.status === 400 ) {
+          this.setState({
+            invitationKeyInvalid: true
+          });
+          if (input) {
+            input.focus();
+          }
+        } else {
+          this.props.onInvitationKeyVerify(this.state.invitationKey);
+          return;
+        }})
+      .catch(error => console.log(error));
+  }
+
+  render() {
+    return <div style={{padding: '3rem 3rem 0 3rem'}}>
+      <div style={{marginTop: '0', paddingTop: '.5rem'}}>
+        <BoldHeader>
+          Enter your Invitation Key:
+        </BoldHeader>
+        <FormInput type='text' id='invitationKey' value={this.state.invitationKey}
+                   placeholder='Invitation Key' onChange={input =>
+                                                 this.setState({invitationKey: input.target.value})}
+                   inputref={this.inputElement} autoFocus/>
+        {this.state.invitationKeyReq &&
+         <AlertDanger>
+           <div style={{fontWeight: 'bolder'}}> Invitation Key is required.</div>
+         </AlertDanger>
+        }
+        {this.state.invitationKeyInvalid &&
+         <AlertDanger>
+           <div style={{fontWeight: 'bolder'}}>
+             Invitation Key is not Valid.
+           </div>
+         </AlertDanger>
+        }
+        <div>
+          <Button style={{width: '10rem', height: '2rem', margin: '.25rem .5rem .25rem 0'}}
+                  onClick={() => this.next()}>
+            Next
+          </Button>
+        </div>
+      </div>
+    </div>;
+  }
+}
+
+export default InvitationKey;
---
+++ ui/src/app/views/login/style.tsx
@@ -0,0 +1,26 @@
+import * as React from 'react';
+
+export const styles = {
+  sign: {
+    backgroundSize: 'contain',
+    backgroundRepeat: 'noRepeat',
+    backgroundPosition: 'center',
+    display: 'flex',
+    justifyContent: 'spaceAround',
+    alignItems: 'flexStart',
+    width: 'auto'
+  },
+  button: {
+    marginTop: '0.5rem',
+    display: 'flex',
+    alignItems: 'center',
+    height: 'auto',
+    paddingLeft: '0',
+    fontSize: '18px',
+    fontStyle: 'normal',
+    textTransform: 'none',
+    borderRadius: '2px',
+    justifyContent: 'baseline',
+    maxWidth: '11.45rem'
+  }
+};
