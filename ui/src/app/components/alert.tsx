3c6812fc86b98c28fbc9e66ee616e990839543d1
Neha Saxena <nsaxena@broadinstitute.org>
2019-01-17 17:59:36 -0500

    using wrapper react style

--- ui/src/app/components/alert.tsx
+++ ui/src/app/components/alert.tsx
@@ -1,14 +1,15 @@
 import * as React from 'react';
+import {reactStyles} from "app/utils";
 
-export const styles = {
+export const styles = reactStyles({
   alert: {
     fontSize: '.54167rem',
     letterSpacing: 'normal',
     lineHeight: '.75rem',
-    position: 'relative' as 'relative',
-    boxSizing: 'border-box' as 'border-box',
+    position: 'relative',
+    boxSizing: 'border-box',
     display: 'flex',
-    flexDirection: 'row' as 'row',
+    flexDirection: 'row',
     width: 'auto',
     borderRadius: '.125rem',
     marginTop: '.25rem',
@@ -22,7 +23,7 @@ export const styles = {
     color: '#565656',
     border: '1px solid #ebafa6'
   }
-};
+});
 
 export const Alert = ({style = {}, ...props}) =>
     <div {...props} style={{...styles.alert, ...style}} />;
