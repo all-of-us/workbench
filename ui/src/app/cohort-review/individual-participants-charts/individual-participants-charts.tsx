f7032170500070f13acf6989a1ffb6110ee79ad0
smazumd <42041705+smazumd@users.noreply.github.com>
2019-01-18 10:06:20 -0600

    [RW-1810] [risk = no] CB - change high charts from "angular2-highcharts": "^0.5.5" to "highcharts-react" -- Angular to React Conversion (#1651)
    * added highcharts-react
    
    * commented all chart data calculation
    
    * changes with react
    
    * added spec file
    
    * lint fixes
    
    * fix imports
    
    * fix imports
    
    * cleanup
    
    * turbotherosl value change
    
    * [RW-1939][risk=no] Do not delete workspaces when keeping FC/AoU in sync (#1653)
    
    * DB-190 [Wildcard* search causing issue on source vocabulary search] (#1649)
    
    * DB-190 fix
    
    * DB-190 fix
    
    * fixing test
    
    * addressing comments
    
    * addressing comments
    
    * addressing comments
    
    * using the enum in common api
    
    * adding comment in the code
    
    * changing enum
    
    * fxing nit
    
    * add react-interactive (#1650)
    
    * using resizeobserver
    
    * removed unused flag
    
    * lint fixes
    
    * lint fixes
    
    * cleanup
    
    * deleted html file
    
    * [risk=no][RW-1950] Fix quick tour image expand/shrink; use base wrapper (#1656)
    
    * fix quick tour image expand/shrink; use react-to-angular wrapper
    
    * cleanup
    
    * Delete unnecessary html file
    
    * cleaner merge
    
    * fix close function to match wrapper syntax
    
    * [RW-1896][risk=no] Confirm delete modal in react (#1652)
    
    * Update README.md with a note on Docker memory
    
    Most dev team members seem to allocate ~6GiB to Docker. This should be documented as part of the setup process because it's painful to see things lag & crash with the default 1GiB on Mac.
    
    Anecdotally, increasing memory was necessary for me to successfully run the './project.rb dev-up' command in the workbench/ui folder.
    
    * [risk=no] Split out karma/jest tsconfigs - only compile one or the other (#1664)
    
    * [RW-1972][risk=low] Add a fetch module (#1662)
    
    * [RW-1971][P1][risk=no] Databrowser hotfix and deploy --environment flag (#1668)
    
    * clean up account creation success modals (#1659)
    
    * Move to onChanges (#1672)
    
    * [No Ticket][Risk=no] Code cleanup (#1678)
    
    * [No Ticket][Risk=no] Code cleanup
    
    * Clean up to data-test-id and imports
    
    * [risk=no] Ensure we fetch all tags for deployment (#1671)
    
    * [RW-1898][risk=no] Edit Modal React (#1669)
    
    * use data-test-id in edit modal tests, correctly name spec (#1680)
    
    * [RW-1900][risk=no] Convert edit icon to react (#1643)
    
    * migrate edit icon to react
    
    * [RW-1972][risk=low] ts fetch swagger API clients attempt 2 (#1676)
    
    * convert resource card menu to React (#1657)
    
    * [RW-1828][Risk=low] Account Creation Flow/Sign In template React conversion/refactor (#1675)
    
    * registration flow I
    
    * removing ref invitation key
    
    * tying it all together
    
    * de-linting
    
    * fix for tests
    
    * fixing more tests
    
    * getting branches in sync
    
    * trying to fix tests
    
    * forgot to take this out after debugging
    
    * PR comments
    
    * function component
    
    * PR comments
    
    * PR comments
    
    * whitespace errors
    
    * using wrapper react style
    
    * error
    
    * [RW-1829][RISK=LOW] Replace fetch logic for invitation key verification (#1683)
    
    * replacing api calls to utility fetch
    
    * PR Comments
    
    * added highcharts-react
    
    * commented all chart data calculation
    
    * changes with react
    
    * added spec file
    
    * lint fixes
    
    * fix imports
    
    * fix imports
    
    * cleanup
    
    * turbotherosl value change
    
    * using resizeobserver
    
    * removed unused flag
    
    * lint fixes
    
    * lint fixes
    
    * cleanup
    
    * deleted html file

---
+++ ui/src/app/cohort-review/individual-participants-charts/individual-participants-charts.tsx
@@ -0,0 +1,238 @@
+import {Component, DoCheck, Input, OnChanges, OnInit, ViewChild} from '@angular/core';
+import {ParticipantChartData} from 'generated';
+import * as highCharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+import * as moment from 'moment';
+import * as React from 'react';
+import * as ReactDOM from 'react-dom';
+
+interface DisplayParticipantChartData extends ParticipantChartData {
+  yAxisValue?: number;
+  newStartDate?: number;
+}
+
+interface ChartData {
+  loading: boolean;
+  conditionTitle: string;
+  items: Array<DisplayParticipantChartData>;
+}
+
+export interface ChartReactProps {
+  chartData: ChartData;
+  chartKey: number;
+}
+
+export class IndividualParticipantsReactCharts extends React.Component<ChartReactProps> {
+  props: ChartReactProps;
+  chartOptions = {};
+  trimmedData = [];
+  duplicateItems = [];
+  yAxisNames = [''];
+  chart: any;
+
+  setYaxisValue() {
+    let yAxisValue = 1;
+    this.props.chartData.items.reverse().map(items => { // find standardName in duplicate items
+      const duplicateFound = this.duplicateItems.find(
+        findName =>
+          findName.name === items.standardName
+      );
+
+      // duplicate items found return true otherwise push the the item in duplicateItems array
+      if (duplicateFound) {
+        Object.assign(items, {
+          yAxisValue: duplicateFound.yAxisValue,
+          // format date to unix timestamp
+          newStartDate: moment(items.startDate, 'YYYY-MM-DD').unix()
+        });
+        return true;
+      }
+      this.duplicateItems.push({name: items.standardName, yAxisValue});
+      Object.assign(items, {
+        yAxisValue,
+        newStartDate: moment(items.startDate, 'YYYY-MM-DD').unix() // format date to unix timestamp
+      });
+      yAxisValue ++;
+    });
+
+    this.props.chartData.items.map(i => {
+      const temp = {
+        x: i.newStartDate,
+        y: i.yAxisValue,
+        ageAtEvent: i.ageAtEvent,
+        standardName: i.standardName,
+        rank: i.rank,
+        startDate: moment.unix(i.newStartDate).format('MM-DD-YYYY'),
+        standardVocabulary: i.standardVocabulary,
+      };
+      this.trimmedData.push(temp);
+      this.trimmedData.reverse();
+    });
+    this.duplicateItems.map(d => {
+      this.yAxisNames.push(d.name.substring(0, 13));
+    });
+
+    if (this.trimmedData.length) {
+       this.getChartsData();
+    }
+  }
+
+  getChartsData() {
+    const names = this.yAxisNames;
+    const header = this.props.chartData.conditionTitle;
+
+    this.chartOptions = {
+      chart: {
+        type: 'scatter',
+        zoomType: 'xy',
+      },
+      credits: {
+        enabled: false
+      },
+      title: {
+        text: 'Top' + ' ' + header + ' ' + 'over Time',
+      },
+      xAxis: {
+        title: {
+          enabled: true,
+          text: 'Entry Date',
+        },
+        labels: {
+          formatter: function () {
+            return moment.unix(this.value).format('YYYY');
+          },
+        },
+        startOnTick: true,
+        endOnTick: true,
+        tickInterval: 40 * 3600 * 1000,
+      },
+      yAxis: [{
+        title: {
+          enabled: true,
+          text: header,
+        },
+        labels: {
+          formatter: function () {
+            return names[this.value];
+          }
+        },
+        tickInterval: 1,
+        lineWidth: 1,
+      },
+        {
+          title: {
+            enabled: false,
+          },
+          opposite: true,
+          lineWidth: 1,
+        }],
+      plotOptions: {
+        scatter: {
+          marker: {
+            radius: 5,
+            states: {
+              hover: {
+                enabled: true,
+                lineColor: 'rgb(100,100,100)'
+              }
+            }
+          },
+          states: {
+            hover: {
+              marker: {
+                enabled: false
+              }
+            }
+          },
+        }
+      },
+      tooltip: {
+
+        pointFormat: '<div>' +
+          '<b>Date:</b>{point.startDate}<br/>' +
+          '<b>Standard Vocab:</b>{point.standardVocabulary}<br/>' +
+          '<b>Standard Name:</b>{point.standardName}<br/>' +
+          '<b>Age at Event:</b>{point.ageAtEvent}<br/>' +
+          '</div>',
+        style: {
+          color: '#565656',
+          fontSize: 12
+        },
+        shared: true
+      },
+      series: [{
+        type: 'scatter',
+        name: 'Details',
+        data: this.trimmedData,
+        turboThreshold: 5000,
+        showInLegend: false,
+      }],
+    };
+  }
+
+  getChartObj(chartObj: any) {
+    this.chart = chartObj;
+    const chartRef = this.chart.container.parentElement;
+    if (this.chart && typeof ResizeObserver === 'function') {
+      // Unbind window.onresize handler so we don't do double redraws
+      if (this.chart.unbindReflow) {
+        this.chart.unbindReflow();
+      }
+      // create observer to redraw charts on div resize
+      const ro = new ResizeObserver(() => {
+        if (this.chart) {
+          this.chart.reflow();
+        }
+      });
+      ro.observe(chartRef);
+    }
+  }
+
+  render() {
+     const chartData = this.props.chartData;
+    if (!chartData.loading && chartData) {
+      this.setYaxisValue();
+      const ScatterChart = () => <div>
+        <HighchartsReact
+          highcharts={highCharts}
+          options={this.chartOptions}
+          callback={this.getChartObj}
+        />
+      </div>;
+      return <ScatterChart/>;
+    }
+    return <div/>;
+  }
+}
+
+@Component({
+  selector: 'app-individual-participants-charts',
+  template: `<div class="react-chart chart-container"></div>`,
+  styleUrls: ['./individual-participants-charts.css']
+})
+export class IndividualParticipantsChartsComponent implements OnChanges, OnInit {
+  @Input() chartData: ChartData;
+  @Input() chartKey = 0;
+  @Input() shouldReRender: boolean;
+  componentClass = 'react-chart';
+  constructor() {}
+
+  ngOnChanges (): void {
+    this.triggerReRender();
+  }
+
+  ngOnInit (): void {
+    this.triggerReRender();
+  }
+
+  triggerReRender(): void {
+    ReactDOM.render(React.createElement(IndividualParticipantsReactCharts,
+      {chartData: this.chartData,
+        chartKey: this.chartKey,
+      }),
+      document.getElementsByClassName(this.componentClass)[this.chartKey]);
+  }
+
+}
+
+
