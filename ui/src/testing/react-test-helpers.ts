7cffd69275a4acbbee70fbd962a05a18e03d8f02
Scout Rubenstein <brubenst@broadinstitute.org>
2019-01-18 13:01:54 -0500

    Add more comments on failure

--- ui/src/testing/react-test-helpers.ts
+++ ui/src/testing/react-test-helpers.ts
@@ -1,8 +1,11 @@
 import {ReactWrapper} from 'enzyme';
 
-
 // This file is necessary because angular imports complain if there
 // is no zone, regardless of whether the imports are used.
+// The error is from:
+//   import {ComponentFixture, tick} from '@angular/core/testing';
+// And fails with:
+//   ReferenceError: Zone is not defined
 export async function completeApiCall(wrapper: ReactWrapper) {
   await new Promise(setImmediate).then(() => wrapper.update());
 }
