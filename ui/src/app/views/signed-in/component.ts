75d63b64fa56f1663a22329abd35c0ea9fbf3478
Gregory Jordan <gjordan@verily.com>
2019-01-22 14:06:02 -0500

    Tiny Zendesk cleanup: return early and reduce nesting.

--- ui/src/app/views/signed-in/component.ts
+++ ui/src/app/views/signed-in/component.ts
@@ -188,42 +188,43 @@ export class SignedInComponent implements OnInit, OnDestroy {
     // this property is dynamically loaded by the Zendesk web widget snippet,
     // and can't properly be typed. If for some reason the support widget is
     // unavailable, we'll show a notice to the user.
-    if (window['zE'] != null) {
-      // In theory, this webWidget call should identify the user who is filing
-      // this support request.
-      //
-      // In practice, the values provided via 'identify' don't seem to do much.
-      // Zendesk uses values from the 'prefill' action to assign the user email
-      // in the created ticket, which means that for AoU these tickets won't be
-      // correctly associated with the researcher's AoU Google Account. See
-      // the Zendesk integration doc for more discussion.
-      window['zE']('webWidget', 'identify', {
-        name: `${this.givenName} ${this.familyName}`,
-        email: this.aouAccountEmailAddress,
-      });
-
-      window['zE']('webWidget', 'prefill', {
-        name: {
-          value: `${this.givenName} ${this.familyName}`,
-          readOnly: true,
-        },
-        // For the contact email we use the user's *contact* email address,
-        // since the AoU email address is not a valid email inbox.
-        email: {
-          value: this.contactEmailAddress,
-          readOnly: true,
-        },
-      });
-
-      // Trigger the widget to open the full contact form (instead of the
-      // help icon, which is the starting state).
-      window['zE']('webWidget', 'open');
-    } else {
+    if (window['zE'] == null) {
       // Show an error message to the user, asking them to reload or contact
       // support via email.
       this.zendeskLoadError = true;
       console.error('Error loading Zendesk widget');
+      return;
     }
+
+    // In theory, this webWidget call should identify the user who is filing
+    // this support request.
+    //
+    // In practice, the values provided via 'identify' don't seem to do much.
+    // Zendesk uses values from the 'prefill' action to assign the user email
+    // in the created ticket, which means that for AoU these tickets won't be
+    // correctly associated with the researcher's AoU Google Account. See
+    // the Zendesk integration doc for more discussion.
+    window['zE']('webWidget', 'identify', {
+      name: `${this.givenName} ${this.familyName}`,
+      email: this.aouAccountEmailAddress,
+    });
+
+    window['zE']('webWidget', 'prefill', {
+      name: {
+        value: `${this.givenName} ${this.familyName}`,
+        readOnly: true,
+      },
+      // For the contact email we use the user's *contact* email address,
+      // since the AoU email address is not a valid email inbox.
+      email: {
+        value: this.contactEmailAddress,
+        readOnly: true,
+      },
+    });
+
+    // Trigger the widget to open the full contact form (instead of the
+    // help icon, which is the starting state).
+    window['zE']('webWidget', 'open');
   }
 
   openHubForum(): void {
