4825bad5451c26ba3c3424ee6e879dc413b02b84
Greg Jordan <gjordan@verily.com>
2019-01-22 16:33:07 -0500

    [RW-1885][risk=low] Add Zendesk web widget and help forum redirect (#1688)
    * Add an Environment interface and Zendesk-specific env flags.
    
    While adding feature flags and env variables for our Zendesk integration, I took the chance to clean up the env settings a bit:
    
    1) Created an Environment type, clearly documenting each environmen variable and its use.
    2) Created a slight separation between long-standing environment flags (which track different service endpoints and client configs for local/test/staging/etc), and transient feature flags (which help control the gradual rollout of a feature over time).
    
    * Integrate the Zendesk web widget and add left-hand nav entries.
    
    * Fix lint and typos.
    
    * Add a clr-alert shown when the Zendesk widget is unavailable.
    
    * Add console.error statement when Zendesk fails to load.
    
    * Tiny Zendesk cleanup: return early and reduce nesting.
    
    * Remove trailing slash on Zendesk HC URLs.
    
    * Remove empty comments (and add a real one) to the Zendesk web widget Javascript.

--- ui/src/app/views/signed-in/component.ts
+++ ui/src/app/views/signed-in/component.ts
@@ -27,13 +27,25 @@ export class SignedInComponent implements OnInit, OnDestroy {
   hasDataAccess = true;
   hasReviewResearchPurpose = false;
   hasReviewIdVerification = false;
+  showBugReportItem = !environment.useZendeskForSupport;
+  showHelpDeskItem = environment.useZendeskForSupport;
+  showHubForumItem = environment.useZendeskForSupport;
   headerImg = '/assets/images/all-of-us-logo.svg';
   givenName = '';
   familyName = '';
+  // The email address of the AoU researcher's Google Account. Note that this
+  // address should *not* be used for contact purposes, since the AoU GSuite
+  // doesn't provide Gmail!
+  aouAccountEmailAddress = '';
+  // The researcher's preferred contact email address.
+  contactEmailAddress = '';
   profileImage = '';
   sidenavToggle = false;
   publicUiUrl = environment.publicUiUrl;
   minimizeChrome = false;
+  // True if the user tried to open the Zendesk support widget and an error
+  // occurred.
+  zendeskLoadError = false;
   billingProjectInitialized = false;
   billingProjectQuery: NodeJS.Timer;
   private profileLoadingSub: Subscription;
@@ -80,6 +92,8 @@ export class SignedInComponent implements OnInit, OnDestroy {
           profile.authorities.includes(Authority.REVIEWIDVERIFICATION);
         this.givenName = profile.givenName;
         this.familyName = profile.familyName;
+        this.aouAccountEmailAddress = profile.username;
+        this.contactEmailAddress = profile.contactEmail;
         this.minimizeChrome = this.shouldMinimize();
       });
     });
@@ -167,6 +181,54 @@ export class SignedInComponent implements OnInit, OnDestroy {
 
   openDataBrowser(): void {
     window.open(this.publicUiUrl, '_blank');
+  }
+
+  openZendeskWidget(): void {
+    // Note: we're string-protecting our access of the 'zE' property, since
+    // this property is dynamically loaded by the Zendesk web widget snippet,
+    // and can't properly be typed. If for some reason the support widget is
+    // unavailable, we'll show a notice to the user.
+    if (window['zE'] == null) {
+      // Show an error message to the user, asking them to reload or contact
+      // support via email.
+      this.zendeskLoadError = true;
+      console.error('Error loading Zendesk widget');
+      return;
+    }
+
+    // In theory, this webWidget call should identify the user who is filing
+    // this support request.
+    //
+    // In practice, the values provided via 'identify' don't seem to do much.
+    // Zendesk uses values from the 'prefill' action to assign the user email
+    // in the created ticket, which means that for AoU these tickets won't be
+    // correctly associated with the researcher's AoU Google Account. See
+    // the Zendesk integration doc for more discussion.
+    window['zE']('webWidget', 'identify', {
+      name: `${this.givenName} ${this.familyName}`,
+      email: this.aouAccountEmailAddress,
+    });
+
+    window['zE']('webWidget', 'prefill', {
+      name: {
+        value: `${this.givenName} ${this.familyName}`,
+        readOnly: true,
+      },
+      // For the contact email we use the user's *contact* email address,
+      // since the AoU email address is not a valid email inbox.
+      email: {
+        value: this.contactEmailAddress,
+        readOnly: true,
+      },
+    });
+
+    // Trigger the widget to open the full contact form (instead of the
+    // help icon, which is the starting state).
+    window['zE']('webWidget', 'open');
+  }
+
+  openHubForum(): void {
+    window.open(environment.zendeskHelpCenterUrl, '_blank');
   }
 
 }
