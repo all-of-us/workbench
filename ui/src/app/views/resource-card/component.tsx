970266ece6042ae9e7692c1adcc488ea4652c261
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-01-22 14:45:44 -0500

    resource cards first pass

--- ui/src/app/views/resource-card/component.tsx
+++ ui/src/app/views/resource-card/component.tsx
@@ -1,21 +1,19 @@
-import {Component, EventEmitter, Input, OnInit, Output, ViewChild} from '@angular/core';
-import {Router} from '@angular/router';
+import {Component, Input,} from '@angular/core';
 import * as React from 'react';
 
 import {Clickable} from 'app/components/buttons';
 import {ClrIcon} from 'app/components/icons';
 import {PopupTrigger} from 'app/components/popups';
-import {ReactWrapperBase, switchCase} from 'app/utils/index';
+import {reactStyles, ReactWrapperBase, switchCase} from 'app/utils';
 import {ResourceType} from 'app/utils/resourceActions';
+import {navigate, navigateByUrl} from 'app/utils/navigation';
 
 import {
-  CohortsService, ConceptSetsService,
+  Cohort, CohortsService, ConceptSetsService,
+  ConceptSet,FileDetail,
   NotebookRename, RecentResource,
   WorkspacesService
 } from 'generated';
-
-import {SignInService} from 'app/services/sign-in.service';
-import {EditModalComponent} from 'app/views/edit-modal/component';
 
 const MenuItem = ({ icon, children, ...props }) => {
   return <Clickable
@@ -95,241 +93,88 @@ export class ResourceCardMenuComponent extends ReactWrapperBase {
   }
 }
 
-@Component ({
-  selector : 'app-resource-card',
-  styleUrls: ['../../styles/buttons.css',
-    '../../styles/cards.css',
-    '../../styles/template.css',
-    './component.css'],
-  templateUrl: './component.html'
-})
-export class ResourceCardComponent implements OnInit {
-  resourceType: ResourceType;
-  @Input('resourceCard')
-  resourceCard: RecentResource;
-  @Input('cssClass')
-  cssClass: string;
-  @Output() onUpdate: EventEmitter<void | NotebookRename> = new EventEmitter();
-  @Output() duplicateNameError: EventEmitter<string> = new EventEmitter();
-  @Output() invalidNameError: EventEmitter<string> = new EventEmitter();
-  wsNamespace: string;
-  wsId: string;
-  resource: any;
-  router: Router;
-  invalidResourceError = false;
-  confirmDeleting = false;
-
-  renaming = false;
-  editing = false;
-
-  constructor(
-      private cohortsService: CohortsService,
-      private workspacesService: WorkspacesService,
-      private conceptSetsService: ConceptSetsService,
-      private signInService: SignInService,
-      private route: Router,
-  ) {
+const styles = reactStyles({
+  card: {
+    borderRadius: '0.2rem',
+    boxShadow: '0 0.125rem 0.125rem 0 #d7d7d7',
+    width: '200px',
+    height: '223px',
+    marginRight: '1rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'flex-start',
+    padding: '0.75rem'
+  },
+  cardName: {
+    fontSize: '18px',
+    fontWeight: 500,
+    lineHeight: '22px',
+    color: '#2691D0',
+    cursor: 'pointer',
+    wordBreak: 'break-all'
+  },
+  lastModified: {
+    color: '#4A4A4A',
+    fontSize: '11px',
+    display: 'inline-block',
+    lineHeight: '14px',
+    fontWeight: 300,
+    marginBottom: '0.2rem'
   }
+});
 
-  ngOnInit() {
-    this.wsNamespace = this.resourceCard.workspaceNamespace;
-    // RW-1298 this should be updated
-    this.wsId = this.resourceCard.workspaceFirecloudName;
-    if (this.resourceCard) {
-      if (this.resourceCard.notebook) {
-        this.resourceType = ResourceType.NOTEBOOK;
-      } else if (this.resourceCard.cohort) {
-        this.resourceType = ResourceType.COHORT;
-      } else if (this.resourceCard.conceptSet) {
-        this.resourceType = ResourceType.CONCEPT_SET;
+export interface ResourceCardProps {
+  resourceCard: RecentResource,
+  onUpdate: Function,
+  duplicateNameError: Function,
+  invalidNameError: Function
+}
+
+export interface ResourceCardState {
+  resourceType: ResourceType,
+  renaming: boolean,
+  editing: boolean,
+  confirmDeleting: boolean,
+  invalidResourceError: boolean,
+}
+
+export class ResourceCard extends React.Component<ResourceCardProps, ResourceCardState> {
+
+  constructor(props: ResourceCardProps) {
+    super(props);
+    const defaultState = {
+      editing: false,
+      renaming: false,
+      confirmDeleting: false
+    };
+    if (props.resourceCard) {
+      if (props.resourceCard.notebook) {
+        this.state = {
+          resourceType: ResourceType.NOTEBOOK,
+          invalidResourceError: false,
+          ...defaultState
+        };
+      } else if (props.resourceCard.cohort) {
+        this.state  = {
+          resourceType: ResourceType.COHORT,
+          invalidResourceError: false,
+            ...defaultState
+        };
+      } else if (props.resourceCard.conceptSet) {
+        this.state = {
+          resourceType: ResourceType.CONCEPT_SET,
+          invalidResourceError: false,
+          ...defaultState
+        };
       } else {
-        this.resourceType = ResourceType.INVALID;
-        this.invalidResourceError = true;
+        this.state = {
+          resourceType: ResourceType.INVALID,
+          invalidResourceError: true,
+          ...defaultState
+        };
       }
     }
-  }
-
-  renameNotebook(): void {
-    this.renaming = true;
-  }
-
-  cancelRename(): void {
-    this.renaming = false;
-  }
-
-  openConfirmDelete(): void {
-    this.confirmDeleting = true;
-  }
-
-  closeConfirmDelete(): void {
-    this.confirmDeleting = false;
-  }
-
-  receiveNotebookRename(rename: NotebookRename): void {
-    let newName = rename.newName;
-    if (!(new RegExp('^.+\.ipynb$').test(newName))) {
-      newName = rename.newName + '.ipynb';
-      rename.newName = newName;
-    }
-    if (new RegExp('.*\/.*').test(newName)) {
-      this.renaming = false;
-      this.invalidNameError.emit(newName);
-      return;
-    }
-    this.workspacesService.getNoteBookList(this.wsNamespace, this.wsId)
-      .switchMap((fileList) => {
-        if (fileList.filter((nb) => nb.name === newName).length > 0) {
-          throw new Error(newName);
-        } else {
-          return this.workspacesService.renameNotebook(this.wsNamespace, this.wsId, rename);
-        }
-      })
-      .subscribe(() => {
-          this.renaming = false;
-          this.onUpdate.emit(rename);
-        },
-        (dupName) => {
-          this.duplicateNameError.emit(dupName);
-          this.renaming = false;
-    });
-  }
-
-  receiveEdit(resource: RecentResource): void {
-    if (resource.cohort) {
-      this.cohortsService.updateCohort(
-          this.wsNamespace,
-          this.wsId,
-          resource.cohort.id,
-          resource.cohort
-      ).subscribe( () => {
-        this.closeEditModal();
-        this.onUpdate.emit();
-      });
-    } else if (resource.conceptSet) {
-      this.conceptSetsService.updateConceptSet(
-          this.wsNamespace,
-          this.wsId,
-          resource.conceptSet.id,
-          resource.conceptSet
-      ).subscribe( () => {
-        this.closeEditModal();
-        this.onUpdate.emit();
-      });
-    }
-  }
-
-  cloneResource(resource: RecentResource): void {
-    switch (this.resourceType) {
-      case ResourceType.NOTEBOOK: {
-        this.workspacesService.cloneNotebook(this.wsNamespace, this.wsId, resource.notebook.name)
-          .map(() => this.onUpdate.emit())
-          .subscribe(() => {});
-        break;
-      }
-      case ResourceType.COHORT: {
-        const url =
-          '/workspaces/' + this.wsNamespace + '/' + this.wsId + '/cohorts/build?cohortId=';
-        this.route.navigateByUrl(url + resource.cohort.id);
-        this.onUpdate.emit();
-        break;
-      }
-    }
-  }
-
-  deleteResource(resource: RecentResource): void {
-    switch (this.resourceType) {
-      case ResourceType.NOTEBOOK: {
-        this.resource = resource.notebook;
-        break;
-      }
-      case ResourceType.COHORT: {
-        this.resource = resource.cohort;
-        break;
-      }
-      case ResourceType.CONCEPT_SET: {
-        this.resource = resource.conceptSet;
-        break;
-      }
-    }
-    this.openConfirmDelete();
-  }
-
-  receiveDelete(): void {
-    switch (this.resourceType) {
-      case ResourceType.NOTEBOOK: {
-        this.workspacesService.deleteNotebook(this.wsNamespace, this.wsId, this.resource.name)
-          .subscribe(() => {
-            this.closeConfirmDelete();
-            this.onUpdate.emit();
-          });
-        break;
-      }
-      case ResourceType.COHORT: {
-        this.cohortsService.deleteCohort(this.wsNamespace, this.wsId, this.resource.id)
-          .subscribe(() => {
-            this.closeConfirmDelete();
-            this.onUpdate.emit();
-          });
-        break;
-      }
-      case ResourceType.CONCEPT_SET: {
-        this.conceptSetsService.deleteConceptSet(this.wsNamespace, this.wsId, this.resource.id)
-          .subscribe(() => {
-            this.closeConfirmDelete();
-            this.onUpdate.emit();
-          });
-      }
-    }
-  }
-
-  openResource(resource: RecentResource): void {
-    switch (this.resourceType) {
-      case ResourceType.COHORT: {
-        this.reviewCohort(resource);
-        break;
-      }
-      case ResourceType.CONCEPT_SET: {
-        this.route.navigate(['workspaces', this.wsNamespace, this.wsId, 'concepts',
-        'sets', resource.conceptSet.id], {relativeTo: null});
-        break;
-      }
-      case ResourceType.NOTEBOOK: {
-        let queryParams = null;
-        if (this.notebookReadOnly) {
-          queryParams = { playgroundMode: true };
-        }
-        this.route.navigate(
-          ['workspaces', this.wsNamespace, this.wsId, 'notebooks',
-           encodeURIComponent(this.resourceCard.notebook.name)], {
-             queryParams,
-             relativeTo: null,
-           });
-      }
-    }
-  }
-
-  editCohort(): void {
-    // This ensures the cohort binding is picked up before the open resolves.
-    setTimeout(_ => this.editing = true, 0);
-  }
-
-  editConceptSet(): void {
-    this.editing = true;
-  }
-
-  closeEditModal(): void {
-    this.editing = false;
-  }
-
-  reviewCohort(resource: RecentResource): void {
-    const url =
-        '/workspaces/' + this.wsNamespace
-        + '/' + this.wsId + '/cohorts/' + resource.cohort.id + '/review';
-    this.route.navigateByUrl(url);
-  }
-
-  get resourceTypeInvalidError(): boolean {
-    return this.invalidResourceError;
+    console.log(this.state.resourceType);
   }
 
   get actionsDisabled(): boolean {
@@ -337,19 +182,233 @@ export class ResourceCardComponent implements OnInit {
   }
 
   get writePermission(): boolean {
-    return this.resourceCard.permission === 'OWNER'
-      || this.resourceCard.permission === 'WRITER';
+    return this.props.resourceCard.permission === 'OWNER'
+        || this.props.resourceCard.permission === 'WRITER';
   }
 
   get notebookReadOnly(): boolean {
-    return this.resourceType === ResourceType.NOTEBOOK
-      && this.resourceCard.permission === 'READER';
+    return this.state.resourceType === ResourceType.NOTEBOOK
+        && this.props.resourceCard.permission === 'READER';
   }
 
-  get notebookDisplayName(): string {
-    if (this.resourceType === ResourceType.NOTEBOOK) {
-      return this.resourceCard.notebook.name.replace(/\.ipynb$/, '');
+  get displayName(): string {
+    if (this.state.resourceType === ResourceType.NOTEBOOK) {
+      return this.props.resourceCard.notebook.name.replace(/\.ipynb$/, '');
+    } else if (this.state.resourceType === ResourceType.COHORT) {
+      return this.props.resourceCard.cohort.name;
+    } else if (this.state.resourceType === ResourceType.CONCEPT_SET) {
+      return this.props.resourceCard.conceptSet.name;
     }
   }
 
+  editCohort(): void {
+    // This ensures the cohort binding is picked up before the open resolves.
+    setTimeout(_ => this.editConceptSet(), 0);
+  }
+
+  editConceptSet(): void {
+    this.setState({editing: true});
+  }
+
+  reviewCohort(): void {
+    navigateByUrl('/workspaces/' + this.props.resourceCard.workspaceNamespace
+        + '/' + this.props.resourceCard.workspaceFirecloudName + '/cohorts/'
+        + this.props.resourceCard.cohort.id + '/review');
+  }
+
+  closeEditModal(): void {
+    this.setState({editing: false});
+  }
+
+  renameNotebook(): void {
+    this.setState({renaming: true});
+  }
+
+  cancelRename(): void {
+    this.setState({renaming: false});
+  }
+
+  openConfirmDelete(): void {
+    this.setState({confirmDeleting: true});
+  }
+
+  closeConfirmDelete(): void {
+    this.setState({confirmDeleting: false});
+  }
+
+  cloneResource(): void {
+    // switch (this.state.resourceType) {
+    //   case ResourceType.NOTEBOOK: {
+    //     this.workspacesService.cloneNotebook(this.wsNamespace, this.wsId, resource.notebook.name)
+    //         .map(() => this.onUpdate.emit())
+    //         .subscribe(() => {});
+    //     break;
+    //   }
+    //   case ResourceType.COHORT: {
+    //     const url =
+    //         '/workspaces/' + this.wsNamespace + '/' + this.wsId + '/cohorts/build?cohortId=';
+    //     navigateByUrl(url + resource.cohort.id);
+    //     this.onUpdate.emit();
+    //     break;
+    //   }
+    // }
+  }
+
+  // TODO: how do i call the different services?
+  receiveDelete(): void {
+    // switch (this.state.resourceType) {
+    //   case ResourceType.NOTEBOOK: {
+    //     this.workspacesService.deleteNotebook(
+    //         this.props.resourceCard.workspaceNamespace,
+    //         this.props.resourceCard.workspaceFirecloudName,
+    //         this.state.resource.name)
+    //         .subscribe(() => {
+    //           this.closeConfirmDelete();
+    //           this.onUpdate.emit();
+    //         });
+    //     break;
+    //   }
+    //   case ResourceType.COHORT: {
+    //     this.cohortsService.deleteCohort(
+    //         this.props.resourceCard.workspaceNamespace,
+    //         this.props.resourceCard.workspaceFirecloudName,
+    //         this.state.resource.id)
+    //         .subscribe(() => {
+    //           this.closeConfirmDelete();
+    //           this.onUpdate.emit();
+    //         });
+    //     break;
+    //   }
+    //   case ResourceType.CONCEPT_SET: {
+    //     this.conceptSetsService.deleteConceptSet(
+    //         this.props.resourceCard.workspaceNamespace,
+    //         this.props.resourceCard.workspaceFirecloudName,
+    //         this.state.resource.id)
+    //         .subscribe(() => {
+    //           this.closeConfirmDelete();
+    //           this.onUpdate.emit();
+    //         });
+    //   }
+    // }
+  }
+
+  receiveEdit(): void {
+    // if (resource.cohort) {
+    //   this.cohortsService.updateCohort(
+    //       this.wsNamespace,
+    //       this.wsId,
+    //       resource.cohort.id,
+    //       resource.cohort
+    //   ).subscribe( () => {
+    //     this.closeEditModal();
+    //     this.onUpdate.emit();
+    //   });
+    // } else if (resource.conceptSet) {
+    //   this.conceptSetsService.updateConceptSet(
+    //       this.wsNamespace,
+    //       this.wsId,
+    //       resource.conceptSet.id,
+    //       resource.conceptSet
+    //   ).subscribe( () => {
+    //     this.closeEditModal();
+    //     this.onUpdate.emit();
+    //   });
+    // }
+  }
+
+  receiveNotebookRename(rename: NotebookRename): void {
+    // let newName = rename.newName;
+    // if (!(new RegExp('^.+\.ipynb$').test(newName))) {
+    //   newName = rename.newName + '.ipynb';
+    //   rename.newName = newName;
+    // }
+    // if (new RegExp('.*\/.*').test(newName)) {
+    //   this.renaming = false;
+    //   this.invalidNameError.emit(newName);
+    //   return;
+    // }
+    // this.workspacesService.getNoteBookList(this.wsNamespace, this.wsId)
+    //     .switchMap((fileList) => {
+    //       if (fileList.filter((nb) => nb.name === newName).length > 0) {
+    //         throw new Error(newName);
+    //       } else {
+    //         return this.workspacesService.renameNotebook(this.wsNamespace, this.wsId, rename);
+    //       }
+    //     })
+    //     .subscribe(() => {
+    //           this.renaming = false;
+    //           this.onUpdate.emit(rename);
+    //         },
+    //         (dupName) => {
+    //           this.duplicateNameError.emit(dupName);
+    //           this.renaming = false;
+    //         });
+  }
+
+  openResource(): void {
+    switch (this.state.resourceType) {
+      case ResourceType.COHORT: {
+        this.reviewCohort();
+        break;
+      }
+      case ResourceType.CONCEPT_SET: {
+        navigate(['workspaces', this.props.resourceCard.workspaceNamespace,
+          this.props.resourceCard.workspaceFirecloudName, 'concepts', 'sets',
+          this.props.resourceCard.conceptSet.id], {relativeTo: null});
+        break;
+      }
+      case ResourceType.NOTEBOOK: {
+        let queryParams = null;
+        if (this.notebookReadOnly) {
+          queryParams = { playgroundMode: true };
+        }
+        navigate(
+            ['workspaces', this.props.resourceCard.workspaceNamespace,
+              this.props.resourceCard.workspaceFirecloudName, 'notebooks',
+              encodeURIComponent(this.props.resourceCard.notebook.name)], {
+              queryParams,
+              relativeTo: null,
+            });
+      }
+    }
+  }
+
+  render() {
+    return <React.Fragment>
+      <div style={styles.card}>
+        <div style={{display: 'flex', alignItems: 'flex-start'}}>
+          <ResourceCardMenu disabled={this.actionsDisabled}
+                            resourceType={this.state.resourceType}
+                            onCloneResource={this.cloneResource}
+                            onDeleteResource={this.openConfirmDelete}
+                            onRenameNotebook={this.renameNotebook}
+                            onEditCohort={this.editCohort}
+                            onEditConceptSet={this.editConceptSet}
+                            onReviewCohort={this.reviewCohort}/>
+          <div style={styles.cardName}
+               onClick={() => this.openResource}>{this.displayName}</div>
+        </div>
+        <div>description goes here</div>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          <div style={styles.lastModified}>Last Modified: {this.props.resourceCard.modifiedTime}</div>
+        </div>
+      </div>
+    </React.Fragment>
+  }
+}
+
+@Component ({
+  selector : 'app-resource-card',
+  template: '<div #root></div>'
+})
+export class ResourceCardComponent extends ReactWrapperBase {
+  resourceType: ResourceType;
+  @Input('resourceCard') resourceCard: ResourceCardProps['resourceCard'];
+  @Input('onUpdate') onUpdate: ResourceCardProps['onUpdate'];
+  @Input('duplicateNameError') duplicateNameError: ResourceCardProps['duplicateNameError'];
+  @Input('invalidNameError') invalidNameError: ResourceCardProps['invalidNameError'];
+
+  constructor() {
+    super(ResourceCard, ['resourceCard', 'onUpdate'])
+  }
 }
