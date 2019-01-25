688d40d051254b0164a91a06dcf7826ab822fc83
CH Albach <calbach@google.com>
2019-01-25 15:17:11 -0800

    Enable prod mode for non-local builds

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -1,9 +1,16 @@
 // The main file is boilerplate; AppModule configures our app.
 
+import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
+import {environment} from 'environments/environment';
 import * as ReactModal from 'react-modal';
 
 import {AppModule} from 'app/app.module';
+
+
+if (!environment.debug) {
+  enableProdMode();
+}
 
 ReactModal.defaultStyles.overlay = {};
 ReactModal.defaultStyles.content = {};
