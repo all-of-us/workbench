0065558ebacdc43856b7437a59c11b743af3b09c
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-01-28 13:11:25 +0530

    moving top concepts block down

--- public-ui/src/app/views/ehr-view/ehr-view.component.ts
+++ public-ui/src/app/views/ehr-view/ehr-view.component.ts
@@ -53,6 +53,15 @@ export class EhrViewComponent implements OnInit, OnDestroy {
     'The sources represent the many different ways that the standard medical concept ' +
     'returned in the search results has been recorded in patient records. \n' +
     'The sources bar chart provides the top 10 source concepts from the All of Us data.';
+  matchingConceptsHelptText = 'Medical concepts are similar to medical terms; they capture medical information\n' +
+    'in an individual’s records and may sometimes have values associated with them.\n' +
+    'For example, “height” is a medical concept that has a measurement value (in centimeters).\n' +
+    'These concepts are categorized into different domains. Domains are types of medical information.\n' +
+    'The Data Browser searches the All of Us public data for medical concepts that\n' +
+    'match the keyword or code entered in the search bar.\n' +
+    'The Data Browser counts how many participants have at least\n' +
+    'one mention of the matching medical concepts in their records.\n' +
+    'Matching medical concepts that have the highest participant counts are returned at the top of the list.';
 
   /* Show different graphs depending on domain we are in */
   // defaults,  most domains
@@ -234,7 +243,7 @@ export class EhrViewComponent implements OnInit, OnDestroy {
     r.expanded = true;
   }
   public toggleTopConcepts(domainName) {
-    if (!this.showTopConcepts[domainName] ) {
+    if (!this.showTopConcepts[domainName]) {
       this.showTopConcepts[domainName] = true;
     } else {
       this.showTopConcepts[domainName] = false;
