a43e2e0d81e4f42330ec456587f7123dce6441d4
Brian Freeman <brian.d.freeman@vumc.org>
2019-01-30 11:51:03 -0600

    RW-1846 rebase.

--- api/src/main/java/org/pmiops/workbench/api/CohortBuilderController.java
+++ api/src/main/java/org/pmiops/workbench/api/CohortBuilderController.java
@@ -18,7 +18,6 @@ import org.pmiops.workbench.cdr.dao.CriteriaAttributeDao;
 import org.pmiops.workbench.cdr.dao.CriteriaDao;
 import org.pmiops.workbench.cdr.model.Criteria;
 import org.pmiops.workbench.cdr.model.CriteriaAttribute;
-import org.pmiops.workbench.cdr.model.CriteriaId;
 import org.pmiops.workbench.cohortbuilder.ParticipantCounter;
 import org.pmiops.workbench.cohortbuilder.ParticipantCriteria;
 import org.pmiops.workbench.db.dao.CdrVersionDao;
@@ -123,18 +122,14 @@ public class CohortBuilderController implements CohortBuilderApiDelegate {
     cdrVersionService.setCdrVersion(cdrVersionDao.findOne(cdrVersionId));
     Long resultLimit = Optional.ofNullable(limit).orElse(DEFAULT_LIMIT);
     String matchExp = modifyKeywordMatch(value);
-    List<CriteriaId> ids;
+    List<Criteria> criteriaList;
     if (subtype == null) {
-      ids = criteriaDao.findCriteriaByTypeForCodeOrName(type, matchExp, value, new PageRequest(0, resultLimit.intValue()));
+      criteriaList = criteriaDao.findCriteriaByTypeForCodeOrName(type, matchExp, value, new PageRequest(0, resultLimit.intValue()));
     } else {
-      ids = type.equals(TreeType.SNOMED.name()) ?
-        criteriaDao.findCriteriaByTypeAndSubtypeForCodeOrName(type, subtype, matchExp, new PageRequest(0, resultLimit.intValue())) :
+      criteriaList = type.equals(TreeType.SNOMED.name()) ?
+        criteriaDao.findCriteriaByTypeAndSubtypeForName(type, subtype, matchExp, new PageRequest(0, resultLimit.intValue())) :
         criteriaDao.findCriteriaByTypeAndSubtypeForCodeOrName(type, subtype, matchExp, value, new PageRequest(0, resultLimit.intValue()));
     }
-
-    List<Criteria> criteriaList = ids.isEmpty() ?
-      new ArrayList<>() :
-      criteriaDao.findCriteriaByIds(ids.stream().map(CriteriaId::getId).collect(Collectors.toList()));
     CriteriaListResponse criteriaResponse = new CriteriaListResponse();
     criteriaResponse.setItems(criteriaList.stream().map(TO_CLIENT_CRITERIA).collect(Collectors.toList()));
 
@@ -322,7 +317,8 @@ public class CohortBuilderController implements CohortBuilderApiDelegate {
         }
         return "+" + keywords[i] + "*";
       })
-      .collect(Collectors.joining());
+      .collect(Collectors.joining())
+      + "+\"[rank1]\"";
   }
 
 }
--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/CriteriaDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/CriteriaDao.java
@@ -31,41 +31,35 @@ public interface CriteriaDao extends CrudRepository<Criteria, Long> {
 
   List<Criteria> findCriteriaByType(@Param("type") String type);
 
-  @Query(value = "select min(cr.id) as id from Criteria cr " +
+  @Query(value = "select cr from Criteria cr " +
     "    where cr.type = upper(?1) " +
     "    and (match(synonyms, ?2) > 0 or cr.code like upper(concat(?3,'%')))" +
-    "    group by cr.name, cr.count " +
-    "    order by convert(cr.count, decimal) desc")
-  List<CriteriaId> findCriteriaByTypeForCodeOrName(String type,
-                                                   String modifiedValue,
-                                                   String value,
-                                                   Pageable page);
+    "    order by cr.count desc")
+  List<Criteria> findCriteriaByTypeForCodeOrName(String type,
+                                                 String modifiedValue,
+                                                 String value,
+                                                 Pageable page);
 
-  @Query(value = "select min(cr.id) as id from Criteria cr " +
+  @Query(value = "select cr from Criteria cr " +
     "    where cr.type = upper(?1) " +
     "    and cr.subtype = upper(?2) " +
     "    and (match(synonyms, ?3) > 0 or cr.code like upper(concat(?4,'%')))" +
-    "    group by cr.name, cr.count " +
-    "    order by convert(cr.count, decimal) desc")
-  List<CriteriaId> findCriteriaByTypeAndSubtypeForCodeOrName(String type,
-                                                             String subtype,
-                                                             String modifiedValue,
-                                                             String value,
-                                                             Pageable page);
+    "    order by cr.count desc")
+  List<Criteria> findCriteriaByTypeAndSubtypeForCodeOrName(String type,
+                                                           String subtype,
+                                                           String modifiedValue,
+                                                           String value,
+                                                           Pageable page);
 
-  @Query(value = "select min(cr.id) as id from Criteria cr " +
+  @Query(value = "select cr from Criteria cr " +
     "    where cr.type = upper(?1) " +
     "    and cr.subtype = upper(?2) " +
     "    and match(synonyms, ?3) > 0 " +
-    "    group by cr.name, cr.count " +
-    "    order by convert(cr.count, decimal) desc")
-  List<CriteriaId> findCriteriaByTypeAndSubtypeForCodeOrName(String type,
-                                                             String subtype,
-                                                             String value,
-                                                             Pageable page);
-
-  @Query(value = "select c from Criteria c where c.id in :ids")
-  List<Criteria> findCriteriaByIds(@Param("ids") List<Long> ids);
+    "    order by cr.count desc")
+  List<Criteria> findCriteriaByTypeAndSubtypeForName(String type,
+                                                     String subtype,
+                                                     String value,
+                                                     Pageable page);
 
   @Query(value = "select * from criteria c " +
     "where c.type = :type " +
