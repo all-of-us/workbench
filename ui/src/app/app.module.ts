7da413332e7def5d49b2b05e9ac30331ea752fe1
Greg Rushton <rushtong@users.noreply.github.com>
2019-01-31 15:14:49 -0500

    [RW-1698][risk=low] NIH Callback Endpoint (#1710)
    * nih callback url

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -38,6 +38,7 @@ import {ErrorHandlerComponent} from './views/error-handler/component';
 import {HomepageComponent} from './views/homepage/component';
 import {InitialErrorComponent} from './views/initial-error/component';
 import {NewNotebookModalComponent} from './views/new-notebook-modal/component';
+import {NihCallbackComponent} from './views/nih-callback/component';
 import {NotebookListComponent} from './views/notebook-list/component';
 import {NotebookRedirectComponent} from './views/notebook-redirect/component';
 import {PageTemplateSignedOutComponent} from './views/page-template-signed-out/component';
@@ -149,6 +150,7 @@ export function getLeoConfiguration(signInService: SignInService): LeoConfigurat
     ErrorHandlerComponent,
     InitialErrorComponent,
     NewNotebookModalComponent,
+    NihCallbackComponent,
     NotebookListComponent,
     NotebookRedirectComponent,
     PageTemplateSignedOutComponent,
--- ui/src/testing/stubs/profile-api-stub.ts
+++ ui/src/testing/stubs/profile-api-stub.ts
@@ -1,10 +1,33 @@
 import {
+  BillingProjectStatus,
+  DataAccessLevel,
+  NihToken,
+  Profile,
   ProfileApi
 } from 'generated/fetch';
 
 // TODO: Port functionality from ProfileServiceStub as needed.
 export class ProfileApiStub extends ProfileApi {
+
+  profile: Profile;
+
   constructor() {
     super(undefined, undefined, (..._: any[]) => { throw Error('cannot fetch in tests'); });
+    this.profile = <Profile>{
+      username: 'testers',
+      contactEmail: 'tester@mactesterson.edu🀓⚚><script>alert("hello");</script>',
+      freeTierBillingProjectName: 'all-of-us-free-abcdefg',
+      freeTierBillingProjectStatus: BillingProjectStatus.Ready,
+      dataAccessLevel: DataAccessLevel.Registered,
+      givenName: 'Tester!@#$%^&*()><script>alert("hello");</script>',
+      familyName: 'MacTesterson!@#$%^&*()><script>alert("hello");</script>',
+      phoneNumber: '999-999-9999',
+      pageVisits: [{page: 'test'}],
+    };
   }
+
+  public updateNihToken(token?: NihToken, options?: any) {
+    return Promise.resolve(this.profile);
+  }
+
 }
