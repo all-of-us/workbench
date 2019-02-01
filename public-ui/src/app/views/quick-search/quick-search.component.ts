3613c94765c76c8853951c36fec4be4ab969cccc
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-02-01 11:39:41 +0530

    adding space before function

--- public-ui/src/app/views/quick-search/quick-search.component.ts
+++ public-ui/src/app/views/quick-search/quick-search.component.ts
@@ -160,7 +160,6 @@ export class QuickSearchComponent implements OnInit, OnDestroy {
     this.surveyResults = results.surveyModules;
     this.loading = false;
   }
-  
   public searchDomains(query: string) {
     this.prevSearchText = query;
     localStorage.setItem('searchText', query);
