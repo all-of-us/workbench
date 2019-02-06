8931e8817576224abbd8c13c36691b0b9141bd55
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-02-06 12:47:28 -0600

    RW-2058 wrap component in withCurrentWorkspace for workspace info

--- ui/src/app/cohort-review/detail-tab-table/detail-tab-table.component.tsx
+++ ui/src/app/cohort-review/detail-tab-table/detail-tab-table.component.tsx
@@ -1,6 +1,7 @@
 import {Component, Input} from '@angular/core';
+import {WorkspaceData} from 'app/resolvers/workspace';
 import {cohortReviewApi} from 'app/services/swagger-fetch-clients';
-import {reactStyles, ReactWrapperBase} from 'app/utils';
+import {reactStyles, ReactWrapperBase, withCurrentWorkspace} from 'app/utils';
 import {PageFilterRequest, PageFilterType, SortOrder} from 'generated/fetch';
 import {Column} from 'primereact/column';
 import {DataTable} from 'primereact/datatable';
@@ -9,10 +10,15 @@ import * as React from 'react';
 
 const styles = reactStyles({
   pDatatable: {
-    fontSize: '12px'
+    fontSize: '12px',
+    border: '1px solid #ccc'
   },
   pDatatableTbody: {
-    padding: 0
+    padding: '5px',
+    verticalAlign: 'top',
+    borderLeft: 0,
+    borderRight: 0,
+    lineHeight: '0.6rem'
   }
 });
 
@@ -22,87 +28,109 @@ export interface DetailTabTableProps {
   domain: string;
   filterType: PageFilterType;
   participantId: number;
-  cdrId: number;
-  namespace: string;
-  workspaceId: string;
   cohortId: number;
+  workspace: WorkspaceData;
 }
 
 export interface DetailTabTableState {
   data: Array<any>;
+  rowsData: Array<any>;
   loading: boolean;
   totalCount: number;
   start: number;
+  rows: number;
 }
 
-export class DetailTabTable extends React.Component<DetailTabTableProps, DetailTabTableState> {
-  dt: any;
-  constructor(props: DetailTabTableProps) {
-    super(props);
-    this.state = {
-      data: [],
-      loading: true,
-      totalCount: null,
-      start: 0
-    };
-  }
+export const DetailTabTable = withCurrentWorkspace()(
+  class extends React.Component<DetailTabTableProps, DetailTabTableState> {
+    dt: any;
+    constructor(props: DetailTabTableProps) {
+      super(props);
+      this.state = {
+        data: [],
+        rowsData: [],
+        loading: true,
+        totalCount: null,
+        start: 0,
+        rows: 25
+      };
+    }
 
-  onPageChange = (event: any) => {
-    console.log(event);
-    this.setState({
-      start: event.first
-    });
-  }
+    onPageChange = (event: any) => {
+      console.log(event);
+      const startIndex = event.first;
+      const endIndex = event.first + this.state.rows;
+      this.setState({
+        start: event.first,
+        rowsData: this.state.data.slice(startIndex, endIndex)
+      });
+    }
 
-  componentDidMount() {
-    console.log(this.props);
-    const pageFilterRequest = {
-      page: 0,
-      pageSize: 10000,
-      sortOrder: SortOrder.Asc,
-      sortColumn: this.props.columns[0].name,
-      pageFilterType: this.props.filterType,
-      domain: this.props.domain,
-    } as PageFilterRequest;
+    componentDidMount() {
+      console.log(this.props);
+      const {cdrVersionId, id, namespace} = this.props.workspace;
+      const pageFilterRequest = {
+        page: 0,
+        pageSize: 10000,
+        sortOrder: SortOrder.Asc,
+        sortColumn: this.props.columns[0].name,
+        pageFilterType: this.props.filterType,
+        domain: this.props.domain,
+      } as PageFilterRequest;
 
-    cohortReviewApi().getParticipantData(
-      this.props.namespace,
-      this.props.workspaceId,
-      this.props.cohortId,
-      this.props.cdrId,
-      this.props.participantId,
-      pageFilterRequest
-    ).then(response => {
-      this.setState({data: response.items, loading: false});
-    });
-  }
+      cohortReviewApi().getParticipantData(
+        namespace,
+        id,
+        this.props.cohortId,
+        +cdrVersionId,
+        this.props.participantId,
+        pageFilterRequest
+      ).then(response => {
+        this.setState({
+          data: response.items,
+          rowsData: response.items.slice(0, this.state.rows),
+          loading: false
+        });
+      });
+    }
 
-  render() {
-    const dynamicColumns = this.props.columns.map((col) => {
-      return <Column style={styles.pDatatableTbody} key={col.name} field={col.name} header={col.displayName}  sortable={true} filter={true} filterMatchMode='contains' />;
-    });
+    render() {
+      const columns = this.props.columns.map((col) => {
+        return <Column
+          style={styles.pDatatableTbody}
+          key={col.name} field={col.name}
+          header={col.displayName}
+          sortable={true}
+          filter={true}
+          filterMatchMode='contains' />;
+      });
 
-    const footer = <Paginator
-      first={this.state.start} rows={25} totalRecords={this.state.data.length} onPageChange={this.onPageChange}
-      template='FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink'></Paginator>;
-
-    return <React.Fragment>
-      <DataTable
-        footer={footer}
-        style={styles.pDatatable}
-        ref={(el) => this.dt = el}
-        value={this.state.data}
-        loading={this.state.loading}
+      const footer = <Paginator
         first={this.state.start}
-        rows={25}
+        rows={this.state.rows}
         totalRecords={this.state.data.length}
-        scrollable={true}
-        scrollHeight='calc(100vh - 380px)'>
-        {dynamicColumns}
-      </DataTable>
-    </React.Fragment>;
+        onPageChange={this.onPageChange}
+        template='FirstPageLink PrevPageLink CurrentPageReport NextPageLink LastPageLink'>
+      </Paginator>;
+
+      return <React.Fragment>
+        <DataTable
+          footer={footer}
+          style={styles.pDatatable}
+          ref={(el) => this.dt = el}
+          value={this.state.rowsData}
+          loading={this.state.loading}
+          first={this.state.start}
+          rows={this.state.rows}
+          totalRecords={this.state.data.length}
+          scrollable={true}
+          scrollHeight='calc(100vh - 380px)'>
+          {columns}
+        </DataTable>
+      </React.Fragment>;
+    }
   }
-}
+);
 
 @Component ({
   selector : 'app-detail-tab-table',
@@ -114,12 +142,16 @@ export class DetailTabTableComponent extends ReactWrapperBase {
   @Input('domain') domain: DetailTabTableProps['domain'];
   @Input('filterType') filterType: DetailTabTableProps['filterType'];
   @Input('participantId') participantId: DetailTabTableProps['participantId'];
-  @Input('namespace') namespace: DetailTabTableProps['namespace'];
-  @Input('workspaceId') workspaceId: DetailTabTableProps['workspaceId'];
   @Input('cohortId') cohortId: DetailTabTableProps['cohortId'];
-  @Input('cdrId') cdrId: DetailTabTableProps['cdrId'];
 
   constructor() {
-    super(DetailTabTable, ['tabname', 'columns', 'domain', 'filterType', 'participantId', 'namespace', 'workspaceId', 'cohortId', 'cdrId']);
+    super(DetailTabTable, [
+      'tabname',
+      'columns',
+      'domain',
+      'filterType',
+      'participantId',
+      'cohortId',
+    ]);
   }
 }
