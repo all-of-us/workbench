ee9496233caeff27f12ecce20eca22000488f4f1
CH Albach <calbach@google.com>
2019-02-05 19:27:27 -0800

    gradlew...

--- api/db/run-migrations.sh
+++ api/db/run-migrations.sh
@@ -28,6 +28,6 @@ echo "Creating database if it does not exist..."
 mysql -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} < ${CREATE_DB_FILE}
 
 echo "Upgrading database..."
-../gradlew update $activity $context
+"$(dirname "${BASH_SOURCE}")/../gradlew" update $activity $context
 
 mysql -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} < ${GRANT_PERMISSIONS_FILE}
