4e97c0eaec70260468fa2e0bef1627a909a5fcac
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-02-08 10:59:51 -0500

    missing return statement on nested observable (#1761)

--- ui/src/app/app-routing.module.ts
+++ ui/src/app/app-routing.module.ts
@@ -43,7 +43,7 @@ const routes: Routes = [
     path: '',
     component: SignedInComponent,
     canActivate: [SignInGuard],
-    canActivateChild: [SignInGuard, RegistrationGuard],
+    canActivateChild: [SignInGuard, RegistrationGuard, AccessTasksGuard],
     runGuardsAndResolvers: 'always',
     children: [
       {
