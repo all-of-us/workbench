fad7956a3586f18f8257083812bfceb8f2d89538
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-02-11 18:02:49 -0500

    wip

--- ui/src/app/views/homepage/component.tsx
+++ ui/src/app/views/homepage/component.tsx
@@ -2,26 +2,24 @@ import {Component, Input, OnDestroy, OnInit, ViewChild} from '@angular/core';
 import {ProfileStorageService} from 'app/services/profile-storage.service';
 import {ServerConfigService} from 'app/services/server-config.service';
 import {navigate} from 'app/utils/navigation';
-import {BugReportComponent} from 'app/views/bug-report/component';
 import {environment} from 'environments/environment';
 
 import * as React from 'react';
 
 import {ActivatedRoute} from '@angular/router';
+import {Spinner} from "app/components/spinners";
 import {
   Clickable,
   styles as buttonStyles
 } from 'app/components/buttons';
 import {ClrIcon} from 'app/components/icons';
-import {profileApi} from 'app/services/swagger-fetch-clients';
+import {configApi, profileApi} from 'app/services/swagger-fetch-clients';
 import {reactStyles, ReactWrapperBase, withStyle} from 'app/utils';
 import {
   BillingProjectStatus,
-  IdVerificationStatus,
   PageVisit,
-  Profile,
-  ProfileService
-} from 'generated';
+} from 'generated/fetch';
+import {QuickTourReact} from "../quick-tour-modal/component";
 
 
 const styles = reactStyles({
@@ -200,140 +198,87 @@ export class WorkbenchAccessTasksComponent extends ReactWrapperBase {
   }
 }
 
+const homepageStyles = reactStyles({
+  backgroundImage: {
+    backgroundImage: 'url("/assets/images/AoU-HP-background.jpg")',
+    backgroundRepeat: 'no-repeat',
+    // -webkit-background-size: cover;
+    // -moz-background-size: cover;
+    // -o-background-size: cover;
+    backgroundSize: 'cover',
+    height: '100%',
+    marginLeft: '-1rem',
+    marginRight: '-0.6rem',
+    display: 'flex',
+    flexDirection: 'column',
+    justifyContent: 'space-between'
+  },
+  singleCard: {
+    height: '33.4%',
+    width: '87.34%',
+    minHeight: '18rem',
+    maxHeight: '26rem',
+    display: 'flex',
+    flexDirection: 'column',
+    borderRadius: '5px',
+    backgroundColor: 'rgba(255, 255, 255, 0.15)',
+    boxShadow: '0 0 2px 0 rgba(0, 0, 0, 0.12), 0 3px 2px 0 rgba(0, 0, 0, 0.12)',
+    border: 'none',
+    marginTop: '1rem'
+  },
+  headingLinks: {
+    marginTop: '2.74%',
+    height: '2.68%',
+    fontSize: '14px',
+    color: '#FFFFFF'
+  },
+  quickRow: {
+    display: 'flex', justifyContent: 'flex-start', maxHeight: '26rem',
+    flexDirection: 'row', marginLeft: '4rem', padding: '1rem'
+  },
+  quickTourLabel: {
+    fontSize: 28, lineHeight: '34px', color: '#fff', paddingRight: '2.3rem',
+    marginTop: '2rem', width: '31%'
+  }
+});
 
-@Component({
-  styleUrls: ['./component.css'],
-  templateUrl: './component.html',
-})
-export class HomepageComponent implements OnInit, OnDestroy {
+export class Homepage extends React.Component<{}, {
+  billingProjectInitialized: boolean,
+  firstVisit: boolean;
+  videoOpen: boolean,
+  accessTasksLoaded: boolean,
+  accessTasksRemaining: boolean,
+  eraCommonsLinked: boolean,
+  eraCommonsError: string,
+  trainingCompleted: boolean,
+  quickTour: boolean
+}> {
   private static pageId = 'homepage';
-  @ViewChild('myVideo') myVideo: any;
-  profile: Profile;
-  view: any[] = [180, 180];
-  // TODO [RW-1887]: this task completion state is (probably unused)
-  // Clean it up when converting this page to React.
-  numberOfTotalTasks = 4;
-  completedTasksName = 'Completed';
-  unfinishedTasksName = 'Unfinished';
-  open = false;
-  src = '';
-  spinnerValues = [
-    {
-      'name': this.completedTasksName,
-      'value': this.completedTasks
-    },
-    {
-      'name': this.unfinishedTasksName,
-      'value': this.numberOfTotalTasks - this.completedTasks
-    }
-  ];
-  billingProjectInitialized = false;
-  billingProjectQuery: NodeJS.Timer;
-  firstSignIn: Date;
-  firstVisit = true;
-  newPageVisit: PageVisit = { page: HomepageComponent.pageId};
-  footerLinks = [
-    {
-      title: 'Working Within Researcher Workbench',
-      links: ['Researcher Workbench Mission',
-        'User interface components',
-        'What to do when things go wrong',
-        'Contributing to the Workbench']
-    },
-    {
-      title: 'Workspace',
-      links: ['Workspace interface components',
-        'User interface components',
-        'Collaborating with other researchers',
-        'Sharing and Publishing Workspaces']
-    },
-    {
-      title: 'Working with Notebooks',
-      links: ['Notebook interface components',
-        'Notebooks and data',
-        'Collaborating with other researchers',
-        'Sharing and Publishing Notebooks']
-    }];
-  @ViewChild(BugReportComponent)
-  bugReportComponent: BugReportComponent;
-  quickTour: boolean;
-  accessTasksRemaining: boolean;
-  eraCommonsLinked: boolean;
-  eraCommonsError = '';
-  // TODO RW-1184; defaulting to true
-  trainingCompleted = true;
+  private route: ActivatedRoute;
 
-  constructor(
-    private profileService: ProfileService,
-    private profileStorageService: ProfileStorageService,
-    private serverConfigService: ServerConfigService,
-    private route: ActivatedRoute,
-  ) {
-    // create bound methods to use as callbacks
-    this.closeQuickTour = this.closeQuickTour.bind(this);
+  constructor(props: Object) {
+    super(props);
+    this.state = {
+      billingProjectInitialized: false,
+      firstVisit: undefined,
+      eraCommonsLinked: undefined,
+      accessTasksRemaining: undefined,
+      eraCommonsError: '',
+      trainingCompleted: true,
+      videoOpen: false,
+      accessTasksLoaded: false,
+      quickTour: false
+    }
   }
 
-  ngOnInit(): void {
+  // TODO: this probably needs to check falsy more explicitly for loading case
+   get accessTasksRemaining(): boolean {
+    return !!(this.state.eraCommonsLinked && this.state.trainingCompleted);
+   }
+
+  componentDidMount() {
     this.validateNihToken();
-
-    // TODO: combine these two profile() requests
-    this.profileService.getMe().subscribe(profile => {
-      if (profile.pageVisits) {
-        this.firstVisit = !profile.pageVisits.some(v =>
-        v.page === HomepageComponent.pageId);
-      }
-
-      // Set Access Tasks flags
-      // TODO RW-1184 set trainingCompleted flag
-      this.eraCommonsLinked = !!profile.linkedNihUsername;
-
-      if (this.route.snapshot.queryParams.workbenchAccessTasks) {
-        // To reach the access tasks component from dev use /?workbenchAccessTasks=true
-        this.accessTasksRemaining = true;
-      } else {
-        this.serverConfigService.getConfig().subscribe((config) => {
-          if (environment.enableComplianceLockout && config.enforceRegistered) {
-            this.accessTasksRemaining = !this.eraCommonsLinked;
-          } else {
-            this.accessTasksRemaining = false;
-          }
-        });
-      }
-    },
-      e => {},
-      () => {
-        if (this.firstVisit) {
-          this.quickTour = true;
-        }
-        this.profileService.updatePageVisits(this.newPageVisit).subscribe();
-      });
-
-    this.profileStorageService.profile$.subscribe((profile) => {
-      // This will block workspace creation until the billing project is initialized
-      if (profile.freeTierBillingProjectStatus === BillingProjectStatus.Ready) {
-        this.billingProjectInitialized = true;
-      } else {
-        this.billingProjectQuery = setTimeout(() => {
-          this.profileStorageService.reload();
-        }, 10000);
-      }
-      this.profile = profile;
-      this.reloadSpinner();
-    });
-    this.profileStorageService.reload();
-
-  }
-
-  public closeQuickTour(): void {
-    this.quickTour = false;
-  }
-
-  play(type): void {
-    this.src = '/assets/videos/Workbench Tutorial - Cohorts.mp4';
-    if (type === 'notebook') {
-      this.src = '/assets/videos/Workbench Tutorial - Notebooks.mp4';
-    }
-    this.open = true;
+    this.callProfile();
   }
 
   async validateNihToken() {
@@ -342,72 +287,254 @@ export class HomepageComponent implements OnInit, OnDestroy {
       try {
         await profileApi().updateNihToken({ jwt: token });
       } catch (e) {
-        this.eraCommonsError = 'Error saving NIH Authentication status.';
+        this.setState({eraCommonsError: 'Error saving NIH Authentication status.'});
       }
     }
   }
 
-  // TODO [RW-1887] Dead code, please remove
-  public get completedTasks() {
-    let completedTasks = 0;
-    if (this.profile === undefined) {
-      return completedTasks;
+  async callProfile() {
+    try {
+      const profile = await profileApi().getMe();
+      if (profile.pageVisits) {
+        this.setState({firstVisit: !profile.pageVisits.some(v =>
+          v.page === Homepage.pageId)});
+      }
+      this.setState({eraCommonsLinked: !!profile.linkedNihUsername});
+
+      if (this.route.snapshot.queryParams.workbenchAccessTasks) {
+        // To reach the access tasks component from dev use /?workbenchAccessTasks=true
+        this.setState({accessTasksRemaining: true, accessTasksLoaded: true});
+      } else {
+        try {
+          const config = await configApi().getConfig();
+          if (environment.enableComplianceLockout && config.enforceRegistered) {
+            this.setState({
+              accessTasksRemaining: this.accessTasksRemaining,
+              accessTasksLoaded: true});
+          } else {
+            this.setState({accessTasksRemaining: false, accessTasksLoaded: true});
+          }
+
+        } catch(ex) {
+          console.error('error fetching config: ' + ex.toString());
+        }
+      }
+
+      if (profile.freeTierBillingProjectStatus === BillingProjectStatus.Ready) {
+        this.setState({billingProjectInitialized: true});
+      } else {
+        // todo
+        // this.billingProjectQuery = setTimeout(() => {
+        //   this.profileStorageService.reload();
+        // }, 10000);
+      }
+    } catch(e) {
+      console.log('error fetching profile');
+    } finally {
+      await profileApi().updatePageVisits({ page: Homepage.pageId});
     }
-    if (this.profile.idVerificationStatus === IdVerificationStatus.VERIFIED) {
-      completedTasks += 1;
-    }
-    if (this.profile.demographicSurveyCompletionTime !== null) {
-      completedTasks += 1;
-    }
-    if (this.profile.ethicsTrainingCompletionTime !== null) {
-      completedTasks += 1;
-    }
-    if (this.profile.termsOfServiceCompletionTime !== null) {
-      completedTasks += 1;
-    }
-    return completedTasks;
   }
 
-  public get completedTasksAsPercentage() {
-    return this.completedTasks / this.numberOfTotalTasks * 100;
+  openVideo(type: string): string {
+    this.setState({videoOpen: true});
+    if (type === 'notebook') {
+      return '/assets/videos/Workbench Tutorial - Notebooks.mp4';
+    } else {
+      return '/assets/videos/Workbench Tutorial - Cohorts.mp4';
+    }
   }
 
-  // TODO [RW-1887] Dead code, please remove
-  reloadSpinner(): void {
-    this.spinnerValues = [
+
+  render() {
+    const {billingProjectInitialized, firstVisit, videoOpen, accessTasksLoaded,
+        accessTasksRemaining, eraCommonsLinked, eraCommonsError, trainingCompleted} = this.state;
+    const quickTourResources = [
       {
-        'name': this.completedTasksName,
-        'value': this.completedTasks
-      },
-      {
-        'name': this.unfinishedTasksName,
-        'value': this.numberOfTotalTasks - this.completedTasks
+        src: '/assets/images/QT-thumbnail.svg',
+        //onClick: () => this.setState({quickTour: true})
+      }, {
+        src: '/assets/images/cohorts-thumbnail.png',
+        //onClick: () => this.openVideo('cohort')
+      }, {
+        src: '/assets/images/notebooks-thumbnail.png',
+        //onClick: () => this.openVideo('notebook')
       }
     ];
+
+    return <React.Fragment>
+      <div style={homepageStyles.backgroundImage}>
+        <div style={{display: 'flex', justifyContent: 'center'}}>
+          <div style={homepageStyles.singleCard}>
+            {accessTasksLoaded ?
+              (accessTasksRemaining ?
+                (<WorkbenchAccessTasks eraCommonsLinked={eraCommonsLinked}
+                                eraCommonsError={eraCommonsError}
+                                trainingCompleted={trainingCompleted}/>
+                ) : (
+                <div style={{flexDirection: 'row', height: '17.47%', marginBottom: '0.5rem',
+                  justifyContent: 'flex-start', flexWrap: 'nowrap'}}>
+                  <div style={styles.mainHeader}>Researcher Workbench</div>
+                  <div style={homepageStyles.headingLinks}>Create a Workspace</div>
+                </div>)
+                ) :
+              <Spinner dark={true} style={{width: '100%', marginTop: '5rem'}}/>}
+          </div>
+
+        </div>
+        <div style={homepageStyles.quickRow}>
+          <div style={homepageStyles.quickTourLabel}>Quick Tour & Videos</div>
+            {quickTourResources.map(thumbnail => {
+              return <img style={{maxHeight: '121px', width: '8rem', marginRight: '1rem'}} src={thumbnail.src}/>;
+              {/*<Clickable onClick={this.setState({quickTour: true})}>*/}
+                {/*<img src={thumbnail.src}/>*/}
+              {/*</Clickable>*/}
+          })}
+        </div>
+      </div>
+      {/*{firstVisit &&*/}
+        {/*<QuickTourReact closeFunction={() => this.setState({firstVisit: false})} />}*/}
+    </React.Fragment>;
   }
 
-  ngOnDestroy(): void {
-    clearTimeout(this.billingProjectQuery);
-  }
+}
 
-  addWorkspace(): void {
-    navigate(['workspaces/build']);
-  }
-
-  navigateToProfile(): void {
-    navigate(['profile']);
-  }
-
-  listWorkspaces(): void {
-    navigate(['workspaces']);
-  }
-
-  get twoFactorBannerEnabled() {
-    if (this.firstSignIn == null) {
-      return false;
-    }
-    // Don't show the banner after 1 week as their account would
-    // have been disabled had they not enabled 2-factor auth.
-    return !(new Date().getTime() - this.firstSignIn.getTime() > 7 * 24 * 60 * 60 * 1000);
+@Component({
+  template: '<div #root></div>'
+})
+export class HomepageComponent extends ReactWrapperBase {
+  constructor() {
+    super(Homepage, []);
   }
 }
+
+
+// @Component({
+//   styleUrls: ['./component.css'],
+//   templateUrl: './component.html',
+// })
+// export class HomepageComponent implements OnInit, OnDestroy {
+//   private static pageId = 'homepage';
+//   @ViewChild('myVideo') myVideo: any;
+//   open = false;
+//   src = '';
+//   billingProjectInitialized = false;
+//   billingProjectQuery: NodeJS.Timer;
+//   firstSignIn: Date;
+//   firstVisit = true;
+//   newPageVisit: PageVisit = { page: HomepageComponent.pageId};
+//   footerLinks = [
+//     {
+//       title: 'Working Within Researcher Workbench',
+//       links: ['Researcher Workbench Mission',
+//         'User interface components',
+//         'What to do when things go wrong',
+//         'Contributing to the Workbench']
+//     },
+//     {
+//       title: 'Workspace',
+//       links: ['Workspace interface components',
+//         'User interface components',
+//         'Collaborating with other researchers',
+//         'Sharing and Publishing Workspaces']
+//     },
+//     {
+//       title: 'Working with Notebooks',
+//       links: ['Notebook interface components',
+//         'Notebooks and data',
+//         'Collaborating with other researchers',
+//         'Sharing and Publishing Notebooks']
+//     }];
+//   quickTour: boolean;
+//   accessTasksRemaining: boolean;
+//   eraCommonsLinked: boolean;
+//   eraCommonsError = '';
+//   // TODO RW-1184; defaulting to true
+//   trainingCompleted = true;
+//
+//   constructor(
+//     private profileStorageService: ProfileStorageService,
+//     private serverConfigService: ServerConfigService,
+//     private route: ActivatedRoute,
+//   ) {
+//     // create bound methods to use as callbacks
+//     this.closeQuickTour = this.closeQuickTour.bind(this);
+//   }
+//
+//   ngOnInit(): void {
+//     // this.validateNihToken();
+//     //
+//     // // TODO: combine these two profile() requests
+//     // this.profileService.getMe().subscribe(profile => {
+//     //   if (profile.pageVisits) {
+//     //     this.firstVisit = !profile.pageVisits.some(v =>
+//     //     v.page === HomepageComponent.pageId);
+//     //   }
+//     //
+//     //   // Set Access Tasks flags
+//     //   // TODO RW-1184 set trainingCompleted flag
+//     //   this.eraCommonsLinked = !!profile.linkedNihUsername;
+//     //
+//     //   if (this.route.snapshot.queryParams.workbenchAccessTasks) {
+//     //     // To reach the access tasks component from dev use /?workbenchAccessTasks=true
+//     //     this.accessTasksRemaining = true;
+//     //   } else {
+//     //     this.serverConfigService.getConfig().subscribe((config) => {
+//     //       if (environment.enableComplianceLockout && config.enforceRegistered) {
+//     //         this.accessTasksRemaining = !this.eraCommonsLinked;
+//     //       } else {
+//     //         this.accessTasksRemaining = false;
+//     //       }
+//     //     });
+//     //   }
+//     // },
+//     //   e => {},
+//     //   () => {
+//     //     if (this.firstVisit) {
+//     //       this.quickTour = true;
+//     //     }
+//     //     this.profileService.updatePageVisits(this.newPageVisit).subscribe();
+//     //   });
+//     //
+//     // this.profileStorageService.profile$.subscribe((profile) => {
+//     //   // This will block workspace creation until the billing project is initialized
+//     //   if (profile.freeTierBillingProjectStatus === BillingProjectStatus.Ready) {
+//     //     this.billingProjectInitialized = true;
+//     //   } else {
+//     //     this.billingProjectQuery = setTimeout(() => {
+//     //       this.profileStorageService.reload();
+//     //     }, 10000);
+//     //   }
+//     // });
+//
+//   }
+//
+//   public closeQuickTour(): void {
+//     this.quickTour = false;
+//   }
+//
+//
+//   // ngOnDestroy(): void {
+//   //   clearTimeout(this.billingProjectQuery);
+//   // }
+//   //
+//   // addWorkspace(): void {
+//   //   navigate(['workspaces/build']);
+//   // }
+//   //
+//   // navigateToProfile(): void {
+//   //   navigate(['profile']);
+//   // }
+//   //
+//   // listWorkspaces(): void {
+//   //   navigate(['workspaces']);
+//   // }
+//   //
+//   // get twoFactorBannerEnabled() {
+//   //   if (this.firstSignIn == null) {
+//   //     return false;
+//   //   }
+//   //   // Don't show the banner after 1 week as their account would
+//   //   // have been disabled had they not enabled 2-factor auth.
+//   //   return !(new Date().getTime() - this.firstSignIn.getTime() > 7 * 24 * 60 * 60 * 1000);
+//   // }
+// }
