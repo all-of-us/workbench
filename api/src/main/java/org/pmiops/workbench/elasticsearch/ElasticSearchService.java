36375838dba8a3f8ead69b0b239e0b3c98effd60
Brian Freeman <brian.d.freeman@vumc.org>
2019-02-12 16:12:34 -0600

    RW-1956 fixing some test cases.

--- api/src/main/java/org/pmiops/workbench/elasticsearch/ElasticSearchService.java
+++ api/src/main/java/org/pmiops/workbench/elasticsearch/ElasticSearchService.java
@@ -5,7 +5,9 @@ import com.google.cloud.bigquery.QueryJobConfiguration;
 import com.google.cloud.bigquery.TableResult;
 import jnr.ffi.annotations.Synchronized;
 import org.apache.http.HttpHost;
-import org.elasticsearch.action.main.MainResponse;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthRequest;
+import org.elasticsearch.action.admin.cluster.health.ClusterHealthResponse;
+import org.elasticsearch.client.RequestOptions;
 import org.elasticsearch.client.RestClient;
 import org.elasticsearch.client.RestHighLevelClient;
 import org.pmiops.workbench.api.BigQueryService;
@@ -46,19 +48,21 @@ public class ElasticSearchService {
    * the fallback.
    *
    * @param userRequest
-   * @return
    */
   public Long elasticCount(org.pmiops.workbench.model.SearchRequest userRequest) {
     //For now if enableElasticsearchBackend is true
-    //we get the cluster name and log it then run the bigquery count.
+    //we get the cluster name, status and log it
+    //then fallback to bigquery count.
+    //TODO: Implement this method to call either getElasticCount()
+    //or getBigQueryCount() based on certain condition(s) like -> if it's
+    //temporal or not
     if (configProvider.get().elasticsearch.enableElasticsearchBackend) {
       try {
-        MainResponse response = client().info();
-        log.info("Cluser Name: " + response.getClusterName().toString());
-        log.info("Cluster Uuid: " + response.getClusterUuid());
-        log.info("Node Name: " + response.getNodeName());
-        log.info("Version: " + response.getVersion().toString());
-        log.info("Build: " + response.getBuild().toString());
+        ClusterHealthRequest healthRequest = new ClusterHealthRequest();
+        ClusterHealthResponse response = client().cluster().health(healthRequest, RequestOptions.DEFAULT);
+
+        log.info(String.format("Cluster Name: %s - Cluster Status: %s",
+          response.getClusterName(), response.getStatus().name()));
       } catch (IOException e) {
         log.severe(e.getMessage());
       }
@@ -69,21 +73,22 @@ public class ElasticSearchService {
   /**
    * Get count with Elasticsearch.
    * @param userRequest
-   * @return
    */
   private Long getElasticCount(org.pmiops.workbench.model.SearchRequest userRequest) {
-    //implement this in a future story
+    //TODO: implement this in a future story
+    //Will probably use elasticsearch querybuilders
+    //https://www.elastic.co/guide/en/elasticsearch/client/java-rest/current/java-rest-high-query-builders.html
     return 0L;
   }
 
   /**
    * Get the count with BigQuery.
    * @param userRequest
-   * @return
    */
   private Long getBigQueryCount(org.pmiops.workbench.model.SearchRequest userRequest) {
-    QueryJobConfiguration qjc = bigQueryService.filterBigQueryConfig(participantCounter.buildParticipantCounterQuery(
-      new ParticipantCriteria(userRequest)));
+    QueryJobConfiguration qjc = bigQueryService.filterBigQueryConfig(
+      participantCounter.buildParticipantCounterQuery(new ParticipantCriteria(userRequest))
+    );
     TableResult result = bigQueryService.executeQuery(qjc);
     Map<String, Integer> rm = bigQueryService.getResultMapper(result);
 
@@ -91,9 +96,16 @@ public class ElasticSearchService {
     return bigQueryService.getLong(row, rm.get("count"));
   }
 
+  /**
+   * Implementing RestHighLevelClient init here because injecting
+   * Provider<WorkbenchConfig> into a Configuration singleton class
+   * was causing a BeanInstantiationException due to WorkbenchConfig
+   * being request scoped. This works but need to add Synchronized
+   * annotation to make this method thread safe.
+   */
   @Synchronized
   private RestHighLevelClient client() {
-    if (configProvider.get().elasticsearch.enableElasticsearchBackend && client == null) {
+    if (client == null) {
       String[] vars = configProvider.get().elasticsearch.host.split(":");
       String host = vars[0];
       int port = Integer.parseInt(vars[1]);
