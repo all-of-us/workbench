66bc9e9b16428feb0471e46df8ffd1d79609ce78
Brian Freeman <brian.d.freeman@vumc.org>
2019-02-12 16:12:34 -0600

    RW-1956 fixing some test cases.

--- api/src/bigquerytest/java/org/pmiops/workbench/testconfig/TestWorkbenchConfig.java
+++ api/src/bigquerytest/java/org/pmiops/workbench/testconfig/TestWorkbenchConfig.java
@@ -1,11 +1,13 @@
 package org.pmiops.workbench.testconfig;
 
+import org.pmiops.workbench.config.WorkbenchConfig;
+
 public class TestWorkbenchConfig {
 
-    public static class BigQueryConfig {
-        public String dataSetId;
-        public String projectId;
-    }
+  public static class BigQueryConfig {
+    public String dataSetId;
+    public String projectId;
+  }
 
-    public BigQueryConfig bigquery;
+  public BigQueryConfig bigquery;
 }
