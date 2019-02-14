d2858739189b632605f8aa903cd84dbaab30f31f
Neha Saxena <nsaxena@broadinstitute.org>
2019-02-14 14:08:02 -0500

    higher minimum length for workbench usernames

--- api/src/main/java/org/pmiops/workbench/api/ProfileController.java
+++ api/src/main/java/org/pmiops/workbench/api/ProfileController.java
@@ -413,6 +413,9 @@ public class ProfileController implements ProfileApiDelegate {
   @Override
   public ResponseEntity<Profile> createAccount(CreateAccountRequest request) {
     verifyInvitationKey(request.getInvitationKey());
+    String userName = request.getProfile().getUsername();
+    if(userName.isEmpty() || userName.length() < 3 )
+      throw new BadRequestException("Username should be of atleast 3 characters");
     request.getProfile().setUsername(request.getProfile().getUsername().toLowerCase());
     validateProfileFields(request.getProfile());
     com.google.api.services.admin.directory.model.User googleUser =
