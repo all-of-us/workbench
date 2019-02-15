4bdcf29b688796b1ab109995ca69f77dc3f6197e
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2019-02-15 12:59:46 -0500

    undoing this change

--- ui/karma.conf.js
+++ ui/karma.conf.js
@@ -43,10 +43,6 @@ module.exports = function (config) {
     logLevel: config.LOG_INFO,
     autoWatch: true,
     browsers: ['Chrome'],
-    singleRun: false,
-    browserConsoleLogOptions: {
-      terminal: true,
-      level: ""
-    }
+    singleRun: false
   });
 };
