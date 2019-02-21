70bfa42ab769e7d1e0a3a7e019ece16fc8bb72ea
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2019-02-20 19:26:15 -0500

    [RW-2181][risk=low] 500 error calling Profile API syncTrainingStatus() (#1812)
    * Moving token to google bucket
    
    * text rather than json in moodle-key.txt
    
    * PR Comments 2

--- api/src/main/java/org/pmiops/workbench/compliance/ComplianceServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/compliance/ComplianceServiceImpl.java
@@ -1,10 +1,12 @@
 package org.pmiops.workbench.compliance;
 
+import org.pmiops.workbench.google.CloudStorageService;
 import org.pmiops.workbench.moodle.ApiException;
 import org.pmiops.workbench.moodle.api.MoodleApi;
 import org.pmiops.workbench.moodle.model.BadgeDetails;
 import org.pmiops.workbench.moodle.model.MoodleUserResponse;
 import org.pmiops.workbench.moodle.model.UserBadgeResponse;
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
 
@@ -15,14 +17,23 @@ public class ComplianceServiceImpl implements ComplianceService {
 
   private MoodleApi api = new MoodleApi();
   private static final String RESPONSE_FORMAT = "json";
-  private static final String TOKEN = "d106acbb7eb050a10e9b200eb82b8be8";
   private static final String GET_MOODLE_ID_SEARCH_FIELD = "email";
   private static final String MOODLE_EXCEPTION = "moodle_exception";
   private static final String MOODLE_USER_NOT_ALLOWED_ERROR_CODE = "guestsarenotallowed";
+  private CloudStorageService cloudStorageService;
+
+  @Autowired
+  public ComplianceServiceImpl(CloudStorageService cloudStorageService) {
+    this.cloudStorageService = cloudStorageService;
+  }
+
+  private String getToken() {
+    return this.cloudStorageService.getMoodleApiKey();
+  }
 
   @Override
   public Integer getMoodleId(String email) throws ApiException {
-    List<MoodleUserResponse> response = api.getMoodleId(TOKEN, GET_MOODLE_ID_SEARCH_FIELD, email);
+    List<MoodleUserResponse> response = api.getMoodleId(getToken(), GET_MOODLE_ID_SEARCH_FIELD, email);
     if (response.size() == 0) {
       return null;
     }
@@ -31,7 +42,7 @@ public class ComplianceServiceImpl implements ComplianceService {
 
   @Override
   public List<BadgeDetails>  getUserBadge(int userMoodleId) throws ApiException {
-    UserBadgeResponse response = api.getMoodleBadge(RESPONSE_FORMAT, TOKEN, userMoodleId);
+    UserBadgeResponse response = api.getMoodleBadge(RESPONSE_FORMAT, getToken(), userMoodleId);
     if (response.getException() != null && response.getException().equals(MOODLE_EXCEPTION)) {
       if (response.getErrorcode().equals(MOODLE_USER_NOT_ALLOWED_ERROR_CODE)) {
         throw new ApiException(HttpStatus.NOT_FOUND.value(), response.getMessage());
