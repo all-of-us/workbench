29ab51caa35475f0395156724d2c96e26986ca98
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2019-02-26 18:33:57 -0500

    fixes

--- ui/src/app/views/dataset/component.tsx
+++ ui/src/app/views/dataset/component.tsx
@@ -5,7 +5,7 @@ import {Button} from 'app/components/buttons';
 import {FadeBox} from 'app/components/containers';
 import {ClrIcon} from 'app/components/icons';
 import {WorkspaceData} from 'app/resolvers/workspace';
-import {conceptSetsApi} from 'app/services/swagger-fetch-clients';
+import {conceptsApi, conceptSetsApi} from 'app/services/swagger-fetch-clients';
 import {ReactWrapperBase, withCurrentWorkspace} from 'app/utils';
 import {navigate} from 'app/utils/navigation';
 import {CreateConceptSetModal} from 'app/views/conceptset-create-modal/component';
@@ -45,6 +45,14 @@ export const DataSet = withCurrentWorkspace()(class extends React.Component<
     };
   }
 
+  componentDidMount() {
+    const {namespace, id} = this.props.workspace;
+    this.loadResources();
+    conceptsApi().getDomainInfo(namespace, id).then((response) => {
+      this.setState({conceptDomainList: response.items});
+    });
+  }
+
   async loadResources() {
     try {
       const {namespace, id} = this.props.workspace;
@@ -65,49 +73,49 @@ export const DataSet = withCurrentWorkspace()(class extends React.Component<
         <div style={{color: '#000000', fontSize: '14px'}}>Build a dataset by selecting the
           variables and values for one or more of your cohorts. Then export the completed dataset
           to Notebooks where you can perform your analysis</div>
-      </FadeBox>
-      <div style={{display: 'flex'}}>
-        <div style={{marginLeft: '1.5rem', marginRight: '1.5rem', width: '33%'}}>
-          <h2>Select Cohorts</h2>
-          <div style={{backgroundColor: 'white', border: '1px solid #E5E5E5'}}>
-            <div style={styles.selectBoxHeader}>
-              Cohorts
-              <ClrIcon shape='plus-circle' class='is-solid' style={styles.addIcon}
-              onClick={() => navigate(['workspaces', namespace, id,  'cohorts', 'build'])}/>
-            </div>
-            {/*TODO: load cohorts and display here*/}
-            <div style={{height: '8rem'}}/>
-          </div>
-        </div>
-        <div style={{width: '58%'}}>
-          <h2>Select Concept Sets</h2>
-          <div style={{display: 'flex', backgroundColor: 'white', border: '1px solid #E5E5E5'}}>
-            <div style={{flexGrow: 1}}>
-              <div style={{...styles.selectBoxHeader, borderRight: '1px solid #E5E5E5'}}>
-                Concept Sets
-                <ClrIcon shape='plus-circle' class='is-solid' style={styles.addIcon}
-                onClick={() => this.setState({creatingConceptSet: true})}/>
-              </div>
-              {/*TODO: load concept steps and display here*/}
-              <div style={{height: '8rem', borderRight: '1px solid #E5E5E5'}}/>
-            </div>
-            <div style={{flexGrow: 1}}>
+        <div style={{display: 'flex'}}>
+          <div style={{marginLeft: '1.5rem', marginRight: '1.5rem', width: '33%'}}>
+            <h2>Select Cohorts</h2>
+            <div style={{backgroundColor: 'white', border: '1px solid #E5E5E5'}}>
               <div style={styles.selectBoxHeader}>
-                Values
+                Cohorts
+                <ClrIcon shape='plus-circle' class='is-solid' style={styles.addIcon}
+                  onClick={() => navigate(['workspaces', namespace, id,  'cohorts', 'build'])}/>
               </div>
-              {/*TODO: load values and display here*/}
+              {/*TODO: load cohorts and display here*/}
               <div style={{height: '8rem'}}/>
             </div>
           </div>
+          <div style={{width: '58%'}}>
+            <h2>Select Concept Sets</h2>
+            <div style={{display: 'flex', backgroundColor: 'white', border: '1px solid #E5E5E5'}}>
+              <div style={{flexGrow: 1}}>
+                <div style={{...styles.selectBoxHeader, borderRight: '1px solid #E5E5E5'}}>
+                  Concept Sets
+                  <ClrIcon shape='plus-circle' class='is-solid' style={styles.addIcon}
+                           onClick={() => this.setState({creatingConceptSet: true})}/>
+                </div>
+                {/*TODO: display concept sets here*/}
+                <div style={{height: '8rem', borderRight: '1px solid #E5E5E5'}}/>
+              </div>
+              <div style={{flexGrow: 1}}>
+                <div style={styles.selectBoxHeader}>
+                  Values
+                </div>
+                {/*TODO: load values and display here*/}
+                <div style={{height: '8rem'}}/>
+              </div>
+            </div>
+          </div>
         </div>
-      </div>
+      </FadeBox>
       <FadeBox style={{marginTop: '1rem'}}>
         <div style={{backgroundColor: 'white', border: '1px solid #E5E5E5'}}>
           <div style={{...styles.selectBoxHeader, display: 'flex', position: 'relative'}}>
             <div>Preview Dataset</div>
             <div style={{marginLeft: '1rem', color: '#000000', fontSize: '14px'}}>A visualization
               of your data table based on the variable and value you selected above</div>
-            <Button style={{position: 'absolute', right: '1rem', top: '.25rem'}}>
+            <Button style={{position: 'absolute', right: '1rem', top: '.25rem'}} disabled={true}>
               SAVE DATASET
             </Button>
           </div>
