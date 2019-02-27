eedc4f749d3b4a7eb142ad4e78829de1c4f37c7c
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-02-27 15:39:32 -0600

    check if null before fetching the results

--- public-ui/src/app/views/survey-view/survey-view.component.ts
+++ public-ui/src/app/views/survey-view/survey-view.component.ts
@@ -77,30 +77,32 @@ export class SurveyViewComponent implements OnInit, OnDestroy {
           // Todo -- add this to api maybe
           let didNotAnswerCount  = this.survey.participantCount;
           q.selectedAnalysis = q.genderAnalysis;
-          for (const a of q.countAnalysis.results) {
-            if (q.subQuestions) {
-              const matchedQuestionConcepts: QuestionConcept[] = q.subQuestions.filter(
-                w => w.conceptName.toLowerCase() === a.stratum4.toLowerCase());
-              if (matchedQuestionConcepts.length === 0) {
-                q.subQuestions.push({
-                  conceptId: a.stratum3,
-                  conceptName: a.stratum4,
-                  domainId: 'ppi',
-                  conceptCode: '',
-                  countValue: a.countValue,
-                  prevalence: a.prevalence,
-                  countAnalysis: this.makeAnalysis(q.countAnalysis),
-                  genderAnalysis: this.makeAnalysis(q.genderAnalysis),
-                  ageAnalysis: this.makeAnalysis(q.ageAnalysis),
-                  genderIdentityAnalysis: this.makeAnalysis(q.genderIdentityAnalysis),
-                  subQuestions: null
-                });
+          if (q.countAnalysis && q.countAnalysis.results) {
+            for (const a of q.countAnalysis.results) {
+              if (q.subQuestions) {
+                const matchedQuestionConcepts: QuestionConcept[] = q.subQuestions.filter(
+                  w => w.conceptName.toLowerCase() === a.stratum4.toLowerCase());
+                if (matchedQuestionConcepts.length === 0) {
+                  q.subQuestions.push({
+                    conceptId: a.stratum3,
+                    conceptName: a.stratum4,
+                    domainId: 'ppi',
+                    conceptCode: '',
+                    countValue: a.countValue,
+                    prevalence: a.prevalence,
+                    countAnalysis: this.makeAnalysis(q.countAnalysis),
+                    genderAnalysis: this.makeAnalysis(q.genderAnalysis),
+                    ageAnalysis: this.makeAnalysis(q.ageAnalysis),
+                    genderIdentityAnalysis: this.makeAnalysis(q.genderIdentityAnalysis),
+                    subQuestions: null
+                  });
+                }
+                q.subQuestions.map(sq => sq.selectedAnalysis = sq.genderAnalysis);
+                q.subQuestions = q.subQuestions.filter(sq => (sq.countAnalysis.results.length > 0));
               }
-              q.subQuestions.map(sq => sq.selectedAnalysis = sq.genderAnalysis);
-              q.subQuestions = q.subQuestions.filter(sq => (sq.countAnalysis.results.length > 0));
+              didNotAnswerCount = didNotAnswerCount - a.countValue;
+              a.countPercent = this.countPercentage(a.countValue);
             }
-            didNotAnswerCount = didNotAnswerCount - a.countValue;
-            a.countPercent = this.countPercentage(a.countValue);
           }
           const result = q.countAnalysis.results[0];
           if (didNotAnswerCount < 0 ) { didNotAnswerCount = 0; }
