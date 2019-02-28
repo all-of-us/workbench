d4b7b94156baad449a1ba086c732476b555c8756
SrushtiGangireddy <srushti.gangireddy@vumc.org>
2019-02-28 10:41:40 -0600

    fixing text overriding bug

--- public-ui/src/app/highlight-search/highlight-search.component.ts
+++ public-ui/src/app/highlight-search/highlight-search.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-highlight-search',
   templateUrl: './highlight-search.component.html',
   styleUrls: ['./highlight-search.component.css']
 })
-export class HighlightSearchComponent implements OnChanges {
+export class HighlightSearchComponent implements OnChanges, OnDestroy {
   @Input() text: string;
   @Input() searchTerm: string;
   @Input() isSource: boolean;
@@ -15,18 +15,15 @@ export class HighlightSearchComponent implements OnChanges {
   words: string[] = [];
   matchString: RegExp;
   ngOnChanges() {
-    if(!this.searchTerm) {
+    if (!this.searchTerm) {
       this.words = [this.text];
     }
-  
     let searchWords = this.searchTerm.split(new RegExp(',| '));
     searchWords = searchWords.filter(w => w.length > 0 );
     searchWords = searchWords.map(word => word.replace(/[&!^\/\\#,+()$~%.'":*?<>{}]/g, ''));
     this.matchString = new RegExp(searchWords.join('|'));
-    
     const matches = this.text.match(new RegExp(this.matchString, 'gi'));
     const splits = this.text.split(new RegExp(this.matchString, 'i'));
-    
     if (matches) {
       this.words = [];
       for (let i = 0; i < matches.length; i++) {
@@ -37,7 +34,6 @@ export class HighlightSearchComponent implements OnChanges {
       this.words = [this.text];
     }
   }
-  
   ngOnDestroy() {
     this.words = [];
   }
