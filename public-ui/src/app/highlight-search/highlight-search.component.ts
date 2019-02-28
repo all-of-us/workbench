f35b531e374fb40b59d41e7b1f69a93a49ca9083
Srushti Gangireddy <srushti.gangireddy@gmail.com>
2019-02-28 11:22:55 -0600

    [DB-246] [risk=low] fixing text repeat bug (#1854)
    * fixing text overriding bug
    
    * fixing text overriding bug

--- public-ui/src/app/highlight-search/highlight-search.component.ts
+++ public-ui/src/app/highlight-search/highlight-search.component.ts
@@ -1,11 +1,11 @@
-import { Component, Input, OnChanges } from '@angular/core';
+import {Component, Input, OnChanges, OnDestroy} from '@angular/core';
 
 @Component({
   selector: 'app-highlight-search',
   templateUrl: './highlight-search.component.html',
   styleUrls: ['./highlight-search.component.css']
 })
-export class HighlightSearchComponent implements OnChanges {
+export class HighlightSearchComponent implements OnChanges, OnDestroy {
   @Input() text: string;
   @Input() searchTerm: string;
   @Input() isSource: boolean;
@@ -15,21 +15,26 @@ export class HighlightSearchComponent implements OnChanges {
   words: string[] = [];
   matchString: RegExp;
   ngOnChanges() {
-    if (this.searchTerm) {
-      let searchWords = this.searchTerm.split(new RegExp(',| '));
-      searchWords = searchWords.filter(w => w.length > 0 );
-      searchWords = searchWords.map(word => word.replace(/[&!^\/\\#,+()$~%.'":*?<>{}]/g, ''));
-      this.matchString = new RegExp(searchWords.join('|'));
+    if (!this.searchTerm) {
+      this.words = [this.text];
     }
+    let searchWords = this.searchTerm.split(new RegExp(',| '));
+    searchWords = searchWords.filter(w => w.length > 0 );
+    searchWords = searchWords.map(word => word.replace(/[&!^\/\\#,+()$~%.'":*?<>{}]/g, ''));
+    this.matchString = new RegExp(searchWords.join('|'));
     const matches = this.text.match(new RegExp(this.matchString, 'gi'));
     const splits = this.text.split(new RegExp(this.matchString, 'i'));
-    if (matches && this.searchTerm) {
+    if (matches) {
+      this.words = [];
       for (let i = 0; i < matches.length; i++) {
         this.words.push(splits[i], matches[i]);
       }
       this.words.push(splits[splits.length - 1]);
     } else {
-      this.words.push(this.text);
+      this.words = [this.text];
     }
+  }
+  ngOnDestroy() {
+    this.words = [];
   }
 }
