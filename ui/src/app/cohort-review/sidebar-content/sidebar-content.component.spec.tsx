5843b8296f3975da39a21049dc8d539f061627f6
Brett Heath-Wlaz <bheathwl@broadinstitute.org>
2019-03-07 14:08:06 -0500

    convert cohort review sidebar to react

---
+++ ui/src/app/cohort-review/sidebar-content/sidebar-content.component.spec.tsx
@@ -0,0 +1,19 @@
+import {mount} from 'enzyme';
+import * as React from 'react';
+
+import {SidebarContent} from './sidebar-content.component';
+
+describe('SidebarContent', () => {
+  it('should render', () => {
+    const wrapper = mount(<SidebarContent
+      participant={{}}
+      setParticipant={() => {}}
+      annotations={[]}
+      annotationDefinitions={[]}
+      setAnnotations={() => {}}
+      openCreateDefinitionModal={() => {}}
+      openEditDefinitionsModal={() => {}}
+    />);
+    expect(wrapper.exists()).toBeTruthy();
+  });
+});
