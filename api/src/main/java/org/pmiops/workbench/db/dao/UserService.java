60c94108c07405104078fdd927dd5c74c461a598
Ursa Stutsman <ursa.bear.stutsman@gmail.com>
2019-03-07 14:34:46 -0500

    really not with it today

--- api/src/main/java/org/pmiops/workbench/db/dao/UserService.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/UserService.java
@@ -108,11 +108,11 @@ public class UserService {
     // user.getDataUseAgreementBypassTime() != null || !configProvider.get().access.enableDataUseAgreement;
     boolean eraCommonsCompliant = user.getEraCommonsBypassTime() != null ||
       !configProvider.get().access.enableEraCommons || user.getEraCommonsCompletionTime() != null;
+    Timestamp current = new Timestamp(clock.instant().toEpochMilli());
     boolean complianceTrainingNotExpired = user.getComplianceTrainingExpirationTime() == null ||
       current < user.getComplianceTrainingExpirationTime();
     boolean complianceTrainingCompliant = complianceTrainingNotExpired && (user.getComplianceTrainingCompletionTime() != null ||
       user.getComplianceTrainingBypassTime() != null | !configProvider.get().access.enableComplianceTraining);
-    Timestamp current = new Timestamp(clock.instant().toEpochMilli());
     boolean idVerificationCompliant = user.getIdVerificationCompletionTime() != null ||
       user.getIdVerificationBypassTime() != null || !configProvider.get().access.enableIdVerification ||
       Optional.ofNullable(user.getIdVerificationIsValid()).orElse(false);
