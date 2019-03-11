fd73bfddce7f208f4a3dc82169aa6197f833a73e
Gregory Jordan <gjordan@verily.com>
2019-03-11 14:03:03 -0400

    More work on getting tools build working.

--- api/src/main/java/org/pmiops/workbench/config/CommonConfig.java
+++ api/src/main/java/org/pmiops/workbench/config/CommonConfig.java
@@ -3,6 +3,7 @@ package org.pmiops.workbench.config;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
+import org.pmiops.workbench.auth.ServiceAccounts;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
@@ -26,4 +27,7 @@ public class CommonConfig {
   
   @Bean
   GoogleCredential.Builder googleCredentialBuilder() { return new GoogleCredential.Builder(); }
+
+  @Bean
+  ServiceAccounts serviceAccounts() { return new ServiceAccounts(); }
 }
