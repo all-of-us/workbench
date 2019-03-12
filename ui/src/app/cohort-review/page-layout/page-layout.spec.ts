f113d82160bb9addd8c64808668ad484686ae937
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-03-12 10:55:34 -0500

    RW-2026 fix broken tests

--- ui/src/app/cohort-review/page-layout/page-layout.spec.ts
+++ ui/src/app/cohort-review/page-layout/page-layout.spec.ts
@@ -5,19 +5,30 @@ import {ClarityModule} from '@clr/angular';
 
 import {CreateReviewPage} from 'app/cohort-review/create-review-page/create-review-page';
 import {cohortReviewStore} from 'app/cohort-review/review-state.service';
+import {registerApiClient} from 'app/services/swagger-fetch-clients';
 import {currentWorkspaceStore, NavStore} from 'app/utils/navigation';
 import {CohortReviewServiceStub, cohortReviewStub} from 'testing/stubs/cohort-review-service-stub';
-import {CohortsServiceStub} from 'testing/stubs/cohort-service-stub';
+import {CohortsApiStub} from 'testing/stubs/cohorts-api-stub';
 import {workspaceDataStub} from 'testing/stubs/workspace-storage-service-stub';
 import {PageLayout} from './page-layout';
 
-import {CohortReviewService, CohortsService} from 'generated';
+import {CohortBuilderService, CriteriaListResponse} from 'generated';
+import {CohortReviewApi, CohortsApi} from 'generated/fetch';
+import {Observable} from 'rxjs/Observable';
+
+class BuilderApiStub {
+  getCriteriaBy(): Observable<CriteriaListResponse> {
+    return Observable.of({items: []});
+  }
+}
 
 describe('PageLayout', () => {
   let component: PageLayout;
   let fixture: ComponentFixture<PageLayout>;
 
   beforeEach(async(() => {
+    registerApiClient(CohortReviewApi, new CohortReviewServiceStub());
+    registerApiClient(CohortsApi, new CohortsApiStub());
 
     TestBed.configureTestingModule({
       declarations: [
@@ -26,8 +37,7 @@ describe('PageLayout', () => {
       ],
       imports: [ClarityModule, ReactiveFormsModule, RouterTestingModule],
       providers: [
-        {provide: CohortReviewService, useValue: new CohortReviewServiceStub()},
-        {provide: CohortsService, useValue: new CohortsServiceStub()},
+        {provide: CohortBuilderService, useValue: new BuilderApiStub()}
       ],
     })
       .compileComponents();
