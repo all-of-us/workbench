56719c76c51f572feb72d5d632a87baf4cf0bf36
Scout Rubenstein <brubenst@broadinstitute.org>
2019-03-12 16:40:14 -0400

    [No Ticket][Risk=no] Remove old, unused dependencies

--- ui/src/typings.d.ts
+++ ui/src/typings.d.ts
@@ -10,6 +10,4 @@ This gets included by the compiler; its symbols do not need to be imported by
 other source files but are globally available.
 */
 
-declare module "dom-to-image";
-
 declare var ResizeObserver: any;
