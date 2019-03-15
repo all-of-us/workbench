9b6d40e4f985eef4216e7d3c3dd60505f36ffa9a
blrubenstein <blrubenstein@wpi.edu>
2019-03-15 17:40:44 -0400

    [RW-2127][Risk=no] Srubenst/notebook policy reminder text (#1923)
    * Add reminder icon
    
    * Add policy reminder
    
    * Add comment
    
    * Make lint more specific
    
    * Attempt testing fix
    
    * Fix tests
    
    * Trigger circle

--- ui/src/app/icons/icons.module.ts
+++ ui/src/app/icons/icons.module.ts
@@ -3,6 +3,7 @@ import {NgModule} from '@angular/core';
 import {EditComponent} from 'app/icons/edit/component';
 import {ExpandComponent} from 'app/icons/expand/component';
 import {NotebookComponent} from 'app/icons/notebook/component';
+import {ReminderComponent} from 'app/icons/reminder/component';
 import {ShareComponent} from 'app/icons/share/component';
 import {ShrinkComponent} from 'app/icons/shrink/component';
 import {TrashComponent} from 'app/icons/trash/component';
@@ -14,6 +15,7 @@ import {TrashComponent} from 'app/icons/trash/component';
     EditComponent,
     ExpandComponent,
     NotebookComponent,
+    ReminderComponent,
     ShareComponent,
     ShrinkComponent
   ],
@@ -22,6 +24,7 @@ import {TrashComponent} from 'app/icons/trash/component';
     EditComponent,
     ExpandComponent,
     NotebookComponent,
+    ReminderComponent,
     ShareComponent,
     ShrinkComponent
   ],
