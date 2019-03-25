7b2f62f25dede1d867fc9f0a5688e900ab87356a
Gregory Jordan <gjordan@verily.com>
2019-03-25 16:10:35 -0400

    Fix Codacy errors.

--- api/src/main/java/org/pmiops/workbench/api/OfflineUserController.java
+++ api/src/main/java/org/pmiops/workbench/api/OfflineUserController.java
@@ -9,7 +9,6 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.io.IOException;
-import java.util.List;
 import java.util.logging.Logger;
 
 /**
--- api/src/test/java/org/pmiops/workbench/db/dao/UserServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/db/dao/UserServiceTest.java
@@ -1,6 +1,5 @@
 package org.pmiops.workbench.db.dao;
 
-import org.junit.After;
 import org.junit.Before;
 import org.junit.Test;
 import org.junit.runner.RunWith;
