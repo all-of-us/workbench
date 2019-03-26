87bf6dbe1642bb48d0b16d52c0bdb78871358ba4
Eric Song <songe@wm73e-d52.broadinstitute.org>
2019-03-26 17:15:29 -0400

    have a banner show up if a non chrome browser is detected

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,6 +3,7 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {environment} from 'environments/environment';
+import outdatedBrowserRework from 'outdated-browser-rework';
 import * as ReactModal from 'react-modal';
 
 import {AppModule} from 'app/app.module';
@@ -17,3 +18,33 @@ setupCustomValidators();
 ReactModal.defaultStyles.overlay = {};
 ReactModal.defaultStyles.content = {};
 platformBrowserDynamic().bootstrapModule(AppModule);
+checkBrowserSupport();
+
+function checkBrowserSupport() {
+  outdatedBrowserRework({
+    browserSupport: {
+      Chrome: 67, // Includes Chrome for mobile devices
+      Edge: false,
+      Safari: false,
+      "Mobile Safari": false,
+      Opera: false,
+      Firefox: false,
+      Vivaldi: false,
+      IE: false
+    },
+    isUnknownBrowserOK: false,
+    messages: {
+      en: {
+        outOfDate: "Terra may not function correctly in this browser.",
+        update: {
+          web: "If you experience issues, please try " + (!!window['chrome']? "updating" : "using") + " Google Chrome.",
+          googlePlay: "Please install Chrome from Google Play",
+          appStore: "Please update iOS from the Settings App"
+        },
+        url: "https://www.google.com/chrome/",
+        callToAction: (!!window['chrome'] ? "Update" : "Download") + " Chrome now",
+        close: "Close"
+      }
+    }
+  });
+}
