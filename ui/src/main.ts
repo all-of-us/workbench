c75dde9ab3c45e13a5c80cd7decc43249d769c92
Eric Song <songe@broadinstitute.org>
2019-03-28 10:17:42 -0400

    move browser check to app/component

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,7 +3,6 @@
 import {enableProdMode} from '@angular/core';
 import {platformBrowserDynamic} from '@angular/platform-browser-dynamic';
 import {environment} from 'environments/environment';
-import outdatedBrowserRework from 'outdated-browser-rework';
 import * as ReactModal from 'react-modal';
 
 import {AppModule} from 'app/app.module';
@@ -18,36 +17,3 @@ setupCustomValidators();
 ReactModal.defaultStyles.overlay = {};
 ReactModal.defaultStyles.content = {};
 platformBrowserDynamic().bootstrapModule(AppModule);
-checkBrowserSupport();
-
-function checkBrowserSupport() {
-  const minChromeVersion = 67;
-
-  outdatedBrowserRework({
-    browserSupport: {
-      Chrome: minChromeVersion, // Includes Chrome for mobile devices
-      Edge: false,
-      Safari: false,
-      'Mobile Safari': false,
-      Opera: false,
-      Firefox: false,
-      Vivaldi: false,
-      IE: false
-    },
-    isUnknownBrowserOK: false,
-    messages: {
-      en: {
-        outOfDate: 'Researcher Workbench may not function correctly in this browser.',
-        update: {
-          web: `If you experience issues, please install Google Chrome \
-          version ${minChromeVersion} or greater.`,
-          googlePlay: 'Please install Chrome from Google Play',
-          appStore: 'Please install Chrome from the App Store'
-        },
-        url: 'https://www.google.com/chrome/',
-        callToAction: 'Download Chrome now',
-        close: 'Close'
-      }
-    }
-  });
-}
