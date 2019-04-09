e0e27bdf86cae0390af491287ea1e3587dbb9a8e
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-04-09 15:02:41 -0400

    [RW-2006][risk=no] Remove dead code after zendesk release (#2000)

--- ui/src/app/views/bug-report/component.spec.tsx
+++ ui/src/app/views/bug-report/component.spec.tsx
@@ -2,9 +2,8 @@ import {mount} from 'enzyme';
 import * as React from 'react';
 
 import {BugReportModal} from './component';
-import {BugReportApiStub, EMPTY_BUG_REPORT} from 'testing/stubs/bug-report-api-stub';
-import {bugReportApi, profileApi, registerApiClient} from 'app/services/swagger-fetch-clients';
-import {BugReportApi, ProfileApi} from "generated/fetch/api";
+import {registerApiClient} from 'app/services/swagger-fetch-clients';
+import {ProfileApi} from 'generated/fetch/api';
 import {Profile} from 'generated';
 import {ProfileApiStub} from 'testing/stubs/profile-api-stub';
 import {ProfileStubVariables} from 'testing/stubs/profile-service-stub';
@@ -14,11 +13,6 @@ import {userProfileStore} from 'app/utils/navigation';
 describe('BugReport', () => {
   const description = 'test';
   const profile = ProfileStubVariables.PROFILE_STUB as unknown as Profile;
-  const bugReport = {
-    ...EMPTY_BUG_REPORT,
-    shortDescription: description,
-    contactEmail: profile.username
-  };
   let profileApi: ProfileApiStub;
   const reload = jest.fn();
 
@@ -31,7 +25,6 @@ describe('BugReport', () => {
 
   beforeEach(() => {
     registerApiClient(ProfileApi, new ProfileApiStub());
-    registerApiClient(BugReportApi, new BugReportApiStub());
 
     // mocking because we don't have access to the angular service
     reload.mockImplementation(async () => {
@@ -42,13 +35,9 @@ describe('BugReport', () => {
     userProfileStore.next({profile, reload});
   });
 
-  it('submits a bug report', () => {
-    const spy = jest.spyOn(bugReportApi(), 'sendBugReport');
+  it('should render', () => {
     const wrapper = component();
     expect(wrapper.exists()).toBeTruthy();
-    wrapper.find('[data-test-id="submit-bug-report"]').first().simulate('click');
-    expect(spy).toHaveBeenCalledWith(bugReport);
-
   });
 
 });
