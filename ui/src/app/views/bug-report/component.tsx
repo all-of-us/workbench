119253bd19512608124bbe9096b5e871d6fa84b3
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-04-09 12:45:40 -0400

    remove unused code

--- ui/src/app/views/bug-report/component.tsx
+++ ui/src/app/views/bug-report/component.tsx
@@ -8,69 +8,19 @@ import {Button} from 'app/components/buttons';
 import {Modal, ModalBody, ModalFooter, ModalTitle} from 'app/components/modals';
 import {openZendeskWidget} from 'app/utils/zendesk';
 import {
-  BugReport,
-  BugReportType,
   Profile
 } from 'generated/fetch';
 
-
-const styles = reactStyles({
-  fieldHeader: {
-    fontSize: 14,
-    color: '#262262',
-    fontWeight: 200,
-    display: 'block',
-    marginTop: '1rem'
-  },
-  field: {
-    fontWeight: 400,
-    width: '100%'
-  }
-});
 
 export const BugReportModal = withUserProfile()
 (class extends React.Component<
     {profileState: { profile: Profile, reload: Function },
       bugReportDescription: string, onClose: Function},
-    {bugReport: BugReport, submitting: boolean,
-      sendBugReportError: boolean}> {
-
-  private emptyBugReport = {
-    bugType: BugReportType.APPLICATION,
-    shortDescription: '',
-    reproSteps: '',
-    includeNotebookLogs: true,
-    contactEmail: ''
-  };
+    {}> {
 
   constructor(props) {
     super(props);
-    this.state = {
-      submitting: false,
-      sendBugReportError: false,
-      bugReport: {...this.emptyBugReport,
-        shortDescription: props.bugReportDescription || '',
-        contactEmail: props.profileState.profile.username
-      }
-    };
   }
-
-  // TODO: these are unused for now. Reenable if we want to submit to zendesk through modal.
-  // send() {
-  //   this.setState({submitting: true, sendBugReportError: false});
-  //   bugReportApi().sendBugReport(this.state.bugReport).then(() => {
-  //     this.setState({submitting: false});
-  //     this.props.onClose();
-  //   }).catch(() => {
-  //     this.setState({sendBugReportError: true, submitting: false});
-  //   });
-  // }
-  //
-  // updateBugReport(attribute: string, value: any) {
-  //   const newReport = this.state.bugReport;
-  //   newReport[attribute] = value;
-  //   this.setState(({bugReport}) => ({bugReport: fp.set(attribute, value, bugReport)}));
-  // }
 
   openZendesk() {
     const {profileState: {profile}} = this.props;
@@ -79,48 +29,16 @@ export const BugReportModal = withUserProfile()
   }
 
   render() {
-    const {bugReport, bugReport: {shortDescription}, submitting, sendBugReportError} = this.state;
-    const {onClose} = this.props;
-    const errors = validate({shortDescription}, {
+    const {bugReportDescription, onClose} = this.props;
+    const errors = validate({bugReportDescription}, {
       shortDescription: {presence: {allowEmpty: false}}
     });
 
-    return <Modal width={700} loading={submitting}>
+    return <Modal>
       <ModalTitle style={{fontWeight: 400}}>Something went wrong.</ModalTitle>
       <ModalBody>
-        {shortDescription}
+        {bugReportDescription}
         Please submit a bug report to the help desk.
-        {/*<label style={styles.fieldHeader}>Description: </label>*/}
-        {/*<TextInput value={shortDescription}*/}
-                    {/*onChange={v => this.updateBugReport('shortDescription', v)}/>*/}
-        {/*<ValidationError>*/}
-          {/*{summarizeErrors(errors && errors.shortDescription)}*/}
-        {/*</ValidationError>*/}
-        {/*<label style={styles.fieldHeader}>Bug Type: </label>*/}
-        {/*<select style={{height: '1.5rem', width: '100%'}}*/}
-                {/*onChange={e => this.updateBugReport('bugReportType', e.target.value)}>*/}
-          {/*<option value={BugReportType.APPLICATION}>Workbench Application</option>*/}
-          {/*<option value={BugReportType.DATA}>Data Quality</option>*/}
-        {/*</select>*/}
-        {/*<label style={styles.fieldHeader}>How to Reproduce: </label>*/}
-        {/*<TextArea onChange={v => this.updateBugReport('reproSteps', v) }/>*/}
-        {/*<div style={{display: 'flex', flexDirection: 'row', marginTop: '1rem'}}>*/}
-          {/*<input type='checkbox'*/}
-                 {/*defaultChecked={bugReport.includeNotebookLogs}*/}
-                 {/*onClick={() => this.updateBugReport('includeNotebookLogs',*/}
-                   {/*!bugReport.includeNotebookLogs)}*/}
-          {/*/>*/}
-          {/*<label style={{...styles.fieldHeader, marginTop: '0rem', marginLeft: '0.2rem'}}>*/}
-            {/*Attach notebook logs? </label>*/}
-        {/*</div>*/}
-        {/*<label style={styles.fieldHeader}>Contact Email: </label>*/}
-        {/*<TextInput value={bugReport.contactEmail}*/}
-                    {/*onChange={e => this.updateBugReport('contactEmail', e)}/>*/}
-        {/*{sendBugReportError && <AlertDanger style={{justifyContent: 'space-between'}}>*/}
-          {/*<div>Error sending the bug report.</div>*/}
-          {/*<ClrIcon shape='times'*/}
-                   {/*onClick={() => this.setState({sendBugReportError: false})}/>*/}
-        {/*</AlertDanger>}*/}
         <ModalFooter>
           <Button type='secondary' onClick={onClose}>Cancel</Button>
           <Button style={{marginLeft: '0.5rem'}}
