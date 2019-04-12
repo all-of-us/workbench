3f3b77490f8187aa01e11297b8254870b7fff007
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro.local>
2019-04-12 12:25:34 -0500

    RW-2403 fix tooltip trigger

--- ui/src/app/cohort-review/participants-charts/participants-charts.tsx
+++ ui/src/app/cohort-review/participants-charts/participants-charts.tsx
@@ -137,7 +137,7 @@ export const ParticipantsCharts = withCurrentWorkspace()(
 
     checkWidth = (i: number) => {
       const el = this.nameRefs[i].current;
-      return el ? el.offsetWidth >= el.scrollWidth : false;
+      return el ? el.offsetWidth >= el.scrollWidth : true;
     }
 
     render() {
