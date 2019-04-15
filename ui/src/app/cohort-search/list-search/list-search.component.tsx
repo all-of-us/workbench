84a1aa14ace40af3a08325349808f4f73c40069a
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-04-15 15:27:11 -0500

    RW-2284 create search-state service

--- ui/src/app/cohort-search/list-search/list-search.component.tsx
+++ ui/src/app/cohort-search/list-search/list-search.component.tsx
@@ -1,6 +1,8 @@
 import {Component} from '@angular/core';
+import {cohortBuilderApi} from 'app/services/swagger-fetch-clients';
 import {WorkspaceData} from 'app/services/workspace-storage.service';
 import {ReactWrapperBase, withCurrentWorkspace} from 'app/utils';
+import {DomainType} from 'generated/fetch';
 import * as React from 'react';
 
 export const ListSearch = withCurrentWorkspace()(
@@ -8,6 +10,13 @@ export const ListSearch = withCurrentWorkspace()(
     constructor(props: any) {
       super(props);
       this.state = {};
+    }
+
+    componentDidMount(): void {
+      const {cdrVersionId} = this.props.workspace;
+      cohortBuilderApi().findCriteriaByDomainAndSearchTerm(
+        +cdrVersionId, DomainType[DomainType.MEASUREMENT], 'amino', true
+      ).then(resp => console.log(resp));
     }
 
     render() {
