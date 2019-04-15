ecf7a8a82e69cea5519fb268cba64f231e0b0609
Brian Freeman <brian.d.freeman@gmail.com>
2019-04-15 13:40:16 -0500

    RW-2473 adding new calls.

--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
@@ -4,18 +4,19 @@ import org.pmiops.workbench.cdr.model.CBCriteria;
 import org.springframework.data.domain.Pageable;
 import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 import java.util.List;
 
 public interface CBCriteriaDao extends CrudRepository<CBCriteria, Long> {
 
   @Query(value = "select cr from CBCriteria cr " +
-    "    where cr.domainId = ?1 " +
-    "    and cr.standard = ?2 " +
-    "    and match(synonyms, ?3) > 0 " +
-    "    order by cr.count desc")
-  List<CBCriteria> findCriteriaByDomainAndSearchTerm(String domain,
-                                                   Boolean isStandard,
-                                                   String term,
-                                                   Pageable page);
+    "where cr.domainId = :domain " +
+    "and cr.standard = :standard " +
+    "and match(synonyms, :term) > 0 " +
+    "order by cr.count desc")
+  List<CBCriteria> findCriteriaByDomainAndSearchTerm(@Param("domain") String domain,
+                                                     @Param("standard") Boolean isStandard,
+                                                     @Param("term") String term,
+                                                     Pageable page);
 }
