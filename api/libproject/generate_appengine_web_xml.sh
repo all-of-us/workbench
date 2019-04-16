d47fdb8d27eeacb477906e240742cb11f63d173e
Eric Song <songe@broadinstitute.org>
2019-04-16 16:02:55 -0400

    fix linting

--- api/libproject/generate_appengine_web_xml.sh
+++ api/libproject/generate_appengine_web_xml.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-for line in `awk '!/^ *#/ && NF' db/vars.env`; do
+for line in $(awk '!/^ *#/ && NF' db/vars.env); do
   IFS='=' read -r var val <<< "$line"
 
   if [ "$var" == "DB_HOST" ] && [ $OVERWRITE_WORKBENCH_DB_HOST ]; then
@@ -16,8 +16,8 @@ done
 set -euo pipefail
 IFS=$'\n\t'
 
-cat src/main/webapp/WEB-INF/appengine-web.xml.template \
-  | sed "s|\${DB_DRIVER}|${DB_DRIVER}|" \
+< src/main/webapp/WEB-INF/appengine-web.xml.template \
+  sed "s|\${DB_DRIVER}|${DB_DRIVER}|" \
   | sed "s|\${DB_CONNECTION_STRING}|${DB_CONNECTION_STRING}|" \
   | sed "s|\${WORKBENCH_DB_USER}|${WORKBENCH_DB_USER}|" \
   | sed "s|\${WORKBENCH_DB_PASSWORD}|${WORKBENCH_DB_PASSWORD}|" \
