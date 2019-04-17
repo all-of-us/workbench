9dfab358196a96a7b2f614e274f0c5025369a8db
Eric Song <songe@broadinstitute.org>
2019-04-17 14:03:22 -0400

    trying different quotation styling

--- api/libproject/generate_appengine_web_xml.sh
+++ api/libproject/generate_appengine_web_xml.sh
@@ -14,7 +14,7 @@ for line in $(awk '!/^ *#/ && NF' db/vars.env); do
   echo "Exporting value $var=$evaluatedString"
   export $var=$evaluatedString
 
-  var="sed -i '' 's|\${$var}|$evaluatedString|g' ./src/main/webapp/WEB-INF/appengine-web.xml"
+  var="sed -i \"\" 's|\${$var}|$evaluatedString|g' ./src/main/webapp/WEB-INF/appengine-web.xml"
   eval $var
 
 done
