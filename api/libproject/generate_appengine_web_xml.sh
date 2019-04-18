3dafcf60268658bf0d4e248d8ce2cff97d455f9b
Eric Song <songe@broadinstitute.org>
2019-04-18 12:06:47 -0400

    log appengine-web.xml

--- api/libproject/generate_appengine_web_xml.sh
+++ api/libproject/generate_appengine_web_xml.sh
@@ -4,3 +4,6 @@ IFS=$'\n\t'
 
 envsubst < src/main/webapp/WEB-INF/appengine-web.xml.template \
   > src/main/webapp/WEB-INF/appengine-web.xml
+
+echo "Generated appengine-web.xml"
+cat src/main/webapp/WEB-INF/appengine-web.xml
