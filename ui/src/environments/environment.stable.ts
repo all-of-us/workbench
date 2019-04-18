7933f524acc94428ab0480a4c102335c7bd8359f
CH Albach <calbach@users.noreply.github.com>
2019-04-18 09:37:06 -0700

    [RW-1975][risk=no] Drop unused environment property (#2013)

--- ui/src/environments/environment.stable.ts
+++ ui/src/environments/environment.stable.ts
@@ -8,7 +8,6 @@ export const environment: Environment = {
   tcellappid: 'AoUNonProd-WZFW2',
   tcellapikey: 'AQEBBAEkx4iE2KxNyI7Wx08EwU1ycTM7E4FMSmaibbMUQxNU6uQvuAJt7fyABAtFYSYfgEE',
   leoApiUrl: 'https://notebooks.firecloud.org',
-  publicApiUrl: 'https://public-api-dot-all-of-us-rw-stable.appspot.com',
   publicUiUrl: 'https://www.databrowser.stable.fake-research-aou.org',
   debug: false,
   gaId: 'UA-112406425-3',
