4eecb16a63018230a1c3f00539bfff14e14fa09c
Eric Song <songe@broadinstitute.org>
2019-04-19 15:40:02 -0400

    Revert "API Server start speed up - take 2 (#2016)"
    This reverts commit 997cf2a2bbebf44088e7aa2ee6bbd42d54b8048d.

--- api/libproject/generate_appengine_web_xml.rb
+++
@@ -1,6 +0,0 @@
-xml = File.read("src/main/webapp/WEB-INF/appengine-web.xml.template")
-ENV.each { |k, v| xml.gsub! "${#{k}}", v }
-File.write("src/main/webapp/WEB-INF/appengine-web.xml", xml)
-
-puts "Generated appengine-web.xml"
-puts xml
