f554826ead54e551b0c2cd12949502b40b314f21
Scout Rubenstein <brubenst@broadinstitute.org>
2019-04-19 15:18:14 -0400

    [RW-2325][Risk=no] Move synonyms

--- ui/src/app/views/concept-table/component.tsx
+++ ui/src/app/views/concept-table/component.tsx
@@ -1,7 +1,8 @@
 import {Component, Input} from '@angular/core';
-import {Clickable} from 'app/components/buttons';
+import {Clickable, Link} from 'app/components/buttons';
+import {ClrIcon} from 'app/components/icons';
 import {CheckBox} from 'app/components/inputs';
-import {PopupTrigger} from 'app/components/popups';
+import {PopupTrigger, TooltipTrigger} from 'app/components/popups';
 import colors from 'app/styles/colors';
 import {ReactWrapperBase, toggleIncludes} from 'app/utils';
 import {reactStyles} from 'app/utils';
@@ -18,10 +19,60 @@ const styles = reactStyles({
   }
 });
 
+export class SynonymsObject extends React.Component<{},
+    {seeMore: boolean, willOverflow: boolean}> {
+  domElement: any;
+  constructor(props) {
+    super(props);
+    this.state = {seeMore: false, willOverflow: false};
+  }
 
-export class ConceptTable extends React.Component<{concepts: Concept[];
-  loading: boolean; placeholderValue: string, getSelectedConcepts: Function,
-  setSelectedConcepts: Function; },
+  componentDidMount() {
+    const element = this.domElement;
+    const hasOverflowingChildren = element.offsetHeight < element.scrollHeight ||
+      element.offsetWidth < element.scrollWidth;
+    this.setState({willOverflow: hasOverflowingChildren});
+  }
+
+  render() {
+    return <div style={{display: 'flex'}}>
+      <div style={{minWidth: '170px', maxWidth: '170px',
+        fontStyle: 'italic',
+        color: colors.gray[2]}}>
+        Also Known As:
+        <TooltipTrigger
+          side='top'
+          content='Medical concepts often have alternative names and descriptions,
+            known as synonyms. Alternate names and descriptions, if available, are
+            listed for each medical concept'>
+          <ClrIcon
+            shape='info-standard'
+            className='is-solid'
+            style={{marginLeft: 10, verticalAlign: 'middle', color: colors.blue[2]}}
+          />
+        </TooltipTrigger>
+      </div>
+      <div style={{
+        textOverflow: this.state.seeMore ? 'auto' : 'hidden',
+        minWidth: '810px',
+        maxWidth: '810px',
+        height: this.state.seeMore ? 'auto' : '1rem',
+        overflow: this.state.seeMore ? 'auto' : 'hidden'
+      }} ref={el => this.domElement = el}>
+        {this.props.children}
+      </div>
+      {this.state.willOverflow ?
+        <Link onClick={() => this.setState({seeMore: !this.state.seeMore})}>
+          {this.state.seeMore ? 'See Less' : 'See More...'}
+        </Link> : null}
+    </div>;
+  }
+}
+
+
+export class ConceptTable extends React.Component<{concepts: Concept[],
+  getSelectedConcepts: Function, loading: boolean, placeholderValue: string,
+  reactKey: string, setSelectedConcepts: Function},
   {selectedConcepts: Concept[]; selectedVocabularies: string[]; }> {
 
   private dt: DataTable;
@@ -34,10 +85,6 @@ export class ConceptTable extends React.Component<{concepts: Concept[];
       selectedVocabularies: []
     };
     this.filterImageSrc = 'filter';
-  }
-
-  conceptSynonymColTemplate(rowData) {
-    return <div>{fp.uniq(rowData.conceptSynonyms).join(', ')}</div>;
   }
 
   updateSelectedConceptList(selectedConcepts) {
@@ -72,19 +119,14 @@ export class ConceptTable extends React.Component<{concepts: Concept[];
   }
 
   rowExpansionTemplate(data) {
-    return (<div style={{display: 'flex'}}>
-      <div style={{minWidth: '170px', maxWidth: '170px',
-        fontStyle: 'italic',
-        color: colors.gray[2]}}>
-        Also Known As:
-      </div>
-      <div>{data.conceptSynonyms}</div>
-    </div>);
+    return (<SynonymsObject>
+      {fp.uniq(data.conceptSynonyms).join(', ')}
+    </SynonymsObject>);
   }
 
   render() {
     const {selectedConcepts, selectedVocabularies} = this.state;
-    const {concepts, placeholderValue, loading} = this.props;
+    const {concepts, placeholderValue, loading, reactKey} = this.props;
     const vocabularyFilter = <PopupTrigger
         side='bottom'
         content={
@@ -103,18 +145,17 @@ export class ConceptTable extends React.Component<{concepts: Concept[];
              src={'/assets/icons/' + this.filterImageSrc + '.svg'}/>
       </Clickable>
     </PopupTrigger>;
-    return <div data-test-id='conceptTable'>
+    return <div data-test-id='conceptTable' key={reactKey}>
       <DataTable emptyMessage={loading ? '' : placeholderValue} ref={(el) => this.dt = el}
                  value={concepts} paginator={true} rows={50} scrollable={true} loading={loading}
-                 selection={selectedConcepts}
-                 expandedRows={this.props.concepts}
+                 selection={selectedConcepts} style={{minWidth: 1100}}
+                 expandedRows={this.props.concepts
+                   .filter(concept => concept.conceptSynonyms.length > 0)}
                  rowExpansionTemplate={this.rowExpansionTemplate}
                  onSelectionChange={e => this.updateSelectedConceptList(e.value)} >
       <Column bodyStyle={{...styles.colStyle, width: '3rem'}} headerStyle = {{width: '3rem'}}
               selectionMode='multiple' />
       <Column bodyStyle={styles.colStyle} field='conceptName' header='Name'/>
-      <Column bodyStyle={styles.colStyle} field='conceptSynonyms' header='Synonyms'
-              body={this.conceptSynonymColTemplate}/>
       <Column bodyStyle={styles.colStyle} field='conceptCode' header='Code'/>
       <Column field='vocabularyId' header='Vocabulary' bodyStyle={styles.colStyle}
               filter={true} headerStyle={{display: 'flex', textAlign: 'center', paddingTop: '0.6rem'
@@ -134,14 +175,15 @@ export class ConceptTableComponent extends ReactWrapperBase {
   @Input() concepts: Object[];
   @Input() getSelectedConcepts;
   @Input() loading = false;
-  @Input() searchTerm = '';
   @Input() placeholderValue = '';
+  @Input() reactKey;
+  @Input() searchTerm = '';
 
   selectedConcepts: Array<any> = [];
 
   constructor() {
-    super(ConceptTable, ['concepts', 'loading', 'placeholderValue', 'getSelectedConcepts',
-      'setSelectedConcepts']);
+    super(ConceptTable, ['concepts', 'getSelectedConcepts', 'loading', 'placeholderValue',
+      'reactKey', 'setSelectedConcepts']);
     this.setSelectedConcepts = this.setSelectedConcepts.bind(this);
   }
 
