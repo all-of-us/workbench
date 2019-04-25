dec726e9738f85ef9020ec22bb7d463368201c78
Brian Freeman <brian.d.freeman@gmail.com>
2019-04-25 13:12:06 -0500

    RW-2542 adding implementation that calls new criteria attributes.

---
+++ api/src/test/java/org/pmiops/workbench/cdr/dao/CBCriteriaAttributeDaoTest.java
@@ -0,0 +1,42 @@
+package org.pmiops.workbench.cdr.dao;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.pmiops.workbench.cdr.model.CBCriteriaAttribute;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.context.annotation.Import;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+import static org.junit.Assert.assertEquals;
+
+@RunWith(SpringRunner.class)
+@DataJpaTest
+@Import(LiquibaseAutoConfiguration.class)
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+@Transactional
+public class CBCriteriaAttributeDaoTest {
+
+  @Autowired
+  private CBCriteriaAttributeDao cbCriteriaAttributeDao;
+
+  @Test
+  public void findCriteriaAttributeByConceptId() throws Exception {
+    CBCriteriaAttribute attribute = new CBCriteriaAttribute()
+      .conceptId(1L)
+      .conceptName("test")
+      .estCount("10")
+      .type("type")
+      .valueAsConceptId(12345678L);
+    cbCriteriaAttributeDao.save(attribute);
+    List<CBCriteriaAttribute> attributes =
+      cbCriteriaAttributeDao.findCriteriaAttributeByConceptId(1L);
+    assertEquals(1, attributes.size());
+    assertEquals(attribute, attributes.get(0));
+  }
+}
