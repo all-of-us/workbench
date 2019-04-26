9b6f5380ba66feb11044182890737523c33fde90
Brian Freeman <brian.d.freeman@gmail.com>
2019-04-26 12:07:11 -0500

    RW-2542 removed unused import.

--- api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaAttributeDao.java
+++ api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaAttributeDao.java
@@ -1,7 +1,6 @@
 package org.pmiops.workbench.cdr.dao;
 
 import org.pmiops.workbench.cdr.model.CBCriteriaAttribute;
-import org.pmiops.workbench.cdr.model.CriteriaAttribute;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
