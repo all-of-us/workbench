16ca2def8ce97355d1804088d273dbd64c3a3871
Brian Freeman <brian.d.freeman@gmail.com>
2019-05-02 16:36:10 -0500

    RW-2524 adding test cases.

--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
@@ -8,6 +8,7 @@ import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.query.Param;
 
 import java.util.List;
+import java.util.Set;
 
 public interface CBCriteriaDao extends CrudRepository<CBCriteria, Long> {
 
@@ -55,6 +56,6 @@ public interface CBCriteriaDao extends CrudRepository<CBCriteria, Long> {
                                                     @Param("limit") Long limit);
 
   @Query(value = "select * from cb_criteria c inner join ( select cr.concept_id_2 from criteria_relationship cr join concept c1 on (cr.concept_id_2 = c1.concept_id " +
-    "and cr.concept_id_1 = :conceptId and c1.concept_class_id = 'Ingredient') ) cr1 on c.concept_id = cr1.concept_id_2", nativeQuery = true)
-  List<CBCriteria> findDrugIngredientByConceptId(@Param("conceptId") Long conceptId);
+    "and cr.concept_id_1 in (:conceptIds) and c1.concept_class_id = 'Ingredient') ) cr1 on c.concept_id = cr1.concept_id_2 and c.domain_id = 'DRUG' and c.type = 'RXNORM'", nativeQuery = true)
+  List<CBCriteria> findDrugIngredientByConceptId(@Param("conceptIds") List<Long> conceptIds);
 }
