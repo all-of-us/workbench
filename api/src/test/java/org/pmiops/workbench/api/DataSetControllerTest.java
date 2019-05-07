d87cfb6d7e7cf620fb5bff8fc48355b0fc377b10
blrubenstein <brubenst@broadinstitute.org>
2019-05-07 13:33:42 -0400

    [RW-2250][Risk=low] Notebook export data set (#2079)
    * Notebook export data set
    
    * Fix codacy
    
    * Fix tests in api
    
    * Fix UI tests
    
    * Fix tests
    
    * PR feedback
    
    * Remove unused code
    
    * Fix lint
    
    * Fix ui tests

--- api/src/test/java/org/pmiops/workbench/api/DataSetControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/DataSetControllerTest.java
@@ -219,8 +219,8 @@ public class DataSetControllerTest {
   @Before
   public void setUp() throws Exception {
     dataSetService = new DataSetServiceImpl(bigQueryService, cdrBigQuerySchemaConfigService, cohortDao, conceptSetDao, participantCounter);
-    dataSetController = new DataSetController(bigQueryService, CLOCK,
-        cohortDao, conceptDao, conceptSetDao, dataSetService, userProvider, workspaceService);
+    dataSetController = new DataSetController(bigQueryService, CLOCK, cloudStorageService,
+        cohortDao, conceptDao, conceptSetDao, dataSetService, fireCloudService, userProvider, workspaceService);
     WorkspacesController workspacesController =
         new WorkspacesController(workspaceService, workspaceMapper, cdrVersionDao, cohortDao, cohortFactory, conceptSetDao, userDao,
             userProvider, fireCloudService, cloudStorageService, CLOCK, notebooksService, userService);
@@ -417,7 +417,7 @@ public class DataSetControllerTest {
     assertThat(response.getQueryList().get(0).getQuery())
         .isEqualTo("SELECT PERSON_ID FROM " +
             "`all-of-us-ehr-dev.synthetic_cdr20180606.condition_occurrence` " +
-            "c_occurrence WHERE (condition_concept_id IN () OR condition_source_concept_id IN ()) " +
+            "c_occurrence WHERE \n(condition_concept_id IN () OR \ncondition_source_concept_id IN ()) \n" +
             "AND (PERSON_ID IN (SELECT * FROM person_id from `all-of-us-ehr-dev.synthetic_cdr20180606.person` person))");
   }
 
