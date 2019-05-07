277eafca52af8f74dfb8b6257772667e46c4a17b
CH Albach <calbach@users.noreply.github.com>
2019-05-07 15:42:38 -0700

    [risk=no][RW-2508] Prevent bad view/workspace caching (#2081)

--- ui/src/app/utils/navigation.tsx
+++ ui/src/app/utils/navigation.tsx
@@ -1,6 +1,9 @@
+import {ActivatedRouteSnapshot, DetachedRouteHandle, RouteReuseStrategy} from '@angular/router';
+
 import {WorkspaceData} from 'app/services/workspace-storage.service';
 import {Profile} from 'generated';
 import {Cohort, ConceptSet} from 'generated/fetch';
+import * as fp from 'lodash/fp';
 import {BehaviorSubject} from 'rxjs/BehaviorSubject';
 
 export const NavStore = {
@@ -20,6 +23,28 @@ export const userProfileStore =
     reload: () => {},
     updateCache: (profile) => {},
   });
+
+
+/**
+ * Slightly stricter variant of Angular's DefaultRouteReuseStrategy. This
+ * strategy requires identical params for route reuse, in addition to route
+ * config matching.
+ *
+ * This avoid bugs from invalid parameter assumptions in views and for the most
+ * part should not affect performance, based on parameter usage in the
+ * application.
+ */
+export class WorkbenchRouteReuseStrategy extends RouteReuseStrategy {
+  // The following methods are copied from Angular's DefaultRouteReuseStrategy.
+  shouldDetach(route: ActivatedRouteSnapshot): boolean { return false; }
+  store(route: ActivatedRouteSnapshot, detachedTree: DetachedRouteHandle): void {}
+  shouldAttach(route: ActivatedRouteSnapshot): boolean { return false; }
+  retrieve(route: ActivatedRouteSnapshot): DetachedRouteHandle|null { return null; }
+
+  shouldReuseRoute(future: ActivatedRouteSnapshot, curr: ActivatedRouteSnapshot): boolean {
+    return future.routeConfig === curr.routeConfig && fp.isEqual(future.params, curr.params);
+  }
+}
 
 // NOTE: Because these are wired up directly to the router component,
 // all navigation done from here will effectively use absolute paths.
