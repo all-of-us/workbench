5fe68d2df115d88a6887fbe08f586c8140dd502e
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-05-09 12:09:53 -0400

    wip

--- ui/src/app/views/concept-set-details/component.tsx
+++ ui/src/app/views/concept-set-details/component.tsx
@@ -2,32 +2,82 @@ import {Component} from '@angular/core';
 import * as fp from 'lodash/fp';
 import * as React from 'react';
 
+import {Button, Clickable, MenuItem} from 'app/components/buttons';
 import {FadeBox} from 'app/components/containers';
+import {ClrIcon} from 'app/components/icons';
+import {TextArea, TextInput} from 'app/components/inputs';
+import {Modal, ModalFooter, ModalTitle} from 'app/components/modals';
+import {PopupTrigger, TooltipTrigger} from 'app/components/popups';
 import {SpinnerOverlay} from 'app/components/spinners';
+import {EditComponentReact} from 'app/icons/edit/component';
 import {conceptSetsApi} from 'app/services/swagger-fetch-clients';
 import {WorkspaceData} from 'app/services/workspace-storage.service';
 import {reactStyles, ReactWrapperBase, withCurrentWorkspace, withUrlParams} from 'app/utils';
+import {navigate} from 'app/utils/navigation';
+import {ResourceType} from 'app/utils/resourceActionsReact';
 import {ConceptTable} from 'app/views/concept-table/component';
+import {ConfirmDeleteModal} from 'app/views/confirm-delete-modal/component';
 import {Concept, ConceptSet, WorkspaceAccessLevel} from 'generated/fetch';
 
 const styles = reactStyles({
   conceptSetHeader: {
-    color: '#2F2E7E', fontSize: 20, fontWeight: 600, marginBottom: '1.5rem'
+    color: '#2F2E7E', fontSize: 20, fontWeight: 600, marginBottom: '1.5rem',
+    display: 'flex', flexDirection: 'row'
+  },
+  conceptSetMetadataWrapper: {
+    flexDirection: 'column', alignItems: 'space-between', marginLeft: '0.5rem'
   },
   conceptSetData: {
     display: 'flex', flexDirection: 'row', color: '#000', fontWeight: 600
   }
 });
 
+const ConceptSetMenu: React.FunctionComponent<{
+  disabled: boolean, canDelete: boolean, canEdit: boolean, onEdit: Function, onDelete: Function
+}> = ({disabled, canDelete, canEdit, onEdit, onDelete}) => {
+
+  return <PopupTrigger
+    side='right'
+    closeOnClick
+    content={ <React.Fragment>
+      <TooltipTrigger content={<div>Requires Write Permission</div>}
+                      disabled={canEdit}>
+        <MenuItem icon='pencil'
+                  onClick={() => onEdit}
+                  disabled={!canEdit}>
+          Edit
+        </MenuItem>
+      </TooltipTrigger>
+      <TooltipTrigger content={<div>Requires Owner Permission</div>}
+                      disabled={canDelete}>
+        <MenuItem icon='trash' onClick={onDelete} disabled={!canDelete}>
+          Delete
+        </MenuItem>
+      </TooltipTrigger>
+    </React.Fragment>}
+  >
+    <Clickable disabled={disabled} data-test-id='workspace-menu'>
+      <ClrIcon shape='ellipsis-vertical' size={21}
+               style={{color: disabled ? '#9B9B9B' : '#216FB4', marginLeft: -9,
+                 cursor: disabled ? 'auto' : 'pointer'}}/>
+    </Clickable>
+  </PopupTrigger>;
+};
+
 export const ConceptSetDetails = fp.flow(withUrlParams(), withCurrentWorkspace())(
   class extends React.Component<{
     urlParams: any, workspace: WorkspaceData}, {
-      conceptSet: ConceptSet, deleting: boolean, editing: boolean, loading: boolean,
+      conceptSet: ConceptSet, conceptSetDeletionError: boolean, deleting: boolean, editName: string,
+      editDescription: string, editSaving: boolean, editing: boolean, loading: boolean,
       selectedConcepts: Concept[]}> {
     constructor(props) {
       super(props);
       this.state = {
         conceptSet: undefined,
+        conceptSetDeletionError: false,
+        editName: '',
+        editDescription: '',
+        editSaving: false,
         deleting: false,
         editing: false,
         loading: true,
@@ -43,10 +93,26 @@ export const ConceptSetDetails = fp.flow(withUrlParams(), withCurrentWorkspace()
       const {urlParams: {ns, wsid, csid}} = this.props;
       try {
         const resp = await conceptSetsApi().getConceptSet(ns, wsid, csid);
-        this.setState({conceptSet: resp, loading: false});
+        this.setState({conceptSet: resp, editName: resp.name,
+          editDescription: resp.description, loading: false});
       } catch (error) {
         console.log(error);
         // todo: cannot find concept set
+      }
+    }
+
+    async submitEdits() {
+      const {urlParams: {ns, wsid, csid}} = this.props;
+      const {conceptSet, editName, editDescription} = this.state;
+      try {
+        this.setState({editSaving: true});
+        await conceptSetsApi().updateConceptSet(ns, wsid, csid,
+          {...conceptSet, name: editName, description: editDescription});
+        await this.getConceptSet();
+        this.setState({editing: false, editSaving: false});
+      } catch (error) {
+        console.log(error);
+        this.setState({editing: false, editSaving: false});
       }
     }
 
@@ -58,25 +124,78 @@ export const ConceptSetDetails = fp.flow(withUrlParams(), withCurrentWorkspace()
       // TODO
     }
 
+    async onDeleteConceptSet() {
+      const {urlParams: {ns, wsid, csid}} = this.props;
+      try {
+        await conceptSetsApi().deleteConceptSet(ns, wsid, csid);
+        this.setState({deleting: false});
+        navigate(['workspaces', ns, wsid, 'concepts', 'sets']);
+      } catch (error) {
+        console.log(error);
+        this.setState({deleting: false, conceptSetDeletionError: true});
+      }
+    }
+
     get canEdit(): boolean {
-      return this.props.workspace.accessLevel as unknown as WorkspaceAccessLevel === WorkspaceAccessLevel.OWNER
-          || this.props.workspace.accessLevel as unknown as WorkspaceAccessLevel === WorkspaceAccessLevel.WRITER;
+      return this.isOwner || this.isWriter;
+    }
+
+    get isWriter(): boolean {
+      return this.props.workspace.accessLevel as unknown as WorkspaceAccessLevel
+        === WorkspaceAccessLevel.WRITER;
+    }
+
+    get isOwner(): boolean {
+      return this.props.workspace.accessLevel as unknown as WorkspaceAccessLevel
+        === WorkspaceAccessLevel.OWNER;
     }
 
     render() {
-      const {conceptSet, loading, selectedConcepts} = this.state;
+      const {conceptSet, conceptSetDeletionError, editing, editDescription, editName,
+        editSaving, deleting, loading, selectedConcepts} = this.state;
       return <FadeBox style={{margin: 'auto', marginTop: '1rem', width: '95.7%'}}>
         {loading ? <SpinnerOverlay/> :
         <div style={{display: 'flex', flexDirection: 'column'}}>
           <div style={{display: 'flex', flexDirection: 'row', paddingBottom: '1.5rem'}}>
-            <div style={{flexDirection: 'column', alignItems: 'space-between'}}>
-              <div style={styles.conceptSetHeader}>{conceptSet.name}</div>
-              <div style={styles.conceptSetData}>
-                <div>Participant Count: {conceptSet.participantCount}</div>
-                <div style={{marginLeft: '2rem'}}>
-                  Domain: {fp.capitalize(conceptSet.domain.toString())}</div>
+            <ConceptSetMenu canDelete={this.isOwner} canEdit={this.canEdit}
+                            disabled={!this.canEdit}
+                            onDelete={() => this.setState({deleting: true})}
+                            onEdit={() => this.setState({editing: true})}/>
+            <div style={styles.conceptSetMetadataWrapper}>
+            {editing ?
+              <div style={{display: 'flex', flexDirection: 'column'}}>
+                <TextInput value={editName} disabled={editSaving}
+                           style={{marginBottom: '0.5rem'}}
+                           onChange={v => this.setState({editName: v})}/>
+                <TextArea value={editDescription} disabled={editSaving}
+                          style={{marginBottom: '0.5rem'}}
+                          onChange={v => this.setState({editDescription: v})}/>
+                <div style={{marginBottom: '0.5rem'}}>
+                  <Button type='primary' style={{marginRight: '0.5rem'}}
+                          disabled={editSaving} onClick={() => this.submitEdits()}>Save</Button>
+                  <Button type='secondary' disabled={editSaving}
+                          onClick={() => this.setState({
+                            editing: false,
+                            editName: conceptSet.name,
+                            editDescription: conceptSet.description
+                          })}>Cancel</Button>
+                </div>
+              </div> :
+              <React.Fragment>
+                <div style={styles.conceptSetHeader}>{conceptSet.name}
+                  <Clickable disabled={!this.canEdit}
+                             onClick={() => this.setState({editing: true})}>
+                    <EditComponentReact disabled={!this.canEdit} style={{marginTop: '0.1rem'}}/>
+                  </Clickable>
+                </div>
+              </React.Fragment>
+                }
+                  <div style={styles.conceptSetData}>
+                      <div>Participant Count: {conceptSet.participantCount}</div>
+                      <div style={{marginLeft: '2rem'}}>
+                          Domain: {fp.capitalize(conceptSet.domain.toString())}</div>
+                  </div>
               </div>
-            </div>
           </div>
           <ConceptTable concepts={conceptSet.concepts} loading={loading}
                         reactKey={conceptSet.domain.toString()}
@@ -84,6 +203,19 @@ export const ConceptSetDetails = fp.flow(withUrlParams(), withCurrentWorkspace()
                         placeholderValue={'No Concepts Found'}
                         selectedConcepts={selectedConcepts}/>
         </div>}
+        {!loading && deleting &&
+        <ConfirmDeleteModal closeFunction={() => this.setState({deleting: false})}
+                            receiveDelete={() => this.onDeleteConceptSet()}
+                            resourceName={conceptSet.name}
+                            resourceType={ResourceType.CONCEPT_SET}/>}
+        {conceptSetDeletionError && <Modal>
+            <ModalTitle>Error: Could not concept set '{conceptSet.name}'</ModalTitle>
+            <ModalFooter>
+                <Button type='secondary'
+                        onClick={() =>
+                          this.setState({conceptSetDeletionError: false})}>Close</Button>
+            </ModalFooter>
+        </Modal>}
       </FadeBox>;
     }
   });
@@ -97,131 +229,3 @@ export class ConceptSetDetailsComponent extends ReactWrapperBase {
     super(ConceptSetDetails, []);
   }
 }
-
-
-// @Component({
-//   styleUrls: ['../../styles/buttons.css',
-//     '../../styles/cards.css',
-//     '../../styles/headers.css',
-//     '../../styles/inputs.css',
-//     '../../styles/errors.css',
-//     './component.css'],
-//   templateUrl: './component.html',
-// })
-// export class ConceptSetDetailsComponent implements OnInit, OnDestroy {
-//   @ViewChild(ConceptTableComponent) conceptTable;
-//
-//   wsNamespace: string;
-//   wsId: string;
-//   accessLevel: WorkspaceAccessLevel;
-//   conceptSet: ConceptSet;
-//
-//   editing = false;
-//   editSubmitting = false;
-//   editName: string;
-//   editDescription: string;
-//
-//   removing = false;
-//   removeSubmitting = false;
-//   confirmDeleting = false;
-//   selectedConcepts: Concept[] = [];
-//
-//   constructor(
-//     private conceptSetsService: ConceptSetsService,
-//   ) {
-//     this.receiveDelete = this.receiveDelete.bind(this);
-//     this.closeConfirmDelete = this.closeConfirmDelete.bind(this);
-//     this.removeFab = this.removeFab.bind(this);
-//     this.onSelectConcepts = this.onSelectConcepts.bind(this);
-//   }
-//
-//   ngOnInit() {
-//     const {ns, wsid, csid} = urlParamsStore.getValue();
-//     this.wsNamespace = ns;
-//     this.wsId = wsid;
-//     const {accessLevel} = currentWorkspaceStore.getValue();
-//     this.accessLevel = accessLevel;
-//     this.conceptSetsService.getConceptSet(ns, wsid, csid).subscribe(conceptSet => {
-//       currentConceptSetStore.next(conceptSet as unknown as FetchConceptSet);
-//       this.conceptSet = conceptSet;
-//       this.editName = conceptSet.name;
-//       this.editDescription = conceptSet.description;
-//     });
-//   }
-//
-//   ngOnDestroy() {
-//     currentConceptSetStore.next(undefined);
-//   }
-//
-//   validateEdits(): boolean {
-//     return !!this.editName;
-//   }
-//
-//   removeFab(): void {
-//     this.removing = true;
-//   }
-//
-//   submitEdits() {
-//     if (!this.validateEdits() || this.editSubmitting) {
-//       return;
-//     }
-//     this.editSubmitting = true;
-//     this.conceptSetsService.updateConceptSet(this.wsNamespace, this.wsId, this.conceptSet.id, {
-//       ...this.conceptSet,
-//       name: this.editName,
-//       description: this.editDescription
-//     }).subscribe((updated) => {
-//       this.conceptSet = updated;
-//       this.editSubmitting = false;
-//       this.editing = false;
-//     }, () => {
-//       // TODO(calbach): Handle errors.
-//       this.editSubmitting = false;
-//     });
-//   }
-//
-//   closeConfirmDelete(): void {
-//     this.confirmDeleting = false;
-//   }
-//
-//   receiveDelete() {
-//     this.conceptSetsService.deleteConceptSet(this.wsNamespace, this.wsId, this.conceptSet.id)
-//       .subscribe(() => {
-//         navigate(['workspaces', this.wsNamespace, this.wsId, 'concepts', 'sets']);
-//         this.closeConfirmDelete();
-//       });
-//   }
-//
-//   removeConcepts() {
-//     this.removeSubmitting = true;
-//     this.conceptSetsService.updateConceptSetConcepts(
-//       this.wsNamespace, this.wsId, this.conceptSet.id, {
-//         etag: this.conceptSet.etag,
-//         removedIds: this.conceptTable.selectedConcepts.map(c => c.conceptId)
-//       }).subscribe((cs) => {
-//         this.conceptSet = cs;
-//         this.removing = false;
-//         this.removeSubmitting = false;
-//       }, () => {
-//         // TODO(calbach): Handle errors.
-//         this.removeSubmitting = false;
-//       });
-//   }
-//
-//   onSelectConcepts(concepts) {
-//     this.selectedConcepts = concepts;
-//   }
-//
-//   get canEdit(): boolean {
-//     return this.accessLevel === WorkspaceAccessLevel.OWNER
-//         || this.accessLevel === WorkspaceAccessLevel.WRITER;
-//   }
-//
-//   get selectedConceptsCount(): number {
-//     return !this.conceptTable ? 0 : this.conceptTable.selectedConcepts.length;
-//   }
-//
-//   get disableRemoveFab(): boolean {
-//     return !this.canEdit || this.selectedConceptsCount === 0;
-//   }
-// }
