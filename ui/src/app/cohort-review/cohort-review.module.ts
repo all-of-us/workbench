4ab9a38797dd5fe55d6a3fa5d34561b90e58680c
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro.local>
2019-05-15 15:06:55 -0500

    RW-2636 remove unused angular files and references

--- ui/src/app/cohort-review/cohort-review.module.ts
+++ ui/src/app/cohort-review/cohort-review.module.ts
@@ -5,23 +5,17 @@ import {NgModule} from '@angular/core';
 import {ReactiveFormsModule} from '@angular/forms';
 import {ClarityModule} from '@clr/angular';
 import {NgxChartsModule} from '@swimlane/ngx-charts';
-import { ChartModule } from 'angular2-highcharts';
-import { HighchartsStatic } from 'angular2-highcharts/dist/HighchartsService';
+import {ChartModule} from 'angular2-highcharts';
+import {HighchartsStatic} from 'angular2-highcharts/dist/HighchartsService';
 import * as highCharts from 'highcharts';
 import {NgxPopperModule} from 'ngx-popper';
 
 /* Pages */
 import {CohortCommonModule} from 'app/cohort-common/module';
-// This is to get cohortsaerchstore acces, might need to change
-import {AddAnnotationDefinitionModalComponent, EditAnnotationDefinitionsModalComponent} from './annotation-definition-modals/annotation-definition-modals.component';
 import {CreateReviewPage} from './create-review-page/create-review-page';
-// This is a temporary measure until we have specs and APIs for overview specific charts
-import {DetailHeaderComponent} from './detail-header/detail-header.component';
 import {DetailPageComponent} from './detail-page/detail-page';
-import {DetailTabsComponent} from './detail-tabs/detail-tabs.component';
 import {PageLayout} from './page-layout/page-layout';
 import {CohortReviewRoutingModule} from './routing/routing.module';
-import {SidebarContentComponent} from './sidebar-content/sidebar-content.component';
 import {TablePage} from './table-page/table-page';
 
 
@@ -50,16 +44,6 @@ import {TablePage} from './table-page/table-page';
     DetailPageComponent,
     PageLayout,
     TablePage,
-
-    /* Annotations */
-    AddAnnotationDefinitionModalComponent,
-    EditAnnotationDefinitionsModalComponent,
-
-    /* Participant Table */
-    /* Participant Detail */
-    SidebarContentComponent,
-    DetailHeaderComponent,
-    DetailTabsComponent,
   ],
   providers: [
     {
