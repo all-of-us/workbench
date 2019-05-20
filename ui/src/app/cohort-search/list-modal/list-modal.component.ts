ffefcd05fdc2f5a8c04bb897da74b28977a92e6f
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-05-20 15:09:36 -0500

    cleanup list-modal component

--- ui/src/app/cohort-search/list-modal/list-modal.component.ts
+++ ui/src/app/cohort-search/list-modal/list-modal.component.ts
@@ -1,19 +1,4 @@
-import {select} from '@angular-redux/store';
 import {Component, OnDestroy, OnInit } from '@angular/core';
-import {DOMAIN_TYPES, PROGRAM_TYPES} from 'app/cohort-search/constant';
-import {
-  activeCriteriaSubtype,
-  activeCriteriaTreeType,
-  activeCriteriaType,
-  activeItem,
-  activeParameterList,
-  CohortSearchActions,
-  nodeAttributes,
-  previewStatus,
-  scrollId,
-  subtreeSelected,
-  wizardOpen,
-} from 'app/cohort-search/redux';
 import {
   attributesStore,
   autocompleteStore,
@@ -24,11 +9,9 @@ import {
   subtreeSelectedStore,
   wizardStore
 } from 'app/cohort-search/search-state.service';
-import {stripHtml, subtypeToTitle, typeToTitle} from 'app/cohort-search/utils';
+import {stripHtml} from 'app/cohort-search/utils';
 import {TreeSubType, TreeType} from 'generated';
 import {DomainType} from 'generated/fetch';
-import {Map} from 'immutable';
-import {Observable} from 'rxjs/Observable';
 import {Subscription} from 'rxjs/Subscription';
 
 
@@ -41,26 +24,14 @@ import {Subscription} from 'rxjs/Subscription';
   ]
 })
 export class ListModalComponent implements OnInit, OnDestroy {
-  @select(wizardOpen) open$: Observable<boolean>;
-  @select(activeCriteriaSubtype) criteriaSubtype$: Observable<any>;
-  @select(activeCriteriaType) criteriaType$: Observable<string>;
-  @select(activeCriteriaTreeType) isFullTree$: Observable<boolean>;
-  @select(activeItem) item$: Observable<any>;
-  @select(activeParameterList) selection$: Observable<any>;
-  @select(nodeAttributes) attributes$: Observable<any>;
-  @select(scrollId) scrollTo$: Observable<any>;
-  @select(subtreeSelected) subtree$: Observable<any>;
-  @select(previewStatus) preview$;
 
   readonly domainType = DomainType;
-  readonly treeType = TreeType;
   readonly treeSubType = TreeSubType;
   ctype: string;
   subtype: string;
   itemType: string;
   fullTree: boolean;
   subscription: Subscription;
-  attributesNode: Map<any, any> = Map();
   selections = {};
   selectionIds: Array<string>;
   selectionList: Array<any>;
@@ -70,16 +41,14 @@ export class ListModalComponent implements OnInit, OnDestroy {
   mode = 'list';
   backMode: string;
   count = 0;
-  originalNode: any;
   disableCursor = false;
   modifiersDisabled = false;
-  preview = Map();
   conceptType: string = null;
   wizard: any;
   attributesCrit: any;
   hierarchyCrit: any;
 
-  constructor(private actions: CohortSearchActions) {}
+  constructor() {}
 
   ngOnInit() {
     this.subscription = wizardStore
@@ -107,95 +76,8 @@ export class ListModalComponent implements OnInit, OnDestroy {
         this.attributesCrit = criterion;
         this.mode = 'attributes';
       }));
-
-    this.subscription.add(this.criteriaSubtype$
-      .subscribe(subtype => {
-        this.subtype = subtype;
-      })
-    );
-
-    this.subscription.add(this.isFullTree$.subscribe(fullTree => this.fullTree = fullTree));
-    this.subscription.add(this.selection$
-      .subscribe(selections => {
-        this.selections = {};
-        // this.noSelection = selections.size === 0;
-        selections.forEach(selection => {
-          if (this.isCondOrProc) {
-            this.conceptType = selection.get('type') === TreeType[TreeType.SNOMED]
-              ? 'standard' : 'source';
-          }
-          this.addSelectionToGroup(selection);
-        });
-        if (this.conceptType === 'standard') {
-          this.setMode('snomed');
-        }
-      })
-    );
-    this.subscription.add(this.attributes$
-      .subscribe(node => {
-        this.attributesNode = node;
-        if (node.size === 0) {
-          // this.mode = 'tree';
-        } else {
-          // this.mode = 'attributes';
-        }
-      })
-    );
-
-    this.subscription.add(this.scrollTo$
-      .filter(nodeId => !!nodeId)
-      .subscribe(nodeId => {
-        this.setScroll(nodeId);
-      })
-    );
-
-    this.subscription.add(this.subtree$
-      .filter(nodeIds => !!nodeIds)
-      .subscribe(nodeIds => {
-        this.disableCursor = nodeIds.length > 0;
-      })
-    );
-
-    this.subscription.add(this.item$.subscribe(item => {
-      this.itemType = item.get('type');
-      // this.title = 'Codes';
-      for (const crit of DOMAIN_TYPES) {
-        const regex = new RegExp(`.*${crit.type}.*`, 'i');
-        if (regex.test(this.itemType)) {
-          this.title = crit.name;
-          if (crit.type === TreeType.DEMO) {
-            this.title += ' - ' + subtypeToTitle(this.subtype);
-          }
-        }
-      }
-      for (const crit of PROGRAM_TYPES) {
-        const regex = new RegExp(`.*${crit.type}.*`, 'i');
-        if (regex.test(this.itemType)) {
-          this.title = crit.name;
-        }
-      }
-    }));
-    this.originalNode = this.rootNode;
   }
-  addSelectionToGroup(selection: any) {
-    if (selection.get('type') === TreeType[TreeType.DEMO]) {
-      if (selection.get('subtype') === this.subtype) {
-        const key = selection.get('subtype');
-        if (this.selections[key] && !this.selections[key].includes(selection)) {
-          this.selections[key].push(selection);
-        } else {
-          this.selections[key] = [selection];
-        }
-      }
-    } else {
-      const key = selection.get('type');
-      if (this.selections[key] && !this.selections[key].includes(selection)) {
-        this.selections[key].push(selection);
-      } else {
-        this.selections[key] = [selection];
-      }
-    }
-  }
+
   setScroll(id: string) {
     const nodeId = `node${id}`;
     const node = document.getElementById(nodeId);
@@ -235,7 +117,7 @@ export class ListModalComponent implements OnInit, OnDestroy {
         this.mode = 'list';
         break;
       case 'snomed':
-        this.setMode('tree');
+        this.mode = 'tree';
     }
   }
 
@@ -251,27 +133,6 @@ export class ListModalComponent implements OnInit, OnDestroy {
     }
     searchRequestStore.next(searchRequest);
     this.close();
-    // this.actions.finishWizard();
-  }
-
-  /* Used to bootstrap the criteria tree */
-  get rootNode() {
-    return Map({
-      type: this.ctype,
-      subtype: this.subtype,
-      fullTree: this.fullTree,
-      id: 0,    // root parent ID is always 0
-    });
-  }
-
-  get snomedNode() {
-    return Map({
-      type: TreeType.SNOMED,
-      subtype: this.subtype === TreeSubType[TreeSubType.CM]
-        ? TreeSubType.CM : TreeSubType.PCS,
-      fullTree: this.fullTree,
-      id: 0,    // root parent ID is always 0
-    });
   }
 
   get attributeTitle() {
@@ -312,23 +173,7 @@ export class ListModalComponent implements OnInit, OnDestroy {
   }
 
   setMode(mode: any) {
-    if (mode !== 'tree' && this.ctype !== TreeType[TreeType.SNOMED]) {
-      this.originalNode = Map({
-        type: this.ctype,
-        subtype: this.subtype,
-        fullTree: this.fullTree,
-        id: 0,
-      });
-    }
-    if (mode !== 'modifiers') {
-      const node = mode === 'tree' ? this.originalNode : this.snomedNode;
-      const criteriaType = node.get('type');
-      const criteriaSubtype = node.get('subtype');
-      const context = {criteriaType, criteriaSubtype};
-      this.actions.setWizardContext(context);
-    } else {
-      this.backMode = this.mode;
-    }
+    this.backMode = this.mode;
     this.mode = mode;
   }
 
@@ -346,18 +191,11 @@ export class ListModalComponent implements OnInit, OnDestroy {
     this.mode = 'tree';
   }
 
-  selectionHeader(_type: string) {
-    return this.itemType === TreeType[TreeType.DEMO] ? subtypeToTitle(_type) : typeToTitle(_type);
-  }
-
   modifiersFlag = (disabled: boolean) => {
     this.modifiersDisabled = disabled;
   }
 
   get disableFlag() {
-    return this.noSelection
-      || this.preview.get('requesting')
-      || this.preview.get('count') === 0
-      || this.modifiersDisabled;
+    return this.noSelection || this.modifiersDisabled;
   }
 }
