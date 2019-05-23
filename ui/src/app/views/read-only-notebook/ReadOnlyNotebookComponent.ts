8abf658534ef65184aa1857cebc6d237665003cd
Eric Song <songe@broadinstitute.org>
2019-05-23 14:56:40 -0400

    add manually generated cluster endpoint

--- ui/src/app/views/read-only-notebook/ReadOnlyNotebookComponent.ts
+++ ui/src/app/views/read-only-notebook/ReadOnlyNotebookComponent.ts
@@ -4,6 +4,8 @@ import {Cluster} from "../../../generated";
 import {environment} from "../../../environments/environment";
 import {DomSanitizer} from "@angular/platform-browser";
 import {ClusterService as LeoClusterService} from "../../../notebooks-generated";
+import {Headers, RequestMethod, RequestOptions, RequestOptionsArgs, Response} from "@angular/http";
+import {Observable} from "rxjs";
 
 declare const gapi: any;
 
@@ -44,4 +46,50 @@ export class ReadOnlyNotebookComponent implements OnInit, OnDestroy {
       + namespace + '/'
       + name + '/notebooks/' + nbName);
   }
+
+  // This was added to cluster.service.ts
+  // 
+  // public readOnly(body: string, extraHttpRequestParams?: RequestOptionsArgs): Observable<Response> {
+  //   let headers = new Headers(this.defaultHeaders.toJSON()); // https://github.com/angular/angular/issues/6845
+  //
+  //   // authentication (googleoauth) required
+  //   if (this.configuration.accessToken) {
+  //     const accessToken = typeof this.configuration.accessToken === 'function'
+  //       ? this.configuration.accessToken()
+  //       : this.configuration.accessToken;
+  //     headers.set('Authorization', 'Bearer ' + accessToken);
+  //   }
+  //
+  //   // to determine the Accept header
+  //   let httpHeaderAccepts: string[] = [
+  //     'application/json'
+  //   ];
+  //   const httpHeaderAcceptSelected: string | undefined = this.configuration.selectHeaderAccept(httpHeaderAccepts);
+  //   if (httpHeaderAcceptSelected != undefined) {
+  //     headers.set('Accept', httpHeaderAcceptSelected);
+  //   }
+  //
+  //   // to determine the Content-Type header
+  //   const consumes: string[] = [
+  //     'application/json'
+  //   ];
+  //   const httpContentTypeSelected: string | undefined = this.configuration.selectHeaderContentType(consumes);
+  //   if (httpContentTypeSelected != undefined) {
+  //     headers.set('Content-Type', httpContentTypeSelected);
+  //   }
+  //
+  //   let requestOptions: RequestOptionsArgs = new RequestOptions({
+  //     method: RequestMethod.Post,
+  //     headers: headers,
+  //     body: body,
+  //     withCredentials:this.configuration.withCredentials
+  //   });
+  //   // https://github.com/swagger-api/swagger-codegen/issues/4037
+  //   if (extraHttpRequestParams) {
+  //     requestOptions = (<any>Object).assign(requestOptions, extraHttpRequestParams);
+  //   }
+  //
+  //
+  //   return this.http.request(`https://terra-calhoun-dev.appspot.com/api/convert`, requestOptions);
+  // }
 }
