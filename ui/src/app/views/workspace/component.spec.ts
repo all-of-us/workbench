3a473136a5e76a1828c27e998644d8bc70f76da2
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-05-28 12:21:35 -0400

    legacy ui tests

--- ui/src/app/views/workspace/component.spec.ts
+++ ui/src/app/views/workspace/component.spec.ts
@@ -1,7 +1,6 @@
 import {ComponentFixture, fakeAsync, TestBed} from '@angular/core/testing';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
 import {Http} from '@angular/http';
-import {By} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouterTestingModule} from '@angular/router/testing';
 import {ClarityModule} from '@clr/angular';
