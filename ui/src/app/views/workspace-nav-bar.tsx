4a3aca19cba76c089060a516e03fc7901e98a4b4
CH Albach <calbach@users.noreply.github.com>
2019-06-04 12:36:57 -0700

    [risk=no] Fix accidental duplicate menu text (#2199)

--- ui/src/app/views/workspace-nav-bar.tsx
+++ ui/src/app/views/workspace-nav-bar.tsx
@@ -104,7 +104,7 @@ export const WorkspaceNavBarReact = fp.flow(
           <MenuItem
             icon='copy'
             onClick={() => NavStore.navigate(['/workspaces', namespace, id, 'duplicate'])}>
-            Duplicate Test
+            Duplicate
           </MenuItem>
           <MenuItem
             icon='pencil'
