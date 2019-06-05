098eb15776cf3e35e360ba5ec971b1fe114b45de
CH Albach <calbach@users.noreply.github.com>
2019-06-05 16:42:49 -0700

    [RW-2769][risk=no] rm unused eager fetch on user model (#2206)

--- api/src/main/java/org/pmiops/workbench/db/model/User.java
+++ api/src/main/java/org/pmiops/workbench/db/model/User.java
@@ -64,7 +64,6 @@ public class User {
   private Short freeTierBillingProjectStatus;
   private Timestamp firstSignInTime;
   private Set<Short> authorities = new HashSet<>();
-  private Set<WorkspaceUserRole> workspaceUserRoles = new HashSet<>();
   private Boolean idVerificationIsValid;
   private Timestamp termsOfServiceCompletionTime;
   private Timestamp demographicSurveyCompletionTime;
@@ -287,19 +286,6 @@ public class User {
 
   public void setPageVisits(Set<PageVisit> newPageVisits) {
     this.pageVisits = newPageVisits;
-  }
-
-  @OneToMany(fetch = FetchType.EAGER, mappedBy = "user")
-  public Set<WorkspaceUserRole> getWorkspaceUserRoles() {
-    return workspaceUserRoles;
-  }
-
-  /**
-   * Necessary for Spring initialization of the object. Not actually supported because it won't
-   * delete old entries.
-   */
-  public void setWorkspaceUserRoles(Set<WorkspaceUserRole> userRoles) {
-    this.workspaceUserRoles = userRoles;
   }
 
   @Column(name = "id_verification_is_valid")
