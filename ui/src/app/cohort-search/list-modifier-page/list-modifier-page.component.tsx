f28365091a410bbfc5a4e940af77f341bc28662f
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-06-05 10:00:32 -0500

    RW-2716 move form data into state

--- ui/src/app/cohort-search/list-modifier-page/list-modifier-page.component.tsx
+++ ui/src/app/cohort-search/list-modifier-page/list-modifier-page.component.tsx
@@ -1,12 +1,12 @@
-import {Component, Input, OnDestroy, OnInit} from '@angular/core';
-import {FormArray, FormControl, FormGroup, Validators} from '@angular/forms';
+import {Component, Input} from '@angular/core';
+import {FormControl} from '@angular/forms';
 import {wizardStore} from 'app/cohort-search/search-state.service';
-import {dateValidator, integerAndRangeValidator} from 'app/cohort-search/validators';
+import {ClrIcon} from 'app/components/icons';
+import {Select} from 'app/components/inputs';
 import {cohortBuilderApi} from 'app/services/swagger-fetch-clients';
 import {ReactWrapperBase, withCurrentWorkspace} from 'app/utils';
-import {currentWorkspaceStore} from 'app/utils/navigation';
 import {WorkspaceData} from 'app/utils/workspace-data';
-import {ModifierType, Operator, TreeType} from 'generated';
+import {ModifierType, TreeType} from 'generated';
 import {CriteriaType, DomainType} from 'generated/fetch';
 import {List} from 'immutable';
 import * as moment from 'moment';
@@ -20,37 +20,79 @@ interface Props {
 }
 
 interface State {
-  formValues: any;
+  formState: any;
 }
 
-export const ListModifierPage  = withCurrentWorkspace()(
+export const ListModifierPage = withCurrentWorkspace()(
   class extends React.Component<Props, State> {
     constructor(props: Props) {
       super(props);
       this.state = {
-        formValues: {
-          ageAtEvent: {
+        formState: [{
+          name: 'ageAtEvent',
+          label: 'Age At Event',
+          values: {
             operator: undefined,
             valueA: undefined,
             valueB: undefined,
           },
-          hasOccurrences: {
+          options: [{
+            label: 'Any',
+            value: undefined,
+          }, {
+            label: 'Greater Than or Equal To',
+            value: 'GREATER_THAN_OR_EQUAL_TO',
+          }, {
+            label: 'Less Than or Equal To',
+            value: 'LESS_THAN_OR_EQUAL_TO',
+          }, {
+            label: 'Between',
+            value: 'BETWEEN',
+          }]
+        }, {
+          name: 'hasOccurrences',
+          label: 'Has Occurrences',
+          values: {
             operator: undefined,
             valueA: undefined,
             valueB: undefined,
           },
-          eventDate: {
+          options: [{
+            label: 'Any',
+            value: undefined,
+          }, {
+            label: 'N or More',
+            value: 'GREATER_THAN_OR_EQUAL_TO',
+          }]
+        }, {
+          name: 'eventDate',
+          label: 'Shifted Event Date',
+          values: {
             operator: undefined,
             valueA: undefined,
             valueB: undefined,
           },
-        }
+          options: [{
+            label: 'Any',
+            value: undefined,
+          }, {
+            label: 'Is On or Before',
+            value: 'LESS_THAN_OR_EQUAL_TO',
+          }, {
+            label: 'Is On or After',
+            value: 'GREATER_THAN_OR_EQUAL_TO',
+          }, {
+            label: 'Is Between',
+            value: 'BETWEEN',
+          }]
+        }]
       };
     }
+
     formChanges = false;
     existing = List();
     preview: any = {count: -1};
-    dateObjs = [null , null];
+    dateObjs = [null, null];
     subscription: Subscription;
     dropdownOption = {
       selected: ['Any', 'Any', 'Any', 'Any']
@@ -66,16 +108,16 @@ export const ListModifierPage  = withCurrentWorkspace()(
       maxLength: 3,
       modType: ModifierType.AGEATEVENT,
       operators: [{
-        name: 'Any',
+        label: 'Any',
         value: undefined,
       }, {
-        name: 'Greater Than or Equal To',
+        label: 'Greater Than or Equal To',
         value: 'GREATER_THAN_OR_EQUAL_TO',
       }, {
-        name: 'Less Than or Equal To',
+        label: 'Less Than or Equal To',
         value: 'LESS_THAN_OR_EQUAL_TO',
       }, {
-        name: 'Between',
+        label: 'Between',
         value: 'BETWEEN',
       }],
     }, {
@@ -87,16 +129,16 @@ export const ListModifierPage  = withCurrentWorkspace()(
       maxLength: null,
       modType: ModifierType.EVENTDATE,
       operators: [{
-        name: 'Any',
+        label: 'Any',
         value: undefined,
       }, {
-        name: 'Is On or Before',
+        label: 'Is On or Before',
         value: 'LESS_THAN_OR_EQUAL_TO',
       }, {
-        name: 'Is On or After',
+        label: 'Is On or After',
         value: 'GREATER_THAN_OR_EQUAL_TO',
       }, {
-        name: 'Is Between',
+        label: 'Is Between',
         value: 'BETWEEN',
       }]
     }, {
@@ -108,10 +150,10 @@ export const ListModifierPage  = withCurrentWorkspace()(
       maxLength: 2,
       modType: ModifierType.NUMOFOCCURRENCES,
       operators: [{
-        name: 'Any',
+        label: 'Any',
         value: undefined,
       }, {
-        name: 'N or More',
+        label: 'N or More',
         value: 'GREATER_THAN_OR_EQUAL_TO',
       }]
     }];
@@ -122,7 +164,7 @@ export const ListModifierPage  = withCurrentWorkspace()(
 
     componentDidMount() {
       const {workspace: {cdrVersionId}, wizard} = this.props;
-      let {formValues} = this.state;
+      const {formState} = this.state;
       if (this.addEncounters) {
         this.modifiers.push({
           name: 'encounters',
@@ -133,61 +175,64 @@ export const ListModifierPage  = withCurrentWorkspace()(
           maxLength: null,
           modType: ModifierType.ENCOUNTERS,
           operators: [{
-            name: 'Any',
+            label: 'Any',
             value: undefined,
           }]
         });
-        formValues = {
-          ...formValues,
-          encounters: {
-            operator: undefined,
-            valueA: undefined,
-            valueB: undefined,
-          }
-        };
-        this.setState({formValues});
         cohortBuilderApi()
-          .getCriteriaBy(
-            +cdrVersionId, DomainType[DomainType.VISIT], CriteriaType[CriteriaType.VISIT]
-          )
-          .then(response => {
-            this.visitCounts = {};
-            response.items.forEach(option => {
-              if (option.parentId === 0 && option.count > 0) {
-                this.modifiers[3].operators.push({
-                  name: option.name,
-                  value: option.conceptId.toString()
-                });
-                this.visitCounts[option.conceptId] = option.count;
-              }
-            });
-            this.getExisting();
+        .getCriteriaBy(+cdrVersionId, DomainType[DomainType.VISIT])
+        .then(response => {
+          this.visitCounts = {};
+          const encounters = {
+            name: 'encounters',
+            label: 'During Visit Type',
+            values: {
+              operator: undefined,
+              encounterType: undefined,
+            },
+            options: [{
+              label: 'Any',
+              value: undefined,
+            }]
+          };
+          response.items.forEach(option => {
+            if (option.parentId === 0 && option.count > 0) {
+              encounters.options.push({
+                label: option.name,
+                value: option.conceptId.toString()
+              });
+              this.visitCounts[option.conceptId] = option.count;
+            }
           });
+          formState.push(encounters);
+          this.setState({formState});
+          this.getExisting();
+        });
       } else {
         this.getExisting();
       }
 
-      this.subscription = this.form.valueChanges
-        .map(this.currentMods)
-        .subscribe(newMods => {
-          wizard.item.modifiers = newMods.filter(mod => !!mod);
-          wizardStore.next(wizard);
-        });
-
-      this.subscription.add(this.dateA.valueChanges.subscribe(value => {
-        const formatted = moment(value).format('YYYY-MM-DD');
-        this.form.get(['eventDate', 'valueA']).setValue(formatted);
-      }));
-
-      this.subscription.add(this.dateB.valueChanges.subscribe(value => {
-        const formatted = moment(value).format('YYYY-MM-DD');
-        this.form.get(['eventDate', 'valueB']).setValue(formatted);
-      }));
+      // this.subscription = this.form.valueChanges
+      // .map(this.currentMods)
+      // .subscribe(newMods => {
+      //   wizard.item.modifiers = newMods.filter(mod => !!mod);
+      //   wizardStore.next(wizard);
+      // });
+      //
+      // this.subscription.add(this.dateA.valueChanges.subscribe(value => {
+      //   const formatted = moment(value).format('YYYY-MM-DD');
+      //   this.form.get(['eventDate', 'valueA']).setValue(formatted);
+      // }));
+      //
+      // this.subscription.add(this.dateB.valueChanges.subscribe(value => {
+      //   const formatted = moment(value).format('YYYY-MM-DD');
+      //   this.form.get(['eventDate', 'valueB']).setValue(formatted);
+      // }));
     }
 
     getExisting() {
       const {wizard} = this.props;
-      const {formValues} = this.state;
+      const {formState} = this.state;
       // This reseeds the form with existing data if we're editing an existing group
       wizard.item.modifiers.forEach(mod => {
         const meta = this.modifiers.find(_mod => mod.name === _mod.modType);
@@ -198,8 +243,8 @@ export const ListModifierPage  = withCurrentWorkspace()(
                 && operator.value.toString() === mod.operands[0]
             );
             if (selected) {
-              this.dropdownOption.selected[3] = selected.name;
-              formValues[meta.name] = {
+              this.dropdownOption.selected[3] = selected.label;
+              formState[meta.name] = {
                 operator: mod.operands[0],
                 encounterType: mod.encounterType
               };
@@ -209,14 +254,14 @@ export const ListModifierPage  = withCurrentWorkspace()(
               operator => operator.value === mod.operator
             );
             const index = this.modifiers.indexOf(meta);
-            this.dropdownOption.selected[index] = selected.name;
+            this.dropdownOption.selected[index] = selected.label;
             if (meta.modType === ModifierType.EVENTDATE) {
               this.dateObjs = [
                 new Date(mod.operands[0] + 'T08:00:00'),
                 new Date(mod.operands[1] + 'T08:00:00')
               ];
             }
-            formValues[meta.name] = {
+            formState[meta.name] = {
               operator: mod.operator,
               valueA: mod.operands[0],
               valueB: mod.operands[1],
@@ -224,99 +269,78 @@ export const ListModifierPage  = withCurrentWorkspace()(
           }
         }
       });
-      this.setState({formValues});
+      this.setState({formState});
     }
 
     showCount(modName: string, optName: string) {
       return modName === 'encounters' && optName !== 'Any';
     }
 
-    selectChange(opt, index, e, mod) {
-      const {formValues} = this.state;
-      this.dropdownOption.selected[index] = opt.name;
-      if (mod.name === 'encounters') {
-        formValues.encounters.encounterType = opt.name;
-      } else if (opt.name === 'Any') {
-        formValues[mod.name] = {
+    selectChange(sel, index) {
+      const {formState} = this.state;
+      const {name} = formState[index];
+      if (name === 'encounters') {
+        formState[index].values.encounterType = sel;
+      } else if (!sel) {
+        formState[index].values = {
           operator: undefined,
           valueA: undefined,
           valueB: undefined,
         };
       }
-      formValues[mod.name].operator = opt.value;
-      this.setState({formValues});
+      formState[index].values.operator = sel;
+      this.setState({formState});
     }
 
-    currentMods = (vals) => {
-      this.errors = new Set();
-      return this.modifiers.map(mod => {
-        const {name, inputType, maxLength, modType} = mod;
-        if (modType === ModifierType.ENCOUNTERS) {
-          if (!vals[name].operator) {
-            return;
-          }
-          return {name: modType, operator: 'IN',
-            encounterType: vals[name].encounterType.toString(),
-            operands: [vals[name].operator.toString()]};
-        } else {
-          const {operator, valueA, valueB} = vals[name];
-          const between = operator === 'BETWEEN';
-          if (!operator || (!valueA && !valueB)) {
-            if (inputType !== 'date'
-              && (this.form.get([name, 'valueA']).dirty || this.form.get([name, 'valueB']).dirty)) {
-              this.errors.add({name, type: 'integer'});
-            }
-            return;
-          }
-          if (inputType === 'date') {
-            const dateValueA = moment(valueA).format('YYYY-MM-DD');
-            const dateValueB = moment(valueB).format('YYYY-MM-DD');
-            const operands = [dateValueA];
-            if (between && dateValueB) {
-              operands.push(dateValueB);
-            }
-            return {name: modType, operator, operands};
-          } else {
-            const operands = [valueA];
-            if (between) {
-              operands.push(valueB);
-            }
-            operands.forEach((value, i) => {
-              const input = i === 0 ? 'valueA' : 'valueB';
-              if (value && value.length > maxLength) {
-                value = value.slice(0, maxLength);
-                this.form.get([name, input]).setValue(value, {emitEvent: false});
-              }
-            });
-            return {name: modType, operator, operands};
-          }
-        }
-      });
+    inputChange = (index: string, field: string, value: any) => {
+      const {formState} = this.state;
+      formState[index].values[field] = value;
+      this.setState({formState});
     }
 
     get addEncounters() {
       const {wizard: {domain}} = this.props;
       return [TreeType[TreeType.PM], TreeType[TreeType.VISIT]].indexOf(TreeType[domain]) === -1
-        && !this.modifiers.find(modifier => modifier.modType === ModifierType.ENCOUNTERS);
+          && !this.modifiers.find(modifier => modifier.modType === ModifierType.ENCOUNTERS);
     }
 
     requestPreview() {
       // TODO calculate count when new api call is ready
     }
 
-    ngOnDestroy() {
-      this.subscription.unsubscribe();
-    }
+    // dateBlur(index: number) {
+    //   const control = index === 0 ? 'valueA' : 'valueB';
+    //   this.dateObjs[index] = new Date(this.form.get(['eventDate', control]).value + 'T08:00:00');
+    // }
+    //
+    // get disableCalculate() {
+    //   const disable = !!this.preview.requesting || !!this.errors.size || this.form.invalid;
+    //   this.props.disabled(disable);
+    //   return disable;
+    // }
 
-    dateBlur(index: number) {
-      const control = index === 0 ? 'valueA' : 'valueB';
-      this.dateObjs[index] = new Date(this.form.get(['eventDate', control]).value + 'T08:00:00');
-    }
-
-    get disableCalculate() {
-      const disable = !!this.preview.requesting || !!this.errors.size || this.form.invalid;
-      this.props.disabled(disable);
-      return disable;
+    render() {
+      const {formState} = this.state;
+      return <div>
+        {formState.map((mod, i) => {
+          const {label, name, options, values: {operator, valueA, valueB}} = mod;
+          return <div key={i}>
+            <label>{label}</label>
+            {name === 'eventDate' && <ClrIcon shape='info-standard'/>}
+            <Select value={operator}
+              onChange={(e) => this.selectChange(e, i)}
+              options={options} />
+            {operator && name !== 'encounters' && <React.Fragment>
+              <input value={valueA}
+                onChange={(e) => this.inputChange(i, 'valueA', e.target.value)}/>
+              {operator === 'Between' &&
+                <input value={valueB}
+                  onChange={(e) => this.inputChange(i, 'valueB', e.target.value)}/>
+              }
+            </React.Fragment>}
+          </div>;
+        })}
+      </div>;
     }
   }
 );
@@ -328,6 +352,7 @@ export const ListModifierPage  = withCurrentWorkspace()(
 export class ListModifierPageComponent extends ReactWrapperBase {
   @Input('disabled') disabled: Props['disabled'];
   @Input('wizard') wizard: Props['wizard'];
+
   constructor() {
     super(ListModifierPage, ['disabled', 'wizard']);
   }
