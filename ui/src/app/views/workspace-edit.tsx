7d69c41fb2dd1f1d8c2c507970ad021c3557b7a4
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-06-11 15:45:45 -0400

    fix null pointer error (#2226)

--- ui/src/app/views/workspace-edit.tsx
+++ ui/src/app/views/workspace-edit.tsx
@@ -460,7 +460,8 @@ export const WorkspaceEdit = fp.flow(withRouteConfigData(), withCurrentWorkspace
 
     get noSpecificPopulationSelected() {
       return this.state.workspace.researchPurpose.population &&
-        this.state.workspace.researchPurpose.populationDetails.length === 0;
+        (!this.state.workspace.researchPurpose.populationDetails ||
+          this.state.workspace.researchPurpose.populationDetails.length === 0);
     }
 
     get noDiseaseOfFocusSpecified() {
@@ -738,8 +739,8 @@ export const WorkspaceEdit = fp.flow(withRouteConfigData(), withCurrentWorkspace
                    onChange={v => this.updateSpecificPopulation(SpecificPopulationEnum.OTHER, v)}
                    disabled={!this.state.workspace.researchPurpose.population}/>
                 <TextInput type='text' autoFocus placeholder='Please specify'
-                           disabled={!this.state.workspace.researchPurpose.populationDetails
-                             .includes(SpecificPopulationEnum.OTHER)}/>
+                           disabled={!fp.includes(SpecificPopulationEnum.OTHER,
+                             this.state.workspace.researchPurpose.populationDetails)}/>
               </div>
             </div>
           </div>
