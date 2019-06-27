aa057aaae23a9f45cd7fda6ba2566f07456fb1b6
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-06-27 13:48:11 -0500

    RW-2682 fix ui test

--- ui/src/app/cohort-search/list-search-group-item/list-search-group-item.component.spec.ts
+++ ui/src/app/cohort-search/list-search-group-item/list-search-group-item.component.spec.ts
@@ -7,26 +7,31 @@ import {ListSearchGroupItemComponent} from './list-search-group-item.component';
 
 import {registerApiClient} from 'app/services/swagger-fetch-clients';
 import {currentWorkspaceStore} from 'app/utils/navigation';
-import {TreeType} from 'generated';
-import {CohortBuilderApi} from 'generated/fetch';
+import {CohortBuilderApi, CriteriaType, DomainType} from 'generated/fetch';
 import {CohortBuilderServiceStub} from 'testing/stubs/cohort-builder-service-stub';
 import {workspaceDataStub} from 'testing/stubs/workspaces-api-stub';
 
 const zeroCrit = {
   id: 0,
-  type: TreeType[TreeType.ICD9],
+  name: 'Test param',
+  domain: DomainType.CONDITION,
+  type: CriteriaType.ICD9CM,
   code: 'CodeA',
 };
 
 const oneCrit = {
   id: 1,
-  type: TreeType[TreeType.ICD9],
+  name: 'Test param',
+  domain: DomainType.CONDITION,
+  type: CriteriaType.ICD9CM,
   code: 'CodeB',
 };
 
 const baseItem = {
   id: 'item001',
-  type: TreeType[TreeType.ICD9],
+  name: 'Test item',
+  domain: DomainType.CONDITION,
+  type: CriteriaType.ICD9CM,
   searchParameters: [],
   modifiers: [],
   count: null,
@@ -79,7 +84,7 @@ describe('ListSearchGroupItemComponent', () => {
     expect(display.childElementCount).toBe(2);
 
     const trimmedText = display.textContent.replace(/\s+/g, ' ').trim();
-    expect(trimmedText).toEqual('Contains ICD9 Codes');
+    expect(trimmedText).toEqual('Contains ICD9CM Codes');
   });
 
   it('Should properly pluralize \'Code\'', () => {
