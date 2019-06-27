0c20346073e68f5216bff02c4ad925c99e14ca81
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-06-27 10:39:07 -0400

    wip

--- ui/src/app/views/workspace-library.tsx
+++ ui/src/app/views/workspace-library.tsx
@@ -1,18 +1,35 @@
 import {Component} from '@angular/core';
 import * as React from 'react';
 
-import {ReactWrapperBase, withUserProfile} from 'app/utils';
+import {reactStyles, ReactWrapperBase, withUserProfile} from 'app/utils';
+
+const styles = reactStyles({
+  navPanel: {
+    display: 'flex', flexDirection: 'column', backgroundColor: '#E7E6F0',
+    minWidth: '300px', maxWidth: '300px', marginLeft: '-0.6rem', padding: '1rem'
+  }
+});
 
 export const WorkspaceLibrary = withUserProfile()
 (class extends React.Component<{}, {}> {
   constructor(props) {
     super(props);
   }
+
+  render() {
+    return <div style={{display: 'flex', flexDirection: 'row', height: '100%'}}>
+      <div style={styles.navPanel}>
+        nav
+      </div>
+      <div>
+        Researcher Workbench Workspace Library
+      </div>
+    </div>;
+  }
 });
 
-
 @Component({
-  template: '<div #root></div>'
+  template: '<div #root style="height: 100%"></div>'
 })
 export class WorkspaceLibraryComponent extends ReactWrapperBase {
   constructor() {
