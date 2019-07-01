195a34d89489077c7792331c48dd219c5f824b02
Eric Song <songe@broadinstitute.org>
2019-07-01 11:37:17 -0400

    add hashcode/equals

--- api/src/main/java/org/pmiops/workbench/google/GoogleCloudLocators.java
+++ api/src/main/java/org/pmiops/workbench/google/GoogleCloudLocators.java
@@ -1,6 +1,7 @@
 package org.pmiops.workbench.google;
 
 import com.google.cloud.storage.BlobId;
+import java.util.Objects;
 
 public class GoogleCloudLocators {
   public final BlobId blobId;
@@ -9,5 +10,20 @@ public class GoogleCloudLocators {
   public GoogleCloudLocators(BlobId blobId, String fullPath) {
     this.blobId = blobId;
     this.fullPath = fullPath;
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(blobId, fullPath);
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (!(obj instanceof GoogleCloudLocators)) {
+      return false;
+    }
+    GoogleCloudLocators that = (GoogleCloudLocators) obj;
+    return this.blobId.equals(that.blobId)
+        && this.fullPath.equals(that.fullPath);
   }
 }
