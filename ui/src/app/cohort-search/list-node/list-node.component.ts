34cf4bd1a51e33567f0e4bd734e7c2b0392e0e42
Will Dolbeer <WillDolbeer@victrs-mbp.dhcp.mc.vanderbilt.edu>
2019-07-01 12:23:25 -0500

    RW-2943 add separate api call for full trees

--- ui/src/app/cohort-search/list-node/list-node.component.ts
+++ ui/src/app/cohort-search/list-node/list-node.component.ts
@@ -33,6 +33,9 @@ export class ListNodeComponent implements OnInit, OnDestroy {
         setTimeout(() => scrollStore.next(this.node.id));
       }
     }));
+    if (this.wizard && this.wizard.fullTree) {
+      this.expanded = this.node.expanded || false;
+    }
   }
 
   ngOnDestroy() {
@@ -56,32 +59,70 @@ export class ListNodeComponent implements OnInit, OnDestroy {
     const {domainId, id, isStandard, name} = this.node;
     const type = domainId === DomainType.DRUG ? CriteriaType[CriteriaType.ATC] : this.node.type;
     try {
-      cohortBuilderApi().getCriteriaBy(cdrId, domainId, type, isStandard, id)
-        .then(resp => {
-          if (resp.items.length === 0 && domainId === DomainType.DRUG) {
-            cohortBuilderApi()
-              .getCriteriaBy(cdrId, domainId, CriteriaType[CriteriaType.RXNORM], isStandard, id)
-              .then(rxResp => {
-                this.empty = rxResp.items.length === 0;
-                this.children = rxResp.items;
-                this.loading = false;
-              }, () => this.error = true);
-          } else {
-            this.empty = resp.items.length === 0;
-            this.children = resp.items;
-            this.loading = false;
-            if (!this.empty && domainId === DomainType.SURVEY && !resp.items[0].group) {
-              // save questions in the store so we can display them along with answers if selected
-              const questions = ppiQuestions.getValue();
-              questions[id] = name;
-              ppiQuestions.next(questions);
+      if (!this.wizard.fullTree) {
+        cohortBuilderApi().getCriteriaBy(cdrId, domainId, type, isStandard, id)
+          .then(resp => {
+            if (resp.items.length === 0 && domainId === DomainType.DRUG) {
+              cohortBuilderApi()
+                .getCriteriaBy(cdrId, domainId, CriteriaType[CriteriaType.RXNORM], isStandard, id)
+                .then(rxResp => {
+                  this.empty = rxResp.items.length === 0;
+                  this.children = rxResp.items;
+                  this.loading = false;
+                }, () => this.error = true);
+            } else {
+              this.empty = resp.items.length === 0;
+              this.children = resp.items;
+              this.loading = false;
+              if (!this.empty && domainId === DomainType.SURVEY && !resp.items[0].group) {
+                // save questions in the store so we can display them along with answers if selected
+                const questions = ppiQuestions.getValue();
+                questions[id] = name;
+                ppiQuestions.next(questions);
+              }
             }
-          }
-        });
+          });
+      } else {
+        cohortBuilderApi().getCriteriaBy(cdrId, domainId, type)
+          .then(resp => {
+            console.log(resp);
+            this.empty = resp.items.length === 0;
+            this.loading = false;
+            let children = [];
+            resp.items.forEach(child => {
+              child['children'] = [];
+              if (child.parentId === 0) {
+                children.push(child);
+              } else {
+                children = this.addChildToParent(child, children);
+              }
+            });
+            this.children = children;
+          });
+      }
     } catch (error) {
-      console.log(error);
+      console.error(error);
       this.error = true;
       this.loading = false;
+    }
+  }
+
+  addChildToParent(child, itemList) {
+    for (const item of itemList) {
+      if (!item.group) {
+        continue;
+      }
+      if (item.id === child.parentId) {
+        item.children.push(child);
+        return itemList;
+      }
+      if (item.children.length) {
+        const childList = this.addChildToParent(child, item.children);
+        if (childList) {
+          item.children = childList;
+          return itemList;
+        }
+      }
     }
   }
 
