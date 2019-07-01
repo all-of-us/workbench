48f7ea68f4a651f3d2fcaa9c87037e8aa7a91e36
Jackie Roberti <jacqueline.roberti93@gmail.com>
2019-07-01 17:51:57 -0400

    ui wip. todo: button hoover/selected, workspaces render

--- ui/src/app/views/workspace-library.tsx
+++ ui/src/app/views/workspace-library.tsx
@@ -1,9 +1,9 @@
 import {Component} from '@angular/core';
 import * as React from 'react';
 
+import {Clickable} from 'app/components/buttons';
 import {Header} from 'app/components/headers';
 import {ClrIcon} from 'app/components/icons';
-import {TextInput} from 'app/components/inputs';
 import {reactStyles, ReactWrapperBase, withUserProfile} from 'app/utils';
 
 const styles = reactStyles({
@@ -15,31 +15,81 @@ const styles = reactStyles({
     height: '2rem', fontSize: '16px', lineHeight: '19px', paddingLeft: '2rem',
     marginTop: '0.5rem', borderStyle: 'none'
   },
+  iconStyling: {
+    color: '#262262', marginRight: '0.5rem', marginLeft: '0.5rem'
+  },
+  menuLink: {
+    color: '#216FB4', fontSize: 14, fontWeight: 600, display: 'flex',
+    flexDirection: 'row', alignItems: 'center', padding: '0.3rem 0'
+  },
+  divider: {
+    width: '100%', backgroundColor: '#262262', borderWidth: '0px', height: '1px',
+    marginBottom: '0.5rem'
+  }
 });
 
+const libraryTabEnums = {
+  FEATURED_WORKSPACES: {
+    title: 'Featured Workspaces',
+    icon: 'star'
+  },
+  PUBLISHED_WORKSPACES: {
+    title: 'Published Workspaces',
+    icon: 'library'
+  }
+};
+
+const libraryTabs = [
+  libraryTabEnums.FEATURED_WORKSPACES,
+  libraryTabEnums.PUBLISHED_WORKSPACES
+];
+
+const LibraryTab: React.FunctionComponent<{title: string, icon: string, onClick: Function}> =
+  ({title, icon, onClick}) => {
+    return <Clickable style={styles.menuLink} onClick={onClick}>
+      <ClrIcon shape={icon} style={styles.iconStyling} class='is-solid' size={24}/>
+      {title}
+    </Clickable>;
+  };
+
 export const WorkspaceLibrary = withUserProfile()
-(class extends React.Component<{}, {}> {
+(class extends React.Component<{}, {currentTab: {title: string, icon: string}}> {
   constructor(props) {
     super(props);
-  }
-
-  triggerSearch(e) {
-
+    this.state = {
+      currentTab: libraryTabEnums.PUBLISHED_WORKSPACES
+    };
   }
 
   render() {
     return <div style={{display: 'flex', flexDirection: 'row', height: '100%'}}>
       <div style={styles.navPanel}>
-        <div style={{display: 'flex', flexDirection: 'row'}}>
-          <ClrIcon shape='search' style={{position: 'absolute', height: '1rem', width: '1rem',
-            fill: '#216FB4', left: 'calc(1.4rem)', top: '5.5%'}}/>
-          <TextInput style={styles.searchBar}
-                     placeholder='Search'
-                     onKeyDown={e => {this.triggerSearch(e); }}/>
+        <div style={{display: 'flex', flexDirection: 'column'}}>
+          {libraryTabs.map((tab, i) => {
+            return <React.Fragment key={i}>
+              <LibraryTab icon={tab.icon} title={tab.title}
+                          onClick={() => this.setState({currentTab: tab})}/>
+                {i !== libraryTabs.length - 1 &&
+                <hr style={{width: '100%', marginBottom: '0.5rem'}}/>}
+            </React.Fragment>;
+          })}
         </div>
       </div>
-      <div style={{padding: '1rem'}}>
+      <div style={{padding: '1rem', width: '100%'}}>
         <Header>RESEARCHER WORKBENCH WORKSPACE LIBRARY</Header>
+        <div style={{color: '#262262', fontSize: 16, marginTop: '1rem'}}>
+          Search through featured and public workspaces.
+        </div>
+        <div style={{display: 'flex', flexDirection: 'column', marginTop: '2rem'}}>
+          <div style={{display: 'flex', flexDirection: 'row'}}>
+            <ClrIcon shape={this.state.currentTab.icon} style={styles.iconStyling}
+                     class='is-solid' size={24}/>
+            <div style={{color: '#262262', fontSize: 18, fontWeight: 600}}>
+              {this.state.currentTab.title}
+            </div>
+          </div>
+          <hr style={styles.divider}/>
+        </div>
       </div>
     </div>;
   }
