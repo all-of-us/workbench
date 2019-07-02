55b04a64d74ad4f6a7bfd7c45133ad860cd0609b
Andrew <ajang@broadinstitute.org>
2019-07-02 13:11:20 -0400

    Update load_vars.sh
    for zsh (and bash) compatibility

--- api/db/load_vars.sh
+++ api/db/load_vars.sh
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
 # This script will load the necessary environment variables into your environment
 # This needs to be run when starting our services without docker which has its own mechanisms
@@ -7,11 +7,11 @@
 for line in $(awk '!/^ *#/ && NF' $WORKBENCH_DIR/api/db/vars.env); do
   IFS='=' read -r var val <<< "$line"
 
-  if [ "$var" == "DB_HOST" ]; then
+  if [[ "$var" == "DB_HOST" ]]; then
     export DB_HOST=localhost
     continue
   fi
 
-  evaluatedString=$(echo $(eval "echo $val"))
-  export $var=$evaluatedString
+  evaluatedString=$(echo $val) #these 2 lines expand the variable inside $val without using eval, for zsh compatibility
+  export $var=$(echo "$evaluatedString")
 done
