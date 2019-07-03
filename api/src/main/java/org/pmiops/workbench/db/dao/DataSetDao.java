7242272dfb2c8bc6a1cb72141a9017de4a94ce13
Neha Saxena <nsaxena@broadinstitute.org>
2019-07-02 22:12:57 -0400

    Change text for cohort/concept used in dataset is deleted

--- api/src/main/java/org/pmiops/workbench/db/dao/DataSetDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/DataSetDao.java
@@ -7,10 +7,6 @@ import org.springframework.data.repository.CrudRepository;
 public interface DataSetDao extends CrudRepository<DataSet, Long> {
   List<DataSet> findByWorkspaceIdAndInvalid(long workspaceId, boolean invalid);
 
-  int countByCohortSetIdContains(long cohortId);
-
-  int countByConceptSetIdContains(long conceptId);
-
   List<DataSet> findDataSetsByCohortSetId(long cohortId);
 
   List<DataSet> findDataSetsByConceptSetId(long conceptId);
