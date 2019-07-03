e92716dc9c9348155a1cf9e36e44c616e529307a
Gregory Jordan <gjordan@verily.com>
2019-07-03 08:41:45 -0400

    Add a opacity param to SpinnerOverlay.

--- ui/src/app/components/spinners.tsx
+++ ui/src/app/components/spinners.tsx
@@ -1,10 +1,11 @@
 import colors from 'app/styles/colors';
 import {reactStyles} from 'app/utils/index';
 import * as React from 'react';
+import * as Color from 'Color';
 
 const styles = reactStyles({
   overlay: {
-    backgroundColor: 'rgba(0, 0, 0, 0)',
+    backgroundColor: 'rgba(200, 200, 200, 0)',
     position: 'absolute', top: 0, left: 0, bottom: 0, right: 0,
     display: 'flex', justifyContent: 'center', alignItems: 'center',
     zIndex: 1
@@ -30,10 +31,20 @@ export const Spinner = ({dark = false, size = 72, style = {}, ...props}) => {
   </svg>;
 };
 
+interface SpinnerOverlayProps {
+  dark?: boolean,
+  opacity?: number,
+  overrideStylesOverlay?: React.CSSProperties,
+  overrideStylesSquare?: React.CSSProperties
+};
+
 export const SpinnerOverlay = ({dark = false,
-                                 overrideStylesOverlay = {},
-                                 overrideStylesSquare = {}}) => {
-  return <div style={{...styles.overlay, ...overrideStylesOverlay}}>
+                                 opacity = 0,
+                                 overrideStylesOverlay= {},
+                                 overrideStylesSquare = {}}: SpinnerOverlayProps) => {
+  return <div style={{...styles.overlay, ...overrideStylesOverlay,
+    backgroundColor: Color(overrideStylesOverlay.backgroundColor || styles.overlay.backgroundColor)
+      .alpha(opacity).toString()}}>
     <div style={{...styles.square, ...overrideStylesSquare}}><Spinner dark={dark} /></div>
   </div>;
 };
