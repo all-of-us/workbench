f8bbb554efeb65e3a5e9eaf44e313f305b061b86
Neha Saxena <nsaxena@broadinstitute.org>
2019-07-16 23:02:02 -0400

    fix test

--- common-api/src/main/java/org/pmiops/workbench/db/model/CommonStorageEnums.java
+++ common-api/src/main/java/org/pmiops/workbench/db/model/CommonStorageEnums.java
@@ -18,6 +18,7 @@ public class CommonStorageEnums {
           .put(Domain.OBSERVATION, (short) 5)
           .put(Domain.PROCEDURE, (short) 6)
           .put(Domain.VISIT, (short) 7)
+          .put(Domain.SURVEY, (short)8)
           .build();
 
   // A mapping from our Domain enum to OMOP domain ID values.
@@ -31,6 +32,7 @@ public class CommonStorageEnums {
           .put(Domain.OBSERVATION, "Observation")
           .put(Domain.PROCEDURE, "Procedure")
           .put(Domain.VISIT, "Visit")
+          .put(Domain.SURVEY, "Survey")
           .build();
 
   public static Domain domainFromStorage(Short domain) {
