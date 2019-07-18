029e889d02bcee55fdfca1e2aed64852d2506ccb
CH Albach <calbach@users.noreply.github.com>
2019-07-18 14:03:47 -0700

    [RW-3077][risk=low] rm unused tcell (#2388)

--- ui/src/environments/environment.staging.ts
+++ ui/src/environments/environment.staging.ts
@@ -5,8 +5,6 @@ export const environment: Environment = {
   shouldShowDisplayTag: true,
   allOfUsApiUrl: 'https://api-dot-all-of-us-rw-staging.appspot.com',
   clientId: '657299777109-kvb5qafr70bl01i6bnpgsiq5nt6v1o8u.apps.googleusercontent.com',
-  tcellappid: 'AoUNonProd-WZFW2',
-  tcellapikey: 'AQEBBAEkx4iE2KxNyI7Wx08EwU1ycTM7E4FMSmaibbMUQxNU6uQvuAJt7fyABAtFYSYfgEE',
   leoApiUrl: 'https://notebooks.firecloud.org',
   publicUiUrl: 'https://aou-db-staging.appspot.com',
   debug: false,
