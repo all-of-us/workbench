272cb765e1e86ca28731fc03c3fbb31d6753ae73
CH Albach <calbach@google.com>
2019-07-18 12:51:47 -0700

    rm unused tcell

--- ui/src/environments/environment.stable.ts
+++ ui/src/environments/environment.stable.ts
@@ -5,8 +5,6 @@ export const environment: Environment = {
   shouldShowDisplayTag: true,
   allOfUsApiUrl: 'https://api-dot-all-of-us-rw-stable.appspot.com',
   clientId: '56507752110-ovdus1lkreopsfhlovejvfgmsosveda6.apps.googleusercontent.com',
-  tcellappid: 'AoUNonProd-WZFW2',
-  tcellapikey: 'AQEBBAEkx4iE2KxNyI7Wx08EwU1ycTM7E4FMSmaibbMUQxNU6uQvuAJt7fyABAtFYSYfgEE',
   leoApiUrl: 'https://notebooks.firecloud.org',
   publicUiUrl: 'https://www.databrowser.stable.fake-research-aou.org',
   debug: false,
