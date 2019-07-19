54b8e85d8371eb33885dae94a7756a1de8f2191e
Brian Freeman <brian.d.freeman@gmail.com>
2019-07-19 10:03:56 -0500

    fixing weird jpa problem.

--- common-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
+++ common-api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
@@ -11,7 +11,7 @@ import org.springframework.data.repository.query.Param;
 /**
  * Some of our trees are polyhierarchical(Snomed and drug). Since a node may exist multiple times in
  * this scenario with the same concept_id we only want to return it once from a user search. The
- * query to rank/min this to a single row was expensive. Instead, when build the the cb_criteria
+ * query to rank/min this to a single row was expensive. Instead, when building the cb_criteria
  * table we add domain_rank1 to the synonyms column for the first node that matches in the tree for
  * a specific concept_id. This allows us to use the full text index and makes the query much faster.
  */
@@ -19,11 +19,34 @@ public interface CBCriteriaDao extends CrudRepository<CBCriteria, Long> {
 
   /**
    * This query returns the parents in addition to the criteria ancestors for each leave in order to
-   * allow the client to determine the relationship between the returned Criteria.
+   * allow the client to determine the relationship between the returned Criteria. Since we use
+   * the @Id annotation in the entity, JPA will treat those rows with the given id as a unique
+   * element. For example if you have 3 rows where this @Id annotated field is the same, JPA will
+   * use the first result elements and just duplicate them.
+   *
+   * <p>select statement returns:
+   *
+   * <p>id: 111, concept_id : 111
+   *
+   * <p>id: 111, concept_id : 222
+   *
+   * <p>id: 111, concept_id : 333
+   *
+   * <p>The result will be 3 rows retrieved by JPA:
+   *
+   * <p>id: 111 concept_id: 111
+   *
+   * <p>id: 111 concept_id: 111
+   *
+   * <p>id: 111 concept_id: 111
+   *
+   * <p>So to keep this from happening we use the descendant_id from cb_criteria_ancestor as the
+   * unique id. Please do not use this method if you need proper cb_criteria ids, we don't in the
+   * current use case.
    */
   @Query(
       value =
-          "select distinct id,parent_id,domain_id,is_standard,type,subtype,ca.descendant_id as concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,"
+          "select ca.descendant_id as id,parent_id,domain_id,is_standard,type,subtype,ca.descendant_id as concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,"
               + "has_ancestor_data,path,synonyms "
               + "from cb_criteria_ancestor ca "
               + "   join (select a.* "
@@ -33,7 +56,7 @@ public interface CBCriteriaDao extends CrudRepository<CBCriteria, Long> {
               + "                  where concept_id in (:parentConceptIds) "
               + "                    and domain_id = :domain "
               + "                    and type = :type "
-              + "                    and is_group = 1) b "
+              + "                    and is_group = :group) b "
               + "             on (a.path like b.path) "
               + "          where domain_id = :domain "
               + "            and is_group = 0  "
@@ -51,6 +74,7 @@ public interface CBCriteriaDao extends CrudRepository<CBCriteria, Long> {
   List<CBCriteria> findCriteriaAncestors(
       @Param("domain") String domain,
       @Param("type") String type,
+      @Param("group") Boolean group,
       @Param("parentConceptIds") Set<String> parentConceptIds);
 
   /** This query returns all parents matching the parentConceptIds. */
