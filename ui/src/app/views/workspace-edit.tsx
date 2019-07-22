67086c146b44a422db6436ddc0a5ec7928ecdb97
Jesse Engle <jengle@broadinstitute.org>
2019-07-22 18:37:39 -0400

    [RW-3070][risk=low] Fixes race condition in disease ontology search.

--- ui/src/app/views/workspace-edit.tsx
+++ ui/src/app/views/workspace-edit.tsx
@@ -378,28 +378,58 @@ function getDiseaseNames(keyword) {
   });
 }
 
+// Fifo accepts a function 'fn' that returns a promise, and returns a
+// new function that returns a promise. Promises returned by the
+// resulting function return their result of the last-instantiated
+// promise. For example, if a0, a1 are the start and end times of a
+// computation represented by promise A, and b0, b1 are the start and end
+// times of a computation represented by promise B, and a0 < b0 < b1 <
+// a1, then the value returned by promise A at time a1 will be the result
+// of promise B, *not* promise A, because B was instantiated after A.
+function fifo(fn) {
+  let nextCallID = 1;
+  let lastCompletedCallID = 0;
+  let lastCompletedResult = null;
+  return (...args) => {
+    const thisCallID = nextCallID++;
+    return new Promise((accept, reject) => {
+      fn.apply(null, args).then((newResult) => {
+        if (thisCallID > lastCompletedCallID) {
+          lastCompletedCallID = thisCallID;
+          lastCompletedResult = newResult;
+        }
+        accept(lastCompletedResult);
+      }).catch((e) => {
+        reject(e);
+      });
+    });
+  }
+}
+
 class SearchInput extends React.Component<any, SearchInputState> {
   // Forward declarations
-  searchTermChangedEvent: Function;
-  START: number;
-  ACTIVE: number;
-  SUGGEST: number;
-  HOVER: number;
+  _searchTermChangedEvent: Function;
+  _onSearch: Function;
+  _START: number;
+  _ACTIVE: number;
+  _SUGGEST: number;
+  _HOVER: number;
 
   constructor(props) {
     super(props);
     this.state = {
       matches: [],
       hover: [],
-      state: this.START,
+      state: this._START,
     };
-    this.searchTermChangedEvent = fp.debounce(300, this._search);
+    this._onSearch = fifo(props.onSearch);
+    this._searchTermChangedEvent = fp.debounce(300, this._search);
 
     // State machine states
-    this.START = 0;   // Input box lacks focus, drop-down invisible
-    this.ACTIVE = 1;  // Input box focused, drop-down invisible
-    this.SUGGEST = 2; // Input box focused, search results visible
-    this.HOVER = 3;   // Input box focused, search results visible, item activated
+    this._START = 0;   // Input box lacks focus, drop-down invisible
+    this._ACTIVE = 1;  // Input box focused, drop-down invisible
+    this._SUGGEST = 2; // Input box focused, search results visible
+    this._HOVER = 3;   // Input box focused, search results visible, item activated
   }
 
   _toggleHover(j) {
@@ -414,45 +444,47 @@ class SearchInput extends React.Component<any, SearchInputState> {
     if (!keyword) {
       return;
     }
-    this.props.onSearch(keyword).then((matches) => {
+    this._onSearch(keyword).then((matches) => {
       this.setState({
         matches: matches,
         state: matches.length > 0 ?
-          this.SUGGEST : this.ACTIVE,
+          this._SUGGEST : this._ACTIVE,
         hover: matches.map(() => false),
       });
-    }).catch((e) => {});
+    }).catch((e) => {
+      console.error(e);
+    });
   }
 
   _onBlur() {
     const state = this.state.state;
-    if (state === this.ACTIVE  ||
-        state === this.SUGGEST ||
-        state === this.HOVER) {
-      this.setState({state: this.START});
+    if (state === this._ACTIVE  ||
+        state === this._SUGGEST ||
+        state === this._HOVER) {
+      this.setState({state: this._START});
     }
   }
 
   _onChange(match) {
-    this.setState({state: this.ACTIVE});
-    this.searchTermChangedEvent(match);
+    this.setState({state: this._ACTIVE});
+    this._searchTermChangedEvent(match);
     this.props.onChange(match);
   }
 
   _onFocus() {
-    this.setState({state: this.ACTIVE});
+    this.setState({state: this._ACTIVE});
   }
 
   _onMouseOver(j) {
     this.setState({
-      state: this.HOVER,
+      state: this._HOVER,
       hover: this._toggleHover(j)
     });
   }
 
   _onMouseOut(j) {
     this.setState({
-      state: this.SUGGEST,
+      state: this._SUGGEST,
       hover: this._toggleHover(j)
     });
   }
@@ -464,7 +496,7 @@ class SearchInput extends React.Component<any, SearchInputState> {
   _suggest() {
     const s = this.state.state;
     const rval = this.props.enabled &&
-      (s === this.SUGGEST || s === this.HOVER);
+      (s === this._SUGGEST || s === this._HOVER);
     return rval;
   }
 
@@ -486,13 +518,13 @@ class SearchInput extends React.Component<any, SearchInputState> {
         {this._suggest() &&
           <div data-test-id='drop-down'
             style={{...styles.dropdownMenu, ...styles.open, minWidth: '90%'}}>
-            {this.state.matches.map((disease, j) => {
+            {this.state.matches.map((match, j) => {
               return (
                 <div key={j} style={this.state.hover[j] ? styles.boxHover : styles.box}
                   onMouseOver={this._onMouseOver.bind(this, j)}
                   onMouseOut={this._onMouseOut.bind(this, j)}
-                  onMouseDown={this._onMouseDown.bind(this, disease)}>
-                  <h5 style={styles.boxHoverElement}>{disease}</h5>
+                  onMouseDown={this._onMouseDown.bind(this, match)}>
+                  <h5 style={styles.boxHoverElement}>{match}</h5>
                 </div>
               );
             })}
