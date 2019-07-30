2e5e53f2eb62983c6cb21a236cd80d5327feca40
CH Albach <calbach@users.noreply.github.com>
2019-07-30 13:57:41 -0700

    [RW-3077][risk=no] Drop remaining missed reference to tcell

--- ui/src/environments/test-env-base.ts
+++ ui/src/environments/test-env-base.ts
@@ -3,8 +3,6 @@
 export const testEnvironmentBase = {
   allOfUsApiUrl: 'https://api-dot-all-of-us-workbench-test.appspot.com',
   clientId: '602460048110-5uk3vds3igc9qo0luevroc2uc3okgbkt.apps.googleusercontent.com',
-  tcellappid: 'AoUNonProd-WZFW2',
-  tcellapikey: 'AQEBBAEkx4iE2KxNyI7Wx08EwU1ycTM7E4FMSmaibbMUQxNU6uQvuAJt7fyABAtFYSYfgEE',
   // Keep in sync with config_test.json.
   leoApiUrl: 'https://leonardo.dsde-dev.broadinstitute.org',
   publicUiUrl: 'https://aou-db-test.appspot.com',
