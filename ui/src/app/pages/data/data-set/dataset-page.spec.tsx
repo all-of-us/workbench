0070bfccf44903b34665f8c4449ca21cb6bc4864
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2019-08-12 11:14:03 -0400

    [RW-3231][RISK=NO] Update Data set page (#2461)
    * Update data set page
    
    * extracting styles
    
    * test update for data set page change
    
    * PR comments indent disable link
    
    * Term data set instead of dataset
    
    * data set instead of dataset
    
    * concept set button text change

--- ui/src/app/pages/data/data-set/dataset-page.spec.tsx
+++ ui/src/app/pages/data/data-set/dataset-page.spec.tsx
@@ -1,10 +1,10 @@
 import {mount} from 'enzyme';
 import * as React from 'react';
 
-import {Button} from 'app/components/buttons';
+import {Button, Clickable} from 'app/components/buttons';
+import {DataSetPage} from 'app/pages/data/data-set/dataset-page';
 import {dataSetApi, registerApiClient} from 'app/services/swagger-fetch-clients';
 import {currentWorkspaceStore, NavStore, urlParamsStore} from 'app/utils/navigation';
-import {DataSetPage} from 'app/pages/data/data-set/dataset-page';
 import {
   CohortsApi,
   ConceptsApi,
@@ -89,7 +89,7 @@ describe('DataSet', () => {
       const saveButton = wrapper.find(Button).find('[data-test-id="save-button"]')
         .first();
       expect(saveButton.prop('disabled')).toBeTruthy();
-      const previewButton = wrapper.find(Button).find('[data-test-id="preview-button"]')
+      const previewButton = wrapper.find(Clickable).find('[data-test-id="preview-button"]')
         .first();
       expect(previewButton.prop('disabled')).toBeTruthy();
 
@@ -111,8 +111,8 @@ describe('DataSet', () => {
       const buttons = wrapper.find(Button);
       expect(buttons.find('[data-test-id="save-button"]').first().prop('disabled'))
         .toBeFalsy();
-      expect(buttons.find('[data-test-id="preview-button"]').first().prop('disabled'))
-        .toBeFalsy();
+      expect(wrapper.find(Clickable).find('[data-test-id="preview-button"]').first()
+        .prop('disabled')).toBeFalsy();
     });
 
   it('should display preview data table once preview button is clicked', async() => {
@@ -147,81 +147,6 @@ describe('DataSet', () => {
 
   });
 
-  it('should refresh preview data table once refresh preview link is clicked', async() => {
-    const spy = jest.spyOn(dataSetApi(), 'previewQuery');
-    const wrapper = mount(<DataSetPage />);
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-
-    // Should click one cohort concept and value
-    wrapper.find('[data-test-id="cohort-list-item"]').first()
-      .find('input').first().simulate('change');
-    wrapper.update();
-
-    wrapper.find('[data-test-id="concept-set-list-item"]').first()
-      .find('input').first().simulate('change');
-
-    await waitOneTickAndUpdate(wrapper);
-
-    wrapper.find('[data-test-id="value-list-items"]').find('input').first()
-      .simulate('change');
-    await waitOneTickAndUpdate(wrapper);
-
-    // Click preview button to load preview
-    wrapper.find({'data-test-id': 'preview-button'}).first().simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-
-    wrapper.find('[data-test-id="refresh-preview-clickable-text"]').first().simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-
-    expect(spy).toHaveBeenCalledTimes(2);
-  });
-
-  // This test verifies that the refresh preview link is enabled only if cohort, concept, and value
-  // are all selected AND the preview button has been clicked. Otherwise the link is disabled.
-  it('should enable the refresh preview link when preview button is clicked', async() => {
-    const wrapper = mount(<DataSetPage />);
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-
-    // Check refresh preview link is disabled by default
-    expect(wrapper.find('[data-test-id="refresh-preview-clickable-text"]').first().prop('disabled'))
-      .toBeTruthy();
-
-    // Click one cohort concept and value
-    wrapper.find('[data-test-id="cohort-list-item"]').first()
-      .find('input').first().simulate('change');
-    wrapper.update();
-
-    wrapper.find('[data-test-id="concept-set-list-item"]').first()
-      .find('input').first().simulate('change');
-
-    await waitOneTickAndUpdate(wrapper);
-
-    wrapper.find('[data-test-id="value-list-items"]').find('input').first()
-      .simulate('change');
-    await waitOneTickAndUpdate(wrapper);
-
-    // Check refresh preview link is still disabled since Preview button was not clicked.
-    expect(wrapper.find('[data-test-id="refresh-preview-clickable-text"]').first().prop('disabled'))
-      .toBeTruthy();
-
-    // Click preview button to load preview
-    wrapper.find({'data-test-id': 'preview-button'}).first().simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-
-    // Check refresh preview link is now enabled
-    expect(wrapper.find('[data-test-id="refresh-preview-clickable-text"]').first().prop('disabled'))
-      .toBeFalsy();
-
-    // Uncheck the Value checkbox to verify that the refresh button is disabled.
-    wrapper.find('[data-test-id="value-list-items"]').find('input').first()
-      .simulate('change');
-    await waitOneTickAndUpdate(wrapper);
-
-    expect(wrapper.find('[data-test-id="refresh-preview-clickable-text"]').first().prop('disabled'))
-      .toBeTruthy();
-  });
 
   it('should check that the Cohorts and Concept Sets "+" links go to their pages.', async() => {
     const wrapper = mount(<DataSetPage />);
