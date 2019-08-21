93e0a5bcf86b118fcdf31cfc4fbbd4f74286c399
Ariel Schwartz <als364@cornell.edu>
2019-08-21 14:40:29 -0400

    [RW-3229] [risk=low] Enable copy notebook / concept set from readonly workspace (#2488)
    * copy concept set to other workspace
    
    * update props / state conventions in concept-set-details
    
    * name collision oops
    
    * this should(tm) enable copying notebooks with READER
    
    * linting
    
    * review comments
    
    * linting again

--- ui/src/app/components/resources.tsx
+++ ui/src/app/components/resources.tsx
@@ -9,12 +9,13 @@ import {ResourceType} from 'app/utils/resourceActionsReact';
 
 export const ResourceCardMenu: React.FunctionComponent<{
   disabled: boolean, resourceType: ResourceType, onRenameResource?: Function,
-  onCloneResource?: Function, onCopyConceptSet?: Function, onDeleteResource?: Function,
-  onEdit?: Function, onExportDataSet: Function, onReviewCohort?: Function,
+  onCloneResource?: Function, onCopyConceptSet?: Function, canDelete: boolean,
+  onDeleteResource?: Function, canEdit: boolean, onEdit?: Function,
+  onExportDataSet: Function, onReviewCohort?: Function,
 }> = ({
         disabled, resourceType, onRenameResource = () => {}, onCloneResource = () => {},
-        onCopyConceptSet = () => {}, onDeleteResource = () => {}, onEdit = () => {},
-        onExportDataSet = () => {}, onReviewCohort = () => {}
+        onCopyConceptSet = () => {}, canDelete, onDeleteResource = () => {},
+        canEdit, onEdit = () => {}, onExportDataSet = () => {}, onReviewCohort = () => {}
       }) => {
   return <PopupTrigger
     data-test-id='resource-card-menu'
@@ -24,32 +25,48 @@ export const ResourceCardMenu: React.FunctionComponent<{
       switchCase(resourceType,
         ['cohort', () => {
           return <React.Fragment>
-            <MenuItem icon='note' onClick={onRenameResource}>Rename</MenuItem>
-            <MenuItem icon='copy' onClick={onCloneResource}>Duplicate</MenuItem>
-            <MenuItem icon='pencil' onClick={onEdit}>Edit</MenuItem>
-            <MenuItem icon='grid-view' onClick={onReviewCohort}>Review</MenuItem>
-            <MenuItem icon='trash' onClick={onDeleteResource}>Delete</MenuItem>
+            <MenuItem icon='note' onClick={onRenameResource} disabled={!canEdit}>Rename</MenuItem>
+            <MenuItem icon='copy' onClick={onCloneResource} disabled={!canEdit}>
+              Duplicate
+            </MenuItem>
+            <MenuItem icon='pencil' onClick={onEdit} disabled={!canEdit}>Edit</MenuItem>
+            <MenuItem icon='grid-view' onClick={onReviewCohort} disabled={!canEdit}>
+              Review
+            </MenuItem>
+            <MenuItem icon='trash' onClick={onDeleteResource} disabled={!canDelete}>
+              Delete
+            </MenuItem>
           </React.Fragment>;
         }],
         ['cohortReview', () => {
           return <React.Fragment>
-            <MenuItem icon='note' onClick={onRenameResource}>Rename</MenuItem>
-            <MenuItem icon='trash' onClick={onDeleteResource}>Delete</MenuItem>
+            <MenuItem icon='note' onClick={onRenameResource} disabled={!canEdit}>Rename</MenuItem>
+            <MenuItem icon='trash' onClick={onDeleteResource} disabled={!canDelete}>
+              Delete
+            </MenuItem>
           </React.Fragment>;
         }],
         ['conceptSet', () => {
           return <React.Fragment>
-            <MenuItem icon='pencil' onClick={onEdit}>Rename</MenuItem>
+            <MenuItem icon='pencil' onClick={onEdit} disabled={!canEdit}>Rename</MenuItem>
             <MenuItem icon='copy' onClick={onCopyConceptSet}>Copy to another workspace</MenuItem>
-            <MenuItem icon='trash' onClick={onDeleteResource}>Delete</MenuItem>
+            <MenuItem icon='trash' onClick={onDeleteResource} disabled={!canDelete}>
+              Delete
+            </MenuItem>
           </React.Fragment>;
         }],
         ['dataSet', () => {
           return <React.Fragment>
-            <MenuItem icon='pencil' onClick={onRenameResource}>Rename Data Set</MenuItem>
-            <MenuItem icon='pencil' onClick={onEdit}>Edit</MenuItem>
-            <MenuItem icon='clipboard' onClick={onExportDataSet}>Export to Notebook</MenuItem>
-            <MenuItem icon='trash' onClick={onDeleteResource}>Delete</MenuItem>
+            <MenuItem icon='pencil' onClick={onRenameResource} disabled={!canEdit}>
+              Rename Data Set
+            </MenuItem>
+            <MenuItem icon='pencil' onClick={onEdit} disabled={!canEdit}>Edit</MenuItem>
+            <MenuItem icon='clipboard' onClick={onExportDataSet} disabled={!canEdit}>
+              Export to Notebook
+            </MenuItem>
+            <MenuItem icon='trash' onClick={onDeleteResource} disabled={!canDelete}>
+              Delete
+            </MenuItem>
           </React.Fragment>;
         }]
       )
