52fb54b2c337c1bfd293b34f691c63cb3a321c13
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro.local>
2019-08-26 17:20:58 -0500

    RW-2484 add container for content with open/close transitions

--- ui/src/app/components/help-sidebar.tsx
+++ ui/src/app/components/help-sidebar.tsx
@@ -2,7 +2,7 @@ import {Component, Input} from '@angular/core';
 import * as React from 'react';
 
 import {ClrIcon} from 'app/components/icons';
-import colors from 'app/styles/colors';
+import colors, {colorWithWhiteness} from 'app/styles/colors';
 import {reactStyles, ReactWrapperBase} from 'app/utils';
 
 const styles = reactStyles({
@@ -11,34 +11,81 @@ const styles = reactStyles({
     top: 0,
     right: 0,
     height: '100%',
-    background: colors.secondary,
-    padding: '0.25rem'
+    marginRight: '-45px',
+  },
+  content: {
+    position: 'absolute',
+    top: 0,
+    right: '45px',
+    height: '100%',
+    overflow: 'hidden',
+    background: colors.light,
+    transition: 'width 0.5s ease-out'
+  },
+  iconContainer: {
+    position: 'absolute',
+    top: 0,
+    right: 0,
+    textAlign: 'center',
+    height: '100%',
+    width: '45px',
+    background: colorWithWhiteness(colors.primary, 0.4),
+    zIndex: 10010
   },
   icon: {
-    color: colors.white
+    color: colors.white,
+    marginTop: '1.25rem',
+    cursor: 'pointer'
   }
 });
+
+const contentStyles = {
+  closed: {
+    ...styles.content,
+    width: 0
+  },
+  open: {
+    ...styles.content,
+    width: '10rem'
+  },
+};
 
 interface Props {
   location: string;
 }
 
 interface State {
-  open: boolean;
+  activeIcon: string;
+  sidebarOpen: boolean;
   searchTerm: string;
 }
 export class HelpSidebar extends React.Component<Props, State> {
   constructor(props: Props) {
     super(props);
     this.state = {
-      open: false,
+      activeIcon: undefined,
+      sidebarOpen: false,
       searchTerm: undefined
     };
   }
 
   render() {
+    const {location} = this.props;
+    const {activeIcon, sidebarOpen} = this.state;
     return <div style={styles.sidebar}>
-      <div><ClrIcon className='is-solid' shape='info-standard' size={22} style={styles.icon}/></div>
+      <div style={sidebarOpen ? contentStyles.open : contentStyles.closed}>
+        <h3>Sidebar Content</h3>
+      </div>
+      <div style={styles.iconContainer}>
+        <ClrIcon className='is-solid' shape='info-standard' size={28} style={styles.icon}
+          onClick={() => this.setState({activeIcon: 'info', sidebarOpen: !sidebarOpen})} />
+        <ClrIcon className='is-solid' shape='book' size={32} style={styles.icon}
+          onClick={() => this.setState({activeIcon: 'book', sidebarOpen: !sidebarOpen})} />
+        {location === 'review' &&
+          <ClrIcon shape='note' size={32} style={styles.icon}
+            onClick={() => this.setState({activeIcon: 'annotations', sidebarOpen: !sidebarOpen})} />
+        }
+      </div>
     </div>;
   }
 }
