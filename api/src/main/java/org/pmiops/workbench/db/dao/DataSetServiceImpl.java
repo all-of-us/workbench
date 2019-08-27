9b92f2a9b5bf75037d48f9efe52ed87d8ae7c9fe
Scout Rubenstein <brubenst@broadinstitute.org>
2019-08-27 11:26:34 -0400

    Fixing

--- api/src/main/java/org/pmiops/workbench/db/dao/DataSetServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/DataSetServiceImpl.java
@@ -211,8 +211,8 @@ public class DataSetServiceImpl implements DataSetService {
 
     // If pre packaged all survey concept set is selected create a temp concept set with concept ids
     // of all survey question
-    if (dataSet.getPrePackagedConceptSet().equals(PrePackagedConceptSetEnum.SURVEY)
-        || dataSet.getPrePackagedConceptSet().equals(PrePackagedConceptSetEnum.BOTH)) {
+    if (PrePackagedConceptSetEnum.SURVEY.equals(dataSet.getPrePackagedConceptSet())
+        || PrePackagedConceptSetEnum.BOTH.equals(dataSet.getPrePackagedConceptSet())) {
       conceptSetsSelected.add(handlePrePackagedSurveyConceptSet());
     }
 
