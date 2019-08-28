8950c3d1927e9089d46f198998daa51ffef3a311
Neha Saxena <nsaxena@broadinstitute.org>
2019-08-28 11:05:36 -0400

    remove RAB from message

--- api/cluster-resources/aou-download-policy-extension.js
+++ api/cluster-resources/aou-download-policy-extension.js
@@ -10,7 +10,7 @@ define([
       return confirm("It is All of Us data use policy that researchers should not make copies of " +
         "or download individual-level data (including taking screenshots or other means of viewing " +
         "individual-level data) outside of the All of Us research environment without approval from " +
-        "All of Us Resource Access Board (RAB). So, please make sure that the output cells in the notebook " +
+        "All of Us Resource Access Board. So, please make sure that the output cells in the notebook " +
         "you are downloading do not contain individual-level data");
     });
   };
