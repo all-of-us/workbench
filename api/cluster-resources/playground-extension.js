b8d9f594278d3ed56c2dd54cdc1ca14b25882732
Neha Saxena <nsaxena@broadinstitute.org>
2019-08-28 00:06:09 -0400

    revert space changes

--- api/cluster-resources/playground-extension.js
+++ api/cluster-resources/playground-extension.js
@@ -9,9 +9,9 @@ define([
     'base/js/namespace'
 ], (Jupyter) => {
   const load = () => {
-  // This prefix must be kept in sync with the Workbench localization API,
-  // see https://github.com/all-of-us/workbench/blob/master/api/src/main/java/org/pmiops/workbench/api/ClusterController.java
-  const nbPath = Jupyter.notebook.notebook_path;
+    // This prefix must be kept in sync with the Workbench localization API,
+    // see https://github.com/all-of-us/workbench/blob/master/api/src/main/java/org/pmiops/workbench/api/ClusterController.java
+    const nbPath = Jupyter.notebook.notebook_path;
     if (!nbPath.startsWith('workspaces_playground/')) {
       return;
     }
