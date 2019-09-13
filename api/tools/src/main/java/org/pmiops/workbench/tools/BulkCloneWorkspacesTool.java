8d9c1883385c4a9101a8eb5b4478182010039eaf
Eric Song <songe@broadinstitute.org>
2019-09-13 18:21:31 -0400

    Revert "test - move all imports into bulk:"
    This reverts commit 545c50a3b7b645e26de9b4b833c7f7010b01a050.

--- api/tools/src/main/java/org/pmiops/workbench/tools/BulkCloneWorkspacesTool.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/BulkCloneWorkspacesTool.java
@@ -8,23 +8,11 @@ import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import javax.inject.Provider;
-import org.pmiops.workbench.api.BigQueryService;
 import org.pmiops.workbench.billing.BillingProjectBufferService;
 import org.pmiops.workbench.cdr.CdrDbConfig;
-import org.pmiops.workbench.cdr.ConceptBigQueryService;
-import org.pmiops.workbench.cdr.dao.ConceptService;
-import org.pmiops.workbench.cohorts.CohortCloningService;
-import org.pmiops.workbench.cohorts.CohortFactoryImpl;
-import org.pmiops.workbench.compliance.ComplianceServiceImpl;
-import org.pmiops.workbench.config.BigQueryConfig;
-import org.pmiops.workbench.config.CacheSpringConfiguration;
-import org.pmiops.workbench.config.CdrBigQuerySchemaConfigService;
 import org.pmiops.workbench.config.WorkbenchConfig;
-import org.pmiops.workbench.db.WorkbenchDbConfig;
 import org.pmiops.workbench.db.dao.CdrVersionDao;
 import org.pmiops.workbench.db.dao.UserDao;
-import org.pmiops.workbench.db.dao.UserRecentResourceServiceImpl;
-import org.pmiops.workbench.db.dao.UserService;
 import org.pmiops.workbench.db.dao.WorkspaceDao;
 import org.pmiops.workbench.db.model.CdrVersion;
 import org.pmiops.workbench.db.model.User;
@@ -45,7 +33,6 @@ import org.pmiops.workbench.google.DirectoryService;
 import org.pmiops.workbench.model.CloneWorkspaceRequest;
 import org.pmiops.workbench.model.CloneWorkspaceResponse;
 import org.pmiops.workbench.model.WorkspaceActiveStatus;
-import org.pmiops.workbench.notebooks.NotebooksServiceImpl;
 import org.pmiops.workbench.workspaces.WorkspaceService;
 import org.pmiops.workbench.workspaces.WorkspacesController;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -65,22 +52,7 @@ import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 @EnableJpaRepositories("org.pmiops.workbench.db.dao")
 @EntityScan({"org.pmiops.workbench.db.model"})
 @Import({
-    CdrDbConfig.class,
-    BigQueryConfig.class,
-    CacheSpringConfiguration.class,
-    WorkbenchDbConfig.class,
-    CohortCloningService.class,
-    CohortFactoryImpl.class,
-    ConceptService.class,
-    ConceptBigQueryService.class,
-    BigQueryService.class,
-    CdrBigQuerySchemaConfigService.class,
-    UserService.class,
-    ComplianceServiceImpl.class,
-    WorkspacesController.class,
-    BillingProjectBufferService.class,
-    NotebooksServiceImpl.class,
-    UserRecentResourceServiceImpl.class
+    CdrDbConfig.class
 })
 public class BulkCloneWorkspacesTool {
 
