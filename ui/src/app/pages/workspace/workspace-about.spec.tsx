e8949dcabf5d67336e0a28b3cfe55b9a281387cd
CH Albach <calbach@google.com>
2019-09-24 15:17:06 -0700

    test fixes

--- ui/src/app/pages/workspace/workspace-about.spec.tsx
+++ ui/src/app/pages/workspace/workspace-about.spec.tsx
@@ -12,7 +12,7 @@ import {cdrVersionStore, currentWorkspaceStore, serverConfigStore, userProfileSt
 import {userRolesStub, workspaceStubs} from 'testing/stubs/workspaces-api-stub';
 import {waitOneTickAndUpdate} from 'testing/react-test-helpers';
 import {ClusterApiStub} from 'testing/stubs/cluster-api-stub';
-import {cdrVersions, CdrVersionsStubVariables} from '../../../testing/stubs/cdr-versions-api-stub';
+import {CdrVersionsStubVariables, cdrVersionListResponse} from '../../../testing/stubs/cdr-versions-api-stub';
 
 describe('WorkspaceAbout', () => {
   const profile = ProfileStubVariables.PROFILE_STUB as unknown as Profile;
@@ -49,7 +49,7 @@ describe('WorkspaceAbout', () => {
       publicApiKeyForErrorReports: 'aaa',
       enableEraCommons: true,
     });
-    cdrVersionStore.next(cdrVersions);
+    cdrVersionStore.next(cdrVersionListResponse);
   });
 
   it('should render', () => {
