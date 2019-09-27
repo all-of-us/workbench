b8850fd3ef3178b4d48aa5024b86e8e373235939
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-09-27 15:12:32 -0400

    Cloud Logging Client Test

--- api/src/main/java/org/pmiops/workbench/api/StatusController.java
+++ api/src/main/java/org/pmiops/workbench/api/StatusController.java
@@ -1,5 +1,16 @@
 package org.pmiops.workbench.api;
 
+import com.google.cloud.MonitoredResource;
+import com.google.cloud.logging.LogEntry;
+import com.google.cloud.logging.Logging;
+import com.google.cloud.logging.LoggingOptions;
+import com.google.cloud.logging.Payload.JsonPayload;
+import com.google.cloud.logging.Payload.StringPayload;
+import com.google.cloud.logging.Severity;
+import com.google.common.collect.ImmutableMap;
+import java.sql.Struct;
+import java.util.Collections;
+import java.util.Map;
 import org.pmiops.workbench.firecloud.FireCloudService;
 import org.pmiops.workbench.model.StatusResponse;
 import org.pmiops.workbench.notebooks.LeonardoNotebooksClient;
@@ -22,9 +33,45 @@ public class StatusController implements StatusApiDelegate {
 
   @Override
   public ResponseEntity<StatusResponse> getStatus() {
+    testStackDriverLogging();
     StatusResponse statusResponse = new StatusResponse();
     statusResponse.setFirecloudStatus(fireCloudService.getFirecloudStatus());
     statusResponse.setNotebooksStatus(leonardoNotebooksClient.getNotebooksStatus());
     return ResponseEntity.ok(statusResponse);
+  }
+
+  private void testStackDriverLogging() {
+    Logging logging = LoggingOptions.getDefaultInstance().getService();
+
+    // The name of the log to write to
+    String logName = "action-audit-test";
+
+    // The data to write to the log
+    String text = "Hello, world!";
+
+    LogEntry stringEntry = LogEntry.newBuilder(StringPayload.of(text))
+        .setSeverity(Severity.INFO)
+        .setLogName(logName)
+        .setResource(MonitoredResource.newBuilder("global").build())
+        .build();
+
+    // Writes the log entry asynchronously
+    logging.write(Collections.singleton(stringEntry));
+
+    Map<String, ?> data = ImmutableMap.of(
+        "name", "Bond, James Bond",
+        "occupation", "007",
+        "shaken", true,
+        "stirred", false,
+        "numFilms", 25);
+
+    LogEntry jsonEntry = LogEntry.newBuilder(JsonPayload.of(data))
+        .setSeverity(Severity.INFO)
+        .setLogName(logName)
+        .setResource(MonitoredResource.newBuilder("global").build())
+        .build();
+    logging.write(Collections.singleton(jsonEntry));
+
+    System.out.printf("Logged: %s%n", text);
   }
 }
