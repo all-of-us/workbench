6fa026bd781cf2902ba2ab2fdebb62e2120e984f
Joel Thibault <thibault@broad.dev>
2019-10-01 11:29:38 -0400

    Use 204 No Content for cron jobs which return nothing

--- api/src/main/java/org/pmiops/workbench/billing/OfflineBillingController.java
+++ api/src/main/java/org/pmiops/workbench/billing/OfflineBillingController.java
@@ -2,7 +2,6 @@ package org.pmiops.workbench.billing;
 
 import org.pmiops.workbench.api.OfflineBillingApiDelegate;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -26,30 +25,30 @@ public class OfflineBillingController implements OfflineBillingApiDelegate {
   @Override
   public ResponseEntity<Void> billingProjectGarbageCollection() {
     billingGarbageCollectionService.deletedWorkspaceGarbageCollection();
-    return ResponseEntity.status(HttpStatus.NO_CONTENT).build();
+    return ResponseEntity.noContent().build();
   }
 
   @Override
   public ResponseEntity<Void> bufferBillingProject() {
     billingProjectBufferService.bufferBillingProject();
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.noContent().build();
   }
 
   @Override
   public ResponseEntity<Void> syncBillingProjectStatus() {
     billingProjectBufferService.syncBillingProjectStatus();
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.noContent().build();
   }
 
   @Override
   public ResponseEntity<Void> cleanBillingBuffer() {
     billingProjectBufferService.cleanBillingBuffer();
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.noContent().build();
   }
 
   @Override
   public ResponseEntity<Void> checkFreeTierBillingUsage() {
     billingAlertsService.checkFreeTierBillingUsage();
-    return ResponseEntity.status(HttpStatus.OK).build();
+    return ResponseEntity.noContent().build();
   }
 }
