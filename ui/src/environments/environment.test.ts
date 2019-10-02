f11b3930fd43a32a27f70b175fef4c1d40735c7b
Ariel Schwartz <aschwart@broadinstitute.org>
2019-10-02 15:02:29 -0400

    fix confusing thing

--- ui/src/environments/environment.test.ts
+++ ui/src/environments/environment.test.ts
@@ -1,6 +1,7 @@
 import {Environment} from 'environments/environment-type';
 import {testEnvironmentBase} from 'environments/test-env-base';
 
+// This file is used in the deployed test environment
 export const environment: Environment = {
   ...testEnvironmentBase,
   displayTag: 'Test',
