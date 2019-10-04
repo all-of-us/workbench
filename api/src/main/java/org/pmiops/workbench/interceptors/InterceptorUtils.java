8efd82db9608f2be596a179db9509904b8e817f9
Brian Freeman <brian.d.freeman@gmail.com>
2019-10-04 14:38:09 -0500

    removing controllers

--- api/src/main/java/org/pmiops/workbench/interceptors/InterceptorUtils.java
+++ api/src/main/java/org/pmiops/workbench/interceptors/InterceptorUtils.java
@@ -11,11 +11,9 @@ public class InterceptorUtils {
   private static Map<String, String> apiImplMap =
       ImmutableMap.of(
           "org.pmiops.workbench.api.WorkspacesApiController",
-          "org.pmiops.workbench.workspaces.WorkspacesController",
-          "org.pmiops.workbench.api.OfflineBillingApiController",
-          "org.pmiops.workbench.billing.OfflineBillingController",
-          "org.pmiops.workbench.api.DataSetApiController",
-          "org.pmiops.workbench.dataset.DataSetController");
+              "org.pmiops.workbench.workspaces.WorkspacesController",
+          "org.pmiops.workbench.api.BillingApiController",
+              "org.pmiops.workbench.billing.BillingController");
 
   private InterceptorUtils() {}
 
