e38ff5dae46e911a7254708de1a5f677c59b51c1
Scout Rubenstein <brubenst@broadinstitute.org>
2019-10-08 14:02:52 -0400

    add tests

---
+++ ui/src/app/utils/index.spec.tsx
@@ -0,0 +1,27 @@
+import * as React from 'react';
+
+import {apiCallWithGatewayTimeoutRetries} from 'app/utils/index';
+
+const functionStub = {
+  successfulFunction() {
+    return Promise.resolve();
+  },
+  failedFunction() {
+    return Promise.resolve({status: 504}).then(response => {throw response});
+  }
+}
+
+
+describe('IndexUtils', () => {
+  it('should not retry if successful', async() => {
+    const successfulFunctionSpy = spyOn(functionStub, 'successfulFunction').and.callThrough();
+    await apiCallWithGatewayTimeoutRetries(() => functionStub.successfulFunction());
+    expect(successfulFunctionSpy).toHaveBeenCalledTimes(1);
+  });
+
+  it('should retry three times by default', async() => {
+    const failedFunctionSpy = spyOn(functionStub, 'failedFunction').and.callThrough();
+    await apiCallWithGatewayTimeoutRetries(() => functionStub.failedFunction(), 3, 1).catch(() => {});
+    expect(failedFunctionSpy).toHaveBeenCalledTimes(4);
+  });
+});
--- ui/src/app/utils/index.tsx
+++ ui/src/app/utils/index.tsx
@@ -464,17 +464,31 @@ export function highlightSearchTerm(searchTerm: string, stringToHighlight: strin
     </span>);
 }
 
-const BACKOFF_DEFAULT_WAIT_TIME_IN_MS = 1000;
-
+/*
+ * A method to run an api call with a specified number of retries and exponential backoff.
+ * This method will only error
+ * Parameters:
+ *    apiCall: Lambda that will run an API call, in the form of () => apiClient.apiCall(args)
+ *    maxRetries: The amount of retries the system will take before erroring
+ *    defaultWaitTime: How long the base exponential backoff is, in milliseconds
+ *      For example, if 1000 is passed in, it will wait 1s for the first retry,
+ *      2s for the second, etc.
+ */
 export async function apiCallWithGatewayTimeoutRetries<T>(
-  apiCall: () => Promise<T>, maxRetries = 3, retryCount = 1): Promise<T> {
+  apiCall: () => Promise<T>, maxRetries = 3, defaultWaitTime = 1000): Promise<T> {
+  return apiCallWithGatewayTimeoutRetriesAndRetryCount(apiCall, maxRetries, 1, defaultWaitTime);
+}
+
+async function apiCallWithGatewayTimeoutRetriesAndRetryCount<T>(
+  apiCall: () => Promise<T>, maxRetries = 3, retryCount = 1, defaultWaitTime = 1000): Promise<T> {
   try {
     return await apiCall();
   } catch (ex) {
     if (ex.status !== 504 || retryCount > maxRetries) {
       throw ex;
     }
-    await new Promise(resolve => setTimeout(resolve, BACKOFF_DEFAULT_WAIT_TIME_IN_MS * retryCount));
-    return await apiCallWithGatewayTimeoutRetries(apiCall, maxRetries, retryCount + 1);
+    await new Promise(resolve => setTimeout(resolve, defaultWaitTime * Math.pow(2, retryCount)));
+    return await apiCallWithGatewayTimeoutRetriesAndRetryCount(
+      apiCall, maxRetries, retryCount + 1, defaultWaitTime);
   }
 }
