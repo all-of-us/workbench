6702865d40333dc4fbe5308d881e7013b471a001
Eric Song <songe@broadinstitute.org>
2019-10-08 15:18:10 -0400

    update entries in place

--- api/tools/src/main/java/org/pmiops/workbench/tools/LoadDataDictionary.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/LoadDataDictionary.java
@@ -1,7 +1,6 @@
 package org.pmiops.workbench.tools;
 
 import java.io.BufferedReader;
-import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 import java.sql.Timestamp;
@@ -9,12 +8,17 @@ import java.time.Clock;
 import java.util.logging.Logger;
 import org.pmiops.workbench.dataset.DataDictionaryEntryDao;
 import org.pmiops.workbench.db.dao.CdrVersionDao;
+import org.pmiops.workbench.db.model.CdrVersion;
 import org.pmiops.workbench.db.model.DataDictionaryEntry;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.domain.EntityScan;
 import org.springframework.boot.builder.SpringApplicationBuilder;
 import org.springframework.context.annotation.Bean;
+import org.springframework.core.io.ClassPathResource;
+import org.springframework.core.io.Resource;
+import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
+import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
 
@@ -30,31 +34,43 @@ public class LoadDataDictionary {
       CdrVersionDao cdrVersionDao,
       DataDictionaryEntryDao dataDictionaryEntryDao) {
     return (args) -> {
-      InputStream is = getClass().getClassLoader().getResourceAsStream("data_dictionary_export.txt");
 
+      CdrVersion defaultCdrVersion = cdrVersionDao.findByIsDefault(true);
 
-      BufferedReader reader = new BufferedReader(new InputStreamReader(is));
-      String line;
-      while ((line = reader.readLine()) != null) {
-        String[] fields = line.split(":");
-        System.out.println(fields.length);
+      ClassLoader cl = this.getClass().getClassLoader();
+      ResourcePatternResolver resolver = new PathMatchingResourcePatternResolver(cl);
+      Resource[] resources = resolver.getResources("classpath*:/data_dictionary_exports/*.txt") ;
+      for (Resource resource : resources) {
+        InputStream is = resource.getInputStream();
 
-        DataDictionaryEntry entry = new DataDictionaryEntry();
-        entry.setCdrVersion(cdrVersionDao.findByIsDefault(true));
-        entry.setDefinedTime(new Timestamp(clock.instant().toEpochMilli()));
+        BufferedReader reader = new BufferedReader(new InputStreamReader(is));
+        String line;
+        while ((line = reader.readLine()) != null) {
+          String[] fields = line.split(":");
 
-        entry.setRelevantOmopTable(fields[0]);
-        entry.setFieldName(fields[1]);
-        entry.setOmopCdmStandardOrCustomField(fields[2]);
-        entry.setDescription(fields[3]);
-        entry.setFieldType(fields[4]);
-        entry.setDataProvenance(fields[5]);
-        entry.setSourcePpiModule(fields[6]);
-        entry.setTransformedByRegisteredTierPrivacyMethods("true".equals(fields[7]));
+          String relevantOmopTable = fields[0];
+          String fieldName = fields[1];
 
-        dataDictionaryEntryDao.save(entry);
+          DataDictionaryEntry entry = dataDictionaryEntryDao.findByRelevantOmopTableAndFieldNameAndCdrVersion(
+              relevantOmopTable, fieldName, defaultCdrVersion);
 
-        System.out.println(line);
+          if (entry == null) {
+            entry = new DataDictionaryEntry();
+            entry.setRelevantOmopTable(relevantOmopTable);
+            entry.setFieldName(fieldName);
+            entry.setCdrVersion((defaultCdrVersion));
+          }
+
+          entry.setDefinedTime(new Timestamp(clock.instant().toEpochMilli()));
+          entry.setOmopCdmStandardOrCustomField(fields[2]);
+          entry.setDescription(fields[3]);
+          entry.setFieldType(fields[4]);
+          entry.setDataProvenance(fields[5]);
+          entry.setSourcePpiModule(fields[6]);
+          entry.setTransformedByRegisteredTierPrivacyMethods("true".equals(fields[7]));
+
+          dataDictionaryEntryDao.save(entry);
+        }
       }
     };
   }
