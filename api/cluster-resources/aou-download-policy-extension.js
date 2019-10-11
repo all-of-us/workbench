65e6d1d03a15692d9bb034358fb54c72f0db8bab
Andrew Jang <andy7i@users.noreply.github.com>
2019-10-11 12:27:03 -0400

    cleanup

--- api/cluster-resources/aou-download-policy-extension.js
+++ api/cluster-resources/aou-download-policy-extension.js
@@ -8,12 +8,12 @@ define([
     // File will be downloaded only if the user clicks OK else it will just close the dialog box and do nothing.
     $('#download_menu li a').click(function(e) {
       e.preventDefault();
-      return confirm("Policy Reminder" + "\n\n" + //TODO: test this - currently blocked by billing project quota fire
+      return confirm("Policy Reminder" + "\n\n" +
         "It is All of Us data use policy that researchers should not make copies of " +
         "or download individual-level data (including taking screenshots or other means of viewing " +
         "individual-level data) outside of the All of Us research environment without approval from " +
         "All of Us Resource Access Board. So, please make sure that the output cells in the notebook " +
-        "you are downloading do not contain individual-level data");
+        "you are downloading do not contain individual-level data.");
     });
   };
 
