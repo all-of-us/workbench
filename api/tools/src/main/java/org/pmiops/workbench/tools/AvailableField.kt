560665b955afe88e09d433cc1c0f3a1aab6842f7
Eric Song <songe@broadinstitute.org>
2019-10-15 16:28:29 -0400

    add ignore annotation

--- api/tools/src/main/java/org/pmiops/workbench/tools/AvailableField.kt
+++ api/tools/src/main/java/org/pmiops/workbench/tools/AvailableField.kt
@@ -1,5 +1,8 @@
 package org.pmiops.workbench.tools
 
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties
+
+@JsonIgnoreProperties(ignoreUnknown = true)
 internal data class AvailableField(
         val field_name: String,
         val relevant_omop_table: String,
