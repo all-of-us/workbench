a8860587fc60fb6f36f73993de55aac57cb45ff3
Eric Song <songe@broadinstitute.org>
2019-10-15 16:42:42 -0400

    spotless

--- api/tools/src/main/java/org/pmiops/workbench/tools/LoadDataDictionary.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/LoadDataDictionary.java
@@ -22,7 +22,6 @@ import org.springframework.core.io.Resource;
 import org.springframework.core.io.support.PathMatchingResourcePatternResolver;
 import org.springframework.core.io.support.ResourcePatternResolver;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
-import org.springframework.security.access.method.P;
 
 @SpringBootApplication
 @EnableJpaRepositories("org.pmiops.workbench")
