3ce2d4511792505fa28a8d2c6d1b012865447c55
Joel Thibault <thibault@broad.dev>
2019-10-16 13:16:40 -0400

    rm dupe code and attempt to override reviewRequested state

--- ui/src/app/pages/workspace/workspace-edit.tsx
+++ ui/src/app/pages/workspace/workspace-edit.tsx
@@ -402,24 +402,11 @@ export const WorkspaceEdit = fp.flow(withRouteConfigData(), withCurrentWorkspace
               '' : this.props.workspace.researchPurpose.diseaseOfFocus}
         }});
         if (this.isMode(WorkspaceEditMode.Duplicate)) {
-          this.setState({workspace: {
-            ...this.props.workspace,
-              // Replace potential nulls with empty string or empty array
-            researchPurpose: {
-              ...this.props.workspace.researchPurpose,
-              populationDetails: !this.props.workspace.researchPurpose.populationDetails ?
-                [] : this.props.workspace.researchPurpose.populationDetails,
-              diseaseOfFocus: !this.props.workspace.researchPurpose.diseaseOfFocus ?
-                '' : this.props.workspace.researchPurpose.diseaseOfFocus}
-          }});
-          if (this.isMode(WorkspaceEditMode.Duplicate)) {
-            this.setState({workspace: {
-              ...this.props.workspace,
-              // This is the only field which is not automatically handled/differentiated
-              // on the API level.
-              name: 'Duplicate of ' + this.props.workspace.name
-            }});
-          }
+          // This is the only field which is not automatically handled/differentiated
+          // on the API level.
+          this.setState(fp.set(['workspace', 'name'], 'Duplicate of ' + this.props.workspace.name));
+          // if the original workspace was reviewed, it's unlikely that we need a re-review
+          this.setState(fp.set(['workspace', 'reviewRequested'], false));
         }
       }
 
