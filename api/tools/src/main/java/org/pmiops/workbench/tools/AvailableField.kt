37a6a27696f431326292de1be6c0ee3b8ef81eaf
Eric Song <songe@broadinstitute.org>
2019-10-16 12:59:15 -0400

    add kotlin spotless

--- api/tools/src/main/java/org/pmiops/workbench/tools/AvailableField.kt
+++ api/tools/src/main/java/org/pmiops/workbench/tools/AvailableField.kt
@@ -4,12 +4,12 @@ import com.fasterxml.jackson.annotation.JsonIgnoreProperties
 
 @JsonIgnoreProperties(ignoreUnknown = true)
 internal data class AvailableField(
-        val field_name: String,
-        val relevant_omop_table: String,
-        val omop_cdm_standard_or_custom_field: String,
-        val description: String,
-        val field_type: String,
-        val data_provenance: String,
-        val source_ppi_module: String?,
-        val transformed_by_registered_tier_privacy_methods: Boolean
+    val field_name: String,
+    val relevant_omop_table: String,
+    val omop_cdm_standard_or_custom_field: String,
+    val description: String,
+    val field_type: String,
+    val data_provenance: String,
+    val source_ppi_module: String?,
+    val transformed_by_registered_tier_privacy_methods: Boolean
 )
