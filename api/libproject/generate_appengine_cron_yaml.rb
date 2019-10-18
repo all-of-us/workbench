bd0980ec3bb26117b438721e05dbb665dc1110bc
CH Albach <calbach@google.com>
2019-10-18 14:06:37 -0700

    pr feedback

--- api/libproject/generate_appengine_cron_yaml.rb
+++ api/libproject/generate_appengine_cron_yaml.rb
@@ -10,7 +10,7 @@ require 'fileutils'
 env = ARGV[0]
 
 cron_dir = "src/main/webapp/WEB-INF"
-to_cron_path = ->(env) { "#{cron_dir}/cron_#{env}.yaml" }
+to_cron_path = ->(suffix) { "#{cron_dir}/cron_#{suffix}.yaml" }
 
 path = to_cron_path.call("default")
 if File.file?(to_cron_path.call(env))
