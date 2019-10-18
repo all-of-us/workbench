ee43af8671b8f9b3b98ff39693b87a3aeb63bd3d
Gregory Jordan <gjordan@verily.com>
2019-10-18 09:25:59 -0400

    Refactor analytics code to correctly set dimensions; add logged-in dimension tracking.

--- ui/src/app/utils/analytics.ts
+++ ui/src/app/utils/analytics.ts
@@ -1,3 +1,5 @@
+import {environment} from 'environments/environment';
+
 declare let gtag: Function;
 
 /**
@@ -25,4 +27,35 @@ export function triggerEvent(
   } else {
     console.error('Google Analytics gtag.js has not been loaded');
   }
+}
+
+enum UserAuthState {
+  LOGGED_IN = 'Logged in',
+  LOGGED_OUT = 'Logged out'
+}
+
+/**
+ * Sets the Analytics custom dimension indicating whether the current user
+ * session should be treated as logged-in or logged-out. This allows us to
+ * create Analytics segments which only show logged-in user activity.
+ * @param signedIn
+ */
+export function setLoggedInState(loggedIn: boolean) {
+  gtag('set', {
+    [environment.gaLoggedInDimension]: loggedIn ? UserAuthState.LOGGED_IN : UserAuthState.LOGGED_OUT
+  });
+}
+
+/**
+ * Initializes the Google Analytics config using gtag.js. This method looks to
+ * environment variables for various GA configuration parameters, and sets
+ * initial values for contextual custom dimensions.
+ */
+export function initializeAnalytics() {
+  gtag('js', new Date());
+  gtag('set', {
+    [environment.gaUserAgentDimension]: window.navigator.userAgent.slice(0, 100),
+    [environment.gaLoggedInDimension]: UserAuthState.LOGGED_OUT
+  });
+  gtag('config', environment.gaId);
 }
