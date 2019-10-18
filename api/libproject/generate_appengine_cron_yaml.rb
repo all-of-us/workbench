5982d97032ed0df57ef8095058999a8f00bf7d1d
CH Albach <calbach@google.com>
2019-10-18 13:12:25 -0700

    PR feedback

--- api/libproject/generate_appengine_cron_yaml.rb
+++ api/libproject/generate_appengine_cron_yaml.rb
@@ -1,3 +1,10 @@
+# Generates the App Engine cron.yaml file for a given environment. This is meant
+# to be invoked directly via Gradle before running the GAE application.
+# For now, this picks cron_{env}.yaml if it exists, cron_default.yaml otherwise.
+#
+# Future work: it will likely be desirable to support extension rather than
+# wholesale file replacement, e.g. to alter just the schedule or exclude a
+# particular cron for a given environment.
 require 'fileutils'
 
 env = ARGV[0]
