1971b8ad80ba92406cef2fba5427879ae529b03e
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-10-22 14:00:06 -0400

    first batch of merge/format operations

---
+++ ui/src/app/pages/profile/data-use-agreement-text.tsx
@@ -0,0 +1,174 @@
+import * as React from 'react';
+import {BolderHeader, BoldHeader} from '../../components/headers';
+import {IndentedListItem, IndentedOrderedList, IndentedUnorderedList, SecondHeader} from './data-use-agreement';
+
+const CORE_VALUES_URL = 'https://allofus.nih.gov/about/about-all-us-research-program';
+
+{/* NOTE: Make sure to update dataUseAgreementVersion if there is any change to the DUA text. */}
+{/* tslint:disable:max-line-length */}
+export const DataUseAgreementText = () => {
+  return <div>
+    <BolderHeader>All of Us Research Program</BolderHeader>
+    <BoldHeader>Demonstration Project Data Use Agreement</BoldHeader>
+    <div>This data use agreement describes how All of Us Research Program data can and cannot be used for the purposes
+      of program-approved demonstration projects.
+    </div>
+    <div>This is an agreement between Vanderbilt University Medical Center and authorized demonstration users of data
+      from the All of Us Research Program.
+    </div>
+    <div>An authorized demonstration user is a person who is authorized to access and/or work with registered or
+      controlled tier data from the All of Us Research Program for the exclusive purpose of a program-approved
+      demonstration project. Authorized demonstration users are limited to trainees, faculty or staff at All of Us
+      Research Program consortium partner institutions.
+    </div>
+    <div>
+      Before they access and/or work with All of Us Research Program data, authorized demonstration users must:
+      <IndentedOrderedList>
+        <li>complete the All of Us Research Program research ethics training; and</li>
+        <li>read and attest to this data use agreement</li>
+      </IndentedOrderedList>
+    </div>
+    <div>Please read this agreement carefully and completely before signing.</div>
+    <SecondHeader>As “Authorized Demonstration User” of the <i>All of Us</i> Research Program data, I
+      will:</SecondHeader>
+    <IndentedUnorderedList>
+      <li>read and adhere to the <i>All of Us</i> Research
+        Program <a target='_blank' href={CORE_VALUES_URL}>core values</a>.
+      </li>
+      <li>know and follow all applicable US federal, state, and local
+        (applicable in the area where I am conducting research) laws
+        regarding research involving human data and data privacy.
+      </li>
+      <li>respect the privacy of research participants at all times.
+        <IndentedUnorderedList>
+          <IndentedListItem style={{margin: '0.5rem 0'}}>
+            I <strong>will <strong>NOT</strong> use</strong> or disclose any
+            information that directly identifies one or more participants.
+            <br/>
+            If I become aware of any information that directly identifies one or more
+            participants, I will notify the <i>All of Us</i> Research Program immediately
+            using the automatic notification system.
+          </IndentedListItem>
+          <IndentedListItem style={{margin: '0.5rem 0'}}>
+            I <strong>will <strong>NOT</strong> attempt</strong> to re-identify research
+            participants or their relatives.
+            <br/>
+            If I unintentionally re-identify participants through the process of my work, I will
+            contact the <i>All of Us</i> Research Program immediately using the automatic
+            notification system.
+          </IndentedListItem>
+          <IndentedListItem style={{margin: '0.5rem 0'}}>
+            If I become aware of any uses or disclosures of All of Us Research Program data that
+            could endanger the security or privacy of research participants, I will contact the
+            All of Us Research Program immediately using the appropriate process.
+          </IndentedListItem>
+          <IndentedListItem>provide a meaningful description of my research purpose when establishing my All of Us Research
+            Program workspace.</IndentedListItem>
+          <IndentedListItem>Within each workspace, I will use the data for the research purpose I have provided.</IndentedListItem>
+          <IndentedListItem>This description will accurately reflect the demonstration project proposal for which I received
+            All of Us approval.</IndentedListItem>
+          <IndentedListItem>If I have a new research purpose, I will create a new workspace and provide a new research purpose
+            description.</IndentedListItem>
+          <IndentedListItem>Within my workspace, I will only use the data for the demonstration project for which I have
+            received All of Us approval.</IndentedListItem>
+          <IndentedListItem>use a version of the All of Us Research Program database that is current at or after the time my
+            analysis begins</IndentedListItem>
+          <IndentedListItem>take full responsibility for any external data, files, or software that I import into my
+            Workspace and the consequences thereof.</IndentedListItem>
+          <IndentedListItem>I will know and follow all applicable laws, regulations, and policies regarding access and use
+            for any external data, files, or software that I upload into my Workspace.</IndentedListItem>
+          <IndentedListItem>I will NOT upload data or files containing personally identifiable information (PII).</IndentedListItem>
+          <IndentedListItem>PII means information that can be used to distinguish or trace the identity of an individual
+            (e.g., name, social security number, biometric records, etc.) either alone, or when combined with other
+            personal or identifying information that is linked or linkable to a specific individual (2 CFR §
+            200.79)</IndentedListItem>
+          <IndentedListItem>I will use any external data, files, or software that I upload into my Workspace exclusively for
+            the research purpose I have provided for that Workspace.</IndentedListItem>
+          <IndentedListItem>I will NOT use any external data, files, or software that I upload into my Workspace for any
+            malicious purposes.</IndentedListItem>
+          <IndentedListItem>If any import of data, files, or software into my Workspace results in unforeseen consequences
+            and/or unintentional violation of these terms, I will notify the All of Us Research Program as soon as I
+            become aware using the appropriate process.</IndentedListItem>
+        </IndentedUnorderedList>
+        <li>use a version of the All of Us Research Program database that is current at or after the time my
+          analysis begins.</li>
+        <IndentedUnorderedList>
+          <IndentedListItem>Archive versions of the database are maintained for the sole purpose of completion of
+            existing studies or replication of previous studies. New work may not be initiated on
+            archive versions of the database.
+          </IndentedListItem>
+        </IndentedUnorderedList>
+      </li>
+      <li>share the results of my demonstration project and all contents of my Workbench with the All of Us Research Program. </li>
+      <li>My workbench and its contents may be made public for the benefit of all authorized users.</li>
+      <li>honor the contribution of those who take part in <i>All of Us</i> to my work
+        <IndentedUnorderedList>
+          <li>I will acknowledge the <i>All of Us</i> Research Program and its research participants
+            in all oral and written presentations, disclosures, and publications resulting from
+            any analyses of the data.
+            <IndentedUnorderedList>
+              <li>Here is an example acknowledgement statement:</li>
+              <IndentedUnorderedList>
+                <IndentedListItem>
+                  “The All of Us Research Program is supported by the National Institutes of Health, Office of the Director: Regional Medical Centers: 1 OT2 OD026549; 1 OT2 OD026554; 1 OT2 OD026557; 1 OT2 OD026556; 1 OT2 OD026550; 1 OT2 OD 026552; 1 OT2 OD026553; 1 OT2 OD026548; 1 OT2 OD026551; 1 OT2 OD026555; IAA #: AOD 16037; Federally Qualified Health Centers: HHSN 263201600085U; Data and Research Center: 5 U2C OD023196; Biobank: 1 U24 OD023121; The Participant Center: U24 OD023176; Participant Technology Systems Center: 1 U24 OD023163; Communications and Engagement: 3 OT2 OD023205; 3 OT2 OD023206; and Community Partners: 1 OT2 OD025277; 3 OT2 OD025315; 1 OT2 OD025337; 1 OT2 OD025276. In addition, the All of Us Research Program would not be possible without the partnership of its participants.”
+                </IndentedListItem>
+                <IndentedListItem>
+                  I will submit an electronic version of a final, peer-reviewed manuscript to PubMed Central immediately upon acceptance for publication, to be made publicly available no later than 12 months after the official date of publication.
+                </IndentedListItem>
+              </IndentedUnorderedList>
+            </IndentedUnorderedList>
+          </li>
+        </IndentedUnorderedList>
+      </li>
+    </IndentedUnorderedList>
+    <SecondHeader>As “Authorized Demonstration User” of the <i>All of Us</i> Research Program data, I
+      will:</SecondHeader>
+    <IndentedUnorderedList>
+      <li><strong>NOT</strong> share my login information with anyone.
+        <IndentedUnorderedList>
+          <li>I will not share my login information with another authorized demonstration or other user of the All of Us Research Program.
+          </li>
+          <li>I will not create any group or shared accounts.</li>
+        </IndentedUnorderedList>
+      </li>
+      <li>
+        NOT use All of Us Research Program data or any external data, files, or software that I upload into the Research Workbench for research that is discriminatory or stigmatizing of individuals, families, groups, or communities. Please review the All of Us policy on stigmatizing research here.
+        <IndentedUnorderedList>
+          <li>I will contact the <i>All of Us</i> Research Program Resource Access Board (RAB) for
+            further guidance on this point as needed.
+          </li>
+        </IndentedUnorderedList>
+      </li>
+      <li><strong>NOT</strong> attempt to contact <i>All of Us</i> Research Program participants.
+      </li>
+      <li><strong>NOT</strong> make copies of or download individual-level data outside of
+        the <i>All of Us</i> Research Program environment without approval from RAB.
+        <IndentedUnorderedList>
+          <li>I will not take screenshots or attempt any other way of
+            copying individual-level data.
+          </li>
+        </IndentedUnorderedList>
+      </li>
+
+      <li>NOT redistribute or publish registered or controlled tier All of Us Research Program data including aggregate statistics that are more granular than buckets of 20 individuals without explicit approval from the All of Us Research Program.
+      </li>
+      <li>NOT attempt to link participant-level All of Us data from the registered or controlled tier with participant-level data from other sources without explicit permission from the All of Us Research Program.</li>
+      <li><strong>NOT</strong> use <i>All of Us</i> Research Program data for marketing purposes.
+        <IndentedUnorderedList>
+          <li>“Marketing” means a communication about a product or service that encourages recipients of the communication to purchase or use the product or service (US 45 CFR 164.501).
+          </li>
+        </IndentedUnorderedList>
+      </li>
+      <li>NOT represent that the All of Us Research Program endorses or approves of my research unless such endorsement is expressly provided.
+      </li>
+    </IndentedUnorderedList>
+
+    <SecondHeader>Data Disclaimer:</SecondHeader>
+    <div>The <i>All of Us</i> Research Program does not guarantee the accuracy of the data in
+      the <i>All of Us</i> Research Program database. The <i>All of Us</i> Research Program
+      does not guarantee the performance of the software in the <i>All of Us</i> Research
+      Program database. The <i>All of Us</i> Research Program does not warrant or endorse
+      the research results obtained by using the <i>All of Us</i> database.
+    </div>
+  </div>;
+};
--- ui/src/app/pages/profile/data-use-agreement.tsx
+++ ui/src/app/pages/profile/data-use-agreement.tsx
@@ -1,10 +1,7 @@
 import {Component} from '@angular/core';
 import {Button} from 'app/components/buttons';
 import {FlexColumn} from 'app/components/flex';
-import {
-  BolderHeader,
-  styles as headerStyles
-} from 'app/components/headers';
+import {styles as headerStyles} from 'app/components/headers';
 import {TextInput} from 'app/components/inputs';
 import {Modal, ModalBody, ModalFooter, ModalTitle} from 'app/components/modals';
 import {TooltipTrigger} from 'app/components/popups';
@@ -16,6 +13,7 @@ import {Profile} from 'generated/fetch';
 import * as fp from 'lodash/fp';
 import * as React from 'react';
 import {validate} from 'validate.js';
+import {DataUseAgreementText} from './data-use-agreement-text';
 
 const styles = reactStyles({
   dataUseAgreementPage: {
@@ -41,192 +39,27 @@ const styles = reactStyles({
   }
 });
 
-const SecondHeader = (props) => {
+export const SecondHeader = (props) => {
   return <h2 style={{...styles.h2, ...props.style}}>{props.children}</h2>;
 };
 
-const indentedListStyles = {
+export const indentedListStyles = {
   margin: '0.5rem 0 0.5rem 1.5rem', listStylePosition: 'outside'
 };
 
-const IndentedUl = (props) => {
+export const IndentedUnorderedList = (props) => {
   return <ul style={{...indentedListStyles, ...props.style}}>{props.children}</ul>;
 };
 
-const IndentedOl = (props) => {
+export const IndentedOrderedList = (props) => {
   return <ol style={{...indentedListStyles, ...props.style}}>{props.children}</ol>;
 };
 
-const IndentedLi = (props) => {
+export const IndentedListItem = (props) => {
   return <li style={{marginTop: '0.5rem', ...props.style}}>{props.children}</li>;
 };
 
-const coreValuesUrl = 'https://allofus.nih.gov/about/about-all-us-research-program';
-
-const dataUseAgreementVersion = 1;
-
-{/* NOTE: Make sure to update dataUseAgreementVersion if there is any change to the DUA text. */}
-const DataUseAgreementText = () => {
-  return <div>
-    <BolderHeader>All of Us Research Program - Data Use Agreement</BolderHeader>
-    <div>This data use agreement describes how <i>All of Us </i>
-      Research Program data can and cannot be used.
-    </div>
-    <div>This is an agreement between the United States National Institutes of Health and authorized
-      users of data from the <i>All of Us</i> Research Program.
-    </div>
-    <div>An authorized user is a person who is authorized to access and/or work with registered or
-      controlled tier data from the <i>All of Us</i> Research Program.
-    </div>
-    <div>Before they access and/or work with <i>All of Us </i>
-      Research Program data, authorized users must:
-      <IndentedOl>
-        <li>complete the <i>All of Us</i> Research Program research ethics training</li>
-        <li>read and sign this data use agreement</li>
-      </IndentedOl>
-    </div>
-    <div>Please read this agreement carefully and completely before signing.</div>
-    <SecondHeader>As “Authorized User” of the <i>All of Us</i> Research Program data, I
-      will:</SecondHeader>
-    <IndentedUl>
-      <li>read and adhere to the <i>All of Us</i> Research
-        Program <a target='_blank' href={coreValuesUrl}>core values</a>.
-      </li>
-      <li>know and follow all applicable US federal, state, and local
-        (applicable in the area where I am conducting research) laws
-        regarding research involving human data and data privacy.
-      </li>
-      <li>respect the privacy of research participants at all times.
-        <IndentedUl>
-          <IndentedLi style={{margin: '0.5rem 0'}}>
-            I <strong>will <strong>NOT</strong> use</strong> or disclose any
-            information that directly identifies one or more participants.
-            <br/>
-            If I become aware of any information that directly identifies one or more
-            participants, I will notify the <i>All of Us</i> Research Program immediately
-            using the automatic notification system.
-          </IndentedLi>
-          <IndentedLi style={{margin: '0.5rem 0'}}>
-            I <strong>will <strong>NOT</strong> attempt</strong> to re-identify research
-            participants or their relatives.
-            <br/>
-            If I unintentionally re-identify participants through the process of my work, I will
-            contact the <i>All of Us</i> Research Program immediately using the automatic
-            notification system.
-          </IndentedLi>
-          <IndentedLi style={{margin: '0.5rem 0'}}>
-            If I become aware of any uses or disclosures of <i>All of Us</i> Research Program data
-            that could endanger the security or privacy of research participants, I will contact
-            the <i>All of Us</i> Research Program immediately using the automatic
-            notification system.
-          </IndentedLi>
-        </IndentedUl>
-      </li>
-      <li>provide a meaningful and accurate description of my research purpose every time I create
-        an <i>All of Us</i> Research Program workspace.
-        <IndentedUl>
-          <li>
-            Within each workspace, I will use the data for the research purpose I have provided.
-          </li>
-          <li>If I have a new research purpose, I will create a new workspace and provide a new
-            research purpose description.
-          </li>
-        </IndentedUl>
-      </li>
-      <li>use a version of the <i>All of Us</i> Research Program database that is current at or
-        after the time my analysis begins
-        <IndentedUl>
-          <li>Archive versions of the database are maintained for the sole purpose of completion of
-            existing studies or replication of previous studies. New work may not be initiated on
-            archive versions of the database.
-          </li>
-        </IndentedUl>
-      </li>
-      <li>honor the contribution of those who take part in <i>All of Us</i> to my work
-        <IndentedUl>
-          <li>I will acknowledge the <i>All of Us</i> Research Program and its research participants
-            in all oral and written presentations, disclosures, and publications resulting from
-            any analyses of the data.
-            <IndentedUl>
-              <li>
-                Here is an example acknowledgement statement: “The results reported here are based
-                on data contributed by participants to the <i>All of Us</i> Research Program,
-                managed by the National Institutes of Health. Information about
-                the <i>All of Us</i> Research Program can be found
-                at <a href='https://allofus.nih.gov'>https://allofus.nih.gov</a>.”
-              </li>
-            </IndentedUl>
-          </li>
-
-          <li>I will deposit an electronic version of final, peer-reviewed manuscript on National
-            Library of Medicine's PubMed Central no later than 3 months after the official date of
-            publication.
-          </li>
-        </IndentedUl>
-      </li>
-    </IndentedUl>
-    <SecondHeader>As “Authorized User” of the <i>All of Us</i> Research Program data, I
-      will:</SecondHeader>
-    <IndentedUl>
-      <li><strong>NOT</strong> share my login information with anyone.
-        <IndentedUl>
-          <li>I will not share my login information with another authorized user of
-            the <i>All of Us</i> Research Program.
-          </li>
-          <li>I will not create any group or shared accounts.</li>
-        </IndentedUl>
-      </li>
-      <li>
-        <strong>NOT</strong> use <i>All of Us</i> Research Program data for research that is
-        discriminatory or stigmatizing of individuals, families, or communities. Please review
-        the <i>All of Us</i> policy on stigmatizing research here.
-        <IndentedUl>
-          <li>I will contact the <i>All of Us</i> Research Program Resource Access Board (RAB) for
-            further guidance on this point as needed.
-          </li>
-        </IndentedUl>
-      </li>
-      <li><strong>NOT</strong> attempt to contact <i>All of Us</i> Research Program participants.
-      </li>
-      <li><strong>NOT</strong> make copies of or download individual-level data outside of
-        the <i>All of Us</i> Research Program environment without approval from RAB.
-        <IndentedUl>
-          <li>I will not take screenshots or attempt any other way of
-            copying individual-level data.
-          </li>
-        </IndentedUl>
-      </li>
-
-      <li><strong>NOT</strong> redistribute or publish registered access or controlled tier <i>All
-        of Us</i> Research Program data including aggregate statistics that are more granular
-        than buckets of 20 without approval from the RAB.
-      </li>
-      <li><strong>NOT</strong> attempt to link registered or controlled tier <i>All of
-        Us</i> Research Program data at the individual level with data from other
-        sources without explicit permission from RAB.
-      </li>
-      <li><strong>NOT</strong> use <i>All of Us</i> Research Program data for marketing purposes.
-        <IndentedUl>
-          <li>“Marketing” means a communication about a product or service that encourages
-            recipients of the communication to purchase or use the product or service
-            (US 45 CFR §164.501).
-          </li>
-        </IndentedUl>
-      </li>
-      <li><strong>NOT</strong> represent that the <i>All of Us</i> Research Program endorses or
-        approves of my research unless such endorsement is expressly provided by the RAB.
-      </li>
-    </IndentedUl>
-
-    <SecondHeader>Data Disclaimer:</SecondHeader>
-    <div>The <i>All of Us</i> Research Program does not guarantee the accuracy of the data in
-      the <i>All of Us</i> Research Program database. The <i>All of Us</i> Research Program
-      does not guarantee the performance of the software in the <i>All of Us</i> Research
-      Program database. The <i>All of Us</i> Research Program does not warrant or endorse
-      the research results obtained by using the <i>All of Us</i> database.
-    </div>
-  </div>;
-};
+const dataUseAgreementVersion = 2;
 
 const DuaTextInput = (props) => {
   // `fp.omit` used to prevent propagation of test IDs to the rendered child component.
@@ -260,35 +93,35 @@ const SanctionModal = (props) => {
         an investigator has violated the Code of Conduct outlined in the Data Use Agreement
         signed by each user. The RAB notifies the All of Us Research Program office of the
         violation.
-        <IndentedUl>
+        <IndentedUnorderedList>
           <li>
             <label>The <i>All of Us</i> Research Program office and/or the All of Us IRB may
             implement the following sanctions if it is determined that an investigator has violated
             the Code of Conduct:
             </label>
-            <IndentedUl>
-              <IndentedLi>
+            <IndentedUnorderedList>
+              <IndentedListItem>
                 Determine whether any action by the investigator is required to remedy the
                 violation.
-              </IndentedLi>
-              <IndentedLi>
+              </IndentedListItem>
+              <IndentedListItem>
                 Revoke and/or deny access of the violator to all non-public (Registered and
                 Controlled tier) <i>All of Us</i> data.
-              </IndentedLi>
-              <IndentedLi>
+              </IndentedListItem>
+              <IndentedListItem>
                 Post the name and affiliation of the violator on a public <i>All of Us</i>
                 Research Program webpage.
-              </IndentedLi>
-              <IndentedLi>
+              </IndentedListItem>
+              <IndentedListItem>
                 Revoke extant NIH funding and/or prohibit future funding, either permanently or for
                 an explicit period of time
-              </IndentedLi>
-              <IndentedLi>
+              </IndentedListItem>
+              <IndentedListItem>
                 Prosecute the violator for breach of a contract with the federal government
-              </IndentedLi>
-            </IndentedUl>
+              </IndentedListItem>
+            </IndentedUnorderedList>
           </li>
-        </IndentedUl>
+        </IndentedUnorderedList>
       </label>
     </ModalBody>
     <ModalFooter>
