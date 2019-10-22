c464f268ff8277fcaf60555b4e97a1921c99f854
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-10-21 21:24:25 -0400

    fixup qualified occs

--- api/tools/prefix_generated_classes.rb
+++ api/tools/prefix_generated_classes.rb
@@ -10,6 +10,7 @@ IMPORT_PATTERN = "import #{PACKAGE}."
 IMPORT_MARKER = '8675309867530986753098675309'
 NEW_IMPORT_PATTERN = "#{IMPORT_PATTERN}#{PREFIX}"
 QUALIFIED_USAGE_PATTERN = "#{PACKAGE}."
+QUALIFIED_REPLACEMENT = "#{QUALIFIED_USAGE_PATTERN}#{PREFIX}"
 FILE_SEPARATOR = '--------------------------------------------------------------------------------'
 DRY_RUN = ARGV[3] ? true : false
 
@@ -27,16 +28,16 @@ java_sources.each do |source_file|
   puts "\tProcessing #{source_file}"
   original = File.read(source_file)
 
-  # # mask out the import statements so the next replacement doesn't stomp on them
-  # replaced_import = original.gsub(IMPORT_PATTERN, IMPORT_MARKER)
-  #
-  # # we don't need to qualify the class name anymore, since it won't clash
-  # repalced_qualified_imports = replaced_import.gsub(QUALIFIED_USAGE_PATTERN, PREFIX)
-  #
-  # # fixup the import
-  # final_text = repalced_qualified_imports.gsub(IMPORT_MARKER, NEW_IMPORT_PATTERN)
+  # mask out the import statements so the next replacement doesn't stomp on them
+  replaced_import = original.gsub(IMPORT_PATTERN, IMPORT_MARKER)
 
-  final_text = original.gsub(IMPORT_PATTERN, NEW_IMPORT_PATTERN) # not re-qualifying
+  # we don't need to qualify the class name anymore, since it won't clash
+  repalced_qualified_imports = replaced_import.gsub(QUALIFIED_USAGE_PATTERN, PREFIX)
+
+  # fixup the import
+  final_text = repalced_qualified_imports.gsub(IMPORT_MARKER, NEW_IMPORT_PATTERN)
+
+  # final_text = original.gsub(IMPORT_PATTERN, NEW_IMPORT_PATTERN) # not re-qualifying
 
   if DRY_RUN
     puts FILE_SEPARATOR
