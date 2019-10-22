63df95f73c8380f8027a229958585be29f686a2c
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-10-21 21:18:09 -0400

    attempt to simplify

--- api/tools/prefix_generated_classes.rb
+++ api/tools/prefix_generated_classes.rb
@@ -27,14 +27,17 @@ java_sources.each do |source_file|
   puts "\tProcessing #{source_file}"
   original = File.read(source_file)
 
-  # mask out the import statements so the next replacement doesn't stomp on them
-  replaced_import = original.gsub(IMPORT_PATTERN, IMPORT_MARKER)
+  # # mask out the import statements so the next replacement doesn't stomp on them
+  # replaced_import = original.gsub(IMPORT_PATTERN, IMPORT_MARKER)
+  #
+  # # we don't need to qualify the class name anymore, since it won't clash
+  # repalced_qualified_imports = replaced_import.gsub(QUALIFIED_USAGE_PATTERN, PREFIX)
+  #
+  # # fixup the import
+  # final_text = repalced_qualified_imports.gsub(IMPORT_MARKER, NEW_IMPORT_PATTERN)
 
-  # we don't need to qualify the class name anymore, since it won't clash
-  repalced_qualified_imports = replaced_import.gsub(QUALIFIED_USAGE_PATTERN, PREFIX)
+  final_text = original.gsub(IMPORT_PATTERN, NEW_IMPORT_PATTERN) # not re-qualifying
 
-  # fixup the import
-  final_text = repalced_qualified_imports.gsub(IMPORT_MARKER, NEW_IMPORT_PATTERN)
   if DRY_RUN
     puts FILE_SEPARATOR
     puts final_text
