da2408bfa2488440baa8dd75c3341f1f2db3a493
Eric Song <songe@broadinstitute.org>
2019-10-24 19:39:44 -0400

    fix test

--- ui/src/testing/stubs/featured-workspaces-config-api-stub.ts
+++ ui/src/testing/stubs/featured-workspaces-config-api-stub.ts
@@ -1,4 +1,9 @@
-import {FeaturedWorkspace, FeaturedWorkspacesConfigApi, FeaturedWorkspacesConfigResponse} from 'generated/fetch';
+import {
+  FeaturedWorkspace,
+  FeaturedWorkspaceCategory,
+  FeaturedWorkspacesConfigApi,
+  FeaturedWorkspacesConfigResponse
+} from 'generated/fetch';
 import {WorkspaceStubVariables} from './workspaces-api-stub';
 
 
@@ -6,14 +11,14 @@ const phenotypeWorkspace = {
   name: WorkspaceStubVariables.DEFAULT_WORKSPACE_NAME + ' Phenotype Library',
   namespace: WorkspaceStubVariables.DEFAULT_WORKSPACE_NS + ' Phenotype Library',
   id: WorkspaceStubVariables.DEFAULT_WORKSPACE_ID + ' Phenotype Library',
-  category: 'PHENOTYPE_LIBRARY'
+  category: FeaturedWorkspaceCategory.PHENOTYPELIBRARY
 };
 
 const tutorialWorkspace = {
   name: WorkspaceStubVariables.DEFAULT_WORKSPACE_NAME + ' Tutorial Workspace',
   namespace: WorkspaceStubVariables.DEFAULT_WORKSPACE_NS + ' Tutorial Workspace',
   id: WorkspaceStubVariables.DEFAULT_WORKSPACE_ID + ' Tutorial Workspace',
-  category: 'TUTORIAL_WORKSPACES'
+  category: FeaturedWorkspaceCategory.TUTORIALWORKSPACES
 };
 
 export class FeaturedWorkspacesConfigApiStub extends FeaturedWorkspacesConfigApi {
