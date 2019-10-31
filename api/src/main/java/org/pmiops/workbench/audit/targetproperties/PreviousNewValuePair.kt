f304dc607a3f2ac6eefb09fbb5176b71bdaadc24
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-10-31 11:48:17 -0400

    progress

--- api/src/main/java/org/pmiops/workbench/audit/targetproperties/PreviousNewValuePair.kt
+++ api/src/main/java/org/pmiops/workbench/audit/targetproperties/PreviousNewValuePair.kt
@@ -1,12 +1,11 @@
 package org.pmiops.workbench.audit.targetproperties
 
-import java.util.Objects
-
-class PreviousNewValuePair private constructor(// TODO: make these nullable after Kotlin conversion
-        var previousValue: String?, var newValue: String?) {
+class PreviousNewValuePair constructor(var previousValue: String?, var newValue: String?) {
 
     fun valueChanged(): Boolean {
-        return previousValue != newValue
+        return previousValue == null && newValue != null ||
+                newValue == null && previousValue != null ||
+                previousValue != newValue
     }
 
     override fun toString(): String {
@@ -18,47 +17,5 @@ class PreviousNewValuePair private constructor(// TODO: make these nullable afte
                 + newValue
                 + '\''.toString()
                 + '}'.toString())
-    }
-
-    override fun equals(o: Any?): Boolean {
-        if (this === o) {
-            return true
-        }
-        if (o !is PreviousNewValuePair) {
-            return false
-        }
-        val that = o as PreviousNewValuePair?
-        return previousValue == that!!.previousValue && newValue == that.newValue
-    }
-
-    override fun hashCode(): Int {
-        return Objects.hash(previousValue, newValue)
-    }
-
-    class Builder {
-
-        private var previousValue: String? = null
-        private var newValue: String? = null
-
-        fun setPreviousValue(previousValue: String): Builder {
-            this.previousValue = previousValue
-            return this
-        }
-
-        fun setNewValue(newValue: String): Builder {
-            this.newValue = newValue
-            return this
-        }
-
-        fun build(): PreviousNewValuePair {
-            return PreviousNewValuePair(previousValue, newValue)
-        }
-    }
-
-    companion object {
-
-        fun builder(): Builder {
-            return Builder()
-        }
     }
 }
