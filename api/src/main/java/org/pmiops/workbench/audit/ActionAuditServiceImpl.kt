3e881a7bfb33379f99cd2daea29a0167d716333b
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-11-01 10:20:59 -0400

    use correct map type to allow null values

--- api/src/main/java/org/pmiops/workbench/audit/ActionAuditServiceImpl.kt
+++ api/src/main/java/org/pmiops/workbench/audit/ActionAuditServiceImpl.kt
@@ -25,7 +25,6 @@ constructor(private val configProvider: Provider<WorkbenchConfig>, private val c
         try {
             val logEntries: List<LogEntry> = events
                     .map { this.auditEventToLogEntry(it) }
-                    .toList()
             if (logEntries.isNotEmpty()) {
                 cloudLogging.write(logEntries)
             }
@@ -40,42 +39,32 @@ constructor(private val configProvider: Provider<WorkbenchConfig>, private val c
         return LogEntry.newBuilder(toJsonPayload(auditEvent))
                 .setSeverity(Severity.INFO)
                 .setLogName(actionAuditConfig.logName)
-                .setResource(MONITORED_RESOURCE)
+                .setResource(monitoredResource)
                 .build()
     }
 
     private fun toJsonPayload(auditEvent: ActionAuditEvent): JsonPayload {
-        val result = HashMap<String, Any>() // allow null values
-        result[AuditColumn.TIMESTAMP.name] = auditEvent.timestamp
-        result[AuditColumn.ACTION_ID.name] = auditEvent.actionId
-        result[AuditColumn.ACTION_TYPE.name] = auditEvent.actionType
-        result[AuditColumn.AGENT_TYPE.name] = auditEvent.agentType
-        result[AuditColumn.AGENT_ID.name] = auditEvent.agentId
-        if (auditEvent.agentEmailMaybe != null) {
-            result[AuditColumn.AGENT_EMAIL.name] = auditEvent.agentEmailMaybe
-        }
-        result[AuditColumn.TARGET_TYPE.name] = auditEvent.targetType
-        if (auditEvent.targetIdMaybe != null) {
-            result[AuditColumn.TARGET_ID.name] = auditEvent.targetIdMaybe
-        }
-
-        result[AuditColumn.AGENT_ID.name] = auditEvent.agentId
-        if (auditEvent.targetPropertyMaybe != null) {
-            result[AuditColumn.TARGET_PROPERTY.name] = auditEvent.targetPropertyMaybe
-        }
-
-        if (auditEvent.previousValueMaybe != null) {
-            result[AuditColumn.PREV_VALUE.name] = auditEvent.previousValueMaybe
-        }
-        if (auditEvent.newValueMaybe != null) {
-            result[AuditColumn.NEW_VALUE.name] = auditEvent.newValueMaybe
-        }
-        return JsonPayload.of(result)
+        // allow null values so that JSON columns are populated and BQ schema
+        // doesn't have gaps that need to be added later via daily migration
+        val jsonValuesByColumn = HashMap<String, Any?>()
+        jsonValuesByColumn[AuditColumn.TIMESTAMP.name] = auditEvent.timestamp
+        jsonValuesByColumn[AuditColumn.ACTION_ID.name] = auditEvent.actionId
+        jsonValuesByColumn[AuditColumn.ACTION_TYPE.name] = auditEvent.actionType
+        jsonValuesByColumn[AuditColumn.AGENT_TYPE.name] = auditEvent.agentType
+        jsonValuesByColumn[AuditColumn.AGENT_ID.name] = auditEvent.agentId
+        jsonValuesByColumn[AuditColumn.AGENT_EMAIL.name] = auditEvent.agentEmailMaybe
+        jsonValuesByColumn[AuditColumn.TARGET_TYPE.name] = auditEvent.targetType
+        jsonValuesByColumn[AuditColumn.TARGET_ID.name] = auditEvent.targetIdMaybe
+        jsonValuesByColumn[AuditColumn.AGENT_ID.name] = auditEvent.agentId
+        jsonValuesByColumn[AuditColumn.TARGET_PROPERTY.name] = auditEvent.targetPropertyMaybe
+        jsonValuesByColumn[AuditColumn.PREV_VALUE.name] = auditEvent.previousValueMaybe
+        jsonValuesByColumn[AuditColumn.NEW_VALUE.name] = auditEvent.newValueMaybe
+        return JsonPayload.of(jsonValuesByColumn)
     }
 
     companion object {
         private val serviceLogger = Logger.getLogger(ActionAuditServiceImpl::class.java.name)
-        private val MONITORED_RESOURCE_TYPE = "global"
-        val MONITORED_RESOURCE = MonitoredResource.newBuilder(MONITORED_RESOURCE_TYPE).build()
+        private const val MONITORED_RESOURCE_TYPE = "global"
+        private val monitoredResource: MonitoredResource = MonitoredResource.newBuilder(MONITORED_RESOURCE_TYPE).build()
     }
 }
