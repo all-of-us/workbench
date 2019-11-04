7b23fee3d10c2eace00c7fbb4252c7cb20f9fb4f
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-11-04 11:38:13 -0500

    profile target property enum & misc cleanup

--- api/src/main/java/org/pmiops/workbench/audit/targetproperties/ProfileTargetProperty.kt
+++ api/src/main/java/org/pmiops/workbench/audit/targetproperties/ProfileTargetProperty.kt
@@ -3,5 +3,48 @@ package org.pmiops.workbench.audit.targetproperties
 import org.pmiops.workbench.model.Profile
 
 enum class ProfileTargetProperty(val propertyName: String, private val extractor: (Profile) -> String?) {
+    USER_NAME("user_name", Profile::getUsername),
+    CREATION_NONCE("creation_nonce", Profile::getCreationNonce),
+    CONTACT_EMAIL("contact_email", Profile::getContactEmail),
+    DATA_ACCESS_LEVEL("data_access_level", { it.dataAccessLevel.toString() }),
+    GIVEN_NAME("given_name", Profile::getGivenName),
+    FAMILY_NAME("family_name", Profile::getFamilyName),
+    PHONE_NUMBER("phone_number", Profile::getPhoneNumber),
+    CURRENT_POSITION("current_position", Profile::getCurrentPosition),
+    ORGANIZATION("organization", Profile::getOrganization),
+    DISABLED("disabled", { it.disabled.toString() }),
+    ABOUT_YOU("about_you", Profile::getAboutYou),
+    AREA_OF_RESEARCH("area_of_research", Profile::getAreaOfResearch),
+    INSTITUTIONAL_AFFILIATIONS("institutional_affiliations",
+            { it.institutionalAffiliations.joinToString(", ") }),
+    DEMOGRAPHIC_SURVEY("demographic_survey", { it.demographicSurvey.toString() }),
+    ADDRESS("address", { it.address?.toString() }),
 
+    fun extract(profile: Profile): String? {
+        return extractor.invoke(profile)
+    }
+
+    companion object {
+        @JvmStatic
+        fun getPropertyValuesByName(profile: Profile): Map<String, String> {
+            return values()
+                    .filter { it.extract(profile) != null }
+                    .map { it.propertyName to it.extract(profile)!! }
+                    .toMap()
+        }
+
+        @JvmStatic
+        fun getChangedValuesByName(
+                previousProfile: Profile,
+                newProfile: Profile
+        ): Map<String, PreviousNewValuePair> {
+            return values()
+                    .map { it.propertyName to
+                            PreviousNewValuePair(
+                                    previousValue = it.extract(previousProfile),
+                                    newValue = it.extract(newProfile)) }
+                    .filter { it.second.valueChanged }
+                    .toMap()
+        }
+    }
 }
--- api/src/main/java/org/pmiops/workbench/audit/targetproperties/WorkspaceTargetProperty.kt
+++ api/src/main/java/org/pmiops/workbench/audit/targetproperties/WorkspaceTargetProperty.kt
@@ -4,7 +4,7 @@ import org.pmiops.workbench.model.Workspace
 
 // N.B. entries will rarely be referred to by name, but are accessed via values(). I.e. they are
 // not safe to remove, even if an IDE indicates otherwise.
-enum class WorkspaceTargetProperty private constructor(val propertyName: String, private val extractor: (Workspace) -> String?) {
+enum class WorkspaceTargetProperty(val propertyName: String, private val extractor: (Workspace) -> String?) {
     NAME(propertyName = "name", extractor = Workspace::getName),
     INTENDED_STUDY(propertyName = "intended_study", extractor = { it.researchPurpose.intendedStudy }),
     CREATOR(propertyName = "creator", extractor = Workspace::getCreator),
@@ -14,7 +14,7 @@ enum class WorkspaceTargetProperty private constructor(val propertyName: String,
     DISEASE_OF_FOCUS(propertyName = "disease_of_focus", extractor = { w -> w.researchPurpose.diseaseOfFocus }),
     REASON_FOR_ALL_OF_US(propertyName = "reason_for_all_of_us", extractor = { w -> w.researchPurpose.reasonForAllOfUs }),
     NAMESPACE(propertyName = "namespace", extractor = Workspace::getNamespace),
-     CDR_VERSION_ID(propertyName = "cdr_version_id", extractor = Workspace::getCdrVersionId);
+    CDR_VERSION_ID(propertyName = "cdr_version_id", extractor = Workspace::getCdrVersionId) ;
 
     fun extract(workspace: Workspace?): String? {
         return if (workspace == null) {
