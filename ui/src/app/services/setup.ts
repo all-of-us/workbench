bb5e3fb79127de12a1b944f83f14d3aff50946ca
Ariel Schwartz <aschwart@broadinstitute.org>
2019-11-06 11:34:56 -0500

    convert to validate.js

--- ui/src/app/services/setup.ts
+++ ui/src/app/services/setup.ts
@@ -4,4 +4,13 @@ export const setupCustomValidators = () => {
   validate.validators.custom = (value, options, key, attributes) => {
     return options.fn(value, key, attributes);
   };
+
+  validate.validators.truthiness = (value, options) => {
+    if(value !== options) {
+      return `must be ${options}`
+    }
+    else {
+      return undefined;
+    }
+  }
 };
