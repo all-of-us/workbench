0ea2809d6f5653bec68fc500832362e3ff6f4c31
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-11-07 16:49:58 -0500

    spotless

--- api/src/test/java/org/pmiops/workbench/api/UserMetricsControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/UserMetricsControllerTest.java
@@ -155,11 +155,9 @@ public class UserMetricsControllerTest {
             workspace2.getWorkspaceNamespace(), workspace2.getFirecloudName()))
         .thenReturn(workspace2);
 
-    when(mockFireCloudService.getWorkspace(workspace1))
-        .thenReturn(workspaceResponse);
+    when(mockFireCloudService.getWorkspace(workspace1)).thenReturn(workspaceResponse);
 
-    when(mockFireCloudService.getWorkspace(workspace2))
-        .thenReturn(workspaceResponse2);
+    when(mockFireCloudService.getWorkspace(workspace2)).thenReturn(workspaceResponse2);
 
     when(mockCloudStorageService.blobsExist(anyList()))
         .then(
