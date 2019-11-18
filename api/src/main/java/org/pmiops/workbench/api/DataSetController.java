afc9fae56a36e6239da30442a9ce58ae55b9565c
Scout Rubenstein <brubenst@broadinstitute.org>
2019-11-18 17:05:35 -0500

    remove unused imports

--- api/src/main/java/org/pmiops/workbench/api/DataSetController.java
+++ api/src/main/java/org/pmiops/workbench/api/DataSetController.java
@@ -16,7 +16,6 @@ import java.text.DateFormat;
 import java.text.SimpleDateFormat;
 import java.time.Clock;
 import java.time.Instant;
-import java.time.LocalDateTime;
 import java.util.ArrayList;
 import java.util.Collections;
 import java.util.Comparator;
@@ -424,7 +423,8 @@ public class DataSetController implements DataSetApiDelegate {
               value -> {
                 if (!value.equals(EMPTY_CELL_MARKER)) {
                   Double fieldValue = Double.parseDouble(value);
-                  queryValues.add(dateFormat.format(Date.from(Instant.ofEpochSecond(fieldValue.longValue()))));
+                  queryValues.add(
+                      dateFormat.format(Date.from(Instant.ofEpochSecond(fieldValue.longValue()))));
                 } else {
                   queryValues.add(value);
                 }
