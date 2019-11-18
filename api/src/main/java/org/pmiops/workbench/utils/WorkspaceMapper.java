d1aae45f6e59bfce02c8894d0e042514fc55b873
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-11-18 18:19:47 -0500

    remove commented code

--- api/src/main/java/org/pmiops/workbench/utils/WorkspaceMapper.java
+++ api/src/main/java/org/pmiops/workbench/utils/WorkspaceMapper.java
@@ -95,36 +95,4 @@ public interface WorkspaceMapper {
       return WorkspaceAccessLevel.fromValue(firecloudAccessLevel);
     }
   }
-  //  @Mapping(source = "id", target = "workspaceId")
-  //  @Mapping(source = "namespace", target = "workspaceNamespace")
-  //  @Mapping(source = "", target = "firecloudName")
-  //    //  "version, firecloudName, dataAccessLevelEnum,
-  //    //  cdrVersion, lastAccessedTime, diseaseFocusedResearch,
-  //    //  diseaseOfFocus, methodsDevelopment, controlSet, ancestry,
-  //    //  commercialPurpose, socialBehavioral, populationHealth,
-  //    //  educational, drugDevelopment, otherPurpose, otherPurposeDetails,
-  //    //  population, populationDetails, specificPopulationsEnum,
-  //    //  otherPopulationDetails, additionalNotes, reasonForAllOfUs,
-  //    //  intendedStudy, anticipatedFindings, reviewRequested, approved,
-  //    //  timeRequested, cohorts, conceptSets, dataSets, firecloudUuid,
-  //    //  workspaceActiveStatusEnum, billingMigrationStatusEnum,
-  //    //  billingStatus, billingAccountType".
-  //    // I think we need an actual decorator to do the split mapping:
-  // https://mapstruct.org/documentation/1.0/reference/html/index.html#customizing-mappers-using-decorators
-  //  DbWorkspace clientToDbModel(Workspace clientWorkspace);
-  //
-  //  @Mapping(source = "workspaceId", target = "id")
-  //  @Mapping(target = "etag", ignore = true)
-  //  @Mapping(source = "workspaceNamespace", target = "namespace")
-  //  @Mapping(source = "cdrVersion", target = "cdrVersionId")
-  //  @Mapping(target = "googleBucketName", ignore = true)
-  //  @Mapping(target = "researchPurpose", ignore = true) // need to map multiple fields into one...
-  //  Workspace dbModelToClient(DbWorkspace dbWorkspace);
-  //
-  //  @Mapping(target = "populationDetails", ignore = true)
-  //  ResearchPurpose dbWorkspaceToResearchPurpose(DbWorkspace workspace);
-  //
-  //  default String etagFromVersion(int version) {
-  //    return Etags.fromVersion(version);
-  //  }
 }
