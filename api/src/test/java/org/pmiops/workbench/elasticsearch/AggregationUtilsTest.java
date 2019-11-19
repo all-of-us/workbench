ea1ee9ce9f9573a86e4bb416fbbc1d61fdc6c762
Brian Freeman <brian.d.freeman@gmail.com>
2019-11-19 12:05:52 -0600

    changing age ranges for charts.

--- api/src/test/java/org/pmiops/workbench/elasticsearch/AggregationUtilsTest.java
+++ api/src/test/java/org/pmiops/workbench/elasticsearch/AggregationUtilsTest.java
@@ -4,7 +4,7 @@ import static com.google.common.truth.Truth.assertThat;
 import static org.pmiops.workbench.elasticsearch.AggregationUtils.DATE;
 import static org.pmiops.workbench.elasticsearch.AggregationUtils.GENDER;
 import static org.pmiops.workbench.elasticsearch.AggregationUtils.RACE;
-import static org.pmiops.workbench.elasticsearch.AggregationUtils.RANGE_19_44;
+import static org.pmiops.workbench.elasticsearch.AggregationUtils.RANGE_18_44;
 
 import org.elasticsearch.search.aggregations.AggregationBuilder;
 import org.junit.Test;
@@ -16,16 +16,16 @@ public class AggregationUtilsTest {
 
   @Test
   public void buildDemoChartAggregation() throws Exception {
-    AggregationBuilder actualBuilder = AggregationUtils.buildDemoChartAggregation(RANGE_19_44);
-    assertThat(actualBuilder.getName()).isEqualTo(DATE + RANGE_19_44);
+    AggregationBuilder actualBuilder = AggregationUtils.buildDemoChartAggregation(RANGE_18_44);
+    assertThat(actualBuilder.getName()).isEqualTo(DATE + RANGE_18_44);
     assertThat(actualBuilder.getType()).isEqualTo("date_range");
     assertThat(actualBuilder.getSubAggregations().size()).isEqualTo(1);
     AggregationBuilder genderBuilder = actualBuilder.getSubAggregations().iterator().next();
-    assertThat(genderBuilder.getName()).isEqualTo(GENDER + RANGE_19_44);
+    assertThat(genderBuilder.getName()).isEqualTo(GENDER + RANGE_18_44);
     assertThat(genderBuilder.getType()).isEqualTo("terms");
     assertThat(genderBuilder.getSubAggregations().size()).isEqualTo(1);
     AggregationBuilder raceBuilder = genderBuilder.getSubAggregations().iterator().next();
-    assertThat(raceBuilder.getName()).isEqualTo(RACE + RANGE_19_44);
+    assertThat(raceBuilder.getName()).isEqualTo(RACE + RANGE_18_44);
     assertThat(raceBuilder.getType()).isEqualTo("terms");
     assertThat(raceBuilder.getSubAggregations().isEmpty()).isTrue();
   }
