a80b9f9c1c980917152bc2e0efe7777d8e996b9c
Brian Freeman <brian.d.freeman@gmail.com>
2019-11-30 14:11:07 -0600

    dao test cleanup.

--- api/src/test/java/org/pmiops/workbench/cdr/dao/CBCriteriaAttributeDaoTest.java
+++ api/src/test/java/org/pmiops/workbench/cdr/dao/CBCriteriaAttributeDaoTest.java
@@ -8,20 +8,13 @@ import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.pmiops.workbench.cdr.model.DbCriteriaAttribute;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.autoconfigure.liquibase.LiquibaseAutoConfiguration;
-import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
-import org.springframework.context.annotation.Import;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.context.junit4.SpringRunner;
-import org.springframework.transaction.annotation.Transactional;
 
 @RunWith(SpringRunner.class)
 @DataJpaTest
-@Import(LiquibaseAutoConfiguration.class)
 @DirtiesContext(classMode = DirtiesContext.ClassMode.BEFORE_EACH_TEST_METHOD)
-@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
-@Transactional
 public class CBCriteriaAttributeDaoTest {
 
   @Autowired private CBCriteriaAttributeDao cbCriteriaAttributeDao;
