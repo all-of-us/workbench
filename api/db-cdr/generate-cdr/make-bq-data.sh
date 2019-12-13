97ca86dc41b8bbc907cee0b4a8949d0d331cfbde
Brian Freeman <brian.d.freeman@gmail.com>
2019-12-13 11:33:05 -0600

    RW-4148 adding new surveys.

--- api/db-cdr/generate-cdr/make-bq-data.sh
+++ api/db-cdr/generate-cdr/make-bq-data.sh
@@ -113,7 +113,10 @@ bq --quiet --project=$BQ_PROJECT query --nouse_legacy_sql \
 VALUES
 (1585855,'Lifestyle','Survey includes information on participant smoking, alcohol and recreational drug use.',0,0,3),
 (1585710,'Overall Health','Survey provides information about how participants report levels of individual health.',0,0,2),
-(1586134,'The Basics','Survey includes participant demographic information.',0,0,1)"
+(1586134,'The Basics','Survey includes participant demographic information.',0,0,1),
+(43529712,'Personal Medical History','This survey includes information about past medical history, including medical conditions and approximate age of diagnosis.',0,0,4),
+(43528895,'Healthcare Access & Utilization','Survey includes information about a participants access to and use of health care.',0,0,5),
+(43528698,'Family Medical History','Survey includes information about the medical history of a participants immediate biological family members.',0,0,6)"
 
 # Populate some tables from cdr data
 ###############
