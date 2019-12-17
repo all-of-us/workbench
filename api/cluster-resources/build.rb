30223f9d019d26d2ab29bdfb18414e3db2d88ac2
Eric Song <songe@broadinstitute.org>
2019-12-17 15:35:22 -0500

    update snippets

--- api/cluster-resources/build.rb
+++ api/cluster-resources/build.rb
@@ -10,10 +10,12 @@ def build_snippets_menu()
          ("=" * 20) + " */\n" + tmpl
 
   {
-    "{{PY_MENU_JSON}}" => "py-snippets-menu.json",
-    "{{PY_DATASET_MENU_JSON}}" => "py-dataset-snippets-menu.json",
-    "{{R_MENU_JSON}}" => "r-snippets-menu.json",
-    "{{R_DATASET_MENU_JSON}}" => "r-dataset-snippets-menu.json",
+    "{{PY_DATASET_MENU_JSON}}" => "py_dataset_snippets_menu_config.json",
+    "{{PY_GCS_MENU_JSON}}" => "py_gcs_snippets_menu_config.json",
+    "{{PY_SQL_MENU_JSON}}" => "py_sql_snippets_menu_config.json",
+    "{{R_DATASET_MENU_JSON}}" => "r_dataset_snippets_menu_config.json",
+    "{{R_GCS_MENU_JSON}}" => "r_gcs_snippets_menu_config.json",
+    "{{R_SQL_MENU_JSON}}" => "r_sql_snippets_menu_config.json",
   }.each do |var, path|
     # Ruby apparently lacks a basic string replacement function. sub/gsub
     # support pattern inputs and captures, and \ interacts poorly with output
