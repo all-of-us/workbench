bd66119ed32da810114ee8a2b0a7ab6342829e71
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2019-12-18 15:35:43 -0500

    fix @import

--- api/src/test/java/org/pmiops/workbench/api/ClusterControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/ClusterControllerTest.java
@@ -29,6 +29,7 @@ import org.pmiops.workbench.config.WorkbenchConfig.FeatureFlagsConfig;
 import org.pmiops.workbench.db.dao.AdminActionHistoryDao;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.dao.UserRecentResourceService;
+import org.pmiops.workbench.db.dao.UserServiceImpl;
 import org.pmiops.workbench.db.dao.UserServiceInterface;
 import org.pmiops.workbench.db.model.DbCdrVersion;
 import org.pmiops.workbench.db.model.DbUser;
@@ -89,7 +90,7 @@ public class ClusterControllerTest {
   private static DbUser user = new DbUser();
 
   @TestConfiguration
-  @Import({ClusterController.class, UserServiceInterface.class})
+  @Import({ClusterController.class, UserServiceImpl.class})
   @MockBean({
     FireCloudService.class,
     LeonardoNotebooksClient.class,
