ef375e81f3c9c742091eb79cf76c794efdc360af
Eric Song <songe@broadinstitute.org>
2019-12-18 14:23:11 -0500

    better name

--- api/cluster-resources/import_json_from_snippets_repo.sh
+++ api/cluster-resources/import_json_from_snippets_repo.sh
@@ -11,9 +11,9 @@ SNIPPETS_REPO_DIR=${SNIPPETS_REPO_DIR%/}
 
 snippet_filename_prefixes=("py_gcs" "py_sql" "py_dataset" "r_gcs" "r_sql" "r_dataset")
 
-for name in "${snippet_filename_prefixes[@]}"; do
-  source="${SNIPPETS_REPO_DIR}/build/${name}_snippets_menu_config.json"
-  dest="$(git rev-parse --show-toplevel)/api/cluster-resources/$(echo ${name} | tr '_' '-')-snippets-menu.json"
+for prefix in "${snippet_filename_prefixes[@]}"; do
+  source="${SNIPPETS_REPO_DIR}/build/${prefix}_snippets_menu_config.json"
+  dest="$(git rev-parse --show-toplevel)/api/cluster-resources/$(echo ${prefix} | tr '_' '-')-snippets-menu.json"
   jq '.' $source > $dest
   echo "Copied pretty printed $source into $dest"
 done
