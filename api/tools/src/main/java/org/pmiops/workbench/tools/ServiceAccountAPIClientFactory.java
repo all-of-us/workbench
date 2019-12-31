927b454c5959dea89d05f49edf85b88c92c30104
Gregory Jordan <gjordan@verily.com>
2019-12-30 22:43:41 -0500

    Fix command-line tools.

--- api/tools/src/main/java/org/pmiops/workbench/tools/ServiceAccountAPIClientFactory.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/ServiceAccountAPIClientFactory.java
@@ -3,6 +3,8 @@ package org.pmiops.workbench.tools;
 import com.google.auth.oauth2.GoogleCredentials;
 import java.io.IOException;
 import java.util.Arrays;
+
+import com.google.auth.oauth2.GoogleCredentials;
 import org.pmiops.workbench.firecloud.ApiClient;
 import org.pmiops.workbench.firecloud.api.BillingApi;
 import org.pmiops.workbench.firecloud.api.WorkspacesApi;
