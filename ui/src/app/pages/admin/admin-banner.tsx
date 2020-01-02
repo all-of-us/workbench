02d671bd274f41800e37c3aab236c01c19e8d8b5
Scout Rubenstein <brubenst@broadinstitute.org>
2020-01-02 13:19:58 -0500

    Add validation

--- ui/src/app/pages/admin/admin-banner.tsx
+++ ui/src/app/pages/admin/admin-banner.tsx
@@ -6,6 +6,8 @@ import colors from 'app/styles/colors';
 import {ReactWrapperBase} from 'app/utils';
 import * as React from 'react';
 import ReactSwitch from 'react-switch';
+import * as validate from 'validate.js';
+import {TooltipTrigger} from "../../components/popups";
 
 const styles = {
   smallHeaderStyles: {
@@ -21,7 +23,18 @@ interface AdminBannerState {
   bannerHeadline: string;
   readMoreLink: string;
 }
-
+const required = {presence: {allowEmpty: false}};
+const notTooLong = maxLength => ({
+  length: {
+    maximum: maxLength,
+    tooLong: 'must be %{count} characters or less'
+  }
+});
+const validators = {
+  bannerDescription: {...required, ...notTooLong(4000)},
+  bannerHeadline: {...required, ...notTooLong(200)},
+  readMoreLink: {...notTooLong(200)},
+};
 export class AdminBanner extends React.Component<{}, AdminBannerState> {
   constructor(props) {
     super(props);
@@ -66,33 +79,46 @@ export class AdminBanner extends React.Component<{}, AdminBannerState> {
   }
 
   render() {
+    const {bannerDescription, bannerEnabled, bannerHeadline, readMoreLink} = this.state;
+    const errors = validate({
+      bannerDescription, bannerHeadline, readMoreLink
+    }, validators, {
+      prettify: v => ({
+        bannerDescription: 'Banner Description',
+        bannerHeadline: 'Banner Headline',
+        readMoreLink: 'Read More Button'
+      }[v] || validate.prettify(v))
+    });
     return <div style={{width: '24rem'}}>
       <BoldHeader style={{fontSize: 18}}>Service Banners</BoldHeader>
       <Header style={{...styles.smallHeaderStyles, marginTop: '0.5rem'}}>Banner Headline</Header>
       <TextInput onChange={(v) => this.setState({bannerHeadline: v})}
-                 value={this.state.bannerHeadline} data-test-id='banner-headline-input'
+                 value={bannerHeadline} data-test-id='banner-headline-input'
                  placeholder='Type headline text'/>
       <Header style={styles.smallHeaderStyles}>Banner Description</Header>
-      <TextArea value={this.state.bannerDescription}
+      <TextArea value={bannerDescription}
                 onChange={v => this.setState({bannerDescription: v})}
                 data-test-id='banner-description-input'
                 placeholder='Type descriptive banner text'/>
       <Header style={styles.smallHeaderStyles}>Read More Button/CTA <span style={{fontWeight: 400,
         fontSize: 12}}>(Optional)</span></Header>
       <TextInput onChange={(v) => this.setState({readMoreLink: v})}
-                 value={this.state.readMoreLink} data-test-id='read-more-link-input'
+                 value={readMoreLink} data-test-id='read-more-link-input'
                  placeholder='Paste button link'/>
-      <div style={{marginTop: '1.5rem', display: 'flex', alignItems: 'center'}}>
-        <ReactSwitch checked={this.state.bannerEnabled}
-                     checkedIcon={false}
-                     height={17}
-                     onChange={(checked) => this.handleBannerToggle(checked)}
-                     uncheckedIcon={false}
-                     width={34}/>
-        <div style={{textTransform: 'uppercase', color: colors.primary, marginLeft: 4}}>
-          {this.state.bannerEnabled ? 'On' : 'Off'}
+      <TooltipTrigger content={!bannerEnabled && !!errors && 'Headline and description are required.'} side='right'>
+        <div style={{marginTop: '1.5rem', display: 'flex', alignItems: 'center', width: '3rem'}}>
+            <ReactSwitch checked={bannerEnabled}
+                         disabled={!bannerEnabled && errors}
+                         checkedIcon={false}
+                         height={17}
+                         onChange={(checked) => this.handleBannerToggle(checked)}
+                         uncheckedIcon={false}
+                         width={34}/>
+          <div style={{textTransform: 'uppercase', color: colors.primary, marginLeft: 4}}>
+            {bannerEnabled ? 'On' : 'Off'}
+          </div>
         </div>
-      </div>
+      </TooltipTrigger>
     </div>;
   }
 }
