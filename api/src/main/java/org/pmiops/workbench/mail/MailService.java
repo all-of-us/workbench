eaa9ada72b8764a0b5e1fe8ec5c4f8d6cdadde8b
Joel Thibault <thibault@broad.dev>
2020-01-16 12:50:57 -0500

    RW-3661 send alert emails

--- api/src/main/java/org/pmiops/workbench/mail/MailService.java
+++ api/src/main/java/org/pmiops/workbench/mail/MailService.java
@@ -1,7 +1,11 @@
 package org.pmiops.workbench.mail;
 
 import com.google.api.services.directory.model.User;
+import java.time.Instant;
+import java.time.LocalDate;
+import java.util.Optional;
 import javax.mail.MessagingException;
+import org.pmiops.workbench.db.model.DbUser;
 
 public interface MailService {
 
@@ -12,4 +16,21 @@ public interface MailService {
 
   void sendBetaAccessCompleteEmail(final String contactEmail, final String username)
       throws MessagingException;
+
+  void alertUserFreeTierDollarThreshold(
+      final DbUser user,
+      double threshold,
+      double currentUsage,
+      double remainingBalance,
+      final Optional<Instant> expirationTimeIfKnown)
+      throws MessagingException;
+
+  void alertUserFreeTierTimeThreshold(
+      final DbUser user,
+      long daysRemaining,
+      final LocalDate expirationDate,
+      double remainingDollarBalance)
+      throws MessagingException;
+
+  void alertUserFreeTierExpiration(final DbUser user) throws MessagingException;
 }
