894f942e821d1544029eef7384b809fb2640ca0a
Neha Saxena <nsaxena@broadinstitute.org>
2020-01-23 08:49:49 -0500

    Spotless

--- api/src/main/java/org/pmiops/workbench/interceptors/CloudTaskInterceptor.java
+++ api/src/main/java/org/pmiops/workbench/interceptors/CloudTaskInterceptor.java
@@ -2,6 +2,9 @@ package org.pmiops.workbench.interceptors;
 
 import com.google.api.client.http.HttpMethods;
 import io.swagger.annotations.ApiOperation;
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.Set;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 import org.springframework.core.annotation.AnnotationUtils;
@@ -9,19 +12,18 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.handler.HandlerInterceptorAdapter;
 
-import java.util.Arrays;
-import java.util.HashSet;
-import java.util.Set;
-
 /**
- * Interceptor for endpoints with tag cloudTask. All such endpoints should have valid value for
- * request hander X-AppEngine-QueueName
+ * Interceptor for endpoints with tag cloudTask. All such endpoints should have tagged as
+ * "cloudTask" and a valid value for request hander X-AppEngine-QueueName which app engine itself
+ * only sets (and overrides internally) when invoking cloud task. See
+ * https://cloud.google.com/tasks/docs/creating-appengine-handlers#reading_app_engine_task_request_headers
  */
 @Service
 public class CloudTaskInterceptor extends HandlerInterceptorAdapter {
   public static final String QUEUE_NAME_REQUEST_HEADER = "X-AppEngine-QueueName";
   private static final String CLOUD_TASK_TAG = "cloudTask";
-  public static final Set<String> VALID_QUEUE_NAME_SET = new HashSet<>(Arrays.asList("rdrQueueTest"));
+  public static final Set<String> VALID_QUEUE_NAME_SET =
+      new HashSet<>(Arrays.asList("rdrQueueTest"));
 
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
@@ -44,7 +46,8 @@ public class CloudTaskInterceptor extends HandlerInterceptorAdapter {
       }
     }
 
-    boolean hasQueueNameHeader = VALID_QUEUE_NAME_SET.contains(request.getHeader(QUEUE_NAME_REQUEST_HEADER));
+    boolean hasQueueNameHeader =
+        VALID_QUEUE_NAME_SET.contains(request.getHeader(QUEUE_NAME_REQUEST_HEADER));
     if (requireCloudTaskHeader && !hasQueueNameHeader) {
       response.sendError(
           HttpServletResponse.SC_FORBIDDEN,
--- api/src/test/java/org/pmiops/workbench/interceptors/CloudTaskInterceptorTest.java
+++ api/src/test/java/org/pmiops/workbench/interceptors/CloudTaskInterceptorTest.java
@@ -13,59 +13,62 @@ import org.mockito.Mock;
 import org.mockito.junit.MockitoJUnit;
 import org.mockito.junit.MockitoRule;
 import org.pmiops.workbench.api.CloudTaskRdrExportApi;
-
 import org.pmiops.workbench.api.WorkspacesApi;
 import org.springframework.web.method.HandlerMethod;
 
 public class CloudTaskInterceptorTest {
-	@Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
+  @Rule public MockitoRule mockitoRule = MockitoJUnit.rule();
 
-	@Mock private HandlerMethod handler;
-	@Mock private HttpServletRequest request;
-	@Mock private HttpServletResponse response;
+  @Mock private HandlerMethod handler;
+  @Mock private HttpServletRequest request;
+  @Mock private HttpServletResponse response;
 
-	private CloudTaskInterceptor interceptor;
+  private CloudTaskInterceptor interceptor;
 
-	private final String CLOUD_TASK_METHOD_NAME = "exportResearcherData";
+  private final String CLOUD_TASK_METHOD_NAME = "exportResearcherData";
 
-	@Before
-	public void setUp() {
-		interceptor = new CloudTaskInterceptor();
-	}
+  @Before
+  public void setUp() {
+    interceptor = new CloudTaskInterceptor();
+  }
 
-	@Test
-	public void preHandleOptions_OPTIONS() throws Exception {
-		when(request.getMethod()).thenReturn(HttpMethods.OPTIONS);
-		assertThat(interceptor.preHandle(request, response, handler)).isTrue();
-	}
+  @Test
+  public void preHandleOptions_OPTIONS() throws Exception {
+    when(request.getMethod()).thenReturn(HttpMethods.OPTIONS);
+    assertThat(interceptor.preHandle(request, response, handler)).isTrue();
+  }
 
-	@Test
-	public void prehandleForCloudTaskNoHeader() throws Exception {
-		when(request.getMethod()).thenReturn(HttpMethods.POST);
-		when(handler.getMethod()).thenReturn(CloudTaskRdrExportApi.class.getMethod(CLOUD_TASK_METHOD_NAME, Object.class));
-		assertThat(interceptor.preHandle(request, response, handler)).isFalse();
-	}
+  @Test
+  public void prehandleForCloudTaskNoHeader() throws Exception {
+    when(request.getMethod()).thenReturn(HttpMethods.POST);
+    when(handler.getMethod())
+        .thenReturn(CloudTaskRdrExportApi.class.getMethod(CLOUD_TASK_METHOD_NAME, Object.class));
+    assertThat(interceptor.preHandle(request, response, handler)).isFalse();
+  }
 
-	@Test
-	public void prehandleForCloudTaskWithBadHeader() throws Exception {
-		when(request.getMethod()).thenReturn(HttpMethods.POST);
-		when(handler.getMethod()).thenReturn(CloudTaskRdrExportApi.class.getMethod(CLOUD_TASK_METHOD_NAME, Object.class));
-		when(request.getHeader(CloudTaskInterceptor.QUEUE_NAME_REQUEST_HEADER)).thenReturn("asdf");
-		assertThat(interceptor.preHandle(request, response, handler)).isFalse();
-	}
+  @Test
+  public void prehandleForCloudTaskWithBadHeader() throws Exception {
+    when(request.getMethod()).thenReturn(HttpMethods.POST);
+    when(handler.getMethod())
+        .thenReturn(CloudTaskRdrExportApi.class.getMethod(CLOUD_TASK_METHOD_NAME, Object.class));
+    when(request.getHeader(CloudTaskInterceptor.QUEUE_NAME_REQUEST_HEADER)).thenReturn("asdf");
+    assertThat(interceptor.preHandle(request, response, handler)).isFalse();
+  }
 
-	@Test
-	public void prehandleForCloudTaskWithHeader() throws Exception {
-		when(request.getMethod()).thenReturn(HttpMethods.POST);
-		when(handler.getMethod()).thenReturn(CloudTaskRdrExportApi.class.getMethod(CLOUD_TASK_METHOD_NAME, Object.class));
-		when(request.getHeader(CloudTaskInterceptor.QUEUE_NAME_REQUEST_HEADER)).thenReturn("rdrQueueTest");
-		assertThat(interceptor.preHandle(request, response, handler)).isTrue();
-	}
+  @Test
+  public void prehandleForCloudTaskWithHeader() throws Exception {
+    when(request.getMethod()).thenReturn(HttpMethods.POST);
+    when(handler.getMethod())
+        .thenReturn(CloudTaskRdrExportApi.class.getMethod(CLOUD_TASK_METHOD_NAME, Object.class));
+    when(request.getHeader(CloudTaskInterceptor.QUEUE_NAME_REQUEST_HEADER))
+        .thenReturn("rdrQueueTest");
+    assertThat(interceptor.preHandle(request, response, handler)).isTrue();
+  }
 
-	@Test
-	public void prehandleForNonCloudTask() throws Exception {
-		when(request.getMethod()).thenReturn(HttpMethods.GET);
-		when(handler.getMethod()).thenReturn(WorkspacesApi.class.getMethod("getWorkspaces"));
-		assertThat(interceptor.preHandle(request, response, handler)).isTrue();
-	}
+  @Test
+  public void prehandleForNonCloudTask() throws Exception {
+    when(request.getMethod()).thenReturn(HttpMethods.GET);
+    when(handler.getMethod()).thenReturn(WorkspacesApi.class.getMethod("getWorkspaces"));
+    assertThat(interceptor.preHandle(request, response, handler)).isTrue();
+  }
 }
