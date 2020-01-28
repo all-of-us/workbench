79b13b3c3db9c3ca858857688e8c5c1aa2ca61d3
CH Albach <calbach@users.noreply.github.com>
2020-01-28 10:13:04 -0800

    Support reading the active SA from an SA context (#16)

--- aou-utils/serviceaccounts.rb
+++ aou-utils/serviceaccounts.rb
@@ -9,22 +9,26 @@ require_relative "utils/common"
 # The test SA key is a special case for local development, and is initialized
 # but not deleted.
 class ServiceAccountContext
+  attr_reader :project, :service_account, :keyfile_path
 
   SERVICE_ACCOUNT_KEY_PATH = "sa-key.json"
 
-  def initialize(project, service_account = nil, path = nil)
+  def initialize(project, service_account = nil, keyfile_path = nil)
     @project = project
     @service_account = service_account
-    @path = path
-    if not @path
-      @path = File.expand_path(SERVICE_ACCOUNT_KEY_PATH)
+    if not service_account
+      @service_account = "#{@project}@appspot.gserviceaccount.com"
+    end
+    @keyfile_path = keyfile_path
+    if not @keyfile_path
+      @keyfile_path = File.expand_path(SERVICE_ACCOUNT_KEY_PATH)
     end
   end
 
-  def existing_file_account(path)
-    if File.exists?(path)
+  def existing_file_account(keyfile_path)
+    if File.exists?(keyfile_path)
       begin
-        return JSON.parse(File.read(path))["client_email"]
+        return JSON.parse(File.read(keyfile_path))["client_email"]
       rescue JSON::ParserError => e
         return nil
       end
@@ -34,35 +38,31 @@ class ServiceAccountContext
 
   def run()
     common = Common.new
-    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = @path
-    service_account = @service_account
-    if not service_account
-      service_account = "#{@project}@appspot.gserviceaccount.com"
-    end
-    if service_account == existing_file_account(@path)
+    ENV["GOOGLE_APPLICATION_CREDENTIALS"] = @keyfile_path
+    if @service_account == existing_file_account(@keyfile_path)
       # Don't generate another key if this account is already active. This can
       # happen for nested service account contexts, for example.
-      common.status "Attaching to existing keyfile @ #{@path}"
-      yield
+      common.status "Attaching to existing keyfile @ #{@keyfile_path}"
+      yield(self)
       return
     end
 
-    if service_account == "all-of-us-workbench-test@appspot.gserviceaccount.com"
-      unless File.exists?(@path)
-        common.run_inline %W{gsutil cp gs://#{@project}-credentials/app-engine-default-sa.json
-            #{@path}}
-      end
-      yield
+    if @service_account == "all-of-us-workbench-test@appspot.gserviceaccount.com"
+      common.status "Copying key from GCS for #{@service_account} @ #{@keyfile_path}"
+      common.run_inline %W{gsutil cp gs://#{@project}-credentials/app-engine-default-sa.json
+            #{@keyfile_path}}
+      yield(self)
     else
-      common.run_inline %W{gcloud iam service-accounts keys create #{@path}
-          --iam-account=#{service_account} --project=#{@project}}
+      common.status "Creating new key for #{@service_account} @ #{@keyfile_path}"
+      common.run_inline %W{gcloud iam service-accounts keys create #{@keyfile_path}
+          --iam-account=#{@service_account} --project=#{@project}}
       begin
-        yield
+        yield(self)
       ensure
-        tmp_private_key = `grep private_key_id #{@path} | cut -d\\\" -f4`.strip()
+        tmp_private_key = `grep private_key_id #{@keyfile_path} | cut -d\\\" -f4`.strip()
         common.run_inline %W{gcloud iam service-accounts keys delete #{tmp_private_key} -q
-           --iam-account=#{service_account} --project=#{@project}}
-        common.run_inline %W{rm #{@path}}
+           --iam-account=#{@service_account} --project=#{@project}}
+        common.run_inline %W{rm #{@keyfile_path}}
       end
     end
   end
