d6d637c244c28e1db42f25958dd65af8b47f55fd
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-02-04 20:47:46 -0500

    tighten loops

--- api/src/main/java/org/pmiops/workbench/monitoring/LogsBasedMetricServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/monitoring/LogsBasedMetricServiceImpl.java
@@ -2,14 +2,11 @@ package org.pmiops.workbench.monitoring;
 
 import com.google.cloud.logging.LogEntry;
 import com.google.cloud.logging.Logging;
-import com.google.cloud.logging.Payload;
 import com.google.cloud.logging.Payload.JsonPayload;
 import com.google.cloud.logging.Severity;
 import com.google.common.collect.ImmutableMap;
 import com.google.common.collect.ImmutableSet;
-import java.util.Map;
 import java.util.Set;
-import org.pmiops.workbench.monitoring.views.Metric;
 import org.springframework.stereotype.Service;
 
 @Service
@@ -56,22 +53,27 @@ public class LogsBasedMetricServiceImpl implements LogsBasedMetricService {
                 ImmutableMap.toImmutableMap(
                     e -> e.getKey().getName(), e -> e.getValue().asString()));
 
-    final ImmutableSet.Builder<JsonPayload> resultBuilder = ImmutableSet.builder();
-    // Make a JsonPayload for each measurement, and include the whole map.
-    for (Map.Entry<Metric, Number> entry : measurementBundle.getMeasurements().entrySet()) {
-      resultBuilder.add(
-          JsonPayload.of(
-              ImmutableMap.of(
-                  LogsBasedMetricService.METRIC_VALUE_KEY, entry.getValue().doubleValue(),
-                  LogsBasedMetricService.METRIC_NAME_KEY, entry.getKey().getName(),
-                  LogsBasedMetricService.METRIC_LABELS_KEY, labelToValue)));
-    }
-
-    return resultBuilder.build();
+    // Make a JsonPayload for each measurement, and include all the metric labels.
+    return measurementBundle.getMeasurements().entrySet().stream()
+        .map(
+            entry ->
+                JsonPayload.of(
+                    ImmutableMap.of(
+                        LogsBasedMetricService.METRIC_VALUE_KEY, entry.getValue().doubleValue(),
+                        LogsBasedMetricService.METRIC_NAME_KEY, entry.getKey().getName(),
+                        LogsBasedMetricService.METRIC_LABELS_KEY, labelToValue)))
+        .collect(ImmutableSet.toImmutableSet());
   }
 
-  private LogEntry payloadToLogEntry(Payload payload) {
-    return LogEntry.newBuilder(payload)
+  /**
+   * The LogEntry structure should only vary by payload within a single running of the application.
+   * The MonitoredResource should not change during execution.
+   *
+   * @param jsonPayload - A JsonPayload structure with the three keys in LogsBasedMetricService.
+   * @return
+   */
+  private LogEntry payloadToLogEntry(JsonPayload jsonPayload) {
+    return LogEntry.newBuilder(jsonPayload)
         .setSeverity(Severity.INFO)
         .setLogName(METRICS_LOG_NAME)
         .setResource(stackdriverStatsExporterService.getLoggingMonitoredResource())
