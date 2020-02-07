af1b052d4d6ee48fb7f3e67c80d7f058e1233c67
Ariel Schwartz <aschwart@broadinstitute.org>
2020-02-07 17:05:14 -0500

    clusters

--- ui/src/app/pages/admin/admin-workspace.tsx
+++ ui/src/app/pages/admin/admin-workspace.tsx
@@ -6,10 +6,11 @@ import {Button} from 'app/components/buttons';
 import {FlexColumn, FlexRow} from 'app/components/flex';
 import {TextInput} from 'app/components/inputs';
 import {workspaceAdminApi} from 'app/services/swagger-fetch-clients';
+import colors from "app/styles/colors";
 import {ReactWrapperBase} from 'app/utils';
 
 import {
-  AdminWorkspaceResources,
+  AdminWorkspaceResources, BillingAccountType,
   UserRole,
   Workspace
 } from 'generated/fetch';
@@ -78,13 +79,13 @@ export class AdminWorkspace extends React.Component<Props, State> {
   }
 
   render() {
-    const {workspace, resources} = this.state;
+    const {workspace, collaborators, resources} = this.state;
     return <div>
       <h2>Manage Workspaces</h2>
       <FlexRow style={{justifyContent: 'flex-start', alignItems: 'center'}}>
-        <label style={{marginRight: '1rem'}}>GCP Project ID</label>
+        <label style={{marginRight: '1rem'}}>Google Project ID</label>
         <TextInput
-            style={{...styles.columnWithRightMargin}}
+            style={styles.columnWithRightMargin}
             onChange={value => this.updateProject(value)}
             onKeyDown={event => this.maybeGetFederatedWorkspaceInformation(event)}
         />
@@ -92,46 +93,124 @@ export class AdminWorkspace extends React.Component<Props, State> {
             style={{height: '1.5rem'}}
             onClick={() => this.getFederatedWorkspaceInformation()}
         >
-          Find Clusters
+          Search
         </Button>
       </FlexRow>
-      {workspace && (resources.workspaceObjects || resources.cloudStorage) && <FlexColumn style={{flex: '1 0 auto'}}>
-        <h3>Workspace Metadata</h3>
-        {resources.workspaceObjects && <FlexRow style={{width: '100%'}}>
-            <label style={{alignSelf: 'center', ...styles.columnWithRightMargin}}>Workspace
-              Objects</label>
-            <FlexColumn style={{...styles.columnWithRightMargin}}>
-              <label># of Cohorts</label>
-              <div>{resources.workspaceObjects.cohortCount}</div>
+      {
+        workspace && <FlexColumn>
+          <h3>Workspace Admin Actions</h3>
+          <FlexRow style={{justifyContent: 'space-between'}}>
+            <Button>Shut down all VMs</Button>
+            <Button>Disable workspace</Button>
+            <Button>Disable all users associated with workspace</Button>
+            <Button>Exclude workspace from public directory</Button>
+            <Button>Log administrative comment</Button>
+          </FlexRow>
+        </FlexColumn>
+      }
+      {workspace && (resources.workspaceObjects || resources.cloudStorage) &&
+        <FlexColumn style={{flex: '1 0 auto'}}>
+          <h3>Workspace Metadata</h3>
+          {
+            <FlexRow style={{width: '100%'}}>
+              <label style={{alignSelf: 'center', color: colors.primary, ...styles.columnWithRightMargin}}>
+                Basic Information
+              </label>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}>Workspace Name</label>
+                <div>{workspace.name}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}>Google Project ID</label>
+                <div>{workspace.namespace}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}>Billing Status</label>
+                <div>{workspace.billingStatus}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}>Billing Account Type</label>
+                <div>{workspace.billingAccountType}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}>Created Time</label>
+                <div>{new Date(workspace.creationTime).toDateString()}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}>Last Modified Time</label>
+                <div>{new Date(workspace.lastModifiedTime).toDateString()}</div>
+              </FlexColumn>
+            </FlexRow>
+          }
+          {resources.workspaceObjects && <FlexRow style={{width: '100%'}}>
+              <label style={{alignSelf: 'center', color: colors.primary, ...styles.columnWithRightMargin}}>
+                Workspace Objects
+              </label>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}># of Cohorts</label>
+                <div>{resources.workspaceObjects.cohortCount}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}># of Concept Sets</label>
+                <div>{resources.workspaceObjects.conceptSetCount}</div>
+              </FlexColumn>
+              <FlexColumn style={styles.columnWithRightMargin}>
+                <label style={{color: colors.primary}}># of Data Sets</label>
+                <div>{resources.workspaceObjects.datasetCount}</div>
+              </FlexColumn>
+            </FlexRow>
+          }
+          {resources.cloudStorage && <FlexRow style={{width: '100%'}}>
+            <label style={{alignSelf: 'center', color: colors.primary, ...styles.columnWithRightMargin}}>
+              Cloud Storage
+            </label>
+            <FlexColumn style={styles.columnWithRightMargin}>
+              <label style={{color: colors.primary}}># of Notebook Files</label>
+              <div>{resources.cloudStorage.notebookFileCount}</div>
             </FlexColumn>
-            <FlexColumn style={{...styles.columnWithRightMargin}}>
-              <label># of Concept Sets</label>
-              <div>{resources.workspaceObjects.conceptSetCount}</div>
+            <FlexColumn style={styles.columnWithRightMargin}>
+              <label style={{color: colors.primary}}># of Non-Notebook Files</label>
+              <div>{resources.cloudStorage.nonNotebookFileCount}</div>
             </FlexColumn>
-            <FlexColumn style={{...styles.columnWithRightMargin}}>
-              <label># of Data Sets</label>
-              <div>{resources.workspaceObjects.datasetCount}</div>
+            <FlexColumn style={styles.columnWithRightMargin}>
+              <label style={{color: colors.primary}}>Storage used (bytes)</label>
+              <div>{resources.cloudStorage.storageBytesUsed}</div>
             </FlexColumn>
           </FlexRow>
-        }
-        {resources.cloudStorage && <FlexRow style={{width: '100%'}}>
-          <label style={{alignSelf: 'center', ...styles.columnWithRightMargin}}>Cloud
-            Storage</label>
-          <FlexColumn style={{...styles.columnWithRightMargin}}>
-            <label># of Notebook Files</label>
-            <div>{resources.cloudStorage.notebookFileCount}</div>
-          </FlexColumn>
-          <FlexColumn style={{...styles.columnWithRightMargin}}>
-            <label># of Non-Notebook Files</label>
-            <div>{resources.cloudStorage.nonNotebookFileCount}</div>
-          </FlexColumn>
-          <FlexColumn style={{...styles.columnWithRightMargin}}>
-            <label>Storage used (bytes)</label>
-            <div>{resources.cloudStorage.storageBytesUsed}</div>
-          </FlexColumn>
-        </FlexRow>
-        }
-      </FlexColumn>
+          }
+        </FlexColumn>
+      }
+      {
+        workspace && collaborators && <FlexColumn>
+          <h3>Workspace Collaborators</h3>
+          {collaborators.map((userRole, i) =>
+              <div key={i}>
+                {userRole.email + ': ' + userRole.role}
+              </div>
+          )}
+        </FlexColumn>
+      }
+      {
+        workspace && resources.clusters && <FlexColumn>
+          <h3>Clusters</h3>
+          <FlexRow>
+            <label style={{color: colors.primary, ...styles.columnWithRightMargin}}>Cluster Name</label>
+            <label style={{color: colors.primary, ...styles.columnWithRightMargin}}>Google Project</label>
+            <label style={{color: colors.primary, ...styles.columnWithRightMargin}}>Created Time</label>
+            <label style={{color: colors.primary, ...styles.columnWithRightMargin}}>Last Accessed Time</label>
+            <label style={{color: colors.primary, ...styles.columnWithRightMargin}}>Status</label>
+          </FlexRow>
+          {resources.clusters.map((cluster, i) =>
+              <FlexRow key={i}>
+                <div style={styles.columnWithRightMargin}>{cluster.clusterName}</div>
+                <div style={styles.columnWithRightMargin}>{cluster.googleProject}</div>
+                <div style={styles.columnWithRightMargin}>{new Date(cluster.createdDate).toDateString()}</div>
+                <div style={styles.columnWithRightMargin}>{new Date(cluster.dateAccessed).toDateString()}</div>
+                <div style={styles.columnWithRightMargin}>{cluster.status}</div>
+                <Button>Disable</Button>
+              </FlexRow>
+          )}
+        </FlexColumn>
       }
     </div>;
   }
