4f89bdc0ae20b72df9b56c4554f5a543ec008515
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-02-10 09:48:55 -0500

    restore status ctrlr

--- api/src/main/java/org/pmiops/workbench/api/StatusController.java
+++ api/src/main/java/org/pmiops/workbench/api/StatusController.java
@@ -2,7 +2,6 @@ package org.pmiops.workbench.api;
 
 import org.pmiops.workbench.firecloud.FireCloudService;
 import org.pmiops.workbench.model.StatusResponse;
-import org.pmiops.workbench.monitoring.MonitoringService;
 import org.pmiops.workbench.notebooks.LeonardoNotebooksClient;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +12,12 @@ public class StatusController implements StatusApiDelegate {
 
   private final FireCloudService fireCloudService;
   private final LeonardoNotebooksClient leonardoNotebooksClient;
-  private MonitoringService monitoringService;
 
   @Autowired
   StatusController(
-      FireCloudService fireCloudService,
-      LeonardoNotebooksClient leonardoNotebooksClient,
-      MonitoringService monitoringService) {
+      FireCloudService fireCloudService, LeonardoNotebooksClient leonardoNotebooksClient) {
     this.fireCloudService = fireCloudService;
     this.leonardoNotebooksClient = leonardoNotebooksClient;
-    this.monitoringService = monitoringService;
   }
 
   @Override
