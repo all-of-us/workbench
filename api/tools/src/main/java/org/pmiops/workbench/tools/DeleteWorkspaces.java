fbe6549a15d9fe70fe66337bea4aae38f9feadd5
Eric Song <songe@broadinstitute.org>
2020-02-20 17:08:45 -0500

    remove dead code and add exception handler

--- api/tools/src/main/java/org/pmiops/workbench/tools/DeleteWorkspaces.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/DeleteWorkspaces.java
@@ -1,69 +1,40 @@
 package org.pmiops.workbench.tools;
 
-import com.google.common.collect.Streams;
-import com.opencsv.bean.StatefulBeanToCsvBuilder;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.io.FileWriter;
 import java.io.IOException;
-import java.text.SimpleDateFormat;
 import java.time.Clock;
-import java.util.ArrayList;
-import java.util.Collection;
-import java.util.List;
-import java.util.Set;
+import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.Collectors;
 import javax.inject.Provider;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
 import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
-import org.pmiops.workbench.config.WorkbenchConfig;
-import org.pmiops.workbench.db.dao.CohortDao;
-import org.pmiops.workbench.db.dao.ConceptSetDao;
-import org.pmiops.workbench.db.dao.DataSetDao;
 import org.pmiops.workbench.db.dao.UserDao;
-import org.pmiops.workbench.db.dao.UserRecentResourceServiceImpl;
 import org.pmiops.workbench.db.dao.UserRecentWorkspaceDao;
 import org.pmiops.workbench.db.dao.WorkspaceDao;
-import org.pmiops.workbench.db.dao.WorkspaceFreeTierUsageDao;
-import org.pmiops.workbench.db.model.DbCohort;
-import org.pmiops.workbench.db.model.DbConceptSet;
-import org.pmiops.workbench.db.model.DbDataset;
 import org.pmiops.workbench.db.model.DbStorageEnums;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbWorkspace;
-import org.pmiops.workbench.db.model.DbWorkspaceFreeTierUsage;
-import org.pmiops.workbench.exceptions.NotFoundException;
 import org.pmiops.workbench.firecloud.ApiClient;
-import org.pmiops.workbench.firecloud.ApiException;
 import org.pmiops.workbench.firecloud.FireCloudConfig;
 import org.pmiops.workbench.firecloud.FireCloudService;
 import org.pmiops.workbench.firecloud.FireCloudServiceImpl;
-import org.pmiops.workbench.firecloud.FirecloudTransforms;
 import org.pmiops.workbench.firecloud.api.ProfileApi;
 import org.pmiops.workbench.firecloud.api.WorkspacesApi;
-import org.pmiops.workbench.model.FileDetail;
 import org.pmiops.workbench.model.WorkspaceActiveStatus;
-import org.pmiops.workbench.monitoring.MonitoringServiceImpl;
-import org.pmiops.workbench.monitoring.MonitoringSpringConfiguration;
-import org.pmiops.workbench.monitoring.StackdriverStatsExporterService;
-import org.pmiops.workbench.notebooks.NotebooksService;
-import org.pmiops.workbench.notebooks.NotebooksServiceImpl;
 import org.pmiops.workbench.workspaces.WorkspaceService;
 import org.pmiops.workbench.workspaces.WorkspaceServiceImpl;
 import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Primary;
 import org.springframework.context.annotation.Scope;
 
-/** A tool that will generate a CSV export of our workspace data */
 @Configuration
 @Import({
     FireCloudServiceImpl.class,
@@ -128,17 +99,11 @@ public class DeleteWorkspaces {
     return new ProfileApi(apiClient);
   }
 
-
-  private WorkspaceDao workspaceDao;
-  private UserDao userDao;
-
   @Bean
   public CommandLineRunner run(
       WorkspaceDao workspaceDao,
       UserDao userDao,
       WorkspaceService workspaceService) {
-    this.workspaceDao = workspaceDao;
-    this.userDao = userDao;
 
     return (args) -> {
       CommandLine opts = new DefaultParser().parse(options, args);
@@ -158,7 +123,12 @@ public class DeleteWorkspaces {
           }
 
           currentImpersonatedUser = dbWorkspace.getCreator();
-          workspaceService.deleteWorkspace(dbWorkspace);
+          try {
+            workspaceService.deleteWorkspace(dbWorkspace);
+          } catch (Exception e) {
+            log.log(Level.WARNING, "Could not delete workspace (" + namespace + ", " + fcName + ")", e);
+          }
+
           log.info("Deleted workspace (" + dbWorkspace.getWorkspaceNamespace() + ", " + dbWorkspace.getFirecloudName() + ")");
         });
       }
