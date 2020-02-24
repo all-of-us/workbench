2ab076f6089b515d3a5f916eb3959393409dc3b2
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-02-24 15:20:48 -0500

    add options parser

--- ops/ruby/devops-framework/devops.rb
+++ ops/ruby/devops-framework/devops.rb
@@ -1,4 +1,5 @@
 require 'logger'
+require 'optparse'
 require_relative 'tasks/count_monitoring_assets.rb'
 
 # Single entry point for the devops framework. This is the only true Ruby Script file. The
@@ -8,6 +9,30 @@ require_relative 'tasks/count_monitoring_assets.rb'
 # know anything about environment variables such as ARGV, the run directory,  etc. Tasks should also avoid
 # global variables like `logger`, so that we can control logging preferences from the top level.
 
-logger = Logger.new(STDOUT)
-monitoring_assets = MonitoringAssets.new(ARGV[0], logger)
-monitoring_assets.inventory
+# TODO(jaycarlton): work out a scheme for task-specific options (i.e. subcommands)
+def parse_options
+  options = {}
+  OptionParser.new do |parser|
+    parser.on('-t', '--task [TASK]', String, 'Task to be in in each environment')
+    parser.on('-e', '--envs-file [ENVS_FILE]', String, 'Path to environments JSON file.')
+  end.parse!({into: options})
+  options
+end
+
+def run_task(options)
+  logger = Logger.new(STDOUT)
+
+  # New tasks must be included here.
+  case options[:task]
+  when 'inventory'
+    monitoring_assets = MonitoringAssets.new(options[:'envs-file'], logger)
+    monitoring_assets.inventory
+  else
+    logger.error("Unrecognized task #{options[:task]}")
+  end
+end
+
+options = parse_options
+
+# Begin execution
+run_task(options)
