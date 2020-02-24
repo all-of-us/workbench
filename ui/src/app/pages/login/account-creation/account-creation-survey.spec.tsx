2e7e5a4d8a2c57a5fc4d28411982bc50e5d799bf
Gregory Jordan <gjordan@verily.com>
2020-02-24 08:36:09 -0500

    Add test case for demographic survey component.

--- ui/src/app/pages/login/account-creation/account-creation-survey.spec.tsx
+++ ui/src/app/pages/login/account-creation/account-creation-survey.spec.tsx
@@ -2,9 +2,12 @@ import {mount, ReactWrapper, ShallowWrapper} from 'enzyme';
 import * as React from 'react';
 
 import {serverConfigStore} from 'app/utils/navigation';
-import {AccountCreationSurvey, AccountCreationSurveyProps} from 'app/pages/login/account-creation/account-creation-survey';
+import {
+  AccountCreationSurvey,
+  AccountCreationSurveyProps,
+} from 'app/pages/login/account-creation/account-creation-survey';
 import {getEmptyProfile} from 'app/pages/login/test-utils';
-import {Ethnicity, Race} from 'generated/fetch';
+import {Ethnicity, GenderIdentity, Race, SexAtBirth} from 'generated/fetch';
 
 type AnyWrapper = (ShallowWrapper|ReactWrapper);
 const getPrivacyCheckbox = (wrapper: AnyWrapper): AnyWrapper => {
@@ -40,16 +43,29 @@ it('should render', async() => {
 });
 
 it('should load existing profile data', async() => {
-  props.profile.demographicSurvey.race = [Race.AIAN, Race.AA];
-  props.profile.demographicSurvey.identifiesAsLgbtq = true;
-  props.profile.demographicSurvey.ethnicity = Ethnicity.HISPANIC
+  const {demographicSurvey} = props.profile;
+  demographicSurvey.race = [Race.AIAN, Race.AA];
+  demographicSurvey.genderIdentityList = [GenderIdentity.MAN];
+  demographicSurvey.sexAtBirth = [SexAtBirth.MALE];
+  demographicSurvey.identifiesAsLgbtq = true;
+  demographicSurvey.ethnicity = Ethnicity.HISPANIC
   const wrapper = mount(<AccountCreationSurvey {...props} />);
 
+  // Race
   expect(wrapper.find('CheckBox[data-test-id="checkbox-AIAN"]').prop('checked')).toBeTruthy();
   expect(wrapper.find('CheckBox[data-test-id="checkbox-AA"]').prop('checked')).toBeTruthy();
   expect(wrapper.find('CheckBox[data-test-id="checkbox-WHITE"]').prop('checked')).toBeFalsy();
 
+  // Gender identity
+  expect(wrapper.find('CheckBox[data-test-id="checkbox-MAN"]').prop('checked')).toBeTruthy();
+
+  // Sex at birth
+  expect(wrapper.find('CheckBox[data-test-id="checkbox-MALE"]').prop('checked')).toBeTruthy();
+
+  // LGBTQ
   // We use the .hostNodes() call to filter down to just the React component in the result set.
   expect(wrapper.find('[data-test-id="radio-lgbtq-yes"]').hostNodes().prop('checked')).toBeTruthy();
+
+  // Ethnicity
   expect(wrapper.find('[data-test-id="dropdown-ethnicity"]').prop('value')).toEqual(Ethnicity.HISPANIC);
 });
