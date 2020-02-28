7515e94a6415ca9d837c840b4f795e8b65da19f7
Gregory Jordan <gjordan@verily.com>
2020-02-28 16:11:07 -0500

    Sign-in test fixes.

--- ui/src/app/pages/login/sign-in.spec.tsx
+++ ui/src/app/pages/login/sign-in.spec.tsx
@@ -25,7 +25,8 @@ describe('SignInReact', () => {
 
   const defaultConfig = {
     gsuiteDomain: 'researchallofus.org',
-    requireInvitationKey: true
+    requireInvitationKey: true,
+    requireInstitutionalVerification: false,
   };
 
   beforeEach(() => {
@@ -77,7 +78,46 @@ describe('SignInReact', () => {
     const templateImage = wrapper.find('[data-test-id="sign-in-page"]');
   });
 
-  it('should handle sign-up flow', () => {
+  it('should handle sign-up flow without institutional verification', () => {
+    // This test is meant to validate the high-level flow through the sign-in component by checking
+    // that each step of the user registration flow is correctly rendered in order.
+    //
+    // As this is simply a high-level test of this component's ability to render each sub-component,
+    // we use Enzyme's shallow rendering to avoid needing to deal with the DOM-level details of
+    // each of the sub-components. Tests within the 'account-creation' folder should cover those
+    // details.
+    props.serverConfig = {...defaultConfig, requireInstitutionalVerification: false};
+    const requireInstitutionalVerification = false;
+
+    const wrapper = shallowComponent();
+
+    // To start, the landing page / login component should be shown.
+    expect(wrapper.exists(LoginReactComponent)).toBeTruthy();
+    // Simulate the "create account" button being clicked by firing the callback method.
+    wrapper.find(LoginReactComponent).props().onCreateAccount();
+
+    // Invitation key step is next.
+    expect(wrapper.exists(InvitationKey)).toBeTruthy();
+    wrapper.find(InvitationKey).props().onInvitationKeyVerified('asdf');
+
+    // Terms of Service is part of the new-style flow.
+    expect(wrapper.exists(AccountCreationTos)).toBeTruthy();
+    wrapper.find(AccountCreationTos).props().onComplete();
+
+    expect(wrapper.exists(AccountCreation)).toBeTruthy();
+    wrapper.find(AccountCreation).props().onComplete(createEmptyProfile(requireInstitutionalVerification));
+
+    // Account Creation Survey (e.g. demographics) is part of the new-style flow.
+    expect(wrapper.exists(AccountCreationSurvey)).toBeTruthy();
+    wrapper.find(AccountCreationSurvey).props().onComplete(createEmptyProfile(requireInstitutionalVerification));
+
+    expect(wrapper.exists(AccountCreationSuccess)).toBeTruthy();
+  });
+
+  it('should handle sign-up flow with institutional verification', () => {
+    props.serverConfig = {...defaultConfig, requireInstitutionalVerification: true};
+    const requireInstitutionalVerification = true;
+
     const wrapper = shallowComponent();
 
     // To start, the landing page / login component should be shown.
