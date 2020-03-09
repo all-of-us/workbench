7daa6945e3df087013da66db8a2377905427944f
Ariel Schwartz <aschwart@broadinstitute.org>
2020-03-09 11:41:16 -0400

    remove unused things

--- api/src/main/java/org/pmiops/workbench/profile/ProfileService.java
+++ api/src/main/java/org/pmiops/workbench/profile/ProfileService.java
@@ -27,72 +27,6 @@ import org.springframework.stereotype.Service;
 
 @Service
 public class ProfileService {
-  private static final Function<DbInstitutionalAffiliation, InstitutionalAffiliation>
-      TO_CLIENT_INSTITUTIONAL_AFFILIATION =
-          new Function<DbInstitutionalAffiliation, InstitutionalAffiliation>() {
-            @Override
-            public InstitutionalAffiliation apply(
-                DbInstitutionalAffiliation institutionalAffiliation) {
-              InstitutionalAffiliation result = new InstitutionalAffiliation();
-              result.setRole(institutionalAffiliation.getRole());
-              result.setInstitution(institutionalAffiliation.getInstitution());
-
-              return result;
-            }
-          };
-
-  private static final Function<DbPageVisit, PageVisit> TO_CLIENT_PAGE_VISIT =
-      new Function<DbPageVisit, PageVisit>() {
-        @Override
-        public PageVisit apply(DbPageVisit pageVisit) {
-          PageVisit result = new PageVisit();
-          result.setPage(pageVisit.getPageId());
-          result.setFirstVisit(pageVisit.getFirstVisit().getTime());
-          return result;
-        }
-      };
-
-  private static final Function<DbDemographicSurvey, DemographicSurvey>
-      TO_CLIENT_DEMOGRAPHIC_SURVEY =
-          new Function<DbDemographicSurvey, DemographicSurvey>() {
-            @Override
-            public DemographicSurvey apply(DbDemographicSurvey demographicSurvey) {
-              DemographicSurvey result = new DemographicSurvey();
-              if (demographicSurvey.getDisability() != null) {
-                result.setDisability(Disability.TRUE.equals(demographicSurvey.getDisabilityEnum()));
-              }
-              result.setEducation(demographicSurvey.getEducationEnum());
-              result.setEthnicity(demographicSurvey.getEthnicityEnum());
-              result.setIdentifiesAsLgbtq(demographicSurvey.getIdentifiesAsLgbtq());
-              result.setLgbtqIdentity(demographicSurvey.getLgbtqIdentity());
-              result.setRace(demographicSurvey.getRaceEnum());
-              result.setGenderIdentityList(demographicSurvey.getGenderIdentityEnumList());
-              result.setSexAtBirth(demographicSurvey.getSexAtBirthEnum());
-              result.setYearOfBirth(BigDecimal.valueOf(demographicSurvey.getYear_of_birth()));
-
-              return result;
-            }
-          };
-
-  private static final Function<DbAddress, Address> TO_CLIENT_ADDRESS_SURVEY =
-      new Function<DbAddress, Address>() {
-        @Override
-        public Address apply(DbAddress address) {
-          Address result = new Address();
-          if (address != null) {
-            result.setStreetAddress1(address.getStreetAddress1());
-            result.setStreetAddress2(address.getStreetAddress2());
-            result.setCity(address.getCity());
-            result.setState(address.getState());
-            result.setCountry(address.getCountry());
-            result.setZipCode(address.getZipCode());
-            return result;
-          }
-          return result;
-        }
-      };
-
-  private final FreeTierBillingService freeTierBillingService;
   private final ProfileMapper profileMapper;
   private final UserDao userDao;
   private final UserTermsOfServiceDao userTermsOfServiceDao;
@@ -101,13 +35,11 @@ public class ProfileService {
 
   @Autowired
   public ProfileService(
-      FreeTierBillingService freeTierBillingService,
       ProfileMapper profileMapper,
       UserDao userDao,
       UserTermsOfServiceDao userTermsOfServiceDao,
       VerifiedInstitutionalAffiliationDao verifiedInstitutionalAffiliationDao,
       VerifiedInstitutionalAffiliationMapper verifiedInstitutionalAffiliationMapper) {
-    this.freeTierBillingService = freeTierBillingService;
     this.profileMapper = profileMapper;
     this.userDao = userDao;
     this.userTermsOfServiceDao = userTermsOfServiceDao;
@@ -126,10 +58,6 @@ public class ProfileService {
     }
 
     Profile profile = profileMapper.dbUserToProfile(user);
-    profile.setInstitutionalAffiliations(
-        user.getInstitutionalAffiliations().stream()
-            .map(TO_CLIENT_INSTITUTIONAL_AFFILIATION)
-            .collect(Collectors.toList()));
 
     verifiedInstitutionalAffiliationDao
         .findFirstByUser(user)
