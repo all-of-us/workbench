31ff654f434341d5d34d6f761405309e822f00cb
Brian Freeman <brian.d.freeman@gmail.com>
2020-04-06 12:47:46 -0500

    update test

--- api/src/test/java/org/pmiops/workbench/cohortbuilder/mappers/CohortBuilderMapperTest.java
+++ api/src/test/java/org/pmiops/workbench/cohortbuilder/mappers/CohortBuilderMapperTest.java
@@ -18,7 +18,7 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class CriteriaMapperTest {
 
-  @Autowired private CriteriaMapper criteriaMapper;
+  @Autowired private CohortBuilderMapper criteriaMapper;
 
   private DbCriteria dbCriteria;
 
