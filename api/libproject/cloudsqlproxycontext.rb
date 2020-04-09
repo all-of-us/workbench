7b45b4771fd0c6b9040f9401d7e8492bd827707c
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-04-09 11:21:46 -0400

    restore properties file and add status line to sql proxy stuff

--- api/libproject/cloudsqlproxycontext.rb
+++ api/libproject/cloudsqlproxycontext.rb
@@ -7,11 +7,13 @@ class CloudSqlProxyContext < ServiceAccountContext
     # TODO(dmohs): An error here does not cause the main thread to die.
     super do
       @ps = fork do
-        exec(*%W{
+        command = %W{
           cloud_sql_proxy
             -instances #{@project}:us-central1:workbenchmaindb=tcp:0.0.0.0:3307
             -credential_file=#{@path}
-        })
+        }
+        Common.new.status("Forked with command #{command}")
+        exec(*command)
       end
       begin
         sleep 1 # TODO(dmohs): Detect running better.
