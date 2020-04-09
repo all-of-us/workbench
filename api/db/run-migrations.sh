74e7fd57e96f4acb8b3b16b6cf2cff593bd69f12
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-04-09 17:09:21 -0400

    restore script

--- api/db/run-migrations.sh
+++ api/db/run-migrations.sh
@@ -2,9 +2,15 @@
 set -xeuo pipefail
 IFS=$'\n\t'
 
-liquibaseCommand="${1-update}"
-runList="-PrunList=${2-main}"
-liquibaseArgs="${3--PliquibaseCommandValue=}"
+activity="-PrunList=$1"
+if [ -z ${2+x} ]
+then
+    context=""
+else
+    context="-Pcontexts=$2"
+fi
+
+# Ruby is not installed in our dev container and this script is short, so bash is fine.
 
 CREATE_DB_FILE=/tmp/create_db.sql
 
@@ -16,8 +22,7 @@ trap finish EXIT
 envsubst < "$(dirname "${BASH_SOURCE}")/create_db.sql" > $CREATE_DB_FILE
 
 echo "Creating database if it does not exist..."
-# This command is run regardless, and we rely on the commands in CREATE_DB_FILE being idempotent.
 mysql -h ${DB_HOST} --port ${DB_PORT} -u root -p${MYSQL_ROOT_PASSWORD} < ${CREATE_DB_FILE}
 
-echo "Run Liquibase..."
-(cd "$(dirname "${BASH_SOURCE}")" && ../gradlew $liquibaseCommand $runList $liquibaseArgs)
+echo "Upgrading database..."
+(cd "$(dirname "${BASH_SOURCE}")" && ../gradlew update $activity $context)
