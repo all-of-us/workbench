73a9d457aa1b9aa9708723c1d31195b3b5b63b2e
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-04-09 17:06:43 -0400

    rmeove unneeded changes

--- api/libproject/cloudsqlproxycontext.rb
+++ api/libproject/cloudsqlproxycontext.rb
@@ -7,13 +7,11 @@ class CloudSqlProxyContext < ServiceAccountContext
     # TODO(dmohs): An error here does not cause the main thread to die.
     super do
       @ps = fork do
-        command = %W{
+        exec(*%W{
           cloud_sql_proxy
             -instances #{@project}:us-central1:workbenchmaindb=tcp:0.0.0.0:3307
             -credential_file=#{@path}
-        }
-        Common.new.status("Forked with command #{command}")
-        exec(*command)
+        })
       end
       begin
         sleep 1 # TODO(dmohs): Detect running better.
