b5df27767658946436af2b974dd43e69e283866b
Neha Saxena <nsaxena@broadinstitute.org>
2020-04-16 01:02:36 -0400

    Public displayed icon

---
+++ ui/src/app/icons/public-displayed-icon.tsx
@@ -0,0 +1,51 @@
+import {InfoIcon} from 'app/components/icons';
+import {TooltipTrigger} from 'app/components/popups';
+import colors, {colorWithWhiteness} from 'app/styles/colors';
+import * as React from 'react';
+
+const css = `
+  .pointer {
+     position: relative;
+     height: 29px;
+     width: 6.5rem;
+     background-color: ` + colorWithWhiteness(colors.accent, 0.85) + `;
+     line-height:1.2rem;
+   }
+
+  .pointer::after {
+     position: absolute;
+     left: 100%;
+     top:0rem;
+     width:0rem;
+     height:0rem;
+     border-top:0.6rem solid transparent;
+     border-bottom:0.6rem solid transparent;
+  }
+  .pointer:before {
+     content:"";
+     position: absolute;
+     right: 100%;
+     top:0rem;
+     width:0rem;
+     height:0rem;
+     border-top:0.6rem solid transparent;
+     border-right:0.7rem solid ` + colorWithWhiteness(colors.accent, 0.85) + `;
+     border-bottom:0.6rem solid transparent;
+   }
+ `;
+
+export const PublicDisplayed = (props) => {
+  return <div>
+    <style>
+      {css}
+    </style>
+    <div className='pointer' style={{...props.style}}>
+      <label style={{marginLeft: '0.3rem', color: colors.accent}}>
+        Public displayed
+        <TooltipTrigger content='not sure about content'>
+          <InfoIcon style={{marginLeft: '0.5rem', width: '0.7rem'}}/>
+        </TooltipTrigger>
+      </label>
+    </div>
+  </div>;
+};
