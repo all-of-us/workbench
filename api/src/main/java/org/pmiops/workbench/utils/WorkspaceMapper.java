80a5daa4086c8d9e3bd4a67ff2fb1de4e3a6aa3c
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-04-18 14:39:50 -0400

    re-sorted

--- api/src/main/java/org/pmiops/workbench/utils/WorkspaceMapper.java
+++ api/src/main/java/org/pmiops/workbench/utils/WorkspaceMapper.java
@@ -50,12 +50,13 @@ public interface WorkspaceMapper {
   @Mapping(target = "cdrVersionId", source = "dbWorkspace.cdrVersion")
   Workspace toApiWorkspace(DbWorkspace dbWorkspace, FirecloudWorkspace fcWorkspace);
 
-  @Mapping(target = "researchPurpose", source = "dbWorkspace")
+  @Mapping(target = "cdrVersionId", source = "cdrVersion")
+  @Mapping(target = "creator", source = "creator.username")
   @Mapping(target = "etag", source = "version", qualifiedByName = "cdrVersionToEtag")
+  @Mapping(target = "googleBucketName", ignore = true) // available via toApiWorkspace(DbWorkspace dbWorkspace, FirecloudWorkspace fcWorkspace)
   @Mapping(target = "id", source = "firecloudName")
   @Mapping(target = "namespace", source = "workspaceNamespace")
-  @Mapping(target = "creator", source = "creator.username")
-  @Mapping(target = "cdrVersionId", source = "cdrVersion")
+  @Mapping(target = "researchPurpose", source = "dbWorkspace")
   Workspace toApiWorkspace(DbWorkspace dbWorkspace);
 
   @Mapping(target = "workspace", source = "dbWorkspace")
@@ -73,12 +74,13 @@ public interface WorkspaceMapper {
   @Mapping(target = "workspace", source = "dbWorkspace")
   RecentWorkspace toApiRecentWorkspace(DbWorkspace dbWorkspace, WorkspaceAccessLevel accessLevel);
 
-  // I believe the following fields are ignored because they are only meant to be set once
-  // My intent was to keep the same functionality as in the original mapper so I left it in
-  // but we should be handling special business case logic like this in our controller/services
-  @Mapping(target = "approved", ignore = true)
-  @Mapping(target = "reviewRequested", ignore = true)
-  @Mapping(target = "timeRequested", ignore = true)
+  /**
+   * This method was written I think before we realized we could have multiple input arguments.
+   * @deprecated
+   * @param workspace
+   * @param researchPurpose
+   */
+  @Deprecated
   @Mapping(
       target = "specificPopulationsEnum",
       source = "populationDetails",
@@ -92,6 +94,37 @@ public interface WorkspaceMapper {
       target = "researchOutcomeEnumSet",
       source = "researchOutcomeList",
       nullValuePropertyMappingStrategy = SET_TO_DEFAULT)
+
+  // Normally using ignore should be frowned upon. In a merge method
+  // like this one, it's unavoidable; otherwise we'd just make a straight-up translation.
+  // However,
+  @Mapping(target = "approved", ignore = true)
+  @Mapping(target ="billingAccountName", ignore = true)
+  @Mapping(target ="billingAccountType", ignore = true)
+  @Mapping(target ="billingMigrationStatusEnum", ignore = true)
+  @Mapping(target ="billingStatus", ignore = true)
+  @Mapping(target ="cdrVersion", ignore = true)
+  @Mapping(target ="cohorts", ignore = true)
+  @Mapping(target ="conceptSets", ignore = true)
+  @Mapping(target ="creationTime", ignore = true)
+  @Mapping(target ="creator", ignore = true)
+  @Mapping(target ="dataAccessLevel", ignore = true)
+  @Mapping(target ="dataAccessLevelEnum", ignore = true)
+  @Mapping(target ="dataSets", ignore = true)
+  @Mapping(target ="disseminateResearchSet", ignore = true)
+  @Mapping(target ="firecloudName", ignore = true)
+  @Mapping(target ="firecloudUuid", ignore = true)
+  @Mapping(target ="lastAccessedTime", ignore = true)
+  @Mapping(target ="lastModifiedTime", ignore = true)
+  @Mapping(target ="name", ignore = true)
+  @Mapping(target ="published", ignore = true)
+  @Mapping(target ="researchOutcomeSet", ignore = true)
+  @Mapping(target = "reviewRequested", ignore = true)
+  @Mapping(target = "timeRequested", ignore = true)
+  @Mapping(target ="version", ignore = true)
+  @Mapping(target ="workspaceActiveStatusEnum", ignore = true)
+  @Mapping(target ="workspaceId", ignore = true)
+  @Mapping(target ="workspaceNamespace", ignore = true)
   void mergeResearchPurposeIntoWorkspace(
       @MappingTarget DbWorkspace workspace, ResearchPurpose researchPurpose);
 
