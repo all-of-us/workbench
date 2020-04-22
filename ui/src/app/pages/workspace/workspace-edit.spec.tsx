d5721293a36975c69911a89f6608853532f2f86c
Scout Rubenstein <brubenst@broadinstitute.org>
2020-04-22 14:58:58 -0400

    Add back check

--- ui/src/app/pages/workspace/workspace-edit.spec.tsx
+++ ui/src/app/pages/workspace/workspace-edit.spec.tsx
@@ -78,6 +78,9 @@ describe('WorkspaceEdit', () => {
     // Ensure the 'drug development' checkbox is not checked when creating.
     expect(wrapper.find('[data-test-id="researchPurpose-checkbox"]').first().prop('checked'))
       .toEqual(false);
+
+    expect(wrapper.find('[data-test-id="specific-population-no"]').first().prop('checked'))
+      .toEqual(false);
   });
 
   it('displays workspaces duplicate page', async () => {
