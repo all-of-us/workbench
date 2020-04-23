78fd15794e03f503a2b10520a02c2624e8043b56
jmthibault79 <2701406+jmthibault79@users.noreply.github.com>
2020-04-23 12:52:01 -0400

    [RW-4711][risk=no]Missed an 'upgrade' communication (#3455)
    * Move "Provide billing account" behind billing upgrade FF
    * rm "expired" because we don't do free tier by time anymore (RW-4403)

--- ui/src/app/pages/workspace/invalid-billing-banner.tsx
+++ ui/src/app/pages/workspace/invalid-billing-banner.tsx
@@ -20,13 +20,14 @@ export const InvalidBillingBanner = fp.flow(
   withCurrentWorkspace(),
   withUserProfile(),
 )((props: Props) => {
-  const userAction = serverConfigStore.getValue().enableBillingUpgrade ?
+  const {enableBillingUpgrade} = serverConfigStore.getValue();
+  const userAction = enableBillingUpgrade ?
     'Please provide a valid billing account or contact support to extend free credits.' :
     'Please contact support to extend free credits.';
   return <StatusAlertBanner
     onClose={() => props.onClose()}
     title={'This workspace has run out of free credits'}
-    message={'The free credits for the creator of this workspace have run out or expired. ' + userAction}
+    message={'The free credits for the creator of this workspace have run out. ' + userAction}
     footer={
       <div style={{display: 'flex', flexDirection: 'column'}}>
         <Button style={{height: '38px', width: '70%', fontWeight: 400}}
@@ -41,13 +42,12 @@ export const InvalidBillingBanner = fp.flow(
         >
           Request Extension
         </Button>
-        <a style={{marginTop: '.5rem', marginLeft: '.2rem'}}
-           onClick={() => {
-             navigate(['workspaces', props.workspace.namespace, props.workspace.id, 'edit']);
-           }}
-        >
-          Provide billing account
-        </a>
+        {enableBillingUpgrade && <a style={{marginTop: '.5rem', marginLeft: '.2rem'}}
+                                    onClick={() => {
+                                      navigate(['workspaces', props.workspace.namespace, props.workspace.id, 'edit']);
+                                    }}>
+            Provide billing account
+        </a>}
       </div>
     }
   />;
