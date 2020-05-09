1c56871e224504b39e69d5ecec4be964983c3065
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-05-08 21:53:24 -0400

    spotless

--- api/src/main/java/org/pmiops/workbench/utils/mappers/MapStructConfig.java
+++ api/src/main/java/org/pmiops/workbench/utils/mappers/MapStructConfig.java
@@ -4,6 +4,4 @@ import org.mapstruct.MapperConfig;
 import org.mapstruct.ReportingPolicy;
 
 @MapperConfig(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.ERROR)
-public class MapStructConfig {
-
-}
+public class MapStructConfig {}
