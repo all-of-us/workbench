bbbe7517d758496efe58ce6e67fdc99bdd4ac0a7
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-05-11 22:13:16 -0400

    fixes & add groups and notification channels

--- ops/ruby/devops-toolkit/tasks/monitoring_assets.rb
+++ ops/ruby/devops-toolkit/tasks/monitoring_assets.rb
@@ -25,22 +25,24 @@ class MonitoringAssets
       counts  = {}
       # pp env_bundle
       # exit
-      resources = env_bundle[:metric_client].list_monitored_resource_descriptors(env_bundle[:metric_project_path])
+      metric_client = env_bundle[:metric_client]
+
+      resources = metric_client.list_monitored_resource_descriptors(env_bundle[:path])
       logger.info("found #{resources.count} monitored resources")
       counts['monitored_resources'] = resources.count
 
-      all_metrics = env_bundle[:metric_client].list_metric_descriptors(env_bundle[:metric_project_path])
+      all_metrics = metric_client.list_metric_descriptors(env_bundle[:path])
       counts['metric_descriptors'] =  all_metrics.count
       logger.info("found #{all_metrics.count} metric descriptors")
 
-      custom_metrics = env_bundle[:metric_client].list_metric_descriptors(env_bundle[:metric_project_path], {filter: CUSTOM_METRIC_FILTER})
+      custom_metrics = metric_client.list_metric_descriptors(env_bundle[:path], {filter: CUSTOM_METRIC_FILTER})
       logger.info("found  #{custom_metrics.count} custom metrics")
       counts['custom_metrics'] = custom_metrics.count
       custom_metrics.each do |metric|
         logger.info("\t#{metric.name}: #{metric.description}")
       end
 
-      user_logs_based_metrics = metric_client.list_metric_descriptors(env_bundle[:metric_project_path], {filter: USER_LOGS_BASED_METRIC_FILTER})
+      user_logs_based_metrics = env_bundle[:metric_client].list_metric_descriptors(env_bundle[:path], {filter: USER_LOGS_BASED_METRIC_FILTER})
       counts['user_logs_based_metrics'] = user_logs_based_metrics.count
       logger.info("found  #{user_logs_based_metrics.count} user-defined  logs-based metrics")
 
@@ -48,11 +50,10 @@ class MonitoringAssets
         logger.info("\t#{metric.name}: #{metric.description}")
       end
 
-      total_logs_based_metrics = metric_client.list_metric_descriptors(env_bundle[:metric_project_path], {filter: LOGS_BASED_METRIC_FILTER})
+      total_logs_based_metrics = metric_client.list_metric_descriptors(env_bundle[:path], {filter: LOGS_BASED_METRIC_FILTER})
       logger.info("found  #{total_logs_based_metrics.count} total logs-based metrics")
 
-      # alerts_client = Google::Cloud::Monitoring::V3::AlertPolicyServiceClient.new
-      policies = alerts_client.list_alert_policies(current_env.formatted_project_name)
+      policies = env_bundle[:alerts_client].list_alert_policies(env_bundle[:current_env].formatted_project_name)
       logger.info("found #{policies.count} alerting policies")
       counts['policies'] = policies.count
 
@@ -60,17 +61,19 @@ class MonitoringAssets
         logger.info("\t#{policy.display_name}, #{policy.name}, #{policy.conditions.count} conditions")
       end
 
-      logger.info("Total counts for #{current_env.short_name}: #{counts}")
+      logger.info("Total counts for #{env_bundle[:current_env].short_name}: #{counts}")
     end
   end
 
   def backup_config
     visit_envs do |env_bundle|
-      # make_env_output_dir(env_bundle[:current_env].short_name)
+      backup_notification_channels(env_bundle)
 
-      backup_alert_policies(env_bundle)
-
+      # backup_group_members(env_bundle)
+      # backup_alert_policies(env_bundle)
       # backup_monitored_resources(env_bundle)
+      # backup_all_metrics(env_bundle)
+      # backup_notification_channels(env_bundle)
     end
   end
 
@@ -78,31 +81,46 @@ class MonitoringAssets
 
   def backup_alert_policies(env_bundle)
     policies = env_bundle[:alerts_client].list_alert_policies(env_bundle[:path])
-    logger.info("found #{policies.count} alerting policies in #{env_bundle[:current_env].short_name}")
-
-    policies.each do |policy|
-      # path = make_output_path(env_bundle[:current_env].short_name, 'policies')
-      path = make_output_path(policy)
-      file_name = DateTime.now.strftime("%FT%T").gsub(':', '-') + '.json'
-      full_path = File.join(path, file_name)
-      logger.info("Writing #{policy.display_name} to #{full_path}")
-      IO.write(full_path, policy.to_json)
-    end
+    backup_assets(policies)
   end
 
   def backup_monitored_resources(env_bundle)
     resources = env_bundle[:metric_client].list_monitored_resource_descriptors(env_bundle[:path])
-    logger.info("found #{resources.count} monitored resources policies in #{env_bundle[:current_env].short_name}")
-
-    resources.each do |resource|
-      path = make_output_path(env_bundle[:current_env].short_name, 'monitored_resources')
-      logger.info("Writing #{resource.display_name} to #{path}")
-      IO.write(path, resource.to_json)
-    end
+    backup_assets(resources)
   end
 
-  # TODO(jaycarlton): make this reusable outside this class
-  # Thin wrapper around the visitor so we don't have to pass the env around
+  def backup_all_metrics(env_bundle)
+    metrics = env_bundle[:metric_client].list_metric_descriptors(env_bundle[:path])
+    backup_assets(metrics)
+  end
+
+  def backup_group_members(env_bundle)
+    members = env_bundle[:group_client].list_groups(env_bundle[:path])
+    backup_assets(members)
+  end
+
+  def backup_notification_channels(env_bundle)
+    channels = env_bundle[:notification_channel_client].list_notification_channels(env_bundle[:path])
+    backup_assets(channels)
+  end
+
+  def backup_assets(assets)
+    assets.each(&method(:backup_asset))
+  end
+
+  # Backup the cloud asset to a file in a path based on its fully
+  # qualified name and a filename from the based on the current time.
+  def backup_asset(asset)
+    full_path = File.join(make_output_path(asset), make_file_name)
+    logger.info("Writing #{asset.display_name} to #{full_path}")
+    IO.write(full_path, asset.to_json)
+  end
+
+  def make_file_name
+    DateTime.now.strftime("%FT%T").gsub(':', '-') + '.json'
+  end
+
+  # Private visitor that makes the distracting client initializations for each environment.
   def visit_envs
     visitor.visit do |env|
       env_bundle = {}
@@ -110,26 +128,14 @@ class MonitoringAssets
       env_bundle[:metric_client] = Google::Cloud::Monitoring::Metric.new
       env_bundle[:path] = Google::Cloud::Monitoring::V3::MetricServiceClient.project_path(env.project_id)
       env_bundle[:alerts_client] = Google::Cloud::Monitoring::V3::AlertPolicyServiceClient.new
+      env_bundle[:group_client] = Google::Cloud::Monitoring::V3::GroupServiceClient.new
+      env_bundle[:notification_channel_client] = Google::Cloud::Monitoring::V3::NotificationChannelServiceClient.new
       yield env_bundle
     end
   end
 
-  # def make_output_path(env_short_name, category)
-  #   env_output_dir = File.join(output_dir, env_short_name)
-  #   File.join(env_output_dir, "#{category}.json")
-  # end
-
-  # take a name like projects/all-of-us-rw-prod/alertPolicies/6212216586218347852 and make a dir out of it
-  #
   def make_output_path(asset)
     FileUtils.mkdir_p(File.join(output_dir, asset.name))
   end
-  #
-  # def make_env_output_dir(env_short_name)
-  #   env_output_dir = File.join(output_dir, env_short_name)
-  #   Dir.mkdir(env_output_dir) unless Dir.exist?(env_output_dir)
-  #   env_output_dir
-  # end
-
 end
 
