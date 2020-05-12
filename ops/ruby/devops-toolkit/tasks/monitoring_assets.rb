6ff3c7157e8d38e17a926f1f937f2f02a7ed4056
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-05-12 10:59:33 -0400

    fix yaml

--- ops/ruby/devops-toolkit/tasks/monitoring_assets.rb
+++ ops/ruby/devops-toolkit/tasks/monitoring_assets.rb
@@ -1,6 +1,7 @@
 require "google/cloud/monitoring"
+require "google/cloud/trace"
 require 'logger'
-require 'jq'
+require 'json'
 
 require_relative "./lib/service_account_manager"
 require_relative './lib/gcp_environment_visitor'
@@ -8,15 +9,18 @@ require_relative './lib/gcp_environment_visitor'
 CUSTOM_METRIC_FILTER = "metric.type = starts_with(\"custom.googleapis.com/\")"
 LOGS_BASED_METRIC_FILTER = "metric.type = starts_with(\"logging.googleapis.com/\")"
 USER_LOGS_BASED_METRIC_FILTER = "metric.type = starts_with(\"logging.googleapis.com/user/\")"
+TRACE_LIST_INTERVAL_SECONDS = 1800
+FILE_SUFFIXES = {:yaml => '.yaml', :json => '.json'}
 
 class MonitoringAssets
-  def initialize(envs_path, output_dir='.', logger = Logger.new(STDOUT))
+  def initialize(envs_path, output_dir='.', logger = Logger.new(STDOUT), output_format = :yaml)
     @visitor = GcpEnvironmentVisitor.new(envs_path, logger)
     @output_dir = output_dir
     @logger = logger
+    @output_format = output_format
   end
 
-  attr_accessor(:current_env, :metric_client, :metric_project_path, :alerts_client)
+  attr_accessor(:current_env, :metric_client, :project_path, :alert_client)
   attr_reader(:visitor, :logger, :output_dir)
 
   # Demonstrate a simple usage of the AouEnvironmentVisitor with a few read-only operations
@@ -26,24 +30,24 @@ class MonitoringAssets
       counts  = {}
       # pp env_bundle
       # exit
-      metric_client = env_bundle[:metric_client]
+      metric_client = @metric_client
 
-      resources = metric_client.list_monitored_resource_descriptors(env_bundle[:path])
+      resources = metric_client.list_monitored_resource_descriptors(@project_path)
       logger.info("found #{resources.count} monitored resources")
       counts['monitored_resources'] = resources.count
 
-      all_metrics = metric_client.list_metric_descriptors(env_bundle[:path])
+      all_metrics = metric_client.list_metric_descriptors(@project_path)
       counts['metric_descriptors'] =  all_metrics.count
       logger.info("found #{all_metrics.count} metric descriptors")
 
-      custom_metrics = metric_client.list_metric_descriptors(env_bundle[:path], {filter: CUSTOM_METRIC_FILTER})
+      custom_metrics = metric_client.list_metric_descriptors(@project_path, {filter: CUSTOM_METRIC_FILTER})
       logger.info("found  #{custom_metrics.count} custom metrics")
       counts['custom_metrics'] = custom_metrics.count
       custom_metrics.each do |metric|
         logger.info("\t#{metric.name}: #{metric.description}")
       end
 
-      user_logs_based_metrics = env_bundle[:metric_client].list_metric_descriptors(env_bundle[:path], {filter: USER_LOGS_BASED_METRIC_FILTER})
+      user_logs_based_metrics = @metric_client.list_metric_descriptors(@project_path, {filter: USER_LOGS_BASED_METRIC_FILTER})
       counts['user_logs_based_metrics'] = user_logs_based_metrics.count
       logger.info("found  #{user_logs_based_metrics.count} user-defined  logs-based metrics")
 
@@ -51,10 +55,10 @@ class MonitoringAssets
         logger.info("\t#{metric.name}: #{metric.description}")
       end
 
-      total_logs_based_metrics = metric_client.list_metric_descriptors(env_bundle[:path], {filter: LOGS_BASED_METRIC_FILTER})
+      total_logs_based_metrics = metric_client.list_metric_descriptors(@project_path, {filter: LOGS_BASED_METRIC_FILTER})
       logger.info("found  #{total_logs_based_metrics.count} total logs-based metrics")
 
-      policies = env_bundle[:alerts_client].list_alert_policies(env_bundle[:current_env].formatted_project_name)
+      policies = env_bundle[:alert_client].list_alert_policies(env_bundle[:current_env].formatted_project_name)
       logger.info("found #{policies.count} alerting policies")
       counts['policies'] = policies.count
 
@@ -66,44 +70,67 @@ class MonitoringAssets
     end
   end
 
+  DO_BACKUP_ALL = false # TODO(jaycarlton): make this an option
+
+  # For every environment in the envs_file, fetch all items of interest from the appropriate
+  # Stackdriver APIs. (Note that in future we may wish to broaden this to other )
   def backup_config
     visit_envs do |env_bundle|
-      backup_alert_policies(env_bundle)
-      backup_all_metrics(env_bundle)
-      backup_group_members(env_bundle)
-      backup_monitored_resources(env_bundle)
-      backup_notification_channels(env_bundle)
-      backup_notification_channels(env_bundle)
+      backup_alert_policies
+      if DO_BACKUP_ALL
+        backup_all_metrics
+      else
+        backup_custom_metrics
+        backup_user_logs_based_metrics
+      end
+      backup_group_members
+      backup_monitored_resources
+      backup_notification_channels
+      backup_notification_channels
     end
   end
 
   private
 
-  def backup_alert_policies(env_bundle)
-    policies = env_bundle[:alerts_client].list_alert_policies(env_bundle[:path])
+  def backup_alert_policies
+    policies = @alert_client.list_alert_policies(@project_path)
     backup_assets(policies)
   end
 
-  def backup_monitored_resources(env_bundle)
-    resources = env_bundle[:metric_client].list_monitored_resource_descriptors(env_bundle[:path])
+  def backup_custom_metrics
+    custom_metrics = @metric_client.list_metric_descriptors(@project_path, {filter: CUSTOM_METRIC_FILTER})
+    backup_assets(custom_metrics)
+  end
+
+  def backup_user_logs_based_metrics
+    user_logs_based_metrics = @metric_client.list_metric_descriptors(@project_path, {filter: USER_LOGS_BASED_METRIC_FILTER})
+    backup_assets(user_logs_based_metrics)
+  end
+
+  def backup_monitored_resources
+    resources = @metric_client.list_monitored_resource_descriptors(@project_path)
     backup_assets(resources)
   end
 
-  def backup_all_metrics(env_bundle)
-    metrics = env_bundle[:metric_client].list_metric_descriptors(env_bundle[:path])
+  # Grab all the metrics, including very many defined by Google and partners. This method is here
+  # more for completeness and to allow introspection/navigation of them than anything else, as we
+  # can't write to these to restore them.
+  def backup_all_metrics
+    metrics = @metric_client.list_metric_descriptors(@project_path)
     backup_assets(metrics)
   end
 
-  def backup_group_members(env_bundle)
-    members = env_bundle[:group_client].list_groups(env_bundle[:path])
+  def backup_group_members
+    members = @group_client.list_groups(@project_path)
     backup_assets(members)
   end
 
-  def backup_notification_channels(env_bundle)
-    channels = env_bundle[:notification_channel_client].list_notification_channels(env_bundle[:path])
+  def backup_notification_channels
+    channels = @notification_channel_client.list_notification_channels(@project_path)
     backup_assets(channels)
   end
 
+  # Backup a collection of Stackdriver assets
   def backup_assets(assets)
     assets.each(&method(:backup_asset))
   end
@@ -113,28 +140,58 @@ class MonitoringAssets
   def backup_asset(asset)
     full_path = File.join(make_output_path(asset), make_file_name)
     logger.info("Writing #{asset.display_name} to #{full_path}")
-    json = JSON.pretty_generate(asset.to_h)
-    IO.write(full_path, json)
+    text = serialize(asset)
+    IO.write(full_path, text)
   end
 
-  def make_file_name
-    DateTime.now.strftime("%FT%T").gsub(':', '-') + '.json'
-  end
-
-  # Private visitor that makes the distracting client initializations for each environment.
-  def visit_envs
-    visitor.visit do |env|
-      env_bundle = {}
-      env_bundle[:current_env] = env
-      env_bundle[:metric_client] = Google::Cloud::Monitoring::Metric.new
-      env_bundle[:path] = Google::Cloud::Monitoring::V3::MetricServiceClient.project_path(env.project_id)
-      env_bundle[:alerts_client] = Google::Cloud::Monitoring::V3::AlertPolicyServiceClient.new
-      env_bundle[:group_client] = Google::Cloud::Monitoring::V3::GroupServiceClient.new
-      env_bundle[:notification_channel_client] = Google::Cloud::Monitoring::V3::NotificationChannelServiceClient.new
-      yield env_bundle
+  # Serialize the Stackdriver object instance to a persistent format such as YAML or JSON
+  def serialize(asset)
+    hash = asset.to_h
+    case @output_format
+    when :yaml
+      return hash.to_yaml
+    when :json
+      return JSON.pretty_generate(hash)
+    else
+      @logger.WARN("Unrecognized output format '#{@output_format}'")
+      return asset.to_s
     end
   end
 
+  # Generate a file name based on the current 24-hour time. This will
+  # sort such that later times are later alphabetically.
+  def make_file_name
+    DateTime.now.strftime("%FT%T").gsub(':', '-') + FILE_SUFFIXES[@output_format]
+  end
+
+  # Private visitor that makes the distracting client initializations for each environment. The
+  # client constructors take project details from the global scope.
+  # TODO(jaycarlton): explicitly provide constructor parameters for the clients so refactoring
+  # becomes easier
+  def visit_envs
+    visitor.visit do |env|
+      @current_env = env
+      @metric_client = Google::Cloud::Monitoring::Metric.new
+      @project_path = Google::Cloud::Monitoring::V3::MetricServiceClient.project_path(@current_env.project_id)
+      @alert_client = Google::Cloud::Monitoring::V3::AlertPolicyServiceClient.new
+      @group_client = Google::Cloud::Monitoring::V3::GroupServiceClient.new
+      @notification_channel_client = Google::Cloud::Monitoring::V3::NotificationChannelServiceClient.new
+      yield env
+    end
+
+    # These shouldn't work anymore anyway, but tidy them up just in case somebody has code after a
+    # visit_envs block
+    @current_env = nil
+    @metric_client = nil
+    @project_path = nil
+    @alert_client = nil
+    @group_client = nil
+    @notification_channel_client = nil
+  end
+
+  # Build an otput directory hierarchy from the asset's name. This generally looks like
+  # "projects/$project_name/$asset_type/$asset_descriptor". In the case of metric descriptors
+  # there's also an intermediate level for the metric prefix, such as custom.googleapis.com
   def make_output_path(asset)
     FileUtils.mkdir_p(File.join(output_dir, asset.name))
   end
