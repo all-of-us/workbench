b784b4feb2da65f4c6e9aaa927d2d2fb8898a1a3
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-05-12 18:41:36 -0400

    fixes

--- ops/bash/create-dashboard.sh
+++ ops/bash/create-dashboard.sh
@@ -1,6 +1,7 @@
 #!/bin/bash
 
-curl -X POST "https://monitoring.googleapis.com/v1/projects/$(gcloud config get-value project)/dashboards" \
+# Create a dashboard using the JSON filee given in $1 in the project ID $2
+curl -X POST "https://monitoring.googleapis.com/v1/projects/$2/dashboards" \
   -d @$1 \
   --header "Authorization: Bearer $(gcloud auth print-access-token)" \
   --header "Content-Type: application/json"
