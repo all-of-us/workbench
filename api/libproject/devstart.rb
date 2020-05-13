c8244b508d3a82adfc14aeec34c6a36c166525de
CH Albach <calbach@google.com>
2020-05-13 16:32:04 -0700

    PR changes

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -2213,7 +2213,7 @@ Common.register_command({
   :fn => ->(*args) { deploy_api("deploy-api", args) }
 })
 
-def create_workbench_db()
+def create_or_update_workbench_db()
   run_with_redirects(
     "cat db/create_db.sql | envsubst | " \
     "mysql -u \"root\" -p\"#{ENV["MYSQL_ROOT_PASSWORD"]}\" --host 127.0.0.1 --port 3307",
@@ -2221,10 +2221,10 @@ def create_workbench_db()
   )
 end
 
-def create_or_update_workbench_db(cmd_name, args)
+def create_or_update_workbench_db_cmd(cmd_name, args)
   ensure_docker cmd_name, args
   with_cloud_proxy_and_db_env(cmd_name, args) do |ctx|
-    create_workbench_db
+    create_or_update_workbench_db
   end
 end
 
@@ -2237,7 +2237,7 @@ Common.register_command({
                   "migrations as these changes may require root, be necessary " \
                   "to bootstrap Liquibase, or require sensitive variables such " \
                   "as passwords which are unavailable to Liquibase.",
-  :fn => ->(*args) { create_or_update_workbench_db("create-or-update-workbench-db", args) }
+  :fn => ->(*args) { create_or_update_workbench_db_cmd("create-or-update-workbench-db", args) }
 })
 
 def migrate_database(dry_run = false)
@@ -2540,7 +2540,7 @@ def setup_project_data(gcc, cdr_db_name)
 
 
     common.status "Setting up databases and users..."
-    create_workbench_db
+    create_or_update_workbench_db
 
     common.status "Running schema migrations..."
     migrate_database
