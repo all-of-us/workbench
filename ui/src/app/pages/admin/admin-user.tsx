5a409c6625116d8a1f6410e5d3748f32e5a09347
Ariel Schwartz <aschwart@broadinstitute.org>
2020-05-14 16:59:09 -0400

    partially styled first row

--- ui/src/app/pages/admin/admin-user.tsx
+++ ui/src/app/pages/admin/admin-user.tsx
@@ -2,8 +2,13 @@ import {Component} from '@angular/core';
 import * as React from 'react';
 import {Profile} from 'generated/fetch';
 
+import {FlexColumn, FlexRow} from 'app/components/flex';
+import {SmallHeader} from 'app/components/headers';
+import {ClrIcon} from 'app/components/icons';
+import colors, {colorWithWhiteness} from 'app/styles/colors';
 import {withUserProfile} from 'app/utils';
 import {ReactWrapperBase} from 'app/utils';
+import {FadeBox} from "app/components/containers";
 
 const AdminUser = withUserProfile()(class extends React.Component<{
   profileState: {profile: Profile, reload: Function, updateCache: Function}
@@ -13,7 +18,23 @@ const AdminUser = withUserProfile()(class extends React.Component<{
   }
 
   render() {
-    return <p>lol</p>
+    return <FadeBox style={{margin: 'auto', paddingTop: '1rem', width: '96.25%', minWidth: '1232px'}}>
+      <FlexColumn>
+        <FlexRow style={{alignItems: 'center'}}>
+          <div>
+            <ClrIcon shape='arrow' size={37} style={{
+              backgroundColor: colorWithWhiteness(colors.accent, .85),
+              color: colors.accent,
+              borderRadius: '18px',
+              transform: 'rotate(270deg)'
+            }}/>
+          </div>
+          <SmallHeader>
+            User Profile Information
+          </SmallHeader>
+        </FlexRow>
+      </FlexColumn>
+    </FadeBox>
   }
 });
 
