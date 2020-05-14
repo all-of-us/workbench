49573a0232942d38b7412939e78940ebfab0db6b
Joel Thibault <thibault@broad.dev>
2020-05-14 10:33:18 -0400

    refactoring

--- api/tools/src/main/java/org/pmiops/workbench/tools/LoadInstitutions.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/LoadInstitutions.java
@@ -3,7 +3,7 @@ package org.pmiops.workbench.tools;
 import com.fasterxml.jackson.databind.ObjectMapper;
 import java.io.BufferedReader;
 import java.io.FileReader;
-import java.util.Optional;
+import java.io.IOException;
 import java.util.logging.Logger;
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.DefaultParser;
@@ -32,56 +32,46 @@ import org.springframework.transaction.annotation.EnableTransactionManagement;
 public class LoadInstitutions {
 
   private static final Logger log = Logger.getLogger(LoadInstitutions.class.getName());
+  private static final ObjectMapper mapper = new ObjectMapper();
 
-  private static Option importFilename =
+  private static final Option importFilename =
       Option.builder()
           .longOpt("import-filename")
           .desc("File containing JSON for institutions to save")
           .required()
           .hasArg()
           .build();
-  private static Option dryRunOpt =
+  private static final Option dryRunOpt =
       Option.builder()
           .longOpt("dry-run")
           .desc("If specified, the tool runs in dry run mode; no modifications are made")
           .build();
 
-  private static Options options = new Options().addOption(importFilename).addOption(dryRunOpt);
+  private static final Options options =
+      new Options().addOption(importFilename).addOption(dryRunOpt);
 
   @Bean
   public CommandLineRunner run(InstitutionService institutionService) {
     return (args) -> {
-      CommandLine opts = new DefaultParser().parse(options, args);
+      final CommandLine opts = new DefaultParser().parse(options, args);
       boolean dryRun = opts.hasOption(dryRunOpt.getLongOpt());
 
-      try (BufferedReader reader =
-          new BufferedReader(new FileReader(opts.getOptionValue(importFilename.getLongOpt())))) {
-        ObjectMapper mapper = new ObjectMapper();
-        Institution[] institutions = mapper.readValue(reader, Institution[].class);
-
-        for (Institution institution : institutions) {
-          Optional<Institution> fetchedInstitutionMaybe =
-              institutionService.getInstitution(institution.getShortName());
-          if (fetchedInstitutionMaybe.isPresent()) {
-            if (fetchedInstitutionMaybe.get().equals(institution)) {
-              log.info("Skipping... Entry already exists for " + institution.getShortName());
-            } else {
-              if (!dryRun) {
-                institutionService.updateInstitution(institution.getShortName(), institution);
-              }
-              dryLog(dryRun, "Updated " + institution.toString());
-            }
-
-            continue;
-          }
-
-          if (!dryRun) {
-            institutionService.createInstitution(institution);
-          }
-          dryLog(dryRun, "Saved " + institution.toString());
+      for (final Institution institution : read(opts.getOptionValue(importFilename.getLongOpt()))) {
+        if (!dryRun) {
+          institutionService
+              .updateInstitution(institution.getShortName(), institution)
+              // here I mean "ifNotPresent()"
+              .orElseGet(() -> institutionService.createInstitution(institution));
         }
+        dryLog(dryRun, "Updated or Created " + institution.toString());
       }
     };
+  }
+
+  private static Institution[] read(final String filename) throws IOException {
+    try (final BufferedReader reader = new BufferedReader(new FileReader(filename))) {
+      return mapper.readValue(reader, Institution[].class);
+    }
   }
 
   private static void dryLog(boolean dryRun, String msg) {
