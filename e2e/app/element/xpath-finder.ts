7b979befcd3ab136e8cb5313139f882734ada152
Pete Santos <psantos@broadinstitute.org>
2020-05-18 17:16:11 -0400

    revert workbench config change, update xpath

--- e2e/app/element/xpath-finder.ts
+++ e2e/app/element/xpath-finder.ts
@@ -147,7 +147,7 @@ export async function findImage(page: Page, label: string, waitOptions?: WaitFor
 }
 
 export async function findIframe(page: Page, label: string): Promise<Frame> {
-  const iframeNode = await page.waitForXPath(`//body[@id='body']//*[contains(@aria-label, '${label}')]//iframe`)
+  const iframeNode = await page.waitForXPath(xpathDefaults.iframeXpath)
   const srcHandle = await iframeNode.getProperty('src')
   const src = await srcHandle.jsonValue()
   const hasFrame = (): Frame => page.frames().find(frame => frame.url() === src)
