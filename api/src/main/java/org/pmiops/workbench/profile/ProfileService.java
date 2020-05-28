3819ffda201293a8631851b0ea0822ada0b28e65
Pete Santos <psantos@broadinstitute.org>
2020-05-28 14:26:54 -0400

    Pairing on ProfileService for validate institution

--- api/src/main/java/org/pmiops/workbench/profile/ProfileService.java
+++ api/src/main/java/org/pmiops/workbench/profile/ProfileService.java
@@ -2,13 +2,16 @@ package org.pmiops.workbench.profile;
 
 import java.util.Optional;
 import org.pmiops.workbench.billing.FreeTierBillingService;
+import org.pmiops.workbench.db.dao.InstitutionDao;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.dao.UserService;
 import org.pmiops.workbench.db.dao.UserTermsOfServiceDao;
 import org.pmiops.workbench.db.dao.VerifiedInstitutionalAffiliationDao;
+import org.pmiops.workbench.db.model.DbInstitution;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbUserTermsOfService;
 import org.pmiops.workbench.institution.VerifiedInstitutionalAffiliationMapper;
+import org.pmiops.workbench.model.InstitutionalRole;
 import org.pmiops.workbench.model.Profile;
 import org.pmiops.workbench.model.VerifiedInstitutionalAffiliation;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -23,10 +26,12 @@ public class ProfileService {
   private final UserTermsOfServiceDao userTermsOfServiceDao;
   private final VerifiedInstitutionalAffiliationDao verifiedInstitutionalAffiliationDao;
   private final VerifiedInstitutionalAffiliationMapper verifiedInstitutionalAffiliationMapper;
+  private final InstitutionDao institutionDao;
 
   @Autowired
   public ProfileService(
       FreeTierBillingService freeTierBillingService,
+      InstitutionDao institutionDao,
       ProfileMapper profileMapper,
       UserDao userDao,
       UserService userService,
@@ -34,6 +39,7 @@ public class ProfileService {
       VerifiedInstitutionalAffiliationDao verifiedInstitutionalAffiliationDao,
       VerifiedInstitutionalAffiliationMapper verifiedInstitutionalAffiliationMapper) {
     this.freeTierBillingService = freeTierBillingService;
+    this.institutionDao = institutionDao;
     this.profileMapper = profileMapper;
     this.userDao = userDao;
     this.userService = userService;
@@ -74,5 +80,33 @@ public class ProfileService {
     }
 
     return profile;
+  }
+
+  public boolean validateVerifiedInstitution(
+      VerifiedInstitutionalAffiliation verifiedInstitutionalAffiliation) {
+    // inst short name needs to match table in DB
+    // disp name needs to lineup with inst short name
+    // inst role enum needs to exist
+    // inst role other text - if role is other - needs to exist
+    Optional<DbInstitution> institution =
+        institutionDao.findOneByShortName(
+            verifiedInstitutionalAffiliation.getInstitutionShortName());
+    if (!institution.isPresent()
+        || !institution
+            .get()
+            .getDisplayName()
+            .equals(verifiedInstitutionalAffiliation.getInstitutionDisplayName())
+        || verifiedInstitutionalAffiliation.getInstitutionalRoleEnum().equals(null)
+        || (verifiedInstitutionalAffiliation
+                .getInstitutionalRoleEnum()
+                .equals(InstitutionalRole.OTHER)
+            && (verifiedInstitutionalAffiliation.getInstitutionalRoleOtherText().equals("")
+                || verifiedInstitutionalAffiliation
+                    .getInstitutionalRoleOtherText()
+                    .equals(null)))) {
+      return false;
+    } else {
+      return true;
+    }
   }
 }
