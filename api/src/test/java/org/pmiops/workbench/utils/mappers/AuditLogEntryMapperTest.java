89b75ce6f0f25620ed37cbba6cc6a440fe3ac266
Jay Carlton <53479492+jaycarlton@users.noreply.github.com>
2020-06-03 16:30:52 -0400

    new tests

--- api/src/test/java/org/pmiops/workbench/utils/mappers/AuditLogEntryMapperTest.java
+++ api/src/test/java/org/pmiops/workbench/utils/mappers/AuditLogEntryMapperTest.java
@@ -2,13 +2,14 @@ package org.pmiops.workbench.utils.mappers;
 
 import static com.google.common.truth.Truth.assertThat;
 import static com.google.common.truth.Truth8.assertThat;
-import static org.pmiops.workbench.actionaudit.auditors.ActionAuditTestConfig.ACTION_ID;
 
 import com.google.common.collect.ImmutableList;
+import java.util.List;
 import java.util.Optional;
 import org.joda.time.DateTime;
 import org.junit.Test;
 import org.junit.runner.RunWith;
+import org.pmiops.workbench.exceptions.NotFoundException;
 import org.pmiops.workbench.model.AuditAction;
 import org.pmiops.workbench.model.AuditAgent;
 import org.pmiops.workbench.model.AuditEventBundle;
@@ -23,18 +24,25 @@ import org.springframework.test.context.junit4.SpringRunner;
 @RunWith(SpringRunner.class)
 public class AuditLogEntryMapperTest {
 
-  public static final long AGENT_ID = 101L;
-  public static final String USER_AGENT_TYPE = "USER";
-  public static final String AGENT_USERNAME = "paul@beatles.info";
-  public static final long TARGET_ID = 202L;
-  public static final String ACTION_TYPE_DELETE = "DELETE";
-  public static final String TARGET_TYPE_WORKSPACE = "WORKSPACE";
-  public static final String TARGET_PROPERTY = "title";
-  public static final String PREVIOUS_VALUE = "District 5";
-  public static final String NEW_VALUE = "The Mighty Ducks";
-  public static final String LOGIN_ACTION_TYPE = "LOGIN";
-  public static final DateTime EVENT_TIME = new DateTime(1579013840545L);
-  public static final String WORKBENCH_TARGET_TYPE = "WORKBENCH";
+  private static final DateTime EVENT_TIME_1 = DateTime.parse("2010-06-30T01:20+02:00");
+  private static final DateTime EVENT_TIME_2 = DateTime.parse("2015-06-30T01:20+02:00");
+  private static final int TIME_TOLERANCE_MILLIS = 100;
+  private static final long AGENT_ID = 101L;
+  private static final long TARGET_ID = 202L;
+  private static final String ACTION_ID_1 = "11111111-1111-1111-1111-111111111111";
+  private static final String ACTION_ID_2 = "22222222-2222-2222-2222-222222222222";
+  private static final String ACTION_TYPE_CREATE = "CREATE";
+  private static final String ACTION_TYPE_DELETE = "DELETE";
+  private static final String AGENT_USERNAME = "paul@beatles.info";
+  private static final String ETAG = "\"101\"";
+  private static final String ETAG_TARGET_PROPERTY = "etag";
+  private static final String LOGIN_ACTION_TYPE = "LOGIN";
+  private static final String NEW_VALUE = "The Mighty Ducks";
+  private static final String PREVIOUS_VALUE = "District 5";
+  private static final String TARGET_PROPERTY = "title";
+  private static final String USER_AGENT_TYPE = "USER";
+  private static final String WORKBENCH_TARGET_TYPE = "WORKBENCH";
+  private static final String WORKSPACE_TARGET_TYPE = "WORKSPACE";
 
   @Autowired private AuditLogEntryMapper auditLogEntryMapper;
 
@@ -65,14 +73,14 @@ public class AuditLogEntryMapperTest {
             .agentType(USER_AGENT_TYPE)
             .agentUsername(AGENT_USERNAME)
             .targetId(TARGET_ID)
-            .targetType(TARGET_TYPE_WORKSPACE);
+            .targetType(WORKSPACE_TARGET_TYPE);
     final AuditEventBundleHeader header = auditLogEntryMapper.logEntryToEventBundleHeader(logEntry);
     assertThat(header.getActionType()).isEqualTo(ACTION_TYPE_DELETE);
     assertThat(header.getAgent().getAgentUsername()).isEqualTo(AGENT_USERNAME);
     assertThat(header.getAgent().getAgentType()).isEqualTo(USER_AGENT_TYPE);
     assertThat(header.getAgent().getAgentId()).isEqualTo(AGENT_ID);
     assertThat(header.getTarget().getTargetId()).isEqualTo(TARGET_ID);
-    assertThat(header.getTarget().getTargetType()).isEqualTo(TARGET_TYPE_WORKSPACE);
+    assertThat(header.getTarget().getTargetType()).isEqualTo(WORKSPACE_TARGET_TYPE);
   }
 
   @Test
@@ -134,19 +142,19 @@ public class AuditLogEntryMapperTest {
         auditLogEntryMapper.buildAuditAction(
             ImmutableList.of(
                 new AuditLogEntry()
-                    .actionId(ACTION_ID)
+                    .actionId(ACTION_ID_1)
                     .actionType(LOGIN_ACTION_TYPE)
                     .agentId(AGENT_ID)
                     .agentType(USER_AGENT_TYPE)
                     .agentUsername(AGENT_USERNAME)
-                    .eventTime(EVENT_TIME)
+                    .eventTime(EVENT_TIME_1)
                     .newValue(null)
                     .previousValue(null)
                     .targetId(null)
                     .targetProperty(null)
                     .targetType(WORKBENCH_TARGET_TYPE)));
-    assertThat(auditAction.getActionId()).isEqualTo(ACTION_ID);
-    assertThat(auditAction.getActionTime()).isEqualTo(EVENT_TIME);
+    assertThat(auditAction.getActionId()).isEqualTo(ACTION_ID_1);
+    assertThat(auditAction.getActionTime()).isEqualTo(EVENT_TIME_1);
 
     assertThat(auditAction.getEventBundles()).hasSize(1);
 
@@ -159,5 +167,141 @@ public class AuditLogEntryMapperTest {
     assertThat(eventBundle.getHeader().getAgent().getAgentUsername()).isEqualTo(AGENT_USERNAME);
 
     assertThat(eventBundle.getPropertyChanges()).isEmpty();
+  }
+
+  @Test
+  public void testBuildAuditAction_multipleRowAction() {
+    List<AuditLogEntry> logEntries =
+        ImmutableList.of(
+            new AuditLogEntry()
+                .actionId(ACTION_ID_1)
+                .actionType(ACTION_TYPE_CREATE)
+                .agentId(AGENT_ID)
+                .agentType(USER_AGENT_TYPE)
+                .agentUsername(AGENT_USERNAME)
+                .eventTime(EVENT_TIME_1)
+                .newValue("The Grapes of Wrath")
+                .previousValue(null)
+                .targetId(TARGET_ID)
+                .targetProperty(TARGET_PROPERTY)
+                .targetType(WORKSPACE_TARGET_TYPE),
+            new AuditLogEntry()
+                .actionId(ACTION_ID_1)
+                .actionType(ACTION_TYPE_CREATE)
+                .agentId(AGENT_ID)
+                .agentType(USER_AGENT_TYPE)
+                .agentUsername(AGENT_USERNAME)
+                .eventTime(EVENT_TIME_1)
+                .newValue(ETAG)
+                .previousValue(null)
+                .targetId(TARGET_ID)
+                .targetProperty(ETAG_TARGET_PROPERTY)
+                .targetType(WORKSPACE_TARGET_TYPE));
+
+    final AuditAction auditAction = auditLogEntryMapper.buildAuditAction(logEntries);
+    assertThat(auditAction.getActionTime()).isEqualTo(EVENT_TIME_1);
+    assertThat(auditAction.getEventBundles()).hasSize(1);
+
+    final AuditEventBundle eventBundle = auditAction.getEventBundles().get(0);
+    final AuditEventBundleHeader header = eventBundle.getHeader();
+    assertThat(header.getAgent().getAgentId()).isEqualTo(AGENT_ID);
+    assertThat(header.getAgent().getAgentType()).isEqualTo(USER_AGENT_TYPE);
+    assertThat(header.getAgent().getAgentUsername()).isEqualTo(AGENT_USERNAME);
+
+    assertThat(header.getTarget().getTargetType()).isEqualTo(WORKSPACE_TARGET_TYPE);
+    assertThat(header.getTarget().getTargetId()).isEqualTo(TARGET_ID);
+
+    assertThat(header.getActionType()).isEqualTo(ACTION_TYPE_CREATE);
+  }
+
+  @Test
+  public void testLogEntriesToActions() {
+    List<AuditLogEntry> logEntries =
+        ImmutableList.of(
+            new AuditLogEntry()
+                .actionId(ACTION_ID_1)
+                .actionType(ACTION_TYPE_CREATE)
+                .agentId(AGENT_ID)
+                .agentType(USER_AGENT_TYPE)
+                .agentUsername(AGENT_USERNAME)
+                .eventTime(EVENT_TIME_1)
+                .newValue("The Grapes of Wrath")
+                .previousValue(null)
+                .targetId(TARGET_ID)
+                .targetProperty(TARGET_PROPERTY)
+                .targetType(WORKSPACE_TARGET_TYPE),
+            new AuditLogEntry()
+                .actionId(ACTION_ID_1)
+                .actionType(ACTION_TYPE_CREATE)
+                .agentId(AGENT_ID)
+                .agentType(USER_AGENT_TYPE)
+                .agentUsername(AGENT_USERNAME)
+                .eventTime(EVENT_TIME_1)
+                .newValue(ETAG)
+                .previousValue(null)
+                .targetId(TARGET_ID)
+                .targetProperty(ETAG_TARGET_PROPERTY)
+                .targetType(WORKSPACE_TARGET_TYPE),
+            new AuditLogEntry()
+                .actionId(ACTION_ID_2)
+                .actionType(ACTION_TYPE_DELETE)
+                .agentId(AGENT_ID)
+                .agentType(USER_AGENT_TYPE)
+                .agentUsername(AGENT_USERNAME)
+                .eventTime(EVENT_TIME_2)
+                .newValue(ETAG)
+                .previousValue(null)
+                .targetId(TARGET_ID)
+                .targetProperty(null)
+                .targetType(WORKSPACE_TARGET_TYPE));
+
+    final List<AuditAction> actions = auditLogEntryMapper.logEntriesToActions(logEntries);
+
+    assertThat(actions).hasSize(2);
+    final AuditAction action1 =
+        actions.stream()
+            .filter(a -> a.getActionId().equals(ACTION_ID_1))
+            .findFirst()
+            .orElseThrow(() -> new NotFoundException("Action not found"));
+    assertThat((double) action1.getActionTime().getMillis())
+        .isWithin(TIME_TOLERANCE_MILLIS)
+        .of(EVENT_TIME_1.getMillis());
+    assertThat(action1.getEventBundles()).hasSize(1);
+    assertThat(action1.getActionTime()).isEqualTo(EVENT_TIME_1);
+    assertThat(action1.getEventBundles()).hasSize(1);
+
+    final AuditEventBundle eventBundle1 = action1.getEventBundles().get(0);
+    final AuditEventBundleHeader header1 = eventBundle1.getHeader();
+    assertThat(header1.getAgent().getAgentId()).isEqualTo(AGENT_ID);
+    assertThat(header1.getAgent().getAgentType()).isEqualTo(USER_AGENT_TYPE);
+    assertThat(header1.getAgent().getAgentUsername()).isEqualTo(AGENT_USERNAME);
+
+    assertThat(header1.getTarget().getTargetType()).isEqualTo(WORKSPACE_TARGET_TYPE);
+    assertThat(header1.getTarget().getTargetId()).isEqualTo(TARGET_ID);
+
+    assertThat(header1.getActionType()).isEqualTo(ACTION_TYPE_CREATE);
+    assertThat(eventBundle1.getPropertyChanges()).hasSize(2);
+
+    final AuditAction action2 =
+        actions.stream()
+            .filter(a -> a.getActionId().equals(ACTION_ID_2))
+            .findFirst()
+            .orElseThrow(() -> new NotFoundException("Action not found"));
+    assertThat((double) action2.getActionTime().getMillis())
+        .isWithin(TIME_TOLERANCE_MILLIS)
+        .of(EVENT_TIME_2.getMillis());
+    final AuditEventBundle bundle2 = action2.getEventBundles().get(0);
+    assertThat(bundle2.getHeader().getActionType()).isEqualTo(ACTION_TYPE_DELETE);
+
+    final AuditEventBundle eventBundle2 = action2.getEventBundles().get(0);
+    final AuditEventBundleHeader header2 = eventBundle2.getHeader();
+    assertThat(header2.getAgent().getAgentId()).isEqualTo(AGENT_ID);
+    assertThat(header2.getAgent().getAgentType()).isEqualTo(USER_AGENT_TYPE);
+    assertThat(header2.getAgent().getAgentUsername()).isEqualTo(AGENT_USERNAME);
+    assertThat(header2.getTarget().getTargetType()).isEqualTo(WORKSPACE_TARGET_TYPE);
+    assertThat(header2.getTarget().getTargetId()).isEqualTo(TARGET_ID);
+
+    assertThat(header2.getActionType()).isEqualTo(ACTION_TYPE_DELETE);
+    assertThat(eventBundle2.getPropertyChanges()).hasSize(1);
   }
 }
