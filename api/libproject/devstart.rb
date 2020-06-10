d050b72724832ebb41d006063c76789d452a4d0f
CH Albach <calbach@google.com>
2020-06-09 17:24:45 -0700

    automatic restarts

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -2550,14 +2550,13 @@ def start_api_and_incremental_build(cmd_name, args)
     bm = Benchmark.measure {
       # appengineStart must be run with the Gradle daemon or it will stop outputting logs as soon as
       # the application has finished starting.
-      common.run_inline %W{gradle --daemon appengineStart}
+      common.run_inline %W{gradle --daemon --build-cache appengineStart}
       common.run_inline "tail -f -n 0 /w/api/build/dev-appserver-out/dev_appserver.out &"
       # incrementalHotSwap must be run without the Gradle daemon or stdout and stderr will not appear
       # in the output.
     }
     common.status "API server startup complete (#{format_benchmark(bm)})"
-
-    common.run_inline %W{gradle --no-daemon --continuous incrementalHotSwap}
+    common.run_inline %W{gradle --no-daemon --continuous --build-cache explodeWar}
   ensure
     common.run_inline %W{gradle --stop}
   end
