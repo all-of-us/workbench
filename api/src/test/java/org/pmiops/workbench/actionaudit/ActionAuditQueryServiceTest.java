10895d765a3d340dd40be732bcdae7d4a006160e
aweng98 <bweng@broadinstitute.org>
2020-06-09 21:22:24 -0400

    force api-unit-test run

--- api/src/test/java/org/pmiops/workbench/actionaudit/ActionAuditQueryServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/actionaudit/ActionAuditQueryServiceTest.java
@@ -35,8 +35,8 @@ import org.springframework.test.context.junit4.SpringRunner;
 
 @RunWith(SpringRunner.class)
 public class ActionAuditQueryServiceTest {
-//
-  // N.B. Field order must match that of SELECT statement in Workspaces query.
+
+  // N.B. Field order must match that of SELECT statement in Workspaces query
   private static final Schema WORKSPACE_QUERY_SCHEMA =
       Schema.of(
           ImmutableList.of(
