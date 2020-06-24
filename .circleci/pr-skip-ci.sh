0b8e0d240c5d6279cec907e5c57d556f02bc4409
aweng98 <bweng@broadinstitute.org>
2020-06-24 12:41:31 -0400

    revert test code

--- .circleci/pr-skip-ci.sh
+++ .circleci/pr-skip-ci.sh
@@ -3,10 +3,6 @@ set -e
 
 # This script makes CircleCI exit a non pull-request job.
 
-if [[ ${CIRCLE_PULL_REQUEST_DUMMY} ]]; then
-  echo "CIRCLE_PULL_REQUEST_DUMMY: ${CIRCLE_PULL_REQUEST_DUMMY}"
-fi
-
 if [[ ${CIRCLE_PULL_REQUEST} ]]; then
   echo "CIRCLE_PULL_REQUEST: ${CIRCLE_PULL_REQUEST}"
   regexp="[[:digit:]]\+$"
