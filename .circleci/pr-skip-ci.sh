123cfaa988cb4d94659085c3dd7e06d2ca27d244
aweng98 <bweng@broadinstitute.org>
2020-06-24 12:30:28 -0400

    CIRCLE_PULL_REQUEST null

--- .circleci/pr-skip-ci.sh
+++ .circleci/pr-skip-ci.sh
@@ -3,10 +3,12 @@ set -e
 
 # This script makes CircleCI exit a non pull-request job.
 
-echo "CIRCLE_PULL_REQUEST: ${CIRCLE_PULL_REQUEST}"
-regexp="[[:digit:]]\+$"
-PR_NUMBER=`echo $CIRCLE_PULL_REQUEST | grep -o $regexp`
-echo "PR_NUMBER: ${PR_NUMBER}"
+if [[ "${CIRCLE_PULLREQUEST}" ]]; then
+  echo "CIRCLE_PULL_REQUEST: ${CIRCLE_PULL_REQUEST}"
+  regexp="[[:digit:]]\+$"
+  PR_NUMBER=`echo $CIRCLE_PULL_REQUEST | grep -o $regexp`
+  echo "PR_NUMBER: ${PR_NUMBER}"
+fi
 
 # puppeteer-e2e-local-* jobs to skip running if this is not a pull request.
 if [[ -z "${CIRCLE_PULL_REQUEST}" ]] || ([[ ${CIRCLE_BRANCH} != "" ]] && [[ ${CIRCLE_BRANCH} == "master" ]]); then
