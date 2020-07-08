0228905dff47f5ecd35eb83615cfc7a971b8c379
Gregory Jordan <gjordan@verily.com>
2020-07-08 16:10:29 -0400

    Sort institutions by display name.

--- api/src/test/java/org/pmiops/workbench/institution/InstitutionServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/institution/InstitutionServiceTest.java
@@ -8,6 +8,7 @@ import com.google.common.collect.Lists;
 import com.google.common.collect.Sets;
 import java.util.Arrays;
 import java.util.Collections;
+import java.util.Comparator;
 import java.util.List;
 import java.util.Set;
 import org.junit.Before;
@@ -75,13 +76,16 @@ public class InstitutionServiceTest {
     final Institution anotherInst =
         new Institution()
             .shortName("otherInst")
-            .displayName("The Institution of testing")
+            .displayName("An Institution for Testing")
             .emailDomains(Collections.emptyList())
             .emailAddresses(Collections.emptyList())
             .organizationTypeEnum(OrganizationType.INDUSTRY);
     assertThat(service.createInstitution(anotherInst)).isEqualTo(anotherInst);
 
     assertThat(service.getInstitutions()).containsExactly(roundTrippedTestInst, anotherInst);
+    Comparator<Institution> comparator =
+        Comparator.comparing(institution -> institution.getDisplayName());
+    assertThat(service.getInstitutions()).isStrictlyOrdered(comparator);
   }
 
   @Test
