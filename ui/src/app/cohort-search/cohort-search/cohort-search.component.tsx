eedba0e79bdb851bd9dd8532538fea497767530e
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro.local>
2020-07-07 21:55:28 -0500

    RW-4589 convert wizard to full page

--- ui/src/app/cohort-search/cohort-search/cohort-search.component.tsx
+++ ui/src/app/cohort-search/cohort-search/cohort-search.component.tsx
@@ -9,7 +9,7 @@ import {searchRequestStore} from 'app/cohort-search/search-state.service';
 import {SelectionList} from 'app/cohort-search/selection-list/selection-list.component';
 import {CriteriaTree} from 'app/cohort-search/tree/tree.component';
 import {domainToTitle, generateId, stripHtml, typeToTitle} from 'app/cohort-search/utils';
-import {Button} from 'app/components/buttons';
+import {Button, Clickable} from 'app/components/buttons';
 import {ClrIcon} from 'app/components/icons';
 import {SpinnerOverlay} from 'app/components/spinners';
 import {AoU} from 'app/components/text-wrappers';
@@ -20,42 +20,25 @@ import {environment} from 'environments/environment';
 import {Criteria, CriteriaType, DomainType, TemporalMention, TemporalTime} from 'generated/fetch';
 
 const styles = reactStyles({
+  backArrow: {
+    background: `${addOpacity(colors.accent, 0.15)}`,
+    borderRadius: '50%',
+    height: '1.5rem',
+    lineHeight: '1.4rem',
+    textAlign: 'center',
+    width: '1.5rem',
+  },
   footer: {
     marginTop: '0.5rem',
     padding: '0.45rem 0rem',
     display: 'flex',
     justifyContent: 'flex-end',
   },
-  modalContainer: {
-    position: 'absolute',
-    top: '50%',
-    left: '50%',
-    overflowY: 'auto',
-    transform: 'translate(-50%, -50%)',
-    backgroundColor: colors.white,
-    borderRadius: '4px',
-    display: 'flex',
-    flexFlow: 'column nowrap',
-    justifyContent: 'space-between',
-  },
   modalContent: {
     display: 'flex',
     flexWrap: 'wrap',
-    height: '100%',
+    height: '70vh',
     width: '100%',
-  },
-  modalOverlay: {
-    position: 'fixed',
-    top: 0,
-    left: 0,
-    width: '100%',
-    height: '100%',
-    backgroundColor: `${addOpacity(colors.black, 0.5)}`,
-    opacity: 0,
-    visibility: 'hidden',
-    transform: 'scale(1.1)',
-    transition: 'visibility 0.25s linear, opacity 0.25s 0s, transform 0.25s',
-    zIndex: 105,
   },
   panelLeft: {
     display: 'none',
@@ -70,15 +53,6 @@ const styles = reactStyles({
   panelLeftActive: {
     animation: 'fadeEffect 1s',
     display: 'block',
-  },
-  separator: {
-    alignSelf: 'center',
-    display: 'inline-block',
-    margin: '0 0.75rem 0.5rem',
-    boxSizing: 'border-box',
-    height: '26px',
-    width: '1px',
-    border: `1px solid ${colorWithWhiteness(colors.black, 0.6)}`,
   },
   tabButton: {
     borderRadius: 0,
@@ -95,7 +69,6 @@ const styles = reactStyles({
     padding: '0 0.5rem',
   },
   titleBar: {
-    boxShadow: `0 0.12rem 0.125rem 0 ${colors.accent}`,
     marginBottom: '0.5rem',
     padding: '0rem 1rem',
     display: 'flex',
@@ -106,47 +79,7 @@ const styles = reactStyles({
   }
 });
 
-const css = `
-  .modal-container {
-    height: 90vh;
-    width: 80vw;
-  }
-  .modal-container.demographics {
-    width: 50vw;
-    height: auto;
-  }
-  @media (max-width: 1500px) {
-  .modal-container {
-    width: 90vw;
-  }
-  .modal-container.demographics {
-    width: 70vw;
-  }
-  @media (max-width: 1200px) {
-    .modal-container, .modal-container.demographics {
-      width: 100vw;
-    }
-  }
-  @media (max-height: 700px) {
-    .modal-container {
-      height: 100vh;
-    }
-  }
-  @media (max-height: 400px) {
-    .modal-container.demographics {
-      height: 100vh;
-    }
-    .modal-container.demographics.age {
-      height: auto;
-    }
-  }
-  @media (max-height: 300px) {
-    .modal-container.demographics.age {
-      height: 100vh;
-    }
-  }
-}
-`;
+const arrowIcon = '/assets/icons/arrow-left-regular.svg';
 
 function initGroup(role: string, item: any) {
   return {
@@ -184,7 +117,6 @@ interface State {
   hierarchyNode: Criteria;
   loadingSubtree: boolean;
   mode: string;
-  open: boolean;
   selectedIds: Array<string>;
   selections: Array<Selection>;
   title: string;
@@ -205,7 +137,6 @@ export class CohortSearch extends React.Component<Props, State> {
       hierarchyNode: undefined,
       loadingSubtree: false,
       mode: 'list',
-      open: false,
       selectedIds: [],
       selections: [],
       title: '',
@@ -215,27 +146,25 @@ export class CohortSearch extends React.Component<Props, State> {
 
   componentDidMount(): void {
     const {searchContext: {domain, item, standard, type}} = this.props;
-    if (!this.state.open) {
-      const selections = item.searchParameters;
-      const selectedIds = selections.map(s => s.parameterId);
-      if (type === CriteriaType.DECEASED) {
-        this.selectDeceased();
-      } else {
-        const title = domain === DomainType.PERSON ? typeToTitle(type) : domainToTitle(domain);
-        let {backMode, mode} = this.state;
-        let hierarchyNode;
-        if (this.initTree) {
-          hierarchyNode = {
-            domainId: domain,
-            type: type,
-            isStandard: standard,
-            id: 0,
-          };
-          backMode = 'tree';
-          mode = 'tree';
-        }
-        this.setState({backMode, hierarchyNode, mode, open: true, selectedIds, selections, title});
+    const selections = item.searchParameters;
+    const selectedIds = selections.map(s => s.parameterId);
+    if (type === CriteriaType.DECEASED) {
+      this.selectDeceased();
+    } else {
+      const title = domain === DomainType.PERSON ? typeToTitle(type) : domainToTitle(domain);
+      let {backMode, mode} = this.state;
+      let hierarchyNode;
+      if (this.initTree) {
+        hierarchyNode = {
+          domainId: domain,
+          type: type,
+          isStandard: standard,
+          id: 0,
+        };
+        backMode = 'tree';
+        mode = 'tree';
       }
+      this.setState({backMode, hierarchyNode, mode, selectedIds, selections, title});
     }
   }
 
@@ -326,6 +255,7 @@ export class CohortSearch extends React.Component<Props, State> {
   get rightColumnStyle() {
     const width = this.props.searchContext.domain === DomainType.PERSON ? '50%' : '33.33333%';
     return {
+      display: 'none',
       flex: `0 0 ${width}`,
       maxWidth: width,
       position: 'relative',
@@ -438,145 +368,131 @@ export class CohortSearch extends React.Component<Props, State> {
   render() {
     const {closeSearch, searchContext, searchContext: {domain, type}, setSearchContext} = this.props;
     const {attributesNode, autocompleteSelection, conceptType, count, disableFinish, groupSelections, hierarchyNode, loadingSubtree, mode,
-      open, selectedIds, selections, title, treeSearchTerms} = this.state;
-    return !!searchContext ? <React.Fragment>
-      <style>{css}</style>
-      <div style={{
-        ...styles.modalOverlay,
-        ...(open ? {opacity: 1, visibility: 'visible', transform: 'scale(1.0'} : {})
-      }}>
-        <div style={styles.modalContainer}
-             className={`modal-container${domain === DomainType.PERSON ? ' demographics' : ''}${type === CriteriaType.AGE ? ' age' : ''}`}>
-          <div style={styles.modalContent}>
-            <div style={this.leftColumnStyle}>
-              <div style={styles.titleBar}>
-                <div style={{display: 'inline-flex', marginRight: '0.5rem'}}>
-                  {!attributesNode ? <Button
-                      style={{...styles.tabButton, ...((mode === 'list' || mode === 'tree') ? styles.tabButtonActive : {})}}
-                      type='link'
-                      disabled={selections.length !== 0 && conceptType === 'standard'}
-                      onClick={() => this.setState({mode: this.state.backMode})}>
-                      {title}
-                    </Button>
-                    : <Button
-                      style={{...styles.tabButton, ...(mode === 'attributes' ? styles.tabButtonActive : {})}}
-                      type='link'
-                      onClick={() => this.setState({mode: 'attributes'})}>
-                      {this.attributeTitle}
-                    </Button>}
-                  {this.showModifiers && <React.Fragment>
-                    <div style={styles.separator}/>
-                    <Button
-                        style={{...styles.tabButton, ...(mode === 'modifiers' ? styles.tabButtonActive : {})}}
-                        type='link'
-                        disabled={selections.length === 0}
-                        onClick={() => this.setMode('modifiers')}>
-                      Modifiers
-                    </Button>
-                  </React.Fragment>}
-                </div>
-                <div style={{display: 'table', height: '100%'}}>
-                  <div style={{display: 'table-cell', height: '100%', verticalAlign: 'middle'}}>
-                    {domain === DomainType.DRUG && <div>
-                      <a href='https://mor.nlm.nih.gov/RxNav/' target='_blank' rel='noopener noreferrer'>
-                        Explore
-                      </a>
-                      &nbsp;drugs by brand names outside of <AoU/>.
-                    </div>}
-                    {this.showDataBrowserLink && <div>
-                      Explore Source information on the&nbsp;
-                      <a href={environment.publicUiUrl} target='_blank' rel='noopener noreferrer'>Data Browser.</a>
-                    </div>}
-                  </div>
-                </div>
-                {mode === 'attributes' && <Button type='link' onClick={this.back}>
-                  <ClrIcon size='24' shape='close'/>
-                </Button>}
-              </div>
-              <div style={(domain === DomainType.PERSON && type !== CriteriaType.AGE) ? {marginBottom: '3.5rem'} : {height: 'calc(100% - 3.5rem)'}}>
-                {domain === DomainType.PERSON ? <div style={{flex: 1, overflow: 'auto'}}>
-                    <Demographics
-                      count={count}
-                      criteriaType={type}
+      selectedIds, selections, title, treeSearchTerms} = this.state;
+    return !!searchContext ? <div style={styles.modalContent}>
+      <div style={{height: '100%', width: '100%'}}>
+        <div style={styles.titleBar}>
+          <div style={{display: 'inline-flex', marginRight: '0.5rem'}}>
+            <Clickable style={styles.backArrow} onClick={() => closeSearch()}>
+              <img src={arrowIcon} style={{height: '21px', width: '18px'}} alt='Go back' />
+            </Clickable>
+            <h2 style={{color: colors.primary, lineHeight: '1.5rem', margin: '0 0 0 0.75rem'}}>
+              {title}
+            </h2>
+          </div>
+          <div style={{display: 'table', height: '100%'}}>
+            <div style={{display: 'table-cell', height: '100%', verticalAlign: 'middle'}}>
+              {domain === DomainType.DRUG && <div>
+                <a href='https://mor.nlm.nih.gov/RxNav/' target='_blank' rel='noopener noreferrer'>
+                  Explore
+                </a>
+                &nbsp;drugs by brand names outside of <AoU/>.
+              </div>}
+              {this.showDataBrowserLink && <div>
+                Explore Source information on the&nbsp;
+                <a href={environment.publicUiUrl} target='_blank' rel='noopener noreferrer'>Data Browser.</a>
+              </div>}
+            </div>
+          </div>
+          {mode === 'attributes' && <Button type='link' onClick={this.back}>
+            <ClrIcon size='24' shape='close'/>
+          </Button>}
+        </div>
+        <div style={
+          (domain === DomainType.PERSON && type !== CriteriaType.AGE)
+            ? {marginBottom: '3.5rem'}
+            : {height: 'calc(100% - 3.5rem)'}
+        }>
+          {domain === DomainType.PERSON ? <div style={{flex: 1, overflow: 'auto'}}>
+              <Demographics
+                count={count}
+                criteriaType={type}
+                select={this.addSelection}
+                selectedIds={selectedIds}
+                selections={selections}/>
+            </div>
+            : <React.Fragment>
+              {loadingSubtree && <SpinnerOverlay/>}
+              <div style={loadingSubtree ? {height: '100%', pointerEvents: 'none', opacity: 0.3} : {height: '100%'}}>
+                {/* Tree View */}
+                <div style={this.panelLeftStyle('tree')}>
+                  {hierarchyNode && <CriteriaTree
+                      autocompleteSelection={autocompleteSelection}
+                      back={this.back}
+                      groupSelections={groupSelections}
+                      node={hierarchyNode}
+                      scrollToMatch={this.setScroll}
+                      searchTerms={treeSearchTerms}
                       select={this.addSelection}
                       selectedIds={selectedIds}
-                      selections={selections}/>
-                  </div>
-                  : <React.Fragment>
-                    {loadingSubtree && <SpinnerOverlay/>}
-                    <div style={loadingSubtree ? {height: '100%', pointerEvents: 'none', opacity: 0.3} : {height: '100%'}}>
-                      {/* Tree View */}
-                      <div style={this.panelLeftStyle('tree')}>
-                        {hierarchyNode && <CriteriaTree
-                            autocompleteSelection={autocompleteSelection}
-                            back={this.back}
-                            groupSelections={groupSelections}
-                            node={hierarchyNode}
-                            scrollToMatch={this.setScroll}
-                            searchTerms={treeSearchTerms}
-                            select={this.addSelection}
-                            selectedIds={selectedIds}
-                            selectOption={this.setAutocompleteSelection}
-                            setAttributes={this.setAttributes}
-                            setSearchTerms={this.setTreeSearchTerms}/>}
-                      </div>
-                      {/* List View */}
-                      <div style={this.panelLeftStyle('list')}>
-                        <ListSearch hierarchy={this.showHierarchy}
-                                    searchContext={searchContext}
-                                    select={this.addSelection}
-                                    selectedIds={selectedIds}
-                                    setAttributes={this.setAttributes}/>
-                      </div>
-                      {/* Modifiers Page */}
-                      <div style={this.panelLeftStyle('modifiers')}>
-                        {this.showModifiers && <ModifierPage
-                            disabled={this.modifiersFlag}
-                            searchContext={searchContext}
-                            selections={selections}
-                            setSearchContext={setSearchContext}/>}
-                      </div>
-                      {/* Attributes Page */}
-                      <div style={this.panelLeftStyle('attributes')}>
-                        {!!attributesNode && <AttributesPage
-                            close={this.back}
-                            node={attributesNode}
-                            select={this.addSelection}/>}
-                      </div>
-                    </div>
-                  </React.Fragment>}
-                {type === CriteriaType.AGE && <div style={styles.footer}>
-                  <Button style={{height: '1.5rem', margin: '0.25rem 0.5rem'}}
-                          type='link'
-                          onClick={closeSearch}>
-                    Cancel
-                  </Button>
-                  <Button style={{height: '1.5rem', margin: '0.25rem 0.5rem'}}
-                          type='primary'
-                          onClick={this.finish}>
-                    Finish
-                  </Button>
-                </div>}
+                      selectOption={this.setAutocompleteSelection}
+                      setAttributes={this.setAttributes}
+                      setSearchTerms={this.setTreeSearchTerms}/>}
+                </div>
+                {/* List View */}
+                <div style={this.panelLeftStyle('list')}>
+                  <ListSearch hierarchy={this.showHierarchy}
+                              searchContext={searchContext}
+                              select={this.addSelection}
+                              selectedIds={selectedIds}
+                              setAttributes={this.setAttributes}/>
+                </div>
+                {/**
+                 Modifiers Page - This will no longer be rendered, leaving here temporarily for reference
+                 TODO move to sidebar in RW-4594
+                 **/}
+                <div style={this.panelLeftStyle('modifiers')}>
+                  {this.showModifiers && <ModifierPage
+                      disabled={this.modifiersFlag}
+                      searchContext={searchContext}
+                      selections={selections}
+                      setSearchContext={setSearchContext}/>}
+                </div>
+                {/**
+                 Attributes Page - This will no longer be rendered, leaving here temporarily for reference
+                 TODO move to sidebar in RW-4595
+                 **/}
+                <div style={this.panelLeftStyle('attributes')}>
+                  {!!attributesNode && <AttributesPage
+                      close={this.back}
+                      node={attributesNode}
+                      select={this.addSelection}/>}
+                </div>
               </div>
-            </div>
-            {type !== CriteriaType.AGE && <div style={this.rightColumnStyle}>
-              <div style={{height: '100%'}}>
-                <SelectionList
-                    back={this.back}
-                    close={closeSearch}
-                    disableFinish={disableFinish}
-                    domain={domain}
-                    finish={this.finish}
-                    removeSelection={this.removeSelection}
-                    selections={selections}
-                    setView={this.setMode}
-                    view={mode}/>
-              </div>
-            </div>}
-          </div>
+            </React.Fragment>}
+          {type === CriteriaType.AGE && <div style={styles.footer}>
+            <Button style={{height: '1.5rem', margin: '0.25rem 0.5rem'}}
+                    type='link'
+                    onClick={closeSearch}>
+              Cancel
+            </Button>
+            <Button style={{height: '1.5rem', margin: '0.25rem 0.5rem'}}
+                    type='primary'
+                    onClick={this.finish}>
+              Finish
+            </Button>
+          </div>}
         </div>
       </div>
-    </React.Fragment> : '';
+      {/**
+       Selection List - This will no longer be rendered, leaving here temporarily for reference
+       TODO move to sidebar in RW-5113
+     **/}
+      {type !== CriteriaType.AGE && <div style={this.rightColumnStyle}>
+        <div style={{height: '100%'}}>
+          <SelectionList
+              back={this.back}
+              close={closeSearch}
+              disableFinish={disableFinish}
+              domain={domain}
+              finish={this.finish}
+              removeSelection={this.removeSelection}
+              selections={selections}
+              setView={this.setMode}
+              view={mode}/>
+        </div>
+      </div>}
+    </div> : '';
   }
 }
 
