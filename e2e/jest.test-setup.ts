1c80a22521506d8824dfb1d0cb9ed2bed6a3bb7c
aweng98 <bweng@broadinstitute.org>
2020-07-09 17:30:55 -0400

    rework jest.test-setup.ts

--- e2e/jest.test-setup.ts
+++ e2e/jest.test-setup.ts
@@ -1,35 +1,28 @@
 const url = require('url');
-
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36';
 
-// Runs this beforeEach() before test's beforeEach().
-beforeEach(async () => {
+
+beforeAll(async () => {
+  await page.setViewport({ width: 0, height: 0 });
   await page.setUserAgent(userAgent);
-  // Refer to https://github.com/puppeteer/puppeteer/blob/master/docs/api.md#pagesetdefaultnavigationtimeouttimeout
+  // See https://github.com/puppeteer/puppeteer/blob/master/docs/api.md#pagesetdefaultnavigationtimeouttimeout
   await page.setDefaultNavigationTimeout(90000);
   await page.setDefaultTimeout(60000);
 });
 
-// Runs this afterEach() before test's afterEach().
-afterEach(async () => {
-  await page.deleteCookie(...await page.cookies());
-  await jestPuppeteer.resetBrowser();
-});
-
-// Runs this beforeAll() before test's beforeAll().
-beforeAll(async () => {
+beforeEach(async () => {
   await page.setRequestInterception(true);
   page.on('request', async (request) => {
     const requestUrl = url.parse(request.url(), true);
     const host = requestUrl.hostname;
-    // to improve page load performance, block requests if it is not for application functions.
+    // to improve page load performance, block network requests unrelated to application.
     try {
       if (host === 'www.google-analytics.com'
-       || host === 'accounts.youtube.com'
-       || host === 'static.zdassets.com'
-       || host === 'play.google.com'
-       || request.url().endsWith('content-security-index-report')) {
-        await request.abort()
+         || host === 'accounts.youtube.com'
+         || host === 'static.zdassets.com'
+         || host === 'play.google.com'
+         || request.url().endsWith('content-security-index-report')) {
+        await request.abort();
       } else {
         await request.continue();
       }
@@ -39,7 +32,9 @@ beforeAll(async () => {
   });
 });
 
-// Runs this afterAll() before test's afterAll().
-afterAll(async () => {
+afterEach(async () => {
   await page.setRequestInterception(false);
+  await page.deleteCookie(...await page.cookies());
+  await jestPuppeteer.resetPage();
+  await jestPuppeteer.resetBrowser();
 });
