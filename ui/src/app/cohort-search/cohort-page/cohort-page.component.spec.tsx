5d1d444d9cce3763fc5938c91155f161b120c1f2
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro.local>
2020-07-15 11:58:42 -0500

    RW-4589 fix test again

--- ui/src/app/cohort-search/cohort-page/cohort-page.component.spec.tsx
+++ ui/src/app/cohort-search/cohort-page/cohort-page.component.spec.tsx
@@ -2,28 +2,33 @@ import {mount} from 'enzyme';
 import * as React from 'react';
 
 import {registerApiClient} from 'app/services/swagger-fetch-clients';
-import {currentWorkspaceStore, queryParamsStore} from 'app/utils/navigation';
+import {currentWorkspaceStore, queryParamsStore, serverConfigStore} from 'app/utils/navigation';
 import {CohortBuilderApi, CohortsApi} from 'generated/fetch';
+import defaultServerConfig from 'testing/default-server-config';
 import {waitOneTickAndUpdate} from 'testing/react-test-helpers';
 import {CohortBuilderServiceStub} from 'testing/stubs/cohort-builder-service-stub';
 import {CohortsApiStub} from 'testing/stubs/cohorts-api-stub';
 import {workspaceDataStub} from 'testing/stubs/workspaces-api-stub';
-import {CohortSearch} from './cohort-search.component';
+import {CohortPage} from './cohort-page.component';
 
-describe('CohortSearch', () => {
+describe('CohortPage', () => {
   beforeEach(() => {
     currentWorkspaceStore.next(workspaceDataStub);
     registerApiClient(CohortBuilderApi, new CohortBuilderServiceStub());
     registerApiClient(CohortsApi, new CohortsApiStub());
+    serverConfigStore.next({
+      ...defaultServerConfig,
+      enableCohortBuilderV2: false
+    });
   });
 
   it('should render', () => {
-    const wrapper = mount(<CohortSearch setCohortChanged={() => {}} setShowWarningModal={() => {}} setUpdatingCohort={() => {}}/>);
+    const wrapper = mount(<CohortPage setCohortChanged={() => {}} setShowWarningModal={() => {}} setUpdatingCohort={() => {}}/>);
     expect(wrapper).toBeTruthy();
   });
 
   it('should render one search group for each includes/excludes item', async() => {
-    const wrapper = mount(<CohortSearch setCohortChanged={() => {}} setShowWarningModal={() => {}} setUpdatingCohort={() => {}}/>);
+    const wrapper = mount(<CohortPage setCohortChanged={() => {}} setShowWarningModal={() => {}} setUpdatingCohort={() => {}}/>);
     await waitOneTickAndUpdate(wrapper);
     expect(wrapper.find('[data-test-id="includes-search-group"]').length).toBe(0);
     expect(wrapper.find('[data-test-id="excludes-search-group"]').length).toBe(0);
