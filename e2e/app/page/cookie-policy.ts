243cc6114842e0deae2263f304d9d4b66f0733df
CH Albach <calbach@users.noreply.github.com>
2020-07-20 19:25:12 -0700

    [RW-5300][RW-5297][risk=no] Fix Reactified signed out page access + reg test (#3810)

---
+++ e2e/app/page/cookie-policy.ts
@@ -0,0 +1,10 @@
+import {Page} from 'puppeteer';
+
+export default class CookiePolicyPage {
+
+  constructor(private readonly page: Page) {}
+
+  async loaded(): Promise<void> {
+    await this.page.waitForXPath('//h3[contains(text(), "Cookie Policy")]', {visible: true});
+  }
+}
