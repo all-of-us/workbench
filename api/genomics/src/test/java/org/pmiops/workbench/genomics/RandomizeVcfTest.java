9ae4921395013516562bc06d3d9e4a9503c0fe97
Ariel Schwartz <als364@cornell.edu>
2020-07-24 12:38:11 -0400

    [RW-5033][risk=no] Scripts to generate a randomish multisample VCF (#3813)
    * pushing up for environment help
    
    * now in api/src/main
    
    * shelving, again
    
    * good staging point
    
    * working
    
    * revert unnecessary top level gradle changes
    
    * spotless...
    
    * project.rb commands
    
    * better GT generation
    
    * spotless. again
    
    * shelving
    
    * fix whitespace bug
    
    * gradle subproject
    
    * ugh typo
    
    * review feedback
    
    * formatting
    
    * spotless
    
    * try adding genomics to the test glob
    
    * print gradle args for test
    
    * add junitplatform
    
    * trying something with the circle config
    
    * what about this for filtering out genomics
    
    * filter out genomics
    
    * exclude genomics task
    
    * ugh
    
    * now for genomics
    
    * extraneous close paren...
    
    * how is this not finding tests
    
    * oops:
    
    * fml
    
    * incrementally resolving
    
    * yay
    
    * explanatory comment
    
    * review comments
    
    * more review feedback
    
    * dot slash gradle double-u spotless apply
    
    * one last circle tweak
    
    Co-authored-by: Ariel Schwartz <aschwart@broadinstitute.org>
    Co-authored-by: Eric Song <me@ericsong.io>

---
+++ api/genomics/src/test/java/org/pmiops/workbench/genomics/RandomizeVcfTest.java
@@ -0,0 +1,52 @@
+package org.pmiops.workbench.genomics;
+
+import static com.google.common.truth.Truth.assertThat;
+
+import htsjdk.variant.variantcontext.Allele;
+import htsjdk.variant.variantcontext.Genotype;
+import htsjdk.variant.variantcontext.VariantContext;
+import htsjdk.variant.vcf.VCFFileReader;
+import java.io.File;
+import java.util.List;
+import java.util.Random;
+import org.junit.Test;
+
+public class RandomizeVcfTest {
+  private static final VCFFileReader reader =
+      new VCFFileReader(new File("src/test/resources/NA12878_204126160130_R01C01.toy.vcf.gz"));
+  private static final Random random = new Random(0);
+  private static final VariantContext variantContext = reader.iterator().next();
+  private static final RandomizeVcf randomizeVcf = new RandomizeVcf("0", random);
+
+  @Test
+  public void testRandomizeVariant() {
+    VariantContext randomizedVariant = randomizeVcf.randomizeVariant(variantContext);
+    // For each sample/GT in the variant...
+    // For each allele in the genotype...
+    // ...is that allele from the bank of possible alleles at that position?
+    // ignoring ref/var
+    randomizedVariant.getGenotypes().stream()
+        .flatMap(genotype -> genotype.getAlleles().stream())
+        .forEach(
+            allele ->
+                assertThat(
+                        variantContext.getAlleles().stream()
+                            .anyMatch(vcAllele -> vcAllele.basesMatch(allele)))
+                    .isTrue());
+  }
+
+  @Test
+  public void testRandomizeGenotypes() {
+    Genotype genotype = variantContext.getGenotype(0);
+    Genotype randomizedGenotype = randomizeVcf.randomizeGenotype(variantContext, genotype);
+    assertThat(randomizedGenotype.getSampleName()).isNotEqualTo(genotype.getSampleName());
+  }
+
+  @Test
+  public void testRandomizeAlleles() {
+    List<Allele> alleles =
+        randomizeVcf.randomizeAlleles(variantContext, variantContext.getGenotype(0).getAlleles());
+    assertThat(alleles.size()).isEqualTo(2); // humans ought to be diploid.
+    alleles.forEach(allele -> assertThat(variantContext.getAlleles()).contains(allele));
+  }
+}
