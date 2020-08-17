c9e8fa8987b9367f432a78cb6c4d3da76c2023b2
Brian Freeman <brian.d.freeman@gmail.com>
2020-08-17 12:01:06 -0500

    RW-5358 stubbing out

--- api/tools/src/main/java/org/pmiops/workbench/tools/UpdateReviewDemographics.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/UpdateReviewDemographics.java
@@ -1,7 +1,25 @@
 package org.pmiops.workbench.tools;
 
+import java.util.Arrays;
+import org.pmiops.workbench.api.BigQueryService;
+import org.pmiops.workbench.db.dao.ParticipantCohortStatusDaoCustom;
+import org.springframework.boot.CommandLineRunner;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 /** Backfill script to adjust cohort reviews that don't include sex at birth. */
 @Configuration
-public class UpdateReviewDemographics {}
+public class UpdateReviewDemographics {
+
+  @Bean
+  public CommandLineRunner run(
+      BigQueryService bigQueryService,
+      ParticipantCohortStatusDaoCustom participantCohortStatusDaoCustom) {
+    return (args) -> {
+      if (args.length != 1) {
+        throw new IllegalArgumentException("Expected 1 args (dry_run). Got " + Arrays.asList(args));
+      }
+      boolean dryRun = Boolean.parseBoolean(args[0]);
+    };
+  }
+}
