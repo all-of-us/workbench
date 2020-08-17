682db8ce9807bfd8cc842c07f06999c7bc6c2430
Brian Freeman <brian.d.freeman@gmail.com>
2020-08-17 11:55:51 -0500

    [RW-5322][risk=no] move ds_linking table to cloud sql (#3874)
    * RW-5322 adding ds_linking to cloudsql
    
    * bumping cdr versions to include ds_linking table
    
    * RW-5322 fixing tests
    
    * RW-5322 fixing nullable constraint

---
+++ api/src/main/java/org/pmiops/workbench/cdr/dao/DSLinkingDao.java
@@ -0,0 +1,12 @@
+package org.pmiops.workbench.cdr.dao;
+
+import java.util.List;
+import org.pmiops.workbench.cdr.model.DbDSLinking;
+import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
+
+public interface DSLinkingDao extends CrudRepository<DbDSLinking, Long> {
+
+  List<DbDSLinking> findByDomainAndDenormalizedNameIn(
+      @Param("domain") String domain, @Param("names") List<String> names);
+}
