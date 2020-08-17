027f9c0e8a48da505f0b9b0f8d8d75a0d715e496
Brian Freeman <brian.d.freeman@gmail.com>
2020-08-17 10:49:27 -0500

    RW-5358 adding config

---
+++ api/tools/src/main/java/org/pmiops/workbench/tools/UpdateReviewDemographics.java
@@ -0,0 +1,7 @@
+package org.pmiops.workbench.tools;
+
+import org.springframework.context.annotation.Configuration;
+
+/** Backfill script to adjust cohort reviews that don't include sex at birth. */
+@Configuration
+public class UpdateReviewDemographics {}
