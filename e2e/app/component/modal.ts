527854ffe77b6c5541c98f7ac964f929556b1926
aweng98 <bweng@broadinstitute.org>
2020-08-24 17:13:24 -0400

    fix modal

--- e2e/app/component/modal.ts
+++ e2e/app/component/modal.ts
@@ -7,9 +7,7 @@ import Checkbox from 'app/element/checkbox';
 import {savePageToFile, takeScreenshot} from 'utils/save-file-utils';
 import {LinkText} from 'app/text-labels';
 import {getPropValue} from 'utils/element-utils';
-
 import * as fp from 'lodash/fp';
-import {waitWhileLoading} from 'utils/test-utils';
 
 const Selector = {
   defaultDialog: '//*[@role="dialog"]',
@@ -23,10 +21,7 @@ export default class Modal extends Container {
 
   async waitForLoad(): Promise<this> {
     try {
-      await Promise.all([
-        this.waitUntilVisible(),
-        waitWhileLoading(this.page),
-      ]);
+      await this.waitUntilVisible();
     } catch (e) {
       await savePageToFile(this.page);
       await takeScreenshot(this.page);
