4f933d5d5332aed69e87cb182e476afda09a06ca
Rohini <rohini.chavan@vumc.org>
2020-08-24 09:41:27 -0500

    created new test case for Delete-workspace

--- e2e/tests/workspace/workspace-delete.spec.ts
+++ e2e/tests/workspace/workspace-delete.spec.ts
@@ -19,28 +19,28 @@ describe('Delete workspace', () => {
    * - Click the delete workspace button on the modal
    * - Navigate to the "View Workspaces" page
    * - Verify that the workspace has been delete
-   **/
+   */
   
   describe('From "Your Workspaces" page using Workspace card ellipsis menu', () => {
 
     test('As OWNER, user can delete workspace', async () => {
-       const workspacesPage = new WorkspacesPage(page);
-       const workspaceCard = await findWorkspace(page);
-       const workspaceName = await workspaceCard.getWorkspaceName();
-       console.log(workspaceName);
+      const workspacesPage = new WorkspacesPage(page);
+      const workspaceCard = await findWorkspace(page);
+      const workspaceName = await workspaceCard.getWorkspaceName();
+      console.log(workspaceName);
 
       // Verify Workspace Access Level is OWNER.
-       const accessLevel = await workspaceCard.getWorkspaceAccessLevel();
-       expect(accessLevel).toBe(WorkspaceAccessLevel.Owner);
+      const accessLevel = await workspaceCard.getWorkspaceAccessLevel();
+      expect(accessLevel).toBe(WorkspaceAccessLevel.Owner);
       
       await workspaceCard.asElementHandle().hover();
       // click on Ellipsis "Delete"
       await (workspaceCard.getEllipsis()).clickAction(EllipsisMenuAction.Delete, { waitForNav: false });
 
       const modal = new Modal(page);
-      //get the modal content
+      // get the modal content
       await modal.getContent();
-      //click on the Delete workspace button
+      // click on the Delete workspace button
       await modal.clickButton(LinkText.DeleteWorkspace);
       await workspacesPage.isLoaded(); 
 
@@ -50,16 +50,16 @@ describe('Delete workspace', () => {
   });   
 
    /**
-   * Test:
-   * - Find an existing workspace. Create a new workspace if none exists.
-   * - Click on the workspace card
-   * - Navigate to the Data Page
-   * - Select "Delete" thru the Ellipsis menu located inside the Workspace Action menu
-   * - Get the content on the modal
-   * - Click the delete workspace button
-   * - Navigate to the "View Workspaces" page
-   * - Verify that the workspace has been delete
-   **/
+    * Test:
+    * - Find an existing workspace. Create a new workspace if none exists.
+    * - Click on the workspace card
+    * - Navigate to the Data Page
+    * - Select "Delete" thru the Ellipsis menu located inside the Workspace Action menu
+    * - Get the content on the modal
+    * - Click the delete workspace button
+    * - Navigate to the "View Workspaces" page
+    * - Verify that the workspace has been delete
+    */
 
   // describe.('From "Data" page using side ellipsis menu', () => {
 
