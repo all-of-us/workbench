fee026949742446d2af90c63895c34dccc0e863f
Eric Song <me@ericsong.io>
2020-08-26 20:41:41 -0400

    [RW-5312][risk=no] Add genomics microarray field to cdrVersion (#3916)
    * add genomics microarray field to cdrVersion
    
    * add microarry field to CdrVersion
    
    * expose microarray field in API, add test, convert to mapstruct
    
    * spotless
    
    * config field fixe
    
    * convert microarray bq string to boolean when exposed to API
    
    * spotless
    
    * add empty string check

---
+++ api/src/main/java/org/pmiops/workbench/cdr/CdrVersionMapper.java
@@ -0,0 +1,23 @@
+package org.pmiops.workbench.cdr;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+import org.pmiops.workbench.db.model.DbCdrVersion;
+import org.pmiops.workbench.model.CdrVersion;
+import org.pmiops.workbench.utils.mappers.CommonMappers;
+import org.pmiops.workbench.utils.mappers.MapStructConfig;
+
+@Mapper(
+    config = MapStructConfig.class,
+    uses = {CommonMappers.class})
+public interface CdrVersionMapper {
+
+  @Mapping(source = "microarrayBigqueryDataset", target = "hasMicroarrayData")
+  @Mapping(source = "dataAccessLevelEnum", target = "dataAccessLevel")
+  @Mapping(source = "archivalStatusEnum", target = "archivalStatus")
+  CdrVersion dbModelToClient(DbCdrVersion db);
+
+  default boolean isNonEmpty(String s) {
+    return s != null && !s.isEmpty();
+  }
+}
