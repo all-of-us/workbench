370d9e599d2d47769c807b95c46a2351777d6f56
Joel Thibault <thibault@broad.dev>
2020-09-10 10:50:06 -0400

    fix imports

--- api/src/test/java/org/pmiops/workbench/workspaces/WorkspaceServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/workspaces/WorkspaceServiceTest.java
@@ -1,7 +1,10 @@
 package org.pmiops.workbench.workspaces;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.Mockito.*;
+import static org.mockito.Mockito.doReturn;
+import static org.mockito.Mockito.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
 
 import java.sql.Timestamp;
 import java.time.Clock;
