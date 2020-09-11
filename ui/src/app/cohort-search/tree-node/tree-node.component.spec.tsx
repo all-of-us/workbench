28f2caa6c2d62038b2d7f1db9a68cecfc9862588
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2020-09-11 08:58:32 -0400

    [RW-5445][risk=no]UI: Add "versions" indicator for COPE in survey hierarchy (#3983)
    * Add text Versioned to COPE Survey
    
    * Remove Comment
    
    * PR Comments: use concept Id and name check
    
    * Fix UI Test

--- ui/src/app/cohort-search/tree-node/tree-node.component.spec.tsx
+++ ui/src/app/cohort-search/tree-node/tree-node.component.spec.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 
 import {registerApiClient} from 'app/services/swagger-fetch-clients';
 import {currentWorkspaceStore, serverConfigStore} from 'app/utils/navigation';
-import {CohortBuilderApi} from 'generated/fetch';
+import {CohortBuilderApi, DomainType} from 'generated/fetch';
 import defaultServerConfig from 'testing/default-server-config';
 import {CohortBuilderServiceStub} from 'testing/stubs/cohort-builder-service-stub';
 import {workspaceDataStub} from 'testing/stubs/workspaces-api-stub';
@@ -23,6 +23,23 @@ const treeNodeStub = {
   predefinedAttributes: null,
   selectable: true,
   subtype: 'HEIGHT',
+  type: 'PM'
+} as NodeProp;
+
+const surveyCOPETreeNodeStub = {
+  children: [],
+  code: '',
+  conceptId: 1333342,
+  count: 0,
+  domainId: DomainType.SURVEY.toString(),
+  group: true,
+  hasAttributes: false,
+  id: 328232,
+  name: 'COVID-19 Participant Experience (COPE) Survey',
+  parentId: 0,
+  predefinedAttributes: null,
+  selectable: true,
+  subtype: 'SURVEY',
   type: 'PM'
 } as NodeProp;
 describe('TreeNode', () => {
@@ -47,5 +64,17 @@ describe('TreeNode', () => {
                                       selectedIds={[]}
                                       setAttributes={() => {}}/>);
     expect(wrapper).toBeTruthy();
+  });
+  it('should display Versioned if SURVEY is COPE', () => {
+    const wrapper = mount(<TreeNode autocompleteSelection={undefined}
+                                    groupSelections={[]}
+                                    node={surveyCOPETreeNodeStub}
+                                    scrollToMatch={() => {}}
+                                    searchTerms={''}
+                                    select={() => {}}
+                                    selectedIds={[]}
+                                    setAttributes={() => {}}/>);
+    expect(wrapper).toBeTruthy();
+    expect(wrapper.find('[data-test-id="displayName"]').text()).toContain('COVID-19 Participant Experience (COPE) Survey -  Versioned');
   });
 });
