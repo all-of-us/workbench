0452c2645c70dc2df3f6707a351205a703d125a0
CH Albach <calbach@users.noreply.github.com>
2020-09-17 15:52:21 -0700

    [RW-5402][risk=no] Read-only sidebar runtime panel (#4007)
    * Add readonly runtime panel
    
    * Fix compile
    
    * comments, feedback

--- ui/src/app/components/help-sidebar.spec.tsx
+++ ui/src/app/components/help-sidebar.spec.tsx
@@ -57,7 +57,7 @@ describe('HelpSidebar', () => {
     props = {helpContentKey: 'notebookStorage', sidebarOpen: true};
     const wrapper = component();
     expect(wrapper.find('[data-test-id="section-title-0"]').text()).toBe(sidebarContent.notebookStorage[0].title);
-    expect(wrapper.find('[data-test-id="help-sidebar-icon-help"]').get(0).props.icon.iconName).toBe('folder-open');
+    expect(wrapper.find('[data-test-id="help-sidebar-icon-notebooksHelp"]').get(0).props.icon.iconName).toBe('folder-open');
   });
 
   it('should update marginRight style when sidebarOpen prop changes', () => {
@@ -85,6 +85,7 @@ describe('HelpSidebar', () => {
     wrapper.find({'data-test-id': 'Share-menu-item'}).first().simulate('click');
     expect(shareSpy).toHaveBeenCalled();
   });
+
   it('should hide workspace icon if on critera search page', async() => {
     const wrapper = component();
     currentCohortCriteriaStore.next([]);
@@ -92,20 +93,23 @@ describe('HelpSidebar', () => {
     expect(wrapper.find({'data-test-id': 'workspace-menu-button'}).length).toBe(0);
     expect(wrapper.find({'data-test-id': 'criteria-count'}).length).toBe(1);
   });
+
   it('should update count if criteria is added', async() => {
     const wrapper = component();
     currentCohortCriteriaStore.next([criteria1, criteria2]);
     await waitOneTickAndUpdate(wrapper);
     expect(wrapper.find({'data-test-id': 'criteria-count'}).first().props().children).toBe(2);
   });
-  it('should not display cluster control icon for read-only workspaces', () => {
+
+  it('should not display runtime control icon for read-only workspaces', () => {
     props = {workspace: {accessLevel: WorkspaceAccessLevel.READER}}
     const wrapper = component();
-    expect(wrapper.find({'data-test-id': 'help-sidebar-icon-thunderstorm'}).length).toBe(0);
+    expect(wrapper.find({'data-test-id': 'help-sidebar-icon-runtime'}).length).toBe(0);
   });
-  it('should display cluster control icon for writable workspaces', () => {
+
+  it('should display runtime control icon for writable workspaces', () => {
     props = {workspace: {accessLevel: WorkspaceAccessLevel.WRITER}}
     const wrapper = component();
-    expect(wrapper.find({'data-test-id': 'help-sidebar-icon-thunderstorm'}).length).toBe(1);
+    expect(wrapper.find({'data-test-id': 'help-sidebar-icon-runtime'}).length).toBe(1);
   });
 });
