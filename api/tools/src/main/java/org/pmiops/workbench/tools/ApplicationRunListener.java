0f561c7e735c961f39755bbf613e2212a08ce9a7
Gregory Jordan <gjordan@verily.com>
2020-09-21 09:18:05 -0400

    Add some comments and use a typed reference to the Request scope magic string.

--- api/tools/src/main/java/org/pmiops/workbench/tools/ApplicationRunListener.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/ApplicationRunListener.java
@@ -5,6 +5,7 @@ import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
 import org.springframework.context.support.SimpleThreadScope;
 import org.springframework.core.env.ConfigurableEnvironment;
+import org.springframework.web.context.WebApplicationContext;
 
 /**
  * A custom application listener to allow us to mutate the application context before any
@@ -13,7 +14,9 @@ import org.springframework.core.env.ConfigurableEnvironment;
  * <p>This class currently serves a single purpose, which is to create a fake "request" bean scope
  * to allow command-line tools to load RW beans that are request-scoped for use in the main webapp.
  *
- * <p>See https://stackoverflow.com/a/28275111 which is the pattern we roughly followed here.
+ * <p>See https://stackoverflow.com/a/28275111 which is the pattern we roughly followed here. This
+ * class is referenced from resources/META-INF/spring.factories, which causes this class to be
+ * loaded before the Spring context is initialized.
  */
 public class ApplicationRunListener implements SpringApplicationRunListener {
 
@@ -43,7 +46,9 @@ public class ApplicationRunListener implements SpringApplicationRunListener {
    */
   @Override
   public void contextPrepared(ConfigurableApplicationContext context) {
-    context.getBeanFactory().registerScope("request", new SimpleThreadScope());
+    context
+        .getBeanFactory()
+        .registerScope(WebApplicationContext.SCOPE_REQUEST, new SimpleThreadScope());
   }
 
   /**
