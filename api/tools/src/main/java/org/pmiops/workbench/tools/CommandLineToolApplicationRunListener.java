13c3ffd798c51d84fe61117fe3707743c7ca044f
Gregory Jordan <gjordan@verily.com>
2020-09-21 21:51:52 -0400

    PR feedback

--- api/tools/src/main/java/org/pmiops/workbench/tools/CommandLineToolApplicationRunListener.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/CommandLineToolApplicationRunListener.java
@@ -1,5 +1,6 @@
 package org.pmiops.workbench.tools;
 
+import java.util.logging.Logger;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.SpringApplicationRunListener;
 import org.springframework.context.ConfigurableApplicationContext;
@@ -18,23 +19,16 @@ import org.springframework.web.context.WebApplicationContext;
  * class is referenced from resources/META-INF/spring.factories, which causes this class to be
  * loaded before the Spring context is initialized.
  */
-public class ApplicationRunListener implements SpringApplicationRunListener {
+public class CommandLineToolApplicationRunListener implements SpringApplicationRunListener {
 
-  public ApplicationRunListener(SpringApplication application, String[] args) {}
+  private static final Logger log =
+      Logger.getLogger(CommandLineToolApplicationRunListener.class.getName());
 
-  /**
-   * Called immediately when the run method has first started. Can be used for very early
-   * initialization.
-   */
+  public CommandLineToolApplicationRunListener(SpringApplication application, String[] args) {}
+
   @Override
   public void starting() {}
 
-  /**
-   * Called once the environment has been prepared, but before the {@link ApplicationContext} has
-   * been created.
-   *
-   * @param environment the environment
-   */
   @Override
   public void environmentPrepared(ConfigurableEnvironment environment) {}
 
@@ -46,26 +40,15 @@ public class ApplicationRunListener implements SpringApplicationRunListener {
    */
   @Override
   public void contextPrepared(ConfigurableApplicationContext context) {
+    log.info("Registering fake REQUEST scope to the context bean factory for command-line tools");
     context
         .getBeanFactory()
         .registerScope(WebApplicationContext.SCOPE_REQUEST, new SimpleThreadScope());
   }
 
-  /**
-   * Called once the application context has been loaded but before it has been refreshed.
-   *
-   * @param context the application context
-   */
   @Override
   public void contextLoaded(ConfigurableApplicationContext context) {}
 
-  /**
-   * Called immediately before the run method finishes.
-   *
-   * @param context the application context or null if a failure occurred before the context was
-   *     created
-   * @param exception any run exception or null if run completed successfully.
-   */
   @Override
   public void finished(ConfigurableApplicationContext context, Throwable exception) {}
 }
