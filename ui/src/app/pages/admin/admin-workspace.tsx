604a3cacd1a6768461703eb94bb88c8c2a23d155
Joel Thibault <thibault@broad.dev>
2020-09-23 08:48:04 -0400

    Add preview links to file table

--- ui/src/app/pages/admin/admin-workspace.tsx
+++ ui/src/app/pages/admin/admin-workspace.tsx
@@ -6,12 +6,14 @@ import * as React from 'react';
 
 import {Button} from 'app/components/buttons';
 import {FlexColumn, FlexRow} from 'app/components/flex';
-import {Error as ErrorDiv} from 'app/components/inputs';
+import {Error as ErrorDiv, TextArea} from 'app/components/inputs';
 import {Modal, ModalBody, ModalFooter, ModalTitle} from 'app/components/modals';
+import {TooltipTrigger} from 'app/components/popups';
 import {SpinnerOverlay} from 'app/components/spinners';
 import {runtimeApi, workspaceAdminApi} from 'app/services/swagger-fetch-clients';
 import colors from 'app/styles/colors';
 import {reactStyles, ReactWrapperBase, UrlParamsProps, withUrlParams} from 'app/utils';
+import {navigate} from 'app/utils/navigation';
 import {
   getSelectedPopulations,
   getSelectedPrimaryPurposeItems
@@ -24,7 +26,7 @@ import {
 } from 'generated/fetch';
 import {Column} from 'primereact/column';
 import {DataTable} from 'primereact/datatable';
-import {ReactFragment} from 'react';
+import {ReactFragment, useEffect, useState} from 'react';
 
 const styles = reactStyles({
   infoRow: {
@@ -53,6 +55,14 @@ const styles = reactStyles({
     maxWidth: '1000px',
     marginTop: '1rem',
   },
+  textArea: {
+    maxWidth: '1000px',
+    height: '3rem',
+  },
+  button: {
+    marginLeft: '20px',
+    height: '1rem',
+  },
 });
 
 const PurpleLabel = ({style = {}, children}) => {
@@ -77,14 +87,27 @@ const formatMB = (fileSize: number): string => {
   }
 };
 
-const FileDetailsTable = (props: {data: Array<FileDetail>, bucket: string}) => {
-  const {data, bucket} = props;
+const NOTEBOOKS_DIRECTORY = 'notebooks';
+const NOTEBOOKS_SUFFIX = '.ipynb';
+
+interface FileDetailsProps {
+  workspaceNamespace: string;
+  data: Array<FileDetail>;
+  bucket: string;
+}
+
+const FileDetailsTable = (props: FileDetailsProps) => {
+  const {workspaceNamespace, data, bucket} = props;
 
   interface TableEntry {
-    name: string;
-    size: string;
     location: string;
+    rawName: string;
+    nameCell: ReactFragment;
+    size: string;
   }
+
+  const [tableData, setTableData] = useState<Array<TableEntry>>();
+  const [accessReason, setAccessReason] = useState('');
 
   const parseLocation = (file: FileDetail): string => {
     const prefixLength = bucket.length;
@@ -95,35 +118,73 @@ const FileDetailsTable = (props: {data: Array<FileDetail>, bucket: string}) => {
     return file.path.substring(start, end);
   };
 
-  const formattedData: Array<TableEntry> = data.map(file => {
-    return {
-      name: file.name,
-      size: formatMB(file.sizeInBytes),
-      location: parseLocation(file)
+  const nameCell = (file: FileDetail): React.ReactFragment => {
+    const filename = file.name.trim();
+    const navigateToPreview = () => {
+      navigate(['admin', 'workspaces', workspaceNamespace, filename],
+          { queryParams: { accessReason: accessReason } });
     };
-  });
 
-  const sortedData = formattedData.sort((a, b) => {
-    const locCmp = a.location.localeCompare(b.location);
-    if (locCmp === 0) {
-      return a.name.localeCompare(b.name);
+    // remove first check after RW-5626
+    if (NOTEBOOKS_DIRECTORY === parseLocation(file) && filename.endsWith(NOTEBOOKS_SUFFIX)) {
+      return <FlexRow>
+        <span>{filename}</span>
+        <TooltipTrigger content='Please enter an access reason below' disabled={accessReason && accessReason.trim()}>
+          <Button style={styles.button} disabled={!accessReason || !accessReason.trim()} onClick={navigateToPreview}>Preview</Button>
+        </TooltipTrigger>
+      </FlexRow>;
     } else {
-      return locCmp;
+      return <span>{filename}</span>;
     }
-  });
+  };
 
-  return <DataTable
-      data-test-id='object-details-table'
-      value={sortedData}
-      style={styles.fileDetailsTable}
-      scrollable={true}
-      paginator={true}
-      paginatorTemplate='CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown'
-      currentPageReportTemplate='Showing {first} to {last} of {totalRecords} entries'>
-    <Column field='location' header='Location'/>
-    <Column field='name' header='Filename'/>
-    <Column field='size' header='File size (MB)' style={{textAlign: 'right'}}/>
-  </DataTable>;
+  const formatMB = (fileSize: number): string => {
+    const mb = fileSize / 1000000.0;
+    if (mb < 1.0) {
+      return '<1';
+    } else {
+      return mb.toFixed(2);
+    }
+  };
+
+  const setupTable = () => {
+    setTableData(data
+        .map(file => {
+          return {
+            location: parseLocation(file),
+            rawName: file.name,
+            nameCell: nameCell(file),
+            size: formatMB(file.sizeInBytes),
+          }; })
+        .sort((a, b) => {
+          const locCmp = a.location.localeCompare(b.location);
+          if (locCmp === 0) {
+            return a.rawName.localeCompare(b.rawName);
+          } else {
+            return locCmp;
+          }}));
+  };
+
+  useEffect(() => {
+    setupTable();
+  }, [accessReason]);
+
+  return <FlexColumn>
+    <DataTable
+        data-test-id='object-details-table'
+        value={tableData}
+        style={styles.fileDetailsTable}
+        scrollable={true}
+        paginator={true}
+        paginatorTemplate='CurrentPageReport FirstPageLink PrevPageLink PageLinks NextPageLink LastPageLink RowsPerPageDropdown'
+        currentPageReportTemplate='Showing {first} to {last} of {totalRecords} entries'>
+      <Column field='location' header='Location'/>
+      <Column field='nameCell' header='Filename'/>
+      <Column field='size' header='File size (MB)' style={{textAlign: 'right'}}/>
+    </DataTable>
+    <PurpleLabel>To preview notebooks, enter Access Reason (for auditing purposes)</PurpleLabel>
+    <TextArea style={styles.textArea} value={accessReason} onChange={setAccessReason}/>
+  </FlexColumn>;
 };
 
 interface State {
@@ -320,7 +381,10 @@ class AdminWorkspaceImpl extends React.Component<UrlParamsProps, State> {
               {formatMB(resources.cloudStorage.storageBytesUsed)}
             </WorkspaceInfoField>
           </div>
-          {files && <FileDetailsTable data={files} bucket={resources.cloudStorage.storageBucketPath}/>}
+          {files && <FileDetailsTable
+              workspaceNamespace={workspace.namespace}
+              data={files}
+              bucket={resources.cloudStorage.storageBucketPath}/>}
           <h3>Research Purpose</h3>
           <div className='research-purpose' style={{marginTop: '1rem'}}>
             <WorkspaceInfoField labelText='Primary purpose of project'>
