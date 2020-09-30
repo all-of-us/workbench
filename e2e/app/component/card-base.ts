d5e2ba60b8b416a728c9ef0ccfdf85c368e57fdc
aweng98 <bweng@broadinstitute.org>
2020-09-30 16:08:19 -0400

    fixed error

--- e2e/app/component/card-base.ts
+++ e2e/app/component/card-base.ts
@@ -16,7 +16,7 @@ export default abstract class CardBase extends Container {
   }
 
   async clickSnowmanIcon(): Promise<this> {
-    const iconXpath = `.$${snowmanIconXpath}`
+    const iconXpath = `.${snowmanIconXpath}`
     const [snowmanIcon] = await this.asElementHandle().$x(iconXpath)
     await snowmanIcon.click();
     await snowmanIcon.dispose();
