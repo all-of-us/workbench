e7b84f136ec6498ef596f9d72386e4014dbc84a2
Joel Thibault <thibault@broad.dev>
2020-10-13 13:37:07 -0400

    refactor copy-notebook test

--- e2e/tests/notebook/owner-copy-to-workspace.spec.ts
+++ e2e/tests/notebook/owner-copy-to-workspace.spec.ts
@@ -3,10 +3,57 @@ import Modal from 'app/component/modal';
 import WorkspaceDataPage from 'app/page/workspace-data-page';
 import {LinkText, ResourceCard} from 'app/text-labels';
 import {makeRandomName} from 'utils/str-utils';
-import {findWorkspace, signIn} from 'utils/test-utils';
+import {createWorkspace, findWorkspace, signIn} from 'utils/test-utils';
+import {config} from 'resources/workbench-config';
 
 // Notebook server start may take a long time. Set maximum test running time to 20 minutes.
 jest.setTimeout(20 * 60 * 1000);
+
+async function copyNotebook(srcCdrVersionName: string, destCdrVersionName: string) {
+  const destWorkspace = await createWorkspace(page, destCdrVersionName).then(card => card.getWorkspaceName());
+
+  await createWorkspace(page, srcCdrVersionName).then(card => card.clickWorkspaceName());
+
+  // Create notebook in copy-from workspace.
+  const sourceNotebookName = makeRandomName('pytest');
+  const dataPage = new WorkspaceDataPage(page);
+
+  const sourceWorkspacePage = await dataPage.createNotebook(sourceNotebookName);
+
+  // Exit notebook and returns to the Workspace Analysis tab.
+  const analysisPage = await sourceWorkspacePage.goAnalysisPage();
+
+  // Copy to destination Workspace and give notebook a new name.
+  const copiedNotebookName = makeRandomName('copy-of');
+  await analysisPage.copyNotebookToWorkspace(sourceNotebookName, destWorkspace, copiedNotebookName);
+
+  // Verify Copy Success modal.
+  const modal = new Modal(page);
+  await modal.waitForButton(LinkText.GoToCopiedNotebook);
+  const textContent = await modal.getTextContent();
+  const successMsg = `Successfully copied ${sourceNotebookName}  to ${destWorkspace} . Do you want to view the copied Notebook?`;
+  expect(textContent).toContain(successMsg);
+  // Dismiss modal.
+  await modal.clickButton(LinkText.StayHere, {waitForClose: true});
+
+  // Delete notebook
+  const deleteModalTextContent = await analysisPage.deleteResource(sourceNotebookName, ResourceCard.Notebook);
+  expect(deleteModalTextContent).toContain(`Are you sure you want to delete Notebook: ${sourceNotebookName}?`);
+
+  // Perform actions in copied notebook.
+  // Open destination Workspace
+  await findWorkspace(page, {workspaceName: destWorkspace}).then(card => card.clickWorkspaceName());
+
+  // Verify copy-to notebook exists in destination Workspace
+  await dataPage.openAnalysisPage();
+  const dataResourceCard = new DataResourceCard(page);
+  const notebookCard = await dataResourceCard.findCard(copiedNotebookName, ResourceCard.Notebook);
+  expect(notebookCard).toBeTruthy();
+
+  // Delete notebook
+  const modalTextContent = await analysisPage.deleteResource(copiedNotebookName, ResourceCard.Notebook);
+  expect(modalTextContent).toContain('This will permanently delete the Notebook.');
+}
 
 describe('Workspace owner Jupyter notebook action tests', () => {
 
@@ -24,52 +71,8 @@ describe('Workspace owner Jupyter notebook action tests', () => {
    * - Open copied notebook and run code to print WORKSPACE_NAMESPACE. It should match destination Workspace namespace.
    * - Delete notebooks.
    */
-  test('Copy notebook to another Workspace', async () => {
-    // Create destination workspace
-    const toWorkspace = await findWorkspace(page, {create: true}).then(card => card.getWorkspaceName());
-
-    // Create copy-from workspace
-    await findWorkspace(page, {create: true}).then(card => card.clickWorkspaceName());
-
-    // Create notebook in copy-from workspace.
-    const copyFromNotebookName = makeRandomName('pytest');
-    const dataPage = new WorkspaceDataPage(page);
-
-    const copyFromNotebookPage = await dataPage.createNotebook(copyFromNotebookName);
-
-    // Exit notebook and returns to the Workspace Analysis tab.
-    const analysisPage = await copyFromNotebookPage.goAnalysisPage();
-
-    // Copy to destination Workspace and give notebook a new name.
-    const copiedNotebookName = makeRandomName('copy-of');
-    await analysisPage.copyNotebookToWorkspace(copyFromNotebookName, toWorkspace, copiedNotebookName);
-
-    // Verify Copy Success modal.
-    const modal = new Modal(page);
-    await modal.waitForButton(LinkText.GoToCopiedNotebook);
-    const textContent = await modal.getTextContent();
-    const successMsg = `Successfully copied ${copyFromNotebookName}  to ${toWorkspace} . Do you want to view the copied Notebook?`;
-    expect(textContent).toContain(successMsg);
-    // Dismiss modal.
-    await modal.clickButton(LinkText.StayHere, {waitForClose: true});
-
-    // Delete notebook
-    const deleteModalTextContent = await analysisPage.deleteResource(copyFromNotebookName, ResourceCard.Notebook);
-    expect(deleteModalTextContent).toContain(`Are you sure you want to delete Notebook: ${copyFromNotebookName}?`);
-
-    // Perform actions in copied notebook.
-    // Open destination Workspace
-    await findWorkspace(page, {workspaceName: toWorkspace}).then(card => card.clickWorkspaceName());
-
-    // Verify copy-to notebook exists in destination Workspace
-    await dataPage.openAnalysisPage();
-    const dataResourceCard = new DataResourceCard(page);
-    const notebookCard = await dataResourceCard.findCard(copiedNotebookName, ResourceCard.Notebook);
-    expect(notebookCard).toBeTruthy();
-
-    // Delete notebook
-    const modalTextContent = await analysisPage.deleteResource(copiedNotebookName, ResourceCard.Notebook);
-    expect(modalTextContent).toContain('This will permanently delete the Notebook.');
+  test('Copy notebook to another Workspace when CDR versions match', async () => {
+    await copyNotebook(config.defaultCdrVersionName, config.defaultCdrVersionName);
   })
 
 });
