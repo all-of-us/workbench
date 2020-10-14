9f244c988480367014dec4388c3171dcac5a1e4c
Joel Thibault <thibault@broad.dev>
2020-10-13 22:05:56 -0400

    Actually fail the R test when env vars are missing

--- e2e/resources/r-code/sys-print.R
+++ e2e/resources/r-code/sys-print.R
@@ -1,7 +1,19 @@
-print(Sys.getenv('OWNER_EMAIL'))
-print(Sys.getenv('WORKSPACE_CDR'))
-print(Sys.getenv('WORKSPACE_NAMESPACE'))
-print(Sys.getenv('GOOGLE_PROJECT')) # same as WORKSPACE_NAMESPACE
-print(Sys.getenv('CLUSTER_NAME'))
-print(Sys.getenv('WORKSPACE_BUCKET'))
-print('success',quote=FALSE)
+print_env_var <- function(var) {
+    value <- Sys.getenv(var, unset=NA)
+    print(value)
+    return(value)
+}
+
+vars <- c('OWNER_EMAIL',
+          'WORKSPACE_CDR',
+          'WORKSPACE_NAMESPACE',
+          'GOOGLE_PROJECT', # same as WORKSPACE_NAMESPACE
+          'CLUSTER_NAME',
+          'WORKSPACE_BUCKET')
+
+vals <- sapply(vars, print_env_var)
+
+# only print success if all env vars are present
+if (!anyNA(vals)) {
+    print('success',quote=FALSE)
+}
