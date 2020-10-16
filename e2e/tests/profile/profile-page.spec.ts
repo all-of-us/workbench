2fe54c2e137d53e118c898711350fa499af93ed8
aweng98 <bweng@broadinstitute.org>
2020-10-16 17:24:40 -0400

    fail profile-page.spec.ts

--- e2e/tests/profile/profile-page.spec.ts
+++ e2e/tests/profile/profile-page.spec.ts
@@ -1,6 +1,5 @@
 import ProfilePage, {MissingErrorAlias} from 'app/page/profile-page';
 import {signIn} from 'utils/test-utils';
-import navigation, {NavLink} from 'app/component/navigation';
 import {makeString, makeUrl} from 'utils/str-utils';
 import Button from 'app/element/button';
 
@@ -29,7 +28,7 @@ describe('Profile', () => {
 
   beforeEach(async () => {
     await signIn(page);
-    await navigation.navMenu(page, NavLink.PROFILE);
+    // await navigation.navMenu(page, NavLink.PROFILE);
     profilePage = new ProfilePage(page);
     await profilePage.waitForLoad();
 
