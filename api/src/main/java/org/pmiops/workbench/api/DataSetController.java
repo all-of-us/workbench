10a56b400b9bd9b0424872360068b6d6a4a2c042
Eric Song <me@ericsong.io>
2020-10-16 12:32:58 -0400

    add cdr version requirements check

--- api/src/main/java/org/pmiops/workbench/api/DataSetController.java
+++ api/src/main/java/org/pmiops/workbench/api/DataSetController.java
@@ -34,6 +34,7 @@ import org.pmiops.workbench.db.model.DbCdrVersion;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbWorkspace;
 import org.pmiops.workbench.exceptions.BadRequestException;
+import org.pmiops.workbench.exceptions.FailedPreconditionException;
 import org.pmiops.workbench.exceptions.NotFoundException;
 import org.pmiops.workbench.firecloud.FireCloudService;
 import org.pmiops.workbench.firecloud.model.FirecloudWorkspaceResponse;
@@ -339,6 +340,9 @@ public class DataSetController implements DataSetApiDelegate {
             queriesByDomain);
 
     if (GenomicsDataTypeEnum.MICROARRAY.equals(dataSetExportRequest.getGenomicsDataType())) {
+      if (dbWorkspace.getCdrVersion().getMicroarrayBigqueryDataset() == null) {
+        throw new FailedPreconditionException("The workspace CDR version does not have microarray data");
+      }
       if (!dataSetExportRequest.getKernelType().equals(KernelTypeEnum.PYTHON)) {
         throw new BadRequestException("Genomics code generation is only supported in Python");
       }
