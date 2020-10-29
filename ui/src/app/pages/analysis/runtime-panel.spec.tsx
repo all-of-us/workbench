4d7ca59b7056cb18b15be8b3e1c98b62db754510
CH Albach <calbach@google.com>
2020-10-28 22:52:05 -0700

    Initial tests

--- ui/src/app/pages/analysis/runtime-panel.spec.tsx
+++ ui/src/app/pages/analysis/runtime-panel.spec.tsx
@@ -7,13 +7,14 @@ import {Spinner} from 'app/components/spinners';
 import {RuntimePanel, Props} from 'app/pages/analysis/runtime-panel';
 import {workspaceStubs} from 'testing/stubs/workspaces-api-stub';
 import {registerApiClient} from 'app/services/swagger-fetch-clients';
+import defaultServerConfig from 'testing/default-server-config';
 import {waitOneTickAndUpdate} from 'testing/react-test-helpers';
 import {RuntimeApiStub} from 'testing/stubs/runtime-api-stub';
 import {RuntimeApi} from 'generated/fetch/api';
 import {WorkspaceAccessLevel} from 'generated/fetch';
 import {runtimeStore} from 'app/utils/stores';
 import {cdrVersionListResponse, CdrVersionsStubVariables} from 'testing/stubs/cdr-versions-api-stub';
-import {cdrVersionStore} from 'app/utils/navigation';
+import {cdrVersionStore, serverConfigStore} from 'app/utils/navigation';
 
 
 
@@ -37,6 +38,7 @@ describe('RuntimePanel', () => {
 
   beforeEach(() => {
     cdrVersionStore.next(cdrVersionListResponse);
+    serverConfigStore.next({...defaultServerConfig, enableCustomRuntimes: true});
     runtimeApiStub = new RuntimeApiStub();
     runtimeApiStub.runtime.dataprocConfig = null;
     registerApiClient(RuntimeApi, runtimeApiStub);
@@ -65,7 +67,8 @@ describe('RuntimePanel', () => {
     expect(!wrapper.exists(Spinner));
   });
 
-  it('should show the create button when no runtime exists', async() => {
+  it('should allow creation when no runtime exists with defaults', async() => {
+    runtimeApiStub.runtime = null;
     runtimeStore.set({runtime: null, workspaceNamespace: workspaceStubs[0].namespace});
 
     const wrapper = component();
@@ -75,6 +78,93 @@ describe('RuntimePanel', () => {
     const createButton = wrapper.find(Button).find({'aria-label': 'Create'}).first();
     expect(createButton.exists()).toBeTruthy();
     expect(createButton.prop('disabled')).toBeFalsy();
+
+    act(() => { createButton.simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+
+    expect(runtimeApiStub.runtime.status).toEqual('Creating');
+    expect(runtimeApiStub.runtime.gceConfig.machineType).toEqual('n1-standard-4');
+  });
+
+  it('should allow creation with GCE config', async() => {
+    runtimeApiStub.runtime = null;
+    runtimeStore.set({runtime: null, workspaceNamespace: workspaceStubs[0].namespace});
+
+    const wrapper = component();
+    await handleUseEffect(wrapper);
+    await waitOneTickAndUpdate(wrapper);
+
+    act(() => { wrapper.find('#runtime-cpu').first().simulate('click') });
+    act(() => { wrapper.find('#runtime-cpu .p-dropdown-item').find({'aria-label': 8}).first().simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+
+    act(() => { wrapper.find('#runtime-ram').first().find('.p-dropdown-item').first().simulate('click') });
+    act(() => { wrapper.find('#runtime-ram .p-dropdown-item').find({'aria-label': 52}).first().simulate('click') });
+
+    // TODO: Find a way to invoke this without props.
+    act(() => { wrapper.find('*[id="runtime-disk"]').first().prop('onChange')({value: 75} as any)});
+
+    const createButton = wrapper.find(Button).find({'aria-label': 'Create'}).first();
+    expect(createButton.exists()).toBeTruthy();
+    expect(createButton.prop('disabled')).toBeFalsy();
+
+    act(() => { createButton.simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+
+    expect(runtimeApiStub.runtime.status).toEqual('Creating');
+    expect(runtimeApiStub.runtime.gceConfig.machineType).toEqual('n1-highmem-8');
+    expect(runtimeApiStub.runtime.gceConfig.diskSize).toEqual(75);
+    expect(runtimeApiStub.runtime.dataprocConfig).toBeFalsy();
+  });
+
+  it('should allow creation with Dataproc config', async() => {
+    runtimeApiStub.runtime = null;
+    runtimeStore.set({runtime: null, workspaceNamespace: workspaceStubs[0].namespace});
+
+    const wrapper = component();
+    await handleUseEffect(wrapper);
+    await waitOneTickAndUpdate(wrapper);
+
+    // master settings
+    act(() => { wrapper.find('#runtime-cpu').first().simulate('click') });
+    act(() => { wrapper.find('#runtime-cpu .p-dropdown-item').find({'aria-label': 2}).first().simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+    act(() => { wrapper.find('#runtime-ram').first().find('.p-dropdown-item').first().simulate('click') });
+    act(() => { wrapper.find('#runtime-ram .p-dropdown-item').find({'aria-label': 7.5}).first().simulate('click') });
+    act(() => { wrapper.find('#runtime-disk').first().prop('onChange')({value: 100} as any)});
+
+    // pick Dataproc
+    act(() => { wrapper.find('#runtime-compute').first().simulate('click') });
+    act(() => { wrapper.find('#runtime-compute .p-dropdown-item').find({'aria-label': 'Dataproc Cluster'}).first().simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+
+    // worker settings
+    act(() => { wrapper.find('#worker-cpu').first().simulate('click') });
+    act(() => { wrapper.find('#worker-cpu .p-dropdown-item').find({'aria-label': 8}).first().simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+    wrapper.find('#worker-ram').first().find('.p-dropdown-item').first().simulate('click');
+    wrapper.find('#worker-ram .p-dropdown-item').find({'aria-label': 30}).first().simulate('click');
+    act(() => { wrapper.find('#worker-disk').first().prop('onChange')({value: 300} as any)});
+    act(() => { wrapper.find('#num-workers').first().prop('onChange')({value: 10} as any)});
+    act(() => { wrapper.find('#num-preemptible').first().prop('onChange')({value: 20} as any)});
+
+    const createButton = wrapper.find(Button).find({'aria-label': 'Create'}).first();
+    expect(createButton.exists()).toBeTruthy();
+    expect(createButton.prop('disabled')).toBeFalsy();
+
+    act(() => { createButton.simulate('click') });
+    await waitOneTickAndUpdate(wrapper);
+
+    expect(runtimeApiStub.runtime.status).toEqual('Creating');
+    expect(runtimeApiStub.runtime.dataprocConfig).toEqual({
+      masterMachineType: 'n1-standard-2',
+      masterDiskSize: 100,
+      workerMachineType: 'n1-standard-8',
+      workerDiskSize: 300,
+      numberOfWorkers: 10,
+      numberOfPreemptibleWorkers: 20
+    });
+    expect(runtimeApiStub.runtime.gceConfig).toBeFalsy();
   });
 
   it('should restrict memory options by cpu', async() => {
