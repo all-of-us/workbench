6e13479e8e50ed4cb394c57a6ebc9b5d7dde9381
aweng98 <bweng@broadinstitute.org>
2020-11-05 23:08:11 -0500

    test

--- e2e/jest.test-setup.ts
+++ e2e/jest.test-setup.ts
@@ -47,7 +47,7 @@ beforeAll(async () => {
   if (isDebugMode) {
     // Emitted when a request failed. Warning: blocked requests from above will be logged as failed requests, safe to ignore these.
     page.on('requestfailed', request => {
-      console.error(`❌ Failed request => ${request.method()} ${request.url()} Error: ${request.failure()}`);
+      console.error(`❌ Failed request => ${request.method()} ${request.url()} ${JSON.stringify(request.failure())}`);
       // request.continue();
     });
     // Emitted when the page crashed
