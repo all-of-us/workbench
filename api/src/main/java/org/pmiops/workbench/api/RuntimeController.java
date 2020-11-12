084bbc8719f7b7bf39bc0f2db57d10597879fe9b
Eric Song <me@ericsong.io>
2020-11-12 18:16:48 -0500

    address comment

--- api/src/main/java/org/pmiops/workbench/api/RuntimeController.java
+++ api/src/main/java/org/pmiops/workbench/api/RuntimeController.java
@@ -300,7 +300,6 @@ public class RuntimeController implements RuntimeApiDelegate {
 
     leonardoNotebooksClient.updateRuntime(runtimeRequest.getRuntime());
 
-    // TODO eric: why does accepted throw an error?
     return ResponseEntity.ok(new EmptyResponse());
   }
 
