d47bd9d806fa47ca262bc5b1cac7500702effb8a
CH Albach <calbach@google.com>
2020-11-12 17:15:35 -0800

    Fix DUCC test

--- ui/src/app/pages/profile/data-user-code-of-conduct.spec.tsx
+++ ui/src/app/pages/profile/data-user-code-of-conduct.spec.tsx
@@ -6,7 +6,12 @@ import {profileApi, registerApiClient} from 'app/services/swagger-fetch-clients'
 import {serverConfigStore, userProfileStore} from 'app/utils/navigation';
 import {Profile, ProfileApi} from 'generated/fetch';
 import {ProfileApiStub, ProfileStubVariables} from 'testing/stubs/profile-api-stub';
-import {waitOneTickAndUpdate} from '../../../testing/react-test-helpers';
+import {waitOneTickAndUpdate} from 'testing/react-test-helpers';
+
+jest.mock('app/utils/navigation', () => ({
+  ...(jest.requireActual('app/utils/navigation')),
+  navigate: jest.fn()
+}));
 
 const defaultConfig = {
   gsuiteDomain: 'researchallofus.org',
@@ -97,72 +102,78 @@ describe('DataUserCodeOfConduct', () => {
   it('should not allow DataUserCodeOfConduct without identical initials', async() => {
     serverConfigStore.next({...defaultConfig, enableV3DataUserCodeOfConduct: true});
     const wrapper = component();
-    wrapper.setState({proceedDisabled: false}, async() => {
-      wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
-      await waitOneTickAndUpdate(wrapper);
-      wrapper.update();
-      expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
+    // Need to step past the HOC before setting state.
+    wrapper.childAt(0).setState({proceedDisabled: false});
+    await waitOneTickAndUpdate(wrapper);
 
-      // fill required fields
-      wrapper.find('[data-test-id="dua-initials-input"]').forEach((node, index) => {
-        node.simulate('change', {target: {value: 'X' + index.toString()}});
-      });
-      expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
+    wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
+    await waitOneTickAndUpdate(wrapper);
+    expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
+
+    // fill required fields
+    wrapper.find('[data-test-id="dua-initials-input"]').forEach((node, index) => {
+      node.simulate('change', {target: {value: 'X' + index.toString()}});
     });
+    expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
   });
 
   it('should not allow DataUserCodeOfConduct with only one field populated', async() => {
     serverConfigStore.next({...defaultConfig, enableV3DataUserCodeOfConduct: true});
     const wrapper = component();
-    wrapper.setState({proceedDisabled: false}, async() => {
-      wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
-      await waitOneTickAndUpdate(wrapper);
-      expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
+    // Need to step past the HOC before setting state.
+    wrapper.childAt(0).setState({proceedDisabled: false});
+    await waitOneTickAndUpdate(wrapper);
+
+    wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
+    await waitOneTickAndUpdate(wrapper);
+    expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
 
     // fill required fields
-      wrapper.find('[data-test-id="ducc-name-input"]').simulate('change', {target: {value: 'Fake Name'}});
+    wrapper.find('[data-test-id="ducc-name-input"]').simulate('change', {target: {value: 'Fake Name'}});
     // add initials to just one initials input field.
-      wrapper.find('[data-test-id="dua-initials-input"]').first().simulate('change', {target: {value: 'XX'}});
+    wrapper.find('[data-test-id="dua-initials-input"]').first().simulate('change', {target: {value: 'XX'}});
 
-      expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
-    });
+    expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
   });
 
   it('should populate username and name from the profile automatically', async() => {
     serverConfigStore.next({...defaultConfig, enableV3DataUserCodeOfConduct: true});
     const wrapper = component();
-    wrapper.setState({proceedDisabled: false}, async() => {
-      await waitOneTickAndUpdate(wrapper);
-      wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
-      await waitOneTickAndUpdate(wrapper);
-      expect(wrapper.find('[data-test-id="ducc-name-input"]').props().value)
-        .toBe(ProfileStubVariables.PROFILE_STUB.givenName + ' ' + ProfileStubVariables.PROFILE_STUB.familyName);
-      expect(wrapper.find('[data-test-id="ducc-user-id-input"]').props().value)
-        .toBe(ProfileStubVariables.PROFILE_STUB.username);
-    });
+    // Need to step past the HOC before setting state.
+    wrapper.childAt(0).setState({proceedDisabled: false});
+    await waitOneTickAndUpdate(wrapper);
 
+    wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
+    await waitOneTickAndUpdate(wrapper);
+
+    expect(wrapper.find('[data-test-id="ducc-name-input"]').props().value)
+      .toBe(ProfileStubVariables.PROFILE_STUB.givenName + ' ' + ProfileStubVariables.PROFILE_STUB.familyName);
+    expect(wrapper.find('[data-test-id="ducc-user-id-input"]').props().value)
+      .toBe(ProfileStubVariables.PROFILE_STUB.username);
   });
 
   it('should submit DataUserCodeOfConduct acceptance with version number', async() => {
     serverConfigStore.next({...defaultConfig, enableV3DataUserCodeOfConduct: true});
     const wrapper = component();
-    wrapper.setState({proceedDisabled: false}, async() => {
-      const spy = jest.spyOn(profileApi(), 'submitDataUseAgreement');
-      wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
-      await waitOneTickAndUpdate(wrapper);
-      expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
+    // Need to step past the HOC before setting state.
+    wrapper.childAt(0).setState({proceedDisabled: false});
+    await waitOneTickAndUpdate(wrapper);
+
+    const spy = jest.spyOn(profileApi(), 'submitDataUseAgreement');
+    wrapper.find('[data-test-id="ducc-next-button"]').simulate('click');
+    await waitOneTickAndUpdate(wrapper);
+    expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeTruthy();
 
     // fill required fields
-      wrapper.find('[data-test-id="ducc-name-input"]').simulate('change', {target: {value: 'Fake Name'}});
+    wrapper.find('[data-test-id="ducc-name-input"]').simulate('change', {target: {value: 'Fake Name'}});
     // add initials to each initials input field.
-      wrapper.find('[data-test-id="dua-initials-input"]').forEach((node) => {
-        node.simulate('change', {target: {value: 'XX'}});
-      });
-
-      expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeFalsy();
-      wrapper.find('[data-test-id="submit-ducc-button"]').simulate('click');
-      expect(spy).toHaveBeenCalledWith(3, 'XX'); // dataUseAgreementVersion
+    wrapper.find('[data-test-id="dua-initials-input"]').forEach((node) => {
+      node.simulate('change', {target: {value: 'XX'}});
     });
+
+    expect(wrapper.find('[data-test-id="submit-ducc-button"]').prop('disabled')).toBeFalsy();
+    wrapper.find('[data-test-id="submit-ducc-button"]').simulate('click');
+    expect(spy).toHaveBeenCalledWith(3, 'XX'); // dataUseAgreementVersion
   });
 
 });
