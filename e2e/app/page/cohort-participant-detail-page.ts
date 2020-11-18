9f08059c2b9d856d383fac2900ea4d2905cf842a
Rohini <rohini.chavan@vumc.org>
2020-11-18 14:35:03 -0600

    created new functions to clickPenIcon, clicktogothe Prior and Next participant

--- e2e/app/page/cohort-participant-detail-page.ts
+++ e2e/app/page/cohort-participant-detail-page.ts
@@ -26,6 +26,7 @@ export default class CohortParticipantDetailPage extends AuthenticatedPage {
     const selector = `//*[@class="detail-header"]//span[contains(., "Participant ")]`;
     const pID = await this.extractParticipantID(selector);
     console.log(`Participant ID: ${pID}`);
+
     return pID;
   }
 
@@ -60,6 +61,7 @@ export default class CohortParticipantDetailPage extends AuthenticatedPage {
   // click on pi-angle-left button to go to prior participant
   async goToThePriorParticipant(): Promise<void> {
     const iconXpath = '//button[@title ="Go To the Prior Participant"]';
+
     await this.page.waitForXPath(iconXpath, {visible: true}).then(icon => icon.click());
   }
 
