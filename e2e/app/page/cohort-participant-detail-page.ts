916199418907fc94b21449e7af79019c33aa7de5
Rohini <rohini.chavan@vumc.org>
2020-12-01 16:17:40 -0600

    updated function names

--- e2e/app/page/cohort-participant-detail-page.ts
+++ e2e/app/page/cohort-participant-detail-page.ts
@@ -58,7 +58,6 @@ export default class CohortParticipantDetailPage extends AuthenticatedPage {
   }
 
   // click on pi-angle-left button to go to prior participant
-
   async goToThePriorParticipant(): Promise<void> {
     const iconXpath = '//button[@title ="Go To the Prior Participant"]';
     await this.page.waitForXPath(iconXpath, {visible: true}).then(icon => icon.click());
