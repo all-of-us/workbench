9090c820cc0d8eee35e49e27a5c93aa0739606ae
Ariel Schwartz <aschwart@broadinstitute.org>
2020-12-08 16:35:44 -0500

    revert a thing

--- e2e/app/element/dropdown.ts
+++ e2e/app/element/dropdown.ts
@@ -1,5 +1,5 @@
-import BaseElement from 'app/element/base-element';
-import {ElementHandle, Page} from 'puppeteer';
+import BaseElement from "app/element/base-element";
+import {ElementHandle, Page} from "puppeteer";
 
 export default class Dropdown extends BaseElement {
   constructor(page: Page, xpath?: string) {
@@ -7,7 +7,8 @@ export default class Dropdown extends BaseElement {
   }
 
   async selectOption(optionText: string): Promise<void> {
-    await this.click({delay: 20});
+    const dropdown = await this.page.waitForXPath(this.getXpath())
+    await dropdown.click({delay: 20});
     const option = await this.waitForOption(optionText);
     return await option.click({delay: 20});
   }
