1f3528ec18aad83823416045e7a9f84c8c2140ae
Rohini <rohini.chavan@vumc.org>
2021-01-11 11:27:09 -0600

    added waitForClose: true for the edit/delete modal

--- e2e/app/component/edit-delete-annotations-modal.ts
+++ e2e/app/component/edit-delete-annotations-modal.ts
@@ -42,15 +42,13 @@ export default class EditDeleteAnnotationsModal extends Modal {
      const baseElement = BaseElement.asBaseElement(this.page, clearTextSelector);
      await baseElement.clearTextInput();
      await baseElement.type(newName);
-      
-    // await this.beginRenameAnnotationField(newName);
+
      await this.clickButton(LinkText.Rename);
      await this.clickButton(LinkText.Close);
   }
 
   async deleteAnnotationsName(): Promise<void>{
     await this.clickButton(LinkText.Delete);
-    await this.clickButton(LinkText.Yes);
-
+    await this.clickButton(LinkText.Yes, {waitForClose: true});
   }
 }
