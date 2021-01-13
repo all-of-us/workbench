28d93cb9afa7b715469b26116889060c4ae39fd0
Eric Song <me@ericsong.io>
2021-01-13 15:34:11 -0500

    add box shadow to help sidebar

--- ui/src/app/components/help-sidebar.tsx
+++ ui/src/app/components/help-sidebar.tsx
@@ -492,7 +492,7 @@ export const HelpSidebar = fp.flow(
     sidebarContainerStyles(activeIcon, notebookStyles) {
       const sidebarContainerStyle = {
         ...styles.sidebarContainer,
-        width: `calc(${this.sidebarWidth}rem + 55px)`
+        width: `calc(${this.sidebarWidth}rem + 70px)`
       };
       if (notebookStyles) {
         if (activeIcon) {
