1ba7417e8bfefa46c8f78f95ab5ff1af18b71bb5
Eric Song <me@ericsong.io>
2021-01-14 12:47:31 -0500

    actually, just set width to 100%

--- ui/src/app/components/help-sidebar.tsx
+++ ui/src/app/components/help-sidebar.tsx
@@ -492,7 +492,7 @@ export const HelpSidebar = fp.flow(
     sidebarContainerStyles(activeIcon, notebookStyles) {
       const sidebarContainerStyle = {
         ...styles.sidebarContainer,
-        width: `calc(${this.sidebarWidth}rem + 70px)`
+        width: '100%'
       };
       if (notebookStyles) {
         if (activeIcon) {
