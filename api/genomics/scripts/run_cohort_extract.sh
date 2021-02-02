7fc8ddfad2a34a3a538ad83149c17d4ada7bc52d
Eric Song <me@ericsong.io>
2021-02-01 21:06:22 -0500

    [RW-6097][RW-6130][risk=no] Documenting steps to upload 1kg genomes (#4519)
    * update Reblock to take in a list of GSC paths and copy into target GCS bucket
    
    * update docs for uploading 1kg
    
    * add commands for fixing sample names
    
    * final comments from 1kg upload
    
    * load doesn't need max-rows
    
    * but query does
    
    * update WDLs
    
    * add extract notes

--- api/genomics/scripts/run_cohort_extract.sh
+++ api/genomics/scripts/run_cohort_extract.sh
@@ -1,15 +1,16 @@
 PROJECT="all-of-us-workbench-test"
-DATASET="wgs_demo_5"
+PETVET_DATASET="1kg_wgs"
 DEST_TABLE="cohort_extract"
-TEMP_DATASET="wgs_demo_5_temp_tables"
+TEMP_DATASET="1kg_wgs_temp_tables"
+EXTRACT_DATASET="1kg_wgs_extract"
 COHORT_LIST_TABLE="cohort"
 
 python ngs_cohort_extract.py \
-  --fq_petvet_dataset ${PROJECT}.${DATASET} \
+  --fq_petvet_dataset ${PROJECT}.${PETVET_DATASET} \
   --fq_temp_table_dataset ${PROJECT}.${TEMP_DATASET} \
-  --fq_destination_dataset ${PROJECT}.${DATASET} \
+  --fq_destination_dataset ${PROJECT}.${EXTRACT_DATASET} \
   --destination_table ${DEST_TABLE} \
-  --fq_cohort_sample_names ${PROJECT}.${DATASET}.${COHORT_LIST_TABLE} \
+  --fq_cohort_sample_names ${PROJECT}.${EXTRACT_DATASET}.${COHORT_LIST_TABLE} \
   --min_variant_samples 0 \
   --query_project ${PROJECT} \
-  --fq_sample_mapping_table ${PROJECT}.${DATASET}.metadata
+  --fq_sample_mapping_table ${PROJECT}.${PETVET_DATASET}.metadata
