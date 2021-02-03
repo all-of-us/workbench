4faeb645d888913f6fc7d2d248df3f7e68344493
Michael Yessaillian <myessail@broadinstitute.org>
2021-02-03 11:38:14 -0500

    switch on resource type

--- ui/src/app/components/confirm-delete-modal.tsx
+++ ui/src/app/components/confirm-delete-modal.tsx
@@ -1,8 +1,8 @@
-import {Component, Input, } from '@angular/core';
+import {Component, Input} from '@angular/core';
 
 import {Button} from 'app/components/buttons';
 
-import {Modal, ModalBody, ModalFooter, ModalTitle, } from 'app/components/modals';
+import {Modal, ModalBody, ModalFooter, ModalTitle,} from 'app/components/modals';
 
 import {ReactWrapperBase} from 'app/utils';
 import {toDisplay} from 'app/utils/resources';
@@ -45,34 +45,65 @@ export class ConfirmDeleteModal
   }
 
   render() {
-    return <Modal loading={this.state.loading}>
-      <ModalTitle style={{lineHeight: '28px'}}>
-        Warning -- All work in this workspace will be lost.
-        Are you sure you want to
-        delete {toDisplay(this.props.resourceType)}
-        : {this.props.resourceName}?
-      </ModalTitle>
-      <ModalBody style={{marginTop: '0.2rem', lineHeight: '28.px'}}>
-        <div>
-          Deleting this workspace will immediately, permanently delete any items inside the workspace, such as notebooks
-          and cohort definitions. This includes items created or used by other users with access to the workspace.
-          If you still wish to delete this workspace and all items within it, type DELETE below to confirm.
-        </div>
-        <TextInput style={{marginTop: '0.5rem'}} onChange={this.validateDeleteText} onBlur=''/>
-      </ModalBody>
-      <ModalFooter style={{paddingTop: '1rem'}}>
-        <Button
-          type='secondary'
-          onClick={() => this.props.closeFunction()}>Cancel</Button>
-        <Button
-          disabled={this.state.loading || this.state.deleteDisabled}
-          style={{marginLeft: '0.5rem'}}
-          data-test-id='confirm-delete'
-          onClick={() => this.emitDelete()}>
-          Delete {toDisplay(this.props.resourceType)}
-        </Button>
-      </ModalFooter>
-    </Modal>;
+    const {resourceType} = this.props;
+
+    if (resourceType === ResourceType.WORKSPACE) {
+      return <Modal loading={this.state.loading}>
+        <ModalTitle style={{lineHeight: '28px'}}>
+          Warning — All work in this workspace will be lost.
+          Are you sure you want to
+          delete {toDisplay(this.props.resourceType)}
+          : {this.props.resourceName}?
+        </ModalTitle>
+        <ModalBody style={{marginTop: '0.2rem', lineHeight: '28.px'}}>
+          <div>
+            Deleting this workspace will immediately, permanently delete any items inside the workspace, such as
+            notebooks
+            and cohort definitions. This includes items created or used by other users with access to the workspace.
+            If you still wish to delete this workspace and all items within it, type DELETE below to confirm.
+          </div>
+          <TextInput style={{marginTop: '0.5rem'}} onChange={this.validateDeleteText} onBlur=''/>
+        </ModalBody>
+        <ModalFooter style={{paddingTop: '1rem'}}>
+          <Button
+            type='secondary'
+            onClick={() => this.props.closeFunction()}>Cancel</Button>
+          <Button
+            disabled={this.state.loading || this.state.deleteDisabled}
+            style={{marginLeft: '0.5rem'}}
+            data-test-id='confirm-delete'
+            onClick={() => this.emitDelete()}>
+            Delete {toDisplay(this.props.resourceType)}
+          </Button>
+        </ModalFooter>
+      </Modal>;
+    } else {
+      return <Modal loading={this.state.loading}>
+        <ModalTitle style={{lineHeight: '28px'}}>
+          Are you sure you want to
+          delete {toDisplay(this.props.resourceType)}
+          : {this.props.resourceName}?
+        </ModalTitle>
+        <ModalBody style={{marginTop: '0.2rem', lineHeight: '28.px'}}>
+          This will permanently delete
+          the {toDisplay(this.props.resourceType)}
+          {this.props.resourceType === ResourceType.COHORT &&
+          <span> and all associated review sets</span>}.
+        </ModalBody>
+        <ModalFooter style={{paddingTop: '1rem'}}>
+          <Button
+            type='secondary'
+            onClick={() => this.props.closeFunction()}>Cancel</Button>
+          <Button
+            disabled={this.state.loading}
+            style={{marginLeft: '0.5rem'}}
+            data-test-id='confirm-delete'
+            onClick={() => this.emitDelete()}>
+            Delete {toDisplay(this.props.resourceType)}
+          </Button>
+        </ModalFooter>
+      </Modal>;
+    }
   }
 }
 
