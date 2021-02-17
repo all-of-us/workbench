05476fde518a0a250c727fc91251297dddfe6f17
aweng98 <35533885+aweng98@users.noreply.github.com>
2021-02-17 14:42:53 -0500

    [RW-6236][risk=no] Rework CircleCI e2e job (#4557)
    * squash commits
    
    * revert login test
    
    * rename e2e to puppeteer in names
    
    * fix shell script
    
    * ignore pattern test
    
    * update ignore pattern
    
    * more ignore testing
    
    * ignore script works
    
    * dockerize: change tcp to http
    
    * dockerize: change tcp to http
    
    * change sidenav test
    
    * revert sidenav test
    
    * PR feedback
    
    * update ignore pattern for tests
    
    * script name error
    
    * e2e cache name fix
    
    * check changes in .circleci dir
    
    * remove yarn.lock from ignore pattern
    
    * artifacts testing
    
    * revert: no_output_timeout: 25m

---
+++ .circleci/skip-e2e-pr-checks.sh
@@ -0,0 +1,31 @@
+#!/bin/bash
+set -xv
+
+# Applying to PR branch only: Exiting Puppeteer job early if certain conditions are met.
+
+if [[ ${CIRCLE_PULL_REQUEST} ]]; then
+  echo "CIRCLE_PULL_REQUEST: ${CIRCLE_PULL_REQUEST}"
+  regexp="[[:digit:]]\+$"
+  PR_NUMBER=$(echo $CIRCLE_PULL_REQUEST | grep -o $regexp)
+  echo "PR_NUMBER: ${PR_NUMBER}"
+else
+  exit 0
+fi
+
+# Exiting if the commit message contains 'skip e2e' string
+# Double comma is "Parameter Expansion". It converts string to lowercase letters
+if [[ "${COMMIT_MESSAGE,,}" == *"skip e2e"* ]]; then
+  echo "Skip e2e text found in commit message"
+  circleci-agent step halt
+else
+  echo "not found skip e2e text"
+fi
+
+# Exiting on PR branch when all (changed) file names matched ignore pattern
+# The grep command exits with '0' status when it's successful (match were found). While it exits with status '1' when no match was found.
+git diff --name-only $(git merge-base origin/master ${CIRCLE_BRANCH}) | grep -qvFf .circleci/e2e-job-ignore-patterns.txt
+STATUS=$?
+if [[ "$STATUS" -eq 1 ]]; then
+  echo "Workbench application code were not changed. Exiting job."
+  circleci-agent step halt
+fi
