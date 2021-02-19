bf89802b606b2f3c496777c593f239ebfe0e5de9
aweng98 <35533885+aweng98@users.noreply.github.com>
2021-02-19 15:47:49 -0500

    [no ticket][risk=no] Puppeteer rework console log handler (#4559)
    * better log
    
    * handle exception
    
    * catch ExecutionContext exception
    
    * remove whitespaces
    
    * working
    
    * more changes
    
    * fix a code bug
    
    * truncate GET /workspaces response
    
    * fix comments
    
    * fix comments
    
    * save
    
    * wip
    
    * use fp.flow
    
    * rebase master
    
    * more tweaking output
    
    * group flow functions together
    
    * minor update

--- e2e/jest.test-setup.ts
+++ e2e/jest.test-setup.ts
@@ -1,3 +1,5 @@
+import * as fp from 'lodash/fp';
+import {Request} from 'puppeteer';
 const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/80.0.3987.149 Safari/537.36';
 
 /**
@@ -8,79 +10,156 @@ const userAgent = 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_14_6) AppleWebKit/5
  * - waitFor functions timeout
  */
 beforeEach(async () => {
-
   await jestPuppeteer.resetPage();
   await jestPuppeteer.resetBrowser();
-
   await page.setUserAgent(userAgent);
   await page.setViewport({width: 1300, height: 0});
-
   page.setDefaultNavigationTimeout(60000); // Puppeteer default timeout is 30 seconds.
-  page.setDefaultTimeout(30000);
-
   await page.setRequestInterception(true);
 
+  const getTitle = async () => {
+    return await page.$eval('title', title => {
+      return title.textContent;
+    });
+  }
+
+  const stringifyData = (data: string): string => {
+    return data !== undefined ? JSON.stringify(JSON.parse(data), null, 2) : '';
+  }
+
+  const includeUrl = (request: Request): Request | null => {
+    const filters = [
+      'google',
+      'content-security-index-report',
+      'analytics',
+      'status-alert',
+      'publicDetails'
+    ];
+    return !filters.some((urlPart) => request && request.url().includes(urlPart))
+       ? request
+       : null;
+  }
+
+  const includeResourceType = (request: Request): Request | null => {
+    const filters = [
+      'xhr',
+      'fetch',
+      'websocket'
+    ];
+    return filters.some((resource) => request && request.resourceType().includes(resource))
+       ? request
+       : null;
+  }
+
+  const isWorkbenchApi = (request: Request): Request | null => {
+    return request && request.url().match('all-of-us-workbench-(test|staging).appspot.com') != null
+       ? request
+       : null;
+  }
+
+  // Api "/workspaces" or "/cdrVersions" response can be truncated
+  const isWorkspacesApi = (request: Request): boolean => {
+    return request && (request.url().endsWith('/v1/workspaces') || request.url().endsWith('/v1/cdrVersions'));
+  }
+
+  const isApiFailure = (request: Request): boolean => {
+    return request && request.failure() != null || !request.response().ok();
+  }
+
+  const notOptionsRequest = (request: Request): Request | null => {
+    return request && request.method() !== 'OPTIONS' ? request : null;
+  }
+
+  const getRequestPostData = (request: Request): string | undefined => {
+    return request && request.postData() ? request.postData() : undefined;
+  }
+
+  const notRedirectRequest = (request: Request): boolean => {
+    // Response body can only be accessed for non-redirect requests
+    return request && request.redirectChain().length === 0;
+  }
+
+  const getResponseText = async (request: Request): Promise<string> => {
+    return (await (request.response()).buffer()).toString();
+  }
+
+  const logError = async (request: Request): Promise<void> => {
+    const response = request.response();
+    const failureText = request.failure() !== null ? stringifyData(request.failure().errorText) : '';
+    const responseText = stringifyData(await getResponseText(request));
+    console.error('❗ Request failed: ' +
+       `${response.status()} ${request.method()} ${request.url()}\n${responseText}\n${failureText}`);
+  }
+
+  const logResponse = async (request: Request): Promise<void> => {
+    let responseText = stringifyData(await getResponseText(request));
+    if (isWorkspacesApi(request)) {
+      // truncate long response. get first two workspace details.
+      responseText = fp.isEmpty(JSON.parse(responseText).items)
+         ? responseText
+         : 'truncated...\n' + JSON.stringify(JSON.parse(responseText).items.slice(0, 2), null, 2);
+    }
+    console.debug('❗ Request finished: ' +
+       `${request.response().status()} ${request.method()} ${request.url()}\n${responseText}`);
+  }
+
+  const isWorkbenchRequest = fp.flow(isWorkbenchApi, notOptionsRequest, includeResourceType, includeUrl);
+  const getRequestData = fp.flow(getRequestPostData, stringifyData);
+  const canLogResponse = fp.flow(isWorkbenchRequest, notRedirectRequest);
+
+  // New request initiated
   page.on('request', (request) => {
+    if (isWorkbenchRequest(request)) {
+      console.debug('❗ Request issued: ' +
+         `${request.method()} ${request.url()}\n${getRequestData(request)}`);
+    }
     try {
-      if (request.url().includes('api-dot-all-of-us')) {
-        const method = request.method();
-        if (method !== 'OPTIONS') {
-          console.debug(`❗Request issued: ${request.url()}`);
-        }
-      }
       request.continue();
       // tslint:disable-next-line:no-empty
     } catch (e) {
     }
-  })
-  .on('requestfinished', async (request) => {
-    try {
-      const response = await request.response();
-      const status = request.response().status();
-      let responseBody;
-      if (request.redirectChain().length === 0) {
-        // response body can only be accessed for non-redirect responses.
-        if (request.url().includes('api-dot-all-of-us')){
-          responseBody = await response.buffer();
-          if (responseBody !== undefined) {
-            responseBody = JSON.stringify(JSON.parse(responseBody.toString()), null, 2);
-          }
-          const method = request.method();
-          console.debug(`❗Request finished: ${status} ${method} ${request.url()}  \n ${responseBody}`, {depth: null, colors: true});
-        }
+  });
+
+  page.on('requestfinished', async (request) => {
+    if (canLogResponse(request)) {
+      if (isApiFailure(request)) {
+        await logError(request);
+      } else {
+        await logResponse(request);
       }
+    }
+    try {
       await request.continue();
       // tslint:disable-next-line:no-empty
     } catch (e) {
     }
-  })
-  .on('requestfailed', async (request) => {
+  });
+
+  page.on('console', async (message) => {
+    if (!message.args().length) {
+      return;
+    }
     try {
-      const response = request.response();
-      if (response !== null) {
-        const status = response.status();
-        const responseText = await response.text();
-        const failureError = request.failure().errorText;
-        console.debug(`❗Request failed: ${status} ${request.method()} ${request.url()}  \n ${failureError} \n ${responseText}`);
-      }
+      const title = await getTitle();
+      const args = await Promise.all(message.args().map(a => a.jsonValue()));
+      console[message.type() === 'warning' ? 'warn' : message.type()](`❗ ${title}\n`, ...args);
       // tslint:disable-next-line:no-empty
     } catch (err) {
+      console.log(err);
     }
-  })
-  .on('console', (message) => {
-    if (message != null) {
-      // Don't log "log", "info" or "debug"
-      if (['ERROR', 'WARNING'].includes(message.type().toUpperCase())) {
-        console.debug(`❗Page console: ${message.type()}: ${message.text()}`);
-      }
-    }
-  })
-  .on('error', (error) => {
-    console.debug(`❗Page error: ${error}`);
-  })
-  .on('pageerror', (error) => {
-    if (error != null) {
-      console.debug(`❗Page error: ${error}`);
+  });
+
+  page.on('error', async (error) => {
+    const title = await getTitle();
+    console.error(`❗ ${title}\npage error: ${error}`);
+  });
+
+  page.on('pageerror', async (error) => {
+    const title = await getTitle();
+    try {
+      console.error(`❗ ${title}\npage error: ${error}`);
+      // tslint:disable-next-line:no-empty
+    } catch (err) {
     }
   })
 
