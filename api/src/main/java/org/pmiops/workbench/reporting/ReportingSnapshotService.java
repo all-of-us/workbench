2b42f31505c15e57aa125ee7c6b04e06ede99af7
Joel Thibault <thibault@broad.dev>
2021-02-25 15:08:37 -0500

    update readme and comment

--- api/src/main/java/org/pmiops/workbench/reporting/ReportingSnapshotService.java
+++ api/src/main/java/org/pmiops/workbench/reporting/ReportingSnapshotService.java
@@ -3,8 +3,8 @@ package org.pmiops.workbench.reporting;
 import org.pmiops.workbench.model.ReportingSnapshot;
 
 /**
- * @deprecated This is memory-wasting implementation, for new payloads, write a query to return
- *     Steam of batches.
+ * @deprecated in favor of ReportingQueryService, because this implementation consumes too much
+ *     memory. Please write a streaming query in ReportingQueryService for new payloads.
  *     <p>This service encapsulate the business of obtaining the current analytics snapshot from the
  *     application MySQL DB and third party sources (e.g. Terra).
  */
