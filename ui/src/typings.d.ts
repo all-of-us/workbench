0cc2d3ac8a4a1ef8524cd140f476cb49df3a2afc
petesantos <psantos@broadinstitute.org>
2021-03-01 12:06:54 -0500

    ignore no-var on typing.d.ts

--- ui/src/typings.d.ts
+++ ui/src/typings.d.ts
@@ -1,5 +1,6 @@
 /* SystemJS module definition */
-declare let module: NodeModule;
+/* eslint-disable no-var */
+declare var module: NodeModule;
 interface NodeModule {
   id: string;
 }
@@ -10,6 +11,8 @@ This gets included by the compiler; its symbols do not need to be imported by
 other source files but are globally available.
 */
 
-declare let ResizeObserver: any;
+declare var ResizeObserver: any;
+
+/* eslint-enable no-var */
 
 declare module 'outdated-browser-rework';
