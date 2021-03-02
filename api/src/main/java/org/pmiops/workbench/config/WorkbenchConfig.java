7ed9cade69cccbf5cdd6b95b91563dae52d6bc2c
yonghao <yyhao1@gmail.com>
2021-03-01 20:02:52 -0500

    feature flag

--- api/src/main/java/org/pmiops/workbench/config/WorkbenchConfig.java
+++ api/src/main/java/org/pmiops/workbench/config/WorkbenchConfig.java
@@ -270,6 +270,8 @@ public class WorkbenchConfig {
     public boolean enableResearchPurposePrompt;
     // If true, reporting cron job will write data to configured BigQuery reporting dataset.
     public boolean enableReportingUploadCron;
+    // If true, user account setup requires link account via RAS account instead of eRA Commons.
+    public boolean enableRasLinking;
   }
 
   public static class ActionAuditConfig {
