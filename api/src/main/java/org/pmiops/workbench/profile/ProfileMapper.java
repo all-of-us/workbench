44f571f63f3803ff2a810013a7903e5b7e311d9e
yonghao <yyhao1@gmail.com>
2021-03-08 13:04:40 -0500

    spotless

--- api/src/main/java/org/pmiops/workbench/profile/ProfileMapper.java
+++ api/src/main/java/org/pmiops/workbench/profile/ProfileMapper.java
@@ -29,8 +29,12 @@ public interface ProfileMapper {
   @Mapping(source = "latestTermsOfService.agreementTime", target = "latestTermsOfServiceTime")
   @Mapping(source = "dbUser.userId", target = "userId")
   @Mapping(target = "rasLinkLoginGovName", ignore = true) // TODO(RW-6364): Remove this once ready
-  @Mapping(target = "rasLinkLoginGovExpireTime", ignore = true) // TODO(RW-6364): Remove this once ready
-  @Mapping(target = "rasLinkLoginGovCompletionTime", ignore = true) // TODO(RW-6364): Remove this once ready
+  @Mapping(
+      target = "rasLinkLoginGovExpireTime",
+      ignore = true) // TODO(RW-6364): Remove this once ready
+  @Mapping(
+      target = "rasLinkLoginGovCompletionTime",
+      ignore = true) // TODO(RW-6364): Remove this once ready
   Profile toModel(
       DbUser dbUser,
       VerifiedInstitutionalAffiliation verifiedInstitutionalAffiliation,
