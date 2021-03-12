710783ee29c6e98bf85f8860a217d1f01fd6bbd9
aweng98 <bweng@broadinstitute.org>
2021-03-11 19:19:26 -0500

    update choose-e2e-env.sh

--- .circleci/choose-e2e-env.sh
+++ .circleci/choose-e2e-env.sh
@@ -1,15 +1,13 @@
 #!/bin/bash
 set -xv
 
+# PR branch only
 if [[ ! -z $CIRCLE_PULL_REQUEST ]]; then
-  # Count total number of files changed
-  CHANGED_COUNT=$(git diff --name-only $(git merge-base origin/master ${CIRCLE_BRANCH}) | wc -l | xargs)
-
-  # Count total number of e2e files
-  E2E_CHANGED_COUNT=$(git diff --name-only $(git merge-base origin/master ${CIRCLE_BRANCH}) -- e2e | wc -l | xargs)
-
-  # If e2e files are the only files that have changed, update WORKBENCH_ENV to "test"
-  if [ $E2E_CHANGED_COUNT -gt 0 ] && [ $E2E_CHANGED_COUNT -eq $CHANGED_COUNT ]; then
+  # Count total number of changed files that are not e2e and circleci
+  CHANGES=$(git diff --name-only $(git merge-base origin/master ${CIRCLE_BRANCH}) -- . ':!e2e' ':!.circleci' | wc -l | xargs)
+  # If count is zero, update WORKBENCH_ENV to "test"
+  if [ $CHANGES -gt 0 ]; then
+    echo "Set WORKBENCH_ENV=test"
     echo "export WORKBENCH_ENV=test" >> $BASH_ENV
     echo "export USER_NAME=${PUPPETEER_USER_TEST}@fake-research-aou.org" >> $BASH_ENV
     source $BASH_ENV
