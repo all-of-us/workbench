8fd284832f53aecb65222cc1df736924946ef07f
aweng98 <bweng@broadinstitute.org>
2021-03-11 19:31:25 -0500

    include more ignore patterns

--- .circleci/choose-e2e-env.sh
+++ .circleci/choose-e2e-env.sh
@@ -6,7 +6,7 @@ if [[ ! -z $CIRCLE_PULL_REQUEST ]]; then
   # Count total number of changed files that are not e2e and circleci
   CHANGES=$(git diff --name-only $(git merge-base origin/master ${CIRCLE_BRANCH}) -- . ':!e2e' ':!.circleci' | wc -l | xargs)
   # If count is zero, update WORKBENCH_ENV to "test"
-  if [ $CHANGES -gt 0 ]; then
+  if [ $CHANGES -eg 0 ]; then
     echo "Set WORKBENCH_ENV=test"
     echo "export WORKBENCH_ENV=test" >> $BASH_ENV
     echo "export USER_NAME=${PUPPETEER_USER_TEST}@fake-research-aou.org" >> $BASH_ENV
