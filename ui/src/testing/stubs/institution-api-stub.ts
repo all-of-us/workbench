e4a252f378f09b4c254b650afcf6dadf7c4d84d0
aweng98 <bweng@broadinstitute.org>
2021-03-17 17:48:27 -0400

    revert institution-api-stub

--- ui/src/testing/stubs/institution-api-stub.ts
+++ ui/src/testing/stubs/institution-api-stub.ts
@@ -48,6 +48,15 @@ export class InstitutionApiStub extends InstitutionApi {
     });
   }
 
+  deleteInstitution(shortName: string): Promise<Response> {
+    return new Promise<Response>(resolve => {
+      this.institutions = this.institutions.filter(institution => {
+        return institution.shortName !== shortName;
+      });
+      resolve(new Response());
+    });
+  }
+
   getInstitution(shortName: string): Promise<Institution> {
     return new Promise((resolve, reject) => {
       const institution = this.institutions.find(x => x.shortName === shortName);
