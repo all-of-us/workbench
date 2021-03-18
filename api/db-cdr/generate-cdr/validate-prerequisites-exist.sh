42653b9adf6d81d9fa44f534bc3aa95a1703fab6
Brian Freeman <brian.d.freeman@gmail.com>
2021-03-18 13:30:44 -0500

    RW-6441 finished purge.

--- api/db-cdr/generate-cdr/validate-prerequisites-exist.sh
+++ api/db-cdr/generate-cdr/validate-prerequisites-exist.sh
@@ -23,54 +23,71 @@ if [[ ${INCOMPATIBLE_DATASETS[@]} =~ $BQ_DATASET ]];
   exit 1
 fi
 
+# Purge all backup csv files except for the last 10 versions
+fileCount=$(gsutil ls gs://all-of-us-workbench-private-cloudsql/DummySR/cdr_csv_files/backup | wc -l)
+numberToDelete=$(($fileCount - ${#All_FILES[@]} * 10))
+if [[ $numberToDelete > 0 ]];
+then
+  echo "Purging backup files"
+  while IFS= read -r line; do
+    ts=$(echo $line | cut -d_ -f1)
+    if [[ ${#ts} > 0 ]];
+    then
+      echo "Removing $line"
+      gsutil rm gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/backup/$line
+    fi
+  # This lists all the files in the backup bucket sorted by timestamp and gets only the number to delete
+  done < <(gsutil ls gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/backup | rev | cut -d/ -f1 | rev | sort | head -$numberToDelete)
+fi
+
 rm -rf $TEMP_FILE_DIR
 mkdir $TEMP_FILE_DIR
 
 if gsutil -m cp gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/*.csv $TEMP_FILE_DIR
  then
+  timestamp=$(date +%s)
   for file in ${All_FILES[@]}; do
     read -r header < $TEMP_FILE_DIR/$file
     IFS=',' read -r -a columns <<< $header
     case $file in
       $CRITERIA_MENU)
-        echo "Processing $CRITERIA_MENU"
+        echo "Processing $file"
         if [[ $columns =~ id ]];
         then
-          echo "Removing $CRITERIA_MENU header"
-          # remove the first line of file
+          echo "Removing $file header"
+          # Remove the first line of file
           sed 1d $TEMP_FILE_DIR/$file > $TEMP_FILE_DIR/temp_$file
-          # zip the file
+          # Zip the file
           gzip -cvf $TEMP_FILE_DIR/temp_$file > $TEMP_FILE_DIR/$file.gz
         else
-          # zip the file
+          # Zip the file
           gzip $TEMP_FILE_DIR/$file
         fi
-        # copy it back to bucket
+        # Copy it back to bucket
         gsutil cp $TEMP_FILE_DIR/$file.gz gs://$BUCKET/$BQ_DATASET/$CDR_VERSION/
+        # Backup the csv file
+        echo "Backing up $file"
+        gsutil cp $TEMP_FILE_DIR/$file.gz gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/backup/"$timestamp"_"$file".gz
       ;;
     $PREP_CRITERIA|$PREP_CRITERIA_ANCESTOR|$PREP_CLINICAL_TERMS)
-      echo "Processing $file"
       tableName=${file%.*}
       if [[ $columns =~ id || $columns =~ ancestor_id || $columns =~ parent ]];
       then
         echo "Removing $file header"
-        # remove the first line of file
+        # Remove the first line of file
         sed 1d $TEMP_FILE_DIR/$file > $TEMP_FILE_DIR/temp_$file
-        # rename file
+        # Rename file
         mv $TEMP_FILE_DIR/temp_$file $TEMP_FILE_DIR/$file
-        # copy it back to bucket
+        # Copy it back to bucket
         echo "Copying $file"
         gsutil cp $TEMP_FILE_DIR/$file gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/
       fi
 
-      # backup the csv file
+      # Backup the csv file
       echo "Backing up $file"
-      timestamp=$(date +%s)
-      # cutoffTimestamp=$(date -d "2019-10-01 23:29:40" +%s)
-      echo "$cutoffTimestamp"
       bq extract --project_id=$BQ_PROJECT --compression GZIP --print_header=false $BQ_DATASET.$tableName gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/backup/"$timestamp"_"$file".gz
 
-      # load the csv file into table
+      # Load the csv file into table
       echo "Starting load of $file"
       schema_path=generate-cdr/bq-schemas
       bq --project_id=$BQ_PROJECT rm -f $BQ_DATASET.$tableName
@@ -84,4 +101,3 @@ if gsutil -m cp gs://$BUCKET/$BQ_DATASET/$CSV_HOME_DIR/*.csv $TEMP_FILE_DIR
 fi
 
 rm -rf $TEMP_FILE_DIR
-# More validation to come in future stories
