cd87057b1b256ca92a5b3fa64ec5cb1bf7cf4f3b
Brian Freeman <brian.d.freeman@gmail.com>
2021-03-24 13:26:19 -0500

    [no ticket][risk=no] missing import statement (#4736)
    * fix-import
    
    * spotless

--- api/src/main/java/org/pmiops/workbench/cohortbuilder/CohortBuilderServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/cohortbuilder/CohortBuilderServiceImpl.java
@@ -17,6 +17,7 @@ import java.util.Comparator;
 import java.util.List;
 import java.util.Map;
 import java.util.Optional;
+import java.util.Set;
 import java.util.stream.Collectors;
 import java.util.stream.IntStream;
 import java.util.stream.StreamSupport;
