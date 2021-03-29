cb171d83db0ec241043904df2e626fc060bca7df
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2021-03-29 16:17:45 -0400

    [noTicket][risk=nO]Update existing services that read from data_dictionary entry (Data Dictionary)  (#4752)
    * Query just one non ds_ entry in case of multiple rows in ds_Data_dictionary table
    
    * add equals and hascode and fix service test
    
    * Fix DAO test

--- api/src/main/java/org/pmiops/workbench/cdr/dao/DSDataDictionaryDao.java
+++ api/src/main/java/org/pmiops/workbench/cdr/dao/DSDataDictionaryDao.java
@@ -1,8 +1,27 @@
 package org.pmiops.workbench.cdr.dao;
 
 import org.pmiops.workbench.cdr.model.DbDSDataDictionary;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface DSDataDictionaryDao extends CrudRepository<DbDSDataDictionary, Long> {
-  DbDSDataDictionary findFirstByFieldNameAndDomain(String field_name, String domain);
+  @Query(
+      value =
+          "select * from \n"
+              + "(\n"
+              + "select * \n"
+              + "from ds_data_dictionary \n"
+              + "where field_name = :name \n"
+              + "and domain = :domain\n"
+              + "and relevant_omop_table not like 'ds_%'\n"
+              + "UNION\n"
+              + "select * \n"
+              + "from ds_data_dictionary \n"
+              + "where field_name = :name \n"
+              + "and domain = :domain\n"
+              + "and relevant_omop_table like 'ds_%') d limit 1",
+      nativeQuery = true)
+  DbDSDataDictionary findFirstByFieldNameAndDomain(
+      @Param("name") String name, @Param("domain") String domain);
 }
