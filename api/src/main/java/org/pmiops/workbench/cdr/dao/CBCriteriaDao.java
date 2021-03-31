88b4f1a6ff56d988fc35398c3e0b1f3047119ebc
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2021-03-31 11:53:05 -0400

    Cleaning up Physical Measurement code (#4770)

--- api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
+++ api/src/main/java/org/pmiops/workbench/cdr/dao/CBCriteriaDao.java
@@ -287,17 +287,6 @@ public interface CBCriteriaDao extends CrudRepository<DbCriteria, Long> {
 
   @Query(
       value =
-          "select count(*) from concept c "
-              + "where (c.count_value > 0 or c.source_count_value > 0) "
-              + "and match(c.concept_name, c.concept_code, c.vocabulary_id, c.synonyms) against(:term in boolean mode) "
-              + "and c.vocabulary_id = 'PPI' "
-              + "and c.concept_class_id = 'Clinical Observation' "
-              + "and c.standard_concept IN ('')",
-      nativeQuery = true)
-  Long findPhysicalMeasurementCount(@Param("term") String term);
-
-  @Query(
-      value =
           "select count from cb_criteria c "
               + "join(select substring_index(path,'.',1) as survey_version_concept_id, count(*) as count "
               + "       from cb_criteria "
