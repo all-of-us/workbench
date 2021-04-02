db90cbb81362e87b4ab39c20a9db4a1619dfa7fc
aweng98 <35533885+aweng98@users.noreply.github.com>
2021-04-02 19:16:12 -0400

    [no ticket][risk=no] Puppeteer remove password login test (#4784)
    * [no ticket] remove one login test
    
    * [skip e2e]

--- e2e/tests/user/login.spec.ts
+++ e2e/tests/user/login.spec.ts
@@ -1,7 +1,6 @@
 import CookiePolicyPage from 'app/page/cookie-policy';
-import GoogleLoginPage, { FieldSelector } from 'app/page/google-login';
+import GoogleLoginPage from 'app/page/google-login';
 import { config } from 'resources/workbench-config';
-import { waitForText } from 'utils/waits-utils';
 
 describe('Login tests:', () => {
   test('Cookie banner visible on login page', async () => {
@@ -23,25 +22,5 @@ describe('Login tests:', () => {
     await page.goto(url, { waitUntil: 'networkidle0' });
     const loginPage = new GoogleLoginPage(page);
     expect(await loginPage.loginButton()).toBeTruthy();
-  });
-
-  test('Entered wrong password', async () => {
-    const INCORRECT_PASSWORD = 'wrongpassword123';
-    const loginPage = new GoogleLoginPage(page);
-    await loginPage.load();
-
-    const naviPromise = page.waitForNavigation({ waitUntil: 'networkidle0' });
-    const googleButton = await loginPage.loginButton();
-    await googleButton.click();
-    await naviPromise;
-
-    await loginPage.enterEmail(config.userEmail);
-    await loginPage.enterPassword(INCORRECT_PASSWORD);
-
-    const button = await page.waitForXPath(FieldSelector.NextButton, { visible: true });
-    await button.click();
-
-    const err = await waitForText(page, 'Wrong password. Try again');
-    expect(err).toBeTruthy();
   });
 });
