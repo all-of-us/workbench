8963e773c6aec38a94a1f3e1071a32d09c2ad97b
petesantos <psantos@broadinstitute.org>
2021-04-09 11:43:18 -0400

    clean up cond test

--- ui/src/app/utils/index.spec.tsx
+++ ui/src/app/utils/index.spec.tsx
@@ -4,18 +4,10 @@ import * as fp from 'lodash/fp';
 
 describe('Helper functions', () => {
   it('(cond) Should handle conditionals and defaults correctly', async() => {
-    const test = (x, y) => {
-      return [
-        Utils.cond<number | string>([x === 5, () => 1], [y === 10, () => 2], () => 'default'),
-        Utils.cond<number | string>([x === 4, () => 1], [y === 10, () => 2], () => 'default'),
-        Utils.cond<number | string>([x === 4, () => 1], [y === 9, () => 2], () => 'default')
-      ]
-    }
-
-    const [r1, r2, r3] = test(5, 10);
-    expect(r1).toBe(1);
-    expect(r2).toBe(2);
-    expect(r3).toBe('default');
+    const test = x =>  Utils.cond<number | string>([x === 5, () => 1], [x === 10, () => 2], () => 'default');     
+    expect(test(5)).toBe(1);
+    expect(test(10)).toBe(2);
+    expect(test(9)).toBe('default');
   });
 
   it('(maybe) Should return nothing when the value is not set and execute the function when it is', async() => {
