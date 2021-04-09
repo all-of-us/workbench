49f8e2fcb1db02c2d80fce7a55d1a1a19c791f63
petesantos <psantos@broadinstitute.org>
2021-04-09 11:45:05 -0400

    linting

--- ui/src/app/utils/index.spec.tsx
+++ ui/src/app/utils/index.spec.tsx
@@ -4,7 +4,7 @@ import * as fp from 'lodash/fp';
 
 describe('Helper functions', () => {
   it('(cond) Should handle conditionals and defaults correctly', async() => {
-    const test = x =>  Utils.cond<number | string>([x === 5, () => 1], [x === 10, () => 2], () => 'default');     
+    const test = x =>  Utils.cond<number | string>([x === 5, () => 1], [x === 10, () => 2], () => 'default');
     expect(test(5)).toBe(1);
     expect(test(10)).toBe(2);
     expect(test(9)).toBe('default');
