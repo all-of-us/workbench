ddf586a18293db6b217a941f221741c8bc01ff08
Pete Santos <50371603+petesantos@users.noreply.github.com>
2021-04-09 11:56:11 -0400

    [RW-6256][risk=no] Add data access panel to profile (controlled tier) (#4803)
    * Base Changes
    
    * Add tests
    
    * Update configs for controlled tier
    
    * update configs
    
    * Pass tiers to access panel
    
    * update cond and maybe
    
    * Add registered tier text
    
    * Update test
    
    * linting
    
    * linting
    
    * remove console statement
    
    * remove export
    
    * update href
    
    * Move test helpers to existing test file
    
    * use fp.includes
    
    * Turn off the controlled tier sections
    
    * Update link, comment
    
    * Added comments, fixed typo
    
    * Added comment
    
    * Add test for cond
    
    * Add test for maybe
    
    * Update maybe test desc
    
    * linting
    
    * clean up cond test
    
    * linting

--- ui/src/app/utils/index.tsx
+++ ui/src/app/utils/index.tsx
@@ -630,3 +630,26 @@ export const useId = () => {
   const [id] = useState(() => fp.uniqueId('element-'));
   return id;
 };
+
+export const nothing = {};
+
+// maybe - takes a function and a value. If the value is not defined returns "nothing"
+// Example usage: fp.flow(maybe(doSomethingIfIhaveData), maybe(doAnotherThingIfThereIsAResult))(getData())
+export const maybe = fp.curry((fn, value) => value !== nothing && value ? fn(value) : nothing);
+
+// cond - useful for representing conditionals as an expression
+// Operates like fp.cond, but a bit more concise - no need for array of arrays and allows for a clear default case
+// Example usage: cond<numer | string>(
+//  [v === 1, () => v + 2],
+//  [v === 2, () => v * 2],
+//  () => 'default' // Default case
+// )
+export const cond = <T extends unknown>(...args: ([boolean, () => T] | (() => T))[]) => {
+  for (const arg of args) {
+    // If the arg is an array, conditionally execute (maybe). If not an array, then this is the default case.
+    const result = Array.isArray(arg) ? maybe(...fp.reverse(arg)) : arg();
+    if (result !== nothing) {
+      return result;
+    }
+  }
+};
