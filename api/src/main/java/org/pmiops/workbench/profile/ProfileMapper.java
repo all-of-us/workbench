d4e4bf467f3d62b0a32ab63d2a5a5d6d5a2b6afe
jmthibault79 <2701406+jmthibault79@users.noreply.github.com>
2021-04-21 10:13:22 -0400

    [risk=no][RW-6189] Remove the final references to DataAccessLevel (#4845)

--- api/src/main/java/org/pmiops/workbench/profile/ProfileMapper.java
+++ api/src/main/java/org/pmiops/workbench/profile/ProfileMapper.java
@@ -8,12 +8,11 @@ import javax.annotation.Nullable;
 import org.apache.commons.lang3.StringUtils;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.pmiops.workbench.db.dao.UserDao;
+import org.pmiops.workbench.db.dao.UserDao.DbAdminTableUser;
 import org.pmiops.workbench.db.model.DbStorageEnums;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbUserTermsOfService;
 import org.pmiops.workbench.model.AdminTableUser;
-import org.pmiops.workbench.model.DataAccessLevel;
 import org.pmiops.workbench.model.Profile;
 import org.pmiops.workbench.model.VerifiedInstitutionalAffiliation;
 import org.pmiops.workbench.utils.mappers.CommonMappers;
@@ -41,10 +40,9 @@ public interface ProfileMapper {
       DbUserTermsOfService latestTermsOfService,
       Double freeTierUsage,
       Double freeTierDollarQuota,
-      List<String> accessTierShortNames,
-      DataAccessLevel dataAccessLevel);
+      List<String> accessTierShortNames);
 
-  AdminTableUser adminViewToModel(UserDao.DbAdminTableUser dbUser, DataAccessLevel dataAccessLevel);
+  List<AdminTableUser> adminViewToModel(List<DbAdminTableUser> adminTableUsers);
 
   // used by the generated impl of adminViewToModel()
   default List<String> splitAccessTierShortNames(
