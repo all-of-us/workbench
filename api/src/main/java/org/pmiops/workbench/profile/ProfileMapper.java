ede5dd30e3fb3e5ad7c3a5cf58a31e0c3a95efae
petesantos <psantos@broadinstitute.org>
2021-04-22 12:14:43 -0400

    Update API

--- api/src/main/java/org/pmiops/workbench/profile/ProfileMapper.java
+++ api/src/main/java/org/pmiops/workbench/profile/ProfileMapper.java
@@ -34,6 +34,7 @@ public interface ProfileMapper {
   @Mapping(source = "latestTermsOfService.tosVersion", target = "latestTermsOfServiceVersion")
   @Mapping(source = "latestTermsOfService.agreementTime", target = "latestTermsOfServiceTime")
   @Mapping(source = "dbUser.userId", target = "userId")
+  @Mapping(target = "accessHasExpired", ignore = true)
   Profile toModel(
       DbUser dbUser,
       VerifiedInstitutionalAffiliation verifiedInstitutionalAffiliation,
