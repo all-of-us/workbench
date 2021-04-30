f2e38a82caff2561197029f7c2d14668705a034a
Eric Song <me@ericsong.io>
2021-04-30 16:57:44 -0400

    make change to force puppeteer test

--- api/src/main/java/org/pmiops/workbench/Controller.java
+++ api/src/main/java/org/pmiops/workbench/Controller.java
@@ -3,6 +3,7 @@ package org.pmiops.workbench;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+// XXX: trigger CI - revert.
 @RestController
 public class Controller {
   @RequestMapping("/")
