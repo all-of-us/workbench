807d87f77a21020824d45325bd55b927d87dd964
Zahra Rasuli <zahrarasuli@VICTRs-MacBook-Pro.local>
2021-04-30 11:16:52 -0500

    converted some of python code into bash

--- api/db-cdr/prep-tables/make-prep-survey.sh
+++ api/db-cdr/prep-tables/make-prep-survey.sh
@@ -14,6 +14,38 @@ CSV_OUTPUT_DIR="output"
 ALL_CSV="_updated_all.csv"
 REGISTERED_CSV="_updated_registered.csv"
 CONTROLLED_CSV="_updated_controlled.csv"
+# we will increment this as the row number as we write each row in each output file
+idNumControlled = 1
+idNumRegistered = 1
+idNumAll = 1
+
+# created an array of Field Names to skip and can add to this if needed
+skipFieldNames = ('record_id')
+
+# these are the column names that we are writing in the controlled and registered output files
+    headers = ('id', 'parent_id', 'code', 'name', 'type', 'min', 'max',
+               'answers_bucketed')
+
+    # these are the headers for outputAll file
+    headersAll = ('id', 'parent_id', 'code', 'name', 'type', 'min', 'max'
+        , 'registered_topic_suppressed', 'registered_question_suppressed',
+                  'registered_answer_bucketed'
+        , 'controlled_topic_suppressed', 'controlled_question_suppressed',
+                  'controlled_answer_bucketed')
+
+    # these are the flags that we will look for in the original file
+    possibleFlags = ('REGISTERED_QUESTION_SUPPRESSED',
+                     'REGISTERED_TOPIC_SUPPRESSED',
+                     'REGISTERED_ANSWERS_BUCKETED'
+        , 'CONTROLLED_QUESTION_SUPPRESSED', 'CONTROLLED_TOPIC_SUPPRESSED',
+                     'CONTROLLED_ANSWERS_BUCKETED')
+    # initializing these variables in the case that the topic is null
+        topicIdAll = 0
+        topicIdControlled = 0
+        topicIdRegister = 0
+
+        # we will use this when the rowNumber = 1 to add in as the first row
+        rowNumber = 0
 
 function main() {
   rm -rf $TEMP_FILE_DIR
@@ -54,8 +86,53 @@ function main() {
 
       initalizeHeaderRows $file
 
-      while IFS= read -r line
+      while IFS="," read -r rec_column1 rec_column2 rec_column3 rec_column4 rec_column5 rec_column6 rec_column7 rec_column8 rec_column9 rec_column10 rec_column11 rec_column12 rec_column13 rec_column14 rec_column15 rec_column16 rec_column17 rec_column18
+      #Variable / Field Name Form Name Section Header Field Type Field Label Choices,Calculations, OR Slider Labels Field Note Text Validation Type OR Show Slider Number Text Validation Min Text Validation Max Identifier? Branching Logic Required Field? Custom Alignment Question Number Matrix Group Name Matrix Ranking? Field Annotation
       do
+        if [ ! "${skipFieldNames[@]}" =~ $rec_column1 ] && [ "${rec_column4,,}" != "descriptive" ] && ["${rec_column5,,}" != *"please specify"*]; then
+          if [ ${rec_column18[@]} -eq 0 ]; then
+            IFS=',' read -ra fieldAnnotationArray <<< "$rec_column18"
+            for i in "${fieldAnnotationArray[@]}"; do
+            done
+            questionLabel = tr '\n' ' ' < rec_column5  # replace all '\n' with empty string
+            questionCode = rec_column1
+            textValidation = rec_column8
+            choices = rec_column6
+            ((rowNumber+=1))
+
+            declare -A itemFlags
+            for item in "${!itemFlags[@]}"; do
+            if [[$item == "$rec_column18" ]]; then
+                  itemFlags[item] = 1
+              else:
+                  itemFlags[item] = 0
+
+            # in the case that fieldAnnotationsArray contain long code = short code
+            declare -A fieldNames
+            for item in " ${fieldAnnotationArray[@]} "; do
+              if [[ ! " ${possibleFlags[@]} " =~ $item ]]; then
+                IFS='=' read -ra splitCodes <<< "$item"
+                  # in the case that it doesn't have a code ex: Launched 5/30/2017 (PTSC) & 12/10/2019 (CE).
+                  if [ "${#splitCodes[@]}" -gt 1]; then
+                    fieldNames["${splitCodes[0],,}"] = "${splitCodes[1],,}"
+
+             # for the first row of survey
+             if["$rowNumber" == 1]; then
+            surveryIdAll = $idNumAll
+            buildRowAll(allFile, 0, questionCode, questionLabel,
+                        'SURVEY',
+                        None, None, None, None)
+
+            #continue
+
+
+        exit 1
+
+
+
+
+
+
 
         # All of your work goes here. Below some code to query the BQ
         # database and write some csv files for All, Controlled and Registered
@@ -102,6 +179,45 @@ function initalizeHeaderRows() {
 function writeAllRow() {
   # Write a row to All file
   filePrefix $1
+  parentId $2
+  code $3
+  name $4
+  itemType $5
+  minValue $6
+  maxValue, $7
+  itemFlags $8
+  fieldNames $9
+  declare -A dictRow=( ["id"]= "$idNumAll" ["parent_id"]="$parent_id" ["code"]="$code" ["name"]="$name" ["type"]="$itemType" ["min"]="$minValue" ["max"]="$maxValue" )
+ if [[ "$itemType" == "TOPIC"]]; then
+dictRow['registered_topic_suppressed'] = itemFlags[
+            "REGISTERED_TOPIC_SUPPRESSED"]
+        dictRow['registered_question_suppressed'] = '0'
+        dictRow['registered_answer_bucketed'] = '0'
+        dictRow['controlled_topic_suppressed'] = itemFlags[
+            "CONTROLLED_TOPIC_SUPPRESSED"]
+        dictRow['controlled_question_suppressed'] = '0'
+        dictRow['controlled_answer_bucketed'] = '0'
+elif [[ "$itemType" == "QUESTION"]]; then
+#  questionShortCode = getShortCode(code, fieldNames)
+#        dictRow['code'] = questionShortCode
+        dictRow['registered_topic_suppressed'] = '0'
+        dictRow['registered_question_suppressed'] = itemFlags[
+            "REGISTERED_QUESTION_SUPPRESSED"]
+        dictRow['registered_answer_bucketed'] = itemFlags[
+            "REGISTERED_ANSWERS_BUCKETED"]
+        dictRow['controlled_topic_suppressed'] = '0'
+        dictRow['controlled_question_suppressed'] = itemFlags[
+            "CONTROLLED_QUESTION_SUPPRESSED"]
+        dictRow['controlled_answer_bucketed'] = itemFlags[
+            "CONTROLLED_ANSWERS_BUCKETED"]
+  else
+        dictRow['registered_topic_suppressed'] = '0'
+        dictRow['registered_question_suppressed'] = '0'
+        dictRow['registered_answer_bucketed'] = '0'
+        dictRow['controlled_topic_suppressed'] = '0'
+        dictRow['controlled_question_suppressed'] = '0'
+        dictRow['controlled_answer_bucketed'] = '0'
+
   echo "0,0,001,name,type,0,0,0,0,0,0,0,0" >> "$TEMP_FILE_DIR/$prefix$ALL_CSV"
 }
 
