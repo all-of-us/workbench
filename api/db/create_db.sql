29b7c37d6c66242d93dd75ebb3b8d0ead94eb349
Brian Freeman <brian.d.freeman@gmail.com>
2021-05-05 10:22:53 -0500

    adding comment

--- api/db/create_db.sql
+++ api/db/create_db.sql
@@ -12,7 +12,7 @@ SET PASSWORD FOR '${LIQUIBASE_DB_USER}'@'%' = '${LIQUIBASE_DB_PASSWORD}';
 GRANT SELECT, CREATE TEMPORARY TABLES ON *.* TO '${DEV_READONLY_DB_USER}'@'%';
 
 -- Give main db access and wildcard permission to cdr databases for workbench
--- cdr* is the older and/or local naming convention, synth_r_*, r_* is new
+-- cdr* is the older and/or local naming convention, synth_r_*, r_* is registered tier and c_* is controlled tier
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES ON ${DB_NAME}.* TO '${WORKBENCH_DB_USER}'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES ON `cdr%`.* TO '${WORKBENCH_DB_USER}'@'%';
 GRANT SELECT, INSERT, UPDATE, DELETE, CREATE TEMPORARY TABLES ON `r_%`.* TO '${WORKBENCH_DB_USER}'@'%';
