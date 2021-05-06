8972a1d022f21f56d37cf39c7caaecc58d3384e1
Brian Freeman <brian.d.freeman@gmail.com>
2021-05-06 16:29:42 -0500

    writing files

--- api/db-cdr/prep-tables/make-prep-survey.py
+++ api/db-cdr/prep-tables/make-prep-survey.py
@@ -4,12 +4,20 @@ from google.cloud import bigquery
 from google.cloud import storage
 from io import StringIO
 
-# key = csv
+# key = csv prefix, value = csv output file name
 surveys = {"ENGLISHBasics_DataDictionary": "Basics",
            "ENGLISHHealthCareAccessUtiliza_DataDictionary": "HealthCareAccessUtiliza",
            "ENGLISHLifestyle_DataDictionary": "Lifestyle",
            "ENGLISHOverallHealth_DataDictionary": "OverallHealth",
            "ENGLISHPersonalMedicalHistory_DataDictionary": "PersonalMedicalHistory"}
+# these are the column names for the controlled and registered output files
+headers = ['id', 'parent_id', 'code', 'name', 'type', 'min', 'max',
+           'answers_bucketed']
+# these are the headers for outputAll file
+headersAll = ['id', 'parent_id', 'code', 'name', 'type', 'min', 'max',
+              'registered_topic_suppressed', 'registered_question_suppressed',
+              'registered_answer_bucketed', 'controlled_topic_suppressed',
+              'controlled_question_suppressed', 'controlled_answer_bucketed']
 
 
 def main():
@@ -21,21 +29,31 @@ def main():
 
     for name in surveys:
         rows = read_csv(name + '_' + date + '.csv')
+
+        # open your file writers for this survey and write headers
+        csv_controlled = open(surveys.get(name) + "_controlled.csv", 'w')
+        csv_registered = open(surveys.get(name) + "_registered.csv", 'w')
+        csv_all = open(surveys.get(name) + "_all.csv", 'w')
+        controlled_writer = csv.DictWriter(csv_controlled, fieldnames=headers)
+        controlled_writer.writeheader()
+        registered_writer = csv.DictWriter(csv_registered, fieldnames=headers)
+        registered_writer.writeheader()
+        all_writer = csv.DictWriter(csv_all, fieldnames=headersAll)
+        all_writer.writeheader()
+
         for row in rows:
+            # print the first column of each row
             print(f"First Column: {row[0]}")
 
-    big_query_client = bigquery.Client.from_service_account_json(
-        "../../sa-key.json")
-    query = """
-    SELECT concept_code FROM `$project.dataset.concept`
-    WHERE concept_name = "$concept_name" and vocabulary_id = "PPI" and
-    concept_class_id = "Topic"
-    """.replace("$project.dataset", project + "." + dataset).replace(
-        "$concept_name", "Respiratory Conditions")
-    query_job = big_query_client.query(query)
-    rows = query_job.result()
-    for row in rows:
-        print(row.concept_code)
+            # This is where your code will go
+
+            buildRowAll(allFile, 0, questionCode, questionLabel, 'SURVEY', None,
+                        None, None, None)
+
+    # This will query the concept table using concept_name
+    results = query_concept_table(project, dataset, "Respiratory Conditions")
+    for result in results:
+        print(result.concept_code)
 
 
 def parse_args():
@@ -77,6 +95,19 @@ def get_blob(file):
     bucket = storage_client.get_bucket(
         'all-of-us-workbench-private-cloudsql')
     return bucket.blob('cb_prep_tables/redcap/' + file)
+
+
+def query_concept_table(project, dataset, concept_name):
+    big_query_client = bigquery.Client.from_service_account_json(
+        "../../sa-key.json")
+    query = """
+    SELECT concept_code FROM `$project.dataset.concept`
+    WHERE concept_name = "$concept_name" and vocabulary_id = "PPI" and
+    concept_class_id = "Topic"
+    """.replace("$project.dataset", project + "." + dataset).replace(
+        "$concept_name", concept_name)
+    query_job = big_query_client.query(query)
+    return query_job.result()
 
 
 if __name__ == '__main__':
