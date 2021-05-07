e52b8a1294b0f1dc292d7e8d4a39d0b68d8e315b
Brian Freeman <brian.d.freeman@gmail.com>
2021-05-07 11:43:34 -0500

    writing files to csv directory

--- api/db-cdr/prep-tables/make-prep-survey.py
+++ api/db-cdr/prep-tables/make-prep-survey.py
@@ -1,8 +1,14 @@
 import argparse
 import csv
+import os
+import shutil
 from google.cloud import bigquery
 from google.cloud import storage
 from io import StringIO
+
+all_seq = 1
+
+home_dir = "../csv"
 
 # key = csv prefix, value = csv output file name
 surveys = {"ENGLISHBasics_DataDictionary": "Basics",
@@ -27,13 +33,19 @@ def main():
     # validate that all files for a specific date exist in the bucket
     files_exist(date)
 
+    # setup csv directory to hold output csv files
+    if os.path.exists(home_dir):
+        shutil.rmtree(home_dir)
+    os.mkdir(home_dir)
+
     for name in surveys:
         rows = read_csv(name + '_' + date + '.csv')
 
         # open your file writers for this survey and write headers
-        csv_controlled = open(surveys.get(name) + "_controlled.csv", 'w')
-        csv_registered = open(surveys.get(name) + "_registered.csv", 'w')
-        csv_all = open(surveys.get(name) + "_all.csv", 'w')
+        file_prefix = home_dir + "/" + surveys.get(name)
+        csv_controlled = open(file_prefix + "_controlled.csv", 'w')
+        csv_registered = open(file_prefix + "_registered.csv", 'w')
+        csv_all = open(file_prefix + "_all.csv", 'w')
         controlled_writer = csv.DictWriter(csv_controlled, fieldnames=headers)
         controlled_writer.writeheader()
         registered_writer = csv.DictWriter(csv_registered, fieldnames=headers)
@@ -47,13 +59,16 @@ def main():
 
             # This is where your code will go
 
-            buildRowAll(allFile, 0, questionCode, questionLabel, 'SURVEY', None,
-                        None, None, None)
+            write_row_all(all_writer, 0, "question_code", "question_label",
+                          'SURVEY', None, None, None, None)
 
     # This will query the concept table using concept_name
     results = query_concept_table(project, dataset, "Respiratory Conditions")
     for result in results:
         print(result.concept_code)
+
+    # when done remove directory and all files
+    shutil.rmtree(home_dir)
 
 
 def parse_args():
@@ -108,6 +123,46 @@ def query_concept_table(project, dataset, concept_name):
         "$concept_name", concept_name)
     query_job = big_query_client.query(query)
     return query_job.result()
+
+
+def write_row_all(writer, parent_id, code, name, item_type, min_val, max_val,
+    item_flags, field_names):
+    global all_seq
+    new_row = {'id': all_seq, 'parent_id': parent_id, 'code': code,
+               'name': name, 'type': item_type, 'min': min_val, 'max': max_val,
+               'registered_topic_suppressed': '0',
+               'registered_question_suppressed': '0',
+               'registered_answer_bucketed': '0',
+               'controlled_topic_suppressed': '0',
+               'controlled_question_suppressed': '0',
+               'controlled_answer_bucketed': '0'}
+    if item_type == 'TOPIC':
+        new_row['registered_topic_suppressed'] = item_flags[
+            "REGISTERED_TOPIC_SUPPRESSED"]
+        new_row['controlled_topic_suppressed'] = item_flags[
+            "CONTROLLED_TOPIC_SUPPRESSED"]
+    elif item_type == 'QUESTION':
+        # we have to replace questionCode with short code in Field Annotation
+        new_row['code'] = get_short_code(code, field_names)
+        new_row['registered_question_suppressed'] = item_flags[
+            "REGISTERED_QUESTION_SUPPRESSED"]
+        new_row['registered_answer_bucketed'] = item_flags[
+            "REGISTERED_ANSWERS_BUCKETED"]
+        new_row['controlled_question_suppressed'] = item_flags[
+            "CONTROLLED_QUESTION_SUPPRESSED"]
+        new_row['controlled_answer_bucketed'] = item_flags[
+            "CONTROLLED_ANSWERS_BUCKETED"]
+    writer.writerow(new_row)
+    all_seq += 1
+
+
+def get_short_code(code, field_names):
+    for item in field_names:
+        if code in field_names:
+            code = field_names[code]
+        else:
+            code
+        return code
 
 
 if __name__ == '__main__':
