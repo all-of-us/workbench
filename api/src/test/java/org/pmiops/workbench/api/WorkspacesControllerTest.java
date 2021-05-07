8f7bb6a60c915c9cf500c5b853e3613e0cf1b427
yonghao <yyhao1@gmail.com>
2021-05-06 21:31:24 -0400

    fixing NOW_time

--- api/src/test/java/org/pmiops/workbench/api/WorkspacesControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/WorkspacesControllerTest.java
@@ -21,6 +21,7 @@ import static org.mockito.Mockito.verifyZeroInteractions;
 import static org.mockito.Mockito.when;
 import static org.pmiops.workbench.BaseTestConfiguration.CLOCK;
 import static org.pmiops.workbench.BaseTestConfiguration.NOW;
+import static org.pmiops.workbench.BaseTestConfiguration.NOW_TIME;
 import static org.pmiops.workbench.utils.TestMockFactory.DEFAULT_GOOGLE_PROJECT;
 
 import com.google.api.services.cloudbilling.Cloudbilling;
