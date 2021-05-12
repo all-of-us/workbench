a0cf931683fe03c6d6bf2447730e0485c254acd8
petesantos <psantos@broadinstitute.org>
2021-05-12 16:17:25 -0400

    Update comment and port

--- api/tools/src/cron/expire-users.sh
+++ api/tools/src/cron/expire-users.sh
@@ -1,6 +1,5 @@
 #!/bin/bash
 
-# Start hit the reporting snapshot & upload cron endpoint (insert query mode)
-# locally.
-curl -X GET 'http://localhost:8001/v1/cron/expireNonCompliantUsers' \
+# Expire users who have not completed their compliance modules in over a year
+curl -X GET 'http://localhost:8081/v1/cron/expireNonCompliantUsers' \
   --header "X-AppEngine-Cron: true"
