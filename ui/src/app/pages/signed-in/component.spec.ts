c73ee6c93fe2b6b5bf1dc48532724474775d6c71
jmthibault79 <2701406+jmthibault79@users.noreply.github.com>
2021-05-14 08:47:47 -0400

    [risk=low][no ticket] Disable flaky Angular test (#4995)

--- ui/src/app/pages/signed-in/component.spec.ts
+++ ui/src/app/pages/signed-in/component.spec.ts
@@ -20,8 +20,9 @@ import {RoutingSpinnerComponent} from 'app/components/routing-spinner/component'
 import {TextModalComponent} from 'app/components/text-modal';
 import {SignedInComponent} from 'app/pages/signed-in/component';
 import {NavBarComponent} from 'app/pages/signed-in/nav-bar';
-import {CdrVersionsApi, StatusAlertApi} from 'generated/fetch';
+import {CdrVersionsApi, ProfileApi, StatusAlertApi} from 'generated/fetch';
 import {CdrVersionsApiStub} from 'testing/stubs/cdr-versions-api-stub';
+import {ProfileApiStub} from 'testing/stubs/profile-api-stub';
 import {StatusAlertApiStub} from 'testing/stubs/status-alert-api-stub';
 
 describe('SignedInComponent', () => {
@@ -52,6 +53,7 @@ describe('SignedInComponent', () => {
   }));
 
   beforeEach(() => {
+    registerApiClient(ProfileApi, new ProfileApiStub());
     registerApiClient(CdrVersionsApi, new CdrVersionsApiStub());
     registerApiClient(StatusAlertApi, new StatusAlertApiStub());
   });
