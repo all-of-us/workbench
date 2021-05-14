dd31544211592ab50b73c9ec8cd76926951f2bc0
Joel Thibault <thibault@broad.dev>
2021-05-14 16:48:15 -0400

    lint

--- ui/src/app/app.module.ts
+++ ui/src/app/app.module.ts
@@ -1,6 +1,6 @@
+import {HttpClientModule} from '@angular/common/http';
 import {NgModule} from '@angular/core';
 import {FormsModule, ReactiveFormsModule} from '@angular/forms';
-import {HttpClientModule} from '@angular/common/http';
 import {BrowserModule} from '@angular/platform-browser';
 import {BrowserAnimationsModule} from '@angular/platform-browser/animations';
 import {RouteReuseStrategy} from '@angular/router';
