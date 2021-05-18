130248b1452f742a88bfd6de94c8a2b390b6c356
CH Albach <calbach@google.com>
2021-05-17 18:05:08 -0700

    [RW-6723][risk=no] Split out WGS CDR publishing

--- api/db-cdr/generate-cdr/libproject/devstart.rb
+++ api/db-cdr/generate-cdr/libproject/devstart.rb
@@ -12,6 +12,7 @@ ENVIRONMENTS = {
     :accessTiers => {
       "registered" => {
         :source_cdr_project => "all-of-us-ehr-dev",
+        :source_cdr_wgs_project => "all-of-us-workbench-test",
         :ingest_cdr_project => "fc-aou-vpc-ingest-test",
         :dest_cdr_project => "fc-aou-cdr-synth-test",
         :auth_domain_group_email => "GROUP_all-of-us-registered-test@dev.test.firecloud.org",
@@ -66,6 +67,7 @@ ENVIRONMENTS = {
     :accessTiers => {
       "registered" => {
         :source_cdr_project => "all-of-us-ehr-dev",
+        :source_cdr_wgs_project => "aou-genomics-curation-stable",
         :ingest_cdr_project => "fc-aou-vpc-ingest-stable",
         :dest_cdr_project => "fc-aou-cdr-synth-stable",
         :auth_domain_group_email => "GROUP_all-of-us-registered-stable@firecloud.org",
@@ -84,6 +86,7 @@ ENVIRONMENTS = {
       },
       "controlled" => {
         :source_cdr_project => "aou-res-curation-output-prod",
+        :source_cdr_wgs_project => "aou-genomics-curation-prod",
         :ingest_cdr_project => "fc-aou-vpc-ingest-preprod-ct",
         :dest_cdr_project => "fc-aou-cdr-preprod-ct",
         :auth_domain_group_email => "all-of-us-controlled-preprod@firecloud.org",
@@ -104,6 +107,20 @@ ENVIRONMENTS = {
   }
 }
 
+def prefixes_to_grep_filter(prefixes)
+  return "^\\(#{prefixes.join("\\|")}\\)"
+end
+
+WGS_TABLE_PREFIXES = [
+  "alt_allele",
+  "filter_set_",
+  "pet_",
+  "sample_info",
+  "vet_"
+]
+WGS_TABLE_FILTER = prefixes_to_grep_filter(WGS_TABLE_PREFIXES)
+
+
 # TODO: RW-6426. Refactor ENVIRONMENTS object in this file and api/libproject/devstart.rb
 # Share ENVIRONMENT related code and config parsing
 def must_get_env_value(env, key)
@@ -122,6 +139,12 @@ def get_config(project)
   return JSON.parse(File.read(get_config_file(project)))
 end
 
+def must_get_wgs_proxy_group(project)
+  v = get_config(project).fetch("wgsCohortExtraction", {}).fetch("serviceAccountTerraProxyGroup")
+  raise ArgumentError.new("no WGS proxy group configured for env #{project}") unless v
+  return v
+end
+
 def ensure_docker(cmd_name, args=nil)
   args = (args or [])
   unless Workbench.in_docker?
@@ -129,12 +152,82 @@ def ensure_docker(cmd_name, args=nil)
   end
 end
 
+def service_account_context_for_bq(project, account)
+  common = Common.new
+  # TODO(RW-3208): Investigate using a temporary / impersonated SA credential instead of a key.
+  key_file = Tempfile.new(["#{account}-key", ".json"], "/tmp")
+  ServiceAccountContext.new(
+    project, account, key_file.path).run do
+    # TODO(RW-3768): This currently leaves the user session with an activated service
+    # account user. Ideally the activation would be hermetic within the docker
+    # session, or else we would revert the active account after running.
+    common.run_inline %W{gcloud auth activate-service-account -q --key-file #{key_file.path}}
+    yield
+  end
+end
+
+# By default, skip empty lines only.
+def bq_ingest(tier, source_project, dataset_name, table_match_filter="", table_skip_filter="^$")
+  common = Common.new
+  source_dataset = "#{source_project}:#{dataset_name}"
+  ingest_dataset = "#{tier.fetch(:ingest_cdr_project)}:#{dataset_name}"
+  dest_dataset = "#{tier.fetch(:dest_cdr_project)}:#{dataset_name}"
+  common.status "Copying from '#{source_dataset}' -> '#{ingest_dataset}' -> '#{dest_dataset}'"
+
+  # If you receive an error from "bq" like "Invalid JWT Signature", you may
+  # need to delete cached BigQuery creds on your local machine. Try running
+  # bq init --delete_credentials as recommended in the output.
+  # TODO(RW-3768): Find a better solution for Google credentials in docker.
+
+  # Copy through an intermediate project and delete after (include TTL in case later steps fail).
+  # See https://docs.google.com/document/d/1EHw5nisXspJjA9yeZput3W4-vSIcuLBU5dPizTnk1i0/edit
+  common.run_inline %W{bq mk -f --default_table_expiration 7200 --dataset #{ingest_dataset}}
+  common.run_inline %W{./copy-bq-dataset.sh
+      #{source_dataset} #{ingest_dataset} #{source_project}
+      #{table_match_filter} #{table_skip_filter}}
+
+  common.run_inline %W{bq mk -f --dataset #{dest_dataset}}
+  common.run_inline %W{./copy-bq-dataset.sh
+      #{ingest_dataset} #{dest_dataset} #{tier.fetch(:ingest_cdr_project)}
+      #{table_match_filter} #{table_skip_filter}}
+
+  # Delete the intermediate dataset.
+  common.run_inline %W{bq rm -r -f --dataset #{ingest_dataset}}
+end
+
+def bq_update_acl(dataset)
+  common = Common.new
+  common.status(dataset)
+
+  config_file = Tempfile.new("bq-acls.json")
+  begin
+    json = JSON.parse(
+      common.capture_stdout %{bq show --format=prettyjson #{dataset}})
+    existing_groups = Set[]
+    existing_users = Set[]
+    for entry in json["access"]
+      if entry.key?("groupByEmail")
+        existing_groups.add(entry["groupByEmail"])
+      end
+      if entry.key?("userByEmail")
+        existing_users.add(entry["userByEmail"])
+      end
+    end
+
+    json = yield(json, existing_groups, existing_users)
+    File.open(config_file.path, "w") do |f|
+      f.write(JSON.pretty_generate(json))
+    end
+    common.run_inline %W{bq update --source #{config_file.path} #{dataset}}
+  ensure
+    config_file.unlink
+  end
+end
+
 def publish_cdr(cmd_name, args)
   ensure_docker cmd_name, args
 
   op = WbOptionsParser.new(cmd_name, args)
-  op.opts.exclude_sa_acl = false
-  op.opts.exclude_auth_domain_acl = false
 
   op.add_option(
     "--bq-dataset [dataset]",
@@ -164,24 +257,6 @@ def publish_cdr(cmd_name, args)
     "was an issue with the publish. In general, CDRs should be treated as " +
     "immutable after the initial publish."
   )
-  op.add_typed_option(
-      "--exclude-sa-acl",
-      TrueClass,
-      ->(opts, v) { opts.exclude_sa_acl = v},
-      "When true, does not include the default service account in the ACL. Defaults to false which includes the SA.")
-  op.add_typed_option(
-      "--exclude-auth-domain-acl",
-      TrueClass,
-      ->(opts, v) { opts.exclude_auth_domain_acl = v},
-      "When true, does not include the auth domain in the ACL. Defaults to false which includes the auth domain.")
-  op.add_option(
-      "--additional-reader-group [reader_email]",
-      ->(opts, v) { opts.additional_reader_group = v},
-      "Additional Google group to include in the reader ACL.")
-  op.add_option(
-    "--source-cdr-project-override [source-cdr-project]",
-    ->(opts, v) { opts.source_cdr_project_override = v},
-    "Override for the source cdr project where the source dataset is.")
   op.add_validator ->(opts) { raise ArgumentError unless opts.bq_dataset and opts.project and opts.tier }
   op.add_validator ->(opts) { raise ArgumentError.new("unsupported project: #{opts.project}") unless ENVIRONMENTS.key? opts.project }
   op.add_validator ->(opts) { raise ArgumentError.new("unsupported tier: #{opts.tier}") unless ENVIRONMENTS[opts.project][:accessTiers].key? opts.tier }
@@ -200,103 +275,34 @@ def publish_cdr(cmd_name, args)
 
   common = Common.new
   env = ENVIRONMENTS[op.opts.project]
-  account = env.fetch(:publisher_account)
-  app_sa = "#{op.opts.project}@appspot.gserviceaccount.com"
   tier = env.fetch(:accessTiers)[op.opts.tier]
+  source_cdr_project = tier.fetch(:source_cdr_project)
+  dest_dataset = "#{tier.fetch(:dest_cdr_project)}:#{op.opts.bq_dataset}"
 
-  # TODO(RW-3208): Investigate using a temporary / impersonated SA credential instead of a key.
-  key_file = Tempfile.new(["#{account}-key", ".json"], "/tmp")
-  ServiceAccountContext.new(
-    op.opts.project, account, key_file.path).run do
-    # TODO(RW-3768): This currently leaves the user session with an activated service
-    # account user. Ideally the activation would be hermetic within the docker
-    # session, or else we would revert the active account after running.
-    common.run_inline %W{gcloud auth activate-service-account -q --key-file #{key_file.path}}
+  service_account_context_for_bq(op.opts.project, env.fetch(:publisher_account)) do
+    bq_ingest(tier, source_cdr_project, op.opts.bq_dataset, table_match_filter, table_skip_filter)
 
-    source_cdr_project = op.opts.source_cdr_project_override || tier.fetch(:source_cdr_project)
-    source_dataset = "#{source_cdr_project}:#{op.opts.bq_dataset}"
-    ingest_dataset = "#{tier.fetch(:ingest_cdr_project)}:#{op.opts.bq_dataset}"
-    dest_dataset = "#{tier.fetch(:dest_cdr_project)}:#{op.opts.bq_dataset}"
-    common.status "Copying from '#{source_dataset}' -> '#{ingest_dataset}' -> '#{dest_dataset}' as #{account}"
-
-    # If you receive an error from "bq" like "Invalid JWT Signature", you may
-    # need to delete cached BigQuery creds on your local machine. Try running
-    # bq init --delete_credentials as recommended in the output.
-    # TODO(RW-3768): Find a better solution for Google credentials in docker.
-
-    # Copy through an intermediate project and delete after (include TTL in case later steps fail).
-    # See https://docs.google.com/document/d/1EHw5nisXspJjA9yeZput3W4-vSIcuLBU5dPizTnk1i0/edit
-    common.run_inline %W{bq mk -f --default_table_expiration 7200 --dataset #{ingest_dataset}}
-    common.run_inline %W{./copy-bq-dataset.sh
-        #{source_dataset} #{ingest_dataset} #{source_cdr_project}
-        #{table_match_filter} #{table_skip_filter}}
-
-    common.run_inline %W{bq mk -f --dataset #{dest_dataset}}
-    common.run_inline %W{./copy-bq-dataset.sh
-        #{ingest_dataset} #{dest_dataset} #{tier.fetch(:ingest_cdr_project)}
-        #{table_match_filter} #{table_skip_filter}}
-
-    # Delete the intermediate dataset.
-    common.run_inline %W{bq rm -r -f --dataset #{ingest_dataset}}
-
-    auth_domain_group_email = tier.fetch(:auth_domain_group_email)
-
-    config_file = Tempfile.new("#{op.opts.bq_dataset}-config.json")
-    begin
-      json = JSON.parse(
-        common.capture_stdout %{bq show --format=prettyjson #{dest_dataset}})
-      existing_groups = Set[]
-      existing_users = Set[]
-      for entry in json["access"]
-        if entry.key?("groupByEmail")
-          existing_groups.add(entry["groupByEmail"])
-        end
-        if entry.key?("userByEmail")
-          existing_users.add(entry["userByEmail"])
-        end
+    bq_update_acl(dest_dataset) do |acl_json, existing_groups, existing_users|
+      auth_domain_group_email = tier.fetch(:auth_domain_group_email)
+      if existing_groups.include?(auth_domain_group_email)
+        common.status "#{auth_domain_group_email} already in ACL, skipping..."
+      else
+        common.status "Adding #{auth_domain_group_email} as a READER..."
+        acl_json["access"].push({
+          "groupByEmail" => auth_domain_group_email,
+          "role" => "READER"
+        })
       end
 
-      unless op.opts.exclude_auth_domain_acl
-        if existing_groups.include?(auth_domain_group_email)
-          common.status "#{auth_domain_group_email} already in ACL, skipping..."
-        else
-          common.status "Adding #{auth_domain_group_email} as a READER..."
-          new_entry = { "groupByEmail" => auth_domain_group_email, "role" => "READER"}
-          json["access"].push(new_entry)
-        end
+      app_sa = "#{op.opts.project}@appspot.gserviceaccount.com"
+      if existing_users.include?(app_sa)
+        common.status "#{app_sa} already in ACL, skipping..."
+      else
+        common.status "Adding #{app_sa} as a READER..."
+        acl_json["access"].push({ "userByEmail" => app_sa, "role" => "READER"})
       end
 
-      # if the app SA's in too many groups, it won't gain READER transitively.
-      # add it directly, to make sure.
-      # See discussion at https://pmi-engteam.slack.com/archives/CHRN2R51N/p1609869521078200?thread_ts=1609796171.063800&cid=CHRN2R51N
-      unless op.opts.exclude_sa_acl
-        if existing_users.include?(app_sa)
-          common.status "#{app_sa} already in ACL, skipping..."
-        else
-          common.status "Adding #{app_sa} as a READER..."
-          new_entry = { "userByEmail" => app_sa, "role" => "READER"}
-          json["access"].push(new_entry)
-        end
-      end
-
-      if op.opts.additional_reader_group
-        new_group = op.opts.additional_reader_group
-
-        if existing_groups.include?(new_group)
-          common.status "#{new_group} already in ACL, skipping..."
-        else
-          common.status "Adding #{new_group} as a READER..."
-          new_entry = { "groupByEmail" => new_group, "role" => "READER"}
-          json["access"].push(new_entry)
-        end
-      end
-
-      File.open(config_file.path, "w") do |f|
-        f.write(JSON.pretty_generate(json))
-      end
-      common.run_inline %{bq update --source #{config_file.path} #{dest_dataset}}
-    ensure
-      config_file.unlink
+      return acl_json
     end
   end
 end
@@ -305,6 +311,68 @@ Common.register_command({
   :invocation => "publish-cdr",
   :description => "Publishes a CDR dataset by copying it into a Firecloud CDR project and making it readable by registered users in the corresponding environment",
   :fn => ->(*args) { publish_cdr("publish-cdr", args) }
+})
+
+def publish_cdr_wgs(cmd_name, args)
+  ensure_docker cmd_name, args
+
+  op = WbOptionsParser.new(cmd_name, args)
+
+  op.add_option(
+    "--bq-dataset [dataset]",
+    ->(opts, v) { opts.bq_dataset = v},
+    "BigQuery dataset name for the CDR version (project not included), e.g. " +
+    "'2019Q4R3'. Required."
+  )
+  op.add_option(
+    "--project [project]",
+    ->(opts, v) { opts.project = v},
+    "The Google Cloud project associated with this workbench environment, " +
+    "e.g. all-of-us-rw-staging. Required."
+  )
+  op.opts.tier = "controlled"
+  op.add_option(
+     "--tier [tier]",
+     ->(opts, v) { opts.tier = v},
+     "The access tier associated with this CDR, e.g. controlled." +
+     "Default is controlled (WGS only exists in controlled tier, for the foreseeable future)."
+   )
+  op.add_validator ->(opts) { raise ArgumentError unless opts.bq_dataset and opts.project and opts.tier }
+  op.add_validator ->(opts) { raise ArgumentError.new("unsupported project: #{opts.project}") unless ENVIRONMENTS.key? opts.project }
+  op.add_validator ->(opts) { raise ArgumentError.new("unsupported tier: #{opts.tier}") unless ENVIRONMENTS[opts.project][:accessTiers].key? opts.tier }
+  op.parse.validate
+
+  common = Common.new
+  env = ENVIRONMENTS[op.opts.project]
+  app_sa = "#{op.opts.project}@appspot.gserviceaccount.com"
+  tier = env.fetch(:accessTiers)[op.opts.tier]
+  dest_dataset = "#{tier.fetch(:dest_cdr_project)}:#{op.opts.bq_dataset}"
+
+  source_project = tier.fetch(:source_cdr_wgs_project)
+  unless source_project
+    raise ArgumentError.new("missing WGS source project value for env #{op.opts.project}")
+  end
+  extraction_proxy_group = must_get_wgs_proxy_group(op.opts.project)
+
+  service_account_context_for_bq(op.opts.project, env.fetch(:publisher_account)) do
+    bq_ingest(tier, source_project, op.opts.bq_dataset, WGS_TABLE_FILTER)
+
+    bq_update_acl(dest_dataset) do |acl_json, existing_groups, existing_users|
+      if existing_groups.include?(extraction_proxy_group)
+        common.status "#{extraction_proxy_group} already in ACL, skipping..."
+      else
+        common.status "Adding #{extraction_proxy_group} as a READER..."
+        acl_json["access"].push({"groupByEmail" => extraction_proxy_group, "role" => "READER"})
+      end
+      return acl_json
+    end
+  end
+end
+
+Common.register_command({
+  :invocation => "publish-cdr-wgs",
+  :description => "Publishes a WGS CDR dataset by copying it into a Firecloud CDR project and making it readable by AoU service accounts",
+  :fn => ->(*args) { publish_cdr_wgs("publish-cdr-wgs", args) }
 })
 
 def create_wgs_extraction_dataset(cmd_name, args)
@@ -342,37 +410,22 @@ def create_wgs_extraction_dataset(cmd_name, args)
 
   common = Common.new
   env = ENVIRONMENTS[op.opts.project]
-  account = env.fetch(:publisher_account)
   tier = env.fetch(:accessTiers)[op.opts.tier]
+  proxy_group = must_get_wgs_proxy_group(op.opts.project)
 
-  # TODO(RW-3208): Investigate using a temporary / impersonated SA credential instead of a key.
-  key_file = Tempfile.new(["#{account}-key", ".json"], "/tmp")
-  ServiceAccountContext.new(
-    op.opts.project, account, key_file.path).run do
-    # TODO(RW-3768): This currently leaves the user session with an activated service
-    # account user. Ideally the activation would be hermetic within the docker
-    # session, or else we would revert the active account after running.
-    common.run_inline %W{gcloud auth activate-service-account -q --key-file #{key_file.path}}
-
+  service_account_context_for_bq(op.opts.project, env.fetch(:publisher_account)) do
     dest_dataset = "#{tier.fetch(:dest_cdr_project)}:#{op.opts.bq_dataset}"
     common.run_inline %W{bq mk --default_table_expiration #{op.opts.ttl}  --dataset #{dest_dataset}}
 
-    config_file = Tempfile.new("#{op.opts.bq_dataset}-config.json")
-    begin
-      json = JSON.parse(
-        common.capture_stdout %{bq show --format=prettyjson #{dest_dataset}})
-
-      proxy_group = get_config(op.opts.project)["wgsCohortExtraction"]["serviceAccountTerraProxyGroup"]
-      common.status "Adding #{proxy_group} as a WRITER..."
-      new_entry = { "groupByEmail" => proxy_group, "role" => "WRITER"}
-      json["access"].push(new_entry)
-
-      File.open(config_file.path, "w") do |f|
-        f.write(JSON.pretty_generate(json))
+    bq_update_acl(dest_dataset) do |acl_json, existing_groups, existing_users|
+      if existing_groups.includes?(proxy_group)
+        common.status "#{proxy_group} already in ACL, skipping..."
+      else
+        common.status "Adding #{proxy_group} as a WRITER..."
+        acl_json["access"].push({"groupByEmail" => proxy_group, "role" => "WRITER"})
       end
-      common.run_inline %{bq update --source #{config_file.path} #{dest_dataset}}
-    ensure
-      config_file.unlink
+
+      return acl_json
     end
   end
 end
