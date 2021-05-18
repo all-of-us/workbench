3e8b5a587537b0e9076404cb32275f6a84331725
Ariel Schwartz <als364@cornell.edu>
2021-05-18 10:49:53 -0400

    [RW-6642][risk=no] ABORTED status for extraction (#4989)
    * add ABORTED TerraJobStatus
    
    * lint, spotless
    
    * handle ABORTING status
    
    * spotless
    
    * review feedback
    
    * lint
    
    * remove unnecessary save
    
    Co-authored-by: Ariel Schwartz <aschwart@broadinstitute.org>

--- api/src/main/java/org/pmiops/workbench/genomics/GenomicExtractionMapper.java
+++ api/src/main/java/org/pmiops/workbench/genomics/GenomicExtractionMapper.java
@@ -29,10 +29,12 @@ public interface GenomicExtractionMapper {
   default TerraJobStatus convertWorkflowStatus(FirecloudWorkflowStatus status) {
     if (status == FirecloudWorkflowStatus.SUCCEEDED) {
       return TerraJobStatus.SUCCEEDED;
-    } else if (status == FirecloudWorkflowStatus.ABORTED
-        || status == FirecloudWorkflowStatus.ABORTING
-        || status == FirecloudWorkflowStatus.FAILED) {
+    } else if (status == FirecloudWorkflowStatus.FAILED) {
       return TerraJobStatus.FAILED;
+    } else if (status == FirecloudWorkflowStatus.ABORTED) {
+      return TerraJobStatus.ABORTED;
+    } else if (status == FirecloudWorkflowStatus.ABORTING) {
+      return TerraJobStatus.ABORTING;
     } else {
       // Launching, Queued, Running, Submitted
       return TerraJobStatus.RUNNING;
