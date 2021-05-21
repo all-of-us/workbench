1cc5cffd24cb88b40bf2f8ef7a7e99c3d49b1ca7
aweng98 <bweng@broadinstitute.org>
2021-05-21 17:01:41 -0400

    add retry in share modal

--- e2e/app/modal/share-modal.ts
+++ e2e/app/modal/share-modal.ts
@@ -21,30 +21,57 @@ export default class ShareModal extends Modal {
   }
 
   async shareWithUser(userName: string, level: WorkspaceAccessLevel): Promise<void> {
-    const dropDownXpath = `${this.getXpath()}//*[@data-test-id="drop-down"]`;
+    const dropDownXpath = this.getXpath() + '//*[@data-test-id="drop-down"][.//clr-icon[@shape="plus-circle"]]';
     const waitForDropDown = async () => {
       await waitWhileLoading(this.page);
       await this.page.waitForXPath(dropDownXpath, { visible: true });
     };
+
     const waitForClose = async () => {
       await waitWhileLoading(this.page);
       await this.page.waitForXPath(dropDownXpath, { hidden: true });
     };
 
-    const searchBox = this.waitForSearchBox();
-    await searchBox.type(`${userName}`, { delay: 20 });
+    const findCollaboratorAddIcon = () => {
+      return ClrIconLink.findByName(
+        this.page,
+        { type: ElementType.Icon, iconShape: 'plus-circle', containsText: userName, ancestorLevel: 2 },
+        this
+      );
+    };
 
-    await waitForDropDown(); // Needed for the dropdown
-    const addCollab = this.waitForAddCollaboratorIcon(userName);
-    await addCollab.click();
-    await waitForClose();
+    const pickUserRole = async () => {
+      const roleInput = await this.waitForRoleSelectorForUser(userName);
+      await roleInput.click();
 
-    const roleInput = await this.waitForRoleSelectorForUser(userName);
-    await roleInput.click();
+      const ownerOpt = await this.waitForRoleOption(level);
+      await ownerOpt.click();
+    };
 
-    const ownerOpt = await this.waitForRoleOption(level);
-    await ownerOpt.click();
+    let maxAttempts = 2;
+    // Try 2 times: enter email and find add icon.
+    const typeAndAddUser = async () => {
+      await this.waitForSearchBox().type(userName, { delay: 50 });
+      await waitForDropDown();
+      try {
+        const addIcon = findCollaboratorAddIcon();
+        await addIcon.click();
+        await waitForClose();
+        return;
+      } catch (err) {
+        // Click failed.
+      }
+      if (maxAttempts <= 0) {
+        return;
+      }
+      maxAttempts--;
+      await this.page.waitForTimeout(1000).then(() => {
+        return typeAndAddUser();
+      });
+    };
 
+    await typeAndAddUser();
+    await pickUserRole();
     await this.clickButton(LinkText.Save, { waitForClose: true });
     logger.info(`Shared workspace to ${userName} with role ${level}`);
   }
@@ -79,14 +106,6 @@ export default class ShareModal extends Modal {
     return Textbox.findByName(this.page, { name: 'Find Collaborators' }, this);
   }
 
-  waitForAddCollaboratorIcon(email: string): ClrIconLink {
-    return ClrIconLink.findByName(
-      this.page,
-      { type: ElementType.Icon, iconShape: 'plus-circle', containsText: email, ancestorLevel: 2 },
-      this
-    );
-  }
-
   async waitForRoleSelectorForUser(username: string): Promise<Textbox> {
     const box = new Textbox(this.page, `${this.collabRowXPath(username)}//input[@type="text"]`);
     await box.waitForXPath({ visible: true });
@@ -97,5 +116,20 @@ export default class ShareModal extends Modal {
     // The label in the select menu uses title case.
     const levelText = level[0].toUpperCase() + level.substring(1).toLowerCase();
     return this.page.waitForXPath(`//*[starts-with(@id,"react-select") and text()="${levelText}"]`, { visible: true });
+  }
+
+  async notFindUser(email: string): Promise<boolean> {
+    const notFoundXpath =
+      this.getXpath() + '//*[@data-test-id="drop-down"][not(.//*[text()="No results based on your search"])]';
+    await this.waitForSearchBox().type(email, { delay: 20 });
+    await waitWhileLoading(this.page);
+    return await this.page
+      .waitForXPath(notFoundXpath, { visible: true })
+      .then(() => {
+        return true;
+      })
+      .catch(() => {
+        return false;
+      });
   }
 }
