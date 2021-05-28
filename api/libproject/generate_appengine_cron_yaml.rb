7ef059b5092729a295d147d8fc6bd9402b09ea05
CH Albach <calbach@google.com>
2021-05-27 17:23:48 -0700

    [RW-6280][risk=low] Temporarily disable free tier cron in test/perf

--- api/libproject/generate_appengine_cron_yaml.rb
+++ api/libproject/generate_appengine_cron_yaml.rb
@@ -1,11 +1,13 @@
 # Generates the App Engine cron.yaml file for a given environment. This is meant
 # to be invoked directly via Gradle before running the GAE application.
-# For now, this picks cron_{env}.yaml if it exists, cron_default.yaml otherwise.
 #
-# Future work: it will likely be desirable to support extension rather than
-# wholesale file replacement, e.g. to alter just the schedule or exclude a
-# particular cron for a given environment.
+# - Extends cron_base.yaml with cron_{env}.yaml, if any
+# - "url" property is treated as the primary key for the cron
+# - Environment-specific yaml files may define the following special properties:
+#   - aou_skip_reason: omit this job from the cron yaml, for the given reason
 require 'fileutils'
+require 'yaml'
+require_relative "../../aou-utils/utils/common"
 
 aou_env = ARGV[0]
 
@@ -15,9 +17,32 @@ def to_cron_path(suffix)
   return "#{CRON_DIR}/cron_#{suffix}.yaml"
 end
 
-from_path = to_cron_path("default")
-if File.file?(to_cron_path(aou_env))
-  from_path = to_cron_path(aou_env)
+def merge_cron_yaml(base_yaml_file, env_yaml_file, output_yaml_file)
+  common = Common.new
+
+  out_yaml = YAML.load_file(base_yaml_file)
+  base_crons = out_yaml['cron'].map { |c| [c['url'], c] }.to_h
+
+  if File.file?(env_yaml_file)
+    env_yaml = YAML.load_file(env_yaml_file)
+    env_yaml['cron'].each do |env_cron|
+
+      raise ArgumentError.new("missing required 'url' key for #{env_cron}") unless env_cron.key?('url')
+      url = env_cron['url']
+      raise ArgumentError.new("cron URL '#{url}' does not exist in base yaml") unless base_crons.key?(url)
+
+      if env_cron.key?('aou_skip_reason')
+        common.status "Excluding cron '#{url}', reason: '#{env_cron['aou_skip_reason']}'"
+        out_yaml['cron'].reject! {|o| o['url'] == url}
+        next
+      end
+
+      # This indirectly mutates out_yaml
+      base_crons[url].merge!(env_cron)
+    end
+  end
+
+  File.open(output_yaml_file, 'w') { |f| YAML.dump(out_yaml, f) }
 end
 
-FileUtils.cp(from_path, "#{CRON_DIR}/cron.yaml")
+merge_cron_yaml(to_cron_path("base"), to_cron_path(aou_env), "#{CRON_DIR}/cron.yaml")
