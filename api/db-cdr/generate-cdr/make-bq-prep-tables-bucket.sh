b1111e36fb18c61651857a767806bd0d430b6468
Brian Freeman <brian.d.freeman@gmail.com>
2021-06-15 09:54:36 -0500

    RW-6828 adding script

--- api/db-cdr/generate-cdr/make-bq-prep-tables-bucket.sh
+++ api/db-cdr/generate-cdr/make-bq-prep-tables-bucket.sh
@@ -1,11 +1,23 @@
 #!/bin/bash
 
-# This validates that prep tables, cohort builder menu, data dictionary, cope survey versions and whole genome variant tables exist.
+# Create a new CDR bucket and copy over reusable csv files from previous CDR bucket.
 set -e
 
-export NEW_CDR=$1       # New CDR version
-export PREVIOUS_CDR=$2  # Previous CDR version
+export NEW_CDR_VERSION=$1       # New CDR version
+export PREVIOUS_CDR_VERSION=$2  # Previous CDR version
 
 BUCKET="all-of-us-workbench-private-cloudsql"
+FOLDER="cdr_csv_files"
+CSV_FILES=("cb_criteria_menu.csv"
+"cb_survey_version.csv"
+"ds_data_dictionary.csv"
+"prep_cdr_date.csv"
+"prep_clinical_terms_nc.csv"
+"prep_concept.csv"
+"prep_concept_relationship.csv"
+"prep_criteria.csv"
+"prep_physical_measurement.csv")
 
-gsutil cp gs://$BUCKET/$PREVIOUS_CDR/cdr_csv_files gs://$BUCKET/$NEW_CDR/cdr_csv_files
+for file in ${CSV_FILES[@]}; do
+  gsutil cp gs://$BUCKET/$PREVIOUS_CDR_VERSION/$FOLDER/$file gs://$BUCKET/$NEW_CDR_VERSION/$FOLDER/
+done
