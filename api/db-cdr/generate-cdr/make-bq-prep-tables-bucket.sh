682319d73a757800b95a768ebb6c36ecab148a19
Brian Freeman <brian.d.freeman@gmail.com>
2021-06-17 07:15:49 -0500

    [RW-6828, RW-6829][risk=no] created project.rb command to create a new bucket and copy csv files (#5122)
    * RW-6628 adding new file
    
    * RW-6828 adding script
    
    * RW-6828 fixing codacy problem

---
+++ api/db-cdr/generate-cdr/make-bq-prep-tables-bucket.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Create a new CDR bucket and copy over reusable csv files from previous CDR bucket.
+set -e
+
+export NEW_CDR_VERSION=$1       # New CDR version
+export PREVIOUS_CDR_VERSION=$2  # Previous CDR version
+
+BUCKET="all-of-us-workbench-private-cloudsql"
+FOLDER="cdr_csv_files"
+CSV_FILES=("cb_criteria_menu.csv"
+"cb_survey_version.csv"
+"ds_data_dictionary.csv"
+"prep_cdr_date.csv"
+"prep_clinical_terms_nc.csv"
+"prep_concept.csv"
+"prep_concept_relationship.csv"
+"prep_criteria.csv"
+"prep_physical_measurement.csv")
+
+for file in ${CSV_FILES[@]}; do
+  gsutil cp gs://"$BUCKET"/"$PREVIOUS_CDR_VERSION"/"$FOLDER"/"$file" gs://"$BUCKET"/"$NEW_CDR_VERSION"/"$FOLDER"/
+done
