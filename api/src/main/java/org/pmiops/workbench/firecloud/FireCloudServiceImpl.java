b1f53c61afa1e74519bab0bc1043f67eb4c45adc
Eric Song <me@ericsong.io>
2021-06-17 14:41:34 -0400

    Revert "Merge branch 'songe/update-extract-wdl' into songe/RW-6900"
    This reverts commit d3638bee9d7e4b424d3336a14033f4a23b037831, reversing
    changes made to a33b16aa861e8c488d10da7c2a423f8b3cf632a1.

--- api/src/main/java/org/pmiops/workbench/firecloud/FireCloudServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/firecloud/FireCloudServiceImpl.java
@@ -347,10 +347,7 @@ public class FireCloudServiceImpl implements FireCloudService {
             .authorizationDomain(
                 ImmutableList.of(new FirecloudManagedGroupRef().membersGroupName(authDomainName)));
 
-    return retryHandler.run((context) -> {
-      throw new ApiException("hi");
-//      return workspacesApi.createWorkspace(workspaceIngest);
-    });
+    return retryHandler.run((context) -> workspacesApi.createWorkspace(workspaceIngest));
   }
 
   @Override
--- api/src/main/java/org/pmiops/workbench/genomics/GenomicExtractionService.java
+++ api/src/main/java/org/pmiops/workbench/genomics/GenomicExtractionService.java
@@ -213,6 +213,9 @@ public class GenomicExtractionService {
                                 EXTRACT_WORKFLOW_NAME + ".gvs_dataset",
                                 "\"" + workspace.getCdrVersion().getWgsBigqueryDataset() + "\"")
                             .put(
+                                EXTRACT_WORKFLOW_NAME + ".fq_gvs_extraction_cohorts_dataset",
+                                "\"" + cohortExtractionConfig.extractionCohortsDataset + "\"")
+                            .put(
                                 EXTRACT_WORKFLOW_NAME + ".fq_gvs_extraction_destination_dataset",
                                 "\"" + cohortExtractionConfig.extractionDestinationDataset + "\"")
                             .put(
@@ -239,7 +242,7 @@ public class GenomicExtractionService {
                             .put(EXTRACT_WORKFLOW_NAME + ".output_gcs_dir", "\"" + outputDir + "\"")
                             .put(
                                 EXTRACT_WORKFLOW_NAME + ".gatk_override",
-                                "\"gs://all-of-us-workbench-test-genomics/wgs/gatk-package-4.2.0.0-344-g7325330-SNAPSHOT-local.jar\"")
+                                "\"gs://all-of-us-workbench-test-genomics/wgs/gatk-package-4.2.0.0-326-g84ce13a-SNAPSHOT-local.jar\"")
                             .build())
                     .methodConfigVersion(
                         cohortExtractionConfig.extractionMethodConfigurationVersion)
@@ -251,22 +254,18 @@ public class GenomicExtractionService {
                 cohortExtractionConfig.operationalTerraWorkspaceName)
             .getMethodConfiguration();
 
-    FirecloudSubmissionResponse submissionResponse = null;
-    try {
-      submissionResponse =
-          submissionApiProvider
-              .get()
-              .createSubmission(
-                  new FirecloudSubmissionRequest()
-                      .deleteIntermediateOutputFiles(false)
-                      .methodConfigurationNamespace(methodConfig.getNamespace())
-                      .methodConfigurationName(methodConfig.getName())
-                      .useCallCache(false),
-                  cohortExtractionConfig.operationalTerraWorkspaceNamespace,
-                  cohortExtractionConfig.operationalTerraWorkspaceName);
-    } catch (ApiException e) {
-      System.out.println(e.getResponseBody());
-    }
+    FirecloudSubmissionResponse submissionResponse =
+        submissionApiProvider
+            .get()
+            .createSubmission(
+                new FirecloudSubmissionRequest()
+                    .deleteIntermediateOutputFiles(false)
+                    .methodConfigurationNamespace(methodConfig.getNamespace())
+                    .methodConfigurationName(methodConfig.getName())
+                    .useCallCache(false),
+                cohortExtractionConfig.operationalTerraWorkspaceNamespace,
+                cohortExtractionConfig.operationalTerraWorkspaceName);
+
     // Note: if this save fails we may have an orphaned job. Will likely need a cleanup task to
     // check for such jobs.
     DbWgsExtractCromwellSubmission dbSubmission = new DbWgsExtractCromwellSubmission();
