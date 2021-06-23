df19ce1c43220f4865215763526bd1118ea965dd
chavanr2019 <rohini.chavan@vumc.org>
2021-06-23 13:43:51 -0500

    created a page for the admin-user-profile-info

---
+++ e2e/app/page/admin-user-profile-info.ts
@@ -0,0 +1,16 @@
+import { Page } from 'puppeteer';
+import AuthenticatedPage from 'app/page/authenticated-page';
+import { waitForDocumentTitle, waitWhileLoading } from 'utils/waits-utils';
+
+const PageTitle = 'User Admin | All of Us Researcher Workbench';
+
+export default class UserProfileInfo extends AuthenticatedPage {
+  constructor(page: Page) {
+    super(page);
+  }
+
+  async isLoaded(): Promise<boolean> {
+    await Promise.all([waitForDocumentTitle(this.page, PageTitle), waitWhileLoading(this.page)]);
+    return true;
+  }
+}
