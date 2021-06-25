1a80361447257e291d115fd15d221cd5494e7979
Eric Song <me@ericsong.io>
2021-06-25 14:55:59 -0400

    [ticket=no][risk=no] Fix bug where workspace share from workspace card was missing an access level (#5167)
    * load profile solely in workspace share card and fix bug where access level wasn't being provided
    
    * fix and refactor tests
    
    * remove comment

--- ui/src/app/pages/workspace/workspace-share.tsx
+++ ui/src/app/pages/workspace/workspace-share.tsx
@@ -5,7 +5,6 @@ import colors, {colorWithWhiteness} from 'app/styles/colors';
 import {
   isBlank,
   reactStyles,
-  withCurrentWorkspace,
   withUserProfile
 } from 'app/utils';
 import {currentWorkspaceStore} from 'app/utils/navigation';
@@ -161,14 +160,14 @@ export interface State {
 
 export interface Props {
   onClose: Function;
+  workspace: WorkspaceData;
 }
 
 interface HocProps extends Props {
-  workspace: WorkspaceData;
   profileState: {profile: Profile, reload: Function, updateCache: Function};
 }
 
-export const WorkspaceShare = fp.flow(withCurrentWorkspace(), withUserProfile())(class extends React.Component<HocProps, State> {
+export const WorkspaceShare = fp.flow(withUserProfile())(class extends React.Component<HocProps, State> {
   searchTermChangedEvent: Function;
   searchingNode: HTMLElement;
 
