f8eeefee1717769d39edc94ab3d6d365dfde943f
yonghao <yyhao1@gmail.com>
2021-06-24 21:29:04 -0400

    add domain

--- .circleci/export-e2e-test-user-vars.sh
+++ .circleci/export-e2e-test-user-vars.sh
@@ -7,27 +7,27 @@ for arg in "$@"
 do
   case "$arg" in
     "perf")
-       echo "export READER_USER=$PUPPETEER_READER_PERF" >> $BASH_ENV
-       echo "export WRITER_USER=$PUPPETEER_WRITER_PERF" >> $BASH_ENV
-       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_PERF" >> $BASH_ENV
+       echo "export READER_USER=$PUPPETEER_READER_PERF@perf.fake-research-aou.org" >> $BASH_ENV
+       echo "export WRITER_USER=$PUPPETEER_WRITER_PERF@perf.fake-research-aou.org" >> $BASH_ENV
+       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_PERF@perf.fake-research-aou.org" >> $BASH_ENV
        echo "export USER_NAME=$PUPPETEER_USER_PERF@perf.fake-research-aou.org" >> $BASH_ENV
        ;;
     "staging")
-       echo "export READER_USER=$PUPPETEER_READER_STAGING" >> $BASH_ENV
-       echo "export WRITER_USER=$PUPPETEER_WRITER_STAGING" >> $BASH_ENV
-       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_STAGING" >> $BASH_ENV
+       echo "export READER_USER=$PUPPETEER_READER_STAGING@staging.fake-research-aou.org" >> $BASH_ENV
+       echo "export WRITER_USER=$PUPPETEER_WRITER_STAGING@staging.fake-research-aou.org" >> $BASH_ENV
+       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_STAGING@staging.fake-research-aou.org" >> $BASH_ENV
        echo "export USER_NAME=$PUPPETEER_USER_STAGING@staging.fake-research-aou.org" >> $BASH_ENV
        ;;
     "test")
-       echo "export READER_USER=$PUPPETEER_READER_TEST" >> $BASH_ENV
-       echo "export WRITER_USER=$PUPPETEER_WRITER_TEST" >> $BASH_ENV
-       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_TEST" >> $BASH_ENV
+       echo "export READER_USER=$PUPPETEER_READER_TEST@fake-research-aou.org" >> $BASH_ENV
+       echo "export WRITER_USER=$PUPPETEER_WRITER_TEST@fake-research-aou.org" >> $BASH_ENV
+       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_TEST@fake-research-aou.org" >> $BASH_ENV
        echo "export USER_NAME=$PUPPETEER_USER_TEST@fake-research-aou.org" >> $BASH_ENV
        ;;
     "local")
-       echo "export READER_USER=$PUPPETEER_READER_LOCAL" >> $BASH_ENV
-       echo "export WRITER_USER=$PUPPETEER_WRITER_LOCAL" >> $BASH_ENV
-       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_LOCAL" >> $BASH_ENV
+       echo "export READER_USER=$PUPPETEER_READER_LOCAL@fake-research-aou.org" >> $BASH_ENV
+       echo "export WRITER_USER=$PUPPETEER_WRITER_LOCAL@fake-research-aou.org" >> $BASH_ENV
+       echo "export COLLABORATOR_USER=$PUPPETEER_COLLABORATOR_LOCAL@fake-research-aou.org" >> $BASH_ENV
        echo "export USER_NAME=$PUPPETEER_USER_LOCAL@fake-research-aou.org" >> $BASH_ENV
        ;;
    esac
