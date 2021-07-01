2f7a5967f0d72c53063bc1a2dbb391e02c77896f
Ariel Schwartz <aschwart@broadinstitute.org>
2021-07-01 15:02:25 -0400

    user disabled

--- ui/src/app/pages/user-disabled.tsx
+++ ui/src/app/pages/user-disabled.tsx
@@ -4,17 +4,21 @@ import {StyledAnchorTag} from 'app/components/buttons';
 import {BoldHeader} from 'app/components/headers';
 import {PublicLayout} from 'app/components/public-layout';
 import colors from 'app/styles/colors';
+import {WithSpinnerOverlayProps} from "../components/with-spinner-overlay";
+import {useEffect} from "react";
 
 const supportUrl = 'support@researchallofus.org';
 
-export class UserDisabled extends React.Component {
-  render() {
-    return <PublicLayout>
-      <BoldHeader>Your account has been disabled</BoldHeader>
-      <section style={{color: colors.primary, fontSize: '18px', marginTop: '.5rem'}}>
-        Contact <StyledAnchorTag href={'mailto:' + supportUrl}>{supportUrl}</StyledAnchorTag> for
-        more information.
-      </section>
-    </PublicLayout>;
-  }
+export const UserDisabled = (spinnerProps: WithSpinnerOverlayProps) => {
+  useEffect(() => {
+    spinnerProps.hideSpinner();
+  }, [spinnerProps.spinnerVisible]);
+
+  return <PublicLayout>
+    <BoldHeader>Your account has been disabled</BoldHeader>
+    <section style={{color: colors.primary, fontSize: '18px', marginTop: '.5rem'}}>
+      Contact <StyledAnchorTag href={'mailto:' + supportUrl}>{supportUrl}</StyledAnchorTag> for
+      more information.
+    </section>
+  </PublicLayout>;
 }
