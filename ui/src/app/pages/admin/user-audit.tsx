05adf82bcd3ff8efd349f615b9bafdfbfbaa238d
Ariel Schwartz <aschwart@broadinstitute.org>
2021-07-01 15:00:43 -0400

    user audit

--- ui/src/app/pages/admin/user-audit.tsx
+++ ui/src/app/pages/admin/user-audit.tsx
@@ -2,6 +2,8 @@ import {AuditPageComponent} from 'app/components/admin/audit-page-component';
 import {profileApi} from 'app/services/swagger-fetch-clients';
 import * as React from 'react';
 import {useParams} from 'react-router-dom';
+import {useEffect} from "react";
+import {WithSpinnerOverlayProps} from "app/components/with-spinner-overlay";
 
 const getAuditLog = (subject: string) => {
   const bqRowLimit = 1000; // Workspaces take many rows because of the Research Purpose fields
@@ -27,7 +29,11 @@ const getAdminPageUrl = (subject: string) => {
   return [`/admin/users/${subject}`];
 };
 
-export const UserAudit = () => {
+export const UserAudit = (spinnerProps: WithSpinnerOverlayProps) => {
+  useEffect(() => {
+    spinnerProps.hideSpinner();
+  }, [spinnerProps.spinnerVisible]);
+
   const {username = ''} = useParams<{username: string}>();
   return <AuditPageComponent auditSubjectType='User'
                              buttonLabel='Username without domain'
