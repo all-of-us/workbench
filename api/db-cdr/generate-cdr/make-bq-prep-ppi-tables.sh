31b71975c75888dd9eb7169285674711d371cc2a
Brian Freeman <brian.d.freeman@gmail.com>
2021-07-06 14:13:38 -0500

    RW-6830 adding double quotes

--- api/db-cdr/generate-cdr/make-bq-prep-ppi-tables.sh
+++ api/db-cdr/generate-cdr/make-bq-prep-ppi-tables.sh
@@ -9,21 +9,20 @@ export DATE=$3              # Redcap survey file date
 export TIER=$4              # Ex: registered or controlled
 
 BUCKET="all-of-us-workbench-private-cloudsql"
-TEMP_FILE_DIR="csv"
 CSV_HOME_DIR="cb_prep_tables/redcap/$DATE"
 
 echo "Starting load of prep ppi tables into $BQ_PROJECT:$BQ_DATASET"
 
-TABLES=$(gsutil ls gs://$BUCKET/$CSV_HOME_DIR/*_"$TIER".csv | cut -d'/' -f7 | cut -d'.' -f1 | awk -F"_$TIER" '{print $1}')
+TABLES=$(gsutil ls gs://"$BUCKET"/"$CSV_HOME_DIR"/*_"$TIER".csv | cut -d'/' -f7 | cut -d'.' -f1 | awk -F"_$TIER" '{print $1}')
 
 schema_path=generate-cdr/bq-schemas
 
 for tableName in $TABLES
 do
   echo "Processing $tableName table"
-  bq --project_id=$BQ_PROJECT rm -f $BQ_DATASET.$tableName
-  bq load --skip_leading_rows=1 --project_id=$BQ_PROJECT --source_format=CSV $BQ_DATASET.$tableName \
-  gs://$BUCKET/$CSV_HOME_DIR/"${tableName}_$TIER.csv" $schema_path/$tableName.json
+  bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.$tableName"
+  bq load --skip_leading_rows=1 --project_id="$BQ_PROJECT" --source_format=CSV "$BQ_DATASET.$tableName" \
+  gs://"$BUCKET"/"$CSV_HOME_DIR"/"${tableName}_$TIER.csv" "$schema_path/$tableName.json"
 done
 
 echo "Finished loading prep ppi tables into $BQ_PROJECT:$BQ_DATASET"
