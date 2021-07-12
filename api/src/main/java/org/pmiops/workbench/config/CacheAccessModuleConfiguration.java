04949941068b8638c5d7ca9038587df7e14b79df
yonghao <yyhao1@gmail.com>
2021-07-12 11:08:22 -0400

    supplier

--- api/src/main/java/org/pmiops/workbench/config/CacheAccessModuleConfiguration.java
+++ api/src/main/java/org/pmiops/workbench/config/CacheAccessModuleConfiguration.java
@@ -1,6 +1,6 @@
 package org.pmiops.workbench.config;
 
-import com.google.common.base.Supplier;
+import java.util.function.Supplier;
 import com.google.common.base.Suppliers;
 import java.util.List;
 import java.util.concurrent.TimeUnit;
