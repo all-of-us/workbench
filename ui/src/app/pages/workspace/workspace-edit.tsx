e337b65b2af8e7ea82d83a0aaabf96528ca9b832
Joel Thibault <thibault@broad.dev>
2021-07-14 14:43:36 -0400

    update workspace-edit text

--- ui/src/app/pages/workspace/workspace-edit.tsx
+++ ui/src/app/pages/workspace/workspace-edit.tsx
@@ -1373,8 +1373,7 @@ export const WorkspaceEdit = fp.flow(withCurrentWorkspace(), withCdrVersions(), 
             may not be possible to completely prevent misuse of research for stigmatizing purposes,
             data users can take important steps to minimize the risk of this happening–
             <TooltipTrigger content={toolTipTextDucc}>
-              <Link style={{display: 'inline-block'}}>taking this step is a condition of your
-                Data Use Agreement.</Link>
+              <Link style={{display: 'inline-block'}}>taking this step is a condition of your Data User Code of Conduct agreement.</Link>
             </TooltipTrigger>
             &nbsp;If you are concerned that your research could inadvertently stigmatize
             participants or communities, or if you are unsure, let us know. We encourage you to
