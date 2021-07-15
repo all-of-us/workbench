3bdc6ec818363adbac835ecf2e918d0b18d65add
Eric Song <me@ericsong.io>
2021-07-15 16:09:37 -0400

    WIP

--- api/src/main/java/org/pmiops/workbench/genomics/GenomicExtractionService.java
+++ api/src/main/java/org/pmiops/workbench/genomics/GenomicExtractionService.java
@@ -278,7 +278,7 @@ public class GenomicExtractionService {
                             .put(EXTRACT_WORKFLOW_NAME + ".output_gcs_dir", "\"" + outputDir + "\"")
                             .put(
                                 EXTRACT_WORKFLOW_NAME + ".gatk_override",
-                                "\"gs://all-of-us-workbench-test-genomics/wgs/gatk-package-4.2.0.0-344-g7325330-SNAPSHOT-local.jar\"")
+                                "\"gs://all-of-us-workbench-test-genomics/wgs/gatk-package-4.2.0.0-359-gb3f0558-SNAPSHOT-local.jar\"")
                             .build())
                     .methodConfigVersion(
                         cohortExtractionConfig.extractionMethodConfigurationVersion)
