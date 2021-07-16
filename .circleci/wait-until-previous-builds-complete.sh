a0442f43c5cc9b8c8a6109fcb46c3f2e27709300
aweng98 <bweng@broadinstitute.org>
2021-07-16 11:28:02 -0400

    format script

--- .circleci/wait-until-previous-builds-complete.sh
+++ .circleci/wait-until-previous-builds-complete.sh
@@ -9,7 +9,7 @@ set -o functrace
 # *******************
 api_root="https://circleci.com/api/v2/"
 # polling branch name. Other branches (PRs) are excluded from consideration.
-branch="master";
+branch="master"
 # Polling workflow name. Other workflow names (nightly-tests and deploy-staging) are excluded from consideration.
 workflow_name="build-test-deploy"
 project_slug="gh/all-of-us"
@@ -27,23 +27,23 @@ check_circleci_token() {
   fi
 }
 
-post () {
+post() {
   local url="${api_root}${1}"
-  printf "curl POST ${url}\n" > /dev/tty
+  printf "curl POST ${url}\n" >/dev/tty
   curl -X POST \
-       -H "Content-Type: application/json" \
-       -H "Circle-Token: ${CIRCLECI_TOKEN}" \
-       -d "$2" \
-       "${url}"
+    -H "Content-Type: application/json" \
+    -H "Circle-Token: ${CIRCLECI_TOKEN}" \
+    -d "$2" \
+    "${url}"
 }
 
-get () {
+get() {
   local url="${api_root}${1}"
-  printf "curl GET ${url}\n" > /dev/tty
+  printf "curl GET ${url}\n" >/dev/tty
   curl -X GET -sS \
-      -H "Content-Type: application/json" \
-      -H "Circle-Token: ${CIRCLECI_TOKEN}" \
-      "${url}"
+    -H "Content-Type: application/json" \
+    -H "Circle-Token: ${CIRCLECI_TOKEN}" \
+    "${url}"
 }
 
 # Get list of recently built pipelines. Save results to json file.
@@ -51,8 +51,7 @@ pipeline_json="/tmp/master_branch_pipelines.json"
 fetch_pipeline_ids() {
   local get_path="pipeline?org-slug=${project_slug}"
   local get_result=$(get "${get_path}")
-  # Debug echo ${get_result} | jq .
-  echo ${get_result} | jq '[.items[] | select(.vcs.branch=="master")][] | {created_at: .created_at, id: .id, number: .number}' > ${pipeline_json}
+  echo ${get_result} | jq '[.items[] | select(.vcs.branch=="master")][] | {created_at: .created_at, id: .id, number: .number}' >${pipeline_json}
   printf "Found following pipelines on ${branch} branch:\n"
   cat ${pipeline_json}
   printf "\n"
@@ -63,7 +62,6 @@ fetch_pipeline_number() {
   local id=$(echo $1 | xargs echo)
   local get_path="pipeline/${id}"
   local get_result=$(get "${get_path}")
-  # Debug echo ${get_result} | jq .
   __=$(echo "${get_result}" | jq -r .number)
 }
 
@@ -74,33 +72,45 @@ fetch_workflow_status() {
   local id=$(echo $1 | xargs echo)
   local get_path="pipeline/${id}/workflow"
   local get_result=$(get ${get_path})
-  # Debug echo $get_result | jq .
   local workflow_summary=$(echo ${get_result} | jq '.items[] | {name: .name, id: .id, status: .status, pipeline_number: .pipeline_number}')
   printf "${workflow_summary}\n"
   # workflow branch name is bound by $workflow_name variable.
-  # Rerunning a failed workflow produces an array. Get the status in the first array element.
-  __=$(echo ${get_result} | jq -r 'first(.items[]) | select(.name=='\"$workflow_name\"') | .status | @sh' )
+  # Rerunning a failed workflow produces a nested datetime sorted array. Get the status of latest run (first array element).
+  __=$(echo ${get_result} | jq -r 'first(.items[]) | select(.name=='\"$workflow_name\"') | .status | @sh')
 }
 
 fetch_this_pipeline_id() {
-  # Debug printf "CIRCLE_WORKFLOW_ID: ${CIRCLE_WORKFLOW_ID}\n"
   local get_path="workflow/${CIRCLE_WORKFLOW_ID}"
   local get_result=$(get ${get_path})
-  # Debug printf "this pipeline id: $(echo ${get_result} | jq .)\n"
   __=$(echo "${get_result}" | jq -r '.pipeline_number')
 }
 
+# Skip checking once condition is met.
+should_skip() {
+  if [[ "$CIRCLE_BRANCH" != "master" ]]; then
+    printf "Not on master branch. Skip checking.\n"
+    return 0
+  fi
+
+  # Get the pipeline id of this job.
+  fetch_this_pipeline_id
+  this_pipeline_id=$__
+  printf "This pipeline id is ${this_pipeline_id}. This workflow id is ${CIRCLE_WORKFLOW_ID}\n\n"
+  if [[ "$this_pipeline_id" == $1 ]]; then
+    # Don't check this pipeline if this pipeline is itself.
+    printf '%s\n' "Not waiting on myself (pipeline id: ${this_pipeline_id}). Skip checking.\n" >&2
+    return 0
+  fi
+
+  return 1
+}
+
 #********************
-# ACTIONS
+# RUNNING
 # *******************
 
 printf "\n"
 check_circleci_token
-
-# Get self pipeline id for comparison later.
-fetch_this_pipeline_id
-this_pipeline_id=$__
-printf "This pipeline id is ${this_pipeline_id}. This workflow id is ${CIRCLE_WORKFLOW_ID}\n\n"
 
 # Get recent pipelines.
 fetch_pipeline_ids
@@ -114,16 +124,12 @@ for id in ${pipeline_ids}; do
   fetch_pipeline_number "${id}"
   pipeline_num=$__
 
-  if [[ $this_pipeline_id == $pipeline_num ]]; then
-    # Don't check this pipeline if this pipeline is itself.
-    printf '%s\n' "Not waiting on myself (pipeline id: ${this_pipeline_id}).\n" >&2
-    continue
-  fi
+  if should_skip "${pipeline_num}"; then continue; fi
 
   is_running=true
   # Max wait time is 40 minutes because e2e tests may take a long time to finish.
   # DISCLAIMER This max time may not be enough if there are 2 or more workflows in waiting before this workflow.
-  max_time_seconds=$(( 40 * 60 ))
+  max_time_seconds=$((40 * 60))
   waited_time=0
   sleep_time=30 # seconds
   printf "Polling workflow status in pipeline number \"${pipeline_num}\" while status is failing or running. Max wait time is ${max_time_seconds} seconds. Please wait...\n"
@@ -140,10 +146,10 @@ for id in ${pipeline_ids}; do
     fi
 
     # An active workflow has status running or failing.
-    if [[ ( $status == "'running'" ) || ( $status == "'failing'" ) ]]; then
+    if [[ ($status == "'running'") || ($status == "'failing'") ]]; then
       printf "sleeping ${wait} seconds (workflow status is ${status}).\n"
       sleep $sleep_time
-      waited_time=$(( sleep_time + waited_time ))
+      waited_time=$((sleep_time + waited_time))
     else
       is_running=false
       printf "Finished waiting for workflow in its pipeline_id: ${id}. It finished with status of ${status}!\n"
