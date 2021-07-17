cd7d25d7afbd20b1fc8da54be1b2deaf76143b6c
chavanr2019 <rohini.chavan@vumc.org>
2021-07-17 12:35:21 -0500

    split the test case to shorten it

--- e2e/tests/nightly/admin/user-admin-ui.spec.ts
+++ e2e/tests/nightly/admin/user-admin-ui.spec.ts
@@ -12,7 +12,7 @@ describe('Admin', () => {
     await navigation.navMenu(page, NavLink.USER_ADMIN);
   });
 
-  test('verify admin-table and admin-user-profile page reflect the same modules and active status', async () => {
+  test('verify admin-user-profile page reflect the same modules and active status as on admin-table', async () => {
     const userAdminPage = new UserAdminPage(page);
     await userAdminPage.waitForLoad();
 
@@ -21,20 +21,7 @@ describe('Admin', () => {
     await userAdminPage.waitForLoad();
 
     //Verify table column names match.
-    const columns = [
-      'Status',
-      'Institution',
-      'User Name',
-      'Contact Email',
-      'User Lockout',
-      'First Sign-in',
-      '2FA',
-      'Training',
-      'eRA Commons',
-      'DUCC',
-      'Bypass',
-      'Audit'
-    ];
+    const columns = ['Status', 'Institution', 'User Name', 'Contact Email', 'User Lockout', 'First Sign-in', '2FA', 'Training', 'eRA Commons', 'DUCC', 'Bypass','Audit'];
 
     const adminTable = new AdminTable(page);
     const columnNames = await adminTable.getColumnNames();
@@ -44,96 +31,33 @@ describe('Admin', () => {
 
     //get the User Name to verify the search result matches
     const userNameValue = await userAdminPage.getUserNameText(1, usernameColIndex);
-
-    //get the usernameEmail to match with the username on the admin-user-profile page
-    const userNameEmail = await userAdminPage.getUserNameEmail(1, usernameColIndex);
-
     //validate that search box result matches
     expect(userNameValue).toEqual(userEmail);
 
     //get the index of the Bypass column
     const bypassColIndex = await adminTable.getColumnIndex('Bypass');
-
     //click on the bypass link
     const bypassPopup = await userAdminPage.clickBypassLink(1, bypassColIndex);
 
     // Verify toggle option names match.
-    const toggleOptions = ['Compliance Training', 'eRA Commons Linking', 'Two Factor Auth', 'Data Use Agreement'];
+    const toggleOptions = ['Compliance Training', 'eRA Commons Linking', 'Two Factor Auth', 'Data User Code of Conduct'];
 
     const toggleTexts = await bypassPopup.getAllToggleTexts();
     expect(toggleTexts.sort()).toEqual(toggleOptions.sort());
 
-    await bypassPopup.getAllToggleInputs();
-
     // get status of each modules individually
-    const trainingToggleStatus = await bypassPopup.getEachModuleStatus('Compliance Training');
-    const eraCommToggleStatus = await bypassPopup.getEachModuleStatus('eRA Commons Linking');
-    const twoFAToggleStatus = await bypassPopup.getEachModuleStatus('Two Factor Auth');
-    const dUCCToggleStatus = await bypassPopup.getEachModuleStatus('Data Use Agreement');
-
-    //get the index of the User Lockout column
-    const userLockoutColIndex = await adminTable.getColumnIndex('User Lockout');
-
-    // get the text in the User Lockout column
-    const userLockout = await userAdminPage.getUserLockoutText(1, userLockoutColIndex);
-
-    //get the index of the Status column
-    const statusColIndex = await adminTable.getColumnIndex('Status');
-
-    //get the text in the Status column to match the status on admin-user-profile page
-    const status = await userAdminPage.getStatusText(1, statusColIndex);
-
-    //switch statement to validate that the status reflects the user lockout access
-    switch (userLockout) {
-      case 'DISABLE':
-        expect(status).toEqual('Active');
-        break;
-      case 'ENABLE':
-        expect(status).toEqual('Disabled');
-        break;
-    }
+    const trainingToggleStatus = await bypassPopup.getAccessModuleStatus('Compliance Training');
+    const eraCommToggleStatus = await bypassPopup.getAccessModuleStatus('eRA Commons Linking');
+    const twoFAToggleStatus = await bypassPopup.getAccessModuleStatus('Two Factor Auth');
+    const dUCCToggleStatus = await bypassPopup.getAccessModuleStatus('Data User Code of Conduct');
 
     //get the name column index separately since it is a frozen column
     const nameColIndex = await adminTable.getNameColindex();
 
     //click on the name link to navigate to the admin-user-profile page
     const userProfileInfo = await userAdminPage.clickNameLink(1, nameColIndex);
-
     //navigate to admin-user-profile page
     await userProfileInfo.waitForLoad();
-
-    //verify that the save button is disabled
-    expect(await userProfileInfo.getSaveButton().isCursorNotAllowed()).toBe(true);
-
-    // get the Account Access toggle status (true or false) and the text (enabled or disabled)
-    const accountAccessToggle = userProfileInfo.getAccountAccessToggle();
-    const accountAccessStatus = await userProfileInfo.getAccountAccessText();
-
-    //switch statement to validate that the Account Access text and toggle reflects the user lockout on user-admin-table.
-    switch (userLockout) {
-      case 'DISABLE':
-        expect(accountAccessStatus).toEqual('Enabled');
-        expect(await accountAccessToggle.isChecked()).toBe(true);
-        break;
-      case 'ENABLE':
-        expect(accountAccessStatus).toEqual('Disabled');
-        expect(await accountAccessToggle.isChecked()).toBe(false);
-        break;
-    }
-
-    //verify that userfulName, username and FreeCreditsUsed input fields are disabled
-    expect(await userProfileInfo.getNameInput().isCursorNotAllowed()).toBe(true);
-    expect(await userProfileInfo.getUsernameInput().isCursorNotAllowed()).toBe(true);
-    expect(await userProfileInfo.getFreeCreditsUsedInput().isCursorNotAllowed()).toBe(true);
-
-    // get the username field placeholder to verify the email on user-admin-table page and admin-user-profile page matches
-    const userNamePlaceHolder = await userProfileInfo.getUserNamePlaceHolder();
-    expect(userNameEmail).toEqual(userNamePlaceHolder);
-
-    //verify the credit limit value matches the max value in the free credits used field
-    const freeCreditLimit = await userProfileInfo.getFreeCreditsLimitValue();
-    const freeCreditMaxValue = await userProfileInfo.getFreeCreditMaxValue();
-    expect(freeCreditLimit).toEqual(freeCreditMaxValue);
 
     //get the bypass access status of all modules on admin-user-profile page
     const twoFAToggleStatus2 = await userProfileInfo.getEachBypassStatus('twoFactorAuthBypassToggle');
@@ -145,8 +69,86 @@ describe('Admin', () => {
     expect(twoFAToggleStatus2).toEqual(twoFAToggleStatus);
     expect(trainingToggle2).toEqual(trainingToggleStatus);
     expect(eraCommToggleStatus2).toEqual(eraCommToggleStatus);
-    expect(dUCCToggleStatus2).toEqual(dUCCToggleStatus);
+    expect(dUCCToggleStatus2).toEqual(dUCCToggleStatus);            
   });
+
+  test('Verify all the fields on the admin-user-profile page', async () => {
+    const userAdminPage = new UserAdminPage(page);
+    await userAdminPage.waitForLoad();
+
+    //look up the user
+    await userAdminPage.searchUser(userEmail);
+    await userAdminPage.waitForLoad();
+    const adminTable = new AdminTable(page);
+
+    const usernameColIndex = await adminTable.getColumnIndex('User Name');
+    //get the index of the User Lockout column
+    const userLockoutColIndex = await adminTable.getColumnIndex('User Lockout');
+  
+    // get the text in the User Lockout column
+    const userLockout = await userAdminPage.getUserLockoutText(1, userLockoutColIndex);
+  
+    //get the index of the Status column
+    const statusColIndex = await adminTable.getColumnIndex('Status');
+  
+    //get the text in the Status column to match the status on admin-user-profile page
+    const status = await userAdminPage.getStatusText(1, statusColIndex);
+  
+    //switch statement to validate that the status reflects the user lockout access
+    switch (userLockout) {
+      case 'DISABLE':
+        expect(status).toEqual('Active');
+        break;
+      case 'ENABLE':
+        expect(status).toEqual('Disabled');
+        break;
+    }
+     //get the name column index separately since it is a frozen column
+     const nameColIndex = await adminTable.getNameColindex();
+
+     //click on the name link to navigate to the admin-user-profile page
+     const userProfileInfo = await userAdminPage.clickNameLink(1, nameColIndex);
+ 
+     //navigate to admin-user-profile page
+     await userProfileInfo.waitForLoad();
+  
+    //get the usernameEmail to match with the username on the admin-user-profile page
+    const userNameEmail = await userAdminPage.getUserNameEmail(1, usernameColIndex);
+  
+    //verify that the save button is disabled
+    expect(await userProfileInfo.getSaveButton().isCursorNotAllowed()).toBe(true);
+  
+    // get the Account Access toggle status (true or false) and the text (enabled or disabled)
+    const accountAccessToggle = userProfileInfo.getAccountAccessToggle();
+    const accountAccessStatus = await userProfileInfo.getAccountAccessText();
+  
+    //switch statement to validate that the Account Access text and toggle reflects the user lockout on user-admin-table.
+    switch (userLockout) {
+      case 'DISABLE':
+        expect(accountAccessStatus).toEqual('Enabled');
+        expect(await accountAccessToggle.isChecked()).toBe(true);
+        break;
+      case 'ENABLE':
+        expect(accountAccessStatus).toEqual('Disabled');
+        expect(await accountAccessToggle.isChecked()).toBe(false);
+        break;
+    }
+  
+    //verify that userfulName, username and FreeCreditsUsed input fields are disabled
+    expect(await userProfileInfo.getNameInput().isCursorNotAllowed()).toBe(true);
+    expect(await userProfileInfo.getUsernameInput().isCursorNotAllowed()).toBe(true);
+    expect(await userProfileInfo.getFreeCreditsUsedInput().isCursorNotAllowed()).toBe(true);
+  
+    // get the username field placeholder to verify the email on user-admin-table page and admin-user-profile page matches
+    const userNamePlaceHolder = await userProfileInfo.getUserNamePlaceHolder();
+    expect(userNameEmail).toEqual(userNamePlaceHolder);
+  
+    //verify the credit limit value matches the max value in the free credits used field
+    const freeCreditLimit = await userProfileInfo.getFreeCreditsLimitValue();
+    const freeCreditMaxValue = await userProfileInfo.getFreeCreditMaxValue();
+    expect(freeCreditLimit).toEqual(freeCreditMaxValue);
+  
+    });
 
   test('Verify that the user-audit page UI renders correctly', async () => {
     const userAdminPage = new UserAdminPage(page);
@@ -178,4 +180,5 @@ describe('Admin', () => {
     const userProfileInfo = new UserProfileInfo(page);
     await userProfileInfo.waitForLoad();
   });
+  
 });
