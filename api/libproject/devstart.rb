d943ae998f5aa829b2487416964daa944f9cb0af
CH Albach <calbach@users.noreply.github.com>
2021-07-27 15:21:55 -0700

    [RW-6699][risk=no] rm oneoff user affiliation backfill tool (#5311)

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -1726,69 +1726,6 @@ Common.register_command({
     :fn => ->(*args) {load_institutions(LOAD_INSTITUTIONS_CMD, *args)}
 })
 
-def populate_user_affiliations(cmd_name, *args)
-  common = Common.new
-  ensure_docker(cmd_name, args)
-
-  op = WbOptionsParser.new(cmd_name, args)
-  op.opts.dry_run = true
-  op.opts.project = TEST_PROJECT
-
-  op.add_typed_option(
-      "--dry_run=[dry_run]",
-      TrueClass,
-      ->(opts, v) { opts.dry_run = v},
-      "When true, print debug lines instead of performing writes. Defaults to true.")
-
-  op.add_typed_option(
-      "--import-filename [import-filename]",
-      String,
-      ->(opts, v) { opts.importFilename = v},
-      "CSV File containing list of users to apply affiliations")
-
-  op.add_typed_option(
-      "--user-type [user-type]",
-      String,
-      ->(opts, v) { opts.userType = v},
-      "Which user type to populate: OPS or RESEARCHERS")
-
-  op.add_validator ->(opts) { raise ArgumentError unless (opts.importFilename and opts.userType)}
-
-  # Create a cloud context and apply the DB connection variables to the environment.
-  # These will be read by Gradle and passed as Spring Boot properties to the command-line.
-  gcc = GcloudContextV2.new(op)
-  op.parse.validate
-  gcc.validate()
-
-  if op.opts.dry_run
-    common.status "DRY RUN -- CHANGES WILL NOT BE PERSISTED"
-  end
-
-  gradle_args = ([
-      ["--import-filename", op.opts.importFilename],
-      ["--user-type", op.opts.userType]
-  ]).map { |kv| "#{kv[0]}=#{kv[1]}" }
-  if op.opts.dry_run
-    gradle_args += ["--dry-run"]
-  end
-  # Gradle args need to be single-quote wrapped.
-  gradle_args.map! { |f| "'#{f}'" }
-
-  with_cloud_proxy_and_db(gcc) do
-    common.run_inline %W{
-        gradle populateUserAffiliations
-       -PappArgs=[#{gradle_args.join(',')}]}
-  end
-end
-
-POPULATE_USER_AFFILIATIONS_CMD = "populate-user-affiliations"
-
-Common.register_command({
-    :invocation => POPULATE_USER_AFFILIATIONS_CMD,
-    :description => "Populate the institutional affiliations for the users in the specified CSV file.",
-    :fn => ->(*args) {populate_user_affiliations(POPULATE_USER_AFFILIATIONS_CMD, *args)}
-})
-
 def delete_workspaces(cmd_name, *args)
   common = Common.new
   ensure_docker cmd_name, args
