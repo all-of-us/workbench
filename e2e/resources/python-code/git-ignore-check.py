c9268c36112d05342688faa38fcdd92123404c84
CH Albach <calbach@google.com>
2021-07-29 10:56:49 -0700

    Test improvements

--- e2e/resources/python-code/git-ignore-check.py
+++ e2e/resources/python-code/git-ignore-check.py
@@ -1,3 +1,5 @@
+# Any git repo will do here, workbench-snippets is small and relevant to AoU's
+# notebook images.
 ! git clone https://github.com/all-of-us/workbench-snippets.git /tmp/workbench-snippets
 
 ! touch /tmp/workbench-snippets/should_be_ignored.png
@@ -11,15 +13,20 @@
 ! touch /tmp/workbench-snippets/should_be_visible.wdl
 ! touch /tmp/workbench-snippets/should_be_visible.sh
 ! touch /tmp/workbench-snippets/should_be_visible.md
-! mkdir -p /tmp/workbench-snippets/some_diretory/
-! touch /tmp/workbench-snippets/some_diretory/should_be_ignored.CSV
-! touch /tmp/workbench-snippets/some_diretory/should_be_visible.sh
+! touch /tmp/workbench-snippets/should_be_visible.ipynb
+! touch /tmp/workbench-snippets/should_be_visible.rst
+! touch /tmp/workbench-snippets/LICENSE.should_be_visible
+! mkdir -p /tmp/workbench-snippets/some_directory/
+! cd /tmp/workbench-snippets/; git add some_directory
+! touch /tmp/workbench-snippets/some_directory/should_be_ignored.CSV
+! touch /tmp/workbench-snippets/some_directory/should_be_visible.sh
 
-actual = !cd /tmp/workbench-snippets/; git status
+actual = !cd /tmp/workbench-snippets/; git status --porcelain
 
-print(actual)
+visible = list(filter(lambda s : 'should_be_visible' in s, actual))
+assert len(visible) == 10, f"wrong number of visible files ({len(visible)}): {visible}"
 
-assert(any('should_be_visible' in s for s in actual))
-assert(not any('should_be_ignored' in s for s in actual))
+ignored = list(filter(lambda s : 'should_be_ignored' in s, actual))
+assert len(ignored) == 0, f"found {len(ignored)} files which should have been ignored: {ignored}"
 
 print("success")
