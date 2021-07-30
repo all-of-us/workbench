24248255ec2977d12fabb39d1b856278c783693a
CH Albach <calbach@users.noreply.github.com>
2021-07-30 09:46:16 -0700

    Update notebook-create-python.spec.ts

--- e2e/tests/notebook/notebook-create-python.spec.ts
+++ e2e/tests/notebook/notebook-create-python.spec.ts
@@ -62,7 +62,7 @@ describe('Create python kernel notebook', () => {
     // toContain() is not a strong enough check: error text also includes "success" because it's in the code
     expect(cell2OutputText.endsWith('success')).toBeTruthy();
 
-    const cell3OutputText = await notebook.runCodeCell(3, {
+    await notebook.runCodeCell(3, {
       codeFile: 'resources/python-code/git-ignore-check.py',
       markdownWorkaround: true
     });
