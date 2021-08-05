ff05fcbb70f16c7095901193c5e333254b757dc2
Brian Freeman <brian.d.freeman@gmail.com>
2021-08-05 17:47:16 -0500

    RW-7083 updating comment

--- api/db-cdr/generate-cdr/build-cdr-indices.sh
+++ api/db-cdr/generate-cdr/build-cdr-indices.sh
@@ -36,9 +36,9 @@ echo ""
 echo "Creating cb_survey_version table"
 if ./generate-cdr/make-bq-cb-survey-version.sh "$BQ_PROJECT" "$BQ_DATASET"
 then
-    echo "Prep table creation is complete"
+    echo "cb_survey_version table creation is complete"
 else
-    echo "Prep table creation failed!"
+    echo "cb_survey_version table creation failed!"
     exit 1
 fi
 
