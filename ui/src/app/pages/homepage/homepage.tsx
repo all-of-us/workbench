30b5b38a3b61582cf433b9bb89ea434fa8244a37
Joel Thibault <thibault@broad.dev>
2021-08-06 14:44:35 -0400

    mini cleanup

--- ui/src/app/pages/homepage/homepage.tsx
+++ ui/src/app/pages/homepage/homepage.tsx
@@ -63,26 +63,23 @@ export const styles = reactStyles({
 });
 
 const WelcomeHeader = () => {
-  return <FlexRow style={{marginLeft: '3%'}}>
-    <FlexColumn style={{width: '50%'}}>
-      <FlexRow>
-        <FlexColumn>
-          <Header style={{fontWeight: 500, color: colors.secondary, fontSize: '0.92rem'}}>
-            Welcome to the</Header>
-          <Header style={{textTransform: 'uppercase', marginTop: '0.2rem'}}>
-            Researcher Workbench</Header>
-        </FlexColumn>
-        <FlexRow style={{alignItems: 'flex-end', marginLeft: '1rem'}}>
-          <img style={styles.welcomeMessageIcon} src='/assets/images/workspace-icon.svg'/>
-          <img style={styles.welcomeMessageIcon} src='/assets/images/cohort-icon.svg'/>
-          <img style={styles.welcomeMessageIcon} src='/assets/images/analysis-icon.svg'/>
-        </FlexRow>
+  return <FlexColumn style={{marginLeft: '3%', width: '50%'}}>
+    <FlexRow>
+      <FlexColumn>
+        <Header style={{fontWeight: 500, color: colors.secondary, fontSize: '0.92rem'}}>
+          Welcome to the</Header>
+        <Header style={{textTransform: 'uppercase', marginTop: '0.2rem'}}>
+          Researcher Workbench</Header>
+      </FlexColumn>
+      <FlexRow style={{alignItems: 'flex-end', marginLeft: '1rem'}}>
+        <img style={styles.welcomeMessageIcon} src='/assets/images/workspace-icon.svg'/>
+        <img style={styles.welcomeMessageIcon} src='/assets/images/cohort-icon.svg'/>
+        <img style={styles.welcomeMessageIcon} src='/assets/images/analysis-icon.svg'/>
       </FlexRow>
-      <SmallHeader style={{color: colors.primary, marginTop: '0.25rem'}}>
-        The secure platform to analyze <i>All of Us</i> data</SmallHeader>
-    </FlexColumn>
-    <div/>
-  </FlexRow>;
+    </FlexRow>
+    <SmallHeader style={{color: colors.primary, marginTop: '0.25rem'}}>
+      The secure platform to analyze <i>All of Us</i> data</SmallHeader>
+  </FlexColumn>;
 };
 
 const Workspaces = () => {
@@ -363,16 +360,12 @@ export const Homepage = withUserProfile()(class extends React.Component<Props, S
                     ) : (
                         <React.Fragment>
                           <Workspaces/>
-                          <FlexColumn>
                             {userWorkspacesResponse &&
-                              <React.Fragment>
-                                {this.userHasWorkspaces() ?
+                              (this.userHasWorkspaces() ?
                                     <RecentResources workspaces={userWorkspacesResponse.items}/> :
                                     <GettingStarted/>
-                                }
-                              </React.Fragment>
+                              )
                             }
-                          </FlexColumn>
                         </React.Fragment>
                       )
                 ) :
