11df0dccc9921da4c2a48b744d22a64e7dc3b878
aweng98 <bweng@broadinstitute.org>
2021-08-12 19:26:07 -0400

    not working

--- .circleci/workflow-queue-v1.sh
+++ .circleci/workflow-queue-v1.sh
@@ -77,9 +77,8 @@ fetch_older_pipelines() {
   jq_filter="select(.status | test(\"running|pending|queued\"))"
   jq_filter+=" | select(.workflows.workflow_name==\"${workflow_name}\" and .workflows.workflow_id!=\"${CIRCLE_WORKFLOW_ID}\")"
   jq_object="{ workflow_name: .workflows.workflow_name, workflow_id: .workflows.workflow_id, job_name: .workflows.job_name, build_num: .build_num, start_time: .start_time, status: .status }"
-  echo "${get_result}" | jq -r ".[] | ${jq_filter}"
-  printf "done echo"
-  __=$(echo "${get_result}" | jq -r ".[] | ${jq_filter} | select(.start_time>=\"${1}\")")
+  printf "DEBUG\n%s\nEND OF DEBUG" "${get_result}" | jq -r ".[] | ${jq_filter}"
+  __=$(echo "${get_result}" | jq -r ".[] | ${jq_filter} | select(.start_time>=\"${1}\") | ${jq_object}")
 }
 
 
