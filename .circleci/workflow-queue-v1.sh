8bd8eab9d8fca606142785fa8f7c99eade1467fd
aweng98 <bweng@broadinstitute.org>
2021-08-12 17:54:21 -0400

    not working

--- .circleci/workflow-queue-v1.sh
+++ .circleci/workflow-queue-v1.sh
@@ -59,7 +59,7 @@ fetch_current_pipeline_start_time() {
   # local get_path="project/${project_slug}/tree/${branch}?filter=running&shallow=true"
   local get_path="project/${project_slug}?filter=running&shallow=true"
   local get_result=$(circle_get "${get_path}")
-  jq_filter="select(.workflows.workflow_name==\"${workflow_name}\") | select(.workflows.workflow_id==\"${CIRCLE_WORKFLOW_ID}\") | .start_time | @sh"
+  jq_filter="select(.build_num < $CIRCLE_BUILD_NUM and .workflows.workflow_name==\"${workflow_name}\") | select(.workflows.workflow_id==\"${CIRCLE_WORKFLOW_ID}\") | .start_time | @sh"
   __=$(echo "${get_result}" | jq -r ".[] | ${jq_filter}")
 }
 
@@ -97,7 +97,7 @@ check_circleci_workflow_id
 
 # Get pipeline that is doing the polling.
 printf "%s\n" "Current pipeline CIRCLE_BUILD_NUM: ${CIRCLE_BUILD_NUM}"
-printf "%s\n" "Current pipeline CIRCLE_WORKFLOW_ID: ${CIRCLE_WORKFLOW_ID}"
+printf "%s\n\n" "Current pipeline CIRCLE_WORKFLOW_ID: ${CIRCLE_WORKFLOW_ID}"
 
 fetch_current_pipeline_start_time
 current_pipeline_start_time=$__
