8f222a1e2b9f8769369dc0b30703ff6164639c44
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2021-08-12 15:38:26 -0400

    [RW-7115 ][risk=no] UI - when calling API for findCriteriaByDomainAndSearchTerm (#5415)
    * Use standard flag API if the flag is turned on
    
    * Fix Concept Set call
    
    * PR Comment
    
    * Update the standard

--- ui/src/app/cohort-search/search-group-list/search-group-list.component.tsx
+++ ui/src/app/cohort-search/search-group-list/search-group-list.component.tsx
@@ -117,7 +117,7 @@ const css = `
 `;
 
 function mapMenuItem(item: CriteriaMenu) {
-  const {category, domainId, group, id, name, sortOrder, type} = item;
+  const {category, domainId, group, id, name, sortOrder, type, standard} = item;
   return {
     category,
     children: null,
@@ -126,7 +126,7 @@ function mapMenuItem(item: CriteriaMenu) {
     id,
     name,
     sortOrder,
-    standard: domainId === Domain.VISIT.toString() ? true : null,
+    standard,
     type
   };
 }
