deb3547b9fbbd15e03bf1780bfbb6d881caa4613
Eric Song <me@ericsong.io>
2021-08-17 15:47:07 -0400

    manually add stack driver dependency

--- ui/src/main.ts
+++ ui/src/main.ts
@@ -3,10 +3,16 @@
 import * as React from 'react';
 import * as ReactDOM from 'react-dom';
 import * as ReactModal from 'react-modal';
+import * as StackTrace from 'stacktrace-js';
 
 import {ErrorHandler} from 'app/components/error-handler';
 import {setupCustomValidators} from 'app/services/setup';
 import {AppRoutingComponent} from './app/routing/app-routing';
+
+// Unfortunately stackdriver-errors-js doesn't properly declare dependencies, so
+// we need to explicitly load its StackTrace dep:
+// https://github.com/GoogleCloudPlatform/stackdriver-errors-js/issues/2
+(<any>window).StackTrace = StackTrace;
 
 setupCustomValidators();
 ReactModal.defaultStyles.overlay = {};
