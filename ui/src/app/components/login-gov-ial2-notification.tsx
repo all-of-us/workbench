7dde14bcfa037d7136211b6cca83ae75f2fdd806
Joel Thibault <thibault@broad.dev>
2021-08-23 08:21:20 -0400

    post rebase test fixes

--- ui/src/app/components/login-gov-ial2-notification.tsx
+++ ui/src/app/components/login-gov-ial2-notification.tsx
@@ -3,7 +3,7 @@ import * as React from 'react';
 import colors from 'app/styles/colors';
 import {reactStyles} from 'app/utils';
 import {getAccessModuleStatusByName} from 'app/utils/access-utils';
-import {navigate} from 'app/utils/navigation';
+import {useNavigation} from 'app/utils/navigation';
 import {profileStore, serverConfigStore, useStore} from 'app/utils/stores';
 import {AccessModule} from 'generated/fetch';
 import {Button} from './buttons';
@@ -66,6 +66,7 @@ const styles = reactStyles({
 
 
 const LoginGovIAL2Notification = () => {
+  const [navigate, ] = useNavigation();
   return <FlexRow data-test-id='ial2-notification' style={styles.box}>
     <AlarmExclamation style={styles.icon}/>
     <div style={styles.text}>Please verify your identity by 10/06/2021.
