384e3a7dbca81511c8464e5ac75e9053eebc2627
Ariel Schwartz <aschwart@broadinstitute.org>
2021-08-26 14:47:30 -0400

    tests run now

---
+++ ui/jest.config.js
@@ -0,0 +1,28 @@
+const config = {
+  "preset": "ts-jest",
+    "clearMocks": true,
+    "moduleFileExtensions": [
+    "ts",
+    "tsx",
+    "js",
+    "json",
+    "node"
+  ],
+      "moduleDirectories": [
+    "node_modules",
+    "src"
+  ],
+      "transform": {
+    "^.+\\.tsx?$": "ts-jest"
+  },
+    "testMatch": [
+    "**/*.spec.tsx"
+  ],
+      "globals": {
+    "ts-jest": {
+      "tsconfig": "<rootDir>/src/tsconfig.jest.json"
+    }
+  }
+}
+
+module.exports = config;
