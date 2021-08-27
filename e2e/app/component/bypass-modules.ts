339e32c02498fc299a767435fdc767b3c01f5d21
chavanr2019 <48066285+chavanr2019@users.noreply.github.com>
2021-08-26 21:18:10 -0500

    [risk=no][RW-7216]Chavanr/user admin UI (#5152)
    * added a function to get the link of the cell
    
    * added the audit page button linktext
    
    * created a component for admin table
    
    * created a component for the bypass module's modal
    
    * created a page for the user-admin-table
    
    * created a page for the admin-user-profile-info
    
    * created a test suite for the RW's admin feature
    
    * removed all verify functions and update to gettoggle functions
    
    * removed the waitwhileloading(this.page)
    
    * added a new function to get the username email and updated the code for page opening in new tab
    
    * updated the test case with verify useremail value, togglecheckboxes and user lockout/status
    
    * refactored code to read the title of the active tab
    
    * updated the title of the user audit page
    
    * added new function to get the email from user-admin table
    
    * created functions to get the input field and values on admin-user-profile page
    
    * moved the admin tests to nightly folder
    
    * delete the admin test and moved to nightly folder
    
    * combined the institutionSelectValue
    
    * created new functione for user-audit page validation
    
    * combined the institutionSelectValue and moved to text-labels file
    
    * added tests to validate the user-audit-page
    
    * created functions to get the status of the bypass-link modules
    
    * created functions to get the status of each modules
    
    * created single function to get the modules bypass status on user-profile-page
    
    * added test steps to verify that module status on admin & user-profile match
    
    * removed no-op functions
    
    * updated the function name to getAccessModuleStatus
    
    * split the test case to shorten it
    
    * removed no-op function
    
    * created functions to update the free credits
    
    * created a function to update the free credits
    
    * update the test case to reflect the ui changes
    
    * created a function to verify the bypass popup in test & staging env
    
    * created function to verify the bypass status in test & staging env
    
    * updated the test case to verify in both test & staging
    
    * lint fixes
    
    * added code test-nightly-staging:debug
    
    * refactored the code to make it run in both env
    
    * typo - corrected
    
    * app/component/base-menu.ts
    
    * resolved conflicts
    
    * fixed the xpath length
    
    * signInWithAccessToken updated to use token file

---
+++ e2e/app/component/bypass-modules.ts
@@ -0,0 +1,88 @@
+import { Page } from 'puppeteer';
+import BaseMenu from './base-menu';
+import Checkbox from 'app/element/checkbox';
+import { getPropValue } from 'utils/element-utils';
+
+const defaultXpath = '//*[@id="popup-root"]';
+
+export default class BypassPopup extends BaseMenu {
+  constructor(page: Page, xpath: string = defaultXpath) {
+    super(page, xpath);
+  }
+
+  /**
+   *  Get labels of all modules.
+   */
+  async getAllToggleTexts(): Promise<string[]> {
+    const selector = `${this.getXpath()}//label/span/text()`;
+    return this.getMenuItemTexts(selector);
+  }
+
+  // xpath of the bypass-link-checkbox in user admin table
+  getMenuItemXpath(accessModule: string): string {
+    return `${this.getXpath()}//span[text()= "${accessModule}"]/preceding-sibling::div/input[@type='checkbox']`;
+  }
+
+  //get status of each bypass access modules
+  async getAccessModuleStatus(accessModule: string): Promise<boolean> {
+    const selector = this.getMenuItemXpath(accessModule);
+    const btn = new Checkbox(this.page, selector);
+    const accessModuleStatus = await getPropValue<boolean>(await btn.asElementHandle(), 'checked');
+    return accessModuleStatus;
+  }
+
+  // get the module name and the status of each modules in test env
+  async getBypassModuleTest(): Promise<boolean[]> {
+    const bypassAdminTest = [
+      'Compliance Training',
+      'eRA Commons Linking',
+      'Two Factor Auth',
+      'Data User Code of Conduct',
+      'RAS Login.gov Link'
+    ];
+    const moduleNames: string[] = [];
+    let i: number;
+    const btnStatus: boolean[] = [];
+    for (i = 0; i < bypassAdminTest.length; i++) {
+      moduleNames.push(bypassAdminTest[i]);
+      btnStatus.push(await this.getAccessModuleStatus(moduleNames[i]));
+    }
+    return btnStatus;
+  }
+
+  // get the module name and the status of each modules in staging env
+  async getBypassModuleStaging(): Promise<boolean[]> {
+    const bypassAdminStaging = ['eRA Commons Linking', 'Two Factor Auth', 'Data User Code of Conduct'];
+    const moduleNames: string[] = [];
+    let i: number;
+    const btnStatus: boolean[] = [];
+    for (i = 0; i < bypassAdminStaging.length; i++) {
+      moduleNames.push(bypassAdminStaging[i]);
+      btnStatus.push(await this.getAccessModuleStatus(moduleNames[i]));
+    }
+    return btnStatus;
+  }
+
+  // check the env and run the respective function to get the module status
+  async getBypassAdminEnv(): Promise<boolean[]> {
+    const checkEnv = process.env.WORKBENCH_ENV;
+    switch (checkEnv) {
+      case 'test':
+        return this.getBypassModuleTest();
+      case 'staging':
+        return this.getBypassModuleStaging();
+    }
+  }
+
+  // click the checkmark on the bypass-popup to save the bypass access
+  async clickCheckmarkBypass(): Promise<void> {
+    const xpath = `${this.getXpath()}//*[local-name()='svg' and @data-icon='check']`;
+    await new Checkbox(this.page, xpath).click();
+  }
+
+  // click the cancel icon to close the bypass-popup
+  async clickCancelBypass(): Promise<void> {
+    const xpath = `${this.getXpath()}//*[local-name()='svg' and @data-icon='times']`;
+    await new Checkbox(this.page, xpath).click();
+  }
+}
