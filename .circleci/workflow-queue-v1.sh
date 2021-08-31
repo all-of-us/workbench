d7766be0c4148606d4e10a9066498f90820842b1
aweng98 <bweng@broadinstitute.org>
2021-08-31 14:09:56 -0400

    script error

--- .circleci/workflow-queue-v1.sh
+++ .circleci/workflow-queue-v1.sh
@@ -77,8 +77,8 @@ fetch_older_pipelines() {
   # TODO jq_filter=".branch==\"${branch}\" and (.status | test(\"running|pending|queued\")) "
   jq_filter="(.status | test(\"running|pending|queued\")) "
   jq_filter+="and .workflows.workflow_name==\"${workflow_name}\" and .workflows.workflow_id!=\"${CIRCLE_WORKFLOW_ID}\""
-  jq_object="[{ workflow_name: .workflows.workflow_name, workflow_id: .workflows.workflow_id, "
-  jq_object+="job_name: .workflows.job_name, build_num, start_time, status }]"
+  jq_object="{ workflow_name: .workflows.workflow_name, workflow_id: .workflows.workflow_id, "
+  jq_object+="job_name: .workflows.job_name, build_num, start_time, status }"
   __=$(echo "${curl_result}" | jq -r ".[] | select(${jq_filter}) | select(.start_time < \"${1}\") | ${jq_object} | workflow_id")
 }
 
