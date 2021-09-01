617919097faeb89974f13c55f5b02a23129a0526
aweng98 <bweng@broadinstitute.org>
2021-09-01 12:38:56 -0400

    get unique workflow id

--- .circleci/workflow-queue-v1.sh
+++ .circleci/workflow-queue-v1.sh
@@ -71,7 +71,7 @@ fetch_older_pipelines() {
   jq_filter=".branch==\"${BRANCH}\" and (.status | test(\"running|pending|queued\")) "
   jq_filter+="and .workflows.workflow_name==\"${WORKFLOW_NAME}\" and .workflows.workflow_id!=\"${CIRCLE_WORKFLOW_ID}\""
 
-  __=$(echo "${curl_result}" | jq -r ".[] | select(${jq_filter}) | select(.start_time < \"${1}\") | .workflows.workflow_id")
+  __=$(echo "${curl_result}" | jq -r ".[] | select(${jq_filter}) | select(.start_time < \"${1}\") | .workflows.workflow_id | unique")
 }
 
 fetch_pipeline_jobs() {
