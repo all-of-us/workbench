063d8249262b0ff04acbc53a5391d3202d11bdc5
Yonghao Yu <yyhao1@gmail.com>
2021-09-02 13:48:22 -0400

    [RW-7234] support RAS module in set-access-module-timestamps (#5517)
    * support era and RAS module
    
    * era
    
    * era
    
    * revert
    
    * revert
    
    * simply code
    
    * update text
    
    * set env var

--- api/tools/src/main/java/org/pmiops/workbench/tools/SetAccessModuleTimestamps.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/SetAccessModuleTimestamps.java
@@ -11,6 +11,7 @@ import org.apache.commons.cli.Option;
 import org.apache.commons.cli.Options;
 import org.pmiops.workbench.access.AccessModuleService;
 import org.pmiops.workbench.access.AccessModuleServiceImpl;
+import org.pmiops.workbench.access.AccessUtils;
 import org.pmiops.workbench.access.UserAccessModuleMapperImpl;
 import org.pmiops.workbench.actionaudit.ActionAuditServiceImpl;
 import org.pmiops.workbench.actionaudit.auditors.UserServiceAuditorImpl;
@@ -20,10 +21,13 @@ import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.model.DbAccessModule.AccessModuleName;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.utils.mappers.CommonMappers;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Primary;
+import org.springframework.context.annotation.Scope;
 
 @Configuration
 @Import({
@@ -49,9 +53,10 @@ public class SetAccessModuleTimestamps {
 
   private static final Options OPTIONS = new Options().addOption(USER_OPT);
 
-  // very crude POC implementation - only handles ProfileConfirmation and completion (not bypass)
-  private final AccessModuleName moduleName = AccessModuleName.PROFILE_CONFIRMATION;
-  private final Timestamp timestamp = Timestamp.from(Instant.parse("2011-08-01T00:00:00.00Z"));
+  private static final Timestamp PROFILE_CONFIRMATION_TIMESTAMP =
+      Timestamp.from(Instant.parse("2011-08-01T00:00:00.00Z"));
+
+  private static DbUser dbUser;
 
   void applyTimestampUpdateToUser(
       UserDao userDao,
@@ -60,29 +65,25 @@ public class SetAccessModuleTimestamps {
       AccessModuleName moduleName,
       @Nullable Timestamp timestamp,
       boolean isBypass) {
-    DbUser dbUser = userDao.findUserByUsername(username);
-    if (dbUser == null) {
-      throw new IllegalArgumentException(String.format("User %s not found!", username));
-    }
+    accessModuleService.updateCompletionTime(dbUser, moduleName, timestamp);
 
-    if (moduleName == AccessModuleName.PROFILE_CONFIRMATION && !isBypass) {
-      final String field = isBypass ? "bypass" : "completion";
-      final String time = Optional.ofNullable(timestamp).map(Timestamp::toString).orElse("NULL");
-      LOG.info(
-          String.format(
-              "Updating %s %s time for user %s to %s", moduleName, field, username, time));
-
+    if (moduleName == AccessModuleName.PROFILE_CONFIRMATION) {
       // dual-write to DbUser and AccessModuleService
       // we will remove the module fields in DbUser soon
       // see the Access Module Update epic
       // https://precisionmedicineinitiative.atlassian.net/browse/RW-6237
-
       dbUser.setProfileLastConfirmedTime(timestamp);
       dbUser = userDao.save(dbUser);
-      accessModuleService.updateCompletionTime(dbUser, moduleName, timestamp);
     } else {
-      throw new IllegalArgumentException("Not implemented!");
+      accessModuleService.updateBypassTime(
+          dbUser.getUserId(), AccessUtils.storageAccessModuleToClient(moduleName), isBypass);
     }
+
+    final String time = Optional.ofNullable(timestamp).map(Timestamp::toString).orElse("NULL");
+    LOG.info(
+        String.format(
+            "Updating %s bypass and/or completion time for user %s to %s; bypass mode is %s",
+            moduleName, username, time, isBypass));
   }
 
   @Bean
@@ -90,10 +91,28 @@ public class SetAccessModuleTimestamps {
     return (args) -> {
       final CommandLine opts = new DefaultParser().parse(OPTIONS, args);
       final String username = opts.getOptionValue(USER_OPT.getLongOpt());
+      dbUser = userDao.findUserByUsername(username);
+      if (dbUser == null) {
+        throw new IllegalArgumentException(String.format("User %s not found!", username));
+      }
 
       applyTimestampUpdateToUser(
-          userDao, accessModuleService, username, moduleName, timestamp, false);
+          userDao,
+          accessModuleService,
+          username,
+          AccessModuleName.PROFILE_CONFIRMATION,
+          PROFILE_CONFIRMATION_TIMESTAMP,
+          false);
+      applyTimestampUpdateToUser(
+          userDao, accessModuleService, username, AccessModuleName.RAS_LOGIN_GOV, null, false);
     };
+  }
+
+  @Bean
+  @Primary
+  @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+  DbUser user() {
+    return dbUser;
   }
 
   public static void main(String[] args) throws Exception {
