b575a05502c2ee41ecd8a70f004184738ac22a26
Joel Thibault <thibault@broad.dev>
2021-09-07 17:23:21 -0400

    test fix

--- ui/src/app/pages/profile/data-access-panel.spec.tsx
+++ ui/src/app/pages/profile/data-access-panel.spec.tsx
@@ -1,14 +1,30 @@
 import {mount} from 'enzyme';
 import * as React from 'react';
 
-import * as TestHelpers from 'testing/react-test-helpers';
 import {DataAccessPanel, DataAccessPanelProps} from 'app/pages/profile/data-access-panel';
-import {CheckCircle} from 'app/components/icons'
 import {cdrVersionStore} from 'app/utils/stores';
 import {cdrVersionTiersResponse} from 'testing/stubs/cdr-versions-api-stub';
-import {CdrVersionTier} from 'generated/fetch';
-import {AccessTierShortNames} from 'app/utils/access-tiers';
 
+const findRTGranted = (wrapper) => wrapper.find(`[data-test-id="registered-tier-access-granted"]`);
+const findRTDenied = (wrapper) => wrapper.find(`[data-test-id="registered-tier-access-denied"]`);
+const findCTGranted = (wrapper) => wrapper.find(`[data-test-id="controlled-tier-access-granted"]`);
+const findCTDenied = (wrapper) => wrapper.find(`[data-test-id="controlled-tier-access-denied"]`);
+
+const expectAccessStatus = (wrapper, rtStatus: boolean, ctStatus: boolean) => {
+  expect(findRTGranted(wrapper).exists()).toEqual(rtStatus);
+  expect(findRTDenied(wrapper).exists()).not.toEqual(rtStatus);
+  expect(findCTGranted(wrapper).exists()).toEqual(ctStatus);
+  expect(findCTDenied(wrapper).exists()).not.toEqual(ctStatus);
+}
+
+const expectAccessStatusRtOnly = (wrapper, rtStatus: boolean) => {
+  expect(findRTGranted(wrapper).exists()).toEqual(rtStatus);
+  expect(findRTDenied(wrapper).exists()).not.toEqual(rtStatus);
+
+  // Controlled Tier does not render at all
+  expect(findCTGranted(wrapper).exists()).toBeFalsy();
+  expect(findCTDenied(wrapper).exists()).toBeFalsy();
+}
 
 describe('Data Access Panel', () => {
   const component = (props: DataAccessPanelProps) => {
@@ -20,63 +36,32 @@ describe('Data Access Panel', () => {
   });
 
   it('Should show success status for registered tier when the user has access', async() => {
-    const wrapper = component({accessTierShortNames: ['registered']});
-    expect(wrapper.find(CheckCircle).length).toBe(1);
-    // one section of "Please complete the data access requirements to gain access", for CT
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access').length).toBe(1);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access to controlled tier data').length).toBe(1);
+    const wrapper = component({accessTiersInEnvironment: ['registered', 'controlled'], userAccessTiers: ['registered']});
+    expectAccessStatus(wrapper, true, false);
   });
 
   it('Should show success status for controlled tier when the user has access', async() => {
-    const wrapper = component({accessTierShortNames: ['controlled']});
-    expect(wrapper.find(CheckCircle).length).toBe(1);
-    // one section of "Please complete the data access requirements to gain access", for RT
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access').length).toBe(1);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access to registered tier data').length).toBe(1);
+    const wrapper = component({accessTiersInEnvironment: ['registered', 'controlled'], userAccessTiers: ['controlled']});
+    expectAccessStatus(wrapper, false, true);
   });
 
   it('Should show success status when the user is in the registered tier and controlled tier', async() => {
-    const wrapper = component({accessTierShortNames: ['registered', 'controlled']});
-    expect(wrapper.find(CheckCircle).length).toBe(2);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access').length).toBe(0);
+    const wrapper = component({accessTiersInEnvironment: ['registered', 'controlled'], userAccessTiers: ['registered', 'controlled']});
+    expectAccessStatus(wrapper, true, true);
   });
 
-  it('Should not show success status when the user is in not the registered tier or controlled tier', async() => {
-    const wrapper = component({accessTierShortNames: []});
-    expect(wrapper.find(CheckCircle).length).toBe(0);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access').length).toBe(2);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access to registered tier data').length).toBe(1);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access to controlled tier data').length).toBe(1);
+  it('Should not show success status when the user is not in the registered tier or controlled tier', async() => {
+    const wrapper = component({accessTiersInEnvironment: ['registered', 'controlled'], userAccessTiers: []});
+    expectAccessStatus(wrapper, false, false);
   });
 
   it('Should only show the registered tier in environments without a controlled tier (user has access)', async() => {
-    const rtOnly: CdrVersionTier = cdrVersionTiersResponse.tiers.find(tier => tier.accessTierShortName === AccessTierShortNames.Registered);
-    // sanity check
-    expect(rtOnly).toBeTruthy();
-    
-    cdrVersionStore.set({
-      ...cdrVersionTiersResponse,
-      tiers: [rtOnly]
-    });
-
-    const wrapper = component({accessTierShortNames: ['registered']});
-    expect(wrapper.find(CheckCircle).length).toBe(1);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access').length).toBe(0);
+    const wrapper = component({accessTiersInEnvironment: ['registered'], userAccessTiers: ['registered']});
+    expectAccessStatusRtOnly(wrapper, true);
   });
   
   it('Should only show the registered tier in environments without a controlled tier (user does not have access)', async() => {
-    const rtOnly: CdrVersionTier = cdrVersionTiersResponse.tiers.find(tier => tier.accessTierShortName === AccessTierShortNames.Registered);
-    // sanity check
-    expect(rtOnly).toBeTruthy();
-
-    cdrVersionStore.set({
-      ...cdrVersionTiersResponse,
-      tiers: [rtOnly]
-    });
-
-    const wrapper = component({accessTierShortNames: []});
-    expect(wrapper.find(CheckCircle).length).toBe(0);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access').length).toBe(1);
-    expect(TestHelpers.findNodesContainingText(wrapper, 'gain access to registered tier data').length).toBe(1);
+    const wrapper = component({accessTiersInEnvironment: ['registered'], userAccessTiers: []});
+    expectAccessStatusRtOnly(wrapper, false);
   });
 });
