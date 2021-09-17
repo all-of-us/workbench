35f427fe3c70cb57c7aca686ad25a102eac342b3
chavanr2019 <rohini.chavan@vumc.org>
2021-09-17 15:44:22 -0500

    institution admin test suite

--- e2e/tests/nightly/admin/institution-admin-ui.spec.ts
+++ e2e/tests/nightly/admin/institution-admin-ui.spec.ts
@@ -7,47 +7,40 @@ import InstitutionEditPage from 'app/page/admin-institution-edit-page';
 
 describe('Institution Admin', () => {
   const testInstituteName = 'Admin testing';
-  const emailAddress = 'testing@vumc.org';
+  //const emailAddress = 'testing@vumc.org';
 
   beforeEach(async () => {
     await signInWithAccessToken(page, config.ADMIN_TEST_ACCESS_TOKEN_FILE);
     await navigation.navMenu(page, NavLink.INSTITUTION_ADMIN);
   });
 
-  test('check Institution Admin UI and institute edit page', async () => {
+  test('check the Institution Admin page UI and test-Institute edit page', async () => {
     const institutionAdminPage = new InstitutionAdminPage(page);
     await institutionAdminPage.waitForLoad();
 
     //Verify table column names match.
-    const columns = [
-      'Institution Type',
-      'Data access tiers',
-      'User Email Instruction'
-    ];
+    const columns = ['Institution Type', 'Data access tiers', 'User Email Instruction'];
 
     const adminTable = new AdminTable(page);
     const columnNames = await adminTable.getColumnNames();
-    console.log(columnNames);
     expect(columnNames).toHaveLength(columns.length);
     expect(columnNames.sort()).toEqual(columns.sort());
 
     //click on the Institution Name link
     await institutionAdminPage.clickInstitutionNameLink(testInstituteName);
+    //navigate to institute edit page
     const institutionEditPage = new InstitutionEditPage(page);
     await institutionEditPage.waitForLoad();
+    // verify the fields
+    // await institutionEditPage.waitForSaveButton(false);
     const instituteName = await institutionEditPage.getInstituteNameValue();
     expect(instituteName).toBe(testInstituteName);
     expect(await institutionEditPage.getInstitutionTypeValue()).toBe('Other');
-    const acceptedCTAEmail = institutionEditPage.getCTAcceptedEmailInput();
-    // remove Email from  CTA Accepted Email Addresses textarea
-    await acceptedCTAEmail.clear();
-    // save button is disabled and error message is displayed
-    await institutionEditPage.waitForSaveButton(false);
-    // make a change, causing the Save button to activate
-    await acceptedCTAEmail.type(emailAddress);
-    // save button is enabled and no error message is displayed
-    await institutionEditPage.waitForSaveButton(true);
+    expect(await institutionEditPage.getRTeRAtoggle().isChecked()).toBe(true);
+    expect(await institutionEditPage.getCTEnabledtoggle().isChecked()).toBe(true);
     institutionEditPage.getCancelButton();
+    await institutionEditPage.clickCancelButton();
+    await institutionAdminPage.waitForLoad();
   });
 
   test.only('add new institute page UI check', async () => {
@@ -58,30 +51,23 @@ describe('Institution Admin', () => {
     await institutionEditPage.waitForLoad();
     await institutionEditPage.getAddNewInstituteFields();
     expect(await institutionEditPage.getRTeRAtoggle().isChecked()).toBe(true);
-    institutionEditPage.getRTDomainsDropdown();
+    institutionEditPage.getRTEmailDropdown();
     expect(await institutionEditPage.getCTeRAtoggle().isDisabled()).toBe(true);
     expect(await institutionEditPage.getCTEnabledtoggle().isChecked()).toBe(false);
     await institutionEditPage.clickCTEnabledtoggle();
     expect(await institutionEditPage.getCTEnabledtoggle().isChecked()).toBe(true);
-    //verify if the Select type dropdown and Accepted Email Domains textarea now display
-    await institutionEditPage.isDivWithLabelPresent("A user is considered part of this institution and eligible");
-    await institutionEditPage.isDivWithLabelPresent("Accepted Email Domains");
-    
-    // const ctAccessEmailOptions = institutionEditPage.getCTDomainsDropdown();
-    // const selectTypeOptions = await ctAccessEmailOptions.getAllOptionTexts();
-    // expect(selectTypeOptions).toEqual(expect.arrayContaining(['Email address is at one of the following domains:', 'Individual email address is listed below:']));
-
+    institutionEditPage.getCTEmailDropdown();
+    //verify if the Accepted Email Domains textarea now display
+    await institutionEditPage.isCTEmailDomainPresent();
     // select from dropdown option- Individual email address is listed below:
     await institutionEditPage.selectCTEmailOption('Individual email address is listed below:');
     //verify if the Accepted Email Addresses textarea now displays
-    await institutionEditPage.isDivWithLabelPresent("Accepted Email Addresses");
+    await institutionEditPage.isCTEmailAddressPresent();
+
     // verify that the ADD button is disabled
     expect(await institutionEditPage.getAddButton().isCursorNotAllowed()).toBe(true);
-  });
-
-  test.skip('verify user is not-bypassed and status is disable', async () => {
-    //verify that the User Lockout column is displaying DISABLE (default state)
-    //verify that the status column is displaying Active (default status)
-    //verify that the admin is able to update the free credits
+    institutionEditPage.getCancelButton();
+    await institutionEditPage.clickCancelButton();
+    await institutionAdminPage.waitForLoad();
   });
 });
