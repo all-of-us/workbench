3a529e835896338b0a3210cc10813e973e6e3c83
chavanr2019 <rohini.chavan@vumc.org>
2021-09-17 15:43:21 -0500

    institution edit page

--- e2e/app/page/admin-institution-edit-page.ts
+++ e2e/app/page/admin-institution-edit-page.ts
@@ -20,11 +20,10 @@ const DataTestIdAlias = {
 };
 
 export const LabelAlias = {
-  InstitutionType: 'Organization Type',
+  InstitutionType: 'Institution Type',
   SelectRTEmailType: 'Select type',
-  SelectCTEmailType: 'Email address is at one of the following domains:',
+  SelectCTEmailType: 'Email address is at one of the following domains:'
 };
-
 
 export const InstitutionTypeSelectValue = {
   Industry: 'Industry',
@@ -35,8 +34,8 @@ export const InstitutionTypeSelectValue = {
 };
 
 export const AcceptedAddressSelectValue = {
-  Domains : 'Email address is at one of the following domains:',
-  Individual : 'Individual email address is listed below:'
+  Domains: 'Email address is at one of the following domains:',
+  Individual: 'Individual email address is listed below:'
 };
 
 export const Field = {
@@ -60,10 +59,8 @@ export const Field = {
       name: LabelAlias.SelectRTEmailType,
       ancestorLevel: 2
     }
-  },
+  }
 };
-
-
 
 export default class InstitutionEditPage extends AuthenticatedPage {
   constructor(page: Page) {
@@ -78,8 +75,8 @@ export default class InstitutionEditPage extends AuthenticatedPage {
   getSaveButton(): Button {
     return Button.findByName(this.page, { name: LinkText.Save });
   }
-  
-  getCancelButton():Button {
+
+  getCancelButton(): Button {
     return Button.findByName(this.page, { name: LinkText.Cancel });
   }
 
@@ -101,117 +98,116 @@ export default class InstitutionEditPage extends AuthenticatedPage {
     return button;
   }
 
-  getHeaderText(instituteName: string): string{
-    return `//div/h3[text()= '${instituteName}']`;
-  }
-
   // get the institute name from the Institution Name input field
   async getInstituteNameValue(): Promise<string> {
     const instituteNameValue = this.getInstituteNameInput();
-      const instituteName = await instituteNameValue.getProperty<string>('value');
-      //const regex = new RegExp(/(?<=of ).*(?= limit)/);
-      return instituteName;
-    }
+    const instituteName = await instituteNameValue.getProperty<string>('value');
+    return instituteName;
+  }
 
-    // get the Institution Name input field
+  // get the Institution Name input field
   getInstituteNameInput(): Textbox {
     return Textbox.findByName(this.page, { dataTestId: DataTestIdAlias.displayName });
   }
 
-  // get the Institution Type dropdown field
+  // get User Email Instructions Textarea field
+  getInstructionsTextarea(): Textarea {
+    return Textarea.findByName(this.page, { id: DataTestIdAlias.userEmailInstructions });
+  }
+
+  // get the Institution Type dropdown
   getInstituteTypeDropdown(): SelectMenu {
     return SelectMenu.findByName(this.page, Field.InstitutionTypeSelect.textOption);
   }
 
-  // get User Email Instructions Textarea field
-  getInstructionsTextarea(): Textarea { 
-    return Textarea.findByName(this.page, { id: DataTestIdAlias.userEmailInstructions });
-    }
-
-  // select Institution Type from a dropdown
+  // select Institution Type from the dropdown
   async selectInstitutionType(selectTextValue: string): Promise<void> {
-    //const dropdown = SelectMenu.findByName(this.page, FieldSelector.InstitutionTypeSelect.textOption);
     const dropdown = this.getInstituteTypeDropdown();
-    return dropdown.select(selectTextValue);
+    return await dropdown.select(selectTextValue);
   }
 
-    // get the institute type value
-    async getInstitutionTypeValue(): Promise<string> {
-      const dropdown = SelectMenu.findByName(this.page, Field.InstitutionTypeSelect.textOption);
-      return dropdown.getSelectedValue();
-    }
+  // get the institute type value
+  async getInstitutionTypeValue(): Promise<string> {
+    const dropdown = this.getInstituteTypeDropdown();
+    return await dropdown.getSelectedValue();
+  }
 
-    // select Institution Type from a dropdown
-    // async selectAgreementTypeType(selectTextValue: string): Promise<void> {
-    //   const dropdown = SelectMenu.findByName(this.page, Field.AgreementTypeSelect.textOption);
-    //   return dropdown.select(selectTextValue);
-    // }
+  async getAddNewInstituteFields(): Promise<void> {
+    this.getInstituteNameInput();
+    this.getInstructionsTextarea();
+    this.getInstituteTypeDropdown();
+  }
 
-    async getAddNewInstituteFields(): Promise<void> {
-      this.getInstituteNameInput();
-      this.getInstructionsTextarea();
-      this.selectInstitutionType(InstitutionTypeSelectValue.Industry);
-    }
+  // edit the whitelist emails Controlled tier access
+  getCTAcceptedEmailInput(): Textarea {
+    return Textarea.findByName(this.page, { dataTestId: DataTestIdAlias.ctAcceptedEmailAddresses });
+  }
 
-    // edit the whitelist emails Controlled tier access
-    getCTAcceptedEmailInput(): Textarea {
-      return Textarea.findByName(this.page, { dataTestId: DataTestIdAlias.ctAcceptedEmailAddresses });
-    }
-  
-    // edit the whitelist emails Registered tier access
-    getRTAcceptedEmailInput(): Textarea {
-      return Textarea.findByName(this.page, { dataTestId: DataTestIdAlias.rtAcceptedEmailAddresses });
-    }
+  // edit the whitelist emails Registered tier access
+  getRTAcceptedEmailInput(): Textarea {
+    return Textarea.findByName(this.page, { dataTestId: DataTestIdAlias.rtAcceptedEmailAddresses });
+  }
 
-    // get the Institution's CT-eRA-account-required-switch and verify if enabled
-    getRTeRAtoggle(): Checkbox {
-      const xpath = '//div[@data-test-id="rt-era-required-switch"]/div/input[@type="checkbox"]';
-      return new Checkbox(this.page, xpath);
-    }
+  // get the Institution's CT-eRA-account-required-switch and verify if enabled
+  getRTeRAtoggle(): Checkbox {
+    const xpath = '//div[@data-test-id="rt-era-required-switch"]/div/input[@type="checkbox"]';
+    return new Checkbox(this.page, xpath);
+  }
 
-    // get the Institution's CT-eRA-account-required-switch and verify if disabled
-    getCTeRAtoggle(): Checkbox {
-      const xpath = '//div[@data-test-id="ct-era-required-switch"]/div/input[@type="checkbox"]';
-      return new Checkbox(this.page, xpath);
-    }
+  // get the Institution's CT-eRA-account-required-switch and verify if disabled
+  getCTeRAtoggle(): Checkbox {
+    const xpath = '//div[@data-test-id="ct-era-required-switch"]/div/input[@type="checkbox"]';
+    return new Checkbox(this.page, xpath);
+  }
 
-    // get the Institution's CT-enabled-switch to verify if it is enabled or disabled
-    getCTEnabledtoggle(): Checkbox {
-      const xpath = '//div[@data-test-id="ct-enabled-switch"]/div/input[@type="checkbox"]';
-      return new Checkbox(this.page, xpath);
-    }
+  // get the Institution's CT-enabled-switch to verify if it is enabled or disabled
+  getCTEnabledtoggle(): Checkbox {
+    const xpath = '//div[@data-test-id="ct-enabled-switch"]/div/input[@type="checkbox"]';
+    return new Checkbox(this.page, xpath);
+  }
 
-    // enable the CT toggle
-    async clickCTEnabledtoggle(): Promise<boolean> {
-      const ctEnabled = this.getCTEnabledtoggle();
-      await ctEnabled.toggle(true);
-      return true;
-    }
+  // enable the CT toggle
+  async clickCTEnabledtoggle(): Promise<boolean> {
+    const ctEnabled = this.getCTEnabledtoggle();
+    await ctEnabled.toggle(true);
+    return true;
+  }
 
-    getCTDomainsDropdown(): SelectMenu {
-      return SelectMenu.findByName(this.page, Field.AcceptedCTAddressSelect.textOption);
-    }
+  getCTEmailDropdown(): SelectMenu {
+    return SelectMenu.findByName(this.page, Field.AcceptedCTAddressSelect.textOption);
+  }
 
-    async selectCTEmailOption(value: string): Promise<void> {
-      const options =this.getCTDomainsDropdown();
-      await options.select(value);
-    }
+  async selectCTEmailOption(value: string): Promise<void> {
+    const options = this.getCTEmailDropdown();
+    await options.select(value);
+  }
 
-    async isDivCardContainerPresent(labelText: string): Promise<boolean> {
-      const selector = `//div[@data-test-id="ct-card-container"]//label[text()='${labelText}']`;
-      const elements = await page.$x(selector);
-      return elements.length > 0;
-    }
+  async isCTEmailAddressPresent(): Promise<void> {
+    const selector = '//div[@data-test-id="ctEmailAddress"]';
+    await page.$x(selector);
+  }
 
-    async isDivWithLabelPresent(labelText: string): Promise<boolean> {
-      return this.isDivCardContainerPresent(`${labelText}`);
-    }
+  async isCTEmailDomainPresent(): Promise<void> {
+    const selector = '//div[@data-test-id="ctEmailDomain"]';
+    await page.$x(selector);
+  }
 
-    getRTDomainsDropdown(): SelectMenu {
-      // const xpath = '//div//label[text()="Registered tier access"]/following-sibling::div//div[@role="button"]';
-      // const button = new Button(this.page, xpath);
-      // await button.click();
-      return SelectMenu.findByName(this.page, Field.AcceptedRTAddressSelect.textOption);
-      //return new SelectMenu(this.page);
-    }
+  getRTEmailDropdown(): SelectMenu {
+    return SelectMenu.findByName(this.page, Field.AcceptedRTAddressSelect.textOption);
+  }
+
+  async getCTEmailAcceptedValue(): Promise<string> {
+    const dropdown = this.getCTEmailDropdown();
+    return await dropdown.getSelectedValue();
+  }
+
+  async getRTEmailAcceptedValue(): Promise<string> {
+    const dropdown = this.getRTEmailDropdown();
+    return await dropdown.getSelectedValue();
+  }
+
+  async clickCancelButton(): Promise<void> {
+    const button = this.getCancelButton();
+    await button.click();
+  }
 }
