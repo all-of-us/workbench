297b77dd2d142d71081cc0582a86fdda8c920bb0
CH Albach <calbach@users.noreply.github.com>
2021-09-17 09:46:59 -0700

    Update WorkbenchConfig.java

--- api/src/main/java/org/pmiops/workbench/config/WorkbenchConfig.java
+++ api/src/main/java/org/pmiops/workbench/config/WorkbenchConfig.java
@@ -256,8 +256,6 @@ public class WorkbenchConfig {
     public boolean enableFireCloudV2Billing;
     // If true, use the new rewrite version of access module.
     public boolean enableAccessModuleRewrite;
-    // TODO(RW-6877): remove
-    @Deprecated public boolean enableUnifiedUserAccessCron;
     // If true, cohort and concept set will show source domains and standard domains options
     public boolean enableStandardSourceDomains;
     // If true, the backend and UI will support gpu for standard vm
