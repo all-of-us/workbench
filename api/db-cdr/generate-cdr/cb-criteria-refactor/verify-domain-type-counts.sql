c58eb66a78c9a05709eac695d44a50f49fd8cb2c
Chenchal Subraveti <c.subraveti@vumc.org>
2021-09-21 14:03:10 -0500

    Updated for PPI-SURVEYS

--- api/db-cdr/generate-cdr/cb-criteria-refactor/verify-domain-type-counts.sql
+++ api/db-cdr/generate-cdr/cb-criteria-refactor/verify-domain-type-counts.sql
@@ -47,19 +47,31 @@ select * from (
               )
 order by domain_id, type, is_group, name, run_type;
 
--- All 'type' -- in progress... what to do with 'name'
+-- PPI - SURVEYS - SQL-ORDER = 3
 select * from (
-                  select '01-sequential' run_type, type, is_group, 'cpt-name' name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` group by is_group,name,type
+                  select '01-sequential' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
+                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
-                  select '02-parallel' run_type, type, is_group, 'cpt-name' name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` group by is_group,name,type
+                  select '02-parallel' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
+                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
-                  select '03-parallel-multi' run_type, type, is_group, 'cpt-name' name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` group by is_group,name,type
+                  select '03-parallel-multi' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
+                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
-                  select '10-original' run_type, type, is_group, 'cpt-name' name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria`  group by is_group,name,type
+                  select '10-original' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
-                  select '20-src' run_type, type, is_group, 'cpt-name' name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` group by is_group,name,type
+                  select '20-std-src' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
               )
-order by 2,3,4,1;
+order by domain_id, type, is_group, name, run_type;
+
+
 --==========truncate...
 
 -- truncate table `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria`;
