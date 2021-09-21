909f971f591208a65296d6ed829ab71df5b567f8
Chenchal Subraveti <c.subraveti@vumc.org>
2021-09-21 14:37:38 -0500

    Updated for PM - CONCEPT-SETS

---
+++ api/db-cdr/generate-cdr/cb-criteria-refactor/make-cb-criteria-04-pm-concept-set.sh
@@ -0,0 +1,112 @@
+#!/bin/bash
+# set -ex
+# do not output cmd-line for now
+set -e
+SQL_FOR='PHYSICAL MEASUREMENTS - CONCEPT SET'
+SQL_SCRIPT_ORDER=4
+TBL_CBC='cb_criteria'
+####### common block for all make-cb-criteria-dd-*.sh scripts ###########
+function createTmpTable(){
+  local tmpTbl="temp_"$1"_"$SQL_SCRIPT_ORDER
+  res=$(bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+    "CREATE OR REPLACE TABLE \`$BQ_PROJECT.$BQ_DATASET.$tmpTbl\` AS
+      SELECT * FROM \`$BQ_PROJECT.$BQ_DATASET.$1\` LIMIT 0")
+  echo $res >&2
+  echo "$tmpTbl"
+}
+function cpToMain(){
+  local tbl_to=`echo "$1" | perl -pe 's/(temp_)|(_\d+)//g'`
+  bq cp --append_table=true --quiet --project_id=$BQ_PROJECT \
+     $BQ_DATASET.$1 $BQ_DATASET.$tbl_to
+}
+export BQ_PROJECT=$1        # project
+export BQ_DATASET=$2        # dataset
+# export DATA_BROWSER=$3      # data browser flag
+RUN_PARALLEL=$3
+if [[ "$RUN_PARALLEL" == "par" ]]; then
+  echo "Running in parallel mode - " "$SQL_SCRIPT_ORDER - $SQL_FOR"
+  STEP=$SQL_SCRIPT_ORDER
+  CB_CRITERIA_START_ID=$[$STEP*10**9] # 3  billion
+  CB_CRITERIA_END_ID=$[$[STEP+1]*10**9] # 4  billion
+elif [[ "$RUN_PARALLEL" == "seq" ]]; then
+    echo "Running in sequential mode - "  "$SQL_SCRIPT_ORDER - $SQL_FOR"
+    CB_CRITERIA_START_ID=0
+    CB_CRITERIA_END_ID=$[50*10**9] # max(id) from cb_criteria
+elif [[ "$RUN_PARALLEL" == "mult" ]]; then
+    echo "Running in parallel and Multitable mode - " "$SQL_SCRIPT_ORDER - $SQL_FOR"
+    STEP=$SQL_SCRIPT_ORDER
+    CB_CRITERIA_START_ID=$[$STEP*10**9] # 3  billion
+    CB_CRITERIA_END_ID=$[$[STEP+1]*10**9] # 4  billion
+    echo "Creating temp table for $TBL_CBC"
+    TBL_CBC=$(createTmpTable $TBL_CBC)
+fi
+####### end common block ###########
+
+# make-cb-criteria-04-pm-concept-set.sh
+#892 - #890 : make-bq-criteria-tables.sh
+# ---------ORDER - 4 - PHYSICAL MEASUREMENTS - CONCEPT SET---------
+# ORDER 4: #891 - #947: PHYSICAL MEASUREMENTS - CONCEPT SET
+# cb_criteria: Uses : cb_criteria, concept, measurement
+################################################
+# PHYSICAL MEASUREMENTS - CONCEPT SET
+################################################
+echo "PHYSICAL MEASUREMENTS - CONCEPT SET"
+bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+"INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria\`
+    (
+          id
+        , parent_id
+        , domain_id
+        , is_standard
+        , type
+        , concept_id
+        , code
+        , name
+        , rollup_count
+        , item_count
+        , est_count
+        , is_group
+        , is_selectable
+        , has_attribute
+        , has_hierarchy
+        , path
+    )
+SELECT
+      ROW_NUMBER() OVER (ORDER BY concept_name)
+       + (SELECT COALESCE(MAX(id),$CB_CRITERIA_START_ID) FROM \`$BQ_PROJECT.$BQ_DATASET.cb_criteria\` WHERE id > $CB_CRITERIA_START_ID AND id < $CB_CRITERIA_END_ID ) AS id
+    , -1
+    , 'PHYSICAL_MEASUREMENT_CSS'
+    , 0
+    , vocabulary_id as type
+    , concept_id
+    , concept_code as code
+    , concept_name as name
+    , 0 as rollup_count
+    , b.cnt as item_count
+    , b.cnt as est_count
+    , 0
+    , 1
+    , 0
+    , 0
+    , CAST(ROW_NUMBER() OVER (ORDER BY concept_name) +
+      (SELECT COALESCE(MAX(id),$CB_CRITERIA_START_ID) FROM \`$BQ_PROJECT.$BQ_DATASET.cb_criteria\` where id > $CB_CRITERIA_START_ID AND id < $CB_CRITERIA_END_ID) as STRING)
+FROM
+    (
+        SELECT *
+        FROM \`$BQ_PROJECT.$BQ_DATASET.concept\`
+        WHERE vocabulary_id = 'PPI'
+        and concept_class_id = 'Clinical Observation'
+        and domain_id = 'Measurement'
+    ) a
+JOIN
+    (   --- get the count of distinct patients coded with each concept
+        SELECT measurement_source_concept_id , COUNT(DISTINCT person_id) cnt
+        FROM \`$BQ_PROJECT.$BQ_DATASET.measurement\`
+        GROUP BY 1
+    ) b on a.concept_id = b.measurement_source_concept_id"
+
+wait $!
+## copy temp tables back to main tables, and delete temp?
+if [[ "$RUN_PARALLEL" == "mult" ]]; then
+  cpToMain "$TBL_CBC" &
+fi
--- api/db-cdr/generate-cdr/cb-criteria-refactor/verify-domain-type-counts.sql
+++ api/db-cdr/generate-cdr/cb-criteria-refactor/verify-domain-type-counts.sql
@@ -2,24 +2,24 @@
 -- CPT4 Counts - SQL-ORDER = 1
 select * from (
                   select '01-sequential' run_type, domain_id, type, is_group, 'cpt4-name' name, count(is_group) sum_grp_count
-                    , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                    from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where type='CPT4' group by 4,5,3,2
+                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where domain_id='PROCEDURE' and type='CPT4' group by 4,5,3,2
                   union all
                   select '02-parallel' run_type, domain_id, type, is_group, 'cpt4-name' name, count(is_group) sum_grp_count
-                    , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                    from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where type='CPT4' group by 4,5,3,2
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where domain_id='PROCEDURE' and type='CPT4' group by 4,5,3,2
                   union all
                   select '03-parallel-multi' run_type, domain_id, type, is_group, 'cpt4-name' name, count(is_group) sum_grp_count
-                    , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                    from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where type='CPT4' group by 4,5,3,2
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where domain_id='PROCEDURE' and type='CPT4' group by 4,5,3,2
                   union all
                   select '10-original' run_type, domain_id, type, is_group, 'cpt4-name' name, count(is_group) sum_grp_count
-                    , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                    from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where type='CPT4' group by 4,5,3,2
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where domain_id='PROCEDURE' and type='CPT4' group by 4,5,3,2
                   union all
                   select '20-std-src' run_type, domain_id, type, is_group, 'cpt4-name' name, count(is_group) sum_grp_count
-                    , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                    from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where type='CPT4' group by 4,5,3,2
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where domain_id='PROCEDURE' and type='CPT4' group by 4,5,3,2
               )
 order by domain_id, type, is_group, name, run_type;
 
@@ -27,23 +27,23 @@ order by domain_id, type, is_group, name, run_type;
 select * from (
                   select '01-sequential' run_type, domain_id, type, is_group, name, count(is_group) sum_grp_count
                        , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                  from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where type='PPI' group by 4,5,3,2
+                  from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT' and type='PPI' group by 4,5,3,2
                   union all
                   select '02-parallel' run_type, domain_id, type, is_group, name, count(is_group) sum_grp_count
                           , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                  from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where type='PPI' group by 4,5,3,2
+                  from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT' and type='PPI' group by 4,5,3,2
                   union all
                   select '03-parallel-multi' run_type, domain_id, type, is_group, name, count(is_group) sum_grp_count
                           , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                  from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where type='PPI' group by 4,5,3,2
+                  from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT' and type='PPI' group by 4,5,3,2
                   union all
                   select '10-original' run_type, domain_id, type, is_group, name, count(is_group) sum_grp_count
                           , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                  from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where type='PPI' group by 4,5,3,2
+                  from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT' and type='PPI' group by 4,5,3,2
                   union all
                   select '20-std-src' run_type, domain_id, type, is_group, name, count(is_group) sum_grp_count
                           , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
-                  from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where type='PPI' group by 4,5,3,2
+                  from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT' and type='PPI' group by 4,5,3,2
               )
 order by domain_id, type, is_group, name, run_type;
 
@@ -54,11 +54,11 @@ select * from (
                   from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
                   select '02-parallel' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
-                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
                   from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
                   select '03-parallel-multi' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
-                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
                   from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where domain_id='SURVEY' and type='PPI' group by 4,5,3,2
                   union all
                   select '10-original' run_type, domain_id, type, is_group, 'survey-name' name, count(is_group) sum_grp_count
@@ -71,20 +71,47 @@ select * from (
               )
 order by domain_id, type, is_group, name, run_type;
 
+-- PM - CONCEPT SETS - SQL-ORDER = 4
+select * from (
+                  select '01-sequential' run_type, domain_id, type, is_group, 'pm-concept-name' name, count(is_group) sum_grp_count
+                       , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT_CSS' and type='PPI' group by 4,5,3,2
+                  union all
+                  select '02-parallel' run_type, domain_id, type, is_group, 'pm-concept-name' name, count(is_group) sum_grp_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT_CSS' and type='PPI' group by 4,5,3,2
+                  union all
+                  select '03-parallel-multi' run_type, domain_id, type, is_group, 'pm-concept-name' name, count(is_group) sum_grp_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT_CSS' and type='PPI' group by 4,5,3,2
+                  union all
+                  select '10-original' run_type, domain_id, type, is_group, 'pm-concept-name' name, count(is_group) sum_grp_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT_CSS' and type='PPI' group by 4,5,3,2
+                  union all
+                  select '20-std-src' run_type, domain_id, type, is_group, 'pm-concept-name' name, count(is_group) sum_grp_count
+                          , sum(item_count) sum_item_count, sum(rollup_count) sum_rollup_count, sum(est_count) sum_est_count
+                  from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where domain_id='PHYSICAL_MEASUREMENT_CSS' and type='PPI' group by 4,5,3,2
+              )
+order by domain_id, type, is_group, name, run_type;
 
 --==========truncate...
 
 -- truncate table `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria`;
 -- truncate table `all-of-us-ehr-dev.ChenchalDummySeq.prep_cpt_ancestor`;
---
+-- truncate table `all-of-us-ehr-dev.ChenchalDummySeq.prep_concept_ancestor`;
+
 -- truncate table `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria`;
 -- truncate table `all-of-us-ehr-dev.ChenchalDummyPar.prep_cpt_ancestor`;
---
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyPar.prep_concept_ancestor`;
+
 -- truncate table `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria`;
 -- truncate table `all-of-us-ehr-dev.ChenchalDummyMult.prep_cpt_ancestor`;
---
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyMult.prep_concept_ancestor`;
+
 -- truncate table `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria`;
 -- truncate table `all-of-us-ehr-dev.ChenchalDummyOri.prep_cpt_ancestor`;
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyOri.prep_concept_ancestor`;
 
 
 
