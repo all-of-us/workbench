bec4cd654c08716540907913560f3a4f38f5e049
Chenchal Subraveti <c.subraveti@vumc.org>
2021-09-21 10:21:37 -0500

    Updated with verifying counts for PPI and CPT4...

--- api/db-cdr/generate-cdr/cb-criteria-refactor/verify-domain-type-counts.sql
+++ api/db-cdr/generate-cdr/cb-criteria-refactor/verify-domain-type-counts.sql
@@ -1,52 +1,47 @@
--- Row counts by domain_id and type
+-- ========== Below are SQL snippets for verifying counts for different domains/vocabularies/ SQL-BLOCKS ==============
+-- CPT4 Counts - SQL-ORDER = 1
 select * from (
-                  select distinct '1-sequential' as run_type, domain_id, type, subtype, coalesce(count(*),0) as row_count from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` group by 2,3,4
+                  select '01-sequential' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where type='CPT4' group by is_group,name, type
                   union all
-                  select distinct '2-parallel' as run_type, domain_id, type, subtype, coalesce(count(*),0) as row_count from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` group by 2,3,4
+                  select '02-parallel' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where type='CPT4' group by is_group,name, type
                   union all
-                  select distinct '3-parallel-multi' as run_type, domain_id, type, subtype, coalesce(count(*),0) as row_count from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` group by 2,3,4
+                  select '03-parallel-multi' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where type='CPT4' group by is_group,name, type
                   union all
-                  select distinct '9-original' as run_type, domain_id, type, subtype, coalesce(count(*),0) as row_count from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria`  group by 2,3,4
+                  select '10-original' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where type='CPT4' group by is_group, name, type
+                  union all
+                  select '20-src' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where type='CPT4' group by is_group, name, type
               )
-where type='CPT4' and domain_id='PROCEDURE' and subtype is null
-order by 1 desc;
+order by 2,3,4,1;
 
--- Item Counts
+-- PPI - Physical Measurement - SQL-ORDER = 2
 select * from (
-                  select distinct '1-sequential' as run_type, domain_id, type, id, coalesce(item_count,0) as item_count from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` --where rollup_count > 0
+                  select '01-sequential' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` where type='PPI' group by is_group,name, type
                   union all
-                  select distinct '2-parallel' as run_type, domain_id, type, id-1000000000, coalesce(item_count,0) as item_count from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` --where rollup_count > 0
+                  select '02-parallel' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` where type='PPI' group by is_group,name, type
                   union all
-                  select distinct '3-parallel-multi' as run_type, domain_id, type, id-1000000000, coalesce(item_count,0) as item_count from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` --where rollup_count > 0
+                  select '03-parallel-multi' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` where type='PPI' group by is_group,name, type
                   union all
-                  select distinct '9-original' as run_type, domain_id, type, id, coalesce(item_count,0) as item_count from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` --where rollup_count > 0
+                  select '10-original' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` where type='PPI' group by is_group, name, type
+                  union all
+                  select '20-std-src' as run_type, type, is_group, 'cpt-name' as name, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` where type='PPI' group by is_group, name, type
               )
-where type='CPT4' and domain_id='PROCEDURE' and item_count >0
-order by 4, 1 desc;
-
--- Rollup Counts
-select * from (
-  select distinct '1-sequential' as run_type, domain_id, type, id, coalesce(rollup_count,0) as rollup_count from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` --where rollup_count > 0
-  union all
-  select distinct '2-parallel' as run_type, domain_id, type, id-1000000000, coalesce(rollup_count,0) as rollup_count from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` --where rollup_count > 0
-  union all
-  select distinct '3-parallel-multi' as run_type, domain_id, type, id-1000000000, coalesce(rollup_count,0) as rollup_count from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` --where rollup_count > 0
-  union all
-  select distinct '9-original' as run_type, domain_id, type, id, coalesce(rollup_count,0) as rollup_count from `all-of-us-ehr-dev.ChenchalDummySrc.cb_criteria` --where rollup_count > 0
-)
-where type='CPT4' and domain_id='PROCEDURE' and rollup_count >0
-order by 4, 1 desc;
+order by 2,3,4,1;
 
 
+--==========truncate...
 
-select * from (
-    select '1-sequential' as run_type, is_group, count(is_group) group_count, sum(item_count) sum_item_count,sum(rollup_count) sum_rollup_count,sum(est_count) sum_est_count from `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria` group by 2
-    union all
-    select '2-parallel' as run_type, is_group, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria` group by 2
-    union all
-    select '3-parallel-multi' as run_type, is_group, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria` group by 2
-    union all
-    select '9-original' as run_type, is_group, count(is_group), sum(item_count),sum(rollup_count),sum(est_count) from `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria` group by 2
-)
-order by 2,1
+-- truncate table `all-of-us-ehr-dev.ChenchalDummySeq.cb_criteria`;
+-- truncate table `all-of-us-ehr-dev.ChenchalDummySeq.prep_cpt_ancestor`;
+--
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyPar.cb_criteria`;
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyPar.prep_cpt_ancestor`;
+--
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyMult.cb_criteria`;
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyMult.prep_cpt_ancestor`;
+--
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyOri.cb_criteria`;
+-- truncate table `all-of-us-ehr-dev.ChenchalDummyOri.prep_cpt_ancestor`;
+
+
+
 
