aed435796bfe46f4e45cffefd30f8b77b935fbaf
CH Albach <calbach@google.com>
2021-09-27 18:04:56 -0700

    [RW-6752][risk=no] Add logging for table-driven profile test

--- e2e/tests/profile/profile-page.spec.ts
+++ e2e/tests/profile/profile-page.spec.ts
@@ -145,6 +145,10 @@ describe('Profile', () => {
     ]) {
       const originalValue = await element.getValue();
 
+      console.log(
+        `missingness test '${originalValue}' -> '${testText}' -> (empty), for element: `,
+        element.getXpath());
+
       // make a change, causing the Save button to activate
       await element.type(testText);
 
