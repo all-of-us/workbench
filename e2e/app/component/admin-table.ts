a03f871c6b1f8bef0d70ff81cb4deea3acaf9c6a
chavanr2019 <rohini.chavan@vumc.org>
2021-09-28 16:02:38 -0500

    created a function to get the names of all the columns

--- e2e/app/component/admin-table.ts
+++ e2e/app/component/admin-table.ts
@@ -6,6 +6,7 @@ const defaultXpath = '//*[contains(concat(normalize-space(@class), " "), "p-data
 
 export default class adminTable extends Table {
   protected userLinkElement: ElementHandle;
+  columns1: any;
 
   constructor(page: Page, xpath: string = defaultXpath, container?: Container) {
     super(page, xpath, container);
@@ -72,14 +73,35 @@ export default class adminTable extends Table {
    * Finds table column names. Returns in array of string.
    * @returns {Array<string>}
    */
-  async getColumnNames(): Promise<string[]> {
+  //  async getAllColumnNames(): Promise<string[]> {
+  //   const colnames = await this.getUnfrozenColNames();
+  //   return colnames;
+  // }
+
+  async getAllColumnNames(): Promise<string[]> {
+    const columns1: Array<string> = await this.getFrozenColNames();
+    console.log(`columns1: ${columns1}`);
+    const columns2: Array<string> = await this.getUnfrozenColNames();
+    console.log(`columns2: ${columns2}`);
+    const allColumnNames = columns1.concat(columns2);
+    console.log(`allColumnNames: ${allColumnNames}`);
+    return allColumnNames;
+  }
+
+  getUnfrozenColNames(): Promise<string[]> {
     const headerTable = this.getHeaderTable();
     return headerTable.getColumnNames();
   }
 
+  getFrozenColNames(): Promise<string[]> {
+    const headerTable = this.getFrozenHeader();
+    return headerTable.getColumnNames();
+  }
+
   async getNameColindex(): Promise<number> {
-    const dataTable = this.getFrozenHeader();
-    const columnName = await dataTable.getColumnNames();
+    // const dataTable = this.getFrozenHeader();
+    // const columnName = await dataTable.getColumnNames();
+    const columnName = await this.getFrozenColNames();
     const colIndexNum = columnName.indexOf('Name');
     return colIndexNum + 1;
   }
