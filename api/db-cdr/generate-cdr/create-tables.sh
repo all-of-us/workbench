b8502d917c029c5645dd23f4dbfeb2f4b772b1dc
Brian Freeman <brian.d.freeman@gmail.com>
2021-09-30 14:50:13 -0500

    [RW-7304][risk=no] Testing out building of survey trees (#5684)
    * adding new shell script
    
    * adding new project rb command to insert prep_survey into cb_criteria
    
    * RW-7304 renaming things
    
    * RW-7304 adding new script
    
    * RW-7304 making sh executable
    
    * RW-7304 codacy

---
+++ api/db-cdr/generate-cdr/create-tables.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+# This script removes/creates all CDR indices specific tables.
+
+set -e
+
+export BQ_PROJECT=$1         # CDR project
+export BQ_DATASET=$2         # CDR dataset
+
+schema_path=generate-cdr/bq-schemas
+create_tables=(prep_survey cb_criteria)
+
+for t in "${create_tables[@]}"
+do
+    bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.$t"
+    bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$t.json" "$BQ_DATASET.$t"
+done
