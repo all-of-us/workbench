a0478d0e897efa86be5156a8d56aa9dfa59f73d1
yonghao <yyhao1@gmail.com>
2021-09-30 15:27:50 -0400

    fixing e2e test

--- ui/src/app/utils/workbench-gapi-client.ts
+++ ui/src/app/utils/workbench-gapi-client.ts
@@ -12,13 +12,19 @@ export async function getBillingAccountInfo(googleProject: string) {
   return new Promise<GoogleBillingAccountInfo>((resolve) => {
     gapi.load('client', () => {
       if(isTestAccessTokenActive()) {
-        gapi.client.setToken(window.localStorage.getItem(LOCAL_STORAGE_KEY_TEST_ACCESS_TOKEN))
+        gapi.client.load('cloudbilling', 'v1', () => {
+          gapi.client.setToken(window.localStorage.getItem(LOCAL_STORAGE_KEY_TEST_ACCESS_TOKEN))
+          gapi.client.cloudbilling.projects.getBillingInfo({
+            name: 'projects/' + googleProject
+          }).then(response => resolve(JSON.parse(response.body)));
+        });
+      } else {
+        gapi.client.load('cloudbilling', 'v1', () => {
+          gapi.client.cloudbilling.projects.getBillingInfo({
+            name: 'projects/' + googleProject
+          }).then(response => resolve(JSON.parse(response.body)));
+        });
       }
-      gapi.client.load('cloudbilling', 'v1', () => {
-        gapi.client.cloudbilling.projects.getBillingInfo({
-          name: 'projects/' + googleProject
-        }).then(response => resolve(JSON.parse(response.body)));
-      });
     });
   });
 }
