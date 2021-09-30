82b3e8f9a68502e638c470cbd41ab2d38c250a4f
yonghao <yyhao1@gmail.com>
2021-09-30 19:23:38 -0400

    update comment

--- ui/src/app/routing/guards.tsx
+++ ui/src/app/routing/guards.tsx
@@ -12,7 +12,7 @@ export const signInGuard: Guard = {
 };
 
 export const disabledGuard = (userDisabled: boolean): Guard => ({
-  // Show disable screen when user account is disabled or remoevd from registered tier by institution admin.
+  // Show disable screen when user account is disabled or remoevd from institution registered tier requirement.
   allowed: (): boolean => (!userDisabled && eligibaleForRegisteredForTier(profileStore.get().profile.tierEligibilities)),
   redirectPath: '/user-disabled'
 });
