988c97ba0f460565e6915ec34b80ab898890a159
yonghao <yyhao1@gmail.com>
2021-09-30 15:56:02 -0400

    lint

--- ui/src/app/utils/workbench-gapi-client.ts
+++ ui/src/app/utils/workbench-gapi-client.ts
@@ -13,18 +13,16 @@ export async function getBillingAccountInfo(googleProject: string) {
     gapi.load('client', () => {
       if(isTestAccessTokenActive()) {
         gapi.client.load('cloudbilling', 'v1', () => {
-          gapi.client.setToken(window.localStorage.getItem(LOCAL_STORAGE_KEY_TEST_ACCESS_TOKEN))
-          gapi.client.cloudbilling.projects.getBillingInfo({
-            name: 'projects/' + googleProject
-          }).then(response => resolve(JSON.parse(response.body)));
-        });
-      } else {
-        gapi.client.load('cloudbilling', 'v1', () => {
-          gapi.client.cloudbilling.projects.getBillingInfo({
-            name: 'projects/' + googleProject
-          }).then(response => resolve(JSON.parse(response.body)));
+          gapi.client.setToken({
+            access_token: window.localStorage.getItem(LOCAL_STORAGE_KEY_TEST_ACCESS_TOKEN)
+          });
         });
       }
+      gapi.client.load('cloudbilling', 'v1', () => {
+        gapi.client.cloudbilling.projects.getBillingInfo({
+          name: 'projects/' + googleProject
+        }).then(response => resolve(JSON.parse(response.body)));
+      });
     });
   });
 }
