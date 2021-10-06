68ff02a5f58f41c48c2f757dd4360e20e70a8eed
CH Albach <calbach@google.com>
2021-10-05 17:42:46 -0700

    [RW-2408][risk=no] Add unit test to validate all config_*.json

--- api/src/test/java/org/pmiops/workbench/config/WorkbenchConfigTest.java
+++ api/src/test/java/org/pmiops/workbench/config/WorkbenchConfigTest.java
@@ -6,12 +6,20 @@ import com.google.gson.Gson;
 import java.io.FileNotFoundException;
 import java.io.FileReader;
 import org.junit.jupiter.api.Test;
-import org.springframework.context.annotation.Bean;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.ValueSource;
 
 public class WorkbenchConfigTest {
 
+  @ParameterizedTest(name = "config_{0}.json is valid")
+  @ValueSource(strings = {"local", "test", "perf", "staging", "stable", "preprod", "prod"})
+  public void testConfigFilesAreValid(String env) throws Exception {
+    String path = String.format("../api/config/config_%s.json", env);
+    assertThat(FileConfigs.loadConfig(path, WorkbenchConfig.class)).isNotNull();
+  }
+
   @Test
-  public void testUnsafeEndpointsDisabledInProd() throws FileNotFoundException {
+  public void testUnsafeEndpointsDisabledInProd() throws Exception {
     WorkbenchConfig workbenchConfig = getConfigFromFile("../api/config/config_prod.json");
     assertThat(workbenchConfig.access.unsafeAllowSelfBypass).isFalse();
     assertThat(workbenchConfig.access.unsafeAllowUserCreationFromGSuiteData).isFalse();
@@ -19,7 +27,7 @@ public class WorkbenchConfigTest {
   }
 
   @Test
-  public void testUnsafeEndpointsDisabledInStable() throws FileNotFoundException {
+  public void testUnsafeEndpointsDisabledInStable() throws Exception {
     WorkbenchConfig workbenchConfig = getConfigFromFile("../api/config/config_stable.json");
     assertThat(workbenchConfig.access.unsafeAllowSelfBypass).isFalse();
     assertThat(workbenchConfig.access.unsafeAllowUserCreationFromGSuiteData).isFalse();
@@ -27,14 +35,13 @@ public class WorkbenchConfigTest {
   }
 
   @Test
-  public void testUnsafeEndpointsDisabledInStaging() throws FileNotFoundException {
+  public void testUnsafeEndpointsDisabledInStaging() throws Exception {
     WorkbenchConfig workbenchConfig = getConfigFromFile("../api/config/config_staging.json");
     assertThat(workbenchConfig.access.unsafeAllowSelfBypass).isFalse();
     assertThat(workbenchConfig.access.unsafeAllowUserCreationFromGSuiteData).isFalse();
     assertThat(workbenchConfig.featureFlags.unsafeAllowDeleteUser).isFalse();
   }
 
-  @Bean
   private WorkbenchConfig getConfigFromFile(String path) throws FileNotFoundException {
     return new Gson().fromJson(new FileReader(path), WorkbenchConfig.class);
   }
