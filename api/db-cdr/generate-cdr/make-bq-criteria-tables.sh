69a80022b4fe128c4f26bfddc64a33b3ecaeda9c
Zahra Rasuli <zahrarasuli@VICTRs-MacBook-Pro.local>
2021-10-12 15:15:42 -0500

    added insert statement to add in max/min for Select a value answers

--- api/db-cdr/generate-cdr/make-bq-criteria-tables.sh
+++ api/db-cdr/generate-cdr/make-bq-criteria-tables.sh
@@ -5091,7 +5091,6 @@ FROM
         GROUP BY 1,2,3
     ) a"
 
-
 # set has_attribute=1 for any observation criteria that has data in cb_criteria_attribute
 echo "CB_CRITERIA - update has_attribute"
 bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
@@ -5302,7 +5301,7 @@ FROM
 ################################################
 # CB_CRITERIA_ATTRIBUTE
 ################################################
-# we hard code the min/max for these PPI questions because we can't get the information programmatically
+# inserting min/max for Select a value answers
 echo "CB_CRITERIA_ATTRIBUTE - PPI SURVEY - add values for certain questions"
 bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_attribute\`
@@ -5314,68 +5313,52 @@ bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
         , type
         , est_count
     )
-VALUES
-    (1, 1585889, 0, 'MIN', 'NUM', '0'),
-    (2, 1585889, 0, 'MAX', 'NUM', '20'),
-    (3, 1585890, 0, 'MIN', 'NUM', '0'),
-    (4, 1585890, 0, 'MAX', 'NUM', '20'),
-    (5, 1585864, 0, 'MIN', 'NUM', '0'),
-    (6, 1585864, 0, 'MAX', 'NUM', '99'),
-    (7, 1585870, 0, 'MIN', 'NUM', '0'),
-    (8, 1585870, 0, 'MAX', 'NUM', '99'),
-    (9, 1585873, 0, 'MIN', 'NUM', '0'),
-    (10, 1585873, 0, 'MAX', 'NUM', '99'),
-    (11, 1586159, 0, 'MIN', 'NUM', '0'),
-    (12, 1586159, 0, 'MAX', 'NUM', '99'),
-    (13, 1586162, 0, 'MIN', 'NUM', '0'),
-    (14, 1586162, 0, 'MAX', 'NUM', '99'),
-    (15, 1585795, 0, 'MIN', 'NUM', '0'),
-    (16, 1585795, 0, 'MAX', 'NUM', '99'),
-    (17, 1585802, 0, 'MIN', 'NUM', '0'),
-    (18, 1585802, 0, 'MAX', 'NUM', '99'),
-    (19, 1585820, 0, 'MIN', 'NUM', '0'),
-    (20, 1585820, 0, 'MAX', 'NUM', '255'),
-    (21, 1333015, 0, 'MIN', 'NUM', '0'),
-    (22, 1333015, 0, 'MAX', 'NUM', '20'),
-    (23, 1333023, 0, 'MIN', 'NUM', '1'),
-    (24, 1333023, 0, 'MAX', 'NUM', '20'),
-    (25, 715717, 0, 'MIN', 'NUM', '0'),
-    (26, 715717, 0, 'MAX', 'NUM', '24'),
-    (27, 903642, 0, 'MIN', 'NUM', '0'),
-    (28, 903642, 0, 'MAX', 'NUM', '1440'),
-    (29, 715721, 0, 'MIN', 'NUM', '1'),
-    (30, 715721, 0, 'MAX', 'NUM', '99'),
-    (31, 715720, 0, 'MIN', 'NUM', '1'),
-    (32, 715720, 0, 'MAX', 'NUM', '12'),
-    (33, 715719, 0, 'MIN', 'NUM', '1'),
-    (34, 715719, 0, 'MAX', 'NUM', '52'),
-    (35, 1332870, 0, 'MIN', 'NUM', '1'),
-    (36, 1332870, 0, 'MAX', 'NUM', '7'),
-    (37, 903633, 0, 'MIN', 'NUM', '0'),
-    (38, 903633, 0, 'MAX', 'NUM', '24'),
-    (39, 715712, 0, 'MIN', 'NUM', '0'),
-    (40, 715712, 0, 'MAX', 'NUM', '1440'),
-    (41, 1332871, 0, 'MIN', 'NUM', '1'),
-    (42, 1332871, 0, 'MAX', 'NUM', '7'),
-    (43, 903634, 0, 'MIN', 'NUM', '0'),
-    (44, 903634, 0, 'MAX', 'NUM', '24'),
-    (45, 715715, 0, 'MIN', 'NUM', '0'),
-    (46, 715715, 0, 'MAX', 'NUM', '1440'),
-    (47, 1332872, 0, 'MIN', 'NUM', '1'),
-    (48, 1332872, 0, 'MAX', 'NUM', '7'),
-    (49, 903635, 0, 'MIN', 'NUM', '0'),
-    (50, 903635, 0, 'MAX', 'NUM', '7'),
-    (51, 715716, 0, 'MIN', 'NUM', '0'),
-    (52, 715716, 0, 'MAX', 'NUM', '1440'),
-    (53, 715718, 0, 'MIN', 'NUM', '0'),
-    (54, 715718, 0, 'MAX', 'NUM', '1440'),
-    (55, 715723, 0, 'MIN', 'NUM', '1'),
-    (56, 715723, 0, 'MAX', 'NUM', '99'),
-    (57, 715713, 0, 'MIN', 'NUM', '1'),
-    (58, 715713, 0, 'MAX', 'NUM', '12'),
-    (59, 715722, 0, 'MIN', 'NUM', '1'),
-    (60, 715722, 0, 'MAX', 'NUM', '52')
-"
+SELECT
+      ROW_NUMBER() OVER (ORDER BY concept_id) + (SELECT MAX(id) FROM \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_attribute\`) as id
+    , concept_id
+    , value_as_concept_id
+    , concept_name
+    , type
+    , cnt
+FROM
+    (
+        SELECT
+              observation_source_concept_id as concept_id
+            , 0 as value_as_concept_id
+            , 'MIN' as concept_name
+            , 'NUM' as type
+            , CAST(MIN(value_as_number) as STRING) as cnt
+        FROM \`$BQ_PROJECT.$BQ_DATASET.observation\`
+        WHERE observation_source_concept_id in
+            (
+                SELECT concept_id
+                from \`$BQ_PROJECT.$BQ_DATASET.prep_survey\`
+                WHERE subtype = 'ANSWER'
+                and name = 'Select a value'
+            )
+            and value_as_number is not null
+        GROUP BY 1
+        HAVING NOT (min(value_as_number) = 0 and max(value_as_number) = 0)
+
+        UNION ALL
+        SELECT
+              observation_source_concept_id as concept_id
+            , 0 as value_as_concept_id
+            , 'MAX' as concept_name
+            , 'NUM' as type
+            , CAST(max(value_as_number) as STRING) as cnt
+        FROM \`$BQ_PROJECT.$BQ_DATASET.observation\`
+        WHERE observation_source_concept_id in
+            (
+                SELECT concept_id
+                from \`$BQ_PROJECT.$BQ_DATASET.prep_survey\`
+                WHERE subtype = 'ANSWER'
+                and name = 'Select a value'
+            )
+            and value_as_number is not null
+        GROUP BY 1
+        HAVING NOT (min(value_as_number) = 0 and max(value_as_number) = 0)
+    ) a"
 
 # this will add the min/max values for all numeric measurement concepts
 # this code will filter out any labs WHERE all results = 0
