9ad9e56a8923445c10c526db30452e93b570d273
yonghao <yyhao1@gmail.com>
2021-10-12 11:36:19 -0400

    lint

--- api/src/main/java/org/pmiops/workbench/db/dao/UserServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/UserServiceImpl.java
@@ -263,9 +263,10 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
         accessModuleService.isModuleCompliant(user, AccessModuleName.PUBLICATION_CONFIRMATION);
     boolean profileConfirmationComplete =
         accessModuleService.isModuleCompliant(user, AccessModuleName.PROFILE_CONFIRMATION);
+
     // A temporary work around to check RAS completion status. Currently RAS is marked as complete
     // when enforce featuer flag is off OR the module is complete. But after cleaning up enableRas
-    // feature flag, this should be moved to accessModuleService.
+    // feature flag, this should be moved to accessModuleService's isModuleEnabled check.
     boolean rasLoginGovComplete =
         !configProvider.get().access.enforceRasLoginGovLinking
             || accessModuleService.isModuleCompliant(user, AccessModuleName.RAS_LOGIN_GOV);
