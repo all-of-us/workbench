f11ce4ead6de8bce85e3bfd97dae97e51364ab6d
yonghao <yyhao1@gmail.com>
2021-10-15 14:09:26 -0400

    typos

--- api/src/main/java/org/pmiops/workbench/google/CloudBillingClient.java
+++ api/src/main/java/org/pmiops/workbench/google/CloudBillingClient.java
@@ -8,14 +8,14 @@ public interface CloudBillingClient {
   /**
    * Poll GCP project's billing info until matches the expected billing account name.
    *
-   * <p>This is needed because we uses Terra to update billing accounts, and in Terra's model, a
-   * billing project can be assosicated to many workspaces. So it is an asynchronous process in
-   * Terra. Terra has backgroun job that runs every 5s to update billing account for all workspaces.
-   * We have to poll this from Google to verify the operatiohn succeeds.
+   * <p>This is needed because we use Terra to update billing accounts, and in Terra's model, a
+   * billing project can be associated to many workspaces. So it is an asynchronous process in
+   * Terra. Terra has background job that runs every 5s to update billing account for all workspaces.
+   * We have to poll this from Google to verify the operation succeeds.
    */
   ProjectBillingInfo pollUntilBillingAccountLinked(String projectId, String billingAccountName)
       throws IOException, InterruptedException;
 
-  /** Get a project's billing accont info. */
+  /** Get a project's billing account info. */
   ProjectBillingInfo getProjectBillingInfo(String projectId) throws IOException;
 }
