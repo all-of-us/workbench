d007af9c7d8a32dfe008c5c24648e8879c140e15
Yonghao Yu <yyhao1@gmail.com>
2021-10-18 11:26:06 -0400

    [RW-7333][risk=low]: Poll billing account info from GCP until it match (#5750)
    * poll billing account
    
    * lint
    
    * lint
    
    * fixing test
    
    * fix tests
    
    * revert billing update
    
    * test
    
    * remove debug info
    
    * add javadoc, swap order
    
    * typos
    
    * fix test
    
    * lint

--- api/src/main/java/org/pmiops/workbench/google/CloudBillingClient.java
+++ api/src/main/java/org/pmiops/workbench/google/CloudBillingClient.java
@@ -1,9 +1,21 @@
 package org.pmiops.workbench.google;
 
-import com.google.api.services.cloudbilling.model.BillingAccount;
+import com.google.api.services.cloudbilling.model.ProjectBillingInfo;
 import java.io.IOException;
 
 /** Encapsulate Google APIs for interfacing with Google Cloud Billing APIs. */
 public interface CloudBillingClient {
-  BillingAccount getBillingAccount(String billingAccount) throws IOException;
+  /**
+   * Poll GCP project's billing info until matches the expected billing account name.
+   *
+   * <p>This is needed because we use Terra to update billing accounts, and in Terra's model, a
+   * billing project can be associated to many workspaces. So it is an asynchronous process in
+   * Terra. Terra has background job that runs every 5s to update billing account for all
+   * workspaces. We have to poll this from Google to verify the operation succeeds.
+   */
+  ProjectBillingInfo pollUntilBillingAccountLinked(String projectId, String billingAccountName)
+      throws IOException, InterruptedException;
+
+  /** Get a project's billing account info. */
+  ProjectBillingInfo getProjectBillingInfo(String projectId) throws IOException;
 }
--- api/src/main/java/org/pmiops/workbench/google/CloudBillingClientImpl.java
+++ api/src/main/java/org/pmiops/workbench/google/CloudBillingClientImpl.java
@@ -3,8 +3,10 @@ package org.pmiops.workbench.google;
 import static org.pmiops.workbench.google.GoogleConfig.END_USER_CLOUD_BILLING;
 
 import com.google.api.services.cloudbilling.Cloudbilling;
-import com.google.api.services.cloudbilling.model.BillingAccount;
+import com.google.api.services.cloudbilling.model.ProjectBillingInfo;
 import java.io.IOException;
+import java.time.Duration;
+import java.time.Instant;
 import javax.inject.Provider;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
@@ -23,13 +25,32 @@ public class CloudBillingClientImpl implements CloudBillingClient {
     this.retryHandler = retryHandler;
   }
 
-  public BillingAccount getBillingAccount(String billingAccountName) throws IOException {
+  @Override
+  public ProjectBillingInfo pollUntilBillingAccountLinked(
+      String projectId, String billingAccountName) throws IOException, InterruptedException {
+    Duration pollInterval = Duration.ofSeconds(2);
+    for (Instant deadline = Instant.now().plusSeconds(40);
+        Instant.now().isBefore(deadline);
+        Thread.sleep(pollInterval.toMillis())) {
+      ProjectBillingInfo projectBillingInfo = getProjectBillingInfo(projectId);
+      if (projectBillingInfo.getBillingAccountName().equals(billingAccountName)) {
+        return projectBillingInfo;
+      }
+    }
+    throw new InterruptedException(
+        String.format(
+            "Timeout during poll billing account %s for project %s",
+            billingAccountName, projectId));
+  }
+
+  @Override
+  public ProjectBillingInfo getProjectBillingInfo(String projectId) throws IOException {
     return retryHandler.runAndThrowChecked(
         (context) -> {
           return endUserCloudBillingProvider
               .get()
-              .billingAccounts()
-              .get(billingAccountName)
+              .projects()
+              .getBillingInfo("projects/" + projectId)
               .execute();
         });
   }
