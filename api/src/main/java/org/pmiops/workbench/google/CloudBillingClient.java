ac40e49014f9faeae3f02ac02b09abfda51cace5
yonghao <yyhao1@gmail.com>
2021-10-18 11:04:36 -0400

    lint

--- api/src/main/java/org/pmiops/workbench/google/CloudBillingClient.java
+++ api/src/main/java/org/pmiops/workbench/google/CloudBillingClient.java
@@ -10,8 +10,8 @@ public interface CloudBillingClient {
    *
    * <p>This is needed because we use Terra to update billing accounts, and in Terra's model, a
    * billing project can be associated to many workspaces. So it is an asynchronous process in
-   * Terra. Terra has background job that runs every 5s to update billing account for all workspaces.
-   * We have to poll this from Google to verify the operation succeeds.
+   * Terra. Terra has background job that runs every 5s to update billing account for all
+   * workspaces. We have to poll this from Google to verify the operation succeeds.
    */
   ProjectBillingInfo pollUntilBillingAccountLinked(String projectId, String billingAccountName)
       throws IOException, InterruptedException;
