85e5b44d5a7343b36ddc7f0b6c1e09a27115130c
Brian Freeman <brian.d.freeman@gmail.com>
2021-10-26 15:07:12 -0500

    [RW-7486][risk=no] Add data dictionary entries for zip code socioeconomics (#5813)
    * changing name to All of Us Controlled Tier Dataset v5
    
    * RW-7486 adding data dictionary for zip codes status

--- api/src/main/java/org/pmiops/workbench/dataset/BigQueryTableInfo.java
+++ api/src/main/java/org/pmiops/workbench/dataset/BigQueryTableInfo.java
@@ -17,7 +17,8 @@ public enum BigQueryTableInfo {
   FITBITHEARTRATESUMMARY(Domain.FITBIT_HEART_RATE_SUMMARY, "heart_rate_summary"),
   FITBITHEARTRATELEVEL(Domain.FITBIT_HEART_RATE_LEVEL, "heart_rate_minute_level"),
   FITBITACTIVITY(Domain.FITBIT_ACTIVITY, "activity_summary"),
-  FITBITINTRADAYSTEPS(Domain.FITBIT_INTRADAY_STEPS, "steps_intraday");
+  FITBITINTRADAYSTEPS(Domain.FITBIT_INTRADAY_STEPS, "steps_intraday"),
+  ZIPCODESOCIOECONOMIC(Domain.ZIP_CODE_SOCIOECONOMIC, "zip3_ses_map");
 
   private final Domain domain;
   private final String tableName;
