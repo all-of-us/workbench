b2e6e83038585d2083ce83b8c85ae63336a8bcba
Joel Thibault <thibault@broad.dev>
2021-10-26 16:21:52 -0400

    rename styles

--- ui/src/app/pages/homepage/data-access-requirements.tsx
+++ ui/src/app/pages/homepage/data-access-requirements.tsx
@@ -1,6 +1,7 @@
 import * as fp from 'lodash/fp';
 import * as React from 'react';
 import {useEffect, useState} from 'react';
+
 import {Button, Clickable} from 'app/components/buttons';
 import {FadeBox} from 'app/components/containers';
 import {FlexColumn, FlexRow} from 'app/components/flex';
@@ -134,7 +135,7 @@ const styles = reactStyles({
     lineHeight: '22px',
     marginBottom: '0.5em',
   },
-  rtData: {
+  dataHeader: {
     fontSize: '16px',
     fontWeight: 600,
     marginBottom: '0.5em',
@@ -146,10 +147,10 @@ const styles = reactStyles({
     fontWeight: 'normal',
     marginBottom: '0.5em'
   },
-  rtDetailsIcon: {
+  dataDetailsIcon: {
     marginRight: '0.5em',
   },
-  rtDataDetails: {
+  dataDetails: {
     fontSize: '14px',
     fontWeight: 100,
     marginBottom: '0.5em',
@@ -582,21 +583,21 @@ const Survey = survey;
 const Wearable = wearable;
 
 const renderIcon = (iconName: string) => switchCase(iconName,
-    ['additional', () => <Additional style={styles.rtDetailsIcon}/>],
-    ['electronic', () => <Electronic style={styles.rtDetailsIcon}/>],
-    ['genomic', () => <Genomic style={styles.rtDetailsIcon}/>],
-    ['identifying', () => <Identifying style={styles.rtDetailsIcon}/>],
-    ['individual', () => <Individual style={styles.rtDetailsIcon}/>],
-    ['physical', () => <Physical style={styles.rtDetailsIcon}/>],
-    ['survey', () => <Survey style={styles.rtDetailsIcon}/>],
-    ['wearable', () => <Wearable style={styles.rtDetailsIcon}/>],
+    ['additional', () => <Additional style={styles.dataDetailsIcon}/>],
+    ['electronic', () => <Electronic style={styles.dataDetailsIcon}/>],
+    ['genomic', () => <Genomic style={styles.dataDetailsIcon}/>],
+    ['identifying', () => <Identifying style={styles.dataDetailsIcon}/>],
+    ['individual', () => <Individual style={styles.dataDetailsIcon}/>],
+    ['physical', () => <Physical style={styles.dataDetailsIcon}/>],
+    ['survey', () => <Survey style={styles.dataDetailsIcon}/>],
+    ['wearable', () => <Wearable style={styles.dataDetailsIcon}/>],
 );
 
 const DataDetail = (props: {icon: string, text: string}) => {
   const {icon, text} = props;
   return <FlexRow>
     {renderIcon(icon)}
-    <div style={styles.rtDataDetails}>{text}</div>
+    <div style={styles.dataDetails}>{text}</div>
   </FlexRow>;
 };
 
@@ -608,9 +609,9 @@ const RegisteredTierCard = (props: {profile: Profile, activeModule: AccessModule
       <div style={styles.cardHeader}>Complete Registration</div>
       <FlexRow>
         <RegisteredTierBadge/>
-        <div style={styles.rtData}>Registered Tier data</div>
+        <div style={styles.dataHeader}>Registered Tier data</div>
       </FlexRow>
-      <div style={styles.rtDataDetails}>Once registered, you’ll have access to:</div>
+      <div style={styles.dataDetails}>Once registered, you’ll have access to:</div>
       <DataDetail icon='individual' text='Individual (not aggregated) data'/>
       <DataDetail icon='identifying' text='Identifying information removed'/>
       <DataDetail icon='electronic' text='Electronic health records'/>
@@ -640,11 +641,11 @@ const ControlledTierCard = (props: {profile: Profile, spinnerProps: WithSpinnerO
       <div style={styles.cardHeader}>Additional Data Access</div>
       <FlexRow>
         <ControlledTierBadge/>
-        <div style={styles.rtData}>Controlled Tier data - </div>
+        <div style={styles.dataHeader}>Controlled Tier data - </div>
         <div style={styles.ctDataOptional}>&nbsp;Optional</div>
       </FlexRow>
-      <div style={styles.rtDataDetails}>You are eligible to access Controlled Tier Data</div>
-      <div style={styles.rtDataDetails}>In addition to Registered Tier data, the Controlled Tier curated dataset contains:</div>
+      <div style={styles.dataDetails}>You are eligible to access Controlled Tier Data</div>
+      <div style={styles.dataDetails}>In addition to Registered Tier data, the Controlled Tier curated dataset contains:</div>
       <DataDetail icon='genomic' text='Genomic data'/>
       <DataDetail icon='additional' text='Additional demographic details'/>
     </FlexColumn>
