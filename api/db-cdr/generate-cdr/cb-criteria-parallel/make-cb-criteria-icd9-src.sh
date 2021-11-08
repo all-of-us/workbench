d64f9a4a22aee7b09a9499865c0af1b75078f4dc
Bill Bridges <bill.bridges@vumc.org>
2021-11-08 14:21:02 -0600

    VDA-126 - Moving common functions to cb-criteria-utils.sh

--- api/db-cdr/generate-cdr/cb-criteria-parallel/make-cb-criteria-icd9-src.sh
+++ api/db-cdr/generate-cdr/cb-criteria-parallel/make-cb-criteria-icd9-src.sh
@@ -11,19 +11,7 @@ export BQ_DATASET=$2        # dataset
 ID_PREFIX=$3
 
 ####### common block for all make-cb-criteria-dd-*.sh scripts ###########
-function createTmpTable(){
-  local tmpTbl="prep_temp_"$1"_"$ID_PREFIX
-  res=$(bq --quiet --project_id=$BQ_PROJECT query --batch --nouse_legacy_sql \
-    "CREATE OR REPLACE TABLE \`$BQ_PROJECT.$BQ_DATASET.$tmpTbl\` AS
-      SELECT * FROM \`$BQ_PROJECT.$BQ_DATASET.$1\` LIMIT 0")
-  echo $res >&2
-  echo "$tmpTbl"
-}
-function cpToMain(){
-  local tbl_to=`echo "$1" | sed -e 's/prep_temp_\(.*\)_[0-9]*/\1/'`
-  bq cp --append_table=true --quiet --project_id=$BQ_PROJECT \
-     $BQ_DATASET.$1 $BQ_DATASET.$tbl_to
-}
+source ./generate-cdr/cb-criteria-parallel/cb-criteria-utils.sh
 echo "Running in parallel and Multitable mode - " "$ID_PREFIX - $SQL_FOR"
 CB_CRITERIA_START_ID=$[$ID_PREFIX*10**9] # 3  billion
 CB_CRITERIA_END_ID=$[$[ID_PREFIX+1]*10**9] # 4  billion
