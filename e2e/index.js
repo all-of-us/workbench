7e802528484afe5712360d29460a7c52c83f2845
CH Albach <calbach@users.noreply.github.com>
2021-11-09 11:37:38 -0800

    [RW-7325][risk=low] Nightly egress e2e test (#5880)

--- e2e/index.js
+++ e2e/index.js
@@ -1,21 +1,18 @@
 require('dotenv').config({ path: process.env.DOTENV_CONFIG_PATH });
 
-// Note: If DOTENV_CONFIG_PATH is undefined, ".env" config file is used.
 console.log('');
-console.log(`DOTENV_CONFIG_PATH: ${process.env.DOTENV_CONFIG_PATH}`);
 
-const u = process.env.USER_NAME.split('@');
-console.log(`USER_NAME: ${u.toString()}`);
-
-const r = process.env.READER_USER.split('@');
-console.log(`READER_USER: ${r.toString()}`);
-
-const w = process.env.WRITER_USER.split('@');
-console.log(`WRITER_USER: ${w.toString()}`);
-
-const c = process.env.COLLABORATOR_USER.split('@');
-console.log(`COLLABORATOR_USER: ${c.toString()}`);
-
-const a = process.env.ACCESS_TEST_USER.split('@');
-console.log(`ACCESS_TEST_USER: ${a.toString()}`);
+// Note: If DOTENV_CONFIG_PATH is undefined, ".env" config file is used.
+[
+  'DOTENV_CONFIG_PATH',
+  'USER_NAME',
+  'READER_USER',
+  'WRITER_USER',
+  'COLLABORATOR_USER',
+  'ACCESS_TEST_USER',
+  'ADMIN_TEST_USER',
+  'EGRESS_TEST_USER'
+].forEach((k) => {
+  console.log(`${k}: ${process.env[k]}`);
+});
 console.log('');
---
+++ e2e/tests/nightly/egress-suspension.spec.ts
@@ -0,0 +1,33 @@
+import { findOrCreateWorkspace, signInWithAccessToken } from 'utils/test-utils';
+import { config } from 'resources/workbench-config';
+import { makeRandomName } from 'utils/str-utils';
+import WorkspaceDataPage from 'app/page/workspace-data-page';
+import path from 'path';
+
+// 45 minutes. Test could take a long time.
+jest.setTimeout(45 * 60 * 1000);
+
+describe('egress suspension', () => {
+  beforeEach(async () => {
+    await signInWithAccessToken(page, config.EGRESS_TEST_USER);
+  });
+
+  const notebookName = makeRandomName('egress-notebook');
+  const egressFilename = 'generate-egress.py';
+  const egressFilePath = path.relative(process.cwd(), __dirname + `../../../resources/python-code/${egressFilename}`);
+
+  test("VM egress suspends user's compute", async () => {
+    await findOrCreateWorkspace(page);
+
+    const dataPage = new WorkspaceDataPage(page);
+    const notebookPage = await dataPage.createNotebook(notebookName);
+
+    await notebookPage.uploadFile(egressFilename, egressFilePath);
+    await notebookPage.runCodeFile(1, egressFilename, 5 * 60 * 1000);
+
+    await notebookPage.save();
+
+    // Wait until we become suspended.
+    await notebookPage.waitForSecuritySuspendedStatus(true);
+  });
+});
