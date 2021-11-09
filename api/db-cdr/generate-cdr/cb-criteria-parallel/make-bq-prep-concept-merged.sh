e2573009dea030fa8014ec78d02b337c7389bf72
Bill Bridges <bill.bridges@vumc.org>
2021-11-09 10:53:52 -0600

    VDA-126 - Combining files

--- api/db-cdr/generate-cdr/cb-criteria-parallel/make-bq-prep-concept-merged.sh
+++ api/db-cdr/generate-cdr/cb-criteria-parallel/make-bq-prep-concept-merged.sh
@@ -13,3 +13,12 @@ SELECT * FROM \`$BQ_PROJECT.$BQ_DATASET.concept\`
 UNION ALL
 SELECT * FROM \`$BQ_PROJECT.$BQ_DATASET.prep_concept\`"
 
+#253 - #259 : prep_concept_relationship_merged : make-bq-criteria-tables.sh
+# create merged table of concept_relationship and prep_concept_relationship
+echo "CREATE TABLES - prep_concept_relationship_merged"
+bq --quiet --project_id=$BQ_PROJECT query --batch --nouse_legacy_sql \
+"CREATE OR REPLACE TABLE \`$BQ_PROJECT.$BQ_DATASET.prep_concept_relationship_merged\` AS
+SELECT * FROM \`$BQ_PROJECT.$BQ_DATASET.concept_relationship\`
+UNION ALL
+SELECT * FROM \`$BQ_PROJECT.$BQ_DATASET.prep_concept_relationship\`"
+
