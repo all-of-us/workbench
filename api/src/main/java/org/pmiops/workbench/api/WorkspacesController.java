a0208dd6986b603bdc0fb0f2b2b3b80c4c59e53a
Neha Saxena <nsaxena@broadinstitute.org>
2021-11-11 00:37:38 -0500

    Some cleanup

--- api/src/main/java/org/pmiops/workbench/api/WorkspacesController.java
+++ api/src/main/java/org/pmiops/workbench/api/WorkspacesController.java
@@ -4,7 +4,6 @@ import com.google.common.base.Strings;
 import com.google.common.collect.ImmutableMap;
 import java.sql.Timestamp;
 import java.time.Clock;
-import java.time.OffsetDateTime;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
@@ -166,7 +165,6 @@ public class WorkspacesController implements WorkspacesApiDelegate {
             workspaceId.getWorkspaceNamespace(),
             workspaceId.getWorkspaceName(),
             accessTier.getAuthDomainName());
-    fcWorkspace.setCompletedCloneWorkspaceFileTransfer(OffsetDateTime.now());
     DbWorkspace dbWorkspace = createDbWorkspace(workspace, cdrVersion, user, fcWorkspace);
     try {
       dbWorkspace = workspaceDao.save(dbWorkspace);
@@ -294,7 +292,6 @@ public class WorkspacesController implements WorkspacesApiDelegate {
     Workspace workspace = request.getWorkspace();
     FirecloudWorkspaceDetails fcWorkspace =
         fireCloudService.getWorkspace(workspaceNamespace, workspaceId).getWorkspace();
-    fcWorkspace.setCompletedCloneWorkspaceFileTransfer(OffsetDateTime.now());
     if (workspace == null) {
       throw new BadRequestException("No workspace provided in request");
     }
