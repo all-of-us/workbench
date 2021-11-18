acc661db1b87e4ce27e3b36ebfe19a230900dd61
CH Albach <calbach@google.com>
2021-11-18 11:04:35 -0800

    fixes

--- e2e/utils/runtime-utils.ts
+++ e2e/utils/runtime-utils.ts
@@ -2,18 +2,18 @@ import { Page } from 'puppeteer';
 import Button from 'app/element/button';
 
 const Selector = {
-  runtimeInitializerCreateButton: '//*[data-test-id="runtime-initializer-create")]'
+  runtimeInitializerCreateButton: '//*[@data-test-id="runtime-initializer-create")]'
 };
 
 export async function initializeRuntimeIfModalPresented(page: Page): Promise<void> {
   let initializeButton: Button;
   try {
     initializeButton = new Button(page, Selector.runtimeInitializerCreateButton);
+    await initializeButton.waitUntilEnabled();
   } catch (e) {
     // Expected if the runtime already exists.
     return;
   }
 
-  await initializeButton.waitUntilEnabled();
   await initializeButton.clickAndWait();
 }
