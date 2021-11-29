f63cad09e57390a9c1581966b03bad948e4031e3
zahrarasuli-vumc <76025360+zahrarasuli-vumc@users.noreply.github.com>
2021-11-29 14:21:41 -0600

    Update api/db-cdr/generate-cdr/create-local-csv-files.sh
    Co-authored-by: Brian Freeman <brian.d.freeman@gmail.com>

--- api/db-cdr/generate-cdr/create-local-csv-files.sh
+++ api/db-cdr/generate-cdr/create-local-csv-files.sh
@@ -29,7 +29,7 @@ done
 for t in "${TABLES_WITH_SCHEMA[@]}"
 do
     temp=${t%_temp}
-    bq --project_id=$BQ_PROJECT rm -f $BQ_DATASET.$t
+    bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.$t"
     bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$temp.json" "$BQ_DATASET.$t"
 done
 
