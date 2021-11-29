c9d70a570c2090acc30e36087c53cb5d47db5817
Zahra Rasuli <zahrarasuli@VICTRs-MacBook-Pro.local>
2021-11-29 15:12:05 -0600

    resolving codacy problems

--- api/db-cdr/generate-cdr/create-local-csv-files.sh
+++ api/db-cdr/generate-cdr/create-local-csv-files.sh
@@ -66,7 +66,7 @@ WHERE domain_id = 'MEASUREMENT'
 ORDER by id"
 
 echo "Insert cb_criteria_observation_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_observation_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -75,7 +75,7 @@ WHERE domain_id = 'OBSERVATION'
 ORDER by id"
 
 echo "Insert cb_criteria_person_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_person_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -84,7 +84,7 @@ WHERE domain_id = 'PERSON'
 ORDER by id"
 
 echo "Insert cb_criteria_physical_measurement_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_physical_measurement_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -93,7 +93,7 @@ WHERE domain_id in ('PHYSICAL_MEASUREMENT', 'PHYSICAL_MEASUREMENT_CSS')
 ORDER by id"
 
 echo "Insert cb_criteria_procedure_standard_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_procedure_standard_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -104,7 +104,7 @@ order by id
 limit 20000"
 
 echo "Insert cb_criteria_procedure_source_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_procedure_source_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -115,7 +115,7 @@ order by id
 limit 20000"
 
 echo "Insert cb_criteria_survey_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_survey_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -124,7 +124,7 @@ where domain_id = 'SURVEY'
 order by id"
 
 echo "Insert cb_criteria_visit_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_visit_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -133,7 +133,7 @@ where domain_id = 'VISIT'
 order by id"
 
 echo "Insert cb_criteria_fitbit_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_fitbit_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -142,7 +142,7 @@ where domain_id = 'FITBIT'
 order by id"
 
 echo "Insert cb_criteria_condition_source_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_condition_source_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -153,7 +153,7 @@ order by id
 limit 20000"
 
 echo "Insert cb_criteria_condition_standard_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_condition_standard_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -164,7 +164,7 @@ order by id
 limit 20000"
 
 echo "Insert cb_criteria_wgv_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_wgv_temp\`
 (id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms)
    SELECT id,parent_id,domain_id,is_standard,type,subtype,concept_id,code,name,value,est_count,is_group,is_selectable,has_attribute,has_hierarchy,has_ancestor_data,path,synonyms,rollup_count,item_count,full_text,display_synonyms
@@ -173,37 +173,37 @@ where domain_id = 'WHOLE_GENOME_VARIANT'
 order by id"
 
 echo "Insert cb_criteria_relationship_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_relationship_temp\`
    SELECT *
 FROM \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_relationship\`"
 
 echo "Insert cb_criteria_menu_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_menu_temp\`
    SELECT *
 FROM \`$BQ_PROJECT.$BQ_DATASET.cb_criteria_menu\`"
 
 echo "Insert cb_survey_attribute_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_survey_attribute_temp\`
    SELECT *
 FROM \`$BQ_PROJECT.$BQ_DATASET.cb_survey_attribute\`"
 
 echo "Insert cb_survey_version_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_survey_version_temp\`
    SELECT *
 FROM \`$BQ_PROJECT.$BQ_DATASET.cb_survey_version\`"
 
 echo "Insert ds_data_dictionary_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.ds_data_dictionary_temp\`
    SELECT *
 FROM \`$BQ_PROJECT.$BQ_DATASET.ds_data_dictionary\`"
 
 echo "Insert ds_linking_temp"
-bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.ds_linking_temp\`
    SELECT *
 FROM \`$BQ_PROJECT.$BQ_DATASET.ds_linking\`"
