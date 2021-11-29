84939e32c73e0869ee8e712187e593d6f37d3e89
zahrarasuli-vumc <76025360+zahrarasuli-vumc@users.noreply.github.com>
2021-11-29 10:54:50 -0600

    Update api/db-cdr/generate-cdr/create-local-csv-files.sh
    Co-authored-by: Brian Freeman <brian.d.freeman@gmail.com>

--- api/db-cdr/generate-cdr/create-local-csv-files.sh
+++ api/db-cdr/generate-cdr/create-local-csv-files.sh
@@ -217,7 +217,7 @@ do
     bq extract --project_id "$BQ_PROJECT" --destination_format=CSV --field_delimiter=',' "$BQ_PROJECT:$BQ_DATASET.$i" \
     "gs://$BUCKET/${i%_temp}.csv"
     echo "Deleting tables"
-    bq rm -f -t $BQ_PROJECT:$BQ_DATASET.$i
+    bq rm -f -t "$BQ_PROJECT:$BQ_DATASET.$i"
 done
 
 for i in "${TABLES_WITH_SCHEMA[@]}"
