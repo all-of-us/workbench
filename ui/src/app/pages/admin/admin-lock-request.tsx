d7914528c970fa4498680783a0665d204c6c0434
Neha Saxena <nsaxena@broadinstitute.org>
2021-11-30 11:30:19 -0500

    Add error message

--- ui/src/app/pages/admin/admin-lock-request.tsx
+++ ui/src/app/pages/admin/admin-lock-request.tsx
@@ -7,7 +7,6 @@ import {DatePicker, TextArea} from 'app/components/inputs';
 import {Button} from 'app/components/buttons';
 import {workspaceAdminApi} from 'app/services/swagger-fetch-clients';
 import {useState} from 'react';
-import {useToggle} from 'app/utils';
 import {TooltipTrigger} from 'app/components/popups';
 import colors from 'app/styles/colors';
 
@@ -20,30 +19,30 @@ interface Props {
 export const AdminLockRequest = (props: Props) => {
   const [requestReason, setRequestReason] = useState('');
   const [requestDate, setRequestDate] = useState(new Date());
-  const [showError, setShowError] = useToggle();
+  const [showError, setShowError] = useState(false);
 
 
   const disableLockWorkspace = !requestReason ||
-      requestReason === '' || !requestDate || requestDate?.toString() === '';
+      requestReason === '' || !requestDate || requestDate?.toString() === '' || showError;
+
+  const getToolTipContent = showError ? 'Error occurred while Locking Workspace' :
+      'Request Reason & Request Date are required to lock workspace';
 
   const onLockWorkspace = () => {
+    const {workspace , onClose, onLock} = props;
     const adminLockingRequest = {
       requestReason,
       requestDateInMillis: requestDate.valueOf()
     }
 
-    workspaceAdminApi().setAdminLockedState(props.workspace, adminLockingRequest)
-        .then(() =>  {
-          props.onLock()
-        })
-        .catch(error => {
-          if(error.status === 400) {
-            setShowError(true);
-          }
-          })
-        .finally(() => {
-          props.onClose();
-        });
+    workspaceAdminApi().setAdminLockedState(workspace, adminLockingRequest)
+      .then(() => {
+        onLock();
+        onClose();
+      })
+      .catch(error => {
+         setShowError(true);
+      });
   }
 
    return <Modal>
@@ -51,23 +50,29 @@ export const AdminLockRequest = (props: Props) => {
        <SemiBoldHeader>Lock workspace</SemiBoldHeader>
      </ModalTitle>
      <ModalBody>
-       {showError && <div>
-         Something went Wrong while Locking the workspace
-       </div>}
+       {showError && <label style={{color: colors.danger}}>
+         Something went Wrong while Locking the workspace.
+       </label>}
        <div>
-         <label style={{fontWeight: 'bold', color: colors.primary}}>Enter reason for researcher on
-           why workspace access is locked</label>
+         <label style={{fontWeight: 'bold', color: colors.primary}}>
+           Enter reason for researcher on why workspace access is locked
+         </label>
        </div>
-       <div style={{paddingTop: '0.3rem', paddingBottom: '1rem'}}>
-         <label style={{color: colors.primary, paddingBottom: '0.3rem'}}><i>Any message in the input box will automatically
-           be sent to researcher when the workspace is locked</i></label>
 
-         <TextArea value={requestReason}
-                   onChange={(text) => setRequestReason(text)}/>
+       {/* Text area to enter the reason for locking workspace */}
+       <div style={{paddingTop: '0.3rem', paddingBottom: '1rem'}}>
+         <label style={{color: colors.primary, paddingBottom: '0.3rem'}}>
+           <i>Any message in the input box will automatically be sent to researcher when the
+             workspace is locked</i>
+         </label>
+         <TextArea value={requestReason} onChange={(text) => setRequestReason(text)}/>
        </div>
+
+       {/* Locking workspace request Date*/}
        <div>
-         <div style={{fontWeight: 'bold', color: colors.primary, paddingBottom: '0.3rem'}}>Add date
-           of RAB request date</div>
+         <div style={{fontWeight: 'bold', color: colors.primary, paddingBottom: '0.3rem'}}>
+           Add date of RAB request date
+         </div>
          <DatePicker
              value={requestDate}
              placeholder='YYYY-MM-DD'
@@ -77,9 +82,13 @@ export const AdminLockRequest = (props: Props) => {
        </div>
      </ModalBody>
      <ModalFooter>
-       <Button type='secondary' style={{marginRight: '0.5rem'}} onClick={() => props.onClose()}>CANCEL</Button>
-       <TooltipTrigger content={'Request Reason & Request Date are required to lock workspace'} disabled={!disableLockWorkspace}>
-       <Button type='primary' onClick={() => onLockWorkspace()} disabled={disableLockWorkspace}>LOCK WORKSPACE</Button>
+       <Button type='secondary' style={{marginRight: '0.5rem'}} onClick={() => props.onClose()}>
+         CANCEL
+       </Button>
+       <TooltipTrigger content={getToolTipContent} disabled={!disableLockWorkspace}>
+         <Button type='primary' onClick={() => onLockWorkspace()} disabled={disableLockWorkspace}>
+           LOCK WORKSPACE
+         </Button>
        </TooltipTrigger>
      </ModalFooter>
    </Modal>;
