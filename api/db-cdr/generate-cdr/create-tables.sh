972c5018209d0a9a17a7fd6cf3c92bfcf05c9d4b
Brian Freeman <brian.d.freeman@gmail.com>
2021-12-01 10:22:46 -0600

    adding output

--- api/db-cdr/generate-cdr/create-tables.sh
+++ api/db-cdr/generate-cdr/create-tables.sh
@@ -22,23 +22,29 @@ for filename in bq-schemas/*.json;
 do
     json_name=${filename##*/}
     table_name=${json_name%.json}
+    echo "Deleting $table_name"
     bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.$table_name"
     if [[ "$table_name" == 'cb_search_all_events' ]]
     then
+      echo "Creating $table_name"
       bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" --time_partitioning_type=DAY --clustering_fields concept_id "$BQ_DATASET.$table_name"
     elif [[ "$table_name" == 'cb_review_survey' || "$table_name" == 'cb_search_person' ]]
     then
+      echo "Creating $table_name"
       bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" --time_partitioning_type=DAY --clustering_fields person_id "$BQ_DATASET.$table_name"
     elif [[ "$table_name" == 'cb_review_all_events' ]]
     then
+      echo "Creating $table_name"
       bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" --time_partitioning_type=DAY --clustering_fields person_id,domain "$BQ_DATASET.$table_name"
     elif [[ "$table_name" == 'ds_zip_code_socioeconomic' ]]
     then
       if [[ "$TABLE_LIST" == *"zip3_ses_map"* ]]
       then
+        echo "Creating $table_name"
         bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.$table_name"
       fi
     else
+      echo "Creating $table_name"
       bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.$table_name"
     fi
 done
