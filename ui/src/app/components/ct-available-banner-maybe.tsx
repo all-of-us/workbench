2942c91a8382db0f79e26eef5d5b29fc360b7c4e
Joel Thibault <thibault@broad.dev>
2021-12-01 15:23:16 -0500

    first attempt at ct notification logic

--- ui/src/app/components/ct-available-banner-maybe.tsx
+++ ui/src/app/components/ct-available-banner-maybe.tsx
@@ -4,13 +4,31 @@ import {useEffect, useState} from 'react';
 import {ToastBanner, ToastType} from './toast-banner';
 import {cookiesEnabled} from 'app/utils/cookies';
 import {StyledRouterLink} from './buttons';
+import {environment} from "../../environments/environment";
+import {AccessTierShortNames} from "../utils/access-tiers";
+import {AccessModule, CdrVersionTier, CdrVersionTiersResponse, Profile} from "../../generated/fetch";
+import {eligibleForTier, getAccessModuleStatusByName} from "../utils/access-utils";
+import {getCdrVersionTier} from "../utils/cdr-versions";
+import {cdrVersionStore, profileStore, useStore} from "../utils/stores";
 
 const CT_COOKIE_KEY = 'controlled-tier-available';
 const DAR_PATH = '/data-access-requirements';
 
-const shouldShowBanner = () => {
-  // TODO implement the real logic
-  const shouldShow = window.location.pathname !== DAR_PATH;
+const shouldShowBanner = (profile: Profile, cdrVersionTiers: CdrVersionTier[]) => {
+  // all of the following must be true
+  const shouldShow =
+    // the user is eligible for the CT
+    eligibleForTier(profile, AccessTierShortNames.Controlled) &&
+    // the user does not currently have CT access
+    !profile.accessTierShortNames.includes(AccessTierShortNames.Controlled) &&
+    // the environment allows users to see the CT (in the UI)
+    environment.accessTiersVisibleToUsers.includes(AccessTierShortNames.Controlled) &&
+    // the user's DUCC access module completion time was before the release of the default CT CDR Version
+    (getAccessModuleStatusByName(profile, AccessModule.DATAUSERCODEOFCONDUCT).completionEpochMillis <
+      cdrVersionTiers.find(v => v.accessTierShortName === AccessTierShortNames.Controlled)?.defaultCdrVersionCreationTime) &&
+    // the user is not currently visiting the DAR page
+    (window.location.pathname !== DAR_PATH);
+
   if (cookiesEnabled()) {
     const cookie = localStorage.getItem(CT_COOKIE_KEY);
     return !cookie && shouldShow;
@@ -21,8 +39,10 @@ const shouldShowBanner = () => {
 
 export const CTAvailableBannerMaybe = () => {
   const [showBanner, setShowBanner] = useState(false);
+  const {profile} = useStore(profileStore);
+  const {tiers} = useStore(cdrVersionStore);
 
-  useEffect(() => setShowBanner(shouldShowBanner()), []);
+  useEffect(() => setShowBanner(shouldShowBanner(profile, tiers)), []);
 
   const acknowledgeBanner = () => {
     if (cookiesEnabled()) {
