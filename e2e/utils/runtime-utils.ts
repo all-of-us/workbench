c28ba0f22f6d2c2f04c0d478191faf1bfcd3206f
aweng98 <bweng@broadinstitute.org>
2021-12-02 10:23:19 -0500

    [risk=no] refactor initializeRuntimeIfModalPresented func

--- e2e/utils/runtime-utils.ts
+++ e2e/utils/runtime-utils.ts
@@ -6,14 +6,9 @@ const Selector = {
 };
 
 export async function initializeRuntimeIfModalPresented(page: Page): Promise<void> {
-  let initializeButton: Button;
-  try {
-    initializeButton = new Button(page, Selector.runtimeInitializerCreateButton);
-    await initializeButton.waitUntilEnabled();
-  } catch (e) {
-    // Expected if the runtime already exists.
-    return;
+  const initializeButton = new Button(page, Selector.runtimeInitializerCreateButton);
+  const existsButton = await initializeButton.exists();
+  if (existsButton) {
+    await initializeButton.clickAndWait();
   }
-
-  await initializeButton.clickAndWait();
 }
