7c3c634f044cd9373ecf7d03791a974c69d8172a
chavanr2019 <rohini.chavan@vumc.org>
2021-12-07 11:23:44 -0600

    added the lockWorkspace test case

--- e2e/tests/nightly/admin/workspace-admin-ui.spec.ts
+++ e2e/tests/nightly/admin/workspace-admin-ui.spec.ts
@@ -2,7 +2,7 @@ import { signInWithAccessToken } from 'utils/test-utils';
 import { config } from 'resources/workbench-config';
 import navigation, { NavLink } from 'app/component/navigation';
 import WorkspaceAdminPage from 'app/page/admin-workspace-page';
-import { ConceptSetSelectValue, WorkspaceHeadings, CloudStorageHeader, Language } from 'app/text-labels';
+import { ConceptSetSelectValue, CloudStorageHeader, Language } from 'app/text-labels';
 import RuntimePanel from 'app/component/runtime-panel';
 import WorkspacesPage from 'app/page/workspaces-page';
 import WorkspaceCard from 'app/component/workspace-card';
@@ -11,6 +11,7 @@ import AdminNotebookPreviewPage from 'app/page/admin-notebook-preview-page';
 import { waitForDocumentTitle } from 'utils/waits-utils';
 import { Page } from 'puppeteer';
 import NotebookPreviewPage from 'app/page/notebook-preview-page';
+
 
 describe('Workspace Admin', () => {
   const workspaceName = 'e2eAdminWorkspace';
@@ -23,7 +24,7 @@ describe('Workspace Admin', () => {
     await navigation.navMenu(page, NavLink.WORKSPACE_ADMIN);
   });
 
-  test('check the Workspace Admin page UI', async () => {
+  test('Verify the lock-Workspace feature and the Workspace Admin page UI', async () => {
     const workspacesPage = new WorkspacesPage(page);
     await workspacesPage.load();
     await workspacesPage.createWorkspace(workspaceName);
@@ -34,30 +35,30 @@ describe('Workspace Admin', () => {
     await navigation.navMenu(page, NavLink.WORKSPACE_ADMIN);
     const workspaceAdminPage = new WorkspaceAdminPage(page);
     await workspaceAdminPage.waitForLoad();
+
     await workspaceAdminPage.getWorkspaceNamespaceInput().type(workspaceNamespace);
     await workspaceAdminPage.clickLoadWorkspaceButton();
     await workspaceAdminPage.waitForLoad();
-    expect(await workspaceAdminPage.getWorkspaceHeader()).toEqual('Workspace');
-    const headings3 = await workspaceAdminPage.getAllHeadings3();
-    expect(headings3).toEqual(
-      expect.arrayContaining([
-        WorkspaceHeadings.BasicInformation,
-        WorkspaceHeadings.Collaborators,
-        WorkspaceHeadings.CohortBuilder,
-        WorkspaceHeadings.CloudStorageObjects,
-        WorkspaceHeadings.ResearchPurpose
-      ])
-    );
-    const headings2 = await workspaceAdminPage.getAllHeadings2();
-    expect(headings2).toEqual(
-      expect.arrayContaining([WorkspaceHeadings.CloudStorageTraffic, WorkspaceHeadings.Runtimes])
-    );
+
+    //verify if the admin navigated to correct workspace-Namespace-admin page
+    expect(await workspaceAdminPage.getWorkspaceNamespaceText()).toEqual(workspaceNamespace);
+
     const cloudStorageCols = await workspaceAdminPage.getcloudStorageColNames();
     expect(cloudStorageCols).toEqual(
       expect.arrayContaining([CloudStorageHeader.Location, CloudStorageHeader.Filename, CloudStorageHeader.FileSize])
     );
+    //verify that this newly created workspace has NO active runtime and also expect the RUNTIME DELETE button is falsy.
     expect(await workspaceAdminPage.getRuntimeDeleteButton().exists()).toBeFalsy();
     expect(await workspaceAdminPage.getNoActiveRuntimeText()).toEqual(noActiveRuntimeText);
+
+    //click on the LOCK WORKSPACE button
+    const lockWorkspacedModal = await workspaceAdminPage.clickLockWorkspaceButton();
+    expect(await lockWorkspacedModal.getLockWorkspaceButton().isCursorNotAllowed()).toBe(true);
+   
+    await lockWorkspacedModal.createLockWorkspaceMessage();
+    expect(await lockWorkspacedModal.getLockWorkspaceButton().isCursorNotAllowed()).toBe(false);
+    lockWorkspacedModal.clickCancelButton();
+    await workspaceAdminPage.waitForLoad();
   });
 
   test('Verify that admin is able to preview the Notebook', async () => {
@@ -77,11 +78,12 @@ describe('Workspace Admin', () => {
     await adminNotebookPreviewPage.waitForLoad();
     // verify that the notebook preview page loaded
     const previewCode = await adminNotebookPreviewPage.getFormattedCode();
+
     // verify that the notebook file is in Python programming language
     expect(previewCode.some((item) => item.includes('import pandas'))).toBe(true);
     expect(previewCode.some((item) => item.includes('import os'))).toBe(true);
     expect(await adminNotebookPreviewPage.getNamespaceText()).toEqual(workspaceNamespace);
-    // click on the namespace link to navigate back to the workpsace admin page
+    // click on the namespace link to navigate back to the workpsace-admin page
     await adminNotebookPreviewPage.clickNamespaceLink();
     await workspaceAdminPage.waitForLoad();
   });
@@ -89,10 +91,6 @@ describe('Workspace Admin', () => {
   test('Verify that admin is able to delete runtime', async () => {
     const workspaceAdminPage = new WorkspaceAdminPage(page);
     await workspaceAdminPage.waitForLoad();
-    await workspaceAdminPage.getWorkspaceNamespaceInput().type(workspaceNamespace);
-    await workspaceAdminPage.clickLoadWorkspaceButton();
-    await workspaceAdminPage.waitForLoad();
-    expect(await workspaceAdminPage.getNoActiveRuntimeText()).toEqual(noActiveRuntimeText);
     await new WorkspacesPage(page).load();
     const workspaceCard = await WorkspaceCard.findCard(page, workspaceName);
     await workspaceCard.clickWorkspaceName(true);
@@ -102,7 +100,7 @@ describe('Workspace Admin', () => {
     // Create runtime
     await runtimePanel.createRuntime();
     await page.waitForTimeout(2000);
-    // navigate back to workspace admin page to delete the workspace
+    // navigate back to workspace admin page
     await navigation.navMenu(page, NavLink.WORKSPACE_ADMIN);
     await workspaceAdminPage.waitForLoad();
     await workspaceAdminPage.getWorkspaceNamespaceInput().type(workspaceNamespace);
@@ -116,10 +114,10 @@ describe('Workspace Admin', () => {
     await workspaceAdminPage.clickRuntimeDeleteButton();
     // delete the runtime
     await deleteRuntimeModal.clickDeleteButton();
-    await workspaceAdminPage.waitForLoad();
+    await page.waitForTimeout(4000);
+
     //verify the runtime status is deleting
     expect(await workspaceAdminPage.getRuntimeStatus()).toEqual('Deleting');
-    await page.waitForTimeout(2000);
   });
 
   async function createNotebook(page: Page, pyNotebookName: string): Promise<NotebookPreviewPage> {
@@ -134,11 +132,6 @@ describe('Workspace Admin', () => {
 
     const createModal = await datasetBuildPage.clickCreateButton();
     await createModal.createDataset();
-
-    // // Preview table exists and has one or more table rows.
-    // const previewTable = await datasetBuildPage.getPreviewTable();
-    // expect(await previewTable.exists()).toBe(true);
-    // expect(await previewTable.getRowCount()).toBeGreaterThan(1);
 
     const exportModal = await datasetBuildPage.clickAnalyzeButton();
 
