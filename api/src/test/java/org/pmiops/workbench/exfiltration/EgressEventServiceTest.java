f31c843034a276b752bbc6ecdc266f89e73f8983
CH Albach <calbach@users.noreply.github.com>
2021-12-07 10:14:54 -0800

    [RW-7372][risk=no] Teardown egress alert v2 flag (#5998)

--- api/src/test/java/org/pmiops/workbench/exfiltration/EgressEventServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/exfiltration/EgressEventServiceTest.java
@@ -1,22 +1,14 @@
 package org.pmiops.workbench.exfiltration;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyLong;
-import static org.mockito.ArgumentMatchers.anyString;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.verifyZeroInteractions;
-import static org.mockito.Mockito.when;
-import static org.pmiops.workbench.exfiltration.EgressEventServiceImpl.NOT_FOUND_WORKSPACE_NAMESPACE;
 import static org.pmiops.workbench.utils.TestMockFactory.DEFAULT_GOOGLE_PROJECT;
 
 import com.google.common.collect.ImmutableList;
 import com.google.common.collect.Iterables;
-import com.ifountain.opsgenie.client.swagger.ApiException;
-import com.ifountain.opsgenie.client.swagger.api.AlertApi;
-import com.ifountain.opsgenie.client.swagger.model.CreateAlertRequest;
-import com.ifountain.opsgenie.client.swagger.model.SuccessResponse;
 import java.sql.Timestamp;
 import java.time.Duration;
 import java.time.Instant;
@@ -24,17 +16,13 @@ import java.time.OffsetDateTime;
 import java.time.format.DateTimeFormatter;
 import java.util.List;
 import java.util.Optional;
-import java.util.regex.Pattern;
 import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.mockito.ArgumentCaptor;
-import org.mockito.Captor;
 import org.pmiops.workbench.FakeClockConfiguration;
 import org.pmiops.workbench.FakeJpaDateTimeConfiguration;
 import org.pmiops.workbench.actionaudit.auditors.EgressEventAuditor;
 import org.pmiops.workbench.cloudtasks.TaskQueueService;
-import org.pmiops.workbench.config.WorkbenchConfig;
 import org.pmiops.workbench.db.dao.EgressEventDao;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.dao.UserService;
@@ -43,52 +31,34 @@ import org.pmiops.workbench.db.model.DbEgressEvent;
 import org.pmiops.workbench.db.model.DbEgressEvent.DbEgressEventStatus;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbWorkspace;
-import org.pmiops.workbench.institution.InstitutionService;
-import org.pmiops.workbench.jira.api.JiraApi;
-import org.pmiops.workbench.model.Institution;
 import org.pmiops.workbench.model.SumologicEgressEvent;
 import org.pmiops.workbench.model.User;
-import org.pmiops.workbench.model.Workspace;
 import org.pmiops.workbench.model.WorkspaceAccessLevel;
-import org.pmiops.workbench.model.WorkspaceAdminView;
 import org.pmiops.workbench.model.WorkspaceUserAdminView;
 import org.pmiops.workbench.test.FakeClock;
-import org.pmiops.workbench.utils.TestMockFactory;
-import org.pmiops.workbench.workspaceadmin.WorkspaceAdminService;
 import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
-import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
-import org.springframework.context.annotation.Scope;
 
 @DataJpaTest
 @Import({FakeClockConfiguration.class, FakeJpaDateTimeConfiguration.class})
 public class EgressEventServiceTest {
 
   private static final Instant NOW = Instant.parse("2020-06-11T01:30:00.02Z");
-  private static final String INSTITUTION_2_NAME = "Auburn University";
   private static final String WORKSPACE_NAMEPACE = "aou-namespace";
-  private static WorkbenchConfig workbenchConfig;
 
-  @MockBean private AlertApi mockAlertApi;
-  @MockBean private JiraApi mockJiraApi;
-  @MockBean private EgressEventAuditor egressEventAuditor;
-  @MockBean private InstitutionService mockInstitutionService;
-  @MockBean private UserService mockUserService;
-  @MockBean private WorkspaceAdminService mockWorkspaceAdminService;
-  @MockBean private TaskQueueService mockTaskQueueService;
   @Autowired private WorkspaceDao workspaceDao;
   @Autowired private EgressEventDao egressEventDao;
   @Autowired private UserDao userDao;
 
-  @Captor private ArgumentCaptor<CreateAlertRequest> alertRequestCaptor;
+  @Autowired private EgressEventAuditor mockEgressEventAuditor;
+  @Autowired private TaskQueueService mockTaskQueueService;
+  @Autowired private UserService mockUserService;
 
   @Autowired private EgressEventService egressEventService;
   @Autowired private FakeClock fakeClock;
-  private static final TestMockFactory TEST_MOCK_FACTORY = new TestMockFactory();
   private static final User USER_1 =
       new User()
           .givenName("Fredward")
@@ -105,8 +75,6 @@ public class EgressEventServiceTest {
           .userAccountCreatedTime(
               OffsetDateTime.parse(
                   "2018-08-30T01:20+02:00", DateTimeFormatter.ISO_OFFSET_DATE_TIME));
-
-  private static final String INSTITUTION_1_NAME = "Verily Life Sciences";
 
   private static final User USER_2 =
       new User()
@@ -127,21 +95,11 @@ public class EgressEventServiceTest {
 
   @TestConfiguration
   @Import({FakeClockConfiguration.class, EgressEventServiceImpl.class})
-  static class Configuration {
-
-    @Bean
-    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
-    WorkbenchConfig getWorkbenchConfig() {
-      return workbenchConfig;
-    }
-  }
+  @MockBean({EgressEventAuditor.class, TaskQueueService.class, UserService.class})
+  static class Configuration {}
 
   @BeforeEach
   public void setUp() {
-    workbenchConfig = WorkbenchConfig.createEmptyConfig();
-    workbenchConfig.server.uiBaseUrl = "https://workbench.researchallofus.org";
-    workbenchConfig.featureFlags.enableEgressAlertingV2 = false;
-
     dbWorkspace = new DbWorkspace();
     dbWorkspace.setWorkspaceNamespace(WORKSPACE_NAMEPACE);
     dbWorkspace.setGoogleProject(DEFAULT_GOOGLE_PROJECT);
@@ -150,30 +108,11 @@ public class EgressEventServiceTest {
     dbUser1 = userDao.save(workspaceAdminUserViewToUser(ADMIN_VIEW_1));
     dbUser2 = userDao.save(workspaceAdminUserViewToUser(ADMIN_VIEW_2));
 
-    final Workspace workspace =
-        TEST_MOCK_FACTORY
-            .createWorkspace(WORKSPACE_NAMEPACE, "The Whole #!")
-            .creator(USER_1.getUserName());
-    final WorkspaceAdminView workspaceAdminView =
-        new WorkspaceAdminView()
-            .workspace(workspace)
-            .workspaceDatabaseId(101010L)
-            .collaborators(ImmutableList.of(ADMIN_VIEW_1, ADMIN_VIEW_2));
-    doReturn(workspaceAdminView).when(mockWorkspaceAdminService).getWorkspaceAdminView(anyString());
-
     doReturn(Optional.of(dbUser1)).when(mockUserService).getByDatabaseId(dbUser1.getUserId());
     doReturn(Optional.of(dbUser1)).when(mockUserService).getByUsername(dbUser1.getUsername());
 
     doReturn(Optional.of(dbUser2)).when(mockUserService).getByDatabaseId(dbUser2.getUserId());
     doReturn(Optional.of(dbUser2)).when(mockUserService).getByUsername(dbUser2.getUsername());
-
-    final Institution institution1 = new Institution().displayName(INSTITUTION_1_NAME);
-    doReturn(Optional.of(institution1)).when(mockInstitutionService).getByUser(dbUser1);
-
-    final Institution institution2 = new Institution().displayName(INSTITUTION_2_NAME);
-
-    doReturn(Optional.of(institution2)).when(mockInstitutionService).getByUser(dbUser2);
-    fakeClock.setInstant(NOW);
   }
 
   @AfterEach
@@ -183,37 +122,10 @@ public class EgressEventServiceTest {
   }
 
   @Test
-  public void testCreateEgressEventAlert() throws ApiException {
-    when(mockAlertApi.createAlert(any())).thenReturn(new SuccessResponse().requestId("12345"));
-
+  public void testCreateEgressEventAlert() throws Exception {
     SumologicEgressEvent event = recentEgressEventForUser(dbUser1);
     egressEventService.handleEvent(event);
-    verify(mockAlertApi).createAlert(alertRequestCaptor.capture());
-    verify(egressEventAuditor).fireEgressEvent(event);
-
-    String vmPrefix = "all-of-us-" + dbUser1.getUserId();
-
-    final CreateAlertRequest request = alertRequestCaptor.getValue();
-    assertThat(request.getDescription())
-        .contains("Terra Billing Project/Firecloud Namespace: " + WORKSPACE_NAMEPACE);
-    assertThat(request.getDescription()).contains("Google Project Id: " + DEFAULT_GOOGLE_PROJECT);
-    assertThat(request.getDescription())
-        .contains(
-            "https://workbench.researchallofus.org/admin/workspaces/" + WORKSPACE_NAMEPACE + "/");
-    assertThat(request.getDescription())
-        .containsMatch(
-            Pattern.compile("Institution:\\s+Verily\\s+Life\\s+Sciences,\\s+Account\\s+Age:"));
-    assertThat(request.getAlias()).isEqualTo(WORKSPACE_NAMEPACE + " | " + vmPrefix);
-
-    verifyZeroInteractions(mockTaskQueueService);
-  }
-
-  @Test
-  public void testCreateEgressEventAlert_v2() throws ApiException {
-    workbenchConfig.featureFlags.enableEgressAlertingV2 = true;
-    SumologicEgressEvent event = recentEgressEventForUser(dbUser1);
-    egressEventService.handleEvent(event);
-    verify(egressEventAuditor).fireEgressEvent(event);
+    verify(mockEgressEventAuditor).fireEgressEvent(event);
     verify(mockTaskQueueService).pushEgressEventTask(anyLong());
 
     List<DbEgressEvent> dbEvents = ImmutableList.copyOf(egressEventDao.findAll());
@@ -225,84 +137,10 @@ public class EgressEventServiceTest {
     assertThat(dbEvent.getLastModifiedTime()).isNotNull();
     assertThat(dbEvent.getSumologicEvent()).isNotNull();
     assertThat(dbEvent.getEgressWindowSeconds()).isEqualTo(event.getTimeWindowDuration());
-
-    verifyZeroInteractions(mockAlertApi);
   }
 
   @Test
-  public void testCreateEgressEventAlert_institutionNotFound() throws ApiException {
-    when(mockAlertApi.createAlert(any())).thenReturn(new SuccessResponse().requestId("12345"));
-    doReturn(Optional.empty()).when(mockInstitutionService).getByUser(any(DbUser.class));
-
-    egressEventService.handleEvent(recentEgressEventForUser(dbUser1));
-    verify(mockAlertApi).createAlert(alertRequestCaptor.capture());
-
-    final CreateAlertRequest request = alertRequestCaptor.getValue();
-    assertThat(request.getDescription())
-        .contains("Terra Billing Project/Firecloud Namespace: " + WORKSPACE_NAMEPACE);
-    assertThat(request.getDescription()).contains("Google Project Id: " + DEFAULT_GOOGLE_PROJECT);
-    assertThat(request.getDescription())
-        .contains(
-            "https://workbench.researchallofus.org/admin/workspaces/" + WORKSPACE_NAMEPACE + "/");
-    assertThat(request.getDescription())
-        .containsMatch(Pattern.compile("Institution:\\s+not\\s+found,\\s+Account\\s+Age:"));
-    assertThat(request.getAlias())
-        .isEqualTo(WORKSPACE_NAMEPACE + " | all-of-us-" + dbUser1.getUserId());
-
-    verifyZeroInteractions(mockTaskQueueService);
-  }
-
-  @Test
-  public void testCreateEgressEventAlert_workspaceNotFound() throws ApiException {
-    when(mockAlertApi.createAlert(any())).thenReturn(new SuccessResponse().requestId("12345"));
-
-    String notFoundProjectName = "NOT_FOUND123";
-    SumologicEgressEvent event = recentEgressEventForUser(dbUser1).projectName(notFoundProjectName);
-    egressEventService.handleEvent(event);
-    verify(mockAlertApi).createAlert(alertRequestCaptor.capture());
-    verify(egressEventAuditor).fireEgressEvent(event);
-
-    final CreateAlertRequest request = alertRequestCaptor.getValue();
-    assertThat(request.getDescription())
-        .contains("Terra Billing Project/Firecloud Namespace: " + NOT_FOUND_WORKSPACE_NAMESPACE);
-    assertThat(request.getDescription()).contains("Google Project Id: " + notFoundProjectName);
-    assertThat(request.getDescription())
-        .contains(
-            "https://workbench.researchallofus.org/admin/workspaces/"
-                + NOT_FOUND_WORKSPACE_NAMESPACE
-                + "/");
-    assertThat(request.getDescription())
-        .containsMatch(
-            Pattern.compile("\\s+Institution:\\s+Verily\\s+Life\\s+Sciences,\\s+Account\\s+Age:"));
-    assertThat(request.getAlias())
-        .isEqualTo(NOT_FOUND_WORKSPACE_NAMESPACE + " | all-of-us-" + dbUser1.getUserId());
-  }
-
-  @Test
-  public void testCreateEgressEventAlert_staleEvent() throws ApiException {
-    when(mockAlertApi.createAlert(any())).thenReturn(new SuccessResponse().requestId("12345"));
-
-    SumologicEgressEvent oldEgressEvent =
-        recentEgressEventForUser(dbUser1)
-            .timeWindowDuration(60 * 60L)
-            .timeWindowStart(NOW.minus(Duration.ofMinutes(125)).toEpochMilli());
-
-    egressEventService.handleEvent(oldEgressEvent);
-    verify(mockAlertApi).createAlert(alertRequestCaptor.capture());
-    verify(egressEventAuditor).fireEgressEvent(oldEgressEvent);
-
-    final CreateAlertRequest request = alertRequestCaptor.getValue();
-    assertThat(request.getMessage()).contains("[>60 mins old] High-egress event");
-    assertThat(request.getNote()).contains("[>60 mins old] Time window");
-
-    Iterable<DbEgressEvent> dbEvents = egressEventDao.findAll();
-    assertThat(dbEvents).hasSize(1);
-  }
-
-  @Test
-  public void testCreateEgressEventAlert_stalePersistedEvent_v2() {
-    workbenchConfig.featureFlags.enableEgressAlertingV2 = true;
-
+  public void testCreateEgressEventAlert_stalePersistedEvent() {
     SumologicEgressEvent oldEgressEvent =
         recentEgressEventForUser(dbUser1)
             .timeWindowDuration(60 * 60L)
@@ -326,9 +164,7 @@ public class EgressEventServiceTest {
   }
 
   @Test
-  public void testCreateEgressEventAlert_staleEventsMultiwindow_v2() {
-    workbenchConfig.featureFlags.enableEgressAlertingV2 = true;
-
+  public void testCreateEgressEventAlert_staleEventsMultiwindow() {
     SumologicEgressEvent oldEgressEvent =
         recentEgressEventForUser(dbUser1)
             .timeWindowDuration(60 * 60L)
@@ -352,9 +188,7 @@ public class EgressEventServiceTest {
   }
 
   @Test
-  public void testCreateEgressEventAlert_staleEventsDifferentUsers_v2() {
-    workbenchConfig.featureFlags.enableEgressAlertingV2 = true;
-
+  public void testCreateEgressEventAlert_staleEventsDifferentUsers() {
     SumologicEgressEvent oldEgressEvent =
         recentEgressEventForUser(dbUser1)
             .timeWindowDuration(60 * 60L)
@@ -371,7 +205,7 @@ public class EgressEventServiceTest {
 
     fakeClock.setInstant(NOW);
     egressEventService.handleEvent(oldEgressEvent);
-    verify(egressEventAuditor).fireEgressEvent(oldEgressEvent);
+    verify(mockEgressEventAuditor).fireEgressEvent(oldEgressEvent);
     verify(mockTaskQueueService).pushEgressEventTask(anyLong());
 
     Iterable<DbEgressEvent> dbEvents = egressEventDao.findAll();
@@ -379,9 +213,7 @@ public class EgressEventServiceTest {
   }
 
   @Test
-  public void testCreateEgressEventAlert_staleEventShortWindowPersisted_v2() {
-    workbenchConfig.featureFlags.enableEgressAlertingV2 = true;
-
+  public void testCreateEgressEventAlert_staleEventShortWindowPersisted() {
     SumologicEgressEvent oldEgressEvent =
         recentEgressEventForUser(dbUser1)
             // > 2 windows into the past
@@ -399,7 +231,7 @@ public class EgressEventServiceTest {
 
     fakeClock.setInstant(NOW);
     egressEventService.handleEvent(oldEgressEvent);
-    verify(egressEventAuditor).fireEgressEvent(oldEgressEvent);
+    verify(mockEgressEventAuditor).fireEgressEvent(oldEgressEvent);
     verify(mockTaskQueueService).pushEgressEventTask(anyLong());
 
     Iterable<DbEgressEvent> dbEvents = egressEventDao.findAll();
