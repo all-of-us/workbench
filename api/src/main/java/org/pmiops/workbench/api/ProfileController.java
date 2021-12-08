all-of-us/workbench

commit 7c375039a15ee4a5c5c76ab42774c63f6c3536fc

[risk=low][no ticket] make updateUserAccessTiers() public to avoid excessive user saves (#6018)
* change updateUserWithRetries() to updateUserAccessTiers() where appropriate

api/src/main/java/org/pmiops/workbench/profile/ProfileService.java
@@ -252,11 +252,10 @@ public class ProfileService {
     user.setDemographicSurvey(dbDemographicSurvey);
 
     // update institution and synchronize access tiers
-    final DbUser updatedUser =
-        userService.updateUserWithRetries(
-            u -> upsertAffiliation(u, updatedProfile.getVerifiedInstitutionalAffiliation()),
-            user,
-            agent);
+    DbUser updatedUser =
+        upsertAffiliation(user, updatedProfile.getVerifiedInstitutionalAffiliation());
+    updatedUser = userService.updateUserAccessTiers(updatedUser, agent);
+
     final Profile appliedUpdatedProfile = getProfile(updatedUser);
     profileAuditor.fireUpdateAction(previousProfile, appliedUpdatedProfile, agent);
     return updatedUser;

api/src/test/java/org/pmiops/workbench/profile/ProfileServiceTest.java
@@ -370,7 +370,7 @@ public class ProfileServiceTest {
             newAffiliation, mockInstitutionService))
         .thenReturn(dbVerifiedInstitutionalAffiliation);
 
-    when(mockUserService.updateUserWithRetries(any(), any(), any())).thenReturn(user);
+    when(mockUserService.updateUserAccessTiers(any(), any())).thenReturn(user);
 
     profileService.updateProfile(
         user, Agent.asAdmin(loggedInUser), updatedProfile, previousProfile);
@@ -428,7 +428,7 @@ public class ProfileServiceTest {
     targetUser.setGivenName("John");
     targetUser.setFamilyName("Doe");
 
-    when(mockUserService.updateUserWithRetries(any(), any(), any())).thenReturn(targetUser);
+    when(mockUserService.updateUserAccessTiers(any(), any())).thenReturn(targetUser);
 
     profileService.updateProfile(
         targetUser, Agent.asAdmin(loggedInUser), updatedProfile, previousProfile);

api/src/main/java/org/pmiops/workbench/api/CloudTaskUserController.java
@@ -6,7 +6,6 @@ import java.io.IOException;
 import java.util.List;
 import java.util.Optional;
 import java.util.Set;
-import java.util.function.Function;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import java.util.stream.Collectors;
@@ -139,7 +138,7 @@ public class CloudTaskUserController implements CloudTaskUserApiDelegate {
         // should result in access expiration changes here, but this is also a backstop for ensuring
         // the database is consistent with our access tier groups, e.g. due to partial system
         // failures or bugs.
-        userService.updateUserWithRetries(Function.identity(), user, Agent.asSystem());
+        userService.updateUserAccessTiers(user, Agent.asSystem());
       } catch (WorkbenchException e) {
         log.log(Level.SEVERE, "failed to synchronize access for user " + user.getUsername(), e);
         errorCount++;

api/src/main/java/org/pmiops/workbench/db/dao/UserService.java
@@ -18,7 +18,18 @@ import org.pmiops.workbench.model.UserAccessExpiration;
 import org.springframework.data.domain.Sort;
 
 public interface UserService {
+  /**
+   * Updates a user record with a modifier function.
+   *
+   * <p>Ensures that the data access tiers for the user reflect the state of other fields on the
+   * user; handles conflicts with concurrent updates by retrying.
+   */
   DbUser updateUserWithRetries(Function<DbUser, DbUser> userModifier, DbUser dbUser, Agent agent);
+
+  /**
+   * Ensures that the data access tiers for the user reflect the state of other fields on the user
+   */
+  DbUser updateUserAccessTiers(DbUser dbUser, Agent agent);
 
   DbUser createServiceAccountUser(String email);
 

api/src/main/java/org/pmiops/workbench/db/dao/UserServiceImpl.java
@@ -166,7 +166,7 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
     int statementClosedCount = 0;
     while (true) {
       dbUser = userModifier.apply(dbUser);
-      updateUserAccessTiers(dbUser, agent);
+      dbUser = updateUserAccessTiers(dbUser, agent);
       try {
         return userDao.save(dbUser);
       } catch (ObjectOptimisticLockingFailureException e) {
@@ -215,7 +215,11 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
     }
   }
 
-  private void updateUserAccessTiers(DbUser dbUser, Agent agent) {
+  /**
+   * Ensures that the data access tiers for the user reflect the state of other fields on the user
+   */
+  @Override
+  public DbUser updateUserAccessTiers(DbUser dbUser, Agent agent) {
     final List<DbAccessTier> previousAccessTiers = accessTierService.getAccessTiersForUser(dbUser);
 
     final List<DbAccessTier> newAccessTiers = getUserAccessTiersList(dbUser);
@@ -231,6 +235,8 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
     final List<DbAccessTier> tiersForRemoval =
         Lists.difference(accessTierService.getAllTiers(), newAccessTiers);
     tiersForRemoval.forEach(tier -> accessTierService.removeUserFromTier(dbUser, tier));
+
+    return dbUser;
   }
 
   // missing ERA_COMMONS (special-cased below)
@@ -628,16 +634,12 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
           BADGE_BY_COMPLIANCE_MODULE.keySet().stream()
               .collect(Collectors.toMap(Function.identity(), determineCompletionTime));
 
-      return updateUserWithRetries(
-          u -> {
-            completionTimes.forEach(
-                (accessModuleName, timestamp) ->
-                    accessModuleService.updateCompletionTime(
-                        u, accessModuleName, timestamp.orElse(null)));
-            return u;
-          },
-          dbUser,
-          agent);
+      completionTimes.forEach(
+          (accessModuleName, timestamp) ->
+              accessModuleService.updateCompletionTime(
+                  dbUser, accessModuleName, timestamp.orElse(null)));
+
+      return updateUserAccessTiers(dbUser, agent);
     } catch (NumberFormatException e) {
       log.severe("Incorrect date expire format from Moodle");
       throw e;
@@ -742,27 +744,23 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
       return targetUser;
     }
 
-    return updateUserWithRetries(
-        user -> {
-          if (isEnrolledIn2FA) {
-            final boolean needsDbCompletionUpdate =
-                accessModuleService
-                    .getAccessModuleStatus(user, AccessModuleName.TWO_FACTOR_AUTH)
-                    .map(status -> status.getCompletionEpochMillis() == null)
-                    .orElse(true);
+    if (isEnrolledIn2FA) {
+      final boolean needsDbCompletionUpdate =
+          accessModuleService
+              .getAccessModuleStatus(targetUser, AccessModuleName.TWO_FACTOR_AUTH)
+              .map(status -> status.getCompletionEpochMillis() == null)
+              .orElse(true);
 
-            if (needsDbCompletionUpdate) {
-              Timestamp timestamp = new Timestamp(clock.instant().toEpochMilli());
-              accessModuleService.updateCompletionTime(
-                  user, AccessModuleName.TWO_FACTOR_AUTH, timestamp);
-            }
-          } else {
-            accessModuleService.updateCompletionTime(user, AccessModuleName.TWO_FACTOR_AUTH, null);
-          }
-          return user;
-        },
-        targetUser,
-        agent);
+      if (needsDbCompletionUpdate) {
+        Timestamp timestamp = new Timestamp(clock.instant().toEpochMilli());
+        accessModuleService.updateCompletionTime(
+            targetUser, AccessModuleName.TWO_FACTOR_AUTH, timestamp);
+      }
+    } else {
+      accessModuleService.updateCompletionTime(targetUser, AccessModuleName.TWO_FACTOR_AUTH, null);
+    }
+
+    return updateUserAccessTiers(targetUser, agent);
   }
 
   @Override
@@ -773,22 +771,18 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
       return targetUser;
     }
 
-    return updateUserWithRetries(
-        user -> {
-          // convert Integer to int to prevent a NPE from comparison-unboxing
-          final int signedVersionForComparison =
-              Optional.ofNullable(signedDuccVersion)
-                  // null is invalid, so convert to a known-invalid int
-                  .orElse(accessModuleService.getCurrentDuccVersion() - 1);
+    // convert Integer to int to prevent a NPE from comparison-unboxing
+    final int signedVersionForComparison =
+        Optional.ofNullable(signedDuccVersion)
+            // null is invalid, so convert to a known-invalid int
+            .orElse(accessModuleService.getCurrentDuccVersion() - 1);
 
-          if (signedVersionForComparison != accessModuleService.getCurrentDuccVersion()) {
-            accessModuleService.updateCompletionTime(
-                user, AccessModuleName.DATA_USER_CODE_OF_CONDUCT, null);
-          }
-          return user;
-        },
-        targetUser,
-        agent);
+    if (signedVersionForComparison != accessModuleService.getCurrentDuccVersion()) {
+      accessModuleService.updateCompletionTime(
+          targetUser, AccessModuleName.DATA_USER_CODE_OF_CONDUCT, null);
+    }
+
+    return updateUserAccessTiers(targetUser, agent);
   }
 
   @Override
@@ -881,30 +875,22 @@ public class UserServiceImpl implements UserService, GaugeDataCollector {
   /** Confirm that a user's profile is up to date, for annual renewal compliance purposes. */
   @Override
   public DbUser confirmProfile(DbUser dbUser) {
-    return updateUserWithRetries(
-        user -> {
-          Timestamp timestamp = new Timestamp(clock.instant().toEpochMilli());
-          accessModuleService.updateCompletionTime(
-              user, AccessModuleName.PROFILE_CONFIRMATION, timestamp);
-          return user;
-        },
-        dbUser,
-        Agent.asUser(dbUser));
+    Timestamp timestamp = new Timestamp(clock.instant().toEpochMilli());
+    accessModuleService.updateCompletionTime(
+        dbUser, AccessModuleName.PROFILE_CONFIRMATION, timestamp);
+
+    return updateUserAccessTiers(dbUser, Agent.asUser(dbUser));
   }
 
   /** Confirm that a user has either reported any AoU-related publications, or has none. */
   @Override
   public DbUser confirmPublications() {
     final DbUser dbUser = userProvider.get();
-    return updateUserWithRetries(
-        user -> {
-          Timestamp timestamp = new Timestamp(clock.instant().toEpochMilli());
-          accessModuleService.updateCompletionTime(
-              user, AccessModuleName.PUBLICATION_CONFIRMATION, timestamp);
-          return user;
-        },
-        dbUser,
-        Agent.asUser(dbUser));
+    Timestamp timestamp = new Timestamp(clock.instant().toEpochMilli());
+    accessModuleService.updateCompletionTime(
+        dbUser, AccessModuleName.PUBLICATION_CONFIRMATION, timestamp);
+
+    return updateUserAccessTiers(dbUser, Agent.asUser(dbUser));
   }
 
   /** Send an Access Renewal Expiration or Warning email to the user, if appropriate */

api/src/test/java/org/pmiops/workbench/access/UserServiceAccessTest.java
@@ -69,8 +69,10 @@ import org.springframework.test.annotation.DirtiesContext;
 /**
  * Tests to cover access change determinations by executing {@link
  * UserService#updateUserWithRetries(java.util.function.Function,
- * org.pmiops.workbench.db.model.DbUser, org.pmiops.workbench.actionaudit.Agent)} with different
- * configurations, which ultimately executes the private method {
+ * org.pmiops.workbench.db.model.DbUser, org.pmiops.workbench.actionaudit.Agent)} or {@link
+ * UserService#updateUserAccessTiers(org.pmiops.workbench.db.model.DbUser,
+ * org.pmiops.workbench.actionaudit.Agent)} with different configurations, which ultimately executes
+ * the private method {
  * UserServiceImpl#shouldGrantUserTierAccess(org.pmiops.workbench.db.model.DbUser, List, String)} to
  * make this determination.
  */
@@ -202,7 +204,7 @@ public class UserServiceAccessTest {
   public void test_updateUserWithRetries_never_registered() {
     assertThat(userAccessTierDao.findAll()).isEmpty();
 
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     // the user has never been registered so they have no DbUserAccessTier entry
 
@@ -240,20 +242,13 @@ public class UserServiceAccessTest {
 
     // Simulate time passing, user is no longer compliant
     advanceClockDays(2);
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierDisabled(dbUser);
 
     // Simulate user filling out DUA, becoming compliant again
-    dbUser =
-        updateUserWithRetries(
-            user -> {
-              accessModuleService.updateCompletionTime(
-                  dbUser,
-                  AccessModuleName.DATA_USER_CODE_OF_CONDUCT,
-                  new Timestamp(PROVIDED_CLOCK.millis()));
-              return user;
-            });
-
+    accessModuleService.updateCompletionTime(
+        dbUser, AccessModuleName.DATA_USER_CODE_OF_CONDUCT, new Timestamp(PROVIDED_CLOCK.millis()));
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
   }
 
@@ -951,14 +946,14 @@ public class UserServiceAccessTest {
     // Now make user eRA not complete, expect user removed from Registered tier;
     accessModuleService.updateBypassTime(dbUser.getUserId(), AccessModule.ERA_COMMONS, false);
     accessModuleService.updateCompletionTime(dbUser, AccessModuleName.ERA_COMMONS, null);
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierDisabled(dbUser);
 
     // Make eRA is optional for that institution, verify user become registered
     institutionService.updateInstitution(
         institution.getShortName(),
         institution.tierConfigs(ImmutableList.of(rtTierConfig.eraRequired(false))));
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
   }
 
@@ -974,12 +969,12 @@ public class UserServiceAccessTest {
         institution.tierConfigs(ImmutableList.of(rtTierConfig.eraRequired(false))));
     accessModuleService.updateBypassTime(dbUser.getUserId(), AccessModule.ERA_COMMONS, false);
     accessModuleService.updateCompletionTime(dbUser, AccessModuleName.ERA_COMMONS, null);
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
 
     // Now login.gov flag disabled, eRA is always required.
     providedWorkbenchConfig.access.enableRasLoginGovLinking = false;
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierDisabled(dbUser);
   }
 
@@ -995,7 +990,7 @@ public class UserServiceAccessTest {
         institution.tierConfigs(ImmutableList.of(rtTierConfig.eraRequired(true))));
     accessModuleService.updateBypassTime(dbUser.getUserId(), AccessModule.ERA_COMMONS, false);
     accessModuleService.updateCompletionTime(dbUser, AccessModuleName.ERA_COMMONS, null);
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
   }
 
@@ -1009,13 +1004,13 @@ public class UserServiceAccessTest {
     // Incomplete RAS module, expect user removed from Registered tier;
     accessModuleService.updateBypassTime(
         dbUser.getUserId(), AccessModule.RAS_LINK_LOGIN_GOV, false);
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierDisabled(dbUser);
 
     // Complete RAS Linking, verify user become registered
     accessModuleService.updateCompletionTime(
         dbUser, AccessModuleName.RAS_LOGIN_GOV, new Timestamp(PROVIDED_CLOCK.millis()));
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
   }
 
@@ -1030,7 +1025,7 @@ public class UserServiceAccessTest {
     // Incomplete RAS module, expect user is still Registered;
     accessModuleService.updateBypassTime(
         dbUser.getUserId(), AccessModule.RAS_LINK_LOGIN_GOV, false);
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
   }
 
@@ -1047,13 +1042,13 @@ public class UserServiceAccessTest {
     // Incomplete RAS module, expect user removed from Registered tier;
     accessModuleService.updateBypassTime(
         dbUser.getUserId(), AccessModule.RAS_LINK_LOGIN_GOV, false);
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierDisabled(dbUser);
 
     // Complete RAS Linking, verify user become registered
     accessModuleService.updateCompletionTime(
         dbUser, AccessModuleName.RAS_LOGIN_GOV, new Timestamp(PROVIDED_CLOCK.millis()));
-    updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
     assertRegisteredTierEnabled(dbUser);
   }
 
@@ -1081,13 +1076,9 @@ public class UserServiceAccessTest {
 
     dbUser = completeRTAndCTRequirements(dbUser);
 
-    dbUser =
-        updateUserWithRetries(
-            user -> {
-              accessModuleService.updateBypassTime(
-                  dbUser.getUserId(), AccessModule.CT_COMPLIANCE_TRAINING, false);
-              return userDao.save(user);
-            });
+    accessModuleService.updateBypassTime(
+        dbUser.getUserId(), AccessModule.CT_COMPLIANCE_TRAINING, false);
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertUserNotInAccessTier(dbUser, controlledTier);
@@ -1107,13 +1098,8 @@ public class UserServiceAccessTest {
     ctTierConfig.setEraRequired(true);
     updateInstitutionTier(ctTierConfig);
 
-    dbUser =
-        updateUserWithRetries(
-            user -> {
-              accessModuleService.updateBypassTime(
-                  user.getUserId(), AccessModule.ERA_COMMONS, false);
-              return userDao.save(user);
-            });
+    accessModuleService.updateBypassTime(dbUser.getUserId(), AccessModule.ERA_COMMONS, false);
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertUserNotInAccessTier(dbUser, controlledTier);
@@ -1129,13 +1115,8 @@ public class UserServiceAccessTest {
     ctTierConfig.setEraRequired(false);
     updateInstitutionTier(ctTierConfig);
 
-    dbUser =
-        updateUserWithRetries(
-            user -> {
-              accessModuleService.updateBypassTime(
-                  user.getUserId(), AccessModule.ERA_COMMONS, false);
-              return userDao.save(user);
-            });
+    accessModuleService.updateBypassTime(dbUser.getUserId(), AccessModule.ERA_COMMONS, false);
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
@@ -1150,14 +1131,14 @@ public class UserServiceAccessTest {
     dbUser = completeRTAndCTRequirements(dbUser);
     ctTierConfig.setEraRequired(true);
     updateInstitutionTier(ctTierConfig);
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
 
     ctTierConfig.setEraRequired(false);
     updateInstitutionTier(ctTierConfig);
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
@@ -1171,7 +1152,7 @@ public class UserServiceAccessTest {
     ctTierConfig.setEmailDomains(Arrays.asList("fakeDomain.com"));
     updateInstitutionTier(ctTierConfig);
 
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertUserNotInAccessTier(dbUser, controlledTier);
@@ -1184,7 +1165,7 @@ public class UserServiceAccessTest {
     ctTierConfig.setEmailDomains(Arrays.asList("domain.com"));
     updateInstitutionTier(ctTierConfig);
 
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
@@ -1202,7 +1183,7 @@ public class UserServiceAccessTest {
                 tier.getAccessTierShortName().equals(AccessTierService.CONTROLLED_TIER_SHORT_NAME));
     institutionService.updateInstitution(institution.getShortName(), institution);
 
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertUserNotInAccessTier(dbUser, controlledTier);
@@ -1218,18 +1199,13 @@ public class UserServiceAccessTest {
     ctTierConfig.setEraRequired(true);
     updateInstitutionTier(ctTierConfig);
 
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
 
-    dbUser =
-        updateUserWithRetries(
-            user -> {
-              accessModuleService.updateBypassTime(
-                  user.getUserId(), AccessModule.ERA_COMMONS, false);
-              return userDao.save(user);
-            });
+    accessModuleService.updateBypassTime(dbUser.getUserId(), AccessModule.ERA_COMMONS, false);
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
@@ -1241,18 +1217,14 @@ public class UserServiceAccessTest {
     assertThat(userAccessTierDao.findAll()).isEmpty();
 
     dbUser = completeRTAndCTRequirements(dbUser);
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
 
-    dbUser =
-        updateUserWithRetries(
-            user -> {
-              accessModuleService.updateBypassTime(
-                  user.getUserId(), AccessModule.CT_COMPLIANCE_TRAINING, false);
-              return userDao.save(user);
-            });
+    accessModuleService.updateBypassTime(
+        dbUser.getUserId(), AccessModule.CT_COMPLIANCE_TRAINING, false);
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertControlledTierEnabled(dbUser);
@@ -1267,7 +1239,7 @@ public class UserServiceAccessTest {
     TestMockFactory.removeControlledTierForTests(accessTierDao);
     removeCTConfigFromInstitution();
 
-    dbUser = updateUserWithRetries(Function.identity());
+    dbUser = updateUserAccessTiers();
 
     assertRegisteredTierEnabled(dbUser);
     assertUserNotInAccessTier(dbUser, controlledTier);
@@ -1315,9 +1287,14 @@ public class UserServiceAccessTest {
     assertRegisteredTierDisabled(dbUser);
   }
 
-  // we can trim the signature since we always call this in the same way
+  // we can trim the signatures since we always call these in the same way
+
   private DbUser updateUserWithRetries(Function<DbUser, DbUser> userModifier) {
     return userService.updateUserWithRetries(userModifier, dbUser, Agent.asUser(dbUser));
+  }
+
+  private DbUser updateUserAccessTiers() {
+    return userService.updateUserAccessTiers(dbUser, Agent.asUser(dbUser));
   }
 
   private void updateInstitutionTier(InstitutionTierConfig updatedTierConfig) {

api/src/test/java/org/pmiops/workbench/api/CloudTaskUserControllerTest.java
@@ -110,7 +110,7 @@ public class CloudTaskUserControllerTest {
 
     // normally we would expect the userService sync methods to add to this count, but userService
     // is mocked so we only see the direct calls from synchronizeUserAccess(), one per user
-    verify(mockUserService, times(2)).updateUserWithRetries(any(), any(), any());
+    verify(mockUserService, times(2)).updateUserAccessTiers(any(), any());
     verifyNoMoreInteractions(mockUserService);
   }
 }

commit 883a4324b2125f405983397c8451bc2670b02e2c

[risk=low][no ticket] Actually update the user DB object when calling updateProfile() (#6023)
* fix the error introduced by #6018

api/src/main/java/org/pmiops/workbench/api/ProfileController.java
@@ -470,7 +470,7 @@ public class ProfileController implements ProfileApiDelegate {
       throw new BadRequestException("Cannot update Verified Institutional Affiliation");
     }
 
-    DbUser updatedUser =
+    final DbUser updatedUser =
         profileService.updateProfile(user, Agent.asUser(user), updatedProfile, previousProfile);
     userService.confirmProfile(updatedUser);
 

api/src/main/java/org/pmiops/workbench/profile/ProfileService.java
@@ -251,10 +251,12 @@ public class ProfileService {
     }
     user.setDemographicSurvey(dbDemographicSurvey);
 
-    // update institution and synchronize access tiers
+    // save user, update institution, and synchronize access tiers
     DbUser updatedUser =
-        upsertAffiliation(user, updatedProfile.getVerifiedInstitutionalAffiliation());
-    updatedUser = userService.updateUserAccessTiers(updatedUser, agent);
+        userService.updateUserWithRetries(
+            u -> upsertAffiliation(u, updatedProfile.getVerifiedInstitutionalAffiliation()),
+            user,
+            agent);
 
     final Profile appliedUpdatedProfile = getProfile(updatedUser);
     profileAuditor.fireUpdateAction(previousProfile, appliedUpdatedProfile, agent);

api/src/test/java/org/pmiops/workbench/profile/ProfileServiceTest.java
@@ -370,7 +370,7 @@ public class ProfileServiceTest {
             newAffiliation, mockInstitutionService))
         .thenReturn(dbVerifiedInstitutionalAffiliation);
 
-    when(mockUserService.updateUserAccessTiers(any(), any())).thenReturn(user);
+    when(mockUserService.updateUserWithRetries(any(), any(), any())).thenReturn(user);
 
     profileService.updateProfile(
         user, Agent.asAdmin(loggedInUser), updatedProfile, previousProfile);
@@ -428,7 +428,7 @@ public class ProfileServiceTest {
     targetUser.setGivenName("John");
     targetUser.setFamilyName("Doe");
 
-    when(mockUserService.updateUserAccessTiers(any(), any())).thenReturn(targetUser);
+    when(mockUserService.updateUserWithRetries(any(), any(), any())).thenReturn(targetUser);
 
     profileService.updateProfile(
         targetUser, Agent.asAdmin(loggedInUser), updatedProfile, previousProfile);