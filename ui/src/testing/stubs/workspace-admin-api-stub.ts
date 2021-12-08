8158a36e71dc90b921b654b8d2b870588810f86d
CH Albach <calbach@google.com>
2021-12-07 16:09:38 -0800

    lint

--- ui/src/testing/stubs/workspace-admin-api-stub.ts
+++ ui/src/testing/stubs/workspace-admin-api-stub.ts
@@ -1,6 +1,6 @@
-import { EmptyResponse, ResearchPurposeReviewRequest, WorkspaceAdminApi, WorkspaceListResponse } from "generated/fetch";
-import { stubNotImplementedError } from "./stub-utils";
-import { workspaceStubs } from "./workspaces";
+import { EmptyResponse, ResearchPurposeReviewRequest, WorkspaceAdminApi, WorkspaceListResponse } from 'generated/fetch';
+import { stubNotImplementedError } from './stub-utils';
+import { workspaceStubs } from './workspaces';
 
 
 export class WorkspaceAdminApiStub extends WorkspaceAdminApi {
