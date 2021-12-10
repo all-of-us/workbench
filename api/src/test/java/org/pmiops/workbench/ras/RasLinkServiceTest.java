0408bb856bb812606b2c270ab483931a8f12f48d
yonghao <yyhao1@gmail.com>
2021-12-10 13:36:43 -0500

    tmp

--- api/src/test/java/org/pmiops/workbench/ras/RasLinkServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/ras/RasLinkServiceTest.java
@@ -279,7 +279,7 @@ public class RasLinkServiceTest {
     if (!dbAccessModule.isPresent()) {
       assertThat(timestamp).isNull();
     } else {
-      assertThat(dbAccessModule.get().getCompletionTime()).isEqualTo(timestamp);
+      assertThat(dbAccessModule.get().getCompletionTime()).isEqualTo(timestamp.getTime());
     }
   }
 }
