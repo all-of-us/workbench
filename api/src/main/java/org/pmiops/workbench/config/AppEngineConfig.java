5ee0cd5d19f05c70e9fd1b1a0a7330741aeb5e99
yonghao <yyhao1@gmail.com>
2021-12-14 12:22:51 -0500

    tmp

--- api/src/main/java/org/pmiops/workbench/config/AppEngineConfig.java
+++ api/src/main/java/org/pmiops/workbench/config/AppEngineConfig.java
@@ -2,14 +2,37 @@ package org.pmiops.workbench.config;
 
 import com.google.api.client.extensions.appengine.http.UrlFetchTransport;
 import com.google.api.client.http.HttpTransport;
+import com.google.api.client.http.javanet.NetHttpTransport;
+import java.util.logging.Level;
+import java.util.logging.Logger;
+import javax.inject.Provider;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 
 @Configuration
 public class AppEngineConfig {
+  private static final Logger log = Logger.getLogger(AppEngineConfig.class.getName());
+
+/*  @Bean
+  HttpTransport httpTransport(Provider<WorkbenchConfig> workbenchConfigProvider) {
+    // UrlFetchTransport only works in Appengine environment, when running local server, use
+    // NetHttpTransport
+    // See
+    // https://github.com/googleapis/google-http-java-client/blob/main/google-http-client/src/main/java/com/google/api/client/http/HttpTransport.java#L31L59
+    if (workbenchConfigProvider.get().server.shortName.equals("Local")) {
+      log.log(Level.INFO, "Use NetHttpTransport in local server.");
+      return new NetHttpTransport();
+    } else {
+      return UrlFetchTransport.getDefaultInstance();
+    }
+  }*/
 
   @Bean
   HttpTransport httpTransport() {
-    return UrlFetchTransport.getDefaultInstance();
+    // UrlFetchTransport only works in Appengine environment, when running local server, use
+    // NetHttpTransport
+    // See
+    // https://github.com/googleapis/google-http-java-client/blob/main/google-http-client/src/main/java/com/google/api/client/http/HttpTransport.java#L31L59
+      return new NetHttpTransport();
   }
 }
