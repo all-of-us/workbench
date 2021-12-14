3acfa22c5000eca0565439a8711397bde157d6d0
chavanr2019 <rohini.chavan@vumc.org>
2021-12-14 16:54:32 -0600

    correction

--- e2e/app/page/admin-workspace-page.ts
+++ e2e/app/page/admin-workspace-page.ts
@@ -64,7 +64,7 @@ export default class WorkspaceAdminPage extends AuthenticatedPage {
     return textContent;
   }
 
-
+  
   getCloudStorageTable(): Table {
     const selector = '//table[@class="p-datatable-scrollable-header-table"]';
     return new Table(this.page, selector);
