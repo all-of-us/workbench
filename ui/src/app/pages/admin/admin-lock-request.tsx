a45037c722be498ca26e4d47db3f4b98661d7234
Joel Thibault <thibault@broad.dev>
2021-12-14 11:15:57 -0500

    cleanup

--- ui/src/app/pages/admin/admin-lock-request.tsx
+++ ui/src/app/pages/admin/admin-lock-request.tsx
@@ -10,6 +10,7 @@ import {workspaceAdminApi} from 'app/services/swagger-fetch-clients';
 import {useState} from 'react';
 import {TooltipTrigger} from 'app/components/popups';
 import colors from 'app/styles/colors';
+import {isBlank} from 'app/utils';
 
 const MIN_REASON = 10;
 const MAX_REASON = 4000;
@@ -25,17 +26,16 @@ export const AdminLockRequest = (props: Props) => {
   const [requestDate, setRequestDate] = useState(new Date());
   const [apiError, setApiError] = useState(false);
 
-  const enableLockButton = !apiError
-    && requestReason?.length >= MIN_REASON && requestReason?.length <= MAX_REASON
-    && requestDate?.toString() !== '' && !isNaN(requestDate.valueOf());
-
+  const invalidReason = !requestReason || requestReason.length < MIN_REASON || requestReason.length > MAX_REASON;
+  const invalidDate = !requestDate || isBlank(requestDate.toString()) || isNaN(requestDate.valueOf());
+  const lockButtonDisabled = apiError || invalidReason || invalidDate;
 
   const getToolTipContent = apiError
     ? 'Error occurred while Locking Workspace'
     : <div>Required to lock workspace:
       <ul>
-        <li>Request Reason (minimum length {MIN_REASON}, maximum {MAX_REASON})</li>
-        <li>Valid Request Date (in YYYY-MM-DD Format)</li>
+        {invalidReason && <li>Request Reason (minimum length {MIN_REASON}, maximum {MAX_REASON})</li>}
+        {invalidDate && <li>Valid Request Date (in YYYY-MM-DD Format)</li>}
       </ul></div>;
 
   const onLockWorkspace = () => {
@@ -66,12 +66,12 @@ export const AdminLockRequest = (props: Props) => {
        {/* Text area to enter the reason for locking workspace */}
        <div>
          <label style={{fontWeight: 'bold', color: colors.primary}}>
-           Enter reason for researcher on why workspace access is locked
+           Enter reason for researchers on why workspace access is locked
          </label>
        </div>
        <div style={{paddingTop: '0.3rem', paddingBottom: '1rem'}}>
          <label style={{color: colors.primary, paddingBottom: '0.3rem'}}>
-           <i>Any message in the input box will automatically be sent to researcher when the
+           <i>Any message in the input box will automatically be sent to researchers when the
              workspace is locked</i>
          </label>
          <TextAreaWithLengthValidationMessage
@@ -92,7 +92,7 @@ export const AdminLockRequest = (props: Props) => {
        {/* Locking workspace request Date*/}
        <div>
          <div style={{fontWeight: 'bold', color: colors.primary, paddingBottom: '0.3rem'}}>
-           Add date of RAB request date
+           Add date of RAB request
          </div>
          <DatePicker
              value={requestDate}
@@ -111,8 +111,8 @@ export const AdminLockRequest = (props: Props) => {
        <Button type='secondary' style={{marginRight: '0.5rem'}} onClick={() => props.onCancel()}>
          CANCEL
        </Button>
-       <TooltipTrigger content={getToolTipContent} disabled={enableLockButton}>
-         <Button type='primary' onClick={() => onLockWorkspace()} disabled={!enableLockButton}>
+       <TooltipTrigger content={getToolTipContent} disabled={!lockButtonDisabled}>
+         <Button type='primary' onClick={() => onLockWorkspace()} disabled={lockButtonDisabled}>
            LOCK WORKSPACE
          </Button>
        </TooltipTrigger>
