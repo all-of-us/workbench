d8aacf1844b07e7568ef257f46118705126d4164
CH Albach <calbach@google.com>
2022-01-07 16:05:50 -0800

    lint fix

--- e2e/utils/notebook-preview-utils.ts
+++ e2e/utils/notebook-preview-utils.ts
@@ -5,8 +5,7 @@ import { waitForFn } from 'utils/waits-utils';
 export async function waitForPreviewCellsRendered(previewFrame: Frame): Promise<void> {
   await waitForFn(
     async () => {
-      return (await previewFrame.$$('.jp-CodeCell')).length ===
-        (await previewFrame.$$('.jp-CodeMirrorEditor')).length;
+      return (await previewFrame.$$('.jp-CodeCell')).length === (await previewFrame.$$('.jp-CodeMirrorEditor')).length;
     },
     1000,
     30000
@@ -18,5 +17,4 @@ export async function getFormattedPreviewCode(previewFrame: Frame): Promise<stri
   await previewFrame.waitForSelector(css, { visible: true });
   const elements = await previewFrame.$$(css);
   return Promise.all(elements.map(async (content) => await getPropValue<string>(content, 'textContent')));
-
 }
