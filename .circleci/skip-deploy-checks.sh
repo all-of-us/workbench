05d08a8e68b46eb62afe5990f98404d6bd3b96e4
aweng98 <bweng@broadinstitute.org>
2022-01-12 13:11:13 -0500

    new skip-deploy script

---
+++ .circleci/skip-deploy-checks.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+set -xv
+
+# Applying to main branch only: Exiting deploy UI and API jobs early if last commit message contains "skip-deploy" string.
+COMMIT_MESSAGE=$(git log -1 --pretty=format:"%s")
+
+# Double comma is "Parameter Expansion". It converts string to lowercase letters.
+if [[ "${COMMIT_MESSAGE,,}" == *"skip-deploy"* ]]; then
+  echo "[skip-deploy] found in commit message. Deploy job will not run."
+  circleci-agent step halt
+fi
