bf24e26518559f17dde04e10d08e8618adf0c8b9
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro-2.local>
2022-01-17 11:32:00 -0600

    RW-7703 prevent edit of array search items

--- ui/src/app/cohort-search/search-group-item/search-group-item.component.tsx
+++ ui/src/app/cohort-search/search-group-item/search-group-item.component.tsx
@@ -338,8 +338,7 @@ export const SearchGroupItem = withCurrentWorkspace()(
     launchSearch() {
       triggerEvent('Edit', 'Click', 'Snowman - Edit Criteria - Cohort Builder');
       const { groupId, item, setSearchContext, role } = this.props;
-      // items of type 'DECEASED` cannot be edited
-      if (item.searchParameters[0].type !== CriteriaType.DECEASED.toString()) {
+      if (!this.preventItemEdit) {
         const _item = JSON.parse(JSON.stringify(item));
         const { searchParameters } = _item;
         const domain = _item.type;
@@ -418,17 +417,22 @@ export const SearchGroupItem = withCurrentWorkspace()(
       }
     }
 
+    get preventItemEdit() {
+      const {
+        item: { searchParameters, type },
+      } = this.props;
+      return type === Domain.ARRAYDATA.toString() ||
+        type === Domain.FITBIT.toString() ||
+        type === Domain.WHOLEGENOMEVARIANT.toString() ||
+        (searchParameters[0] && searchParameters[0].type === CriteriaType.DECEASED.toString());
+    }
+
     render() {
       const {
-        item: { count, modifiers, name, searchParameters, status, type },
+        item: { count, modifiers, name, searchParameters, status },
       } = this.props;
       const { error, loading, paramListOpen, renaming } = this.state;
       const showCount = !loading && status !== 'hidden' && count !== undefined;
-      const hideEdit =
-        type === Domain.FITBIT.toString() ||
-        type === Domain.WHOLEGENOMEVARIANT.toString() ||
-        (searchParameters[0] &&
-          searchParameters[0].type === CriteriaType.DECEASED.toString());
       const actionItems = [
         {
           label: 'Edit criteria name',
@@ -437,7 +441,7 @@ export const SearchGroupItem = withCurrentWorkspace()(
         {
           label: 'Edit criteria',
           command: () => this.launchSearch(),
-          style: hideEdit ? { display: 'none' } : {},
+          style: this.preventItemEdit ? { display: 'none' } : {},
         },
         {
           label: 'Suppress criteria from total count',
