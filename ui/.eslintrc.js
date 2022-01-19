29bf9f508b8f4466c73acaee18de06a606fbbf39
Joel Thibault <thibault@broad.dev>
2022-01-19 11:31:07 -0500

    enable @typescript-eslint/no-unused-vars

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -16,13 +16,13 @@ module.exports = {
     ecmaVersion: 6,
     sourceType: 'module',
     ecmaFeatures: {
-      jsx: true
-    }
+      jsx: true,
+    },
   },
   settings: {
     react: {
-      version: 'detect'
-    }
+      version: 'detect',
+    },
   },
 
   // Some of these rules were ported over from our common-ui/tslint.json file
@@ -34,8 +34,8 @@ module.exports = {
     /* Code Quality */
 
     'constructor-super': 'warn',
-    'curly': 'warn',
-    'eqeqeq': ['warn', 'always', {'null': 'ignore'}],
+    curly: 'warn',
+    eqeqeq: ['warn', 'always', { null: 'ignore' }],
     'guard-for-in': 'warn',
     'no-bitwise': 'warn',
     'no-caller': 'warn',
@@ -45,11 +45,11 @@ module.exports = {
     'no-fallthrough': 'warn', // For switch statements
     'no-irregular-whitespace': 'warn',
     'no-new-wrappers': 'warn',
-    'no-restricted-imports': ['error', {paths: ['rxjs'], patterns: ['../']}],
+    'no-restricted-imports': ['error', { paths: ['rxjs'], patterns: ['../'] }],
     'no-throw-literal': 'warn',
     'no-undef-init': 'warn',
     'no-var': 'warn',
-    'radix': 'warn', // Add radix on parseInt
+    radix: 'warn', // Add radix on parseInt
     // 'dot-notation': 'warn',  // 39 instances as of 3 Jan 2022
     // 'no-console': 'warn',  // 69 instances as of 3 Jan 2022
     // 'prefer-arrow/prefer-arrow-functions': ['warn'], // Lots of 'newable' functions in the code base
@@ -58,17 +58,21 @@ module.exports = {
     'no-use-before-define': 'off', // Needed for the below rule
     '@typescript-eslint/no-use-before-define': 'warn',
 
+    'no-unused-vars': 'off', // Needed for the below rule
+    '@typescript-eslint/no-unused-vars': ['warn', { argsIgnorePattern: '_' }], // 271 instances as of 3 Jan 2022
+
     '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-empty-interface': 'warn',
-    '@typescript-eslint/no-inferrable-types': ['warn', {ignoreParameters: true}],
+    '@typescript-eslint/no-inferrable-types': [
+      'warn',
+      { ignoreParameters: true },
+    ],
     '@typescript-eslint/no-misused-new': 'warn',
     '@typescript-eslint/no-non-null-assertion': 'warn',
     '@typescript-eslint/no-shadow': 'warn',
     '@typescript-eslint/unified-signatures': 'warn',
     // '@typescript-eslint/member-ordering': ['warn', { 'classExpressions': ['method', 'field'] }],
-    // 'no-unused-vars': 'off', // Needed for the below rule
-    // '@typescript-eslint/no-unused-vars': 'warn', // 271 instances as of 3 Jan 2022
 
     'react/jsx-uses-vars': 'warn',
     'react-hooks/rules-of-hooks': 'warn',
@@ -98,5 +102,5 @@ module.exports = {
 
     /* Jest */
     'jest/no-focused-tests': 'warn',
-  }
+  },
 };
