edd39e4043e015e1ac626dd2ec23543b4e167421
CH Albach <calbach@users.noreply.github.com>
2022-01-19 17:12:27 -0800

    Update generate-egress.py

--- e2e/resources/python-code/generate-egress.py
+++ e2e/resources/python-code/generate-egress.py
@@ -20,7 +20,8 @@ newfile.write(os.urandom(20000000))    # generate 20MB random content file
 newfile.close()
 # Upload to the internet.
 #
-# Upload the file 9 times and expect to upload 180MB data. 
+# Upload the file 9 times and expect to upload 180MB data. This stays under the 200MB limit, which could affect
+# tests up to an hour later via the larger egress window.
 for x in range(1, 10):
     print('Uploading to the internet at '+ timestamp() + ' , current count: ' + str(x))
     with open('data.txt', 'rb') as f:
