282662780ccf1e0fba84c65f8052a2aaa2e9282d
jmthibault79 <2701406+jmthibault79@users.noreply.github.com>
2022-01-24 16:18:37 -0500

    [risk=no][RW-7280] Enable some Prettier-compatible eslint rules (#6199)
    * enable max-len
    * enable no-trailing-spaces

--- ui/src/app/pages/workspace/workspace-edit.spec.tsx
+++ ui/src/app/pages/workspace/workspace-edit.spec.tsx
@@ -429,7 +429,9 @@ describe('WorkspaceEdit', () => {
       CdrVersionsStubVariables.DEFAULT_WORKSPACE_CDR_VERSION_ID
     );
 
-    const expectedUpgradeMessage = `${CdrVersionsStubVariables.ALT_WORKSPACE_CDR_VERSION} to ${CdrVersionsStubVariables.DEFAULT_WORKSPACE_CDR_VERSION}.`;
+    const { ALT_WORKSPACE_CDR_VERSION, DEFAULT_WORKSPACE_CDR_VERSION } =
+      CdrVersionsStubVariables;
+    const expectedUpgradeMessage = `${ALT_WORKSPACE_CDR_VERSION} to ${DEFAULT_WORKSPACE_CDR_VERSION}.`;
     const cdrUpgradeMessage = wrapper
       .find('[data-test-id="cdr-version-upgrade"]')
       .first()
