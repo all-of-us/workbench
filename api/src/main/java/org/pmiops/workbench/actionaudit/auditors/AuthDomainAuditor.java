b6497f6233acd87076f669aa03a8acb09fdcfe6e
Joel Thibault <thibault@broad.dev>
2022-01-28 10:41:51 -0500

    rm AuthDomainAuditor

--- api/src/main/java/org/pmiops/workbench/actionaudit/auditors/AuthDomainAuditor.java
+++
@@ -1,6 +0,0 @@
-package org.pmiops.workbench.actionaudit.auditors;
-
-public interface AuthDomainAuditor {
-  void fireSetAccountDisabledStatus(
-      long userId, boolean newDisabledValue, boolean oldDisabledValue);
-}
