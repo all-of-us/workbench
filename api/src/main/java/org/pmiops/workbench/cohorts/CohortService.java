7a88ebafffe929b5464f581e1a36b21c69c67b41
Neha Saxena <nsaxena@broadinstitute.org>
2022-01-30 22:10:54 -0500

    Throw error if cohort is not found

--- api/src/main/java/org/pmiops/workbench/cohorts/CohortService.java
+++ api/src/main/java/org/pmiops/workbench/cohorts/CohortService.java
@@ -7,6 +7,7 @@ import org.pmiops.workbench.api.BigQueryService;
 import org.pmiops.workbench.cohortbuilder.CohortQueryBuilder;
 import org.pmiops.workbench.db.dao.CohortDao;
 import org.pmiops.workbench.db.model.DbCohort;
+import org.pmiops.workbench.exceptions.NotFoundException;
 import org.pmiops.workbench.model.Cohort;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -38,7 +39,12 @@ public class CohortService {
   }
 
   public DbCohort findDbCohortByCohortId(Long cohortId) {
-    return cohortDao.findById(cohortId).orElse(new DbCohort());
+    return cohortDao
+        .findById(cohortId)
+        .orElseThrow(
+            () ->
+                new NotFoundException(
+                    String.format("Cohort not found for cohortId: %d", cohortId)));
   }
 
   public List<Cohort> findByWorkspaceId(Long workspaceId) {
