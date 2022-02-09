9a85b68bafcfa7f1460d55711016e4afbdf0bf2c
Joel Thibault <thibault@broad.dev>
2022-02-09 18:46:06 -0500

    specify platform linux/amd64

--- api/db/run-migrations.sh
+++ api/db/run-migrations.sh
@@ -28,6 +28,7 @@ function run_mysql() {
   else
     echo "Outside docker: invoking mysql via docker for portability"
     docker run --rm --network host --entrypoint '' -i \
+      --platform linux/amd64 \
       mysql:8.0.27 \
       mysql $@
   fi
