fe0f90b73d2c3347f6b1f1653dc97125adc4c398
Joel Thibault <thibault@broad.dev>
2022-02-15 14:02:00 -0500

    AccessModuleNameMapperTest

--- api/src/test/java/org/pmiops/workbench/access/AccessModuleNameMapperTest.java
+++ api/src/test/java/org/pmiops/workbench/access/AccessModuleNameMapperTest.java
@@ -2,7 +2,13 @@ package org.pmiops.workbench.access;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.collect.ImmutableList;
+import java.util.Arrays;
+import java.util.List;
 import org.junit.jupiter.api.Test;
+import org.pmiops.workbench.compliance.ComplianceService.BadgeName;
+import org.pmiops.workbench.db.model.DbAccessModule.DbAccessModuleName;
+import org.pmiops.workbench.model.AccessModule;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Import;
 import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
@@ -13,7 +19,44 @@ public class AccessModuleNameMapperTest {
   @Autowired private AccessModuleNameMapper mapper;
 
   @Test
-  public void test() {
-    assertThat(true).isTrue();
+  public void testStorageToClientMapping() {
+    for (DbAccessModuleName name : DbAccessModuleName.values()) {
+      AccessModule clientName = mapper.storageAccessModuleToClient(name);
+      assertThat(clientName).isNotNull();
+      assertThat(mapper.clientAccessModuleToStorage(clientName)).isEqualTo(name);
+    }
+  }
+
+  @Test
+  public void testClientToStorageMapping() {
+    for (AccessModule name : AccessModule.values()) {
+      DbAccessModuleName dbName = mapper.clientAccessModuleToStorage(name);
+      assertThat(dbName).isNotNull();
+      assertThat(mapper.storageAccessModuleToClient(dbName)).isEqualTo(name);
+    }
+  }
+
+  @Test
+  public void testStorageToBypassPropertyMapping() {
+    // these can't be bypassed, so they don't have a mapping
+    List<DbAccessModuleName> unbypassable =
+        ImmutableList.of(
+            DbAccessModuleName.PROFILE_CONFIRMATION, DbAccessModuleName.PUBLICATION_CONFIRMATION);
+    unbypassable.forEach(name -> assertThat(mapper.auditAccessModuleFromStorage(name)).isNull());
+
+    assertThat(
+            Arrays.stream(DbAccessModuleName.values())
+                .filter(name -> !unbypassable.contains(name))
+                .allMatch(name -> mapper.auditAccessModuleFromStorage(name) != null))
+        .isTrue();
+  }
+
+  @Test
+  public void testBadgeMapping() {
+    for (BadgeName name : BadgeName.values()) {
+      DbAccessModuleName dbName = mapper.moduleFromBadge(name);
+      assertThat(dbName).isNotNull();
+      assertThat(mapper.badgeFromModule(dbName)).isEqualTo(name);
+    }
   }
 }
