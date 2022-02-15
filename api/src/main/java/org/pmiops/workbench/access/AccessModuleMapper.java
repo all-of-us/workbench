f3801548a5b3d5ff02f3c81fd754dce3c446feb0
Joel Thibault <thibault@broad.dev>
2022-02-15 11:35:05 -0500

    WIP

--- api/src/main/java/org/pmiops/workbench/access/AccessModuleMapper.java
+++ api/src/main/java/org/pmiops/workbench/access/AccessModuleMapper.java
@@ -4,6 +4,7 @@ import org.mapstruct.Mapper;
 import org.mapstruct.MappingConstants;
 import org.mapstruct.ValueMapping;
 import org.pmiops.workbench.actionaudit.targetproperties.BypassTimeTargetProperty;
+import org.pmiops.workbench.compliance.ComplianceService.BadgeName;
 import org.pmiops.workbench.db.model.DbAccessModule.DbAccessModuleName;
 import org.pmiops.workbench.model.AccessModule;
 import org.pmiops.workbench.utils.mappers.MapStructConfig;
@@ -22,4 +23,14 @@ public interface AccessModuleMapper {
   @ValueMapping(source = "PROFILE_CONFIRMATION", target = MappingConstants.NULL)
   @ValueMapping(source = "PUBLICATION_CONFIRMATION", target = MappingConstants.NULL)
   BypassTimeTargetProperty auditAccessModuleFromStorage(DbAccessModuleName source);
+
+  /*
+  > Task :compileJava
+/Users/thibault/src/aou/api/src/main/java/org/pmiops/workbench/access/AccessModuleMapper.java:27: error: The following constants from the source enum have no corresponding constant in the target enum and must be be mapped via adding additional mappings: ERA_COMMONS, TWO_FACTOR_AUTH, RAS_LOGIN_GOV, RT_COMPLIANCE_TRAINING, DATA_USER_CODE_OF_CONDUCT, PROFILE_CONFIRMATION, PUBLICATION_CONFIRMATION, CT_COMPLIANCE_TRAINING.
+  BadgeName badgeFromStorage(DbAccessModuleName source);
+
+
+accessmodulemappertest
+   */
+  BadgeName badgeFromStorage(DbAccessModuleName source);
 }
