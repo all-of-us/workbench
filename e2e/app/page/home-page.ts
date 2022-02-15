65ef8a8d0a24da2df692ad1c527a3d764f5e3d7d
aweng98 <bweng@broadinstitute.org>
2022-02-15 17:48:18 -0500

    e2e testing changes

--- e2e/app/page/home-page.ts
+++ e2e/app/page/home-page.ts
@@ -7,6 +7,7 @@ import { waitForDocumentTitle, waitWhileLoading, waitWhileSpinnerDisplayed } fro
 import WorkspacesPage from './workspaces-page';
 import { getAttrValue } from 'utils/element-utils';
 import { logger } from 'libs/logger';
+import { makeDateTimeStr } from 'utils/str-utils';
 
 export const PageTitle = 'Homepage';
 
@@ -67,6 +68,7 @@ export default class HomePage extends AuthenticatedPage {
       await this.page.waitForTimeout(5000);
       await waitFor(30 * 1000);
     }
+    await takeScreenshot(this.page, makeDateTimeStr('home-page'));
     return true;
   }
 
