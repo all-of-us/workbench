5557e09d8467369af1fc3f9ebb615eda57401ff0
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro-2.local>
2022-02-18 03:17:51 -0600

    RW-7829 cleanup modifiers

--- ui/src/app/pages/data/cohort-review/cohort-definition.component.tsx
+++ ui/src/app/pages/data/cohort-review/cohort-definition.component.tsx
@@ -1,17 +1,22 @@
 import * as React from 'react';
+import { RouteComponentProps, withRouter } from 'react-router-dom';
 
 import {
   CohortReview,
+  Criteria,
   CriteriaType,
   Domain,
   Modifier,
+  ModifierType,
   SearchGroup,
   SearchParameter,
 } from 'generated/fetch';
 
 import { domainToTitle } from 'app/cohort-search/utils';
+import { cohortBuilderApi } from 'app/services/swagger-fetch-clients';
 import colors, { colorWithWhiteness } from 'app/styles/colors';
 import { reactStyles } from 'app/utils';
+import { MatchParams } from 'app/utils/stores';
 
 const css = `
   @media print{
@@ -70,241 +75,266 @@ interface ParamItem {
   items: string;
 }
 
-export class CohortDefinition extends React.Component<
-  { review: CohortReview },
-  { definition: any }
-> {
-  constructor(props: any) {
-    super(props);
-    this.state = { definition: null };
-  }
-
-  componentDidMount() {
-    this.mapDefinition();
-  }
-
-  mapDefinition() {
-    const {
-      review: { cohortDefinition },
-    } = this.props;
-    const def = JSON.parse(cohortDefinition);
-    const definition = ['includes', 'excludes'].reduce((acc, role) => {
-      if (def[role].length) {
-        const roleObj = { role, groups: [] };
-        def[role].forEach((group) => {
-          roleObj.groups.push(this.mapGroup(group));
-        });
-        acc.push(roleObj);
-      }
-      return acc;
-    }, []);
-    this.setState({ definition });
-  }
-
-  mapGroup(group: SearchGroup) {
-    const mappedItems = [];
-    group.items.forEach(({ modifiers, searchParameters, type }) => {
-      const domain = Domain[type];
-      if (
-        (domain === Domain.CONDITION || domain === Domain.PROCEDURE) &&
-        searchParameters.some((param) => param.domain !== domain)
-      ) {
-        const multipleDomains = searchParameters.reduce((acc, param) => {
-          const index = acc.findIndex(
-            (list) => list[0].domain === param.domain
-          );
-          if (index > -1) {
-            acc[index].push(param);
-          } else {
-            acc.push([param]);
-          }
-          return acc;
-        }, []);
-        multipleDomains.forEach((params) =>
-          mappedItems.push(this.mapParams(params, modifiers))
-        );
-      } else {
-        mappedItems.push(this.mapParams(searchParameters, modifiers));
-      }
-    });
-    return mappedItems;
-  }
-
-  mapParams(parameters: Array<SearchParameter>, modifiers: Array<Modifier>) {
-    const groupedParameters = parameters.reduce((parameterList, parameter) => {
-      const { domain, group, name, type } = parameter;
-      const displayName =
-        group && showParameterParent(domain) ? `Parent ${name}` : name;
-      if (parameterList[type]) {
-        parameterList[type].items += `, ${displayName}`;
-      } else {
-        parameterList[type] = {
-          domain,
-          items: `${domainToTitle(domain)}${
-            showParameterType(domain) ? ` | ${type}` : ''
-          } | ${displayName}`,
-        };
-      }
-      return parameterList;
-    }, {} as ParamItem);
-    return modifiers.length > 0
-      ? Object.values(groupedParameters).map((param) => {
-          param.items += ` | ${this.getModifierName(modifiers)}`;
-          return param;
-        })
-      : Object.values(groupedParameters);
-  }
-
-  getModifierName(modifiers: Array<Modifier>) {
-    return modifiers.reduce((acc, m) => {
-      const concatOperand = m.operands.reduce((final, o) => {
-        return final !== '' ? `${final} & ${o}` : `${final} ${o}`;
-      }, '');
-      return acc !== ''
-        ? `${acc} ,  ${this.operatorConversion(m.name)}
-            ${this.operatorConversion(m.operator)}
-            ${concatOperand}`
-        : `${this.operatorConversion(m.name)} ${this.operatorConversion(
-            m.operator
-          )}
-            ${concatOperand}`;
-    }, '');
-  }
-
-  // utils
-  getGroupedData(p, t) {
-    const groupedData = p.reduce((acc, i) => {
-      const key = i[t];
-      acc[key] = acc[key] || { data: [] };
-      acc[key].data.push(i);
-      return acc;
-    }, {});
-    return Object.keys(groupedData).map((k) => {
-      return Object.assign(
-        {},
-        {
-          group: k,
-          data: groupedData[k].data,
-          customString: groupedData[k].data.reduce((acc, d) => {
-            return this.getFormattedString(acc, d);
-          }, ''),
-        }
-      );
-    });
-  }
-
-  getFormattedString(acc, d) {
-    if (d.domain === Domain.DRUG) {
-      if (d.group === false) {
-        return acc === '' ? `RXNORM | ${d.name}` : `${acc}, ${d.name}`;
-      } else {
-        return acc === '' ? `ATC | ${d.name}` : `${acc}, ${d.name}`;
-      }
-    } else if (
-      d.domain === Domain.PHYSICALMEASUREMENT ||
-      d.domain === Domain.VISIT
-    ) {
-      return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
-    } else if (d.domain === Domain.SURVEY) {
-      if (!d.group) {
-        return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
-      } else if (d.group && !d.conceptId) {
-        return acc === '' ? `Survey - ${d.name}` : `${acc}, Survey - ${d.name}`;
-      } else {
-        return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
-      }
-    } else {
-      if (d.group === false) {
-        return acc === '' ? d.name : `${acc}, ${d.name}`;
-      } else {
-        return acc === '' ? `Parent ${d.name}` : `${acc}, Parent ${d.name}`;
-      }
-    }
-  }
-
-  removeDuplicates(arr) {
-    return arr.filter(
-      (thing, index, self) =>
-        index ===
-        self.findIndex(
-          (t) => t.items === thing.items && t.domain === thing.domain
-        )
-    );
-  }
-
-  operatorConversion(operator) {
-    switch (operator) {
-      case 'GREATER_THAN_OR_EQUAL_TO':
-        return '>=';
-      case 'LESS_THAN_OR_EQUAL_TO':
-        return '<=';
-      case 'EQUAL':
-        return '=';
-      case 'BETWEEN':
-        return 'Between';
-      case 'ETHNICITY':
-        return 'Ethnicity';
-      case 'RACE':
-        return 'Race';
-      case 'AGE':
-        return 'Age';
-      case 'GENDER':
-        return 'Gender';
-      case 'AGE_AT_EVENT':
-        return 'Age at Event';
-      case 'NUM_OF_OCCURRENCES':
-        return 'Num of Occurrences';
-    }
-  }
-
-  render() {
-    const { definition } = this.state;
-    return (
-      <div style={{ marginTop: '1rem', marginBottom: '1rem' }}>
-        <style>{css}</style>
-        <div style={styles.defTitle}>Cohort Definition</div>
-        {definition?.map((group, g) => (
-          <React.Fragment key={g}>
-            {group.role === 'excludes' && (
-              <div className='page-break' style={styles.wrapper}>
-                <div style={styles.exclude}>EXCLUDING</div>
-              </div>
-            )}
-            {group.groups.map((item, i) => (
-              <React.Fragment key={i}>
-                {i > 0 && (
-                  <div className='page-break' style={styles.wrapper}>
-                    <div style={styles.andCircle}>AND</div>
-                  </div>
-                )}
-                <div style={styles.groupBackground}>
-                  {item.map((param, p) => (
-                    <React.Fragment key={p}>
-                      {p > 0 && <div>OR</div>}
-                      <div>
-                        {param.map((crit, c) => (
-                          <React.Fragment key={c}>
-                            {c > 0 && (
-                              <React.Fragment>
-                                {crit.domain === Domain.PERSON ? (
-                                  <div>AND</div>
-                                ) : (
-                                  <div>OR</div>
-                                )}
-                              </React.Fragment>
-                            )}
-                            <div>{crit.items}</div>
-                          </React.Fragment>
-                        ))}
-                      </div>
-                    </React.Fragment>
-                  ))}
-                </div>
-              </React.Fragment>
-            ))}
-          </React.Fragment>
-        ))}
-      </div>
-    );
-  }
+interface Props extends RouteComponentProps<MatchParams> {
+  review: CohortReview;
 }
+
+interface State {
+  definition: any;
+  visits: Array<Criteria>;
+}
+
+export const CohortDefinition = withRouter(
+  class extends React.Component<Props, State> {
+    constructor(props: any) {
+      super(props);
+      this.state = { definition: undefined, visits: undefined };
+    }
+
+    async componentDidMount() {
+      const { ns, wsid } = this.props.match.params;
+      const visits = await cohortBuilderApi().findCriteriaBy(
+        ns,
+        wsid,
+        Domain.VISIT.toString(),
+        CriteriaType.VISIT.toString()
+      );
+      this.setState({ visits: visits.items }, () => this.mapDefinition());
+    }
+
+    mapDefinition() {
+      const {
+        review: { cohortDefinition },
+      } = this.props;
+      const def = JSON.parse(cohortDefinition);
+      const definition = ['includes', 'excludes'].reduce((acc, role) => {
+        if (def[role].length) {
+          const roleObj = { role, groups: [] };
+          def[role].forEach((group) => {
+            roleObj.groups.push(this.mapGroup(group));
+          });
+          acc.push(roleObj);
+        }
+        return acc;
+      }, []);
+      this.setState({ definition });
+    }
+
+    mapGroup(group: SearchGroup) {
+      const mappedItems = [];
+      group.items.forEach(({ modifiers, searchParameters, type }) => {
+        const domain = Domain[type];
+        if (
+          (domain === Domain.CONDITION || domain === Domain.PROCEDURE) &&
+          searchParameters.some((param) => param.domain !== domain)
+        ) {
+          const multipleDomains = searchParameters.reduce((acc, param) => {
+            const index = acc.findIndex(
+              (list) => list[0].domain === param.domain
+            );
+            if (index > -1) {
+              acc[index].push(param);
+            } else {
+              acc.push([param]);
+            }
+            return acc;
+          }, []);
+          multipleDomains.forEach((params) =>
+            mappedItems.push(this.mapParams(params, modifiers))
+          );
+        } else {
+          mappedItems.push(this.mapParams(searchParameters, modifiers));
+        }
+      });
+      return mappedItems;
+    }
+
+    mapParams(parameters: Array<SearchParameter>, modifiers: Array<Modifier>) {
+      const groupedParameters = parameters.reduce(
+        (parameterList, parameter) => {
+          const { domain, group, name, type } = parameter;
+          const displayName =
+            group && showParameterParent(domain) ? `Parent ${name}` : name;
+          if (parameterList[type]) {
+            parameterList[type].items += `, ${displayName}`;
+          } else {
+            parameterList[type] = {
+              domain,
+              items: `${domainToTitle(domain)}${
+                showParameterType(domain) ? ` | ${type}` : ''
+              } | ${displayName}`,
+            };
+          }
+          return parameterList;
+        },
+        {} as ParamItem
+      );
+      return modifiers.length > 0
+        ? Object.values(groupedParameters).map((param) => {
+            param.items += ` | ${this.getModifierName(modifiers)}`;
+            return param;
+          })
+        : Object.values(groupedParameters);
+    }
+
+    getModifierName(modifiers: Array<Modifier>) {
+      const modifiersArray = modifiers.reduce((modsArray, modifier) => {
+        const operands =
+          modifier.name === ModifierType.ENCOUNTERS
+            ? this.state.visits.find(
+                (visit) => visit.conceptId.toString() === modifier.operands[0]
+              )?.name || ''
+            : modifier.operands.join(' & ');
+        modsArray.push(`${this.operatorConversion(
+          modifier.name
+        )} ${this.operatorConversion(modifier.operator)}
+              ${operands}`);
+        return modsArray;
+      }, []);
+      return modifiersArray.join(', ');
+    }
+
+    // utils
+    getGroupedData(p, t) {
+      const groupedData = p.reduce((acc, i) => {
+        const key = i[t];
+        acc[key] = acc[key] || { data: [] };
+        acc[key].data.push(i);
+        return acc;
+      }, {});
+      return Object.keys(groupedData).map((k) => {
+        return Object.assign(
+          {},
+          {
+            group: k,
+            data: groupedData[k].data,
+            customString: groupedData[k].data.reduce((acc, d) => {
+              return this.getFormattedString(acc, d);
+            }, ''),
+          }
+        );
+      });
+    }
+
+    getFormattedString(acc, d) {
+      if (d.domain === Domain.DRUG) {
+        if (d.group === false) {
+          return acc === '' ? `RXNORM | ${d.name}` : `${acc}, ${d.name}`;
+        } else {
+          return acc === '' ? `ATC | ${d.name}` : `${acc}, ${d.name}`;
+        }
+      } else if (
+        d.domain === Domain.PHYSICALMEASUREMENT ||
+        d.domain === Domain.VISIT
+      ) {
+        return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
+      } else if (d.domain === Domain.SURVEY) {
+        if (!d.group) {
+          return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
+        } else if (d.group && !d.conceptId) {
+          return acc === ''
+            ? `Survey - ${d.name}`
+            : `${acc}, Survey - ${d.name}`;
+        } else {
+          return acc === '' ? `${d.name}` : `${acc}, ${d.name}`;
+        }
+      } else {
+        if (d.group === false) {
+          return acc === '' ? d.name : `${acc}, ${d.name}`;
+        } else {
+          return acc === '' ? `Parent ${d.name}` : `${acc}, Parent ${d.name}`;
+        }
+      }
+    }
+
+    removeDuplicates(arr) {
+      return arr.filter(
+        (thing, index, self) =>
+          index ===
+          self.findIndex(
+            (t) => t.items === thing.items && t.domain === thing.domain
+          )
+      );
+    }
+
+    operatorConversion(operator) {
+      switch (operator) {
+        case 'GREATER_THAN_OR_EQUAL_TO':
+          return '>=';
+        case 'LESS_THAN_OR_EQUAL_TO':
+          return '<=';
+        case 'EQUAL':
+          return '=';
+        case 'BETWEEN':
+          return 'Between';
+        case 'IN':
+          return '';
+        case 'ETHNICITY':
+          return 'Ethnicity';
+        case 'RACE':
+          return 'Race';
+        case 'AGE':
+          return 'Age';
+        case 'GENDER':
+          return 'Gender';
+        case 'AGE_AT_EVENT':
+          return 'Age at Event';
+        case 'NUM_OF_OCCURRENCES':
+          return 'Num of Occurrences';
+        case 'ENCOUNTERS':
+          return 'During Visit Type - ';
+      }
+    }
+
+    render() {
+      const { definition } = this.state;
+      return (
+        <div style={{ marginTop: '1rem', marginBottom: '1rem' }}>
+          <style>{css}</style>
+          <div style={styles.defTitle}>Cohort Definition</div>
+          {definition?.map((group, g) => (
+            <React.Fragment key={g}>
+              {group.role === 'excludes' && (
+                <div className='page-break' style={styles.wrapper}>
+                  <div style={styles.exclude}>EXCLUDING</div>
+                </div>
+              )}
+              {group.groups.map((item, i) => (
+                <React.Fragment key={i}>
+                  {i > 0 && (
+                    <div className='page-break' style={styles.wrapper}>
+                      <div style={styles.andCircle}>AND</div>
+                    </div>
+                  )}
+                  <div style={styles.groupBackground}>
+                    {item.map((param, p) => (
+                      <React.Fragment key={p}>
+                        {p > 0 && <div>OR</div>}
+                        <div>
+                          {param.map((crit, c) => (
+                            <React.Fragment key={c}>
+                              {c > 0 && (
+                                <React.Fragment>
+                                  {crit.domain === Domain.PERSON ? (
+                                    <div>AND</div>
+                                  ) : (
+                                    <div>OR</div>
+                                  )}
+                                </React.Fragment>
+                              )}
+                              <div>{crit.items}</div>
+                            </React.Fragment>
+                          ))}
+                        </div>
+                      </React.Fragment>
+                    ))}
+                  </div>
+                </React.Fragment>
+              ))}
+            </React.Fragment>
+          ))}
+        </div>
+      );
+    }
+  }
+);
