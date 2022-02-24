142301a4d5fd45623b9f2a39cee7a6c740a4bfa3
aweng98 <35533885+aweng98@users.noreply.github.com>
2022-02-24 18:40:05 -0500

    [RW-7732][risk=no] e2e test for User Profile Admin (#6334)
    * test description
    
    * new e2e test user-profile-admin.spec
    
    * wip
    
    * test working
    
    * remove console log
    
    * wip
    
    * add new tests
    
    * enable all tests
    
    * revert .env.circleci.local
    
    * clean up
    
    * rebase
    
    * PR feedback
    
    * add table values checks
    
    * PR feedback

---
+++ e2e/app/component/switch.ts
@@ -0,0 +1,49 @@
+import BaseElement from 'app/element/base-element';
+import { Page } from 'puppeteer';
+import { ElementType, XPathOptions } from 'app/xpath-options';
+import Container from 'app/container';
+import { buildXPath } from 'app/xpath-builders';
+import { getPropValue } from 'utils/element-utils';
+
+export const defaultSwitchXpath = '//label[.//input[@type="checkbox" and @role="switch"]]';
+
+export default class Switch extends BaseElement {
+  static findByName(page: Page, xOpt: XPathOptions, container?: Container): Switch {
+    xOpt.type = ElementType.Switch;
+    const xpath = buildXPath(xOpt, container);
+    return new Switch(page, xpath);
+  }
+
+  constructor(page: Page, xpath = defaultSwitchXpath, container?: Container) {
+    super(page, container === undefined ? xpath : `${container.getXpath()}${xpath}`);
+  }
+
+  async getLabel(): Promise<string> {
+    const labelElement = await this.asElementHandle();
+    return getPropValue<string>(labelElement, 'innerText');
+  }
+
+  async toggle(): Promise<void> {
+    await this.click();
+  }
+
+  async toggleOff(): Promise<void> {
+    const checked = await this.isOn();
+    if (checked) {
+      await this.toggle();
+    }
+  }
+
+  async toggleOn(): Promise<void> {
+    const checked = await this.isOn();
+    if (!checked) {
+      await this.toggle();
+    }
+  }
+
+  async isOn(): Promise<boolean> {
+    const labelElement = await this.asElementHandle();
+    const [input] = await labelElement.$x('.//input[@type="checkbox" and @role="switch"]');
+    return await getPropValue<boolean>(input, 'checked');
+  }
+}
