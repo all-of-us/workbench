5237858e38510b7003b92cd2ef3d4d13de8ed7e4
CH Albach <calbach@google.com>
2022-02-25 16:13:03 -0800

    Fix mariadb connection check and password commands

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -80,9 +80,11 @@ def start_local_db_service()
   bm = Benchmark.measure {
     common.run_inline %W{docker-compose up -d db}
 
+    root_pass = Workbench.read_vars_file("db/vars.env")["MYSQL_ROOT_PASSWORD"]
+
     common.status "waiting up to #{deadlineSec}s for mysql service to start..."
     start = Time.now
-    until (common.run %W{docker-compose exec -T db mysqladmin ping --silent}).success?
+    until (common.run "docker-compose exec -T db mysql -p#{root_pass} --silent -e 'SELECT 1;'").success?
       if Time.now - start >= deadlineSec
         raise("mysql docker service did not become available after #{deadlineSec}s")
       end
