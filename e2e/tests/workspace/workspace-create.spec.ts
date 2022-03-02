a96ce72e93adfedf7144740cee626eb9b477ee66
Joel Thibault <thibault@broad.dev>
2022-03-02 14:34:56 -0500

    lint e2e

--- e2e/tests/workspace/workspace-create.spec.ts
+++ e2e/tests/workspace/workspace-create.spec.ts
@@ -1,11 +1,10 @@
 import WorkspaceDataPage from 'app/page/workspace-data-page';
-import WorkspacesPage, { FieldSelector } from 'app/page/workspaces-page';
+import WorkspacesPage from 'app/page/workspaces-page';
 import { signInWithAccessToken, performActions } from 'utils/test-utils';
-import Button from 'app/element/button';
 import * as testData from 'resources/data/workspace-data';
 import { makeWorkspaceName } from 'utils/str-utils';
 import { UseFreeCredits } from 'app/page/workspace-base';
-import WorkspaceEditPage, { AccessTierDisplayNames } from 'app/page/workspace-edit-page';
+import { AccessTierDisplayNames } from 'app/page/workspace-edit-page';
 import { config } from 'resources/workbench-config';
 
 describe('Creating new workspaces', () => {
