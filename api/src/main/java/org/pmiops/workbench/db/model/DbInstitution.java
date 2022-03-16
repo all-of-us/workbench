cdb4b00e257b11a01e37e5163c9ffc5462390d39
evrii <erollins@broadinstitute.org>
2022-03-16 10:48:15 -0400

    Updated formatting based on linting.

--- api/src/main/java/org/pmiops/workbench/db/model/DbInstitution.java
+++ api/src/main/java/org/pmiops/workbench/db/model/DbInstitution.java
@@ -20,8 +20,7 @@ public class DbInstitution {
   private String organizationTypeOtherText;
   private String requestAccessLink;
 
-  public DbInstitution() {
-  }
+  public DbInstitution() {}
 
   @Id
   @GeneratedValue(strategy = GenerationType.IDENTITY)
--- api/src/test/java/org/pmiops/workbench/db/dao/InstitutionDaoTest.java
+++ api/src/test/java/org/pmiops/workbench/db/dao/InstitutionDaoTest.java
@@ -21,8 +21,7 @@ import org.springframework.test.annotation.DirtiesContext.ClassMode;
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
 public class InstitutionDaoTest {
 
-  @Autowired
-  InstitutionDao institutionDao;
+  @Autowired InstitutionDao institutionDao;
 
   private DbInstitution testInst;
 
