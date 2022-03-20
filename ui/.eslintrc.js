a7585be2b66479f7a2862154e4b32eca213a93dd
Joel Thibault <thibault@broad.dev>
2022-03-20 17:18:05 -0400

    enable '@typescript-eslint/explicit-function-return-type

--- ui/.eslintrc.js
+++ ui/.eslintrc.js
@@ -64,6 +64,7 @@ module.exports = {
 
     '@typescript-eslint/consistent-type-definitions': ['warn', 'interface'],
     '@typescript-eslint/dot-notation': 'warn',
+    '@typescript-eslint/explicit-function-return-type': 'warn',
     '@typescript-eslint/explicit-member-accessibility': 'off',
     '@typescript-eslint/no-empty-interface': 'warn',
     '@typescript-eslint/no-inferrable-types': [
