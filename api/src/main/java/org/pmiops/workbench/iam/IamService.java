607bb38969f7ba8b16c4f759bbf86eb1b5bc6349
Joel Thibault <thibault@broad.dev>
2022-03-30 08:59:54 -0400

    Text cleanup for IamService

--- api/src/main/java/org/pmiops/workbench/iam/IamService.java
+++ api/src/main/java/org/pmiops/workbench/iam/IamService.java
@@ -4,26 +4,25 @@ import java.util.List;
 
 public interface IamService {
   /**
-   * Grants user permissions to run Google lifescience jobs to user in currenet context.
+   * Grants user permissions to run Google Life Sciences jobs to the user in the current context.
    *
-   * <p>The users's Terra PET service account will get: lifescienceRunner and serviceAccountUser(on
-   * the petSA itself).
+   * <p>The user's Terra PET service account will get: lifesciences.workflowsRunner and
+   * serviceAccountUser (on the petSA itself).
    */
   void grantWorkflowRunnerRoleToCurrentUser(String googleProject);
 
   /**
-   * Grants permissions to run Google lifescience jobs for the provideded user email lists.
+   * Grants permissions to run Google Life Sciences jobs for the provided user email lists.
    *
-   * <p>The users's Terra PET service account will get: lifescienceRunner and serviceAccountUser(on
-   * the petSA itself).
+   * <p>The users' Terra PET service accounts will get: lifesciences.workflowsRunner and
+   * serviceAccountUser (on the petSA itself).
    */
   void grantWorkflowRunnerRoleForUsers(String googleProject, List<String> userEmails);
 
   /**
-   * Revokes permissions to run Google lifescience jobs for the provideded user email lists.
+   * Revokes permissions to run Google Life Sciences jobs for the provided user email lists.
    *
-   * <p>The users's Terra PET service account will get: lifescienceRunner. For now just revoke
-   * lifescience runner permisison but keep petSA ActAS permission.
+   * <p>For now just revoke lifesciences.workflowsRunner permission but keep petSA ActAS permission.
    */
   void revokeWorkflowRunnerRoleForUsers(String googleProject, List<String> userEmails);
 }
