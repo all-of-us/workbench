003cfc867534b59f750fc6debab50bd7bb1a4e2f
Chenchal Subraveti <chenchals@users.noreply.github.com>
2022-04-06 13:47:40 -0500

    Fix table name bug for v52 ds_procedure_occurrence table (#6537)

--- api/db-cdr/generate-cdr/create-tables.sh
+++ api/db-cdr/generate-cdr/create-tables.sh
@@ -53,8 +53,8 @@ do
          bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.$table_name"
     elif [[ "$table_name" == 'ds_procedure_occurrence_52' && ! "$TABLE_LIST" == *'visit_detail'* ]]
        then
-         echo "Creating $table_name"
-         bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.$table_name"
+         echo "Creating ds_procedure_occurrence"
+         bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.ds_procedure_occurrence"
     else
       echo "Creating $table_name"
       bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.$table_name"
