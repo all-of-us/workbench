42c1177d59cabe47977ffad6cef3196b149e75e0
CH Albach <calbach@users.noreply.github.com>
2022-04-26 11:14:43 -0700

    [RW-7996][RW-6880][risk=no] Refactor RDR export/tests for quality of life (#6616)

--- api/src/main/java/org/pmiops/workbench/rdr/RdrExportEnums.java
+++ api/src/main/java/org/pmiops/workbench/rdr/RdrExportEnums.java
@@ -2,80 +2,77 @@ package org.pmiops.workbench.rdr;
 
 import com.google.common.collect.BiMap;
 import com.google.common.collect.ImmutableBiMap;
-import org.pmiops.workbench.model.*;
+import org.pmiops.workbench.model.Degree;
+import org.pmiops.workbench.model.Disability;
+import org.pmiops.workbench.model.Education;
+import org.pmiops.workbench.model.Ethnicity;
+import org.pmiops.workbench.model.GenderIdentity;
+import org.pmiops.workbench.model.Race;
+import org.pmiops.workbench.model.SexAtBirth;
+import org.pmiops.workbench.model.SpecificPopulationEnum;
+import org.pmiops.workbench.rdr.model.RdrDegree;
+import org.pmiops.workbench.rdr.model.RdrDisability;
+import org.pmiops.workbench.rdr.model.RdrEducation;
+import org.pmiops.workbench.rdr.model.RdrEthnicity;
+import org.pmiops.workbench.rdr.model.RdrGender;
+import org.pmiops.workbench.rdr.model.RdrRace;
+import org.pmiops.workbench.rdr.model.RdrSexAtBirth;
 import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic;
 
 public class RdrExportEnums {
 
-  private static final BiMap<Race, org.pmiops.workbench.rdr.model.Race> CLIENT_TO_RDR_RACE =
-      ImmutableBiMap.<Race, org.pmiops.workbench.rdr.model.Race>builder()
-          .put(Race.AA, org.pmiops.workbench.rdr.model.Race.AA)
-          .put(Race.AIAN, org.pmiops.workbench.rdr.model.Race.AIAN)
-          .put(Race.ASIAN, org.pmiops.workbench.rdr.model.Race.ASIAN)
-          .put(Race.NHOPI, org.pmiops.workbench.rdr.model.Race.NHOPI)
-          .put(Race.WHITE, org.pmiops.workbench.rdr.model.Race.WHITE)
-          .put(Race.PREFER_NO_ANSWER, org.pmiops.workbench.rdr.model.Race.PREFER_NOT_TO_ANSWER)
-          .put(Race.NONE, org.pmiops.workbench.rdr.model.Race.NONE)
+  private static final BiMap<Race, RdrRace> CLIENT_TO_RDR_RACE =
+      ImmutableBiMap.<Race, RdrRace>builder()
+          .put(Race.AA, RdrRace.AA)
+          .put(Race.AIAN, RdrRace.AIAN)
+          .put(Race.ASIAN, RdrRace.ASIAN)
+          .put(Race.NHOPI, RdrRace.NHOPI)
+          .put(Race.WHITE, RdrRace.WHITE)
+          .put(Race.PREFER_NO_ANSWER, RdrRace.PREFER_NOT_TO_ANSWER)
+          .put(Race.NONE, RdrRace.NONE)
           .build();
 
-  private static final BiMap<Ethnicity, org.pmiops.workbench.rdr.model.Ethnicity>
-      CLIENT_TO_RDR_ETHNICITY =
-          ImmutableBiMap.<Ethnicity, org.pmiops.workbench.rdr.model.Ethnicity>builder()
-              .put(Ethnicity.HISPANIC, org.pmiops.workbench.rdr.model.Ethnicity.HISPANIC)
-              .put(Ethnicity.NOT_HISPANIC, org.pmiops.workbench.rdr.model.Ethnicity.NOT_HISPANIC)
-              .put(
-                  Ethnicity.PREFER_NO_ANSWER,
-                  org.pmiops.workbench.rdr.model.Ethnicity.PREFER_NOT_TO_ANSWER)
-              .build();
+  private static final BiMap<Ethnicity, RdrEthnicity> CLIENT_TO_RDR_ETHNICITY =
+      ImmutableBiMap.<Ethnicity, RdrEthnicity>builder()
+          .put(Ethnicity.HISPANIC, RdrEthnicity.HISPANIC)
+          .put(Ethnicity.NOT_HISPANIC, RdrEthnicity.NOT_HISPANIC)
+          .put(Ethnicity.PREFER_NO_ANSWER, RdrEthnicity.PREFER_NOT_TO_ANSWER)
+          .build();
 
-  private static final BiMap<GenderIdentity, org.pmiops.workbench.rdr.model.Gender>
-      CLIENT_TO_RDR_GENDER =
-          ImmutableBiMap.<GenderIdentity, org.pmiops.workbench.rdr.model.Gender>builder()
-              .put(GenderIdentity.MAN, org.pmiops.workbench.rdr.model.Gender.MAN)
-              .put(GenderIdentity.WOMAN, org.pmiops.workbench.rdr.model.Gender.WOMAN)
-              .put(GenderIdentity.NON_BINARY, org.pmiops.workbench.rdr.model.Gender.NON_BINARY)
-              .put(GenderIdentity.TRANSGENDER, org.pmiops.workbench.rdr.model.Gender.TRANSGENDER)
-              .put(
-                  GenderIdentity.NONE_DESCRIBE_ME,
-                  org.pmiops.workbench.rdr.model.Gender.NONE_DESCRIBE_ME)
-              .put(
-                  GenderIdentity.PREFER_NO_ANSWER,
-                  org.pmiops.workbench.rdr.model.Gender.PREFER_NOT_TO_ANSWER)
-              .build();
+  private static final BiMap<GenderIdentity, RdrGender> CLIENT_TO_RDR_GENDER =
+      ImmutableBiMap.<GenderIdentity, RdrGender>builder()
+          .put(GenderIdentity.MAN, RdrGender.MAN)
+          .put(GenderIdentity.WOMAN, RdrGender.WOMAN)
+          .put(GenderIdentity.NON_BINARY, RdrGender.NON_BINARY)
+          .put(GenderIdentity.TRANSGENDER, RdrGender.TRANSGENDER)
+          .put(GenderIdentity.NONE_DESCRIBE_ME, RdrGender.NONE_DESCRIBE_ME)
+          .put(GenderIdentity.PREFER_NO_ANSWER, RdrGender.PREFER_NOT_TO_ANSWER)
+          .build();
 
-  private static final BiMap<Education, org.pmiops.workbench.rdr.model.Education>
-      CLIENT_TO_RDR_EDUCATION =
-          ImmutableBiMap.<Education, org.pmiops.workbench.rdr.model.Education>builder()
-              .put(Education.NO_EDUCATION, org.pmiops.workbench.rdr.model.Education.NO_EDUCATION)
-              .put(Education.GRADES_1_12, org.pmiops.workbench.rdr.model.Education.GRADES_1_12)
-              .put(
-                  Education.COLLEGE_GRADUATE,
-                  org.pmiops.workbench.rdr.model.Education.COLLEGE_GRADUATE)
-              .put(Education.UNDERGRADUATE, org.pmiops.workbench.rdr.model.Education.UNDERGRADUATE)
-              .put(Education.MASTER, org.pmiops.workbench.rdr.model.Education.MASTER)
-              .put(Education.DOCTORATE, org.pmiops.workbench.rdr.model.Education.DOCTORATE)
-              .build();
+  private static final BiMap<Education, RdrEducation> CLIENT_TO_RDR_EDUCATION =
+      ImmutableBiMap.<Education, RdrEducation>builder()
+          .put(Education.NO_EDUCATION, RdrEducation.NO_EDUCATION)
+          .put(Education.GRADES_1_12, RdrEducation.GRADES_1_12)
+          .put(Education.COLLEGE_GRADUATE, RdrEducation.COLLEGE_GRADUATE)
+          .put(Education.UNDERGRADUATE, RdrEducation.UNDERGRADUATE)
+          .put(Education.MASTER, RdrEducation.MASTER)
+          .put(Education.DOCTORATE, RdrEducation.DOCTORATE)
+          .build();
 
-  private static final BiMap<SexAtBirth, org.pmiops.workbench.rdr.model.SexAtBirth>
-      CLIENT_TO_RDR_SEX_AT_BIRTH =
-          ImmutableBiMap.<SexAtBirth, org.pmiops.workbench.rdr.model.SexAtBirth>builder()
-              .put(SexAtBirth.MALE, org.pmiops.workbench.rdr.model.SexAtBirth.MALE)
-              .put(SexAtBirth.FEMALE, org.pmiops.workbench.rdr.model.SexAtBirth.FEMALE)
-              .put(SexAtBirth.INTERSEX, org.pmiops.workbench.rdr.model.SexAtBirth.INTERSEX)
-              .put(
-                  SexAtBirth.NONE_OF_THESE_DESCRIBE_ME,
-                  org.pmiops.workbench.rdr.model.SexAtBirth.NONE_OF_THESE_DESCRIBE_ME)
-              .put(
-                  SexAtBirth.PREFER_NO_ANSWER,
-                  org.pmiops.workbench.rdr.model.SexAtBirth.PREFER_NOT_TO_ANSWER)
-              .build();
+  private static final BiMap<SexAtBirth, RdrSexAtBirth> CLIENT_TO_RDR_SEX_AT_BIRTH =
+      ImmutableBiMap.<SexAtBirth, RdrSexAtBirth>builder()
+          .put(SexAtBirth.MALE, RdrSexAtBirth.MALE)
+          .put(SexAtBirth.FEMALE, RdrSexAtBirth.FEMALE)
+          .put(SexAtBirth.INTERSEX, RdrSexAtBirth.INTERSEX)
+          .put(SexAtBirth.NONE_OF_THESE_DESCRIBE_ME, RdrSexAtBirth.NONE_OF_THESE_DESCRIBE_ME)
+          .put(SexAtBirth.PREFER_NO_ANSWER, RdrSexAtBirth.PREFER_NOT_TO_ANSWER)
+          .build();
 
-  private static final BiMap<Disability, org.pmiops.workbench.rdr.model.Disability>
-      CLIENT_TO_RDR_DISABILITY =
-          ImmutableBiMap.<Disability, org.pmiops.workbench.rdr.model.Disability>builder()
-              .put(Disability.TRUE, org.pmiops.workbench.rdr.model.Disability.YES)
-              .put(Disability.FALSE, org.pmiops.workbench.rdr.model.Disability.NO)
-              .build();
+  private static final BiMap<Disability, RdrDisability> CLIENT_TO_RDR_DISABILITY =
+      ImmutableBiMap.<Disability, RdrDisability>builder()
+          .put(Disability.TRUE, RdrDisability.YES)
+          .put(Disability.FALSE, RdrDisability.NO)
+          .build();
 
   private static final BiMap<SpecificPopulationEnum, RdrWorkspaceDemographic.RaceEthnicityEnum>
       CLIENT_TO_RDR_WORKSPACE_DEMOGRAPHIC_RACE_ETHNICITY =
@@ -108,62 +105,57 @@ public class RdrExportEnums {
                   RdrWorkspaceDemographic.AgeEnum.AGE_75_AND_MORE)
               .build();
 
-  private static final BiMap<Degree, org.pmiops.workbench.rdr.model.Degree> CLIENT_TO_RDR_DEGREE =
-      ImmutableBiMap.<Degree, org.pmiops.workbench.rdr.model.Degree>builder()
-          .put(Degree.PHD, org.pmiops.workbench.rdr.model.Degree.PHD)
-          .put(Degree.MD, org.pmiops.workbench.rdr.model.Degree.MD)
-          .put(Degree.JD, org.pmiops.workbench.rdr.model.Degree.JD)
-          .put(Degree.EDD, org.pmiops.workbench.rdr.model.Degree.EDD)
-          .put(Degree.MSN, org.pmiops.workbench.rdr.model.Degree.MSN)
-          .put(Degree.MS, org.pmiops.workbench.rdr.model.Degree.MS)
-          .put(Degree.MA, org.pmiops.workbench.rdr.model.Degree.MA)
-          .put(Degree.MBA, org.pmiops.workbench.rdr.model.Degree.MBA)
-          .put(Degree.ME, org.pmiops.workbench.rdr.model.Degree.ME)
-          .put(Degree.MSW, org.pmiops.workbench.rdr.model.Degree.MSW)
-          .put(Degree.MPH, org.pmiops.workbench.rdr.model.Degree.MPH)
-          .put(Degree.BA, org.pmiops.workbench.rdr.model.Degree.BA)
-          .put(Degree.BS, org.pmiops.workbench.rdr.model.Degree.BS)
-          .put(Degree.BSN, org.pmiops.workbench.rdr.model.Degree.BSN)
-          .put(Degree.NONE, org.pmiops.workbench.rdr.model.Degree.UNSET)
+  private static final BiMap<Degree, RdrDegree> CLIENT_TO_RDR_DEGREE =
+      ImmutableBiMap.<Degree, RdrDegree>builder()
+          .put(Degree.PHD, RdrDegree.PHD)
+          .put(Degree.MD, RdrDegree.MD)
+          .put(Degree.JD, RdrDegree.JD)
+          .put(Degree.EDD, RdrDegree.EDD)
+          .put(Degree.MSN, RdrDegree.MSN)
+          .put(Degree.MS, RdrDegree.MS)
+          .put(Degree.MA, RdrDegree.MA)
+          .put(Degree.MBA, RdrDegree.MBA)
+          .put(Degree.ME, RdrDegree.ME)
+          .put(Degree.MSW, RdrDegree.MSW)
+          .put(Degree.MPH, RdrDegree.MPH)
+          .put(Degree.BA, RdrDegree.BA)
+          .put(Degree.BS, RdrDegree.BS)
+          .put(Degree.BSN, RdrDegree.BSN)
+          .put(Degree.NONE, RdrDegree.UNSET)
           .build();
 
-  public static org.pmiops.workbench.rdr.model.Race raceToRdrRace(Race race) {
+  public static RdrRace raceToRdrRace(Race race) {
     if (race == null) return null;
     return CLIENT_TO_RDR_RACE.get(race);
   }
 
-  public static org.pmiops.workbench.rdr.model.Ethnicity ethnicityToRdrEthnicity(
-      Ethnicity ethnicity) {
+  public static RdrEthnicity ethnicityToRdrEthnicity(Ethnicity ethnicity) {
     if (ethnicity == null) return null;
     return CLIENT_TO_RDR_ETHNICITY.get(ethnicity);
   }
 
-  public static org.pmiops.workbench.rdr.model.Gender genderToRdrGender(
-      GenderIdentity genderIdentity) {
+  public static RdrGender genderToRdrGender(GenderIdentity genderIdentity) {
     if (genderIdentity == null) return null;
     return CLIENT_TO_RDR_GENDER.get(genderIdentity);
   }
 
-  public static org.pmiops.workbench.rdr.model.Education educationToRdrEducation(
-      Education education) {
+  public static RdrEducation educationToRdrEducation(Education education) {
     if (education == null) return null;
     return CLIENT_TO_RDR_EDUCATION.get(education);
   }
 
-  public static org.pmiops.workbench.rdr.model.SexAtBirth sexAtBirthToRdrSexAtBirth(
-      SexAtBirth sexAtBirth) {
+  public static RdrSexAtBirth sexAtBirthToRdrSexAtBirth(SexAtBirth sexAtBirth) {
     if (sexAtBirth == null) return null;
     return CLIENT_TO_RDR_SEX_AT_BIRTH.get(sexAtBirth);
   }
 
-  public static org.pmiops.workbench.rdr.model.Disability disabilityToRdrDisability(
-      Disability disability) {
-    if (disability == null) return org.pmiops.workbench.rdr.model.Disability.PREFER_NOT_TO_ANSWER;
+  public static RdrDisability disabilityToRdrDisability(Disability disability) {
+    if (disability == null) return RdrDisability.PREFER_NOT_TO_ANSWER;
     return CLIENT_TO_RDR_DISABILITY.get(disability);
   }
 
-  public static org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.RaceEthnicityEnum
-      specificPopulationToRaceEthnicity(SpecificPopulationEnum specificPopulationEnum) {
+  public static RdrWorkspaceDemographic.RaceEthnicityEnum specificPopulationToRaceEthnicity(
+      SpecificPopulationEnum specificPopulationEnum) {
     if (CLIENT_TO_RDR_WORKSPACE_DEMOGRAPHIC_RACE_ETHNICITY.containsKey(specificPopulationEnum)) {
       return CLIENT_TO_RDR_WORKSPACE_DEMOGRAPHIC_RACE_ETHNICITY.get(specificPopulationEnum);
     } else {
@@ -171,8 +163,8 @@ public class RdrExportEnums {
     }
   }
 
-  public static org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.AgeEnum
-      specificPopulationToAge(SpecificPopulationEnum specificPopulationEnum) {
+  public static RdrWorkspaceDemographic.AgeEnum specificPopulationToAge(
+      SpecificPopulationEnum specificPopulationEnum) {
     if (CLIENT_TO_RDR_WORKSPACE_DEMOGRAPHIC_AGE.containsKey(specificPopulationEnum)) {
       return CLIENT_TO_RDR_WORKSPACE_DEMOGRAPHIC_AGE.get(specificPopulationEnum);
     } else {
@@ -180,7 +172,7 @@ public class RdrExportEnums {
     }
   }
 
-  public static org.pmiops.workbench.rdr.model.Degree degreeToRdrDegree(Degree degree) {
+  public static RdrDegree degreeToRdrDegree(Degree degree) {
     if (degree == null) return null;
     return CLIENT_TO_RDR_DEGREE.get(degree);
   }
--- api/src/test/java/org/pmiops/workbench/rdr/RdrMapperTest.java
+++ api/src/test/java/org/pmiops/workbench/rdr/RdrMapperTest.java
@@ -2,15 +2,57 @@ package org.pmiops.workbench.rdr;
 
 import static com.google.common.truth.Truth.assertThat;
 
+import com.google.common.collect.ImmutableList;
+import com.google.common.collect.ImmutableSet;
+import java.sql.Timestamp;
+import java.time.Instant;
 import java.util.stream.Stream;
 import javax.annotation.Nullable;
+import org.junit.jupiter.api.Test;
 import org.junit.jupiter.params.ParameterizedTest;
 import org.junit.jupiter.params.provider.Arguments;
 import org.junit.jupiter.params.provider.MethodSource;
 import org.pmiops.workbench.db.model.DbAccessTier;
+import org.pmiops.workbench.db.model.DbAddress;
 import org.pmiops.workbench.db.model.DbCdrVersion;
+import org.pmiops.workbench.db.model.DbDemographicSurvey;
+import org.pmiops.workbench.db.model.DbInstitution;
+import org.pmiops.workbench.db.model.DbUser;
+import org.pmiops.workbench.db.model.DbVerifiedInstitutionalAffiliation;
 import org.pmiops.workbench.db.model.DbWorkspace;
+import org.pmiops.workbench.model.Degree;
+import org.pmiops.workbench.model.Disability;
+import org.pmiops.workbench.model.Education;
+import org.pmiops.workbench.model.Ethnicity;
+import org.pmiops.workbench.model.GenderIdentity;
+import org.pmiops.workbench.model.InstitutionalRole;
+import org.pmiops.workbench.model.Race;
+import org.pmiops.workbench.model.SexAtBirth;
+import org.pmiops.workbench.model.SpecificPopulationEnum;
+import org.pmiops.workbench.rdr.model.RdrDegree;
+import org.pmiops.workbench.rdr.model.RdrDisability;
+import org.pmiops.workbench.rdr.model.RdrEducation;
+import org.pmiops.workbench.rdr.model.RdrEthnicity;
+import org.pmiops.workbench.rdr.model.RdrGender;
+import org.pmiops.workbench.rdr.model.RdrRace;
+import org.pmiops.workbench.rdr.model.RdrResearcher;
+import org.pmiops.workbench.rdr.model.RdrResearcherVerifiedInstitutionalAffiliation;
+import org.pmiops.workbench.rdr.model.RdrSexAtBirth;
+import org.pmiops.workbench.rdr.model.RdrWorkspace;
 import org.pmiops.workbench.rdr.model.RdrWorkspace.AccessTierEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspace.StatusEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.AccessToCareEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.AgeEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.DisabilityStatusEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.EducationLevelEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.GenderIdentityEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.GeographyEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.IncomeLevelEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.RaceEthnicityEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.SexAtBirthEnum;
+import org.pmiops.workbench.rdr.model.RdrWorkspaceDemographic.SexualOrientationEnum;
+import org.pmiops.workbench.utils.TestMockFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.context.annotation.Import;
@@ -18,6 +60,9 @@ import org.springframework.test.context.junit.jupiter.SpringJUnitConfig;
 
 @SpringJUnitConfig
 public class RdrMapperTest {
+
+  private static final Timestamp TIME1 = Timestamp.from(Instant.parse("2000-01-01T00:00:00.00Z"));
+  private static final Timestamp TIME2 = Timestamp.from(Instant.parse("2001-06-01T00:00:00.00Z"));
 
   @TestConfiguration
   @Import(RdrMapperImpl.class)
@@ -46,6 +91,134 @@ public class RdrMapperTest {
     DbWorkspace from = new DbWorkspace();
     from.setCdrVersion(cdrVersion);
 
-    assertThat(rdrMapper.toRdrModel(from).getAccessTier()).isEqualTo(wantRdrTier);
+    assertThat(rdrMapper.toRdrWorkspace(from).getAccessTier()).isEqualTo(wantRdrTier);
+  }
+
+  @Test
+  public void testMapRdrWorkspace() {
+    assertThat(
+            rdrMapper.toRdrWorkspace(
+                TestMockFactory.createDbWorkspaceStub(
+                    TestMockFactory.createWorkspace("ns", "name"), 1L)))
+        .isEqualTo(
+            new RdrWorkspace()
+                .workspaceId(1)
+                .name("name")
+                .status(StatusEnum.ACTIVE)
+                .diseaseFocusedResearch(true)
+                .diseaseFocusedResearchName("cancer")
+                .methodsDevelopment(true)
+                .controlSet(true)
+                .ancestry(true)
+                .socialBehavioral(true)
+                .populationHealth(true)
+                .drugDevelopment(true)
+                .commercialPurpose(true)
+                .educational(true)
+                .ethicalLegalSocialImplications(false)
+                .otherPurpose(false)
+                .intendToStudy("intended study")
+                .findingsFromStudy("anticipated findings")
+                .focusOnUnderrepresentedPopulations(false)
+                .workspaceDemographic(
+                    new RdrWorkspaceDemographic()
+                        .raceEthnicity(ImmutableList.of(RaceEthnicityEnum.UNSET))
+                        .age(ImmutableList.of(AgeEnum.UNSET))
+                        .sexAtBirth(SexAtBirthEnum.UNSET)
+                        .genderIdentity(GenderIdentityEnum.UNSET)
+                        .sexualOrientation(SexualOrientationEnum.UNSET)
+                        .geography(GeographyEnum.UNSET)
+                        .disabilityStatus(DisabilityStatusEnum.UNSET)
+                        .accessToCare(AccessToCareEnum.UNSET)
+                        .educationLevel(EducationLevelEnum.UNSET)
+                        .incomeLevel(IncomeLevelEnum.UNSET))
+                .accessTier(AccessTierEnum.UNSET));
+  }
+
+  @Test
+  public void testMapRdrWorkspace_otherPopulations() {
+    RdrWorkspace got =
+        rdrMapper.toRdrWorkspace(
+            TestMockFactory.createDbWorkspaceStub(TestMockFactory.createWorkspace("ns", "name"), 1L)
+                .setSpecificPopulationsEnum(ImmutableSet.of(SpecificPopulationEnum.OTHER))
+                .setOtherPopulationDetails("reptilians"));
+    assertThat(got.getWorkspaceDemographic().getOthers()).isEqualTo("reptilians");
+  }
+
+  @Test
+  public void testMapRdrResearcher() {
+    assertThat(
+            rdrMapper.toRdrResearcher(
+                new DbUser()
+                    .setUserId(1)
+                    .setCreationTime(TIME1)
+                    .setLastModifiedTime(TIME2)
+                    .setGivenName("falco")
+                    .setFamilyName("lombardi")
+                    .setAddress(
+                        new DbAddress()
+                            .setStreetAddress1("123 fake st")
+                            .setStreetAddress2("apt 2")
+                            .setCity("Mountain View")
+                            .setState("CA")
+                            .setZipCode("12345")
+                            .setCountry("US"))
+                    .setDemographicSurvey(
+                        new DbDemographicSurvey()
+                            .setEthnicityEnum(Ethnicity.PREFER_NO_ANSWER)
+                            .setGenderIdentityEnumList(
+                                ImmutableList.of(GenderIdentity.PREFER_NO_ANSWER))
+                            .setRaceEnum(ImmutableList.of(Race.PREFER_NO_ANSWER))
+                            .setSexAtBirthEnum(ImmutableList.of(SexAtBirth.PREFER_NO_ANSWER))
+                            .setIdentifiesAsLgbtq(false)
+                            .setLgbtqIdentity("n/a")
+                            .setEducationEnum(Education.NO_EDUCATION)
+                            .setDisabilityEnum(Disability.TRUE))
+                    .setDegreesEnum(ImmutableList.of(Degree.MD))
+                    .setContactEmail("contact@asdf.com"),
+                new DbVerifiedInstitutionalAffiliation()
+                    .setInstitution(
+                        new DbInstitution().setShortName("Foo").setDisplayName("Foo Bar"))
+                    .setInstitutionalRoleEnum(InstitutionalRole.FELLOW)))
+        .isEqualTo(
+            new RdrResearcher()
+                .userId(1)
+                .creationTime(rdrMapper.toModelOffsetTime(TIME1))
+                .modifiedTime(rdrMapper.toModelOffsetTime(TIME2))
+                .givenName("falco")
+                .familyName("lombardi")
+                .streetAddress1("123 fake st")
+                .streetAddress2("apt 2")
+                .city("Mountain View")
+                .state("CA")
+                .zipCode("12345")
+                .country("US")
+                .ethnicity(RdrEthnicity.PREFER_NOT_TO_ANSWER)
+                .gender(ImmutableList.of(RdrGender.PREFER_NOT_TO_ANSWER))
+                .race(ImmutableList.of(RdrRace.PREFER_NOT_TO_ANSWER))
+                .sexAtBirth(ImmutableList.of(RdrSexAtBirth.PREFER_NOT_TO_ANSWER))
+                .identifiesAsLgbtq(false)
+                .lgbtqIdentity("n/a")
+                .education(RdrEducation.NO_EDUCATION)
+                .disability(RdrDisability.YES)
+                .degrees(ImmutableList.of(RdrDegree.MD))
+                .email("contact@asdf.com")
+                .verifiedInstitutionalAffiliation(
+                    new RdrResearcherVerifiedInstitutionalAffiliation()
+                        .institutionShortName("Foo")
+                        .institutionDisplayName("Foo Bar")
+                        .institutionalRole("FELLOW")));
+  }
+
+  @Test
+  public void testMapRdrResearcher_otherRole() {
+    RdrResearcher got =
+        rdrMapper.toRdrResearcher(
+            new DbUser(),
+            new DbVerifiedInstitutionalAffiliation()
+                .setInstitution(new DbInstitution().setShortName("Foo").setDisplayName("Foo Bar"))
+                .setInstitutionalRoleEnum(InstitutionalRole.OTHER)
+                .setInstitutionalRoleOtherText("CEO"));
+    assertThat(got.getVerifiedInstitutionalAffiliation().getInstitutionalRole()).isEqualTo("CEO");
   }
 }
