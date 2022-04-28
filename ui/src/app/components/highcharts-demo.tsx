0d5a3c929a809c43e3234dde17732df667b5d20c
Will Dolbeer <WillDolbeer@VICTRs-MacBook-Pro-2.local>
2022-04-28 09:47:33 -0500

    create test component for highcharts

---
+++ ui/src/app/components/highcharts-demo.tsx
@@ -0,0 +1,131 @@
+import * as React from 'react';
+import * as highCharts from 'highcharts';
+import HighchartsReact from 'highcharts-react-official';
+
+import { getChartObj } from 'app/cohort-search/utils';
+import colors from 'app/styles/colors';
+
+interface Props {
+  mode: string;
+  data: any;
+}
+
+interface State {
+  options: any;
+}
+
+export class DemoChart extends React.Component<Props, State> {
+  constructor(props: Props) {
+    super(props);
+    this.state = { options: null };
+  }
+
+  componentDidMount(): void {
+    this.getChartOptions();
+  }
+
+  componentDidUpdate(prevProps: any): void {
+    if (prevProps.mode !== this.props.mode) {
+      this.getChartOptions();
+    }
+  }
+
+  getChartOptions() {
+    const { mode } = this.props;
+    const normalized = mode === 'normalized';
+    const { categories, series } = this.getCategoriesAndSeries();
+    const height = Math.max(categories.length * 30, 200);
+    const options = {
+      chart: {
+        height,
+        type: 'bar',
+      },
+      credits: {
+        enabled: false,
+      },
+      title: {
+        text: '',
+      },
+      xAxis: {
+        categories,
+        tickLength: 0,
+        tickPixelInterval: 50,
+      },
+      yAxis: {
+        labels: {
+          format: '{value}' + (normalized ? '%' : ''),
+        },
+        min: 0,
+        title: {
+          text: '',
+        },
+      },
+      colors: colors.chartColors,
+      legend: {
+        enabled: false,
+      },
+      plotOptions: {
+        bar: {
+          groupPadding: 0,
+          pointPadding: 0.1,
+        },
+        series: {
+          stacking: normalized ? 'percent' : 'normal',
+        },
+      },
+      series,
+    };
+    this.setState({ options });
+  }
+
+  getCategoriesAndSeries() {
+    const { data } = this.props;
+    const codeMap = {
+      M: 'Male',
+      F: 'Female',
+      'No matching concept': 'Unknown',
+    };
+    const getKey = (dat) => {
+      const gender = !!codeMap[dat.name] ? codeMap[dat.name] : dat.name;
+      return `${gender} ${dat.ageRange || 'Unknown'}`;
+    };
+    const categories = data
+      .reduce((acc, datum) => {
+        const key = getKey(datum);
+        if (!acc.includes(key)) {
+          acc.push(key);
+        }
+        return acc;
+      }, [])
+      .sort((a, b) => (a > b ? 1 : -1));
+    const series = data
+      .reduce((acc, datum) => {
+        const key = getKey(datum);
+        const obj = { x: categories.indexOf(key), y: datum.count };
+        const index = acc.findIndex((d) => d.name === datum.race);
+        if (index === -1) {
+          acc.push({ name: datum.race, data: [obj] });
+        } else {
+          acc[index].data.push(obj);
+        }
+        return acc;
+      }, [])
+      .sort((a, b) => (a.name < b.name ? 1 : -1));
+    return { categories, series };
+  }
+
+  render() {
+    const { options } = this.state;
+    return (
+      <div style={{ minHeight: 200 }}>
+        {options && (
+          <HighchartsReact
+            highcharts={highCharts}
+            options={options}
+            callback={getChartObj}
+          />
+        )}
+      </div>
+    );
+  }
+}
--- ui/src/app/pages/data/cohort-review/query-report.component.tsx
+++ ui/src/app/pages/data/cohort-review/query-report.component.tsx
@@ -15,6 +15,7 @@ import {
 } from 'generated/fetch';
 
 import { ComboChart } from 'app/components/combo-chart.component';
+import { DemoChart } from 'app/components/highcharts-demo';
 import { SpinnerOverlay } from 'app/components/spinners';
 import { WithSpinnerOverlayProps } from 'app/components/with-spinner-overlay';
 import { CohortDefinitionComponent } from 'app/pages/data/cohort-review/cohort-definition.component';
@@ -620,6 +621,22 @@ export const QueryReport = fp.flow(
                                 />
                               </div>
                             ))}
+                        </div>
+                        {/* --------------------------------------------------- */}
+                        {/* Demo chart component for testing Highcharts options */}
+                        {/* --------------------------------------------------- */}
+                        <div
+                          style={{
+                            ...styles.col,
+                            flex: '0 0 75%',
+                            maxWidth: '75%',
+                          }}
+                        >
+                          <div style={demoTitle}>Highcharts Test</div>
+                          <div style={styles.graphBorder}>
+                            {data && <DemoChart mode={'stacked'} data={data} />}
+                            {chartsLoading && <SpinnerOverlay />}
+                          </div>
                         </div>
                       </div>
                     </div>
