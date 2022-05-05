cb950131b68266fb2faf477d7a0dedd1d5e254eb
Brian Freeman <brian.d.freeman@gmail.com>
2022-05-05 13:40:47 -0500

    RW-8220 adding code to merge duplicated questions

--- api/db-cdr/generate-cdr/build-cb-survey-version.sh
+++ api/db-cdr/generate-cdr/build-cb-survey-version.sh
@@ -10,7 +10,7 @@ export BQ_DATASET=$2  # dataset
 #####################################
 # INSERT into cb_survey_version TABLE
 #####################################
-echo "Insert cb_survey_version"
+echo "Insert COPE Survey into cb_survey_version"
 bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_survey_version\`
     (survey_version_concept_id,survey_concept_id,display_name,display_order)
@@ -18,21 +18,21 @@ SELECT survey_version_concept_id, survey_concept_id, display_name, ROW_NUMBER() 
 FROM (
   SELECT distinct o.survey_version_concept_id,
   (SELECT concept_id FROM \`$BQ_PROJECT.$BQ_DATASET.concept\` WHERE concept_class_id = 'Module' AND concept_code = 'cope') as survey_concept_id,
-  REPLACE (c.concept_name, 'COPE Survey', '') as display_name
+  CONCAT(REPLACE(c.concept_name, 'COPE Survey', ''), ' ', 'COPE Survey') as display_name
 FROM \`$BQ_PROJECT.$BQ_DATASET.observation_ext\` o
 JOIN \`$BQ_PROJECT.$BQ_DATASET.concept\` c on c.concept_id = o.survey_version_concept_id
 WHERE o.survey_version_concept_id is not null
 AND c.vocabulary_id = 'AoU_Custom') x"
 
-echo "Insert cb_survey_version"
+echo "Insert Vaccine Survey into cb_survey_version"
 bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.cb_survey_version\`
     (survey_version_concept_id,survey_concept_id,display_name,display_order)
-SELECT survey_version_concept_id, survey_concept_id, display_name, ROW_NUMBER() OVER (ORDER BY concept_code) AS display_order
+SELECT survey_version_concept_id, survey_concept_id, display_name, ROW_NUMBER() OVER (ORDER BY concept_code) + (SELECT MAX(display_order) FROM \`$BQ_PROJECT.$BQ_DATASET.cb_survey_version\`) AS display_order
 FROM (
   SELECT distinct o.survey_version_concept_id,
   (SELECT concept_id FROM \`$BQ_PROJECT.$BQ_DATASET.concept\` WHERE concept_class_id = 'Module' AND concept_code = 'cope_vaccine3') as survey_concept_id,
-  CASE WHEN c.concept_name = 'COVID-19 Vaccine Survey' THEN 'Winter 2021' ELSE REPLACE (c.concept_name, ' Minute Survey on COVID-19 Vaccines', '') END as display_name,
+  CASE WHEN c.concept_name = 'COVID-19 Vaccine Survey' THEN 'Winter 2021 Minute Survey' ELSE REPLACE (c.concept_name, ' on COVID-19 Vaccines', '') END as display_name,
   concept_code
 FROM \`$BQ_PROJECT.$BQ_DATASET.observation_ext\` o
 JOIN \`$BQ_PROJECT.$BQ_DATASET.concept\` c on c.concept_id = o.survey_version_concept_id
