73a3865668caa4b0b329260130a7599cc48f8228
Joel Thibault <thibault@broad.dev>
2022-05-06 16:28:01 -0400

    unrelated - cleanup UserAccessModuleMapper

--- api/src/main/java/org/pmiops/workbench/access/UserAccessModuleMapper.java
+++ api/src/main/java/org/pmiops/workbench/access/UserAccessModuleMapper.java
@@ -1,13 +1,8 @@
 package org.pmiops.workbench.access;
 
 import java.sql.Timestamp;
-import javax.annotation.Nullable;
-import org.mapstruct.AfterMapping;
-import org.mapstruct.Context;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
-import org.mapstruct.NullValueMappingStrategy;
 import org.pmiops.workbench.db.model.DbUserAccessModule;
 import org.pmiops.workbench.model.AccessModuleStatus;
 import org.pmiops.workbench.utils.mappers.CommonMappers;
@@ -15,22 +10,11 @@ import org.pmiops.workbench.utils.mappers.MapStructConfig;
 
 @Mapper(
     config = MapStructConfig.class,
-    uses = {CommonMappers.class, AccessModuleNameMapper.class},
-    nullValueMappingStrategy = NullValueMappingStrategy.RETURN_DEFAULT)
+    uses = {CommonMappers.class, AccessModuleNameMapper.class})
 public interface UserAccessModuleMapper {
-  @Mapping(target = "moduleName", source = "accessModule.name")
-  @Mapping(target = "completionEpochMillis", source = "completionTime")
-  @Mapping(target = "bypassEpochMillis", source = "bypassTime")
-  // expirationEpochMillis is derived value, will calculate & set it later.
-  @Mapping(target = "expirationEpochMillis", ignore = true)
-  AccessModuleStatus dbToModule(DbUserAccessModule source, @Context Timestamp expirationTime);
-
-  @AfterMapping
-  default void afterMappingDbToModel(
-      @MappingTarget AccessModuleStatus accessModuleStatus,
-      @Nullable @Context Timestamp expirationTime) {
-    if (expirationTime != null) {
-      accessModuleStatus.setExpirationEpochMillis(expirationTime.getTime());
-    }
-  }
+  @Mapping(target = "moduleName", source = "source.accessModule.name")
+  @Mapping(target = "completionEpochMillis", source = "source.completionTime")
+  @Mapping(target = "bypassEpochMillis", source = "source.bypassTime")
+  @Mapping(target = "expirationEpochMillis", source = "expirationTime")
+  AccessModuleStatus dbToModule(DbUserAccessModule source, Timestamp expirationTime);
 }
