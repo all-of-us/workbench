26aa3f8f6cc595a4b59fab0e06f79d47612351e8
CH Albach <calbach@google.com>
2022-05-06 17:49:58 -0700

    [risk=low] Initial workflow config files

--- api/src/main/webapp/static/initialize_notebook_runtime.sh
+++ api/src/main/webapp/static/initialize_notebook_runtime.sh
@@ -49,3 +49,84 @@ cat <<EOF | sudo -E -u jupyter tee ${ignore_file}
 EOF
 
 sudo -E -u jupyter /usr/bin/git config --global core.excludesfile ${ignore_file}
+
+
+# Initialize a default nextflow config. Don't overwrite in case the user has
+# customized their nextflow config file on their PD.
+nextflow_config=/home/jupyter/nextflow.config
+if [ ! -f "${nextflow_config}" ]; then
+  cat <<EOF > "${nextflow_config}"
+profiles {
+  gls {
+      process.executor = "google-lifesciences"
+      process.container = "gcr.io/google-containers/ubuntu-slim:0.14"
+      workDir = "${WORKSPACE_BUCKET}/workflows/nextflow-scratch"
+      google.location = "us-central1"
+      google.zone  = "us-central1-a"
+      google.project = "${GOOGLE_PROJECT}"
+      google.enableRequesterPaysBuckets = true
+      google.lifeSciences.debug = true
+      google.lifeSciences.serviceAccountEmail = "${PET_SA_EMAIL}"
+      google.lifeSciences.network = "network"
+      google.lifeSciences.subnetwork = "subnetwork"
+      google.lifeSciences.usePrivateAddress = false
+      google.lifeSciences.copyImage = "gcr.io/google.com/cloudsdktool/cloud-sdk:alpine"
+  }
+}
+EOF
+fi
+
+# Initialize a default Cromwell config. Don't overwrite in case the user has
+# customized their nextflow config file on their PD.
+cromwell_config=/home/jupyter/cromwell.config
+if [ ! -f "${cromwell_config}" ]; then
+  cat <<EOF > "${cromwell_config}"
+include required(classpath("application"))
+
+google {
+  application-name = "cromwell"
+  auths = [{
+    name = "application_default"
+    scheme = "application_default"
+  }]
+}
+
+backend {
+  default = "PAPIv2-beta"
+  providers {
+
+    # Disables the Local backend
+    Local.config.root = "/dev/null"
+
+    PAPIv2-beta {
+      actor-factory = "cromwell.backend.google.pipelines.v2beta.PipelinesApiLifecycleActorFactory"
+
+      config {
+        project = "${GOOGLE_PROJECT}"
+        concurrent-job-limit = 10
+        root = "${WORKSPACE_BUCKET}/workflows/cromwell-executions"
+
+        virtual-private-cloud {
+          network-label-key = "vpc-network-name"
+          subnetwork-label-key = "vpc-subnetwork-name"
+          auth = "application_default"
+        }
+
+        genomics {
+          auth = "application_default"
+          compute-service-account = "${PET_SA_EMAIL}"
+          endpoint-url = "https://lifesciences.googleapis.com/"
+          location = "us-central1"
+        }
+
+        filesystems {
+          gcs {
+            auth = "application_default"
+          }
+        }
+      }
+    }
+  }
+}
+EOF
+fi
