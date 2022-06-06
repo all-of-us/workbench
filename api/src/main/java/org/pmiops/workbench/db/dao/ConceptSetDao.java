14e135f3f794b72dd28bf496f9140f1dde99f21c
Brian Freeman <brian.d.freeman@gmail.com>
2022-06-06 15:13:47 -0500

    RW-8420 fix db call

--- api/src/main/java/org/pmiops/workbench/db/dao/ConceptSetDao.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/ConceptSetDao.java
@@ -4,7 +4,9 @@ import java.util.Collection;
 import java.util.List;
 import java.util.Optional;
 import org.pmiops.workbench.db.model.DbConceptSet;
+import org.springframework.data.jpa.repository.Query;
 import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.repository.query.Param;
 
 public interface ConceptSetDao extends CrudRepository<DbConceptSet, Long> {
 
@@ -19,5 +21,11 @@ public interface ConceptSetDao extends CrudRepository<DbConceptSet, Long> {
 
   int countByWorkspaceId(long workspaceId);
 
-  long countByConceptSetId(long conceptSetId);
+  @Query(
+      value =
+          "select count(*) "
+              + "from concept_set_concept_id "
+              + "where concept_set_id = :conceptSetId",
+      nativeQuery = true)
+  long countByConceptSetId(@Param("conceptSetId") Long conceptSetId);
 }
