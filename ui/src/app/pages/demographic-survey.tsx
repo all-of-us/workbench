990323812afd7c49745bee516dff58e50adbce32
evrii <erollins@broadinstitute.org>
2022-06-14 14:21:06 -0400

    Added submission button and error handling.

--- ui/src/app/pages/demographic-survey.tsx
+++ ui/src/app/pages/demographic-survey.tsx
@@ -1,10 +1,15 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 
+import { Profile } from 'generated/fetch';
+
+import { Button } from 'app/components/buttons';
 import DemographicSurveyV2 from 'app/components/demographic-survey-v2';
+import { TooltipTrigger } from 'app/components/popups';
 import { profileStore } from 'app/utils/stores';
 
 export const DemographicSurvey = (props) => {
+  const [errors, setErrors] = useState(null);
   const [profile, setProfile] = useState(null);
   const [loading, setLoading] = useState(true);
 
@@ -14,11 +19,54 @@ export const DemographicSurvey = (props) => {
     setLoading(false);
   }, []);
 
+  const handleSubmit = () => {
+    props.showSpinner();
+  };
+
+  const handleUpdate = (updatedProfile: Profile, updatedErrors: any) => {
+    setErrors(updatedErrors);
+  };
+
   if (loading) {
     return <></>;
   }
+
   return profile ? (
-    <DemographicSurveyV2 profile={profile} />
+    <>
+      <DemographicSurveyV2 profile={profile} onUpdate={handleUpdate} />
+      <TooltipTrigger
+        content={
+          errors && (
+            <>
+              <div>Please review the following:</div>
+              <ul>
+                {errors && (
+                  <>
+                    {Object.keys(errors).map((key) => (
+                      <li key={errors[key][0]}>{errors[key][0]}</li>
+                    ))}
+                    <li>
+                      You may select "Prefer not to answer" for each unfilled
+                      item listed above to continue
+                    </li>
+                  </>
+                )}
+              </ul>
+            </>
+          )
+        }
+      >
+        <Button
+          disabled={!!errors}
+          type='primary'
+          data-test-id={'submit-button'}
+          onClick={handleSubmit}
+          style={{ margin: '1rem 0rem' }}
+        >
+          Submit
+        </Button>
+      </TooltipTrigger>
+    </>
   ) : (
     <div style={{ marginTop: '1rem' }}>
       Unfortunately, your profile did not load. Please try again later.
