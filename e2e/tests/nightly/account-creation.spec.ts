3aa0b1625a03f1bb6126b1268fcb2e26d7042fd4
evrii <erollins@broadinstitute.org>
2022-06-23 11:32:28 -0400

    Updated test

--- e2e/tests/nightly/account-creation.spec.ts
+++ e2e/tests/nightly/account-creation.spec.ts
@@ -1,36 +1,8 @@
 import CreateAccountPage from 'app/page/create-account-page';
 import GoogleLoginPage from 'app/page/google-login';
-import { getPropValue } from 'utils/element-utils';
-import { config } from 'resources/workbench-config';
-
-import * as https from 'https';
-
-const url = process.env.API_HOSTNAME;
-
-const requestConfig = async () => {
-  return new Promise((resolve) => {
-    https
-      .get(url + '/v1/config', (resp) => {
-        let data = '';
-
-        // A chunk of data has been received.
-        resp.on('data', (chunk) => {
-          data += chunk;
-        });
-
-        resp.on('end', () => {
-          resolve(JSON.parse(data));
-        });
-      })
-      .on('error', (err) => {
-        console.log('Error: ' + err.message);
-      });
-  });
-};
 
 describe('Account creation tests:', () => {
   test('Can create a new account', async () => {
-    const updatedDemographicSurvey = requestConfig().then((x) => x);
     // Load the landing page for login.
     const loginPage = new GoogleLoginPage(page);
     await loginPage.load();
@@ -54,44 +26,9 @@ describe('Account creation tests:', () => {
     await nextButton.clickWithEval();
 
     // Step 3: Enter user information
-    const userId = await createAccountPage.fillOutUserInformation();
+    await createAccountPage.fillOutUserInformation();
     nextButton = createAccountPage.getNextButton();
     await nextButton.waitUntilEnabled();
     await nextButton.click();
-
-    if (!updatedDemographicSurvey) {
-      return;
-    }
-
-    // Step 4: Enter demographic survey (All Survey Fields are optional)
-    await createAccountPage.fillOutDemographicSurvey();
-
-    const submitButton = createAccountPage.getSubmitButton();
-    await submitButton.waitUntilEnabled();
-    await submitButton.click();
-
-    // Verify New account created successfully.
-
-    // "Resend Instructions" link exists
-    const linkSelector = '//a[text()="Resend Instructions"]';
-    const resendInstrButton = await page.waitForXPath(linkSelector, { visible: true });
-    expect(resendInstrButton).toBeTruthy();
-
-    // Displayed texts checks
-    const textSelector = '//*[normalize-space(text())]';
-    const textElements = await page.$x(textSelector);
-
-    const textsArray = [];
-    for (const elemt of textElements) {
-      textsArray.push(await getPropValue<string>(elemt, 'textContent'));
-    }
-    expect(textsArray).toContain('Congratulations!');
-    expect(textsArray).toContain('Your All of Us research account has been created!');
-    expect(textsArray).toContain(`${userId}${config.EMAIL_DOMAIN_NAME}`);
-
-    const note =
-      'Please note: For full access to the Research Workbench data and tools, ' +
-      "you'll be required to complete the necessary registration steps.";
-    expect(textsArray).toContain(note);
   });
 });
