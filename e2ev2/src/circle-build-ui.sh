753556e578f21959d502d07ebbee77e5d46b8195
David Mohs <dgit@mohsinc.com>
2022-06-28 18:13:33 -0400

    First pass at deploy-then-run

---
+++ e2ev2/src/circle-build-ui.sh
@@ -0,0 +1,11 @@
+#!/usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+set -xv
+
+echo "$SA_KEY_JSON" | gcloud auth activate-service-account "$SA_EMAIL" --key-file=-
+gsutil cp gs://all-of-us-workbench-test-credentials/.npmrc .
+yarn install
+yarn codegen
+REACT_APP_ENVIRONMENT=test yarn run build --aot --no-watch --no-progress
---
+++ e2ev2/src/circle-run-tests.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/env bash
+
+# http://redsymbol.net/articles/unofficial-bash-strict-mode/
+set -euo pipefail
+set -xv
+
+# https://github.com/nodejs/node/discussions/43184?sort=new
+cat /etc/ssl/openssl.cnf | grep -v 'providers = provider_sect' > openssl.cnf
+sudo cp openssl.cnf /etc/ssl/openssl.cnf
+
+mkdir screenshots
+
+PUPPETEER_EXECUTABLE_PATH=/usr/bin/google-chrome
+JEST_SILENT_REPORTER_DOTS=true
+JEST_SILENT_REPORTER_SHOW_PATHS=true
+yarn test --reporters=jest-silent-reporter
