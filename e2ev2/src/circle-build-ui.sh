0255c75b5d5a16ee6108e091283c20a9a2b6e8f0
David Mohs <dmohs@broadinstitute.org>
2022-06-28 21:31:18 -0400

    WIP

--- e2ev2/src/circle-build-ui.sh
+++ e2ev2/src/circle-build-ui.sh
@@ -2,9 +2,11 @@
 
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
-set -xv
 
 echo "$SA_KEY_JSON" | gcloud auth activate-service-account "$SA_EMAIL" --key-file=-
+
+set -xv
+
 gsutil cp gs://all-of-us-workbench-test-credentials/.npmrc .
 yarn install
 yarn codegen
--- e2ev2/src/circle-deploy-ui.sh
+++ e2ev2/src/circle-deploy-ui.sh
@@ -2,8 +2,10 @@
 
 # http://redsymbol.net/articles/unofficial-bash-strict-mode/
 set -euo pipefail
-set -xv
 
 echo "$SA_KEY_JSON" | gcloud auth activate-service-account "$SA_EMAIL" --key-file=-
+
+set -xv
+
 SHORT_HASH="$(git log -n 1 --pretty='format:%C(auto)%h')"
 gcloud app deploy --project=all-of-us-workbench-test --version="$SHORT_HASH" --no-promote
