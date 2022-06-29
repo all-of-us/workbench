b5a272d486ed6d74b1c9814003b511f8ca3fd6f7
David Mohs <dmohs@broadinstitute.org>
2022-06-28 22:59:35 -0400

    WIP

--- e2ev2/src/circle-deploy-ui.sh
+++ e2ev2/src/circle-deploy-ui.sh
@@ -7,10 +7,17 @@ echo "$SA_KEY_JSON" | gcloud auth activate-service-account "$SA_EMAIL" --key-fil
 
 set -v
 
+# Collect garbage
 gcloud app versions list --service=default --project=all-of-us-workbench-test \
   --sort-by=version.createTime --filter='id~pr-[a-z0-9]{7}' --format='value(id)' \
   > deployed-pr-versions.txt
-
+MAX_PR_VERSIONS=20
+VERSIONS_TO_DELETE=$(expr $(cat deployed-pr-versions.txt|wc -l) - $MAX_PR_VERSIONS)
+if [[ $VERSIONS_TO_DELETE > 0 ]]; then
+  echo deleting $VERSIONS_TO_DELETE old versions...
+  cat deployed-pr-versions.txt | head -n $VERSIONS_TO_DELETE | \
+    xargs gcloud app versions delete --service=default --project=all-of-us-workbench-test
+fi
 
 SHORT_HASH="$(git log -n 1 --pretty='format:%C(auto)%h')"
 gcloud app deploy --project=all-of-us-workbench-test --version="pr-$SHORT_HASH" --no-promote
--- e2ev2/src/config.js
+++ e2ev2/src/config.js
@@ -1,3 +1,5 @@
+const assert = require('assert').strict
+
 const export_ = o => Object.keys(o).forEach(k => { module.exports[k] = o[k] })
 
 const projectName = 'all-of-us-workbench-test'
@@ -6,6 +8,9 @@ export_({projectName})
 const usernames = ['puppeteer-tester-6@fake-research-aou.org']
 export_({usernames})
 
-const urlRoot = () => `https://pr-${process.env.SHORT_HASH}-dot-${projectName}.appspot.com`
+const urlRoot = () => {
+  assert(process.env.SHORT_HASH, 'SHORT_HASH is not defined')
+  `https://pr-${process.env.SHORT_HASH}-dot-${projectName}.appspot.com`
+}
 export_({urlRoot})
 
