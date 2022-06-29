977170863a549a8b1d053b908144e61ea3e1616b
David Mohs <dmohs@broadinstitute.org>
2022-06-28 22:24:08 -0400

    WIP

--- e2ev2/src/circle-deploy-ui.sh
+++ e2ev2/src/circle-deploy-ui.sh
@@ -8,7 +8,7 @@ echo "$SA_KEY_JSON" | gcloud auth activate-service-account "$SA_EMAIL" --key-fil
 set -v
 
 SHORT_HASH="$(git log -n 1 --pretty='format:%C(auto)%h')"
-gcloud app deploy --project=all-of-us-workbench-test --version="$SHORT_HASH" --no-promote
+gcloud app deploy --project=all-of-us-workbench-test --version="pr-$SHORT_HASH" --no-promote
 
 # Warm it up:
-curl https://"$SHORT_HASH"-dot-all-of-us-workbench-test.uc.r.appspot.com/
+curl https://"pr-$SHORT_HASH"-dot-all-of-us-workbench-test.uc.r.appspot.com/
