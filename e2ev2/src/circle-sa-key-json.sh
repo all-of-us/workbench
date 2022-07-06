6224671167d047c7c9f6f1c05ce7e677866cac92
David Mohs <dgit@mohsinc.com>
2022-07-06 17:18:44 -0400

    WIP

--- e2ev2/src/circle-sa-key-json.sh
+++
@@ -1,4 +0,0 @@
-SA_KEY_JSON="$(echo "$GCLOUD_CREDENTIALS" \
-  | openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "$GCLOUD_CREDENTIALS_KEY")"
-
-SA_EMAIL="$(echo "$SA_KEY_JSON" | jq -r .client_email)"
