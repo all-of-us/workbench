e0325c2511ca71e11a8a7f214e104daec925bb82
David Mohs <dgit@mohsinc.com>
2022-07-14 13:53:30 -0400

    lint

--- ui/src/environments/environment.pr-site-4.ts
+++ ui/src/environments/environment.pr-site-4.ts
@@ -4,7 +4,8 @@ import { testEnvironmentBase } from 'environments/test-env-base';
 // This file is used in the deployed test environment
 export const environment: Environment = {
   ...testEnvironmentBase,
-  allOfUsApiUrl: 'https://pr-4-dot-api-dot-all-of-us-workbench-test.appspot.com',
+  allOfUsApiUrl:
+    'https://pr-4-dot-api-dot-all-of-us-workbench-test.appspot.com',
   displayTag: 'Test',
   debug: false,
 };
