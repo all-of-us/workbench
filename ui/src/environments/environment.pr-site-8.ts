eec2b5c3ccc5282e431c081d19f4a4351c8a71a8
David Mohs <dgit@mohsinc.com>
2022-07-14 10:40:37 -0400

    First pass at API PR tests

---
+++ ui/src/environments/environment.pr-site-8.ts
@@ -0,0 +1,10 @@
+import { Environment } from 'environments/environment-type';
+import { testEnvironmentBase } from 'environments/test-env-base';
+
+// This file is used in the deployed test environment
+export const environment: Environment = {
+  ...testEnvironmentBase,
+  allOfUsApiUrl: 'https://pr-8-dot-api-dot-all-of-us-workbench-test.appspot.com',
+  displayTag: 'Test',
+  debug: false,
+};
