a0a6a56ea4386114e8a7c6772441194b74455b08
David Mohs <dgit@mohsinc.com>
2022-07-21 17:16:41 -0400

    Share log file name

--- e2ev2/src/circle-run-tests.sh
+++ e2ev2/src/circle-run-tests.sh
@@ -22,6 +22,7 @@ FAILED_TESTS=$(gsutil cat $BKT_ROOT/\*.$CIRCLE_SHA1.txt || echo -n)
 gsutil rm $BKT_ROOT/\*.$CIRCLE_SHA1.txt || true
 
 set +e
+export FAILED_TESTS_LOG=failed-tests.txt
 # This should do the right thing. If $FAILED_TESTS is empty, nothing is specified, so Jest runs
 # all tests.
 yarn test $FAILED_TESTS \
@@ -30,7 +31,7 @@ TESTS_EXIT_CODE=$?
 set -e
 
 if [[ $TESTS_EXIT_CODE -ne 0 ]]; then
-  gsutil cp failed-tests.txt $BKT_ROOT/$CIRCLE_BUILD_NUM.$CIRCLE_SHA1.txt || true
+  gsutil cp $FAILED_TESTS_LOG $BKT_ROOT/$CIRCLE_BUILD_NUM.$CIRCLE_SHA1.txt || true
 
   # Collect garbage
   gsutil ls $BKT_ROOT | tail -n 10 > latest.txt
--- e2ev2/src/failure-reporter.js
+++ e2ev2/src/failure-reporter.js
@@ -4,7 +4,8 @@ class FailureReporter {
   onTestResult(test, testResult) {
     // Stole this "isFailed" check from another reporter.
     if (!testResult.skipped && testResult.failureMessage) {
-      fs.writeFileSync(logFileName, test.path.slice(process.env.PWD.length+1)+'\n', {flag: 'a'})
+      fs.writeFileSync(process.env.FAILED_TESTS_LOG,
+        test.path.slice(process.env.PWD.length+1)+'\n', {flag: 'a'})
     }
   }
 }
