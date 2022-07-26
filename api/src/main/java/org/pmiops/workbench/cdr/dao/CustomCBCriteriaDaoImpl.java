f38ce467ffdf7e18f968765be800c0e9166827ae
freemabd <brian.d.freeman@gmail.com>
2022-07-26 14:18:41 -0500

    allow for queries iwth multiple ends with terms

---
+++ api/src/main/java/org/pmiops/workbench/cdr/dao/CustomCBCriteriaDaoImpl.java
@@ -0,0 +1,132 @@
+package org.pmiops.workbench.cdr.dao;
+
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+import java.util.Optional;
+import java.util.StringJoiner;
+import java.util.stream.IntStream;
+import org.jetbrains.annotations.NotNull;
+import org.jetbrains.annotations.Nullable;
+import org.pmiops.workbench.cdr.model.DbCriteria;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.PageImpl;
+import org.springframework.data.domain.Pageable;
+import org.springframework.jdbc.core.BeanPropertyRowMapper;
+import org.springframework.jdbc.core.RowMapper;
+import org.springframework.jdbc.core.namedparam.MapSqlParameterSource;
+import org.springframework.jdbc.core.namedparam.NamedParameterJdbcTemplate;
+
+public class CustomCBCriteriaDaoImpl implements CustomCBCriteriaDao {
+
+  public static class QueryAndParameters {
+
+    private final String query;
+    private final MapSqlParameterSource parameters;
+
+    QueryAndParameters(String query, MapSqlParameterSource parameters) {
+      this.query = query;
+      this.parameters = parameters;
+    }
+
+    public String getQuery() {
+      return query;
+    }
+
+    public MapSqlParameterSource getParameters() {
+      return parameters;
+    }
+  }
+
+  private static final String ENDS_WITH_WITHOUT_TERM =
+      "select *\n"
+          + "from cb_criteria\n"
+          + "where is_standard = :standard\n"
+          + "and match(full_text) against(concat('+[', :domain, '_rank1]') in boolean mode)\n"
+          + "and (%s)\n"
+          + "order by est_count desc, name asc\n";
+
+  private static final String ENDS_WITH_WITH_TERM =
+      "select *\n"
+          + "from cb_criteria\n"
+          + "where is_standard = :standard\n"
+          + "and match(full_text) against(concat(:term, '+[', :domain, '_rank1]') in boolean mode)\n"
+          + "and (%s)\n"
+          + "order by est_count desc, name asc\n";
+
+  private static final String DYNAMIC_SQL = "upper(name) like upper(%s)";
+
+  private static final String LIMIT_OFFSET = "limit %s offset %s\n";
+
+  private static final String OR = "\nor\n";
+
+  @Autowired private NamedParameterJdbcTemplate namedParameterJdbcTemplate;
+
+  @Override
+  public Page<DbCriteria> findCriteriaByDomainAndStandardAndNameEndsWith(
+      String domain, Boolean standard, List<String> endsWithList, Pageable page) {
+    QueryAndParameters queryAndParameters =
+        generateQueryAndParameters(ENDS_WITH_WITHOUT_TERM, domain, standard, Optional.empty(), endsWithList);
+    return new PageImpl<>(
+        queryForPaginatedList(page, queryAndParameters), page, count(queryAndParameters));
+  }
+
+  @Override
+  public Page<DbCriteria> findCriteriaByDomainAndStandardAndTermAndNameEndsWith(
+      String domain, Boolean standard, String term, List<String> endsWithList, Pageable page) {
+    QueryAndParameters queryAndParameters =
+        generateQueryAndParameters(
+            ENDS_WITH_WITH_TERM, domain, standard, Optional.of(term), endsWithList);
+    return new PageImpl<>(
+        queryForPaginatedList(page, queryAndParameters), page, count(queryAndParameters));
+  }
+
+  protected QueryAndParameters generateQueryAndParameters(
+      String sql, String domain, Boolean standard, Optional term, List<String> endsWithList) {
+    MapSqlParameterSource parameters = new MapSqlParameterSource();
+    parameters.addValue("domain", domain).addValue("standard", standard);
+    if (term.isPresent()) {
+      parameters.addValue("term", term.get());
+    }
+    StringJoiner joiner = new StringJoiner(OR);
+
+    IntStream.range(0, endsWithList.size())
+        .forEach(
+            idx -> {
+              String endsWith = endsWithList.get(idx);
+              String parameterName = "endsWith" + idx;
+              parameters.addValue(parameterName, endsWith);
+              joiner.add(String.format(DYNAMIC_SQL, ":" + parameterName));
+            });
+
+    return new QueryAndParameters(String.format(sql, joiner), parameters);
+  }
+
+  @NotNull
+  private List<DbCriteria> queryForPaginatedList(
+      Pageable page, QueryAndParameters queryAndParameters) {
+    List<DbCriteria> dbCriteriaList =
+        namedParameterJdbcTemplate.query(
+            queryAndParameters.getQuery()
+                + String.format(LIMIT_OFFSET, page.getPageSize(), page.getOffset()),
+            queryAndParameters.getParameters(),
+            new DBCriteriaRowMapper());
+    return dbCriteriaList;
+  }
+
+  @Nullable
+  private Integer count(QueryAndParameters queryAndParameters) {
+    return namedParameterJdbcTemplate.queryForObject(
+        queryAndParameters.getQuery().replace("*", "count(*)"),
+        queryAndParameters.getParameters(),
+        Integer.class);
+  }
+
+  private class DBCriteriaRowMapper implements RowMapper<DbCriteria> {
+    @Override
+    public DbCriteria mapRow(ResultSet rs, int rowNum) throws SQLException {
+      return (new BeanPropertyRowMapper<>(DbCriteria.class)).mapRow(rs, rowNum);
+    }
+  }
+}
