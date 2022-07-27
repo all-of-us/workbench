5699e84615c58b7f69dd9db79b427b333924f30b
Neha Saxena <nsaxena@broadinstitute.org>
2022-07-26 21:04:24 -0400

    Add question number to error tooltip

--- ui/src/app/utils/constants.tsx
+++ ui/src/app/utils/constants.tsx
@@ -4,3 +4,21 @@ export const DEMOGRAPHIC_SURVEY_V2_PATH = '/demographic-survey';
 
 export const DEMOGRAPHIC_SURVEY_SESSION_KEY =
   'demographic-survey-banner-dismissed';
+
+export const ETHNIC_CATEGORIES = 'ethnicCategories';
+
+export const GENDER_IDENTITIES = 'genderIdentities';
+
+export const SEX_AT_BIRTH = 'sexAtBirth';
+
+export const SEXUAL_ORIENTATIONS = 'sexualOrientations';
+
+export const DISABILITY_HEARING = 'disabilityHearing';
+export const DISABILITY_SEEING = 'disabilitySeeing';
+export const DISABILITY_CONCENTRATING = 'disabilityConcentrating';
+export const DISABILITY_WALKING = 'disabilityWalking';
+export const DISABILITY_DRESSING = 'disabilityDressing';
+export const DISABILITY_ERRANDS = 'disabilityErrands';
+export const YEAR_OF_BIRTH = 'yearOfBirth';
+export const EDUCATION = 'education';
+export const DISADVANTAGED = 'disadvantaged';
