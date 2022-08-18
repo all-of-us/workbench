7e847a608639a1b0b1bf14135fb0260e7e24d551
David Mohs <dmohs@broadinstitute.org>
2022-08-18 13:00:23 -0400

    Rename handlers to path-first

--- api-proxy/handlers/*.options.mjs
+++ api-proxy/handlers/*.options.mjs
@@ -1,22 +1,19 @@
-const matchReq = req =>
-  req.url.pathname === '/v1/workspaces/aou-rw-test-ca61ee0f/testwsshare202207311932/genomicExtractionJobs'
-  && req.url.search === ''
-  && req.method === 'OPTIONS'
-
-const body = ``
+const matchReq = req => req.method === 'OPTIONS'
 
 const headers = {
   'access-control-allow-credentials': 'true',
   'access-control-allow-origin': '*',
   'access-control-allow-methods': 'GET, HEAD, POST, PUT, DELETE, PATCH, TRACE, OPTIONS',
   'access-control-allow-headers': 'Origin, X-Requested-With, Content-Type, Accept, Authorization',
+  vary: 'Origin, Access-Control-Request-Method, Access-Control-Request-Headers',
   allow: 'GET, HEAD, POST, PUT, DELETE, TRACE, OPTIONS, PATCH',
-  'content-type': 'text/html'
+  'content-type': 'text/html',
+  server: 'Google Frontend'
 }
 
 const handleReq = (req, res) => {
   if (!matchReq(req)) { return }
   Object.keys(headers).forEach(h => res.setHeader(h, headers[h]))
-  res.status(200).mwrite(body).mend()
+  res.status(200).mend()
 }
 export default handleReq
