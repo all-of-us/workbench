4dc96fa506ee633a1d21c8a03a11008a507b4985
Joel Thibault <thibault@broad.dev>
2022-09-27 15:28:13 -0400

    fix CohortReviewControllerBQTest

--- api/src/main/java/org/pmiops/workbench/db/dao/WorkspaceDaoCustom.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/WorkspaceDaoCustom.java
@@ -4,6 +4,5 @@ import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbWorkspace;
 
 public interface WorkspaceDaoCustom {
-
   DbWorkspace saveWithLastModified(DbWorkspace workspace, DbUser user);
 }
