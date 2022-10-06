eb64f6a93c13f5fa3e29f7f30f05fe610558e4cd
Neha Saxena <nsaxena@broadinstitute.org>
2022-10-06 11:50:03 -0400

    Merge resource-list

--- ui/src/app/utils/resource-list.tsx
+++ ui/src/app/utils/resource-list.tsx
@@ -1,20 +1,37 @@
 import * as React from 'react';
-import { useEffect, useState } from 'react';
+import { CSSProperties, useEffect, useState } from 'react';
+import { Link as RouterLink } from 'react-router-dom';
 import * as fp from 'lodash/fp';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
+import { faLockAlt } from '@fortawesome/pro-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
-import { ResourceType, WorkspaceResource } from 'generated/fetch';
+import {
+  CdrVersionTiersResponse,
+  ResourceType,
+  Workspace,
+  WorkspaceResource,
+} from 'generated/fetch';
 
+import { Clickable } from 'app/components/buttons';
+import { TooltipTrigger } from 'app/components/popups';
 import { renderResourceCard } from 'app/components/render-resource-card';
 import {
   ResourceNavigation,
   StyledResourceType,
 } from 'app/components/resource-card';
+import colors from 'app/styles/colors';
 import { reactStyles, withCdrVersions } from 'app/utils';
 import { displayDate, displayDateWithoutHours } from 'app/utils/dates';
-import { getDisplayName, getType } from 'app/utils/resources';
+import {
+  getDisplayName,
+  getType,
+  getTypeString,
+  isNotebook,
+} from 'app/utils/resources';
 
+import { getCdrVersion } from './cdr-versions';
 import { ROWS_PER_PAGE_RESOURCE_TABLE } from './constants';
 
 const styles = reactStyles({
@@ -40,19 +57,50 @@ const styles = reactStyles({
   },
 });
 
+interface NavProps {
+  workspace: Workspace;
+  resource: WorkspaceResource;
+  style?: CSSProperties;
+}
+
+const WorkspaceNavigation = (props: NavProps) => {
+  const {
+    workspace: { name, namespace, id },
+    resource,
+    style,
+  } = props;
+  const tab = isNotebook(resource) ? 'notebooks' : 'data';
+  const url = `/workspaces/${namespace}/${id}/${tab}`;
+
+  return (
+    <Clickable>
+      <RouterLink to={url} style={style} data-test-id='workspace-navigation'>
+        {name}
+      </RouterLink>
+    </Clickable>
+  );
+};
+
 interface TableData {
   menu: JSX.Element;
   resourceType: JSX.Element;
+  resourceTypeAsString: string;
   resourceName: JSX.Element;
   resourceNameAsString: string;
   formattedLastModified: string;
   lastModifiedDateAsString: string;
+  lastModifiedBy: string;
+  workspaceName: JSX.Element;
+  cdrVersionName: string;
 }
 
 interface Props {
   existingNameList: string[];
   workspaceResources: WorkspaceResource[];
   onUpdate: Function;
+  workspaceMap?: Map<string, Workspace>;
+  cdrVersionTiersResponse: CdrVersionTiersResponse;
+  recentResourceSource?: boolean;
 }
 
 export const ResourcesList = fp.flow(withCdrVersions())((props: Props) => {
@@ -86,6 +134,32 @@ export const ResourcesList = fp.flow(withCdrVersions())((props: Props) => {
     });
   };
 
+  const getWorkspace = (r: WorkspaceResource) => {
+    const { workspaceMap } = props;
+    return workspaceMap?.get(r.workspaceFirecloudName);
+  };
+
+  const getCdrVersionName = (r: WorkspaceResource) => {
+    const { cdrVersionTiersResponse, recentResourceSource } = props;
+    if (!recentResourceSource) {
+      return '';
+    }
+    const cdrVersion = getCdrVersion(getWorkspace(r), cdrVersionTiersResponse);
+    return cdrVersion?.name;
+  };
+
+  const addAdminLockToNameColumn = () => {
+    return (
+      <TooltipTrigger content={<div>Workspace compliance action required</div>}>
+        <FontAwesomeIcon
+          style={{ color: colors.warning, marginRight: '0.5rem' }}
+          size={'sm'}
+          icon={faLockAlt}
+        />
+      </TooltipTrigger>
+    );
+  };
+
   useEffect(() => {
     const { workspaceResources } = props;
     if (workspaceResources) {
@@ -98,8 +172,10 @@ export const ResourcesList = fp.flow(withCdrVersions())((props: Props) => {
                 <StyledResourceType resource={r} />
               </ResourceNavigation>
             ),
+            resourceTypeAsString: getTypeString(r),
             resourceName: (
               <ResourceNavigation resource={r} style={styles.navigation}>
+                {r.adminLocked && addAdminLockToNameColumn()}
                 {getDisplayName(r)}
               </ResourceNavigation>
             ),
@@ -108,6 +184,15 @@ export const ResourcesList = fp.flow(withCdrVersions())((props: Props) => {
               r.lastModifiedEpochMillis
             ),
             lastModifiedDateAsString: displayDate(r.lastModifiedEpochMillis),
+            cdrVersionName: getCdrVersionName(r),
+            workspaceName: (
+              <WorkspaceNavigation
+                workspace={getWorkspace(r)}
+                resource={r}
+                style={styles.navigation}
+              />
+            ),
+            lastModifiedBy: r.lastModifiedBy,
           };
         })
       );
@@ -127,13 +212,14 @@ export const ResourcesList = fp.flow(withCdrVersions())((props: Props) => {
             value={tableData}
             scrollable={true}
             sortMode='multiple'
-            style={{ width: '44rem' }}
+            // style={{ width: '44rem' }}
             paginator
             rows={ROWS_PER_PAGE_RESOURCE_TABLE}
           >
             <Column field='menu' style={styles.menu} />
             <Column
               field='resourceType'
+              sortField='resourceTypeAsString'
               header='Item type'
               sortable
               style={styles.typeColumn}
@@ -149,12 +235,31 @@ export const ResourcesList = fp.flow(withCdrVersions())((props: Props) => {
               filterMatchMode='custom'
               filterFunction={filterNameColumn}
             />
+            {props.recentResourceSource && (
+              <Column
+                field='workspaceName'
+                header='Workspace name'
+                style={{ ...styles.column }}
+              />
+            )}
             <Column
               field='formattedLastModified'
               header='Last Modified Date'
               style={styles.modifiedDateColumn}
               sortField={'lastModifiedDateAsString'}
               sortable
+            />
+            {props.recentResourceSource && (
+              <Column
+                field='cdrVersionName'
+                header='Dataset'
+                style={{ ...styles.column }}
+              />
+            )}
+            <Column
+              field='lastModifiedBy'
+              header='Last Modified By'
+              style={{ ...styles.column }}
             />
           </DataTable>
         )}
