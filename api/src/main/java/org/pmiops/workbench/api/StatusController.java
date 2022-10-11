f8a3b14356d1743ea4dc9a96a103d405e9a71326
yyhao1@gmail.com <yonghao@google.com>
2022-10-11 13:49:49 -0400

    rename StatusController

--- api/src/main/java/org/pmiops/workbench/api/StatusController.java
+++ api/src/main/java/org/pmiops/workbench/api/StatusController.java
@@ -11,19 +11,19 @@ import org.springframework.web.bind.annotation.RestController;
 public class StatusController implements StatusApiDelegate {
 
   private final FireCloudService fireCloudService;
-  private final LeonardoApiClient leonardoNotebooksClient;
+  private final LeonardoApiClient leonardoApiClient;
 
   @Autowired
-  StatusController(FireCloudService fireCloudService, LeonardoApiClient leonardoNotebooksClient) {
+  StatusController(FireCloudService fireCloudService, LeonardoApiClient leonardoApiClient) {
     this.fireCloudService = fireCloudService;
-    this.leonardoNotebooksClient = leonardoNotebooksClient;
+    this.leonardoApiClient = leonardoApiClient;
   }
 
   @Override
   public ResponseEntity<StatusResponse> getStatus() {
     StatusResponse statusResponse = new StatusResponse();
     statusResponse.setFirecloudStatus(fireCloudService.getFirecloudStatus());
-    statusResponse.setNotebooksStatus(leonardoNotebooksClient.getLeonardoStatus());
+    statusResponse.setNotebooksStatus(leonardoApiClient.getLeonardoStatus());
     return ResponseEntity.ok(statusResponse);
   }
 }
