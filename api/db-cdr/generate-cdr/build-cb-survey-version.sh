67bc18502e316330deb0b47ab00c684669273d51
Brian Freeman <brian.d.freeman@gmail.com>
2022-10-20 16:42:24 -0500

    [no ticket][risk=no] fixing Cope Minute Survey (#7083)
    * fixing surveys versions
    
    * fixing survey name

--- api/db-cdr/generate-cdr/build-cb-survey-version.sh
+++ api/db-cdr/generate-cdr/build-cb-survey-version.sh
@@ -31,8 +31,8 @@ bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 SELECT survey_version_concept_id, survey_concept_id, display_name, ROW_NUMBER() OVER (ORDER BY concept_code) + (SELECT MAX(display_order) FROM \`$BQ_PROJECT.$BQ_DATASET.cb_survey_version\`) AS display_order
 FROM (
   SELECT distinct o.survey_version_concept_id,
-  (SELECT concept_id FROM \`$BQ_PROJECT.$BQ_DATASET.concept\` WHERE concept_class_id = 'Module' AND concept_code = 'cope_vaccine3') as survey_concept_id,
-  CASE WHEN c.concept_name = 'COVID-19 Vaccine Survey' THEN 'Winter 2021 Minute Survey' ELSE REPLACE (c.concept_name, ' on COVID-19 Vaccines', '') END as display_name,
+  (SELECT concept_id FROM \`$BQ_PROJECT.$BQ_DATASET.concept\` WHERE concept_class_id = 'Module' AND concept_code = 'cope_vaccine4') as survey_concept_id,
+  REPLACE (c.concept_name, ' on COVID-19 Vaccines', '') as display_name,
   concept_code
 FROM \`$BQ_PROJECT.$BQ_DATASET.observation_ext\` o
 JOIN \`$BQ_PROJECT.$BQ_DATASET.concept\` c on c.concept_id = o.survey_version_concept_id
