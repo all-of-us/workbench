f3e368440d2b63a997e28af7a0f3743bba9c6b8c
yyhao1@gmail.com <yonghao@google.com>
2022-10-20 19:07:25 -0400

    fixing test

--- api/src/test/java/org/pmiops/workbench/api/AppsControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/AppsControllerTest.java
@@ -34,9 +34,12 @@ import org.springframework.context.annotation.Import;
 import org.springframework.context.annotation.Scope;
 import org.springframework.test.annotation.DirtiesContext;
 import org.springframework.test.annotation.DirtiesContext.ClassMode;
+import org.springframework.transaction.annotation.Propagation;
+import org.springframework.transaction.annotation.Transactional;
 
 @DataJpaTest
 @DirtiesContext(classMode = ClassMode.BEFORE_EACH_TEST_METHOD)
+@Transactional(propagation = Propagation.NOT_SUPPORTED)
 public class AppsControllerTest {
   @TestConfiguration
   @Import({
