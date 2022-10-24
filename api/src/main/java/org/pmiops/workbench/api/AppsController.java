bec108ea44e491646fafe9343c2ec52b07865a7c
Peter Lavigne <peterlavigne@google.com>
2022-10-24 12:10:25 -0400

    Update validateCanPerformApiAction doc
    We only check billing when creating an app

--- api/src/main/java/org/pmiops/workbench/api/AppsController.java
+++ api/src/main/java/org/pmiops/workbench/api/AppsController.java
@@ -94,7 +94,6 @@ public class AppsController implements AppsApiDelegate {
    *   <li>Feature is enabled
    *   <li>User compute is not suspend due to security reason (e.g. egress alert)
    *   <li>User is OWNER or WRITER of the workspace
-   *   <li>Workspace has valid billing.
    * </ul>
    */
   @VisibleForTesting
