453065543d610d1d0cff04e8f1bcfaf02cbd1063
David Mohs <dgit@mohsinc.com>
2022-10-27 21:32:03 -0400

    Remove accidental include

--- ui/shell.nix
+++
@@ -1,7 +0,0 @@
-{ pkgs ? import <nixpkgs> {} }:
-  pkgs.mkShell {
-    # nativeBuildInputs is usually what you want -- tools you need to run
-    nativeBuildInputs = with pkgs.buildPackages; [
-      docker-sync git-secrets jdk8_headless python ruby yarn
-    ];
-}
