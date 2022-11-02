d46d75edacb3ab40a590abbb941c8d0a75ca26e4
Brian Freeman <brian.d.freeman@gmail.com>
2022-11-02 10:57:19 -0500

    DT-117 adding ds_ tables for fitbit sleep (#7119)
    [DT-117][risk=no] adding ds_ tables for fitbit sleep

--- api/db-cdr/generate-cdr/build-ds-tables.sh
+++ api/db-cdr/generate-cdr/build-ds-tables.sh
@@ -347,3 +347,21 @@ if [[ "$TABLE_LIST" == *"zip3_ses_map"* ]]; then
   where observation_source_concept_id = 1585250
   and o.value_as_string not like 'Res%'"
 fi
+
+echo "ds_sleep_daily_summary - inserting data"
+bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+"INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.ds_sleep_daily_summary\`
+    (person_id, sleep_date, is_main_sleep, minute_in_bed, minute_asleep, minute_after_wakeup, minute_awake, minute_restless, minute_deep, minute_light, minute_rem, minute_wake)
+SELECT person_id, sleep_date, is_main_sleep, minute_in_bed, minute_asleep, minute_after_wakeup, minute_awake, minute_restless, minute_deep, minute_light, minute_rem, minute_wake
+FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY person_id ) AS rank
+FROM \`$BQ_PROJECT.$BQ_DATASET.sleep_daily_summary\`)
+where rank = 1"
+
+echo "ds_sleep_level - inserting data"
+bq --quiet --project_id=$BQ_PROJECT query --nouse_legacy_sql \
+"INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.ds_sleep_level\`
+    (person_id, sleep_date, is_main_sleep, level, start_datetime, duration_in_min)
+SELECT person_id, sleep_date, ins_main_sleep, level, start_datetime, duration_in_min
+FROM (SELECT *, ROW_NUMBER() OVER (PARTITION BY person_id ) AS rank
+FROM \`$BQ_PROJECT.$BQ_DATASET.sleep_level\`)
+where rank = 1"
