33737d08ffe3f5b1ab9426883cee4dbd8c818cd1
David Mohs <dgit@mohsinc.com>
2022-11-03 15:01:44 -0400

    Disable survey prompt

--- .circleci/activate-service-account.sh
+++ .circleci/activate-service-account.sh
@@ -1,4 +1,5 @@
 source "$(dirname $0)"/SA_KEY_JSON.sh
 source "$(dirname $0)"/SA_EMAIL.sh
+gcloud config set survey/disable_prompts True
 echo "$SA_KEY_JSON" | gcloud auth activate-service-account "$SA_EMAIL" --key-file=-
 
