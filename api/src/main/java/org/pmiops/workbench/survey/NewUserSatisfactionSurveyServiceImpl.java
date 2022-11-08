a0d3a59a2c13ea1a980619fc991c344f6ac26b68
Peter Lavigne <peterlavigne@google.com>
2022-11-08 11:44:59 -0500

    Use Optional 'map' instead of 'get'

--- api/src/main/java/org/pmiops/workbench/survey/NewUserSatisfactionSurveyServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/survey/NewUserSatisfactionSurveyServiceImpl.java
@@ -3,12 +3,10 @@ package org.pmiops.workbench.survey;
 import java.time.Clock;
 import java.time.Instant;
 import java.time.temporal.ChronoUnit;
-import java.util.Optional;
 import org.pmiops.workbench.access.AccessTierService;
 import org.pmiops.workbench.db.dao.UserAccessTierDao;
 import org.pmiops.workbench.db.model.DbAccessTier;
 import org.pmiops.workbench.db.model.DbUser;
-import org.pmiops.workbench.db.model.DbUserAccessTier;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -35,21 +33,21 @@ public class NewUserSatisfactionSurveyServiceImpl implements NewUserSatisfaction
 
     DbAccessTier registeredAccessTier = accessTierService.getRegisteredTierOrThrow();
 
-    Optional<DbUserAccessTier> userRegisteredAccessTier =
-        userAccessTierDao.getByUserAndAccessTier(user, registeredAccessTier);
+    return userAccessTierDao
+        .getByUserAndAccessTier(user, registeredAccessTier)
+        .map(
+            userRegisteredAccessTier -> {
+              Instant enabledTime = userRegisteredAccessTier.getFirstEnabled().toInstant();
 
-    if (!userRegisteredAccessTier.isPresent()) {
-      return false;
-    }
+              final Instant twoWeeksAgo = clock.instant().minus(2 * 7, ChronoUnit.DAYS);
+              final boolean enabledMoreThanTwoWeeksAgo = enabledTime.isBefore(twoWeeksAgo);
 
-    Instant enabledTime = userRegisteredAccessTier.get().getFirstEnabled().toInstant();
+              final Instant twoMonthsTwoWeeksAgo = twoWeeksAgo.minus(61, ChronoUnit.DAYS);
+              final boolean eligibleForLessThanTwoMonths =
+                  enabledTime.isAfter(twoMonthsTwoWeeksAgo);
 
-    final Instant twoWeeksAgo = clock.instant().minus(2 * 7, ChronoUnit.DAYS);
-    final boolean enabledMoreThanTwoWeeksAgo = enabledTime.isBefore(twoWeeksAgo);
-
-    final Instant twoMonthsTwoWeeksAgo = twoWeeksAgo.minus(61, ChronoUnit.DAYS);
-    final boolean eligibleForLessThanTwoMonths = enabledTime.isAfter(twoMonthsTwoWeeksAgo);
-
-    return enabledMoreThanTwoWeeksAgo && eligibleForLessThanTwoMonths;
+              return enabledMoreThanTwoWeeksAgo && eligibleForLessThanTwoMonths;
+            })
+        .orElse(false);
   }
 }
