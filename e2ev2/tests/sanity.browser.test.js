bc00cfa3e44a79f9b17ff9dc157a2c035e0005b4
Eric Rollins <ericvanrollinsii@gmail.com>
2022-11-22 10:46:14 -0500

    Added a pulse check to useAPIProxy to ensure that the api is functional before proceeding further with tests.

--- e2ev2/tests/sanity.browser.test.js
+++ e2ev2/tests/sanity.browser.test.js
@@ -13,19 +13,24 @@ browserTest('page loads', async browser => {
 browserTest('view cookie policy page', async browser => {
   const page = browser.initialPage
   await tu.useApiProxy(page)
-  await page.goto(config.urlRoot()+'/login', {waitUntil: 'networkidle0'})
-  const cpLink = await page.waitForSelector('a[href="/cookie-policy"]')
-  expect(cpLink).toBeDefined()
-  // click and wait need to happen simultaneously to avoid a race
-  const [, target] = await Promise.all([
-    cpLink.click(),
-    browser.waitForTarget(target => target.opener() === page.target(), {timeout: 2e3})
-  ])
-  const cpPage = await target.page()
-  cpPage.setDefaultTimeout(2000)
-  const h3 = await cpPage.waitForSelector('h3')
-  expect(await h3.evaluate(n => n.innerText))
-    .toBe('All of Us Research Program Cookie Policy')
+    .then(async () => await page.goto(config.urlRoot()+'/login', {waitUntil: 'networkidle0'}))
+    .then(async () => {
+      const cpLink = await page.waitForSelector('a[href="/cookie-policy"]')
+      expect(cpLink).toBeDefined()
+      // click and wait need to happen simultaneously to avoid a race
+      const [, target] = await Promise.all([
+        cpLink.click(),
+        browser.waitForTarget(target => target.opener() === page.target(), {timeout: 2e3})
+      ])
+      const cpPage = await target.page()
+      cpPage.setDefaultTimeout(2000)
+      const h3 = await cpPage.waitForSelector('h3')
+      expect(await h3.evaluate(n => n.innerText))
+        .toBe('All of Us Research Program Cookie Policy')})
+    .catch((err) => {
+      throw err
+    })
+
 }, 10e3) // page loads take some time
 
 for (const p of ['/workspaces', '/profile']) {
