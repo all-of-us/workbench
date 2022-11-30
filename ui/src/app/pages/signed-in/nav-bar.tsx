9428a36be2c364eca00d8d0411eed531b45cb073
Eric Rollins <ericvanrollinsii@gmail.com>
2022-11-30 16:36:21 -0500

    Change listeners to be attached to root node rather than document.

--- ui/src/app/pages/signed-in/nav-bar.tsx
+++ ui/src/app/pages/signed-in/nav-bar.tsx
@@ -77,10 +77,12 @@ export const NavBar = () => {
   };
 
   useEffect(() => {
-    document.addEventListener('click', onClickOutside);
+    document.getElementById('root').addEventListener('click', onClickOutside);
 
     return () => {
-      document.removeEventListener('click', onClickOutside);
+      document
+        .getElementById('root')
+        .removeEventListener('click', onClickOutside);
     };
   });
 
