58fcfe1dde677bccd3475ee591b2efdf0d749aac
David Mohs <dmohs@broadinstitute.org>
2022-12-01 11:59:46 -0500

    Make build script idempotent

--- ui/src/terraui/build.sh
+++ ui/src/terraui/build.sh
@@ -4,17 +4,25 @@
 set -euo pipefail
 set -v
 
-# The date specified here must be early-enough to contain the specified hash.
-git clone --shallow-since='2022-11-29T00:00' \
-  https://github.com/DataBiosphere/terra-ui.git \
-  .repo
+WD="$(dirname $0)"
+cd "$WD"
 
-(cd .repo; git checkout 6731645eeae3bbdbae08546f5d97ad6b3741fe86)
+# Making this a hidden directory avoids having to exclude it from the parent build.
+REPODIR=.repo
+
+set +e # ignore if repo already exists
+# The date specified here must be early-enough to contain the specified hash.
+git clone -n --shallow-since='2022-11-29T00:00' \
+  https://github.com/DataBiosphere/terra-ui.git \
+  $REPODIR
+set -e
+
+(cd $REPODIR; git checkout --detach 6731645eeae3bbdbae08546f5d97ad6b3741fe86)
 
 yarn install
 
 (
-  cd .repo;
+  cd $REPODIR;
   ../node_modules/.bin/esbuild --bundle --format=cjs --tsconfig=<(echo '{}') \
     --outdir=../out \
     --external:react --external:lodash/fp \
