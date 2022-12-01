58e8deef7d5e55b5e330ce22ee50e90b03d6e419
Eric Rollins <erollins@broadinstitute.org>
2022-12-01 10:56:28 -0500

    [risk=no][RW-8975] Added tests to the Select an Application modal. (#7204)

--- ui/src/app/pages/appAnalysis/apps-list.tsx
+++ ui/src/app/pages/appAnalysis/apps-list.tsx
@@ -94,6 +94,7 @@ export const AppsList = withCurrentWorkspace()((props) => {
               Your Analysis
             </ListPageHeader>
             <Button
+              aria-label='start'
               data-test-id='start-button'
               style={styles.startButton}
               onClick={() => {
@@ -105,21 +106,27 @@ export const AppsList = withCurrentWorkspace()((props) => {
               }
             >
               <div style={{ paddingRight: '0.5rem' }}>Start</div>
-              <FontAwesomeIcon icon={faPlusCircle}></FontAwesomeIcon>
+              <FontAwesomeIcon icon={faPlusCircle} />
             </Button>
           </FlexRow>
         </FlexColumn>
       </FadeBox>
       {showSelectAppModal && (
-        <Modal data-test-id='select-application-modal'>
+        <Modal
+          data-test-id='select-application-modal'
+          aria={{
+            label: 'Select Applications Modal',
+          }}
+        >
           <ModalTitle>Analyze Data</ModalTitle>
           <ModalBody>
             <div style={styles.appsLabel}>Select an application</div>
             <Dropdown
-              data-test-id={'application-list-dropdown'}
+              data-test-id='application-list-dropdown'
+              ariaLabel='Application List Dropdown'
               value={selectedApp}
               options={APP_LIST}
-              placeholder={'Choose One'}
+              placeholder='Choose One'
               onChange={(e) => setSelectedApp(e.value)}
               style={{ width: '9rem' }}
             />
@@ -127,16 +134,16 @@ export const AppsList = withCurrentWorkspace()((props) => {
           <ModalFooter style={{ paddingTop: '2rem' }}>
             <Button
               style={{ marginRight: '2rem' }}
-              type={'secondary'}
-              label={'Close'}
+              type='secondary'
+              aria-label='close'
               onClick={() => onClose()}
             >
               Close
             </Button>
             <Button
-              data-test-id={'next-btn'}
-              type={'primary'}
-              label={'Next'}
+              data-test-id='next-btn'
+              type='primary'
+              aria-label='next'
               onClick={() => onNext()}
               disabled={selectedApp === ''}
             >
@@ -147,7 +154,7 @@ export const AppsList = withCurrentWorkspace()((props) => {
       )}
       {showJupyterModal && !showSelectAppModal && (
         <NewNotebookModal
-          data-test-id={'jupyter-modal'}
+          data-test-id='jupyter-modal'
           onClose={() => onClose()}
           workspace={props.workspace}
           existingNameList={null}
