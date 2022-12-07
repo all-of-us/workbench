f3874f6f44c1e6ead23caf2ec939f3e17db1379b
Joel Thibault <thibault@broad.dev>
2022-12-07 17:14:19 -0500

    WIP compiles

--- api/src/main/java/org/pmiops/workbench/api/OfflineTestUsersController.java
+++ api/src/main/java/org/pmiops/workbench/api/OfflineTestUsersController.java
@@ -2,10 +2,13 @@ package org.pmiops.workbench.api;
 
 import static org.pmiops.workbench.firecloud.IntegrationTestUsers.COMPLIANT_USER;
 
+import java.util.List;
 import java.util.logging.Logger;
 import javax.inject.Provider;
 import org.pmiops.workbench.config.WorkbenchConfig;
 import org.pmiops.workbench.impersonation.ImpersonatedUserService;
+import org.pmiops.workbench.impersonation.ImpersonatedWorkspaceService;
+import org.pmiops.workbench.model.WorkspaceResponse;
 import org.pmiops.workbench.utils.UserUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.ResponseEntity;
@@ -17,13 +20,16 @@ public class OfflineTestUsersController implements OfflineTestUsersApiDelegate {
 
   private final Provider<WorkbenchConfig> workbenchConfigProvider;
   private final ImpersonatedUserService impersonatedUserService;
+  private final ImpersonatedWorkspaceService impersonatedWorkspaceService;
 
   @Autowired
   public OfflineTestUsersController(
       Provider<WorkbenchConfig> workbenchConfigProvider,
-      ImpersonatedUserService impersonatedUserService) {
+      ImpersonatedUserService impersonatedUserService,
+      ImpersonatedWorkspaceService impersonatedWorkspaceService) {
     this.workbenchConfigProvider = workbenchConfigProvider;
     this.impersonatedUserService = impersonatedUserService;
+    this.impersonatedWorkspaceService = impersonatedWorkspaceService;
   }
 
   public ResponseEntity<Void> ensureTestUserTosCompliance() {
@@ -56,5 +62,29 @@ public class OfflineTestUsersController implements OfflineTestUsersApiDelegate {
       LOGGER.info(String.format("Accepting the Terra Terms of Service for test user %s", username));
       impersonatedUserService.acceptTerraTermsOfService(username);
     }
+  }
+
+  public ResponseEntity<Void> deleteAllTestUserWorkspaces() {
+    WorkbenchConfig config = workbenchConfigProvider.get();
+    WorkbenchConfig.E2ETestUserConfig testUserConf = config.e2eTestUsers;
+
+    // only some environments have test users
+    if (testUserConf == null) {
+      LOGGER.info("This environment does not have a test user config block.  Exiting.");
+    } else {
+      testUserConf.testUserEmails.forEach(this::deleteAllOwnedWorkspaces);
+    }
+
+    return ResponseEntity.ok().build();
+  }
+
+  private void deleteAllOwnedWorkspaces(String username) {
+    List<WorkspaceResponse> workspaces = impersonatedWorkspaceService.getOwnedWorkspaces(username);
+    LOGGER.info(
+        String.format("Test user %s currently owns %d workspaces", username, workspaces.size()));
+
+    workspaces.forEach(
+        workspace ->
+            impersonatedWorkspaceService.deleteWorkspace(username, workspace.getWorkspace()));
   }
 }
---
+++ api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedWorkspaceServiceImpl.java
@@ -0,0 +1,113 @@
+package org.pmiops.workbench.impersonation;
+
+import java.io.IOException;
+import java.util.List;
+import java.util.logging.Logger;
+import java.util.stream.Collectors;
+import javax.inject.Provider;
+import org.pmiops.workbench.actionaudit.auditors.BillingProjectAuditor;
+import org.pmiops.workbench.actionaudit.auditors.WorkspaceAuditor;
+import org.pmiops.workbench.db.dao.UserDao;
+import org.pmiops.workbench.db.dao.WorkspaceDao;
+import org.pmiops.workbench.db.model.DbUser;
+import org.pmiops.workbench.db.model.DbWorkspace;
+import org.pmiops.workbench.exceptions.ServerErrorException;
+import org.pmiops.workbench.firecloud.FireCloudService;
+import org.pmiops.workbench.model.Workspace;
+import org.pmiops.workbench.model.WorkspaceAccessLevel;
+import org.pmiops.workbench.model.WorkspaceActiveStatus;
+import org.pmiops.workbench.model.WorkspaceResponse;
+import org.pmiops.workbench.utils.mappers.WorkspaceMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+/**
+ * An impersonation-enabled version of {@link org.pmiops.workbench.workspaces.WorkspaceServiceImpl}
+ *
+ * <p>REMINDER: With great power comes great responsibility. Impersonation should not be used in
+ * production, except where absolutely necessary.
+ */
+@Service
+public class ImpersonatedWorkspaceServiceImpl implements ImpersonatedWorkspaceService {
+  private static final Logger LOGGER =
+      Logger.getLogger(ImpersonatedWorkspaceServiceImpl.class.getName());
+
+  private final BillingProjectAuditor billingProjectAuditor;
+  private final FireCloudService firecloudService;
+  private final ImpersonatedFirecloudService impersonatedFirecloudService;
+  private final Provider<DbUser> userProvider;
+  private final UserDao userDao;
+  private final WorkspaceAuditor workspaceAuditor;
+  private final WorkspaceDao workspaceDao;
+  private final WorkspaceMapper workspaceMapper;
+
+  @Autowired
+  public ImpersonatedWorkspaceServiceImpl(
+      BillingProjectAuditor billingProjectAuditor,
+      FireCloudService firecloudService,
+      ImpersonatedFirecloudService impersonatedFirecloudService,
+      Provider<DbUser> userProvider,
+      UserDao userDao,
+      WorkspaceAuditor workspaceAuditor,
+      WorkspaceDao workspaceDao,
+      WorkspaceMapper workspaceMapper) {
+    this.billingProjectAuditor = billingProjectAuditor;
+    this.firecloudService = firecloudService;
+    this.impersonatedFirecloudService = impersonatedFirecloudService;
+    this.userProvider = userProvider;
+    this.userDao = userDao;
+    this.workspaceAuditor = workspaceAuditor;
+    this.workspaceDao = workspaceDao;
+    this.workspaceMapper = workspaceMapper;
+  }
+
+  @Override
+  public List<WorkspaceResponse> getOwnedWorkspaces(String username) {
+    final DbUser dbUser = userDao.findUserByUsername(username);
+    try {
+      return workspaceMapper
+          .toApiWorkspaceResponses(workspaceDao, impersonatedFirecloudService.getWorkspaces(dbUser))
+          .stream()
+          .filter(response -> response.getAccessLevel() == WorkspaceAccessLevel.OWNER)
+          .collect(Collectors.toList());
+    } catch (IOException e) {
+      throw new ServerErrorException(e);
+    }
+  }
+
+  @Override
+  public void deleteWorkspace(String username, Workspace workspace) {
+    final DbUser dbUser = userDao.findUserByUsername(username);
+    DbWorkspace dbWorkspace = workspaceDao.getRequired(workspace.getNamespace(), workspace.getId());
+
+    // This deletes all Firecloud and google resources, however saves all references
+    // to the workspace and its resources in the Workbench database.
+    // This is for auditing purposes and potentially workspace restore.
+    // TODO: do we want to delete workspace resource references and save only metadata?
+
+    // This automatically handles access control to the workspace.
+    try {
+      impersonatedFirecloudService.deleteWorkspace(
+          dbUser, workspace.getNamespace(), workspace.getId());
+    } catch (IOException e) {
+      throw new ServerErrorException(e);
+    }
+
+    dbWorkspace.setWorkspaceActiveStatusEnum(WorkspaceActiveStatus.DELETED);
+    dbWorkspace = workspaceDao.saveWithLastModified(dbWorkspace, userProvider.get());
+    workspaceAuditor.fireDeleteAction(dbWorkspace);
+
+    String billingProjectName = workspace.getNamespace();
+    try {
+      // use the real FirecloudService here because impersonation is not needed;
+      // billing projects are owned by the App SA
+      firecloudService.deleteBillingProject(billingProjectName);
+      billingProjectAuditor.fireDeleteAction(billingProjectName);
+    } catch (Exception e) {
+      String msg =
+          String.format(
+              "Error deleting billing project %s: %s", billingProjectName, e.getMessage());
+      LOGGER.warning(msg);
+    }
+  }
+}
