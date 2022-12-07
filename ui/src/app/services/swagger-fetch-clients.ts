98fda33c0302742882b04745bcb5a047e7698389
Neha Saxena <nsaxena@broadinstitute.org>
2022-12-06 21:26:24 -0500

    Migrate to use new DisksApi

--- ui/src/app/services/swagger-fetch-clients.ts
+++ ui/src/app/services/swagger-fetch-clients.ts
@@ -33,6 +33,7 @@ import {
   Configuration as FetchConfiguration,
   DataSetApi,
   DiskApi,
+  DisksApi,
   EgressEventsAdminApi,
   FeaturedWorkspacesConfigApi, // internal
   InstitutionApi,
@@ -124,6 +125,7 @@ export const userMetricsApi = bindCtor(UserMetricsApi);
 export const workspaceAdminApi = bindCtor(WorkspaceAdminApi);
 export const workspacesApi = bindCtor(WorkspacesApi);
 export const diskApi = bindCtor(DiskApi);
+export const disksApi = bindCtor(DisksApi);
 export const surveysApi = bindCtor(SurveysApi);
 
 export const getApiBaseUrl = () => {
---
+++ ui/src/testing/stubs/disks-api-stub.ts
@@ -0,0 +1,79 @@
+import {
+  Disk,
+  DisksApi,
+  DiskType,
+  EmptyResponse,
+  ListDisksResponse,
+} from 'generated/fetch';
+
+import { stubNotImplementedError } from 'testing/stubs/stub-utils';
+
+export const stubDisk = () => ({
+  size: 1000,
+  diskType: DiskType.Standard,
+  name: 'stub-disk',
+  blockSize: 1,
+});
+
+export class DisksApiStub extends DisksApi {
+  constructor(public disk?: Disk) {
+    super(undefined, undefined, (..._: any[]) => {
+      throw stubNotImplementedError;
+    });
+  }
+
+  deleteDisk(
+    _workspaceNamespace: string,
+    _diskName: string,
+    _options?: any
+  ): Promise<EmptyResponse> {
+    return new Promise<EmptyResponse>((resolve, reject) => {
+      if (!this.disk) {
+        reject(Error('disk not found'));
+      }
+      this.disk = null;
+      resolve({});
+    });
+  }
+
+  lis(_workspaceNamespace: string, _options?: any): Promise<Disk> {
+    return new Promise<Disk>((resolve, reject) => {
+      if (!this.disk) {
+        reject(Error('disk not found'));
+      }
+      resolve(this.disk);
+    });
+  }
+
+  listDisksInWorkspace(
+    _workspaceNamespace: string,
+    _options?: any
+  ): Promise<ListDisksResponse> {
+    return new Promise<ListDisksResponse>((resolve, reject) => {
+      if (!this.disk) {
+        reject(Error('disk not found'));
+      }
+      const disksResponse: ListDisksResponse = null;
+      disksResponse.push(this.disk);
+      resolve(disksResponse);
+    });
+  }
+
+  updateDisk(
+    _workspaceNamespace: string,
+    _diskName: string,
+    diskSize: number,
+    _options?: any
+  ): Promise<EmptyResponse> {
+    return new Promise<EmptyResponse>((resolve, reject) => {
+      if (!this.disk) {
+        reject(Error('disk not found'));
+      }
+      if (this.disk.size > diskSize) {
+        reject(Error('cannot decrease disk size'));
+      }
+      this.disk.size = diskSize;
+      resolve({});
+    });
+  }
+}
