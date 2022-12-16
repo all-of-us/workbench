fe011399e3322b429080cb17b1da69e2add65ef6
Eric Rollins <ericvanrollinsii@gmail.com>
2022-12-16 15:33:22 -0500

    Backing out PrimeReact Changes in order to deal with Clarity first.

--- ui/src/index.ts
+++ ui/src/index.ts
@@ -10,7 +10,7 @@ import 'nouislider/distribute/nouislider.min.css';
 import 'outdated-browser-rework/dist/style.css';
 import 'primeicons/primeicons.css';
 import 'primereact/resources/primereact.min.css';
-import 'primereact/resources/themes/nova/theme.css';
+import 'primereact/resources/themes/nova-light/theme.css';
 import 'react-calendar/dist/Calendar.css';
 import 'styles.css';
 
