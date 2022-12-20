f776b1feb57051dc359ca01debe9cdf78f439fdb
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2022-12-20 10:58:14 -0500

    [risk=low][RW-9270] Cron endpoint to delete test user workspaces (#7229)

--- api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedFirecloudServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedFirecloudServiceImpl.java
@@ -1,13 +1,17 @@
 package org.pmiops.workbench.impersonation;
 
+import static org.pmiops.workbench.firecloud.FireCloudServiceImpl.FIRECLOUD_WORKSPACE_REQUIRED_FIELDS;
 import static org.pmiops.workbench.firecloud.FireCloudServiceImpl.TERMS_OF_SERVICE_BODY;
 
 import java.io.IOException;
+import java.util.List;
 import javax.annotation.Nonnull;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.firecloud.FirecloudApiClientFactory;
 import org.pmiops.workbench.firecloud.FirecloudRetryHandler;
 import org.pmiops.workbench.firecloud.api.TermsOfServiceApi;
+import org.pmiops.workbench.firecloud.api.WorkspacesApi;
+import org.pmiops.workbench.firecloud.model.FirecloudWorkspaceResponse;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
@@ -32,18 +36,41 @@ public class ImpersonatedFirecloudServiceImpl implements ImpersonatedFirecloudSe
 
   @Override
   public void acceptTermsOfService(@Nonnull DbUser dbUser) throws IOException {
-    TermsOfServiceApi termsOfServiceApi = getTosApi(dbUser);
+    TermsOfServiceApi termsOfServiceApi = getImpersonatedTosApi(dbUser);
     retryHandler.run((context) -> termsOfServiceApi.acceptTermsOfService(TERMS_OF_SERVICE_BODY));
   }
 
   @Override
   public boolean getUserTermsOfServiceStatus(@Nonnull DbUser dbUser) throws IOException {
-    TermsOfServiceApi termsOfServiceApi = getTosApi(dbUser);
+    TermsOfServiceApi termsOfServiceApi = getImpersonatedTosApi(dbUser);
     return retryHandler.run((context) -> termsOfServiceApi.getTermsOfServiceStatus());
   }
 
-  private TermsOfServiceApi getTosApi(@Nonnull DbUser dbUser) throws IOException {
+  @Override
+  public List<FirecloudWorkspaceResponse> getWorkspaces(@Nonnull DbUser dbUser) throws IOException {
+    WorkspacesApi workspacesApi = getImpersonatedWorkspacesApi(dbUser);
+    return retryHandler.run(
+        (context) -> workspacesApi.listWorkspaces(FIRECLOUD_WORKSPACE_REQUIRED_FIELDS));
+  }
+
+  @Override
+  public void deleteWorkspace(
+      @Nonnull DbUser dbUser, String workspaceNamespace, String firecloudName) throws IOException {
+    WorkspacesApi workspacesApi = getImpersonatedWorkspacesApi(dbUser);
+    retryHandler.run(
+        (context) -> {
+          workspacesApi.deleteWorkspace(workspaceNamespace, firecloudName);
+          return null;
+        });
+  }
+
+  private TermsOfServiceApi getImpersonatedTosApi(@Nonnull DbUser dbUser) throws IOException {
     return new TermsOfServiceApi(
+        firecloudApiClientFactory.newImpersonatedApiClient(dbUser.getUsername()));
+  }
+
+  private WorkspacesApi getImpersonatedWorkspacesApi(@Nonnull DbUser dbUser) throws IOException {
+    return new WorkspacesApi(
         firecloudApiClientFactory.newImpersonatedApiClient(dbUser.getUsername()));
   }
 }
