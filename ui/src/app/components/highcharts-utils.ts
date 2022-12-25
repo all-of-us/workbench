ceb96c5e68a26fcfc3a621f8c3416799cd98e643
Chenchal Subraveti <c.subraveti@vumc.org>
2022-12-25 01:00:05 -0600

    added cout distribution by age-range - need to sort by categoryName

--- ui/src/app/components/highcharts-utils.ts
+++ ui/src/app/components/highcharts-utils.ts
@@ -1,3 +1,4 @@
+import { forEach, forIn } from 'lodash';
 import * as highCharts from 'highcharts';
 
 import { ChartData, Domain } from 'generated/fetch';
@@ -58,17 +59,14 @@ function getCategorySortKey(
   }
 }
 
-function getXAxis(ageCategories, rightSide, titleText?, linkedToVal?) {
+function getXAxis(categories, rightSide, titleText?, linkedToVal?) {
   return {
     title: {
       text: titleText ? titleText : '',
     },
-    categories: ageCategories,
+    categories: categories,
     opposite: rightSide,
     reversed: false,
-    labels: {
-      step: 1,
-    },
     linkedTo: linkedToVal,
   };
 }
@@ -132,7 +130,7 @@ export function getChartCategoryCounts(
     });
     return accum;
   }, []);
-  //console.log('series:', series);
+  // console.log('series:', series);
 
   return {
     chart: {
@@ -177,6 +175,118 @@ export function getChartCategoryCounts(
           highCharts.numberFormat(Math.abs(this.point.y), 2) +
           '%'
         );
+      },
+    },
+    series: series,
+  };
+}
+
+export function getChartCategoryCountsByAgeBin(
+  dataForCharts: Array<ChartData>,
+  domain: Domain,
+  category: Category
+) {
+  const categories = Array.from(
+    new Set(dataForCharts.map((dat) => dat.ageBin))
+  ).sort((a, b) => (a > b ? 1 : -1));
+
+  const categoryProp = category.toString();
+  const total = {};
+  let categoryColorIndex = -1;
+  interface ReduceReturnType {
+    id: number; // 👈️ 👈️ 👈️ no longer optional
+    name: string;
+    color: string;
+    sortKey: any;
+    data: [any];
+  }
+
+  const categoryCounts = dataForCharts.reduce<ReduceReturnType>(
+    (accum, record) => {
+      const key = record[categoryProp];
+      const ageBin = record.ageBin;
+      const rec = accum[key]; // accum['Female']
+      // console.log('key:',key,'rec:',rec);
+      if (!rec) {
+        // 1st data record
+        categoryColorIndex += 1;
+        const seriesObj = {
+          name: key,
+          color: hcColors.hcColors[categoryProp][categoryColorIndex],
+          sortKey: getCategorySortKey(domain, categoryProp, record),
+          data: [
+            {
+              ageBin: record.ageBin,
+              category: categoryProp,
+              categoryName: key,
+              categoryCount: record.count,
+            },
+          ],
+        };
+        accum[key] = seriesObj;
+        total[key] = record.count;
+      } else {
+        const dataRec = accum[key].data.find((item) => item.ageBin === ageBin);
+        if (!dataRec) {
+          // next data record
+          accum[key].data.push({
+            ageBin: record.ageBin,
+            category: categoryProp,
+            categoryName: key,
+            categoryCount: record.count,
+          });
+        } else {
+          dataRec.categoryCount += record.count;
+        }
+        total[key] += record.count;
+      }
+      return accum;
+    },
+    {} as ReduceReturnType
+  );
+  // compute %
+  // categoryTotal * 100 / sum(categoryTotal)
+  Object.values(categoryCounts).reduce((accum, serObj) => {
+    serObj.data.reduce((recAccum, rec) => {
+      rec.categoryTotal = total[serObj.name];
+      rec.x = categories.findIndex((cat) => rec.ageBin === cat);
+      rec.y = (rec.categoryCount * 100) / total[serObj.name];
+    }, []);
+  }, {});
+  const series = Object.values(categoryCounts);
+
+  return {
+    chart: {
+      type: 'column',
+      inverted: true,
+    },
+    title: {
+      text: CHART_CATEGORY_KEY_NAME[categoryProp],
+    },
+    xAxis: [getXAxis(categories, false, 'Age Range(Y)')],
+    yAxis: [getYAxis('Percent(%)')],
+    legend: {
+      layout: 'horizontal',
+      // align: 'right',
+      verticalAlign: 'top',
+      itemMarginBottom: 5,
+      useHTML: true,
+      labelFormatter: function () {
+        return this.name.slice(0, 15) + (this.name.length > 15 ? '...' : '');
+      },
+    },
+    plotOptions: {
+      series: {
+        stacking: 'normal', // 'normal',
+      },
+    },
+    tooltip: {
+      formatter: function () {
+        let tip = this.point.ageBin + '(y), ';
+        tip += this.point.categoryName + ', <br/>';
+        tip += 'counts: (' + this.point.categoryCount;
+        tip += '/' + this.point.categoryTotal + ')<br/>';
+        return tip + 'percent:' + parseFloat(this.point.y).toFixed(2) + '%';
       },
     },
     series: series,
