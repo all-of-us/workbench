c525659f9c0f23ec4301bdca20062f69eccf985e
Chenchal Subraveti <chenchals@users.noreply.github.com>
2023-01-19 16:27:27 -0600

    [DT-189-7][risk=no] CDR indexing build - to build or not to build prep_survey (#7317)
    * added wait at the end, since the last table-creation might not have completed
    
    * prepsurveycount only if results has 'count'
    
    * removed testing code for associative arrays
    
    * updated to use temp table to keep track of newly created/retained prep tables with row_count column. This is used by build-prep-survey.sh to decide whether to create prep_survey table data
    
    * updated to use temp table to keep track of newly created/retained prep tables with row_count column. This is used by build-prep-survey.sh to decide whether to create prep_survey table data
    
    * update row counts for prep_survey when not creating new
    
    * fix codacy word splitting warning

--- api/db-cdr/generate-cdr/create-tables.sh
+++ api/db-cdr/generate-cdr/create-tables.sh
@@ -1,30 +1,16 @@
-#!/bin/bash
+#!/usr/bin/env bash
 
-# #!/bin/bash
+# using #!/usr/bin/env bash else need to change for local development
 # #!/usr/bin/env bash -> we can use associative arrays
-# This script removes/creates all CDR indices specific tables.
+# #!/bin/bash -> we can use associative arrays
 
+# This script removes/creates all CDR indices specific tables.
 set -e
 
 export BQ_PROJECT=$1         # CDR project
 export BQ_DATASET=$2         # CDR dataset
 
-function testCiBashAssociativeEnabled(){
-  echo `bash --version`
-
-  declare -A C_TABLES
-  C_TABLES["cb_search_all_events"]="concept_id"
-  C_TABLES["cb_review_survey"]="person_id"
-  C_TABLES["cb_search_person"]="person_id"
-  C_TABLES["cb_review_all_events"]="person_id,domain"
-
-  echo "Associative Array Keys: ${!C_TABLES[@]}"
-  echo "Associative Array Values: ${C_TABLES[@]}"
-  key="cb_review_survey"
-  echo "Value for key: $key = ${C_TABLES[$key]} "
-}
-# remove this after running once on Circle CI
-testCiBashAssociativeEnabled
+export table_names_table="prep_create_tables_list"
 
 function deleteAndCreateTable(){
   local table_name=$1
@@ -43,6 +29,25 @@ function deleteAndCreateTable(){
     bq --quiet --project_id="$BQ_PROJECT" mk --schema="$schema_path/$json_name" "$BQ_DATASET.$table_name"
   fi
   wait
+  updateRowCounts "$table_name"
+}
+
+function createTableForRowCounts(){
+  echo "Creating $table_names_table"
+  ddl="CREATE OR REPLACE TABLE \`$BQ_PROJECT.$BQ_DATASET.$table_names_table\` AS SELECT '' table_name, null row_count"
+  bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "$ddl"
+  wait
+}
+
+function updateRowCounts(){
+  local table_name=$1
+  bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
+    "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.$table_names_table\`
+      (table_name, row_count)
+      (SELECT '$table_name' as table_name, count(*) as row_count
+      FROM \`$BQ_PROJECT.$BQ_DATASET.$table_name\`)
+    "
+  wait
 }
 
 INCOMPATIBLE_DATASETS=("R2019Q4R3" "R2019Q4R4" "R2020Q4R3", "R2021Q3R5", "C2021Q2R1", "C2021Q3R6", "R2022Q2R2", "C2022Q2R2", "R2022Q2R6")
@@ -53,10 +58,18 @@ if [[ ${INCOMPATIBLE_DATASETS[@]} =~ $BQ_DATASET ]];
   exit 1
 fi
 
+# now that we can run cdr-indexing build - create a temp table to keep track of newly created/retained tables
+createTableForRowCounts
+
 TABLE_LIST=$(bq ls -n 1000 "$BQ_PROJECT:$BQ_DATASET" | tail -n +3 | cut -d " " -f 3 )
 
 SKIP_TABLES=("cb_data_filter" "cb_person" "survey_module" "domain_card")
-CLUSTERED_TABLES=("cb_search_all_events" "cb_review_survey" "cb_search_person" "cb_review_all_events")
+
+declare -A CLUSTERED_TABLES
+CLUSTERED_TABLES["cb_search_all_events"]="concept_id"
+CLUSTERED_TABLES["cb_review_survey"]="person_id"
+CLUSTERED_TABLES["cb_search_person"]="person_id"
+CLUSTERED_TABLES["cb_review_all_events"]="person_id,domain"
 
 schema_path=generate-cdr/bq-schemas
 for filename in generate-cdr/bq-schemas/*.json;
@@ -64,14 +77,8 @@ do
     json_name=${filename##*/}
     table_name=${json_name%.json}
 
-    if [[ ${CLUSTERED_TABLES[@]} =~ "$table_name" ]]; then
-      if [[ "$table_name" =~ cb_search_all_events ]]; then
-        deleteAndCreateTable "$table_name" "concept_id"
-      elif [[ "$table_name" =~ cb_review_survey|cb_search_person ]]; then
-        deleteAndCreateTable "$table_name" "person_id"
-      elif [[ "$table_name" =~ cb_review_all_events ]]; then
-        deleteAndCreateTable "$table_name" "person_id,domain"
-      fi
+    if [[ -n "${CLUSTERED_TABLES[$table_name]}" ]]; then
+      deleteAndCreateTable "$table_name" "${CLUSTERED_TABLES[$table_name]}"
     elif [[ ${SKIP_TABLES[@]} =~ "$table_name"  ]]; then
       echo "Skipping table $table_name"
     elif [[ "$table_name" == 'ds_zip_code_socioeconomic' ]]; then
@@ -82,9 +89,13 @@ do
       if [[ "$TABLE_LIST" != *"prep_survey"* ]]; then
         deleteAndCreateTable "$table_name"
       else
-        echo "Keeping existing prep_survey table"
+        echo "Keeping existing prep_survey table and updating row count"
+        updateRowCounts "$table_name"
       fi
     else
       deleteAndCreateTable "$table_name"
     fi
 done
+wait
+echo "Done creating tables"
+
