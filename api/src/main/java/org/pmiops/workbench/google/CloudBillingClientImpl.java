59d66051329b65928756a4bd1d848c211369184f
Yonghao Yu <yonghao@google.com>
2023-01-20 16:40:04 -0500

    increase retry attempp

--- api/src/main/java/org/pmiops/workbench/google/CloudBillingClientImpl.java
+++ api/src/main/java/org/pmiops/workbench/google/CloudBillingClientImpl.java
@@ -29,7 +29,7 @@ public class CloudBillingClientImpl implements CloudBillingClient {
   public ProjectBillingInfo pollUntilBillingAccountLinked(
       String projectId, String billingAccountName) throws IOException, InterruptedException {
     Duration pollInterval = Duration.ofSeconds(2);
-    for (Instant deadline = Instant.now().plusSeconds(180);
+    for (Instant deadline = Instant.now().plusSeconds(300);
         Instant.now().isBefore(deadline);
         Thread.sleep(pollInterval.toMillis())) {
       ProjectBillingInfo projectBillingInfo = getProjectBillingInfo(projectId);
