5c577c6200f34b044a8388a6093aff071fea28e6
Chenchal Subraveti <c.subraveti@vumc.org>
2023-01-25 08:26:13 -0600

    fix codacy globbing

--- api/db-cdr/generate-cdr/build-ds-tables.sh
+++ api/db-cdr/generate-cdr/build-ds-tables.sh
@@ -208,7 +208,7 @@ function do_ds_visit_occurrence(){
 }
 
 function do_ds_zip_code_socioeconomic(){
-  if [[ "$(bq ls -n 1000 $BQ_PROJECT:$BQ_DATASET)" == *"zip3_ses_map"* ]]; then
+  if [[ "$(bq ls -n 1000 "$BQ_PROJECT":"$BQ_DATASET")" == *"zip3_ses_map"* ]]; then
     echo "ds_zip_code_socioeconomic - inserting data - Controlled Tier"
     bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
     "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.ds_zip_code_socioeconomic\`
