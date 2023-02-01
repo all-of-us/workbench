ac744ddcbd9e56445a8d239670e157fe964955a3
Eric Rollins <ericvanrollinsii@gmail.com>
2023-02-01 13:00:23 -0500

    Removed unnecessary hook.

--- ui/src/app/components/configuration-panel.tsx
+++ ui/src/app/components/configuration-panel.tsx
@@ -3,52 +3,51 @@ import { useEffect, useState } from 'react';
 import * as fp from 'lodash/fp';
 
 import { workspacesApi } from 'app/services/swagger-fetch-clients';
-import { withCdrVersions, withCurrentWorkspace } from 'app/utils';
+import { withCurrentWorkspace } from 'app/utils';
 
 import { CromwellConfigurationPanel } from './cromwell-configuration-panel';
 import { RuntimeConfigurationPanel } from './runtime-configuration-panel';
 
-export const ConfigurationPanel = fp.flow(
-  withCdrVersions(),
-  withCurrentWorkspace()
-)(({ onClose, workspace, type = 'runtime', runtimeConfPanelInitialState }) => {
-  const { namespace, id } = workspace;
-  const [creatorFreeCreditsRemaining, setCreatorFreeCreditsRemaining] =
-    useState(null);
+export const ConfigurationPanel = fp.flow(withCurrentWorkspace())(
+  ({ onClose, workspace, type = 'runtime', runtimeConfPanelInitialState }) => {
+    const { namespace, id } = workspace;
+    const [creatorFreeCreditsRemaining, setCreatorFreeCreditsRemaining] =
+      useState(null);
 
-  useEffect(() => {
-    const aborter = new AbortController();
-    const fetchFreeCredits = async () => {
-      const { freeCreditsRemaining } =
-        await workspacesApi().getWorkspaceCreatorFreeCreditsRemaining(
-          namespace,
-          id,
-          { signal: aborter.signal }
-        );
-      setCreatorFreeCreditsRemaining(freeCreditsRemaining);
-    };
+    useEffect(() => {
+      const aborter = new AbortController();
+      const fetchFreeCredits = async () => {
+        const { freeCreditsRemaining } =
+          await workspacesApi().getWorkspaceCreatorFreeCreditsRemaining(
+            namespace,
+            id,
+            { signal: aborter.signal }
+          );
+        setCreatorFreeCreditsRemaining(freeCreditsRemaining);
+      };
 
-    fetchFreeCredits();
+      fetchFreeCredits();
 
-    return function cleanup() {
-      aborter.abort();
-    };
-  }, []);
+      return function cleanup() {
+        aborter.abort();
+      };
+    }, []);
 
-  return (
-    <div id='configuration-panel-container'>
-      {type === 'runtime' ? (
-        <div>
-          <RuntimeConfigurationPanel
-            {...{ onClose, creatorFreeCreditsRemaining }}
-            initialPanelContent={runtimeConfPanelInitialState}
+    return (
+      <div id='configuration-panel-container'>
+        {type === 'runtime' ? (
+          <div>
+            <RuntimeConfigurationPanel
+              {...{ onClose, creatorFreeCreditsRemaining }}
+              initialPanelContent={runtimeConfPanelInitialState}
+            />
+          </div>
+        ) : (
+          <CromwellConfigurationPanel
+            {...{ onClose, creatorFreeCreditsRemaining, workspace }}
           />
-        </div>
-      ) : (
-        <CromwellConfigurationPanel
-          {...{ onClose, creatorFreeCreditsRemaining, workspace }}
-        />
-      )}
-    </div>
-  );
-});
+        )}
+      </div>
+    );
+  }
+);
