77f93be4720b32d650fd05ef0d7380f994b9e21a
Eric Rollins <ericvanrollinsii@gmail.com>
2023-02-09 16:22:39 -0500

    Updated test code to work with PrimeReact's new DataTable structure.

--- e2e/app/page/cohort-review-page.ts
+++ e2e/app/page/cohort-review-page.ts
@@ -40,7 +40,7 @@ export default class CohortReviewPage extends AuthenticatedPage {
    */
   async clickParticipantLink(rowIndex = 1, colIndex = 1): Promise<string> {
     const dataTable = this.getDataTable();
-    const bodyTable = dataTable.getBodyTable();
+    const bodyTable = dataTable.getTable();
     const cell = await bodyTable.getCell(rowIndex, colIndex);
     const innerText = await getPropValue<string>(cell, 'innerText');
     await cell.click();
@@ -54,7 +54,7 @@ export default class CohortReviewPage extends AuthenticatedPage {
    */
   async getParticipantLinkId(rowIndex = 1, colIndex = 1): Promise<string> {
     const dataTable = this.getDataTable();
-    const bodyTable = dataTable.getBodyTable();
+    const bodyTable = dataTable.getTable();
     const cell = await bodyTable.getCell(rowIndex, colIndex);
     const textContent = await getPropValue<string>(cell, 'textContent');
     return textContent;
