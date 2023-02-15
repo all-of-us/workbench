85b1804b6d0179adf01aadae96fc451bbc76c9b4
Joel Thibault <thibault@broad.dev>
2023-02-15 10:01:31 -0500

    tweaks

--- ui/src/app/pages/signed-in/access-renewal-notification.tsx
+++ ui/src/app/pages/signed-in/access-renewal-notification.tsx
@@ -35,7 +35,9 @@ export const AccessRenewalNotificationMaybe = (props: {
       buttonText='Get Started'
       buttonPath={ACCESS_RENEWAL_PATH}
       buttonDisabled={fullPagePath === ACCESS_RENEWAL_PATH}
-      bannerTextWidth='177px'
+      bannerTextWidth={
+        props.accessTier === AccessTierShortNames.Registered ? '177px' : '250px'
+      }
     />
   ) : null;
 };
