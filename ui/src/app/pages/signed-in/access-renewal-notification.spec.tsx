96f11ac95461c3067f42dfe41da89bcd312316f7
Joel Thibault <thibault@broad.dev>
2023-02-15 11:18:21 -0500

    more tests

--- ui/src/app/pages/signed-in/access-renewal-notification.spec.tsx
+++ ui/src/app/pages/signed-in/access-renewal-notification.spec.tsx
@@ -1,7 +1,11 @@
 import * as React from 'react';
+import { MemoryRouter } from 'react-router';
 import { mount } from 'enzyme';
 
+import { AccessModule, AccessModuleStatus } from 'generated/fetch';
+
 import { AccessTierShortNames } from 'app/utils/access-tiers';
+import { nowPlusDays } from 'app/utils/dates';
 import { profileStore, serverConfigStore } from 'app/utils/stores';
 
 import defaultServerConfig from 'testing/default-server-config';
@@ -12,28 +16,153 @@ import {
   AccessRenewalNotificationProps,
 } from './access-renewal-notification';
 
-const profile = ProfileStubVariables.PROFILE_STUB;
 const load = jest.fn();
 const reload = jest.fn();
 const updateCache = jest.fn();
 
+const allModules = [
+  AccessModule.DATAUSERCODEOFCONDUCT,
+  AccessModule.COMPLIANCETRAINING,
+  AccessModule.CTCOMPLIANCETRAINING,
+  AccessModule.ERACOMMONS,
+  AccessModule.TWOFACTORAUTH,
+  AccessModule.RASLINKLOGINGOV,
+  AccessModule.PROFILECONFIRMATION,
+  AccessModule.PUBLICATIONCONFIRMATION,
+];
+
+const allCompleteNotExpiring: AccessModuleStatus[] = allModules.map(
+  (moduleName) => ({
+    moduleName,
+    completionEpochMillis: Date.now(),
+    expirationEpochMillis: nowPlusDays(365),
+  })
+);
+
+const allCompleteOneExpiring = (moduleName: AccessModule) => [
+  ...allCompleteNotExpiring,
+  {
+    moduleName,
+    completionEpochMillis: Date.now(),
+    expirationEpochMillis: nowPlusDays(1),
+  },
+];
+
+const allCompleteOneExpired = (moduleName: AccessModule) => [
+  ...allCompleteNotExpiring,
+  {
+    moduleName,
+    completionEpochMillis: Date.now(),
+    expirationEpochMillis: nowPlusDays(-1),
+  },
+];
+
+const updateModules = (modules: AccessModuleStatus[]) => {
+  profileStore.set({
+    profile: {
+      ...ProfileStubVariables.PROFILE_STUB,
+      accessModules: { modules },
+    },
+    load,
+    reload,
+    updateCache,
+  });
+};
+
 describe('Access Renewal Notification', () => {
   const component = (props: AccessRenewalNotificationProps) => {
-    return mount(<AccessRenewalNotificationMaybe {...props} />);
+    return mount(
+      <MemoryRouter>
+        <AccessRenewalNotificationMaybe {...props} />
+      </MemoryRouter>
+    );
   };
 
   beforeEach(() => {
     serverConfigStore.set({ config: defaultServerConfig });
-    profileStore.set({ profile, load, reload, updateCache });
-  });
-
-  it('Should not render when renewal is not needed (no modules)', async () => {
-    const wrapper = component({ accessTier: AccessTierShortNames.Registered });
-    expect(wrapper.exists()).toBeTruthy();
-
-    const banner = wrapper.find({
-      'data-test-id': 'access-renewal-notification',
+    profileStore.set({
+      profile: ProfileStubVariables.PROFILE_STUB,
+      load,
+      reload,
+      updateCache,
     });
-    expect(banner.exists()).toBeFalsy();
   });
+
+  test.each([
+    [
+      false,
+      'for RT when there are no modules',
+      AccessTierShortNames.Registered,
+      [],
+    ],
+    [
+      false,
+      'for CT when there are no modules',
+      AccessTierShortNames.Controlled,
+      [],
+    ],
+    [
+      false,
+      'for RT when there are no expiring modules',
+      AccessTierShortNames.Registered,
+      allCompleteNotExpiring,
+    ],
+    [
+      false,
+      'for CT when there are no expiring modules',
+      AccessTierShortNames.Controlled,
+      allCompleteNotExpiring,
+    ],
+    [
+      true,
+      'for RT when there is one expiring module',
+      AccessTierShortNames.Registered,
+      allCompleteOneExpiring(AccessModule.DATAUSERCODEOFCONDUCT),
+    ],
+    [
+      true,
+      'for CT when there is one expiring module',
+      AccessTierShortNames.Controlled,
+      allCompleteOneExpiring(AccessModule.DATAUSERCODEOFCONDUCT),
+    ],
+    [
+      true,
+      'for RT when there is one expired module',
+      AccessTierShortNames.Registered,
+      allCompleteOneExpired(AccessModule.DATAUSERCODEOFCONDUCT),
+    ],
+    [
+      true,
+      'for CT when there is one expired module',
+      AccessTierShortNames.Controlled,
+      allCompleteOneExpired(AccessModule.DATAUSERCODEOFCONDUCT),
+    ],
+    [
+      false,
+      'for RT when only CT training is expiring',
+      AccessTierShortNames.Registered,
+      allCompleteOneExpiring(AccessModule.CTCOMPLIANCETRAINING),
+    ],
+    [
+      false,
+      'for RT when only CT training is expired',
+      AccessTierShortNames.Registered,
+      allCompleteOneExpired(AccessModule.CTCOMPLIANCETRAINING),
+    ],
+  ])(
+    'display=%s %s',
+    async (expected, condition, accessTier, moduleStatuses) => {
+      updateModules(moduleStatuses);
+
+      const wrapper = component({
+        accessTier,
+      });
+      expect(wrapper.exists()).toBeTruthy();
+
+      const banner = wrapper.find({
+        'data-test-id': 'access-renewal-notification',
+      });
+      expect(banner.exists()).toEqual(expected);
+    }
+  );
 });
