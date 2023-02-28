1394d8588ad3388278b76daed9eff45d0a4d927a
Joel Thibault <thibault@broad.dev>
2023-02-28 13:56:55 -0500

    pause/resume tests

--- ui/src/app/components/apps-panel/expanded-app.spec.tsx
+++ ui/src/app/components/apps-panel/expanded-app.spec.tsx
@@ -11,13 +11,17 @@ import {
 } from 'generated/fetch';
 
 import { environment } from 'environments/environment';
-import { leoAppsApi } from 'app/services/notebooks-swagger-fetch-clients';
+import {
+  leoAppsApi,
+  leoRuntimesApi,
+} from 'app/services/notebooks-swagger-fetch-clients';
 import { registerApiClient as leoRegisterApiClient } from 'app/services/notebooks-swagger-fetch-clients';
 import { appsApi, registerApiClient } from 'app/services/swagger-fetch-clients';
 import { runtimeStore, serverConfigStore } from 'app/utils/stores';
 import { AppsApi as LeoAppsApi, RuntimesApi } from 'notebooks-generated/fetch';
 
 import defaultServerConfig from 'testing/default-server-config';
+import { waitOneTickAndUpdate } from 'testing/react-test-helpers';
 import { AppsApiStub } from 'testing/stubs/apps-api-stub';
 import { LeoAppsApiStub } from 'testing/stubs/leo-apps-api-stub';
 import { NotebooksApiStub } from 'testing/stubs/notebooks-api-stub';
@@ -28,7 +32,11 @@ import { workspaceDataStub } from 'testing/stubs/workspaces';
 import { ExpandedApp } from './expanded-app';
 import { UIAppType } from './utils';
 
-const workspace = workspaceDataStub;
+const googleProject = 'project-for-test';
+const workspace = {
+  ...workspaceDataStub,
+  googleProject,
+};
 const onClickRuntimeConf = jest.fn();
 const onClickDeleteRuntime = jest.fn();
 
@@ -51,6 +59,7 @@ const component = async (
 describe('ExpandedApp', () => {
   const appsStub = new AppsApiStub();
   const runtimeStub = new RuntimeApiStub();
+  runtimeStub.runtime.googleProject = googleProject;
   beforeEach(() => {
     serverConfigStore.set({ config: defaultServerConfig });
     environment.showAppsPanel = true;
@@ -95,40 +104,70 @@ describe('ExpandedApp', () => {
     }
   );
 
-  // test.each([
-  //   ['should', RuntimeStatus.Running, true],
-  //   // ['should', RuntimeStatus.Stopped, true],
-  //   // ['should not', RuntimeStatus.Stopping, false],
-  //   // ['should not', RuntimeStatus.Starting, false],
-  // ])(
-  //   '%s allow pausing when the Jupyter app status is %s',
-  //   async (shouldOrNot, status, expectedCanPause) => {
-  //     runtimeStub.runtime.status = status;
-  //
-  //     const wrapper = await component(UIAppType.JUPYTER, undefined);
-  //     expect(wrapper.exists()).toBeTruthy();
-  //
-  //     const pauseButton = wrapper.find({ 'data-test-id': 'pause-resume-Jupyter' });
-  //     expect(pauseButton.exists()).toBeTruthy();
-  //     const cursorStyle = pauseButton.prop('style').cursor;
-  //
-  //     if (expectedCanPause) {
-  //       expect(cursorStyle).not.toEqual('not-allowed');
-  //
-  //       const pauseSpy = jest.spyOn(leoRuntimesApi(), 'stopRuntime');
-  //       const { onClick } = pauseButton.props();
-  //       await onClick();
-  //       await waitOneTickAndUpdate(wrapper);
-  //
-  //       expect(pauseSpy).toHaveBeenCalledWith(
-  //         workspace.googleProject,
-  //         runtimeStub.runtime.runtimeName
-  //       );
-  //     } else {
-  //       expect(cursorStyle).toEqual('not-allowed');
-  //     }
-  //   }
-  // );
+  test.each([
+    ['should', 'pausing', RuntimeStatus.Running, true, false],
+    ['should', 'resuming', RuntimeStatus.Stopped, false, true],
+
+    ['should not', 'pausing/resuming', RuntimeStatus.Stopping, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Starting, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Deleted, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Deleting, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Unknown, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Error, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Creating, false, false],
+    ['should not', 'pausing/resuming', RuntimeStatus.Updating, false, false],
+
+    ['should not', 'pausing/resuming', undefined, false, false],
+    ['should not', 'pausing/resuming', null, false, false],
+  ])(
+    '%s allow pausing when the Jupyter app status is %s',
+    async (
+      shouldOrNot,
+      action,
+      status,
+      expectedCanPause,
+      expectedCanResume
+    ) => {
+      runtimeStub.runtime.status = status;
+
+      const wrapper = await component(UIAppType.JUPYTER, undefined);
+      expect(wrapper.exists()).toBeTruthy();
+
+      const pauseButton = wrapper.find({
+        'data-test-id': 'pause-resume-Jupyter',
+      });
+      expect(pauseButton.exists()).toBeTruthy();
+      const { disabled } = pauseButton.props();
+
+      if (expectedCanPause) {
+        expect(disabled).toBeFalsy();
+
+        const pauseSpy = jest.spyOn(leoRuntimesApi(), 'stopRuntime');
+        const { onClick } = pauseButton.props();
+        await onClick();
+        await waitOneTickAndUpdate(wrapper);
+
+        expect(pauseSpy).toHaveBeenCalledWith(
+          workspace.googleProject,
+          runtimeStub.runtime.runtimeName
+        );
+      } else if (expectedCanResume) {
+        expect(disabled).toBeFalsy();
+
+        const resumeSpy = jest.spyOn(leoRuntimesApi(), 'startRuntime');
+        const { onClick } = pauseButton.props();
+        await onClick();
+        await waitOneTickAndUpdate(wrapper);
+
+        expect(resumeSpy).toHaveBeenCalledWith(
+          workspace.googleProject,
+          runtimeStub.runtime.runtimeName
+        );
+      } else {
+        expect(disabled).toBeTruthy();
+      }
+    }
+  );
 
   test.each([
     ['should', AppStatus.RUNNING, true],
@@ -175,6 +214,17 @@ describe('ExpandedApp', () => {
   test.each([
     ['should', 'pausing', AppStatus.RUNNING, true, false],
     ['should', 'resuming', AppStatus.STOPPED, false, true],
+
+    ['should not', 'pause/resume', AppStatus.DELETED, false, false],
+    ['should not', 'pause/resume', AppStatus.DELETING, false, false],
+    ['should not', 'pause/resume', AppStatus.ERROR, false, false],
+    ['should not', 'pause/resume', AppStatus.PROVISIONING, false, false],
+    ['should not', 'pause/resume', AppStatus.STARTING, false, false],
+    ['should not', 'pause/resume', AppStatus.STATUSUNSPECIFIED, false, false],
+    ['should not', 'pause/resume', AppStatus.STOPPING, false, false],
+
+    ['should not', 'pause/resume', undefined, false, false],
+    ['should not', 'pause/resume', null, false, false],
   ])(
     '%s allow %s when the Cromwell app status is %s',
     async (
@@ -187,9 +237,10 @@ describe('ExpandedApp', () => {
       const appName = 'my-cromwell';
 
       const wrapper = await component(UIAppType.CROMWELL, {
-        appName,
-        status,
         appType: AppType.CROMWELL,
+        appName,
+        googleProject,
+        status,
       });
       expect(wrapper.exists()).toBeTruthy();
 
@@ -197,10 +248,10 @@ describe('ExpandedApp', () => {
         'data-test-id': 'pause-resume-CROMWELL',
       });
       expect(pauseButton.exists()).toBeTruthy();
-      const cursorStyle = pauseButton.prop('style').cursor;
+      const { disabled } = pauseButton.props();
 
       if (expectedCanPause) {
-        expect(cursorStyle).not.toEqual('not-allowed');
+        expect(disabled).toBeFalsy();
 
         const pauseSpy = jest.spyOn(leoAppsApi(), 'stopApp');
         const { onClick } = pauseButton.props();
@@ -208,7 +259,7 @@ describe('ExpandedApp', () => {
 
         expect(pauseSpy).toHaveBeenCalledWith(workspace.googleProject, appName);
       } else if (expectedCanResume) {
-        expect(cursorStyle).not.toEqual('not-allowed');
+        expect(disabled).toBeFalsy();
 
         const resumeSpy = jest.spyOn(leoAppsApi(), 'startApp');
         const { onClick } = pauseButton.props();
@@ -219,7 +270,7 @@ describe('ExpandedApp', () => {
           appName
         );
       } else {
-        expect(cursorStyle).toEqual('not-allowed');
+        expect(disabled).toBeTruthy();
       }
     }
   );
