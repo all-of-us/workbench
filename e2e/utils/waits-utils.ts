3455329db68a10344beac9915297d6f821b7ac3f
Peter Lavigne <peterlavigne@google.com>
2023-03-09 14:50:43 -0500

    Fix lint error

--- e2e/utils/waits-utils.ts
+++ e2e/utils/waits-utils.ts
@@ -5,7 +5,11 @@ import Container from 'app/container';
 import { makeDateTimeStr } from './str-utils';
 import { getAttrValue } from './element-utils';
 
-export const waitForFn = async (fn: () => any, interval = 2000, timeout = 10000): Promise<boolean> => {
+export const waitForFn = async (
+  fn: () => any,
+  interval = 2000,
+  timeout = 10000
+): Promise<boolean> => {
   const start = Date.now();
   while (Date.now() < start + timeout) {
     if (fn()) {
