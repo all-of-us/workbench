89866a771dcd02825afc9680a818ad58c8cad0d8
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2023-03-10 10:43:55 -0500

    [RW-8657 ][risk=no] Clean up Demographic survey banner text logic after 30 days deadline is reached (#7466)

--- ui/src/app/utils/constants.tsx
+++ ui/src/app/utils/constants.tsx
@@ -1,5 +1,3 @@
-export const DEMOGRAPHIC_SURVEY_V2_NOTIFICATION_END_DATE = '8/21/2022';
-
 export const DEMOGRAPHIC_SURVEY_V2_PATH = '/demographic-survey';
 
 export const DEMOGRAPHIC_SURVEY_SESSION_KEY =
