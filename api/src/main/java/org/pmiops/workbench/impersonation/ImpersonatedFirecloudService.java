7a37a332a0f6c8e833af46f217e89a880dad8086
Yonghao Yu <yonghao@google.com>
2023-03-16 10:05:39 -0400

    [RW-9706][risk=low] Try to delete k8s resource during test workspace cleanup (#7471)
    * delte k8s test
    
    * delte k8s test
    
    * delte k8s test
    
    * delte k8s test
    
    * lint
    
    * address comments

--- api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedFirecloudService.java
+++ api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedFirecloudService.java
@@ -7,7 +7,7 @@ import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.firecloud.model.FirecloudWorkspaceResponse;
 
 /**
- * An impersonation-enabled version of {@link org.pmiops.workbench.firecloud.FireCloudService}
+ * * An impersonation-enabled version of {@link org.pmiops.workbench.firecloud.FireCloudService}
  *
  * <p>REMINDER: With great power comes great responsibility. Impersonation should not be used in
  * production, except where absolutely necessary.
@@ -22,5 +22,9 @@ public interface ImpersonatedFirecloudService {
   List<FirecloudWorkspaceResponse> getWorkspaces(@Nonnull DbUser dbUser) throws IOException;
 
   void deleteWorkspace(@Nonnull DbUser dbUser, String workspaceNamespace, String firecloudName)
+      throws IOException;
+
+  /** Deletes SAM Kubernetes within a Google project. */
+  void deleteSamKubernetesResourcesInWorkspace(@Nonnull DbUser dbUser, String googleProjectId)
       throws IOException;
 }
