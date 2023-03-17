bd12f83d2406ab13996c6309405c553f4d6b5ce0
Joel Thibault <thibault@broad.dev>
2023-03-17 15:44:01 -0400

    refactor

--- e2e/tests/gke-apps/cromwell.spec.ts
+++ e2e/tests/gke-apps/cromwell.spec.ts
@@ -4,30 +4,9 @@ import Button from 'app/element/button';
 import CromwellConfigurationPanel from 'app/sidebar/cromwell-configuration-panel';
 import BaseElement from 'app/element/base-element';
 import { waitForFn } from 'utils/waits-utils';
-import { Page } from 'puppeteer';
 
 // 10 minutes
 jest.setTimeout(10 * 60 * 1000);
-
-// TODO is this already a thing?
-const getTextFromXPath = async (page: Page, xPath: string): Promise<string> =>
-  BaseElement.asBaseElement(page, await page.waitForXPath(xPath)).getTextContent();
-
-const pollForStatus = async (page: Page, appsPanel: AppsPanel, expandedCromwellXpath: string, status: string) => {
-  const success = await waitForFn(
-    async () => {
-      await appsPanel.close();
-      await appsPanel.open();
-
-      return (await getTextFromXPath(page, expandedCromwellXpath)).includes(`status: ${status}`);
-    },
-    10e3, // every 10 sec
-    10 * 60e3 // with a 10 min timeout
-  );
-  expect(success).toBeTruthy();
-
-  console.log(`Cromwell status: ${status}`);
-};
 
 describe('Cromwell GKE App', () => {
   beforeEach(async () => {
@@ -39,12 +18,12 @@ describe('Cromwell GKE App', () => {
   test('Create and delete a Cromwell GKE app', async () => {
     await findOrCreateWorkspace(page, { workspaceName });
 
-    const applicationsPanel = new AppsPanel(page);
-    await applicationsPanel.open();
+    const appsPanel = new AppsPanel(page);
+    await appsPanel.open();
 
     // Cromwell is not running, so it appears in unexpanded mode
 
-    const unexpandedCromwellXPath = `${applicationsPanel.getXpath()}//*[@data-test-id="Cromwell-unexpanded"]`;
+    const unexpandedCromwellXPath = `${appsPanel.getXpath()}//*[@data-test-id="Cromwell-unexpanded"]`;
     const unexpandedCromwell = new Button(page, unexpandedCromwellXPath);
 
     expect(await unexpandedCromwell.exists()).toBeTruthy();
@@ -71,17 +50,21 @@ describe('Cromwell GKE App', () => {
 
     await page.waitForXPath(configPanel.getXpath(), { visible: false });
 
-    await applicationsPanel.isVisible();
-    const expandedCromwellXpath = `${applicationsPanel.getXpath()}//*[@data-test-id="Cromwell-expanded"]`;
+    await appsPanel.isVisible();
+    const expandedCromwellXpath = `${appsPanel.getXpath()}//*[@data-test-id="Cromwell-expanded"]`;
     await page.waitForXPath(expandedCromwellXpath);
 
     // the Cromwell status should say PROVISIONING
 
-    expect(await getTextFromXPath(page, expandedCromwellXpath)).toContain('status: PROVISIONING');
+    const cromwellText = await BaseElement.asBaseElement(
+      page,
+      await page.waitForXPath(expandedCromwellXpath)
+    ).getTextContent();
+    expect(cromwellText).toContain('status: PROVISIONING');
     console.log('Cromwell status: PROVISIONING');
 
     // poll for "Running" by repeatedly closing and opening
-    await pollForStatus(page, applicationsPanel, expandedCromwellXpath, 'Running');
+    await appsPanel.pollForStatus(expandedCromwellXpath, 'Running');
 
     const deleteXPath = `${expandedCromwellXpath}//*[@data-test-id="Cromwell-delete-button"]`;
     const deleteButton = new Button(page, deleteXPath);
@@ -89,14 +72,14 @@ describe('Cromwell GKE App', () => {
     await deleteButton.click();
 
     // poll for "DELETING" by repeatedly closing and opening
-    await pollForStatus(page, applicationsPanel, expandedCromwellXpath, 'DELETING');
+    await appsPanel.pollForStatus(expandedCromwellXpath, 'DELETING');
 
     // poll for deleted (unexpanded) by repeatedly closing and opening
 
     const isDeleted = await waitForFn(
       async () => {
-        await applicationsPanel.close();
-        await applicationsPanel.open();
+        await appsPanel.close();
+        await appsPanel.open();
         const unexpanded = new Button(page, unexpandedCromwellXPath);
         return await unexpanded.exists();
       },
