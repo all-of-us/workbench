69e47d4f0f48736ffada74d91992b16a8bdc7778
Joel Thibault <thibault@broad.dev>
2023-03-17 15:29:44 -0400

    BaseEnvironmentPanel

--- e2e/app/sidebar/apps-panel.ts
+++ e2e/app/sidebar/apps-panel.ts
@@ -1,25 +1,11 @@
 import { Page } from 'puppeteer';
 import { SideBarLink } from 'app/text-labels';
-import BaseSidebar from './base-sidebar';
-import { logger } from 'libs/logger';
+import BaseEnvironmentPanel from './base-environment-panel';
 
 const defaultXpath = '//*[@data-test-id="apps-panel"]';
 
-export default class AppsPanel extends BaseSidebar {
-  constructor(page: Page, xpath: string = defaultXpath) {
-    super(page);
-    super.setXpath(`${super.getXpath()}${xpath}`);
-  }
-
-  // todo: combine with other sidebar panels?
-  async open(): Promise<void> {
-    const isOpen = await this.isVisible();
-    if (isOpen) {
-      return;
-    }
-    await this.clickIcon(SideBarLink.UserApps);
-
-    await this.page.waitForXPath(`${this.getXpath()}//h3`, { visible: true });
-    logger.info(`Opened "${await this.getTitle()}" sidebar`);
+export default class AppsPanel extends BaseEnvironmentPanel {
+  constructor(page: Page) {
+    super(page, defaultXpath, SideBarLink.UserApps);
   }
 }
--- e2e/app/sidebar/cromwell-configuration-panel.ts
+++ e2e/app/sidebar/cromwell-configuration-panel.ts
@@ -1,25 +1,11 @@
-import BaseSidebar from './base-sidebar';
 import { Page } from 'puppeteer';
 import { SideBarLink } from 'app/text-labels';
-import { logger } from 'libs/logger';
+import BaseEnvironmentPanel from './base-environment-panel';
 
 const defaultXpath = '//*[@id="cromwell-configuration-panel"]';
 
-export default class CromwellConfigurationPanel extends BaseSidebar {
-  constructor(page: Page, xpath: string = defaultXpath) {
-    super(page);
-    super.setXpath(`${super.getXpath()}${xpath}`);
-  }
-
-  // todo: combine with other sidebar panels?
-  async open(): Promise<void> {
-    const isOpen = await this.isVisible();
-    if (isOpen) {
-      return;
-    }
-    await this.clickIcon(SideBarLink.CromwellConfiguration);
-
-    await this.page.waitForXPath(`${this.getXpath()}//h3`, { visible: true });
-    logger.info(`Opened "${await this.getTitle()}" sidebar`);
+export default class CromwellConfigurationPanel extends BaseEnvironmentPanel {
+  constructor(page: Page) {
+    super(page, defaultXpath, SideBarLink.CromwellConfiguration);
   }
 }
