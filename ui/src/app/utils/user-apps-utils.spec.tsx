3988300669dd9abb17759cc36d3b8957cd7d8e7a
Eric Rollins <ericvanrollinsii@gmail.com>
2023-03-22 16:22:11 -0400

    Added shell for desired tests.

---
+++ ui/src/app/utils/user-apps-utils.spec.tsx
@@ -0,0 +1,11 @@
+describe('User Apps Helper functions', () => {
+  it('Create User App without an existing update process', async () => {});
+
+  it('Create User App with an existing update process', async () => {});
+
+  it('Update User Apps with an existing update process', async () => {});
+
+  it('Update User Apps that does not require a subsequent update', async () => {});
+
+  it('Update User Apps that requires a subsequent update', async () => {});
+});
