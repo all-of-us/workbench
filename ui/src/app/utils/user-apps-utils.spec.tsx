022c64882e6efbe932076936fa6ec48c0426e758
Eric Rollins <ericvanrollinsii@gmail.com>
2023-03-23 11:29:40 -0400

    Added tests for creating and getting user apps.

--- ui/src/app/utils/user-apps-utils.spec.tsx
+++ ui/src/app/utils/user-apps-utils.spec.tsx
@@ -1,11 +1,101 @@
+import {
+  AppsApi,
+  AppStatus,
+  AppType,
+  CreateAppRequest,
+  DiskType,
+  ListAppsResponse,
+  RuntimeStatus,
+} from 'generated/fetch';
+
+import { appsApi, registerApiClient } from 'app/services/swagger-fetch-clients';
+
+import { AppsApiStub } from 'testing/stubs/apps-api-stub';
+
+import { userAppsStore } from './stores';
+import * as userAppsUtils from './user-apps-utils';
+
+const fakeCromwellConfig: CreateAppRequest = {
+  appType: AppType.CROMWELL,
+  kubernetesRuntimeConfig: {
+    numNodes: 1,
+    machineType: 'Fake Machine Name',
+    autoscalingEnabled: true,
+  },
+  persistentDiskRequest: {
+    size: 50000,
+    diskType: DiskType.Ssd,
+  },
+};
+
 describe('User Apps Helper functions', () => {
-  it('Create User App without an existing update process', async () => {});
+  let appsApiStub: AppsApiStub;
+  beforeEach(async () => {
+    appsApiStub = new AppsApiStub();
+    registerApiClient(AppsApi, appsApiStub);
+  });
 
-  it('Create User App with an existing update process', async () => {});
+  afterEach(async () => {
+    jest.clearAllMocks();
+  });
 
-  it('Update User Apps with an existing update process', async () => {});
+  it('Create User App without an existing update process', async () => {
+    jest
+      .spyOn(appsApi(), 'createApp')
+      .mockImplementationOnce(() => Promise.resolve({}));
+    const spyListAppsAPI = jest.spyOn(appsApi(), 'listAppsInWorkspace');
+    await userAppsUtils.createUserApp('fakeNameSpace', fakeCromwellConfig);
+    expect(spyListAppsAPI).toHaveBeenCalledTimes(1);
+  });
 
-  it('Update User Apps that does not require a subsequent update', async () => {});
+  it('Create User App with an existing update process', async () => {
+    userAppsStore.set({ updating: true });
+    jest
+      .spyOn(appsApi(), 'createApp')
+      .mockImplementationOnce(() => Promise.resolve({}));
+    const spyListAppsAPI = jest.spyOn(appsApi(), 'listAppsInWorkspace');
+    await userAppsUtils.createUserApp('fakeNameSpace', fakeCromwellConfig);
+    expect(spyListAppsAPI).toHaveBeenCalledTimes(0);
+  });
 
-  it('Update User Apps that requires a subsequent update', async () => {});
+  it('Update User Apps with an existing update process', async () => {
+    userAppsStore.set({ updating: true });
+    const spyListAppsAPI = jest.spyOn(appsApi(), 'listAppsInWorkspace');
+    await userAppsUtils.getUserApps('fakeNameSpace');
+    expect(spyListAppsAPI).toHaveBeenCalledTimes(0);
+  });
+
+  it('Update User Apps that does not require a subsequent update', async () => {
+    const spyListAppsAPI = jest
+      .spyOn(appsApi(), 'listAppsInWorkspace')
+      .mockImplementation(
+        () =>
+          new Promise<ListAppsResponse>(() => [
+            { status: RuntimeStatus.Running, appType: AppType.CROMWELL },
+          ])
+      );
+    await userAppsUtils.getUserApps('fakeNameSpace');
+    expect(spyListAppsAPI).toHaveBeenCalledTimes(1);
+  });
+
+  // Waiting just over one polling interval
+  jest.setTimeout(11 * 1000);
+  it('Update User Apps that requires a subsequent update', async () => {
+    const spyListAppsAPI = jest
+      .spyOn(appsApi(), 'listAppsInWorkspace')
+      .mockImplementationOnce(() =>
+        Promise.resolve([
+          { status: AppStatus.STARTING, appType: AppType.CROMWELL },
+        ])
+      )
+      .mockImplementationOnce(() =>
+        Promise.resolve([
+          { status: AppStatus.RUNNING, appType: AppType.CROMWELL },
+        ])
+      );
+    await userAppsUtils.getUserApps('fakeNameSpace');
+
+    await new Promise((resolve) => setTimeout(resolve, 10 * 1000));
+    expect(spyListAppsAPI).toHaveBeenCalledTimes(2);
+  });
 });
