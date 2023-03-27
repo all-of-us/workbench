fef9d61bd7d66616fd02f997b808327a8d14193a
Eric Rollins <erollins@broadinstitute.org>
2023-03-27 09:47:52 -0400

    [RW-9608][risk=no] Broke up status icon logic to work with both runtimes and apps. (#7512)

---
+++ ui/src/app/components/app-status-icon.tsx
@@ -0,0 +1,46 @@
+import * as React from 'react';
+import { CSSProperties } from 'react';
+
+import { AppStatus } from 'generated/fetch';
+
+import { FlexRow } from './flex';
+import {
+  ErrorIcon,
+  RunningIcon,
+  StoppedIcon,
+  StoppingIcon,
+  SuspendedIcon,
+  UpdatingIcon,
+} from './status-icon';
+
+export const AppStatusIcon = (props: {
+  style: CSSProperties;
+  userSuspended: boolean;
+  appStatus: AppStatus;
+}) => {
+  const { appStatus, style, userSuspended } = props;
+
+  return (
+    <FlexRow {...{ style }} data-test-id='runtime-status-icon-container'>
+      {(() => {
+        if (userSuspended) {
+          return <SuspendedIcon />;
+        }
+        switch (appStatus) {
+          case AppStatus.STARTING:
+          case AppStatus.PROVISIONING:
+            return <UpdatingIcon />;
+          case AppStatus.STOPPED:
+            return <StoppedIcon />;
+          case AppStatus.RUNNING:
+            return <RunningIcon />;
+          case AppStatus.STOPPING:
+          case AppStatus.DELETING:
+            return <StoppingIcon />;
+          case AppStatus.ERROR:
+            return <ErrorIcon />;
+        }
+      })()}
+    </FlexRow>
+  );
+};
