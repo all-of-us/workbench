a54cef6c73a42c3555d5302378615824c93a0be8
David Mohs <dgit@mohsinc.com>
2023-04-07 15:40:33 -0400

    Use Cloud SQL Proxy for Liquibase

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -2282,11 +2282,13 @@ Common.register_command({
   :fn => ->(*args) { create_or_update_workbench_db_cmd("create-or-update-workbench-db", args) }
 })
 
-def migrate_database(dry_run = false)
+def migrate_database(gcpProject, dry_run = false)
   common = Common.new
   common.status "Migrating main database..."
-  Dir.chdir("db") do
-    run_inline_or_log(dry_run, %W{../gradlew update -PrunList=main})
+  CloudSqlProxyContext.new(gcpProject).run do
+    Dir.chdir("db") do
+      run_inline_or_log(dry_run, %W{../gradlew update -PrunList=main})
+    end
   end
 end
 
@@ -2402,8 +2404,8 @@ def deploy(cmd_name, args)
   common = Common.new
   common.status "Running database migrations..."
   ENV.update(read_db_vars(gcc))
-  migrate_database(op.opts.dry_run)
-  load_config(ctx.project, op.opts.dry_run)
+  migrate_database(gcc.project, op.opts.dry_run)
+  load_config(gcc.project, op.opts.dry_run)
   cdr_config_file = must_get_env_value(gcc.project, :cdr_config_json)
   update_cdr_config_for_project("config/#{cdr_config_file}", op.opts.dry_run)
 
@@ -2431,7 +2433,7 @@ def run_cloud_migrations(cmd_name, args)
   op.parse.validate
   gcc.validate()
   ENV.update(read_db_vars(gcc))
-  migrate_database
+  migrate_database(gcc.project)
 end
 
 Common.register_command({
@@ -2446,7 +2448,7 @@ def update_cloud_config(cmd_name, args)
   op.parse.validate
   gcc.validate()
   ENV.update(read_db_vars(gcc))
-  load_config(ctx.project)
+  load_config(gcc.project)
 end
 
 Common.register_command({
