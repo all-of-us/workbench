4f021fbd17712372a23f221369c5f7492134be49
Brian Freeman <brian.d.freeman@gmail.com>
2023-04-19 13:32:48 -0500

    [DT-361][risk=no] fixing host and port problem with mysql import (#7603)
    * DT-361 fixing host and port problem with mysql import
    
    * DT-361 fixing host and port problem with mysql import

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -930,6 +930,8 @@ def import_cdr_indices_build_to_cloudsql(cmd_name, *args)
 
   ENV.update(read_db_vars(gcc))
   ENV.update(must_get_env_value(gcc.project, :gae_vars))
+  ENV["DB_HOST"] = "127.0.0.1" # Temporary fix until we decide on how to handle this correctly.
+  ENV["DB_PORT"] = "3307" # Temporary fix until we decide on how to handle this correctly.
 
   common = Common.new
   CloudSqlProxyContext.new(gcc.project).run do
