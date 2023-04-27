e47003fd58ed6f0c27db7a6d25547e7229fd655f
Joel Thibault <thibault@broad.dev>
2023-04-27 12:55:47 -0400

    rm appsLabel

--- ui/src/app/pages/appAnalysis/app-files-list.tsx
+++ ui/src/app/pages/appAnalysis/app-files-list.tsx
@@ -5,7 +5,6 @@ import { FadeBox } from 'app/components/containers';
 import { FlexColumn, FlexRow } from 'app/components/flex';
 import { ListPageHeader } from 'app/components/headers';
 import { WithSpinnerOverlayProps } from 'app/components/with-spinner-overlay';
-import colors from 'app/styles/colors';
 import { reactStyles, withCurrentWorkspace } from 'app/utils';
 import { WorkspaceData } from 'app/utils/workspace-data';
 
@@ -16,13 +15,6 @@ const styles = reactStyles({
     margin: 'auto',
     marginTop: '1.5rem',
     width: '95.7%',
-  },
-  appsLabel: {
-    color: colors.primary,
-    fontWeight: 600,
-    fontSize: '14px',
-    lineHeight: '24px',
-    paddingBottom: '0.75rem',
   },
 });
 
