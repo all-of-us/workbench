7f3cc760d2525a40fd448e32c3635b5710609675
Joel Thibault <thibault@broad.dev>
2023-05-01 13:20:45 -0400

    add notebooklist

--- ui/src/app/pages/appAnalysis/app-files-list.tsx
+++ ui/src/app/pages/appAnalysis/app-files-list.tsx
@@ -1,11 +1,16 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
+
+import { ResourceType, WorkspaceResource } from 'generated/fetch';
 
 import { FadeBox } from 'app/components/containers';
 import { FlexColumn, FlexRow } from 'app/components/flex';
 import { ListPageHeader } from 'app/components/headers';
+import { ResourceList } from 'app/components/resource-list';
 import { WithSpinnerOverlayProps } from 'app/components/with-spinner-overlay';
+import { getExistingNotebooks } from 'app/pages/analysis/util';
 import { reactStyles, withCurrentWorkspace } from 'app/utils';
+import { convertToResource } from 'app/utils/resources';
 import { WorkspaceData } from 'app/utils/workspace-data';
 
 import { AppSelector } from './app-selector';
@@ -24,8 +29,24 @@ interface AppFilesListProps extends WithSpinnerOverlayProps {
 export const AppFilesList = withCurrentWorkspace()(
   (props: AppFilesListProps) => {
     const { workspace } = props;
+    const [notebookResources, setNotebookResources] =
+      useState<WorkspaceResource[]>();
 
-    useEffect(() => props.hideSpinner(), []);
+    const loadNotebooks = async () =>
+      getExistingNotebooks(workspace)
+        .then((notebookList) =>
+          notebookList.map((notebook) =>
+            convertToResource(notebook, ResourceType.NOTEBOOK, workspace)
+          )
+        )
+        .then(setNotebookResources);
+
+    useEffect(() => {
+      props.hideSpinner();
+      if (workspace) {
+        loadNotebooks();
+      }
+    }, []);
 
     return (
       <FadeBox style={styles.fadeBox}>
@@ -35,6 +56,13 @@ export const AppFilesList = withCurrentWorkspace()(
               Your Analyses
             </ListPageHeader>
             <AppSelector {...{ workspace }} />
+            {workspace && notebookResources && (
+              <ResourceList
+                workspaces={[workspace]}
+                workspaceResources={notebookResources}
+                onUpdate={() => loadNotebooks()}
+              />
+            )}
           </FlexRow>
         </FlexColumn>
       </FadeBox>
