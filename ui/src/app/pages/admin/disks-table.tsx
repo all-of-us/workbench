09b8765b73a0b202f7900f0655d841a05300a176
Eric Rollins <ericvanrollinsii@gmail.com>
2023-05-22 14:36:57 -0400

    Added environment type.

--- ui/src/app/pages/admin/disks-table.tsx
+++ ui/src/app/pages/admin/disks-table.tsx
@@ -1,8 +1,10 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
+import * as fp from 'lodash/fp';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 
+import { UIAppType } from 'app/components/apps-panel/utils';
 import { Button } from 'app/components/buttons';
 import { Spinner } from 'app/components/spinners';
 import { disksAdminApi } from 'app/services/swagger-fetch-clients';
@@ -42,6 +44,12 @@ export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
         body={(disk) => moment(disk.createdDate).format('YYYY-MM-DD HH:MM')}
       />
       <Column field='status' header='Status' />
+      <Column
+        header='Environment Type'
+        body={(disk) =>
+          fp.capitalize(disk.isGceRuntime ? UIAppType.JUPYTER : disk.appType)
+        }
+      />
       <Column field='size' header='Size (GB)' />
       <Column
         body={(disk) => (
