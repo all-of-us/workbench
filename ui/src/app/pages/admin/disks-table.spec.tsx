be2a0426daf234fafa55a460b35d378dd7f400a5
Eric Rollins <ericvanrollinsii@gmail.com>
2023-05-24 11:15:56 -0400

    Added basic test for disks table.

--- ui/src/app/pages/admin/disks-table.spec.tsx
+++ ui/src/app/pages/admin/disks-table.spec.tsx
@@ -1,16 +1,37 @@
 import '@testing-library/jest-dom';
 
-import { Disk, DiskStatus, DiskType } from 'generated/fetch';
+import * as fp from 'lodash/fp';
+
+import {
+  AppType,
+  Disk,
+  DiskStatus,
+  DiskType,
+  ListDisksResponse,
+} from 'generated/fetch';
 
 import { screen } from '@testing-library/dom';
-import { render, waitFor } from '@testing-library/react';
+import { render, waitFor, within } from '@testing-library/react';
 import * as swaggerClients from 'app/services/swagger-fetch-clients';
+import moment from 'moment';
 
 import { DisksTable } from './disks-table';
 
 jest.mock('app/services/swagger-fetch-clients');
 
-test('loads and displays greeting', async () => {
+const convertDate = (originalDate: String) => {
+  const date = originalDate.substring(0, 10);
+  const hour =
+    parseInt(originalDate.substring(11, 13), 10) + moment().utcOffset() / 60;
+  const minutes = originalDate.substring(14, 16);
+  return `${date} ${hour}:${minutes}`;
+};
+
+const getEnvironmentType = (isGceRuntime: boolean, appType: AppType) => {
+  return isGceRuntime ? 'Jupyter' : fp.capitalize(appType.toString());
+};
+
+test('loads and displays table', async () => {
   const mockdisksAdminApi = jest.spyOn(swaggerClients, 'disksAdminApi');
 
   const mockDisk: Disk = {
@@ -24,14 +45,31 @@ test('loads and displays greeting', async () => {
     creator: '"evrii@fake-research-aou.org"',
     createdDate: '2023-05-22T18:55:10.108838Z',
   };
+  const mockDisks: ListDisksResponse = [mockDisk];
   // @ts-ignore: Expects full implementation which includes a protected property(configuration) which is hard to mock
   mockdisksAdminApi.mockImplementation(() => ({
-    listDisksInWorkspace: () => Promise.resolve([mockDisk]),
+    listDisksInWorkspace: () => Promise.resolve(mockDisks),
   }));
   render(<DisksTable sourceWorkspaceNamespace='123' />);
   await waitFor(() => {
     expect(screen.queryByTestId('disks spinner')).not.toBeInTheDocument();
   });
-  // expect(DataTable).toBeInTheDocument();
-  // screen.getByText(/disks/i);
+
+  mockDisks.forEach((disk) => {
+    const row = screen.getByText(disk.name).closest('tr');
+    const rowScope = within(row);
+    expect(rowScope.getByText(disk.name)).toBeInTheDocument();
+    expect(rowScope.getByText(disk.creator)).toBeInTheDocument();
+    expect(
+      rowScope.getByText(convertDate(disk.createdDate))
+    ).toBeInTheDocument();
+    expect(rowScope.getByText(disk.status)).toBeInTheDocument();
+    expect(
+      rowScope.getByText(getEnvironmentType(disk.isGceRuntime, disk.appType))
+    ).toBeInTheDocument();
+    expect(rowScope.getByText(disk.size)).toBeInTheDocument();
+    expect(
+      rowScope.getByText('Delete').closest('div[role="button"]')
+    ).not.toBeDisabled();
+  });
 });
