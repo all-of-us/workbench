d3215a9ccf14298e6ada4bfabc9b299cfcd979d2
Eric Rollins <ericvanrollinsii@gmail.com>
2023-06-01 16:07:19 -0400

    First pass at filtering based on environment type.

--- ui/src/app/pages/admin/disks-table.tsx
+++ ui/src/app/pages/admin/disks-table.tsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from 'react';
 import * as fp from 'lodash/fp';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
+import { MultiSelect } from 'primereact/multiselect';
 
-import { Disk } from 'generated/fetch';
+import { AppType, Disk } from 'generated/fetch';
 
 import { UIAppType } from 'app/components/apps-panel/utils';
 import { Button } from 'app/components/buttons';
@@ -17,10 +18,30 @@ interface Props {
   sourceWorkspaceNamespace?: string;
 }
 
+const validEnvironmentTypes = [
+  ...Object.values(AppType),
+  UIAppType.JUPYTER,
+].map(fp.capitalize);
+
 export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
   const [loading, setLoading] = useState(true);
   const [deleting, setDeleting] = useState(false);
   const [disks, setDisks] = useState([]);
+  const [visibleEnvironmentTypes, setVisibleEnvironmentTypes] = useState([]);
+  const [filteredDisks, setFilteredDisks] = useState([]);
+
+  const filterByEnvironmentType = fp.curry((currentFilters, disk) => {
+    return (
+      currentFilters.length === 0 ||
+      currentFilters.includes(fp.capitalize(disk.appType)) ||
+      (currentFilters.includes(UIAppType.JUPYTER) && disk.isGceRuntime)
+    );
+  });
+
+  const applyFilters = () => {
+    const x = disks.filter(filterByEnvironmentType(visibleEnvironmentTypes));
+    setFilteredDisks(x);
+  };
 
   const refreshDisks = () => {
     setLoading(true);
@@ -29,6 +50,9 @@ export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
       .then(setDisks)
       .finally(() => setLoading(false));
   };
+
+  // Update filters whenever the underlying disks or any filters change
+  useEffect(() => applyFilters(), [disks, visibleEnvironmentTypes]);
 
   useEffect(() => refreshDisks(), []);
 
@@ -46,6 +70,21 @@ export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
       (disk.isGceRuntime ? UIAppType.JUPYTER : disk.appType).toString()
     );
 
+  const environmentTypeFilterTemplate = () => {
+    return (
+      <MultiSelect
+        value={visibleEnvironmentTypes}
+        options={validEnvironmentTypes}
+        onChange={(e) => {
+          setVisibleEnvironmentTypes(e.value ?? []);
+        }}
+        showClear={true}
+        showSelectAll={false}
+        panelHeaderTemplate={<></>}
+      />
+    );
+  };
+
   const compareByEnvironmentType = fp.curry(
     (sortOrder, firstDisk, secondDisk) => {
       console.log(sortOrder);
@@ -59,7 +98,6 @@ export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
   );
 
   const onSortEnvironmentType = (sortEvent) => {
-    console.log('What is event? ', sortEvent);
     // @ts-ignore
     return disks.sort(compareByEnvironmentType(sortEvent.order));
   };
@@ -67,7 +105,7 @@ export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
   return loading ? (
     <Spinner title='disks loading spinner' />
   ) : (
-    <DataTable value={disks} emptyMessage='No disks found'>
+    <DataTable value={filteredDisks} emptyMessage='No disks found'>
       <Column
         body={(disk) => (
           <Button disabled={deleting} onClick={() => onClickDelete(disk)}>
@@ -89,6 +127,14 @@ export const DisksTable = ({ sourceWorkspaceNamespace }: Props) => {
         body={getEnvironmentType}
         sortable={true}
         sortFunction={onSortEnvironmentType}
+        filter
+        showApplyButton={false}
+        showClearButton={false}
+        showFilterMatchModes={false}
+        showFilterOperator={false}
+        showFilterMenuOptions={false}
+        filterElement={environmentTypeFilterTemplate}
+        filterMatchMode='custom'
       />
       <Column field='size' header='Size (GB)' />
     </DataTable>
