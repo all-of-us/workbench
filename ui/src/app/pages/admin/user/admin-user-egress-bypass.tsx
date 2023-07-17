b94d59ec224b3e508efb4b7f2f1b60705ebb6acb
Yonghao Yu <yonghao@google.com>
2023-07-18 06:08:24 +0800

    fix test

--- ui/src/app/pages/admin/user/admin-user-egress-bypass.tsx
+++ ui/src/app/pages/admin/user/admin-user-egress-bypass.tsx
@@ -51,7 +51,7 @@ export const AdminUserEgressBypass = (props: Props) => {
     'Error occurred while creating egress bypass request'
   ) : (
     <div>
-      Required to bypass Request for large downloads:
+      Required to enable large file downloads:
       <ul>
         {invalidReason && (
           <li>
