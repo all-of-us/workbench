e8057abae009af81abfcdaeaa51f4f2968a8f000
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2023-07-20 15:30:27 -0400

    [risk=low][RW-8831] Egress bypass tweaks (#7808)
    * "in milliseconds" is duplicated in the Swagger YAML
    * new maybeToSingleDate() conversion

--- ui/src/app/pages/admin/user/admin-user-egress-bypass.tsx
+++ ui/src/app/pages/admin/user/admin-user-egress-bypass.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
-import { Calendar, CalendarValueType } from 'primereact/calendar';
+import { Calendar } from 'primereact/calendar';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 
@@ -15,7 +15,7 @@ import { TextAreaWithLengthValidationMessage } from 'app/components/inputs';
 import { TooltipTrigger } from 'app/components/popups';
 import { userAdminApi } from 'app/services/swagger-fetch-clients';
 import colors from 'app/styles/colors';
-import { formatDate, isDateValid } from 'app/utils/dates';
+import { formatDate, isDateValid, maybeToSingleDate } from 'app/utils/dates';
 const MIN_BYPASS_DESCRIPTION = 10;
 const MAX_BYPASS_DESCRIPTION = 4000;
 
@@ -24,20 +24,23 @@ interface Props {
 }
 
 export const AdminUserEgressBypass = (props: Props) => {
-  const [startTime, setStartTime] = useState<CalendarValueType>(null);
+  const [startTime, setStartTime] = useState(new Date());
   const [bypassDescription, setBypassDescription] = useState('');
   const [apiError, setApiError] = useState(false);
   const [bypassWindowsList, setBypassWindowsList] = useState<
     EgressBypassWindow[]
   >([]);
-  const [reload, setReload] = useState(true);
 
-  useEffect(() => {
+  const loadEgressWindows = () => {
     const { userId } = props;
     userAdminApi()
       .listEgressBypassWindows(userId)
       .then((res) => setBypassWindowsList(res.bypassWindows));
-  }, [reload]);
+  };
+
+  useEffect(() => {
+    loadEgressWindows();
+  }, []);
 
   const invalidReason =
     !bypassDescription ||
@@ -45,7 +48,7 @@ export const AdminUserEgressBypass = (props: Props) => {
     bypassDescription.length > MAX_BYPASS_DESCRIPTION;
 
   let egressBypassButtonDisabled =
-    apiError || invalidReason || !isDateValid(startTime as Date);
+    apiError || invalidReason || !isDateValid(startTime);
 
   const getToolTipContent = apiError ? (
     'Error occurred while creating egress bypass request'
@@ -59,7 +62,7 @@ export const AdminUserEgressBypass = (props: Props) => {
             {MAX_BYPASS_DESCRIPTION})
           </li>
         )}
-        {!isDateValid(startTime as Date) && (
+        {!isDateValid(startTime) && (
           <li>Valid Request Date (in YYYY-MM-DD Format)</li>
         )}
       </ul>
@@ -68,14 +71,12 @@ export const AdminUserEgressBypass = (props: Props) => {
 
   const onCreateBypassRequest = () => {
     const { userId } = props;
-    setReload(false);
     egressBypassButtonDisabled = true;
     const createEgressBypassWindowRequest: CreateEgressBypassWindowRequest = {
-      startTime: (startTime as Date).valueOf(),
+      startTime: startTime.valueOf(),
       byPassDescription: bypassDescription,
     };
     setBypassDescription('');
-    setStartTime(null);
 
     userAdminApi()
       .createEgressBypassWindow(userId, createEgressBypassWindowRequest)
@@ -83,7 +84,7 @@ export const AdminUserEgressBypass = (props: Props) => {
         setApiError(true);
       })
       .finally(() => {
-        setReload(true);
+        loadEgressWindows();
       });
   };
 
@@ -147,7 +148,7 @@ export const AdminUserEgressBypass = (props: Props) => {
             minDate={new Date()}
             onChange={(e) => {
               setApiError(false);
-              setStartTime(e.value);
+              setStartTime(maybeToSingleDate(e.value));
             }}
           />
         </FlexRow>
