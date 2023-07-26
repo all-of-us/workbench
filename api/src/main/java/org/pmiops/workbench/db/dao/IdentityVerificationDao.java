803ed8d4035d0b214c0e80568597137a91bce3a2
Eric Rollins <erollins@broadinstitute.org>
2023-07-26 14:42:33 -0400

    First pass at tracking identity system.

---
+++ api/src/main/java/org/pmiops/workbench/db/dao/IdentityVerificationDao.java
@@ -0,0 +1,11 @@
+package org.pmiops.workbench.db.dao;
+
+import java.util.Optional;
+import org.pmiops.workbench.db.model.DbIdentityVerification;
+import org.pmiops.workbench.db.model.DbUser;
+import org.springframework.data.repository.CrudRepository;
+
+public interface IdentityVerificationDao extends CrudRepository<DbIdentityVerification, Long> {
+  Optional<DbIdentityVerification> getByUser(DbUser user);
+
+}
---
+++ api/src/main/java/org/pmiops/workbench/identityVerification/IdentityVerificationService.java
@@ -0,0 +1,37 @@
+package org.pmiops.workbench.identityVerification;
+
+import org.pmiops.workbench.db.dao.IdentityVerificationDao;
+import org.pmiops.workbench.db.dao.UserService;
+import org.pmiops.workbench.db.model.DbIdentityVerification;
+import org.pmiops.workbench.db.model.DbIdentityVerification.DbIdentityVerificationSystem;
+import org.pmiops.workbench.db.model.DbUser;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+
+@Service
+public class IdentityVerificationService {
+
+  private final IdentityVerificationDao identityVerificationDao;
+  private final UserService userService;
+
+  @Autowired
+  public IdentityVerificationService(
+      IdentityVerificationDao identityVerificationDao,
+      UserService userService) {
+    this.identityVerificationDao = identityVerificationDao;
+    this.userService = userService;
+  }
+
+public void updateIdentityVerificationSystem(String userName, DbIdentityVerificationSystem identityVerificationSystem){
+  DbIdentityVerification identityVerification = retrieveIdentityVerificationOrCreate(    userService.getByUsernameOrThrow(userName));
+  identityVerification.setIdentityVerificationSystem(identityVerificationSystem);
+}
+
+  private DbIdentityVerification retrieveIdentityVerificationOrCreate(
+      DbUser user) {
+    return identityVerificationDao
+        .getByUser(user)
+        .orElse(new DbIdentityVerification().setUser(user));
+  }
+
+}
