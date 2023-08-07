1b7b3fa61591e84f0c346ecfdccddbcd8901b6ff
Will Dolbeer <WillDolbeer@VICTRs-MBP-2.attlocal.net>
2023-08-07 14:36:27 -0400

    DT-298 implement navigation between workbench and tanagra iframe

--- ui/src/app/pages/tanagra-dev/tanagra-dev.tsx
+++ ui/src/app/pages/tanagra-dev/tanagra-dev.tsx
@@ -1,30 +1,83 @@
 import * as React from 'react';
 
+import { Button } from 'app/components/buttons';
+import { SpinnerOverlay } from 'app/components/spinners';
 import { withSpinnerOverlay } from 'app/components/with-spinner-overlay';
 
-const { useEffect } = React;
+const { useCallback, useEffect, useState } = React;
+
+const tanagraUrl = 'http://localhost:3000';
+
+export function useExitActionListener(callback: () => void) {
+  const listener = useCallback(
+    (event: MessageEvent) => {
+      if (
+        event.origin !== tanagraUrl ||
+        typeof event.data !== 'object' ||
+        event.data.message !== 'CLOSE'
+      ) {
+        return;
+      }
+      callback();
+    },
+    [callback]
+  );
+
+  useEffect(() => {
+    window.addEventListener('message', listener);
+    return () => {
+      window.removeEventListener('message', listener);
+    };
+  }, [listener]);
+}
 
 export const TanagraDev = withSpinnerOverlay()(({ hideSpinner }) => {
+  const [loadingIframe, setLoadingIframe] = useState(false);
+  const [showIframe, setShowIframe] = useState(false);
   useEffect(() => {
     hideSpinner();
   }, []);
 
-  return (
+  useExitActionListener(() => {
+    setShowIframe(false);
+  });
+
+  // Temporary variable to hardcode existing study ids until we have a link between workspaces and studies
+  const studyId =
+    process.env.REACT_APP_ENVIRONMENT === 'local' ? 'tqmXfZ4qzu' : 'UslvvbIYxk';
+
+  return showIframe ? (
     <div
       style={{
-        height: '25rem',
-        margin: '2rem auto',
-        width: '98%',
+        height: '95vh',
       }}
     >
+      {loadingIframe && <SpinnerOverlay />}
       <iframe
+        onLoad={() => setLoadingIframe(false)}
         style={{
           border: 0,
           height: '100%',
           width: '100%',
         }}
-        src='http://localhost:3000/#/'
+        src={`${tanagraUrl}/#/tanagra/underlays/SR2022Q4R6/studies/${studyId}/export`}
       />
+    </div>
+  ) : (
+    <div style={{ padding: '2rem' }}>
+      <div style={{ marginBottom: '1rem' }}>
+        This is a temporary interface to demonstrate the transition from the
+        Workbench to Tanagra and back. This transition will eventually take
+        place on the Data tab page.
+      </div>
+      <Button
+        onClick={() => {
+          setLoadingIframe(true);
+          setShowIframe(true);
+        }}
+      >
+        Show Tanagra iframe
+      </Button>
     </div>
   );
 });
