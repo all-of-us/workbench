fee8c9cf7eaf81c777716e73bc2c003b82422ac1
Will Dolbeer <40036095+dolbeew@users.noreply.github.com>
2023-08-08 15:52:47 -0500

    [DT-298][risk=no] Update style, navigation for Tanagra iframe (#7867)
    * DT-298 implement navigation between workbench and tanagra iframe

--- ui/src/app/pages/tanagra-dev/tanagra-dev.tsx
+++ ui/src/app/pages/tanagra-dev/tanagra-dev.tsx
@@ -1,30 +1,76 @@
 import * as React from 'react';
+import * as fp from 'lodash/fp';
 
 import { withSpinnerOverlay } from 'app/components/with-spinner-overlay';
+import { withCdrVersions, withCurrentWorkspace } from 'app/utils';
+import { findCdrVersion } from 'app/utils/cdr-versions';
+import { useNavigation } from 'app/utils/navigation';
+import { serverConfigStore } from 'app/utils/stores';
 
-const { useEffect } = React;
+const { useCallback, useEffect } = React;
 
-export const TanagraDev = withSpinnerOverlay()(({ hideSpinner }) => {
-  useEffect(() => {
-    hideSpinner();
-  }, []);
-
-  return (
-    <div
-      style={{
-        height: '25rem',
-        margin: '2rem auto',
-        width: '98%',
-      }}
-    >
-      <iframe
-        style={{
-          border: 0,
-          height: '100%',
-          width: '100%',
-        }}
-        src='http://localhost:3000/#/'
-      />
-    </div>
+export function useExitActionListener(callback: () => void) {
+  const listener = useCallback(
+    (event: MessageEvent) => {
+      const tanagraUrl = serverConfigStore.get().config.tanagraBaseUrl;
+      if (
+        event.origin !== tanagraUrl ||
+        typeof event.data !== 'object' ||
+        event.data.message !== 'CLOSE'
+      ) {
+        return;
+      }
+      callback();
+    },
+    [callback]
   );
-});
+
+  useEffect(() => {
+    window.addEventListener('message', listener);
+    return () => {
+      window.removeEventListener('message', listener);
+    };
+  }, [listener]);
+}
+
+export const TanagraDev = fp.flow(
+  withCdrVersions(),
+  withCurrentWorkspace(),
+  withSpinnerOverlay()
+)(
+  ({
+    cdrVersionTiersResponse,
+    hideSpinner,
+    workspace: { cdrVersionId, id, namespace },
+  }) => {
+    const [navigate] = useNavigation();
+
+    useExitActionListener(() => {
+      // Navigate to Data tab when exiting Tanagra iframe
+      navigate(['workspaces', namespace, id, 'data']);
+    });
+
+    const { bigqueryDataset } = findCdrVersion(
+      cdrVersionId,
+      cdrVersionTiersResponse
+    );
+    const tanagraUrl = serverConfigStore.get().config.tanagraBaseUrl;
+    return (
+      <div
+        style={{
+          height: '95vh',
+        }}
+      >
+        <iframe
+          onLoad={() => hideSpinner()}
+          style={{
+            border: 0,
+            height: '100%',
+            width: '100%',
+          }}
+          src={`${tanagraUrl}/#/tanagra/underlays/${bigqueryDataset}/studies/${namespace}/export`}
+        />
+      </div>
+    );
+  }
+);
