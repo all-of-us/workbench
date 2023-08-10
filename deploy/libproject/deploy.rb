bce135fac80559fd18af7afef999af3481e4b29f
Neha Saxena <nsaxena@broadinstitute.org>
2023-08-10 12:03:00 -0400

    Adding some console messages

--- deploy/libproject/deploy.rb
+++ deploy/libproject/deploy.rb
@@ -32,7 +32,9 @@ def get_live_gae_version(project, services, validate_version=true)
   end
   common.status services.length()
   if services.length() == 1
-    common.status JSON.parse(versions).select{|v| services.include?(v["service"])}
+    versions =  JSON.parse(versions).select{|v| services.include?(v["service"])}
+    common.status "Neha"
+    common.status "#{versions}"
   end
   actives = JSON.parse(versions).select{|v| v["traffic_split"] == 1.0}
   common.status "#{actives.to_a.join(',')}"
