b818acbec988191dc09ae550266fb16f74dd25b6
Eric Rollins <erollins@broadinstitute.org>
2023-08-10 16:17:18 -0400

    Linting.

--- api/src/main/java/org/pmiops/workbench/ras/RasLinkService.java
+++ api/src/main/java/org/pmiops/workbench/ras/RasLinkService.java
@@ -103,8 +103,8 @@ import org.springframework.stereotype.Service;
  *
  * <p>Step4: Use step3's RAS username to update AoU database by {@link *
  * UserService#updateRasLinkLoginGovStatus(String)} or {@link *
- * UserService#updateIdentityStatus(String)} (based on which service was used). Then return it as
- * * user profile.
+ * UserService#updateIdentityStatus(String)} (based on which service was used). Then return it as *
+ * user profile.
  *
  * <p>TODO(yonghao): Fow now we return {@link ForbiddenException} for all scenarios, determine if we
  * need to differentiate IAL vs Login.gov scenarios, and give that information to UI.
