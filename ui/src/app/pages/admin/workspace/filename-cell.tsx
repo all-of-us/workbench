8890f5db7299838ef9594417233ec6ffb73add53
Joel Thibault <thibault@broad.dev>
2023-08-25 14:14:49 -0400

    filename-cell

---
+++ ui/src/app/pages/admin/workspace/filename-cell.tsx
@@ -0,0 +1,89 @@
+import { FileDetail } from 'generated/fetch';
+
+import { Button } from 'app/components/buttons';
+import { FlexRow } from 'app/components/flex';
+import { TooltipTrigger } from 'app/components/popups';
+import { parseLocation } from 'app/pages/analysis/util';
+import { cond, reactStyles } from 'app/utils';
+import { JUPYTER_FILE_EXT } from 'app/utils/constants';
+import { useNavigation } from 'app/utils/navigation';
+
+import { formatMB } from './file-table';
+
+const MAX_NOTEBOOK_READ_SIZE_BYTES = 5 * 1000 * 1000; // see NotebooksServiceImpl
+const NOTEBOOKS_DIRECTORY = 'notebooks';
+
+const styles = reactStyles({
+  previewButton: {
+    marginLeft: '20px',
+    height: '1.5rem',
+  },
+});
+
+interface Props {
+  file: FileDetail;
+  storageBucketPath: string;
+  workspaceNamespace: string;
+  accessReason: string;
+}
+export const FilenameCell = (props: Props) => {
+  const [navigate] = useNavigation();
+  const { file, storageBucketPath, workspaceNamespace, accessReason } = props;
+  const filename = file.name.trim();
+
+  const fileTooLarge = () => (
+    <FlexRow>
+      <span>{filename}</span>
+      <TooltipTrigger
+        content={`Files larger than ${formatMB(
+          MAX_NOTEBOOK_READ_SIZE_BYTES
+        )} MB are too large to preview`}
+      >
+        <Button style={styles.previewButton} disabled={true}>
+          Preview
+        </Button>
+      </TooltipTrigger>
+    </FlexRow>
+  );
+
+  const navigateToPreview = () =>
+    navigate(
+      ['admin', 'workspaces', workspaceNamespace, encodeURIComponent(filename)],
+      { queryParams: { accessReason: accessReason } }
+    );
+
+  const fileWithPreviewButton = () => (
+    <FlexRow>
+      <span>{filename}</span>
+      <TooltipTrigger
+        content='Please enter an access reason below'
+        disabled={accessReason?.trim()}
+      >
+        <Button
+          style={styles.previewButton}
+          disabled={!accessReason?.trim()}
+          onClick={navigateToPreview}
+        >
+          Preview
+        </Button>
+      </TooltipTrigger>
+    </FlexRow>
+  );
+
+  // remove first check after RW-5626
+  const isNotebook =
+    NOTEBOOKS_DIRECTORY === parseLocation(file, storageBucketPath) &&
+    filename.endsWith(JUPYTER_FILE_EXT);
+
+  const isTooLargeNotebook =
+    isNotebook && file.sizeInBytes > MAX_NOTEBOOK_READ_SIZE_BYTES;
+
+  // if (tooLarge()) fileTooLarge();
+  // else if (isNotebook()) fileWithPreviewButton();
+  // else filenameSpan();
+  return cond(
+    [isTooLargeNotebook, fileTooLarge],
+    [isNotebook, fileWithPreviewButton],
+    () => <span>{filename}</span>
+  );
+};
