2ff6e500fc893a4abe987cb744fac9babcf35fd1
Neha Saxena <nsaxena@broadinstitute.org>
2023-08-28 15:54:36 -0400

    Check if this works

--- ui/libproject/try.js
+++ ui/libproject/try.js
@@ -1,23 +1,7 @@
-import exec from 'child_process'
 const REPO_BASE_URL = 'https://github.com/all-of-us/workbench';
 
 function checking(text) {
   console.log('in here');
   console.log(text)
   console.log(text.replace(/\(#([0-9]+)\)/g, `([#$1|${REPO_BASE_URL}/pull/$1])`));
-}
-
-
-function example(project) {
-  let gcs_uri = `gs://${project}-credentials/jira-login.json`
-  exec(`gsutil cat ${gcsUri}`, (error, stdout, stderr) => {
-    if (error) {
-    //  console.error(`Error executing command: ${error}`);
-      return;
-    }
-    if (stderr) {
-      //console.error(`Command STDERR: ${stderr}`);
-    }
-    //console.log('Command output:', stdout);
-  });
 }
