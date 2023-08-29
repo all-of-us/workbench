dc1864e60d3ba07b08654650c136b2679ce0fe66
Neha Saxena <nsaxena@broadinstitute.org>
2023-08-29 09:35:29 -0400

    Check if this works

--- ui/libproject/try.js
+++ ui/libproject/try.js
@@ -1,8 +1,24 @@
 function validate_options1(arg) {
   'use strict';
-  // if (!!arg.project || !!arg.version || !!arg.promote) {
-  //   return false;
-  // }
+  return false;
+  if (!!arg.project || !!arg.version || !!arg.promote) {
+    return false;
+  }
   console.log('hey there');
   return true;
 }
+
+const { spawn } = require("child_process");
+const listening = spawn('gcloud', ['app', 'deploy']);
+
+listening.stdout.on("data", data => {
+  console.log(`stdout: ${data}`);
+});
+
+listening.stderr.on("data", data => {
+  console.log(`stderr: ${data}`);
+});
+
+listening.on('error', (error) => {
+  console.log(`error: ${error.message}`);
+});
