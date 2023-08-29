de186c68b802e7e51ef6b6c1474ba254c41838d4
Neha Saxena <nsaxena@broadinstitute.org>
2023-08-29 09:42:35 -0400

    Commenting out gclooud code

--- ui/libproject/try.js
+++ ui/libproject/try.js
@@ -8,17 +8,17 @@ function validate_options1(arg) {
   return true;
 }
 
-const { spawn } = require("child_process");
-const listening = spawn('gcloud', ['app', 'deploy']);
-
-listening.stdout.on("data", data => {
-  console.log(`stdout: ${data}`);
-});
-
-listening.stderr.on("data", data => {
-  console.log(`stderr: ${data}`);
-});
-
-listening.on('error', (error) => {
-  console.log(`error: ${error.message}`);
-});
+// const { spawn } = require("child_process");
+// const listening = spawn('gcloud', ['app', 'deploy']);
+//
+// listening.stdout.on("data", data => {
+//   console.log(`stdout: ${data}`);
+// });
+//
+// listening.stderr.on("data", data => {
+//   console.log(`stderr: ${data}`);
+// });
+//
+// listening.on('error', (error) => {
+//   console.log(`error: ${error.message}`);
+// });
