dfd407ffd98effae2bcd3d974b67e2fee7e37de3
Neha Saxena <nsaxena@broadinstitute.org>
2023-09-05 10:37:15 -0400

    Try printing the option

--- deploy/libproject/deploy.rb
+++ deploy/libproject/deploy.rb
@@ -235,6 +235,7 @@ def deploy(cmd_name, args)
     raise ArgumentError.new("--git-version is required when running within docker")
   end
   common = Common.new
+  common.status "ui script '#{op.opts.script_ui}'"
   common.run_inline %W{gcloud auth activate-service-account -q --key-file #{op.opts.key_file}}
 
   jira_client = nil
