6376c758094f346799782efd7b0d67ba3265975e
Neha Saxena <nsaxena@broadinstitute.org>
2023-09-05 13:59:41 -0400

    Fix import

--- ui/libproject/buildAndDeploy.mjs
+++ ui/libproject/buildAndDeploy.mjs
@@ -1,5 +1,5 @@
 import { execSync} from "child_process";
-import {ServiceAccountContext} from "serviceaccounts.mjs"
+import {ServiceAccountContext} from "../libproject/serviceaccounts.mjs"
 
 const YARN_DEPS = 'yarn deps';
 const CREDENTIALS_COPY = 'gsutil cp gs://all-of-us-workbench-test-credentials/.npmrc ..';
