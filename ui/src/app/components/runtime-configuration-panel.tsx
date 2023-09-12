21d348545dd5b1469cd90011a64010fe5be4f671
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2023-09-12 17:43:31 -0400

    [risk=low][RW-10794] Set analysisConfig in Runtime panel in useEffect() (#7944)

--- ui/src/app/components/runtime-configuration-panel.tsx
+++ ui/src/app/components/runtime-configuration-panel.tsx
@@ -224,6 +224,19 @@ const PanelMain = fp.flow(
     const [analysisConfig, setAnalysisConfig] = useState(
       withAnalysisConfigDefaults(existingAnalysisConfig, gcePersistentDisk)
     );
+
+    // TODO: simplify the state logic here!  At least, try to understand why this is happening.
+    // Somehow, only setting the analysisConfig in the above useState() step means that
+    // a gcePersistentDisk update from the diskStore does not always cause analysisConfig to be updated.
+    // So we must set this explicit dependency trigger instead.
+    useEffect(
+      () =>
+        setAnalysisConfig(
+          withAnalysisConfigDefaults(analysisConfig, gcePersistentDisk)
+        ),
+      [gcePersistentDisk]
+    );
+
     const requestAnalysisConfig = (config: AnalysisConfig) =>
       setRuntimeRequest({
         runtime: fromAnalysisConfig(config),
