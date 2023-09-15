621be2fab53297bf1d22d02b98f42333a825de04
Qi Wang <qi@broadinstitute.org>
2023-09-15 14:11:34 -0400

    revert for release for now

--- ui/libproject/devstart.rb
+++ ui/libproject/devstart.rb
@@ -33,8 +33,8 @@ def build(cmd_name, args)
 
   common = Common.new
 
-  fontawesomeCredsLineFilePath = "gs://all-of-us-workbench-test-credentials/dot-npmrc-fontawesome-creds-line.txt"
-  common.run_inline "gsutil cat #{fontawesomeCredsLineFilePath} >> ~/.npmrc"
+#   fontawesomeCredsLineFilePath = "gs://all-of-us-workbench-test-credentials/dot-npmrc-fontawesome-creds-line.txt"
+  common.run_inline %W{gsutil cp gs://all-of-us-workbench-test-credentials/.npmrc ..}
   common.run_inline %W{yarn install --frozen-lockfile}
   common.run_inline %W{yarn run deps}
 
