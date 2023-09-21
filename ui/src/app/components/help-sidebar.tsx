a9c7cb90e8119f054737cf6140b0a157061da441
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2023-09-21 15:08:50 -0400

    [risk=low][RW-10290] Add Delete Environment links to GKE App Configuration Panels (#7964)

--- ui/src/app/components/help-sidebar.tsx
+++ ui/src/app/components/help-sidebar.tsx
@@ -530,8 +530,8 @@ export const HelpSidebar = fp.flow(
             renderBody: () => (
               <ConfigurationPanel
                 {...{ runtimeConfPanelInitialState }}
-                onClose={() => this.setActiveIcon(null)}
                 appType={UIAppType.JUPYTER}
+                onClose={() => this.setActiveIcon(null)}
               />
             ),
             showFooter: false,
@@ -585,9 +585,9 @@ export const HelpSidebar = fp.flow(
             ),
             renderBody: () => (
               <ConfigurationPanel
+                {...{ gkeAppConfPanelInitialState }}
                 appType={UIAppType.CROMWELL}
                 onClose={() => this.setActiveIcon(null)}
-                gkeAppConfPanelInitialState={gkeAppConfPanelInitialState}
               />
             ),
           };
@@ -617,9 +617,9 @@ export const HelpSidebar = fp.flow(
             ),
             renderBody: () => (
               <ConfigurationPanel
+                {...{ gkeAppConfPanelInitialState }}
                 appType={UIAppType.RSTUDIO}
                 onClose={() => this.setActiveIcon(null)}
-                gkeAppConfPanelInitialState={gkeAppConfPanelInitialState}
               />
             ),
           };
@@ -649,9 +649,9 @@ export const HelpSidebar = fp.flow(
             ),
             renderBody: () => (
               <ConfigurationPanel
+                {...{ gkeAppConfPanelInitialState }}
                 appType={UIAppType.SAS}
                 onClose={() => this.setActiveIcon(null)}
-                gkeAppConfPanelInitialState={gkeAppConfPanelInitialState}
               />
             ),
           };
