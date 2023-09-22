76f8d4f9f7fee7af773186f0c0cd5a871b798d77
Neha Saxena <nsaxena@broadinstitute.org>
2023-09-22 14:19:15 -0400

    Use free icons vs pro

--- ui/src/app/pages/admin/user/admin-user-profile.tsx
+++ ui/src/app/pages/admin/user/admin-user-profile.tsx
@@ -4,7 +4,7 @@ import * as fp from 'lodash/fp';
 import { Column } from 'primereact/column';
 import { DataTable } from 'primereact/datatable';
 import validate from 'validate.js';
-import { faLink } from '@fortawesome/pro-solid-svg-icons';
+import { faLink } from '@fortawesome/free-solid-svg-icons';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 
 import {
