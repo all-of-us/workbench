7001e818f72950f2f738f8aa5b58cb0f3500d80b
Yonghao Yu <yonghao@google.com>
2023-10-10 11:16:51 -0400

    [RW-11090][risk=low] Fix sync user access cron job (#8020)
    * Update cron_base.yaml
    * Update synchronize-user-access-cron.sh

--- api/tools/src/cron/synchronize-user-access-cron.sh
+++ api/tools/src/cron/synchronize-user-access-cron.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
 
 # Expire users who have not completed their compliance modules in over a year
-curl -X GET 'http://localhost:8081/v1/cron/synchronizeUserAccess' \
+curl -X GET 'http://localhost:8081/v1/cron/groupAndPushSynchronizeAccessTasks' \
   --header "X-AppEngine-Cron: true"
