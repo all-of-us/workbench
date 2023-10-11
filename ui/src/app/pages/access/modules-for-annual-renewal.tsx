68893190b39c4f0bd42470514e6929e012c0136e
Peter Lavigne <peterlavigne@google.com>
2023-10-11 16:24:50 -0400

    Remove unnecessary duccSignedVersion prop

--- ui/src/app/pages/access/modules-for-annual-renewal.tsx
+++ ui/src/app/pages/access/modules-for-annual-renewal.tsx
@@ -210,7 +210,6 @@ interface RenewalCardBodyProps {
   textStyle?: CSSProperties;
   radioButtonStyle?: CSSProperties;
   showTimeEstimate?: boolean;
-  duccSignedVersion: number;
   profile: Profile;
 }
 
@@ -220,7 +219,6 @@ export const RenewalCardBody = ({
   textStyle,
   radioButtonStyle,
   showTimeEstimate = false,
-  duccSignedVersion,
   profile,
 }: RenewalCardBodyProps) => {
   const [, navigateByUrl] = useNavigation();
@@ -229,6 +227,8 @@ export const RenewalCardBody = ({
   const [publications, setPublications] = useState<boolean>(null);
   const [trainingRefreshButtonDisabled, setTrainingRefreshButtonDisabled] =
     useState(true);
+
+  const { duccSignedVersion } = profile;
 
   const { AARTitleComponent, renewalTimeEstimate } = getAccessModuleConfig(
     moduleStatus.moduleName
@@ -582,7 +582,6 @@ export const ModulesForAnnualRenewal = (props: RenewalCardProps) => {
               textStyle={{ fontSize: '0.9rem' }}
               radioButtonStyle={{ marginRight: '0.5em' }}
               showTimeEstimate={true}
-              duccSignedVersion={profile.duccSignedVersion}
               profile={profile}
             />
           </FlexColumn>
