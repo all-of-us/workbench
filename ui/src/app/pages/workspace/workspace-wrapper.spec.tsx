8d818738c5354508879be41d1e5c81e67af2fc45
Joel Thibault <thibault@broad.dev>
2023-10-13 15:27:33 -0400

    test wip

--- ui/src/app/pages/workspace/workspace-wrapper.spec.tsx
+++ ui/src/app/pages/workspace/workspace-wrapper.spec.tsx
@@ -7,10 +7,20 @@ import { Route } from 'react-router-dom';
 import { AppsApi, RuntimeApi, WorkspacesApi } from 'generated/fetch';
 
 import { screen } from '@testing-library/dom';
-import { render, waitForElementToBeRemoved } from '@testing-library/react';
+import {
+  render,
+  waitFor,
+  waitForElementToBeRemoved,
+} from '@testing-library/react';
 import { WorkspaceWrapper } from 'app/pages/workspace/workspace-wrapper';
-import { registerApiClient } from 'app/services/swagger-fetch-clients';
-import { currentWorkspaceStore } from 'app/utils/navigation';
+import {
+  registerApiClient,
+  workspacesApi,
+} from 'app/services/swagger-fetch-clients';
+import {
+  currentWorkspaceStore,
+  nextWorkspaceWarmupStore,
+} from 'app/utils/navigation';
 import { cdrVersionStore, serverConfigStore } from 'app/utils/stores';
 
 import defaultServerConfig from 'testing/default-server-config';
@@ -46,22 +56,16 @@ describe(WorkspaceWrapper.name, () => {
     registerApiClient(RuntimeApi, new RuntimeApiStub());
     registerApiClient(AppsApi, new AppsApiStub());
 
-    const workspacesApi = new WorkspacesApiStub();
-    registerApiClient(WorkspacesApi, workspacesApi);
-    workspacesApi.getWorkspace = jest
+    const workspacesApiStub = new WorkspacesApiStub();
+    registerApiClient(WorkspacesApi, workspacesApiStub);
+    workspacesApiStub.getWorkspace = jest
       .fn()
       .mockResolvedValue({ workspace: workspaceData });
-
-    // this captures the behavior of a current bug (RW-11140) with this component:
-    // it doesn't work when the currentWorkspaceStore matches the route being served
-    currentWorkspaceStore.next({
-      ...workspaceDataStub,
-      namespace: 'something else',
-      id: 'some other ID',
-    });
   });
 
-  const createWrapperAndWaitForLoad = async () => {
+  const createWrapperAndWaitForLoad = async (
+    expectingSpinner: boolean = false
+  ) => {
     render(
       <MemoryRouter
         initialEntries={[
@@ -74,12 +78,124 @@ describe(WorkspaceWrapper.name, () => {
       </MemoryRouter>
     );
 
-    await waitForElementToBeRemoved(() =>
-      screen.getByTitle('loading workspaces spinner')
+    if (expectingSpinner) {
+      await waitForElementToBeRemoved(() =>
+        screen.getByTitle('loading workspaces spinner')
+      );
+    }
+
+    await waitFor(() =>
+      expect(screen.queryByText('Mock Workspace Routes')).toBeInTheDocument()
     );
   };
 
+  it('loads the workspace specified by the route when the workspace stores are empty', async () => {
+    currentWorkspaceStore.next(undefined);
+    nextWorkspaceWarmupStore.next(undefined);
+
+    const spy = jest.spyOn(workspacesApi(), 'getWorkspace');
+    await createWrapperAndWaitForLoad();
+    expect(spy).toHaveBeenCalledWith(
+      workspaceDataStub.namespace,
+      workspaceDataStub.id
+    );
+
+    // populated with the result of getWorkspace()
+    expect(currentWorkspaceStore.getValue()).toEqual({
+      ...workspaceData,
+      accessLevel: workspaceData.accessLevel,
+    });
+    // unchanged
+    expect(nextWorkspaceWarmupStore.getValue()).toBeUndefined();
+  });
+
+  it('loads the workspace specified by the route when neither store matches the route', async () => {
+    currentWorkspaceStore.next({
+      ...workspaceDataStub,
+      namespace: 'something else',
+      id: 'some other ID',
+    });
+    nextWorkspaceWarmupStore.next({
+      ...workspaceDataStub,
+      namespace: 'another one',
+      id: 'definitely not a match',
+    });
+
+    const spy = jest.spyOn(workspacesApi(), 'getWorkspace');
+    await createWrapperAndWaitForLoad();
+    expect(spy).toHaveBeenCalledWith(
+      workspaceDataStub.namespace,
+      workspaceDataStub.id
+    );
+
+    // populated with the result of getWorkspace()
+    expect(currentWorkspaceStore.getValue()).toEqual({
+      ...workspaceData,
+      accessLevel: workspaceData.accessLevel,
+    });
+    // clears because invalid
+    expect(nextWorkspaceWarmupStore.getValue()).toBeUndefined();
+  });
+
+  it('renders but does not need to load the workspace when the currentWorkspaceStore matches the route', async () => {
+    currentWorkspaceStore.next(workspaceDataStub);
+    const nextWs = {
+      ...workspaceDataStub,
+      namespace: 'something else',
+      id: 'some other ID',
+    };
+    nextWorkspaceWarmupStore.next(nextWs);
+
+    const spy = jest.spyOn(workspacesApi(), 'getWorkspace');
+    const expectingSpinner = false; // no spinner when loading from the store
+    await createWrapperAndWaitForLoad(expectingSpinner);
+    expect(spy).not.toHaveBeenCalled();
+
+    // unchanged
+    expect(currentWorkspaceStore.getValue()).toEqual(workspaceDataStub);
+    expect(nextWorkspaceWarmupStore.getValue()).toEqual(nextWs);
+  });
+
+  it('renders but does not need to load the workspace when the nextWorkspaceWarmupStore matches the route', async () => {
+    currentWorkspaceStore.next(undefined);
+    nextWorkspaceWarmupStore.next(workspaceDataStub);
+
+    const spy = jest.spyOn(workspacesApi(), 'getWorkspace');
+    const expectingSpinner = false; // no spinner when loading from the store
+    await createWrapperAndWaitForLoad(expectingSpinner);
+    expect(spy).not.toHaveBeenCalled();
+
+    // the nextWorkspaceWarmupStore is consumed/cleared
+    expect(nextWorkspaceWarmupStore.getValue()).toBeUndefined();
+    // the currentWorkspaceStore is set to the previous nextWorkspaceWarmupStore
+    expect(currentWorkspaceStore.getValue()).toEqual(workspaceDataStub);
+  });
+
+  it(
+    'renders but does not need to load the workspace when the nextWorkspaceWarmupStore matches the route' +
+      'and the currentWorkspaceStore mismatches the route',
+    async () => {
+      currentWorkspaceStore.next({
+        ...workspaceDataStub,
+        namespace: 'something else',
+        id: 'some other ID',
+      });
+      nextWorkspaceWarmupStore.next(workspaceDataStub);
+
+      const spy = jest.spyOn(workspacesApi(), 'getWorkspace');
+      const expectingSpinner = false; // no spinner when loading from the store
+      await createWrapperAndWaitForLoad(expectingSpinner);
+      expect(spy).not.toHaveBeenCalled();
+
+      // the nextWorkspaceWarmupStore is consumed/cleared
+      expect(nextWorkspaceWarmupStore.getValue()).toBeUndefined();
+      // the currentWorkspaceStore is set to the previous nextWorkspaceWarmupStore
+      expect(currentWorkspaceStore.getValue()).toEqual(workspaceDataStub);
+    }
+  );
+
   it('should show the multi-region workspace notification for workspaces created before 6/15/2022', async () => {
+    currentWorkspaceStore.next(undefined);
     workspaceData.creationTime = new Date(2022, 5, 14).getTime();
     await createWrapperAndWaitForLoad();
     expect(
@@ -91,6 +207,7 @@ describe(WorkspaceWrapper.name, () => {
   });
 
   it('should not show the multi-region workspace notification for workspaces created during or after 6/15/2022', async () => {
+    currentWorkspaceStore.next(undefined);
     workspaceData.creationTime = new Date(2022, 5, 15).getTime();
     await createWrapperAndWaitForLoad();
     expect(
