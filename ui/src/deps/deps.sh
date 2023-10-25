b1b8464f8f984091caff81ab9bb999ad6f37822f
freemabd <brian.d.freeman@gmail.com>
2023-10-25 12:14:39 -0500

    DT-549 adding conditions around yarn run deps

--- ui/src/deps/deps.sh
+++ ui/src/deps/deps.sh
@@ -1,5 +1,15 @@
 #!/usr/bin/env bash
 
+set -e
+
+envs="local, test, staging, stable, preprod, prod"
+
+if [[ -z ${1} || $envs != *"$1"* ]]; then
+  echo "Please provide an environment arg (ex: yarn run deps local)"
+  echo "Valid args are: $envs"
+  exit 1
+fi
+
 ./project.rb tanagra-dep --env "$1"
 
 yarn run codegen && yarn run build-terra-deps
