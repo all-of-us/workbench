75c6cf06205256b8e1ee8d1bf496fa972abc5664
Joel Thibault <thibault@broad.dev>
2023-10-30 17:27:05 -0400

    rm all fontawesome/npmrc logic

--- ui/src/circle-build.sh
+++ ui/src/circle-build.sh
@@ -4,9 +4,6 @@
 set -euo pipefail
 set -v
 
-gsutil cat gs://all-of-us-workbench-test-credentials/dot-npmrc-fontawesome-creds-line.txt \
-  >> ~/.npmrc
-
 yarn install
 yarn deps test
 REACT_APP_ENVIRONMENT=test yarn run build --aot --no-watch --no-progress
