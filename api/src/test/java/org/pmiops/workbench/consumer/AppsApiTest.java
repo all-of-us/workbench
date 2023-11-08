acc189c757321f9e7027ea97002be3b3c047d313
Eric Rollins <erollins@broadinstitute.org>
2023-11-08 14:13:04 -0500

    Update import

--- api/src/test/java/org/pmiops/workbench/consumer/AppsApiTest.java
+++ api/src/test/java/org/pmiops/workbench/consumer/AppsApiTest.java
@@ -9,49 +9,49 @@ import static org.junit.jupiter.api.Assertions.assertEquals;
 import com.github.tomakehurst.wiremock.WireMockServer;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
+import org.junit.jupiter.api.extension.ExtendWith;
 import org.pmiops.workbench.leonardo.ApiClient;
 import org.pmiops.workbench.leonardo.ApiException;
 import org.pmiops.workbench.leonardo.api.AppsApi;
 import org.pmiops.workbench.leonardo.model.LeonardoGetAppResponse;
 import org.springframework.boot.web.client.RestTemplateBuilder;
 import org.springframework.web.client.RestTemplate;
+import au.com.dius.pact.consumer.junit5.PactConsumerTestExt;
 
+@ExtendWith(PactConsumerTestExt.class)
 class ProductServiceTest {
 
-    private WireMockServer wireMockServer;
-    private AppsApi leoAppService;
+  private WireMockServer wireMockServer;
+  private AppsApi leoAppService;
 
-    @BeforeEach
-    void setUp() {
-        wireMockServer = new WireMockServer(options().dynamicPort());
+  @BeforeEach
+  void setUp() {
+    wireMockServer = new WireMockServer(options().dynamicPort());
 
-        wireMockServer.start();
+    wireMockServer.start();
 
-        RestTemplate restTemplate = new RestTemplateBuilder()
-                .rootUri(wireMockServer.baseUrl())
-                .build();
+    RestTemplate restTemplate = new RestTemplateBuilder().rootUri(wireMockServer.baseUrl()).build();
 
-        ApiClient client = new ApiClient();
-        client.setBasePath(wireMockServer.baseUrl());
-        leoAppService = new AppsApi(client);
-    }
+    ApiClient client = new ApiClient();
+    client.setBasePath(wireMockServer.baseUrl());
+    leoAppService = new AppsApi(client);
+  }
 
-    @Test
-    void getApp() throws ApiException {
-        wireMockServer.stubFor(get(urlPathEqualTo("/api/google/v1/apps/x/y"))
-            .willReturn(aResponse()
-                .withStatus(200)
-                .withHeader("Content-Type", "application/json")
-                .withBody("[" +
-                    "{\"id\":\"9\",\"type\":\"CREDIT_CARD\",\"name\":\"GEM Visa\",\"version\":\"v2\"},"+
-                    "{\"id\":\"10\",\"type\":\"CREDIT_CARD\",\"name\":\"28 Degrees\",\"version\":\"v1\"}"+
-                    "]")));
+  @Test
+  void getApp() throws ApiException {
+    wireMockServer.stubFor(
+        get(urlPathEqualTo("/api/google/v1/apps/x/y"))
+            .willReturn(
+                aResponse()
+                    .withStatus(200)
+                    .withHeader("Content-Type", "application/json")
+                    .withBody(
+                        "{\"id\":\"10\",\"type\":\"CREDIT_CARD\",\"name\":\"28 Degrees\",\"version\":\"v1\"}")));
 
-        LeonardoGetAppResponse expected = null;
+    LeonardoGetAppResponse expected = null;
 
-        LeonardoGetAppResponse appResponse = leoAppService.getApp("x", "y");
+    LeonardoGetAppResponse appResponse = leoAppService.getApp("x", "y");
 
-        assertEquals(expected, appResponse);
-    }
-
+    assertEquals(expected, appResponse);
+  }
 }
