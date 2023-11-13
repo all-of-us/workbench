f6b281c345c18937c4dee666d431516ef2063353
Peter Lavigne <31020403+Peter-Lavigne@users.noreply.github.com>
2023-11-13 15:31:27 -0500

    Inline inactivityWarningBeforeMs (#8148)

--- ui/src/app/pages/signed-in/inactivity-modal.spec.tsx
+++ ui/src/app/pages/signed-in/inactivity-modal.spec.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { environment } from 'environments/environment';
 import { screen } from '@testing-library/dom';
 import { render } from '@testing-library/react';
 import userEvent from '@testing-library/user-event';
@@ -11,12 +12,15 @@ import {
 const createProps = (): InactivityModalProps => ({
   currentTimeMs: 0,
   signOutForInactivityTimeMs: 0,
-  inactivityWarningBeforeMs: 0,
   closeFunction: () => {},
 });
 
 describe(InactivityModal.name, () => {
-  const setup = (props = createProps()) => {
+  const setup = (
+    props = createProps(),
+    inactivityWarningBeforeSeconds: number = 0
+  ) => {
+    environment.inactivityWarningBeforeSeconds = inactivityWarningBeforeSeconds;
     render(
       <div id='popup-root'>
         <InactivityModal {...props} />)
@@ -30,23 +34,27 @@ describe(InactivityModal.name, () => {
   };
 
   it('should show the modal if there is less time until sign out than inactivityWarningBeforeMs', async () => {
-    setup({
-      ...createProps(),
-      currentTimeMs: 901,
-      signOutForInactivityTimeMs: 1000,
-      inactivityWarningBeforeMs: 100,
-    });
+    setup(
+      {
+        ...createProps(),
+        currentTimeMs: 10001,
+        signOutForInactivityTimeMs: 20000,
+      },
+      10
+    );
 
     expect(queryForModal()).not.toBeNull();
   });
 
   it('should not show the modal if there is more time until sign out than inactivityWarningBeforeMs', async () => {
-    setup({
-      ...createProps(),
-      currentTimeMs: 899,
-      signOutForInactivityTimeMs: 1000,
-      inactivityWarningBeforeMs: 100,
-    });
+    setup(
+      {
+        ...createProps(),
+        currentTimeMs: 9999,
+        signOutForInactivityTimeMs: 20000,
+      },
+      10
+    );
 
     expect(queryForModal()).toBeNull();
   });
