6dde0d665d897fe65272f3e677491495d36ae960
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2023-11-30 15:02:30 -0500

    [risk=low][RW-11416][RW-11413] Update Enforce Test User ToS cron to use new get endpoint (#8198)

--- api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedUserServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedUserServiceImpl.java
@@ -3,15 +3,20 @@ package org.pmiops.workbench.impersonation;
 import java.io.IOException;
 import java.sql.Timestamp;
 import java.time.Clock;
+import org.broadinstitute.dsde.workbench.client.sam.api.TermsOfServiceApi;
+import org.broadinstitute.dsde.workbench.client.sam.model.UserTermsOfServiceDetails;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.dao.UserTermsOfServiceDao;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.exceptions.ServerErrorException;
+import org.pmiops.workbench.sam.SamApiClientFactory;
+import org.pmiops.workbench.sam.SamRetryHandler;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
 /**
- * An impersonation-enabled version of {@link org.pmiops.workbench.db.dao.UserServiceImpl}
+ * An impersonation-enabled version of a subset of {@link
+ * org.pmiops.workbench.db.dao.UserServiceImpl}
  *
  * <p>REMINDER: With great power comes great responsibility. Impersonation should not be used in
  * production, except where absolutely necessary.
@@ -23,32 +28,38 @@ public class ImpersonatedUserServiceImpl implements ImpersonatedUserService {
   private final ImpersonatedFirecloudService impersonatedFirecloudService;
   private final UserDao userDao;
   private final UserTermsOfServiceDao userTermsOfServiceDao;
+  private final SamApiClientFactory samApiClientFactory;
+  private final SamRetryHandler samRetryHandler;
 
   @Autowired
   public ImpersonatedUserServiceImpl(
       Clock clock,
       ImpersonatedFirecloudService impersonatedFirecloudService,
       UserDao userDao,
-      UserTermsOfServiceDao userTermsOfServiceDao) {
+      UserTermsOfServiceDao userTermsOfServiceDao,
+      SamApiClientFactory samApiClientFactory,
+      SamRetryHandler samRetryHandler) {
     this.clock = clock;
     this.impersonatedFirecloudService = impersonatedFirecloudService;
     this.userDao = userDao;
     this.userTermsOfServiceDao = userTermsOfServiceDao;
+    this.samApiClientFactory = samApiClientFactory;
+    this.samRetryHandler = samRetryHandler;
   }
 
   @Override
-  public boolean getUserTerraTermsOfServiceStatus(String username) {
-    final DbUser dbUser = userDao.findUserByUsername(username);
-
+  public UserTermsOfServiceDetails getTerraTermsOfServiceStatusForUser(String username) {
     try {
-      return impersonatedFirecloudService.getUserTermsOfServiceStatus(dbUser);
+      var samClient = samApiClientFactory.newImpersonatedApiClient(username);
+      return samRetryHandler.run(
+          context -> new TermsOfServiceApi(samClient).userTermsOfServiceGetSelf());
     } catch (IOException e) {
       throw new ServerErrorException(e);
     }
   }
 
   @Override
-  public void acceptTerraTermsOfService(String username) {
+  public void acceptTerraTermsOfServiceForUser(String username) {
     final DbUser dbUser = userDao.findUserByUsername(username);
 
     try {
@@ -61,5 +72,8 @@ public class ImpersonatedUserServiceImpl implements ImpersonatedUserService {
         userTermsOfServiceDao
             .findByUserIdOrThrow(dbUser.getUserId())
             .setTerraAgreementTime(new Timestamp(clock.instant().toEpochMilli())));
+
+    // TODO: save the terra tos version as well, when that's available
+    // after RW-11433 or RW-11434
   }
 }
