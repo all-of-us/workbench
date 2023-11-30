f15e9d1befc543a96fce36060f327d77fa9acf21
Joel Thibault <thibault@broad.dev>
2023-11-30 13:32:44 -0500

    switch back to FC accept TOS due to sam client bug

--- api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedUserServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/impersonation/ImpersonatedUserServiceImpl.java
@@ -7,6 +7,7 @@ import org.broadinstitute.dsde.workbench.client.sam.api.TermsOfServiceApi;
 import org.broadinstitute.dsde.workbench.client.sam.model.UserTermsOfServiceDetails;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.dao.UserTermsOfServiceDao;
+import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.exceptions.ServerErrorException;
 import org.pmiops.workbench.sam.SamApiClientFactory;
 import org.pmiops.workbench.sam.SamRetryHandler;
@@ -24,6 +25,7 @@ import org.springframework.stereotype.Service;
 public class ImpersonatedUserServiceImpl implements ImpersonatedUserService {
 
   private final Clock clock;
+  private final ImpersonatedFirecloudService impersonatedFirecloudService;
   private final UserDao userDao;
   private final UserTermsOfServiceDao userTermsOfServiceDao;
   private final SamApiClientFactory samApiClientFactory;
@@ -32,11 +34,13 @@ public class ImpersonatedUserServiceImpl implements ImpersonatedUserService {
   @Autowired
   public ImpersonatedUserServiceImpl(
       Clock clock,
+      ImpersonatedFirecloudService impersonatedFirecloudService,
       UserDao userDao,
       UserTermsOfServiceDao userTermsOfServiceDao,
       SamApiClientFactory samApiClientFactory,
       SamRetryHandler samRetryHandler) {
     this.clock = clock;
+    this.impersonatedFirecloudService = impersonatedFirecloudService;
     this.userDao = userDao;
     this.userTermsOfServiceDao = userTermsOfServiceDao;
     this.samApiClientFactory = samApiClientFactory;
@@ -56,20 +60,17 @@ public class ImpersonatedUserServiceImpl implements ImpersonatedUserService {
 
   @Override
   public void acceptTerraTermsOfServiceForUser(String username) {
+    final DbUser dbUser = userDao.findUserByUsername(username);
+
     try {
-      var samClient = samApiClientFactory.newImpersonatedApiClient(username);
-      samRetryHandler.run(
-          context -> {
-            new TermsOfServiceApi(samClient).userTermsOfServiceAcceptSelf();
-            return null; // retry handlers can't return void, but anything else is fine
-          });
+      impersonatedFirecloudService.acceptTermsOfService(dbUser);
     } catch (IOException e) {
       throw new ServerErrorException(e);
     }
 
     userTermsOfServiceDao.save(
         userTermsOfServiceDao
-            .findByUserIdOrThrow(userDao.findUserByUsername(username).getUserId())
+            .findByUserIdOrThrow(dbUser.getUserId())
             .setTerraAgreementTime(new Timestamp(clock.instant().toEpochMilli())));
 
     // TODO: save the terra tos version as well, when that's available
