b3c387e753a8d9ae418cec67e40700d44dca3031
Joel Thibault <thibault@broad.dev>
2023-12-07 10:19:16 -0500

    placeholder for RTL tests

---
+++ ui/src/app/components/runtime-configuration-panel.rtl.spec.tsx
@@ -0,0 +1,13 @@
+import {
+  deriveConfiguration,
+  deriveErrorsAndWarnings,
+  derivePanelContent,
+} from './runtime-configuration-panel';
+
+// TODO
+
+describe(deriveConfiguration.name, () => {});
+
+describe(derivePanelContent.name, () => {});
+
+describe(deriveErrorsAndWarnings.name, () => {});
