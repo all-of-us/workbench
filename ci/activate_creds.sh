e26e6392b1c5386e5580a76dcec9d7767821eb99
Peter Lavigne <peterlavigne@google.com>
2023-12-07 15:41:32 -0500

    Change env var name

--- ci/activate_creds.sh
+++ ci/activate_creds.sh
@@ -3,14 +3,14 @@
 # Creates credentials file $1 from two environment variables (see
 # below) which combine to decrypt the keys for a service account.
 # Does gcloud auth using the result.
-if [ ! "${SERVICE_ACCOUNT_CREDENTIALS_STABLE_ENV}" ]
+if [ ! "${SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV}" ]
 then
-  echo "No SERVICE_ACCOUNT_CREDENTIALS_STABLE_ENV env var defined, aborting creds activation."
+  echo "No SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV env var defined, aborting creds activation."
   exit 1
 fi
 
-echo $SERVICE_ACCOUNT_CREDENTIALS_STABLE_ENV | \
-     openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "${SERVICE_ACCOUNT_CREDENTIALS_STABLE_ENV_KEY}" \
+echo $SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV | \
+     openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "${SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV_KEY}" \
      > $1
 
 gcloud auth activate-service-account --key-file $1
