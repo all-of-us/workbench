61998fefde950ee21d84f6d48ca7987b236ad550
Peter Lavigne <peterlavigne@google.com>
2023-12-12 13:56:19 -0500

    Refactor CCI config

--- ci/activate_creds.sh
+++ ci/activate_creds.sh
@@ -3,14 +3,18 @@
 # Creates credentials file $1 from two environment variables (see
 # below) which combine to decrypt the keys for a service account.
 # Does gcloud auth using the result.
-if [ ! "${SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV}" ]
-then
-  echo "No SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV env var defined, aborting creds activation."
+
+# Default values for environment variables
+CREDENTIALS_ENV_VAR_NAME=${2:-GCLOUD_CREDENTIALS}
+KEY_ENV_VAR_NAME=${3:-GCLOUD_CREDENTIALS_KEY}
+
+if [ ! "${!CREDENTIALS_ENV_VAR_NAME}" ]; then
+  echo "No ${CREDENTIALS_ENV_VAR_NAME} env var defined, aborting creds activation."
   exit 1
 fi
 
-echo $SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV | \
-     openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "${SERVICE_ACCOUNT_CREDENTIALS_DEPLOY_STABLE_ENV_KEY}" \
+echo "${!CREDENTIALS_ENV_VAR_NAME}" | \
+     openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "${!KEY_ENV_VAR_NAME}" \
      > $1
 
 gcloud auth activate-service-account --key-file $1
