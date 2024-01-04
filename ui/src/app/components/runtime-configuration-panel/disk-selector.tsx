8f8af5a96e1605b4112b064aaf30b8aca3c9a9e7
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2024-01-04 16:43:39 -0500

    [risk=low][no ticket] Split disk-selector into Persistent and Standard components (#8265)
    * cleaned up onChange

--- ui/src/app/components/runtime-configuration-panel/disk-selector.tsx
+++ ui/src/app/components/runtime-configuration-panel/disk-selector.tsx
@@ -1,33 +1,25 @@
 import * as React from 'react';
-import { Dropdown } from 'primereact/dropdown';
-
-import { Disk, DiskType } from 'generated/fetch';
 
 import { StyledExternalLink } from 'app/components/buttons';
 import { styles } from 'app/components/common-env-conf-panels/styles';
 import { FlexColumn, FlexRow } from 'app/components/flex';
-import { WarningMessage } from 'app/components/messages';
-import { TooltipTrigger } from 'app/components/popups';
-import { AoU } from 'app/components/text-wrappers';
-import { maybeWithExistingDiskName } from 'app/utils/analysis-config';
 import { ComputeType } from 'app/utils/machines';
-import { DiskConfig, diskTypeLabels } from 'app/utils/runtime-utils';
+import { DiskConfig } from 'app/utils/runtime-utils';
 import { supportUrls } from 'app/utils/zendesk';
 
-import { DiskSizeSelector } from './disk-size-selector';
+import { PersistentDiskSelector } from './persistent-disk-selector';
+import { StandardDiskSelector } from './standard-disk-selector';
 
 interface Props {
   diskConfig: DiskConfig;
-  onChange: (c: DiskConfig) => void;
+  onChange: (c: Partial<DiskConfig>) => void;
   disabled: boolean;
-  existingDisk: Disk | null;
   computeType: string | null;
 }
 export const DiskSelector = ({
   diskConfig,
   onChange,
   disabled,
-  existingDisk,
   computeType,
 }: Props) => {
   return (
@@ -50,117 +42,9 @@ export const DiskSelector = ({
         </FlexRow>
       </FlexColumn>
       {computeType === ComputeType.Standard ? (
-        <>
-          <WarningMessage>
-            <AoU /> will now only support reattachable persistent disks as the
-            storage disk option for Standard VMs and will discontinue standard
-            disks. You will continue to use standard disks with Dataproc
-            clusters. Refer to the
-            <a
-              href={
-                'https://support.researchallofus.org/hc/en-us/articles/5140493753620-Persistent-Disk'
-              }
-              target='_blank'
-            >
-              {' '}
-              article{' '}
-            </a>{' '}
-            to learn more.
-          </WarningMessage>
-          <FlexRow style={styles.diskRow}>
-            <FlexColumn>
-              <label style={styles.diskLabel}>
-                Reattachable persistent disk
-              </label>
-              <span>
-                A reattachable disk is saved even when your compute environment
-                is deleted.
-              </span>
-              {diskConfig.detachable && (
-                <FlexRow style={{ ...styles.formGrid2, marginTop: '11px' }}>
-                  <FlexRow style={styles.labelAndInput}>
-                    <label
-                      style={{ ...styles.label, minWidth: '4.5rem' }}
-                      htmlFor='disk-type'
-                    >
-                      Disk type
-                    </label>
-                    <Dropdown
-                      id={'disk-type'}
-                      options={[DiskType.STANDARD, DiskType.SSD].map(
-                        (value) => ({
-                          label: diskTypeLabels[value],
-                          value,
-                        })
-                      )}
-                      style={{ width: '150px' }}
-                      disabled={disabled}
-                      onChange={({ value }) =>
-                        onChange(
-                          maybeWithExistingDiskName(
-                            {
-                              ...diskConfig,
-                              detachableType: value,
-                            },
-                            existingDisk
-                          )
-                        )
-                      }
-                      value={diskConfig.detachableType}
-                    />
-                  </FlexRow>
-                  <DiskSizeSelector
-                    idPrefix='detachable'
-                    diskSize={diskConfig.size}
-                    disabled={disabled}
-                    onChange={(size: number) =>
-                      onChange(
-                        maybeWithExistingDiskName(
-                          {
-                            ...diskConfig,
-                            size,
-                          },
-                          existingDisk
-                        )
-                      )
-                    }
-                  />
-                </FlexRow>
-              )}
-            </FlexColumn>
-          </FlexRow>
-        </>
+        <PersistentDiskSelector {...{ diskConfig, onChange, disabled }} />
       ) : (
-        <TooltipTrigger content='Reattachable disks are unsupported for this compute type'>
-          <FlexRow style={styles.diskRow}>
-            <FlexColumn>
-              <label style={styles.diskLabel}>Standard disk</label>
-              <span>
-                A standard disk is created and deleted with your cloud
-                environment.
-              </span>
-              {diskConfig.detachable || (
-                <DiskSizeSelector
-                  {...{ disabled }}
-                  idPrefix='standard'
-                  diskSize={diskConfig.size}
-                  style={{ marginTop: '11px' }}
-                  onChange={(size: number) =>
-                    onChange(
-                      maybeWithExistingDiskName(
-                        {
-                          ...diskConfig,
-                          size,
-                        },
-                        existingDisk
-                      )
-                    )
-                  }
-                />
-              )}
-            </FlexColumn>
-          </FlexRow>
-        </TooltipTrigger>
+        <StandardDiskSelector {...{ diskConfig, onChange, disabled }} />
       )}
     </FlexColumn>
   );
