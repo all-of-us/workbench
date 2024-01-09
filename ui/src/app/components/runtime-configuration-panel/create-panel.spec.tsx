79b78d921b3a5cc630939b2b733e874ac789c36c
Eric Rollins <erollins@broadinstitute.org>
2024-01-09 15:29:03 -0500

    Testing tooltip in the create panel.

--- ui/src/app/components/runtime-configuration-panel/create-panel.spec.tsx
+++ ui/src/app/components/runtime-configuration-panel/create-panel.spec.tsx
@@ -5,6 +5,7 @@ import * as React from 'react';
 import { RuntimeStatus } from 'generated/fetch';
 
 import { render, screen, waitFor } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { toAnalysisConfig } from 'app/utils/analysis-config';
 import { ComputeType } from 'app/utils/machines';
 import { runtimePresets } from 'app/utils/runtime-presets';
@@ -105,5 +106,32 @@ describe(CreatePanel.name, () => {
       expect(requestAnalysisConfig).not.toHaveBeenCalled();
       expect(onClose).not.toHaveBeenCalled();
     });
+  });
+
+  it('shows a tooltip when hovering over createButton if you cannot create a runtime', async () => {
+    const user = userEvent.setup();
+    await component({
+      runtimeCanBeCreated: false,
+      runtimeCannotBeCreatedExplanation: 'Tooltip for testing',
+    });
+
+    const createButton = screen.queryByRole('button', { name: 'Create' });
+    expect(createButton).toBeInTheDocument();
+    await user.pointer([{ pointerName: 'mouse', target: createButton }]);
+    // Show tooltip when hovering over disabled button.
+    expect(screen.queryByText('Tooltip for testing')).toBeInTheDocument();
+  });
+  it('does not show a tooltip when hovering over createButton if you can create a runtime', async () => {
+    const user = userEvent.setup();
+    await component({
+      runtimeCanBeCreated: true,
+      runtimeCannotBeCreatedExplanation: 'Tooltip for testing',
+    });
+
+    const createButton = screen.queryByRole('button', { name: 'Create' });
+    expect(createButton).toBeInTheDocument();
+    await user.pointer([{ pointerName: 'mouse', target: createButton }]);
+    // Show tooltip when hovering over disabled button.
+    expect(screen.queryByText('Tooltip for testing')).not.toBeInTheDocument();
   });
 });
