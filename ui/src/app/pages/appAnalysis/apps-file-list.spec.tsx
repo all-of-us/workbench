eb3efccde99768b30bd343ab3d7f83239c2aab2b
Eric Rollins <erollins@broadinstitute.org>
2024-01-10 16:24:47 -0500

    Convert first apps-file-list test from Enzyme to RTL.

--- ui/src/app/pages/appAnalysis/apps-file-list.spec.tsx
+++ ui/src/app/pages/appAnalysis/apps-file-list.spec.tsx
@@ -1,23 +1,26 @@
 import * as React from 'react';
 import { MemoryRouter } from 'react-router';
-import { mount } from 'enzyme';
 
 import { NotebooksApi, WorkspacesApi } from 'generated/fetch';
 
+import { render, screen, waitFor, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { AppFilesList } from 'app/pages/appAnalysis/app-files-list';
 import { analysisTabPath } from 'app/routing/utils';
 import { registerApiClient } from 'app/services/swagger-fetch-clients';
 import { displayDateWithoutHours } from 'app/utils/dates';
 import { currentWorkspaceStore } from 'app/utils/navigation';
 
-import { waitOneTickAndUpdate } from 'testing/react-test-helpers';
 import { NotebooksApiStub } from 'testing/stubs/notebooks-api-stub';
 import { workspaceDataStub } from 'testing/stubs/workspaces';
 import { WorkspacesApiStub } from 'testing/stubs/workspaces-api-stub';
 
-const appsFilesTable = (wrapper) =>
-  wrapper.find('[data-test-id="apps-file-list"]').find('tbody');
-const appsFilesTableColumns = (wrapper) => appsFilesTable(wrapper).find('td');
+const appsFilesTable = (container) =>
+  container
+    .querySelector('[data-test-id="apps-file-list"]')
+    .querySelector('tbody');
+const appsFilesTableColumns = (container) =>
+  appsFilesTable(container).querySelector('td');
 
 const MENU_COLUMN_NUMBER = 0;
 const APPLICATION_COLUMN_NUMBER = 1;
@@ -25,6 +28,8 @@ const NAME_COLUMN_NUMBER = 2;
 const MODIFIED_DATE_COLUMN_NUMBER = 3;
 const MODIFIED_BY_COLUMN_NUMBER = 4;
 
+// There are two header rows, so this is the first row with data.
+const FIRST_DATA_ROW_NUMBER = 2;
 describe('AppsList', () => {
   beforeEach(() => {
     registerApiClient(WorkspacesApi, new WorkspacesApiStub());
@@ -33,67 +38,55 @@ describe('AppsList', () => {
 
   it('should render new Analysis tab', async () => {
     currentWorkspaceStore.next(workspaceDataStub);
-    const wrapper = mount(
+    const { container } = render(
       <MemoryRouter>
         <AppFilesList showSpinner={() => {}} hideSpinner={() => {}} />
       </MemoryRouter>
     );
-    await waitOneTickAndUpdate(wrapper);
 
-    // First Column : Menu icon should be a kebab icon with circle
-    expect(
-      appsFilesTableColumns(wrapper)
-        .at(MENU_COLUMN_NUMBER)
-        .first()
-        .find('[data-icon="circle-ellipsis-vertical"]')
-    ).toBeTruthy();
+    let firstDataRow;
+    const firstNotebook = NotebooksApiStub.stubNotebookList()[0];
+    // First Column : Menu icon
+    await waitFor(() => {
+      console.log(screen.logTestingPlaygroundURL());
+      firstDataRow = screen.getAllByRole('row')[FIRST_DATA_ROW_NUMBER];
+      expect(
+        within(firstDataRow).getByTitle('Notebook Action Menu')
+      ).toBeTruthy();
+    });
 
     // Second Column displays the type of Application: In this case Jupyter
-    expect(
-      appsFilesTableColumns(wrapper)
-        .at(APPLICATION_COLUMN_NUMBER)
-        .find('img')
-        .prop('alt')
-    ).toBe('Jupyter');
+    within(firstDataRow).getByAltText('Jupyter');
 
-    // Fourth column of table displays file name with extension
-    expect(
-      appsFilesTableColumns(wrapper).at(NAME_COLUMN_NUMBER).text()
-    ).toMatch(NotebooksApiStub.stubNotebookList()[0].name);
+    // // Fourth column of table displays file name with extension
+    within(firstDataRow).getByText(firstNotebook.name);
 
-    // Fifth column of notebook table displays last modified time
-    expect(
-      appsFilesTableColumns(wrapper).at(MODIFIED_DATE_COLUMN_NUMBER).text()
-    ).toMatch(
-      displayDateWithoutHours(
-        NotebooksApiStub.stubNotebookList()[0].lastModifiedTime
-      )
+    // // Fifth column of notebook table displays last modified time
+    within(firstDataRow).getByText(
+      displayDateWithoutHours(firstNotebook.lastModifiedTime)
     );
 
     // Sixth column of notebook table displays last modified by
-    expect(
-      appsFilesTableColumns(wrapper).at(MODIFIED_BY_COLUMN_NUMBER).text()
-    ).toMatch(NotebooksApiStub.stubNotebookList()[0].lastModifiedBy);
+    within(firstDataRow).getByText(firstNotebook.lastModifiedBy);
   });
 
-  it('should redirect to notebook playground mode when file name of Jupyter App is clicked', async () => {
-    currentWorkspaceStore.next(workspaceDataStub);
-    const wrapper = mount(
-      <MemoryRouter>
-        <AppFilesList showSpinner={() => {}} hideSpinner={() => {}} />
-      </MemoryRouter>
-    );
-    await waitOneTickAndUpdate(wrapper);
-
-    const expected = `${analysisTabPath(
-      workspaceDataStub.namespace,
-      workspaceDataStub.id
-    )}/preview/mockFile.ipynb`;
-    expect(
-      appsFilesTableColumns(wrapper)
-        .at(NAME_COLUMN_NUMBER)
-        .find('a')
-        .prop('href')
-    ).toBe(expected);
-  });
+  // it('should redirect to notebook playground mode when file name of Jupyter App is clicked', async () => {
+  //   currentWorkspaceStore.next(workspaceDataStub);
+  //   const { container } = render(
+  //     <MemoryRouter>
+  //       <AppFilesList showSpinner={() => {}} hideSpinner={() => {}} />
+  //     </MemoryRouter>
+  //   );
+  //
+  //   const expected = `${analysisTabPath(
+  //     workspaceDataStub.namespace,
+  //     workspaceDataStub.id
+  //   )}/preview/mockFile.ipynb`;
+  //   expect(
+  //     appsFilesTableColumns(container)
+  //       .at(NAME_COLUMN_NUMBER)
+  //       .querySelector('a')
+  //       .prop('href')
+  //   ).toBe(expected);
+  // });
 });
