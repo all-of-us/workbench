ea094366ec50801367dc9413d2b78559e8309cde
Eric Rollins <erollins@broadinstitute.org>
2024-01-11 09:31:00 -0500

    Start of a test for the existence of a modal.

--- ui/src/app/pages/appAnalysis/apps-file-list.spec.tsx
+++ ui/src/app/pages/appAnalysis/apps-file-list.spec.tsx
@@ -5,6 +5,7 @@ import { MemoryRouter } from 'react-router';
 
 import { NotebooksApi, WorkspacesApi } from 'generated/fetch';
 
+import { RuntimeApiStub } from '../../../testing/stubs/runtime-api-stub';
 import { render, screen, waitFor, within } from '@testing-library/react';
 import { AppFilesList } from 'app/pages/appAnalysis/app-files-list';
 import { analysisTabPath } from 'app/routing/utils';
@@ -19,9 +20,11 @@ import { WorkspacesApiStub } from 'testing/stubs/workspaces-api-stub';
 // There are two header rows, so this is the first row with data.
 const FIRST_DATA_ROW_NUMBER = 2;
 describe('AppsList', () => {
+  let notebooksApiStub: NotebooksApiStub;
   beforeEach(() => {
     registerApiClient(WorkspacesApi, new WorkspacesApiStub());
-    registerApiClient(NotebooksApi, new NotebooksApiStub());
+    notebooksApiStub = new NotebooksApiStub();
+    registerApiClient(NotebooksApi, notebooksApiStub);
   });
 
   it('should render new Analysis tab', async () => {
@@ -33,7 +36,7 @@ describe('AppsList', () => {
     );
 
     let firstDataRow;
-    const firstNotebook = NotebooksApiStub.stubNotebookList()[0];
+    const firstNotebook = (await notebooksApiStub.getNoteBookList())[0];
     // First Column : Menu icon
     await waitFor(() => {
       firstDataRow = screen.getAllByRole('row')[FIRST_DATA_ROW_NUMBER];
@@ -61,5 +64,26 @@ describe('AppsList', () => {
 
     // Sixth column of notebook table displays last modified by
     within(firstDataRow).getByText(firstNotebook.lastModifiedBy);
+  });
+
+  it('should render new Analysis tab modal', async () => {
+    currentWorkspaceStore.next(workspaceDataStub);
+    const { container } = render(
+      <MemoryRouter>
+        <AppFilesList showSpinner={() => {}} hideSpinner={() => {}} />
+      </MemoryRouter>
+    );
+
+    let firstDataRow;
+    notebooksApiStub.notebookList[0].sizeInBytes = 5 * 1024 * 1024;
+
+    const firstNotebook = (await notebooksApiStub.getNoteBookList())[0];
+    // First Column : Menu icon
+    await waitFor(() => {
+      firstDataRow = screen.getAllByRole('row')[FIRST_DATA_ROW_NUMBER];
+      expect(
+        within(firstDataRow).getByRole('link', { name: firstNotebook.name })
+      ).toBeTruthy();
+    });
   });
 });
