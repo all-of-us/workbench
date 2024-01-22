ee5e3316eabdb2bb90d33f4df8af44b6f6738772
Eric Rollins <erollins@broadinstitute.org>
2024-01-22 09:12:34 -0500

    Trying to trigger verification.

--- api/src/contracttest/java/org/pmiops/workbench/consumer/RuntimesApiTest.java
+++ api/src/contracttest/java/org/pmiops/workbench/consumer/RuntimesApiTest.java
@@ -55,7 +55,7 @@ class RuntimesApiTest {
                           context -> {
                             context.stringType("creator", "Bugs Bunny");
                             context.stringType("createdDate", "Yesterday");
-                            context.stringType("dateAccessed", "Tuesday");
+                            context.stringType("dateAccessed", "Wednesday");
                           });
                     })
                 .build())
