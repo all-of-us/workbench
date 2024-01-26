bf34005fde95a755cc8377bd2547b5e14147c61f
Eric Rollins <erollins@broadinstitute.org>
2024-01-26 09:55:38 -0500

    Add contract tests for updating.

--- api/src/contracttest/java/org/pmiops/workbench/consumer/RuntimesApiTest.java
+++ api/src/contracttest/java/org/pmiops/workbench/consumer/RuntimesApiTest.java
@@ -100,6 +100,56 @@ class RuntimesApiTest {
         .toPact();
   }
 
+  @Pact(consumer = "aou-rwb-api", provider = "leonardo")
+  RequestResponsePact updateRuntime(PactDslWithProvider builder) {
+    return builder
+        .given("there is a runtime in a Google project")
+        .uponReceiving("a request to get that runtime")
+        .method("PATCH")
+        .path("/api/google/v1/runtimes/googleProject/runtimeName")
+        .willRespondWith()
+        .status(200)
+        .headers(contentTypeJsonHeader)
+        .body(
+            newJsonBody(
+                body -> {
+                  body.stringType("runtimeName", "sample-cromwell-study");
+                  body.stringType("status", "Running");
+                  body.stringType("autopauseThreshold", "57");
+                  body.stringType("proxyUrl", "http://www.proxy.com");
+                  body.array("errors", errors -> {});
+                  body.object(
+                      "cloudContext",
+                      context -> {
+                        context.stringType("cloudProvider", "GCP");
+                        context.stringType("cloudResource", "terra-vpc-xx-fake-70e4eb32");
+                      });
+                  body.object(
+                      "auditInfo",
+                      context -> {
+                        context.stringType("creator", "Bugs Bunny");
+                        context.stringType("createdDate", "Yesterday");
+                        context.stringType("dateAccessed", "Tuesday");
+                      });
+                })
+                .build())
+        .toPact();
+  }
+
+  @Pact(consumer = "aou-rwb-api", provider = "leonardo")
+  RequestResponsePact updateMissingRuntime(PactDslWithProvider builder) {
+    return builder
+        .given("there is not a runtime in a Google project")
+        .uponReceiving("a request to get that runtime")
+        .method("PATCH")
+        .path("/api/google/v1/runtimes/googleProject/runtimeName")
+        .willRespondWith()
+        .status(404)
+        .headers(contentTypeJsonHeader)
+        .body(newJsonBody(body -> {}).build())
+        .toPact();
+  }
+
   @Test
   @PactTestFor(pactMethod = "createNewRuntime")
   void testCreateRuntimeWhenRuntimeDoesNotExist(MockServer mockServer) throws ApiException {
