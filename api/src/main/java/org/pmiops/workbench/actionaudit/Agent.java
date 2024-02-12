b99c97f519ca8c6fa4488cd025401168ea1d194a
Joel Thibault <thibault@broad.dev>
2024-02-12 11:04:49 -0500

    mark Agent params as @Nullable

--- api/src/main/java/org/pmiops/workbench/actionaudit/Agent.java
+++ api/src/main/java/org/pmiops/workbench/actionaudit/Agent.java
@@ -1,8 +1,9 @@
 package org.pmiops.workbench.actionaudit;
 
+import org.jetbrains.annotations.Nullable;
 import org.pmiops.workbench.db.model.DbUser;
 
-public record Agent(AgentType agentType, Long idMaybe, String emailMaybe) {
+public record Agent(AgentType agentType, @Nullable Long idMaybe, @Nullable String emailMaybe) {
   public static Agent asUser(DbUser u) {
     return new Agent(AgentType.USER, u.getUserId(), u.getUsername());
   }
