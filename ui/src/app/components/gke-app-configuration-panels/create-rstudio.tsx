80ae59888267623282366abb777c6a6015266d5f
Eric Rollins <erollins@broadinstitute.org>
2024-02-23 17:01:29 -0500

    [RW-11588][risk=no] Added support articles to RStudio tab. (#8381)

--- ui/src/app/components/gke-app-configuration-panels/create-rstudio.tsx
+++ ui/src/app/components/gke-app-configuration-panels/create-rstudio.tsx
@@ -2,21 +2,46 @@ import * as React from 'react';
 
 import { AppType } from 'generated/fetch';
 
+import { styles } from 'app/components/common-env-conf-panels/styles';
 import { InfoMessage } from 'app/components/messages';
+import { supportUrls } from 'app/utils/zendesk';
 
 import { CommonCreateGkeAppProps, CreateGkeApp } from './create-gke-app';
 
+const rStudioSupportArticles = [
+  {
+    text: 'How to run RStudio in All of Us Researcher Workbench',
+    link: supportUrls.rStudioHowToRun,
+  },
+  {
+    text: 'How to use Dataset Builder with RStudio',
+    link: supportUrls.rStudioHowToDataset,
+  },
+];
+
 const SupportNote = () => (
-  <InfoMessage>
-    <h4 style={{ marginTop: 0, fontSize: '1rem' }}>
-      How to create RStudio artifacts:
-    </h4>
-    <p style={{ marginTop: '0.5rem' }}>
-      You can create R and RMD files within RStudio’s menu bar. Saved files will
-      appear in the analysis tab and be stored in the workspace bucket. Access
-      your files in RStudio through the Output pane.
-    </p>
-  </InfoMessage>
+  <>
+    <InfoMessage>
+      <h4 style={{ marginTop: 0, fontSize: '1rem' }}>
+        How to create RStudio artifacts:
+      </h4>
+      <p style={{ marginTop: '0.5rem' }}>
+        You can create R and RMD files within RStudio’s menu bar. Saved files
+        will appear in the analysis tab and be stored in the workspace bucket.
+        Access your files in RStudio through the Output pane.
+      </p>
+    </InfoMessage>
+    <div style={{ ...styles.controlSection }}>
+      <div style={{ fontWeight: 'bold' }}>RStudio support articles</div>
+      {rStudioSupportArticles.map((article, index) => (
+        <div key={index} style={{ display: 'block' }}>
+          <a href={article.link} target='_blank'>
+            {index + 1}. {article.text}
+          </a>
+        </div>
+      ))}
+    </div>
+  </>
 );
 
 export const CreateRStudio = (props: CommonCreateGkeAppProps) => (
--- ui/src/app/utils/aou_external_links.tsx
+++ ui/src/app/utils/aou_external_links.tsx
@@ -1,5 +1,3 @@
-export const CROMWELL_INFORMATION_LINK =
-  'https://support.researchallofus.org/hc/en-us/articles/14428263737620';
 export const CROMWELL_INTRO_LINK =
   'https://cromwell.readthedocs.io/en/stable/tutorials/FiveMinuteIntro/';
 export const WORKFLOW_AND_WDL_LINK =
