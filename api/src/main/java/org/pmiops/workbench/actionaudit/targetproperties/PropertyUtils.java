cebb3cb936a7de41450749766d0d553f1ded2c7b
Joel Thibault <thibault@broad.dev>
2024-02-26 17:14:51 -0500

    stringOrNull javadoc

--- api/src/main/java/org/pmiops/workbench/actionaudit/targetproperties/PropertyUtils.java
+++ api/src/main/java/org/pmiops/workbench/actionaudit/targetproperties/PropertyUtils.java
@@ -3,6 +3,17 @@ package org.pmiops.workbench.actionaudit.targetproperties;
 import java.util.function.Function;
 
 public class PropertyUtils {
+
+  /**
+   * Returns a property extractor to transform a member field of an entity object to String or null.
+   *
+   * <p>For example: `stringOrNull(Profile::getAddress)` will return a function which is equivalent
+   * to `profile -> profile.getAddress() == null ? null : profile.getAddress().toString()`.
+   *
+   * @param entityExtractor a function to extract a member field from an entity object
+   * @return a function to transform the extracted field of the entity to String or null
+   * @param <T> the type of the entity object
+   */
   static <T> Function<T, String> stringOrNull(Function<T, Object> entityExtractor) {
     return entity -> {
       var value = entityExtractor.apply(entity);
