307d6a1858d71fac66506e282c546836a86a79d5
Joel Thibault <thibault@broad.dev>
2024-03-01 14:56:12 -0500

    WIP bad

--- api/src/main/java/org/pmiops/workbench/monitoring/MonitoringServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/monitoring/MonitoringServiceImpl.java
@@ -1,22 +1,17 @@
 package org.pmiops.workbench.monitoring;
 
-import com.google.common.collect.Iterables;
-import io.opencensus.stats.Measure.MeasureDouble;
-import io.opencensus.stats.Measure.MeasureLong;
-import io.opencensus.stats.MeasureMap;
-import io.opencensus.stats.StatsRecorder;
-import io.opencensus.stats.ViewManager;
-import io.opencensus.tags.TagContextBuilder;
-import io.opencensus.tags.TagKey;
-import io.opencensus.tags.TagValue;
-import io.opencensus.tags.Tagger;
-import java.util.Arrays;
+import io.opentelemetry.api.baggage.Baggage;
+import io.opentelemetry.api.baggage.BaggageBuilder;
+import io.opentelemetry.api.common.AttributeKey;
+import io.opentelemetry.api.metrics.DoubleCounter;
+import io.opentelemetry.api.metrics.LongCounter;
+import io.opentelemetry.api.metrics.Meter;
+import io.opentelemetry.sdk.metrics.SdkMeterProvider;
+import io.opentelemetry.sdk.metrics.export.MetricProducer;
+import io.opentelemetry.exporter.otlp.metrics.OtlpGrpcMetricExporter;
 import java.util.Map;
 import java.util.logging.Level;
 import java.util.logging.Logger;
-import java.util.stream.StreamSupport;
-import org.jetbrains.annotations.NotNull;
-import org.pmiops.workbench.monitoring.views.GaugeMetric;
 import org.pmiops.workbench.monitoring.views.Metric;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
@@ -25,21 +20,21 @@ import org.springframework.stereotype.Service;
 public class MonitoringServiceImpl implements MonitoringService {
   private static final Logger logger = Logger.getLogger(MonitoringServiceImpl.class.getName());
   private boolean viewsAreRegistered = false;
-  private ViewManager viewManager;
-  private StatsRecorder statsRecorder;
-  private StackdriverStatsExporterService stackdriverStatsExporterService;
-  private Tagger tagger;
+  private SdkMeterProvider sdkMeterProvider;
+  private Meter meter;
+  private OtlpGrpcMetricExporter otlpGrpcMetricExporter;
+  private Baggage baggage;
 
   @Autowired
   MonitoringServiceImpl(
-      ViewManager viewManager,
-      StatsRecorder statsRecorder,
-      StackdriverStatsExporterService stackdriverStatsExporterService,
-      Tagger tagger) {
-    this.viewManager = viewManager;
-    this.statsRecorder = statsRecorder;
-    this.stackdriverStatsExporterService = stackdriverStatsExporterService;
-    this.tagger = tagger;
+      SdkMeterProvider sdkMeterProvider,
+      Meter meter,
+      OtlpGrpcMetricExporter otlpGrpcMetricExporter,
+      Baggage baggage) {
+    this.sdkMeterProvider = sdkMeterProvider;
+    this.meter = meter;
+    this.otlpGrpcMetricExporter = otlpGrpcMetricExporter;
+    this.baggage = baggage;
   }
 
   private void initStatsConfigurationIdempotent() {
@@ -47,57 +42,38 @@ public class MonitoringServiceImpl implements MonitoringService {
       registerMetricViews();
       viewsAreRegistered = true;
     }
-    stackdriverStatsExporterService.createAndRegister();
+    otlpGrpcMetricExporter.export(sdkMeterProvider.collectAllMetrics());
   }
 
-  /**
-   * We don't want to register LogsBasedMetric enum values with OpenCensus. It makes it confusing ot
-   * see custom.googleapis.com/metric_name in addition to logging/user/metric_name.
-   * TODO(jaycarlton): restore event metrics here once OpenCensus is working
-   */
   private void registerMetricViews() {
-    StreamSupport.stream(
-            Iterables.concat(Arrays.<Metric>asList(GaugeMetric.values())).spliterator(), false)
-        .map(Metric::toView)
-        .forEach(viewManager::registerView);
+    // TODO: Implement the equivalent of registering views in OpenTelemetry
   }
 
   @Override
-  public void recordValues(Map<Metric, Number> metricToValue, Map<TagKey, TagValue> tags) {
+  public void recordValues(Map<Metric, Number> metricToValue, Map<AttributeKey<?>, Object> tags) {
     initStatsConfigurationIdempotent();
     if (metricToValue.isEmpty()) {
       logger.warning("recordValue() called with empty map.");
       return;
     }
-    final MeasureMap measureMap = statsRecorder.newMeasureMap();
-    metricToValue.forEach((metric, value) -> addToMeasureMap(measureMap, metric, value));
 
-    final TagContextBuilder tagContextBuilder = tagger.currentBuilder();
-    tags.forEach(tagContextBuilder::putLocal);
+    final BaggageBuilder baggageBuilder = baggage.toBuilder();
+    tags.forEach((tagKey, tagValue) -> baggageBuilder.put(tagKey.toString(), tagValue.toString()));
 
-    // Finally, send the data to the backend (Stackdriver/Cloud Monitoring for now).
-    measureMap.record(tagContextBuilder.build());
+    metricToValue.forEach((metric, value) -> {
+      if (metric.getMeasureClass().equals(Long.class)) {
+        LongCounter longCounter = meter.counterBuilder(metric.getName()).build();
+        longCounter.add(value.longValue(), baggageBuilder.build());
+      } else if (metric.getMeasureClass().equals(Double.class)) {
+        DoubleCounter doubleCounter = meter.counterBuilder(metric.getName()).build();
+        doubleCounter.add(value.doubleValue(), baggageBuilder.build());
+      } else {
+        logger.log(
+            Level.WARNING,
+            String.format("Unrecognized measure class %s", metric.getMeasureClass().getName()));
+      }
+    });
+
     logger.fine(String.format("Record measurements: %s, tags: %s", metricToValue, tags));
-  }
-
-  /**
-   * Insert either a MeasureLong or MeasureDouble with appropriate value into the appropriate
-   * MeasureMap.
-   *
-   * @param measureMap map to be populated
-   * @param metric properties that will give a view
-   * @param value
-   */
-  private void addToMeasureMap(
-      @NotNull MeasureMap measureMap, @NotNull Metric metric, @NotNull Number value) {
-    if (metric.getMeasureClass().equals(MeasureLong.class)) {
-      measureMap.put(metric.getMeasureLong(), value.longValue());
-    } else if (metric.getMeasureClass().equals(MeasureDouble.class)) {
-      measureMap.put(metric.getMeasureDouble(), value.doubleValue());
-    } else {
-      logger.log(
-          Level.WARNING,
-          String.format("Unrecognized measure class %s", metric.getMeasureClass().getName()));
-    }
   }
 }
