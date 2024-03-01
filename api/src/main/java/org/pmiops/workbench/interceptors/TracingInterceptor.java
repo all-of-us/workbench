073ea1451fbf4ee91140669c3eb68572aeb6eb65
Joel Thibault <thibault@broad.dev>
2024-03-01 14:29:19 -0500

    WIP

--- api/src/main/java/org/pmiops/workbench/interceptors/TracingInterceptor.java
+++ api/src/main/java/org/pmiops/workbench/interceptors/TracingInterceptor.java
@@ -1,17 +1,20 @@
 package org.pmiops.workbench.interceptors;
 
 import com.google.api.client.http.HttpMethods;
-import io.opencensus.common.Scope;
-import io.opencensus.exporter.trace.stackdriver.StackdriverTraceConfiguration;
-import io.opencensus.exporter.trace.stackdriver.StackdriverTraceExporter;
-import io.opencensus.trace.AttributeValue;
-import io.opencensus.trace.Span;
-import io.opencensus.trace.SpanBuilder;
-import io.opencensus.trace.Tracer;
-import io.opencensus.trace.Tracing;
-import io.opencensus.trace.samplers.Samplers;
+import io.opentelemetry.api.GlobalOpenTelemetry;
+import io.opentelemetry.api.trace.Span;
+import io.opentelemetry.api.trace.SpanBuilder;
+import io.opentelemetry.api.trace.SpanKind;
+import io.opentelemetry.api.trace.Tracer;
+import io.opentelemetry.api.trace.propagation.W3CTraceContextPropagator;
+import io.opentelemetry.context.Context;
+import io.opentelemetry.context.Scope;
+import io.opentelemetry.context.propagation.ContextPropagators;
+import io.opentelemetry.exporter.otlp.trace.OtlpGrpcSpanExporter;
+import io.opentelemetry.sdk.OpenTelemetrySdk;
+import io.opentelemetry.sdk.trace.SdkTracerProvider;
+import io.opentelemetry.sdk.trace.export.BatchSpanProcessor;
 import io.swagger.annotations.ApiOperation;
-import java.io.IOException;
 import java.util.logging.Level;
 import java.util.logging.Logger;
 import javax.inject.Provider;
@@ -24,10 +27,9 @@ import org.springframework.stereotype.Service;
 import org.springframework.web.method.HandlerMethod;
 import org.springframework.web.servlet.AsyncHandlerInterceptor;
 
-// Interceptor to create a trace of the lifecycle of api calls.
 @Service
 public class TracingInterceptor implements AsyncHandlerInterceptor {
-  private static final Tracer tracer = Tracing.getTracer();
+  private static final Tracer tracer = GlobalOpenTelemetry.getTracer("org.pmiops.workbench");
   private static final Logger log = Logger.getLogger(TracingInterceptor.class.getName());
 
   private final Provider<WorkbenchConfig> workbenchConfigProvider;
@@ -36,60 +38,48 @@ public class TracingInterceptor implements AsyncHandlerInterceptor {
   public TracingInterceptor(Provider<WorkbenchConfig> workbenchConfigProvider) {
     this.workbenchConfigProvider = workbenchConfigProvider;
     try {
-      StackdriverTraceExporter.createAndRegister(StackdriverTraceConfiguration.builder().build());
-    } catch (IOException e) {
+      OtlpGrpcSpanExporter spanExporter = OtlpGrpcSpanExporter.getDefault();
+      BatchSpanProcessor spanProcessor = BatchSpanProcessor.builder(spanExporter).build();
+      SdkTracerProvider tracerProvider =
+          SdkTracerProvider.builder().addSpanProcessor(spanProcessor).build();
+      OpenTelemetrySdk.builder()
+          .setTracerProvider(tracerProvider)
+          .setPropagators(ContextPropagators.create(W3CTraceContextPropagator.getInstance()))
+          .buildAndRegisterGlobal();
+    } catch (Exception e) {
       log.log(Level.WARNING, "Failed to setup tracing", e);
     }
   }
 
-  /**
-   * @param handler The Swagger-generated ApiController. It contains our handler as a private
-   *     delegate.
-   */
   @Override
   public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler)
       throws Exception {
-    // OPTIONS methods requests don't need traces.
     if (request.getMethod().equals(HttpMethods.OPTIONS)) {
       return true;
     }
-    // All of our Swagger-generated handlers extend the HandlerMethod class, but we'll check this
-    // before casting to avoid runtime exceptions.
     if (!(handler instanceof HandlerMethod)) {
       return true;
     }
 
-    // Create a new scoped span, which will set the span context for any other traces created
-    // from within this request-handling thread.
-    //
-    // This span will end up in Stackdriver with the name format like "Recv.[methodName]" where
-    // methodName is the method name defined in our Swagger YAML.
-    // Example: Recv.syncBillingProjectStatus
     HandlerMethod handlerMethod = (HandlerMethod) handler;
     SpanBuilder requestSpanBuilder =
-        tracer.spanBuilder(handlerMethod.getMethod().getName()).setSpanKind(Span.Kind.SERVER);
+        tracer.spanBuilder(handlerMethod.getMethod().getName()).setSpanKind(SpanKind.SERVER);
     if (workbenchConfigProvider.get().server.traceAllRequests) {
-      requestSpanBuilder.setSampler(Samplers.alwaysSample());
+      requestSpanBuilder.setSpanKind(SpanKind.SERVER);
     }
-    Scope requestScopedSpan = requestSpanBuilder.startScopedSpan();
+    Context requestScopedSpan = requestSpanBuilder.startSpan().storeInContext(Context.current());
 
-    // Log some additional key-value attributes, which will be visible in the details pane on
-    // Stackdriver.
-    final Span currentSpan = tracer.getCurrentSpan();
-    currentSpan.putAttribute(
-        "aou-env",
-        AttributeValue.stringAttributeValue(workbenchConfigProvider.get().server.shortName));
-    currentSpan.putAttribute("method", AttributeValue.stringAttributeValue(request.getMethod()));
-    currentSpan.putAttribute("path", AttributeValue.stringAttributeValue(request.getRequestURI()));
+    final Span currentSpan = Span.fromContext(requestScopedSpan);
+    currentSpan.setAttribute("aou-env", workbenchConfigProvider.get().server.shortName);
+    currentSpan.setAttribute("method", request.getMethod());
+    currentSpan.setAttribute("path", request.getRequestURI());
     ApiOperation apiOp =
         AnnotationUtils.findAnnotation(handlerMethod.getMethod(), ApiOperation.class);
     if (apiOp != null) {
-      currentSpan.putAttribute("description", AttributeValue.stringAttributeValue(apiOp.notes()));
-      currentSpan.putAttribute(
-          "responseType", AttributeValue.stringAttributeValue(apiOp.response().toString()));
+      currentSpan.setAttribute("description", apiOp.notes());
+      currentSpan.setAttribute("responseType", apiOp.response().toString());
     }
 
-    // Store the span as a payload within our request so we can close the span on completion.
     request.setAttribute(RequestAttribute.TRACE.toString(), requestScopedSpan);
     return true;
   }
