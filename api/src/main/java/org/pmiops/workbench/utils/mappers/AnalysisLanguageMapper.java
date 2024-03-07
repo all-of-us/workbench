d19434e546e9b126a61a26750f9776934902a33d
Joel Thibault <thibault@broad.dev>
2024-03-07 14:23:57 -0500

    Add AnalysisLanguage and AnalysisLanguageMapper

---
+++ api/src/main/java/org/pmiops/workbench/utils/mappers/AnalysisLanguageMapper.java
@@ -0,0 +1,13 @@
+package org.pmiops.workbench.utils.mappers;
+
+import org.mapstruct.Mapper;
+import org.mapstruct.MappingConstants;
+import org.mapstruct.ValueMapping;
+import org.pmiops.workbench.model.AnalysisLanguage;
+import org.pmiops.workbench.model.KernelTypeEnum;
+
+@Mapper(config = MapStructConfig.class)
+public interface AnalysisLanguageMapper {
+  @ValueMapping(source = "SAS", target = MappingConstants.NULL) // not a valid Jupyter kernel type
+  KernelTypeEnum analysisLanguageToKernelType(AnalysisLanguage analysisLanguage);
+}
