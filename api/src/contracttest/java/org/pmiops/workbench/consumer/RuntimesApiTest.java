06fae4255281edc213e925ff6494f0108424036b
Eric Rollins <erollins@broadinstitute.org>
2024-03-11 16:57:32 -0400

    Cleanup.

--- api/src/contracttest/java/org/pmiops/workbench/consumer/RuntimesApiTest.java
+++ api/src/contracttest/java/org/pmiops/workbench/consumer/RuntimesApiTest.java
@@ -30,8 +30,10 @@ import org.pmiops.workbench.leonardo.model.LeonardoUpdateRuntimeRequest;
 class RuntimesApiTest {
 
   static Map<String, String> contentTypeJsonHeader = Map.of("Content-Type", "application/json");
-  static String gcrRegex = "(^((?:us\\.|eu\\.|asia\\.)?gcr.io)/([\\w.-]+/[\\w.-]+)(?::(\\w[\\w.-]+))?(?:@([\\w+.-]+:[A-Fa-f0-9]{32,}))?$)";
-  static String ghcrRegex = "(^(ghcr.io)/((?:[\\w.-]+/)+[\\w.-]+)(?::(\\w[\\w.-]+))?(?:@([\\w+.-]+:[A-Fa-f0-9]{32,}))?$)";
+  static String gcrRegex =
+      "(^((?:us\\.|eu\\.|asia\\.)?gcr.io)/([\\w.-]+/[\\w.-]+)(?::(\\w[\\w.-]+))?(?:@([\\w+.-]+:[A-Fa-f0-9]{32,}))?$)";
+  static String ghcrRegex =
+      "(^(ghcr.io)/((?:[\\w.-]+/)+[\\w.-]+)(?::(\\w[\\w.-]+))?(?:@([\\w+.-]+:[A-Fa-f0-9]{32,}))?$)";
 
   static String dockerRegex = gcrRegex + "|" + ghcrRegex;
   static LambdaDslJsonBody createBody =
@@ -47,7 +49,8 @@ class RuntimesApiTest {
             body.booleanType("autopause");
             body.numberType("autopauseThreshold");
             body.stringType("defaultClientId");
-            body.stringMatcher("toolDockerImage",dockerRegex, "us.gcr.io/example/image-for-contract-test:2.2.7");
+            body.stringMatcher(
+                "toolDockerImage", dockerRegex, "us.gcr.io/example/image-for-contract-test:2.2.7");
           });
 
   @Pact(consumer = "aou-rwb-api", provider = "leonardo")
