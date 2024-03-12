af787bb20f6934dcd87c5c3f143420f0e6308793
NehaBroad <34481816+NehaBroad@users.noreply.github.com>
2024-03-12 15:50:32 -0400

    [RW-11668][risk=no] Link to SAS support article from SAS cloud env page (#8430)

--- ui/src/app/components/gke-app-configuration-panels/create-sas.tsx
+++ ui/src/app/components/gke-app-configuration-panels/create-sas.tsx
@@ -2,8 +2,26 @@ import * as React from 'react';
 
 import { AppType } from 'generated/fetch';
 
+import { SupportNoteSection } from 'app/components/common-env-conf-panels/support-note-section';
+import { supportUrls } from 'app/utils/zendesk';
+
 import { CommonCreateGkeAppProps, CreateGkeApp } from './create-gke-app';
 
+const SASSupportArticles = [
+  {
+    text: 'How to run SAS in the Researcher Workbench',
+    link: supportUrls.sasHowToRun,
+  },
+  {
+    text: 'Exploring All of Us data using SAS',
+    link: supportUrls.sasExplore,
+  },
+];
+
 export const CreateSAS = (props: CommonCreateGkeAppProps) => (
-  <CreateGkeApp {...props} appType={AppType.SAS} />
+  <CreateGkeApp
+    {...props}
+    SupportNote={() => SupportNoteSection(AppType.SAS, SASSupportArticles)}
+    appType={AppType.SAS}
+  />
 );
