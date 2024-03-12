f26961146f9cf69c60f5d91470f5ec4a6a62c98f
NehaBroad <nsaxena@broadinstitute.org>
2024-03-12 15:36:43 -0400

    Use zendesk urls

--- ui/src/app/components/gke-app-configuration-panels/create-sas.tsx
+++ ui/src/app/components/gke-app-configuration-panels/create-sas.tsx
@@ -3,18 +3,18 @@ import * as React from 'react';
 import { AppType } from 'generated/fetch';
 
 import { SupportNoteSection } from 'app/components/common-env-conf-panels/support-note-section';
-import { SAS_EXPLORING, SAS_INTRO_LINK } from 'app/utils/aou_external_links';
+import { supportUrls } from 'app/utils/zendesk';
 
 import { CommonCreateGkeAppProps, CreateGkeApp } from './create-gke-app';
 
 const SASSupportArticles = [
   {
     text: 'How to run SAS in the Researcher Workbench',
-    link: SAS_INTRO_LINK,
+    link: supportUrls.sasHowToRun,
   },
   {
     text: 'Exploring All of Us data using SAS',
-    link: SAS_EXPLORING,
+    link: supportUrls.sasExplore,
   },
 ];
 
