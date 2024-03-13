f0c526711d0c05836287299fe8ebd247f422be66
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2024-03-13 10:40:03 -0400

    [risk=low][no ticket] Remove the unused "runtimeImages" config block (#8433)

--- api/src/main/java/org/pmiops/workbench/config/WorkbenchConfigMapper.java
+++ api/src/main/java/org/pmiops/workbench/config/WorkbenchConfigMapper.java
@@ -1,45 +1,20 @@
 package org.pmiops.workbench.config;
 
 import java.util.List;
-import java.util.stream.Collectors;
-import java.util.stream.Stream;
-import org.mapstruct.BeforeMapping;
 import org.mapstruct.Mapper;
 import org.mapstruct.Mapping;
-import org.mapstruct.MappingTarget;
 import org.pmiops.workbench.access.AccessModuleNameMapper;
 import org.pmiops.workbench.db.model.DbAccessModule;
-import org.pmiops.workbench.leonardo.model.LeonardoRuntimeConfig.CloudServiceEnum;
 import org.pmiops.workbench.model.AccessModuleConfig;
 import org.pmiops.workbench.model.ConfigResponse;
-import org.pmiops.workbench.model.RuntimeImage;
 import org.pmiops.workbench.utils.mappers.MapStructConfig;
 
 @Mapper(
     config = MapStructConfig.class,
     uses = {AccessModuleNameMapper.class})
 public interface WorkbenchConfigMapper {
-  default RuntimeImage dataprocToModel(String imageName) {
-    return new RuntimeImage().cloudService(CloudServiceEnum.DATAPROC.toString()).name(imageName);
-  }
-
-  default RuntimeImage gceToModel(String imageName) {
-    return new RuntimeImage().cloudService(CloudServiceEnum.GCE.toString()).name(imageName);
-  }
-
-  @BeforeMapping
-  default void mapRuntimeImages(WorkbenchConfig source, @MappingTarget ConfigResponse target) {
-    target.runtimeImages(
-        Stream.concat(
-                source.firecloud.runtimeImages.dataproc.stream().map(this::dataprocToModel),
-                source.firecloud.runtimeImages.gce.stream().map(this::gceToModel))
-            .collect(Collectors.toList()));
-  }
-
   AccessModuleConfig mapAccessModule(DbAccessModule accessModule);
 
-  // handled by mapRuntimeImages()
-  @Mapping(target = "runtimeImages", ignore = true)
   @Mapping(target = "accessRenewalLookback", source = "config.access.renewal.lookbackPeriod")
   @Mapping(target = "gsuiteDomain", source = "config.googleDirectoryService.gSuiteDomain")
   @Mapping(target = "projectId", source = "config.server.projectId")
