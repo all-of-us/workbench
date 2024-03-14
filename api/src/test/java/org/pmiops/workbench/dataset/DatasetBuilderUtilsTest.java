f88563c154e1b0fc1f617a475df7740a6fb25ca2
Yonghao Yu <yonghao@google.com>
2024-03-14 11:19:17 -0400

    update

--- api/src/test/java/org/pmiops/workbench/dataset/DatasetBuilderUtilsTest.java
+++ api/src/test/java/org/pmiops/workbench/dataset/DatasetBuilderUtilsTest.java
@@ -8,7 +8,7 @@ public class DatasetBuilderUtilsTest {
   @Test
   public void testSplitWithLineBreaks_MultipleChunks() {
     String input = "1,2,3,4,5,6,7,8,9,10,11,12,13,14,15,16,17,18,19,20";
-    String expected = "1,2,3,4,5\n6,7,8,9,10\n11,12,13,14,15\n16,17,18,19,20";
+    String expected = "1,2,3,4,5,\n6,7,8,9,10,\n,11,12,13,14,15,\n16,17,18,19,20";
     assertThat(DatasetBuilderUtils.splitWithLineBreaks(input, 5)).isEqualTo(expected);
   }
 
