6548b19f469530b2c0d637f6586c8c969bb24aa0
NehaBroad <nsaxena@broadinstitute.org>
2024-03-20 11:19:24 -0400

    Show donwload pop up when user selects NON jupyter file and clicks View

--- api/src/main/webapp/static/aou-file-tree-policy-extension.js
+++ api/src/main/webapp/static/aou-file-tree-policy-extension.js
@@ -15,12 +15,15 @@ define([
         'is uploaded into the Workspace should be exclusively for the research purpose that was ' +
         'provided for this Workspace.'));
 
-    // notebook-list.js in the Jupyter UI serves the download by using it's own
+    // notebook-list.js in the Jupyter UI serves the download and view button by using it's own
     // jQuery click handler. We have no way of ordering our click handler ahead
     // of it without digging into jQuery internals. Instead, we insert a span
     // on top of the button to gain click priority.
     $('.dynamic-buttons .download-button').empty().append(
         '<span id="aou-download-button-overlay">Download</span>');
+    $('.dynamic-buttons .view-button').empty().append(
+        '<span id="aou-view-button-overlay">View</span>');
+
     $('#aou-download-button-overlay').click(() => {
       const affirm = prompt(
         'The All of Us Data Use Policies prohibit you from removing participant-level data from ' +
@@ -30,6 +33,37 @@ define([
         'To continue, affirm that this download will be used in accordance with the All of Us ' +
         'data use policy by typing "affirm" below.');
       return !!affirm && 'affirm' === affirm.replace(/\W/g, '').toLowerCase();
+    });
+
+    // Clicking the view button will
+    // 1) Open All Jupyter files in a new tab.
+    // 2) Display a download policy prompt for all other files as they will be downloaded
+    $('#aou-view-button-overlay').click(() => {
+      var isJupyterFile = true;
+      $('#notebook_list input:checked').each(function() {
+        // Find the selected file name
+        const elem = $(this).siblings().find($('.item_link .item_name'));
+        const fileName = elem.text();
+        var fileExt = fileName.substring( (fileName.lastIndexOf('.') +1) );
+        if (fileExt !== 'ipynb') {
+          isJupyterFile = false;
+          return false;
+        }
+       });
+      // If all selected Files are jupyter files return now
+      if (!!isJupyterFile) {
+        return
+      }
+      
+      const affirm = prompt('The All of Us Data Use Policies prohibit you from removing participant-level data from ' +
+          'the workbench. You are also prohibited from publishing or otherwise distributing any data ' +
+          'or aggregate statistics corresponding to fewer than 20 participants unless ' +
+          'expressly permitted by our data use policies.\n\n' +
+          'To continue, affirm that this download will be used in accordance with the All of Us ' +
+          'data use policy by typing "affirm" below.');
+      return !!affirm && 'affirm' === affirm.replace(/\W/g, '').toLowerCase();
+
+
     });
   };
 
