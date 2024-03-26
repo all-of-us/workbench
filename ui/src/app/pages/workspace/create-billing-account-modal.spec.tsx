c0b7e0f34c6ed7260d6757a4d6234ec36f8e1812
Eric Rollins <erollins@broadinstitute.org>
2024-03-26 11:19:40 -0400

    Convert test with help of Copilot.

--- ui/src/app/pages/workspace/create-billing-account-modal.spec.tsx
+++ ui/src/app/pages/workspace/create-billing-account-modal.spec.tsx
@@ -1,8 +1,14 @@
-import * as React from 'react';
-import { mount } from 'enzyme';
+import '@testing-library/jest-dom';
 
+import * as React from 'react';
+
+import { fireEvent, render, screen } from '@testing-library/react';
 import { profileStore } from 'app/utils/stores';
 
+import {
+  expectButtonElementDisabled,
+  getHTMLInputElementValue,
+} from 'testing/react-test-helpers';
 import { BROAD } from 'testing/stubs/institution-api-stub';
 import { ProfileStubVariables } from 'testing/stubs/profile-api-stub';
 
@@ -15,7 +21,7 @@ describe('CreateBillingAccountModal', () => {
   const profile = ProfileStubVariables.PROFILE_STUB;
 
   const component = () => {
-    return mount(<CreateBillingAccountModal onClose={() => {}} />);
+    return render(<CreateBillingAccountModal onClose={() => {}} />);
   };
 
   beforeEach(() => {
@@ -23,116 +29,78 @@ describe('CreateBillingAccountModal', () => {
   });
 
   it('simulate creating billing account with all steps correctly', async () => {
-    const wrapper = component();
+    const { getByTestId } = component();
     // Show step 0 at start.
-    expect(wrapper.find('[data-test-id="step-0-modal"]').exists()).toBeTruthy();
-    expect(wrapper.find('[data-test-id="step-1-modal"]').exists()).toBeFalsy();
-    expect(wrapper.find('[data-test-id="step-2-modal"]').exists()).toBeFalsy();
-    expect(wrapper.find('[data-test-id="step-3-modal"]').exists()).toBeFalsy();
+    expect(screen.getByTestId('step-0-modal')).toBeInTheDocument();
+    expect(screen.queryByTestId('step-1-modal')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('step-2-modal')).not.toBeInTheDocument();
+    expect(screen.queryByTestId('step-3-modal')).not.toBeInTheDocument();
 
     // Show step 1 when click use-billing-partner-button button, verify value populated.
-    wrapper
-      .find('[data-test-id="use-billing-partner-button"]')
-      .first()
-      .simulate('click');
-    expect(wrapper.find('[data-test-id="step-0-modal"]').exists()).toBeFalsy();
-    expect(wrapper.find('[data-test-id="step-1-modal"]').exists()).toBeTruthy();
+    fireEvent.click(screen.getByText('USE A BILLING PARTNER'));
+    expect(screen.queryByTestId('step-0-modal')).not.toBeInTheDocument();
+    expect(screen.getByTestId('step-1-modal')).toBeInTheDocument();
 
     // The only enabled input is user phone number. Others are populated from profile API.
-    expect(
-      wrapper.find('[data-test-id="user-first-name"]').first().prop('disabled')
-    ).toBeTruthy();
-    expect(
-      wrapper.find('[data-test-id="user-last-name"]').first().prop('disabled')
-    ).toBeTruthy();
-    expect(
-      wrapper
-        .find('[data-test-id="user-contact-email"]')
-        .first()
-        .prop('disabled')
-    ).toBeTruthy();
-    expect(
-      wrapper
-        .find('[data-test-id="user-workbench-id"]')
-        .first()
-        .prop('disabled')
-    ).toBeTruthy();
-    expect(
-      wrapper.find('[data-test-id="user-institution"]').first().prop('disabled')
-    ).toBeTruthy();
-    expect(
-      wrapper
-        .find('[data-test-id="user-phone-number"]')
-        .first()
-        .prop('disabled')
-    ).toBeFalsy();
+    expect(getByTestId('user-first-name')).toBeDisabled();
+    expect(getByTestId('user-last-name')).toBeDisabled();
+    expect(getByTestId('user-contact-email')).toBeDisabled();
+    expect(getByTestId('user-workbench-id')).toBeDisabled();
+    expect(getByTestId('user-institution')).toBeDisabled();
+    expect(getByTestId('user-phone-number')).not.toBeDisabled();
 
-    expect(
-      wrapper.find('[data-test-id="user-first-name"]').first().prop('value')
-    ).toEqual('Tester!@#$%^&*()><script>alert("hello");</script>');
-    expect(
-      wrapper.find('[data-test-id="user-last-name"]').first().prop('value')
-    ).toEqual('MacTesterson!@#$%^&*()><script>alert("hello");</script>');
-    expect(
-      wrapper.find('[data-test-id="user-phone-number"]').first().prop('value')
-    ).toBeUndefined();
-    expect(
-      wrapper.find('[data-test-id="user-contact-email"]').first().prop('value')
-    ).toEqual('tester@mactesterson.edu><script>alert("hello");</script>');
-    expect(
-      wrapper.find('[data-test-id="user-workbench-id"]').first().prop('value')
-    ).toEqual('tester@fake-research-aou.org');
-    expect(
-      wrapper.find('[data-test-id="user-institution"]').first().prop('value')
-    ).toEqual(BROAD.displayName);
+    expect(getHTMLInputElementValue(getByTestId('user-first-name'))).toEqual(
+      'Tester!@#$%^&*()><script>alert("hello");</script>'
+    );
+    expect((getByTestId('user-last-name') as HTMLInputElement).value).toEqual(
+      'MacTesterson!@#$%^&*()><script>alert("hello");</script>'
+    );
+    expect(getHTMLInputElementValue(getByTestId('user-phone-number'))).toEqual(
+      ''
+    );
+    expect(getHTMLInputElementValue(getByTestId('user-contact-email'))).toEqual(
+      'tester@mactesterson.edu><script>alert("hello");</script>'
+    );
+    expect(getHTMLInputElementValue(getByTestId('user-workbench-id'))).toEqual(
+      'tester@fake-research-aou.org'
+    );
+    expect(getHTMLInputElementValue(getByTestId('user-institution'))).toEqual(
+      BROAD.displayName
+    );
 
     // Type invalid phone number then valid phone number
-    wrapper
-      .find('[data-test-id="user-phone-number"]')
-      .first()
-      .simulate('change', { target: { value: '1234x' } });
-    expect(
-      wrapper.find('[data-test-id="next-button"]').first().prop('disabled')
-    ).toBeTruthy();
-    wrapper
-      .find('[data-test-id="user-phone-number"]')
-      .first()
-      .simulate('change', { target: { value: '1234567890' } });
-    expect(
-      wrapper.find('[data-test-id="next-button"]').first().prop('disabled')
-    ).toBeFalsy();
+    fireEvent.change(getByTestId('user-phone-number'), {
+      target: { value: '1234x' },
+    });
+    expectButtonElementDisabled(screen.getByRole('button', { name: /next/i }));
+    fireEvent.change(getByTestId('user-phone-number'), {
+      target: { value: '1234567890' },
+    });
+    expect(screen.getByRole('button', { name: /next/i })).not.toBeDisabled();
 
     // Go to step 2 by clicking next button
-    wrapper.find('[data-test-id="next-button"]').first().simulate('click');
-    expect(wrapper.find('[data-test-id="step-1-modal"]').exists()).toBeFalsy();
-    expect(wrapper.find('[data-test-id="step-2-modal"]').exists()).toBeTruthy();
+    fireEvent.click(screen.getByRole('button', { name: /next/i }));
+    expect(screen.queryByTestId('step-1-modal')).not.toBeInTheDocument();
+    expect(screen.getByTestId('step-2-modal')).toBeInTheDocument();
 
-    expect(
-      wrapper.find('[data-test-id="user-first-name-text"]').first().props()
-        .children
-    ).toEqual('Tester!@#$%^&*()><script>alert("hello");</script>');
-    expect(
-      wrapper.find('[data-test-id="user-last-name-text"]').first().props()
-        .children
-    ).toEqual('MacTesterson!@#$%^&*()><script>alert("hello");</script>');
-    expect(
-      wrapper.find('[data-test-id="user-phone-number-text"]').first().props()
-        .children
-    ).toEqual('1234567890');
-    expect(
-      wrapper.find('[data-test-id="user-contact-email-text"]').first().props()
-        .children
-    ).toEqual('tester@mactesterson.edu><script>alert("hello");</script>');
-    expect(
-      wrapper.find('[data-test-id="user-workbench-id-text"]').first().props()
-        .children
-    ).toEqual('tester@fake-research-aou.org');
-    expect(
-      wrapper.find('[data-test-id="user-institution-text"]').first().props()
-        .children
-    ).toEqual(BROAD.displayName);
-    expect(
-      wrapper.find('[data-test-id="nih-funded-text"]').first().props().children
-    ).toEqual('No');
+    expect(screen.getByTestId('user-first-name-text')).toHaveTextContent(
+      'Tester!@#$%^&*()><script>alert("hello");</script>'
+    );
+    expect(screen.getByTestId('user-last-name-text')).toHaveTextContent(
+      'MacTesterson!@#$%^&*()><script>alert("hello");</script>'
+    );
+    expect(screen.getByTestId('user-phone-number-text')).toHaveTextContent(
+      '1234567890'
+    );
+    expect(screen.getByTestId('user-contact-email-text')).toHaveTextContent(
+      'tester@mactesterson.edu><script>alert("hello");</script>'
+    );
+    expect(screen.getByTestId('user-workbench-id-text')).toHaveTextContent(
+      'tester@fake-research-aou.org'
+    );
+    expect(screen.getByTestId('user-institution-text')).toHaveTextContent(
+      BROAD.displayName
+    );
+    expect(screen.getByTestId('nih-funded-text')).toHaveTextContent('No');
   });
 });
