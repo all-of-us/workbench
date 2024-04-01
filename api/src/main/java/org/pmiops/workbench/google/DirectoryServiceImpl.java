e8c746f9f43e2cc658ef4dee2b5ccca5bd60e033
Joel Thibault <thibault@broad.dev>
2024-04-01 10:27:12 -0400

    rm unused metrics code

--- api/src/main/java/org/pmiops/workbench/google/DirectoryServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/google/DirectoryServiceImpl.java
@@ -36,8 +36,6 @@ import org.pmiops.workbench.auth.ServiceAccounts;
 import org.pmiops.workbench.config.WorkbenchConfig;
 import org.pmiops.workbench.exceptions.ExceptionUtils;
 import org.pmiops.workbench.exceptions.NotFoundException;
-import org.slf4j.Logger;
-import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.stereotype.Service;
@@ -45,7 +43,6 @@ import org.springframework.stereotype.Service;
 @Service
 public class DirectoryServiceImpl implements DirectoryService {
 
-  private static final Logger log = LoggerFactory.getLogger(DirectoryService.class.getName());
   private static final String ALLOWED =
       "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuvwxyz0123456789!@#$%^&*()";
   private static final String APPLICATION_NAME = "All of Us Researcher Workbench";
@@ -62,9 +59,6 @@ public class DirectoryServiceImpl implements DirectoryService {
   // Details: https://docs.google.com/document/d/1xgcvow0xPL6K4vxyM3PVlW-1E5Ye3X1Y5wZSD5rrPlc
   private static final String GSUITE_FIELD_ABSORB_EXTERNAL_DEPARTMENT_ID =
       "Absorb_external_department_ID";
-  private static final int MAX_USERS_LIST_PAGE_SIZE = 500;
-  private static final String EMAIL_USER_FIELD = "email";
-  private static final String USER_VIEW_TYPE = "domain_public";
 
   private static final String ADMIN_SERVICE_ACCOUNT_NAME = "gsuite-admin";
 
@@ -127,15 +121,6 @@ public class DirectoryServiceImpl implements DirectoryService {
 
   private String gSuiteDomain() {
     return configProvider.get().googleDirectoryService.gSuiteDomain;
-  }
-
-  private String getTopLevelGSuiteDomain() {
-    final String domain = gSuiteDomain();
-    final String[] parts = domain.split("\\.");
-    // return the last two parts
-    final int numParts = parts.length;
-    final int firstIndex = numParts - 2;
-    return String.format("%s.%s", parts[firstIndex], parts[firstIndex + 1]);
   }
 
   /**
@@ -310,35 +295,6 @@ public class DirectoryServiceImpl implements DirectoryService {
   @Override
   public void signOut(String username) {
     retryHandler.run((context) -> getGoogleDirectoryService().users().signOut(username).execute());
-  }
-
-  private long countUsersInDomain(String gSuiteDomain) {
-    long result = 0;
-    try {
-      final Directory directoryService = getGoogleDirectoryService();
-      Optional<String> nextPageToken = Optional.empty();
-      do {
-        final Directory.Users.List listQuery =
-            directoryService
-                .users()
-                .list()
-                .setDomain(gSuiteDomain)
-                .setViewType(USER_VIEW_TYPE)
-                .setCustomFieldMask("email")
-                .setMaxResults(MAX_USERS_LIST_PAGE_SIZE)
-                .setOrderBy(EMAIL_USER_FIELD);
-        nextPageToken.ifPresent(listQuery::setPageToken);
-
-        final Users usersQueryResult = listQuery.execute();
-
-        result += usersQueryResult.getUsers().size();
-        nextPageToken = Optional.ofNullable(usersQueryResult.getNextPageToken());
-      } while (nextPageToken.isPresent());
-      return result;
-    } catch (IOException e) {
-      log.warn("Failed to retrieve GSuite User List.", e);
-      return 0;
-    }
   }
 
   private String randomString() {
