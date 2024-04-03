79f538f1eaf17a31097a547ff69befd4d462544a
Will Dolbeer <william.dolbeer@vumc.org>
2024-04-03 00:09:44 -0400

    DT-796 fix ui test

--- ui/src/app/pages/data/cohort/variant-search.spec.tsx
+++ ui/src/app/pages/data/cohort/variant-search.spec.tsx
@@ -6,13 +6,14 @@ import { CohortBuilderApi } from 'generated/fetch';
 
 import { VariantSearch } from 'app/pages/data/cohort/variant-search';
 import { registerApiClient } from 'app/services/swagger-fetch-clients';
-import { currentWorkspaceStore } from 'app/utils/navigation';
+import { currentCohortSearchContextStore, currentWorkspaceStore } from 'app/utils/navigation';
 
 import { CohortBuilderServiceStub } from 'testing/stubs/cohort-builder-service-stub';
 import { workspaceDataStub } from 'testing/stubs/workspaces';
 
 describe('VariantSearch', () => {
   beforeEach(() => {
+    currentCohortSearchContextStore.next({});
     currentWorkspaceStore.next(workspaceDataStub);
     registerApiClient(CohortBuilderApi, new CohortBuilderServiceStub());
   });
