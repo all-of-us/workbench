752a841be2250ed69a5565ba33d11d825aeb6c75
Eric Rollins <erollins@broadinstitute.org>
2024-04-12 10:30:35 -0400

    Convert cohort overview test to RTL.

--- ui/src/app/pages/data/cohort/overview.spec.tsx
+++ ui/src/app/pages/data/cohort/overview.spec.tsx
@@ -1,11 +1,32 @@
+import '@testing-library/jest-dom';
+
 import * as React from 'react';
-import { shallow } from 'enzyme';
+
+import { Cohort } from 'generated/fetch';
+
+import { renderWithRouter } from '../../../../testing/react-test-helpers';
+import { exampleCohortStubs } from '../../../../testing/stubs/cohorts-api-stub';
+import { screen } from '@testing-library/react';
+
+import { workspaceDataStub } from 'testing/stubs/workspaces';
 
 import { ListOverview } from './overview';
 
 describe('ListOverview', () => {
+  const searchRequestStub = { includes: [], excludes: [], dataFilters: [] };
   it('should render', () => {
-    const wrapper = shallow(<ListOverview searchRequest={{}} update={0} />);
-    expect(wrapper.exists()).toBeTruthy();
+    renderWithRouter(
+      <ListOverview
+        searchRequest={searchRequestStub}
+        update={0}
+        cohort={exampleCohortStubs[0]}
+        workspace={workspaceDataStub}
+      />
+    );
+    expect(
+      screen.getByRole('button', {
+        name: /save cohort/i,
+      })
+    ).toBeInTheDocument();
   });
 });
