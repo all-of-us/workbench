eed75c84db1e38033a747f50a7c475e57d7a3343
Eric Rollins <erollins@broadinstitute.org>
2024-04-12 10:52:07 -0400

    Convert cohort tree test to RTL.

--- ui/src/app/pages/data/cohort/tree.spec.tsx
+++ ui/src/app/pages/data/cohort/tree.spec.tsx
@@ -1,8 +1,15 @@
+import '@testing-library/jest-dom';
+
 import * as React from 'react';
-import { shallow } from 'enzyme';
 
-import { CohortBuilderApi } from 'generated/fetch';
+import {
+  CohortBuilderApi,
+  CriteriaSubType,
+  CriteriaType,
+  Domain,
+} from 'generated/fetch';
 
+import { render, screen } from '@testing-library/react';
 import { registerApiClient } from 'app/services/swagger-fetch-clients';
 import { currentWorkspaceStore } from 'app/utils/navigation';
 
@@ -12,6 +19,28 @@ import { workspaceDataStub } from 'testing/stubs/workspaces';
 import { CriteriaTree } from './tree';
 
 describe('CriteriaTree', () => {
+  const criteriaStub = {
+    childCount: 197614,
+    code: '',
+    conceptId: 903133,
+    count: 197614,
+    domainId: Domain.PHYSICAL_MEASUREMENT.toString(),
+    group: false,
+    hasAncestorData: false,
+    hasAttributes: true,
+    hasHierarchy: true,
+    id: 328005,
+    standard: false,
+    name: 'Height',
+    parentCount: 0,
+    parentId: 0,
+    path: '',
+    selectable: true,
+    subtype: CriteriaSubType.HEIGHT.toString(),
+    type: CriteriaType.PPI.toString(),
+    value: '',
+  };
+  const searchTermsStub = 'example search';
   beforeEach(() => {
     registerApiClient(CohortBuilderApi, new CohortBuilderServiceStub());
     currentWorkspaceStore.next({
@@ -20,7 +49,13 @@ describe('CriteriaTree', () => {
     });
   });
   it('should create', () => {
-    const wrapper = shallow(<CriteriaTree />);
-    expect(wrapper).toBeTruthy();
+    render(
+      <CriteriaTree
+        node={criteriaStub}
+        domain={Domain.SURVEY}
+        searchTerms={searchTermsStub}
+      />
+    );
+    expect(screen.getByDisplayValue(searchTermsStub)).toBeInTheDocument();
   });
 });
