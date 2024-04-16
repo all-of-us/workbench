2a8a9eb0630af9396d3228b260dcf354cbe4f947
Yonghao Yu <yonghao@google.com>
2024-04-16 11:54:58 -0400

    fix test

--- ui/src/app/pages/signed-in/inactivity-monitor.spec.tsx
+++ ui/src/app/pages/signed-in/inactivity-monitor.spec.tsx
@@ -5,9 +5,14 @@ import { environment } from 'environments/environment';
 import { InactivityMonitor } from 'app/pages/signed-in/inactivity-monitor';
 import * as Authentication from 'app/utils/authentication';
 import { setLastActive } from 'app/utils/inactivity';
-import { authStore, notificationStore } from 'app/utils/stores';
+import { authStore, notificationStore, profileStore } from 'app/utils/stores';
 
 import { waitOneTickAndUpdate } from 'testing/react-test-helpers';
+import { ProfileStubVariables } from 'testing/stubs/profile-api-stub';
+
+const load = jest.fn();
+const reload = jest.fn();
+const updateCache = jest.fn();
 
 describe(InactivityMonitor.name, () => {
   it('should show an error when signout fails', async () => {
@@ -18,6 +23,12 @@ describe(InactivityMonitor.name, () => {
     authStore.set({
       authLoaded: true,
       isSignedIn: true,
+    });
+    profileStore.set({
+      profile: ProfileStubVariables.PROFILE_STUB,
+      load,
+      reload,
+      updateCache,
     });
 
     setLastActive(
