a426655f3d538cf69d17ec6ef926dc66e9885c14
Eric Rollins <erollins@broadinstitute.org>
2024-04-17 13:09:14 -0400

    Cleanup

--- ui/src/app/pages/data/cohort-review/annotation-definition-modals.component.spec.tsx
+++ ui/src/app/pages/data/cohort-review/annotation-definition-modals.component.spec.tsx
@@ -2,9 +2,9 @@ import '@testing-library/jest-dom';
 
 import * as React from 'react';
 
-import { renderWithRouter } from '../../../../testing/react-test-helpers';
 import { screen } from '@testing-library/react';
 
+import { renderWithRouter } from 'testing/react-test-helpers';
 import { cohortAnnotationDefinitionStub } from 'testing/stubs/cohort-annotation-definition-service-stub';
 
 import {
