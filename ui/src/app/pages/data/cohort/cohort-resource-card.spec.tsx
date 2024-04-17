cc09b79cfb56fbb62b5263668debcc76ed5460b2
Eric Rollins <erollins@broadinstitute.org>
2024-04-17 13:18:59 -0400

    Cleanup

--- ui/src/app/pages/data/cohort/cohort-resource-card.spec.tsx
+++ ui/src/app/pages/data/cohort/cohort-resource-card.spec.tsx
@@ -2,7 +2,7 @@ import '@testing-library/jest-dom';
 
 import * as React from 'react';
 
-import { render, screen } from '@testing-library/react';
+import { screen } from '@testing-library/react';
 import { CohortResourceCard } from 'app/pages/data/cohort/cohort-resource-card';
 
 import { renderWithRouter } from 'testing/react-test-helpers';
