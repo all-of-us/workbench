4e3c87541f85c0099911112a158a28b486dc639b
Joel Thibault <thibault@broad.dev>
2024-04-25 12:39:40 -0400

    rm style

--- ui/src/app/components/card.tsx
+++ ui/src/app/components/card.tsx
@@ -33,13 +33,6 @@ export const styles = reactStyles({
     minHeight: '223px',
     maxHeight: '223px',
   },
-  resourceCard: {
-    ...baseStyles.card,
-    minWidth: '200px',
-    maxWidth: '200px',
-    minHeight: '223px',
-    maxHeight: '223px',
-  },
   cohortActionCard: {
     ...baseStyles.card,
     width: '30%',
