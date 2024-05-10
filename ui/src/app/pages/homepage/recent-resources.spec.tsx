897f547be996011c99a2943654b1c88fceb9674a
Eric Rollins <erollins@broadinstitute.org>
2024-05-10 16:10:00 -0400

    Converted recent-resources.spec.tsx to RTL.

--- ui/src/app/pages/homepage/recent-resources.spec.tsx
+++ ui/src/app/pages/homepage/recent-resources.spec.tsx
@@ -1,9 +1,9 @@
 import * as React from 'react';
 import { MemoryRouter } from 'react-router-dom';
-import { mount } from 'enzyme';
 
 import { UserMetricsApi, WorkspaceAccessLevel } from 'generated/fetch';
 
+import { findByText, render, screen, waitFor } from '@testing-library/react';
 import {
   MODIFIED_DATE_COLUMN_NUMBER,
   NAME_COLUMN_NUMBER,
@@ -13,7 +13,10 @@ import {
 import { RecentResources } from 'app/pages/homepage/recent-resources';
 import { registerApiClient } from 'app/services/swagger-fetch-clients';
 
-import { waitOneTickAndUpdate } from 'testing/react-test-helpers';
+import {
+  renderWithRouter,
+  waitOneTickAndUpdate,
+} from 'testing/react-test-helpers';
 import {
   UserMetricsApiStub,
   userMetricsApiStubResources,
@@ -26,54 +29,31 @@ describe('RecentResourcesComponent', () => {
   });
 
   it('should render resources in a workspace', async () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <RecentResources
-          workspaces={[
-            {
-              workspace: workspaceStubs[0],
-              accessLevel: WorkspaceAccessLevel.OWNER,
-            },
-          ]}
-        />
-      </MemoryRouter>
+    renderWithRouter(
+      <RecentResources
+        workspaces={[
+          {
+            workspace: workspaceStubs[0],
+            accessLevel: WorkspaceAccessLevel.OWNER,
+          },
+        ]}
+      />
     );
-    await waitOneTickAndUpdate(wrapper);
-    expect(wrapper.exists()).toBeTruthy();
+    await screen.findByText('Recently Accessed Items');
 
+    expect(screen.getByText('Cohort')).toBeInTheDocument();
     expect(
-      resourceTableColumns(wrapper)
-        .at(RESOURCE_TYPE_COLUMN_NUMBER)
-        .find('div')
-        .first()
-        .text()
-    ).toBe('Cohort');
-    expect(
-      resourceTableColumns(wrapper)
-        .at(NAME_COLUMN_NUMBER)
-        .find('div')
-        .first()
-        .text()
-    ).toBe(userMetricsApiStubResources[0].cohort.name);
+      screen.getByText(userMetricsApiStubResources[0].cohort.name)
+    ).toBeInTheDocument();
   });
 
   it('should not render resources when their workspace is not available', async () => {
-    const wrapper = mount(
-      <MemoryRouter>
-        <RecentResources workspaces={[]} />
-      </MemoryRouter>
-    );
-    await waitOneTickAndUpdate(wrapper);
-    expect(wrapper.exists()).toBeTruthy();
+    renderWithRouter(<RecentResources workspaces={[]} />);
 
-    expect(
-      resourceTableColumns(wrapper).at(RESOURCE_TYPE_COLUMN_NUMBER).exists()
-    ).toBeFalsy();
-    expect(
-      resourceTableColumns(wrapper).at(NAME_COLUMN_NUMBER).exists()
-    ).toBeFalsy();
-    expect(
-      resourceTableColumns(wrapper).at(MODIFIED_DATE_COLUMN_NUMBER).exists()
-    ).toBeFalsy();
+    await screen.findByText('Recently Accessed Items');
+
+    expect(screen.queryByText(/item type/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/name/i)).not.toBeInTheDocument();
+    expect(screen.queryByText(/last modified date/i)).not.toBeInTheDocument();
   });
 });
