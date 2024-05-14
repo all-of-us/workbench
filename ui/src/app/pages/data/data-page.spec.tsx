9623e47620854b89d3b1f6c16377d29ad84e6713
Eric Rollins <erollins@broadinstitute.org>
2024-05-14 10:24:01 -0400

    Converted data-page.spec.tsx to RTL.

--- ui/src/app/pages/data/data-page.spec.tsx
+++ ui/src/app/pages/data/data-page.spec.tsx
@@ -10,14 +10,18 @@ import {
   WorkspacesApi,
 } from 'generated/fetch';
 
-import { resourceTableRows } from 'app/components/resources/resource-list.spec';
+import { screen, waitFor, within } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
 import { DataComponent } from 'app/pages/data/data-component';
 import { registerApiClient } from 'app/services/swagger-fetch-clients';
 import { ROWS_PER_PAGE_RESOURCE_TABLE } from 'app/utils/constants';
 import { currentWorkspaceStore } from 'app/utils/navigation';
 import { serverConfigStore } from 'app/utils/stores';
 
-import { waitOneTickAndUpdate } from 'testing/react-test-helpers';
+import {
+  renderWithRouter,
+  waitOneTickAndUpdate,
+} from 'testing/react-test-helpers';
 import {
   CohortReviewServiceStub,
   cohortReviewStubs,
@@ -31,8 +35,13 @@ import { DataSetApiStub } from 'testing/stubs/data-set-api-stub';
 import { workspaceDataStub } from 'testing/stubs/workspaces';
 import { WorkspacesApiStub } from 'testing/stubs/workspaces-api-stub';
 
+export const resourceTableRows = () =>
+  within(screen.getAllByRole('rowgroup')[1]).getAllByRole('row');
+
 describe('DataPage', () => {
+  let user;
   beforeEach(() => {
+    user = userEvent.setup();
     registerApiClient(CohortReviewApi, new CohortReviewServiceStub());
     registerApiClient(CohortsApi, new CohortsApiStub());
     registerApiClient(ConceptSetsApi, new ConceptSetsApiStub());
@@ -45,102 +54,85 @@ describe('DataPage', () => {
   });
 
   const component = () => {
-    return mount(
-      <MemoryRouter>
-        <DataComponent hideSpinner={() => {}} showSpinner={() => {}} />
-      </MemoryRouter>
+    return renderWithRouter(
+      <DataComponent hideSpinner={() => {}} showSpinner={() => {}} />
     );
   };
 
   it('should render', async () => {
-    const wrapper = component();
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-    expect(wrapper.exists()).toBeTruthy();
+    component();
+    await screen.findByLabelText('Please Wait');
   });
 
   it('should show all datasets, cohorts, and concept sets', async () => {
-    const wrapper = component();
+    component();
     const resourceTableRowsExpected =
       ConceptSetsApiStub.stubConceptSets().length +
       exampleCohortStubs.length +
       cohortReviewStubs.length +
       DataSetApiStub.stubDataSets().length;
-    await waitOneTickAndUpdate(wrapper);
 
     expect(resourceTableRowsExpected).toBeGreaterThan(
       ROWS_PER_PAGE_RESOURCE_TABLE
     );
-    // Since we have pagination only 10 rows at a time should be displayed
-    expect(resourceTableRows(wrapper).length).toBe(
-      ROWS_PER_PAGE_RESOURCE_TABLE
+
+    await waitFor(() =>
+      expect(screen.queryByLabelText('Please Wait')).not.toBeInTheDocument()
     );
 
+    // Since we have pagination only 10 rows at a time should be displayed
+    expect(resourceTableRows().length).toBe(ROWS_PER_PAGE_RESOURCE_TABLE);
+
     // Click the next button to confirm the number of rows
-    const paginationNextButton = wrapper
-      .find('button')
-      .findWhere(
-        (element) =>
-          element.prop('className') ===
-          'p-paginator-next p-paginator-element p-link'
-      );
-    paginationNextButton.simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-    expect(resourceTableRows(wrapper).length).toBe(
+    const paginationNextButton = screen.getByRole('button', {
+      name: /next page/i,
+    });
+
+    await user.click(paginationNextButton);
+    expect(resourceTableRows().length).toBe(
       resourceTableRowsExpected - ROWS_PER_PAGE_RESOURCE_TABLE
     );
   });
 
   it('should show only cohorts when selected', async () => {
-    const wrapper = component();
+    component();
     const resourceTableRowsExpected = exampleCohortStubs.length;
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-    wrapper
-      .find('[data-test-id="view-only-cohorts"]')
-      .first()
-      .simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-    expect(resourceTableRows(wrapper).length).toBe(resourceTableRowsExpected);
+
+    const cohortsFilterButton = await screen.findByRole('button', {
+      name: 'Cohorts',
+    });
+    await user.click(cohortsFilterButton);
+    expect(resourceTableRows().length).toBe(resourceTableRowsExpected);
   });
 
   it('should show only cohort reviews when selected', async () => {
-    const wrapper = component();
+    component();
     const resourceTableRowsExpected = cohortReviewStubs.length;
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-    wrapper
-      .find('[data-test-id="view-only-cohort-reviews"]')
-      .first()
-      .simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-    expect(resourceTableRows(wrapper).length).toBe(resourceTableRowsExpected);
+    const cohortReviewsFilterButton = await screen.findByRole('button', {
+      name: 'Cohort Reviews',
+    });
+    await user.click(cohortReviewsFilterButton);
+    expect(resourceTableRows().length).toBe(resourceTableRowsExpected);
   });
 
   it('should show only conceptSets when selected', async () => {
-    const wrapper = component();
+    component();
     const resourceTableRowsExpected =
       ConceptSetsApiStub.stubConceptSets().length;
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-    wrapper
-      .find('[data-test-id="view-only-concept-sets"]')
-      .first()
-      .simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-    expect(resourceTableRows(wrapper).length).toBe(resourceTableRowsExpected);
+    const conceptSetsFilterButton = await screen.findByRole('button', {
+      name: 'Concept Sets',
+    });
+    await user.click(conceptSetsFilterButton);
+    expect(resourceTableRows().length).toBe(resourceTableRowsExpected);
   });
 
   it('should show only dataSets when selected', async () => {
-    const wrapper = component();
+    component();
     const resourceTableRowsExpected = DataSetApiStub.stubDataSets().length;
-    await waitOneTickAndUpdate(wrapper);
-    await waitOneTickAndUpdate(wrapper);
-    wrapper
-      .find('[data-test-id="view-only-data-sets"]')
-      .first()
-      .simulate('click');
-    await waitOneTickAndUpdate(wrapper);
-    expect(resourceTableRows(wrapper).length).toBe(resourceTableRowsExpected);
+    const datatsetsFilterButton = await screen.findByRole('button', {
+      name: 'Datasets',
+    });
+    await user.click(datatsetsFilterButton);
+    expect(resourceTableRows().length).toBe(resourceTableRowsExpected);
   });
 });
