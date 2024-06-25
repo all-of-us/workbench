fec23099ff924c31c6aa0ba99447b6ad0bf2a475
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2024-06-25 16:06:54 -0400

    [risk=low][RW-11850] Remove uses of stored credentials in CircleCI (#8626)
    * centralize test and staging params
    * rm stored-credentials
    * rm unused scripts

--- .circleci/SA_KEY_JSON.sh
+++
@@ -1,6 +0,0 @@
-# Redirection here suppresses this warning:
-#   *** WARNING : deprecated key derivation used.
-#   Using -iter or -pbkdf2 would be better.
-SA_KEY_JSON="$(echo "$GCLOUD_CREDENTIALS" \
-  | openssl enc -d -md sha256 -aes-256-cbc -base64 -A -k "$GCLOUD_CREDENTIALS_KEY" 2>/dev/null)"
-
