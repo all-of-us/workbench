a23fb101d6f9b2ab2727c0c450a83376adf3dca8
Eric Rollins <erollins@broadinstitute.org>
2024-07-08 16:58:01 -0400

    Updated published dropdown to use appropriate enum values.

--- ui/src/app/pages/admin/workspace/basic-information.tsx
+++ ui/src/app/pages/admin/workspace/basic-information.tsx
@@ -3,6 +3,7 @@ import { Link } from 'react-router-dom';
 
 import {
   AnnotationType,
+  FeaturedWorkspaceCategory,
   Workspace,
   WorkspaceActiveStatus,
 } from 'generated/fetch';
@@ -58,10 +59,19 @@ export const BasicInformation = ({ workspace, activeStatus }: Props) => (
         <Select
           value={null}
           options={[
-            { value: 'COMMUNITY', label: 'Community' },
-            { value: 'DEMO_PROJECTS', label: 'Demo Projects' },
-            { value: 'PHENOTYPE_LIBRAY', label: 'Phenotype Library' },
-            { value: 'TUTORIAL_WORKSPACES', label: 'Tutorial Workspaces' },
+            {
+              value: FeaturedWorkspaceCategory.DEMO_PROJECTS,
+              label: 'Demo Projects',
+            },
+            {
+              value: FeaturedWorkspaceCategory.PHENOTYPE_LIBRARY,
+              label: 'Phenotype Library',
+            },
+            {
+              value: FeaturedWorkspaceCategory.TUTORIAL_WORKSPACES,
+              label: 'Tutorial Workspaces',
+            },
+            { value: FeaturedWorkspaceCategory.COMMUNITY, label: 'Community' },
           ]}
           onChange={(v) => console.log(v)}
         />
