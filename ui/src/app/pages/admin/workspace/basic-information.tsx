fc195e117e91b47843cc87d172daf2e691322c86
Eric Rollins <erollins@broadinstitute.org>
2024-07-19 16:59:37 -0400

    Fixed 'should show unpublished workspace'

--- ui/src/app/pages/admin/workspace/basic-information.tsx
+++ ui/src/app/pages/admin/workspace/basic-information.tsx
@@ -19,74 +19,93 @@ interface Props {
   workspace: Workspace;
   activeStatus: WorkspaceActiveStatus;
 }
-export const BasicInformation = ({ workspace, activeStatus }: Props) => (
-  <>
-    <h3>Basic Information</h3>
-    <div className='basic-info' style={{ marginTop: '1.5rem' }}>
-      <WorkspaceInfoField labelText='Active Status'>
-        {activeStatus}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Billing Status'>
-        {workspace.billingStatus}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Workspace Name'>
-        {workspace.name}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Terra Name (often incorrectly called "id")'>
-        {workspace.id}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Workspace Namespace'>
-        {workspace.namespace}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Access Tier'>
-        {workspace.accessTierShortName?.toUpperCase()}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Google Project Id'>
-        {workspace.googleProject}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Billing Account Type'>
-        {isUsingFreeTierBillingAccount(workspace)
-          ? 'Initial credits'
-          : 'User provided'}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Creation Time'>
-        {new Date(workspace.creationTime).toDateString()}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Last Modified Time'>
-        {new Date(workspace.lastModifiedTime).toDateString()}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Workspace Published'>
-        <Select
-          value={null}
-          options={[
-            {
-              value: FeaturedWorkspaceCategory.DEMO_PROJECTS,
-              label: 'Demo Projects',
-            },
-            {
-              value: FeaturedWorkspaceCategory.PHENOTYPE_LIBRARY,
-              label: 'Phenotype Library',
-            },
-            {
-              value: FeaturedWorkspaceCategory.TUTORIAL_WORKSPACES,
-              label: 'Tutorial Workspaces',
-            },
-            { value: FeaturedWorkspaceCategory.COMMUNITY, label: 'Community' },
-          ]}
-          onChange={(v) => console.log(v)}
-        />
-        <Button type='primary' disabled>
-          Publish
-        </Button>
-        <Button type='secondaryOutline'>Unpublish</Button>
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Audit'>
-        {
-          <Link to={`/admin/workspace-audit/${workspace.namespace}`}>
-            Audit History
-          </Link>
-        }
-      </WorkspaceInfoField>
-    </div>
-  </>
-);
+export const BasicInformation = ({ workspace, activeStatus }: Props) => {
+  const [featuredCategory, setFeaturedCategory] =
+    React.useState<FeaturedWorkspaceCategory>(workspace.featuredCategory);
+  return (
+    <>
+      <h3>Basic Information</h3>
+      <div className='basic-info' style={{ marginTop: '1.5rem' }}>
+        <WorkspaceInfoField labelText='Active Status'>
+          {activeStatus}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Billing Status'>
+          {workspace.billingStatus}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Workspace Name'>
+          {workspace.name}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Terra Name (often incorrectly called "id")'>
+          {workspace.id}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Workspace Namespace'>
+          {workspace.namespace}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Access Tier'>
+          {workspace.accessTierShortName?.toUpperCase()}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Google Project Id'>
+          {workspace.googleProject}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Billing Account Type'>
+          {isUsingFreeTierBillingAccount(workspace)
+            ? 'Initial credits'
+            : 'User provided'}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Creation Time'>
+          {new Date(workspace.creationTime).toDateString()}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Last Modified Time'>
+          {new Date(workspace.lastModifiedTime).toDateString()}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Workspace Published'>
+          <Select
+            value={workspace.featuredCategory}
+            placeholder='Select a category...'
+            options={[
+              {
+                value: FeaturedWorkspaceCategory.DEMO_PROJECTS,
+                label: 'Demo Projects',
+              },
+              {
+                value: FeaturedWorkspaceCategory.PHENOTYPE_LIBRARY,
+                label: 'Phenotype Library',
+              },
+              {
+                value: FeaturedWorkspaceCategory.TUTORIAL_WORKSPACES,
+                label: 'Tutorial Workspaces',
+              },
+              {
+                value: FeaturedWorkspaceCategory.COMMUNITY,
+                label: 'Community',
+              },
+            ]}
+            onChange={(v) => setFeaturedCategory(v)}
+          />
+          <Button
+            type='primary'
+            disabled={
+              !featuredCategory ||
+              featuredCategory === workspace.featuredCategory
+            }
+          >
+            Publish
+          </Button>
+          <Button
+            type='secondaryOutline'
+            disabled={!workspace.featuredCategory}
+          >
+            Unpublish
+          </Button>
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Audit'>
+          {
+            <Link to={`/admin/workspace-audit/${workspace.namespace}`}>
+              Audit History
+            </Link>
+          }
+        </WorkspaceInfoField>
+      </div>
+    </>
+  );
+};
