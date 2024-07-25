3470e3f7ab48a281f9502fc57a96b64cab3a5ef5
Eric Rollins <erollins@broadinstitute.org>
2024-07-25 09:50:54 -0400

    [RW-12090][risk=no] Added ability for admin to change a workspace's featuredCategory (#8632)

--- ui/src/app/pages/admin/workspace/basic-information.tsx
+++ ui/src/app/pages/admin/workspace/basic-information.tsx
@@ -1,8 +1,19 @@
 import * as React from 'react';
+import { useEffect } from 'react';
 import { Link } from 'react-router-dom';
 
-import { Workspace, WorkspaceActiveStatus } from 'generated/fetch';
+import {
+  FeaturedWorkspaceCategory,
+  Workspace,
+  WorkspaceActiveStatus,
+} from 'generated/fetch';
 
+import { Button } from 'app/components/buttons';
+import { Select } from 'app/components/inputs';
+import { TooltipTrigger } from 'app/components/popups';
+import { Spinner } from 'app/components/spinners';
+import { FeaturedWorkspaceCategoryOptions } from 'app/pages/admin/admin-featured-category-options';
+import { workspaceAdminApi } from 'app/services/swagger-fetch-clients';
 import { isUsingFreeTierBillingAccount } from 'app/utils/workspace-utils';
 
 import { WorkspaceInfoField } from './workspace-info-field';
@@ -10,53 +21,130 @@ import { WorkspaceInfoField } from './workspace-info-field';
 interface Props {
   workspace: Workspace;
   activeStatus: WorkspaceActiveStatus;
+  reload: () => Promise<void>;
 }
-export const BasicInformation = ({ workspace, activeStatus }: Props) => (
-  <>
-    <h3>Basic Information</h3>
-    <div className='basic-info' style={{ marginTop: '1.5rem' }}>
-      <WorkspaceInfoField labelText='Active Status'>
-        {activeStatus}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Billing Status'>
-        {workspace.billingStatus}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Workspace Name'>
-        {workspace.name}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Terra Name (often incorrectly called "id")'>
-        {workspace.id}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Workspace Namespace'>
-        {workspace.namespace}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Access Tier'>
-        {workspace.accessTierShortName?.toUpperCase()}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Google Project Id'>
-        {workspace.googleProject}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Billing Account Type'>
-        {isUsingFreeTierBillingAccount(workspace)
-          ? 'Initial credits'
-          : 'User provided'}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Creation Time'>
-        {new Date(workspace.creationTime).toDateString()}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Last Modified Time'>
-        {new Date(workspace.lastModifiedTime).toDateString()}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Workspace Published'>
-        {workspace.published ? 'Yes' : 'No'}
-      </WorkspaceInfoField>
-      <WorkspaceInfoField labelText='Audit'>
-        {
-          <Link to={`/admin/workspace-audit/${workspace.namespace}`}>
-            Audit History
-          </Link>
-        }
-      </WorkspaceInfoField>
-    </div>
-  </>
-);
+export const BasicInformation = ({
+  workspace,
+  activeStatus,
+  reload,
+}: Props) => {
+  const [featuredCategory, setFeaturedCategory] =
+    React.useState<FeaturedWorkspaceCategory>(workspace.featuredCategory);
+  const [featuredCategoryLoading, setFeaturedCategoryLoading] =
+    React.useState<boolean>(false);
+
+  useEffect(() => {
+    setFeaturedCategory(workspace.featuredCategory);
+    setFeaturedCategoryLoading(false);
+  }, [workspace.featuredCategory]);
+
+  const publishingDisabled =
+    featuredCategoryLoading ||
+    !featuredCategory ||
+    featuredCategory === workspace.featuredCategory;
+
+  const getWorkspacePublishTooltip = () => {
+    if (featuredCategoryLoading) {
+      return 'Your workspace is loading, please wait until loading is completed before publishing.';
+    } else if (!featuredCategory) {
+      return 'Please select a category to publish the workspace.';
+    } else if (featuredCategory === workspace.featuredCategory) {
+      return 'This workspace is already published in the selected category.';
+    } else {
+      return '';
+    }
+  };
+  return (
+    <>
+      <h3>Basic Information</h3>
+      <div className='basic-info' style={{ marginTop: '1.5rem' }}>
+        <WorkspaceInfoField labelText='Active Status'>
+          {activeStatus}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Billing Status'>
+          {workspace.billingStatus}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Workspace Name'>
+          {workspace.name}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Terra Name (often incorrectly called "id")'>
+          {workspace.id}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Workspace Namespace'>
+          {workspace.namespace}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Access Tier'>
+          {workspace.accessTierShortName?.toUpperCase()}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Google Project Id'>
+          {workspace.googleProject}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Billing Account Type'>
+          {isUsingFreeTierBillingAccount(workspace)
+            ? 'Initial credits'
+            : 'User provided'}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Creation Time'>
+          {new Date(workspace.creationTime).toDateString()}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Last Modified Time'>
+          {new Date(workspace.lastModifiedTime).toDateString()}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Workspace Published'>
+          <Select
+            key={featuredCategory || 'placeholder'}
+            value={featuredCategory}
+            placeholder='Select a category...'
+            isDisabled={featuredCategoryLoading}
+            options={FeaturedWorkspaceCategoryOptions}
+            onChange={(v) => setFeaturedCategory(v)}
+          />
+          <TooltipTrigger
+            disabled={!publishingDisabled}
+            content={getWorkspacePublishTooltip()}
+          >
+            <Button
+              type='primary'
+              disabled={publishingDisabled}
+              onClick={() => {
+                setFeaturedCategoryLoading(true);
+                workspaceAdminApi()
+                  .publishWorkspaceViaDB(workspace.namespace, {
+                    category: featuredCategory,
+                  })
+                  .then(async () => {
+                    await reload();
+                  });
+              }}
+            >
+              Publish
+            </Button>
+          </TooltipTrigger>
+          <Button
+            type='secondaryOutline'
+            disabled={featuredCategoryLoading || !workspace.featuredCategory}
+            onClick={() => {
+              setFeaturedCategory(null);
+              setFeaturedCategoryLoading(true);
+              workspaceAdminApi()
+                .unpublishWorkspaceViaDB(workspace.namespace)
+                .then(async () => {
+                  await reload();
+                });
+            }}
+          >
+            Unpublish
+          </Button>
+          {featuredCategoryLoading && <Spinner size={36} />}
+        </WorkspaceInfoField>
+        <WorkspaceInfoField labelText='Audit'>
+          {
+            <Link to={`/admin/workspace-audit/${workspace.namespace}`}>
+              Audit History
+            </Link>
+          }
+        </WorkspaceInfoField>
+      </div>
+    </>
+  );
+};
