74e656ae4307416cde5fb3bd625e1213c00c5c49
Joel Thibault <thibault@broad.dev>
2024-07-29 11:51:56 -0400

    inst mapper cleanups

--- api/src/main/java/org/pmiops/workbench/db/model/DbWorkspace.java
+++ api/src/main/java/org/pmiops/workbench/db/model/DbWorkspace.java
@@ -107,6 +107,7 @@ public class DbWorkspace {
   @Deprecated(since = "July 2024", forRemoval = true)
   private boolean published;
 
+  // referenced by featured_workspace table
   private DbFeaturedCategory featuredCategory;
 
   public DbWorkspace() {
