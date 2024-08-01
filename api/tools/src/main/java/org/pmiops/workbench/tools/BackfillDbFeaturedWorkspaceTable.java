ff00fb0ec23fa414a98ec83a1b3b19aa961c1d31
NehaBroad <nsaxena@broadinstitute.org>
2024-08-01 13:19:58 -0400

    Attempt to use firecloudservice

--- api/tools/src/main/java/org/pmiops/workbench/tools/BackfillDbFeaturedWorkspaceTable.java
+++ api/tools/src/main/java/org/pmiops/workbench/tools/BackfillDbFeaturedWorkspaceTable.java
@@ -2,24 +2,83 @@ package org.pmiops.workbench.tools;
 
 import com.fasterxml.jackson.databind.JsonNode;
 import com.fasterxml.jackson.databind.ObjectMapper;
+import com.google.cloud.bigquery.BigQuery;
+import com.google.cloud.bigquery.BigQueryOptions;
 import com.google.cloud.iam.credentials.v1.IamCredentialsClient;
 import java.io.IOException;
+import java.time.Clock;
+import java.time.Duration;
 import java.util.List;
 import java.util.logging.Logger;
+
+import com.google.cloud.logging.Logging;
+import com.google.cloud.logging.LoggingOptions;
+import jakarta.inject.Provider;
+import jakarta.persistence.Entity;
 import org.apache.commons.cli.*;
+import org.pmiops.workbench.access.AccessTierService;
+import org.pmiops.workbench.access.AccessTierServiceImpl;
+import org.pmiops.workbench.actionaudit.ActionAuditServiceImpl;
+import org.pmiops.workbench.actionaudit.ActionAuditSpringConfiguration;
+import org.pmiops.workbench.actionaudit.auditors.BillingProjectAuditorImpl;
+import org.pmiops.workbench.api.BigQueryService;
+import org.pmiops.workbench.cdr.ConceptBigQueryService;
+import org.pmiops.workbench.cdr.dao.CBCriteriaAttributeDao;
+import org.pmiops.workbench.cdr.model.DbCriteriaAttribute;
+import org.pmiops.workbench.cohortbuilder.CohortBuilderServiceImpl;
+import org.pmiops.workbench.cohortbuilder.CohortQueryBuilder;
+import org.pmiops.workbench.cohorts.CohortCloningService;
+import org.pmiops.workbench.cohorts.CohortFactory;
+import org.pmiops.workbench.cohorts.CohortFactoryImpl;
+import org.pmiops.workbench.conceptset.ConceptSetService;
 import org.pmiops.workbench.config.FeaturedWorkspacesConfig;
 import org.pmiops.workbench.config.FileConfigs;
-import org.pmiops.workbench.db.dao.FeaturedWorkspaceDao;
-import org.pmiops.workbench.db.dao.WorkspaceDao;
+import org.pmiops.workbench.config.WorkbenchConfig;
+import org.pmiops.workbench.db.dao.*;
+import org.pmiops.workbench.db.model.DbAccessTier;
 import org.pmiops.workbench.db.model.DbFeaturedWorkspace;
 import org.pmiops.workbench.db.model.DbFeaturedWorkspace.DbFeaturedCategory;
 import org.pmiops.workbench.db.model.DbWorkspace;
+import org.pmiops.workbench.firecloud.FireCloudConfig;
+import org.pmiops.workbench.firecloud.FireCloudService;
+import org.pmiops.workbench.firecloud.FireCloudServiceImpl;
+import org.pmiops.workbench.firecloud.FirecloudTransforms;
+import org.pmiops.workbench.google.GoogleConfig;
+import org.pmiops.workbench.rawls.RawlsApiClientFactory;
+import org.pmiops.workbench.rawls.RawlsConfig;
+import org.pmiops.workbench.sam.SamRetryHandler;
+import org.pmiops.workbench.workspaces.WorkspaceServiceImpl;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.boot.CommandLineRunner;
-import org.springframework.context.annotation.Bean;
-import org.springframework.stereotype.Component;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.domain.EntityScan;
+import org.springframework.context.annotation.*;
+import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 
-@Component
+@Configuration
+@Import({
+        FireCloudConfig.class,
+        FireCloudServiceImpl.class,
+        GoogleConfig.class, // injects com.google.cloud.iam.credentials.v1.IamCredentialsClient
+        RawlsApiClientFactory.class,
+        RawlsConfig.class,
+        SamRetryHandler.class,
+        WorkspaceServiceImpl.class,
+        AccessTierServiceImpl.class,
+        BillingProjectAuditorImpl.class,
+        ActionAuditSpringConfiguration.class,
+        ActionAuditServiceImpl.class,
+        CohortCloningService.class,
+        CohortFactoryImpl.class,
+        ConceptSetService.class,
+        ConceptBigQueryService.class,
+        BigQueryService.class,
+        CohortBuilderServiceImpl.class,
+        CohortQueryBuilder.class
+})
+@EntityScan(basePackages = {"org.pmiops.workbench.cdr.model"})
+@EnableJpaRepositories(basePackages = {"org.pmiops.workbench.cdr.dao"})
 public class BackfillDbFeaturedWorkspaceTable extends Tool {
 
   private static final Logger log =
@@ -51,18 +110,29 @@ public class BackfillDbFeaturedWorkspaceTable extends Tool {
     return IamCredentialsClient.create();
   }
 
-  private final FeaturedWorkspaceDao featuredWorkspaceDao;
-  private final WorkspaceDao workspaceDao;
-
-  @Autowired
-  public BackfillDbFeaturedWorkspaceTable(
-      FeaturedWorkspaceDao featuredWorkspaceDao, WorkspaceDao workspaceDao) {
-    this.featuredWorkspaceDao = featuredWorkspaceDao;
-    this.workspaceDao = workspaceDao;
+  @Bean
+  public Logging getCloudLogging() {
+    return LoggingOptions.getDefaultInstance().getService();
   }
 
   @Bean
-  public CommandLineRunner run() {
+  public BigQuery bigQuery() {
+    return BigQueryOptions.getDefaultInstance().getService();
+  }
+
+  @Bean
+  public Duration defaultBigQueryTimeout() {
+    return Duration.ofMinutes(5);
+  }
+
+  @Bean
+  public DbCriteriaAttribute dbCriteriaAttribute() {
+    return new DbCriteriaAttribute();
+  };
+
+  @Bean
+  public CommandLineRunner run(WorkspaceDao workspaceDao, FeaturedWorkspaceDao featuredWorkspaceDao,
+                               FireCloudService fireCloudService){
     return args -> {
       try {
         CommandLine opts = new DefaultParser().parse(OPTIONS, args);
@@ -103,7 +173,14 @@ public class BackfillDbFeaturedWorkspaceTable extends Tool {
                     .setWorkspace(publishedWorkspace)
                     .setCategory(
                         DbFeaturedCategory.valueOf(featuredWorkspace.getCategory().toString()));
-            featuredWorkspaceDao.save(dbFeaturedWorkspace);
+            if (!dryRun) {
+              fireCloudService.updateWorkspaceAclForPublishing(
+                      publishedWorkspace.getWorkspaceNamespace(),
+                      publishedWorkspace.getFirecloudName(),
+                      true);
+
+              featuredWorkspaceDao.save(dbFeaturedWorkspace);
+            }
           } else {
             log.info(
                 String.format(
