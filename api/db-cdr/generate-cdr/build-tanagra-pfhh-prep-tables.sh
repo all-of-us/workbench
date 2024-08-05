5adaba6c1ba3a7630e4f8c194f884e9d0c14b9f2
freemabd <brian.d.freeman@gmail.com>
2024-08-05 16:26:29 -0500

    DT-817 adding new table

--- api/db-cdr/generate-cdr/build-tanagra-pfhh-prep-tables.sh
+++ api/db-cdr/generate-cdr/build-tanagra-pfhh-prep-tables.sh
@@ -30,9 +30,11 @@ bq load --project_id="$BQ_PROJECT" --source_format=CSV "$BQ_DATASET.prep_pfhh_no
 echo "prep_pfhh_observation - inserting PFHH source data"
 bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_observation\`
-SELECT o.*
+SELECT DISTINCT o.person_id, o.observation_date, o.observation_datetime, o.observation_source_concept_id, p.birth_datetime, o.visit_detail_id, o.visit_occurrence_id, o.value_as_concept_id, o.value_as_number, o.value_source_concept_id, oe.survey_version_concept_id, sc.survey_concept_id, sc.collection_method_concept_id
 FROM \`$BQ_PROJECT.$BQ_DATASET.observation\` o
-JOIN \`$BQ_PROJECT.$BQ_DATASET.survey_conduct\` sc ON o.questionnaire_response_id = sc.survey_conduct_id
+JOIN \`$BQ_PROJECT.$BQ_DATASET.person\` p on p.person_id = o.person_id
+LEFT JOIN \`$BQ_PROJECT.$BQ_DATASET.observation_ext\` oe on o.observation_id = oe.observation_id
+LEFT JOIN \`$BQ_PROJECT.$BQ_DATASET.survey_conduct\` sc ON o.questionnaire_response_id = sc.survey_conduct_id
 WHERE sc.survey_concept_id IN (43529712, 43528698, 1740639)"
 
 ##############################################################
@@ -41,16 +43,52 @@ WHERE sc.survey_concept_id IN (43529712, 43528698, 1740639)"
 echo "prep_pfhh_observation - updating all questions to new pfhh survey"
 bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
 "UPDATE \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_observation\` x
-SET x.observation_source_concept_id = y.new_question
+SET x.observation_source_concept_id = y.new_question,
+x.survey_concept_id = 1740639
 FROM (
   SELECT DISTINCT pfhh.person_id, pfhh.observation_date, pfhh.observation_source_concept_id AS historic_question, pfhh.value_source_concept_id AS answer, pfhh_mapping.pfhh_question_concept_id AS new_question
   FROM \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_observation\` pfhh
   JOIN \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_mapping\` pfhh_mapping ON pfhh.observation_source_concept_id = pfhh_mapping.historic_question_concept_id AND pfhh.value_source_concept_id = pfhh_mapping.pfhh_answer_concept_id
   AND survey_concept_id IN (43528698, 43529712)
-  AND is_standard = 0
 ) y
 WHERE x.concept_id = y.historic_question
 AND x.value_source_concept_id = y.answer
 AND x.person_id = y.person_id
-AND x.entry_date = y.entry_date
-AND x.is_standard = 0"
+AND x.observation_date = y.observation_date"
+
+echo "Removing deprecated questions"
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
+"DELETE FROM \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_observation\` y
+WHERE EXISTS (
+  SELECT 'x'
+  FROM \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_remove_from_index\` x
+  WHERE x.concept_id = y.observation_source_concept_id
+  AND x.value = y.value_source_concept_id
+)"
+
+echo "Insert skip rows into new pfhh survey"
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
+"INSERT INTO \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_observation\`
+SELECT DISTINCT
+      a.person_id
+    , a.entry_date
+    , a.entry_datetime
+    , a.concept_id
+    , a.domain
+    , a.age_at_event
+    , a.visit_concept_id
+    , a.visit_occurrence_id
+    , a.value_as_number
+    , a.value_as_concept_id
+    , a.value_source_concept_id
+    , a.survey_version_concept_id
+    , a.survey_concept_id
+    , a.cati_concept_id
+FROM \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_non_answer_insert\` a
+JOIN \`$BQ_PROJECT.$BQ_DATASET.person\` b on a.person_id = b.person_id"
+
+echo "Updating all survey_concept_ids for Med History/Personal History to new PFHH survey"
+bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql \
+"UPDATE \`$BQ_PROJECT.$BQ_DATASET.prep_pfhh_observation\`
+ SET survey_concept_id = 1740639
+ WHERE survey_concept_id IN (43528698, 43529712)"
