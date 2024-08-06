29df7b6599bc924fa0a7aace5610bd8eb3fc514e
Brian Freeman <brian.d.freeman@gmail.com>
2024-08-06 15:58:12 -0500

    [DT-817][risk=no] Adding PFHH table for tanagra indexing (#8712)
    * DT-815 adding new table for pfhh survey data
    
    * DT-817 adding inserts for table
    
    * DT-817 adding new table
    
    * DT-817 updating pfhh table for tanagra
    
    * DT-817 adding ruby command for building PFHH table for tanagra

--- api/db-cdr/generate-cdr/create-tanagra-tables.sh
+++ api/db-cdr/generate-cdr/create-tanagra-tables.sh
@@ -6,33 +6,40 @@ set -e
 export BQ_PROJECT=$1         # CDR project
 export BQ_DATASET=$2         # CDR dataset
 
-echo "Deleting $table_name"
+echo "Deleting $BQ_DATASET.prep_concept"
 bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.prep_concept"
 wait
   
-echo "Creating $table_name"
+echo "Creating $BQ_DATASET.prep_concept"
 bq --quiet --project_id="$BQ_PROJECT" mk --schema="generate-cdr/bq-schemas/prep_concept.json" "$BQ_DATASET.prep_concept"
 
-echo "Deleting $table_name"
+echo "Deleting $BQ_DATASET.prep_concept_relationship"
 bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.prep_concept_relationship"
 wait
   
-echo "Creating $table_name"
+echo "Creating $BQ_DATASET.prep_concept_relationship"
 bq --quiet --project_id="$BQ_PROJECT" mk --schema="generate-cdr/bq-schemas/prep_concept_relationship.json" "$BQ_DATASET.prep_concept_relationship"
 
-echo "Deleting $table_name"
+echo "Deleting $BQ_DATASET.prep_cpt"
 bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.prep_cpt"
 wait
   
-echo "Creating $table_name"
+echo "Creating $BQ_DATASET.prep_cpt"
 bq --quiet --project_id="$BQ_PROJECT" mk --schema="generate-cdr/bq-schemas/prep_cpt.json" "$BQ_DATASET.prep_cpt"
 
-echo "Deleting $table_name"
+echo "Deleting $BQ_DATASET.prep_survey"
 bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.prep_survey"
 wait
   
-echo "Creating $table_name"
+echo "Creating $BQ_DATASET.prep_survey"
 bq --quiet --project_id="$BQ_PROJECT" mk --schema="generate-cdr/bq-schemas/prep_survey.json" "$BQ_DATASET.prep_survey"
+
+echo "Deleting $BQ_DATASET.prep_pfhh_observation"
+bq --project_id="$BQ_PROJECT" rm -f "$BQ_DATASET.prep_pfhh_observation"
+wait
+  
+echo "Creating $BQ_DATASET.prep_pfhh_observation"
+bq --quiet --project_id="$BQ_PROJECT" mk --schema="generate-cdr/bq-schemas/prep_pfhh_observation.json" "$BQ_DATASET.prep_pfhh_observation"
 
 wait
 echo "Done creating tables"
