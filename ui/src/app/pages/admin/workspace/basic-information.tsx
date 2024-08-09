992d13d0aaeadcb1bb728719b137cd09dc8f84ab
Joel Thibault <thibault@broad.dev>
2024-08-09 10:46:25 -0400

    extract a WorkspacePublishingInfo from BasicInformation

--- ui/src/app/pages/admin/workspace/basic-information.tsx
+++ ui/src/app/pages/admin/workspace/basic-information.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useEffect } from 'react';
+import { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 
 import {
@@ -20,33 +20,27 @@ import { isUsingFreeTierBillingAccount } from 'app/utils/workspace-utils';
 
 import { WorkspaceInfoField } from './workspace-info-field';
 
-interface Props {
+interface PublishingProps {
   workspace: Workspace;
-  activeStatus: WorkspaceActiveStatus;
   reload: () => Promise<void>;
 }
-export const BasicInformation = ({
-  workspace,
-  activeStatus,
-  reload,
-}: Props) => {
-  const [featuredCategory, setFeaturedCategory] =
-    React.useState<FeaturedWorkspaceCategory>(workspace.featuredCategory);
-  const [featuredCategoryLoading, setFeaturedCategoryLoading] =
-    React.useState<boolean>(false);
+const WorkspacePublishingInfo = ({ workspace, reload }: PublishingProps) => {
   const { enablePublishedWorkspacesViaDb } = serverConfigStore.get().config;
+
+  const [featuredCategory, setFeaturedCategory] = useState(
+    workspace.featuredCategory
+  );
+  const [featuredCategoryLoading, setFeaturedCategoryLoading] = useState(false);
 
   useEffect(() => {
     setFeaturedCategory(workspace.featuredCategory);
     setFeaturedCategoryLoading(false);
   }, [workspace.featuredCategory]);
-
   const publishingDisabled =
     featuredCategoryLoading ||
     workspace.adminLocked ||
     !featuredCategory ||
     featuredCategory === workspace.featuredCategory;
-
   const getWorkspacePublishTooltip = () => {
     return cond(
       [
@@ -65,6 +59,75 @@ export const BasicInformation = ({
     );
   };
 
+  return (
+    <WorkspaceInfoField labelText='Workspace Published'>
+      {enablePublishedWorkspacesViaDb ? (
+        <>
+          <Select
+            key={featuredCategory || 'placeholder'}
+            value={featuredCategory}
+            placeholder='Select a category...'
+            isDisabled={featuredCategoryLoading}
+            options={FeaturedWorkspaceCategoryOptions}
+            onChange={(v: FeaturedWorkspaceCategory) => setFeaturedCategory(v)}
+          />
+          <TooltipTrigger
+            disabled={!publishingDisabled}
+            content={getWorkspacePublishTooltip()}
+          >
+            <Button
+              type='primary'
+              disabled={publishingDisabled}
+              onClick={() => {
+                setFeaturedCategoryLoading(true);
+                workspaceAdminApi()
+                  .publishWorkspaceViaDB(workspace.namespace, {
+                    category: featuredCategory,
+                  })
+                  .then(async () => {
+                    await reload();
+                  });
+              }}
+            >
+              Publish
+            </Button>
+          </TooltipTrigger>
+          <Button
+            type='secondaryOutline'
+            disabled={featuredCategoryLoading || !workspace.featuredCategory}
+            onClick={() => {
+              setFeaturedCategory(null);
+              setFeaturedCategoryLoading(true);
+              workspaceAdminApi()
+                .unpublishWorkspaceViaDB(workspace.namespace)
+                .then(async () => {
+                  await reload();
+                });
+            }}
+          >
+            Unpublish
+          </Button>
+          {featuredCategoryLoading && <Spinner size={36} />}
+        </>
+      ) : workspace.published ? (
+        'Yes'
+      ) : (
+        'No'
+      )}
+    </WorkspaceInfoField>
+  );
+};
+
+interface Props {
+  workspace: Workspace;
+  activeStatus: WorkspaceActiveStatus;
+  reload: () => Promise<void>;
+}
+export const BasicInformation = ({
+  workspace,
+  activeStatus,
+  reload,
+}: Props) => {
   return (
     <>
       <h3>Basic Information</h3>
@@ -101,63 +164,7 @@ export const BasicInformation = ({
         <WorkspaceInfoField labelText='Last Modified Time'>
           {new Date(workspace.lastModifiedTime).toDateString()}
         </WorkspaceInfoField>
-        <WorkspaceInfoField labelText='Workspace Published'>
-          {enablePublishedWorkspacesViaDb ? (
-            <>
-              <Select
-                key={featuredCategory || 'placeholder'}
-                value={featuredCategory}
-                placeholder='Select a category...'
-                isDisabled={featuredCategoryLoading}
-                options={FeaturedWorkspaceCategoryOptions}
-                onChange={(v) => setFeaturedCategory(v)}
-              />
-              <TooltipTrigger
-                disabled={!publishingDisabled}
-                content={getWorkspacePublishTooltip()}
-              >
-                <Button
-                  type='primary'
-                  disabled={publishingDisabled}
-                  onClick={() => {
-                    setFeaturedCategoryLoading(true);
-                    workspaceAdminApi()
-                      .publishWorkspaceViaDB(workspace.namespace, {
-                        category: featuredCategory,
-                      })
-                      .then(async () => {
-                        await reload();
-                      });
-                  }}
-                >
-                  Publish
-                </Button>
-              </TooltipTrigger>
-              <Button
-                type='secondaryOutline'
-                disabled={
-                  featuredCategoryLoading || !workspace.featuredCategory
-                }
-                onClick={() => {
-                  setFeaturedCategory(null);
-                  setFeaturedCategoryLoading(true);
-                  workspaceAdminApi()
-                    .unpublishWorkspaceViaDB(workspace.namespace)
-                    .then(async () => {
-                      await reload();
-                    });
-                }}
-              >
-                Unpublish
-              </Button>
-              {featuredCategoryLoading && <Spinner size={36} />}
-            </>
-          ) : workspace.published ? (
-            'Yes'
-          ) : (
-            'No'
-          )}
-        </WorkspaceInfoField>
+        <WorkspacePublishingInfo {...{ workspace, reload }} />
         <WorkspaceInfoField labelText='Audit'>
           {
             <Link to={`/admin/workspace-audit/${workspace.namespace}`}>
