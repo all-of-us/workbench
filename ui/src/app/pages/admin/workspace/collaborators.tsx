531cc72432787e095f132b814c6d80bc68ee09f0
Joel Thibault <2701406+jmthibault79@users.noreply.github.com>
2024-08-21 14:53:07 -0400

    [risk=low][no ticket] Add creator to the Workspace Admin page (#8685)

--- ui/src/app/pages/admin/workspace/collaborators.tsx
+++ ui/src/app/pages/admin/workspace/collaborators.tsx
@@ -1,17 +1,59 @@
 import * as React from 'react';
 
-import { WorkspaceUserAdminView } from 'generated/fetch';
+import { WorkspaceAccessLevel, WorkspaceUserAdminView } from 'generated/fetch';
+
+import { reactStyles } from 'app/utils';
+
+const styles = reactStyles({
+  group: {
+    marginBottom: '1rem',
+  },
+  item: {
+    marginLeft: '1rem',
+  },
+});
+
+const collabList = (users: WorkspaceUserAdminView[]) => {
+  return users?.length > 0
+    ? users.map((c) => (
+        <div style={styles.item} key={c.userModel.userName}>
+          {c.userModel.userName}
+        </div>
+      ))
+    : 'None';
+};
 
 interface Props {
+  creator: string;
   collaborators: WorkspaceUserAdminView[];
 }
-export const Collaborators = ({ collaborators }: Props) => (
-  <>
-    <h3>Collaborators</h3>
-    <div className='collaborators' style={{ marginTop: '1.5rem' }}>
-      {collaborators.map((c, i) => (
-        <div key={i}>{`${c.userModel.userName}: ${c.role}`}</div>
-      ))}
-    </div>
-  </>
-);
+export const Collaborators = ({ creator, collaborators }: Props) => {
+  return (
+    <>
+      <h3>Collaborators</h3>
+      <div className='collaborators' style={{ marginTop: '1.5rem' }}>
+        <div style={styles.group}>Creator and Owner: {creator}</div>
+        <div style={styles.group}>
+          Other Owners:{' '}
+          {collabList(
+            collaborators
+              ?.filter((c) => c.role === WorkspaceAccessLevel.OWNER)
+              ?.filter((c) => c.userModel.userName !== creator)
+          )}
+        </div>
+        <div style={styles.group}>
+          Writers:{' '}
+          {collabList(
+            collaborators?.filter((c) => c.role === WorkspaceAccessLevel.WRITER)
+          )}
+        </div>
+        <div style={styles.group}>
+          Readers:{' '}
+          {collabList(
+            collaborators?.filter((c) => c.role === WorkspaceAccessLevel.READER)
+          )}
+        </div>
+      </div>
+    </>
+  );
+};
