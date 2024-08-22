77f39b932fba7218688e449ea1365eda24d96c50
Eric Rollins <erollins@broadinstitute.org>
2024-08-22 15:38:39 -0400

    Restructure code for readability

--- api/src/main/java/org/pmiops/workbench/access/AccessSyncServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/access/AccessSyncServiceImpl.java
@@ -68,20 +68,36 @@ public class AccessSyncServiceImpl implements AccessSyncService {
 
     final List<DbAccessTier> newAccessTiers = getUserAccessTiersList(dbUser);
 
-    boolean enableInitialCreditsExpiration =
-        workbenchConfigProvider.get().featureFlags.enableInitialCreditsExpiration;
-    long freeTierCreditValidityPeriodDays =
-        workbenchConfigProvider.get().billing.freeTierCreditValidityPeriodDays;
-
     if (!newAccessTiers.equals(previousAccessTiers)) {
       userServiceAuditor.fireUpdateAccessTiersAction(
           dbUser, previousAccessTiers, newAccessTiers, agent);
     }
 
+    addInitialCreditsExpirationIfAppropriate(dbUser, previousAccessTiers, newAccessTiers);
+
+    // add user to each Access Tier DB table and the tiers' Terra Auth Domains
+    newAccessTiers.forEach(tier -> accessTierService.addUserToTier(dbUser, tier));
+
+    // remove user from all other Access Tier DB tables and the tiers' Terra Auth Domains
+    final List<DbAccessTier> tiersForRemoval =
+        Lists.difference(accessTierService.getAllTiers(), newAccessTiers);
+    tiersForRemoval.forEach(tier -> accessTierService.removeUserFromTier(dbUser, tier));
+
+    return userDao.save(dbUser);
+  }
+
+  private void addInitialCreditsExpirationIfAppropriate(
+      DbUser dbUser, List<DbAccessTier> previousAccessTiers, List<DbAccessTier> newAccessTiers) {
+    boolean enableInitialCreditsExpiration =
+        workbenchConfigProvider.get().featureFlags.enableInitialCreditsExpiration;
+    long freeTierCreditValidityPeriodDays =
+        workbenchConfigProvider.get().billing.freeTierCreditValidityPeriodDays;
+
     if (enableInitialCreditsExpiration) {
       DbUserInitialCreditsExpiration maybeCreditsExpiration =
           dbUser.getUserInitialCreditsExpiration();
 
+      // A user's credits should begin to expire when they gain access to their first tier.
       if (previousAccessTiers.isEmpty()
           && !newAccessTiers.isEmpty()
           && null == maybeCreditsExpiration) {
@@ -96,16 +112,6 @@ public class AccessSyncServiceImpl implements AccessSyncService {
                 .setExpirationTime(expirationTime));
       }
     }
-
-    // add user to each Access Tier DB table and the tiers' Terra Auth Domains
-    newAccessTiers.forEach(tier -> accessTierService.addUserToTier(dbUser, tier));
-
-    // remove user from all other Access Tier DB tables and the tiers' Terra Auth Domains
-    final List<DbAccessTier> tiersForRemoval =
-        Lists.difference(accessTierService.getAllTiers(), newAccessTiers);
-    tiersForRemoval.forEach(tier -> accessTierService.removeUserFromTier(dbUser, tier));
-
-    return userDao.save(dbUser);
   }
 
   private List<DbAccessTier> getUserAccessTiersList(DbUser dbUser) {
