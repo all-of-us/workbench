89f3f02128db3c0f6b6d0438b1e8c2fb425ac960
Eric Rollins <erollins@broadinstitute.org>
2024-08-28 10:31:59 -0400

    Updated test

--- api/src/test/java/org/pmiops/workbench/initialcredits/InitialCreditsExpirationServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/initialcredits/InitialCreditsExpirationServiceTest.java
@@ -4,6 +4,7 @@ import static com.google.common.truth.Truth8.assertThat;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.never;
+import static org.mockito.Mockito.reset;
 import static org.mockito.Mockito.times;
 import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
@@ -12,21 +13,30 @@ import jakarta.mail.MessagingException;
 import java.sql.Timestamp;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pmiops.workbench.FakeClockConfiguration;
+import org.pmiops.workbench.config.WorkbenchConfig;
 import org.pmiops.workbench.db.dao.UserDao;
 import org.pmiops.workbench.db.dao.UserInitialCreditsExpirationDao;
+import org.pmiops.workbench.db.dao.WorkspaceDao;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbUserInitialCreditsExpiration;
+import org.pmiops.workbench.db.model.DbWorkspace;
 import org.pmiops.workbench.mail.MailService;
+import org.pmiops.workbench.model.BillingStatus;
 import org.pmiops.workbench.model.InitialCreditExpirationNotificationStatus;
 import org.pmiops.workbench.test.FakeClock;
-import org.pmiops.workbench.workspaces.WorkspaceService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.config.ConfigurableBeanFactory;
 import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+import org.springframework.boot.test.context.TestConfiguration;
 import org.springframework.boot.test.mock.mockito.MockBean;
+import org.springframework.boot.test.mock.mockito.SpyBean;
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Import;
+import org.springframework.context.annotation.Scope;
 
 @DataJpaTest
 @Import({
@@ -38,23 +48,43 @@ public class InitialCreditsExpirationServiceTest {
 
   @MockBean private MailService mailService;
 
-  @MockBean private UserDao userDao;
+  @SpyBean
+  private UserDao userDao;
 
-  @MockBean private WorkspaceService workspaceService;
+  @SpyBean private WorkspaceDao workspaceDao;
 
   @MockBean private UserInitialCreditsExpirationDao userInitialCreditsExpirationDao;
 
   @MockBean private FakeClock fakeClock;
 
+  private static WorkbenchConfig config;
   private static final Timestamp NOW = Timestamp.from(FakeClockConfiguration.NOW.toInstant());
   private static final Timestamp NOW_PLUS_ONE_DAY =
       Timestamp.from(FakeClockConfiguration.NOW.toInstant().plusSeconds(24 * 60 * 60));
   private static final Timestamp NOW_MINUS_ONE_DAY =
       Timestamp.from(FakeClockConfiguration.NOW.toInstant().minusSeconds(24 * 60 * 60));
 
+  private DbWorkspace workspace;
+
+  @TestConfiguration
+  static class Configuration {
+    @Bean
+    @Scope(ConfigurableBeanFactory.SCOPE_PROTOTYPE)
+    public WorkbenchConfig config() {
+      return config;
+    }
+  }
+
   @BeforeEach
   public void setUp() {
+    reset(userDao, mailService, userInitialCreditsExpirationDao, workspaceDao);
+    config = WorkbenchConfig.createEmptyConfig();
+    config.billing.accountId = "billingAccountId";
     when(fakeClock.instant()).thenReturn(FakeClockConfiguration.NOW.toInstant());
+
+    workspace = workspaceDao.save(
+        new DbWorkspace().setBillingAccountName("billingAccounts/"+config.billing.accountId).setWorkspaceId(1L));
+    when(workspaceDao.findAllByCreator(any())).thenReturn(Set.of(workspace));
   }
 
   @Test
@@ -109,94 +139,106 @@ public class InitialCreditsExpirationServiceTest {
 
   @Test
   public void test_checkCreditsExpirationForUserIDs_noExpirationRecord() throws MessagingException {
-    DbUser user = new DbUser();
+    DbUser user = userDao.save(new DbUser());
     when(userDao.findAllById(List.of(user.getUserId()))).thenReturn(List.of(user));
+
     service.checkCreditsExpirationForUserIDs(List.of(user.getUserId()));
+
     verify(mailService, never()).alertUserInitialCreditsExpired(any());
     verify(userInitialCreditsExpirationDao, never()).save(any());
-    verify(workspaceService, never()).updateFreeTierWorkspacesStatus(any(), any());
+    verify(workspaceDao, never()).updateBillingStatus(workspace.getWorkspaceId(), BillingStatus.EXPIRED);
   }
 
   @Test
   public void test_checkCreditsExpirationForUserIDs_bypassed() throws MessagingException {
     DbUser user =
-        new DbUser()
+        userDao.save(new DbUser()
             .setUserInitialCreditsExpiration(
-                new DbUserInitialCreditsExpiration().setBypassed(true).setExpirationTime(NOW));
+                new DbUserInitialCreditsExpiration().setBypassed(true).setExpirationTime(NOW)));
     when(userDao.findAllById(List.of(user.getUserId()))).thenReturn(List.of(user));
+
     service.checkCreditsExpirationForUserIDs(List.of(user.getUserId()));
+
     verify(mailService, never()).alertUserInitialCreditsExpired(any());
     verify(userInitialCreditsExpirationDao, never()).save(any());
-    verify(workspaceService, never()).updateFreeTierWorkspacesStatus(any(), any());
+    verify(workspaceDao, never()).updateBillingStatus(workspace.getWorkspaceId(), BillingStatus.EXPIRED);
   }
 
   @Test
   public void test_checkCreditsExpirationForUserIDs_unexpired() throws MessagingException {
     DbUser user =
-        new DbUser()
+        userDao.save(new DbUser()
             .setUserInitialCreditsExpiration(
                 new DbUserInitialCreditsExpiration()
                     .setBypassed(false)
-                    .setExpirationTime(NOW_PLUS_ONE_DAY));
+                    .setExpirationTime(NOW_PLUS_ONE_DAY)));
     when(userDao.findAllById(List.of(user.getUserId()))).thenReturn(List.of(user));
+
     service.checkCreditsExpirationForUserIDs(List.of(user.getUserId()));
+
     verify(mailService, never()).alertUserInitialCreditsExpired(any());
     verify(userInitialCreditsExpirationDao, never()).save(any());
-    verify(workspaceService, never()).updateFreeTierWorkspacesStatus(any(), any());
+    verify(workspaceDao, never()).updateBillingStatus(workspace.getWorkspaceId(), BillingStatus.EXPIRED);
   }
 
   @Test
   public void test_checkCreditsExpirationForUserIDs_expiredPreviouslySentNotification()
       throws MessagingException {
-    DbUser user =
+    DbUser user = userDao.save(
         new DbUser()
             .setUserInitialCreditsExpiration(
                 new DbUserInitialCreditsExpiration()
                     .setBypassed(false)
                     .setExpirationTime(NOW_MINUS_ONE_DAY)
                     .setNotificationStatus(
-                        InitialCreditExpirationNotificationStatus.EXPIRATION_NOTIFICATION_SENT));
+                        InitialCreditExpirationNotificationStatus.EXPIRATION_NOTIFICATION_SENT)));
     when(userDao.findAllById(List.of(user.getUserId()))).thenReturn(List.of(user));
+
     service.checkCreditsExpirationForUserIDs(List.of(user.getUserId()));
+
     verify(mailService, never()).alertUserInitialCreditsExpired(any());
     verify(userInitialCreditsExpirationDao, never()).save(any());
-    verify(workspaceService, never()).updateFreeTierWorkspacesStatus(any(), any());
+    verify(workspaceDao, never()).updateBillingStatus(workspace.getWorkspaceId(), BillingStatus.EXPIRED);
   }
 
   @Test
   public void test_checkCreditsExpirationForUserIDs_expiredUnsentNotification()
       throws MessagingException {
-    DbUser user =
+    DbUser user = userDao.save(
         new DbUser()
             .setUserInitialCreditsExpiration(
                 new DbUserInitialCreditsExpiration()
                     .setBypassed(false)
                     .setExpirationTime(NOW_MINUS_ONE_DAY)
                     .setNotificationStatus(
-                        InitialCreditExpirationNotificationStatus.NO_NOTIFICATION_SENT));
+                        InitialCreditExpirationNotificationStatus.NO_NOTIFICATION_SENT)));
     when(userDao.findAllById(List.of(user.getUserId()))).thenReturn(List.of(user));
+
     service.checkCreditsExpirationForUserIDs(List.of(user.getUserId()));
+
     verify(mailService, times(1)).alertUserInitialCreditsExpired(any());
     verify(userInitialCreditsExpirationDao, times(1)).save(any());
-    verify(workspaceService, times(1)).updateFreeTierWorkspacesStatus(any(), any());
+    verify(workspaceDao, times(1)).updateBillingStatus(workspace.getWorkspaceId(), BillingStatus.EXPIRED);
   }
 
   @Test
   public void test_checkCreditsExpirationForUserIDs_expiredUnsentNotificationWithMailException()
       throws MessagingException {
-    DbUser user =
+    DbUser user = userDao.save(
         new DbUser()
             .setUserInitialCreditsExpiration(
                 new DbUserInitialCreditsExpiration()
                     .setBypassed(false)
                     .setExpirationTime(NOW_MINUS_ONE_DAY)
                     .setNotificationStatus(
-                        InitialCreditExpirationNotificationStatus.NO_NOTIFICATION_SENT));
+                        InitialCreditExpirationNotificationStatus.NO_NOTIFICATION_SENT)));
     when(userDao.findAllById(List.of(user.getUserId()))).thenReturn(List.of(user));
     doThrow(new MessagingException()).when(mailService).alertUserInitialCreditsExpired(any());
+
     service.checkCreditsExpirationForUserIDs(List.of(user.getUserId()));
+
     verify(mailService, times(1)).alertUserInitialCreditsExpired(any());
     verify(userInitialCreditsExpirationDao, never()).save(any());
-    verify(workspaceService, times(1)).updateFreeTierWorkspacesStatus(any(), any());
+    verify(workspaceDao, times(1)).updateBillingStatus(workspace.getWorkspaceId(), BillingStatus.EXPIRED);
   }
 }
