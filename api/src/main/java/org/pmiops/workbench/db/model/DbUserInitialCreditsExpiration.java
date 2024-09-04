d13d31bd0e4bbe7f49e6fd069b1e10da5c80ae93
Joel Thibault <thibault@broad.dev>
2024-09-04 13:41:50 -0400

    db user fix

--- api/src/main/java/org/pmiops/workbench/db/model/DbUserInitialCreditsExpiration.java
+++ api/src/main/java/org/pmiops/workbench/db/model/DbUserInitialCreditsExpiration.java
@@ -2,6 +2,8 @@ package org.pmiops.workbench.db.model;
 
 import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
+import jakarta.persistence.EnumType;
+import jakarta.persistence.Enumerated;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
@@ -86,6 +88,7 @@ public class DbUserInitialCreditsExpiration {
   }
 
   @Column(name = "notification_status")
+  @Enumerated(EnumType.STRING)
   public NotificationStatus getNotificationStatus() {
     return notificationStatus;
   }
