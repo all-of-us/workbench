6e088192d48cf1fc3c9bf663d95e90ddf71c2264
freemabd <brian.d.freeman@gmail.com>
2024-09-11 13:51:20 -0500

    DT-874 testing bug fix

---
+++ api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# This generates the cb menu for cohort builder.
+
+set -e
+
+export BQ_PROJECT=$1   # project
+export BQ_DATASET=$2   # dataset
+
+TABLE_LIST=$(bq ls -n 1000 "$BQ_PROJECT:$BQ_DATASET")
+
+echo "Getting self_reported_category_concept_id column count"
+selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
+from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"" | tr -dc '0-9')
+
+echo "$selfReportedCategoryDataCount"
