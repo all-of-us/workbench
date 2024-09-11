2b3e7f71c1d30bfe7785f806ec8da1b800b95a74
freemabd <brian.d.freeman@gmail.com>
2024-09-11 16:30:31 -0500

    trying something else

--- api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
+++ api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
@@ -11,4 +11,7 @@ echo "Getting self_reported_category_concept_id column count"
 selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
 from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"")
 
+selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
+from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"" | tr -dc '0-9')
+
 echo "$selfReportedCategoryDataCount"
