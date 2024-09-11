9c95ebcbcf6d5a7fd0877e28dd501a80cb85178f
Brian Freeman <brian.d.freeman@gmail.com>
2024-09-11 16:23:50 -0500

    trying something else (#8782)
    [no ticket][risk=no] testing script in circleci

--- api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
+++ api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
@@ -9,6 +9,6 @@ export BQ_DATASET=$2   # dataset
 
 echo "Getting self_reported_category_concept_id column count"
 selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
-from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"" | tr -dc '0-9')
+from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"")
 
 echo "$selfReportedCategoryDataCount"
