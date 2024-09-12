42a994c098b6aca2a0f4ff08cbc7e9498726315d
freemabd <brian.d.freeman@gmail.com>
2024-09-12 10:24:10 -0500

    adding fitbit query

--- api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
+++ api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
@@ -7,11 +7,18 @@ set -e
 export BQ_PROJECT=$1   # project
 export BQ_DATASET=$2   # dataset
 
-echo "Getting self_reported_category_concept_id column count"
-selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
-from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"")
+echo "Getting fitbit count"
+query="select count(*) as count from \`$BQ_PROJECT.$BQ_DATASET.cb_search_person\`
+where has_fitbit = 1"
+fitbitCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "$query" | tr -dc '0-9')
+
+echo "fitbit count: $fitbitCount"
+
+#echo "Getting self_reported_category_concept_id column count"
+#selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
+#from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"")
 
 selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
 from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"" | tr -dc '0-9')
 
-echo "$selfReportedCategoryDataCount"
+echo "self reported: $selfReportedCategoryDataCount"
