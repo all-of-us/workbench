d7b582f6c1e55f748ad20672276f196757cdbbc0
freemabd <brian.d.freeman@gmail.com>
2024-09-12 10:34:56 -0500

    adding fitbit query

--- api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
+++ api/db-cdr/generate-cdr/build-cb-criteria-menu-test.sh
@@ -14,6 +14,13 @@ fitbitCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "$q
 
 echo "fitbit count: $fitbitCount"
 
+echo "Getting fitbit count"
+query="select count(*) as count from \`$BQ_PROJECT.$BQ_DATASET.cb_search_person\`
+where has_fitbit = 1"
+fitbitCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "$query" | tr -dc '0-9')
+
+echo "fitbit count: $fitbitCount"
+
 selfReportedCategoryDataCount=$(bq --quiet --project_id="$BQ_PROJECT" query --nouse_legacy_sql "select count(column_name) as count 
 from \`$BQ_PROJECT.$BQ_DATASET.INFORMATION_SCHEMA.COLUMNS\` where table_name=\"person\" AND column_name = \"self_reported_category_concept_id\"" | tr -dc '0-9')
 
