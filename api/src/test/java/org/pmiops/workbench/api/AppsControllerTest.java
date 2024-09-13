cddd44ed8351dbb919c0d8d02a2dded5eb1ec250
Eric Rollins <erollins@broadinstitute.org>
2024-09-13 16:34:11 -0400

    Removed unnecessary matchers.

--- api/src/test/java/org/pmiops/workbench/api/AppsControllerTest.java
+++ api/src/test/java/org/pmiops/workbench/api/AppsControllerTest.java
@@ -1,7 +1,6 @@
 package org.pmiops.workbench.api;
 
 import static org.junit.jupiter.api.Assertions.assertThrows;
-import static org.mockito.ArgumentMatchers.eq;
 import static org.mockito.Mockito.doReturn;
 import static org.mockito.Mockito.doThrow;
 import static org.mockito.Mockito.verify;
@@ -106,7 +105,7 @@ public class AppsControllerTest {
   public void testCreateAppFail_validateActiveBilling() {
     doThrow(new ForbiddenException())
         .when(mockWorkspaceAuthService)
-        .validateActiveBilling(eq(WORKSPACE_NS), eq(WORKSPACE_ID));
+        .validateActiveBilling(WORKSPACE_NS, WORKSPACE_ID);
 
     assertThrows(
         ForbiddenException.class, () -> controller.createApp(WORKSPACE_NS, createAppRequest));
