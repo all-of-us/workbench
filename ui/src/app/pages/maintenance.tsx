e99e57451c07d2606f5f1358e722662e4b72734c
Eric Rollins <erollins@broadinstitute.org>
2024-09-16 14:26:51 -0400

    Add a maintenance page

---
+++ ui/src/app/pages/maintenance.tsx
@@ -0,0 +1,22 @@
+import * as React from 'react';
+
+import { FlexColumn, FlexRow, FlexSpacer } from 'app/components/flex';
+import { PageHeader } from 'app/components/headers';
+import logo from 'assets/images/all-of-us-logo.svg';
+
+export const Maintenance = () => {
+  return (
+    <FlexColumn style={{ height: '100vh' }}>
+      <FlexSpacer />
+      <FlexColumn style={{ alignItems: 'center' }}>
+        <img
+          style={{ height: '15vh', marginBottom: '1rem' }}
+          src={logo}
+          alt='all of us logo'
+        />
+        <PageHeader>Down for System Maintenance</PageHeader>
+      </FlexColumn>
+      <FlexSpacer />
+    </FlexColumn>
+  );
+};
