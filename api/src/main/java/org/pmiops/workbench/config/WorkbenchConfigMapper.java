554cfd98fb0fae6b2640a8bd240254b8e790deb6
Eric Rollins <erollins@broadinstitute.org>
2024-09-23 11:14:33 -0400

    Lint

--- api/src/main/java/org/pmiops/workbench/config/WorkbenchConfigMapper.java
+++ api/src/main/java/org/pmiops/workbench/config/WorkbenchConfigMapper.java
@@ -55,8 +55,6 @@ public interface WorkbenchConfigMapper {
   @Mapping(
       target = "enableInitialCreditsExpiration",
       source = "config.featureFlags.enableInitialCreditsExpiration")
-  @Mapping(
-      target = "isDownForMaintenance",
-      source = "config.server.isDownForMaintenance")
+  @Mapping(target = "isDownForMaintenance", source = "config.server.isDownForMaintenance")
   ConfigResponse toModel(WorkbenchConfig config, List<DbAccessModule> accessModules);
 }
