1b40107507a711031616a00c008ca5c04605545f
Qi Wang <qi@broadinstitute.org>
2024-10-17 20:37:18 -0400

    more test removal

--- api/src/test/java/org/pmiops/workbench/ras/RasLinkServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/ras/RasLinkServiceTest.java
@@ -267,21 +267,6 @@ public class RasLinkServiceTest {
   }
 
   @Test
-  public void testLinkRasSuccess_eRAAlreadyLinked() throws Exception {
-    // ERA is linked before, expect ERA record not update by RAS linking again.
-    Timestamp eRATime = Timestamp.from(Instant.parse("2000-01-01T00:00:00.00Z"));
-    accessModuleService.updateCompletionTime(currentUser, DbAccessModuleName.ERA_COMMONS, eRATime);
-    mockCodeExchangeResponse(TOKEN_RESPONSE_IAL2);
-    mockAccessTokenResponse(USER_INFO_JSON_LOGIN_GOV_WITH_ERA);
-    rasLinkService.linkRasAccount(AUTH_CODE, REDIRECT_URL);
-
-    assertThat(userDao.findUserByUserId(userId).getRasLinkUsername()).isEqualTo(LOGIN_GOV_USERNAME);
-    assertModuleCompletionTime(DbAccessModuleName.IDENTITY, NOW);
-    assertModuleCompletionTime(DbAccessModuleName.RAS_LOGIN_GOV, NOW);
-    assertModuleCompletionTime(DbAccessModuleName.ERA_COMMONS, eRATime);
-  }
-
-  @Test
   public void testLinkRasFail_ial1() throws Exception {
     mockCodeExchangeResponse(TOKEN_RESPONSE_IAL1);
     verify(mockIdentityVerificationService, never()).updateIdentityVerificationSystem(any(), any());
