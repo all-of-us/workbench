7ecc0690340266072b79405b17a295e9c6f1e472
Eric Rollins <erollins@broadinstitute.org>
2024-10-23 13:17:03 -0400

    More logging

--- api/src/main/java/org/pmiops/workbench/db/dao/UserServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/db/dao/UserServiceImpl.java
@@ -435,6 +435,10 @@ public class UserServiceImpl implements UserService {
 
   @Override
   public boolean hasSignedLatestTermsOfServiceForBoth(@Nonnull DbUser dbUser) {
+    boolean hasSignedLatestAouTos = hasSignedLatestAoUTermsOfService(dbUser);
+    boolean hasSignedLatestTerraTos = fireCloudService.hasUserAcceptedLatestTerraToS();
+
+    log.log(Level.INFO, String.format("User %s has signed latest AoU TOS: %s, and latest Terra TOS: %s", dbUser.getUsername(), hasSignedLatestAouTos, hasSignedLatestTerraTos));
     return hasSignedLatestAoUTermsOfService(dbUser)
         && fireCloudService.hasUserAcceptedLatestTerraToS();
   }
