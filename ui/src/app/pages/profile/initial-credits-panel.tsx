a5ff2f3baecf2a3cb9c4e0cbda284a204ca6f63f
Eric Rollins <erollins@broadinstitute.org>
2024-11-12 16:13:28 -0500

    Add extension and info buttons.

--- ui/src/app/pages/profile/initial-credits-panel.tsx
+++ ui/src/app/pages/profile/initial-credits-panel.tsx
@@ -1,5 +1,6 @@
 import * as React from 'react';
 
+import { Button } from 'app/components/buttons';
 import { FlexColumn, FlexRow } from 'app/components/flex';
 import { AoU } from 'app/components/text-wrappers';
 import { formatInitialCreditsUSD } from 'app/utils';
@@ -15,31 +16,61 @@ interface Props {
 export const InitialCreditsPanel = (props: Props) => {
   const { expirationDate, freeTierUsage, freeTierDollarQuota } = props;
   return (
-    <FlexRow style={styles.initialCreditsBox}>
-      <FlexColumn>
-        <div>
-          <AoU /> initial credits used:
-        </div>
-        <div>
-          Remaining <AoU /> initial credits:
-        </div>
-        {expirationDate && (
+    <div style={styles.initialCreditsBox}>
+      <FlexRow>
+        <FlexColumn>
           <div>
-            <AoU /> initial credits epiration date:
+            <AoU /> initial credits used:
           </div>
-        )}
-      </FlexColumn>
-      <FlexColumn style={{ flex: 1, alignItems: 'flex-end' }}>
-        <div style={{ fontWeight: 600 }}>
-          {formatInitialCreditsUSD(freeTierUsage)}
-        </div>
-        <div style={{ fontWeight: 600 }}>
-          {formatInitialCreditsUSD(freeTierDollarQuota - (freeTierUsage ?? 0))}
-        </div>
-        <div style={{ fontWeight: 600 }}>
-          {displayDateWithoutHours(expirationDate)}
-        </div>
-      </FlexColumn>
-    </FlexRow>
+          <div>
+            Remaining <AoU /> initial credits:
+          </div>
+          {expirationDate && (
+            <div>
+              <AoU /> initial credits epiration date:
+            </div>
+          )}
+        </FlexColumn>
+        <FlexColumn style={{ flex: 1, alignItems: 'flex-end' }}>
+          <div style={{ fontWeight: 600 }}>
+            {formatInitialCreditsUSD(freeTierUsage)}
+          </div>
+          <div style={{ fontWeight: 600 }}>
+            {formatInitialCreditsUSD(
+              freeTierDollarQuota - (freeTierUsage ?? 0)
+            )}
+          </div>
+          {expirationDate && (
+            <div style={{ fontWeight: 600 }}>
+              {displayDateWithoutHours(expirationDate)}
+            </div>
+          )}
+        </FlexColumn>
+      </FlexRow>
+      {expirationDate && (
+        <FlexRow>
+          <Button
+            type='primarySmall'
+            style={{ marginTop: '1rem' }}
+            onClick={() => console.log('Out of hand')}
+          >
+            Request credit extension
+          </Button>
+        </FlexRow>
+      )}
+      <FlexRow>
+        <Button
+          style={{
+            maxWidth: 'none',
+            padding: '0',
+            textTransform: 'none',
+          }}
+          type='link'
+          onClick={() => console.log('Come on love')}
+        >
+          Learn more about credits & setting up a billing account
+        </Button>
+      </FlexRow>
+    </div>
   );
 };
