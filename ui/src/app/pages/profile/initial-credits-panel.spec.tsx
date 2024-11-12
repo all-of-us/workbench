40bdb61247e3188cdd1b2f9425567d67084592db
Eric Rollins <erollins@broadinstitute.org>
2024-11-12 15:28:35 -0500

    Added basic tests.

---
+++ ui/src/app/pages/profile/initial-credits-panel.spec.tsx
@@ -0,0 +1,49 @@
+import '@testing-library/jest-dom';
+
+import * as React from 'react';
+
+import { screen } from '@testing-library/dom';
+import { render } from '@testing-library/react';
+import userEvent from '@testing-library/user-event';
+
+import { InitialCreditsPanel } from './initial-credits-panel';
+
+const setup = (
+  freeTierUsage: number,
+  freeTierDollarQuota: number,
+  expirationDate: number
+) => {
+  return {
+    container: render(
+      <InitialCreditsPanel
+        {...{ freeTierUsage, freeTierDollarQuota, expirationDate }}
+      />
+    ).container,
+    user: userEvent.setup(),
+  };
+};
+
+it('should display initial credits for a user with an expiration record', async () => {
+  const freeTierUsage = 100.0;
+  const freeTierDollarQuota = 234;
+  const expirationDate = new Date('2023-12-03T20:00:00Z').getTime();
+  setup(freeTierUsage, freeTierDollarQuota, expirationDate);
+  expect(screen.getByText(`$100.00`)).toBeInTheDocument();
+  // This should reflect the quota minus the usage
+  expect(screen.getByText(`$134.00`)).toBeInTheDocument();
+  expect(
+    screen.getByText(`initial credits epiration date:`)
+  ).toBeInTheDocument();
+  expect(screen.getByText(`Dec 3, 2023`)).toBeInTheDocument();
+});
+
+it('should display initial credits for a user without an expiration record', async () => {
+  const freeTierUsage = 100.0;
+  const freeTierDollarQuota = 234;
+  const expirationDate = null;
+  setup(freeTierUsage, freeTierDollarQuota, expirationDate);
+  expect(screen.getByText(`$100.00`)).toBeInTheDocument();
+  expect(
+    screen.queryByText(`initial credits epiration date:`)
+  ).not.toBeInTheDocument();
+});
