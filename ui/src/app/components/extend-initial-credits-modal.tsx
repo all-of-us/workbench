8ebd7f68a92efd6e748e7c5110e25c0633201a2d
Eric Rollins <erollins@broadinstitute.org>
2024-11-13 13:40:07 -0500

    Added call to api and result handling

--- ui/src/app/components/extend-initial-credits-modal.tsx
+++ ui/src/app/components/extend-initial-credits-modal.tsx
@@ -9,7 +9,9 @@ import {
   ModalTitle,
 } from 'app/components/modals';
 import { Spinner } from 'app/components/spinners';
+import { profileApi } from 'app/services/swagger-fetch-clients';
 import colors, { colorWithWhiteness } from 'app/styles/colors';
+import { notificationStore } from 'app/utils/stores';
 import { supportUrls } from 'app/utils/zendesk';
 
 import { AoU } from './text-wrappers';
@@ -18,18 +20,45 @@ interface Props {
   onClose: Function;
 }
 
+const ExtensionDescription = () => (
+  <div>
+    Initial credits provided by the <AoU /> Research Program expire after X
+    days. You are eligible to request an extension.
+    <StyledExternalLink href={supportUrls.initialCredits}>
+      Learn more about credits & setting up a billing account
+    </StyledExternalLink>
+  </div>
+);
+
 export const ExtendInitialCreditsModal = ({ onClose }: Props) => {
   const [saving, setSaving] = useState(false); // saving refers to the loading request time
+
+  const onRequestExtension = () => {
+    setSaving(true);
+    profileApi()
+      .extendInitialCreditExpiration()
+      .then(() => {
+        notificationStore.set({
+          title: 'Initial credits extended',
+          message: 'Initial credits will now expire on X',
+        });
+      })
+      .catch((error: Response) => {
+        error.json().then((errorJson) =>
+          notificationStore.set({
+            title: 'Error extending Initial credits',
+            message: errorJson?.message,
+          })
+        );
+      })
+      .finally(() => onClose());
+  };
 
   return (
     <Modal>
       <ModalTitle>Request Credit Expiration Date Extension</ModalTitle>
       <ModalBody style={{ marginTop: '0.3rem' }}>
-        Initial credits provided by the <AoU /> Research Program expire after X
-        days. You are eligible to request an extension.
-        <StyledExternalLink href={supportUrls.initialCredits}>
-          Learn more about credits & setting up a billing account
-        </StyledExternalLink>
+        <ExtensionDescription />
       </ModalBody>
       <ModalFooter>
         <Button
@@ -40,7 +69,7 @@ export const ExtendInitialCreditsModal = ({ onClose }: Props) => {
         >
           Cancel
         </Button>
-        <Button type='primary' onClick={() => console.log('Why?')}>
+        <Button type='primary' onClick={onRequestExtension} disabled={saving}>
           {saving && <Spinner style={{ marginRight: '0.375rem' }} size={18} />}
           Request Extension
         </Button>
