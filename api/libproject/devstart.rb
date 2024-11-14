1aa577724ad8b3cfd179cde2ae1fc01c71c53f46
Yonghao Yu <yyhao1@gmail.com>
2024-11-14 08:47:57 -0500

    revert compose

--- api/libproject/devstart.rb
+++ api/libproject/devstart.rb
@@ -87,8 +87,8 @@ def start_local_db_service()
     common.status "waiting up to #{DEADLINE_SEC}s for mysql service to start..."
     start = Time.now
     until (common.run "docker-compose exec -T db mysql -p#{root_pass} --silent -e 'SELECT 1;'").success?
-      if Time.now - start >= deadlineSec
-        raise("mysql docker service did not become available after #{deadlineSec}s")
+      if Time.now - start >= DEADLINE_SEC
+        raise("mysql docker service did not become available after #{DEADLINE_SEC}s")
       end
       sleep 1
     end
