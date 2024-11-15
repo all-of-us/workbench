5f36f7601fc2361e5aeb7566dfb1ab957f349bfd
Eric Rollins <erollins@broadinstitute.org>
2024-11-15 14:50:47 -0500

    Updated tests to account for extension button.

--- ui/src/app/pages/profile/initial-credits-panel.spec.tsx
+++ ui/src/app/pages/profile/initial-credits-panel.spec.tsx
@@ -11,10 +11,10 @@ import { InitialCreditsPanel } from './initial-credits-panel';
 const setup = (
   freeTierUsage: number,
   freeTierDollarQuota: number,
-  expirationDate: number
+  expirationDate: number,
+  eligibleForExtension: boolean
 ) => {
   const updateInitialCredits = jest.fn();
-  const eligibleForExtension = true;
   return {
     container: render(
       <InitialCreditsPanel
@@ -35,12 +35,18 @@ it('should display initial credits for a user with an expiration record', async 
   const freeTierUsage = 100.0;
   const freeTierDollarQuota = 234;
   const expirationDate = new Date('2023-12-03T20:00:00Z').getTime();
-  setup(freeTierUsage, freeTierDollarQuota, expirationDate);
+  const eligibleForExtension = true;
+  setup(
+    freeTierUsage,
+    freeTierDollarQuota,
+    expirationDate,
+    eligibleForExtension
+  );
   expect(screen.getByText(`$100.00`)).toBeInTheDocument();
   // This should reflect the quota minus the usage
   expect(screen.getByText(`$134.00`)).toBeInTheDocument();
   expect(
-    screen.getByText(`initial credits epiration date:`)
+    screen.getByText(`initial credits expiration date:`)
   ).toBeInTheDocument();
   expect(screen.getByText(`Dec 3, 2023`)).toBeInTheDocument();
   expect(
@@ -48,14 +54,37 @@ it('should display initial credits for a user with an expiration record', async 
   ).toBeInTheDocument();
 });
 
+it('should not show extension button if the user is not eligible', async () => {
+  const freeTierUsage = 100.0;
+  const freeTierDollarQuota = 234;
+  const expirationDate = new Date('2023-12-03T20:00:00Z').getTime();
+  const eligibleForExtension = false;
+  setup(
+    freeTierUsage,
+    freeTierDollarQuota,
+    expirationDate,
+    eligibleForExtension
+  );
+  expect(screen.getByText(`$100.00`)).toBeInTheDocument();
+  expect(
+    screen.queryByRole('button', { name: /request credit extension/i })
+  ).not.toBeInTheDocument();
+});
+
 it('should display initial credits for a user without an expiration record', async () => {
   const freeTierUsage = 100.0;
   const freeTierDollarQuota = 234;
   const expirationDate = null;
-  setup(freeTierUsage, freeTierDollarQuota, expirationDate);
+  const eligibleForExtension = false;
+  setup(
+    freeTierUsage,
+    freeTierDollarQuota,
+    expirationDate,
+    eligibleForExtension
+  );
   expect(screen.getByText(`$100.00`)).toBeInTheDocument();
   expect(
-    screen.queryByText(`initial credits epiration date:`)
+    screen.queryByText(`initial credits expiration date:`)
   ).not.toBeInTheDocument();
   expect(
     screen.queryByRole('button', { name: /request credit extension/i })
