e428e3806a06feb9b35cc0533984fa55c3c95459
Yonghao Yu <yyhao1@gmail.com>
2024-11-22 13:40:48 -0500

    fix unit test

--- api/src/main/java/org/pmiops/workbench/api/VwbEgressAdminController.java
+++ api/src/main/java/org/pmiops/workbench/api/VwbEgressAdminController.java
@@ -2,7 +2,9 @@ package org.pmiops.workbench.api;
 
 import org.pmiops.workbench.model.VwbEgressEventRequest;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
 
+@RestController
 public class VwbEgressAdminController implements VwbEgressAdminApiDelegate {
   @Override
   public ResponseEntity<Void> createVwbEgressEvent(VwbEgressEventRequest body) {
