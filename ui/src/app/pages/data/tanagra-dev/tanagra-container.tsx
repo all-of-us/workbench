55c0b49e6c0682d349edf3dc98d56364b8dc6537
Will Dolbeer <40036095+dolbeew@users.noreply.github.com>
2024-11-22 13:20:54 -0500

    [DT-893][risk=no] Support VCF extractions for Tanagra exports (#8954)

--- ui/src/app/pages/data/tanagra-dev/tanagra-container.tsx
+++ ui/src/app/pages/data/tanagra-dev/tanagra-container.tsx
@@ -2,8 +2,12 @@ import React, { useState } from 'react';
 import { useParams } from 'react-router-dom';
 import * as fp from 'lodash/fp';
 
+import { DataSet, Domain } from 'generated/fetch';
+
 import { environment } from 'environments/environment';
 import { ExportDatasetModal } from 'app/pages/data/data-set/export-dataset-modal';
+import { GenomicExtractionModal } from 'app/pages/data/data-set/genomic-extraction-modal';
+import { dataSetApi } from 'app/services/swagger-fetch-clients';
 import { withCdrVersions, withCurrentWorkspace } from 'app/utils';
 import { getAccessToken } from 'app/utils/authentication';
 import { findCdrVersion } from 'app/utils/cdr-versions';
@@ -14,6 +18,13 @@ import {
   useNavigation,
 } from 'app/utils/navigation';
 import { serverConfigStore } from 'app/utils/stores';
+
+enum ModalState {
+  None,
+  Create,
+  Export,
+  Extract,
+}
 
 export const TanagraContainer = fp.flow(
   withCdrVersions(),
@@ -26,7 +37,9 @@ export const TanagraContainer = fp.flow(
     cdrVersionId,
     cdrVersionTiersResponse
   );
+  const [modalState, setModalState] = useState(ModalState.None);
   const [exportIds, setExportIds] = useState<ExportResources>();
+  const [exportDataset, setExportDataset] = useState<DataSet>();
   const tanagraUrl = `${
     serverConfigStore.get().config.tanagraBaseUrl
   }/tanagra/ui#/tanagra/underlays/aou${bigqueryDataset}/studies/${namespace}/${splat}${
@@ -38,8 +51,37 @@ export const TanagraContainer = fp.flow(
     navigate(['workspaces', namespace, terraName, 'data']);
   });
 
-  useExportListener((exportResourceIds) => {
+  useExportListener(async (exportResourceIds) => {
     setExportIds(exportResourceIds);
+    if (exportResourceIds.predefinedCriteria.includes('_short_read_wgs')) {
+      try {
+        const newDataset = await dataSetApi().createDataSet(
+          workspace.namespace,
+          workspace.terraName,
+          {
+            name: `Tanagra export test ${Date.now()}`,
+            domainValuePairs: [
+              {
+                domain: Domain.WHOLE_GENOME_VARIANT,
+                value: 'VCF Files',
+              },
+            ],
+            conceptSetIds: [],
+            cohortIds: [],
+            prePackagedConceptSet: [],
+          }
+        );
+        if (newDataset) {
+          setExportDataset(newDataset);
+          setModalState(ModalState.Extract);
+        }
+      } catch (error) {
+        console.error(error);
+      }
+    } else {
+      setExportDataset(null);
+      setModalState(ModalState.Export);
+    }
   });
 
   return (
@@ -57,13 +99,32 @@ export const TanagraContainer = fp.flow(
         }}
         src={tanagraUrl}
       />
-      {!!exportIds && (
+      {!!exportIds && modalState === ModalState.Export && (
         <ExportDatasetModal
           {...{ workspace }}
+          dataset={exportDataset}
           closeFunction={() => setExportIds(undefined)}
           tanagraCohortIds={exportIds.cohortIds}
           tanagraFeatureSetIds={exportIds.featureSetIds}
           tanagraAllParticipantsCohort={exportIds.allParticipantsCohort}
+          tanagraHasWGS={exportIds?.predefinedCriteria?.includes(
+            '_short_read_wgs'
+          )}
+        />
+      )}
+      {!!exportIds && modalState === ModalState.Extract && (
+        <GenomicExtractionModal
+          dataSet={exportDataset}
+          workspaceNamespace={namespace}
+          workspaceTerraName={terraName}
+          title={'Would you like to extract genomic variant data as VCF files?'}
+          cancelText={'Skip'}
+          confirmText={'Extract & Continue'}
+          closeFunction={() => setModalState(ModalState.Export)}
+          tanagraCohortIds={exportIds.cohortIds}
+          tanagraFeatureSetIds={exportIds.featureSetIds}
+          tanagraAllParticipantsCohort={exportIds.allParticipantsCohort}
+          tanagraEnabled
         />
       )}
     </div>
