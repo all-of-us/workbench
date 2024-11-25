bab2c350dc89936e7ae2a1da4252c54dc00bca2e
Will Dolbeer <40036095+dolbeew@users.noreply.github.com>
2024-11-25 14:27:57 -0500

    [no ticket][risk=no] Fix person id query for VCF extractions (#8971)

--- api/src/main/java/org/pmiops/workbench/cohortbuilder/CohortQueryBuilder.java
+++ api/src/main/java/org/pmiops/workbench/cohortbuilder/CohortQueryBuilder.java
@@ -98,7 +98,12 @@ public class CohortQueryBuilder extends QueryBuilder {
         .build();
   }
 
-  public QueryJobConfiguration buildPersonIdQuery(String personIdQuery) {
-    return QueryJobConfiguration.newBuilder(personIdQuery).setUseLegacySql(false).build();
+  // This method supports VCF extractions for Tanagra exports
+  public QueryJobConfiguration buildTanagraWGSPersonIdQuery(String personIdQuery) {
+    return QueryJobConfiguration.newBuilder(
+            personIdQuery
+                + " AND id IN (SELECT id FROM `${projectId}.${dataSetId}.T_ENT_person` WHERE has_whole_genome_variant = true)")
+        .setUseLegacySql(false)
+        .build();
   }
 }
