343acde8877cce792d9b47522f5afa329008ca44
freemabd <brian.d.freeman@gmail.com>
2024-12-05 10:05:10 -0600

    DT-934 making script more of a template

--- api/db-cdr/generate-cdr/test-data/delete_preprod_datasets.sh
+++ api/db-cdr/generate-cdr/test-data/delete_preprod_datasets.sh
@@ -2,20 +2,10 @@ gcloud config set auth/impersonate_service_account deploy@all-of-us-rw-preprod.i
 
 # RT
 
-bq rm -r -f -d fc-aou-cdr-preprod:R2022Q4R10
-bq rm -r -f -d fc-aou-cdr-preprod:R2022Q4R10_base
-bq rm -r -f -d fc-aou-cdr-preprod:R2022Q4R9
-bq rm -r -f -d fc-aou-cdr-preprod:R2022Q4R9_base
-bq rm -r -f -d fc-aou-cdr-preprod:R2024Q3R3
-bq rm -r -f -d fc-aou-cdr-preprod:R2024Q3R3_base
+bq rm -r -f -d fc-aou-cdr-preprod:<datasetName>
 
 # CT
 
-bq rm -r -f -d fc-aou-cdr-preprod-ct:C2022Q4R11
-bq rm -r -f -d fc-aou-cdr-preprod-ct:C2022Q4R11_base
-bq rm -r -f -d fc-aou-cdr-preprod-ct:C2022Q4R12
-bq rm -r -f -d fc-aou-cdr-preprod-ct:C2022Q4R12_base
-bq rm -r -f -d fc-aou-cdr-preprod-ct:C2024Q3R3
-bq rm -r -f -d fc-aou-cdr-preprod-ct:C2024Q3R3_base
+bq rm -r -f -d fc-aou-cdr-preprod-ct:<datasetName>
 
 gcloud config unset auth/impersonate_service_account
--- api/db-cdr/generate-cdr/test-data/delete_prod_datasets.sh
+++ api/db-cdr/generate-cdr/test-data/delete_prod_datasets.sh
@@ -2,28 +2,10 @@ gcloud config set auth/impersonate_service_account deploy@all-of-us-rw-prod.iam.
 
 # RT
 
-bq rm -r -f -d fc-aou-cdr-prod:R2019Q4R6                
-bq rm -r -f -d fc-aou-cdr-prod:R2019Q4R6_base        
-bq rm -r -f -d fc-aou-cdr-prod:R2020Q4R5                
-bq rm -r -f -d fc-aou-cdr-prod:R2020Q4R5_base     
-bq rm -r -f -d fc-aou-cdr-prod:R2021Q3R8                
-bq rm -r -f -d fc-aou-cdr-prod:R2021Q3R8_base  
-bq rm -r -f -d fc-aou-cdr-prod:R2022Q2R6                
-bq rm -r -f -d fc-aou-cdr-prod:R2022Q2R6_base    
-bq rm -r -f -d fc-aou-cdr-prod:R2022Q4R9                
-bq rm -r -f -d fc-aou-cdr-prod:R2022Q4R9_base
+bq rm -r -f -d fc-aou-cdr-prod:<datasetName>
 
 # CT
 
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2021Q3R8                   
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2021Q3R8_base
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q2R6                   
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q2R6_base              
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q2R7                   
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q2R7_base   
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q4R11                  
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q4R11_base
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q4R9                   
-bq rm -r -f -d fc-aou-cdr-prod-ct:C2022Q4R9_base
+bq rm -r -f -d fc-aou-cdr-prod-ct:<datasetName>
 
 gcloud config unset auth/impersonate_service_account
