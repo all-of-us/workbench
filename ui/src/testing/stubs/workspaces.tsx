3f216935ef1f10eb56a12448e36672e4954a2691
Eric Rollins <erollins@broadinstitute.org>
2024-12-06 11:51:44 -0500

    Added initial credits to workspace stub.

--- ui/src/testing/stubs/workspaces.tsx
+++ ui/src/testing/stubs/workspaces.tsx
@@ -61,6 +61,11 @@ export function buildWorkspaceStub(suffix = ''): Workspace {
     billingAccountName: 'billing-account',
     billingStatus: BillingStatus.ACTIVE,
     adminLocked: false,
+    initialCredits: {
+      exhausted: false,
+      expired: false,
+      expirationEpochMillis: new Date().getTime(),
+    },
   };
 }
 
