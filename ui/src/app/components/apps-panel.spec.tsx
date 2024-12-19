493947d50840d6da150e30937984b311ca841154
Eric Rollins <erollins@broadinstitute.org>
2024-12-19 09:29:25 -0500

    Ensure that Apps panel is not disabled when the user has bypassed initial credit expiration.

--- ui/src/app/components/apps-panel.spec.tsx
+++ ui/src/app/components/apps-panel.spec.tsx
@@ -9,11 +9,12 @@ import {
   RuntimeApi,
 } from 'generated/fetch';
 
+import defaultServerConfig from '../../testing/default-server-config';
 import { render, screen, waitFor } from '@testing-library/react';
-import userEvent from '@testing-library/user-event';
+import userEvent, { UserEvent } from '@testing-library/user-event';
 import { registerApiClient as registerLeoApiClient } from 'app/services/notebooks-swagger-fetch-clients';
 import { registerApiClient } from 'app/services/swagger-fetch-clients';
-import { runtimeStore } from 'app/utils/stores';
+import { runtimeStore, serverConfigStore } from 'app/utils/stores';
 import { AppsApi as LeoAppsApi } from 'notebooks-generated/fetch';
 
 import { AppsApiStub } from 'testing/stubs/apps-api-stub';
@@ -72,21 +73,78 @@ const expectExpandedApp = (appName: string): HTMLElement => {
   return deleteEnvButton;
 };
 
+const expectExpandableApp = async (user: UserEvent, appName: string) => {
+  expect(findActiveApps()).not.toBeInTheDocument();
+  expect(findAvailableApps(false)).toBeInTheDocument();
+
+  // Click unexpanded Jupyter app
+
+  const jupyter = expectUnexpandedApp(appName);
+  jupyter.click();
+
+  await waitFor(() => expectExpandedApp(appName));
+
+  // the overall apps panel state doesn't change: there are still no ActiveApps
+  // the newly expanded app is in the AvailableApps section
+
+  expect(findActiveApps()).not.toBeInTheDocument();
+  expect(findAvailableApps(false)).toBeInTheDocument();
+};
+
+const expectAppsPanelToBeDisabled = async (user: UserEvent) => {
+  expect(findActiveApps()).not.toBeInTheDocument();
+  expect(findAvailableApps(false)).toBeInTheDocument();
+
+  // Click unexpanded Jupyter app
+
+  const jupyter = expectUnexpandedApp('Jupyter');
+
+  await user.pointer([{ pointerName: 'mouse', target: jupyter }]);
+
+  screen.logTestingPlaygroundURL();
+  // Show tooltip when hovering over disabled Jupyter.
+  await screen.findByText(
+    'You have either run out of initial credits or have an inactive billing account.'
+  );
+
+  jupyter.click();
+  // Expecting Jupyter to be disabled. If it was not, the
+  // "Create New" button would show after clicking on Jupyter.
+  expect(screen.queryByText('Create New')).not.toBeInTheDocument();
+
+  const cromwell = expectUnexpandedApp('Cromwell');
+  await user.pointer([{ pointerName: 'mouse', target: cromwell }]);
+  // Show tooltip when hovering over disabled Cromwell.
+  await screen.findByText(
+    'You have either run out of initial credits or have an inactive billing account.'
+  );
+  cromwell.click();
+  // Expecting GKE Apps, such as Cromwell, to be disabled. If
+  // they were not, the apps panel would close after clicking on
+  // an unexpanded GKE app.
+  expect(findAvailableApps(false)).toBeInTheDocument();
+};
+
 describe(AppsPanel.name, () => {
   const appsStub = new AppsApiStub();
   const runtimeStub = new RuntimeApiStub();
   const leoAppsStub = new LeoAppsApiStub();
-  let user;
+  let user: UserEvent;
   beforeEach(() => {
+    const freeTierBillingAccountId = 'FreeTierBillingAccountId';
     registerApiClient(AppsApi, appsStub);
     registerApiClient(NotebooksApi, new NotebooksApiStub());
     registerApiClient(RuntimeApi, runtimeStub);
     registerLeoApiClient(LeoAppsApi, leoAppsStub);
     workspaceStub.billingStatus = BillingStatus.ACTIVE;
+    workspaceStub.billingAccountName = `billingAccounts/${freeTierBillingAccountId}`;
     runtimeStore.set({
       workspaceNamespace: workspaceStub.namespace,
       runtime: runtimeStub.runtime,
       runtimeLoaded: true,
+    });
+    serverConfigStore.set({
+      config: { ...defaultServerConfig, freeTierBillingAccountId },
     });
     user = userEvent.setup();
   });
@@ -100,24 +158,121 @@ describe(AppsPanel.name, () => {
     const { container } = await component();
     expect(container).toBeInTheDocument();
 
+    await expectExpandableApp(user, 'Jupyter');
+  });
+
+  it('should disable apps when initial credit expiration is true and initial credits are exhausted', async () => {
+    serverConfigStore.set({
+      config: {
+        ...serverConfigStore.get().config,
+        enableInitialCreditsExpiration: true,
+      },
+    });
+    // initial state: no apps exist
+    workspaceStub.initialCredits.exhausted = true;
+    workspaceStub.initialCredits.expired = false;
+    workspaceStub.initialCredits.expirationBypassed = false;
+
+    runtimeStub.runtime.status = undefined;
+    appsStub.listAppsResponse = [];
+
+    const { container } = await component();
+    expect(container).toBeInTheDocument();
+
     expect(findActiveApps()).not.toBeInTheDocument();
     expect(findAvailableApps(false)).toBeInTheDocument();
 
     // Click unexpanded Jupyter app
 
     const jupyter = expectUnexpandedApp('Jupyter');
+    await user.pointer([{ pointerName: 'mouse', target: jupyter }]);
+    // Show tooltip when hovering over disabled Jupyter.
+    await screen.findByText(
+      'You have either run out of initial credits or have an inactive billing account.'
+    );
     jupyter.click();
+    // Expecting Jupyter to be disabled. If it was not, the
+    // "Create New" button would show after clicking on Jupyter.
+    expect(screen.queryByText('Create New')).not.toBeInTheDocument();
 
-    await waitFor(() => expectExpandedApp('Jupyter'));
-
-    // the overall apps panel state doesn't change: there are still no ActiveApps
-    // the newly expanded app is in the AvailableApps section
-
-    expect(findActiveApps()).not.toBeInTheDocument();
+    const cromwell = expectUnexpandedApp('Cromwell');
+    await user.pointer([{ pointerName: 'mouse', target: cromwell }]);
+    // Show tooltip when hovering over disabled Cromwell.
+    await screen.findByText(
+      'You have either run out of initial credits or have an inactive billing account.'
+    );
+    cromwell.click();
+    // Expecting GKE Apps, such as Cromwell, to be disabled. If
+    // they were not, the apps panel would close after clicking on
+    // an unexpanded GKE app.
     expect(findAvailableApps(false)).toBeInTheDocument();
   });
 
-  it('should disable apps when billing is inactive', async () => {
+  it('should disable apps when initial credit expiration is true and initial credits are exhausted', async () => {
+    serverConfigStore.set({
+      config: {
+        ...serverConfigStore.get().config,
+        enableInitialCreditsExpiration: true,
+      },
+    });
+    // initial state: no apps exist
+    workspaceStub.initialCredits.exhausted = true;
+    workspaceStub.initialCredits.expired = false;
+    workspaceStub.initialCredits.expirationBypassed = false;
+
+    runtimeStub.runtime.status = undefined;
+    appsStub.listAppsResponse = [];
+
+    const { container } = await component();
+    expect(container).toBeInTheDocument();
+    await expectAppsPanelToBeDisabled(user);
+  });
+
+  it('should disable apps when initial credit expiration is true and initial credits are expired', async () => {
+    serverConfigStore.set({
+      config: {
+        ...serverConfigStore.get().config,
+        enableInitialCreditsExpiration: true,
+      },
+    });
+    // initial state: no apps exist
+    workspaceStub.initialCredits.exhausted = false;
+    workspaceStub.initialCredits.expired = true;
+    workspaceStub.initialCredits.expirationBypassed = false;
+
+    runtimeStub.runtime.status = undefined;
+    appsStub.listAppsResponse = [];
+
+    const { container } = await component();
+    expect(container).toBeInTheDocument();
+    await expectAppsPanelToBeDisabled(user);
+  });
+
+  it('should disable apps when initial credit expiration is true, initial credits are expired, and expiration is bypassed', async () => {
+    serverConfigStore.set({
+      config: {
+        ...serverConfigStore.get().config,
+        enableInitialCreditsExpiration: true,
+      },
+    });
+    // initial state: no apps exist
+    workspaceStub.initialCredits.exhausted = false;
+    workspaceStub.initialCredits.expired = true;
+    workspaceStub.initialCredits.expirationBypassed = true;
+
+    runtimeStub.runtime.status = undefined;
+    appsStub.listAppsResponse = [];
+
+    const { container } = await component();
+    expect(container).toBeInTheDocument();
+
+    await expectExpandableApp(user, 'Jupyter');
+  });
+
+  it('should disable apps when billing is inactive and credit expiration is not active', async () => {
+    serverConfigStore.set({
+      config: { ...defaultServerConfig, enableInitialCreditsExpiration: false },
+    });
     // initial state: no apps exist
     workspaceStub.billingStatus = BillingStatus.INACTIVE;
 
