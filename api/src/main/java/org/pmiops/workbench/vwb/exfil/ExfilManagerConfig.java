4f039980c164385174a9c1471dc5df94d77bccbe
Yonghao Yu <yyhao1@gmail.com>
2025-01-15 17:51:19 -0500

    try to resolve bean

--- api/src/main/java/org/pmiops/workbench/vwb/exfil/ExfilManagerConfig.java
+++ api/src/main/java/org/pmiops/workbench/vwb/exfil/ExfilManagerConfig.java
@@ -1,12 +1,12 @@
 package org.pmiops.workbench.vwb.exfil;
 
-import jakarta.inject.Qualifier;
 import java.io.IOException;
 import org.pmiops.workbench.auth.ServiceAccounts;
 import org.pmiops.workbench.config.WorkbenchConfig;
 import org.pmiops.workbench.exceptions.ServerErrorException;
 import org.pmiops.workbench.vwb.common.VwbApiClientUtils;
 import org.pmiops.workbench.vwb.exfil.api.EgressEventApi;
+import org.springframework.beans.factory.annotation.Qualifier;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.context.annotation.ScopedProxyMode;
@@ -14,9 +14,11 @@ import org.springframework.web.context.annotation.RequestScope;
 
 @Configuration
 public class ExfilManagerConfig {
-  @Bean
+  public static final String EXFIL_SERVICE_ACCOUNT_API_CLIENT = "EXFIL_SERVICE_ACCOUNT_API_CLIENT";
+
+  @Bean(name = EXFIL_SERVICE_ACCOUNT_API_CLIENT)
   @RequestScope(proxyMode = ScopedProxyMode.DEFAULT)
-  public org.pmiops.workbench.vwb.exfil.ApiClient apiClient(WorkbenchConfig workbenchConfig) {
+  public ApiClient apiClient(WorkbenchConfig workbenchConfig) {
     ApiClient apiClient = new ApiClient();
     apiClient.setBasePath(workbenchConfig.vwb.exfilManagerBaseUrl);
     try {
@@ -30,7 +32,8 @@ public class ExfilManagerConfig {
 
   @Bean
   @RequestScope(proxyMode = ScopedProxyMode.DEFAULT)
-  public EgressEventApi egressEventApi(ApiClient apiClient) {
+  public EgressEventApi egressEventApi(
+      @Qualifier(EXFIL_SERVICE_ACCOUNT_API_CLIENT) ApiClient apiClient) {
     return new EgressEventApi(apiClient);
   }
 }
