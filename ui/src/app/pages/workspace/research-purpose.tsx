577687bb8b6f98be0dfc9718646a6d3a1a5354ab
Eric Rollins <erollins@broadinstitute.org>
2025-01-21 10:10:14 -0500

    Have workspace about page utilize common research purpose component.

--- ui/src/app/pages/workspace/research-purpose.tsx
+++ ui/src/app/pages/workspace/research-purpose.tsx
@@ -3,20 +3,12 @@ import * as fp from 'lodash/fp';
 
 import { Clickable } from 'app/components/buttons';
 import { FadeBox } from 'app/components/containers';
+import { FlexRow } from 'app/components/flex';
+import { ResearchPurposeSection } from 'app/components/research-purpose-section';
 import { EditComponentReact } from 'app/icons/edit';
-import {
-  aianResearchTypeMap,
-  disseminateFindings,
-  researchOutcomes,
-  researchPurposeQuestions,
-} from 'app/pages/workspace/workspace-edit-text';
 import colors, { colorWithWhiteness } from 'app/styles/colors';
 import { reactStyles, withCurrentWorkspace } from 'app/utils';
 import { useNavigation } from 'app/utils/navigation';
-import {
-  getSelectedPopulations,
-  getSelectedResearchPurposeItems,
-} from 'app/utils/research-purpose';
 import { withNavigation } from 'app/utils/with-navigation-hoc';
 import { WorkspaceData } from 'app/utils/workspace-data';
 import { WorkspacePermissionsUtil } from 'app/utils/workspace-permissions';
@@ -35,7 +27,6 @@ const styles = reactStyles({
     fontSize: '18px',
     fontWeight: 600,
     color: colors.primary,
-    marginBottom: '0.75rem',
     display: 'flex',
     flexDirection: 'row',
     alignItems: 'center',
@@ -88,19 +79,11 @@ export const ResearchPurpose = fp.flow(
 )(({ workspace }: { workspace: WorkspaceData }) => {
   const [navigate] = useNavigation();
   const isOwner = WorkspacePermissionsUtil.isOwner(workspace.accessLevel);
-  const selectedResearchPurposeItems = getSelectedResearchPurposeItems(
-    workspace.researchPurpose,
-    true
-  );
-  const selectedPrimaryPurposeItems = getSelectedResearchPurposeItems(
-    workspace.researchPurpose,
-    false
-  );
 
   return (
     <FadeBox>
-      <div style={styles.mainHeader}>
-        Research Use Statement Questions
+      <FlexRow>
+        <div style={styles.mainHeader}>Research Use Statement Questions</div>
         <Clickable
           disabled={!isOwner}
           style={{
@@ -124,137 +107,8 @@ export const ResearchPurpose = fp.flow(
             style={styles.editIcon}
           />
         </Clickable>
-      </div>
-      <div style={styles.sectionHeader}>Primary purpose of project</div>
-      <div style={styles.sectionContentContainer}>
-        {selectedResearchPurposeItems &&
-          selectedResearchPurposeItems.length > 0 && (
-            <div style={styles.sectionSubHeader}>Research Purpose</div>
-          )}
-        {selectedResearchPurposeItems.map((selectedResearchPurposeItem, i) => (
-          <div key={i}>
-            <div
-              data-test-id='primaryResearchPurpose'
-              style={{
-                marginTop: i > 0 ? '1.5rem' : '0.45rem',
-                marginLeft: '1.5rem',
-              }}
-            >
-              {selectedResearchPurposeItem}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div style={styles.sectionContentContainer}>
-        {selectedPrimaryPurposeItems.map((selectedPrimaryPurposeItem, i) => (
-          <div key={i}>
-            <div data-test-id='primaryPurpose' style={{ marginTop: '1.5rem' }}>
-              {selectedPrimaryPurposeItem}
-            </div>
-          </div>
-        ))}
-      </div>
-      <div style={styles.sectionHeader}>Summary of research purpose</div>
-      <div style={styles.sectionContentContainer}>
-        {/* Intended study section*/}
-        <div style={styles.sectionSubHeader}>
-          {researchPurposeQuestions[2].header}
-        </div>
-        <div style={{ ...styles.sectionItemWithBackground, padding: '15px' }}>
-          {workspace.researchPurpose.intendedStudy}
-        </div>
-
-        {/* Scientific approach section*/}
-        <div style={styles.sectionSubHeader}>
-          {researchPurposeQuestions[3].header}
-        </div>
-        <div style={{ ...styles.sectionItemWithBackground, padding: '15px' }}>
-          {workspace.researchPurpose.scientificApproach}
-        </div>
-
-        {/* Anticipated findings section*/}
-        <div style={styles.sectionSubHeader}>
-          {researchPurposeQuestions[4].header}
-        </div>
-        <div style={{ ...styles.sectionItemWithBackground, padding: '15px' }}>
-          {workspace.researchPurpose.anticipatedFindings}
-        </div>
-      </div>
-
-      {/* Findings section*/}
-      <div style={styles.sectionHeader}>Findings will be disseminated via:</div>
-      <div style={styles.sectionContentContainer}>
-        {workspace.researchPurpose.disseminateResearchFindingList.map(
-          (disseminateFinding, i) => (
-            <div
-              key={i}
-              style={{
-                ...styles.sectionItemWithBackground,
-                marginTop: '0.75rem',
-              }}
-            >
-              {
-                disseminateFindings.find(
-                  (finding) => finding.shortName === disseminateFinding
-                ).label
-              }
-            </div>
-          )
-        )}
-      </div>
-
-      {/* Outcomes section*/}
-      <div style={styles.sectionHeader}>
-        Outcomes anticipated from the research:
-      </div>
-      <div style={styles.sectionContentContainer}>
-        {workspace.researchPurpose.researchOutcomeList.map(
-          (workspaceOutcome, i) => (
-            <div
-              key={i}
-              style={{
-                ...styles.sectionItemWithBackground,
-                marginTop: '0.75rem',
-              }}
-            >
-              {
-                researchOutcomes.find(
-                  (outcome) => outcome.shortName === workspaceOutcome
-                ).label
-              }
-            </div>
-          )
-        )}
-      </div>
-
-      {/* Underserved populations section*/}
-      {workspace.researchPurpose.populationDetails.length > 0 && (
-        <React.Fragment>
-          <div style={styles.sectionHeader}>Population of interest</div>
-          <div style={styles.sectionContentContainer}>
-            <div style={{ marginTop: '0.75rem' }}>
-              {getSelectedPopulations(workspace.researchPurpose)}
-            </div>
-          </div>
-        </React.Fragment>
-      )}
-
-      {/* AI/AN Research Approach section*/}
-      <div style={styles.sectionHeader}>AI/AN Research Approach</div>
-      <div style={styles.sectionContentContainer}>
-        <div style={styles.sectionSubHeader}>
-          {researchPurposeQuestions[10].header}
-        </div>
-        <div style={{ ...styles.sectionItemWithBackground, padding: '15px' }}>
-          {aianResearchTypeMap.get(workspace.researchPurpose.aianResearchType)}
-        </div>
-        <div style={styles.sectionSubHeader}>
-          {researchPurposeQuestions[11].header}
-        </div>
-        <div style={{ ...styles.sectionItemWithBackground, padding: '15px' }}>
-          {workspace.researchPurpose.aianResearchDetails}
-        </div>
-      </div>
+      </FlexRow>
+      <ResearchPurposeSection researchPurpose={workspace.researchPurpose} />
     </FadeBox>
   );
 });
