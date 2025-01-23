db532a1f0bda973ca47270d5bf2ab9420fc940df
Eric Rollins <erollins@broadinstitute.org>
2025-01-23 13:47:39 -0500

    First pass at Leo maintenance banner.

--- ui/src/app/pages/login/login-error-banner.tsx
+++ ui/src/app/pages/login/login-error-banner.tsx
@@ -13,15 +13,16 @@ export const LOGIN_ERROR_BANNER = () => (
     }}
   >
     <ErrorMessage>
-      The <AoU /> Researcher Workbench is currently experiencing an outage. Our
-      team continues to work diligently to resolve this issue as quickly as
-      possible, and we apologize for any inconveniences this may cause. We will
-      provide updates via the
-      <a href='https://support.researchallofus.org/hc/en-us' target='blank'>
-        {' '}
-        User Support Hub homepage
-      </a>{' '}
-      when available.
+      <>
+        <div>
+          Some of <AoU /> Researcher Workbench's supporting services will be
+          down for maintenance on Sunday (January 26th) from 10-11AM ET.
+        </div>
+        <div>
+          Users will not be able to access Cromwell, Jupyter, RStudio, or SAS
+          during that time, but all ongoing jobs will not be interrupted.
+        </div>
+      </>
     </ErrorMessage>
   </div>
 );
