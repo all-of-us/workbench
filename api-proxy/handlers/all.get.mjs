4dc954aa22e8eaf35f7857a7397a404df9450212
Eric Rollins <erollins@broadinstitute.org>
2025-01-24 14:43:44 -0500

    Delete renamed files

--- api-proxy/handlers/all.get.mjs
+++
@@ -1,7 +0,0 @@
-const handleReq = (req, res) => {
-  if (!(req.url.pathname === '/'
-    && req.url.search === ''
-    && req.method === 'GET')) { return }
-  res.mwrite(`AllOfUs Workbench API`).mend()
-}
-export default handleReq
