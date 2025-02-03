2337041052e8136f2f74fbf106342a6f7a3525b9
Eric Rollins <erollins@broadinstitute.org>
2025-02-03 09:23:45 -0500

    Add functionality to hide/show AI/AN questions to utility file.

--- ui/src/app/utils/workspace-utils.tsx
+++ ui/src/app/utils/workspace-utils.tsx
@@ -2,6 +2,7 @@ import { FeaturedWorkspaceCategory, Workspace } from 'generated/fetch';
 
 import { serverConfigStore } from 'app/utils/stores';
 
+export const EARLIEST_PUBLIC_CDR_VERSION_NUMBER_INCLUDING_AIAN = 8;
 export const isUsingFreeTierBillingAccount = (
   workspace: Workspace
 ): boolean => {
@@ -14,3 +15,6 @@ export const isUsingFreeTierBillingAccount = (
 export const isCommunityWorkspace = (workspace: Workspace): boolean => {
   return workspace.featuredCategory === FeaturedWorkspaceCategory.COMMUNITY;
 };
+
+export const showAIANReasearchPurpose = (publicCDRReleaseNumber): boolean =>
+  publicCDRReleaseNumber >= EARLIEST_PUBLIC_CDR_VERSION_NUMBER_INCLUDING_AIAN;
