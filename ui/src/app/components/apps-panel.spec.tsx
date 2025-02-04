3e9abea9c3b0b6d29273a58e6fcb7fe863c44501
Eric Rollins <erollins@broadinstitute.org>
2025-02-04 12:25:29 -0500

    Removed use of expired field and consolidated logic.

--- ui/src/app/components/apps-panel.spec.tsx
+++ ui/src/app/components/apps-panel.spec.tsx
@@ -9,6 +9,7 @@ import {
   RuntimeApi,
 } from 'generated/fetch';
 
+import { WorkspaceData } from '../utils/workspace-data';
 import { render, screen, waitFor } from '@testing-library/react';
 import userEvent, { UserEvent } from '@testing-library/user-event';
 import { registerApiClient as registerLeoApiClient } from 'app/services/notebooks-swagger-fetch-clients';
@@ -26,8 +27,9 @@ import { workspaceDataStub } from 'testing/stubs/workspaces';
 import { AppsPanel } from './apps-panel';
 
 const stubFunction = () => ({});
+const NOW = new Date('2020-01-01');
 
-const workspaceStub = workspaceDataStub;
+let workspaceStub: WorkspaceData;
 
 const component = async () =>
   render(
@@ -39,6 +41,18 @@ const component = async () =>
       onClickDeleteGkeApp={stubFunction}
     />
   );
+
+const mockInitialCredits = (
+  exhausted: boolean,
+  expired: boolean,
+  expirationBypassed: boolean
+) => {
+  workspaceStub.initialCredits = {
+    exhausted,
+    expirationEpochMillis: NOW.getTime() + (expired ? -1 : 1),
+    expirationBypassed,
+  };
+};
 
 const findActiveApps = () => screen.queryByText('Active Applications');
 
@@ -101,7 +115,6 @@ const expectAppsInAppsPanelToBeDisabled = async (user: UserEvent) => {
 
   await user.pointer([{ pointerName: 'mouse', target: jupyter }]);
 
-  screen.logTestingPlaygroundURL();
   // Show tooltip when hovering over disabled Jupyter.
   await screen.findByText(
     'You have either run out of initial credits or have an inactive billing account.'
@@ -136,8 +149,13 @@ describe(AppsPanel.name, () => {
     registerApiClient(NotebooksApi, new NotebooksApiStub());
     registerApiClient(RuntimeApi, runtimeStub);
     registerLeoApiClient(LeoAppsApi, leoAppsStub);
-    workspaceStub.billingStatus = BillingStatus.ACTIVE;
-    workspaceStub.billingAccountName = `billingAccounts/${freeTierBillingAccountId}`;
+    runtimeStub.runtime.status = undefined;
+    appsStub.listAppsResponse = [];
+    workspaceStub = {
+      ...workspaceDataStub,
+      billingStatus: BillingStatus.ACTIVE,
+      billingAccountName: `billingAccounts/${freeTierBillingAccountId}`,
+    };
     runtimeStore.set({
       workspaceNamespace: workspaceStub.namespace,
       runtime: runtimeStub.runtime,
@@ -146,6 +164,7 @@ describe(AppsPanel.name, () => {
     serverConfigStore.set({
       config: { ...defaultServerConfig, freeTierBillingAccountId },
     });
+    jest.useFakeTimers().setSystemTime(NOW);
     // AdvanceTimersByTime in order to allow fake timers and RTL queries to work together
     // https://testing-library.com/docs/user-event/options/#advancetimers
     user = userEvent.setup({ advanceTimers: jest.advanceTimersByTime });
@@ -157,16 +176,8 @@ describe(AppsPanel.name, () => {
   });
 
   it('should allow a user to expand Jupyter', async () => {
-    // initial state: no apps exist
-
-    runtimeStub.runtime.status = undefined;
-    appsStub.listAppsResponse = [];
-    jest.useFakeTimers().setSystemTime(new Date('2020-01-01'));
-    workspaceStub.initialCredits.expirationEpochMillis = new Date(
-      '2025-01-01'
-    ).getTime();
-    const { container } = await component();
-    expect(container).toBeInTheDocument();
+    mockInitialCredits(false, false, false);
+    await component();
 
     await expectExpandableApp(user, 'Jupyter');
   });
@@ -178,17 +189,7 @@ describe(AppsPanel.name, () => {
         enableInitialCreditsExpiration: true,
       },
     });
-    jest.useFakeTimers().setSystemTime(new Date('2020-01-01'));
-
-    // initial state: no apps exist
-    workspaceStub.initialCredits.exhausted = true;
-    workspaceStub.initialCredits.expirationEpochMillis = new Date(
-      '2025-01-01'
-    ).getTime();
-    workspaceStub.initialCredits.expirationBypassed = false;
-
-    runtimeStub.runtime.status = undefined;
-    appsStub.listAppsResponse = [];
+    mockInitialCredits(true, false, false);
 
     const { container } = await component();
     expect(container).toBeInTheDocument();
@@ -222,107 +223,42 @@ describe(AppsPanel.name, () => {
     expect(findAvailableApps(false)).toBeInTheDocument();
   });
 
-  it('should disable apps when initial credit expiration is true and initial credits are exhausted', async () => {
+  it('should disable apps when initial credit expiration is enabled and initial credits are expired', async () => {
     serverConfigStore.set({
       config: {
         ...serverConfigStore.get().config,
         enableInitialCreditsExpiration: true,
       },
     });
-    // initial state: no apps exist
-    workspaceStub.initialCredits.exhausted = true;
-    workspaceStub.initialCredits.expired = false;
-    workspaceStub.initialCredits.expirationBypassed = false;
+    mockInitialCredits(false, true, false);
 
-    runtimeStub.runtime.status = undefined;
-    appsStub.listAppsResponse = [];
-
-    const { container } = await component();
-    expect(container).toBeInTheDocument();
+    await component();
     await expectAppsInAppsPanelToBeDisabled(user);
   });
 
-  it('should disable apps when initial credit expiration is true and initial credits are expired', async () => {
+  it('should disable apps when initial credit expiration is enabled, initial credits are expired, and expiration is bypassed', async () => {
     serverConfigStore.set({
       config: {
         ...serverConfigStore.get().config,
         enableInitialCreditsExpiration: true,
       },
     });
-    // initial state: no apps exist
-    workspaceStub.initialCredits.exhausted = false;
-    workspaceStub.initialCredits.expired = true;
-    workspaceStub.initialCredits.expirationBypassed = false;
+    mockInitialCredits(false, true, true);
 
-    runtimeStub.runtime.status = undefined;
-    appsStub.listAppsResponse = [];
-
-    const { container } = await component();
-    expect(container).toBeInTheDocument();
-    await expectAppsInAppsPanelToBeDisabled(user);
-  });
-
-  it('should disable apps when initial credit expiration is true, initial credits are expired, and expiration is bypassed', async () => {
-    serverConfigStore.set({
-      config: {
-        ...serverConfigStore.get().config,
-        enableInitialCreditsExpiration: true,
-      },
-    });
-    // initial state: no apps exist
-    workspaceStub.initialCredits.exhausted = false;
-    workspaceStub.initialCredits.expired = true;
-    workspaceStub.initialCredits.expirationBypassed = true;
-
-    runtimeStub.runtime.status = undefined;
-    appsStub.listAppsResponse = [];
-
-    const { container } = await component();
-    expect(container).toBeInTheDocument();
+    await component();
 
     await expectExpandableApp(user, 'Jupyter');
   });
 
-  it('should disable apps when billing is inactive and credit expiration is not active', async () => {
+  it('should disable apps when initial credit expiration is enabled and billing is inactive', async () => {
     serverConfigStore.set({
       config: { ...defaultServerConfig, enableInitialCreditsExpiration: false },
     });
-    // initial state: no apps exist
     workspaceStub.billingStatus = BillingStatus.INACTIVE;
 
-    runtimeStub.runtime.status = undefined;
-    appsStub.listAppsResponse = [];
+    await component();
 
-    const { container } = await component();
-    expect(container).toBeInTheDocument();
-
-    expect(findActiveApps()).not.toBeInTheDocument();
-    expect(findAvailableApps(false)).toBeInTheDocument();
-
-    // Click unexpanded Jupyter app
-
-    const jupyter = expectUnexpandedApp('Jupyter');
-    await user.pointer([{ pointerName: 'mouse', target: jupyter }]);
-    // Show tooltip when hovering over disabled Jupyter.
-    await screen.findByText(
-      'You have either run out of initial credits or have an inactive billing account.'
-    );
-    jupyter.click();
-    // Expecting Jupyter to be disabled. If it was not, the
-    // "Create New" button would show after clicking on Jupyter.
-    expect(screen.queryByText('Create New')).not.toBeInTheDocument();
-
-    const cromwell = expectUnexpandedApp('Cromwell');
-    await user.pointer([{ pointerName: 'mouse', target: cromwell }]);
-    // Show tooltip when hovering over disabled Cromwell.
-    await screen.findByText(
-      'You have either run out of initial credits or have an inactive billing account.'
-    );
-    cromwell.click();
-    // Expecting GKE Apps, such as Cromwell, to be disabled. If
-    // they were not, the apps panel would close after clicking on
-    // an unexpanded GKE app.
-    expect(findAvailableApps(false)).toBeInTheDocument();
+    await expectAppsInAppsPanelToBeDisabled(user);
   });
 
   it('should show unexpanded apps by default', async () => {
