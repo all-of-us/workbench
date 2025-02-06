dc98e4c27e5ff10670b6c33d9934ed28f458a828
Eric Rollins <erollins@broadinstitute.org>
2025-02-06 17:12:02 -0500

    Adding Dao test.

--- api/src/test/java/org/pmiops/workbench/db/dao/WorkspaceDaoTest.java
+++ api/src/test/java/org/pmiops/workbench/db/dao/WorkspaceDaoTest.java
@@ -9,9 +9,12 @@ import java.util.Set;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.pmiops.workbench.FakeClockConfiguration;
+import org.pmiops.workbench.db.model.DbInstitution;
 import org.pmiops.workbench.db.model.DbUser;
 import org.pmiops.workbench.db.model.DbUserInitialCreditsExpiration;
+import org.pmiops.workbench.db.model.DbVerifiedInstitutionalAffiliation;
 import org.pmiops.workbench.db.model.DbWorkspace;
+import org.pmiops.workbench.model.InstitutionalRole;
 import org.pmiops.workbench.model.ReportingUser;
 import org.pmiops.workbench.testconfig.ReportingTestConfig;
 import org.pmiops.workbench.testconfig.fixtures.ReportingTestFixture;
@@ -28,9 +31,13 @@ public class WorkspaceDaoTest {
   @Autowired UserDao userDao;
   @Autowired ReportingTestFixture<DbUser, ReportingUser> userFixture;
   @Autowired WorkspaceDao workspaceDao;
+  @Autowired InstitutionDao institutionDao;
+  @Autowired VerifiedInstitutionalAffiliationDao verifiedInstitutionalAffiliationDao;
 
   private DbUser dbUser;
   private DbWorkspace dbWorkspace;
+  private DbInstitution dbInstitution;
+  private DbVerifiedInstitutionalAffiliation dbVerifiedInstitutionalAffiliation;
 
   @TestConfiguration
   @Import({FakeClockConfiguration.class, ReportingTestConfig.class})
@@ -48,6 +55,19 @@ public class WorkspaceDaoTest {
     dbWorkspace = workspaceDao.save(dbWorkspace);
 
     dbUser = userDao.save(new DbUser());
+
+    dbInstitution =
+        new DbInstitution()
+            .setShortName("Test Institution")
+            .setDisplayName("Test Institution")
+            .setBypassInitialCreditsExpiration(false);
+    dbInstitution = institutionDao.save(dbInstitution);
+    dbVerifiedInstitutionalAffiliation =
+        verifiedInstitutionalAffiliationDao.save(
+            new DbVerifiedInstitutionalAffiliation()
+                .setInstitution(dbInstitution)
+                .setUser(dbUser)
+                .setInstitutionalRoleEnum(InstitutionalRole.HIGH_SCHOOL_STUDENT));
   }
 
   @Test
@@ -119,10 +139,56 @@ public class WorkspaceDaoTest {
     dbWorkspace.setCreator(dbUser);
     dbWorkspace = workspaceDao.save(dbWorkspace);
 
-    var y = workspaceDao.findCreatorsByActiveInitialCredits(
-        List.of("initialCreditsAccount"), Set.of(dbUser));
+    var y =
+        workspaceDao.findCreatorsByActiveInitialCredits(
+            List.of("initialCreditsAccount"), Set.of(dbUser));
 
     assertThat(y).isEqualTo(Collections.emptySet());
+  }
+
+  @Test
+  public void findCreatorsByActiveInitialCredits_withExpiredCreditsButIndividuallyBypassed() {
+    long MILLIS_IN_A_DAY = 24 * 60 * 60 * 1000;
+    DbUserInitialCreditsExpiration dbUserInitialCreditsExpiration =
+        new DbUserInitialCreditsExpiration()
+            .setCreditStartTime(new Timestamp(System.currentTimeMillis()))
+            .setExpirationTime(new Timestamp(System.currentTimeMillis() - MILLIS_IN_A_DAY))
+            .setBypassed(true)
+            .setUser(dbUser);
+    dbUser = dbUser.setUserInitialCreditsExpiration(dbUserInitialCreditsExpiration);
+    dbUser = userDao.save(dbUser);
+    dbWorkspace.setBillingAccountName("initialCreditsAccount");
+    dbWorkspace.setCreator(dbUser);
+    dbWorkspace = workspaceDao.save(dbWorkspace);
+
+    var y =
+        workspaceDao.findCreatorsByActiveInitialCredits(
+            List.of("initialCreditsAccount"), Set.of(dbUser));
+
+    assertThat(y).isEqualTo(Set.of(dbUser));
+  }
+
+  @Test
+  public void findCreatorsByActiveInitialCredits_withExpiredCreditsButInstitutionallyBypassed() {
+    long MILLIS_IN_A_DAY = 24 * 60 * 60 * 1000;
+    DbUserInitialCreditsExpiration dbUserInitialCreditsExpiration =
+        new DbUserInitialCreditsExpiration()
+            .setCreditStartTime(new Timestamp(System.currentTimeMillis()))
+            .setExpirationTime(new Timestamp(System.currentTimeMillis() - MILLIS_IN_A_DAY))
+            .setUser(dbUser);
+    dbUser = dbUser.setUserInitialCreditsExpiration(dbUserInitialCreditsExpiration);
+    dbUser = userDao.save(dbUser);
+    dbWorkspace.setBillingAccountName("initialCreditsAccount");
+    dbWorkspace.setCreator(dbUser);
+    dbWorkspace = workspaceDao.save(dbWorkspace);
+
+    institutionDao.save(dbInstitution.setBypassInitialCreditsExpiration(true));
+
+    var y =
+        workspaceDao.findCreatorsByActiveInitialCredits(
+            List.of("initialCreditsAccount"), Set.of(dbUser));
+
+    assertThat(y).isEqualTo(Set.of(dbUser));
   }
 
   @Test
