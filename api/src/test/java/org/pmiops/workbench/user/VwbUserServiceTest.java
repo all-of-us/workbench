8649fcdedb224765fba75c029825bbf4df8eb812
Tarek M. Ahmed <tarekahmed@verily.com>
2025-02-13 10:01:25 -0500

    RW-13692 Add new table for vwb pod id

---
+++ api/src/test/java/org/pmiops/workbench/user/VwbUserServiceTest.java
@@ -0,0 +1,11 @@
+package org.pmiops.workbench.user;
+
+import org.junit.jupiter.api.Test;
+
+import static org.junit.jupiter.api.Assertions.*;
+class VwbUserServiceTest {
+
+    @Test
+    void createUser() {
+      }
+}
