afca9c7c296ac8e3d88d3340edb928c70cfc189f
Joel Thibault <thibault@broad.dev>
2025-02-24 17:24:51 -0500

    comment

--- api/src/main/java/org/pmiops/workbench/cloudtasks/EnumSerializer.java
+++ api/src/main/java/org/pmiops/workbench/cloudtasks/EnumSerializer.java
@@ -6,8 +6,7 @@ import com.google.gson.JsonSerializationContext;
 import com.google.gson.JsonSerializer;
 import java.lang.reflect.Type;
 
-// serializes enum values as their values instead of their enum constants
-// needed for local execution of Task Queues
+// serializes enum values as their values instead of their enum constants, needed by Task Queues
 // https://stackoverflow.com/questions/63059972/make-gson-to-use-enum-string-value-instead-of-constant-name
 public class EnumSerializer<T extends Enum<T>> implements JsonSerializer<T> {
   @Override
