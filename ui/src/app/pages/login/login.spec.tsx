5166fbe4b873ff3853044d0f54e602d868c422d6
Eric Rollins <erollins@broadinstitute.org>
2025-03-10 16:49:16 -0400

    Cleanup test

--- ui/src/app/pages/login/login.spec.tsx
+++ ui/src/app/pages/login/login.spec.tsx
@@ -3,10 +3,17 @@ import '@testing-library/jest-dom';
 import * as React from 'react';
 import { MemoryRouter } from 'react-router-dom';
 
-import { render, screen } from '@testing-library/react';
+import { StatusAlertApi, StatusAlertLocation } from 'generated/fetch';
+
+import { render, screen, waitFor } from '@testing-library/react';
+import {
+  clearApiClients,
+  registerApiClient,
+} from 'app/services/swagger-fetch-clients';
 import { serverConfigStore } from 'app/utils/stores';
 
 import defaultServerConfig from 'testing/default-server-config';
+import { StatusAlertApiStub } from 'testing/stubs/status-alert-api-stub';
 
 import LoginReactComponent from './login';
 
@@ -25,24 +32,32 @@ describe('LoginComponent', () => {
       signIn: () => {},
       onCreateAccount: () => {},
     };
-    serverConfigStore.set({
-      config: {
-        ...defaultServerConfig,
-        enableLoginIssueBanner: false,
-      },
-    });
+  });
+
+  afterEach(() => {
+    clearApiClients();
+    jest.resetAllMocks();
   });
 
   it('should render', () => {
+    registerApiClient(
+      StatusAlertApi,
+      new StatusAlertApiStub(StatusAlertLocation.AFTER_LOGIN)
+    );
     component(loginProps);
     expect(
       screen.getByText('Already have a Researcher Workbench account?')
     ).toBeInTheDocument();
   });
 
-  it('should show banner if flag is on', () => {
-    serverConfigStore.get().config.enableLoginIssueBanner = true;
+  it('should show banner status alert is BEFORE_LOGIN', async () => {
+    registerApiClient(
+      StatusAlertApi,
+      new StatusAlertApiStub(StatusAlertLocation.BEFORE_LOGIN)
+    );
     component(loginProps);
-    expect(screen.getByText(/Scheduled Downtime Notice/i)).toBeInTheDocument();
+    expect(
+      await screen.findByText(/Scheduled Downtime Notice/i)
+    ).toBeInTheDocument();
   });
 });
