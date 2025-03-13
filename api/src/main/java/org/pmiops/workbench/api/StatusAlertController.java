2df7015bb78f5b706204871e4b91beb91b3b2d5c
Eric Rollins <erollins@broadinstitute.org>
2025-03-13 13:08:05 -0400

    Change status alert endpoint to handle multiple status alerts.

--- api/src/main/java/org/pmiops/workbench/api/StatusAlertController.java
+++ api/src/main/java/org/pmiops/workbench/api/StatusAlertController.java
@@ -1,5 +1,7 @@
 package org.pmiops.workbench.api;
 
+import java.util.List;
+import java.util.stream.Collectors;
 import org.pmiops.workbench.annotations.AuthorityRequired;
 import org.pmiops.workbench.db.dao.StatusAlertDao;
 import org.pmiops.workbench.db.model.DbStatusAlert;
@@ -22,25 +24,18 @@ public class StatusAlertController implements StatusAlertApiDelegate {
   }
 
   @Override
-  public ResponseEntity<StatusAlert> getStatusAlert() {
+  public ResponseEntity<List<StatusAlert>> getStatusAlerts() {
     return ResponseEntity.ok(
         statusAlertDao
-            .findFirstByOrderByStatusAlertIdDesc()
+            .findAllByOrderByStatusAlertIdDesc()
+            .stream()
             .map(statusAlertMapper::toStatusAlert)
-            .orElse(new StatusAlert()));
+            .collect(Collectors.toList()));
   }
 
   @Override
   @AuthorityRequired(Authority.COMMUNICATIONS_ADMIN)
   public ResponseEntity<StatusAlert> postStatusAlert(StatusAlert statusAlert) {
-    // Current assumption is only one banner at a time, therefore, we must delete all old banners
-    // before saving
-    // a new one.
-    Iterable<DbStatusAlert> dbStatusAlertList = statusAlertDao.findAll();
-    dbStatusAlertList.forEach(
-        statusAlert1 -> statusAlertDao.deleteById(statusAlert1.getStatusAlertId()));
-    // If we support multiple banners at a time, the above lines can be deleted.
-
     DbStatusAlert dbStatusAlert =
         statusAlertDao.save(statusAlertMapper.toDbStatusAlert(statusAlert));
     return ResponseEntity.ok(statusAlertMapper.toStatusAlert(dbStatusAlert));
