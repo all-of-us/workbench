d23846bee448faa3168f91ed221349da13ca850f
Eric Rollins <erollins@broadinstitute.org>
2025-03-18 14:01:21 -0400

    Filter after login banners by datetime

--- ui/src/app/components/status-alert-banner-maybe.tsx
+++ ui/src/app/components/status-alert-banner-maybe.tsx
@@ -17,6 +17,17 @@ import { ToastType } from './toast-banner';
 const getMessageId = (statusAlert: StatusAlert) =>
   `status-alert-${statusAlert.statusAlertId}`;
 
+const isAlertActive = (alert: StatusAlert): boolean => {
+  const now = Date.now();
+  const startTime = alert.startTimeEpochMillis; // If null, starts immediately
+  const endTime = alert.endTimeEpochMillis; // If null, never expires
+  return (
+    alert.alertLocation === StatusAlertLocation.AFTER_LOGIN &&
+    (!startTime || now >= startTime) &&
+    (!endTime || now <= endTime)
+  );
+};
+
 const toToastMessage = (statusAlert: StatusAlert): MultiToastMessage => {
   return {
     id: getMessageId(statusAlert),
@@ -43,10 +54,7 @@ export const StatusAlertBannerMaybe = () => {
         if (statusAlerts && statusAlerts.length > 0) {
           // Process each alert and create a message if it's not dismissed
           statusAlerts.forEach((alert) => {
-            if (
-              alert.alertLocation === StatusAlertLocation.AFTER_LOGIN &&
-              alert.message
-            ) {
+            if (isAlertActive(alert)) {
               if (!isDismissed(getMessageId(alert))) {
                 const apiMessage: MultiToastMessage = toToastMessage(alert);
                 messages.push(apiMessage);
