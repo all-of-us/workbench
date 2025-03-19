42811070383bba2baf6e6740aec0c3e0309f26d4
Eric Rollins <erollins@broadinstitute.org>
2025-03-19 11:59:51 -0400

    Make table easier to read

--- ui/src/app/pages/admin/admin-banner-table.tsx
+++ ui/src/app/pages/admin/admin-banner-table.tsx
@@ -36,12 +36,12 @@ const styles = reactStyles({
   },
   colStyle: {
     fontSize: 12,
-    height: '60px',
+    height: 'auto',
     lineHeight: '1.2rem',
     padding: '.5em',
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
+    overflow: 'visible',
+    whiteSpace: 'normal',
+    wordBreak: 'normal',
   },
   titleCol: {
     width: '20%',
@@ -65,10 +65,9 @@ const styles = reactStyles({
     marginBottom: '1rem',
   },
   messageText: {
-    overflow: 'hidden',
-    textOverflow: 'ellipsis',
-    whiteSpace: 'nowrap',
     display: 'block',
+    whiteSpace: 'normal',
+    wordBreak: 'normal'
   },
 });
 
@@ -184,56 +183,53 @@ export const AdminBannerTable = (props: WithSpinnerOverlayProps) => {
           value={banners}
           emptyMessage='No active banners found'
           style={styles.tableStyle}
-          resizableColumns={true}
           columnResizeMode='expand'
-          scrollable={true}
+          scrollable={false}
           scrollHeight='flex'
         >
-          <Column
-            field='title'
-            header='Title'
-            style={{ ...styles.colStyle, ...styles.titleCol }}
-          />
+          <Column field='title' header='Title' style={styles.colStyle} sortable />
           <Column
             field='message'
             header='Message'
-            style={{ ...styles.colStyle, ...styles.messageCol }}
+            style={styles.colStyle}
             body={messageBodyTemplate}
+            sortable
           />
           <Column
             field='link'
             header='Link'
-            style={{ ...styles.colStyle, ...styles.linkCol }}
+            style={styles.colStyle}
             body={linkBodyTemplate}
+            sortable
           />
           <Column
             field='alertLocation'
             header='Location'
-            style={{ ...styles.colStyle, ...styles.locationCol }}
+            style={styles.colStyle}
             body={(rowData: StatusAlert) =>
               rowData.alertLocation === StatusAlertLocation.BEFORE_LOGIN
                 ? 'Before Login'
                 : 'After Login'
             }
+            sortable
           />
           <Column
             field='startTimeEpochMillis'
             header='Start Time'
             body={(rowData) => timestampTemplate(rowData.startTimeEpochMillis)}
-            style={{ width: '12%' }}
             sortable
           />
           <Column
             field='endTimeEpochMillis'
             header='End Time'
+            style={styles.colStyle}
             body={(rowData) => timestampTemplate(rowData.endTimeEpochMillis)}
-            style={{ width: '12%' }}
             sortable
           />
           <Column
             body={actionBodyTemplate}
             header='Actions'
-            style={{ ...styles.colStyle, ...styles.actionCol }}
+            style={styles.colStyle}
           />
         </DataTable>
       </div>
