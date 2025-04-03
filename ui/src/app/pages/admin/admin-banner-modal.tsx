bcc322397e11c290d9bb2ff0826587f8d1e391f6
Eric Rollins <erollins@broadinstitute.org>
2025-04-03 09:37:37 -0400

    Cleanup

--- ui/src/app/pages/admin/admin-banner-modal.tsx
+++ ui/src/app/pages/admin/admin-banner-modal.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react';
-import { useMemo, useState } from 'react';
+import { useState } from 'react';
 import validate from 'validate.js';
 
 import { StatusAlert, StatusAlertLocation } from 'generated/fetch';
