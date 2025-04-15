7d8859551e78af8f518fdae9c5f1f90b00d29641
Qi Wang <32771737+Qi77Qi@users.noreply.github.com>
2025-04-15 14:57:47 -0400

    update nextflow config to support batch api

--- api/src/main/webapp/static/initialize_notebook_runtime.sh
+++ api/src/main/webapp/static/initialize_notebook_runtime.sh
@@ -60,21 +60,21 @@ if [ ! -f "${nextflow_config}" ]; then
   sudo -E -u jupyter mkdir /home/jupyter/.nextflow
   cat <<EOF | sudo -E -u jupyter tee "${nextflow_config}"
 profiles {
-  gls {
-      process.executor = "google-lifesciences"
+  google-batch {
+      process.executor = "google-batch"
       process.container = "gcr.io/google-containers/ubuntu-slim:0.14"
       workDir = "${WORKSPACE_BUCKET}/workflows/nextflow-scratch"
       google.location = "us-central1"
-      google.zone = "us-central1-a"
+      google.zone = "us-central1-a" 
       google.project = "${GOOGLE_PROJECT}"
       google.enableRequesterPaysBuckets = true
-      google.lifeSciences.debug = true
-      google.lifeSciences.serviceAccountEmail = "${PET_SA_EMAIL}"
-      google.lifeSciences.network = "network"
-      google.lifeSciences.subnetwork = "subnetwork"
-      google.lifeSciences.usePrivateAddress = false
-      google.lifeSciences.copyImage = "gcr.io/google.com/cloudsdktool/cloud-sdk:alpine"
-      google.lifeSciences.bootDiskSize = "20.GB"
+      google.batch.debug = true
+      google.batch.serviceAccountEmail = "${PET_SA_EMAIL}"
+      google.batch.network = "network"
+      google.batch.subnetwork = "subnetwork"
+      google.batch.usePrivateAddress = false
+      google.batch.copyImage = "gcr.io/google.com/cloudsdktool/cloud-sdk:alpine"
+      google.batch.bootDiskSize = "20.GB"
   }
 }
 EOF
