d29805b2241564efcffeea8e992681ba5c0d3df6
Eric Rollins <erollins@broadinstitute.org>
2025-05-21 16:39:56 -0400

    Cleanup

--- ui/src/app/pages/workspace/workspace-about.tsx
+++ ui/src/app/pages/workspace/workspace-about.tsx
@@ -40,7 +40,7 @@ import {
 import { getCdrVersion } from 'app/utils/cdr-versions';
 import { fetchWithErrorModal } from 'app/utils/errors';
 import { currentWorkspaceStore } from 'app/utils/navigation';
-import { profileStore, serverConfigStore } from 'app/utils/stores';
+import { profileStore } from 'app/utils/stores';
 import { WorkspaceData } from 'app/utils/workspace-data';
 import { WorkspacePermissionsUtil } from 'app/utils/workspace-permissions';
 import { isUsingInitialCredits } from 'app/utils/workspace-utils';
