d2d6f42bc590f432435aae1aebb19b7decfb91fa
Eric Rollins <erollins@broadinstitute.org>
2025-05-27 15:59:56 -0400

    [RW-15052][risk=no] Remove extension banners. (#9280)

--- ui/src/app/components/breadcrumb.tsx
+++ ui/src/app/components/breadcrumb.tsx
@@ -7,7 +7,7 @@ import { Cohort, CohortReview, ConceptSet } from 'generated/fetch';
 
 import { cond } from '@terra-ui-packages/core-utils';
 import { dropJupyterNotebookFileSuffix } from 'app/pages/analysis/util';
-import { InvalidBillingBannerMaybe } from 'app/pages/workspace/invalid-billing-banner-maybe';
+import { InvalidBillingBanner } from 'app/pages/workspace/invalid-billing-banner';
 import {
   analysisTabName,
   analysisTabPath,
@@ -24,6 +24,7 @@ import {
 } from 'app/utils';
 import {
   MatchParams,
+  profileStore,
   RouteDataStore,
   routeDataStore,
   withStore,
@@ -296,6 +297,22 @@ const BreadcrumbLink = ({ href, ...props }) => {
   return <Link to={href} {...props} />;
 };
 
+const shouldShowInvalidBillingBanner = (workspace, profile) => {
+  if (!workspace || !profile) {
+    return false;
+  }
+
+  const { creatorUser } = workspace;
+  const isExpired = workspace.initialCredits.expirationEpochMillis < Date.now();
+  const isExhausted = workspace.initialCredits.exhausted;
+
+  return (
+    creatorUser?.givenName &&
+    creatorUser?.familyName &&
+    (isExhausted || (!workspace.initialCredits.expirationBypassed && isExpired))
+  );
+};
+
 interface Props {
   workspace: WorkspaceData;
   cohort: Cohort;
@@ -311,13 +328,17 @@ export const Breadcrumb = fp.flow(
   withCurrentConceptSet(),
   withStore(routeDataStore, 'routeData')
 )((props: Props) => {
-  const [showInvalidBillingBanner, setShowInvalidBillingBanner] =
-    useState(true);
+  const { profile } = profileStore.get();
+  const [showInvalidBillingBanner, setShowInvalidBillingBanner] = useState(
+    shouldShowInvalidBillingBanner(props.workspace, profile)
+  );
 
   useEffect(() => {
     // When user navigates to a different workspace, show the invalid billing banner even if dismissed in the past
-    setShowInvalidBillingBanner(true);
-  }, [props?.workspace]);
+    setShowInvalidBillingBanner(
+      shouldShowInvalidBillingBanner(props.workspace, profile)
+    );
+  }, [props?.workspace, profile]);
 
   const trail = (): Array<BreadcrumbData> => {
     const workspaceMatch = matchPath<MatchParams>(location.pathname, {
@@ -407,7 +428,9 @@ export const Breadcrumb = fp.flow(
   return (
     <>
       {showInvalidBillingBanner && (
-        <InvalidBillingBannerMaybe
+        <InvalidBillingBanner
+          profile={profile}
+          workspace={props.workspace}
           onClose={() => setShowInvalidBillingBanner(false)}
         />
       )}
---
+++ ui/src/app/pages/workspace/invalid-billing-banner.spec.tsx
@@ -0,0 +1,125 @@
+import '@testing-library/jest-dom';
+
+import * as React from 'react';
+import { MemoryRouter } from 'react-router-dom';
+
+import { ProfileApi, User } from 'generated/fetch';
+
+import { screen } from '@testing-library/dom';
+import { render } from '@testing-library/react';
+import { InvalidBillingBanner } from 'app/pages/workspace/invalid-billing-banner';
+import { registerApiClient } from 'app/services/swagger-fetch-clients';
+import { currentWorkspaceStore } from 'app/utils/navigation';
+import { serverConfigStore } from 'app/utils/stores';
+
+import defaultServerConfig from 'testing/default-server-config';
+import {
+  ProfileApiStub,
+  ProfileStubVariables,
+} from 'testing/stubs/profile-api-stub';
+import { workspaceDataStub } from 'testing/stubs/workspaces';
+
+// Mock ReactDOM.createPortal since ToastBanner uses portals
+jest.mock('react-dom', () => {
+  return {
+    ...jest.requireActual('react-dom'),
+    createPortal: (element) => element,
+  };
+});
+
+describe('InvalidBillingBanner', () => {
+  const warningThresholdDays = 5; // arbitrary
+  const me: User = {
+    userName: ProfileStubVariables.PROFILE_STUB.username,
+    givenName: 'Me',
+    familyName: 'Myself',
+  };
+  const someOneElse: User = {
+    userName: 'someOneElse@fake-research-aou.org',
+    givenName: 'Someone',
+    familyName: 'Else',
+  };
+
+  const component = () => {
+    const workspace = currentWorkspaceStore.getValue();
+    const rendered = render(
+      <MemoryRouter>
+        <InvalidBillingBanner
+          onClose={jest.fn()}
+          workspace={workspace}
+          profile={{
+            ...ProfileStubVariables.PROFILE_STUB,
+            ...me,
+          }}
+        />
+      </MemoryRouter>
+    );
+    return rendered;
+  };
+
+  const expectEditWorkspaceButtonExists = () =>
+    screen.getAllByRole('button', { name: /edit workspace/i });
+
+  const expectEditWorkspaceButtonDoesNotExist = () =>
+    expect(
+      screen.queryByRole('button', { name: /edit workspace/i })
+    ).not.toBeInTheDocument();
+
+  beforeEach(() => {
+    registerApiClient(ProfileApi, new ProfileApiStub());
+
+    serverConfigStore.set({
+      config: {
+        ...defaultServerConfig,
+        initialCreditsExpirationWarningDays: warningThresholdDays,
+      },
+    });
+  });
+
+  const setupWorkspace = (ownedByMe: boolean) => {
+    const workspace = {
+      ...workspaceDataStub,
+      namespace: 'test-namespace',
+      terraName: 'test-workspace',
+      creatorUser: ownedByMe ? me : someOneElse,
+    };
+    currentWorkspaceStore.next(workspace);
+  };
+
+  it('should show owner version of banner', async () => {
+    const ownedByMe = true;
+    setupWorkspace(ownedByMe);
+
+    component();
+
+    await screen.findByText('This workspace is out of initial credits');
+    screen.getByText(
+      new RegExp(
+        'your initial credits have run out\\. ' +
+          'to use the workspace, a valid billing account needs to be added \\. ' +
+          'to learn more about establishing a billing account, read "" on the user support hub\\.',
+        'i'
+      )
+    );
+    expectEditWorkspaceButtonExists();
+  });
+
+  it('should show non-creator version of the banner', async () => {
+    const ownedByMe = false;
+    setupWorkspace(ownedByMe);
+
+    component();
+
+    await screen.findByText('This workspace is out of initial credits');
+    screen.getByText(
+      new RegExp(
+        "this workspace creator's initial credits have run out\\. " +
+          'this workspace was created by someone else\\. ' +
+          'to use the workspace, a valid billing account needs to be added\\. ' +
+          'to learn more about establishing a billing account, read "" on the user support hub\\.',
+        'i'
+      )
+    );
+    expectEditWorkspaceButtonDoesNotExist();
+  });
+});
