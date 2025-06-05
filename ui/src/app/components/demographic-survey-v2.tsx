862e9bb2a918afdb16dd0c2b383fbc393993b973
Miguel Silva <msilva@broadinstitute.org>
2025-06-05 10:06:23 -0400

    - shifted validation logic out of demographic-survey-v2 component and into xxxx-validation sister file. - added unit tests for form validation.

--- ui/src/app/components/demographic-survey-v2.tsx
+++ ui/src/app/components/demographic-survey-v2.tsx
@@ -1,10 +1,8 @@
 import * as React from 'react';
 import { useEffect, useState } from 'react';
 import * as fp from 'lodash/fp';
-import validate from 'validate.js';
 
 import {
-  DemographicSurveyV2,
   EducationV2,
   EthnicCategory,
   GenderIdentityV2,
@@ -35,6 +33,14 @@ import {
   YEAR_OF_BIRTH,
 } from 'app/utils/constants';
 
+import {
+  maxYear,
+  minYear,
+  NONE_FULLY_DESCRIBE_MAX_LENGTH,
+  OTHER_DISABILITY_MAX_LENGTH,
+  SURVEY_COMMENTS_MAX_LENGTH,
+  validateDemographicSurvey_V2,
+} from './demographic-survey-v2-validation';
 import {
   CheckBox,
   NumberInput,
@@ -78,8 +84,6 @@ export const questionsIndex = {
   [DEMOGRAPHIC_SURVEY_COMMENTS]: 15,
 };
 
-const maxYear = new Date().getFullYear();
-const minYear = maxYear - 125;
 const styles = reactStyles({
   question: {
     fontSize: 18,
@@ -90,19 +94,8 @@ const styles = reactStyles({
   answer: { margin: '0.0rem 0.375rem', color: colors.primary },
 });
 
-const OTHER_DISABILITY_MAX_LENGTH = 200;
-const MAX_CHARACTERS_VALIDATION_MESSAGE =
-  'can have no more than %{count} characters.';
 const NONE_FULLY_DESCRIBE = 'None of these fully describe me';
-const NONE_FULLY_DESCRIBE_MAX_LENGTH = 200;
-const NONE_FULLY_DESCRIBE_VALIDATION = {
-  length: {
-    maximum: NONE_FULLY_DESCRIBE_MAX_LENGTH,
-    tooLong: MAX_CHARACTERS_VALIDATION_MESSAGE,
-  },
-};
 const NONE_FULLY_DESCRIBE_PLACEHOLDER = 'Please specify (optional)';
-const SURVEY_COMMENTS_MAX_LENGTH = 1000;
 const TWO_SPIRIT_DISABLED_TEXT =
   'unique to people of American Indian and Alaska Native ' +
   'ancestry. If this applies to you, please update your selection in the ' +
@@ -111,113 +104,6 @@ const TWO_SPIRIT_IDENTITY_DISABLED_TEXT =
   'Two Spirit is an identity ' + TWO_SPIRIT_DISABLED_TEXT;
 const TWO_SPIRIT_SEXUAL_ORIENTATION_DISABLED_TEXT =
   'Two Spirit is an orientation ' + TWO_SPIRIT_DISABLED_TEXT;
-
-const validateDemographicSurvey = (demographicSurvey: DemographicSurveyV2) => {
-  validate.validators.nullBoolean = (v) =>
-    v === true || v === false || v === null
-      ? undefined
-      : 'value must be selected';
-
-  // Not well documented, but in Validate.JS, if your message is prefixed with a caret,
-  // the field name will not be included with the message:
-  // https://validatejs.org/docs/validate.html#section-47
-  // https://github.com/ansman/validate.js/issues/68
-  const yearOfBirth = demographicSurvey.yearOfBirthPreferNot
-    ? {}
-    : {
-        yearOfBirth: {
-          presence: {
-            allowEmpty: false,
-            message:
-              '^You must either fill in your year of birth or check the corresponding ' +
-              '"Prefer not to answer" checkbox ',
-          },
-          numericality: {
-            onlyInteger: true,
-            greaterThanOrEqualTo: minYear,
-            lessThanOrEqualTo: maxYear,
-          },
-        },
-      };
-
-  const validationCheck = {
-    ethnicCategories: { presence: { allowEmpty: false } },
-    genderIdentities: { presence: { allowEmpty: false } },
-    sexAtBirth: { presence: { allowEmpty: false } },
-    sexualOrientations: { presence: { allowEmpty: false } },
-    disabilityHearing: {
-      presence: {
-        allowEmpty: false,
-        message: "^ Difficulty hearing can't be blank",
-      },
-    },
-    disabilitySeeing: {
-      presence: {
-        allowEmpty: false,
-        message: "^ Difficulty seeing can't be blank",
-      },
-    },
-    disabilityConcentrating: {
-      presence: {
-        allowEmpty: false,
-        message: "^ Difficulty concentrating can't be blank",
-      },
-    },
-    disabilityWalking: {
-      presence: {
-        allowEmpty: false,
-        message: "^ Difficulty walking can't be blank",
-      },
-    },
-    disabilityDressing: {
-      presence: {
-        allowEmpty: false,
-        message: "^ Difficulty dressing can't be blank",
-      },
-    },
-    disabilityErrands: {
-      presence: {
-        allowEmpty: false,
-        message: "^ Difficulty doing errands can't be blank",
-      },
-    },
-    disabilityOtherText: {
-      length: {
-        maximum: OTHER_DISABILITY_MAX_LENGTH,
-        tooLong: MAX_CHARACTERS_VALIDATION_MESSAGE,
-      },
-    },
-    ...yearOfBirth,
-    education: { presence: { allowEmpty: false } },
-    disadvantaged: { presence: { allowEmpty: false } },
-    surveyComments: {
-      length: {
-        maximum: SURVEY_COMMENTS_MAX_LENGTH,
-        tooLong: MAX_CHARACTERS_VALIDATION_MESSAGE,
-      },
-    },
-    ...[
-      'ethnicityAiAnOtherText',
-      'ethnicityAsianOtherText',
-      'ethnicityBlackOtherText',
-      'ethnicityHispanicOtherText',
-      'ethnicityMeNaOtherText',
-      'ethnicityNhPiOtherText',
-      'ethnicityWhiteOtherText',
-      'ethnicityOtherText',
-      'genderOtherText',
-      'sexAtBirthOtherText',
-      'orientationOtherText',
-    ].reduce(
-      (otherTextValidations, k) => ({
-        ...otherTextValidations,
-        [k]: NONE_FULLY_DESCRIBE_VALIDATION,
-      }),
-      {}
-    ),
-  };
-  return validate(demographicSurvey, validationCheck);
-};
 
 interface DemographicSurveyProps extends WithSpinnerOverlayProps {
   onError: (any) => void;
@@ -266,7 +152,7 @@ export const DemographicSurvey = fp.flow(withProfileErrorModal)(
     ];
 
     useEffect(() => {
-      onError(validateDemographicSurvey(survey));
+      onError(validateDemographicSurvey_V2(survey));
     }, [profile]);
 
     useEffect(() => {
