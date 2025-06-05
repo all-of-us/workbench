24f080bfb0f89cfedb33137173c51f6bd1e8e203
Tarek M. Ahmed <tarekahmed@verily.com>
2025-06-05 09:28:06 -0400

    RW-13692 Add more assertions in test

--- api/src/test/java/org/pmiops/workbench/initialcredits/InitialCreditsServiceTest.java
+++ api/src/test/java/org/pmiops/workbench/initialcredits/InitialCreditsServiceTest.java
@@ -1,9 +1,7 @@
 package org.pmiops.workbench.initialcredits;
 
 import static com.google.common.truth.Truth.assertThat;
-import static org.junit.jupiter.api.Assertions.assertEquals;
-import static org.junit.jupiter.api.Assertions.assertNull;
-import static org.junit.jupiter.api.Assertions.assertThrows;
+import static org.junit.jupiter.api.Assertions.*;
 import static org.mockito.ArgumentMatchers.any;
 import static org.mockito.ArgumentMatchers.anyDouble;
 import static org.mockito.ArgumentMatchers.anyLong;
@@ -32,6 +30,7 @@ import java.util.Collections;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.Optional;
 import java.util.Set;
 import java.util.stream.Stream;
 import org.junit.jupiter.api.AfterEach;
@@ -98,6 +97,7 @@ public class InitialCreditsServiceTest {
   @Autowired WorkspaceFreeTierUsageDao workspaceFreeTierUsageDao;
 
   @Autowired private TaskQueueService taskQueueService;
+  @Autowired private VwbUserPodDao vwbUserPodDao;
 
   private static WorkbenchConfig workbenchConfig;
 
@@ -123,7 +123,6 @@ public class InitialCreditsServiceTest {
               .plusSeconds((warningPeriodDays + 1L) * 24 * 60 * 60));
 
   private DbWorkspace workspace;
-  @Autowired private VwbUserPodDao vwbUserPodDao;
 
   @TestConfiguration
   @Import({InitialCreditsService.class, WorkspaceInitialCreditUsageService.class})
@@ -1260,6 +1259,15 @@ public class InitialCreditsServiceTest {
     final DbWorkspaceFreeTierUsage usage2 = workspaceFreeTierUsageDao.findOneByWorkspace(ws2);
     assertThat(usage2.getUser()).isEqualTo(user2);
     assertWithinBillingTolerance(usage2.getCost(), cost2);
+
+    // Confirm VwbPod costs are correctly applied
+    Optional<DbVwbUserPod> pod1 = vwbUserPodDao.findById(vwbPod1.getVwbUserPodId());
+    assertThat(pod1).isPresent();
+    assertWithinBillingTolerance(vwbCost1, pod1.get().getCost());
+    Optional<DbVwbUserPod> pod2 = vwbUserPodDao.findById(vwbPod2.getVwbUserPodId());
+    assertThat(pod2).isPresent();
+    assertWithinBillingTolerance(vwbCost2, pod2.get().getCost());
+
   }
 
   private void assertSingleWorkspaceTestDbState(
