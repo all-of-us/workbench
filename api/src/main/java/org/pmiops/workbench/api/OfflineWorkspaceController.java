28dba6bb0a755aa05bf2661bc8a368cb94155f68
Eric Rollins <erollins@broadinstitute.org>
2025-07-18 09:25:03 -0400

    Temporary controller

---
+++ api/src/main/java/org/pmiops/workbench/api/OfflineWorkspaceController.java
@@ -0,0 +1,61 @@
+package org.pmiops.workbench.api;
+
+import com.google.common.base.Stopwatch;
+import jakarta.inject.Provider;
+import java.util.List;
+import org.pmiops.workbench.cloudtasks.TaskQueueService;
+import org.pmiops.workbench.workspaces.WorkspaceService;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.RestController;
+
+/** Handles offline / cron-based API requests related to workspace management. */
+@RestController
+public class OfflineWorkspaceController implements OfflineWorkspaceApiDelegate {
+  private final Logger logger = LoggerFactory.getLogger(OfflineWorkspaceController.class);
+
+  private final Provider<Stopwatch> stopwatchProvider;
+  private final TaskQueueService taskQueueService;
+  private final WorkspaceService workspaceService;
+
+  @Autowired
+  public OfflineWorkspaceController(
+      Provider<Stopwatch> stopwatchProvider,
+      TaskQueueService taskQueueService,
+      WorkspaceService workspaceService) {
+    this.stopwatchProvider = stopwatchProvider;
+    this.taskQueueService = taskQueueService;
+    this.workspaceService = workspaceService;
+  }
+
+  /**
+   * Audits GCP access for all users in the database.
+   *
+   * <p>This API method is called by a cron job and is not part of our normal user-facing surface.
+   */
+  @Override
+  public ResponseEntity<Void> cleanupMissingWorkspaces() {
+    List<String> activeNamespaces = workspaceService.getOrphanedWorkspaceNamespacesAsService();
+    //    Stopwatch stopwatch = stopwatchProvider.get().start();
+    //    List<Long> userIds = userService.getAllUserIdsWithActiveInitialCredits();
+    //    Duration elapsed = stopwatch.stop().elapsed();
+    //    logger.info(
+    //        String.format(
+    //            "checkInitialCreditsExpiration: Retrieved %d user IDs from DB in %s",
+    //            userIds.size(), formatDurationPretty(elapsed)));
+    //
+    //    stopwatch.reset().start();
+    //    List<String> taskIds =
+    // taskQueueService.groupAndPushCheckInitialCreditsExpirationTasks(userIds);
+    //    elapsed = stopwatch.stop().elapsed();
+    //    logger.info(
+    //        String.format(
+    //            "checkInitialCreditsExpiration: Grouped and pushed %d user IDs into %d tasks in
+    // %s",
+    //            userIds.size(), taskIds.size(), formatDurationPretty(elapsed)));
+
+    return ResponseEntity.noContent().build();
+  }
+}
