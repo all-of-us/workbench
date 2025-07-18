7a513dc858b5108767728abd936dc3b10da048f7
Eric Rollins <erollins@broadinstitute.org>
2025-07-18 15:10:20 -0400

    Cleanup

--- api/src/main/java/org/pmiops/workbench/api/OfflineWorkspaceController.java
+++ api/src/main/java/org/pmiops/workbench/api/OfflineWorkspaceController.java
@@ -22,31 +22,10 @@ public class OfflineWorkspaceController implements OfflineWorkspaceApiDelegate {
     this.workspaceService = workspaceService;
   }
 
-  /**
-   * Audits GCP access for all users in the database.
-   *
-   * <p>This API method is called by a cron job and is not part of our normal user-facing surface.
-   */
   @Override
   public ResponseEntity<Void> cleanupOrphanedWorkspaces() {
     List<String> orphanedNamespaces = workspaceService.getOrphanedWorkspaceNamespacesAsService();
-    //    Stopwatch stopwatch = stopwatchProvider.get().start();
-    //    List<Long> userIds = userService.getAllUserIdsWithActiveInitialCredits();
-    //    Duration elapsed = stopwatch.stop().elapsed();
-    //    logger.info(
-    //        String.format(
-    //            "checkInitialCreditsExpiration: Retrieved %d user IDs from DB in %s",
-    //            userIds.size(), formatDurationPretty(elapsed)));
-    //
-    //    stopwatch.reset().start();
-    //    List<String> taskIds =
     taskQueueService.groupAndPushCleanupOrphanedWorkspacesTasks(orphanedNamespaces);
-    //    elapsed = stopwatch.stop().elapsed();
-    //    logger.info(
-    //        String.format(
-    //            "checkInitialCreditsExpiration: Grouped and pushed %d user IDs into %d tasks in
-    // %s",
-    //            userIds.size(), taskIds.size(), formatDurationPretty(elapsed)));
 
     return ResponseEntity.noContent().build();
   }
