7c04766c8f1c7e63d5007f7fe1b2294043c8cd37
Marcus Talbott <mtalbott@broadinstitute.org>
2025-07-21 14:44:13 -0400

    add workspace user cache table, unused

---
+++ api/src/main/java/org/pmiops/workbench/db/model/DbWorkspaceUserCache.java
@@ -0,0 +1,84 @@
+package org.pmiops.workbench.db.model;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import java.sql.Timestamp;
+import java.util.Objects;
+
+@Entity
+@Table(name = "workspace_user_cache")
+public class DbWorkspaceUserCache {
+  private long id;
+  private long workspaceId;
+  private long userId;
+  private Timestamp lastUpdated;
+
+  @Id
+  @GeneratedValue(strategy = GenerationType.IDENTITY)
+  public long getId() {
+    return id;
+  }
+
+  public DbWorkspaceUserCache setId(long id) {
+    this.id = id;
+    return this;
+  }
+
+  @Column(name = "workspace_id")
+  public long getWorkspaceId() {
+    return workspaceId;
+  }
+
+  public DbWorkspaceUserCache setWorkspaceId(long workspaceId) {
+    this.workspaceId = workspaceId;
+    return this;
+  }
+
+  @Column(name = "user_id")
+  public long getUserId() {
+    return userId;
+  }
+
+  public DbWorkspaceUserCache setUserId(long userId) {
+    this.userId = userId;
+    return this;
+  }
+
+  @Column(name = "last_updated")
+  public Timestamp getLastUpdated() {
+    return lastUpdated;
+  }
+
+  public DbWorkspaceUserCache setLastUpdated(Timestamp lastUpdated) {
+    this.lastUpdated = lastUpdated;
+    return this;
+  }
+
+  @Override
+  public boolean equals(Object o) {
+    if (this == o) return true;
+    if (o == null || getClass() != o.getClass()) return false;
+    DbWorkspaceUserCache that = (DbWorkspaceUserCache) o;
+    return id == that.id
+        && workspaceId == that.workspaceId
+        && userId == that.userId
+        && Objects.equals(lastUpdated, that.lastUpdated);
+  }
+
+  @Override
+  public int hashCode() {
+    return Objects.hash(id, workspaceId, userId, lastUpdated);
+  }
+
+  public DbWorkspaceUserCache() {}
+
+  public DbWorkspaceUserCache(long workspaceId, long userId, Timestamp lastUpdated) {
+    this.workspaceId = workspaceId;
+    this.userId = userId;
+    this.lastUpdated = lastUpdated;
+  }
+}
