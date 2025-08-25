088aba16e1b1f3a593504109ec147bbf7260b810
Marcus Talbott <mtalbott@broadinstitute.org>
2025-08-25 12:07:49 -0400

    fetch score from Absorb and enforce a passing grade on enrollments

--- api/src/main/java/org/pmiops/workbench/absorb/Enrollment.java
+++ api/src/main/java/org/pmiops/workbench/absorb/Enrollment.java
@@ -7,16 +7,19 @@ import org.apache.commons.lang3.builder.ToStringBuilder;
 public class Enrollment {
   public final String courseId;
   @Nullable public final Instant completionTime;
+  @Nullable public final Integer score;
 
-  public Enrollment(String courseId, @Nullable Instant dateCompleted) {
+  public Enrollment(String courseId, @Nullable Instant dateCompleted, @Nullable Integer score) {
     this.courseId = courseId;
     this.completionTime = dateCompleted;
+    this.score = score;
   }
 
   public String toString() {
     return new ToStringBuilder(this)
         .append("courseId", courseId)
         .append("completionTime", completionTime)
+        .append("score", score)
         .toString();
   }
 }
