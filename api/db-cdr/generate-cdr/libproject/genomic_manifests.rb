73288bd5cd42f53f4d534703eb3fdd9629f9f173
Tarek M. Ahmed <tarekahmed@verily.com>
2025-09-22 10:39:00 -0400

    RW-16059 Enable vwb file transfers

--- api/db-cdr/generate-cdr/libproject/genomic_manifests.rb
+++ api/db-cdr/generate-cdr/libproject/genomic_manifests.rb
@@ -162,8 +162,8 @@ def parse_input_manifest(filename)
       unless missing_keys.empty? and extra_keys.empty?
         raise ArgumentError.new(
           "bad section definition '#{section_key}'\n" +
-            "missing required keys: #{missing_keys.join(',')}\n" +
-            "had unknown keys: #{extra_keys.join(',')}")
+            "missing required keys: #{missing_keys.to_a.join(',')}\n" +
+            "had unknown keys: #{extra_keys.to_a.join(',')}")
       end
 
       section_value.each do |inner_k, inner_v|
