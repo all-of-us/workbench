f2c097cdce48c4e961d050a310d601a81b3ea944
Marc Talbott <mtalbott@broadinstitute.org>
2025-11-03 17:40:35 -0500

    workspace user table has different name in RWB and WRD

--- api/src/main/java/org/pmiops/workbench/reporting/ReportingTableService.java
+++ api/src/main/java/org/pmiops/workbench/reporting/ReportingTableService.java
@@ -67,6 +67,7 @@ public class ReportingTableService {
   private static final String WORKSPACE_FREE_TIER_USAGE_TABLE_NAME = "workspace_free_tier_usage";
   private static final String WORKSPACE_TABLE_NAME = "workspace";
   private static final String WORKSPACE_USER_CACHE_TABLE_NAME = "workspace_user_cache";
+  private static final String WORKSPACE_USER_CACHE_BQ_TABLE_NAME = "workspace_user";
 
   private final ReportingQueryService reportingQueryService;
 
@@ -221,8 +222,8 @@ public class ReportingTableService {
 
   public final ReportingTableParams<ReportingWorkspaceUser> workspaceUser() {
     return new ReportingTableParams<>(
-        WORKSPACE_USER_CACHE_TABLE_NAME,
-        batchSize(WORKSPACE_USER_CACHE_TABLE_NAME),
+        WORKSPACE_USER_CACHE_BQ_TABLE_NAME,
+        batchSize(WORKSPACE_USER_CACHE_BQ_TABLE_NAME),
         WorkspaceUserColumnValueExtractor::values,
         reportingQueryService::getWorkspaceUserBatch,
         () -> reportingQueryService.getTableRowCount(WORKSPACE_USER_CACHE_TABLE_NAME));
