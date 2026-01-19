16f2bca40ecc776d41ea3a01088878873625ef37
Tarek M. Ahmed <tarekahmed@verily.com>
2026-01-19 09:52:36 -0500

    RW-16756 lint

--- api/src/main/java/org/pmiops/workbench/access/AccessSyncServiceImpl.java
+++ api/src/main/java/org/pmiops/workbench/access/AccessSyncServiceImpl.java
@@ -29,7 +29,6 @@ import org.pmiops.workbench.model.Institution;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.dao.DataIntegrityViolationException;
 import org.springframework.stereotype.Service;
-import org.springframework.transaction.annotation.Transactional;
 
 @Service
 public class AccessSyncServiceImpl implements AccessSyncService {
