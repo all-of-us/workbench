f7452b1437e7300bb98117ca44e6fa7e09b2d841
alaidaa <al.al.aidaroos@vumc.org>
2026-04-01 17:16:53 -0500

    comment removed

--- ui/src/app/components/migration/FolderSelection.tsx
+++ ui/src/app/components/migration/FolderSelection.tsx
@@ -35,7 +35,7 @@ const styles = reactStyles({
 
 interface Props {
   folders: string[];
-  onContinue: (folders: string[]) => Promise<void>; // 👈 important change
+  onContinue: (folders: string[]) => Promise<void>;
   workspaceName?: string;
   onBack?: () => void;
   onClose?: () => void;
