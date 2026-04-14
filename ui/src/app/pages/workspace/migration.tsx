70155bd4b3b418f7a79ee545ae8b1585e843ed7e
alaidaa <al.al.aidaroos@vumc.org>
2026-04-14 14:55:01 -0500

    lint fix

--- ui/src/app/pages/workspace/migration.tsx
+++ ui/src/app/pages/workspace/migration.tsx
@@ -111,7 +111,8 @@ export const MigrationPage = withCurrentWorkspace()(({ workspace }: Props) => {
       {!hasAcceptedTos && (
         <VwbImportantBanner
           title='Important'
-          message='Before starting migration, log into Researcher Workbench 2.0 to agree to the terms of service. You only need to do this once.'
+          message={`Before starting migration, log into Researcher Workbench 2.0 
+to agree to the terms of service. You only need to do this once.`}
           actionText='Open Verily Workbench'
           onAction={() => window.open(environment.vwbUiUrl, '_blank')}
         />
