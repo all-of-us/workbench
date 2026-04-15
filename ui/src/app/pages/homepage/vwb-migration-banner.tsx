77d17eec118f89d656cd3a7f791b6f8d2b2bee1f
Will Dolbeer <william.dolbeer@vumc.org>
2026-04-15 09:43:38 -0400

    update homepage banner styles

--- ui/src/app/pages/homepage/vwb-migration-banner.tsx
+++ ui/src/app/pages/homepage/vwb-migration-banner.tsx
@@ -20,37 +20,38 @@ export const VwbMigrationBanner = () => {
       style={{
         background: colors.banner,
         margin: '1.5rem 3% 0 3%',
-        padding: '1.5rem',
-        minWidth: '1450px',
+        padding: '1rem',
+        height: '450px',
+        width: '1350px',
       }}
     >
       {/* LEFT CONTENT */}
-      <FlexColumn style={{ color: colors.primary, flex: 1.15 }}>
+      <FlexColumn style={{ color: colors.primary, flex: 1 }}>
         <div>
           {/* TITLE */}
           <Header
             style={{
               fontWeight: 600,
-              fontSize: '32px',
-              lineHeight: '40px',
+              fontSize: '24px',
+              lineHeight: '32px',
               margin: 0,
             }}
           >
-            Migrate your Workspaces to Researcher Workbench 2.0 by {'<date>'}
+            Migrate your Workspaces to Researcher Workbench 2.0
+            {/*TODO uncomment whe we have an end date*/}
+            {/*by {'<date>'}*/}
           </Header>
 
           {/* DESCRIPTION */}
-          <SmallHeader style={{ marginTop: '1.5rem' }}>
+          <SmallHeader style={{ fontSize: '14px', lineHeight: '1.25rem', marginTop: '1rem' }}>
             You are now able to migrate your workspaces to Researcher Workbench
             2.0, powered on{' '}
             <span style={{ fontWeight: 700 }}>
               <AoU /> Verily Pre
             </span>
             . Review your current workspaces to decide which ones you want to
-            migrate. Any workspaces you don’t migrate will be archived.
-            <br />
-            <br />
-            Any workspace associated with CDR v7 and v8 can be migrated. Review
+            migrate. Any workspaces you don’t migrate will be archived. Any
+            workspace associated with CDR v7 and v8 can be migrated. Review
             the detailed migration instructions and tips on the{' '}
             <StyledExternalLink
               href={VWB_USER_SUPPORT_HUB_URL}
@@ -63,14 +64,14 @@ export const VwbMigrationBanner = () => {
           </SmallHeader>
 
           {/* PRE-REQUISITES */}
-          <div style={{ marginTop: '1.5rem' }}>
+          <div style={{ fontSize: '14px', lineHeight: '1.25rem', marginTop: '1rem' }}>
             <SmallHeader style={{ fontWeight: 600 }}>
               Before you begin workspace migration, you must:
             </SmallHeader>
 
-            <ol style={{ marginTop: '0.75rem', paddingLeft: '1.5rem' }}>
+            <ol style={{ marginTop: 0, paddingLeft: '1.5rem' }}>
               {/* STEP 1 */}
-              <li style={{ marginBottom: '0.5rem' }}>
+              <li style={{ marginBottom: 0 }}>
                 Log into Researcher Workbench 2.0 to accept the terms of
                 service.{' '}
                 <StyledExternalLink
@@ -106,7 +107,7 @@ export const VwbMigrationBanner = () => {
 
           {/* CTA BUTTON */}
           <Button
-            style={{ marginTop: '1.5rem', height: '45px', width: '300px' }}
+            style={{ marginTop: '1rem', height: '45px', width: '300px' }}
             onClick={() => navigate(['workspaces'])}
           >
             Go to My Workspaces
@@ -119,7 +120,7 @@ export const VwbMigrationBanner = () => {
       </FlexColumn>
 
       {/* RIGHT CAROUSEL */}
-      <FlexColumn style={{ marginLeft: '1.5rem', flex: 1 }}>
+      <FlexColumn style={{ marginLeft: '1.5rem', flex: 1.25 }}>
         <img
           src={vwbMigrationImage}
           alt='Workspace migration'
